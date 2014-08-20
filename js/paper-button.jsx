08838a196336b5d3a396b12e503cc9255086a39b
Mark Ragunton <mark@call-em-all.com>
2014-08-20 11:03:54 -0500

    Added 'fab' component.

--- js/paper-button.jsx
+++ js/paper-button.jsx
@@ -10,6 +10,7 @@ var PaperButton = React.createClass({
 
   propTypes: {
     label: React.PropTypes.string.isRequired,
+    size: React.PropTypes.string,
     type: React.PropTypes.oneOf(['raised', 'flat', 'fab']),
     onClick: React.PropTypes.func.isRequired
   },
@@ -18,13 +19,18 @@ var PaperButton = React.createClass({
 
   getDefaultProps: function() {
     return {
+      size: 'regular',
       type: "raised"
     };
   },
 
   render: function() {
-    var classes = this.getClasses('mui-paper-button'),
-      zDepth;
+    var classes = this.getClasses('mui-paper-button', {
+      'mui-fab': ((this.props.type === 'fab') && (this.props.size === 'regular')),
+      'mui-fab-mini': ((this.props.type === 'fab') && (this.props.size === 'mini'))
+    }),
+      zDepth,
+      circle;
 
     switch(this.props.type) {
       case 'flat':
@@ -35,12 +41,13 @@ var PaperButton = React.createClass({
         break;
       case 'fab':
         zDepth = 2;
+        circle = true;
         break;
     }
 
     return (
       <div className={classes} onClick={this._onClick}>
-        <Paper zDepth={zDepth}>
+        <Paper zDepth={zDepth} circle={circle}>
             {this.props.label}
         </Paper>
       </div>
--- js/paper.jsx
+++ js/paper.jsx
@@ -11,20 +11,23 @@ var Paper = React.createClass({
 
   propTypes: {
     zDepth: React.PropTypes.number,
-    rounded: React.PropTypes.bool
+    rounded: React.PropTypes.bool,
+    circle: React.PropTypes.bool
   },
 
   getDefaultProps: function() {
     return {
       zDepth: 1,
-      rounded: true
+      rounded: true,
+      circle: false
     };
   },
 
   render: function() {
     var insideClasses = 'mui-paper-container mui-z-depth-bottom mui-z-depth-' + this.props.zDepth,
       classes = this.getClasses('mui-paper mui-z-depth-top mui-z-depth-' + this.props.zDepth, {
-        'mui-rounded': this.props.rounded
+        'mui-rounded': this.props.rounded,
+        'mui-circle': this.props.circle
       });
 
     return (
