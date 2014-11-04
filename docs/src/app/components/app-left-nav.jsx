b0d4bb74cb37102918aa9ab9207012359fd9bae8
Hai Nguyen <hai@call-em-all.com>
2014-11-03 18:27:48 -0600

    modified left nav navigation

--- docs/src/app/components/app-left-nav.jsx
+++ docs/src/app/components/app-left-nav.jsx
@@ -4,44 +4,36 @@
 
 var React = require('react'),
   Router = require('react-router'),
-  Navigation = Router.Navigation,
   mui = require('mui');
 
 var AppLeftNav = React.createClass({
 
-  mixins: [Navigation],
+  mixins: [Router.Navigation],
 
   getInitialState: function() {
     return {
-      menuItems: [
-        { payload: 'get-started', text: 'Get Started' },
-        { payload: 'css-framework', text: 'CSS Framework' },
-        { payload: 'components', text: 'Components' },
+      selectedIndex: null
+    };
+  },
+
+  render: function() {
+    var header = <div className="logo" onClick={this._onHeaderClick}>material ui</div>,
+      menuItems = [
+        { route: 'get-started', text: 'Get Started' },
+        { route: 'css-framework', text: 'CSS Framework' },
+        { route: 'components', text: 'Components' },
         { type: mui.MenuItem.Types.SUBHEADER, text: 'Resources' },
         { type: mui.MenuItem.Types.LINK, payload: 'https://github.com/callemall/material-ui', text: 'GitHub' },
         { type: mui.MenuItem.Types.LINK, payload: 'http://facebook.github.io/react', text: 'React' },
         { type: mui.MenuItem.Types.LINK, payload: 'https://www.google.com/design/spec/material-design/introduction.html', text: 'Material Design' }
-      ]
-    }
-  },
-
-  componentWillMount: function() {
-    this._setSelectedIndex(this.props.url);
-  },
-
-  componentWillReceiveProps: function(newProps) {
-    this._setSelectedIndex(newProps.url);
-  },
-
-  render: function() {
-    var header = <div className="logo" onClick={this._onHeaderClick}>material ui</div>;
+      ];
 
     return (
       <mui.LeftNav 
         ref="leftNav"
         isInitiallyOpen={false}
         header={header}
-        menuItems={this.state.menuItems}
+        menuItems={menuItems}
         selectedIndex={this.state.selectedIndex}
         onChange={this._onLeftNavChange} />
     );
@@ -51,23 +43,13 @@ var AppLeftNav = React.createClass({
     this.refs.leftNav.toggle();
   },
 
-  _setSelectedIndex: function(url) {
-    var item;
-
-    for (var i = 0; i < this.state.menuItems.length; i++) {
-      item = this.state.menuItems[i];
-
-      //only match the root part of the url
-      if (url && item.payload && item.payload.url.split('/')[0] === url.split('/')[0]) {
-        if (i !== this.state.selectedIndex) this.setState({ selectedIndex: i});
-        return;
-      }
-    };
-
-    this.setState({ selectedIndex: null});
+  _onLeftNavChange: function(e, key, payload) {
+    this.transitionTo(payload.route);
+    this.setState({ selectedIndex: key });
   },
 
   _onHeaderClick: function() {
+    this.setState({ selectedIndex: null });
     this.transitionTo('root');
     this.refs.leftNav.close();
   }
