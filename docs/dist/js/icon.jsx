42b87a2b4d312999db48ff7f6e61cd1412cc0dfd
Hai Nguyen <hai@call-em-all.com>
2014-11-07 17:20:33 -0600

    Added Material UI custom icons

--- docs/dist/js/icon.jsx
+++ docs/dist/js/icon.jsx
@@ -15,7 +15,9 @@ var Icon = React.createClass({
 	},
 
 	render: function() {
-		var classes = this.getClasses('mui-icon mdfi_' + this.props.icon.replace(/-/g, '_'));
+		var isMuiCustomIcon = this.props.icon.indexOf('mui-icon') > -1,
+      iconClassName = isMuiCustomIcon ? this.props.icon : 'mdfi_' + this.props.icon.replace(/-/g, '_'),
+      classes = this.getClasses('mui-icon ' + iconClassName);
 
 		return (
 			<span className={classes} onClick={this._onClick}>
