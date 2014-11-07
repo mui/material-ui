47e1cc3772aa2228b789dc6b904eadcf7cf35371
Hai Nguyen <hai@call-em-all.com>
2014-11-07 10:53:44 -0600

    Added all material design icons

--- docs/dist/js/icon.jsx
+++ docs/dist/js/icon.jsx
@@ -3,7 +3,21 @@
  */
 
 var React = require('react'),
-	Classable = require('./mixins/classable.js');
+	Classable = require('./mixins/classable.js'),
+
+  wideIcons = [
+    'signal-wifi-statusbar-1-bar',
+    'signal-wifi-statusbar-2-bar',
+    'signal-wifi-statusbar-3-bar',
+    'signal-wifi-statusbar-4-bar',
+    'signal-wifi-statusbar-connected-no-internet-1',
+    'signal-wifi-statusbar-connected-no-internet',
+    'signal-wifi-statusbar-connected-no-internet-2',
+    'signal-wifi-statusbar-connected-no-internet-3',
+    'signal-wifi-statusbar-connected-no-internet-4',
+    'signal-wifi-statusbar-not-connected',
+    'signal-wifi-statusbar-null'
+  ];
 
 var Icon = React.createClass({
 
@@ -15,8 +29,10 @@ var Icon = React.createClass({
 	},
 
 	render: function() {
-		var classes = this.getClasses('mui-icon mui-icon-' + this.props.icon);
-
+		var isWide = wideIcons.indexOf(this.props.icon) > -1,
+      suffix = isWide ? '_26x24px' : '_24px',
+      classes = this.getClasses('mui-icon svg-ic_' + this.props.icon.replace(/-/g, '_') + suffix);
+console.log(classes);
 		return (
 			<span className={classes} onClick={this._onClick}>
 				<span className="mui-icon-highlight">&nbsp;</span>
