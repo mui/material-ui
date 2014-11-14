f6a12522a1b45c587cbf4e0795916345bb75e06e
Hai Nguyen <hai@call-em-all.com>
2014-11-14 00:46:10 -0600

    Doc Site - Added responsive styles to secondary nav pages

--- docs/src/app/components/pages/page-with-nav.jsx
+++ docs/src/app/components/pages/page-with-nav.jsx
@@ -17,17 +17,17 @@ var PageWithNav = React.createClass({
 
   render: function() {
     return (
-      <div className="mui-app-content-canvas with-nav">
-        <div className="subNav">
+      <div className="mui-app-content-canvas page-with-nav">
+        <div className="page-with-nav-content">
+          <this.props.activeRouteHandler />
+        </div>
+        <div className="page-with-nav-secondary-nav">
           <Menu 
             ref="menuItems" 
             zDepth={0} 
             menuItems={this.props.menuItems} 
             selectedIndex={this._getSelectedIndex()} 
             onItemClick={this._onMenuItemClick} />
-        </div>
-        <div className="subContent">
-          <this.props.activeRouteHandler />
         </div>
       </div>
     );
