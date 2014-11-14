8dc45f3da2a06b9eba487c5d7da38a05afdd7522
Hai Nguyen <hai@call-em-all.com>
2014-11-13 18:39:58 -0600

    Added onMouseOver & onMouseOut to Paper Component

--- src/js/paper.jsx
+++ src/js/paper.jsx
@@ -15,7 +15,9 @@ var Paper = React.createClass({
     circle: React.PropTypes.bool,
     onClick: React.PropTypes.func,
     onMouseDown: React.PropTypes.func,
-    onMouseUp: React.PropTypes.func
+    onMouseUp: React.PropTypes.func,
+    onMouseOver: React.PropTypes.func,
+    onMouseOut: React.PropTypes.func
   },
 
   getDefaultProps: function() {
@@ -34,7 +36,8 @@ var Paper = React.createClass({
         insideClasses = 'mui-paper-container mui-z-depth-bottom';
 
     return (
-      <div className={classes} onClick={this._onClick} onMouseDown={this._onMouseDown} onMouseUp={this._onMouseUp}>
+      <div className={classes} onClick={this._onClick} onMouseDown={this._onMouseDown} 
+      	onMouseUp={this._onMouseUp} onMouseOver={this._onMouseOver} onMouseOut={this._onMouseOut}>
         <div className={insideClasses}>
           {this.props.children}
         </div>
@@ -42,12 +45,21 @@ var Paper = React.createClass({
     );
   },
 
+
   _onClick: function(e) {
     if (this.props.onClick) this.props.onClick(e);
   },
 
   _onMouseDown: function(e) {
     if (this.props.onMouseDown) this.props.onMouseDown(e);
+  },
+
+  _onMouseOver: function(e) {
+  	if (this.props.onMouseOver) this.props.onMouseOver(e);
+  },
+
+  _onMouseOut: function(e) {
+  	if (this.props.onMouseOut) this.props.onMouseOut(e);
   },
 
   _onMouseUp: function(e) {
