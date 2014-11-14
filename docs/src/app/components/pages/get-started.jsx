055d11afbe4baa534596270eb54ef2093288858a
Hai Nguyen <hai@call-em-all.com>
2014-11-14 00:06:50 -0600

    Doc Site - Added responsive styles to get started page

--- docs/src/app/components/pages/get-started.jsx
+++ docs/src/app/components/pages/get-started.jsx
@@ -34,41 +34,37 @@ var HomePage = React.createClass({
     return (
       <div className="get-started-page mui-app-content-canvas">
         <div className="full-width-section">
+
           <h2 className="mui-font-style-headline">Installation</h2>
-          <p className="content">
+          <p>
             Material-UI is available as an <a href="https://www.npmjs.org/package/material-ui">npm package</a>. 
             Use <a href="http://browserify.org/">browserify</a> and <a href="https://github.com/andreypopp/reactify">reactify</a> for
             dependency management and JSX transformation. The CSS framework is written in <a href="http://lesscss.org/">Less</a>,
             so you'll need to compile that as well.
           </p>
-        </div>
-        
-        <div className="full-width-section">
-          <div className="content">
-            <h2 className="mui-font-style-headline">Usage</h2>
-            <p>
-              Once material-ui is included in your project, you can use the components this way:
-            </p>
-            <mui.Paper className="code-example">
-              <CodeBlock>{usageCode}</CodeBlock>
-            </mui.Paper>
-          </div>
 
-          <div className="content">
-            <h2 className="mui-font-style-headline">Customization</h2>
-            <div>
-              <p>The styles are separated into 2 less files:</p>
-              <li>dist/less/scaffolding.less</li>
-              <li>dist/less/components.less</li>
-              <p>
-                This allows you to override any variables defined in custom-variables.less without having to
-                modify material-ui source files directly. For example, your main.less file could look something like this:
-              </p>
-            </div>
-            <mui.Paper className="code-example">
-              <CodeBlock>{customizationCode}</CodeBlock>
-            </mui.Paper>
+          <h2 className="mui-font-style-headline">Usage</h2>
+          <p>
+            Once material-ui is included in your project, you can use the components this way:
+          </p>
+          <mui.Paper className="code-example">
+            <CodeBlock>{usageCode}</CodeBlock>
+          </mui.Paper>
+
+          <h2 className="mui-font-style-headline">Customization</h2>
+          <div>
+            <p>The styles are separated into 2 less files:</p>
+            <li>dist/less/scaffolding.less</li>
+            <li>dist/less/components.less</li>
+            <p>
+              This allows you to override any variables defined in custom-variables.less without having to
+              modify material-ui source files directly. For example, your main.less file could look something like this:
+            </p>
           </div>
+          <mui.Paper className="code-example">
+            <CodeBlock>{customizationCode}</CodeBlock>
+          </mui.Paper>
+
         </div>
       </div>
     );
