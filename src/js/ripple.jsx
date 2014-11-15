b0735cce8bed9f8443d4349d08d69179f9e8ffe3
Hai Nguyen <hai@call-em-all.com>
2014-11-14 20:01:19 -0600

    Removed jQuery dependency from ripple

--- src/js/ripple.jsx
+++ src/js/ripple.jsx
@@ -2,9 +2,9 @@
  * @jsx React.DOM
  */
 
-var $ = require('jquery'),
-  React = require('react'),
+var React = require('react'),
   CssEvent = require('./utils/css-event.js'),
+  Dom = require('./utils/dom.js'),
   Classable = require('./mixins/classable.js');
 
 var Ripple = React.createClass({
@@ -21,21 +21,18 @@ var Ripple = React.createClass({
 
   animate: function(e, callback) {
     var el = this.getDOMNode(),
-      $ripple = $(el),
-      $offset = $ripple.parent().offset(),
-      x = e.pageX - $offset.left,
-      y = e.pageY - $offset.top;
+      offset = Dom.offset(el.parentNode),
+      x = e.pageX - offset.left,
+      y = e.pageY - offset.top;
 
-    $ripple.css({
-      transition: 'none',
-      top: y,
-      left: x
-    });
+    el.style.transition = 'none';
+    el.style.top = y + 'px';
+    el.style.left = x + 'px';
 
-    $ripple.addClass('mui-show');
+    Dom.addClass(el, 'mui-show');
 
     CssEvent.onAnimationEnd(el, function() {
-      $ripple.removeClass('mui-show');
+      Dom.removeClass(el, 'mui-show');
       if (callback) callback();
     });
   }
--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -9,6 +9,28 @@ module.exports = {
     }
 
     return false;
+  },
+
+  offset: function(el) {
+    var rect = el.getBoundingClientRect()
+    return {
+      top: rect.top + document.body.scrollTop,
+      left: rect.left + document.body.scrollLeft
+    };
+  },
+
+  addClass: function(el, className) {
+    if (el.classList)
+      el.classList.add(className);
+    else
+      el.className += ' ' + className;
+  },
+
+  removeClass: function(el, className) {
+    if (el.classList)
+      el.classList.remove(className);
+    else
+      el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
   }
   
 }
