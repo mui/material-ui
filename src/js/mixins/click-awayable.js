16e9bf0e4301e1286f5f954b17bef002aa8679d8
Hai Nguyen <hai@call-em-all.com>
2014-11-14 19:52:56 -0600

    Removed jQuery dependency from click away mixin

--- src/js/mixins/click-awayable.js
+++ src/js/mixins/click-awayable.js
@@ -1,23 +1,27 @@
-var $ = require('jquery');
+var Events = require('../utils/events.js'),
+  Dom = require('../utils/dom.js');
 
 module.exports = {
 
   //When the component mounts, listen to click events and check if we need to
   //Call the componentClickAway function.
   componentDidMount: function() {
-    var clickEvent = 'click' + this._rootNodeID;
-
-    $(document)
-      .off(clickEvent)
-      .on(clickEvent, function(e) {
-        if (this.isMounted() && !$(e.target).closest(this.getDOMNode()).length) {
-          if (this.componentClickAway) this.componentClickAway();
-        }
-    }.bind(this));
+    Events.on(document, 'click', this._checkClickAway);
   },
 
   componentWillUnmount: function() {
-    $(document).off('click' + this._rootNodeID);
+    Events.off(document, 'click', this._checkClickAway);
+  },
+
+  _checkClickAway: function(e) {
+    var el = this.getDOMNode();
+
+    // Check if the target is inside the current component
+    if (this.isMounted() && 
+      e.target != el &&
+      !Dom.isDescendant(el, e.target)) {
+      if (this.componentClickAway) this.componentClickAway();
+    }
   }
 
 }
