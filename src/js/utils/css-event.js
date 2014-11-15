60ba0da4966b10caffaa69318f08fb79d2b49028
Hai Nguyen <hai@call-em-all.com>
2014-11-14 23:54:49 -0600

    Cleaned up Css Events event binding

--- src/js/utils/css-event.js
+++ src/js/utils/css-event.js
@@ -2,25 +2,51 @@ var Events = require('./events.js');
 
 module.exports = {
 
-  onTransitionEnd: function (el, callback) {
-    Events.once(el, 'webkitTransitionEnd ' +
-      'otransitionend ' +
-      'oTransitionEnd ' +
-      'msTransitionEnd ' +
-      'transitionend', function() {
-        return callback();
+  _testSupportedProps: function(props) {
+    var i,
+      undefined,
+      el = document.createElement('div');
+
+    for (i in props) {
+      if (props.hasOwnProperty(i) && el.style[i] !== undefined) {
+        return props[i];
       }
-    );
+    }
+  },
+
+  //Returns the correct event name to use
+  transitionEndEventName: function() {
+    return this._testSupportedProps({
+      'transition':'transitionend',
+      'OTransition':'otransitionend',  
+      'MozTransition':'transitionend',
+      'WebkitTransition':'webkitTransitionEnd'
+    });
+  },
+
+  animationEndEventName: function() {
+    return this._testSupportedProps({
+      'animation': 'animationend',
+      '-o-animation': 'oAnimationEnd',
+      '-moz-animation': 'animationend',
+      '-webkit-animation': 'webkitAnimationEnd'
+    });
+  },
+
+  onTransitionEnd: function (el, callback) {
+    var transitionEnd = this.transitionEndEventName();
+
+    Events.once(el, transitionEnd, function() {
+      return callback();
+    });
   },
 
   onAnimationEnd: function (el, callback) {
-    Events.once(el, 'webkitAnimationEnd ' +
-      'oanimationend ' +
-      'msAnimationEnd ' +
-      'animationend', function() {
-        return callback();
-      }
-    );
+    var animationEnd = this.animationEndEventName();
+
+    Events.once(el, animationEnd, function() {
+      return callback();
+    });
   }
 
 };
