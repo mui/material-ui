1740692392d9a1670ca91454cffb3dfa4c41504a
Hai Nguyen <hai@call-em-all.com>
2014-11-14 18:35:13 -0600

    Added events and dom utility functions

---
+++ src/js/utils/dom.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  isDescendant: function(parent, child) {
+    var node = child.parentNode;
+
+    while (node != null) {
+      if (node == parent) return true;
+      node = node.parentNode;
+    }
+
+    return false;
+  }
+  
+}
--- src/js/utils/events.js
+++ src/js/utils/events.js
@@ -1,15 +1,23 @@
 module.exports = {
 
-  once: function (node, type, callback) {
-    var typeArray = type.split(' ');
+  once: function (el, type, callback) {
+    var _this = this,
+      typeArray = type.split(' ');
 
     for (var i = typeArray.length - 1; i >= 0; i--) {
-      node.addEventListener(typeArray[i], function(e) {
+      el.addEventListener(typeArray[i], function(e) {
         e.target.removeEventListener(e.type, arguments.callee);
         return callback(e);
       });
     };
-    
+  },
+
+  on: function(el, type, callback, capture) {
+    el.addEventListener(type, callback, capture || false);
+  },
+
+  off: function(el, type, callback, capture) {
+    el.removeEventListener(type, callback, capture || false);
   }
 
 }
