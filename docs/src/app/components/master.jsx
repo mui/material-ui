71ff54bb442593895a74c7523d9eb20319c476ff
Hai Nguyen <hai@call-em-all.com>
2014-11-17 19:19:46 -0600

    Doc Site - updated app bar and footer to use icon buttons

--- docs/src/app/components/master.jsx
+++ docs/src/app/components/master.jsx
@@ -4,36 +4,47 @@
 
 var React = require('react'),
   mui = require('mui'),
+  AppBar = mui.AppBar,
+  AppCanvas = mui.AppCanvas,
   Menu = mui.Menu,
-  Icon = mui.Icon,
+  IconButton = mui.IconButton,
   AppLeftNav = require('./app-left-nav.jsx');
 
 var Master = React.createClass({
 
   render: function() {
     return (
-      <mui.AppCanvas predefinedLayout={1}>
-        <mui.AppBar onMenuIconClick={this._onMenuIconClick} title={this.props.activeRouteHandler().props.pageTitle} zDepth={0}>
-          <Icon icon="mui-icon-github" onClick={this._onGithubClick} />
-        </mui.AppBar>
+      <AppCanvas predefinedLayout={1}>
+
+        <AppBar
+          className="mui-dark-theme"
+          onMenuIconButtonTouchTap={this._onMenuIconButtonTouchTap}
+          title={this.props.activeRouteHandler().props.pageTitle}
+          zDepth={0}>
+          <IconButton className="github-icon-button" icon="mui-icon-github" onTouchTap={this._onGithubTouchTap} />
+        </AppBar>
+
         <AppLeftNav ref="leftNav" />
+
         <this.props.activeRouteHandler />
-        <div className="footer full-width-section">
+
+        <div className="footer full-width-section mui-dark-theme">
           <p>
             Hand crafted with love by the engineers at <a href="http://call-em-all.com">Call-Em-All</a> and our 
             awesome <a href="https://github.com/callemall/material-ui/graphs/contributors">contributors</a>.
           </p>
-          <Icon icon="mui-icon-github" onClick={this._onGithubClick} />
+          <IconButton className="github-icon-button" icon="mui-icon-github" onTouchTap={this._onGithubTouchTap} />
         </div>
-      </mui.AppCanvas>
+
+      </AppCanvas>
     );
   },
 
-  _onGithubClick: function() {
+  _onGithubTouchTap: function() {
     document.location.href='https://github.com/callemall/material-ui';
   },
 
-  _onMenuIconClick: function() {
+  _onMenuIconButtonTouchTap: function() {
     this.refs.leftNav.toggle();
   }
   
