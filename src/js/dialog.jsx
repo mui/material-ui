f1ea5955605984aeeb981062d69d22d718c06b6d
Hai Nguyen <hai@call-em-all.com>
2014-12-07 08:23:56 -0600

    Added transitions to dialog

--- src/js/dialog.jsx
+++ src/js/dialog.jsx
@@ -1,8 +1,10 @@
 var React = require('react'),
   WindowListenable = require('./mixins/window-listenable.js'),
+  Dom = require('./utils/dom.js'),
   Events = require('./utils/events.js'),
   KeyCode = require('./utils/key-code.js'),
   Classable = require('./mixins/classable'),
+  FlatButton = require('./flat-button.jsx'),
   Overlay = require('./overlay.jsx'),
   Paper = require('./paper.jsx');
 
@@ -43,26 +45,21 @@ var Dialog = React.createClass({
   },
 
   render: function() {
-    var mainClasses = this.getClasses('dialog', { 'show': this.state.open }),
-      actions = this.props.actions.map(function(a, index) {
-        if (a.onClick) return <div className="action" key={index} onClick={a.onClick}>{a.text}</div>;
-        return <div className="action"  key={index} onClick={this.dismiss}>{a.text}</div>;
-      }.bind(this));
+    var classes = this.getClasses('mui-dialog', { 'mui-is-shown': this.state.open }),
+      actions = this._getDialogActions();
 
     return (
-      <div className={mainClasses}>
-        <Paper className="dialog-window" zDepth={4}>
-          <h3 className="dialog-title">
-            {this.props.title}
-          </h3>
-          <div className="dialog-content">
+      <div className={classes}>
+        <Paper ref="dialogWindow" className="mui-dialog-window" zDepth={4}>
+
+          <h3 className="mui-dialog-title">{this.props.title}</h3>
+          <div ref="dialogContent" className="mui-dialog-content">
             {this.state.open ? this.props.children : ''}
           </div>
-          <div className="dialog-actions">
-            <div className="actions-right">
-              {actions}
-            </div>
+          <div className="mui-dialog-actions">
+            <div className="mui-dialog-actions-right">{actions}</div>
           </div>
+
         </Paper>
         <Overlay show={this.state.open} onTouchTap={this._handleOverlayTouchTap} />
       </div>
@@ -79,12 +76,42 @@ var Dialog = React.createClass({
     if (this.props.onShow) this.props.onShow();
   },
 
-  _positionDialog: function() {
-    //calculate height and use that to center the dialog vertically
-    var el = this.getDOMNode(),
-      height = el.offsetHeight;
+  _getDialogActions: function() {
+    return this.props.actions.map(function(a, index) {
 
-    el.style.marginTop = -1 * height / 2 + 'px';
+      var onClickHandler = a.onClick ? a.onClick : this.dismiss;
+      return (
+        <FlatButton
+          className="mui-dialog-action"
+          key={index}
+          primary={true}
+          onClick={onClickHandler}
+          label={a.text} />
+      );
+
+    }.bind(this));
+  },
+
+  _positionDialog: function() {
+    
+    if (this.state.open) {
+
+      var container = this.getDOMNode(),
+        dialogWindow = this.refs.dialogWindow.getDOMNode(),
+        dialogContent = this.refs.dialogContent.getDOMNode(),
+        containerHeight = container.offsetHeight,
+        dialogWindowHeight = dialogWindow.offsetHeight,
+        dialogContentHeight = dialogContent.offsetHeight;
+
+      //Vertically center the dialog window, but make sure it doesn't
+      //transition to that position.
+      container.style.paddingTop = ((containerHeight - dialogWindowHeight) / 2) - 64 + 'px';
+
+      //This is needed to keep the height from changing when the dialog
+      //gets closed
+      dialogContent.style.height = dialogContentHeight + 'px';
+
+    }
   },
 
   _handleOverlayTouchTap: function() {
