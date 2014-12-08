4a0e34b9d7343576d41e8ff8acba94ff758934bc
Michael Phan-Ba <michael@mikepb.com>
2014-12-07 23:13:27 -0800

    Update example page for Slider

--- docs/src/app/components/pages/components/sliders.jsx
+++ docs/src/app/components/pages/components/sliders.jsx
@@ -3,20 +3,35 @@
  */
 
 var React = require('react'),
-  mui = require('mui');
+  mui = require('mui'),
+  Slider = mui.Slider,
+  CodeExample = require('../../code-example/code-example.jsx');
 
 var SlidersPage = React.createClass({
 
   render: function() {
+    var code =
+      '// Default\n' +
+      '<Slider name="slider1" />\n\n' +
+      '// With value\n' +
+      '<Slider name="slider2" value={0.5} />\n' +
+      '<Slider name="slider3" value={1} />\n\n' +
+      '// Disabled\n' +
+      '<Slider name="slider1" disabled={true} />\n' +
+      '<Slider name="slider2" disabled={true} value={0.5} />\n' +
+      '<Slider name="slider3" disabled={true} value={1} />';
+
     return (
       <div>
         <h2 className="mui-font-style-headline">Sliders</h2>
-        <mui.Slider name="slider1" />
-        <mui.Slider name="slider2" value={0.5} />
-        <mui.Slider name="slider3" value={1} />
-        <mui.Slider name="slider1" disabled={true} />
-        <mui.Slider name="slider2" disabled={true} value={0.5} />
-        <mui.Slider name="slider3" disabled={true} value={1} />
+        <CodeExample code={code}>
+          <Slider name="slider1" />
+          <Slider name="slider2" value={0.5} />
+          <Slider name="slider3" value={1} />
+          <Slider name="slider1" disabled={true} />
+          <Slider name="slider2" disabled={true} value={0.5} />
+          <Slider name="slider3" disabled={true} value={1} />
+        </CodeExample>
       </div>
     );
   }
