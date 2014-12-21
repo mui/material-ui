f034a6820a570287a0ae489fa1a69882ef1b9171
Hai Nguyen <hai@call-em-all.com>
2014-12-20 22:20:37 -0600

    Added date picker actions

--- src/js/date-picker/date-picker.jsx
+++ src/js/date-picker/date-picker.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var Classable = require('../mixins/classable');
 var DialogWindow = require('../dialog-window.jsx');
+var FlatButton = require('../flat-button.jsx');
 
 var DatePicker = React.createClass({
 
@@ -13,11 +14,22 @@ var DatePicker = React.createClass({
     } = this.props;
     var classes = this.getClasses('mui-date-picker', {
     });
+    var actions = [
+      <FlatButton
+        label="OK"
+        secondary={true} />,
+      <FlatButton
+        label="Cancel"
+        secondary={true}
+        onTouchTap={this._handleCancelTouchTap} />
+    ];
 
     return (
       <DialogWindow {...other}
         ref="dialogWindow"
-        className={classes}>
+        className={classes}
+        actions={actions}
+        contentClassName="mui-date-picker-dialog">
         date picker
       </DialogWindow>
     );
@@ -25,6 +37,14 @@ var DatePicker = React.createClass({
 
   show: function() {
     this.refs.dialogWindow.show();
+  },
+
+  dismiss: function() {
+    this.refs.dialogWindow.dismiss();
+  },
+
+  _handleCancelTouchTap: function() {
+    this.refs.dialogWindow.dismiss();
   }
 
 });
