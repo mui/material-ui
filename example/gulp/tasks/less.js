2f64951de2bdd7a65bc6e3d99381ae99d8f64f0f
Hai Nguyen <hai@call-em-all.com>
2014-12-26 12:53:44 -0600

    added autoprefixer to example

--- example/gulp/tasks/less.js
+++ example/gulp/tasks/less.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
   less = require('gulp-less'),
+  autoprefixer = require('gulp-autoprefixer'),
   sourcemaps = require('gulp-sourcemaps'),
   handleErrors = require('../util/handleErrors'),
   config = require('../config').less;
@@ -9,6 +10,7 @@ gulp.task('less', function() {
     .pipe(sourcemaps.init())
     .pipe(less())
     .on('error', handleErrors)
+    .pipe(autoprefixer({cascade: false, browsers: ['last 2 versions']}))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.dest));
 });
