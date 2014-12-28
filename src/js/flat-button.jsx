1afa787a95668569951d3529a41999a84f439327
Hai Nguyen <hai@call-em-all.com>
2014-12-27 22:59:07 -0600

    Improved performance of flat button touch ripple

--- src/js/flat-button.jsx
+++ src/js/flat-button.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var Classable = require('./mixins/classable.js');
 var EnhancedButton = require('./enhanced-button.jsx');
+var TouchRipple = require('./ripples/touch-ripple.jsx');
 var Ripple = require('./ripple.jsx');
 
 var FlatButton = React.createClass({
@@ -10,29 +11,38 @@ var FlatButton = React.createClass({
   propTypes: {
     className: React.PropTypes.string,
     label: React.PropTypes.string.isRequired,
-    onTouchTap: React.PropTypes.func,
+    onMouseDown: React.PropTypes.func,
+    onMouseUp: React.PropTypes.func,
+    onTouchEnd: React.PropTypes.func,
+    onTouchStart: React.PropTypes.func,
     primary: React.PropTypes.bool,
     secondary: React.PropTypes.bool
   },
 
   render: function() {
-    var 
-      {
-        className,
-        onTouchTap,
+    var {
+        label,
+        primary,
+        secondary,
         ...other
-      } = this.props,
-      classes = this.getClasses('mui-flat-button', {
-        'mui-is-primary': this.props.primary,
-        'mui-is-secondary': !this.props.primary && this.props.secondary
-      });
+      } = this.props;
+    var classes = this.getClasses('mui-flat-button', {
+      'mui-is-primary': this.props.primary,
+      'mui-is-secondary': !this.props.primary && this.props.secondary
+    });
 
     return (
       <EnhancedButton {...other}
         className={classes}
-        onTouchTap={this._onTouchTap}>
+        onMouseUp={this._handleMouseUp}
+        onMouseDown={this._handleMouseDown}
+        onTouchStart={this._handleTouchStart}
+        onTouchEnd={this._handleTouchEnd}>
 
-        <Ripple ref="ripple" className="mui-flat-button-ripple" />
+        <TouchRipple
+          className="mui-flat-button-ripple"
+          ref="touchRipple" />
+
         <Ripple className="mui-flat-button-focus-ripple" />
         {this.props.label}
 
@@ -40,9 +50,25 @@ var FlatButton = React.createClass({
     );
   },
 
-  _onTouchTap: function(e) {
-    if (!this.props.disabled) this.refs.ripple.animate(e);
-    if (this.props.onTouchTap) this.props.onTouchTap(e);
+  _handleMouseDown: function(e) {
+    //only listen to left clicks
+    if (e.button === 0) this.refs.touchRipple.start(e);
+    if (this.props.onMouseDown) this.props.onMouseDown(e);
+  },
+
+  _handleMouseUp: function(e) {
+    this.refs.touchRipple.end();
+    if (this.props.onMouseUp) this.props.onMouseUp(e);
+  },
+
+  _handleTouchStart: function(e) {
+    this.refs.touchRipple.start(e);
+    if (this.props.onTouchStart) this.props.onTouchStart(e);
+  },
+
+  _handleTouchEnd: function(e) {
+    this.refs.touchRipple.end();
+    if (this.props.onTouchEnd) this.props.onTouchEnd(e);
   }
 
 });
--- src/js/ripples/touch-ripple.jsx
+++ src/js/ripples/touch-ripple.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
-var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 var Classable = require('../mixins/classable');
+var Dom = require('../utils/dom.js');
 var RippleCircle = require('./circle.jsx');
 
 var TouchRipple = React.createClass({
@@ -8,8 +8,7 @@ var TouchRipple = React.createClass({
   mixins: [Classable],
 
   propTypes: {
-    className: React.PropTypes.string,
-    rippleClassName: React.PropTypes.string
+    className: React.PropTypes.string
   },
 
   getInitialState: function() {
@@ -23,36 +22,25 @@ var TouchRipple = React.createClass({
   },
 
   render: function() {
-    var {
-      className,
-      ...other
-    } = this.props;
     var classes = this.getClasses('mui-touch-ripple');
-    var ripples = this.state.ripples.map(function(ripple) {
-      return (
-        <RippleCircle
-          innerClassName={this.props.rippleClassName}
-          key={ripple.key}
-          started={ripple.started}
-          ending={ripple.ending} />
-      );
-    }.bind(this));
 
     return (
       <div className={classes}>
-        {ripples}
+        {this._getRippleElements()}
       </div>
     );
   },
 
-  start: function() {
+  start: function(e) {
     var ripples = this.state.ripples;
     var nextKey = ripples[ripples.length-1].key + 1;
-    
+    var style = e ? this._getRippleStyle(e) : {};
+
     //Start the next unstarted ripple
     for (var i = 0; i < ripples.length; i++) {
       if (!ripples[i].started) {
         ripples[i].started = true;
+        ripples[i].style = style;
         break;
       }
     };
@@ -86,13 +74,58 @@ var TouchRipple = React.createClass({
       ripples: ripples
     });
 
-    //Wait 1 second and remove the ripple from DOM
+    //Wait 2 seconds and remove the ripple from DOM
     setTimeout(function() {
       ripples.shift();
       this.setState({
         ripples: ripples
       });
-    }.bind(this), 1000);
+    }.bind(this), 2000);
+  },
+
+  _getRippleStyle: function(e) {
+    var style = {};
+    var el = this.getDOMNode();
+    var elHeight = el.offsetHeight;
+    var elWidth = el.offsetWidth;
+    var offset = Dom.offset(el);
+    var pageX = e.pageX == undefined ? e.nativeEvent.pageX : e.pageX;
+    var pageY = e.pageY == undefined ? e.nativeEvent.pageY : e.pageY;
+    var pointerX = pageX - offset.left;
+    var pointerY = pageY - offset.top;
+    var topLeftDiag = this._calcDiag(pointerX, pointerY);
+    var topRightDiag = this._calcDiag(elWidth - pointerX, pointerY);
+    var botRightDiag = this._calcDiag(elWidth - pointerX, elHeight - pointerY);
+    var botLeftDiag = this._calcDiag(pointerX, elHeight - pointerY);
+    var rippleRadius = Math.max(
+      topLeftDiag, topRightDiag, botRightDiag, botLeftDiag
+    );
+    var rippleSize = rippleRadius * 2;
+    var left = pointerX - (rippleRadius);
+    var top = pointerY - (rippleRadius);
+    
+    style.height = rippleSize + 'px';
+    style.width = rippleSize + 'px';
+    style.top = top + 'px';
+    style.left = left + 'px';
+
+    return style;
+  },
+
+  _calcDiag: function(a, b) {
+    return Math.sqrt((a * a) + (b * b));
+  },
+
+  _getRippleElements: function() {
+    return this.state.ripples.map(function(ripple) {
+      return (
+        <RippleCircle
+          key={ripple.key}
+          started={ripple.started}
+          ending={ripple.ending}
+          style={ripple.style} />
+      );
+    }.bind(this));
   }
 
 });
