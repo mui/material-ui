9410df3d06f0f0abfe7dcd4683d459737f246a59
Hai Nguyen <hai@call-em-all.com>
2015-01-03 15:40:50 -0600

    Fixed a bug with once event binding

--- src/js/utils/events.js
+++ src/js/utils/events.js
@@ -1,30 +1,34 @@
 module.exports = {
-    once: function(el, type, callback) {
-        var typeArray = type.split(' ');
-        var recursiveFunction = function(e){
-            e.target.removeEventListener(e.type, recursiveFunction);
-            return callback(e);
-        };
-        for(var i = typeArray.length - 1; i > 0; i--) {
-            on(el, typeArray[i], recursiveFunction);
-        }
-    },
-    // IE8+ Support
-    on: function(el, type, callback) {
-        if(el.addEventListener) {
-            el.addEventListener(type, callback);
-        } else {
-            el.attachEvent('on' + type, function() {
-                callback.call(el);
-            });
-        }
-    },
-    // IE8+ Support
-    off: function(el, type, callback) {
-        if(el.removeEventListener) {
-            el.removeEventListener(type, callback);
-        } else {
-            el.detachEvent('on' + type, callback);
-        }
+
+  once: function(el, type, callback) {
+    var typeArray = type.split(' ');
+    var recursiveFunction = function(e){
+      e.target.removeEventListener(e.type, recursiveFunction);
+      return callback(e);
+    };
+
+    for (var i = typeArray.length - 1; i >= 0; i--) {
+      this.on(el, typeArray[i], recursiveFunction);
     }
+  },
+
+  // IE8+ Support
+  on: function(el, type, callback) {
+    if(el.addEventListener) {
+      el.addEventListener(type, callback);
+    } else {
+      el.attachEvent('on' + type, function() {
+        callback.call(el);
+      });
+    }
+  },
+
+  // IE8+ Support
+  off: function(el, type, callback) {
+    if(el.removeEventListener) {
+      el.removeEventListener(type, callback);
+    } else {
+      el.detachEvent('on' + type, callback);
+    }
+  }
 };
