36c66bcb40b2f2043cdf0b46a2fd74c68a291055
M03M <muhammad_martinez@yahoo.com>
2015-01-07 18:41:35 -0600

    Accidentally added some whitespace to snackbar, undid these changes.

--- src/js/snackbar.jsx
+++ src/js/snackbar.jsx
@@ -28,7 +28,7 @@ var Snackbar = React.createClass({
 
   componentDidUpdate: function(prevProps, prevState) {
     if (prevState.open != this.state.open) {
-      if (this.state.open) { 
+      if (this.state.open) {
         this._bindClickAway();
       } else {
         this._unbindClickAway();
@@ -70,4 +70,3 @@ var Snackbar = React.createClass({
 });
 
 module.exports = Snackbar;
-
