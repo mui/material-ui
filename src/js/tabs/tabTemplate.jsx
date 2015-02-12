3bdc74db99accfbb116bb474bc88c32db1c18081
Geoffrey <gabdallah222@gmail.com>
2015-02-11 20:44:31 -0600

    Made change to ccs-in-js styles for tabTemplate

--- src/js/tabs/tabTemplate.jsx
+++ src/js/tabs/tabTemplate.jsx
@@ -1,11 +1,21 @@
 var React = require('react');
+var StylePropable = require('../mixins/style-propable.js');
 
 var TabTemplate = React.createClass({
 
+  mixins: [StylePropable],
+
   render: function(){
 
+    var styles = this.mergePropStyles({
+      'display': 'block',
+      'width': '100%',
+      'position': 'relative',
+      'text-align': 'initial'
+    });
+
     return (
-      <div className='mui-tab-template'>
+      <div styles={styles}>
         {this.props.children}
       </div>
     );
