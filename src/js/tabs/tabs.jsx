f8dde6fe37f5b1f19944501bfa3c17e3a74dd303
Geoffrey Abdallah <gabdallah222@gmail.com>
2015-02-24 21:19:22 -0600

    Merge and prefix added to tabs

--- src/js/tabs/tabs.jsx
+++ src/js/tabs/tabs.jsx
@@ -36,7 +36,7 @@ var Tabs = React.createClass({
   },
 
   render: function(){
-    var tabItemContainerStyle = this.mergeStyles({
+    var tabItemContainerStyle = this.mergeAndPrefix({
       margin: '0',
       padding: '0',
       width: '100%',
