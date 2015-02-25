9a7c622774c2aeb440ee868ce2a38d0d5da6b35c
Geoffrey Abdallah <gabdallah222@gmail.com>
2015-02-24 21:29:17 -0600

    Reverted mergeandprefix when unnecessary

--- src/js/tabs/tabs.jsx
+++ src/js/tabs/tabs.jsx
@@ -36,7 +36,7 @@ var Tabs = React.createClass({
   },
 
   render: function(){
-    var tabItemContainerStyle = this.mergeAndPrefix({
+    var tabItemContainerStyle = this.mergeStyles({
       margin: '0',
       padding: '0',
       width: '100%',
