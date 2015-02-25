1d57a89467d3ab1613bbfc0b6bdbe9222e0c82f1
Geoffrey Abdallah <gabdallah222@gmail.com>
2015-02-24 21:19:52 -0600

    Added merge and prefix to tab

--- src/js/tabs/tab.jsx
+++ src/js/tabs/tab.jsx
@@ -18,7 +18,7 @@ var Tab = React.createClass({
   },
 
   render: function(){
-    var styles = this.mergeStyles({
+    var styles = this.mergeAndPrefix({
       'display': 'inline-block',
       'height': '100%',
       'cursor': 'pointer',
