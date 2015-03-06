37b675233a7682e6be5acb7b911ddf2107a785bf
Olivier Tassinari <Olivier.tassinari@gmail.com>
2015-03-06 08:45:58 +0100

    Update date-picker.jsx

--- src/js/date-picker/date-picker.jsx
+++ src/js/date-picker/date-picker.jsx
@@ -93,6 +93,8 @@ var DatePicker = React.createClass({
   },
 
   _handleInputTouchTap: function(e) {
+    e.preventDefault(); // Prevent touchTap to trigger on displayed Dialog
+
     this.setState({
       dialogDate: this.getDate()
     });
