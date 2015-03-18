a097bdc2028cfacb8e450076300b5ea789f9605a
M03M <muhammad_martinez@yahoo.com>
2015-03-18 15:21:58 -0500

    Refactored paper

--- src/js/paper.jsx
+++ src/js/paper.jsx
@@ -1,12 +1,13 @@
 var React = require('react');
-var Classable = require('./mixins/classable');
+var StylePropable = require('./mixins/style-propable');
 
 var Paper = React.createClass({
 
-  mixins: [Classable],
+  mixins: [StylePropable],
 
   propTypes: {
     circle: React.PropTypes.bool,
+    className: React.PropTypes.string,
     innerClassName: React.PropTypes.string,
     innerStyle: React.PropTypes.object,
     rounded: React.PropTypes.bool,
@@ -21,29 +22,44 @@ var Paper = React.createClass({
     };
   },
 
+  /** Styles */
+  _main: function() {
+    return this.mergeAndPrefix({
+      boxShadow: this.getZDepthShadows(this.props.zDepth).boxShadow,
+      borderRadius: this.props.circle ? '50%' : 
+                    this.props.rounded ? '2px' :
+                    '0px',
+    });
+  },
+
+  _inner: function() {
+    var style = this.mergeAndPrefix({
+      boxShadow: this.getZDepthShadows(this.props.zDepth).bottomBoxShadow,
+      width: '100%', 
+      height: '100%',
+    }, this._main());
+
+    if (this.props.innerStyle) {
+      style = this.mergeAndPrefix(style, this.props.innerStyle);
+    }
+
+    return style;
+  },
+
   render: function() {
     var {
       className,
-      circle,
       innerClassName,
+      style,
+      innerStyle,
+      circle,
       rounded,
       zDepth,
-      innerStyle,
-      ...other } = this.props,
-      classes = this.getClasses(
-        'mui-paper ' +
-        'mui-z-depth-' + this.props.zDepth, { 
-        'mui-rounded': this.props.rounded,
-        'mui-circle': this.props.circle
-      }),
-      insideClasses = 
-        this.props.innerClassName + ' ' +
-        'mui-paper-container ' +
-        'mui-z-depth-bottom';
+      ...other } = this.props;
 
     return (
-      <div {...other} className={classes}>
-        <div ref="innerContainer" className={insideClasses} style={innerStyle || {}}>
+      <div {...other} className={this.props.className} style={this._main()}>
+        <div ref="innerContainer" className={this.props.innerClassName} style={this._inner()}>
           {this.props.children}
         </div>
       </div>
@@ -52,6 +68,37 @@ var Paper = React.createClass({
 
   getInnerContainer: function() {
     return this.refs.innerContainer;
+  },
+
+  getZDepthShadows: function(zDepth) {
+    var shadows = [
+      {
+        boxShadow: '',
+        bottomBoxShadow: '',
+      },
+      {
+        boxShadow: '0 1px 4px rgba(0, 0, 0, 0.24)',
+        bottomBoxShadow: '0 1px 6px rgba(0, 0, 0, 0.12)',
+      },
+      {
+        boxShadow: '0 3px 10px rgba(0, 0, 0, 0.23)',
+        bottomBoxShadow: '0 3px 10px rgba(0, 0, 0, 0.16)',
+      },
+      {
+        boxShadow: '0 6px 10px rgba(0, 0, 0, 0.23)',
+        bottomBoxShadow: '0 10px 30px rgba(0, 0, 0, 0.19)',
+      },
+      {
+        boxShadow: '0 10px 18px rgba(0, 0, 0, 0.22)',
+        bottomBoxShadow: '0 14px 45px rgba(0, 0, 0, 0.25)',
+      },
+      {
+        boxShadow: '0 15px 20px rgba(0, 0, 0, 0.22)',
+        bottomBoxShadow: '0 19px 60px rgba(0, 0, 0, 0.30)',
+      },
+    ];
+
+    return shadows[zDepth];
   }
 
 });
