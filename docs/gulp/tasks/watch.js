ed8bb15f940c0035637ac1acde92e34be03bfb9f
M03M <muhammad_martinez@yahoo.com>
2015-05-12 12:26:40 -0500

    [Removal] Removes less related gulp tasks.

--- docs/gulp/tasks/watch.js
+++ docs/gulp/tasks/watch.js
@@ -8,6 +8,5 @@ var gulp   = require('gulp');
 var config = require('../config');
 
 gulp.task('watch', ['setWatch', 'browserSync'], function() {
-  gulp.watch(config.less.watch, ['less']);
   gulp.watch(config.markup.src, ['markup']);
 });
