1393617b050b1a2015aaba124acc455f00b09605
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-06-22 21:36:16 +0200

    [eslint] replacement of jsxhint

--- docs/gulp/tasks/jshint.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-var handleErrors = require('../util/handleErrors');
-
-gulp.task('jshint', shell.task([
-  '"../node_modules/.bin/jsxhint" --harmony "../src/**" "./src/app/**" --exclude ../src/utils/modernizr.custom.js'
-])).on('error', handleErrors);
