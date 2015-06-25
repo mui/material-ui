7f2d34061eec9dfec794f00280b5212be9abc09f
Hai Nguyen <hai@call-em-all.com>
2015-06-25 15:01:02 -0500

    Fixed esLint errors.

--- src/styles/transitions.js
+++ src/styles/transitions.js
@@ -12,12 +12,12 @@ module.exports = {
 
     if (property &&
       Object.prototype.toString.call(property) === '[object Array]' ) {
-      
+
       let transitions = '';
       for (let i = 0; i < property.length; i++) {
         if (transitions) transitions += ',';
         transitions += this.create(duration, property[i], delay, easeFunction);
-      };
+      }
       return transitions;
 
     } else {
@@ -37,4 +37,4 @@ module.exports = {
       easeFunction + ' ' +
       delay;
   }
-};
+}
