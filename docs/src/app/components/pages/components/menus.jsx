834558c65cf9876109415a9cb3ede9ccddeed728
Hai Nguyen <hai@call-em-all.com>
2015-07-07 01:03:29 -0500

    [docs] Fixed menu examples to show

--- docs/src/app/components/pages/components/menus.jsx
+++ docs/src/app/components/pages/components/menus.jsx
@@ -1,4 +1,5 @@
-let React = require('react');
+let React = require('react/addons');
+let ReactTransitionGroup = React.addons.TransitionGroup;
 let Menu = require('menus/menu');
 let MenuItem = require('menus/menu-item');
 let MenuDivider = require('menus/menu-divider');
@@ -201,112 +202,113 @@ class MenusPage extends React.Component {
         desc={desc}
         componentInfo={componentInfo}>
 
-        <Menu style={styles.menu}>
-          <MenuItem>Maps</MenuItem>
-          <MenuItem>Books</MenuItem>
-          <MenuItem>Flights</MenuItem>
-          <MenuItem>Apps</MenuItem>
-        </Menu>
+        <ReactTransitionGroup>
+          <Menu style={styles.menu}>
+            <MenuItem>Maps</MenuItem>
+            <MenuItem>Books</MenuItem>
+            <MenuItem>Flights</MenuItem>
+            <MenuItem>Apps</MenuItem>
+          </Menu>
 
-        <Menu style={styles.menu}>
-          <MenuItem>Refresh</MenuItem>
-          <MenuItem>Help &amp; feedback</MenuItem>
-          <MenuItem>Settings</MenuItem>
-          <MenuItem>Sign out</MenuItem>
-        </Menu>
+          <Menu style={styles.menu}>
+            <MenuItem>Refresh</MenuItem>
+            <MenuItem>Help &amp; feedback</MenuItem>
+            <MenuItem>Settings</MenuItem>
+            <MenuItem>Sign out</MenuItem>
+          </Menu>
 
-        <Menu style={styles.menu} desktop={true}>
-          <MenuItem>Home</MenuItem>
-          <MenuItem>Back</MenuItem>
-          <MenuItem disabled={true}>Forward</MenuItem>
-          <MenuDivider />
-          <MenuItem disabled={true}>Recently closed</MenuItem>
-          <MenuItem disabled={true}>Google</MenuItem>
-          <MenuItem>YouTube</MenuItem>
-        </Menu>
+          <Menu style={styles.menu} desktop={true}>
+            <MenuItem>Home</MenuItem>
+            <MenuItem>Back</MenuItem>
+            <MenuItem disabled={true}>Forward</MenuItem>
+            <MenuDivider />
+            <MenuItem disabled={true}>Recently closed</MenuItem>
+            <MenuItem disabled={true}>Google</MenuItem>
+            <MenuItem>YouTube</MenuItem>
+          </Menu>
 
-        <Menu style={styles.menu}>
-          <MenuItem leftIcon={<RemoveRedEye />}>Preview</MenuItem>
-          <MenuItem leftIcon={<PersonAdd />}>Share</MenuItem>
-          <MenuItem leftIcon={<ContentLink />}>Get link</MenuItem>
-          <MenuDivider />
-          <MenuItem leftIcon={<ContentCopy />}>Make a copy</MenuItem>
-          <MenuItem leftIcon={<Download />}>Download</MenuItem>
-          <MenuDivider />
-          <MenuItem leftIcon={<Delete />}>Remove</MenuItem>
-        </Menu>
+          <Menu style={styles.menu}>
+            <MenuItem leftIcon={<RemoveRedEye />}>Preview</MenuItem>
+            <MenuItem leftIcon={<PersonAdd />}>Share</MenuItem>
+            <MenuItem leftIcon={<ContentLink />}>Get link</MenuItem>
+            <MenuDivider />
+            <MenuItem leftIcon={<ContentCopy />}>Make a copy</MenuItem>
+            <MenuItem leftIcon={<Download />}>Download</MenuItem>
+            <MenuDivider />
+            <MenuItem leftIcon={<Delete />}>Remove</MenuItem>
+          </Menu>
 
-        <Menu style={styles.menu} desktop={true}>
-          <MenuItem>Undo</MenuItem>
-          <MenuItem disabled={true}>Redo</MenuItem>
-          <MenuDivider />
-          <MenuItem disabled={true}>Cut</MenuItem>
-          <MenuItem disabled={true}>Copy</MenuItem>
-          <MenuItem>Paste</MenuItem>
-        </Menu>
+          <Menu style={styles.menu} desktop={true}>
+            <MenuItem>Undo</MenuItem>
+            <MenuItem disabled={true}>Redo</MenuItem>
+            <MenuDivider />
+            <MenuItem disabled={true}>Cut</MenuItem>
+            <MenuItem disabled={true}>Copy</MenuItem>
+            <MenuItem>Paste</MenuItem>
+          </Menu>
 
-        <Menu style={styles.menu} desktop={true}>
-          <MenuItem>Untitled</MenuItem>
-          <MenuItem>Using the z-axis to Solve Design Challenges</MenuItem>
-          <MenuItem>An Extensive History of Dimensionality</MenuItem>
-        </Menu>
+          <Menu style={styles.menu} desktop={true}>
+            <MenuItem>Untitled</MenuItem>
+            <MenuItem>Using the z-axis to Solve Design Challenges</MenuItem>
+            <MenuItem>An Extensive History of Dimensionality</MenuItem>
+          </Menu>
 
-        <Menu style={styles.menu} desktop={true}>
-          <MenuItem>Untitled</MenuItem>
-          <MenuItem>Using the z-axis to Solve Design Challenges</MenuItem>
-          <MenuItem>An Extensive History of Dimensionality: the Abridged Edition</MenuItem>
-        </Menu>
+          <Menu style={styles.menu} desktop={true}>
+            <MenuItem>Untitled</MenuItem>
+            <MenuItem>Using the z-axis to Solve Design Challenges</MenuItem>
+            <MenuItem>An Extensive History of Dimensionality: the Abridged Edition</MenuItem>
+          </Menu>
 
-        <Menu style={styles.menu} desktop={true} width={320}>
-          <MenuItem secondaryText="Cmnd + O">Open</MenuItem>
-          <MenuItem secondaryText="Shift + V">Paste in place</MenuItem>
-          <MenuItem secondaryText="Opt + Shift + Cmnd + I">Research</MenuItem>
-        </Menu>
+          <Menu style={styles.menu} desktop={true} width={320}>
+            <MenuItem secondaryText="Cmnd + O">Open</MenuItem>
+            <MenuItem secondaryText="Shift + V">Paste in place</MenuItem>
+            <MenuItem secondaryText="Opt + Shift + Cmnd + I">Research</MenuItem>
+          </Menu>
 
-        <Menu style={styles.menu} desktop={true} width={320}>
-          <MenuItem secondaryText="&#8984;O">Open</MenuItem>
-          <MenuItem secondaryText="&#8679;&#8984;V">Paste in place</MenuItem>
-          <MenuItem secondaryText="&#8997;&#8679;&#8984;I">Research</MenuItem>
-        </Menu>
+          <Menu style={styles.menu} desktop={true} width={320}>
+            <MenuItem secondaryText="&#8984;O">Open</MenuItem>
+            <MenuItem secondaryText="&#8679;&#8984;V">Paste in place</MenuItem>
+            <MenuItem secondaryText="&#8997;&#8679;&#8984;I">Research</MenuItem>
+          </Menu>
 
-        <Menu style={styles.menu} desktop={true} width={320}>
-          <MenuItem secondaryText="&#8984;B">Bold</MenuItem>
-          <MenuItem secondaryText="&#8984;I">Italic</MenuItem>
-          <MenuItem secondaryText="&#8984;U">Underline</MenuItem>
-          <MenuItem secondaryText="Alt+Shift+5">Strikethrough</MenuItem>
-          <MenuItem secondaryText="&#8984;.">Superscript</MenuItem>
-          <MenuItem secondaryText="&#8984;,">Subscript</MenuItem>
-          <MenuDivider />
-          <MenuItem rightIcon={<ArrowDropRight />}>Paragraph styles</MenuItem>
-          <MenuItem rightIcon={<ArrowDropRight />}>Align</MenuItem>
-          <MenuItem rightIcon={<ArrowDropRight />}>Line spacing</MenuItem>
-          <MenuItem rightIcon={<ArrowDropRight />}>Numbered list</MenuItem>
-          <MenuItem rightIcon={<ArrowDropRight />}>List options</MenuItem>
-          <MenuDivider />
-          <MenuItem secondaryText="&#8984;/">Clear formatting</MenuItem>
-        </Menu>
+          <Menu style={styles.menu} desktop={true} width={320}>
+            <MenuItem secondaryText="&#8984;B">Bold</MenuItem>
+            <MenuItem secondaryText="&#8984;I">Italic</MenuItem>
+            <MenuItem secondaryText="&#8984;U">Underline</MenuItem>
+            <MenuItem secondaryText="Alt+Shift+5">Strikethrough</MenuItem>
+            <MenuItem secondaryText="&#8984;.">Superscript</MenuItem>
+            <MenuItem secondaryText="&#8984;,">Subscript</MenuItem>
+            <MenuDivider />
+            <MenuItem rightIcon={<ArrowDropRight />}>Paragraph styles</MenuItem>
+            <MenuItem rightIcon={<ArrowDropRight />}>Align</MenuItem>
+            <MenuItem rightIcon={<ArrowDropRight />}>Line spacing</MenuItem>
+            <MenuItem rightIcon={<ArrowDropRight />}>Numbered list</MenuItem>
+            <MenuItem rightIcon={<ArrowDropRight />}>List options</MenuItem>
+            <MenuDivider />
+            <MenuItem secondaryText="&#8984;/">Clear formatting</MenuItem>
+          </Menu>
 
-        <Menu style={styles.menu} desktop={true} width={320}>
-          <MenuItem insetChildren={true}>Single</MenuItem>
-          <MenuItem insetChildren={true}>1.15</MenuItem>
-          <MenuItem insetChildren={true}>Double</MenuItem>
-          <MenuItem checked={true} rightIcon={<ArrowDropRight />}>Custom: 1.2</MenuItem>
-          <MenuItem secondaryText="&#8984;.">Superscript</MenuItem>
-          <MenuItem secondaryText="&#8984;,">Subscript</MenuItem>
-          <MenuDivider />
-          <MenuItem>Add space before paragraph</MenuItem>
-          <MenuItem>Add space after paragraph</MenuItem>
-          <MenuDivider />
-          <MenuItem>Custom spacing...</MenuItem>
-        </Menu>
+          <Menu style={styles.menu} desktop={true} width={320}>
+            <MenuItem insetChildren={true}>Single</MenuItem>
+            <MenuItem insetChildren={true}>1.15</MenuItem>
+            <MenuItem insetChildren={true}>Double</MenuItem>
+            <MenuItem checked={true} rightIcon={<ArrowDropRight />}>Custom: 1.2</MenuItem>
+            <MenuItem secondaryText="&#8984;.">Superscript</MenuItem>
+            <MenuItem secondaryText="&#8984;,">Subscript</MenuItem>
+            <MenuDivider />
+            <MenuItem>Add space before paragraph</MenuItem>
+            <MenuItem>Add space after paragraph</MenuItem>
+            <MenuDivider />
+            <MenuItem>Custom spacing...</MenuItem>
+          </Menu>
 
-        <Menu style={styles.menu} desktop={true}>
-          <MenuItem>Show</MenuItem>
-          <MenuItem checked={true}>Grid lines</MenuItem>
-          <MenuItem insetChildren={true}>Page breaks</MenuItem>
-          <MenuItem checked={true}>Rules</MenuItem>
-        </Menu>
-
+          <Menu style={styles.menu} desktop={true}>
+            <MenuItem>Show</MenuItem>
+            <MenuItem checked={true}>Grid lines</MenuItem>
+            <MenuItem insetChildren={true}>Page breaks</MenuItem>
+            <MenuItem checked={true}>Rules</MenuItem>
+          </Menu>
+        </ReactTransitionGroup>
       </ComponentDoc>
     );
 
