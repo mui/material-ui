aa7a144c4b88c2c5226728abdacc31c9464610f2
Roman Kisin <roman@parleypro.com>
2015-07-11 21:17:21 -0700

    fix path to svg icons

--- src/snackbar.jsx
+++ src/snackbar.jsx
@@ -5,7 +5,7 @@ let Transitions = require('./styles/transitions');
 let ClickAwayable = require('./mixins/click-awayable');
 let FlatButton = require('./flat-button');
 let IconButton = require('./icon-button');
-let NavigationClose = require('svg-icons/navigation/close');
+let NavigationClose = require('./svg-icons/navigation/close');
 
 
 
