edac70b31c57e63ef84cd24bdd3f8591962d9032
Joe Walker <joe@chiton.local>
2015-07-17 13:44:37 +0100

    [TimePicker] Update display of 12h clock (Fixes #1173)

--- docs/src/app/components/pages/components/time-picker.jsx
+++ docs/src/app/components/pages/components/time-picker.jsx
@@ -32,6 +32,12 @@ let TimePickerPage = React.createClass({
             type: 'one of: ampm, 24hr',
             header: 'default: ampm',
             desc: 'Tells the component to display the picker in ampm (12hr) format or 24hr format.'
+          },
+          {
+            name: 'pedantic',
+            type: 'boolean',
+            header: 'default: false',
+            desc: 'It\'s technically more correct to refer to "12 noon" and "12 midnight" rather than "12 a.m." and "12 p.m." and it avoids real confusion between different locales. By default (for compatibility reasons) TimePicker uses (12 a.m./12 p.m.) To use (noon/midnight) set pedantic={true}.'
           }
         ]
       },
