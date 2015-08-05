27d6ad7e07b0330874ec7da847320a936f3f736a
Hai Nguyen <hai@call-em-all.com>
2015-08-05 09:25:35 -0500

    Added styles and immutability-helper to index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,12 @@
 module.exports = {
+  ColorManipulator: require('./color-manipulator'),
   CssEvent: require('./css-event'),
   Dom: require('./dom'),
   Events: require('./events'),
+  Extend: require('./extend'),
+  ImmutabilityHelper: require('./immutability-helper'),
   KeyCode: require('./key-code'),
   KeyLine: require('./key-line'),
-  ColorManipulator: require('./color-manipulator'),
-  Extend: require('./extend'),
   UniqueId: require('./unique-id'),
+  Styles: require('./styles'),
 };
