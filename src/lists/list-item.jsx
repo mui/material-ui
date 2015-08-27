e556a578ec3365e72df816c48f36f80bc839dc99
Michael Hahn <mwhahn@gmail.com>
2015-08-27 01:20:27 -0700

    Support passing style attributes down to primaryText and secondaryText

--- src/lists/list-item.jsx
+++ src/lists/list-item.jsx
@@ -47,7 +47,9 @@ const ListItem = React.createClass({
     rightIconButton: React.PropTypes.element,
     rightToggle: React.PropTypes.element,
     primaryText: React.PropTypes.node,
+    primaryTextStyle: React.PropTypes.object,
     secondaryText: React.PropTypes.node,
+    secondaryTextStyle: React.PropTypes.object,
     secondaryTextLines: React.PropTypes.oneOf([1, 2]),
   },
 
@@ -303,7 +305,7 @@ const ListItem = React.createClass({
 
     if (primaryText) {
       const secondaryTextElement = this._createTextElement(
-        styles.primaryText,
+        this.mergeAndPrefix(styles.primaryText, this.props.primaryTextStyle),
         primaryText,
         'primaryText'
       );
@@ -312,7 +314,7 @@ const ListItem = React.createClass({
 
     if (secondaryText) {
       const secondaryTextElement = this._createTextElement(
-        styles.secondaryText,
+        this.mergeAndPrefix(styles.secondaryText, this.props.secondaryTextStyle),
         secondaryText,
         'secondaryText'
       );
