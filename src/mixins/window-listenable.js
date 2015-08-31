f5ae5b09bb97010daea32458c901fa744dbfd0d7
Danny Richelieu <dannyrich21@yahoo.com>
2015-08-31 08:26:47 -0700

    Use let over var

--- src/mixins/window-listenable.js
+++ src/mixins/window-listenable.js
@@ -5,30 +5,30 @@ module.exports = {
 
   componentDidMount() {
     let listeners = this.windowListeners;
-    var stackListeners = this.stackWindowListeners;
+    let stackListeners = this.stackWindowListeners;
 
     for (let eventName in listeners) {
        let callbackName = listeners[eventName];
        Events.on(window, eventName, this[callbackName]);
     }
 
-    for (var eventName in stackListeners) {
-      var callbackName = stackListeners[eventName];
+    for (let eventName in stackListeners) {
+      let callbackName = stackListeners[eventName];
       Events.onstack(window, eventName, this[callbackName]);
     }
   },
 
   componentWillUnmount() {
     let listeners = this.windowListeners;
-    var stackListeners = this.stackWindowListeners;
+    let stackListeners = this.stackWindowListeners;
 
     for (let eventName in listeners) {
        let callbackName = listeners[eventName];
        Events.off(window, eventName, this[callbackName]);
     }
 
-    for (var eventName in stackListeners) {
-      var callbackName = stackListeners[eventName];
+    for (let eventName in stackListeners) {
+      let callbackName = stackListeners[eventName];
       Events.offstack(window, eventName, this[callbackName]);
     }
   },
