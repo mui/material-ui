173b6d725ebadbd3b8a4bc574af381a3e3943fba
Shaurya Arora <shaurya@Shauryas-MacBook-Pro.local>
2015-09-15 14:33:57 -0500

    Refactored theme.jsx component from PR [#797](https://github.com/callemall/material-ui/pull/797)
    Separated out decorator into src/styles/theme-decorator.js so that it no longer relies on another
    wrapper component.

--- src/styles/index.js
+++ src/styles/index.js
@@ -7,4 +7,5 @@ module.exports = {
   Typography: require('./typography'),
   LightRawTheme: require('./raw-themes/light-raw-theme'),
   DarkRawTheme: require('./raw-themes/dark-raw-theme'),
+  ThemeDecorator: require('./theme-decorator'),
 };
