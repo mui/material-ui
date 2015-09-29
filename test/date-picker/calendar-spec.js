1a1a56321b33c4fa2aeb16da21caa43f81cccf5d
yulricsequeira <yulrics@gmail.com>
2015-09-28 21:44:43 -0400

    Added test/date-picker dir which should have all the tests for the date-picker components. Added calendar-spec.js which has tests for the CalendarComponent. Added test to check the height of the dateDisplay component when no mode is provided

---
+++ test/date-picker/calendar-spec.js
@@ -0,0 +1,23 @@
+import Calendar from 'date-picker/calendar';
+import React from 'react';
+import injectTheme from '../fixtures/inject-theme';
+import DateDisplay  from 'date-picker/date-display';
+
+const TestUtils = React.addons.TestUtils;
+
+describe('Calendar', function() {
+    let ThemedCalendar;
+
+    beforeEach(() => {
+        ThemedCalendar = injectTheme(Calendar);
+    });
+
+    it(`should set the DateDisplay height to 64 if the mode prop is not provided 
+        (default mode is potrait)`, () => {
+        let render = TestUtils.renderIntoDocument(<ThemedCalendar />);
+        let dateDisplay = React.findDOMNode(
+            TestUtils.findRenderedComponentWithType(render, DateDisplay));
+
+        expect(dateDisplay.style.height).to.equal('64px');
+    });
+});
