9630a653071fcbb37c89bfa8e2b97ffde97caf6b
yulricsequeira <yulrics@gmail.com>
2015-09-28 22:00:49 -0400

    Added test to calendar-spec.js to check the height of dateDisplay if the mode is landscape and the weekCount is not 5 or 6

--- test/date-picker/calendar-spec.js
+++ test/date-picker/calendar-spec.js
@@ -2,6 +2,7 @@ import Calendar from 'date-picker/calendar';
 import React from 'react';
 import injectTheme from '../fixtures/inject-theme';
 import DateDisplay  from 'date-picker/date-display';
+import DateTime from 'utils/date-time';
 
 const TestUtils = React.addons.TestUtils;
 
@@ -28,5 +29,21 @@ describe('Calendar', function() {
             TestUtils.findRenderedComponentWithType(render, DateDisplay));
 
         expect(dateDisplay.style.height).to.equal('64px');
+    });
+
+    it(`should set the DateDisplay height to 198 if the weekCount is not equal 
+        to 5 or 6 and the mode prop is landscape`, () => {
+        let initialDate = new Date('February 01, 2015');
+        let weekCount = DateTime.getWeekArray(initialDate);
+
+        expect(weekCount).to.not.equal(5);
+        expect(weekCount).to.not.equal(6);
+
+        let render = TestUtils.renderIntoDocument(
+            <ThemedCalendar mode='landscape' initialDate={initialDate}/>);
+        let dateDisplay = React.findDOMNode(
+            TestUtils.findRenderedComponentWithType(render, DateDisplay));
+
+        expect(dateDisplay.style.height).to.equal('198px');
     });
 });
