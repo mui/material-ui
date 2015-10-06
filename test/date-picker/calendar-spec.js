7b43099863e87c306f436f9ee4218d13c5592847
yulricsequeira <yulrics@gmail.com>
2015-10-05 22:39:51 -0400

    Test to check if next month button is enabled if the current month is before the maxDate prop

--- test/date-picker/calendar-spec.js
+++ test/date-picker/calendar-spec.js
@@ -1,3 +1,6 @@
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 import Calendar from 'date-picker/calendar';
 import injectTheme from '../fixtures/inject-theme';
 import React from 'react';
@@ -6,7 +9,7 @@ import IconButton from 'icon-button';
 
 const TestUtils = React.addons.TestUtils;
 
-describe(`Calendar`, () => {
+describe.only(`Calendar`, () => {
     let ThemedCalendar;
 
     beforeEach(() => {
@@ -46,23 +49,21 @@ describe(`Calendar`, () => {
             expect(renderedCalendarToolbar.props.nextMonth).to.be.false;
         });
 
-        it.only(`should reenable the next month button when the current month is before the maxDate prop`, () => {
+        it(`should initially enable the next month button if the current month is before the maxDate prop`, () => {
             let initialDate = new Date();
             let maxDate = new Date(initialDate.toDateString());
+            maxDate.setMonth(maxDate.getMonth() + 1);
 
             let render = TestUtils.renderIntoDocument(
                 <ThemedCalendar 
                     initialDate={initialDate}
                     maxDate={maxDate} />
             );
-            let prevMonthButton = React.findDOMNode(
-                TestUtils.scryRenderedComponentWithType(render, IconButton)[0]);
-            TestUtils.Simulate.touchTap(prevMonthButton);
 
-            let CalendarToolbar = 
+            let renderedCalendarToolbar = 
                 TestUtils.findRenderedComponentWithType(render, CalendarToolbar);
 
-            expect(CalendarToolbar.props.nextButton).to.be.true;
+            expect(renderedCalendarToolbar.props.nextMonth).to.be.true;
         });
     });
 });
