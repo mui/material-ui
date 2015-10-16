acf069e1e3e8a87421426f22d9d4dd31129604f3
Öjvind Otterbjörk <ojvind@otterbjork.com>
2015-10-16 14:40:35 +0200

    tcne-theme updated

--- src/styles/raw-themes/tcne-raw-theme.js
+++ src/styles/raw-themes/tcne-raw-theme.js
@@ -16,12 +16,12 @@ module.exports = {
     primary2Color: Colors.caffelatte,
     primary3Color: Colors.lightBlack,
     accent1Color: Colors.pesca,
-    //accent2Color: Colors.grey100,
-    //accent3Color: Colors.grey500,
+    accent2Color: Colors.grey100,
+    accent3Color: Colors.grey500,
     textColor: Colors.darkBlack,
     alternateTextColor: Colors.white,
     canvasColor: Colors.white,
     borderColor: Colors.grey300,
-    disabledColor: ColorManipulator.fade(Colors.darkBlack, 0.3),
+    disabledColor: ColorManipulator.fade(Colors.darkBlack, 0.3)
   },
 };
