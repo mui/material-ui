e917df7615ea6d1236ff019bdbb6dd82f4469b37
Öjvind Otterbjörk <ojvind@otterbjork.com>
2015-10-16 14:11:47 +0200

    Remove border on generated buttons

--- src/enhanced-button.jsx
+++ src/enhanced-button.jsx
@@ -151,7 +151,8 @@ const EnhancedButton = React.createClass({
     } = this.props;
 
     const mergedStyles = this.mergeAndPrefix({
-      border: 10,
+      // This is because we want to set this ourselves
+      //      border: 10,
       background: 'none',
       boxSizing: 'border-box',
       display: 'inline-block',
