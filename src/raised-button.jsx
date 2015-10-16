59a85303ac3d160b814e45c7267e2b142145eb20
Jon Haygood <haygoodjon@gmail.com>
2015-10-16 11:34:05 -0400

    Correct invalid background-color property value
    What are we going for here? - transparent, inherit, or palette.canvasColor all would make sense to me
    
    http://stackoverflow.com/questions/8739665/is-background-colornone-valid-css

--- src/raised-button.jsx
+++ src/raised-button.jsx
@@ -111,7 +111,7 @@ const RaisedButton = React.createClass({
     let amount = (this.props.primary || this.props.secondary) ? 0.4 : 0.08;
     let styles = {
       root: {
-        backgroundColor: 'none',
+        backgroundColor: 'transparent',
         display: 'inline-block',
         minWidth: this.props.fullWidth ? '100%' : this.getThemeButton().minWidth,
         height: this.getThemeButton().height,
