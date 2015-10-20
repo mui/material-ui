ebbd08f6bb68949dadd8d07aee48a2bdd12d419a
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2015-10-21 00:55:06 +0300

    Make 46px less magic

--- src/toggle.jsx
+++ src/toggle.jsx
@@ -79,7 +79,7 @@ let Toggle = React.createClass({
       },
       label: {
         color: this.props.disabled ? this.getTheme().labelDisabledColor : this.getTheme().labelColor,
-        width: 'calc(100% - 46px)',
+        width: 'calc(100% - ' + (toggleTrackWidth + 10) + 'px)',
       },
     };
 
