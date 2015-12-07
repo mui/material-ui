9f68f0e878daa2056aa6e0f3010ca2f4e9d84087
Neil Gabbadon <neil.gabbadon@emikra.com>
2015-12-07 00:36:39 -0500

    Made fixes relating to DOM onFocus, onBlur, onChange events, tab indexes, and improved docs

--- test/auto-complete-spec.js
+++ test/auto-complete-spec.js
@@ -1,61 +1,66 @@
 import React from 'react';
 import ReactTestUtils from 'react-addons-test-utils';
 import ReactDOM from 'react-dom';
-
-import { spy } from 'sinon';
-
+import {spy} from 'sinon';
 import AutoComplete from 'auto-complete';
 import TextField from 'text-field';
-
+import ListItem from 'lists/list-item';
 import injectTheme from './fixtures/inject-theme';
 
+let injectTapEventPlugin = require('react-tap-event-plugin');
+console.log('test');
 describe('AutoComplete', () => {
 
   let ThemedAutoComplete;
- 
+
   beforeEach(() => {
+    injectTapEventPlugin();
     ThemedAutoComplete = injectTheme(AutoComplete);
   });
 
   it(`should not throw an error when dataSource prop is undefined`, () => {
-
     expect(() => {
       ReactTestUtils.renderIntoDocument(
         <ThemedAutoComplete />
       );
     }).to.not.throw(Error);
-
   });
 
   it(`should bubble 'onChange' event`, () => {
 
-  	var onChange = spy();
+    const onChange = spy();
 
-  	let rendered = ReactTestUtils.renderIntoDocument(
-  		<ThemedAutoComplete onChange={onChange} />
+    let rendered = ReactTestUtils.renderIntoDocument(
+  		<ThemedAutoComplete dataSource={['Red', 'Blue', 'Green']} onChange={onChange} />
   	);
 
     const renderedTextField = ReactTestUtils.findRenderedComponentWithType(rendered, TextField);
-    const textFieldNode = ReactDOM.findDOMNode(renderedTextField);
     const inputNode = renderedTextField._getInputNode();
 
-    inputNode.value = 'ChangedValue';
+    inputNode.value = 'R';
     ReactTestUtils.Simulate.change(inputNode);
 
-    assert(onChange.calledOnce, 'onChange.calledOnce');
+    const renderedListItems = ReactTestUtils.scryRenderedComponentsWithType(rendered, ListItem);
+    const listItemNode = ReactDOM.findDOMNode(renderedListItems[0]);
+    console.log(listItemNode);
+    ReactTestUtils.Simulate.touchTap(listItemNode);
+
+    setTimeout(() => {
+      assert(onChange.calledOnce, 'onChange.calledOnce');
+    }, 100);
 
   });
 
   it(`should bubble 'onFocus' event`, () => {
 
-    var onFocus = spy();
+    const onFocus = spy();
 
     let rendered = ReactTestUtils.renderIntoDocument(
       <ThemedAutoComplete onFocus={onFocus} />
     );
 
     const renderedTextField = ReactTestUtils.findRenderedComponentWithType(rendered, TextField);
-    const textFieldNode = ReactDOM.findDOMNode(renderedTextField);
+    // const textFieldNode = ReactDOM.findDOMNode(renderedTextField);
     const inputNode = renderedTextField._getInputNode();
 
     ReactTestUtils.Simulate.focus(inputNode);
@@ -66,17 +71,17 @@ describe('AutoComplete', () => {
 
   it(`should bubble 'onBlur' event`, () => {
 
-    var onBlur = spy();
+    const onBlur = spy();
 
     let rendered = ReactTestUtils.renderIntoDocument(
       <ThemedAutoComplete onBlur={onBlur} />
     );
 
     const renderedTextField = ReactTestUtils.findRenderedComponentWithType(rendered, TextField);
-    const textFieldNode = ReactDOM.findDOMNode(renderedTextField);
+    // const textFieldNode = ReactDOM.findDOMNode(renderedTextField);
     const inputNode = renderedTextField._getInputNode();
 
-    ReactTestUtils.Simulate.focus(inputNode);
+    ReactTestUtils.Simulate.blur(inputNode);
 
     assert(onBlur.calledOnce, 'onBlur.calledOnce');
 
