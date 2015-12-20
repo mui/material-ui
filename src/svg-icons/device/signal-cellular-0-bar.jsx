55bd0b7d88c9f9fde0cc28caf420b3399d6dc071
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-12-20 12:55:34 +0100

    [SvgIcon] Run the generation script

--- src/svg-icons/device/signal-cellular-0-bar.jsx
+++ src/svg-icons/device/signal-cellular-0-bar.jsx
@@ -9,7 +9,7 @@ const DeviceSignalCellular0Bar = React.createClass({
   render() {
     return (
       <SvgIcon {...this.props}>
-        <path fill-opacity=".3" d="M2 22h20V2z"/>
+        <path fillOpacity=".3" d="M2 22h20V2z"/>
       </SvgIcon>
     );
   }
