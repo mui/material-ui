c13860d1251064a8b7776323c245b6d41eacc16a
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-12-20 11:57:29 +0100

    [SvgIcon] Lint code generation

--- icon-builder/tpl/SvgIcon.js
+++ icon-builder/tpl/SvgIcon.js
@@ -1,6 +1,6 @@
-const React = require('react');
-const PureRenderMixin = require('react-addons-pure-render-mixin');
-const SvgIcon = {{{ muiRequireStmt }}};
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import SvgIcon from '{{{ muiRequireStmt }}}';
 
 const {{className}} = React.createClass({
 
@@ -16,4 +16,4 @@ const {{className}} = React.createClass({
 
 });
 
-module.exports = {{className}};
+export default {{className}};
