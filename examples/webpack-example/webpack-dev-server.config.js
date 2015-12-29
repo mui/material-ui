ae030908f400478f6611b6fb60a153459728fddf
Araphel <bartekus@gmail.com>
2015-12-29 13:21:34 -0700

    Webpack Example Babel 6 Upgrade \n - Babel 6 upgrade - Added es2015, react & stage-0 presets - Configured .babelrc to utilize es2015, react & stage-1 - Adjusted webpack dev-server & production config to reflect babel upgrade changes - Upgraded all other modules to most recent version - Start & Build tested ok

--- examples/webpack-example/webpack-dev-server.config.js
+++ examples/webpack-example/webpack-dev-server.config.js
@@ -54,7 +54,7 @@ var config = {
       {
         //React-hot loader and
         test: /\.(js|jsx)$/,  //All .js and .jsx files
-        loaders: ['react-hot','babel-loader?optional=runtime&stage=0'], //react-hot is like browser sync and babel loads jsx and es6-7
+        loaders: ['react-hot', 'babel'], //react-hot is like browser sync and babel loads jsx and es6-7
         exclude: [nodeModulesPath]
       }
     ]
