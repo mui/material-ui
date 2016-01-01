4b1cb799da1d0a44449e7539ab66c575b176259e
Ali Taheri <ali.taheri.m@gmail.com>
2016-01-01 23:22:25 +0330

    Add example for custom width of dialog
    This seems to be a very common use-case. Specially for mobile
    applications where space is limited. So instead of having to
    look around for answers, users can refer to the documentations.

--- docs/src/app/components/pages/components/Dialog/Page.jsx
+++ docs/src/app/components/pages/components/Dialog/Page.jsx
@@ -5,19 +5,24 @@ import MarkdownElement from '../../../MarkdownElement';
 
 import dialogReadmeText from './README';
 import DialogExampleSimple from './ExampleSimple';
-import DialogExampleSimpleCode from '!raw!./ExampleSimple';
+import dialogExampleSimpleCode from '!raw!./ExampleSimple';
 import DialogExampleModal from './ExampleModal';
-import DialogExampleModalCode from '!raw!./ExampleModal';
+import dialogExampleModalCode from '!raw!./ExampleModal';
+import DialogExampleCustomWidth from './ExampleCustomWidth';
+import dialogExampleCustomWidthCode from '!raw!./ExampleCustomWidth';
 import dialogCode from '!raw!material-ui/lib/dialog';
 
 const DialogPage = () => (
   <div>
     <MarkdownElement text={dialogReadmeText} />
-    <CodeExample code={DialogExampleSimpleCode}>
+    <CodeExample code={dialogExampleSimpleCode}>
       <DialogExampleSimple />
     </CodeExample>
-    <CodeExample code={DialogExampleModalCode}>
+    <CodeExample code={dialogExampleModalCode}>
       <DialogExampleModal />
+    </CodeExample>
+    <CodeExample code={dialogExampleCustomWidthCode}>
+      <DialogExampleCustomWidth />
     </CodeExample>
     <PropTypeDescription code={dialogCode}/>
   </div>
