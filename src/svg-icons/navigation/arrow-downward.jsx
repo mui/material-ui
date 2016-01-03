5274dc17dd6f7b4043145206a8751c88c43e958f
kodermax <kodermax@gmail.com>
2016-01-04 00:10:33 +0300

    Remove path fill.

--- src/svg-icons/navigation/arrow-downward.jsx
+++ src/svg-icons/navigation/arrow-downward.jsx
@@ -9,7 +9,7 @@ const NavigationArrowDownward = React.createClass({
   render() {
     return (
       <SvgIcon {...this.props}>
-        <path fill="#010101" d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"/>
+        <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"/>
       </SvgIcon>
     );
   }
