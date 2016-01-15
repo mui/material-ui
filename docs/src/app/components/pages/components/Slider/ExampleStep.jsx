f0e926fc816f84c1d7bbcbd96eae39469f657093
Alex Drel <alexdrel@gmail.com>
2016-01-15 14:46:29 +0200

    Linter fix

--- docs/src/app/components/pages/components/Slider/ExampleStep.jsx
+++ docs/src/app/components/pages/components/Slider/ExampleStep.jsx
@@ -4,7 +4,9 @@ import Slider from 'material-ui/lib/slider';
 const SliderExampleStep = () => (
   <div>
     <Slider step={0.10} value={.5}/>
-    <Slider defaultValue={50} step={1} min={0} max={100} popover={true}/>
+    <Slider defaultValue={50}
+      step={1} min={0} max={100}
+      popover={true}/>
   </div>
 );
 
