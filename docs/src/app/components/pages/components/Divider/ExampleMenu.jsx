ebb361c1a4a2473cd2a32c8dbe475feaf89b64fb
mbrookes <github@nospam.33m.co>
2016-01-16 12:30:56 +0000

    [Docs] Divider - Add title and description to examples

--- docs/src/app/components/pages/components/Divider/ExampleMenu.jsx
+++ docs/src/app/components/pages/components/Divider/ExampleMenu.jsx
@@ -3,11 +3,16 @@ import Divider from 'material-ui/lib/divider';
 import Menu from 'material-ui/lib/menus/menu';
 import MenuItem from 'material-ui/lib/menus/menu-item';
 
+const style = {
+  // Without this, the menu overflows the CodeExample container.
+  float: 'left',
+};
+
 const DividerExampleMenu = () => (
-  <Menu desktop={true} width={200}>
+  <Menu desktop={true} style={style}>
     <MenuItem primaryText="Settings" />
     <MenuItem primaryText="Help & feedback" />
-    <Divider/>
+    <Divider />
     <MenuItem primaryText="Sign out" />
   </Menu>
 );
--- docs/src/app/components/pages/components/Divider/Page.jsx
+++ docs/src/app/components/pages/components/Divider/Page.jsx
@@ -12,17 +12,37 @@ import DividerExampleMenu from './ExampleMenu';
 import dividerExampleMenuCode from '!raw!./ExampleMenu';
 import dividerCode from '!raw!material-ui/lib/divider';
 
+const descriptions = {
+  simple: 'Here, `Divider` is used to separate [TextField](/#/components/text-field) components. ' +
+  'It defaults to "full-bleed" (full width).',
+  inset: 'The `inset` parameter allows the divider to to align with inset content, ' +
+  'such as inset [List](http://localhost:3000/#/components/list) components.',
+  menu: '`Divider` can alo be used in [Menus](/#/components/menu).',
+};
+
 const DividerPage = () => {
   return (
     <div>
       <MarkdownElement text={dividerReadmeText} />
-      <CodeExample code={dividerExampleFormCode}>
+      <CodeExample
+        title="Form divider"
+        description={descriptions.simple}
+        code={dividerExampleFormCode}
+      >
         <DividerExampleForm />
       </CodeExample>
-      <CodeExample code={dividerExampleListCode}>
+      <CodeExample
+        title="Inset divider"
+        description={descriptions.inset}
+        code={dividerExampleListCode}
+      >
         <DividerExampleList />
       </CodeExample>
-      <CodeExample code={dividerExampleMenuCode}>
+      <CodeExample
+        title="Menu divider"
+        description={descriptions.menu}
+        code={dividerExampleMenuCode}
+      >
         <DividerExampleMenu />
       </CodeExample>
       <PropTypeDescription code={dividerCode}/>
