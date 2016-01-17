b85900cbd933bd6578e9e0dcc849da2b5b17beae
mbrookes <github@nospam.33m.co>
2016-01-17 17:43:41 +0000

    [Docs] IconButton - Add title and description to examples

--- docs/src/app/components/pages/components/IconButton/ExampleSimple.jsx
+++ docs/src/app/components/pages/components/IconButton/ExampleSimple.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import IconButton from 'material-ui/lib/icon-button';
 
 const IconButtonExampleSimple = () => (
-  <IconButton iconClassName="muidocs-icon-custom-github" />
+  <div>
+    <IconButton iconClassName="muidocs-icon-custom-github" />
+    <IconButton iconClassName="muidocs-icon-custom-github" disabled={true} />
+  </div>
 );
 
 export default IconButtonExampleSimple;
