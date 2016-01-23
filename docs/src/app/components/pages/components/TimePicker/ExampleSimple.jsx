0f2af12cc72227f4ff788ccb6f2b67e12371ad96
mbrookes <github@nospam.33m.co>
2016-01-23 01:13:30 +0000

    [Docs] TimePicker - Add title & description to examples.

--- docs/src/app/components/pages/components/TimePicker/ExampleSimple.jsx
+++ docs/src/app/components/pages/components/TimePicker/ExampleSimple.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import TimePicker from 'material-ui/lib/time-picker/time-picker';
 
 const TimePickerExampleSimple = () => (
-  <TimePicker
-    format="ampm"
-    hintText="12hr Format"
-  />
+  <div>
+    <TimePicker
+      hintText="12hr Format"
+    />
+    <TimePicker
+      format="24hr"
+      hintText="24hr Format"
+    />
+  </div>
 );
 
 export default TimePickerExampleSimple;
