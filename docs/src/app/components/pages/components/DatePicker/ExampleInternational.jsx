18d8a4c40aa68388c185505654b056b6990225b5
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-01-23 18:45:20 +0100

    [Docs] Move DateTimeFormat polyfill to the example

--- docs/src/app/components/pages/components/DatePicker/ExampleInternational.jsx
+++ docs/src/app/components/pages/components/DatePicker/ExampleInternational.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import DatePicker from 'material-ui/lib/date-picker/date-picker';
+import areIntlLocalesSupported from 'intl-locales-supported';
+
+let DateTimeFormat;
+
+// Use the native Intl if available
+if (areIntlLocalesSupported('fr')) {
+  DateTimeFormat = global.Intl.DateTimeFormat;
+} else {
+  const IntlPolyfill = require('intl');
+  require('intl/locale-data/jsonp/fr');
+
+  DateTimeFormat = IntlPolyfill.DateTimeFormat;
+}
 
 const DatePickerExampleInternational = () => (
   <DatePicker
     hintText="fr version"
-    DateTimeFormat={Intl.DateTimeFormat}
+    DateTimeFormat={DateTimeFormat}
     // Intl is supported by most modern browsers, see http://caniuse.com/#search=intl
     // for browsers that don't support it use this polyfill https://github.com/andyearnshaw/Intl.js
     wordings={{ok: 'OK', cancel: 'Annuler'}}
