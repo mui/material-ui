164e29adecb85092f666a34745010ab4912292c3
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-02-14 17:27:26 +0100

    [Core] Remove the WindowListenable mixin

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,16 +1,13 @@
 import ClickAwayable from './click-awayable';
-import WindowListenable from './window-listenable';
 import StylePropable from './style-propable';
 import StyleResizable from './style-resizable';
 
 export {ClickAwayable};
-export {WindowListenable};
 export {StylePropable};
 export {StyleResizable};
 
 export default {
   ClickAwayable,
-  WindowListenable,
   StylePropable,
   StyleResizable,
 };
