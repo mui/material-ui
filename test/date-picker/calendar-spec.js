7a2c25e539195a27a450e651805f354a4b22a519
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-02-19 22:07:55 +0100

    [eslint] Upgrade to v2

--- test/date-picker/calendar-spec.js
+++ test/date-picker/calendar-spec.js
@@ -10,15 +10,15 @@ import injectTheme from '../fixtures/inject-theme';
 import DateTime from 'utils/date-time';
 import TestUtils from 'react-addons-test-utils';
 
-describe(`Calendar`, () => {
+describe('Calendar', () => {
   let ThemedCalendar;
 
   beforeEach(() => {
     ThemedCalendar = injectTheme(Calendar);
   });
 
-  describe(`Next Month Button`, () => {
-    it(`should initially be disabled if the current month is the same as the month in the maxDate prop`, () => {
+  describe('Next Month Button', () => {
+    it('should initially be disabled if the current month is the same as the month in the maxDate prop', () => {
       const initialDate = new Date(1448967059892); // Tue, 01 Dec 2015 10:50:59 GMT
       const maxDate = new Date(initialDate.toDateString());
 
@@ -36,7 +36,7 @@ describe(`Calendar`, () => {
       expect(renderedCalendarToolbar.props.nextMonth).to.be.false;
     });
 
-    it(`should initially be disabled if the current month is after the month in the maxDate prop`, () => {
+    it('should initially be disabled if the current month is after the month in the maxDate prop', () => {
       const initialDate = new Date(1448967059892); // Tue, 01 Dec 2015 10:50:59 GMT
       let maxDate = new Date(initialDate.toDateString());
       maxDate = DateTime.addMonths(maxDate, -1);
@@ -55,7 +55,7 @@ describe(`Calendar`, () => {
       expect(renderedCalendarToolbar.props.nextMonth).to.be.false;
     });
 
-    it(`should initially enable the next month button if the current month is before the maxDate prop`, () => {
+    it('should initially enable the next month button if the current month is before the maxDate prop', () => {
       const initialDate = new Date(1448967059892); // Tue, 01 Dec 2015 10:50:59 GMT
       let maxDate = new Date(initialDate.toDateString());
       maxDate = DateTime.addMonths(maxDate, 1);
@@ -75,7 +75,7 @@ describe(`Calendar`, () => {
       expect(renderedCalendarToolbar.props.nextMonth).to.be.true;
     });
 
-    it(`should reenable the next month button when the current month is before the maxDate prop`, () => {
+    it('should reenable the next month button when the current month is before the maxDate prop', () => {
       const initialDate = new Date(1448967059892); // Tue, 01 Dec 2015 10:50:59 GMT
       const maxDate = new Date(initialDate.toDateString());
 
@@ -97,7 +97,7 @@ describe(`Calendar`, () => {
       expect(renderedCalendarToolbar.props.nextMonth).to.be.true;
     });
 
-    it(`should redisable the next month button when the current month is the same as the maxDate prop`, () => {
+    it('should redisable the next month button when the current month is the same as the maxDate prop', () => {
       const initialDate = new Date(1448967059892); // Tue, 01 Dec 2015 10:50:59 GMT
       let maxDate = new Date(initialDate.toDateString());
       maxDate = DateTime.addMonths(maxDate, 1);
@@ -159,7 +159,7 @@ describe(`Calendar`, () => {
       expect(calendarToolbar.props.prevMonth).to.be.false;
     });
 
-    it(`should initially enable the previous month button if the current month is after the minDate month prop`, () => {
+    it('should initially enable the previous month button if the current month is after the minDate month prop', () => {
       const initialDate = new Date(1448967059892); // Tue, 01 Dec 2015 10:50:59 GMT
       let minDate = new Date(initialDate.toDateString());
       minDate = DateTime.addMonths(initialDate, -1);
@@ -177,7 +177,7 @@ describe(`Calendar`, () => {
       expect(calendarToolbar.props.prevMonth).to.be.true;
     });
 
-    it(`should enable the previous month button when the current month is after the minDate month prop`, () => {
+    it('should enable the previous month button when the current month is after the minDate month prop', () => {
       const initialDate = new Date(1448967059892); // Tue, 01 Dec 2015 10:50:59 GMT
       const minDate = new Date(initialDate.toDateString());
 
@@ -197,7 +197,7 @@ describe(`Calendar`, () => {
       expect(calendarToolbar.props.prevMonth).to.be.true;
     });
 
-    it(`should disable the previous month button when the current month is the same as the minDate month prop`, () => {
+    it('should disable the previous month button when the current month is the same as the minDate month prop', () => {
       const initialDate = new Date(1448967059892); // Tue, 01 Dec 2015 10:50:59 GMT
       let minDate = new Date(initialDate.toDateString());
       minDate = DateTime.addMonths(minDate, -1);
--- test/es6-module-exports-spec.js
+++ test/es6-module-exports-spec.js
@@ -11,23 +11,23 @@ import ImportColorManipulator from 'utils/color-manipulator';
 const RequireColorManipulator = require('utils/color-manipulator');
 
 describe('require() style import of ', () => {
-  it(`Divider component should not fail when rendering`, () => {
+  it('Divider component should not fail when rendering', () => {
     expect(() => {
       TestUtils.renderIntoDocument(<Divider />);
     }).to.not.throw(Error);
   });
 
-  it(`ActionAccessibility component should not fail when rendering`, () => {
+  it('ActionAccessibility component should not fail when rendering', () => {
     expect(() => {
       TestUtils.renderIntoDocument(<ActionAccessibility />);
     }).to.not.throw(Error);
   });
 
-  it(`getMuiTheme should have same result as ES6 style import`, () => {
+  it('getMuiTheme should have same result as ES6 style import', () => {
     expect(RequireGetMuiTheme).to.eql(ImportGetMuiTheme);
   });
 
-  it(`ColorManipulator should have same result as ES6 style import`, () => {
+  it('ColorManipulator should have same result as ES6 style import', () => {
     expect(RequireColorManipulator).to.eql(ImportColorManipulator);
   });
 });
