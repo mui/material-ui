ed993b1f00963987d36c9f2fe2c9e80112def893
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2016-03-02 13:09:05 -0500

    [Snackbar] make onRequestClose optional
    - closes #3485 make onRequestClose optional
    - add nodemon as dev dependency
    - add a simple test for Snackbar

---
+++ test/unit/Snackbar.spec.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {assert} from 'chai';
+import Snackbar from 'src/snackbar';
+
+describe('<Snackbar />', () => {
+  it('renders hidden by default', () => {
+    const wrapper = shallow(
+      <Snackbar open={false} message="Message" autoHideDuration={4000} />
+    );
+
+    assert.equal(
+      wrapper.find('div').at(0).node.props.style.visibility,
+      'hidden',
+      'visibility should be hidden'
+    );
+  });
+});
