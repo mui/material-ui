fc85ea761b77bb3f9bbb5487962572bf45e13f31
mbrookes <github@nospam.33m.co>
2016-04-27 20:38:03 +0100

    [colorManipulator] Use noddy code for the hard-of-reading :P

--- src/utils/colorManipulator.js
+++ src/utils/colorManipulator.js
@@ -7,7 +7,13 @@
  * @returns {number} A number in the range [min, max]
  */
 function clamp(value, min, max) {
-  return Math.min(Math.max(value, min), max);
+  if (value < min) {
+    return min;
+  }
+  if (value > max) {
+    return max;
+  }
+  return value;
 }
 
 /**
