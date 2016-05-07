1497ecc1ca09f5c7fe967b687993c53c6b88c958
piglovesyou <thepiglovesyou@gmail.com>
2016-05-07 18:20:47 +0900

    Add basic hotkey-focusing feature to Menu

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -10,6 +10,7 @@ import propTypes from '../utils/propTypes';
 import List from '../List/List';
 import deprecated from '../utils/deprecatedPropType';
 import warning from 'warning';
+import {HotKeyHolder} from './menuUtils';
 
 function getStyles(props, context) {
   const {
@@ -207,6 +208,8 @@ class Menu extends Component {
       isKeyboardFocused: props.initiallyKeyboardFocused,
       keyWidth: props.desktop ? 64 : 56,
     };
+
+    this.hotKeyHolder = new HotKeyHolder();
   }
 
   componentDidMount() {
@@ -376,7 +379,8 @@ class Menu extends Component {
 
   handleKeyDown = (event) => {
     const filteredChildren = this.getFilteredChildren(this.props.children);
-    switch (keycode(event)) {
+    const key = keycode(event);
+    switch (key) {
       case 'down':
         event.preventDefault();
         this.incrementKeyboardFocusIndex(filteredChildren);
@@ -396,9 +400,34 @@ class Menu extends Component {
         event.preventDefault();
         this.decrementKeyboardFocusIndex();
         break;
+      default:
+        if (key.length === 1) {
+          const hotKeys = this.hotKeyHolder.append(key);
+          if (this.setFocusIndexStartsWith(hotKeys)) {
+            event.preventDefault();
+          }
+        }
     }
     this.props.onKeyDown(event);
   };
+
+  setFocusIndexStartsWith(keys) {
+    let foundIndex = -1;
+    React.Children.forEach(this.props.children, (child, index) => {
+      if (foundIndex >= 0) {
+        return;
+      }
+      const {primaryText} = child.props;
+      if (typeof primaryText === 'string' && new RegExp(`^${keys}`, 'i').test(primaryText)) {
+        foundIndex = index;
+      }
+    });
+    if (foundIndex >= 0) {
+      this.setFocusIndex(foundIndex, true);
+      return true;
+    }
+    return false;
+  }
 
   handleMenuItemTouchTap(event, item, index) {
     const children = this.props.children;
