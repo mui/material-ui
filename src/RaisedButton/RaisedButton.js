a302366f13406c698d3c4db0aa14f658b020d15f
leonuh <leonuh@gmail.com>
2016-05-10 11:15:11 +0200

    [RaisedButton] fullWidth fix

--- src/RaisedButton/RaisedButton.js
+++ src/RaisedButton/RaisedButton.js
@@ -61,6 +61,7 @@ function getStyles(props, context, state) {
     root: {
       display: 'inline-block',
       transition: transitions.easeOut(),
+      width: fullWidth ? '100%' : 'auto',
     },
     button: {
       position: 'relative',
