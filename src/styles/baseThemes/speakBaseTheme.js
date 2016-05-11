859146f9f2be5b9a59ae3cd6324dba308bf810cb
dk\anb <anb@sitecore.net>
2016-05-11 15:40:38 +0200

    Divider

--- src/styles/baseThemes/speakBaseTheme.js
+++ src/styles/baseThemes/speakBaseTheme.js
@@ -13,6 +13,7 @@ import {
   scThemeBasicDark,
   scThemeBasicWhite,
   scBackDropColor,
+  scThemeBasicDefault,
 } from '../colors';
 import {fade} from '../../utils/colorManipulator';
 import spacing from '../spacing';
@@ -36,7 +37,7 @@ export default {
     textColor: scThemeForeground,
     alternateTextColor: scThemeBasicWhite,    
     canvasColor: scThemeBackground,
-    borderColor: grey300,
+    borderColor: scThemeBasicDefault,
     disabledColor: fade(darkBlack, 0.3),
     pickerHeaderColor: cyan500,
     clockCircleColor: fade(darkBlack, 0.07),
@@ -44,7 +45,6 @@ export default {
     inverseColor: scThemeBasicDark,
     lightBackground: scThemeBasicWhite,
     darkBackground: scThemeBasicXXDark,
-    backDropColor: scBackDropColor,
-    
+    backDropColor: scBackDropColor,        
   },
 };
