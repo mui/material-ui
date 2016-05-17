aee5ec7433e67db98070d093dcda49a7c7de413b
dk\anb <anb@sitecore.net>
2016-05-17 10:51:13 +0200

    checkbox moved back

--- docs/src/app/components/pages/components/GridList/ExampleSimple.js
+++ docs/src/app/components/pages/components/GridList/ExampleSimple.js
@@ -73,6 +73,62 @@ const tilesData = [
     author: 'BkrmadtyaKarki',
     id:8,
   },
+  {
+    img: 'images/grid-list/00-52-29-429_640.jpg',
+    title: 'Breakfast',
+    url: 'http://www.google.com',
+    author: 'jill111',
+    id:9,
+  },
+  {
+    img: 'images/grid-list/burger-827309_640.jpg',
+    title: 'Tasty burger',
+    url: 'http://www.google.com',
+    author: 'pashminu',
+    id:10,
+  },
+  {
+    img: 'images/grid-list/camera-813814_640.jpg',
+    title: 'Camera',
+    url: 'http://www.google.com',
+    author: 'Danson67',
+    id:11,
+  },
+  {
+    img: 'images/grid-list/morning-819362_640.jpg',
+    title: 'Morning',
+    url: 'http://www.google.com',
+    author: 'fancycrave1',
+    id:12,
+  },
+  {
+    img: 'images/grid-list/hats-829509_640.jpg',
+    title: 'Hats',
+    url: 'http://www.google.com',
+    author: 'Hans',
+    id:13,
+  },
+  {
+    img: 'images/grid-list/honey-823614_640.jpg',
+    title: 'Honey',
+    url: 'http://www.google.com',
+    author: 'fancycravel',
+    id:14,
+  },
+  {
+    img: 'images/grid-list/vegetables-790022_640.jpg',
+    title: 'Vegetables',
+    url: 'http://www.google.com',
+    author: 'jill111',
+    id:15,
+  },
+  {
+    img: 'images/grid-list/water-plant-821293_640.jpg',
+    title: 'Water plant',
+    url: 'http://www.google.com',
+    author: 'BkrmadtyaKarki',
+    id:16,
+  },
 ];
 
 const GridListExampleSimple = () => (
