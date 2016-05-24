6b359017c2d7f4318a36f252a002d3ecabc59434
mbrookes <github@nospam.33m.co>
2016-05-24 22:37:43 +0100

    [Popver] Fix typo
    Closes #4324

--- docs/src/app/components/pages/components/FlatButton/ExampleSimple.js
+++ docs/src/app/components/pages/components/FlatButton/ExampleSimple.js
@@ -3,7 +3,7 @@ import FlatButton from 'material-ui/FlatButton';
 
 const FlatButtonExampleSimple = () => (
   <div>
-    <FlatButton label="Default" />
+    <FlatButton label="Default" style={{width: '200px'}}/>
     <FlatButton label="Primary" primary={true} />
     <FlatButton label="Secondary" secondary={true} />
     <FlatButton label="Disabled" disabled={true} />
