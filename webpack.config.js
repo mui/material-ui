aab20f1fbc77366a828a2015111a8f7a2e0605bc
ANich <alexj.nich@hotmail.com>
2016-05-25 06:56:08 -0700

    Added build and minify npm scripts, references #262

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var webpack = require('webpack')
+
+module.exports = {
+
+	output: {
+		library: 'MaterialUI',
+		libraryTarget: 'umd'
+	},
+
+	externals: [
+		{
+			'react': {
+				root: 'React',
+				commonjs2: 'react',
+				commonjs: 'react',
+				amd: 'react'
+			},
+			'react-dom': {
+				root: 'ReactDOM',
+				commonjs2: 'react-dom',
+				commonjs: 'react-dom',
+				amd: 'react-dom'
+			},
+			'react-tap-event-plugin': {
+				root: 'injectTapEventPlugin',
+				commonjs2: 'react-tap-event-plugin',
+				commonjs: 'react-tap-event-plugin',
+				amd: 'react-tap-event-plugin'
+			}
+		}
+	],
+
+	module: {
+		loaders: [
+		 {
+			 test: /\.js$/,
+			 loaders: ['babel'],
+			 exclude: /node_modules/
+		 }
+		]
+	},
+
+	node: {
+		Buffer: false
+	},
+
+	plugins: [
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+		})	
+	]
+}
+
+
