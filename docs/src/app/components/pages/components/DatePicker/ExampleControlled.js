2ef4c6d0bf41b014f92cf0b64a53f8d97c9e5393
mbrookes <github@nospam.33m.co>
2016-05-31 17:22:07 +0100

    [DatePicker] Fix docs descriptions

--- docs/src/app/components/pages/components/DatePicker/ExampleControlled.js
+++ docs/src/app/components/pages/components/DatePicker/ExampleControlled.js
@@ -2,7 +2,7 @@ import React from 'react';
 import DatePicker from 'material-ui/DatePicker';
 
 /**
- * DatePicker` can be implemented as a controlled input,
+ * `DatePicker` can be implemented as a controlled input,
  * where `value` is handled by state in the parent component.
  */
 export default class DatePickerExampleControlled extends React.Component {
