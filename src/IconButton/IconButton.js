df6415ceec683186d873fea1211aef3dc1870536
Nathan Marks <nathan@poweredbysearch.com>
2016-06-28 08:32:39 -0400

    [IconButton] Use ButtonBase

--- src/IconButton/IconButton.js
+++ src/IconButton/IconButton.js
@@ -1,13 +1,12 @@
 import React, {Component, PropTypes} from 'react';
 import {createStyleSheet} from 'stylishly/lib/styleSheet';
 import ClassNames from 'classnames';
-import {TouchRipple, createRippleHandler} from '../Ripple';
+import ButtonBase from '../Button/ButtonBase';
 
 export const styleSheet = createStyleSheet('IconButton', (theme) => {
   const {palette} = theme;
   return {
     root: {
-      position: 'relative',
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
@@ -15,10 +14,7 @@ export const styleSheet = createStyleSheet('IconButton', (theme) => {
       width: 48,
       height: 48,
       padding: 0,
-      outline: 'none',
-      border: 10,
       borderRadius: '50%',
-      cursor: 'pointer',
       backgroundColor: 'transparent',
       color: 'inherit',
     },
@@ -59,32 +55,13 @@ export default class IconButton extends Component {
     styleManager: PropTypes.object.isRequired,
   };
 
-  ripple = undefined;
-
-  handleMouseDown = createRippleHandler(this, 'MouseDown', 'start');
-  handleMouseUp = createRippleHandler(this, 'MouseUp', 'stop');
-  handleMouseLeave = createRippleHandler(this, 'MouseLeave', 'stop');
-  handleTouchStart = createRippleHandler(this, 'TouchStart', 'start');
-  handleTouchEnd = createRippleHandler(this, 'TouchEnd', 'stop');
-  handleBlur = createRippleHandler(this, 'Blur', 'stop');
-
   render() {
     const {children, className, ripple, ...other} = this.props;
     const classes = this.context.styleManager.render(styleSheet);
     return (
-      <button
-        onBlur={this.handleBlur}
-        onMouseDown={this.handleMouseDown}
-        onMouseLeave={this.handleMouseLeave}
-        onMouseUp={this.handleMouseUp}
-        onTouchEnd={this.handleTouchEnd}
-        onTouchStart={this.handleTouchStart}
-        className={ClassNames(classes.root, className)}
-        {...other}
-      >
-        <span className="material-icons">{children}</span>
-        {ripple && <TouchRipple center={true} ref={(c) => this.ripple = c} />}
-      </button>
+      <ButtonBase className={ClassNames(classes.root, className)} {...other}>
+        {typeof children === 'string' ? <span className="material-icons">{children}</span> : children}
+      </ButtonBase>
     );
   }
 }
