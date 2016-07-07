b9fa09d26af3165390ba0d7a23730031f10e326b
Steve Willard <stevewillard@gmail.com>
2016-07-07 15:35:03 -0400

    [SelectField] Add maxHeight prop to pass down to DropDownMenu

--- src/SelectField/SelectField.js
+++ src/SelectField/SelectField.js
@@ -85,6 +85,10 @@ class SelectField extends Component {
      */
     labelStyle: PropTypes.object,
     /**
+     * Override the default max-height of the underlying `DropDownMenu` element.
+     */
+    maxHeight: PropTypes.number,
+    /**
      * Override the inline-styles of the underlying `DropDownMenu` element.
      */
     menuStyle: PropTypes.object,
@@ -161,6 +165,7 @@ class SelectField extends Component {
       hintText,
       fullWidth,
       errorText,
+      maxHeight,
       menuStyle,
       onFocus,
       onBlur,
@@ -200,6 +205,7 @@ class SelectField extends Component {
           autoWidth={autoWidth}
           value={value}
           onChange={onChange}
+          maxHeight={maxHeight}
         >
           {children}
         </DropDownMenu>
