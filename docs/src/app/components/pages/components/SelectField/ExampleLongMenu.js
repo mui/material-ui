1801e67af05791f95749bb2b0767281fa7637251
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-07-07 23:17:01 +0200

    [SelectField] Add the maxHeight property back to the example

--- docs/src/app/components/pages/components/SelectField/ExampleLongMenu.js
+++ docs/src/app/components/pages/components/SelectField/ExampleLongMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 
@@ -11,18 +11,20 @@ for (let i = 0; i < 100; i++ ) {
  * With the `maxHeight` property set, the Select Field will be scrollable if the number of items causes the height to
  * exceed this limit.
  */
-export default class DropDownMenuLongMenuExample extends React.Component {
+export default class DropDownMenuLongMenuExample extends Component {
 
   constructor(props) {
     super(props);
     this.state = {value: 10};
   }
 
-  handleChange = (event, index, value) => this.setState({value});
+  handleChange = (event, index, value) => {
+    this.setState({value});
+  };
 
   render() {
     return (
-      <SelectField value={this.state.value} onChange={this.handleChange}>
+      <SelectField value={this.state.value} onChange={this.handleChange} maxHeight={200}>
         {items}
       </SelectField>
     );
