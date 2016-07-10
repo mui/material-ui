96fadbbb9795b8370cfdf207f78d46b3abc346fa
Nathan Marks <nathan@poweredbysearch.com>
2016-07-10 14:44:35 -0400

    [Drawer] Add some unit tests

--- src/Drawer/Drawer.spec.js
+++ src/Drawer/Drawer.spec.js
@@ -27,17 +27,78 @@ describe('<Drawer>', () => {
     const wrapper = shallow(
       <Drawer />
     );
+
     const slide = wrapper.childAt(0);
     assert.strictEqual(
       slide.length === 1 && slide.is('Slide'),
       true,
       'immediate wrapper child should be Slide'
     );
+
     const paper = slide.childAt(0);
     assert.strictEqual(
       paper.length === 1 && paper.is('Paper'),
       true,
       'Slide child should be Paper'
     );
+  });
+
+  it('should be closed by default', () => {
+    const wrapper = shallow(<Drawer><h1>Hello</h1></Drawer>);
+
+    const modal = wrapper;
+    const slide = modal.find('Slide');
+
+    assert.strictEqual(modal.prop('show'), false, 'should not show the modal');
+    assert.strictEqual(slide.prop('in'), false, 'should not transition in');
+  });
+
+  describe('opening and closing', () => {
+    let wrapper;
+
+    before(() => wrapper = shallow(<Drawer><h1>Hello</h1></Drawer>));
+
+    it('should start closed', () => {
+      assert.strictEqual(wrapper.prop('show'), false, 'should not show the modal');
+      assert.strictEqual(wrapper.find('Slide').prop('in'), false, 'should not transition in');
+    });
+
+    it('should open', () => {
+      wrapper.setProps({open: true});
+      assert.strictEqual(wrapper.prop('show'), true, 'should show the modal');
+      assert.strictEqual(wrapper.find('Slide').prop('in'), true, 'should transition in');
+    });
+
+    it('should close', () => {
+      wrapper.setProps({open: false});
+      assert.strictEqual(wrapper.prop('show'), false, 'should not show the modal');
+      assert.strictEqual(wrapper.find('Slide').prop('in'), false, 'should not transition in');
+    });
+  });
+
+  describe('docked', () => {
+    let wrapper;
+
+    before(() => wrapper = shallow(<Drawer docked={true}><h1>Hello</h1></Drawer>));
+
+    it('should render a div instead of a Modal when docked', () => {
+      assert.strictEqual(wrapper.is('div'), true, 'should be a div element');
+    });
+
+    it('should render Slide > Paper inside the div', () => {
+      const slide = wrapper.childAt(0);
+      assert.strictEqual(
+        slide.length === 1 && slide.is('Slide'),
+        true,
+        'immediate wrapper child should be Slide'
+      );
+
+      const paper = slide.childAt(0);
+      assert.strictEqual(
+        paper.length === 1 && paper.is('Paper'),
+        true,
+        'Slide child should be Paper'
+      );
+    });
   });
 });
