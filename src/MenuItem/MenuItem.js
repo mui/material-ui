a113f16c0988c6bedffa78b55c1f7890a6d30468
Pascale-Andrée Audet <audetpascale@gmail.com>
2016-07-21 08:00:53 -0400

    [MenuItem] Add right padding when there is icon.
    ### Problem description
    The text of the menu item is below the right icon.
    
    ### Steps to reproduce
    1. Create a dropdown menu with a fixed size.
    1. Enter a long string as value of the menu item.
    1. Add a right icon to the menu item.
    
    ### Versions
    Only tested on...
    
    - Material-UI: 0.15.2
    - React: 15.1.0
    - Browser: Pretty much all of them!

--- src/MenuItem/MenuItem.js
+++ src/MenuItem/MenuItem.js
@@ -13,7 +13,7 @@ const nestedMenuStyle = {
 function getStyles(props, context) {
   const disabledColor = context.muiTheme.baseTheme.palette.disabledColor;
   const textColor = context.muiTheme.baseTheme.palette.textColor;
-  const leftIndent = props.desktop ? 64 : 72;
+  const indent = props.desktop ? 64 : 72;
   const sidePadding = props.desktop ? 24 : 16;
 
   const styles = {
@@ -26,8 +26,8 @@ function getStyles(props, context) {
     },
 
     innerDivStyle: {
-      paddingLeft: props.leftIcon || props.insetChildren || props.checked ? leftIndent : sidePadding,
-      paddingRight: sidePadding,
+      paddingLeft: props.leftIcon || props.insetChildren || props.checked ? indent : sidePadding,
+      paddingRight: props.rightIcon ? indent : sidePadding,
       paddingBottom: 0,
       paddingTop: 0,
     },
