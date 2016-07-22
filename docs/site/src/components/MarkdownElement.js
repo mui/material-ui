2c696d56b9beeb295489ec24e8c3e9e2d8eeaf89
Nathan Marks <nathan@poweredbysearch.com>
2016-07-21 20:22:19 -0400

    [Docs] Fix prop table borders

--- docs/site/src/components/MarkdownElement.js
+++ docs/site/src/components/MarkdownElement.js
@@ -25,7 +25,7 @@ const styleSheet = createStyleSheet('MarkdownElement', (theme) => ({
       fontSize: 13,
       color: theme.palette.text.primary,
     },
-    '@raw td, @raw th': {
+    '@raw td, th': {
       borderBottom: `1px solid ${theme.palette.text.lightDivider}`,
       padding: '0 56px 0 24px',
       '&:last-child': {
