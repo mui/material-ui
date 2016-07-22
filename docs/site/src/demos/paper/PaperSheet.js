d75eff6705560392b282547214260f8a3bd4ab77
Nathan Marks <nathan@poweredbysearch.com>
2016-07-21 20:56:40 -0400

    [Paper] Add component demo, closes #4777

---
+++ docs/site/src/demos/paper/PaperSheet.js
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react';
+import { createStyleSheet } from 'stylishly';
+import Paper from 'material-ui/Paper';
+
+const styleSheet = createStyleSheet('FlatButtons', (theme) => {
+  return {
+    root: theme.mixins.gutters({
+      paddingTop: 5,
+      paddingBottom: 5,
+    }),
+  };
+});
+
+export default function PaperSheet(props, context) {
+  const classes = context.styleManager.render(styleSheet);
+  return (
+    <div>
+      <Paper className={classes.root} zDepth={4}>
+        <h3>This is a sheet of paper.</h3>
+        <p>
+          Paper can be used to build surface or other elements for your application.
+        </p>
+      </Paper>
+    </div>
+  );
+}
+
+PaperSheet.contextTypes = {
+  styleManager: PropTypes.object.isRequired,
+};
---
+++ docs/site/src/demos/paper/paper.md
@@ -0,0 +1,5 @@
+# Paper
+
+In material design, the physical properties of paper are translated to the screen. The background of an application resembles the flat, opaque texture of a sheet of paper, and an application’s behavior mimics paper’s ability to be re-sized, shuffled, and bound together in multiple sheets.
+
+{{demo='paper/PaperSheet.js'}}
