bb85949467efe17b60076edd1327588b18d1e168
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-07-26 22:05:31 +0200

    [Docs] Fix the wrong spelling of ExampleExpandable
    I have also changed the example to illustrate two features
    - The padding button adding with (https://github.com/callemall/material-ui/pull/4820)
    - Move the CardActions at the top with `expandable={false}` as we can see in the [spec](
    https://material.google.com/components/cards.html#cards-behavior)

--- docs/src/app/components/pages/components/Card/ExampleExpandable.js
+++ docs/src/app/components/pages/components/Card/ExampleExpandable.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
-const CardExampleWithoutAvatar = () => (
+const CardExampleExpandable = () => (
   <Card>
     <CardHeader
       title="Without Avatar"
@@ -10,17 +10,17 @@ const CardExampleWithoutAvatar = () => (
       actAsExpander={true}
       showExpandableButton={true}
     />
+    <CardActions>
+      <FlatButton label="Action1" />
+      <FlatButton label="Action2" />
+    </CardActions>
     <CardText expandable={true}>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
       Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
       Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
     </CardText>
-    <CardActions expandable={true}>
-      <FlatButton label="Action1" />
-      <FlatButton label="Action2" />
-    </CardActions>
   </Card>
 );
 
-export default CardExampleWithoutAvatar;
+export default CardExampleExpandable;
