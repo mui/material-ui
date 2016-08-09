d7d64afacd28b7bac8452eabac492bbff95f465a
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-08-09 20:35:35 +0200

    [BottomNavigation] Makes us closer to the next branch

--- docs/src/app/components/pages/components/BottomNavigation/ExampleSimple.js
+++ docs/src/app/components/pages/components/BottomNavigation/ExampleSimple.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 import FontIcon from 'material-ui/FontIcon';
 import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
 import Paper from 'material-ui/Paper';
+import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
 
 const recentsIcon = <FontIcon className="material-icons">restore</FontIcon>;
 const favoritesIcon = <FontIcon className="material-icons">favorite</FontIcon>;
-const nearbyIcon = <FontIcon className="material-icons">location_on</FontIcon>;
+const nearbyIcon = <IconLocationOn />;
 
 /**
  * A simple example of `BottomNavigation`, with three labels and icons
@@ -13,8 +14,12 @@ const nearbyIcon = <FontIcon className="material-icons">location_on</FontIcon>;
  * state (for instance, by the URL).
  */
 class BottomNavigationExampleSimple extends Component {
-  state = {selectedIndex: 0};
+  state = {
+    selectedIndex: 0,
+  };
+
   select = (index) => this.setState({selectedIndex: index});
+
   render() {
     return (
       <Paper zDepth={1}>
