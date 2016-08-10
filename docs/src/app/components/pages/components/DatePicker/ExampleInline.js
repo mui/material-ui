e50ad5400a01e9efd69e0d1fedf9c157880293ef
Caelan Sayler <caelantsayler@gmail.com>
2016-08-10 14:51:23 -0600

    Add docs demo for DatePicker keyboard support

--- docs/src/app/components/pages/components/DatePicker/ExampleInline.js
+++ docs/src/app/components/pages/components/DatePicker/ExampleInline.js
@@ -8,6 +8,7 @@ const DatePickerExampleInline = () => (
   <div>
     <DatePicker hintText="Portrait Inline Dialog" container="inline" />
     <DatePicker hintText="Landscape Inline Dialog" container="inline" mode="landscape" />
+    <DatePicker hintText="Keyboard Enabled Dialog" container="inline" mode="landscape" keyboardEnabled={true} />
   </div>
 );
 
