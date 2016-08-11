029c5dec84e5aa5d4ad062a43e457047eccc3dd8
Caelan Sayler <caelantsayler@gmail.com>
2016-08-11 17:08:04 -0600

    handle enter key when calander open

--- src/DatePicker/DatePicker.js
+++ src/DatePicker/DatePicker.js
@@ -284,7 +284,27 @@ class DatePicker extends Component {
     }
   }
 
+  handleKeyUp = (event) => {
+    if (!this.shouldHandleKeyboard)
+      return;
+
+    const key = keycode(event);
+    switch (key) {
+      case 'enter':
+        if (this.refs.dialogWindow.state.open) {
+          event.stopPropagation();
+          event.preventDefault();
+          this.refs.dialogWindow.dismiss();
+        }
+        break;
+    }
+  }
+
   handleInputChange = (event) => {
+    if (!this.refs.dialogWindow.state.open) {
+      this.refs.dialogWindow.show();
+    }
+
     const filtered = event.target.value.replace(/[^0-9\-\/]/gi, '').replace('/', '-');
     let dt = undefined;
     if (filtered.length === 10) {
@@ -380,6 +400,7 @@ class DatePicker extends Component {
           onFocus={this.handleInputFocus}
           onBlur={this.handleInputBlur}
           onKeyDown={this.handleKeyDown}
+          onKeyUp={this.handleKeyUp}
           onTouchTap={this.handleTouchTap}
           tabIndex={this.shouldHandleKeyboard() ? 0 : 1}
           onChange={this.handleInputChange}
