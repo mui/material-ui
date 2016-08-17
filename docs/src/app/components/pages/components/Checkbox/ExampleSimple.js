5566954bc46cd43d0264079aeccc1e440acd358f
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-08-18 01:18:01 +0200

    [Checkbox] Tweak the transition to allow different shape

--- docs/src/app/components/pages/components/Checkbox/ExampleSimple.js
+++ docs/src/app/components/pages/components/Checkbox/ExampleSimple.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Checkbox from 'material-ui/Checkbox';
 import ActionFavorite from 'material-ui/svg-icons/action/favorite';
 import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
+import Visibility from 'material-ui/svg-icons/action/visibility';
+import VisibilityOff from 'material-ui/svg-icons/action/visibility-off';
 
 const styles = {
   block: {
@@ -19,14 +21,15 @@ const CheckboxExampleSimple = () => (
       style={styles.checkbox}
     />
     <Checkbox
-      label="Checked by default"
-      defaultChecked={true}
-      style={styles.checkbox}
-    />
-    <Checkbox
       checkedIcon={<ActionFavorite />}
       uncheckedIcon={<ActionFavoriteBorder />}
       label="Custom icon"
+      style={styles.checkbox}
+    />
+    <Checkbox
+      checkedIcon={<Visibility />}
+      uncheckedIcon={<VisibilityOff />}
+      label="Custom icon of different shapes"
       style={styles.checkbox}
     />
     <Checkbox
