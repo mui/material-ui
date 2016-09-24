3985f8a78845a5781635422e35344482a97cbdde
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-09-24 12:38:38 +0200

    [AppBar] Add a composition example
    Closes #5053.

--- docs/src/app/components/pages/components/AppBar/ExampleIcon.js
+++ docs/src/app/components/pages/components/AppBar/ExampleIcon.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 
+/**
+ * A simple example of `AppBar` with an icon on the right.
+ * By default, the left icon is a navigation-menu.
+ */
 const AppBarExampleIcon = () => (
   <AppBar
     title="Title"
