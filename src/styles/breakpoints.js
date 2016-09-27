48700a8cec1c327e934693283fa2cce43f9be6b8
Suntharesan Mohan <mohan.kethees@gmail.com>
2016-09-27 15:40:24 -0400

    [style]: Add `priority` and  `isMatch`
    Add `priority` and  `isMatch` properties to `createBreakpoints`. priority is large to small.

--- src/styles/breakpoints.js
+++ src/styles/breakpoints.js
@@ -1,5 +1,12 @@
 // @flow weak
+import { flow, map, sortBy, reverse, toPairs } from 'lodash/fp';
 
+const getPriority = flow(
+  toPairs,
+  sortBy(([, v]) => v),
+  reverse,
+  map(([k]) => k)
+);
 export default function createBreakpoints(
   breakpoints = {
     xs: 360,
@@ -13,6 +20,7 @@ export default function createBreakpoints(
 ) {
   const keys = Object.keys(breakpoints);
   const values = keys.map((n) => breakpoints[n]);
+  const priority = getPriority(breakpoints);
 
   function up(name) {
     const value = breakpoints[name] || name;
@@ -43,5 +51,22 @@ export default function createBreakpoints(
     return breakpoints[name];
   }
 
-  return { keys, values, up, down, only, getWidth };
+  let matched = priority
+    .reduce((a, k) => {
+      const media = window.matchMedia(breakpoints[k]);
+      if (media.matches) {
+        a = k;
+      }
+      return a;
+    });
+
+  function isMatch(breakpoint) {
+    if (matched === breakpoint) return true;
+    if (!breakpoints[breakpoint]) return false;
+    const media = window.matchMedia(breakpoints[breakpoint]);
+    if (media.matches) matched = breakpoint;
+    return media.matches;
+  }
+
+  return { keys, values, priority, up, down, between, only, getWidth, isMatch };
 }
