c2390dd2b7b12b6882d1ff6b10f7e45c1ef868ff
Pradel Léo <pradel.leo@gmail.com>
2016-09-27 12:29:55 +0200

    Add regressions tests

---
+++ test/regressions/site/src/tests/Subheader/InsetSubheader.js
@@ -0,0 +1,10 @@
+// @flow weak
+
+import React from 'react';
+import Subheader from 'material-ui/Subheader';
+
+export default function InsetSubheader() {
+  return (
+    <Subheader inset>Title</Subheader>
+  );
+}
