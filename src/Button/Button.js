6831d54583ef95da2d2865ff0fb6c85d9f8ffff5
nathanmarks <info@nathanmarks.io>
2016-10-10 11:17:39 -0400

    Switch to circleci badge

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -21,7 +21,7 @@ export const styleSheet = createStyleSheet('Button', (theme) => {
       minWidth: 88,
       height: 36,
       padding: '0px 16px',
-      borderRadius: 20,
+      borderRadius: 2,
       color: palette.text.primary,
       backgroundColor: 'transparent',
       transition: transitions.multi(['background-color', 'box-shadow'], '250ms'),
