048944eade73c9e944cd850d560520b9a0cb4d03
Seth McGuinness <seth.mcguinness@yahoo.com>
2016-10-15 02:17:10 -0500

    [DropDownMenu] Fix usage of null child
    @smcguinness Thanks for the original PR
    This is a simple continuation
    
    Closes #5397

--- src/DropDownMenu/DropDownMenu.spec.js
+++ src/DropDownMenu/DropDownMenu.spec.js
@@ -4,20 +4,35 @@ import {shallow} from 'enzyme';
 import {assert} from 'chai';
 import DropDownMenu from './DropDownMenu';
 import getMuiTheme from '../styles/getMuiTheme';
+import MenuItem from '../MenuItem';
 
 describe('<DropDownMenu />', () => {
   const muiTheme = getMuiTheme();
   const shallowWithContext = (node) => shallow(node, {context: {muiTheme}});
 
-  it('displays the text field of menuItems prop at index x when value prop is x', () => {
-    const wrapper = shallowWithContext(
-      <DropDownMenu value={1}>
-        <div value={1} primaryText="Never" />
-        <div value={2} primaryText="Every Night" />
-        <div value={3} primaryText="Weeknights" />
-      </DropDownMenu>
-    );
+  describe('prop: value', () => {
+    it('should display the right selected value text', () => {
+      const wrapper = shallowWithContext(
+        <DropDownMenu value={1}>
+          <MenuItem value={1} primaryText="Never" />
+          <MenuItem value={2} primaryText="Every Night" />
+        </DropDownMenu>
+      );
 
-    assert.strictEqual(wrapper.childAt(0).childAt(0).childAt(0).node, 'Never');
+      assert.strictEqual(wrapper.childAt(0).childAt(0).childAt(0).node, 'Never');
+    });
+  });
+
+  describe('prop: children', () => {
+    it('should work with null child', () => {
+      const wrapper = shallowWithContext(
+        <DropDownMenu value={1}>
+          <MenuItem value={1} primaryText="Never" />
+          {null}
+        </DropDownMenu>
+      );
+
+      assert.strictEqual(wrapper.childAt(0).childAt(0).childAt(0).node, 'Never');
+    });
   });
 });
