cb2be2ee20d4084f5e22c1f32ddb0ffb5209071a
Collin Donahue-Oponski <collindo@gmail.com>
2016-10-20 11:40:37 -0600

    Fix vertical alignment of IconButton within a ToolbarGroup
    ### Problem description
    
    From your [Toolbar docs](http://www.material-ui.com/#/components/toolbar):
    
    Current behavior:
    ![image](https://cloud.githubusercontent.com/assets/185923/19570919/31086ace-96b9-11e6-8dc5-fd1ec2fc2bbd.png)
    
    After adding `align-items: center;` to the ToolbarGroup's `<div>` element:
    ![image](https://cloud.githubusercontent.com/assets/185923/19570904/21ce1c8e-96b9-11e6-81bc-abfe97b12d10.png)
    
    Note the improved vertical alignment of the IconButton.  What do you think?  Note that I haven't tested broadly.

--- src/Toolbar/ToolbarGroup.js
+++ src/Toolbar/ToolbarGroup.js
@@ -22,6 +22,7 @@ function getStyles(props, context) {
       marginRight: lastChild ? -marginHorizontal : undefined,
       display: 'flex',
       justifyContent: 'space-between',
+      alignItems: 'center',
     },
     dropDownMenu: {
       root: {
