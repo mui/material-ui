504098c6afc36abfd1382a7dbde469d3f3313536
Léo Pradel <pradel.leo@gmail.com>
2016-11-08 23:46:09 +0100

    [LinearProgress] Migration of linear-progress component (#5463)

---
+++ src/Progress/LinearProgress.js
@@ -0,0 +1,194 @@
+// @flow weak
+
+import React, { Component, PropTypes } from 'react';
+import { createStyleSheet } from 'jss-theme-reactor';
+import classNames from 'classnames';
+
+export const styleSheet = createStyleSheet('LinearProgress', (theme) => {
+  const { palette } = theme;
+  const transitionDuration = 3;
+  return {
+    root: {
+      position: 'relative',
+      overflow: 'hidden',
+      height: 5,
+      backgroundColor: palette.primary[100],
+    },
+    rootBuffer: {
+      backgroundColor: 'transparent',
+    },
+    rootQuery: {
+      transform: 'rotate(180deg)',
+    },
+    bar: {
+      position: 'absolute',
+      left: 0,
+      bottom: 0,
+      top: 0,
+      height: 5,
+      transition: 'transform 0.2s linear',
+      backgroundColor: palette.primary[500],
+    },
+    dashed: {
+      position: 'absolute',
+      marginTop: 0,
+      height: 5,
+      width: '100%',
+      background:
+        `radial-gradient(${palette.primary[100]} 0%, ${palette.primary[100]} 16%, transparent 42%)`,
+      backgroundSize: '10px 10px',
+      backgroundPosition: '0px -23px',
+      animation: 'buffer 3s infinite linear',
+    },
+    indeterminateBar1: {
+      willChange: 'left, right',
+      animation: 'indeterminate-1 2.1s cubic-bezier(0.65, 0.815, 0.735, 0.395) infinite',
+    },
+    indeterminateBar2: {
+      willChange: 'left, right',
+      animation: 'indeterminate-2 2.1s cubic-bezier(0.165, 0.84, 0.44, 1) infinite',
+      animationDelay: '1.15s',
+    },
+    determinateBar1: {
+      willChange: 'width',
+      transition: `width .${transitionDuration}s linear`,
+    },
+    determinateBar2: {
+      display: 'none',
+    },
+    bufferBar1: {
+      transition: `width .${transitionDuration}s linear`,
+      backgroundColor: palette.primary[100],
+    },
+    bufferBar2: {
+      transition: `width .${transitionDuration}s linear`,
+    },
+    '@keyframes indeterminate-1': {
+      '0%': {
+        left: '-35%',
+        right: '100%',
+      },
+      '60%': {
+        left: '100%',
+        right: '-90%',
+      },
+      '100%': {
+        left: '100%',
+        right: '-90%',
+      },
+    },
+    '@keyframes indeterminate-2': {
+      '0%': {
+        left: '-200%',
+        right: '100%',
+      },
+      '60%': {
+        left: '107%',
+        right: '-8%',
+      },
+      '100%': {
+        left: '107%',
+        right: '-8%',
+      },
+    },
+    '@keyframes buffer': {
+      '0%': {
+        opacity: 1,
+        backgroundPosition: '0px -23px',
+      },
+      '50%': {
+        opacity: 0,
+        backgroundPosition: '0px -23px',
+      },
+      '100%': {
+        opacity: 1,
+        backgroundPosition: '-200px -23px',
+      },
+    },
+    '@keyframes query': {
+      '0%': {
+        opacity: 1,
+        transform: 'translateX(35%) scale(.3, 1)',
+      },
+      '100%': {
+        opacity: 0,
+        transform: 'translateX(-50%) scale(0, 1)',
+      },
+    },
+  };
+});
+
+export default class LinearProgress extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    /**
+     * The mode of show your progress, indeterminate
+     * for when there is no value for progress.
+     */
+    mode: PropTypes.oneOf(['determinate', 'indeterminate', 'buffer', 'query']),
+    /**
+     * The value of progress, only works in determinate and buffer mode.
+     */
+    value: PropTypes.number,
+    /**
+     * The value of buffer, only works in buffer mode.
+     */
+    valueBuffer: PropTypes.number,
+  };
+
+  static defaultProps = {
+    mode: 'indeterminate',
+    value: 0,
+  };
+
+  static contextTypes = {
+    styleManager: PropTypes.object.isRequired,
+  };
+
+  render() {
+    const {
+      className,
+      mode,
+      value,
+      valueBuffer,
+      ...other
+    } = this.props;
+    const classes = this.context.styleManager.render(styleSheet);
+    const rootClasses = classNames(classes.root, {
+      [classes.rootBuffer]: mode === 'buffer',
+      [classes.rootQuery]: mode === 'query',
+    }, className);
+    const dashedClasses = classNames({
+      [classes.dashed]: mode === 'buffer',
+    });
+    const bar1Classes = classNames(classes.bar, {
+      [classes.indeterminateBar1]: mode === 'indeterminate' || mode === 'query',
+      [classes.determinateBar1]: mode === 'determinate',
+      [classes.bufferBar1]: mode === 'buffer',
+    });
+    const bar2Classes = classNames(classes.bar, {
+      [classes.indeterminateBar2]: mode === 'indeterminate' || mode === 'query',
+      [classes.determinateBar2]: mode === 'determinate',
+      [classes.bufferBar2]: mode === 'buffer',
+    });
+    const styles = { bar1: {}, bar2: {} };
+
+    if (mode === 'determinate') {
+      styles.bar1.width = `${value}%`;
+    } else if (mode === 'buffer') {
+      styles.bar1.width = `${valueBuffer}%`;
+      styles.bar2.width = `${value}%`;
+    }
+
+    return (
+      <div
+        className={rootClasses}
+        {...other}
+      >
+        <div className={dashedClasses} />
+        <div className={bar1Classes} style={styles.bar1} />
+        <div className={bar2Classes} style={styles.bar2} />
+      </div>
+    );
+  }
+}
---
+++ src/Progress/LinearProgress.spec.js
@@ -0,0 +1,88 @@
+// @flow weak
+/* eslint-env mocha */
+
+import React from 'react';
+import { assert } from 'chai';
+import { createShallowWithContext } from 'test/utils';
+import LinearProgress, { styleSheet } from './LinearProgress';
+
+describe('<LinearProgress>', () => {
+  let shallow;
+  let classes;
+
+  before(() => {
+    shallow = createShallowWithContext();
+    classes = shallow.context.styleManager.render(styleSheet);
+  });
+
+  it('should render a div with the root class', () => {
+    const wrapper = shallow(<LinearProgress />);
+    assert.strictEqual(wrapper.is('div'), true, 'should be a div');
+    assert.strictEqual(wrapper.hasClass(classes.root), true, 'should have the root class');
+  });
+
+  it('should render with the user and root classes', () => {
+    const wrapper = shallow(<LinearProgress className="woof" />);
+    assert.strictEqual(wrapper.hasClass('woof'), true, 'should have the "woof" class');
+    assert.strictEqual(wrapper.hasClass(classes.root), true, 'should have the root class');
+  });
+
+  it('should render intermediate mode by default', () => {
+    const wrapper = shallow(<LinearProgress />);
+    assert.strictEqual(wrapper.hasClass(classes.root), true, 'should have the root class');
+    assert.strictEqual(wrapper.childAt(1).hasClass(classes.bar), true, 'should have the bar class');
+    assert.strictEqual(wrapper.childAt(1).hasClass(classes.indeterminateBar1), true,
+      'should have the indeterminateBar1 class');
+    assert.strictEqual(wrapper.childAt(2).hasClass(classes.bar), true, 'should have the bar class');
+    assert.strictEqual(wrapper.childAt(1).hasClass(classes.indeterminateBar1), true,
+      'should have the indeterminateBar2 class');
+  });
+
+  it('should render with determinate classes', () => {
+    const wrapper = shallow(<LinearProgress mode="determinate" />);
+    assert.strictEqual(wrapper.hasClass(classes.root), true, 'should have the root class');
+    assert.strictEqual(wrapper.childAt(1).hasClass(classes.bar), true, 'should have the bar class');
+    assert.strictEqual(wrapper.childAt(1).hasClass(classes.determinateBar1), true,
+      'should have the determinateBar1 class');
+    assert.strictEqual(wrapper.childAt(2).hasClass(classes.bar), true, 'should have the bar class');
+    assert.strictEqual(wrapper.childAt(2).hasClass(classes.determinateBar2), true,
+      'should have the determinateBar2 class');
+  });
+
+  it('should set width of bar1 on determinate mode', () => {
+    const wrapper = shallow(<LinearProgress mode="determinate" value={77} />);
+    assert.strictEqual(wrapper.hasClass(classes.root), true, 'should have the root class');
+    assert.strictEqual(wrapper.childAt(1).props().style.width, '77%', 'should have width set');
+  });
+
+  it('should render with buffer classes', () => {
+    const wrapper = shallow(<LinearProgress mode="buffer" />);
+    assert.strictEqual(wrapper.hasClass(classes.root), true, 'should have the root class');
+    assert.strictEqual(wrapper.childAt(0).hasClass(classes.dashed), true, 'should have the dashed class');
+    assert.strictEqual(wrapper.childAt(1).hasClass(classes.bar), true, 'should have the bar class');
+    assert.strictEqual(wrapper.childAt(1).hasClass(classes.bufferBar1), true,
+      'should have the bufferBar1 class');
+    assert.strictEqual(wrapper.childAt(2).hasClass(classes.bar), true, 'should have the bar class');
+    assert.strictEqual(wrapper.childAt(2).hasClass(classes.bufferBar2), true,
+      'should have the bufferBar2 class');
+  });
+
+  it('should set width of bar1 and bar2 on buffer mode', () => {
+    const wrapper = shallow(<LinearProgress mode="buffer" value={77} valueBuffer={85} />);
+    assert.strictEqual(wrapper.hasClass(classes.root), true, 'should have the root class');
+    assert.strictEqual(wrapper.childAt(1).props().style.width, '85%', 'should have width set');
+    assert.strictEqual(wrapper.childAt(2).props().style.width, '77%', 'should have width set');
+  });
+
+  it('should render with query classes', () => {
+    const wrapper = shallow(<LinearProgress mode="query" />);
+    assert.strictEqual(wrapper.hasClass(classes.root), true, 'should have the root class');
+    assert.strictEqual(wrapper.hasClass(classes.rootQuery), true, 'should have the rootQuery class');
+    assert.strictEqual(wrapper.childAt(1).hasClass(classes.bar), true, 'should have the bar class');
+    assert.strictEqual(wrapper.childAt(1).hasClass(classes.indeterminateBar1), true,
+      'should have the indeterminateBar1 class');
+    assert.strictEqual(wrapper.childAt(2).hasClass(classes.bar), true, 'should have the bar class');
+    assert.strictEqual(wrapper.childAt(2).hasClass(classes.indeterminateBar2), true,
+      'should have the indeterminateBar2 class');
+  });
+});
