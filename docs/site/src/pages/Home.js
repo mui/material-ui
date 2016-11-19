2c7addbef9c636c9a3ab3dca26b8521618dbff2f
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-11-19 22:25:08 +0100

    [docs] Improve the readability

--- docs/site/src/pages/Home.js
+++ docs/site/src/pages/Home.js
@@ -17,6 +17,7 @@ export const styleSheet = createStyleSheet('Home', (theme) => {
       flex: '1 0 100%',
     },
     hero: {
+      minHeight: '100%', // Makes the hero full height until we get some more content.
       flex: '0 0 auto',
       display: 'flex',
       justifyContent: 'center',
@@ -35,13 +36,15 @@ export const styleSheet = createStyleSheet('Home', (theme) => {
       marginTop: 20,
     },
     logo: {
-      maxWidth: '180%',
       margin: '20px -40%',
+      width: '100%',
+      height: '40vw',
+      maxHeight: 230,
     },
   };
 });
 
-const Home = (props, context) => {
+function Home(props, context) {
   const classes = context.styleManager.render(styleSheet);
 
   return (
@@ -49,8 +52,10 @@ const Home = (props, context) => {
       <div className={classes.hero}>
         <div className={classes.content}>
           <img src={muiLogo} alt="Material UI Logo" className={classes.logo} />
-          <Text type="display2">Material-UI</Text>
-          <Text type="subheading">
+          <Text type="display2" component="h1">
+            {'Material-UI'}
+          </Text>
+          <Text type="subheading" component="h2">
             {"A React component library implementing Google's Material Design"}
           </Text>
           <Button
@@ -65,7 +70,7 @@ const Home = (props, context) => {
       </div>
     </div>
   );
-};
+}
 
 Home.contextTypes = {
   styleManager: PropTypes.object.isRequired,
