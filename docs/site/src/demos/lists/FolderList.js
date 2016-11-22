32dd850d3898d8787ea623c6b97586f8921f7b3c
Léo Pradel <pradel.leo@gmail.com>
2016-11-22 20:18:18 +0100

    [Docs] Add more examples to list (#5616)

---
+++ docs/site/src/demos/lists/FolderList.js
@@ -0,0 +1,42 @@
+// @flow weak
+
+import React, { PropTypes } from 'react';
+import { createStyleSheet } from 'jss-theme-reactor';
+import {
+  List,
+  ListItem,
+  ListItemText,
+} from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+
+const styleSheet = createStyleSheet('FolderList', (theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '360px',
+    background: theme.palette.background.paper,
+  },
+}));
+
+function FolderList(props, context) {
+  const classes = context.styleManager.render(styleSheet);
+  return (
+    <div className={classes.root}>
+      <List>
+        <ListItem button>
+          <Avatar icon={<span className="material-icons">folder</span>} />
+          <ListItemText primary="Photos" secondary="Jan 9, 2016" />
+        </ListItem>
+        <ListItem button>
+          <Avatar icon={<span className="material-icons">folder</span>} />
+          <ListItemText primary="Work" secondary="Jan 7, 2016" />
+        </ListItem>
+      </List>
+    </div>
+  );
+}
+
+FolderList.contextTypes = {
+  styleManager: PropTypes.object.isRequired,
+};
+
+export default FolderList;
