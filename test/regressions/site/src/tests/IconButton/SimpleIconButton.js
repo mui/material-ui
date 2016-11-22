581b79271400544229630768ed1926afb6ff113e
Pradel Léo <pradel.leo@gmail.com>
2016-11-22 17:16:59 +0100

    [IconButton] Add visual regressions tests
    Add visual regressions tests for IconButton

---
+++ test/regressions/site/src/tests/IconButton/SimpleIconButton.js
@@ -0,0 +1,10 @@
+// @flow weak
+
+import React from 'react';
+import IconButton from 'material-ui/IconButton';
+
+export default function SimpleIconButton() {
+  return (
+    <IconButton>home</IconButton>
+  );
+}
