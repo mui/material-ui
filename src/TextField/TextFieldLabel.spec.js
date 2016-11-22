7e0e519a0abffccb217c3145ee33e3942e693051
Kevin Ross <kevin.ross@alienfast.com>
2016-11-22 16:07:12 -0600

    revert spec back to shallow rendering

--- src/TextField/TextFieldLabel.spec.js
+++ src/TextField/TextFieldLabel.spec.js
@@ -3,35 +3,35 @@
 
 import React from 'react';
 import { assert } from 'chai';
-import { createMountWithContext } from 'test/utils';
+import { createShallowWithContext } from 'test/utils';
 import TextFieldLabel, { styleSheet } from './TextFieldLabel';
 
 describe('<TextFieldLabel />', () => {
-  let mount;
+  let shallow;
   let classes;
 
   before(() => {
-    mount = createMountWithContext();
-    classes = mount.context.styleManager.render(styleSheet);
+    shallow = createShallowWithContext();
+    classes = shallow.context.styleManager.render(styleSheet);
   });
 
   it('should animate by default', () => {
-    const wrapper = mount(<TextFieldLabel />);
+    const wrapper = shallow(<TextFieldLabel />);
     assert.strictEqual(wrapper.hasClass(classes.animated), true, 'should have the animated class');
   });
 
   it('should not animate', () => {
-    const wrapper = mount(<TextFieldLabel animated={false} />);
+    const wrapper = shallow(<TextFieldLabel animated={false} />);
     assert.strictEqual(wrapper.hasClass(classes.animated), false, 'should not have the animated class');
   });
 
   it('should not shrink by default', () => {
-    const wrapper = mount(<TextFieldLabel />);
+    const wrapper = shallow(<TextFieldLabel />);
     assert.strictEqual(wrapper.hasClass(classes.shrink), false, 'should not have the shrink class');
   });
 
   it('should shrink', () => {
-    const wrapper = mount(<TextFieldLabel shrink />);
+    const wrapper = shallow(<TextFieldLabel shrink />);
     assert.strictEqual(wrapper.hasClass(classes.shrink), true, 'should have the shrink class');
   });
 });
