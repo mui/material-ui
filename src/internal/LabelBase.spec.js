83dbdd991dc1d8a2e2da49f94f6b94b8935e59fa
Kevin Ross <kevin.ross@alienfast.com>
2016-11-22 15:25:58 -0600

    LabeBase.spec - Align assertions with implementation and material design spec

--- src/internal/LabelBase.spec.js
+++ src/internal/LabelBase.spec.js
@@ -27,21 +27,41 @@ describe('<LabelBase />', () => {
       const wrapper = shallow(<LabelBase required />);
       const text = wrapper.text();
       assert.strictEqual(text.indexOf('*'), text.length - 1, 'should show an asterisk at the end');
-      assert.strictEqual(wrapper.find('span').hasClass(classes.asterisk), false,
-        'should not have the asterisk class');
+      assert.strictEqual(wrapper.find('span').hasClass(classes.asterisk), true,
+        'should have the asterisk class');
     });
 
     it('should show an asterisk class if dirty is true', () => {
       const wrapper = shallow(<LabelBase required focused />);
       const text = wrapper.text();
       assert.strictEqual(text.indexOf('*'), text.length - 1, 'should show an asterisk at the end');
-      assert.strictEqual(wrapper.find('span').hasClass(classes.asterisk), false,
-        'should not have the asterisk class');
+      assert.strictEqual(wrapper.find('span').hasClass(classes.asterisk), true,
+        'should have the asterisk class');
     });
 
     it('should not show an asterisk by default', () => {
       const wrapper = shallow(<LabelBase />);
       assert.strictEqual(wrapper.text().includes('*'), false, 'should not show an asterisk');
+    });
+
+    it('should show an asterisk with an error class if focused', () => {
+      const wrapper = shallow(<LabelBase required focused />);
+      assert.strictEqual(wrapper.find('span').hasClass(classes.asterisk), true,
+        'should have the asterisk class');
+      assert.strictEqual(wrapper.find('span').hasClass(classes.error), true,
+        'should have the asterisk class');
+    });
+
+    describe('error', () => {
+      it('should show an error class', () => {
+        const wrapper = shallow(<LabelBase required error />);
+        assert.strictEqual(wrapper.find('span').hasClass(classes.asterisk), true,
+          'asterisk should have the asterisk class');
+        assert.strictEqual(wrapper.find('span').hasClass(classes.error), true,
+          'asterisk should have the error class');
+        assert.strictEqual(wrapper.hasClass(classes.error), true,
+          'should have the error class');
+      });
     });
   });
 });
