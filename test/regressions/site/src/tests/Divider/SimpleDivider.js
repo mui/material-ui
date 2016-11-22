43f496a37abfce1671008b4ea3b865a0a5b2af73
Pradel Léo <pradel.leo@gmail.com>
2016-11-22 16:35:58 +0100

    [Divider] Add visual regressions tests

---
+++ test/regressions/site/src/tests/Divider/SimpleDivider.js
@@ -0,0 +1,12 @@
+// @flow weak
+
+import React from 'react';
+import Divider from 'material-ui/Divider';
+
+export default function SimpleDivider() {
+  return (
+    <div style={{ padding: 2, width: 100 }}>
+      <Divider />
+    </div>
+  );
+}
