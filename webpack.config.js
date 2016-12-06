671a013a9c6d8917f53b7f00af5496838183e392
Neale Upstone <neale@nealeupstone.com>
2016-12-06 22:14:39 +0000

    [npm] Add webpack.config.js to give UMD packaging so can be used with AMD

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+/* eslint-disable flowtype/require-valid-file-annotation */
+// webpack.config.js
+
+const path = require('path');
+
+const libraryName = 'material-ui';
+const outputFile = `${libraryName}.js`;
+
+const INDEX = path.join(__dirname, 'src/index.js');
+const DIST = path.join(__dirname, 'dist');
+
+const config = {
+  entry: {
+    'material-ui': INDEX,
+  },
+  devtool: 'source-map',
+  output: {
+    path: DIST,
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  externals: [
+    'react-addons-create-fragment',
+    'react-addons-transition-group',
+    {
+      react: {
+        root: 'React',
+        commonjs2: './react',
+        commonjs: ['./react'],
+        amd: 'react',
+      },
+    },
+    {
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: './react-dom',
+        commonjs: ['./react-dom'],
+        amd: 'react-dom',
+      },
+    },
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel',
+        exclude: /(node_modules)/,
+      },
+    ],
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js'],
+  },
+};
+
+module.exports = config;
+
