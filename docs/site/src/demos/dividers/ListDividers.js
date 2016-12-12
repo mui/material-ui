ca6b3e10901831257209bd3f0c7b1a0a6667388b
Pradel Léo <pradel.leo@gmail.com>
2016-12-12 17:58:43 +0100

    [Docs] Add dividers docs

---
+++ docs/site/src/demos/dividers/ListDividers.js
@@ -0,0 +1,41 @@
+// @flow weak
+
+import React, { PropTypes } from 'react';
+import { createStyleSheet } from 'jss-theme-reactor';
+import {
+  List,
+  ListItem,
+  ListItemText,
+} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+
+const styleSheet = createStyleSheet('ListDividers', (theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '360px',
+    background: theme.palette.background.paper,
+  },
+}));
+
+export default function FlatButtons(props, context) {
+  const classes = context.styleManager.render(styleSheet);
+  return (
+    <List className={classes.root}>
+      <ListItem button>
+        <ListItemText primary="Inbox" />
+      </ListItem>
+      <Divider />
+      <ListItem button>
+        <ListItemText primary="Drafts" />
+      </ListItem>
+      <Divider />
+      <ListItem button>
+        <ListItemText primary="Trash" />
+      </ListItem>
+    </List>
+  );
+}
+
+FlatButtons.contextTypes = {
+  styleManager: PropTypes.object.isRequired,
+};
