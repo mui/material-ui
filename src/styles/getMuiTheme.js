675a549691c043facef95a03191cc9e75b3a6c6a
Remon Georgy <remon.sherin@gmail.com>
2016-12-23 16:50:02 -0800

    PR #5816: scratch that. It should inherit palette.alternateTextColor

--- src/styles/getMuiTheme.js
+++ src/styles/getMuiTheme.js
@@ -293,7 +293,7 @@ export default function getMuiTheme(muiTheme, ...more) {
     },
     timePicker: {
       color: palette.alternateTextColor,
-      textColor: palette.textColor,
+      textColor: palette.alternateTextColor,
       accentColor: palette.primary1Color,
       clockColor: palette.textColor,
       clockCircleColor: palette.clockCircleColor,
