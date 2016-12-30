aae3924a9a458bc51405daa72821446874722894
mbrookes <github@nospam.33m.co>
2016-12-30 22:00:38 +0000

    [svg-icons] Remove category names from exported functions

--- src/svg-icons/add.js
+++ src/svg-icons/add.js
@@ -4,13 +4,13 @@ import React from 'react';
 import pure from 'recompose/pure';
 import SvgIcon from '../SvgIcon';
 
-let ContentAdd = (props) => (
+let Add = (props) => (
   <SvgIcon {...props}>
     <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
   </SvgIcon>
 );
-ContentAdd = pure(ContentAdd);
-ContentAdd.displayName = 'ContentAdd';
-ContentAdd.muiName = 'SvgIcon';
+Add = pure(Add);
+Add.displayName = 'Add';
+Add.muiName = 'SvgIcon';
 
-export default ContentAdd;
+export default Add;
