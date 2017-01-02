d5116b48b6f31577319e80046287dad5d2b05c36
Kevin Ross <kevin.ross@alienfast.com>
2017-01-02 13:45:13 -0600

    update RadioGroup regression tests to use FormControl

--- test/regressions/site/src/tests/RadioGroup/RadioGroupWithLabelError.js
+++ test/regressions/site/src/tests/RadioGroup/RadioGroupWithLabelError.js
@@ -1,17 +1,17 @@
 // @flow weak
 
 import React from 'react';
-import { FormLabel } from 'material-ui/Form';
+import { FormLabel, FormControl } from 'material-ui/Form';
 import { RadioGroup, LabelRadio as Radio } from 'material-ui/Radio';
 
 export default function RadioGroupWithLabelError() {
   return (
-    <div style={{ width: 100 }}>
-      <FormLabel required error>Location</FormLabel>
+    <FormControl style={{ width: 100 }} required error={}>
+      <FormLabel>Location</FormLabel>
       <RadioGroup selectedValue="home">
         <Radio label="Home" value="home" />
         <Radio label="Work" value="work" />
       </RadioGroup>
-    </div>
+    </FormControl>
   );
 }
--- test/regressions/site/src/tests/RadioGroup/RadioGroupWithLabelRequired.js
+++ test/regressions/site/src/tests/RadioGroup/RadioGroupWithLabelRequired.js
@@ -1,17 +1,17 @@
 // @flow weak
 
 import React from 'react';
-import { FormLabel } from 'material-ui/Form';
+import { FormLabel, FormControl } from 'material-ui/Form';
 import { RadioGroup, LabelRadio as Radio } from 'material-ui/Radio';
 
 export default function RadioGroupWithLabelRequired() {
   return (
-    <div style={{ width: 100 }}>
-      <FormLabel required>Location</FormLabel>
+    <FormControl style={{ width: 100 }} required>
+      <FormLabel>Location</FormLabel>
       <RadioGroup selectedValue="home">
         <Radio label="Home" value="home" />
         <Radio label="Work" value="work" />
       </RadioGroup>
-    </div>
+    </FormControl>
   );
 }
