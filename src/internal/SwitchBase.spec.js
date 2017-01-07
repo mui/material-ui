00bffb1039c2347d8d1a96319ea9058f94ae20ad
mbrookes <github@nospam.33m.co>
2017-01-07 00:00:41 +0000

    Fix SwitchBase default icon test

--- src/internal/SwitchBase.spec.js
+++ src/internal/SwitchBase.spec.js
@@ -31,7 +31,7 @@ describe('<SwitchBase />', () => {
     const wrapper = shallow(
       <SwitchBase />,
     );
-    assert.strictEqual(wrapper.childAt(0).is('CheckBoxOutlineBlank'), true, 'should be an SVG icon');
+    assert.strictEqual(wrapper.childAt(0).is('pure(CheckBoxOutlineBlank)'), true, 'should be an SVG icon');
     assert.strictEqual(wrapper.childAt(1).is('input[type="checkbox"]'), true, 'should be a checkbox input');
   });
 
