46d675d365442adf481fc769e55eb7e56a5b762d
Martin Jewell <martin@blackbill.com>
2017-01-11 02:20:08 +0100

    [DatePicker] Add additional test for DatePicker

--- src/DatePicker/DatePicker.spec.js
+++ src/DatePicker/DatePicker.spec.js
@@ -1,7 +1,7 @@
 /* eslint-env mocha */
 import React, {PropTypes} from 'react';
 import {shallow, mount} from 'enzyme';
-import {spy} from 'sinon';
+import {spy, useFakeTimers} from 'sinon';
 import {assert} from 'chai';
 
 import DatePicker from './DatePicker';
@@ -25,6 +25,33 @@ describe('<DatePicker />', () => {
 
       assert.strictEqual(wrapper.find(TextField).props().value, '2015-12-01',
         'should format the date to ISO 8601 (YYYY-MM-DD)');
+    });
+  });
+
+  describe('getDate method', () => {
+    it('should select current date if no defaultDate is passed', () => {
+      const now = new Date();
+      const clock = useFakeTimers(now.getTime());
+
+      const datePicker = shallowWithContext(
+        <DatePicker />
+      ).instance();
+
+      assert.strictEqual(datePicker.getDate().toString(), now.toString(),
+      'should set the date to the current time when defaultDate is missing');
+
+      clock.restore();
+    });
+
+    it('should select defaultDate date if it is passed', () => {
+      const defaultDate = new Date(358327500000); // Sun, 10 May 1981 09:25:00 GMT
+
+      const datePicker = shallowWithContext(
+        <DatePicker value={defaultDate} />
+      ).instance();
+
+      assert.strictEqual(datePicker.getDate().toString(), defaultDate.toString(),
+      'should set the date to the defaultDate that was set');
     });
   });
 
