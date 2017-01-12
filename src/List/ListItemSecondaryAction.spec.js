6c4281a1d7d49a593103276fb42e10276d4a4900
Kevin Ross <kevin.ross@alienfast.com>
2017-01-12 15:40:09 -0600

    revert spec change on ListItemSecondaryAction since we added back some styling

--- src/List/ListItemSecondaryAction.spec.js
+++ src/List/ListItemSecondaryAction.spec.js
@@ -4,13 +4,15 @@
 import React from 'react';
 import { assert } from 'chai';
 import { createShallowWithContext } from 'test/utils';
-import ListItemSecondaryAction from './ListItemSecondaryAction';
+import ListItemSecondaryAction, { styleSheet } from './ListItemSecondaryAction';
 
 describe('<ListItemSecondaryAction />', () => {
   let shallow;
+  let classes;
 
   before(() => {
     shallow = createShallowWithContext();
+    classes = shallow.context.styleManager.render(styleSheet);
   });
 
   it('should render a div', () => {
@@ -18,10 +20,14 @@ describe('<ListItemSecondaryAction />', () => {
       <ListItemSecondaryAction />,
     );
     assert.strictEqual(wrapper.is('div'), true, 'should be a div');
+    assert.strictEqual(wrapper.hasClass(classes.secondaryAction), true,
+      'should have the secondaryAction class');
   });
 
   it('should render with the user and secondaryAction classes', () => {
     const wrapper = shallow(<ListItemSecondaryAction className="woof" />);
     assert.strictEqual(wrapper.hasClass('woof'), true, 'should have the "woof" class');
+    assert.strictEqual(wrapper.hasClass(classes.secondaryAction), true,
+      'should have the secondaryAction class');
   });
 });
