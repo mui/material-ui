fbf8eba2a10dbafe5f8c93a7eb72fe67037edf0a
mbrookes <github@nospam.33m.co>
2017-01-12 01:59:11 +0000

    [Docs] Add API menu and demo button

--- test/regressions/tests/Table/EnhancedTable.js
+++ test/regressions/tests/Table/EnhancedTable.js
@@ -1,5 +1,5 @@
 // @flow weak
 
-import EnhancedTable from 'material-ui-docs/site/src/demos/tables/EnhancedTable';
+import EnhancedTable from 'material-ui-docs/src/pages/demos/tables/EnhancedTable';
 
 export default EnhancedTable;
