80663c2c662fcf6618b5375569925a6424cb45b6
Kevin Ross <kevin.ross@alienfast.com>
2017-01-12 15:30:46 -0600

    Add 8px paddingRight to secondary action

--- src/List/ListItemSecondaryAction.js
+++ src/List/ListItemSecondaryAction.js
@@ -1,12 +1,22 @@
 // @flow weak
 
 import React, { PropTypes } from 'react';
+import { createStyleSheet } from 'jss-theme-reactor';
+import classNames from 'classnames';
 
-export default function ListItemSecondaryAction(props) {
+export const styleSheet = createStyleSheet('ListItemSecondaryAction', () => ({
+  secondaryAction: {
+    paddingRight: 8,
+  },
+}));
+
+export default function ListItemSecondaryAction(props, context) {
   const {
     children,
-    className,
+    className: classNameProp,
   } = props;
+  const classes = context.styleManager.render(styleSheet);
+  const className = classNames(classes.secondaryAction, classNameProp);
 
   return (
     <div className={className}>
