5a67ff0f880d99d6b3b78f5578677b56e1cb26fe
Kevin Ross <kevin.ross@alienfast.com>
2017-01-18 17:02:20 -0600

    Add IconButton regression test with Icon child

---
+++ test/regressions/site/src/tests/IconButton/IconButton.js
@@ -0,0 +1,12 @@
+// @flow weak
+
+import React from 'react';
+import IconButton from 'material-ui/IconButton';
+
+export default function IconButton() {
+  return (
+    <IconButton>
+      <Icon>home</Icon>
+    </IconButton>
+  );
+}
