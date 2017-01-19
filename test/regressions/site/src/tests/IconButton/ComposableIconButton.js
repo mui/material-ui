ccf1e9a46de2800bfa9b9ddb5fc3f91613ab6fdc
Kevin Ross <kevin.ross@alienfast.com>
2017-01-18 19:36:26 -0600

    Failed to add all imports needed for new test

--- test/regressions/site/src/tests/IconButton/ComposableIconButton.js
+++ test/regressions/site/src/tests/IconButton/ComposableIconButton.js
@@ -1,6 +1,7 @@
 // @flow weak
 
 import React from 'react';
+import Icon from 'material-ui/Icon';
 import IconButton from 'material-ui/IconButton';
 
 export default function ComposableIconButton() {
