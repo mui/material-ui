5925ac81499a17a4f65ca6e71d07db65ada6be22
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-02-06 21:27:19 +0100

    [utils] Following the specification, it's now an option

--- src/utils/addEventListener.js
+++ src/utils/addEventListener.js
@@ -3,11 +3,11 @@
 import addEventListener from 'dom-helpers/events/on';
 import removeEventListener from 'dom-helpers/events/off';
 
-export default function (node, event, handler, capture) {
-  addEventListener(node, event, handler, capture);
+export default function (node, event, handler, options) {
+  addEventListener(node, event, handler, options);
   return {
     remove() {
-      removeEventListener(node, event, handler, capture);
+      removeEventListener(node, event, handler, options);
     },
   };
 }
