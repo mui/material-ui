3cbd3368fc8153cf3f7d96812d41d80a6ed28d78
Fabian Kretzer <fabian.kretzer@eddyson.de>
2017-02-14 21:23:53 +0100

    add exports to index.js for inclusion in webpack bundle and streamline exports.

--- src/Paper/index.js
+++ src/Paper/index.js
@@ -1,3 +1,4 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 
 export default from './Paper';
+export Paper from './Paper';
--- src/index.js
+++ src/index.js
@@ -1,4 +1,126 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
+export {
+  AppBar,
+} from './AppBar';
+
+export {
+  Avatar,
+} from './Avatar';
+
+export {
+  Badge,
+} from './Badge';
+
+export {
+  BottomNavigation,
+  BottomNavigationButton,
+} from './BottomNavigation';
+
+export {
+  Button,
+} from './Button';
+
+export {
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+} from './Card';
+
+export {
+  Checkbox,
+} from './Checkbox';
+
+export {
+  Chip,
+} from './Chip';
+
+export {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from './Dialog';
+
+export {
+  Divider,
+} from './Divider';
+
+export {
+  Drawer,
+} from './Drawer';
+
+export {
+  FormControl,
+  FormGroup,
+  FormLabel,
+} from './Form';
+
+export {
+  Icon,
+} from './Icon';
+
+export {
+  IconButton,
+} from './IconButton';
+
+export {
+  Input,
+  InputLabel,
+} from './Input';
+
+export {
+   Layout,
+} from './Layout';
+
+export {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText,
+  ListSubheader,
+} from './List';
+
+export {
+  Menu,
+  MenuItem,
+  MenuList,
+} from './Menu';
+
+export {
+  Paper,
+} from './Paper';
+
+export {
+  CircularProgress,
+  LinearProgress,
+} from './Progress';
+
+export {
+  Radio,
+  RadioGroup,
+} from './Radio';
+
+export {
+  Ripple,
+  TouchRipple,
+} from './Ripple';
+
+export {
+ MuiThemeProvider,
+} from './styles';
+
+export {
+  SvgIcon,
+} from './SvgIcon';
+
+export {
+  Switch,
+  LabelSwitch,
+} from './Switch';
 
 export {
   Table,
@@ -8,4 +130,21 @@ export {
   TableRow,
   TableSortLabel,
 } from './Table';
-export Button from './Button';
+
+export {
+  Tab,
+  Tabs,
+} from './Tabs';
+
+export {
+  Text,
+} from './Text';
+
+export {
+  TextField,
+  TextFieldLabel,
+} from './TextField';
+
+export {
+  Toolbar,
+} from './Toolbar';
