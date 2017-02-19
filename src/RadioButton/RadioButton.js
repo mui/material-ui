5fc5e2c17f961cd0bad9ce5eead49f7807b6dac0
Raphaël Morineau <raphael.morineau@gmail.com>
2017-02-19 13:30:45 +0100

    [docs] Add reference to superselectfield component to related projects
    Closes #6186.
    Closes #5990.

--- src/RadioButton/RadioButton.js
+++ src/RadioButton/RadioButton.js
@@ -172,14 +172,12 @@ class RadioButton extends Component {
     const uncheckedElement = React.isValidElement(uncheckedIcon) ?
       React.cloneElement(uncheckedIcon, {
         style: Object.assign(uncheckedStyles, uncheckedIcon.props.style),
-      }) :
-      <RadioButtonOff style={uncheckedStyles} />;
+      }) : <RadioButtonOff style={uncheckedStyles} />;
 
     const checkedElement = React.isValidElement(checkedIcon) ?
       React.cloneElement(checkedIcon, {
         style: Object.assign(checkedStyles, checkedIcon.props.style),
-      }) :
-      <RadioButtonOn style={checkedStyles} />;
+      }) : <RadioButtonOn style={checkedStyles} />;
 
     const mergedIconStyle = Object.assign(styles.icon, iconStyle);
     const mergedLabelStyle = Object.assign(styles.label, labelStyle);
