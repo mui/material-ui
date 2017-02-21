2a181b36a297b41b2922218eeb78e5b575b937ed
Jess Thrysoee <jess@thrysoee.dk>
2017-02-21 23:34:27 +0100

    Fix lint errors

--- docs/src/app/components/pages/components/SelectField/ExampleMultiSelect.js
+++ docs/src/app/components/pages/components/SelectField/ExampleMultiSelect.js
@@ -1,29 +1,29 @@
-import React, {Component} from "react";
-import SelectField from "material-ui/SelectField";
-import MenuItem from "material-ui/MenuItem";
+import React, {Component} from 'react';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 
 const names = [
-  "Oliver Hansen",
-  "Van Henry",
-  "April Tucker",
-  "Ralph Hubbard",
-  "Omar Alexander",
-  "Carlos Abbott",
-  "Miriam Wagner",
-  "Bradley Wilkerson",
-  "Virginia Andrews",
-  "Kelly Snyder"
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder',
 ];
 
 export default class SelectFieldExampleMultiSelect extends Component {
   state = {
-    values: []
+    values: [],
   };
 
   handleChange = (event, index, values) => this.setState({values});
 
   menuItems(values) {
-    return names.map(name => (
+    return names.map((name) => (
       <MenuItem
         key={name}
         insetChildren={true}
