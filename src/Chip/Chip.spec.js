7d857061b54e4c611c0691924ae5bf703d98c8f0
Agam Rafaeli <agam@rafaeli.net>
2017-03-19 00:41:37 +0200

    Add tests for handleKeyDown for Chip module

--- src/Chip/Chip.spec.js
+++ src/Chip/Chip.spec.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { assert } from 'chai';
+import { spy, stub } from 'sinon';
 import { createShallow } from 'test/utils';
 import Chip, { styleSheet } from './Chip.js';
 import Avatar from '../Avatar/Avatar.js';
@@ -133,5 +134,88 @@ describe('<Chip />', () => {
     it('should not have a tabIndex prop', () => {
       assert.strictEqual(wrapper.prop('tabIndex'), undefined);
     });
+  });
+
+  describe('reacts to keyboard chip', () => {
+    let wrapper;
+    let onKeyDownSpy;
+
+    before(() => {
+      wrapper = shallow(
+        <Chip
+          className="my-Chip"
+          data-my-prop="woof"
+        >
+          Text Chip
+        </Chip>,
+      );
+
+      onKeyDownSpy = spy();
+      wrapper.setProps({ onKeyDown: onKeyDownSpy });
+    });
+
+    it('should call onKeyDown when a key is pressed', () => {
+      let anyKeydownEvent = {
+        preventDefault: () => {},
+        keyCode: 80 // keycode for `p`, which is not `space`, `enter` or `esc`
+      };
+      wrapper.find('button').simulate('keydown', anyKeydownEvent);
+      assert.strictEqual(onKeyDownSpy.callCount, 1, 'should have called onKeyDown');
+      assert(onKeyDownSpy.calledWith(anyKeydownEvent));
+    });
+
+    describe('onClick is defined', () => {
+      let onClickSpy;
+      before(() => {
+        onClickSpy = spy();
+        wrapper.setProps({ onClick: onClickSpy });
+      });
+
+      afterEach(() => {
+        onClickSpy.reset();
+      });
+
+      it('should call onClick when `space` is pressed ', () => {
+        let preventDefaultSpy = spy();
+        let spaceKeydownEvent = {
+          preventDefault: preventDefaultSpy,
+          keyCode: 32 // keycode `space`
+        };
+        wrapper.find('button').simulate('keydown', spaceKeydownEvent);
+        assert.strictEqual(preventDefaultSpy.callCount, 1, 'should have stopped event propagation');
+        assert.strictEqual(onClickSpy.callCount, 1, 'should have called onClick');
+        assert(onClickSpy.calledWith(spaceKeydownEvent));
+      });
+
+      it('should call onClick when `enter` is pressed ', () => {
+        let preventDefaultSpy = spy();
+        let enterKeydownEvent = {
+          preventDefault: preventDefaultSpy,
+          keyCode: 13 // keycode `enter`
+        };
+        wrapper.find('button').simulate('keydown', enterKeydownEvent);
+        assert.strictEqual(preventDefaultSpy.callCount, 1, 'should have stopped event propagation');
+        assert.strictEqual(onClickSpy.callCount, 1, 'should have called onClick');
+        assert(onClickSpy.calledWith(enterKeydownEvent));
+      });
+    });
+
+    describe('onRequestDelete is defined  and `backspace is pressed', () => {
+      it('should call onRequestDelete', () => {
+        let onRequestDeleteSpy = spy();
+        wrapper.setProps({ onRequestDelete: onRequestDeleteSpy });
+
+        let preventDefaultSpy = spy();
+        let backspaceKeydownEvent = {
+          preventDefault: preventDefaultSpy,
+          keyCode: 8 // keycode `backspace`
+        };
+        wrapper.find('button').simulate('keydown', backspaceKeydownEvent);
+
+        assert.strictEqual(preventDefaultSpy.callCount, 1, 'should have stopped event propagation');
+        assert.strictEqual(onRequestDeleteSpy.callCount, 1, 'should have called onClick');
+        assert(onRequestDeleteSpy.calledWith(backspaceKeydownEvent));
+      })
+    })
   });
 });
