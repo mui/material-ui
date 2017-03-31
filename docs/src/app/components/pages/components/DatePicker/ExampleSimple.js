ee28506345b4c9620a439f5bb85bcd9adf372bad
Trevor Hall <trevor.hall@viseo.com>
2017-03-31 15:14:00 +0800

    [DatePicker] update docs to reflect boolean change

--- docs/src/app/components/pages/components/DatePicker/ExampleSimple.js
+++ docs/src/app/components/pages/components/DatePicker/ExampleSimple.js
@@ -4,7 +4,7 @@ import DatePicker from 'material-ui/DatePicker';
 /**
  * The Date Picker defaults to a portrait dialog. The `mode` property can be set to `landscape`.
  * You can also disable the Dialog passing `true` to the `disabled` property.
- * To display the year selection first, set the `initialView` property to "year".
+ * To display the year selection first, set the `openToYearSelection` property to `true`.
  */
 const DatePickerExampleSimple = () => (
   <div>
