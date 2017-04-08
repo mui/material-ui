d90dfdd57806892050aac9593764f2b5b7ed1ae3
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-04-08 11:51:00 +0200

    [test] Makes sure argos run even if diff fails (#6512)

--- scripts/run-circle-tests.sh
+++ scripts/run-circle-tests.sh
@@ -6,7 +6,6 @@ npm run flow
 npm run test:coverage
 npm run test:karma
 DOCKER_TEST_URL=http://$(ip addr show docker0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1):3090 npm run test:regressions
-npm run argos
 
 if git log ${TRAVIS_COMMIT_RANGE} | grep -Ei '\[codemod\]'; then
   cd packages/material-ui-codemod && npm install && npm test
