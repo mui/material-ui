ee5b8ad0b77942fc97f0cb4401887303c42b73e1
Shawn McKnight <mck321@gmail.com>
2017-04-11 07:24:36 -0400

    [Tabs] Add scrollable behavior (#6502)
    * [Tabs] Add scrollable behavior #4795
    
    * Add lint overrides for unused props
    
    * Updated with changes requested
    
    * Added additional requests
    - Rebase/syntax requests
    - Support components docs
    - vrtest baseline image update
    
    * Update TabScrollButton test due to default propType
    Visible now has a default propType of true and the not visible test requires an explicit declaration of false.

---
+++ docs/src/pages/component-demos/tabs/ScrollableTabsButtonAuto.js
@@ -0,0 +1,76 @@
+// @flow weak
+/* eslint-disable react/no-multi-comp */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createStyleSheet } from 'jss-theme-reactor';
+import customPropTypes from 'material-ui/utils/customPropTypes';
+import Paper from 'material-ui/Paper';
+import { Tabs, Tab } from 'material-ui/Tabs';
+
+const TabContainer = (props) => (
+  <div style={{ padding: 20 }}>
+    {props.children}
+  </div>
+);
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styleSheet = createStyleSheet('ScrollableTabsButtonAuto', (theme) => ({
+  root: {
+    marginTop: 30,
+    width: '100%',
+  },
+  appBar: {
+    backgroundColor: theme.palette.primary[500],
+    color: theme.palette.getContrastText(theme.palette.primary[500]),
+  },
+}));
+
+export default class ScrollableTabsButtonAuto extends Component {
+  static contextTypes = {
+    styleManager: customPropTypes.muiRequired,
+  };
+
+  state = {
+    index: 0,
+  };
+
+  handleChange = (event, index) => {
+    this.setState({ index });
+  }
+
+  render() {
+    const classes = this.context.styleManager.render(styleSheet);
+
+    return (
+      <Paper className={classes.root}>
+        <div className={classes.appBar}>
+          <Tabs
+            index={this.state.index}
+            onChange={this.handleChange}
+            scrollable
+            scrollButtons="auto"
+          >
+            <Tab label="Item One" />
+            <Tab label="Item Two" />
+            <Tab label="Item Three" />
+            <Tab label="Item Four" />
+            <Tab label="Item Five" />
+            <Tab label="Item Six" />
+            <Tab label="Item Seven" />
+          </Tabs>
+        </div>
+        {this.state.index === 0 && <TabContainer>{'Item One'}</TabContainer>}
+        {this.state.index === 1 && <TabContainer>{'Item Two'}</TabContainer>}
+        {this.state.index === 2 && <TabContainer>{'Item Three'}</TabContainer>}
+        {this.state.index === 3 && <TabContainer>{'Item Four'}</TabContainer>}
+        {this.state.index === 4 && <TabContainer>{'Item Five'}</TabContainer>}
+        {this.state.index === 5 && <TabContainer>{'Item Six'}</TabContainer>}
+        {this.state.index === 6 && <TabContainer>{'Item Seven'}</TabContainer>}
+      </Paper>
+    );
+  }
+}
---
+++ src/Tabs/TabScrollButton.spec.js
@@ -0,0 +1,74 @@
+// @flow weak
+
+import React from 'react';
+import { assert } from 'chai';
+import { createShallow, createMount } from 'src/test-utils';
+import TabScrollButton, { styleSheet } from './TabScrollButton';
+import ButtonBase from '../internal/ButtonBase';
+import KeyboardArrowLeft from '../svg-icons/keyboard-arrow-left';
+import KeyboardArrowRight from '../svg-icons/keyboard-arrow-right';
+
+describe('<TabScrollButton />', () => {
+  let shallow;
+  let mount;
+  let classes;
+
+  before(() => {
+    shallow = createShallow();
+    classes = shallow.context.styleManager.render(styleSheet);
+    mount = createMount();
+  });
+
+  after(() => {
+    mount.cleanUp();
+  });
+
+  describe('prop: visible', () => {
+    it('should render as a button with the root class', () => {
+      const wrapper = shallow(
+        <TabScrollButton visible />,
+      );
+
+      assert.strictEqual(wrapper.is(ButtonBase), true, 'should be a button');
+      assert.strictEqual(wrapper.hasClass(classes.root), true, 'should have the root class');
+    });
+  });
+
+  describe('prop: !visible', () => {
+    it('should render as a div with root class', () => {
+      const wrapper = shallow(
+        <TabScrollButton visible={false} />,
+      );
+
+      assert.strictEqual(wrapper.is('div'), true, 'should be a div');
+      assert.strictEqual(wrapper.hasClass(classes.root), true, 'should have the root class');
+    });
+  });
+
+  describe('prop: className', () => {
+    it('should render with the user and root classes', () => {
+      const wrapper = shallow(
+        <TabScrollButton className="woof" />,
+      );
+      assert.strictEqual(wrapper.hasClass('woof'), true, 'should have the "woof" class');
+      assert.strictEqual(wrapper.hasClass(classes.root), true, 'should have the root class');
+    });
+  });
+
+  describe('prop: direction', () => {
+    it('should render with the left icon', () => {
+      const wrapper = mount(
+        <TabScrollButton direction={'left'} visible />,
+      );
+      assert.strictEqual(wrapper.childAt(0).is(KeyboardArrowLeft), true, 'should be the left icon');
+    });
+
+    it('should render with the right icon', () => {
+      const wrapper = mount(
+        <TabScrollButton direction={'right'} visible />,
+      );
+      assert.strictEqual(wrapper.childAt(0).is(KeyboardArrowRight), true,
+        'should be the right icon');
+    });
+  });
+});
