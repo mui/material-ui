0ea7142cbac8698915fe8606e7980e92780b2a8d
Benjamin Dummer <dummerbenjamin@gmail.com>
2017-04-13 20:17:10 -0400

    Fixed missing import

--- src/SelectField/index.js
+++ src/SelectField/index.js
@@ -1,3 +1,5 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 
 export default from './SelectField';
+export SelectField from './SelectField';
+export SelectFieldInput from './SelectFieldInput';
