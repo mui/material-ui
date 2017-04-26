9bacd9921c924d4515b846367550a4b3f1657067
Stephan Bijzitter <StephanBijzitter@gmail.com>
2017-04-26 19:22:33 +0200

    [Stepper] Allow single Step child (#6707)
    * [Stepper] Allow single Step child
    
    * [Stepper] Simplify propType matcher

--- src/Stepper/Stepper.js
+++ src/Stepper/Stepper.js
@@ -23,9 +23,9 @@ class Stepper extends Component {
      */
     activeStep: PropTypes.number,
     /**
-     * Should be two or more `<Step />` components
+     * Should be two or more `<Step />` components.
      */
-    children: PropTypes.arrayOf(PropTypes.node),
+    children: PropTypes.node,
     /**
      * A component to be placed between each step.
      */
