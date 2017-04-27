b75eb932c2ad12c1e138b30068d73b4d548e2d24
Kevin Ross <kevin.ross@alienfast.com>
2017-04-27 12:15:40 -0500

    [Layout] convert flow types

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,4 +1,4 @@
-// @flow weak
+// @flow
 
 /**
  * A layout component using the following libs as inspiration.
@@ -12,8 +12,7 @@
  * Follow this flexbox Guide to better understand the underlying model:
  * - https://css-tricks.com/snippets/css/a-guide-to-flexbox/
  */
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Element, PureComponent } from 'react';
 import classNames from 'classnames';
 import { createStyleSheet } from 'jss-theme-reactor';
 import customPropTypes from '../utils/customPropTypes';
@@ -32,8 +31,8 @@ function generateGrid(globalStyles, theme, breakpoint) {
     },
   };
 
-  GRID_SIZES.forEach((size, index) => {
-    if (index === 0) { // Skip the first one as handle above.
+  GRID_SIZES.forEach((size) => {
+    if (typeof size === 'boolean') { // Skip the first one as handle above.
       return;
     }
 
@@ -140,169 +139,166 @@ export const styleSheet = createStyleSheet('MuiLayout', (theme) => {
   };
 });
 
-function Layout(props, context) {
-  const {
-    children,
-    className,
-    component: ComponentProp,
-    container,
-    item,
-    align,
-    direction,
-    xs,
-    sm,
-    md,
-    lg,
-    xl,
-    gutter,
-    justify,
-    wrap,
-    ...other
-  } = props;
+type Align = 'flex-start' | 'center' | 'flex-end' | 'stretch'
+type Direction = 'row' | 'row-reverse' | 'column' | 'column-reverse'
+type Gutters = 0 | 8 | 16 | 24 | 40
+type GridSizes = boolean | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
+type Justify = 'flex-start' | 'center' | 'flex-end' | 'space-between' | 'space-around'
+type Wrap = 'nowrap' | 'wrap' | 'wrap-reverse'
 
-  const classes = context.styleManager.render(styleSheet);
-
-  return (
-    <ComponentProp
-      className={classNames({
-        [classes.typeContainer]: container,
-        [classes.typeItem]: item,
-        [classes[`gutter-xs-${gutter}`]]: container && gutter !== 0,
-        [classes[`direction-xs-${direction}`]]: direction !== Layout.defaultProps.direction,
-        [classes[`wrap-xs-${wrap}`]]: wrap !== Layout.defaultProps.wrap,
-        [classes[`align-xs-${align}`]]: align !== Layout.defaultProps.align,
-        [classes[`justify-xs-${justify}`]]: justify !== Layout.defaultProps.justify,
-        [classes['grid-xs']]: xs === true,
-        [classes[`grid-xs-${xs}`]]: xs && xs !== true,
-        [classes['grid-sm']]: sm === true,
-        [classes[`grid-sm-${sm}`]]: sm && sm !== true,
-        [classes['grid-md']]: md === true,
-        [classes[`grid-md-${md}`]]: md && md !== true,
-        [classes['grid-lg']]: lg === true,
-        [classes[`grid-lg-${lg}`]]: lg && lg !== true,
-        [classes['grid-xl']]: xl === true,
-        [classes[`grid-xl-${xl}`]]: xl && xl !== true,
-      }, className)}
-      {...other}
-    >
-      {children}
-    </ComponentProp>
-  );
+type DefaultProps = {
+  align: Align,
+  component: string | Function,
+  direction: Direction,
+  justify: Justify,
+  wrap: Wrap,
 }
 
-Layout.propTypes = {
+type Props = DefaultProps & {
   /**
    * The content of the component.
    */
-  children: PropTypes.node,
+    children?: Element<any>,
   /**
    * The CSS class name of the root element.
    */
-  className: PropTypes.string,
+    className?: string,
   /**
    * The component used for the root node.
    * Either a string to use a DOM element or a component.
    */
-  component: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.func,
-  ]),
+    component?: string | Function,
   /**
    * If `true`, the component will have the flex *container* behavior.
    * You should be wrapping *items* with a *container*.
    */
-  container: PropTypes.bool,
+    container?: boolean,
   /**
    * It true, the component will have the flex *item* behavior.
    * You should be wrapping *items* with a *container*.
    */
-  item: PropTypes.bool,
+    item?: boolean,
   /**
    * Defines the number of grids the component is going to use.
    * It's applied for all the screen sizes with the lowest priority.
    */
-  xs: PropTypes.oneOf(GRID_SIZES),
+    xs?: GridSizes,
   /**
    * Defines the number of grids the component is going to use.
    * It's applied for the `sm` breakpoint and wider screens if not overridden.
    */
-  sm: PropTypes.oneOf(GRID_SIZES), // eslint-disable-line react/sort-prop-types
+    sm?: GridSizes, // eslint-disable-line react/sort-prop-types
   /**
    * Defines the number of grids the component is going to use.
    * It's applied for the `md` breakpoint and wider screens if not overridden.
    */
-  md: PropTypes.oneOf(GRID_SIZES), // eslint-disable-line react/sort-prop-types
+    md?: GridSizes, // eslint-disable-line react/sort-prop-types
   /**
    * Defines the number of grids the component is going to use.
    * It's applied for the `lg` breakpoint and wider screens if not overridden.
    */
-  lg: PropTypes.oneOf(GRID_SIZES), // eslint-disable-line react/sort-prop-types
+    lg?: GridSizes, // eslint-disable-line react/sort-prop-types
   /**
    * Defines the number of grids the component is going to use.
    * It's applied for the `xl` breakpoint and wider screens.
    */
-  xl: PropTypes.oneOf(GRID_SIZES), // eslint-disable-line react/sort-prop-types
+    xl?: GridSizes, // eslint-disable-line react/sort-prop-types
   /**
    * Defines the `align-items` style property.
    * It's applied for all screen sizes.
    */
-  align: PropTypes.oneOf([ // eslint-disable-line react/sort-prop-types
-    'flex-start',
-    'center',
-    'flex-end',
-    'stretch',
-  ]),
+    align?: Align, // eslint-disable-line react/sort-prop-types
   /**
    * Defines the `flex-direction` style property.
    * It is applied for all screen sizes.
    */
-  direction: PropTypes.oneOf([ // eslint-disable-line react/sort-prop-types
-    'row',
-    'row-reverse',
-    'column',
-    'column-reverse',
-  ]),
+    direction?: Direction, // eslint-disable-line react/sort-prop-types
   /**
    * Defines the space between the type `item` component.
    * It can only be used on a type `container` component.
    */
-  gutter: PropTypes.oneOf(GUTTERS), // eslint-disable-line react/sort-prop-types
+    gutter?: Gutters, // eslint-disable-line react/sort-prop-types
   /**
    * Defines the `justify-content` style property.
    * It is applied for all screen sizes.
    */
-  justify: PropTypes.oneOf([ // eslint-disable-line react/sort-prop-types
-    'flex-start',
-    'center',
-    'flex-end',
-    'space-between',
-    'space-around',
-  ]),
+    justify?: Justify, // eslint-disable-line react/sort-prop-types
   /**
    * Defines the `flex-wrap` style property.
    * It's applied for all screen sizes.
    */
-  wrap: PropTypes.oneOf([ // eslint-disable-line react/sort-prop-types
-    'nowrap',
-    'wrap',
-    'wrap-reverse',
-  ]),
+    wrap?: Wrap, // eslint-disable-line react/sort-prop-types
 };
 
-Layout.defaultProps = {
-  component: 'div',
-  container: false,
-  item: false,
-  align: 'stretch',
-  direction: 'row',
-  gutter: 16,
-  justify: 'flex-start',
-  wrap: 'wrap',
-};
+class Layout extends PureComponent<DefaultProps, Props, void> {
+  static defaultProps: DefaultProps = {
+    component: 'div',
+    container: false,
+    item: false,
+    align: 'stretch',
+    direction: 'row',
+    gutter: 16,
+    justify: 'flex-start',
+    wrap: 'wrap',
+  };
 
-Layout.contextTypes = {
-  styleManager: customPropTypes.muiRequired,
-};
+  static contextTypes = {
+    styleManager: customPropTypes.muiRequired,
+  };
+
+  props: Props;
+
+  render(): Element<any> {
+    const {
+      children,
+      className,
+      component: ComponentProp,
+      container,
+      item,
+      align,
+      direction,
+      xs,
+      sm,
+      md,
+      lg,
+      xl,
+      gutter,
+      justify,
+      wrap,
+      ...other
+    } = this.props;
+
+    const classes = this.context.styleManager.render(styleSheet);
+
+    return (
+      <ComponentProp
+        className={classNames({
+          [classes.typeContainer]: container,
+          [classes.typeItem]: item,
+          [classes[`gutter-xs-${String(gutter)}`]]: container && gutter !== 0,
+          [classes[`direction-xs-${String(direction)}`]]:
+            direction !== Layout.defaultProps.direction,
+          [classes[`wrap-xs-${String(wrap)}`]]: wrap !== Layout.defaultProps.wrap,
+          [classes[`align-xs-${String(align)}`]]: align !== Layout.defaultProps.align,
+          [classes[`justify-xs-${String(justify)}`]]: justify !== Layout.defaultProps.justify,
+          [classes['grid-xs']]: xs === true,
+          [classes[`grid-xs-${String(xs)}`]]: xs && xs !== true,
+          [classes['grid-sm']]: sm === true,
+          [classes[`grid-sm-${String(sm)}`]]: sm && sm !== true,
+          [classes['grid-md']]: md === true,
+          [classes[`grid-md-${String(md)}`]]: md && md !== true,
+          [classes['grid-lg']]: lg === true,
+          [classes[`grid-lg-${String(lg)}`]]: lg && lg !== true,
+          [classes['grid-xl']]: xl === true,
+          [classes[`grid-xl-${String(xl)}`]]: xl && xl !== true,
+        }, className)}
+        {...other}
+      >
+        {children}
+      </ComponentProp>
+    );
+  }
+}
 
 /**
  * Add a wrapper component to generate some helper messages in the development
@@ -313,7 +309,7 @@ let LayoutWrapper = Layout; // eslint-disable-line import/no-mutable-exports
 if (process.env.NODE_ENV !== 'production') {
   const requireProp = requirePropFactory('Layout');
 
-  LayoutWrapper = (props) => <Layout {...props} />;
+  LayoutWrapper = (props: any) => <Layout {...props} />;
 
   LayoutWrapper.propTypes = {
     align: requireProp('container'),
