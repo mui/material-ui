7e72bce64c29ae12aec16308f3fa2795fc4a0d0a
Kevin Ross <kevin.ross@alienfast.com>
2017-04-28 19:14:10 -0500

    [Layout] revert to stateless function

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,5 +1,4 @@
 // @flow
-
 /**
  * A layout component using the following libs as inspiration.
  *
@@ -12,7 +11,7 @@
  * Follow this flexbox Guide to better understand the underlying model:
  * - https://css-tricks.com/snippets/css/a-guide-to-flexbox/
  */
-import React, { Element, PureComponent } from 'react';
+import React, { Element } from 'react';
 import classNames from 'classnames';
 import { createStyleSheet } from 'jss-theme-reactor';
 import customPropTypes from '../utils/customPropTypes';
@@ -146,15 +145,7 @@ type GridSizes = boolean | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
 type Justify = 'flex-start' | 'center' | 'flex-end' | 'space-between' | 'space-around'
 type Wrap = 'nowrap' | 'wrap' | 'wrap-reverse'
 
-type DefaultProps = {
-  align: Align,
-  component: string | Function,
-  direction: Direction,
-  justify: Justify,
-  wrap: Wrap,
-}
-
-type Props = DefaultProps & {
+type Props = {
   /**
    * The content of the component.
    */
@@ -230,75 +221,72 @@ type Props = DefaultProps & {
     wrap?: Wrap, // eslint-disable-line react/sort-prop-types
 };
 
-class Layout extends PureComponent<DefaultProps, Props, void> {
-  static defaultProps: DefaultProps = {
-    component: 'div',
-    container: false,
-    item: false,
-    align: 'stretch',
-    direction: 'row',
-    gutter: 16,
-    justify: 'flex-start',
-    wrap: 'wrap',
-  };
+function Layout(props: Props, context: any) {
+  const {
+    children,
+    className,
+    component,
+    container,
+    item,
+    align,
+    direction,
+    xs,
+    sm,
+    md,
+    lg,
+    xl,
+    gutter,
+    justify,
+    wrap,
+    ...other
+  } = props;
 
-  static contextTypes = {
-    styleManager: customPropTypes.muiRequired,
-  };
+  const classes = context.styleManager.render(styleSheet);
+  // workaround: see https://github.com/facebook/flow/issues/1660#issuecomment-297775427
+  const ComponentProp = component || Layout.defaultProps.component;
 
-  props: Props;
-
-  render(): Element<any> {
-    const {
-      children,
-      className,
-      component: ComponentProp,
-      container,
-      item,
-      align,
-      direction,
-      xs,
-      sm,
-      md,
-      lg,
-      xl,
-      gutter,
-      justify,
-      wrap,
-      ...other
-    } = this.props;
-
-    const classes = this.context.styleManager.render(styleSheet);
-
-    return (
-      <ComponentProp
-        className={classNames({
-          [classes.typeContainer]: container,
-          [classes.typeItem]: item,
-          [classes[`gutter-xs-${String(gutter)}`]]: container && gutter !== 0,
-          [classes[`direction-xs-${String(direction)}`]]:
-            direction !== Layout.defaultProps.direction,
-          [classes[`wrap-xs-${String(wrap)}`]]: wrap !== Layout.defaultProps.wrap,
-          [classes[`align-xs-${String(align)}`]]: align !== Layout.defaultProps.align,
-          [classes[`justify-xs-${String(justify)}`]]: justify !== Layout.defaultProps.justify,
-          [classes['grid-xs']]: xs === true,
-          [classes[`grid-xs-${String(xs)}`]]: xs && xs !== true,
-          [classes['grid-sm']]: sm === true,
-          [classes[`grid-sm-${String(sm)}`]]: sm && sm !== true,
-          [classes['grid-md']]: md === true,
-          [classes[`grid-md-${String(md)}`]]: md && md !== true,
-          [classes['grid-lg']]: lg === true,
-          [classes[`grid-lg-${String(lg)}`]]: lg && lg !== true,
-          [classes['grid-xl']]: xl === true,
-          [classes[`grid-xl-${String(xl)}`]]: xl && xl !== true,
-        }, className)}
-        {...other}
-      >
-        {children}
-      </ComponentProp>
-    );
-  }
+  return (
+    <ComponentProp
+      className={classNames({
+        [classes.typeContainer]: container,
+        [classes.typeItem]: item,
+        [classes[`gutter-xs-${String(gutter)}`]]: container && gutter !== 0,
+        [classes[`direction-xs-${String(direction)}`]]: direction !== Layout.defaultProps.direction,
+        [classes[`wrap-xs-${String(wrap)}`]]: wrap !== Layout.defaultProps.wrap,
+        [classes[`align-xs-${String(align)}`]]: align !== Layout.defaultProps.align,
+        [classes[`justify-xs-${String(justify)}`]]: justify !== Layout.defaultProps.justify,
+        [classes['grid-xs']]: xs === true,
+        [classes[`grid-xs-${String(xs)}`]]: xs && xs !== true,
+        [classes['grid-sm']]: sm === true,
+        [classes[`grid-sm-${String(sm)}`]]: sm && sm !== true,
+        [classes['grid-md']]: md === true,
+        [classes[`grid-md-${String(md)}`]]: md && md !== true,
+        [classes['grid-lg']]: lg === true,
+        [classes[`grid-lg-${String(lg)}`]]: lg && lg !== true,
+        [classes['grid-xl']]: xl === true,
+        [classes[`grid-xl-${String(xl)}`]]: xl && xl !== true,
+      }, className)}
+      {...other}
+    >
+      {children}
+    </ComponentProp>
+  );
 }
+
+Layout.defaultProps = {
+  component: 'div',
+  container: false,
+  item: false,
+  align: 'stretch',
+  direction: 'row',
+  gutter: 16,
+  justify: 'flex-start',
+  wrap: 'wrap',
+};
+
+Layout.contextTypes = {
+  styleManager: customPropTypes.muiRequired,
+};
 
 /**
  * Add a wrapper component to generate some helper messages in the development
