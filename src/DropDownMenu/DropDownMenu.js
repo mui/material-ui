a60b931a9ef036b8c6b4dbef09a652911542a494
Sergey <tyupaevs@gmail.com>
2017-04-29 13:13:36 +0300

    [DropDownMenu] Pass autoWidth property to Menu component (#6632)
    * DropDownMenu fix: Pass autoWidth property down to Menu component
    
    * DropDownMenu fix: Pass width property down to Menu component

--- src/DropDownMenu/DropDownMenu.js
+++ src/DropDownMenu/DropDownMenu.js
@@ -476,6 +476,8 @@ class DropDownMenu extends Component {
             onChange={this.handleChange}
             menuItemStyle={menuItemStyle}
             selectedMenuItemStyle={selectedMenuItemStyle}
+            autoWidth={autoWidth}
+            width={!autoWidth && menuStyle ? menuStyle.width : null}
           >
             {children}
           </Menu>
