22572c5939632fecafd217073c6f64c1b9f011e2
jony89 <ariel@emath.co.il>
2017-04-30 00:53:36 +0300

    [Tabs] Fix transition when isRtl is used (#6737)
    * fixing #6444 focus index on multiselect
    
    * fixing tab transition when isRtl is used
    
    * Update InkBar.js

--- src/Tabs/InkBar.js
+++ src/Tabs/InkBar.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import transitions from '../styles/transitions';
 
 function getStyles(props, context) {
-  const {inkBar} = context.muiTheme;
+  const {inkBar, isRtl} = context.muiTheme;
 
   return {
     root: {
@@ -15,7 +15,7 @@ function getStyles(props, context) {
       height: 2,
       marginTop: -2,
       position: 'relative',
-      transition: transitions.easeOut('1s', 'left'),
+      transition: transitions.easeOut('1s', isRtl ? 'right' : 'left'),
     },
   };
 }
