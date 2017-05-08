2103070a6bd78789d77fbf3bddc648202a8be683
Katherine Mello <katherine.mello8@gmail.com>
2017-05-08 14:45:44 -0400

    fix linting issue

--- src/Tabs/InkBar.js
+++ src/Tabs/InkBar.js
@@ -16,7 +16,7 @@ function getStyles(props, context) {
       marginTop: -2,
       position: 'relative',
       transition: transitions.easeOut('1s', isRtl ? 'right' : 'left'),
-      zIndex: 1000
+      zIndex: 1000,
     },
   };
 }
