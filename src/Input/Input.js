62a615f711c6e9c06e3a96e94280ba197fbf4a2e
Peter Berg <atticusberg@gmail.com>
2017-05-12 14:43:31 -0400

    Position the top of inkbar on top of the underline.

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -27,7 +27,7 @@ export const styleSheet = createStyleSheet('MuiInput', (theme) => {
       '&:after': {
         backgroundColor: theme.palette.primary.A200,
         left: 0,
-        bottom: 0,
+        bottom: -2,
         // Doing the other way around crash on IE11 "''" https://github.com/cssinjs/jss/issues/242
         content: '""',
         height: 2,
