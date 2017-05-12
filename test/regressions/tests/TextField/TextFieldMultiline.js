ed12ff3174011265ab52214367aeac313f98f5dd
Peter Berg <atticusberg@gmail.com>
2017-05-12 14:45:33 -0400

    adds TextFieldMultiline regression test back in with proper capitalization

---
+++ test/regressions/tests/TextField/TextFieldMultiline.js
@@ -0,0 +1,17 @@
+// @flow weak
+
+import React from 'react';
+import TextField from 'material-ui/TextField';
+
+export default function TextFieldMultiline() {
+  return (
+    <div>
+      <TextField
+        label="Foo"
+        multiline
+        rows={4}
+        value="Default text"
+      />
+    </div>
+  );
+}
