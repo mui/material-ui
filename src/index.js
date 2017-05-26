b11a2e86152adeabe4a36eb6d2366ef122f2c80c
Ruslan Kyba <kybargr@gmail.com>
2017-05-27 02:13:11 +0300

    FormHelperText exports

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ export {
 export { default as Divider } from './Divider';
 export { default as Drawer } from './Drawer';
 
-export { FormControl, FormGroup, FormLabel } from './Form';
+export { FormControl, FormGroup, FormLabel, FormHelperText } from './Form';
 
 export { default as Hidden } from './Hidden';
 export { default as Icon } from './Icon';
