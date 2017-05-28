d4f6ad0d9ecfa38ab842d439050613c466e11ad0
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-05-28 14:57:42 +0200

    [test] Fix usage of old API

--- test/regressions/TestViewer.js
+++ test/regressions/TestViewer.js
@@ -1,6 +1,7 @@
 // @flow weak
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { createStyleSheet } from 'jss-theme-reactor';
 import customPropTypes from 'material-ui/utils/customPropTypes';
 
