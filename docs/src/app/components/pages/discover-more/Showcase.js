d248f4c152c7d9146db0cc5e77bd362ed2f5b515
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-06-02 11:01:38 +0200

    Update Showcase.js

--- docs/src/app/components/pages/discover-more/Showcase.js
+++ docs/src/app/components/pages/discover-more/Showcase.js
@@ -70,6 +70,13 @@ const appList = [
     source: 'https://bitbucket.org/wzup/order-form',
   },
   {
+    title: 'React Most Wanted',
+    author: 'Tarik Huber',
+    img: 'images/showcase/react-most-wanted.png',
+    link: 'https://www.react-most-wanted.com/',
+    source: 'https://github.com/TarikHuber/react-most-wanted',
+  },
+  {
     title: 'Cloudcraft',
     author: 'Cloudcraft',
     img: 'images/showcase/cloudcraft.png',
@@ -146,13 +153,6 @@ const appList = [
     author: 'BrainBOK',
     img: 'images/showcase/brainbok-for-pmp-capm-certification.png',
     link: 'https://www.brainbok.com',
-  },
-  {
-    title: 'React Most Wanted',
-    author: 'Tarik Huber',
-    img: 'images/showcase/react-most-wanted.png',
-    link: 'https://www.react-most-wanted.com/',
-    source: 'https://github.com/TarikHuber/react-most-wanted',
   },
 ];
 
