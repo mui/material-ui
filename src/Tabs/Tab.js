5aeb8da6eb52140f7b602d73c24d849524512e68
RejinR <rejin1994@gmail.com>
2017-06-12 14:16:08 +0530

    SVG Icon passed to Tab supports hoverColor for more customisability

--- src/Tabs/Tab.js
+++ src/Tabs/Tab.js
@@ -120,12 +120,18 @@ class Tab extends Component {
       };
       // If it's svg icon set color via props
       if (icon.type.muiName !== 'FontIcon') {
-        if (icon.props && icon.props.style && icon.props.style.color) {
-          iconProps.color = icon.props.style.color;
-        } else if (icon.props && icon.props.color) {
-          iconProps.color = icon.props.color;
-        } else {
-          iconProps.color = styles.root.color;
+        if (icon.props) {
+          if (icon.props.style && icon.props.style.color) {
+            iconProps.color = icon.props.style.color;
+          } else if (icon.props.color) {
+            iconProps.color = icon.props.color;
+          } else {
+            iconProps.color = styles.root.color;
+          }
+
+          if (icon.props.hoverColor) {
+            iconProps.hoverColor = icon.props.hoverColor;
+          }
         }
       }
       iconElement = React.cloneElement(icon, iconProps);
