214abbfedbd18af694fb0af921a113f562e69098
Alex Hayes <alex@alution.com>
2017-06-20 02:21:57 +1000

    [docs] Docs that contain info on how to use the breakpoints attribute in the theme. (#7172)

---
+++ docs/src/pages/layout/css-in-js/MediaQuery.js
@@ -0,0 +1,42 @@
+// @flow
+import React from 'react';
+import PropTypes from 'prop-types';
+import compose from 'recompose/compose';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+import withWidth from 'material-ui/utils/withWidth';
+import Typography from 'material-ui/Typography';
+
+const styleSheet = createStyleSheet('MediaQuery', theme => ({
+  root: {
+    padding: theme.spacing.unit,
+  },
+  [theme.breakpoints.up('md')]: {
+    root: {
+      backgroundColor: theme.palette.primary[500],
+    },
+  },
+  [theme.breakpoints.down('md')]: {
+    root: {
+      backgroundColor: theme.palette.accent.A400,
+    },
+  },
+}));
+
+function MediaQuery(props) {
+  const classes = props.classes;
+
+  return (
+    <div className={classes.root}>
+      <Typography type="subheading">
+        Current width: {props.width}
+      </Typography>
+    </div>
+  );
+}
+
+MediaQuery.propTypes = {
+  classes: PropTypes.object.isRequired,
+  width: PropTypes.string.isRequired,
+};
+
+export default compose(withStyles(styleSheet), withWidth())(MediaQuery);
