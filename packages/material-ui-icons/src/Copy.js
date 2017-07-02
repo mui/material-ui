ee8757d7035bb0d99772588e6972f74471272eb3
zazapeta <sami.ghazouane@gmail.com>
2017-07-02 09:53:40 +0200

    Removed Content prefix from ContentPaste ContentCopy ContentCut to increase consitency

--- packages/material-ui-icons/src/Copy.js
+++ packages/material-ui-icons/src/Copy.js
@@ -2,12 +2,12 @@ import React from 'react';
 import pure from 'recompose/pure';
 import SvgIcon from 'material-ui/SvgIcon';
 
-let ContentCopy = props =>
+let Copy = props =>
   <SvgIcon {...props}>
     <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
   </SvgIcon>;
 
-ContentCopy = pure(ContentCopy);
-ContentCopy.muiName = 'SvgIcon';
+Copy = pure(Copy);
+Copy.muiName = 'SvgIcon';
 
-export default ContentCopy;
+export default Copy;
