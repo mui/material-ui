13548d80b9bc74a990262e6d3cd7e66df79950ea
R4DIC4L <teodorescu_oanamaria@yahoo.com>
2017-07-05 00:19:41 +0300

    [DatePicker] Use pickerHeaderColor muiTheme property for DatePicker header customization (#7295)
    Closes #6926

--- src/DatePicker/DateDisplay.js
+++ src/DatePicker/DateDisplay.js
@@ -15,7 +15,7 @@ function getStyles(props, context, state) {
       float: isLandscape ? 'left' : 'none',
       fontWeight: 700,
       display: 'inline-block',
-      backgroundColor: datePicker.selectColor,
+      backgroundColor: datePicker.headerColor,
       borderTopLeftRadius: 2,
       borderTopRightRadius: isLandscape ? 0 : 2,
       borderBottomLeftRadius: isLandscape ? 2 : 0,
