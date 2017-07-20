e481689b117f6edb5090a1dd4cd7bcf2aedb5f55
Gabriel Aumala <gaumala@espol.edu.ec>
2017-07-20 13:25:31 -0500

    [Table] Don't set height to tbody (#7484)
    * [Table] Don't set height to tbody
    
    fixes #5721. Since the height of the table component is already set elsewhere
    it is no longer necessary to set it the tbody tag.
    
    * Update Table.js

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -186,7 +186,6 @@ class Table extends Component {
         onRowHoverExit: this.onRowHoverExit,
         onRowSelection: this.onRowSelection,
         selectable: this.props.selectable,
-        style: Object.assign({height: this.props.height}, base.props.style),
       }
     );
   }
