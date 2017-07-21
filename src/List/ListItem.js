c759b7dc1ddae5e81b4df9190b5dcc01b33a0f5f
jonashartwig <nemuteph@gmx.de>
2017-07-21 13:48:15 +0200

    [ListItem] Fix triggers onTouchTap when disabled (#7486)
    * #7449
    
    * #7449 - should fix it
    
    * #7449 - should fix it
    
    * added trailing ,
    
    * made requested changes
    
    * fixed stupid mistake

--- src/List/ListItem.js
+++ src/List/ListItem.js
@@ -584,6 +584,7 @@ class ListItem extends Component {
       onMouseLeave, // eslint-disable-line no-unused-vars
       onNestedListToggle, // eslint-disable-line no-unused-vars
       onTouchStart, // eslint-disable-line no-unused-vars
+      onTouchTap, // eslint-disable-line no-unused-vars
       rightAvatar,
       rightIcon,
       rightIconButton,
@@ -729,6 +730,7 @@ class ListItem extends Component {
               onTouchStart={this.handleTouchStart}
               onTouchEnd={this.handleTouchEnd}
               onTouchTap={this.handleTouchTap}
+              disabled={disabled}
               ref={(node) => this.button = node}
               style={Object.assign({}, styles.root, style)}
             >
