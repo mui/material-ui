34da0db9c11a7337b916e1025d318ca8aaed1d5c
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-07-31 23:38:35 +0200

    [docs] Show how to use the insertionPoint (#7611)

--- docs/src/components/App.js
+++ docs/src/components/App.js
@@ -3,6 +3,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { JssProvider } from 'react-jss';
+import { create } from 'jss';
+import preset from 'jss-preset-default';
+import createGenerateClassName from 'material-ui/styles/createGenerateClassName';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import createMuiTheme from 'material-ui/styles/theme';
 import createPalette from 'material-ui/styles/palette';
@@ -11,7 +15,7 @@ import pink from 'material-ui/colors/pink';
 import { lightTheme, darkTheme, setPrismTheme } from 'docs/src/utils/prism';
 import AppRouter from 'docs/src/components/AppRouter';
 
-function App(props) {
+function App1(props) {
   const { dark } = props;
 
   const theme = createMuiTheme({
@@ -35,8 +39,23 @@ function App(props) {
   );
 }
 
-App.propTypes = {
+App1.propTypes = {
   dark: PropTypes.bool.isRequired,
 };
 
-export default connect(state => ({ dark: state.dark }))(App);
+const App2 = connect(state => ({ dark: state.dark }))(App1);
+
+// Configure JSS
+const jss = create(preset());
+jss.options.createGenerateClassName = createGenerateClassName;
+jss.options.insertionPoint = 'insertion-point-jss';
+
+function App() {
+  return (
+    <JssProvider jss={jss}>
+      <App2 />
+    </JssProvider>
+  );
+}
+
+export default App;
