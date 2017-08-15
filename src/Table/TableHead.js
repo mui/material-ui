2fd0fe74106b08ebb92ea9b174273b4aa7b54d7f
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-08-15 17:42:51 +0200

    Update TableHead.js

--- src/Table/TableHead.js
+++ src/Table/TableHead.js
@@ -57,14 +57,13 @@ class TableHead extends Component<DefaultProps, AllProps, void> {
   }
 
   render() {
-    const { classes, className: classNameProp, children, component, ...other } = this.props;
+    const { classes, className: classNameProp, children, component: ComponentProp, ...other } = this.props;
     const className = classNames(classes.root, classNameProp);
-    const ComponentHeader = component;
 
     return (
-      <ComponentHeader className={className} {...other}>
+      <ComponentProp className={className} {...other}>
         {children}
-      </ComponentHeader>
+      </ComponentProp>
     );
   }
 }
