accf459aea87b10d6d64d136621d41f3758b1325
Jared Lewis <jared@jaredlewis.net>
2017-09-01 13:41:04 +0900

    [Drawer] remove docked prop and add type prop to TypeScript definition

--- src/Drawer/Drawer.d.ts
+++ src/Drawer/Drawer.d.ts
@@ -6,13 +6,13 @@ import { Theme } from '../styles/createMuiTheme';
 
 export interface DrawerProps extends ModalProps {
   anchor?: 'left' | 'top' | 'right' | 'bottom';
-  docked?: boolean;
   elevation?: number;
   enterTransitionDuration?: number;
   leaveTransitionDuration?: number;
   open?: boolean;
   SlideProps?: SlideProps;
   theme?: Theme;
+  type: 'permanent' | 'persistent' | 'temporary';
 }
 
 export default class Drawer extends StyledComponent<DrawerProps> {}
