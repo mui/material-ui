12914c869f6766fc27957cfe96e60c53cce0f629
Jared Lewis <jared@jaredlewis.net>
2017-09-04 12:31:39 +0900

    [RadioGroup] Fix TypeScript definition for value property

--- src/Radio/RadioGroup.d.ts
+++ src/Radio/RadioGroup.d.ts
@@ -5,7 +5,7 @@ import { FormGroupProps } from '../Form';
 export type RadioGroupProps = {
   name?: string;
   onChange?: (event: React.ChangeEvent<{}>, value: string) => void;
-  selectedValue?: string;
+  value?: string;
 } & Partial<Omit<FormGroupProps, 'onChange'>>;
 
 export default class RadioGroup extends StyledComponent<RadioGroupProps> {}
