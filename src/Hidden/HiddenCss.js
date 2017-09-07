a9f4c32c0266ac233eb484da997f4052ff21d238
Kevin Ross <kevin.ross@alienfast.com>
2017-09-07 18:12:11 -0500

    ok, add back ref?

--- src/Hidden/HiddenCss.js
+++ src/Hidden/HiddenCss.js
@@ -64,7 +64,8 @@ function HiddenCss(props: AllProps) {
   } = props;
 
   warning(
-    Object.keys(other).length === 0,
+    Object.keys(other).length === 0 ||
+      (Object.keys(other).length === 1 && other.hasOwnProperty('ref')),
     `Material-UI: unsupported properties received ${Object.keys(other).join(
       ', ',
     )} by \`<Hidden />\`.`,
