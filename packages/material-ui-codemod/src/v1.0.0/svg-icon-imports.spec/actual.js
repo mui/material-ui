074153a98ee2049370a0070a916c241e92584bd7
Vividh Chandna <vividhchandna@gmail.com>
2017-09-22 21:38:06 +0530

    Add codemod to update svg-icons to material-ui-icons package

---
+++ packages/material-ui-codemod/src/v1.0.0/svg-icon-imports.spec/actual.js
@@ -0,0 +1,4 @@
+import CheckCircle from 'material-ui/svg-icons/action/check-circle';
+import Rotation3D from 'material-ui/svg-icons/action/three-d-rotation';
+import Comment from 'material-ui/svg-icons/communication/comment';
+const TransferWithinAStation = require('material-ui/svg-icons/maps/transfer-within-a-station');
