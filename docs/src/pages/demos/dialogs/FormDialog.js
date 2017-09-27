00098e9c2323f2b9ca0b22f39248a9afac2e4b76
= <chase.willden@gmail.com>
2017-09-26 22:31:15 -0600

    Added forms dialog in the docs/dialog section for clear example on how to use a form inside of a dialog

---
+++ docs/src/pages/demos/dialogs/FormDialog.js
@@ -0,0 +1,56 @@
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+import React from 'react';
+import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog';
+
+export default class AlertDialog extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleRequestClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.handleClickOpen}>Open form dialog</Button>
+        <Dialog open={this.state.open} onRequestClose={this.handleRequestClose}>
+          <DialogTitle>{"Subscribe"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To subscribe to this website, please enter your email address here. We will send
+              updates occationally.
+            </DialogContentText>
+            <TextField
+              id="name"
+              label="Email Address"
+              fullWidth
+              type='email'
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleRequestClose} color="default">
+              Cancel
+            </Button>
+            <Button onClick={this.handleRequestClose} color="primary">
+              Subscribe
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
