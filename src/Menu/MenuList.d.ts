344829f1b44a7192d669ed7a9c880d6d521c8b8c
Tom Crockett <pelotom@gmail.com>
2017-09-28 00:14:10 -0700

    Make MenuList have the class keys of List

--- src/Menu/MenuList.d.ts
+++ src/Menu/MenuList.d.ts
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { StyledComponent } from '..';
 import { ListProps } from '../List';
+import { ListClassKey } from '../List/List'
 
 export type MenuListProps = {
   onKeyDown?: React.ReactEventHandler<React.KeyboardEvent<any>>;
 } & ListProps;
 
-export type MenuListClassKey = never;
+export type MenuListClassKey =
+  | ListClassKey
+  ;
 
 declare const MenuList: StyledComponent<MenuListProps, MenuListClassKey>;
 
