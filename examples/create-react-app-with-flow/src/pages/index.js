84d87389aaca625872a5b51391efcf5d246bd372
Kevin Ross <kevin.ross@alienfast.com>
2017-09-28 17:47:29 -0500

    [flow] Add examples/create-react-app-with-flow
    https://github.com/callemall/material-ui/issues/8073#issuecomment-332980465

---
+++ examples/create-react-app-with-flow/src/pages/index.js
@@ -0,0 +1,77 @@
+// @flow
+
+import React, { Component } from 'react';
+import Button from 'material-ui/Button';
+import Dialog, {
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from 'material-ui/Dialog';
+import Typography from 'material-ui/Typography';
+import withStyles from 'material-ui/styles/withStyles';
+import withRoot from '../components/withRoot';
+
+const styles = {
+  root: {
+    textAlign: 'center',
+    paddingTop: 200,
+  },
+};
+
+type DefaultProps = {
+  classes: Object,
+};
+
+export type Props = {};
+
+type State = {
+  open: boolean,
+};
+
+class Index extends Component<DefaultProps & Props, State> {
+  state = {
+    open: false,
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  handleClick = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  render() {
+    return (
+      <div className={this.props.classes.root}>
+        <Dialog open={this.state.open} onRequestClose={this.handleRequestClose}>
+          <DialogTitle>Super Secret Password</DialogTitle>
+          <DialogContent>
+            <DialogContentText>1-2-3-4-5</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={this.handleRequestClose}>
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Typography type="display1" gutterBottom>
+          Material-UI
+        </Typography>
+        <Typography type="subheading" gutterBottom>
+          example project
+        </Typography>
+        <Button raised color="accent" onClick={this.handleClick}>
+          Super Secret Password
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default withRoot(withStyles(styles)(Index));
