1e253df11a0aacf692a9b4a054d66dc20283081d
Tom Crockett <pelotom@gmail.com>
2017-09-27 22:46:40 -0700

    Specify Paper class keys

--- src/Paper/Paper.d.ts
+++ src/Paper/Paper.d.ts
@@ -7,6 +7,36 @@ export interface PaperProps extends React.HTMLAttributes<HTMLDivElement> {
   square?: boolean;
 }
 
-declare const Paper: StyledComponent<PaperProps>;
+export type PaperClassKey =
+  | 'root'
+  | 'rounded'
+  | 'shadow0'
+  | 'shadow1'
+  | 'shadow2'
+  | 'shadow3'
+  | 'shadow4'
+  | 'shadow5'
+  | 'shadow6'
+  | 'shadow7'
+  | 'shadow8'
+  | 'shadow9'
+  | 'shadow10'
+  | 'shadow11'
+  | 'shadow12'
+  | 'shadow13'
+  | 'shadow14'
+  | 'shadow15'
+  | 'shadow16'
+  | 'shadow17'
+  | 'shadow18'
+  | 'shadow19'
+  | 'shadow20'
+  | 'shadow21'
+  | 'shadow22'
+  | 'shadow23'
+  | 'shadow24'
+  ;
+
+declare const Paper: StyledComponent<PaperProps, PaperClassKey>;
 
 export default Paper;
