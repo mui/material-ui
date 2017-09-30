240c67dd5d2d1c46b86e7ddc0ba40a5dbb1220e1
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-09-30 23:56:24 +0200

    [Slider] Fix IE11 issue (#8486)

--- docs/src/pages/demos/tables/EnhancedTable.js
+++ docs/src/pages/demos/tables/EnhancedTable.js
@@ -20,6 +20,7 @@ import Typography from 'material-ui/Typography';
 import Paper from 'material-ui/Paper';
 import Checkbox from 'material-ui/Checkbox';
 import IconButton from 'material-ui/IconButton';
+import Tooltip from 'material-ui/Tooltip';
 import DeleteIcon from 'material-ui-icons/Delete';
 import FilterListIcon from 'material-ui-icons/FilterList';
 
@@ -131,13 +132,17 @@ let EnhancedTableToolbar = props => {
       <div className={classes.spacer} />
       <div className={classes.actions}>
         {numSelected > 0 ? (
-          <IconButton aria-label="Delete">
-            <DeleteIcon />
-          </IconButton>
+          <Tooltip title="Delete">
+            <IconButton aria-label="Delete">
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
         ) : (
-          <IconButton aria-label="Filter list">
-            <FilterListIcon />
-          </IconButton>
+          <Tooltip title="Filter list">
+            <IconButton aria-label="Filter list">
+              <FilterListIcon />
+            </IconButton>
+          </Tooltip>
         )}
       </div>
     </Toolbar>
