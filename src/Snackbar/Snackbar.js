mui/material-ui

commit ee3a5f4b11044fa9be79ac584ffdecfcec0e714e

[Snackbar] Only use the needed space (#8258)
* Revert "[Snackbar] Fix click-through issue in IE11 (#8096)"

This reverts commit 277f3e6c3f482b1ed9544c7ae2961555e3ebc227.

* [Snackbar] Only use the needed space

src/Snackbar/Snackbar.js
@@ -22,6 +22,11 @@ export const styles = (theme: Object) => {
   const bottomSpace = { bottom: gutter };
   const rightSpace = { right: gutter };
   const leftSpace = { left: gutter };
+  const center = {
+    left: '50%',
+    right: 'auto',
+    transform: 'translateX(-50%)',
+  };
 
   return {
     root: {
@@ -34,32 +39,36 @@ export const styles = (theme: Object) => {
       alignItems: 'center',
     },
     anchorTopCenter: {
-      extend: [top],
+      extend: [top, center],
     },
     anchorBottomCenter: {
-      extend: [bottom],
+      extend: [bottom, center],
     },
     anchorTopRight: {
       extend: [top, right],
       [theme.breakpoints.up('md')]: {
+        left: 'auto',
         extend: [topSpace, rightSpace],
       },
     },
     anchorBottomRight: {
       extend: [bottom, right],
       [theme.breakpoints.up('md')]: {
+        left: 'auto',
         extend: [bottomSpace, rightSpace],
       },
     },
     anchorTopLeft: {
       extend: [top, left],
       [theme.breakpoints.up('md')]: {
+        right: 'auto',
         extend: [topSpace, leftSpace],
       },
     },
     anchorBottomLeft: {
       extend: [bottom, left],
       [theme.breakpoints.up('md')]: {
+        right: 'auto',
         extend: [bottomSpace, leftSpace],
       },
     },

commit 02024afa60fede1b95f3e1b37fe51e3f352e737e

[Snackbar] Fix position regression
The regression was introduced with ee3a5f4b11

src/Snackbar/Snackbar.js
@@ -39,10 +39,16 @@ export const styles = (theme: Object) => {
       alignItems: 'center',
     },
     anchorTopCenter: {
-      extend: [top, center],
+      extend: [top],
+      [theme.breakpoints.up('md')]: {
+        extend: [center],
+      },
     },
     anchorBottomCenter: {
-      extend: [bottom, center],
+      extend: [bottom],
+      [theme.breakpoints.up('md')]: {
+        extend: [center],
+      },
     },
     anchorTopRight: {
       extend: [top, right],