646d25b5af4211985c19713dde37fc67d4c4243a
Daniel Roth <daniel@encodo.ch>
2017-10-10 10:54:43 +0200

    Fix typings for withTheme
    The implementation does not directly take a component as an argument.

--- src/styles/withTheme.d.ts
+++ src/styles/withTheme.d.ts
@@ -1,5 +1,5 @@
 import { Theme } from './createMuiTheme';
 
-export default function withTheme<P = {}, T extends Theme = Theme>(
-  component: React.ComponentType<P & { theme: T }>
-): React.ComponentClass<P>;
+export default function withTheme<P = {}, T extends Theme = Theme>():
+  (component: React.ComponentType<P & { theme: T }>
+  ) => React.ComponentClass<P>;
