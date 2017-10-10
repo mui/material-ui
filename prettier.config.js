5dc1cbca1055c8440ae8c02fffbad7e6a1e2674e
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-10-10 09:57:45 +0200

    [core] Add global prettier config (#8624)

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+// @flow
+
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  parser: 'babylon',
+  semi: true,
+};
