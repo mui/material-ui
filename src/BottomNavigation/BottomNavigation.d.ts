57394427316b25f6c85c420ba399dd08683ad036
Tom Crockett <pelotom@gmail.com>
2017-10-11 09:56:15 -0700

    Merge named imports

--- src/BottomNavigation/BottomNavigation.d.ts
+++ src/BottomNavigation/BottomNavigation.d.ts
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import { StyledComponent } from '..';
-import { Omit } from '../index';
+import { StyledComponent, Omit } from '..';
 
 export type BottomNavigationProps = {
   children: React.ReactNode;
