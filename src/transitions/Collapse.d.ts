68ee55604a1aa6a062e367c111aecd2c26ee3b83
Daniel Roth <daniel@encodo.ch>
2017-10-11 15:38:51 +0200

    Add partial

--- src/transitions/Collapse.d.ts
+++ src/transitions/Collapse.d.ts
@@ -3,7 +3,7 @@ import { StyledComponent, Omit } from '..';
 import { Theme } from '../styles/createMuiTheme';
 import { TransitionDuration, TransitionProps } from '../internal/Transition';
 
-export interface CollapseProps extends Omit<TransitionProps, 'children'> {
+export interface CollapseProps extends Partial<Omit<TransitionProps, 'children'>> {
   children?: React.ReactNode;
   theme?: Theme;
   transitionDuration?: TransitionDuration | 'auto';
