7cbf64c3bf2e058af51e7cc73ef01aa1dd937bc2
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-10-13 20:33:55 +0200

    [Button] Better render multilines button

---
+++ test/regressions/tests/Button/MultilineButton.js
@@ -0,0 +1,16 @@
+// @flow
+
+import React from 'react';
+import Button from 'material-ui/Button';
+
+export default function MultilineButton() {
+  return (
+    <Button raised style={{ width: 400 }}>
+      {[
+        'Raised buttons are rectangular-shaped buttons.',
+        'They may be used inline.',
+        'They lift and display ink reactions on press.',
+      ].join(' ')}
+    </Button>
+  );
+}
