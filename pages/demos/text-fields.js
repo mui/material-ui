de27edeba2e0f94fcc9405eab1de47b334799324
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-10-27 22:28:07 +0200

    [docs] Show how to change the color of the TextField

--- pages/demos/text-fields.js
+++ pages/demos/text-fields.js
@@ -52,6 +52,13 @@ module.exports = require('fs')
   .readFileSync(require.resolve('docs/src/pages/demos/text-fields/FormattedInputs'), 'utf8')
 `,
         },
+        'pages/demos/text-fields/CustomizedInputs.js': {
+          js: require('docs/src/pages/demos/text-fields/CustomizedInputs').default,
+          raw: preval`
+module.exports = require('fs')
+  .readFileSync(require.resolve('docs/src/pages/demos/text-fields/CustomizedInputs'), 'utf8')
+`,
+        },
       }}
     />
   );
