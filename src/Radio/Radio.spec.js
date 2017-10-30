f26d8a2922cad94bf33737574d8b23bceff561b1
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-10-30 13:16:45 +0100

    [test] Increase test coverage (#8908)

--- src/Radio/Radio.spec.js
+++ src/Radio/Radio.spec.js
@@ -2,14 +2,21 @@
 
 import React from 'react';
 import { assert } from 'chai';
-import { getClasses } from '../test-utils';
+import { getClasses, createMount } from '../test-utils';
 import Radio from './Radio';
+import RadioButtonCheckedIcon from '../svg-icons/RadioButtonChecked';
 
 describe('<Radio />', () => {
   let classes;
+  let mount;
 
   before(() => {
     classes = getClasses(<Radio />);
+    mount = createMount();
+  });
+
+  after(() => {
+    mount.cleanUp();
   });
 
   describe('styleSheet', () => {
@@ -23,6 +30,13 @@ describe('<Radio />', () => {
   describe('default Radio export', () => {
     it('should be a SwitchBase with the displayName set for debugging', () => {
       assert.strictEqual(Radio.displayName, 'Radio');
+    });
+  });
+
+  describe('prop: checked', () => {
+    it('should render a checked icon', () => {
+      const wrapper = mount(<Radio checked />);
+      assert.strictEqual(wrapper.find(RadioButtonCheckedIcon).length, 1);
     });
   });
 });
