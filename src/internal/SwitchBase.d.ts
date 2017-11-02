c49ebe946410bf2e895b3e28498210811bb40f7e
Tom Crockett <pelotom@gmail.com>
2017-11-01 23:24:38 -0700

    Fix import

--- src/internal/SwitchBase.d.ts
+++ src/internal/SwitchBase.d.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StandardProps } from '..';
-import { IconButtonProps } from '../../build/IconButton/IconButton'
+import { IconButtonProps } from '../IconButton';
 
 export interface SwitchBaseProps extends StandardProps<
   IconButtonProps,
