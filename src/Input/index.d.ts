a3c678e829e0b3f5e7615c855e484596498511d3
Dmitrijs Minajevs <d3x133@gmail.com>
2017-11-02 14:04:46 +0200

    [Typings] Correct export for InputAdornment
    Input/index should export new component "InputAdornment".

--- src/Input/index.d.ts
+++ src/Input/index.d.ts
@@ -2,4 +2,6 @@ export { default } from './Input';
 export * from './Input';
 export { default as InputLabel } from './InputLabel';
 export * from './InputLabel';
+export { default as InputAdornment } from './InputAdornment';
+export * from './InputAdornment';
 // NOTE: Textarea is missing from exports (intentional?)
