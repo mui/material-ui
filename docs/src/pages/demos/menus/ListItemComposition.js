08fae8c2fcd741c01cabbee38305d5ef109acfd5
Luciano <lucianosemerini@gmail.com>
2017-11-14 17:42:59 -0500

    [docs] Improved documentation for Menu style overrides (#9126)

---
+++ docs/src/pages/demos/menus/ListItemComposition.js
@@ -0,0 +1,59 @@
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { MenuList, MenuItem } from 'material-ui/Menu';
+import Paper from 'material-ui/Paper';
+import { withStyles } from 'material-ui/styles';
+import { ListItemIcon, ListItemText } from 'material-ui/List';
+import InboxIcon from 'material-ui-icons/MoveToInbox';
+import DraftsIcon from 'material-ui-icons/Drafts';
+import SendIcon from 'material-ui-icons/Send';
+
+const styles = theme => ({
+  menuItem: {
+    '&:focus': {
+      background: theme.palette.primary[500],
+      '& $text, & $icon': {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+  text: {},
+  icon: {},
+});
+
+function ListItemComposition(props) {
+  const { classes } = props;
+
+  return (
+    <Paper>
+      <MenuList>
+        <MenuItem className={classes.menuItem}>
+          <ListItemIcon className={classes.icon}>
+            <SendIcon />
+          </ListItemIcon>
+          <ListItemText classes={{ text: classes.text }} inset primary="Sent mail" />
+        </MenuItem>
+        <MenuItem className={classes.menuItem}>
+          <ListItemIcon className={classes.icon}>
+            <DraftsIcon />
+          </ListItemIcon>
+          <ListItemText classes={{ text: classes.text }} inset primary="Drafts" />
+        </MenuItem>
+        <MenuItem className={classes.menuItem}>
+          <ListItemIcon className={classes.icon}>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText classes={{ text: classes.text }} inset primary="Inbox" />
+        </MenuItem>
+      </MenuList>
+    </Paper>
+  );
+}
+
+ListItemComposition.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ListItemComposition);
