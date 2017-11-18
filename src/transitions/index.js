4ae38a44c7780ad040ce5e955a5f17d44deebe2f
Ajaykumar <pajay2507@gmail.com>
2017-11-18 05:05:19 -0500

    [transitions] Expose the transition components (#9210)

---
+++ src/transitions/index.js
@@ -0,0 +1,6 @@
+// @flow
+
+export { default as Slide } from './Slide';
+export { default as Grow } from './Grow';
+export { default as Fade } from './Fade';
+export { default as Collapse } from './Collapse';
