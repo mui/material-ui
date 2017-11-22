4b6fe4615df793b86fd893220c2ac566bb9af3b8
Niklas Wagner <Skaro@Skaronator.com>
2017-11-22 04:23:05 +0100

    [Table] Add responsive table

---
+++ docs/src/pages/demos/tables/ResponsiveTable.js
@@ -0,0 +1,105 @@
+// @flow weak
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Paper from 'material-ui/Paper';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+  },
+  table: {
+    minWidth: 700,
+  },
+  cell: {
+    whiteSpace: 'nowrap',
+  },
+});
+
+let id = 0;
+function createData(name, calories, fat, carbs, protein) {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein };
+}
+
+const data = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+function ResponsiveTable(props) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table} responsive>
+        <TableHead>
+          <TableRow>
+            <TableCell className={classes.cell}>Dessert (100g serving)</TableCell>
+            <TableCell className={classes.cell} numeric>
+              Calories
+            </TableCell>
+            <TableCell className={classes.cell} numeric>
+              Fat (g)
+            </TableCell>
+            <TableCell className={classes.cell} numeric>
+              Carbs (g)
+            </TableCell>
+            <TableCell className={classes.cell} numeric>
+              Protein (g)
+            </TableCell>
+            <TableCell className={classes.cell} numeric>
+              Fat (g)
+            </TableCell>
+            <TableCell className={classes.cell} numeric>
+              Carbs (g)
+            </TableCell>
+            <TableCell className={classes.cell} numeric>
+              Protein (g)
+            </TableCell>
+            <TableCell className={classes.cell} numeric>
+              Fat (g)
+            </TableCell>
+            <TableCell className={classes.cell} numeric>
+              Carbs (g)
+            </TableCell>
+            <TableCell className={classes.cell} numeric>
+              Protein (g)
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(n => {
+            return (
+              <TableRow key={n.id}>
+                <TableCell>{n.name}</TableCell>
+                <TableCell numeric>{n.calories}</TableCell>
+                <TableCell numeric>{n.fat}</TableCell>
+                <TableCell numeric>{n.carbs}</TableCell>
+                <TableCell numeric>{n.protein}</TableCell>
+                <TableCell numeric>{n.fat}</TableCell>
+                <TableCell numeric>{n.carbs}</TableCell>
+                <TableCell numeric>{n.protein}</TableCell>
+                <TableCell numeric>{n.fat}</TableCell>
+                <TableCell numeric>{n.carbs}</TableCell>
+                <TableCell numeric>{n.protein}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+ResponsiveTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ResponsiveTable);
