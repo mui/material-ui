6159f5c64337822e2eaa4ba6adfdc5644331325f
Tom Crockett <pelotom@gmail.com>
2017-11-25 02:33:01 -0700

    Fix some types that should be using React.CSSProperties (#9294)
    * Remove unnecessary use of Partial
    
    * Use CSSProperties instead of Object in Mixins

--- src/styles/withStyles.d.ts
+++ src/styles/withStyles.d.ts
@@ -8,7 +8,7 @@ import { Theme } from './createMuiTheme';
    * - the `keys` are the class (names) that will be created
    * - the `values` are objects that represent CSS rules (`React.CSSProperties`).
    */
-export type StyleRules<ClassKey extends string = string> = Record<ClassKey, Partial<React.CSSProperties>>;
+export type StyleRules<ClassKey extends string = string> = Record<ClassKey, React.CSSProperties>;
 
 export type StyleRulesCallback<ClassKey extends string = string> = (theme: Theme) => StyleRules<ClassKey>;
 
