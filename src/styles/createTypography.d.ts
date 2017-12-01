ba014e4314f5dece18f7901fac6d55c46bac2817
Andrei Goncharov <frantic1993@gmail.com>
2017-12-01 16:29:08 -0700

    DeepPartial fix

--- src/styles/createTypography.d.ts
+++ src/styles/createTypography.d.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Palette } from './createPalette';
-import { DeepPartial } from './index'
+import { DeepPartial } from 'index'
 
 export type TextStyle =
   | 'display1'
