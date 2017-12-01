9cf66fe18b2d1f5b217e9f3674ea14da80b03599
Andrei Goncharov <frantic1993@gmail.com>
2017-12-01 16:23:40 -0700

    Added DeepPartial to index.d.ts

--- src/styles/index.d.ts
+++ src/styles/index.d.ts
@@ -2,3 +2,7 @@ export { default as MuiThemeProvider } from './MuiThemeProvider';
 export { default as withStyles, WithStyles, StyleRules, StyleRulesCallback, StyledComponentProps } from './withStyles';
 export { default as withTheme } from './withTheme';
 export { default as createMuiTheme, Theme, Direction } from './createMuiTheme';
+
+export type DeepPartial<T> = {
+  [P in keyof T]?: DeepPartial<T[P]>;
+}
