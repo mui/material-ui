d36891e2acaebd6e438656f8fe00c89516798cd9
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-12-02 19:07:23 +0100

    [theme] Fix mixins.gutter override

--- src/styles/createMixins.js
+++ src/styles/createMixins.js
@@ -3,13 +3,16 @@
 export default function createMixins(breakpoints: Object, spacing: Object, mixins: Object) {
   return {
     gutters: (styles: Object) => {
-      styles.paddingLeft = spacing.unit * 2;
-      styles.paddingRight = spacing.unit * 2;
-      styles[breakpoints.up('sm')] = {
-        paddingLeft: spacing.unit * 3,
-        paddingRight: spacing.unit * 3,
+      return {
+        paddingLeft: spacing.unit * 2,
+        paddingRight: spacing.unit * 2,
+        ...styles,
+        [breakpoints.up('sm')]: {
+          paddingLeft: spacing.unit * 3,
+          paddingRight: spacing.unit * 3,
+          ...styles[breakpoints.up('sm')],
+        },
       };
-      return styles;
     },
     toolbar: {
       minHeight: 56,
