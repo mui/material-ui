a4e922cda96bccca258fee3045e0ef93403f5ef3
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-12-06 21:12:38 +0100

    [docs] Add codesandbox edit button (#9416)

--- docs/src/modules/components/Demo.js
+++ docs/src/modules/components/Demo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LZString from 'lz-string';
 import { withStyles } from 'material-ui/styles';
 import IconButton from 'material-ui/IconButton';
 import Collapse from 'material-ui/transitions/Collapse';
+import ModeEditIcon from 'material-ui-icons/ModeEdit';
 import CodeIcon from 'material-ui-icons/Code';
-import MarkdownElement from 'docs/src/modules/components/MarkdownElement';
 import Tooltip from 'material-ui/Tooltip';
+import MarkdownElement from 'docs/src/modules/components/MarkdownElement';
 
 const styles = theme => ({
   root: {
@@ -46,14 +48,18 @@ const styles = theme => ({
     display: 'none',
     padding: 0,
     margin: 0,
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
     '& pre': {
       overflow: 'auto',
       margin: '0px !important',
       borderRadius: '0px !important',
     },
   },
-  [theme.breakpoints.up(600)]: {
-    codeButton: {
+  codesandbox: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
       display: 'block',
       flip: false,
       zIndex: 10,
@@ -64,15 +70,68 @@ const styles = theme => ({
   },
 });
 
-class Demo extends React.Component<any, any> {
+function compress(object) {
+  return LZString.compressToBase64(JSON.stringify(object))
+    .replace(/\+/g, '-') // Convert '+' to '-'
+    .replace(/\//g, '_') // Convert '/' to '_'
+    .replace(/=+$/, ''); // Remove ending '='
+}
+
+class Demo extends React.Component {
   state = {
     codeOpen: false,
   };
 
-  handleCodeButtonClick = () => {
+  codesandboxForm = null;
+
+  handleClickCodeOpen = () => {
     this.setState({
       codeOpen: !this.state.codeOpen,
     });
+  };
+
+  handleClickCodesandbox = () => {
+    const codesandboxValue = {
+      files: {
+        'package.json': {
+          content: {
+            dependencies: {
+              react: 'latest',
+              'react-dom': 'latest',
+              'material-ui': 'next',
+              'material-ui-icons': 'latest',
+            },
+          },
+        },
+        'demo.js': {
+          content: this.props.raw,
+        },
+        'index.js': {
+          content: `
+import React from 'react';
+import { render } from 'react-dom';
+import Demo from './demo';
+
+const rootElement = document.querySelector('#root');
+if (rootElement) {
+  render(<Demo />, rootElement);
+}
+          `,
+        },
+        'index.html': {
+          content: `
+<body>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <div id="root"></div>
+</body>
+            `,
+        },
+      },
+    };
+
+    this.codesandboxForm.querySelector('[name="parameters"]').value = compress(codesandboxValue);
+    this.codesandboxForm.submit();
   };
 
   render() {
@@ -81,8 +140,25 @@ class Demo extends React.Component<any, any> {
 
     return (
       <div className={classes.root}>
+        <Tooltip title="Edit in codesandbox" placement="top">
+          <div className={classes.codesandbox}>
+            <IconButton onClick={this.handleClickCodesandbox}>
+              <ModeEditIcon />
+            </IconButton>
+            <form
+              ref={node => {
+                this.codesandboxForm = node;
+              }}
+              method="get"
+              action="https://codesandbox.io/api/v1/sandboxes/define"
+              target="_blank"
+            >
+              <input type="hidden" name="parameters" value="" />
+            </form>
+          </div>
+        </Tooltip>
         <Tooltip title={codeOpen ? 'Hide the source' : 'Show the source'} placement="top">
-          <IconButton onClick={this.handleCodeButtonClick} className={classes.codeButton}>
+          <IconButton onClick={this.handleClickCodeOpen} className={classes.codeButton}>
             <CodeIcon />
           </IconButton>
         </Tooltip>
