108566fdf2888e80420363fdeff6ba2e01458464
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-12-06 20:18:49 +0100

    [docs] Better Next.js experience

--- next.config.js
+++ next.config.js
@@ -72,4 +72,10 @@ module.exports = {
 
     return map;
   },
+  onDemandEntries: {
+    // Period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 120 * 1e3, // default 25s
+    // Number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 3, // default 2
+  },
 };
