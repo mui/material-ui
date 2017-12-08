5038dc4de512e61b3d5bc93e0358768a01239ecb
Michael Angerman <stormasm@gmail.com>
2017-12-08 14:29:12 -0800

    [examples] remove flow from the examples

--- examples/create-react-app/src/index.js
+++ examples/create-react-app/src/index.js
@@ -1,4 +1,3 @@
-// @flow
 
 import React from 'react';
 import { render } from 'react-dom';
--- examples/nextjs/pages/_document.js
+++ examples/nextjs/pages/_document.js
@@ -1,4 +1,3 @@
-// @flow weak
 
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
