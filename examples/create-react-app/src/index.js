50b2fa9d7eaf6603c621afddeb267e085d984e7c
Michael Angerman <stormasm@gmail.com>
2017-12-08 14:50:13 -0800

    [examples] remove flow from examples part 2

--- examples/create-react-app/src/index.js
+++ examples/create-react-app/src/index.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { render } from 'react-dom';
 import Index from './pages/index';
--- examples/nextjs/pages/_document.js
+++ examples/nextjs/pages/_document.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import JssProvider from 'react-jss/lib/JssProvider';
