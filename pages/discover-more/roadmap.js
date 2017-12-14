0e58a7a92ccf5cfb30db6afd12977947b3acd681
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-12-14 21:30:52 +0100

    [docs] Add see source button

--- pages/discover-more/roadmap.js
+++ pages/discover-more/roadmap.js
@@ -4,7 +4,7 @@ import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
 import markdown from '../../ROADMAP.md';
 
 function Page() {
-  return <MarkdownDocs markdown={markdown} sourceLocation="/ROADMAP.md" />;
+  return <MarkdownDocs markdown={markdown} markdownLocation="/ROADMAP.md" />;
 }
 
 export default withRoot(Page);
