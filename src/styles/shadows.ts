d34a7b2c9bfef21e8c52003caa20f66f71ef6353
Kevin Ross <kevin.ross@alienfast.com>
2017-12-16 11:55:05 -0600

    ts shadows

--- src/styles/shadows.ts
+++ src/styles/shadows.ts
@@ -2,7 +2,7 @@ const shadowKeyUmbraOpacity = 0.2;
 const shadowKeyPenumbraOpacity = 0.14;
 const shadowAmbientShadowOpacity = 0.12;
 
-function createShadow(...px) {
+function createShadow(...px: number[]): string {
   return [
     `${px[0]}px ${px[1]}px ${px[2]}px ${px[3]}px rgba(0, 0, 0, ${shadowKeyUmbraOpacity})`,
     `${px[4]}px ${px[5]}px ${px[6]}px ${px[7]}px rgba(0, 0, 0, ${shadowKeyPenumbraOpacity})`,
@@ -39,3 +39,31 @@ const shadows = [
 ];
 
 export default shadows;
+
+// export type Shadows = [
+//   'none',
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string,
+//   string
+//   ];
