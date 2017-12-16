34ea4652b0b840203430160322c4bcdd446f825f
Kevin Ross <kevin.ross@alienfast.com>
2017-12-15 21:33:36 -0600

    fix createShallow typing

--- src/test-utils/createShallow.d.ts
+++ src/test-utils/createShallow.d.ts
@@ -1,12 +1,15 @@
-import { shallow, EnzymeSelector } from 'enzyme';
+import {shallow, EnzymeSelector, MountRendererProps, ReactWrapper} from 'enzyme';
+import {ReactElement} from "react";
 
 export interface ShallowOptions {
   shallow: typeof shallow;
-  otherContext: Object;
+  otherContext: object;
   dive: boolean;
   untilSelector: EnzymeSelector;
 }
 
-export default function createShallow(
+type MuiShallow<P> = (node: ReactElement<P>, options?: MountRendererProps) => ReactWrapper<P, any>
+
+export default function createShallow<P>(
   options?: Partial<ShallowOptions>
-): typeof shallow;
+): MuiShallow<P>;
