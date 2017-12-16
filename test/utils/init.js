cacfb87321c0a50b7db458608651e2a5e29a67ad
Kevin Ross <kevin.ross@alienfast.com>
2017-12-16 09:36:44 -0600

    add back consoleError in test setup

--- test/utils/init.js
+++ test/utils/init.js
@@ -1,7 +1,7 @@
 import * as enzyme from 'enzyme/build/index';
 import * as Adapter from 'enzyme-adapter-react-16';
-// import consoleError from './consoleError';
+import consoleError from './consoleError';
 
-// consoleError();
+consoleError();
 
 enzyme.configure({ adapter: new Adapter() });
