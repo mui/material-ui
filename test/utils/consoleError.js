eaff0cd764eb2e1fe782ddf495db5490cdddf74a
Kevin Ross <kevin.ross@alienfast.com>
2017-12-15 22:17:27 -0600

    pull flow and omit consoleError for now

--- test/utils/consoleError.js
+++ test/utils/consoleError.js
@@ -1,12 +1,10 @@
-/* eslint-disable flowtype/require-valid-file-annotation, no-console */
+/* eslint-disable no-console */
 
 // Makes sure the tests fails when a PropType validation fails.
-function consoleError() {
+export default function consoleError() {
   console.error = (...args) => {
     // Can't use log as karma is not displaying them.
     console.info(...args);
     throw new Error(...args);
   };
 }
-
-module.exports = consoleError;
