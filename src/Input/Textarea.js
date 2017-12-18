1be2aa948c720cc8fd006425bc9ffc304836e33a
Kevin Ross <kevin.ross@alienfast.com>
2017-12-17 22:01:28 -0600

    down to 6

--- src/Input/Textarea.js
+++ src/Input/Textarea.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import classnames from 'classnames';
+import * as classnames from 'classnames';
 import EventListener from 'react-event-listener';
 import withStyles from '../styles/withStyles';
 import debounce from '../utils/ts-import-workaround/debounce';
