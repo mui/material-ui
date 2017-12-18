020586c3c527504cefce41e2e60a472ad76f4456
Kevin Ross <kevin.ross@alienfast.com>
2017-12-17 22:06:17 -0600

    5 left

--- src/svg-icons/index.spec.js
+++ src/svg-icons/index.spec.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 import * as React from 'react';
 import { assert } from 'chai';
 import { createShallow } from '../test-utils';
