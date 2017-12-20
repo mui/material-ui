ee954ac21e6ea9c5515cc212373d50e930bf64c7
SSW-SCIENTIFIC <S-sword@s-sword.net>
2017-12-20 16:21:27 +0900

    [TypeScript] add typings to reactHelpers
    Utility functions isMuiElement and isMuiComponent had no TypeScript declaration, so I have added the simplest one.

--- src/utils/reactHelpers.d.ts
+++ src/utils/reactHelpers.d.ts
@@ -1,4 +1,9 @@
+import * as React from 'react';
+
 export function cloneChildrenWithClassName<T>(
   children: React.ReactNode,
   className: string
 ): T[];
+
+export function isMuiElement(element: any, muiNames: Array<string>): boolean;
+export function isMuiComponent(element: any, muiNames: Array<string>): boolean;
