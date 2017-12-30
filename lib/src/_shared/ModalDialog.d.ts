a38388cc2032275ea3fd20fd4c7ccbfcda2daadd
Andrew <andrew.cherniavskii@gmail.com>
2017-12-30 14:05:02 +0100

    update ModalDialog TS definitions

--- lib/src/_shared/ModalDialog.d.ts
+++ lib/src/_shared/ModalDialog.d.ts
@@ -7,10 +7,12 @@ import { ButtonProps } from 'material-ui/Button';
 export interface DateTextFieldProps extends DialogProps {
   onAccept: ButtonProps['onClick'];
   onDismiss: ButtonProps['onClick'];
+  onClear: ButtonProps['onClick'];
   dialogContentClassName?: string;
   invalidLabel?: string;
   okLabel?: ReactNode;
   cancelLabel?: ReactNode;
+  clearLabel?: ReactNode;
 }
 
 declare const DateTextField: ComponentClass<DateTextFieldProps>;
