e317a186ad8b2ada2de1a793af545f56cde1d348
Andrew <andrew.cherniavskii@gmail.com>
2018-01-01 13:29:29 +0100

    Update index.d.ts

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -1,5 +1,5 @@
 export { Utils } from './utils/utils';
 
-export { default as DatePicker } from './DatePicker/DatePickerWrapper.jsx';
-export { default as TimePicker } from './TimePicker/TimePickerWrapper.jsx';
-export { default as DateTimePicker } from './DateTimePicker/DateTimePickerWrapper.jsx';
+export { default as DatePicker } from './DatePicker/DatePickerWrapper';
+export { default as TimePicker } from './TimePicker/TimePickerWrapper';
+export { default as DateTimePicker } from './DateTimePicker/DateTimePickerWrapper';
