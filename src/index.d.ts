59d6e15af9fb272469ad10893e2425010f54cac1
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-01-04 23:27:14 +0100

    Make jssPreset public

--- src/index.d.ts
+++ src/index.d.ts
@@ -105,7 +105,7 @@ export { default as Reboot } from './Reboot';
 export { default as Select } from './Select';
 export { default as Snackbar, SnackbarContent } from './Snackbar';
 export { default as Stepper, Step, StepButton, StepContent, StepLabel } from './Stepper';
-export { MuiThemeProvider, withStyles, WithStyles, withTheme, createMuiTheme } from './styles';
+export { MuiThemeProvider, withStyles, WithStyles, withTheme, createMuiTheme, jssPreset } from './styles';
 
 import * as colors from './colors';
 
--- src/styles/index.d.ts
+++ src/styles/index.d.ts
@@ -1,5 +1,6 @@
 export { default as createGenerateClassName } from './createGenerateClassName';
 export { default as createMuiTheme, Theme, Direction } from './createMuiTheme';
+export { default as jssPreset } from './jssPreset';
 export { default as MuiThemeProvider } from './MuiThemeProvider';
 export {
   default as withStyles,
