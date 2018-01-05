mui/material-ui

commit 265463e5d9e20f2ad3c5a19bc4ebc798a78c8efe

[SvgIcon] Add color property (#9367)

src/SvgIcon/SvgIcon.d.ts
@@ -1,16 +1,23 @@
 import * as React from 'react';
-import { StandardProps } from '..';
+import { StandardProps, PropTypes } from '..';
 
 export interface SvgIconProps extends StandardProps<
   React.SVGProps<SVGSVGElement>,
   SvgIconClassKey
-> {
+  > {
+  color?: PropTypes.Color | 'action' | 'contrast' | 'disabled' | 'error';
   titleAccess?: string;
   viewBox?: string;
 }
 
 export type SvgIconClassKey =
   | 'root'
+  | 'colorAccent'
+  | 'colorAction'
+  | 'colorContrast'
+  | 'colorDisabled'
+  | 'colorError'
+  | 'colorPrimary'
   ;
 
 declare const SvgIcon: React.ComponentType<SvgIconProps>;

src/SvgIcon/SvgIcon.js
@@ -4,6 +4,7 @@ import React from 'react';
 import type { Node } from 'react';
 import classNames from 'classnames';
 import withStyles from '../styles/withStyles';
+import { capitalizeFirstLetter } from '../utils/helpers';
 
 export const styles = (theme: Object) => ({
   root: {
@@ -17,7 +18,27 @@ export const styles = (theme: Object) => ({
       duration: theme.transitions.duration.shorter,
     }),
   },
+  colorAccent: {
+    color: theme.palette.secondary.A200,
+  },
+  colorAction: {
+    color: theme.palette.action.active,
+  },
+  colorContrast: {
+    color: theme.palette.getContrastText(theme.palette.primary[500]),
+  },
+  colorDisabled: {
+    color: theme.palette.action.disabled,
+  },
+  colorError: {
+    color: theme.palette.error[500],
+  },
+  colorPrimary: {
+    color: theme.palette.primary[500],
+  },
 });
+
+export type Color = 'inherit' | 'accent' | 'action' | 'contrast' | 'disabled' | 'error' | 'primary';
 
 type ProvidedProps = {
   classes: Object,
@@ -45,6 +66,10 @@ export type Props = {
    */
   className?: string,
   /**
+   * The color of the component. It's using the theme palette when that makes sense.
+   */
+  color: Color,
+  /**
    * Provides a human-readable title for the element that contains it.
    * https://www.w3.org/TR/SVG-access/#Equivalent
    */
@@ -62,16 +87,33 @@ export type Props = {
 class SvgIcon extends React.Component<ProvidedProps & Props> {
   static defaultProps = {
     viewBox: '0 0 24 24',
+    color: 'inherit',
   };
 
   static muiName = 'SvgIcon';
 
   render() {
-    const { children, classes, className, titleAccess, viewBox, ...other } = this.props;
+    const {
+      children,
+      classes,
+      className: classNameProp,
+      color,
+      titleAccess,
+      viewBox,
+      ...other
+    } = this.props;
+
+    const className = classNames(
+      classes.root,
+      {
+        [classes[`color${capitalizeFirstLetter(color)}`]]: color !== 'inherit',
+      },
+      classNameProp,
+    );
 
     return (
       <svg
-        className={classNames(classes.root, className)}
+        className={className}
         focusable="false"
         viewBox={viewBox}
         aria-hidden={titleAccess ? 'false' : 'true'}

docs/src/pages/style/SvgIcons.js
@@ -1,28 +1,24 @@
 // @flow weak
 
 import React from 'react';
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
-import blue from 'material-ui/colors/blue';
 import green from 'material-ui/colors/green';
-import red from 'material-ui/colors/red';
 import SvgIcon from 'material-ui/SvgIcon';
 
-const styles = theme => ({
-  icon: {
-    margin: theme.spacing.unit,
-  },
-  iconBlue: {
-    fill: blue[500],
+const styles = {
+  root: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'space-around',
+    width: '70%',
   },
   iconHover: {
-    fill: red[500],
     '&:hover': {
       fill: green[200],
     },
   },
-});
+};
 
 const HomeIcon = props => (
   <SvgIcon {...props}>
@@ -33,17 +29,23 @@ const HomeIcon = props => (
 function SvgIcons(props) {
   const { classes } = props;
   return (
-    <div>
-      <HomeIcon className={classes.icon} />
+    <div className={classes.root}>
+      <HomeIcon />
+      <HomeIcon color="accent" />
+      <HomeIcon color="action" />
+      <HomeIcon color="contrast" />
+      <HomeIcon color="disabled" />
       <HomeIcon
-        className={classNames(classes.icon, classes.iconBlue)}
+        className={classes.icon}
+        color="primary"
         style={{
           width: 30,
           height: 30,
         }}
       />
       <HomeIcon
-        className={classNames(classes.icon, classes.iconHover)}
+        color="error"
+        className={classes.iconHover}
         style={{
           width: 36,
           height: 36,

src/SvgIcon/SvgIcon.spec.js
@@ -37,12 +37,6 @@ describe('<SvgIcon />', () => {
     assert.strictEqual(wrapper.props().viewBox, '0 0 32 32', 'should override the viewBox');
   });
 
-  it('should render with the user and SvgIcon classes', () => {
-    const wrapper = shallow(<SvgIcon className="meow">{path}</SvgIcon>);
-    assert.strictEqual(wrapper.hasClass('meow'), true, 'should have the "meow" class');
-    assert.strictEqual(wrapper.hasClass(classes.root), true, 'should have the SvgIcon class');
-  });
-
   describe('prop: titleAccess', () => {
     it('should be able to make an icon accessible', () => {
       const wrapper = shallow(
@@ -52,6 +46,59 @@ describe('<SvgIcon />', () => {
       );
       assert.strictEqual(wrapper.find('title').text(), 'Network');
       assert.strictEqual(wrapper.props()['aria-hidden'], 'false');
+    });
+  });
+
+  describe('prop: color', () => {
+    it('should render with the user and SvgIcon classes', () => {
+      const wrapper = shallow(<SvgIcon className="meow">{path}</SvgIcon>);
+      assert.strictEqual(wrapper.hasClass('meow'), true, 'should have the "meow" class');
+      assert.strictEqual(wrapper.hasClass(classes.root), true, 'should have the SvgIcon class');
+    });
+
+    it('should render with the accent color', () => {
+      const wrapper = shallow(<SvgIcon color="accent">{path}</SvgIcon>);
+      assert.strictEqual(
+        wrapper.hasClass(classes.colorAccent),
+        true,
+        'should have the "accent" color',
+      );
+    });
+
+    it('should render with the action color', () => {
+      const wrapper = shallow(<SvgIcon color="action">{path}</SvgIcon>);
+      assert.strictEqual(
+        wrapper.hasClass(classes.colorAction),
+        true,
+        'should have the "action" color',
+      );
+    });
+
+    it('should render with the contrast color', () => {
+      const wrapper = shallow(<SvgIcon color="contrast">{path}</SvgIcon>);
+      assert.strictEqual(
+        wrapper.hasClass(classes.colorContrast),
+        true,
+        'should have the "contrast" color',
+      );
+    });
+
+    it('should render with the error color', () => {
+      const wrapper = shallow(<SvgIcon color="error">{path}</SvgIcon>);
+      assert.strictEqual(
+        wrapper.hasClass(classes.colorError),
+        true,
+        'should have the "error" color',
+      );
+    });
+
+    it('should render with the primary class', () => {
+      const wrapper = shallow(<SvgIcon color="primary">{path}</SvgIcon>);
+      assert.strictEqual(
+        wrapper.hasClass(classes.colorPrimary),
+        true,
+        'should have the "primary" color',
+      );
     });
   });
 });

commit 031fab9c836e80db87eacb85cf805c3ee045a3ca

[SvgIcon] Add a nativeColor property
A breaking change was introduced in #9367 with no workaround.
You can no longer apply a color property to the native SVG element.
This pull request objective is to provide a simple upgrade path.

src/SvgIcon/SvgIcon.d.ts
@@ -4,6 +4,7 @@ import { StandardProps, PropTypes } from '..';
 export interface SvgIconProps
   extends StandardProps<React.SVGProps<SVGSVGElement>, SvgIconClassKey> {
   color?: PropTypes.Color | 'action' | 'contrast' | 'disabled' | 'error';
+  nativeColor?: string;
   titleAccess?: string;
   viewBox?: string;
 }

src/SvgIcon/SvgIcon.js
@@ -42,6 +42,7 @@ function SvgIcon(props) {
     classes,
     className: classNameProp,
     color,
+    nativeColor,
     titleAccess,
     viewBox,
     ...other
@@ -60,6 +61,7 @@ function SvgIcon(props) {
       className={className}
       focusable="false"
       viewBox={viewBox}
+      color={nativeColor}
       aria-hidden={titleAccess ? 'false' : 'true'}
       {...other}
     >
@@ -71,7 +73,7 @@ function SvgIcon(props) {
 
 SvgIcon.propTypes = {
   /**
-   * Node passed into the SVG Icon.
+   * Node passed into the SVG element.
    */
   children: PropTypes.node.isRequired,
   /**
@@ -84,6 +86,7 @@ SvgIcon.propTypes = {
   className: PropTypes.string,
   /**
    * The color of the component. It's using the theme palette when that makes sense.
+   * You can use the `nativeColor` property to apply a color attribute to the SVG element.
    */
   color: PropTypes.oneOf([
     'inherit',
@@ -95,15 +98,19 @@ SvgIcon.propTypes = {
     'primary',
   ]),
   /**
+   * Applies a color attribute to the SVG element.
+   */
+  nativeColor: PropTypes.string,
+  /**
    * Provides a human-readable title for the element that contains it.
    * https://www.w3.org/TR/SVG-access/#Equivalent
    */
   titleAccess: PropTypes.string,
   /**
-   * Allows you to redefine what the coordinates without units mean inside an svg element.
+   * Allows you to redefine what the coordinates without units mean inside an SVG element.
    * For example, if the SVG element is 500 (width) by 200 (height),
    * and you pass viewBox="0 0 50 20",
-   * this means that the coordinates inside the svg will go from the top left corner (0,0)
+   * this means that the coordinates inside the SVG will go from the top left corner (0,0)
    * to bottom right (50,20) and each unit will be worth 10px.
    */
   viewBox: PropTypes.string,