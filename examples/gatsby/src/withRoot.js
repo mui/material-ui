eded8388b7a8ffdc94d852ee54b7e074d1d8415b
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-01-07 20:25:49 +0100

    [examples] Add a Gatsby example

---
+++ examples/gatsby/src/withRoot.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { MuiThemeProvider } from 'material-ui/styles';
+import Reboot from 'material-ui/Reboot';
+import getPageContext from './getPageContext';
+
+function withRoot(Component) {
+  class WithRoot extends React.Component {
+    componentWillMount() {
+      this.pageContext = this.props.pageContext || getPageContext();
+    }
+
+    componentDidMount() {
+      // Remove the server-side injected CSS.
+      const jssStyles = document.querySelector('#jss-server-side');
+      if (jssStyles && jssStyles.parentNode) {
+        jssStyles.parentNode.removeChild(jssStyles);
+      }
+    }
+
+    pageContext = null;
+
+    render() {
+      // MuiThemeProvider makes the theme available down the React tree thanks to React context.
+      return (
+        <MuiThemeProvider
+          theme={this.pageContext.theme}
+          sheetsManager={this.pageContext.sheetsManager}
+        >
+          {/* Reboot kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <Reboot />
+          <Component {...this.props} />
+        </MuiThemeProvider>
+      );
+    }
+  }
+
+  WithRoot.propTypes = {
+    pageContext: PropTypes.object,
+  };
+
+  return WithRoot;
+}
+
+export default withRoot;
