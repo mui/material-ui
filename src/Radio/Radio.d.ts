f1ae1b5a09935a583fda4d0f0d335a6e1a8a5f79
Kevin Ross <kevin.ross@alienfast.com>
2018-01-09 20:33:07 -0600

    Remove duplicate typescript definitions inherited from SwitchBase

--- src/Radio/Radio.d.ts
+++ src/Radio/Radio.d.ts
@@ -2,22 +2,7 @@ import * as React from 'react';
 import { StandardProps } from '..';
 import { SwitchBaseProps, SwitchBaseClassKey } from '../internal/SwitchBase';
 
-export interface RadioProps extends StandardProps<SwitchBaseProps, RadioClassKey> {
-  checked?: boolean | string;
-  checkedClassName?: string;
-  checkedIcon?: React.ReactNode;
-  defaultChecked?: boolean;
-  disabled?: boolean;
-  disabledClassName?: string;
-  disableRipple?: boolean;
-  icon?: React.ReactNode;
-  inputProps?: React.InputHTMLAttributes<HTMLInputElement>;
-  inputRef?: React.Ref<any>;
-  name?: string;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => void;
-  tabIndex?: number;
-  value?: string;
-}
+export interface RadioProps extends StandardProps<SwitchBaseProps, RadioClassKey> {}
 
 export type RadioClassKey = SwitchBaseClassKey;
 
