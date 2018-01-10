e9e8e5d6045a85374454b380b99b507878f2e05f
Kevin Ross <kevin.ross@alienfast.com>
2018-01-09 21:42:22 -0600

    Remove duplicate typescript definitions inherited from SwitchBase (#9816)

--- src/Switch/Switch.d.ts
+++ src/Switch/Switch.d.ts
@@ -2,21 +2,7 @@ import * as React from 'react';
 import { StandardProps } from '..';
 import { SwitchBaseProps, SwitchBaseClassKey } from '../internal/SwitchBase';
 
-export interface SwitchProps extends StandardProps<SwitchBaseProps, SwitchClassKey> {
-  checked?: boolean | string;
-  checkedClassName?: string;
-  checkedIcon?: React.ReactNode;
-  defaultChecked?: boolean;
-  disabled?: boolean;
-  disabledClassName?: string;
-  disableRipple?: boolean;
-  icon?: React.ReactNode;
-  inputProps?: object;
-  name?: string;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => void;
-  tabIndex?: number;
-  value?: string;
-}
+export interface SwitchProps extends StandardProps<SwitchBaseProps, SwitchClassKey> {}
 
 export type SwitchClassKey = SwitchBaseClassKey | 'bar' | 'icon';
 
