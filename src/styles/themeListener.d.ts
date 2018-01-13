00d69921c2350280fdab373528c076e282540485
Tom Crockett <pelotom@gmail.com>
2018-01-12 22:43:38 -0800

    Fix typing of themeListener

--- src/styles/themeListener.d.ts
+++ src/styles/themeListener.d.ts
@@ -1,15 +1,21 @@
 // This is using the API from https://github.com/vesparny/brcast
-interface MuiContext {
-  getState(): Object;
-  subscribe(callback: Function): Function;
+interface Broadcast<S> {
+  setState(state: S): void
+  getState(): S;
+  subscribe(callback: (state: S) => void): number;
+  unsubscribe(subscriptionId: number): void;
 }
 
-export interface ThemeListener {
-  contextTypes: {
-    'material-ui': object;
-  };
-  initial(context: Object): Object;
-  subscribe(context: Object, callback: Function): Function;
+interface MuiContext<S> {
+  __THEMING__?: Broadcast<S>;
 }
 
-export default ThemeListener;
+export interface ThemeListener<S = {}> {
+  initial(context: MuiContext<S>): S | null;
+  subscribe(context: MuiContext<S>, callback: (state: S) => void): number | null;
+  unsubscribe(context: MuiContext<S>, subscriptionId: number): void;
+}
+
+declare const themeListener: ThemeListener
+
+export default themeListener;
