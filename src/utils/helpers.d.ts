45014522aac82f383ed7f714f980c7ea206f68f6
Tom Crockett <pelotom@gmail.com>
2018-01-12 23:07:25 -0800

    Refine createChainedFunction function argument type

--- src/utils/helpers.d.ts
+++ src/utils/helpers.d.ts
@@ -2,4 +2,6 @@ export function capitalizeFirstLetter(str: string): string;
 export function contains(obj: Object, pred: Object): boolean;
 export function findIndex(arr: any[], pred: any): number;
 export function find<T>(arr: T[], pred: any): T;
-export function createChainedFunction(...funcs: Function[]): (...args: any[]) => never;
+export function createChainedFunction(...funcs: ChainedFunction[]): (...args: any[]) => never;
+
+type ChainedFunction = ((...args: any[]) => void) | undefined | null
