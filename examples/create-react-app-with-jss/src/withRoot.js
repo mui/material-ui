f785aad755912e87469671a79548b35be6ac2d32
Matthew Brookes <github@nospam.33m.co>
2018-01-14 21:44:36 +0000

    [examples] Update for revised theme approach

--- examples/create-react-app-with-jss/src/withRoot.js
+++ examples/create-react-app-with-jss/src/withRoot.js
@@ -14,9 +14,15 @@ import Reboot from 'material-ui/Reboot';
 // A theme with custom primary and secondary color.
 // It's optional.
 const theme = createMuiTheme({
-  palette: {
-    primary: purple,
-    secondary: green,
+  primary: {
+    light: purple[300],
+    main: purple[500],
+    dark: purple[700],
+  },
+  secondary: {
+    light: green[300],
+    main: green[500],
+    dark: green[700],
   },
 });
 
