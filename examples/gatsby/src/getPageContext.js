2db4d6f2e68b6e99d85b69b0f45abd8bd9d55851
Mark Larah <mark@larah.me>
2018-01-15 00:15:55 -0800

    [examples] Update gatsby example to work with v1.0.0-beta.28 (#9877)
    * Update gatsby example to work with v1.0.0-beta.28
    
    * Update getPageContext.js

--- examples/gatsby/src/getPageContext.js
+++ examples/gatsby/src/getPageContext.js
@@ -9,8 +9,16 @@ import green from 'material-ui/colors/green';
 // It's optional.
 const theme = createMuiTheme({
   palette: {
-    primary: purple,
-    secondary: green,
+    primary: {
+      light: purple[300],
+      main: purple[500],
+      dark: purple[700],
+    },
+    secondary: {
+      light: green[300],
+      main: green[500],
+      dark: green[700],
+    },
   },
 });
 
