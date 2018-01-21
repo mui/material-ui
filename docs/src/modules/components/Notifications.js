5c1a6c5e5bb11620a8ae4b772f0b0406144e1766
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-01-21 13:10:25 +0100

    commit

--- docs/src/modules/components/Notifications.js
+++ docs/src/modules/components/Notifications.js
@@ -1,7 +1,21 @@
+/* eslint-disable react/no-danger */
+
 import React from 'react';
 import Button from 'material-ui/Button';
 import Snackbar from 'material-ui/Snackbar';
 import messages from 'docs/src/messages';
+
+if (messages.length > 3) {
+  throw new Error('We cannot display more than 3 notifications in a row');
+}
+
+function getLastSeenNotifications() {
+  const seen = document.cookie.replace(
+    /(?:(?:^|.*;\s*)lastSeenNotifications\s*=\s*([^;]*).*$)|^.*$/,
+    '$1',
+  );
+  return seen === '' ? 0 : parseInt(seen, 10);
+}
 
 class Notifications extends React.Component {
   state = {
@@ -13,70 +27,38 @@ class Notifications extends React.Component {
     this.handleMessage();
   };
 
-  getSeenNotifications = () => {
-    const seen = document.cookie.replace(
-      /(?:(?:^|.*;\s*)seenNotifications\s*=\s*([^;]*).*$)|^.*$/,
-      '$1',
-    );
-    return seen === '' ? 0 : parseInt(seen, 10);
-  };
-
-  getClosedNotifications = () => {
-    const closed = document.cookie.replace(
-      /(?:(?:^|.*;\s*)closedNotifications\s*=\s*([^;]*).*$)|^.*$/,
-      '$1',
-    );
-    const closedArray = closed ? closed.split(',') : [];
-    return closedArray.map(a => parseInt(a, 10));
-  };
-
   handleMessage = () => {
-    const seen = this.getSeenNotifications();
-    const closed = this.getClosedNotifications();
-
-    const unseenMessages = messages.filter(
-      message => message.id > seen && !closed.includes(message.id),
-    );
-    const maxMessage = unseenMessages.reduce((a, m) => Math.max(a, m.id), 0);
-
-    if (!(seen >= maxMessage)) {
-      this.setState({ message: unseenMessages.shift(), open: true });
+    const lastSeen = getLastSeenNotifications();
+    const unseenMessages = messages.filter(message => message.id > lastSeen);
+    if (unseenMessages.length > 0) {
+      this.setState({ message: unseenMessages[0], open: true });
     }
   };
 
-  handleClose = (event, reason) => {
+  handleClose = () => {
     this.setState({ open: false });
-    document.cookie = `seenNotifications=${this.state.message.id};path=/`;
-
-    // Close button clicked
-    if (reason === undefined) {
-      const closed = this.getClosedNotifications();
-
-      if (closed.indexOf(this.state.message.id) === -1) {
-        closed.push(this.state.message.id);
-      }
-      document.cookie = `closedNotifications=${closed.join()};path=/;max-age=31536000`;
-    }
+    document.cookie = `lastSeenNotifications=${this.state.message.id};path=/`;
   };
 
   render() {
+    const { message, open } = this.state;
+
     return (
       <Snackbar
+        key={message.id}
         anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
         SnackbarContentProps={{
-          'aria-describedby': 'message',
+          'aria-describedby': 'notification-message',
         }}
         message={
-          // eslint-disable-next-line react/no-danger
-          <span id="message" dangerouslySetInnerHTML={{ __html: this.state.message.text }} />
+          <span id="notification-message" dangerouslySetInnerHTML={{ __html: message.text }} />
         }
-        key={this.state.message.id}
         action={
           <Button dense color="secondary" onClick={this.handleClose}>
             Close
           </Button>
         }
-        open={this.state.open}
+        open={open}
         autoHideDuration={6000}
         onClose={this.handleClose}
         onExited={this.handleMessage}
