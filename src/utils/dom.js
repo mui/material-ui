44281091beb401af5bf887b53ad9a8f131f61b49
Nicolas <n.bellamy@titeya.com>
2018-01-24 13:41:59 +0100

    [dom] replace by window.scroll

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,22 +1,20 @@
 export default {
+	isDescendant(parent, child) {
+		let node = child.parentNode;
 
-  isDescendant(parent, child) {
-    let node = child.parentNode;
+		while (node !== null) {
+			if (node === parent) return true;
+			node = node.parentNode;
+		}
 
-    while (node !== null) {
-      if (node === parent) return true;
-      node = node.parentNode;
-    }
+		return false;
+	},
 
-    return false;
-  },
-
-  offset(el) {
-    const rect = el.getBoundingClientRect();
-    return {
-      top: rect.top + document.body.scrollTop,
-      left: rect.left + document.body.scrollLeft,
-    };
-  },
-
+	offset(el) {
+		const rect = el.getBoundingClientRect();
+		return {
+			top: rect.top + window.scrollY,
+			left: rect.left + window.scrollX
+		};
+	}
 };
