4ba67af9d047a67757f27457cb4f9fbed518ed8e
Melissa Noelle <melissamatara@gmail.com>
2018-01-31 15:57:16 -0700

    Adds StepIcon to Stepper exports

--- src/Stepper/index.js
+++ src/Stepper/index.js
@@ -3,3 +3,4 @@ export { default as Step } from './Step';
 export { default as StepButton } from './StepButton';
 export { default as StepContent } from './StepContent';
 export { default as StepLabel } from './StepLabel';
+export { default as StepIcon } from './StepIcon';
