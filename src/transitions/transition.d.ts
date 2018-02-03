fec29678e32d75dbe5be69023fb553ed33613447
Sebastian Sebald <sebastian.sebald@gmail.com>
2018-02-04 00:03:46 +0100

    Fix "in" prop

--- src/transitions/transition.d.ts
+++ src/transitions/transition.d.ts
@@ -4,6 +4,8 @@ import { TransitionProps as _TransitionProps } from 'react-transition-group/Tran
 export type TransitionProps = Omit<_TransitionProps, 'timeout'> & {
   // timeout is required in `react-transition-group`, Material-UI comes with defaults.
   timeout?: _TransitionProps['timeout'];
+  // FIXME: @types/react-transition-group/Transition includes the object index, this breaks the `in` prop.
+  in?: _TransitionProps['in'];
   // FIXME: Missing from @types/react-transition-group/Transition
   mountOnEnter?: boolean;
   unmountOnExit?: boolean;
