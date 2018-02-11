74413deef9c43bf35ea0712a6a488678ce618fb1
Minh Nguyen <minhnguyenxx@gmail.com>
2018-02-12 07:53:14 +0800

    [build] Apply production Babel transforms when `BABEL_ENV` is set
    Setting `NODE_ENV=production` when `BABEL_ENV=es`
    should now allow production Babel transforms to be applied

--- scripts/material-ui-babel-preset.js
+++ scripts/material-ui-babel-preset.js
@@ -1,15 +1,16 @@
 const env = require('babel-preset-env');
 
 const ENV = process.env.BABEL_ENV;
-let babelPresetEnv;
+const NODE_ENV = process.env.NODE_ENV;
+let preset;
 
 // We release a ES version of Material-UI.
 // It's something that matches the latest official supported features of JavaScript.
 // Nothing more (stage-1, etc), nothing less (require, etc).
 if (ENV === 'es') {
-  babelPresetEnv = {};
+  preset = {};
 } else {
-  babelPresetEnv = {
+  preset = {
     presets: [
       [
         env,
@@ -29,4 +30,26 @@ if (ENV === 'es') {
   };
 }
 
-module.exports = babelPresetEnv;
+// Babel's `env` is `BABEL_ENV || NODE_ENV || 'development'`
+// See https://babeljs.io/docs/usage/babelrc/#env-option
+// However, this means that you can't combine `BABEL_ENV=modules` and `NODE_ENV=production`
+// as `env` will be set to 'modules' and the `NODE_ENV` bit is essentially lost.
+if (NODE_ENV === 'production') {
+  preset = Object.assign({}, preset, {
+    plugins: [
+      require.resolve('babel-plugin-transform-react-constant-elements'),
+      require.resolve('babel-plugin-transform-dev-warning'),
+      require.resolve('babel-plugin-transform-runtime'),
+      [require.resolve('babel-plugin-react-remove-properties'), { properties: ['data-mui-test'] }],
+      [
+        require.resolve('babel-plugin-transform-react-remove-prop-types'),
+        {
+          mode: 'wrap',
+          plugins: [require.resolve('babel-plugin-transform-flow-strip-types')],
+        },
+      ],
+    ],
+  });
+}
+
+module.exports = preset;
