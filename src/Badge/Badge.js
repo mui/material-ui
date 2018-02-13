864d75fb961b78bd5c597fe26ecb22b4854721d4
Andrew <andrew.cherniavskii@gmail.com>
2018-02-13 11:27:59 +0100

    use theme.spacing.unit

--- src/Badge/Badge.js
+++ src/Badge/Badge.js
@@ -4,48 +4,50 @@ import classNames from 'classnames';
 import withStyles from '../styles/withStyles';
 import { capitalize } from '../utils/helpers';
 
-const RADIUS = 12;
+export const styles = theme => {
+  const RADIUS = theme.spacing.unit * 1.5;
 
-export const styles = theme => ({
-  root: {
-    position: 'relative',
-    display: 'inline-flex',
-    // For correct alignment with the text.
-    verticalAlign: 'middle',
-  },
-  badge: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    top: -RADIUS,
-    right: -RADIUS,
-    fontFamily: theme.typography.fontFamily,
-    fontWeight: theme.typography.fontWeight,
-    fontSize: theme.typography.pxToRem(RADIUS),
-    width: RADIUS * 2,
-    height: RADIUS * 2,
-    borderRadius: '50%',
-    backgroundColor: theme.palette.color,
-    color: theme.palette.textColor,
-    zIndex: 1, // Render the badge on top of potential ripples.
-  },
-  colorPrimary: {
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
-  },
-  colorSecondary: {
-    backgroundColor: theme.palette.secondary.main,
-    color: theme.palette.secondary.contrastText,
-  },
-  colorError: {
-    backgroundColor: theme.palette.error.main,
-    color: theme.palette.error.contrastText,
-  },
-});
+  return {
+    root: {
+      position: 'relative',
+      display: 'inline-flex',
+      // For correct alignment with the text.
+      verticalAlign: 'middle',
+    },
+    badge: {
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      alignContent: 'center',
+      alignItems: 'center',
+      position: 'absolute',
+      top: -RADIUS,
+      right: -RADIUS,
+      fontFamily: theme.typography.fontFamily,
+      fontWeight: theme.typography.fontWeight,
+      fontSize: theme.typography.pxToRem(RADIUS),
+      width: RADIUS * 2,
+      height: RADIUS * 2,
+      borderRadius: '50%',
+      backgroundColor: theme.palette.color,
+      color: theme.palette.textColor,
+      zIndex: 1, // Render the badge on top of potential ripples.
+    },
+    colorPrimary: {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.primary.contrastText,
+    },
+    colorSecondary: {
+      backgroundColor: theme.palette.secondary.main,
+      color: theme.palette.secondary.contrastText,
+    },
+    colorError: {
+      backgroundColor: theme.palette.error.main,
+      color: theme.palette.error.contrastText,
+    },
+  };
+};
 
 function Badge(props) {
   const {
