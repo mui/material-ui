12f5abbcee493f495e61cc66bbb53fd45453f47f
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-02-16 16:38:46 +0100

    remove logic

--- src/utils/ClickAwayListener.js
+++ src/utils/ClickAwayListener.js
@@ -35,7 +35,6 @@ class ClickAwayListener extends React.Component {
       const el = findDOMNode(this);
 
       if (
-        (event.target instanceof HTMLElement || event.target instanceof SVGElement) &&
         document.documentElement &&
         document.documentElement.contains(event.target) &&
         !isDescendant(el, event.target)
