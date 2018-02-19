a465651bc5704886abe6b24711b514a448affa17
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-02-20 00:46:21 +0100

    [test] Fix regression tests

--- test/regressions/index.js
+++ test/regressions/index.js
@@ -37,6 +37,8 @@ const blacklistSuite = [
 
 const blacklistName = [
   'docs-getting-started-usage/Usage', // codesandbox inside
+  'docs-demos-drawers/tileData', // raw data
+  'docs-demos-grid-list/tileData', // raw data
 ];
 
 // Also use some of the demos to avoid code duplication.
