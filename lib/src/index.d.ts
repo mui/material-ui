d86b40b4bd63f7825de83a9e92191380d42ede1e
Johannes Lamberts <mail@j-lamberts.de>
2018-03-03 14:02:19 +0100

    fix: remot util export in index.js / index.d.ts Caused `Module not found: Can't resolve 'date-fns/addDays'` if date-fns isn't installed.

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -1,6 +1,3 @@
-export { default as dateFnsUtils } from './utils/date-fns-utils'
-export { default as momentUtils } from './utils/moment-utils'
-
 export { default as DatePicker } from './DatePicker';
 export { default as TimePicker } from './TimePicker';
 export { default as DateTimePicker } from './DateTimePicker';
