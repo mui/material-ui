04780d4f6c08f09b1d85fc4ceeb88ec4fe2b0ad2
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-03-09 00:00:40 +0100

    [docs] Add parcel example

---
+++ examples/parcel/README.md
@@ -0,0 +1,21 @@
+# Parcel example
+
+## How to use
+
+Download the example [or clone the repo](https://github.com/mui-org/material-ui):
+
+```bash
+curl https://codeload.github.com/mui-org/material-ui/tar.gz/v1-beta | tar -xz --strip=2  material-ui-1-beta/examples/parcel
+cd parcel
+```
+
+Install it and run:
+
+```bash
+npm install
+npm run start
+```
+
+## The idea behind the example
+
+[Parcel](https://github.com/parcel-bundler/parcel) is a Blazing fast, zero configuration web application bundler.
---
+++ examples/parcel/src/withRoot.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import purple from 'material-ui/colors/purple';
+import green from 'material-ui/colors/green';
+import Reboot from 'material-ui/Reboot';
+
+// A theme with custom primary and secondary color.
+// It's optional.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: purple[300],
+      main: purple[500],
+      dark: purple[700],
+    },
+    secondary: {
+      light: green[300],
+      main: green[500],
+      dark: green[700],
+    },
+  },
+});
+
+function withRoot(Component) {
+  function WithRoot(props) {
+    // MuiThemeProvider makes the theme available down the React tree
+    // thanks to React context.
+    return (
+      <MuiThemeProvider theme={theme}>
+        {/* Reboot kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <Reboot />
+        <Component {...props} />
+      </MuiThemeProvider>
+    );
+  }
+
+  return WithRoot;
+}
+
+export default withRoot;
