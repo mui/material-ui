ea7436c6ae61754db21634a78676ffab7eed6034
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2018-03-12 17:37:14 +0200

    Fix type inconvenience for utils interface

--- lib/src/typings/utils.d.ts
+++ lib/src/typings/utils.d.ts
@@ -5,28 +5,36 @@ export interface Utils {
   addDays(value: MaterialUiPickersDate, count: number): MaterialUiPickersDate;
   isValid(value: MaterialUiPickersDate): boolean;
   isEqual(value: MaterialUiPickersDate, comparing: MaterialUiPickersDate): boolean;
-  isNull(value: MaterialUiPickersDate): boolean;
+  isSameDay(value: MaterialUiPickersDate, comparing: MaterialUiPickersDate): boolean
+
   isAfter(value: MaterialUiPickersDate, comparing: MaterialUiPickersDate): boolean;
   isAfterDay(value: MaterialUiPickersDate, comparing: MaterialUiPickersDate): boolean;
+  isAfterYear(value: MaterialUiPickersDate, comparing: MaterialUiPickersDate): boolean;
+
   isBeforeDay(value: MaterialUiPickersDate, comparing: MaterialUiPickersDate): boolean;
   isBeforeYear(value: MaterialUiPickersDate, comparing: MaterialUiPickersDate): boolean;
-  isAfterYear(value: MaterialUiPickersDate, comparing: MaterialUiPickersDate): boolean;
+  isBefore(value: MaterialUiPickersDate, comparing: MaterialUiPickersDate): boolean;
+
   startOfDay(value: MaterialUiPickersDate): MaterialUiPickersDate;
   endOfDay(value: MaterialUiPickersDate): MaterialUiPickersDate;
+
   format(value: MaterialUiPickersDate, formatString: string): string;
   formatNumber(number: number): string;
+
   getHours(value: MaterialUiPickersDate): number;
   setHours(value: MaterialUiPickersDate, count: number): MaterialUiPickersDate;
   getMinutes(value: MaterialUiPickersDate): number;
   setMinutes(value: MaterialUiPickersDate, count: number): MaterialUiPickersDate
   getMonth(value: MaterialUiPickersDate): number;
-  isSameDay(value: MaterialUiPickersDate, comparing: MaterialUiPickersDate): boolean
+  getYear(value: MaterialUiPickersDate): number;
+  setYear(value: MaterialUiPickersDate): MaterialUiPickersDate;
+
+
   getStartOfMonth(value: MaterialUiPickersDate): MaterialUiPickersDate;
   getNextMonth(value: MaterialUiPickersDate): MaterialUiPickersDate;
   getPreviousMonth(value: MaterialUiPickersDate): MaterialUiPickersDate;
-  getYear(value: MaterialUiPickersDate): number;
-  setYear(value: MaterialUiPickersDate): MaterialUiPickersDate;
-  getWeekDays(): string[];
+ 
+  getWeekdays(): string[];
   getWeekArray(): MaterialUiPickersDate[];
   getYearRange(): MaterialUiPickersDate[];
 
