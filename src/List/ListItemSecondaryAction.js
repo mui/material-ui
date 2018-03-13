f4cf8df9f5c6ab846a2ced216f35e5443173b543
Andy Edwards <jedwards@fastmail.com>
2018-03-13 15:39:31 -0500

    [ListItemSecondaryAction] Vertically center (#10628)

--- src/List/ListItemSecondaryAction.js
+++ src/List/ListItemSecondaryAction.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import withStyles from '../styles/withStyles';
 
-export const styles = theme => ({
+export const styles = {
   root: {
     position: 'absolute',
     right: 4,
     top: '50%',
-    marginTop: -theme.spacing.unit * 3,
+    transform: 'translateY(-50%)',
   },
-});
+};
 
 function ListItemSecondaryAction(props) {
   const { children, classes, className, ...other } = props;
