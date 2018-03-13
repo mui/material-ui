88359431d16477da0d6b2d00b99117e8cdcaede7
Yuchen Wang <yuchen_wng@hotmail.com>
2018-03-13 10:40:25 +0000

    [docs] Updating the TS example to use CssBaseline (#10633)

--- examples/create-react-app-with-typescript/src/withRoot.tsx
+++ examples/create-react-app-with-typescript/src/withRoot.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import purple from 'material-ui/colors/purple';
 import green from 'material-ui/colors/green';
-import Reboot from 'material-ui/Reboot';
+import CssBaseline from 'material-ui/CssBaseline';
 
 // A theme with custom primary and secondary color.
 // It's optional.
@@ -19,8 +19,8 @@ function withRoot(Component: React.ComponentType) {
     // thanks to React context.
     return (
       <MuiThemeProvider theme={theme}>
-        {/* Reboot kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <Reboot />
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
         <Component {...props} />
       </MuiThemeProvider>
     );
