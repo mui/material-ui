5c3ccfa4ea7b9afa861b423dd69c2ff82f05ea60
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-03-14 21:38:35 +0100

    better separation on Matt feedback

--- docs/src/modules/components/HomeSteps.js
+++ docs/src/modules/components/HomeSteps.js
@@ -11,11 +11,8 @@ import MarkdownElement from 'docs/src/modules/components/MarkdownElement';
 import Link from 'docs/src/modules/components/Link';
 
 const styles = theme => ({
-  steps: {
-    maxWidth: theme.spacing.unit * 150,
-    margin: 'auto',
-  },
   step: {
+    border: `1px solid ${theme.palette.common.white}`,
     padding: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 2}px`,
     [theme.breakpoints.up('sm')]: {
       padding: `${theme.spacing.unit * 5}px ${theme.spacing.unit * 3}px`,
@@ -51,26 +48,25 @@ function HomeSteps(props) {
   const classes = props.classes;
 
   return (
-    <div className={classes.steps}>
-      <Grid container spacing={0}>
-        <Grid item xs={12} sm={6} className={classes.step}>
-          <FileDownloadIcon className={classes.stepIcon} />
-          <Typography variant="title" gutterBottom>
-            Installation
-          </Typography>
-          <Typography gutterBottom>{"Install Material-UI's source files via npm."}</Typography>
-          <MarkdownElement
-            className={classes.markdownElement}
-            text={`
+    <Grid container spacing={0}>
+      <Grid item xs={12} sm={6} className={classes.step}>
+        <FileDownloadIcon className={classes.stepIcon} />
+        <Typography variant="title" gutterBottom>
+          Installation
+        </Typography>
+        <Typography gutterBottom>{"Install Material-UI's source files via npm."}</Typography>
+        <MarkdownElement
+          className={classes.markdownElement}
+          text={`
 \`\`\`sh
 $ npm install material-ui@next
 \`\`\`
               `}
-          />
-          <Typography gutterBottom>{'Load the Roboto font.'}</Typography>
-          <MarkdownElement
-            className={classes.markdownElement}
-            text={`
+        />
+        <Typography gutterBottom>{'Load the Roboto font.'}</Typography>
+        <MarkdownElement
+          className={classes.markdownElement}
+          text={`
 \`\`\`html
 <link
   rel="stylesheet"
@@ -78,32 +74,27 @@ $ npm install material-ui@next
 >
 \`\`\`
               `}
-          />
-          <Divider className={classes.divider} />
-          <Button
-            component={buttonProps => (
-              <Link
-                variant="button"
-                prefetch
-                href="/getting-started/installation"
-                {...buttonProps}
-              />
-            )}
-          >
-            Read installation docs
-          </Button>
-        </Grid>
-        <Grid item xs={12} sm={6} className={classes.step}>
-          <BuildIcon className={classes.stepIcon} />
-          <Typography variant="title" gutterBottom>
-            Usage
-          </Typography>
-          <Typography gutterBottom>
-            {'Material-UI components work in isolation. They are self-supporting.'}
-          </Typography>
-          <MarkdownElement
-            className={classes.markdownElement}
-            text={`
+        />
+        <Divider className={classes.divider} />
+        <Button
+          component={buttonProps => (
+            <Link variant="button" prefetch href="/getting-started/installation" {...buttonProps} />
+          )}
+        >
+          Read installation docs
+        </Button>
+      </Grid>
+      <Grid item xs={12} sm={6} className={classes.step}>
+        <BuildIcon className={classes.stepIcon} />
+        <Typography variant="title" gutterBottom>
+          Usage
+        </Typography>
+        <Typography gutterBottom>
+          {'Material-UI components work in isolation. They are self-supporting.'}
+        </Typography>
+        <MarkdownElement
+          className={classes.markdownElement}
+          text={`
 \`\`\`jsx
 import React from 'react';
 import Button from 'material-ui/Button';
@@ -115,18 +106,17 @@ const App = () => (
 );
 \`\`\`
               `}
-          />
-          <Divider className={classes.divider} />
-          <Button
-            component={buttonProps => (
-              <Link variant="button" prefetch href="/getting-started/usage" {...buttonProps} />
-            )}
-          >
-            Explore the docs
-          </Button>
-        </Grid>
+        />
+        <Divider className={classes.divider} />
+        <Button
+          component={buttonProps => (
+            <Link variant="button" prefetch href="/getting-started/usage" {...buttonProps} />
+          )}
+        >
+          Explore the docs
+        </Button>
       </Grid>
-    </div>
+    </Grid>
   );
 }
 
