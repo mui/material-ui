8615ca79546c32c1d1e3a80d85ca115dea58883c
Nick Young <americanpilot84@gmail.com>
2018-03-15 19:44:05 -0400

    Add missing TypeScript property.

--- src/Dialog/DialogActions.d.ts
+++ src/Dialog/DialogActions.d.ts
@@ -1,8 +1,11 @@
 import * as React from 'react';
+
 import { StandardProps } from '..';
 
 export interface DialogActionsProps
-  extends StandardProps<React.HTMLAttributes<HTMLDivElement>, DialogActionsClassKey> {}
+  extends StandardProps<React.HTMLAttributes<HTMLDivElement>, DialogActionsClassKey> {
+    disableActionSpacing?: boolean;
+  }
 
 export type DialogActionsClassKey = 'root' | 'action';
 
