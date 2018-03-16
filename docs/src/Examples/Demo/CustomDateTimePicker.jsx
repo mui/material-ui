4de89ab5e2b0bcfbbfad67ded84de8f26258e815
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2018-03-16 13:42:00 +0200

    Fix not working parse with moment utils

--- docs/src/Examples/Demo/CustomDateTimePicker.jsx
+++ docs/src/Examples/Demo/CustomDateTimePicker.jsx
@@ -4,7 +4,7 @@ import { IconButton, Icon, InputAdornment } from 'material-ui';
 
 export default class CustomDateTimePicker extends PureComponent {
   state = {
-    selectedDate: new Date(),
+    selectedDate: new Date('2018-01-01 18:54'),
   }
 
   handleDateChange = (date) => {
