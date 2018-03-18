879ab60e9dd49deb8df3b091f09ded5b3734fe17
Daniel Juhl <danieljuhl@gmail.com>
2018-03-18 13:01:40 +0100

    [StepButton] Support vertical stepper (#10698)
    * [StepButton] justify content left
    
    This will render the children/StepLabel correct in a vertical Stepper
    
    * fix visual diff

--- src/Stepper/StepButton.js
+++ src/Stepper/StepButton.js
@@ -15,6 +15,9 @@ export const styles = theme => ({
     margin: `${-theme.spacing.unit * 3}px ${-theme.spacing.unit * 2}px`,
     boxSizing: 'content-box',
   },
+  vertical: {
+    justifyContent: 'left',
+  },
   touchRipple: {
     color: 'rgba(0, 0, 0, 0.3)',
   },
@@ -55,7 +58,13 @@ function StepButton(props) {
     <ButtonBase
       disabled={disabled}
       TouchRippleProps={{ className: classes.touchRipple }}
-      className={classNames(classes.root, classNameProp)}
+      className={classNames(
+        classes.root,
+        {
+          [classes.vertical]: orientation === 'vertical',
+        },
+        classNameProp,
+      )}
       {...other}
     >
       {child}
