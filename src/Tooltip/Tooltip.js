aafa95fb4bc7df01bedb835cc689e255ba3223a6
Eric <moondawg2413@gmail.com>
2018-03-20 15:14:03 -0500

    [Tooltip] Update error message (#10742)
    * Updated error message on Tooltip
    
    The error message for a disabled button child was confusing to read. Rewritten to flow better.
    
    * For consistency
    
    * fix tests

--- src/Tooltip/Tooltip.js
+++ src/Tooltip/Tooltip.js
@@ -112,11 +112,11 @@ class Tooltip extends React.Component {
         !this.children.disabled ||
         !this.children.tagName.toLowerCase() === 'button',
       [
-        'Material-UI: you are providing a disabled button children to the Tooltip component.',
-        'A disabled element do not fire events.',
-        'But the Tooltip needs to listen to the children element events to display the title.',
+        'Material-UI: you are providing a disabled `button` child to the Tooltip component.',
+        'A disabled element does not fire events.',
+        "Tooltip needs to listen to the child element's events to display the title.",
         '',
-        'Place a `div` over top of the element.',
+        'Place a `div` container on top of the element.',
       ].join('\n'),
     );
   }
