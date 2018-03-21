265b1d44c96cf87633d8d2e52b77fc223fe5961b
Blaine Garrett <blaine@blainegarrett.com>
2018-03-20 21:16:37 -0500

    Update next.js PWA color example
    The example theme defined in getPageContext has only light, medium, dark defined for the primary pallet. As such, in the example, the 500 color used for the PWA theme undefined.

--- examples/nextjs/pages/_document.js
+++ examples/nextjs/pages/_document.js
@@ -22,7 +22,7 @@ class MyDocument extends Document {
             }
           />
           {/* PWA primary color */}
-          <meta name="theme-color" content={pageContext.theme.palette.primary[500]} />
+          <meta name="theme-color" content={pageContext.theme.palette.primary.main} />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
