0242e188b9d3c22006a02a4ffeb2c9c48e8ba43e
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-03-31 16:26:45 +0200

    [NoSSR] Document the use cases of the component

--- packages/material-ui-docs/src/NoSSR/NoSSR.js
+++ packages/material-ui-docs/src/NoSSR/NoSSR.js
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types';
 
 const DefaultOnSSR = () => null;
 
+/**
+ * Only render the component on the client.
+ * It can be useful in a variety of situations:
+ * - Reduce the rendering time on the server.
+ * - Under too heavy server load, you can apply service degradation.
+ * - Improve the time-to-first paint on the client by only rendering above the fold.
+ * - Escape hatch for broken dependencies not supporting SSR.
+ */
 class NoSSR extends React.Component {
   state = {
     mounted: false,
