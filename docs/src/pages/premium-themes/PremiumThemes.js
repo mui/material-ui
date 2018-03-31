9690e8f4bca901503943e428fd7fcccf4090e550
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-03-31 15:59:16 +0200

    [docs] Display the two theme versions

--- docs/src/pages/premium-themes/PremiumThemes.js
+++ docs/src/pages/premium-themes/PremiumThemes.js
@@ -7,9 +7,6 @@ import Grid from 'material-ui/Grid';
 import Typography from 'material-ui/Typography';
 
 const styles = {
-  card: {
-    maxWidth: 430,
-  },
   media: {
     height: 230,
   },
@@ -24,45 +21,55 @@ const themes = [
     category: 'Admin & Dashboard',
     href: 'https://www.creative-tim.com/product/material-dashboard-pro-react?partner=104080',
   },
+  {
+    name: 'Material Dashboard',
+    description: 'Material Dashboard React is a Free Material-UI Admin.',
+    src: '/static/images/themes/creative-tim.png',
+    price: 'FREE',
+    category: 'Admin & Dashboard',
+    href: 'https://www.creative-tim.com/product/material-dashboard-react?partner=104080',
+  },
 ];
 
 function PremiumThemes(props) {
   const { classes } = props;
   return (
-    <div>
+    <Grid container spacing={16}>
       {themes.map(theme => (
-        <Card key={theme.name} className={classes.card}>
-          <CardMedia
-            component="a"
-            href={theme.href}
-            className={classes.media}
-            image={theme.src}
-            title={theme.name}
-          />
-          <CardContent>
-            <Grid container alignItems="center">
-              <Grid item xs>
-                <Typography gutterBottom variant="headline" align="left" component="h2">
-                  {theme.name}
-                </Typography>
+        <Grid item xs={12} sm={6} md={4}>
+          <Card key={theme.name}>
+            <CardMedia
+              component="a"
+              href={theme.href}
+              className={classes.media}
+              image={theme.src}
+              title={theme.name}
+            />
+            <CardContent>
+              <Grid container alignItems="center">
+                <Grid item xs>
+                  <Typography gutterBottom variant="headline" align="left" component="h2">
+                    {theme.name}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography gutterBottom>{theme.price}</Typography>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Typography gutterBottom>{theme.price}</Typography>
-              </Grid>
-            </Grid>
-            <Typography gutterBottom color="textSecondary">
-              {theme.category}
-            </Typography>
-            <Typography component="p">{theme.description}</Typography>
-          </CardContent>
-          <CardActions>
-            <Button component="a" href={theme.href} size="small" color="primary">
-              Learn More
-            </Button>
-          </CardActions>
-        </Card>
+              <Typography gutterBottom color="textSecondary">
+                {theme.category}
+              </Typography>
+              <Typography component="p">{theme.description}</Typography>
+            </CardContent>
+            <CardActions>
+              <Button component="a" href={theme.href} size="small" color="primary">
+                Learn More
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 }
 
