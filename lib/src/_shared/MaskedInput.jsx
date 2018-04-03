32d30a65d9862aadcec3728ba74ec95c761cd9cf
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2018-04-03 18:03:18 +0300

    Fix showing mask on clearing timepicker

--- lib/src/_shared/MaskedInput.jsx
+++ lib/src/_shared/MaskedInput.jsx
@@ -5,18 +5,28 @@ import MaskedInput from 'react-text-mask';
 export default class Input extends PureComponent {
   static propTypes = {
     mask: PropTypes.any,
+    value: PropTypes.string,
     inputRef: PropTypes.func.isRequired,
   }
 
   static defaultProps = {
+    value: undefined,
     mask: undefined,
   }
 
+  getMask = () => {
+    if (this.props.value) {
+      return this.props.mask;
+    }
+
+    return [];
+  }
+
   render() {
-    const { inputRef, ...props } = this.props;
+    const { inputRef, mask, ...props } = this.props;
     return (
-      this.props.mask
-        ? <MaskedInput {...props} ref={inputRef} />
+      mask
+        ? <MaskedInput mask={this.getMask()} {...props} ref={inputRef} />
         : <input {...props} ref={inputRef} />
     );
   }
