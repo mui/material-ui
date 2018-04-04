d7eb3182579c83510cae989525455c8cf92f4e8e
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2018-04-04 11:04:26 +0300

    Revert ignoring mask if value is empty code

--- lib/src/_shared/MaskedInput.jsx
+++ lib/src/_shared/MaskedInput.jsx
@@ -5,28 +5,18 @@ import MaskedInput from 'react-text-mask';
 export default class Input extends PureComponent {
   static propTypes = {
     mask: PropTypes.any,
-    value: PropTypes.string,
     inputRef: PropTypes.func.isRequired,
   }
 
   static defaultProps = {
-    value: undefined,
     mask: undefined,
   }
 
-  getMask = () => {
-    if (this.props.value) {
-      return this.props.mask;
-    }
-
-    return [];
-  }
-
   render() {
-    const { inputRef, mask, ...props } = this.props;
+    const { inputRef, ...props } = this.props;
     return (
-      mask
-        ? <MaskedInput mask={this.getMask()} {...props} ref={inputRef} />
+      this.props.mask
+        ? <MaskedInput {...props} ref={inputRef} />
         : <input {...props} ref={inputRef} />
     );
   }
