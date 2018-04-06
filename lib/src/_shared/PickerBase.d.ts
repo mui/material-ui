b34fab65c61a73ba16ce11f06057c86ee7a164a2
Mathieu Richard <mathieu@comeet.in>
2018-04-06 13:46:44 +0200

    change tabs for spaces

--- lib/src/_shared/PickerBase.d.ts
+++ lib/src/_shared/PickerBase.d.ts
@@ -2,9 +2,9 @@ import { MaterialUiPickersDate } from '../typings/date'
 import { DateTextFieldProps } from './ModalDialog';
 
 export interface PickerBaseProps {
-	onChange: (date: MaterialUiPickersDate) => void;
-	autoOk?: boolean;
-	ampm?: boolean;
-	pickerRef?: (e: Element) => void;
+    onChange: (date: MaterialUiPickersDate) => void;
+    autoOk?: boolean;
+    ampm?: boolean;
+    pickerRef?: (e: Element) => void;
 
 }
