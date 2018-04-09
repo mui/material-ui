edcf3ba6144e12af6cc89ec798d888e914fd2905
Simon <sn@hitbox.tv>
2018-04-09 18:55:27 +0200

    wip for luxon support

--- lib/__tests__/test-utils.d.ts
+++ lib/__tests__/test-utils.d.ts
@@ -1,4 +1,5 @@
 import MomentUtils from '../src/utils/moment-utils'
 import DateFnsUtils from '../src/utils/date-fns-utils'
+import LuxonUtils from '../src/utils/luxon-utils';
 
-export const utilsToUse: typeof MomentUtils | typeof DateFnsUtils;
+export const utilsToUse: typeof MomentUtils | typeof DateFnsUtils | typeof LuxonUtils;
