9445ab84691e1ea9ffd309857763194278d127f7
lookapanda <lookapanda@ify.re>
2018-04-09 20:18:12 +0200

    Change argument name to count

--- lib/src/utils/moment-utils.d.ts
+++ lib/src/utils/moment-utils.d.ts
@@ -31,7 +31,7 @@ declare class MomentUtils extends Utils {
   setMinutes(value: Moment, count: number): Moment
   getMonth(value: Moment): number;
   getYear(value: Moment): number;
-  setYear(value: Moment, year: number): Moment;
+  setYear(value: Moment, count: number): Moment;
 
   getStartOfMonth(value: Moment): Moment;
   getNextMonth(value: Moment): Moment;
