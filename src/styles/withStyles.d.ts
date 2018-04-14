5ee667652e84858abba2034813f5727567a723dc
Tom Crockett <pelotom@gmail.com>
2018-04-14 08:34:33 -0700

    Upgrade types, use string index fallback for CSSProperties to allow nested pseudos (#11007)

--- src/styles/withStyles.d.ts
+++ src/styles/withStyles.d.ts
@@ -1,5 +1,11 @@
 import * as React from 'react';
 import { Theme } from './createMuiTheme';
+import * as CSS from 'csstype';
+
+export interface CSSProperties extends CSS.Properties<number | string> {
+  // Allow pseudo selectors and media queries
+  [k: string]: CSS.Properties<number | string>[keyof CSS.Properties] | CSSProperties;
+}
 
 /**
  * This is basically the API of JSS. It defines a Map<string, CSS>,
@@ -8,7 +14,7 @@ import { Theme } from './createMuiTheme';
  * - the `keys` are the class (names) that will be created
  * - the `values` are objects that represent CSS rules (`React.CSSProperties`).
  */
-export type StyleRules<ClassKey extends string = string> = Record<ClassKey, React.CSSProperties>;
+export type StyleRules<ClassKey extends string = string> = Record<ClassKey, CSSProperties>;
 
 export type StyleRulesCallback<ClassKey extends string = string> = (
   theme: Theme,
