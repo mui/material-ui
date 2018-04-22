270bf7a327925dd42072ece9156eed83524df7db
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-04-22 19:15:33 +0200

    [Tabs] Better Ant Design demo

--- docs/src/pages/demos/text-fields/CustomizedInputs.js
+++ docs/src/pages/demos/text-fields/CustomizedInputs.js
@@ -40,6 +40,18 @@ const styles = theme => ({
     padding: '10px 12px',
     width: 'calc(100% - 24px)',
     transition: theme.transitions.create(['border-color', 'box-shadow']),
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
     '&:focus': {
       borderColor: '#80bdff',
       boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
