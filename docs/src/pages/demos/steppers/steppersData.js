b185696ec7949f9d088b882d42daff31c454cc0b
klynger <rafafhgkifh@gmail.com>
2018-04-29 11:04:25 -0300

    Adding content to the mobile stepper - text demo

---
+++ docs/src/pages/demos/steppers/steppersData.js
@@ -0,0 +1,20 @@
+const imagesData = [
+  {
+    source: '/static/images/steppers/horizon.jpeg',
+    alt: 'Beach',
+  },
+  {
+    source: '/static/images/steppers/fern-leaf.jpeg',
+    alt: 'Woman hold a fern leaf',
+  },
+  {
+    source: '/static/images/steppers/plants.jpeg',
+    alt: 'Some plants',
+  },
+  {
+    source: '/static/images/steppers/tractor.jpg',
+    alt: 'A Tractor',
+  },
+];
+
+export default imagesData;
