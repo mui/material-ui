437ddb128c35fbaef06c06618dde9c7262440799
Justin Wesley <jwesley@connekt.com>
2018-05-10 14:15:05 -0600

    [TextField] update doc for customized inputs

--- docs/src/pages/demos/text-fields/CustomizedInputs.js
+++ docs/src/pages/demos/text-fields/CustomizedInputs.js
@@ -23,7 +23,7 @@ const styles = theme => ({
   cssFocused: {},
   cssUnderline: {
     '&:after': {
-      backgroundColor: purple[500],
+      borderBottomColor: purple[500],
     },
   },
   bootstrapRoot: {
