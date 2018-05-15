f42fa1350c7dab0254a517c3435534d506430d4a
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-05-15 12:22:59 +0200

    [core] Fix size-limit for the new Next path

---
+++ .size-limit.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+
+function getMainFile() {
+  const dirname = '.next/static/commons'
+  const files = fs.readdirSync(dirname)
+  const [file] = files
+    .reduce((result, filename) => {
+      if (!/^main-[a-f0-9]+\.js$/.test(filename)) {
+        return result
+      }
+
+      const path = `${dirname}/${filename}`
+      return [...result, { path, ctime: fs.statSync(path).ctimeMs }]
+    }, [])
+    .sort((x, y) => y.ctime - x.ctime)
+  return file
+}
+
+module.exports = [
+  {
+    name: 'The initial cost people pay for using one component',
+    webpack: true,
+    path: 'packages/material-ui/build/Paper/index.js',
+    limit: '26.0 KB',
+  },
+  {
+    name: 'The size of all the modules of material-ui.',
+    webpack: true,
+    path: 'packages/material-ui/build/index.js',
+    limit: '99.8 KB',
+  },
+  {
+    name: 'The main bundle of the docs',
+    webpack: false,
+    path: getMainFile().path,
+    limit: '183 KB',
+  },
+  {
+    name: 'The home page of the docs',
+    webpack: false,
+    path: '.next/bundles/pages/index.js',
+    limit: '6 KB',
+  },
+];
