e08f44fc8d61e5ebf5433fd7cbc6b29f3d1245fc
Gabor Csizmadia <gaborcsiz@gmail.com>
2018-05-15 17:02:34 +0200

    [Popover] Fix max height issue in some mobile browsers (#11349)
    Viewport height is taller than the visible part of the document in some
    mobile browsers.
    https://nicolas-hoizey.com/2015/02/viewport-height-is-taller-than-the-visible-part-of-the-document-in-some-mobile-browsers.html

--- packages/material-ui/src/Menu/Menu.js
+++ packages/material-ui/src/Menu/Menu.js
@@ -23,7 +23,7 @@ export const styles = {
     // specZ: The maximum height of a simple menu should be one or more rows less than the view
     // height. This ensures a tappable area outside of the simple menu with which to dismiss
     // the menu.
-    maxHeight: 'calc(100vh - 96px)',
+    maxHeight: 'calc(100% - 96px)',
     // Add iOS momentum scrolling.
     WebkitOverflowScrolling: 'touch',
   },
