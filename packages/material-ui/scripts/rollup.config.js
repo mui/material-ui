e509b0f9ff8f31cdbb3322de48a7e998d427f184
Bogdan Chadkin <trysound@yandex.ru>
2018-05-16 00:02:18 +0300

    [core] Bundle UMD with rollup (#11360)
    * Bundle UMD with rollup
    
    Sizes before and after
    
    webpack
    
    material-ui.development.js 2.54M
    material-ui.production.min.js 436.3K
    
    rollup
    
    material-ui.development.js 1.01M
    material-ui.production.min.js 370.1K
    
    Added size snapshot to track bundle size change on every commit.
    
    * one iteration

---
+++ packages/material-ui/scripts/rollup.config.js
@@ -0,0 +1,54 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
+
+const input = './src/index.js';
+
+const name = 'material-ui';
+
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+};
+
+const getBabelOptions = () => ({
+  exclude: /node_modules/,
+  // We are using @babel/plugin-transform-runtime
+  runtimeHelpers: true,
+});
+
+const getCommonjsOptions = () => ({
+  include: /node_modules/,
+});
+
+export default [
+  {
+    input,
+    output: { file: `build/umd/${name}.development.js`, format: 'umd', name, globals },
+    external: Object.keys(globals),
+    plugins: [
+      nodeResolve(),
+      babel(getBabelOptions()),
+      commonjs(getCommonjsOptions()),
+      sizeSnapshot(),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+    ],
+  },
+
+  {
+    input,
+    output: { file: `build/umd/${name}.production.min.js`, format: 'umd', name, globals },
+    external: Object.keys(globals),
+    plugins: [
+      nodeResolve(),
+      babel(getBabelOptions()),
+      commonjs(getCommonjsOptions()),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      sizeSnapshot(),
+      uglify(),
+    ],
+  },
+];
