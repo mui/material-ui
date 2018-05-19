f28724881026434bfdd99bd33475290ccafa40ba
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-05-19 20:49:36 +0200

    Update Grid.js

--- packages/material-ui/src/Grid/Grid.js
+++ packages/material-ui/src/Grid/Grid.js
@@ -92,7 +92,6 @@ export const styles = theme => ({
   },
   item: {
     boxSizing: 'border-box',
-    flex: '0 1 auto', // Allow shrink so text will wrap in items by default.
     margin: '0', // For instance, it's useful when used with a `figure` element.
   },
   zeroMinWidth: {
