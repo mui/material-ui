9c5e0cf8b26aaac32f6bd254d74a0dcbbf534e8e
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-05-21 19:32:28 +0200

    [core] Internalize ScrollbarSize (#11523)

--- .size-limit.js
+++ .size-limit.js
@@ -27,7 +27,7 @@ module.exports = [
     name: 'The size of all the modules of material-ui.',
     webpack: true,
     path: 'packages/material-ui/build/index.js',
-    limit: '95.1 KB',
+    limit: '94.5 KB',
   },
   {
     name: 'The main bundle of the docs',
