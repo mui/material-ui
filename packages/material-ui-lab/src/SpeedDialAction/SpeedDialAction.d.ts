b6b52aee23d58618405c1103d0560b11773514de
Michael Tremblay <michael.tremblay@cchic.ca>
2018-05-22 10:01:08 -0400

    Added type definitions to lab, so SpeedDial can be use with TypeScript project

---
+++ packages/material-ui-lab/src/SpeedDialAction/SpeedDialAction.d.ts
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { StandardProps } from "../../../material-ui/src";
+import { ButtonProps } from "../../../material-ui/src/Button";
+import { TooltipProps } from "../../../material-ui/src/Tooltip";
+
+export interface SpeedDialActionProps extends StandardProps<ButtonProps & TooltipProps, SpeedDialActionClassKey> {
+    ButtonProps?: Partial<ButtonProps>;
+    delay?: number;
+    icon: React.ReactNode;
+    tooltipTitle?: React.ReactNode;
+}
+
+export type SpeedDialActionClassKey =
+    | "root"
+    | "button"
+    | "buttonClosed";
+
+declare const SpeedDialAction: React.ComponentType<any>;
+
+export default SpeedDialAction;
