a32ae77a5c57391d214d22ada928c4af9cb72297
Matt Brookes <github@nospam.33m.co>
2018-05-25 21:20:55 +0100

    [docs] Fix Slider demo imports (#11589)

--- docs/src/pages/lab/slider/SimpleSlider.js
+++ docs/src/pages/lab/slider/SimpleSlider.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import Typography from 'material-ui/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/lab/Slider';
 
 const styles = {
