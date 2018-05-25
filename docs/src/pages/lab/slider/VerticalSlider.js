b48270eae5a1e2752720253c24810b21187e4f63
Matthew Brookes <github@nospam.33m.co>
2018-05-25 19:52:30 +0100

    [docs] Fix Slider demo imports

--- docs/src/pages/lab/slider/VerticalSlider.js
+++ docs/src/pages/lab/slider/VerticalSlider.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Slider from '@material-ui/lab/Slider';
 
 const styles = {
