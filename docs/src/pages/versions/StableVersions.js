7fd6e34562e8e3da0b1e3d2c892fcb841129d5fe
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-05-27 14:56:21 +0200

    [docs] Keep the current version into account (#11595)
    * [docs] Keep the current version into account
    
    * take matt feedback

--- docs/src/pages/versions/StableVersions.js
+++ docs/src/pages/versions/StableVersions.js
@@ -1,6 +1,8 @@
 import 'isomorphic-fetch';
 import React from 'react';
 import PropTypes from 'prop-types';
+import orderBy from 'lodash/orderBy';
+import sortedUniqBy from 'lodash/sortedUniqBy';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -14,6 +16,7 @@ const GITHUB_RELEASE_BASE_URL = 'https://github.com/mui-org/material-ui/releases
 
 const styles = {
   root: {
+    minHeight: 50,
     width: '100%',
   },
 };
@@ -43,42 +46,52 @@ async function getBranches() {
 
 class StableVersions extends React.Component {
   state = {
-    versions: [],
+    docs: [],
   };
 
   componentDidMount = async () => {
     const branches = await getBranches();
-    const versions = branches.map(n => n.name);
-    versions.reverse().pop(); // most recent first & remove 'latest'
-    versions.push('v0.20.1');
-
-    this.setState({ versions });
+    let docs = branches.map(n => n.name);
+    docs = docs.filter(version => version !== 'latest');
+    docs = docs.map(version => ({
+      version,
+      // Replace dot with dashes for Netlify branch subdomains
+      url: `https://${version.replace(/\./g, '-')}.material-ui.com`,
+    }));
+    // Current version.
+    docs.push({
+      version: `v${process.env.LIB_VERSION}`,
+      url: document.location.origin,
+    });
+    // Legacy documentation.
+    docs.push({
+      version: 'v0.20.1',
+      url: 'https://v0.material-ui.com',
+    });
+    docs = orderBy(docs, 'version', 'desc');
+    docs = sortedUniqBy(docs, 'version');
+    // The latest version is always using the naked domain.
+    docs[0].url = 'https://material-ui.com';
+    this.setState({ docs });
   };
 
   render() {
     const { classes } = this.props;
-    const { versions } = this.state;
+    const { docs } = this.state;
 
     return (
       <Paper className={classes.root}>
         <Table>
           <TableBody>
-            {versions.map((version, index) => {
-              // Replace dot with dashes for Netlify branch subdomains
-              let url = `https://${version.replace(/\./g, '-')}.material-ui.com`;
-              if (index === 0) {
-                url = 'https://material-ui.com';
-              } else if (version.startsWith('v0')) {
-                url = 'https://v0.material-ui.com';
-              }
+            {docs.map(doc => {
               return (
-                <TableRow key={version}>
+                <TableRow key={doc.version}>
                   <TableCell padding="dense">
-                    <Typography>{version}</Typography>
+                    <Typography>{doc.version}</Typography>
                   </TableCell>
                   <TableCell padding="dense">
                     <Typography
-                      component={props2 => <Link {...props2} variant="secondary" href={url} />}
+                      component={props2 => <Link {...props2} variant="secondary" href={doc.url} />}
                     >
                       Documentation
                     </Typography>
@@ -89,7 +102,7 @@ class StableVersions extends React.Component {
                         <Link
                           {...props2}
                           variant="secondary"
-                          href={`${GITHUB_RELEASE_BASE_URL}${version}`}
+                          href={`${GITHUB_RELEASE_BASE_URL}${doc.version}`}
                         />
                       )}
                     >
