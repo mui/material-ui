591cad090c31fa686b5af6b613c0b64a382ee5b1
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2018-05-31 11:30:46 +0300

    Update codesandbox to actual version of pickers and material-ui

--- docs/src/Routes/GettingStarted/Usage.jsx
+++ docs/src/Routes/GettingStarted/Usage.jsx
@@ -36,7 +36,7 @@ const Usage = ({ classes }) => (
 
     <iframe
       title="codesandbox"
-      src="https://codesandbox.io/embed/l2ykr7kwvz?hidenavigation=1&fontsize=14&view=preview"
+      src="https://codesandbox.io/embed/42kyyw2pmx?hidenavigation=1&fontsize=14&view=preview"
       style={{
         width: '100%',
         height: 650,
