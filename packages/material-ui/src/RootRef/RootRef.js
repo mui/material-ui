8929f906450157a74ea42581a40803b23243a2bc
Bogdan Chadkin <trysound@yandex.ru>
2018-06-03 16:48:51 +0300

    [RootRef] Allow using React.createRef api with RootRef component (#11681)
    * Allow using React.createRef api with RootRef component
    
    `React.createRef` api is a bit simpler than callback and may cover a lot
    of cases. The idea is simple, just mutate `current` field of object.
    
    * let's merge

--- packages/material-ui/src/RootRef/RootRef.js
+++ packages/material-ui/src/RootRef/RootRef.js
@@ -1,19 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import exactProp from '../utils/exactProp';
 
 /**
  * Helper component to allow attaching a ref to a
- * child element that may not accept refs (functional component).
- * It's higly inspired by https://github.com/facebook/react/issues/11401#issuecomment-340543801
+ * wrapped element to access the underlying DOM element.
+ *
+ * It's higly inspired by https://github.com/facebook/react/issues/11401#issuecomment-340543801.
+ * For example:
+ * ```jsx
+ * import React from 'react';
+ * import RootRef from '@material-ui/core/RootRef';
+ *
+ * class MyComponent extends React.Component {
+ *   constructor(props) {
+ *     super(props);
+ *     this.domRef = React.createRef();
+ *   }
+ *
+ *   componentDidMount() {
+ *     console.log(this.domRef.current); // DOM node
+ *   }
+ *
+ *   render() {
+ *     return (
+ *       <RootRef rootRef={this.domRef}>
+ *         <SomeChildComponent />
+ *       </RootRef>
+ *     );
+ *   }
+ * }
+ * ```
  */
 class RootRef extends React.Component {
   componentDidMount() {
-    this.props.rootRef(ReactDOM.findDOMNode(this));
+    const rootRef = this.props.rootRef;
+    const node = ReactDOM.findDOMNode(this);
+    if (typeof rootRef === 'function') {
+      rootRef(node);
+    } else if (rootRef) {
+      rootRef.current = node;
+    }
   }
 
   componentWillUnmount() {
-    this.props.rootRef(null);
+    const rootRef = this.props.rootRef;
+    if (typeof rootRef === 'function') {
+      rootRef(null);
+    } else if (rootRef) {
+      rootRef.current = null;
+    }
   }
 
   render() {
@@ -22,8 +59,17 @@ class RootRef extends React.Component {
 }
 
 RootRef.propTypes = {
+  /**
+   * The wrapped element.
+   */
   children: PropTypes.element.isRequired,
-  rootRef: PropTypes.func.isRequired,
+  /**
+   * Provide a way to access the DOM node of the wrapped element.
+   * You can provide a callback ref or a `React.createRef()` ref.
+   */
+  rootRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
 };
+
+RootRef.propTypes = exactProp(RootRef.propTypes, 'RootRef');
 
 export default RootRef;
--- pages/api/root-ref.md
+++ pages/api/root-ref.md
@@ -7,15 +7,40 @@ filename: /packages/material-ui/src/RootRef/RootRef.js
 # RootRef
 
 Helper component to allow attaching a ref to a
-child element that may not accept refs (functional component).
-It's higly inspired by https://github.com/facebook/react/issues/11401#issuecomment-340543801
+wrapped element to access the underlying DOM element.
+
+It's higly inspired by https://github.com/facebook/react/issues/11401#issuecomment-340543801.
+For example:
+```jsx
+import React from 'react';
+import RootRef from '@material-ui/core/RootRef';
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.domRef = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log(this.domRef.current); // DOM node
+  }
+
+  render() {
+    return (
+      <RootRef rootRef={this.domRef}>
+        <SomeChildComponent />
+      </RootRef>
+    );
+  }
+}
+```
 
 ## Props
 
 | Name | Type | Default | Description |
 |:-----|:-----|:--------|:------------|
-| <span class="prop-name required">children *</span> | <span class="prop-type">element |   |  |
-| <span class="prop-name required">rootRef *</span> | <span class="prop-type">func |   |  |
+| <span class="prop-name required">children *</span> | <span class="prop-type">element |   | The wrapped element. |
+| <span class="prop-name required">rootRef *</span> | <span class="prop-type">union:&nbsp;func&nbsp;&#124;<br>&nbsp;object<br> |   | Provide a way to access the DOM node of the wrapped element. You can provide a callback ref or a `React.createRef()` ref. |
 
 Any other properties supplied will be spread to the root element (native element).
 
