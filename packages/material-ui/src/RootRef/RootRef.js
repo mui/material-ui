0da7886f2ae03dedbfd9b469205976fb013664cd
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-06-03 15:14:24 +0200

    let's merge

--- packages/material-ui/src/RootRef/RootRef.js
+++ packages/material-ui/src/RootRef/RootRef.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import exactProp from '../utils/exactProp';
 
 /**
  * Helper component to allow attaching a ref to a
- * child element that may not accept refs (functional component).
- * It's higly inspired by https://github.com/facebook/react/issues/11401#issuecomment-340543801
+ * wrapped element to access the underlying DOM element.
+ *
+ * It's higly inspired by https://github.com/facebook/react/issues/11401#issuecomment-340543801.
+ * For example:
+ * ```jsx
+ * import React from 'react';
+ * import RootRef from '@material-ui/core/RootRef';
+ *
+ * class MyComponent extends React.Component {
+ *   constructor(props) {
+ *     super(props);
+ *     this.domRef = React.createRef();
+ *   }
+ *
+ *   componentDidMount() {
+ *     console.log(this.domRef.current); // DOM node
+ *   }
+ *
+ *   render() {
+ *     return (
+ *       <RootRef rootRef={this.domRef}>
+ *         <SomeChildComponent />
+ *       </RootRef>
+ *     );
+ *   }
+ * }
+ * ```
  */
 class RootRef extends React.Component {
   componentDidMount() {
@@ -13,7 +39,7 @@ class RootRef extends React.Component {
     const node = ReactDOM.findDOMNode(this);
     if (typeof rootRef === 'function') {
       rootRef(node);
-    } else {
+    } else if (rootRef) {
       rootRef.current = node;
     }
   }
@@ -22,7 +48,7 @@ class RootRef extends React.Component {
     const rootRef = this.props.rootRef;
     if (typeof rootRef === 'function') {
       rootRef(null);
-    } else {
+    } else if (rootRef) {
       rootRef.current = null;
     }
   }
@@ -33,8 +59,17 @@ class RootRef extends React.Component {
 }
 
 RootRef.propTypes = {
+  /**
+   * The wrapped element.
+   */
   children: PropTypes.element.isRequired,
-  rootRef: PropTypes.oneOfType([PropTypes.func.isRequired, PropTypes.object.isRequired]),
+  /**
+   * Provide a way to access the DOM node of the wrapped element.
+   * You can provide a callback ref or a `React.createRef()` ref.
+   */
+  rootRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
 };
+
+RootRef.propTypes = exactProp(RootRef.propTypes, 'RootRef');
 
 export default RootRef;
--- packages/material-ui/src/RootRef/RootRef.test.js
+++ packages/material-ui/src/RootRef/RootRef.test.js
@@ -4,35 +4,41 @@ import { createMount } from '../test-utils';
 import RootRef from './RootRef';
 
 describe('<RootRef />', () => {
+  let mount;
+
+  before(() => {
+    mount = createMount();
+  });
+
+  after(() => {
+    mount.cleanUp();
+  });
+
   it('call rootRef function on mount and unmount', () => {
-    const mount = createMount();
     const Fn = () => <div />;
     const results = [];
-    mount(
+    const wrapper = mount(
       <RootRef rootRef={node => results.push(node)}>
         <Fn />
       </RootRef>,
     );
     assert.strictEqual(results.length, 1);
-    assert.ok(results[0] instanceof window.HTMLDivElement);
-
-    mount.cleanUp();
+    assert.strictEqual(results[0] instanceof window.HTMLDivElement, true);
+    wrapper.unmount();
     assert.strictEqual(results.length, 2);
     assert.strictEqual(results[1], null);
   });
 
   it('set rootRef current field on mount and unmount', () => {
-    const mount = createMount();
     const Fn = () => <div />;
-    const ref = { current: null };
-    mount(
+    const ref = React.createRef();
+    const wrapper = mount(
       <RootRef rootRef={ref}>
         <Fn />
       </RootRef>,
     );
-    assert.ok(ref.current instanceof window.HTMLDivElement);
-
-    mount.cleanUp();
+    assert.strictEqual(ref.current instanceof window.HTMLDivElement, true);
+    wrapper.unmount();
     assert.strictEqual(ref.current, null);
   });
 });
