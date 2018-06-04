f6b31dba0da0d026dea9ee5c9559d95e68b01ad2
Ahmad Ilaiwi <ailaiwi@foothillsolutions.com>
2018-06-04 15:27:44 +0300

    add missing types

--- lib/src/_shared/ModalDialog.d.ts
+++ lib/src/_shared/ModalDialog.d.ts
@@ -3,12 +3,17 @@ import { DateType } from '../constants/prop-types';
 import { Utils } from '../typings/utils';
 import { DialogProps } from '@material-ui/core/Dialog';
 import { ButtonProps } from '@material-ui/core/Button';
+import { EventListenerProps } from 'react-event-listener'
+import { Omit } from '@material-ui/core';
 
-export interface DateTextFieldProps extends DialogProps {
+type DialogBaseProps = Omit<DialogProps, "onKeyDown">
+
+export interface DateTextFieldProps extends DialogBaseProps {
   onAccept: ButtonProps['onClick'];
   onDismiss: ButtonProps['onClick'];
   onClear: ButtonProps['onClick'];
   onSetToday: ButtonProps['onClick'];
+  onKeyDown: EventListenerProps['onKeyDown']
   dialogContentClassName?: string;
   invalidLabel?: string;
   okLabel?: ReactNode;
