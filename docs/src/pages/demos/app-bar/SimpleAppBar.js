4efc36da7596b02572af8f0fe90d6677f2a38adf
Zaid Rehman Qureshi <zqzaidqureshi@gmail.com>
2018-06-07 13:29:26 +0530

    Change for AppBar import module

--- docs/src/pages/demos/app-bar/SimpleAppBar.js
+++ docs/src/pages/demos/app-bar/SimpleAppBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
+import  { AppBar } from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
