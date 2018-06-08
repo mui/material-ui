4cb0fa89ded7f07ab4428dc0ca648bb6b4471a34
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-06-08 23:14:47 +0200

    add comment

--- packages/material-ui/src/CircularProgress/CircularProgress.js
+++ packages/material-ui/src/CircularProgress/CircularProgress.js
@@ -25,7 +25,7 @@ function easeIn(t) {
 export const styles = theme => ({
   root: {
     display: 'inline-block',
-    lineHeight: 1,
+    lineHeight: 1, // Keep the progress centered
   },
   static: {
     transition: theme.transitions.create('transform'),
