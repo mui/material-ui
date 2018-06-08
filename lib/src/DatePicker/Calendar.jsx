5c3f17f886c3dfe8f9561f955eae76af82bd67ca
Mike Joyce <michael.joyce@target.com>
2018-06-08 08:54:33 -0500

    pass isFinish false on mount

--- lib/src/DatePicker/Calendar.jsx
+++ lib/src/DatePicker/Calendar.jsx
@@ -71,17 +71,17 @@ export class Calendar extends Component {
         disablePast,
         disableFuture,
         shouldDisableDate: this.shouldDisableDate,
-      }));
+      }), false);
     }
   }
 
-  onDateSelect = (day) => {
+  onDateSelect = (day, isFinish = true) => {
     const { date, utils } = this.props;
 
     const withHours = utils.setHours(day, utils.getHours(date));
     const withMinutes = utils.setMinutes(withHours, utils.getMinutes(date));
 
-    this.props.onChange(withMinutes);
+    this.props.onChange(withMinutes, isFinish);
   };
 
   handleChangeMonth = (newMonth) => {
@@ -230,7 +230,7 @@ export class Calendar extends Component {
       <Fragment>
         {
           allowKeyboardControl &&
-            <EventListener target="window" onKeyDown={this.handleKeyDown} />
+          <EventListener target="window" onKeyDown={this.handleKeyDown} />
         }
 
         <CalendarHeader
