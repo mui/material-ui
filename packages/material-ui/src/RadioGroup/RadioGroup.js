db5c3ebfb8d7c58d5764ba0ba5877787fe4ba276
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-06-10 00:38:57 +0200

    [RadioGroup] Fix onChange chaining

--- packages/material-ui/src/RadioGroup/RadioGroup.js
+++ packages/material-ui/src/RadioGroup/RadioGroup.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FormGroup from '../FormGroup';
-import { find } from '../utils/helpers';
+import { createChainedFunction, find } from '../utils/helpers';
 
 class RadioGroup extends React.Component {
   radios = [];
@@ -56,7 +56,7 @@ class RadioGroup extends React.Component {
               }
             },
             checked: value === child.props.value,
-            onChange: this.handleRadioChange,
+            onChange: createChainedFunction(child.props.onChange, this.handleRadioChange),
           });
         })}
       </FormGroup>
