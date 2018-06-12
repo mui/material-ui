b2f295b9af75f6de85db5c259787f574deff4926
Charles-Axel Dein <ca@d3in.org>
2018-06-12 10:24:48 +0200

    [Table] Derive sorted rows from state at render time in demo

--- docs/src/pages/demos/tables/EnhancedTable.js
+++ docs/src/pages/demos/tables/EnhancedTable.js
@@ -25,6 +25,12 @@ function createData(name, calories, fat, carbs, protein) {
   return { id: counter, name, calories, fat, carbs, protein };
 }
 
+const getSorting = (order, orderBy) => {
+  return order === 'desc'
+    ? (a, b) => a[orderBy] - b[orderBy]
+    : (a, b) => b[orderBy] - a[orderBy];
+}
+
 const columnData = [
   { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
   { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
@@ -197,7 +203,7 @@ class EnhancedTable extends React.Component {
         createData('Marshmallow', 318, 0, 81, 2.0),
         createData('Nougat', 360, 19.0, 9, 37.0),
         createData('Oreo', 437, 18.0, 63, 4.0),
-      ].sort((a, b) => (a.calories < b.calories ? -1 : 1)),
+      ],
       page: 0,
       rowsPerPage: 5,
     };
@@ -211,12 +217,7 @@ class EnhancedTable extends React.Component {
       order = 'asc';
     }
 
-    const data =
-      order === 'desc'
-        ? this.state.data.sort((a, b) => (b[orderBy] < a[orderBy] ? -1 : 1))
-        : this.state.data.sort((a, b) => (a[orderBy] < b[orderBy] ? -1 : 1));
-
-    this.setState({ data, order, orderBy });
+    this.setState({ order, orderBy });
   };
 
   handleSelectAllClick = (event, checked) => {
@@ -277,7 +278,7 @@ class EnhancedTable extends React.Component {
               rowCount={data.length}
             />
             <TableBody>
-              {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(n => {
+              {data.sort(getSorting(order, orderBy)).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(n => {
                 const isSelected = this.isSelected(n.id);
                 return (
                   <TableRow
