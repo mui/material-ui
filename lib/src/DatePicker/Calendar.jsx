7c261f516d148ee7967e3cb35bf2b2b91527fb4a
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2018-06-12 16:03:19 +0300

    Fix immediate close of modal, if autoOK = true and current date is disbled

--- lib/src/DatePicker/Calendar.jsx
+++ lib/src/DatePicker/Calendar.jsx
@@ -71,17 +71,17 @@ export class Calendar extends Component {
         disablePast,
         disableFuture,
         shouldDisableDate: this.shouldDisableDate,
-      }));
+      }), false);
     }
   }
 
-  onDateSelect = (day) => {
+  onDateSelect = (day, isFinish = true) => {
     const { date, utils } = this.props;
 
     const withHours = utils.setHours(day, utils.getHours(date));
     const withMinutes = utils.setMinutes(withHours, utils.getMinutes(date));
 
-    this.props.onChange(withMinutes);
+    this.props.onChange(withMinutes, isFinish);
   };
 
   handleChangeMonth = (newMonth) => {
