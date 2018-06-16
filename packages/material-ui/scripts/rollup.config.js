f87a3205099a22a2bbd3328e09da9380115be456
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-06-16 13:35:50 +0200

    [core] Fix the UMD release

--- packages/material-ui/scripts/rollup.config.js
+++ packages/material-ui/scripts/rollup.config.js
@@ -2,27 +2,25 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
+import nodeGlobals from 'rollup-plugin-node-globals';
 import { uglify } from 'rollup-plugin-uglify';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 
 const input = './src/index.js';
-
 const name = 'material-ui';
-
 const globals = {
   react: 'React',
   'react-dom': 'ReactDOM',
 };
-
-const getBabelOptions = () => ({
+const babelOptions = {
   exclude: /node_modules/,
   // We are using @babel/plugin-transform-runtime
   runtimeHelpers: true,
-});
-
-const getCommonjsOptions = () => ({
+};
+const commonjsOptions = {
+  ignoreGlobal: true,
   include: /node_modules/,
-});
+};
 
 export default [
   {
@@ -31,9 +29,9 @@ export default [
     external: Object.keys(globals),
     plugins: [
       nodeResolve(),
-      babel(getBabelOptions()),
-      commonjs(getCommonjsOptions()),
-      sizeSnapshot(),
+      babel(babelOptions),
+      commonjs(commonjsOptions),
+      nodeGlobals(),
       replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
     ],
   },
@@ -43,17 +41,12 @@ export default [
     external: Object.keys(globals),
     plugins: [
       nodeResolve(),
-      babel(getBabelOptions()),
-      commonjs(getCommonjsOptions()),
+      babel(babelOptions),
+      commonjs(commonjsOptions),
+      nodeGlobals(),
       replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
       sizeSnapshot(),
       uglify(),
     ],
-  },
-  {
-    input,
-    output: { file: `build/dist/${name}.esm.js`, format: 'es' },
-    external: id => !id.startsWith('.') && !id.startsWith('/'),
-    plugins: [nodeResolve(), babel(getBabelOptions())],
   },
 ];
