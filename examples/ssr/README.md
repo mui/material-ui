33a03a06c51a0d70260a89a022d6e57e80d02629
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-06-16 21:59:54 +0200

    [examples] Add Server Rendering implementation (#11880)

---
+++ examples/ssr/README.md
@@ -0,0 +1,21 @@
+# Server Rendering
+
+## How to use
+
+Download the example [or clone the repo](https://github.com/mui-org/material-ui):
+
+```bash
+curl https://codeload.github.com/mui-org/material-ui/tar.gz/master | tar -xz --strip=2  material-ui-master/examples/ssr
+cd ssr
+```
+
+Install it and run:
+
+```bash
+npm install
+npm run start
+```
+
+## The idea behind the example
+
+This is the reference implementation of the [Server Rendering tutorial](https://material-ui.com/guides/server-rendering/).
---
+++ examples/ssr/client.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { hydrate } from 'react-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+import red from '@material-ui/core/colors/red';
+import App from './App';
+
+class Main extends React.Component {
+  // Remove the server-side injected CSS.
+  componentDidMount() {
+    const jssStyles = document.getElementById('jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    return <App />;
+  }
+}
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+    accent: red,
+    type: 'light',
+  },
+});
+
+hydrate(
+  <MuiThemeProvider theme={theme}>
+    <Main />
+  </MuiThemeProvider>,
+  document.querySelector('#root'),
+);
