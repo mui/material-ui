fbd9b0a0d12a8b6e8495e4e706dc111a2160d800
Nikita Perevyazkin <peretz301@gmail.com>
2018-06-25 17:46:24 +0300

    Update DynamicIcons.js

--- docs/src/pages/style/icons/DynamicIcons.js
+++ docs/src/pages/style/icons/DynamicIcons.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import { MenuList, MenuItem, ListItemText, ListItemIcon } from '@material-ui/core/';
-import * as Icons from '@material-ui/icons/';
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import {
+  MenuList,
+  MenuItem,
+  ListItemText,
+  ListItemIcon
+} from "@material-ui/core/";
+import * as Icons from "@material-ui/icons/";
 
 const menu = [
-  { name: 'Notifications', icon: 'Notifications' },
-  { name: 'Create Notification', icon: 'AddAlert' },
+  { id: 0, name: "Notifications", icon: "Notifications" },
+  { id: 1, name: "Create Notification", icon: "AddAlert" }
 ];
 
-function Menu(props) {
+function Menu() {
   return (
     <Paper>
       <MenuList>
-        {menu.map((item, index) => (
-          <MenuItem key={index}>
-            <ListItemIcon >
-              {React.createElement(Icons[item.icon])}
-            </ListItemIcon>
-            <ListItemText inset primary={item.name}/>
+        {menu.map(item => (
+          <MenuItem key={item.id}>
+            <ListItemIcon>{React.createElement(Icons[item.icon])}</ListItemIcon>
+            <ListItemText inset primary={item.name} />
           </MenuItem>
         ))}
       </MenuList>
     </Paper>
-  )
-};
+  );
+}
 export default Menu;
