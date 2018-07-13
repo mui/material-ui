87595258c00b8d49aaf14e5e5dc8745e87e4c4f5
Matt Brookes <github@nospam.33m.co>
2018-07-13 07:33:16 +0100

    [SpeedDial] Inline the Add icon (#12128)

--- packages/material-ui-lab/src/SpeedDialIcon/SpeedDialIcon.js
+++ packages/material-ui-lab/src/SpeedDialIcon/SpeedDialIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
-import AddIcon from '@material-ui/icons/Add';
+import AddIcon from '../internal/svg-icons/Add';
 
 const styles = theme => ({
   root: {
---
+++ packages/material-ui-lab/src/internal/svg-icons/Add.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import pure from 'recompose/pure';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+/**
+ * @ignore - internal component.
+ */
+let Add = props => (
+  <SvgIcon {...props}>
+    <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+  </SvgIcon>
+);
+
+Add = pure(Add);
+Add.muiName = 'SvgIcon';
+
+export default Add;
