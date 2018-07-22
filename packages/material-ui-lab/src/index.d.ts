267beda0067f2d1dbefb4c90b9e724c9be50cbd6
Renée Kooi <renee@kooi.me>
2018-07-22 11:41:15 +0200

    [lab] Build and export fixes (#12233)
    * [lab] Ship es/ files to npm.
    
    * [lab] Export Slider and ToggleButton from entry points.

--- packages/material-ui-lab/src/index.d.ts
+++ packages/material-ui-lab/src/index.d.ts
@@ -1,3 +1,6 @@
+export { default as Slider } from './Slider';
 export { default as SpeedDial } from './SpeedDial';
 export { default as SpeedDialAction } from './SpeedDialAction';
 export { default as SpeedDialIcon } from './SpeedDialIcon';
+export { default as ToggleButton } from './ToggleButton/ToggleButton';
+export { default as ToggleButtonGroup } from './ToggleButton/ToggleButtonGroup';
