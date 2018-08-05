d967671dc9e156afd81de18e14573a06d0faf7e2
pavelpz <pzika@langmaster.cz>
2018-08-05 13:30:25 +0200

    Update Popper.d.ts
    Porting https://github.com/mui-org/material-ui/blob/master/docs/src/pages/demos/autocomplete/IntegrationDownshift.js to typescript does not works in line 281:
    ```
     <Popper open={isOpen} anchorEl={popperNode}>
                  <Paper square style={{ width: popperNode ? popperNode.clientWidth : null }}>
    ```

--- packages/material-ui/src/Popper/Popper.d.ts
+++ packages/material-ui/src/Popper/Popper.d.ts
@@ -20,12 +20,12 @@ export type PopperPlacementType =
 export interface PopperProps extends React.HTMLAttributes<HTMLDivElement> {
   transition?: boolean;
   anchorEl?: null | HTMLElement | ReferenceObject | ((element: HTMLElement) => HTMLElement);
-  children: (
+  children: React.ReactElement<any> | ((
     props: {
       placement: PopperPlacementType;
       TransitionProps?: TransitionProps;
     },
-  ) => React.ReactElement<any>;
+  ) => React.ReactElement<any>);
   container?: PortalProps['container'];
   disablePortal?: PortalProps['disablePortal'];
   keepMounted?: boolean;
