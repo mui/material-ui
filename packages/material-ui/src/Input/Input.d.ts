0a89981308a30f43f39de4b72efeb454e50fa35d
pavelpz <pzika@langmaster.cz>
2018-08-06 11:57:03 +0200

    readOnly?:boolean; => readOnly?: boolean;

--- packages/material-ui/src/Input/Input.d.ts
+++ packages/material-ui/src/Input/Input.d.ts
@@ -23,7 +23,7 @@ export interface InputProps
   multiline?: boolean;
   name?: string;
   placeholder?: string;
-  readOnly?:boolean;
+  readOnly?: boolean;
   required?: boolean;
   rows?: string | number;
   rowsMax?: string | number;
