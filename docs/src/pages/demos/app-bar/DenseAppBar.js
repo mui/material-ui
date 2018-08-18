468b1930f2eeb4352bf565706dec258c723d74aa
Stanley <stanleypl75@gmail.com>
2018-08-18 08:49:50 -0400

    Update DenseAppBar.js
    Modifying the path [ import MenuIcon from '@material-ui/icons/Menu'; ] to[ import MenuIcon from '@material-ui/core/Menu'; ]

--- docs/src/pages/demos/app-bar/DenseAppBar.js
+++ docs/src/pages/demos/app-bar/DenseAppBar.js
@@ -5,7 +5,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from '@material-ui/core/Menu';
 
 const styles = {
   root: {
