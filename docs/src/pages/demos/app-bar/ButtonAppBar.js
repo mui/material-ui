ca41b71fcf3b4014909ea622803299a2f6195bea
Stanley <stanleypl75@gmail.com>
2018-08-18 07:39:43 -0400

    Update The MenuIcon path in ButtonAppBar.js
    [ButtonAppBar.js]
    the path import MenuIcon from '@material-ui/icons/Menu'; throw an error : Module not found: Can't resolve '@material-ui/icons/Menu'
    by changing  the path [ import MenuIcon from '@material-ui/core/Menu'; ] it works.

--- docs/src/pages/demos/app-bar/ButtonAppBar.js
+++ docs/src/pages/demos/app-bar/ButtonAppBar.js
@@ -6,7 +6,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from '@material-ui/core/Menu';
 
 const styles = {
   root: {
