852d04b0ef094a9ed9747022503bec4d3d7be88b
Matt Brookes <github@nospam.33m.co>
2018-08-21 20:37:18 +0100

    [docs] Consistent content height for variable content in Albumn layout example (#12556)

--- docs/src/pages/page-layout-examples/album/Album.js
+++ docs/src/pages/page-layout-examples/album/Album.js
@@ -45,8 +45,16 @@ const styles = theme => ({
   cardGrid: {
     padding: `${theme.spacing.unit * 8}px 0`,
   },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
   cardMedia: {
     paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
   },
   footer: {
     backgroundColor: theme.palette.background.paper,
@@ -103,13 +111,13 @@ function Album(props) {
           <Grid container spacing={40}>
             {cards.map(card => (
               <Grid item key={card} sm={6} md={4} lg={3}>
-                <Card>
+                <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
                     image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
                     title="Image title"
                   />
-                  <CardContent>
+                  <CardContent className={classes.cardContent}>
                     <Typography gutterBottom variant="headline" component="h2">
                       Heading
                     </Typography>
