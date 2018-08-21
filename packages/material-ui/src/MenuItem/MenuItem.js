8fa53c26bb6af525002b722b94981c169a19b963
Loïc ROUCH <loic.rouch@gmail.com>
2018-08-21 02:07:02 +0200

    Removed selected property from MenuItem. Modified doc to specify selected property is from ListItem. Updated test

--- packages/material-ui/src/MenuItem/MenuItem.js
+++ packages/material-ui/src/MenuItem/MenuItem.js
@@ -18,23 +18,20 @@ export const styles = theme => ({
     whiteSpace: 'nowrap',
     paddingLeft: 16,
     paddingRight: 16,
-    '&$selected': {
-      backgroundColor: theme.palette.action.selected,
-    },
   },
   /* Styles applied to the root element if `selected={true}`. */
   selected: {},
 });
 
 function MenuItem(props) {
-  const { classes, className, component, selected, role, ...other } = props;
+  const { classes, className, component, role, ...other } = props;
 
   return (
     <ListItem
       button
       role={role}
       tabIndex={-1}
-      className={classNames(classes.root, { [classes.selected]: selected }, className)}
+      className={classNames(classes.root, className)}
       component={component}
       {...other}
     />
@@ -64,16 +61,11 @@ MenuItem.propTypes = {
    * @ignore
    */
   role: PropTypes.string,
-  /**
-   * Use to apply selected styling.
-   */
-  selected: PropTypes.bool,
 };
 
 MenuItem.defaultProps = {
   component: 'li',
   role: 'menuitem',
-  selected: false,
 };
 
 export default withStyles(styles, { name: 'MuiMenuItem' })(MenuItem);
--- test/regressions/tests/Menu/SimpleMenuList.js
+++ test/regressions/tests/Menu/SimpleMenuList.js
@@ -8,7 +8,6 @@ export default function SimpleMenuList() {
     <Paper elevation={8}>
       <MenuList>
         <MenuItem>Profile</MenuItem>
-        <MenuItem selected>My Account</MenuItem>
         <MenuItem>Logout</MenuItem>
       </MenuList>
     </Paper>
