ef6320e27ebc2ffe36a95896d7ed14a3bb9770ec
adeelibr <adeelimranr@gmail.com>
2018-08-29 00:04:51 +0500

    appbar with search demo

--- docs/src/pages/demos/app-bar/SearchAppBar.js
+++ docs/src/pages/demos/app-bar/SearchAppBar.js
@@ -1,29 +1,118 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+// Material UI
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import Input from '@material-ui/core/Input';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+// Material UI Icons
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
 
-const styles = {
+const styles = theme => ({
   root: {
     flexGrow: 1,
   },
-};
+  grow: {
+    flex: '1 1 auto',
+  },
+  appBar: {
+    transition: theme.transitions.create('width'),
+    '@media print': {
+      position: 'absolute',
+    },
+  },
+  title: {
+    marginLeft: 24,
+    flex: '0 1 auto',
+  },
+  searchContainer: {
+    position: 'relative',
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: theme.spacing.unit,
+    borderRadius: 2,
+    background: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      background: fade(theme.palette.common.white, 0.25),
+    },
+    '& $inputInput': {
+      transition: theme.transitions.create('width'),
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+  },
+});
 
-function SearchAppBar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography variant="title" color="inherit">
-            Photos
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+class SearchAppBar extends Component {
+  state = {
+    searchInput: '',
+  };
+
+  handleChange = event => {
+    this.setState({ searchInput: event.target.value });
+  };
+
+  render() {
+    const { searchInput } = this.state;
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" className={classes.appBar}>
+          <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography className={classes.title} variant="title" color="inherit" noWrap>
+                Material UI
+              </Typography>
+              <div className={classes.grow} />
+              <section className={classes.searchContainer}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <Input
+                  id="search-input"
+                  placeholder="Search…"
+                  value={searchInput}
+                  onChange={this.handleChange}
+                  disableUnderline
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                />
+              </section>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
 SearchAppBar.propTypes = {
