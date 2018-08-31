cb9ac817c601fcd20314cb662184f42d314dca51
adeelibr <adeelimranr@gmail.com>
2018-09-01 00:34:09 +0500

    improved table sorting logic

--- docs/src/pages/demos/tables/EnhancedTable.js
+++ docs/src/pages/demos/tables/EnhancedTable.js
@@ -37,12 +37,11 @@ function desc(a, b, orderBy) {
 
 function stableSort(array, cmp) {
   const stabilizedThis = array.map((el, index) => [el, index]);
-  const stableCmp = (a, b) => {
+  stabilizedThis.sort((a, b) => {
     const order = cmp(a[0], b[0]);
     if (order !== 0) return order;
     return a[1] - b[1];
-  };
-  stabilizedThis.sort(stableCmp);
+  });
   return stabilizedThis.map(el => el[0]);
 }
 
