ebba2203622327a8d9082a1ced54c20c2ea83e0f
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-09-10 09:59:59 +0200

    let's merge

--- packages/material-ui/src/utils/getDisplayName.js
+++ packages/material-ui/src/utils/getDisplayName.js
@@ -3,13 +3,13 @@
 // Simplified polyfill for IE11 support
 // https://github.com/JamesMGreene/Function.name/blob/58b314d4a983110c3682f1228f845d39ccca1817/Function.name.js#L3
 const fnNameMatchRegex = /^\s*function(?:\s|\s*\/\*.*\*\/\s*)+([^(\s/]*)\s*/;
-export const getFunctionName = fn => {
+export function getFunctionName(fn) {
   const match = `${fn}`.match(fnNameMatchRegex);
   const name = match && match[1];
   return name || '';
-};
+}
 
-const getDisplayName = Component => {
+function getDisplayName(Component) {
   if (typeof Component === 'string') {
     return Component;
   }
@@ -19,6 +19,6 @@ const getDisplayName = Component => {
   }
 
   return Component.displayName || Component.name || getFunctionName(Component) || 'Component';
-};
+}
 
 export default getDisplayName;
