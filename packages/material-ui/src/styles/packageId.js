57dd9ad0d623b8d2342c0a7a80324db1b392d598
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-09-11 22:40:34 +0200

    [styles] Remove the packageId

--- packages/material-ui/src/styles/packageId.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-underscore-dangle, no-plusplus */
-
-if (!global.__MUI_PACKAGE_ID__) {
-  global.__MUI_PACKAGE_ID__ = 0;
-}
-
-// One unique value per @material-ui/core package installation.
-export default global.__MUI_PACKAGE_ID__++;
