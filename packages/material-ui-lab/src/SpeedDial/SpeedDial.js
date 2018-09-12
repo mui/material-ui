851e847764ea3a7640c7aca7b8a72f089521d0f3
Matt Brookes <github@nospam.33m.co>
2018-09-12 08:35:41 +0100

    [SpeedDial] Fix ARIA & fix duplicate id in docs example (#12846)

--- packages/material-ui-lab/src/SpeedDial/SpeedDial.js
+++ packages/material-ui-lab/src/SpeedDial/SpeedDial.js
@@ -208,6 +208,7 @@ class SpeedDial extends React.Component {
         </TransitionComponent>
         <div
           id={`${id}-actions`}
+          role="menu"
           className={classNames(
             classes.actions,
             { [classes.actionsClosed]: !open },
