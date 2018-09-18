9fd66391f32fb4272c8feb2204efb8950dfa013e
PutziSan <putzisan@putzisan.de>
2018-09-19 00:33:34 +0200

    [InputLabel] Fix Chrome's autofill (#12926)

--- packages/material-ui/src/InputLabel/InputLabel.js
+++ packages/material-ui/src/InputLabel/InputLabel.js
@@ -39,6 +39,11 @@ export const styles = theme => ({
   },
   /* Styles applied to the root element if `variant="filled"`. */
   filled: {
+    // Chrome's autofill feature gives the input field a yellow background.
+    // Since the input field is behind the label in the HTML tree,
+    // the input field is drawn last and hides the label with an opaque background color.
+    // zIndex: 1 will raise the label above opaque background-colors of input.
+    zIndex: 1,
     transform: 'translate(12px, 22px) scale(1)',
     '&$marginDense': {
       transform: 'translate(12px, 19px) scale(1)',
@@ -52,6 +57,8 @@ export const styles = theme => ({
   },
   /* Styles applied to the root element if `variant="outlined"`. */
   outlined: {
+    // see comment above on filled.zIndex
+    zIndex: 1,
     transform: 'translate(14px, 22px) scale(1)',
     '&$marginDense': {
       transform: 'translate(14px, 17.5px) scale(1)',
