89f6c30fbf38e0ce87cd6bb5554bd460ef343e25
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2018-09-29 12:20:35 +0200

    [StepConnector] Customize connector based on internal states (#13023)
    * [StepConnector] Introduce active, completed, and disabled properties
    
    The properties are used to render classNames for the line element of the
    `StepConnector`.
    
    * [Stepper] Pass active, completed, disabled props to connectors
    
    * [StepConnector] Update api docs
    
    * [Stepper] Use state object for control and connector
    
    * [StepConnector] Remove default boolean state values
    
    * [StepConnector] Apply state classes to root element
    
    * let's merge
    
    * Update steppers.md
    
    * remove regressions

--- docs/src/pages/demos/steppers/CustomizedStepper.js
+++ docs/src/pages/demos/steppers/CustomizedStepper.js
@@ -4,6 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
+import StepConnector from '@material-ui/core/StepConnector';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
@@ -18,6 +19,17 @@ const styles = theme => ({
     marginTop: theme.spacing.unit,
     marginBottom: theme.spacing.unit,
   },
+  connectorActive: {
+    '& $line': {
+      borderColor: theme.palette.secondary.main,
+    },
+  },
+  connectorCompleted: {
+    '& $line': {
+      borderColor: theme.palette.primary.main,
+    },
+  },
+  line: {},
 });
 
 function getSteps() {
@@ -37,27 +49,15 @@ function getStepContent(step) {
   }
 }
 
-class HorizontalLinearStepper extends React.Component {
+class CustomizedStepper extends React.Component {
   state = {
     activeStep: 0,
-    skipped: new Set(),
-  };
-
-  isStepOptional = step => {
-    return step === 1;
   };
 
   handleNext = () => {
-    const { activeStep } = this.state;
-    let { skipped } = this.state;
-    if (this.isStepSkipped(activeStep)) {
-      skipped = new Set(skipped.values());
-      skipped.delete(activeStep);
-    }
-    this.setState({
-      activeStep: activeStep + 1,
-      skipped,
-    });
+    this.setState(state => ({
+      activeStep: state.activeStep + 1,
+    }));
   };
 
   handleBack = () => {
@@ -66,57 +66,36 @@ class HorizontalLinearStepper extends React.Component {
     }));
   };
 
-  handleSkip = () => {
-    const { activeStep } = this.state;
-    if (!this.isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
-      throw new Error("You can't skip a step that isn't optional.");
-    }
-
-    this.setState(state => {
-      const skipped = new Set(state.skipped.values());
-      skipped.add(activeStep);
-      return {
-        activeStep: state.activeStep + 1,
-        skipped,
-      };
-    });
-  };
-
   handleReset = () => {
     this.setState({
       activeStep: 0,
     });
   };
 
-  isStepSkipped(step) {
-    return this.state.skipped.has(step);
-  }
-
   render() {
     const { classes } = this.props;
-    const steps = getSteps();
     const { activeStep } = this.state;
+    const steps = getSteps();
 
     return (
       <div className={classes.root}>
-        <Stepper activeStep={activeStep}>
-          {steps.map((label, index) => {
-            const props = {};
-            const labelProps = {};
-            if (this.isStepOptional(index)) {
-              labelProps.optional = <Typography variant="caption">Optional</Typography>;
-            }
-            if (this.isStepSkipped(index)) {
-              props.completed = false;
-            }
-            return (
-              <Step key={label} {...props}>
-                <StepLabel {...labelProps}>{label}</StepLabel>
-              </Step>
-            );
-          })}
+        <Stepper
+          activeStep={activeStep}
+          connector={
+            <StepConnector
+              classes={{
+                active: classes.connectorActive,
+                completed: classes.connectorCompleted,
+                line: classes.line,
+              }}
+            />
+          }
+        >
+          {steps.map(label => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
         </Stepper>
         <div>
           {activeStep === steps.length ? (
@@ -139,16 +118,6 @@ class HorizontalLinearStepper extends React.Component {
                 >
                   Back
                 </Button>
-                {this.isStepOptional(activeStep) && (
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={this.handleSkip}
-                    className={classes.button}
-                  >
-                    Skip
-                  </Button>
-                )}
                 <Button
                   variant="contained"
                   color="primary"
@@ -166,8 +135,8 @@ class HorizontalLinearStepper extends React.Component {
   }
 }
 
-HorizontalLinearStepper.propTypes = {
+CustomizedStepper.propTypes = {
   classes: PropTypes.object,
 };
 
-export default withStyles(styles)(HorizontalLinearStepper);
+export default withStyles(styles)(CustomizedStepper);
