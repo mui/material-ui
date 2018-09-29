78fd71aef45af9f5e6ebd62febf83edf385b2ded
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-09-29 16:57:06 +0200

    [docs] Improve the Gatsby demo

---
+++ examples/gatsby/gatsby-browser.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types, import/prefer-default-export */
+
+// It's not ready yet: https://github.com/gatsbyjs/gatsby/issues/8237.
+//
+// import React from 'react';
+// import withRoot from './src/withRoot';
+
+// const WithRoot = withRoot(props => props.children);
+
+// export const wrapRootElement = ({ element }) => {
+//   return <WithRoot key={Math.random()}>{element}</WithRoot>;
+// };
