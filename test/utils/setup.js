7d6ac7f0335252d66d97e5b812a6b7d93581b833
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-10-14 01:10:11 +0200

    [core] Benchmark Material-UI (#13233)

--- test/utils/setup.js
+++ test/utils/setup.js
@@ -1,5 +1,7 @@
 const createDOM = require('./createDOM');
 
+process.browser = true;
+
 // eslint-disable-next-line no-underscore-dangle
 global.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
 createDOM();
