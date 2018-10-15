ef193fe80bc6fcceecc7388db9e83e4691dff32d
Kevin Ross <kevin.ross@alienfast.com>
2018-10-15 12:31:26 -0500

    fix test-utils arg types

--- lib/__tests__/test-utils.tsx
+++ lib/__tests__/test-utils.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
 import * as enzyme from 'enzyme';
-import DateFnsUtils from '../src/utils/date-fns-utils';
-import MomentUtils from '../src/utils/moment-utils';
-import LuxonUtils from '../src/utils/luxon-utils';
+import * as React from 'react';
 import { WithUtilsProps } from '../src/_shared/WithUtils';
 import { Utils } from '../src/typings/utils';
+import DateFnsUtils from '../src/utils/date-fns-utils';
+import LuxonUtils from '../src/utils/luxon-utils';
+import MomentUtils from '../src/utils/moment-utils';
 
 const getUtilToUse = (): Utils<any> => {
   switch (process.env.UTILS) {
@@ -21,24 +21,35 @@ const getUtilToUse = (): Utils<any> => {
 
 export const utilsToUse = getUtilToUse();
 
-const getComponentWithUtils = (Component: any) => React.cloneElement(Component, { utils: utilsToUse });
+const getComponentWithUtils = (element: React.ReactElement<any>) =>
+  React.cloneElement(element, { utils: utilsToUse });
 
-export const shallow = (Component: React.ComponentType<WithUtilsProps>) => enzyme.shallow(getComponentWithUtils(Component));
+export const shallow = <P extends WithUtilsProps>(
+  element: React.ReactElement<P>
+) => enzyme.shallow(getComponentWithUtils(element));
 
-export const mount = (Component: React.ComponentType<WithUtilsProps>) => enzyme.mount(getComponentWithUtils(Component));
+export const mount = <P extends WithUtilsProps>(
+  element: React.ReactElement<P>
+) => enzyme.mount(getComponentWithUtils(element));
 
-export const shallowRender = (render: (props: any) => React.ReactElement<any>) => {
-  return enzyme.shallow(render({ utils: utilsToUse, classes: {} as any, theme: {} as any}))
-}
+export const shallowRender = (
+  render: (props: any) => React.ReactElement<any>
+) => {
+  return enzyme.shallow(
+    render({ utils: utilsToUse, classes: {} as any, theme: {} as any })
+  );
+};
 
 jest.doMock('../src/_shared/WithUtils', () => {
   const WithUtils = () => (Component: React.ComponentType<WithUtilsProps>) => {
-    const withUtils: React.SFC<any> = (props) => <Component utils={utilsToUse} {...props} />;
-    withUtils.displayName = `WithUtils(${Component.displayName || Component.name})`;
+    const withUtils: React.SFC<any> = props => (
+      <Component utils={utilsToUse} {...props} />
+    );
+    withUtils.displayName = `WithUtils(${Component.displayName ||
+      Component.name})`;
 
     return withUtils;
   };
 
   return { default: WithUtils };
 });
-
