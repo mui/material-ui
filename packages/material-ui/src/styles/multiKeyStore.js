c346a87d241e8b2fa74d07cf2fc4300dff0897fb
Minh Nguyen <minhnguyenxx@gmail.com>
2018-10-15 13:30:13 +0100

    [core] Export multiKeyStore as functions to reduce bundle size

--- packages/material-ui/src/styles/multiKeyStore.js
+++ packages/material-ui/src/styles/multiKeyStore.js
@@ -1,24 +1,22 @@
 // Used https://github.com/thinkloop/multi-key-cache as inspiration
 
-const multiKeyStore = {
-  set: (cache, key1, key2, value) => {
-    let subCache = cache.get(key1);
+export function set(cache, key1, key2, value) {
+  let subCache = cache.get(key1);
 
-    if (!subCache) {
-      subCache = new Map();
-      cache.set(key1, subCache);
-    }
+  if (!subCache) {
+    subCache = new Map();
+    cache.set(key1, subCache);
+  }
 
-    subCache.set(key2, value);
-  },
-  get: (cache, key1, key2) => {
-    const subCache = cache.get(key1);
-    return subCache ? subCache.get(key2) : undefined;
-  },
-  delete: (cache, key1, key2) => {
-    const subCache = cache.get(key1);
-    subCache.delete(key2);
-  },
-};
+  subCache.set(key2, value);
+}
 
-export default multiKeyStore;
+export function get(cache, key1, key2) {
+  const subCache = cache.get(key1);
+  return subCache ? subCache.get(key2) : undefined;
+}
+
+export function remove(cache, key1, key2) {
+  const subCache = cache.get(key1);
+  subCache.delete(key2);
+}
