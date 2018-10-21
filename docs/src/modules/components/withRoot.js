4bf3e475197558b08dfaa2ee35ca05c24d52a704
Matthew Brookes <github@nospam.33m.co>
2018-10-21 19:34:48 +0100

    [docs] Reinstate Chinese language support

--- docs/src/modules/components/withRoot.js
+++ docs/src/modules/components/withRoot.js
@@ -9,7 +9,7 @@ import findPages from /* preval */ 'docs/src/modules/utils/findPages';
 import { loadCSS } from 'fg-loadcss/src/loadCSS';
 import acceptLanguage from 'accept-language';
 
-acceptLanguage.languages(['en']);
+acceptLanguage.languages(['en', 'zh']);
 
 if (process.browser) {
   loadCSS(
