a424f2b45a04c272639ad0b217d243e0af469614
Daniel Kohli <drkohlipk@gmail.com>
2018-10-21 13:51:45 -0400

    [FormControl] Reset stacking context (#13327)
    * [FormControl] Added zIndex of 0 to root style
    
    * Update FormControl.js

--- packages/material-ui/src/FormControl/FormControl.js
+++ packages/material-ui/src/FormControl/FormControl.js
@@ -18,6 +18,7 @@ export const styles = {
     margin: 0,
     border: 0,
     verticalAlign: 'top', // Fix alignment issue on Safari.
+    zIndex: 0, // Reset the stacking context for the label z-index.
   },
   /* Styles applied to the root element if `margin="normal"`. */
   marginNormal: {
