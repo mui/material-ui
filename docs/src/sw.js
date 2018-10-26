a1da841133c09dde0a9d36d2fd1515642317a5a0
Michael Siadak <mike.siadak@gmail.com>
2018-10-25 21:41:46 -0600

    Update Workbox to v3.6.3

--- docs/src/sw.js
+++ docs/src/sw.js
@@ -1,7 +1,7 @@
 /* eslint-env serviceworker */
 /* global workbox */
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-beta.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
 workbox.core.setCacheNameDetails({
   // This allows you to work on multiple projects using
