e1e438fec90210db5ac1eb7fd65b6a6a24580c98
Bogdan Chadkin <trysound@yandex.ru>
2018-10-28 19:24:18 +0300

    Format files

--- packages/material-ui/src/utils/commonjsGlobal.js
+++ packages/material-ui/src/utils/commonjsGlobal.js
@@ -1,3 +1,10 @@
-const commonjsGlobal = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
+const commonjsGlobal =
+  typeof window !== 'undefined'
+    ? window
+    : typeof global !== 'undefined'
+      ? global
+      : typeof self !== 'undefined'
+        ? self
+        : {};
 
 export default commonjsGlobal;
