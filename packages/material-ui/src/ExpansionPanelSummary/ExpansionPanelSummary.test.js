52f44609f6f4897512dee80ca063d888e3ee8f05
jmetev1 <jmetevier@gmail.com>
2018-11-13 01:40:22 -0800

    [ExpansionPanelSummary] Fix event forwarding (#13582)
    * expansions summary handleblur now calls onblur
    
    * let's merge

--- packages/material-ui/src/ExpansionPanelSummary/ExpansionPanelSummary.test.js
+++ packages/material-ui/src/ExpansionPanelSummary/ExpansionPanelSummary.test.js
@@ -54,10 +54,10 @@ describe('<ExpansionPanelSummary />', () => {
     assert.strictEqual(iconWrap.hasClass(classes.expandIcon), true);
   });
 
-  it('handleFocus() should set focused state', () => {
+  it('handleFocusVisible() should set focused state', () => {
     const eventMock = 'woofExpansionPanelSummary';
     const wrapper = mount(<ExpansionPanelSummaryNaked classes={{}} />);
-    wrapper.instance().handleFocus(eventMock);
+    wrapper.instance().handleFocusVisible(eventMock);
     assert.strictEqual(wrapper.state().focused, true);
   });
 
@@ -67,6 +67,25 @@ describe('<ExpansionPanelSummary />', () => {
     wrapper.setState({ focused: true });
     wrapper.instance().handleBlur(eventMock);
     assert.strictEqual(wrapper.state().focused, false);
+  });
+
+  describe('event callbacks', () => {
+    it('should fire event callbacks', () => {
+      const events = ['onClick', 'onFocusVisible', 'onBlur'];
+
+      const handlers = events.reduce((result, n) => {
+        result[n] = spy();
+        return result;
+      }, {});
+
+      const wrapper = shallow(<ExpansionPanelSummary {...handlers} />);
+
+      events.forEach(n => {
+        const event = n.charAt(2).toLowerCase() + n.slice(3);
+        wrapper.simulate(event, { persist: () => {} });
+        assert.strictEqual(handlers[n].callCount, 1, `should have called the ${n} handler`);
+      });
+    });
   });
 
   describe('prop: onChange', () => {
