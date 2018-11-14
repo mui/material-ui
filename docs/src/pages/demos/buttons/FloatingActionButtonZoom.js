3f14716c2748adcfa50bd14162a78aef55f0880c
adeelibr <adeelimranr@gmail.com>
2018-11-14 23:49:37 +0500

    updated buttons demo

--- docs/src/pages/demos/buttons/FloatingActionButtonZoom.js
+++ docs/src/pages/demos/buttons/FloatingActionButtonZoom.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import SwipeableViews from 'react-swipeable-views';
@@ -47,86 +47,82 @@ const styles = theme => ({
   },
 });
 
-class FloatingActionButtonZoom extends React.Component {
-  state = {
-    value: 0,
-  };
+function FloatingActionButtonZoom(props) {
+  const { classes, theme } = props;
+  const [value, setValue] = useState(0);
 
-  handleChange = (event, value) => {
-    this.setState({ value });
-  };
-
-  handleChangeIndex = index => {
-    this.setState({ value: index });
-  };
-
-  render() {
-    const { classes, theme } = this.props;
-    const transitionDuration = {
-      enter: theme.transitions.duration.enteringScreen,
-      exit: theme.transitions.duration.leavingScreen,
-    };
-
-    const fabs = [
-      {
-        color: 'primary',
-        className: classes.fab,
-        icon: <AddIcon />,
-      },
-      {
-        color: 'secondary',
-        className: classes.fab,
-        icon: <EditIcon />,
-      },
-      {
-        color: 'inherit',
-        className: classNames(classes.fab, classes.fabGreen),
-        icon: <UpIcon />,
-      },
-    ];
-
-    return (
-      <div className={classes.root}>
-        <AppBar position="static" color="default">
-          <Tabs
-            value={this.state.value}
-            onChange={this.handleChange}
-            indicatorColor="primary"
-            textColor="primary"
-            fullWidth
-          >
-            <Tab label="Item One" />
-            <Tab label="Item Two" />
-            <Tab label="Item Three" />
-          </Tabs>
-        </AppBar>
-        <SwipeableViews
-          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-          index={this.state.value}
-          onChangeIndex={this.handleChangeIndex}
-        >
-          <TabContainer dir={theme.direction}>Item One</TabContainer>
-          <TabContainer dir={theme.direction}>Item Two</TabContainer>
-          <TabContainer dir={theme.direction}>Item Three</TabContainer>
-        </SwipeableViews>
-        {fabs.map((fab, index) => (
-          <Zoom
-            key={fab.color}
-            in={this.state.value === index}
-            timeout={transitionDuration}
-            style={{
-              transitionDelay: `${this.state.value === index ? transitionDuration.exit : 0}ms`,
-            }}
-            unmountOnExit
-          >
-            <Button variant="fab" className={fab.className} color={fab.color}>
-              {fab.icon}
-            </Button>
-          </Zoom>
-        ))}
-      </div>
-    );
+  function handleChange(event, newValue) {
+    setValue(newValue);
   }
+
+  function handleChangeIndex(index) {
+    setValue(index);
+  }
+
+  const transitionDuration = {
+    enter: theme.transitions.duration.enteringScreen,
+    exit: theme.transitions.duration.leavingScreen,
+  };
+
+  const fabs = [
+    {
+      color: 'primary',
+      className: classes.fab,
+      icon: <AddIcon />,
+    },
+    {
+      color: 'secondary',
+      className: classes.fab,
+      icon: <EditIcon />,
+    },
+    {
+      color: 'inherit',
+      className: classNames(classes.fab, classes.fabGreen),
+      icon: <UpIcon />,
+    },
+  ];
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          fullWidth
+        >
+          <Tab label="Item One" />
+          <Tab label="Item Two" />
+          <Tab label="Item Three" />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabContainer dir={theme.direction}>Item One</TabContainer>
+        <TabContainer dir={theme.direction}>Item Two</TabContainer>
+        <TabContainer dir={theme.direction}>Item Three</TabContainer>
+      </SwipeableViews>
+      {fabs.map((fab, index) => (
+        <Zoom
+          key={fab.color}
+          in={value === index}
+          timeout={transitionDuration}
+          style={{
+            transitionDelay: `${value === index ? transitionDuration.exit : 0}ms`,
+          }}
+          unmountOnExit
+        >
+          <Button variant="fab" className={fab.className} color={fab.color}>
+            {fab.icon}
+          </Button>
+        </Zoom>
+      ))}
+    </div>
+  );
 }
 
 FloatingActionButtonZoom.propTypes = {
