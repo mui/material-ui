57bdb92ac2869752c851b2e72dccb70c681f7dd0
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-11-19 23:48:32 +0100

    docs use _app.js

--- docs/src/modules/components/bootstrap.js
+++ docs/src/modules/components/bootstrap.js
@@ -1,3 +1,10 @@
 import { install } from '@material-ui/styles';
 
+// Disable auto highlighting
+// https://github.com/PrismJS/prism/issues/765
+if (process.browser) {
+  window.Prism = window.Prism || {};
+  window.Prism.manual = true;
+}
+
 install();
