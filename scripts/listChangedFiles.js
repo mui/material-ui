2074ae82ebf157d7649b0381e126fc94a3baac62
joshwooding <12938082+joshwooding@users.noreply.github.com>
2018-11-20 00:27:44 +0000

    [core] Made more recommended changed

--- scripts/listChangedFiles.js
+++ scripts/listChangedFiles.js
@@ -2,30 +2,36 @@
 
 // Based on similar script in React
 // https://github.com/facebook/react/blob/b87aabdfe1b7461e7331abb3601d9e6bb27544bc/scripts/shared/listChangedFiles.js
-import { execFileSync } from 'child_process';
+import { promisify } from 'util';
+import { execFile } from 'child_process';
 
-const exec = (command, args) => {
+const execFileAsync = promisify(execFile);
+
+const exec = async (command, args) => {
   const options = {
     cwd: process.cwd(),
     env: process.env,
     stdio: 'pipe',
     encoding: 'utf-8',
   };
-  return execFileSync(command, args, options);
+
+  const results = await execFileAsync(command, args, options);
+  return results.stdout;
 };
 
-const execGitCmd = args =>
-  exec('git', args)
+const execGitCmd = async args => {
+  const gitResults = await exec('git', args);
+  return gitResults
     .trim()
     .toString()
     .split('\n');
+};
 
-const listChangedFiles = () => {
-  const mergeBase = execGitCmd(['merge-base', 'HEAD', 'master']);
-  return new Set([
-    ...execGitCmd(['diff', '--name-only', '--diff-filter=ACMRTUB', mergeBase]),
-    ...execGitCmd(['ls-files', '--others', '--exclude-standard']),
-  ]);
+const listChangedFiles = async () => {
+  const mergeBase = await execGitCmd(['rev-parse', 'origin/master']);
+  const gitDiff = await execGitCmd(['diff', '--name-only'].concat(mergeBase));
+  const gitLs = await execGitCmd(['ls-files', '--others', '--exclude-standard']);
+  return new Set([...gitDiff, ...gitLs]);
 };
 
 module.exports = listChangedFiles;
--- scripts/prettier.js
+++ scripts/prettier.js
@@ -6,62 +6,98 @@
 import glob from 'glob';
 import prettier from 'prettier';
 import fs from 'fs';
-import os from 'os';
 import path from 'path';
 import listChangedFiles from './listChangedFiles';
 
 const prettierConfigPath = path.join(__dirname, '../prettier.config.js');
 
-const mode = process.argv[2] || 'check';
+const mode = process.argv[2] || 'write-changed';
 const shouldWrite = mode === 'write' || mode === 'write-changed';
 const onlyChanged = mode === 'check-changed' || mode === 'write-changed';
 
-const changedFiles = onlyChanged ? listChangedFiles() : null;
-let didWarn = false;
-let didError = false;
+function mapEslintEntriesToGlobs(file) {
+  let prefix = '';
+  let hasSlashPrefix = false;
+  let suffix = '';
 
-const ignoredFiles = fs
-  .readFileSync('.eslintignore', 'utf-8')
-  .split(os.EOL)
-  .filter(notEmpty => notEmpty)
-  .map(file => `**/${file}/**`);
+  if (file.charAt(0) === '/') {
+    hasSlashPrefix = true;
+  } else {
+    prefix = '**/';
+  }
 
-const files = glob
-  .sync('**/*.{js,tsx,d.ts}', { ignore: ['**/node_modules/**', ...ignoredFiles] })
-  .filter(f => !onlyChanged || changedFiles.has(f));
-
-if (!files.length) {
-  process.exit(0);
+  if (!path.extname(file)) {
+    suffix = '/**';
+  }
+  console.log(`${prefix}${hasSlashPrefix ? file.substring(1) : file}${suffix}`);
+  return `${prefix}${hasSlashPrefix ? file.substring(1) : file}${suffix}`;
 }
 
-files.forEach(file => {
-  const options = prettier.resolveConfig.sync(file, {
-    config: prettierConfigPath,
-  });
-  try {
-    const input = fs.readFileSync(file, 'utf8');
-    if (shouldWrite) {
-      console.log(`Formatting ${file}`);
-      const output = prettier.format(input, { ...options, filepath: file });
-      if (output !== input) {
-        fs.writeFileSync(file, output, 'utf8');
+function runPrettier(changedFiles) {
+  let didWarn = false;
+  let didError = false;
+
+  const warnedFiles = [];
+  const ignoredFiles = fs
+    .readFileSync('.eslintignore', 'utf-8')
+    .split(/\r*\n/)
+    .filter(notEmpty => notEmpty)
+    .map(file => mapEslintEntriesToGlobs(file));
+
+  const files = glob
+    .sync('**/*.{js,tsx,d.ts}', { ignore: ['**/node_modules/**', ...ignoredFiles] })
+    .filter(f => !changedFiles || changedFiles.has(f));
+
+  if (!files.length) {
+    process.exit(0);
+  }
+
+  files.forEach(file => {
+    const options = prettier.resolveConfig.sync(file, {
+      config: prettierConfigPath,
+    });
+    try {
+      const input = fs.readFileSync(file, 'utf8');
+      if (shouldWrite) {
+        console.log(`Formatting ${file}`);
+        const output = prettier.format(input, { ...options, filepath: file });
+        if (output !== input) {
+          fs.writeFileSync(file, output, 'utf8');
+        }
+      } else {
+        console.log(`Checking ${file}`);
+        if (!prettier.check(input, { ...options, filepath: file })) {
+          warnedFiles.push(file);
+          didWarn = true;
+        }
       }
-    } else if (!prettier.check(input, { ...options, filepath: file })) {
-      if (!didWarn) {
-        console.log(`\nThis project uses prettier to format all JavaScript code.
-           Please run 'yarn prettier-all' and add changes to files listed
-           below to your commit:\n\n`);
-        didWarn = true;
-      }
+    } catch (error) {
+      didError = true;
+      console.log(`\n\n${error.message}`);
       console.log(file);
     }
-  } catch (error) {
-    didError = true;
-    console.log(`\n\n${error.message}`);
-    console.log(file);
-  }
-});
+  });
 
-if (didWarn || didError) {
-  process.exit(1);
+  if (didWarn) {
+    console.log(
+      '\n\nThis project uses prettier to format all JavaScript code.\n' +
+        `Please run '${!changedFiles ? 'yarn prettier:all' : 'yarn prettier'}'` +
+        'and commit the changes to the files listed below:\n\n',
+    );
+    console.log(warnedFiles.join('\n'));
+  }
+
+  if (didWarn || didError) {
+    process.exit(1);
+  }
+}
+
+if (onlyChanged) {
+  listChangedFiles()
+    .then(changedFiles => {
+      runPrettier(changedFiles);
+    })
+    .catch(console.error);
+} else {
+  runPrettier();
 }
