82e7509936a8b2dc7d39727433bf39aaf6aeb08b
smithgraeme <30961431+smithgraeme@users.noreply.github.com>
2018-11-22 11:53:37 -0700

    Add some spacing between the ListItems
    No functional changes. Just add some whitespace so it's to visually separate different things in this list.

--- docs/src/pages/getting-started/page-layout-examples/dashboard/listItems.js
+++ docs/src/pages/getting-started/page-layout-examples/dashboard/listItems.js
@@ -16,30 +16,38 @@ export const mainListItems = (
       <ListItemIcon>
         <DashboardIcon />
       </ListItemIcon>
+  
       <ListItemText primary="Dashboard" />
     </ListItem>
+  
     <ListItem button>
       <ListItemIcon>
         <ShoppingCartIcon />
       </ListItemIcon>
+  
       <ListItemText primary="Orders" />
     </ListItem>
+  
     <ListItem button>
       <ListItemIcon>
         <PeopleIcon />
       </ListItemIcon>
+  
       <ListItemText primary="Customers" />
     </ListItem>
+  
     <ListItem button>
       <ListItemIcon>
         <BarChartIcon />
       </ListItemIcon>
       <ListItemText primary="Reports" />
     </ListItem>
+  
     <ListItem button>
       <ListItemIcon>
         <LayersIcon />
       </ListItemIcon>
+  
       <ListItemText primary="Integrations" />
     </ListItem>
   </div>
@@ -52,18 +60,23 @@ export const secondaryListItems = (
       <ListItemIcon>
         <AssignmentIcon />
       </ListItemIcon>
+  
       <ListItemText primary="Current month" />
     </ListItem>
+  
     <ListItem button>
       <ListItemIcon>
         <AssignmentIcon />
       </ListItemIcon>
+  
       <ListItemText primary="Last quarter" />
     </ListItem>
+  
     <ListItem button>
       <ListItemIcon>
         <AssignmentIcon />
       </ListItemIcon>
+  
       <ListItemText primary="Year-end sale" />
     </ListItem>
   </div>
