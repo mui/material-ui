021f8a7269b3f4484cc876db9dcfa1b9d9f546a7
Wiktoria Tomzik <wiktoria.tomzik@gmail.com>
2018-11-23 22:31:24 +0100

    [docs] Change &quote; to &apos; (#13678)
    * Change &quote; to &apos;
    
    * fix all the occurrences

--- docs/src/pages/demos/steppers/CustomizedStepper.js
+++ docs/src/pages/demos/steppers/CustomizedStepper.js
@@ -114,7 +114,7 @@ class CustomizedStepper extends React.Component {
           {activeStep === steps.length ? (
             <div>
               <Typography className={classes.instructions}>
-                All steps completed - you&quot;re finished
+                All steps completed - you&apos;re finished
               </Typography>
               <Button onClick={this.handleReset} className={classes.button}>
                 Reset
--- docs/src/pages/demos/steppers/HorizontalLinearStepper.js
+++ docs/src/pages/demos/steppers/HorizontalLinearStepper.js
@@ -122,7 +122,7 @@ class HorizontalLinearStepper extends React.Component {
           {activeStep === steps.length ? (
             <div>
               <Typography className={classes.instructions}>
-                All steps completed - you&quot;re finished
+                All steps completed - you&apos;re finished
               </Typography>
               <Button onClick={this.handleReset} className={classes.button}>
                 Reset
