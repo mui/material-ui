072959c1e1f18f8135a16899c0f93d641dacf8dd
Octave Raimbault <me@dispix.net>
2018-11-27 00:03:33 +0100

    [Dialog] Add xl maxWidth and demo component (#13694)
    * [Dialog] Add xl maxWidth and demo component
    
    * let's merge

--- packages/material-ui/src/Dialog/Dialog.d.ts
+++ packages/material-ui/src/Dialog/Dialog.d.ts
@@ -9,7 +9,7 @@ export interface DialogProps
   children?: React.ReactNode;
   fullScreen?: boolean;
   fullWidth?: boolean;
-  maxWidth?: 'xs' | 'sm' | 'md' | 'lg' | false;
+  maxWidth?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | false;
   PaperProps?: Partial<PaperProps>;
   scroll?: 'body' | 'paper';
   TransitionComponent?: React.ReactType;
