356b14955ad4cbe64fe1f0fb26f94b96274cdab1
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2018-11-26 22:41:40 +0200

    Make next transpile material-ui-pickers module to handle webpack aliases

--- docs/src/pages/demos/pickers/MaterialUIPickers.js
+++ docs/src/pages/demos/pickers/MaterialUIPickers.js
@@ -1,13 +1,15 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import withStyles from '@material-ui/styles/withStyles';
 import DateFnsUtils from '@date-io/date-fns';
-import {
-  MuiPickersUtilsProvider,
-  TimePicker,
-  DatePicker,
-  DateTimePicker,
-} from 'material-ui-pickers';
+import { MuiPickersUtilsProvider, TimePicker, DatePicker } from 'material-ui-pickers';
 
-export default class App extends PureComponent {
+class MuiPickersDemo extends PureComponent {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+
   state = {
     selectedDate: new Date(),
   };
@@ -17,14 +19,24 @@ export default class App extends PureComponent {
   };
 
   render() {
+    const { classes } = this.props;
     const { selectedDate } = this.state;
 
     return (
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <DatePicker value={selectedDate} onChange={this.handleDateChange} />
-        <TimePicker value={selectedDate} onChange={this.handleDateChange} />
-        <DateTimePicker value={selectedDate} onChange={this.handleDateChange} />
+        <Grid container className={classes.pickersContainer} justify="space-around">
+          <DatePicker value={selectedDate} onChange={this.handleDateChange} />
+          <TimePicker value={selectedDate} onChange={this.handleDateChange} />
+        </Grid>
       </MuiPickersUtilsProvider>
     );
   }
 }
+
+const styles = {
+  pickersContainer: {
+    width: '60%',
+  },
+};
+
+export default withStyles(styles)(MuiPickersDemo);
