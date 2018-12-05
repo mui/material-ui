07ba0e7fda2ea4bb88eef1232439db834a89d358
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-12-05 01:03:23 +0100

    add a regression test

--- packages/material-ui/src/Modal/ModalManager.js
+++ packages/material-ui/src/Modal/ModalManager.js
@@ -50,6 +50,7 @@ function setContainerStyle(data) {
 }
 
 function removeContainerStyle(data) {
+  // The modal might be closed before it had the chance to be mounted in the DOM.
   if (data.style) {
     Object.keys(data.style).forEach(key => {
       data.container.style[key] = data.style[key];
