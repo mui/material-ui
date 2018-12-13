fc2be231ff20662e9039becaf270f5434579b3ca
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-12-13 11:45:01 +0100

    Revert "[test] Temporary run regression tests with @material-ui/styles"
    This reverts commit 0e1a67a5290d5dc41adcd8aed890e61e71961cd3.
    
    Could not figure out how to use next styles

--- test/utils/init.js
+++ test/utils/init.js
@@ -1,4 +1,3 @@
-import { install as installNextStyles } from '@material-ui/styles';
 import enzyme from 'enzyme/build/index';
 import Adapter from 'enzyme-adapter-react-16';
 import consoleError from './consoleError';
@@ -6,5 +5,3 @@ import consoleError from './consoleError';
 consoleError();
 
 enzyme.configure({ adapter: new Adapter() });
-
-installNextStyles();
