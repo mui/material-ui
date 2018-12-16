e3eda66ec2796f859b2dcb0b458185a33c3fc42e
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-12-16 17:17:22 +0100

    [docs] Improve the state override story

--- packages/material-ui/src/styles/createMuiTheme.js
+++ packages/material-ui/src/styles/createMuiTheme.js
@@ -49,6 +49,46 @@ function createMuiTheme(options = {}) {
     ),
   };
 
+  if (process.env.NODE_ENV !== 'production') {
+    const statesWarning = ['disabled', 'focused', 'selected', 'checked'];
+    const traverse = (node, parentKey, depth = 1) => {
+      let key;
+
+      // eslint-disable-next-line guard-for-in, no-restricted-syntax
+      for (key in node) {
+        const child = node[key];
+        if (depth === 1) {
+          if (key.indexOf('Mui') === 0 && child) {
+            traverse(child, key, depth + 1);
+          }
+        } else if (statesWarning.indexOf(key) !== -1 && Object.keys(child).length > 0) {
+          warning(
+            false,
+            [
+              `Material-UI: the \`${parentKey}\` component increases ` +
+                `the CSS specificity of the \`${key}\` internal state.`,
+              'You can not override it like this: ',
+              JSON.stringify(node, null, 2),
+              '',
+              'Instead, you need to use the $ruleName syntax:',
+              JSON.stringify(
+                {
+                  [`&$${key}`]: child,
+                },
+                null,
+                2,
+              ),
+              '',
+              'https://material-ui.com/customization/overrides#internal-states',
+            ].join('\n'),
+          );
+        }
+      }
+    };
+
+    traverse(other.overrides);
+  }
+
   warning(
     muiTheme.shadows.length === 25,
     'Material-UI: the shadows array provided to createMuiTheme should support 25 elevations.',
