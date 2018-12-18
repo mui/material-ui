2d37b8d9ecef01a5f74e40a628309bf880c707f9
Belciug Ovidiu-Mihai <ovidiumihaibelciug@gmail.com>
2018-12-18 22:01:17 +0200

    [docs] Fix Typo in BottomNavigationAction label (#13943)

--- docs/src/pages/demos/bottom-navigation/SimpleBottomNavigation.hooks.js
+++ docs/src/pages/demos/bottom-navigation/SimpleBottomNavigation.hooks.js
@@ -25,7 +25,7 @@ function SimpleBottomNavigation() {
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction label="Recentss" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
       <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
       <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
     </BottomNavigation>
