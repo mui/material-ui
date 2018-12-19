e76e337d771b06e9f73d32263c6c2143f68dcb38
adeelibr <adeelimranr@gmail.com>
2018-12-20 00:46:38 +0500

    replaces manual demo prop with webpack raw-loader

--- pages/demos/app-bar.js
+++ pages/demos/app-bar.js
@@ -5,61 +5,32 @@ import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
 
 const req = require.context('markdown', true, /.md$/);
 
+const parseAll = ({ fileNameSuffix, js, raw }) => {
+  const cache = {};
+  const rawKeys = raw.keys();
+  js.keys().forEach((key, index) => {
+    const fileName = `${fileNameSuffix}${key.split('./')[1]}`;
+    cache[fileName] = {
+      js: js(key).default,
+      raw: raw(rawKeys[index]),
+    };
+  });
+  return cache;
+};
+
 function Page(props) {
   return (
     <MarkdownDocs
       markdown={req(`./app-bar${props.lang}.md`)}
-      demos={{
-        'pages/demos/app-bar/SimpleAppBar.js': {
-          js: require('docs/src/pages/demos/app-bar/SimpleAppBar').default,
-          raw: preval`
-module.exports = require('fs')
-  .readFileSync(require.resolve('docs/src/pages/demos/app-bar/SimpleAppBar'), 'utf8')
-`,
-        },
-        'pages/demos/app-bar/ButtonAppBar.js': {
-          js: require('docs/src/pages/demos/app-bar/ButtonAppBar').default,
-          raw: preval`
-module.exports = require('fs')
-  .readFileSync(require.resolve('docs/src/pages/demos/app-bar/ButtonAppBar'), 'utf8')
-`,
-        },
-        'pages/demos/app-bar/MenuAppBar.js': {
-          js: require('docs/src/pages/demos/app-bar/MenuAppBar').default,
-          raw: preval`
-module.exports = require('fs')
-  .readFileSync(require.resolve('docs/src/pages/demos/app-bar/MenuAppBar'), 'utf8')
-`,
-        },
-        'pages/demos/app-bar/DenseAppBar.js': {
-          js: require('docs/src/pages/demos/app-bar/DenseAppBar').default,
-          raw: preval`
-module.exports = require('fs')
-  .readFileSync(require.resolve('docs/src/pages/demos/app-bar/DenseAppBar'), 'utf8')
-`,
-        },
-        'pages/demos/app-bar/SearchAppBar.js': {
-          js: require('docs/src/pages/demos/app-bar/SearchAppBar').default,
-          raw: preval`
-module.exports = require('fs')
-  .readFileSync(require.resolve('docs/src/pages/demos/app-bar/SearchAppBar'), 'utf8')
-`,
-        },
-        'pages/demos/app-bar/PrimarySearchAppBar.js': {
-          js: require('docs/src/pages/demos/app-bar/PrimarySearchAppBar').default,
-          raw: preval`
-module.exports = require('fs')
-  .readFileSync(require.resolve('docs/src/pages/demos/app-bar/PrimarySearchAppBar'), 'utf8')
-`,
-        },
-        'pages/demos/app-bar/BottomAppBar.js': {
-          js: require('docs/src/pages/demos/app-bar/BottomAppBar').default,
-          raw: preval`
-module.exports = require('fs')
-  .readFileSync(require.resolve('docs/src/pages/demos/app-bar/BottomAppBar'), 'utf8')
-`,
-        },
-      }}
+      demos={parseAll({
+        fileNameSuffix: 'pages/demos/app-bar/',
+        js: require.context('docs/src/pages/demos/app-bar/', true, /\.js$/),
+        raw: require.context(
+          '!raw-loader!../../docs/src/pages/demos/app-bar',
+          true,
+          /.js$/,
+        ),
+      })}
     />
   );
 }
