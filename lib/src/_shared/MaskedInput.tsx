99151511a955600fbda6cd48ea2b69d38865af27
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2018-12-21 13:12:51 +0200

    [#784] 🚀 Fix console error when clicking out of masked input

--- lib/src/_shared/MaskedInput.tsx
+++ lib/src/_shared/MaskedInput.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import MaskedInput, { MaskedInputProps } from 'react-text-mask';
 
 export interface CustomMaskedInputProps extends MaskedInputProps {
-  mask?: any;
+  mask?: MaskedInputProps['mask'];
   inputRef: React.Ref<any>;
 }
 
@@ -17,10 +17,16 @@ export default class Input extends React.PureComponent<CustomMaskedInputProps> {
     mask: undefined,
   };
 
+  public createInputRef = (ref: MaskedInput | null) => {
+    // @ts-ignore masked input really has input element in the component instance
+    this.props.inputRef(ref ? ref.inputElement : null);
+  };
+
   public render() {
     const { inputRef, keepCharPositions, ...rest } = this.props;
+
     return this.props.mask ? (
-      <MaskedInput {...rest} keepCharPositions={keepCharPositions} ref={inputRef} />
+      <MaskedInput {...rest} ref={this.createInputRef} keepCharPositions={keepCharPositions} />
     ) : (
       <input {...rest} ref={inputRef} />
     );
