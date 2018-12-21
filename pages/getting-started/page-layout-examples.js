c81f77f4975374cab65020d796b8541f76cad1c2
adeelibr <adeelimranr@gmail.com>
2018-12-21 19:58:13 +0500

    set useSubdirectories=false for require.context in getting-started/page-layout-examples

--- pages/getting-started/page-layout-examples.js
+++ pages/getting-started/page-layout-examples.js
@@ -5,17 +5,18 @@ import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
 
 const req = require.context(
   'docs/src/pages/getting-started/page-layout-examples',
-  true,
+  false,
   /\.md|\.js$/,
 );
 const reqSource = require.context(
   '!raw-loader!../../docs/src/pages/getting-started/page-layout-examples',
-  true,
+  false,
   /\.js$/,
 );
+const reqPrefix = 'pages/getting-started/page-layout-examples';
 
 function Page() {
-  return <MarkdownDocs req={req} reqSource={reqSource} />;
+  return <MarkdownDocs req={req} reqSource={reqSource} reqPrefix={reqPrefix} />;
 }
 
 export default Page;
