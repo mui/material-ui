5f48bde637e36f98748f0bf182de96ac28871655
Rui Botto <rui.botto@eversports.com>
2018-12-24 08:06:36 +0000

    [Fab] Add styles to make size property work with extended property (#13973)
    * [Fab] Add styles to make size property work with extended property
    
    * Increase core modules size limit

--- docs/src/pages/demos/buttons/ButtonSizes.js
+++ docs/src/pages/demos/buttons/ButtonSizes.js
@@ -6,10 +6,14 @@ import Fab from '@material-ui/core/Fab';
 import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
+import NavigationIcon from '@material-ui/icons/Navigation';
 
 const styles = theme => ({
   margin: {
     margin: theme.spacing.unit,
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
   },
 });
 
@@ -59,6 +63,32 @@ function ButtonSizes(props) {
         </Fab>
         <Fab color="secondary" aria-label="Add" className={classes.margin}>
           <AddIcon />
+        </Fab>
+      </div>
+      <div>
+        <Fab
+          variant="extended"
+          size="small"
+          color="primary"
+          aria-label="Add"
+          className={classes.margin}
+        >
+          <NavigationIcon className={classes.extendedIcon} />
+          Extended
+        </Fab>
+        <Fab
+          variant="extended"
+          size="medium"
+          color="primary"
+          aria-label="Add"
+          className={classes.margin}
+        >
+          <NavigationIcon className={classes.extendedIcon} />
+          Extended
+        </Fab>
+        <Fab variant="extended" color="primary" aria-label="Add" className={classes.margin}>
+          <NavigationIcon className={classes.extendedIcon} />
+          Extended
         </Fab>
       </div>
       <div>
