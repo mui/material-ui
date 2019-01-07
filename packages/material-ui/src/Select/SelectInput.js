367f3c06dcf24fd694b77fd5d535e959c3fcbd9b
Dallin Christensen <christensen.dallin@gmail.com>
2019-01-07 00:37:38 -0800

    [MenuList] Wrap focus by default, add disableListWrap (#14100)
    * MenuList now wraps by default, gave disableListWrap prop for users to opt-out, updated docs accordingly
    
    * integration tests added and modified as needed
    
    * integration tests modified and added for MenuList component
    
    * docs updated using yarn docs:api
    
    * [Select] Disable ListWrap

--- packages/material-ui/src/Select/SelectInput.js
+++ packages/material-ui/src/Select/SelectInput.js
@@ -318,6 +318,7 @@ class SelectInput extends React.Component {
           {...MenuProps}
           MenuListProps={{
             role: 'listbox',
+            disableListWrap: true,
             ...MenuProps.MenuListProps,
           }}
           PaperProps={{
