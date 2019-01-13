56a054c641b82e111632caa27a8f753fca30f8f9
Valley <valley.zw@gmail.com>
2019-01-13 17:52:25 +0800

    [docs] Add a new site to showcase (#14163)
    * Add a new site to showcase
    
    * fix react warning
    
    * ImageOptim
    
    I forgot to run it on the last batch, so sneaking it in here...

--- docs/src/pages/discover-more/showcase/Showcase.js
+++ docs/src/pages/discover-more/showcase/Showcase.js
@@ -593,6 +593,15 @@ const appList = [
     stars: 36,
   },
   {
+    title: 'Tree',
+    description:
+      'An open source top 100 documentaries (personal opinion) app ' +
+      'with React Hooks and Material-UI.',
+    link: 'https://tree.valleyease.me/',
+    image: 'tree.jpg',
+    source: 'https://github.com/ValleyZw/tree',
+  },
+  {
     title: 'Material Blog',
     description:
       'An open source blog with a UI built entirely using material-ui v1. ' +
@@ -637,7 +646,7 @@ function Showcase(props) {
   return (
     <div className={classes.root}>
       {appList.map(app => (
-        <div key={app.image}>
+        <div key={app.title}>
           <Typography component="h2" variant="h4" gutterBottom className={classes.title}>
             <span>{app.title}</span>
             {app.source ? (
