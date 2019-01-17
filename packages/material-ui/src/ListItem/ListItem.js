f153908432e3c54d8622b7e42e2cfd0c0c83a27a
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-01-17 09:06:44 +0100

    Remove unrelated change
    At least the rationale for this change wasn't explained.

--- packages/material-ui/src/ListItem/ListItem.js
+++ packages/material-ui/src/ListItem/ListItem.js
@@ -23,9 +23,6 @@ export const styles = theme => ({
     '&$selected, &$selected:hover': {
       backgroundColor: theme.palette.action.selected,
     },
-    '&:focus': {
-      backgroundColor: theme.palette.action.hover,
-    },
   },
   /* Styles applied to the `container` element if `children` includes `ListItemSecondaryAction`. */
   container: {
