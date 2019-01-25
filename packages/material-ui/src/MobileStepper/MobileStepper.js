aab481cb999d74c4cafe892bc09139e8045157b1
Deniz Susman <39295979+DenrizSusam@users.noreply.github.com>
2019-01-25 11:39:42 +0300

    nextButton comment fix
    "...For instance, it can be be a `Button` or a `IconButton`." should be "For instance, it can be a `Button` or an `IconButton`".

--- packages/material-ui/src/MobileStepper/MobileStepper.js
+++ packages/material-ui/src/MobileStepper/MobileStepper.js
@@ -130,7 +130,7 @@ MobileStepper.propTypes = {
    */
   LinearProgressProps: PropTypes.object,
   /**
-   * A next button element. For instance, it can be be a `Button` or a `IconButton`.
+   * A next button element. For instance, it can be a `Button` or an `IconButton`.
    */
   nextButton: PropTypes.node,
   /**
