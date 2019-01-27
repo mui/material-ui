b0e8b8e50f1e87841c48391f6883f13813d28f19
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-01-27 14:46:44 +0100

    [docs] Translate the key wordings (#14317)

--- docs/src/modules/redux/optionsReducer.js
+++ docs/src/modules/redux/optionsReducer.js
@@ -1,15 +1,24 @@
 import { ACTION_TYPES, CODE_VARIANTS } from 'docs/src/modules/constants';
+import translations from 'docs/src/modules/translations';
+import memoize from '@material-ui/system/memoize';
+
+const getT = memoize(userLanguage => key => translations[userLanguage][key]);
 
 const mapping = {
-  [ACTION_TYPES.OPTIONS_CHANGE]: (state, action) => ({
-    codeVariant: action.payload.codeVariant || state.codeVariant,
-    userLanguage: action.payload.userLanguage || state.userLanguage,
-  }),
+  [ACTION_TYPES.OPTIONS_CHANGE]: (state, action) => {
+    const newState = {
+      codeVariant: action.payload.codeVariant || state.codeVariant,
+      userLanguage: action.payload.userLanguage || state.userLanguage,
+    };
+    newState.t = getT(newState.userLanguage);
+    return newState;
+  },
 };
 
 const initialState = {
   codeVariant: CODE_VARIANTS.JS,
   userLanguage: 'en',
+  t: getT('en'),
 };
 
 function optionsReducer(state = initialState, action) {
