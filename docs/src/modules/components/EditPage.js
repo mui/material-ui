f7b2c477a6f9cded3740edcd57d0c265587089f7
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-01-27 14:33:30 +0100

    [docs] Translate the key wordings

--- docs/src/modules/components/EditPage.js
+++ docs/src/modules/components/EditPage.js
@@ -4,35 +4,30 @@ import Button from '@material-ui/core/Button';
 import { connect } from 'react-redux';
 
 function EditPage(props) {
-  const { markdownLocation, options, sourceCodeRootUrl } = props;
-
-  if (options.userLanguage === 'zh') {
-    return (
-      <Button component="a" href="https://translate.material-ui.com/">
-        {'将此页面翻译成中文'}
-      </Button>
-    );
-  }
-
-  if (options.userLanguage === 'pt') {
-    return (
-      <Button component="a" href="https://translate.material-ui.com/">
-        {'Ajude a traduzir esta página'}
-      </Button>
-    );
-  }
+  const { markdownLocation, sourceCodeRootUrl, t, userLanguage } = props;
 
   return (
-    <Button component="a" href={`${sourceCodeRootUrl}${markdownLocation}`}>
-      {'Edit this page'}
+    <Button
+      component="a"
+      href={
+        userLanguage === 'en'
+          ? `${sourceCodeRootUrl}${markdownLocation}`
+          : 'https://translate.material-ui.com/'
+      }
+    >
+      {t('editPage')}
     </Button>
   );
 }
 
 EditPage.propTypes = {
   markdownLocation: PropTypes.string.isRequired,
-  options: PropTypes.object.isRequired,
   sourceCodeRootUrl: PropTypes.string.isRequired,
+  t: PropTypes.func.isRequired,
+  userLanguage: PropTypes.string.isRequired,
 };
 
-export default connect(state => ({ options: state.options }))(EditPage);
+export default connect(state => ({
+  t: state.options.t,
+  userLanguage: state.options.userLanguage,
+}))(EditPage);
