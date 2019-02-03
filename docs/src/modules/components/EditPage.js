7ef5752c390cce8617e91026a474a32d81af8bf8
Matthew Brookes <github@nospam.33m.co>
2019-02-03 15:16:23 +0000

    [docs] Targeted edit button URL
    We can't link directly to the page to be edited, as the CrowdIn URL for the file is just a number, but I noticed that we can target the containing directory.
    
    Also added ga attributes.

--- docs/src/modules/components/EditPage.js
+++ docs/src/modules/components/EditPage.js
@@ -3,8 +3,13 @@ import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import { connect } from 'react-redux';
 
+const LANGUAGES = { zh: 'zh-CN', pt: 'pt-BZ', es: 'es-ES' };
+const CROWDIN_ROOT_URL = 'https://translate.material-ui.com/project/material-ui-docs/';
+
 function EditPage(props) {
   const { markdownLocation, sourceCodeRootUrl, t, userLanguage } = props;
+  const crowdInLanguage = LANGUAGES[userLanguage] || userLanguage;
+  const crowdInPath = markdownLocation.substring(0, markdownLocation.lastIndexOf('/'));
 
   return (
     <Button
@@ -12,8 +17,13 @@ function EditPage(props) {
       href={
         userLanguage === 'en'
           ? `${sourceCodeRootUrl}${markdownLocation}`
-          : 'https://translate.material-ui.com/'
+          : `${CROWDIN_ROOT_URL}${crowdInLanguage}#/master${crowdInPath}`
       }
+      target="_blank"
+      rel="noopener"
+      data-ga-event-category="l10n"
+      data-ga-event-action="edit"
+      data-ga-event-label={userLanguage}
     >
       {t('editPage')}
     </Button>
