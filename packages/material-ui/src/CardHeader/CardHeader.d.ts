239b4b3cdab28b8bf57960318ccdf21688ee76be
Mathias Schreck <schreck.mathias@googlemail.com>
2019-02-11 16:27:48 +0100

    Fix CardHeader props typings
    When I provide a string for the `component` prop of `CardHeader` I get an error form typescript:
    
    ```
    Type 'string' is not assignable to type 'ComponentClass<CardHeaderProps, any> | FunctionComponent<CardHeaderProps> | undefined'.
    ```
    
    The [documentation](https://material-ui.com/api/card-header/) says that strings should be allowed for the prop.

--- packages/material-ui/src/CardHeader/CardHeader.d.ts
+++ packages/material-ui/src/CardHeader/CardHeader.d.ts
@@ -6,7 +6,7 @@ export interface CardHeaderProps
   extends StandardProps<React.HTMLAttributes<HTMLDivElement>, CardHeaderClassKey, 'title'> {
   action?: React.ReactNode;
   avatar?: React.ReactNode;
-  component?: React.ReactType<CardHeaderProps>;
+  component?: string | React.ReactType<CardHeaderProps>;
   disableTypography?: boolean;
   subheader?: React.ReactNode;
   subheaderTypographyProps?: Partial<TypographyProps>;
