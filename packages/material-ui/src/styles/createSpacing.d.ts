c653b6a310c3a8ca43e1be20f97272e8bbb51d17
Matsumoto Toshi <32378535+toshi1127@users.noreply.github.com>
2019-02-15 20:29:26 +0900

    [typescript] Fix theme.spacing to accept up to 4 arguments (#14539)
    Closes #14515

--- packages/material-ui/src/styles/createSpacing.d.ts
+++ packages/material-ui/src/styles/createSpacing.d.ts
@@ -1,4 +1,19 @@
-export type Spacing = (value: number | string) => number | string;
+/* tslint:disable:unified-signatures */
+
+export type SpacingArgument = number | string;
+export type SpacingReturnType = number | string;
+
+export interface Spacing {
+  (value1: SpacingArgument): SpacingReturnType;
+  (value1: SpacingArgument, value2: SpacingArgument): SpacingReturnType;
+  (value1: SpacingArgument, value2: SpacingArgument, value3: SpacingArgument): SpacingReturnType;
+  (
+    value1: SpacingArgument,
+    value2: SpacingArgument,
+    value3: SpacingArgument,
+    value4: SpacingArgument,
+  ): SpacingReturnType;
+}
 
 export type SpacingOptions = number | Spacing;
 
