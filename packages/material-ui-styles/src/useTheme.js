5db5ee392acae1530a0a7b78eac2f9acfbc66cad
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-02-19 23:26:31 +0100

    remove _ as suggested by Sebastian

--- packages/material-ui-styles/src/useTheme.js
+++ packages/material-ui-styles/src/useTheme.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import warning from 'warning';
 import ThemeContext from './ThemeContext';
 
-export default function useTheme(_) {
-  warning(!_, "Material-UI: useTheme() doesn't accept any argument.");
+export default function useTheme() {
   return React.useContext(ThemeContext);
 }
