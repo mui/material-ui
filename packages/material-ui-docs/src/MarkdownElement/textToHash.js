4fa02fc7242b773c78d89c257a3c09c42054cc72
Matthew Brookes <github@nospam.33m.co>
2019-02-19 13:16:42 +0000

    make textToHash output consistent with current implementation

--- packages/material-ui-docs/src/MarkdownElement/textToHash.js
+++ packages/material-ui-docs/src/MarkdownElement/textToHash.js
@@ -4,8 +4,8 @@ export default function textToHash(text) {
       .toLowerCase()
       .replace(/=&gt;|&lt;| \/&gt;|<code>|<\/code>|&#39;/g, '')
       // eslint-disable-next-line no-useless-escape
-      .replace(/[!@#\$%\^&\*\(\)=_\+\[\]{}`~;:'"\|,\.<>\/\?]+/g, '')
-      .replace(/[\s-]+/g, '-')
+      .replace(/[!@#\$%\^&\*\(\)=_\+\[\]{}`~;:'"\|,\.<>\/\?\s]+/g, '-')
+      .replace(/-+/g, '-')
       .replace(/-$/g, ''),
   );
 }
--- packages/material-ui-docs/src/MarkdownElement/textToHash.test.js
+++ packages/material-ui-docs/src/MarkdownElement/textToHash.test.js
@@ -5,7 +5,7 @@ describe('textToHash', () => {
   it('should hash correctly', () => {
     assert.strictEqual(
       textToHash('createMuiTheme(options) => theme'),
-      'createmuithemeoptions-theme',
+      'createmuitheme-options-theme',
     );
     assert.strictEqual(textToHash('Typography - Font family'), 'typography-font-family');
     assert.strictEqual(textToHash('barre d&#39;application'), 'barre-dapplication');
