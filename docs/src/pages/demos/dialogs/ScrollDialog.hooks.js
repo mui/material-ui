15e5d5d343a58cc2f280945912f74d5674155809
Lukas Liesis <liesislukas@gmail.com>
2019-02-21 12:44:37 +0200

    more content for the dialog demo with scrollbar
    dialog demo of scrollbar with 4K display doesn't show the scrollbar

--- docs/src/pages/demos/dialogs/ScrollDialog.hooks.js
+++ docs/src/pages/demos/dialogs/ScrollDialog.hooks.js
@@ -67,6 +67,68 @@ function ScrollDialog() {
             commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel
             augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed
             consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec
+            sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Donec
+            sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
+            consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas
+            eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent
+            commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel
+            augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed
+            consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec
+            sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
+            consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas
+            eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent
+            commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel
+            augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed
+            consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec
+            sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
+            consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas
+            eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent
+            commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel
+            augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed
+            consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec
+            sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
+            consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas
+            eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent
+            commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel
+            augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed
+            consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec
+            sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
+            consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas
+            eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent
+            commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel
+            augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed
+            consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec
+            sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Donec
+            sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
+            consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas
+            eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent
+            commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel
+            augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed
+            consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec
+            sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
+            consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas
+            eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent
+            commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel
+            augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed
+            consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec
+            sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
+            consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas
+            eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent
+            commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel
+            augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed
+            consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec
+            sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
+            consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas
+            eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent
+            commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel
+            augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed
+            consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec
+            sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
+            consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas
+            eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent
+            commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel
+            augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed
+            consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec
             sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
           </DialogContentText>
         </DialogContent>
