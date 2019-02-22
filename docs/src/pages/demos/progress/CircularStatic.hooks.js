d3bdef79b73e624ba50bdc83584789387b3eee8c
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-02-22 16:40:26 +0100

    [docs] Linter appeasement

--- docs/src/pages/demos/progress/CircularStatic.hooks.js
+++ docs/src/pages/demos/progress/CircularStatic.hooks.js
@@ -12,11 +12,11 @@ function CircularStatic() {
   const classes = useStyles();
   const [completed, setCompleted] = React.useState(0);
 
-  function progress() {
-    setCompleted(prevCompleted => (prevCompleted >= 100 ? 0 : prevCompleted + 10));
-  }
-
   React.useEffect(() => {
+    function progress() {
+      setCompleted(prevCompleted => (prevCompleted >= 100 ? 0 : prevCompleted + 10));
+    }
+
     const timer = setInterval(progress, 1000);
     return () => {
       clearInterval(timer);
