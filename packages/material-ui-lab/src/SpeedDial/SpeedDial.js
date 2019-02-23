0740f1b8f64633f9445e996dfa1fdb776cb16b7a
Howard Burrows <howard.burrows@gmail.com>
2019-02-23 07:50:18 -0800

    Add color property for passing to Fab component

--- packages/material-ui-lab/src/SpeedDial/SpeedDial.js
+++ packages/material-ui-lab/src/SpeedDial/SpeedDial.js
@@ -160,6 +160,7 @@ class SpeedDial extends React.Component {
       children: childrenProp,
       classes,
       className: classNameProp,
+      color: colorProp,
       hidden,
       icon: iconProp,
       onClick,
@@ -246,7 +247,7 @@ class SpeedDial extends React.Component {
           {...TransitionProps}
         >
           <Fab
-            color="primary"
+            color={colorProp}
             onKeyDown={this.handleKeyboardNavigation}
             aria-label={ariaLabel}
             aria-haspopup="true"
@@ -304,6 +305,10 @@ SpeedDial.propTypes = {
    */
   className: PropTypes.string,
   /**
+   * The color of the Fab component. It supports those theme colors that make sense for the Fab component.
+   */
+  color: PropTypes.oneOf(['default', 'inherit', 'primary', 'secondary']),
+  /**
    * The direction the actions open relative to the floating action button.
    */
   direction: PropTypes.oneOf(['up', 'down', 'left', 'right']),
@@ -358,6 +363,7 @@ SpeedDial.propTypes = {
 };
 
 SpeedDial.defaultProps = {
+  color: 'primary',
   hidden: false,
   direction: 'up',
   TransitionComponent: Zoom,
