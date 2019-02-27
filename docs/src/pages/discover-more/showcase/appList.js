463f42435fbe591419b8d3a9bd8b1b9f79f36422
johnvincentio <john@johnvincent.io>
2019-02-27 10:26:39 -0500

    prettier cleanup

--- docs/src/pages/discover-more/showcase/appList.js
+++ docs/src/pages/discover-more/showcase/appList.js
@@ -689,15 +689,15 @@ const appList = [
     link: 'https://persona.im/',
     image: 'persona.jpg',
     index: 0,
-	},
+  },
   {
     title: 'TaskMuncher',
     description:
       'TaskMuncher is a responsive MERN stack Task Management Productivity Web based application.' +
-      'TaskMuncher is the easiest way to get it done, whether you’re making a shopping list, ' + 
+      'TaskMuncher is the easiest way to get it done, whether you’re making a shopping list, ' +
       'planning a holiday or managing multiple work projects. ' +
-			'Use TaskMuncher to organize and keep track of everything so you can get it all done ' +
-			'and enjoy more peace of mind.',
+      'Use TaskMuncher to organize and keep track of everything so you can get it all done ' +
+      'and enjoy more peace of mind.',
     link: 'https://taskmuncher.com',
     image: 'taskmuncher.jpg',
     index: 30,
