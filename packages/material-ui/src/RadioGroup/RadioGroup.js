0292dcd10b3fe47ccaa58d222144210fd65927cc
Matthias Sieber <matthiasksieber@gmail.com>
2019-03-13 18:00:28 -0700

    moved warning into componentDidUpdate

--- packages/material-ui/src/RadioGroup/RadioGroup.js
+++ packages/material-ui/src/RadioGroup/RadioGroup.js
@@ -14,17 +14,20 @@ class RadioGroup extends React.Component {
     this.isControlled = props.value != null;
 
     if (!this.isControlled) {
-      warning(
-        this.isControlled,
-        [
-          'Material-UI: the RadioGroup component should not be uncontrolled.',
-          'Consider providing an empty string instead of null as an initial value.',
-        ].join('\n'),
-      );
       this.state = {
         value: props.defaultValue,
       };
     }
+  }
+
+  componentDidUpdate() {
+    warning(
+      this.isControlled,
+      [
+        'Material-UI: the RadioGroup component should not be uncontrolled.',
+        'Consider providing an empty string instead of null as an initial value.',
+      ].join('\n'),
+    );
   }
 
   focus = () => {
