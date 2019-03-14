78318f26eaf56418d37d64b91892c7ab8003ffec
Giulio Gallerini <giulio.gallerini.work@hotmail.com>
2019-03-14 13:15:18 +0100

    remove dynamic progress

--- docs/src/pages/demos/progress/CustomizedProgress.js
+++ docs/src/pages/demos/progress/CustomizedProgress.js
@@ -52,24 +52,6 @@ const styles = theme => ({
 
 function CustomizedProgress(props) {
   const { classes } = props;
-  const [completed, setCompleted] = React.useState(0);
-
-  React.useEffect(() => {
-    function progress() {
-      setCompleted(oldCompleted => {
-        if (oldCompleted === 100) {
-          return 0;
-        }
-        const diff = Math.random() * 10;
-        return Math.min(oldCompleted + diff, 100);
-      });
-    }
-
-    const timer = setInterval(progress, 500);
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
 
   return (
     <Paper className={classes.root}>
@@ -83,7 +65,7 @@ function CustomizedProgress(props) {
       <LinearProgress
         variant="determinate"
         color="secondary"
-        value={completed}
+        value={50}
         classes={{
           bar: classes.linearProgressDeterminateBar,
           root: classes.linearProgressDeterminate,
