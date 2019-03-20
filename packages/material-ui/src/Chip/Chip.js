f8939fa39973304cc4b548f76b6dac30f682852f
Pearce Keesling <pearce.keesling@jolt.com>
2019-03-20 15:46:03 -0600

    [Chip] ran prettier

--- packages/material-ui/src/Chip/Chip.js
+++ packages/material-ui/src/Chip/Chip.js
@@ -260,8 +260,13 @@ const Chip = React.forwardRef(function Chip(props, ref) {
     }
 
     const key = event.key;
-    if (key === ' ' || key === 'Enter' || key === 'Backspace'
-      || key === 'Delete' || key === 'Escape') {
+    if (
+      key === ' ' ||
+      key === 'Enter' ||
+      key === 'Backspace' ||
+      key === 'Delete' ||
+      key === 'Escape'
+    ) {
       event.preventDefault();
     }
   };
