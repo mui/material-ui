8c82f462d424a39737578c9d5234717043721880
Vitaly Kondratiev <vitalkond@gmail.com>
2019-03-20 07:19:22 +0000

    [docs] Add PaperSheet TypeScript demo (#14952)
    <!-- Thanks so much for your PR, your contribution is appreciated! ❤️ -->
    
    - [x] I have followed (at least) the [PR section of the contributing guide](https://github.com/mui-org/material-ui/blob/next/CONTRIBUTING.md#submitting-a-pull-request).

---
+++ docs/src/pages/demos/paper/PaperSheet.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, Theme, createStyles, WithStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      ...theme.mixins.gutters(),
+      paddingTop: theme.spacing(2),
+      paddingBottom: theme.spacing(2),
+    },
+  });
+
+function PaperSheet(props: WithStyles<typeof styles>) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <Paper className={classes.root} elevation={1}>
+        <Typography variant="h5" component="h3">
+          This is a sheet of paper.
+        </Typography>
+        <Typography component="p">
+          Paper can be used to build surface or other elements for your application.
+        </Typography>
+      </Paper>
+    </div>
+  );
+}
+
+PaperSheet.propTypes = {
+  classes: PropTypes.object.isRequired,
+} as any;
+
+export default withStyles(styles)(PaperSheet);
