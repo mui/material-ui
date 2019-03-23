7ee2a08f579498b1d5c1089270940d420900a141
Caroline Roeder <38064741+caroe233@users.noreply.github.com>
2019-03-23 10:46:52 +0100

    [Tab] Enable generic props (#15003)
    * Changed the Tab component to work with generic props (see #13868)
    
    * successfully run yarn prettier with the correct config
    
    * successfully ran yarn typescript

--- packages/material-ui/src/Tab/Tab.d.ts
+++ packages/material-ui/src/Tab/Tab.d.ts
@@ -1,19 +1,23 @@
 import * as React from 'react';
-import { StandardProps } from '..';
-import { ButtonBaseProps } from '../ButtonBase';
+import { ExtendButtonBase } from '../ButtonBase';
+import { SimplifiedPropsOf } from '../OverridableComponent';
 
-export interface TabProps extends StandardProps<ButtonBaseProps, TabClassKey, 'onChange'> {
-  disabled?: boolean;
-  fullWidth?: boolean;
-  icon?: string | React.ReactElement;
-  value?: any;
-  label?: React.ReactNode;
-  onChange?: (event: React.ChangeEvent<{ checked: boolean }>, value: any) => void;
-  onClick?: React.EventHandler<any>;
-  selected?: boolean;
-  style?: React.CSSProperties;
-  textColor?: string | 'secondary' | 'primary' | 'inherit';
-}
+declare const Tab: ExtendButtonBase<{
+  props: {
+    disabled?: boolean;
+    fullWidth?: boolean;
+    icon?: string | React.ReactElement;
+    value?: any;
+    label?: React.ReactNode;
+    onChange?: (event: React.ChangeEvent<{ checked: boolean }>, value: any) => void;
+    onClick?: React.EventHandler<any>;
+    selected?: boolean;
+    style?: React.CSSProperties;
+    textColor?: string | 'secondary' | 'primary' | 'inherit';
+  };
+  defaultComponent: 'div';
+  classKey: TabClassKey;
+}>;
 
 export type TabClassKey =
   | 'root'
@@ -29,6 +33,6 @@ export type TabClassKey =
   | 'label'
   | 'labelWrapped';
 
-declare const Tab: React.ComponentType<TabProps>;
+export type TabProps = SimplifiedPropsOf<typeof Tab>;
 
 export default Tab;
