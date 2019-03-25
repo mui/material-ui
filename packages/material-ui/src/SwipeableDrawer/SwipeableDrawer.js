da2b87bc89eb130a2b77233ed717b1a0fb02aaf3
Maik Marschner <maik@maik.tk>
2019-03-25 22:22:48 +0100

    [SwipeableDrawer] Ignore open swipe if it didn't start on the swipe area (#15045)
    * [SwipeableDrawer] Ignore open swipe if it didn't start on the swipe area
    
    * Update the tests
    
    * Lint the code, fix cherry-picking regression

--- packages/material-ui/src/SwipeableDrawer/SwipeableDrawer.js
+++ packages/material-ui/src/SwipeableDrawer/SwipeableDrawer.js
@@ -30,6 +30,8 @@ class SwipeableDrawer extends React.Component {
 
   isSwiping = null;
 
+  swipeAreaRef = React.createRef();
+
   paperRef = null;
 
   componentDidMount() {
@@ -155,7 +157,7 @@ class SwipeableDrawer extends React.Component {
         : event.touches[0].clientY;
 
     if (!open) {
-      if (disableSwipeToOpen) {
+      if (disableSwipeToOpen || event.target !== this.swipeAreaRef.current) {
         return;
       }
       if (isHorizontal(this.props)) {
@@ -387,9 +389,14 @@ class SwipeableDrawer extends React.Component {
           anchor={anchor}
           {...other}
         />
-        {!disableDiscovery && !disableSwipeToOpen && variant === 'temporary' && (
+        {!disableSwipeToOpen && variant === 'temporary' && (
           <NoSsr>
-            <SwipeArea anchor={anchor} width={swipeAreaWidth} {...SwipeAreaProps} />
+            <SwipeArea
+              anchor={anchor}
+              innerRef={this.swipeAreaRef}
+              width={swipeAreaWidth}
+              {...SwipeAreaProps}
+            />
           </NoSsr>
         )}
       </React.Fragment>
