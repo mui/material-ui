73a6550e3ee9d32ba553008d029ee525b0f2ff21
Mark Fayngersh <mark.fayngersh@gmail.com>
2019-03-25 10:50:58 -0400

    [Box] Update BoxProps to allow any props
    - BoxProps are currently too limiting and will throw errors when using any styled props, such as `ml` or `fontWeight`
    - This change temporarily allows any props to be used with Box while more comprehensive types are being developed

--- packages/material-ui/src/Box/Box.d.ts
+++ packages/material-ui/src/Box/Box.d.ts
@@ -4,6 +4,7 @@ export interface BoxProps extends React.HTMLAttributes<HTMLElement> {
   component?: React.ElementType;
   // styled API
   clone?: boolean;
+  [styledProp: string]: any;
 }
 
 declare const Box: React.ComponentType<BoxProps>;
