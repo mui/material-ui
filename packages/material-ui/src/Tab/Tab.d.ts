68de5f07c7f0d25766427070e118a9f5635b46cc
sperry94 <setherperry@gmail.com>
2019-03-25 21:17:12 -0600

    [docs] Add TS demos for Tabs components

--- packages/material-ui/src/Tab/Tab.d.ts
+++ packages/material-ui/src/Tab/Tab.d.ts
@@ -7,6 +7,7 @@ declare const Tab: ExtendButtonBase<{
     disabled?: boolean;
     fullWidth?: boolean;
     icon?: string | React.ReactElement;
+    indicator?: React.ReactNode;
     value?: any;
     label?: React.ReactNode;
     onChange?: (event: React.ChangeEvent<{ checked: boolean }>, value: any) => void;
@@ -14,6 +15,7 @@ declare const Tab: ExtendButtonBase<{
     selected?: boolean;
     style?: React.CSSProperties;
     textColor?: string | 'secondary' | 'primary' | 'inherit';
+    wrapped?: boolean;
   };
   defaultComponent: 'div';
   classKey: TabClassKey;
