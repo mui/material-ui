3921a411ca422612d28f726b3fcf7c16c83b80f7
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-03-26 15:45:07 +0100

    [AppBar] Test conformance

--- packages/material-ui/src/AppBar/AppBar.test.js
+++ packages/material-ui/src/AppBar/AppBar.test.js
@@ -1,34 +1,42 @@
 import React from 'react';
 import { assert } from 'chai';
-import { createShallow, getClasses } from '@material-ui/core/test-utils';
+import {
+  createMount,
+  createShallow,
+  describeConformance,
+  getClasses,
+} from '@material-ui/core/test-utils';
 import AppBar from './AppBar';
+import Paper from '../Paper';
 
 describe('<AppBar />', () => {
+  let mount;
   let shallow;
   let classes;
 
   before(() => {
+    mount = createMount();
     shallow = createShallow({ dive: true });
     classes = getClasses(<AppBar>Hello World</AppBar>);
   });
 
-  it('should render a Paper component', () => {
-    const wrapper = shallow(<AppBar>Hello World</AppBar>);
-    assert.strictEqual(wrapper.props().elevation, 4);
+  after(() => {
+    mount.cleanUp();
   });
+
+  describeConformance(<AppBar>Conformance?</AppBar>, () => ({
+    classes,
+    inheritComponent: Paper,
+    mount,
+    refInstanceof: window.HTMLElement,
+    testComponentPropWith: false,
+  }));
 
   it('should render with the root class and primary', () => {
     const wrapper = shallow(<AppBar>Hello World</AppBar>);
     assert.strictEqual(wrapper.hasClass(classes.root), true);
     assert.strictEqual(wrapper.hasClass(classes.colorPrimary), true);
     assert.strictEqual(wrapper.hasClass(classes.colorSecondary), false);
-  });
-
-  it('should render the custom className and the appBar class', () => {
-    const wrapper = shallow(<AppBar className="test-class-name">Hello World</AppBar>);
-    assert.strictEqual(wrapper.is('.test-class-name'), true);
-    assert.strictEqual(wrapper.hasClass(classes.root), true);
-    assert.strictEqual(wrapper.hasClass(classes.colorPrimary), true);
   });
 
   it('should render a primary app bar', () => {
