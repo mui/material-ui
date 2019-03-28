9febe8aa92aae7abef437e0fc9d56db1830fe081
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-03-28 22:21:14 +0100

    [Typography] Less opinionated

--- docs/src/pages/demos/cards/RecipeReviewCard.js
+++ docs/src/pages/demos/cards/RecipeReviewCard.js
@@ -72,7 +72,7 @@ function RecipeReviewCard() {
         title="Paella dish"
       />
       <CardContent>
-        <Typography component="p">
+        <Typography variant="body2" color="textSecondary" component="p">
           This impressive paella is a perfect party dish and a fun meal to cook together with your
           guests. Add 1 cup of frozen peas along with the mussels, if you like.
         </Typography>
