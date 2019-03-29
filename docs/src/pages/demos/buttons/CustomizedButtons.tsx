9c23c9eb937a083a779694c5dc9c79b9ed442f93
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-03-29 15:53:53 +0100

    [docs] Migrate FloatingActionButtons to hooks

--- docs/src/pages/demos/buttons/CustomizedButtons.tsx
+++ docs/src/pages/demos/buttons/CustomizedButtons.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { createMuiTheme, createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
