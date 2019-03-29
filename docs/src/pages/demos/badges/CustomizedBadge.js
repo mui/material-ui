4489a3fcd3e1dc2576ea70b59a45ea8aad4c11ab
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-03-29 15:34:45 +0100

    [docs] Migrate DotBadge JS to hooks from docs:typescript:formatted

--- docs/src/pages/demos/badges/CustomizedBadge.js
+++ docs/src/pages/demos/badges/CustomizedBadge.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
 import { makeStyles } from '@material-ui/core/styles';
