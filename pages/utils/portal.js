cc95e55cb9a83c8733138af9405fd52665d96f8e
ralv <5287233+ralvs@users.noreply.github.com>
2019-03-31 06:36:06 -0300

    [docs] SimplePortal example using Hooks (#15125)
    * SimplePortal example using Hooks
    
    * migrate to typescript too

--- pages/utils/portal.js
+++ pages/utils/portal.js
@@ -4,7 +4,11 @@ import React from 'react';
 import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
 
 const req = require.context('docs/src/pages/utils/portal', false, /\.md|\.js$/);
-const reqSource = require.context('!raw-loader!../../docs/src/pages/utils/portal', false, /\.js$/);
+const reqSource = require.context(
+  '!raw-loader!../../docs/src/pages/utils/portal',
+  false,
+  /\.(js|tsx)$/,
+);
 const reqPrefix = 'pages/utils/portal';
 
 function Page() {
