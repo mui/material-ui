9743cd6f790e6ae9efe6b45e536468eff07430ab
joshwooding <12938082+joshwooding@users.noreply.github.com>
2019-04-01 21:07:40 +0100

    [docs] Add virtualized list example

---
+++ docs/src/pages/demos/lists/VirtualizedList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { FixedSizeList } from 'react-window';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    height: 400,
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+function Row(props) {
+  const { index, style } = props;
+
+  return (
+    <ListItem button style={style} key={index}>
+      <ListItemText primary={`Item ${index + 1}`} />
+    </ListItem>
+  );
+}
+
+function VirtualizedList() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <FixedSizeList height={400} width={360} itemSize={46} itemCount={200}>
+        {Row}
+      </FixedSizeList>
+    </div>
+  );
+}
+
+export default VirtualizedList;
