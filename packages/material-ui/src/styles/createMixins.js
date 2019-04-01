aa9840cd032787efa9bfe4763c13e7aabcafea4b
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-04-01 08:36:43 +0200

    [theme] Deprecate theme.mixins.gutters (#15124)

--- packages/material-ui/src/styles/createMixins.js
+++ packages/material-ui/src/styles/createMixins.js
@@ -1,15 +1,36 @@
+// import warning from 'warning';
+
 export default function createMixins(breakpoints, spacing, mixins) {
   return {
-    gutters: (styles = {}) => ({
-      paddingLeft: spacing(2),
-      paddingRight: spacing(2),
-      ...styles,
-      [breakpoints.up('sm')]: {
-        paddingLeft: spacing(3),
-        paddingRight: spacing(3),
-        ...styles[breakpoints.up('sm')],
-      },
-    }),
+    gutters: (styles = {}) => {
+      // To deprecate in v4.1
+      //       warning(
+      //         false,
+      //         [
+      //           'Material-UI: theme.mixins.gutters() is deprecated.',
+      //           'You can use the source of the mixin directly:',
+      //           `
+      // paddingLeft: theme.spacing(2),
+      // paddingRight: theme.spacing(2),
+      // [theme.breakpoints.up('sm')]: {
+      //   paddingLeft: theme.spacing(3),
+      //   paddingRight: theme.spacing(3),
+      // },
+      // `,
+      //         ].join('\n'),
+      //       );
+
+      return {
+        paddingLeft: spacing(2),
+        paddingRight: spacing(2),
+        ...styles,
+        [breakpoints.up('sm')]: {
+          paddingLeft: spacing(3),
+          paddingRight: spacing(3),
+          ...styles[breakpoints.up('sm')],
+        },
+      };
+    },
     toolbar: {
       minHeight: 56,
       [`${breakpoints.up('xs')} and (orientation: landscape)`]: {
