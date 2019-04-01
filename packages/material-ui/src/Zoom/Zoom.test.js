508fa061448cb480167590909f6e8739ef1c21b1
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2019-04-01 22:40:43 +0100

    [Zoom] Convert to function component (#15133)

--- packages/material-ui/src/Zoom/Zoom.test.js
+++ packages/material-ui/src/Zoom/Zoom.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { assert } from 'chai';
 import { spy } from 'sinon';
-import { createShallow, createMount } from '@material-ui/core/test-utils';
+import { createMount } from '@material-ui/core/test-utils';
 import Zoom from './Zoom';
 
 describe('<Zoom />', () => {
-  let shallow;
   let mount;
   const defaultProps = {
     in: true,
@@ -13,7 +12,6 @@ describe('<Zoom />', () => {
   };
 
   before(() => {
-    shallow = createShallow({ dive: true });
     mount = createMount();
   });
 
@@ -22,58 +20,98 @@ describe('<Zoom />', () => {
   });
 
   it('should render a Transition', () => {
-    const wrapper = shallow(<Zoom {...defaultProps} />);
-    assert.strictEqual(wrapper.name(), 'Transition');
+    const wrapper = mount(<Zoom {...defaultProps} />);
+    assert.strictEqual(wrapper.find('Transition').exists(), true);
   });
 
   describe('event callbacks', () => {
-    it('should fire event callbacks', () => {
-      const events = ['onEnter', 'onEntering', 'onEntered', 'onExit', 'onExiting', 'onExited'];
+    describe('entering', () => {
+      it('should fire callbacks', done => {
+        const handleEnter = spy();
+        const handleEntering = spy();
 
-      const handlers = events.reduce((result, n) => {
-        result[n] = spy();
-        return result;
-      }, {});
+        mount(
+          <Zoom
+            onEnter={handleEnter}
+            onEntering={handleEntering}
+            onEntered={() => {
+              assert.strictEqual(handleEnter.callCount, 1);
+              assert.strictEqual(handleEnter.args[0].length, 1);
+              assert.strictEqual(handleEntering.callCount, 1);
+              assert.strictEqual(handleEntering.args[0].length, 2);
+              done();
+            }}
+            {...defaultProps}
+          >
+            <div />
+          </Zoom>,
+        );
+      });
+    });
 
-      const wrapper = shallow(<Zoom {...defaultProps} {...handlers} />);
+    describe('exiting', () => {
+      it('should fire callbacks', done => {
+        const handleExit = spy();
+        const handleExiting = spy();
 
-      events.forEach(n => {
-        const event = n.charAt(2).toLowerCase() + n.slice(3);
-        wrapper.simulate(event, { style: {} });
-        assert.strictEqual(handlers[n].callCount, 1, `should have called the ${n} handler`);
-        assert.strictEqual(handlers[n].args[0].length, 1);
+        const wrapper = mount(
+          <Zoom
+            onExit={handleExit}
+            onExiting={handleExiting}
+            onExited={() => {
+              assert.strictEqual(handleExit.callCount, 1);
+              assert.strictEqual(handleExit.args[0].length, 1);
+              assert.strictEqual(handleExiting.callCount, 1);
+              assert.strictEqual(handleExiting.args[0].length, 1);
+              done();
+            }}
+            {...defaultProps}
+          >
+            <div />
+          </Zoom>,
+        );
+
+        wrapper.setProps({
+          in: false,
+        });
       });
     });
   });
 
   describe('transition lifecycle', () => {
-    let wrapper;
-    let instance;
-
-    before(() => {
-      wrapper = shallow(<Zoom {...defaultProps} />);
-      instance = wrapper.instance();
-    });
-
     describe('handleEnter()', () => {
       it('should set the style properties', () => {
-        const element = { style: {} };
-        instance.handleEnter(element);
-        assert.strictEqual(
-          element.style.transition,
-          'transform 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+        mount(
+          <Zoom
+            {...defaultProps}
+            onEnter={node => {
+              assert.match(
+                node.style.transition,
+                /transform 225ms cubic-bezier\(0.4, 0, 0.2, 1\)( 0ms)?/,
+              );
+            }}
+          />,
         );
       });
     });
 
     describe('handleExit()', () => {
       it('should set the style properties', () => {
-        const element = { style: {} };
-        instance.handleExit(element);
-        assert.strictEqual(
-          element.style.transition,
-          'transform 195ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+        const wrapper = mount(
+          <Zoom
+            {...defaultProps}
+            onExit={node => {
+              assert.match(
+                node.style.transition,
+                /transform 195ms cubic-bezier\(0.4, 0, 0.2, 1\)( 0ms)?/,
+              );
+            }}
+          />,
         );
+
+        wrapper.setProps({
+          in: false,
+        });
       });
     });
   });
