5bbe7aa475cc61b9de3268117dc14f3094141997
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-04-02 15:19:54 +0200

    support all the pages

--- pages/css-in-js/api.js
+++ pages/css-in-js/api.js
@@ -3,7 +3,7 @@ import 'docs/src/modules/components/bootstrap';
 import React from 'react';
 import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
 
-const req = require.context('docs/src/pages/css-in-js/api', false, /\.md|\.js$/);
+const req = require.context('docs/src/pages/css-in-js/api', false, /\.(md|js|tsx)$/);
 const reqSource = require.context('!raw-loader!../../docs/src/pages/css-in-js/api', false, /\.js$/);
 const reqPrefix = 'pages/css-in-js/api';
 
--- pages/utils/click-away-listener.js
+++ pages/utils/click-away-listener.js
@@ -3,7 +3,7 @@ import 'docs/src/modules/components/bootstrap';
 import React from 'react';
 import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
 
-const req = require.context('docs/src/pages/utils/click-away-listener', false, /\.md|\.js$/);
+const req = require.context('docs/src/pages/utils/click-away-listener', false, /\.(md|js|tsx)$/);
 const reqSource = require.context(
   '!raw-loader!../../docs/src/pages/utils/click-away-listener',
   false,
