ccbb95ba5b65187012cc700d5bd6b51bc831e410
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2019-04-03 15:07:43 +0300

    Add tests for keyboard input validation

--- lib/src/__tests__/e2e/KeyboardDatePicker.test.tsx
+++ lib/src/__tests__/e2e/KeyboardDatePicker.test.tsx
@@ -1,3 +1,4 @@
+import addDays from 'date-fns/addDays';
 import { ReactWrapper } from 'enzyme';
 import * as React from 'react';
 import KeyboardDatePicker, { KeyboardDatePickerProps } from '../../DatePicker/KeyboardDatePicker';
@@ -21,14 +22,6 @@ describe('e2e -- DatePicker keyboard input', () => {
         InputLabelProps={{
           htmlFor: 'your-id',
         }}
-        // TODO support
-        // InputProps={{
-        //   endAdornment: (
-        //     <InputAdornment position="end">
-        //       <IconButton>date_range</IconButton>
-        //     </InputAdornment>
-        //   ),
-        // }}
       />
     );
   });
@@ -41,5 +34,49 @@ describe('e2e -- DatePicker keyboard input', () => {
 
     component.find('input').simulate('blur');
     expect(onChangeMock).toBeCalled();
+  });
+});
+
+describe('e2e -- KeyboardDatePicker validation errors', () => {
+  it('Should render error message if date is unparseable', () => {
+    const component = mount(
+      <KeyboardDatePicker
+        format={process.env.UTILS === 'moment' ? 'DD/MM/YYYY' : 'dd/MM/yyyy'}
+        value={new Date(NaN)}
+        onChange={jest.fn()}
+      />
+    );
+
+    expect(component.find('TextField').prop('helperText')).toBe('Invalid Date Format');
+  });
+
+  it('Should render error message if date is after maxDate', () => {
+    const component = mount(
+      <KeyboardDatePicker
+        onChange={jest.fn()}
+        disableFuture
+        value={addDays(new Date(), 2)}
+        format={process.env.UTILS === 'moment' ? 'DD/MM/YYYY' : 'dd/MM/yyyy'}
+      />
+    );
+
+    expect(component.find('TextField').prop('helperText')).toBe(
+      'Date should not be after maximal date'
+    );
+  });
+
+  it('Should render error message if date is after maxDate', () => {
+    const component = mount(
+      <KeyboardDatePicker
+        onChange={jest.fn()}
+        disablePast
+        value={addDays(new Date(), -2)}
+        format={process.env.UTILS === 'moment' ? 'DD/MM/YYYY' : 'dd/MM/yyyy'}
+      />
+    );
+
+    expect(component.find('TextField').prop('helperText')).toBe(
+      'Date should not be before minimal date'
+    );
   });
 });
