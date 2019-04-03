0327d8a1922d46cd2c049997e1080c9e6ec74f8b
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-04-03 17:16:07 +0200

    [ClickAwayListener] Update types

--- packages/material-ui/src/ClickAwayListener/ClickAwayListener.d.ts
+++ packages/material-ui/src/ClickAwayListener/ClickAwayListener.d.ts
@@ -1,7 +1,11 @@
 import * as React from 'react';
 
 export interface ClickAwayListenerProps {
+  /**
+   * @deprecated
+   */
   children: React.ReactNode;
+  getTargetEl: () => Element | null | undefined;
   mouseEvent?: 'onClick' | 'onMouseDown' | 'onMouseUp' | false;
   onClickAway: (event: React.MouseEvent<Document>) => void;
   touchEvent?: 'onTouchStart' | 'onTouchEnd' | false;
--- packages/material-ui/src/ClickAwayListener/ClickAwayListener.spec.tsx
+++ packages/material-ui/src/ClickAwayListener/ClickAwayListener.spec.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 
-const ClickAwayListenerComponentTest = () => (
-  <ClickAwayListener onClickAway={() => {}}>
-    <div />
-  </ClickAwayListener>
-);
+const ClickAwayListenerComponentTest = () => {
+  const ref = React.useRef<HTMLDivElement>(null);
+  return (
+    <ClickAwayListener getTargetEl={() => ref.current} onClickAway={() => {}}>
+      <div ref={ref} />
+    </ClickAwayListener>
+  );
+};
