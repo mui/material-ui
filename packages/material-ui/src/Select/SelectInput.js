81465c18f300d2cef026db14d950ba412c08142f
rostislav <34387358+rreznichenko@users.noreply.github.com>
2019-04-04 09:38:10 +0300

    [Select] Trigger the open callbacks even when uncontrolled  (#15176)
    * [SelectInput] onOpen onClose hook
    
    * prettier commit

--- packages/material-ui/src/Select/SelectInput.js
+++ packages/material-ui/src/Select/SelectInput.js
@@ -44,12 +44,15 @@ class SelectInput extends React.Component {
   }
 
   update = ({ event, open }) => {
-    if (this.isOpenControlled) {
-      if (open) {
+    if (open) {
+      if (this.props.onOpen) {
         this.props.onOpen(event);
-      } else {
-        this.props.onClose(event);
       }
+    } else if (this.props.onClose) {
+      this.props.onClose(event);
+    }
+
+    if (this.isOpenControlled) {
       return;
     }
 
