2f8591db36a3d4ed64b60bed3930632db26ad5b1
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-04-06 09:43:01 +0200

    remove create-react-app-with-jss

--- examples/create-react-app-with-typescript-next/src/theme.ts
+++ examples/create-react-app-with-typescript-next/src/theme.ts
@@ -2,19 +2,21 @@ import red from '@material-ui/core/colors/red';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 // A custom theme for this app
-export const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#556CD6'
-        },
-        secondary: {
-            main: '#03DAC6'
-        },
-        error: {
-            main: red.A400
-        },
-        background: {
-            default: '#E9EDF1'
-        }
-    }
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#03dac6',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#e9edf1',
+    },
+  },
 });
+
+export default theme;
