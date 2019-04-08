616b1b582c07b6c44ca104636eef01f66cc36503
joshwooding <12938082+joshwooding@users.noreply.github.com>
2019-04-08 21:59:15 +0100

    Restructure Fade.test.js

--- packages/material-ui/src/Fade/Fade.test.js
+++ packages/material-ui/src/Fade/Fade.test.js
@@ -28,102 +28,107 @@ describe('<Fade />', () => {
     testComponentPropWith: false,
   }));
 
-  describe('event callbacks', () => {
+  describe('transition lifecycle', () => {
     let clock;
+    let wrapper;
+    let child;
+
+    const handleEnter = spy();
+    const handleEntering = spy();
+    const handleEntered = spy();
+    const handleExit = spy();
+    const handleExiting = spy();
+    const handleExited = spy();
 
     before(() => {
       clock = useFakeTimers();
+      wrapper = mount(
+        <Fade
+          onEnter={handleEnter}
+          onEntering={handleEntering}
+          onEntered={handleEntered}
+          onExit={handleExit}
+          onExiting={handleExiting}
+          onExited={handleExited}
+        >
+          <div id="test" />
+        </Fade>,
+      );
+      child = wrapper.find('#test');
     });
 
     after(() => {
       clock.restore();
     });
 
-    describe('entering', () => {
-      it('should fire callbacks', () => {
-        const handleEnter = spy();
-        const handleEntering = spy();
-        const handleEntered = spy();
-
-        mount(
-          <Fade
-            onEnter={handleEnter}
-            onEntering={handleEntering}
-            onEntered={handleEntered}
-            {...defaultProps}
-          >
-            <div />
-          </Fade>,
-        );
-
-        assert.strictEqual(handleEnter.callCount, 1);
-        assert.strictEqual(handleEnter.args[0].length, 1);
-        assert.strictEqual(handleEntering.callCount, 1);
-        assert.strictEqual(handleEntering.args[0].length, 2);
-        clock.tick(1000);
-        assert.strictEqual(handleEntered.callCount, 1);
-        assert.strictEqual(handleEntered.args[0].length, 2);
+    describe('in', () => {
+      before(() => {
+        wrapper.setProps({ in: true });
       });
-    });
 
-    describe('exiting', () => {
-      it('should fire callbacks', () => {
-        const handleExit = spy();
-        const handleExiting = spy();
-        const handleExited = spy();
-
-        const wrapper = mount(
-          <Fade
-            onExit={handleExit}
-            onExiting={handleExiting}
-            onExited={handleExited}
-            {...defaultProps}
-          >
-            <div />
-          </Fade>,
-        );
-
-        wrapper.setProps({
-          in: false,
+      describe('handleEnter()', () => {
+        it('should call handleEnter()', () => {
+          assert.strictEqual(handleEnter.callCount, 1);
+          assert.strictEqual(handleEnter.args[0][0], child.instance());
         });
 
-        assert.strictEqual(handleExit.callCount, 1);
-        assert.strictEqual(handleExit.args[0].length, 1);
-        assert.strictEqual(handleExiting.callCount, 1);
-        assert.strictEqual(handleExiting.args[0].length, 1);
-        clock.tick(1000);
-        assert.strictEqual(handleExited.callCount, 1);
-        assert.strictEqual(handleExited.args[0].length, 1);
+        it('should set style properties', () => {
+          assert.match(
+            handleEnter.args[0][0].style.transition,
+            /opacity 225ms cubic-bezier\(0.4, 0, 0.2, 1\)( 0ms)?/,
+          );
+        });
+      });
+
+      describe('handleEntering()', () => {
+        it('should call handleEntering()', () => {
+          assert.strictEqual(handleEntering.callCount, 1);
+          assert.strictEqual(handleEntering.args[0][0], child.instance());
+        });
+      });
+
+      describe('handleEntered()', () => {
+        it('should call handleEntered()', () => {
+          clock.tick(1000);
+          assert.strictEqual(handleEntered.callCount, 1);
+          assert.strictEqual(handleEntered.args[0][0], child.instance());
+        });
       });
     });
-  });
 
-  describe('transition lifecycle', () => {
-    describe('handleEnter()', () => {
-      it('should set style properties', () => {
-        const handleEnter = spy();
-        mount(<Fade {...defaultProps} onEnter={handleEnter} />);
-
-        assert.match(
-          handleEnter.args[0][0].style.transition,
-          /opacity 225ms cubic-bezier\(0.4, 0, 0.2, 1\)( 0ms)?/,
-        );
+    describe('out', () => {
+      before(() => {
+        wrapper.setProps({ in: true });
+        wrapper.setProps({ in: false });
       });
-    });
 
-    describe('handleExit()', () => {
-      it('should set style properties', () => {
-        const handleExit = spy();
-        const wrapper = mount(<Fade {...defaultProps} onExit={handleExit} />);
-
-        wrapper.setProps({
-          in: false,
+      describe('handleExit()', () => {
+        it('should call handleExit()', () => {
+          assert.strictEqual(handleExit.callCount, 1);
+          assert.strictEqual(handleExit.args[0][0], child.instance());
         });
 
-        assert.match(
-          handleExit.args[0][0].style.transition,
-          /opacity 195ms cubic-bezier\(0.4, 0, 0.2, 1\)( 0ms)?/,
-        );
+        it('should set style properties', () => {
+          assert.match(
+            handleExit.args[0][0].style.transition,
+            /opacity 195ms cubic-bezier\(0.4, 0, 0.2, 1\)( 0ms)?/,
+          );
+        });
+      });
+
+      describe('handleExiting()', () => {
+        it('should call handleExiting()', () => {
+          assert.strictEqual(handleExiting.callCount, 1);
+          assert.strictEqual(handleExiting.args[0][0], child.instance());
+        });
+      });
+
+      describe('handleExited()', () => {
+        it('should call handleExited()', () => {
+          clock.tick(1000);
+          assert.strictEqual(handleExited.callCount, 1);
+          assert.strictEqual(handleExited.args[0][0], child.instance());
+        });
       });
     });
   });
