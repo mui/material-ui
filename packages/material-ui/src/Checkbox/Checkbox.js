a044ab3099660117cbb8b9765e8e7916fad68838
joshwooding <12938082+joshwooding@users.noreply.github.com>
2019-04-09 22:09:52 +0100

    Update Checkbox types

--- packages/material-ui/src/Checkbox/Checkbox.js
+++ packages/material-ui/src/Checkbox/Checkbox.js
@@ -70,7 +70,7 @@ Checkbox.propTypes = {
   /**
    * If `true`, the component is checked.
    */
-  checked: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  checked: PropTypes.bool,
   /**
    * The icon to display when the component is checked.
    */
