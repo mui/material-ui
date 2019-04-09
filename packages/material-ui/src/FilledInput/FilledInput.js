fa1bee02e602b936321d2fc16ba49b32fc73d3cd
joshwooding <12938082+joshwooding@users.noreply.github.com>
2019-04-09 22:12:11 +0100

    Update FilledInput types

--- packages/material-ui/src/FilledInput/FilledInput.js
+++ packages/material-ui/src/FilledInput/FilledInput.js
@@ -163,15 +163,7 @@ FilledInput.propTypes = {
   /**
    * The default `input` element value, useful when not controlling the component.
    */
-  defaultValue: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-    PropTypes.bool,
-    PropTypes.object,
-    PropTypes.arrayOf(
-      PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool, PropTypes.object]),
-    ),
-  ]),
+  defaultValue: PropTypes.any,
   /**
    * If `true`, the `input` element will be disabled.
    */
@@ -262,15 +254,7 @@ FilledInput.propTypes = {
   /**
    * The value of the `input` element, required for a controlled component.
    */
-  value: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-    PropTypes.bool,
-    PropTypes.object,
-    PropTypes.arrayOf(
-      PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool, PropTypes.object]),
-    ),
-  ]),
+  value: PropTypes.any,
 };
 
 InputBase.defaultProps = {
