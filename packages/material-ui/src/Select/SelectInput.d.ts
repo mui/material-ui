bcf94b83d375573b82f6c36fb5db5e4a8442258f
joshwooding <12938082+joshwooding@users.noreply.github.com>
2019-04-09 22:17:46 +0100

    Update SelectInput types

--- packages/material-ui/src/Select/SelectInput.d.ts
+++ packages/material-ui/src/Select/SelectInput.d.ts
@@ -23,7 +23,7 @@ export interface SelectInputProps {
   renderValue?: (value: SelectInputProps['value']) => React.ReactNode;
   SelectDisplayProps?: React.HTMLAttributes<HTMLDivElement>;
   tabIndex?: number;
-  value: string | number | boolean | Array<string | number | boolean>;
+  value: any;
   variant?: 'standard' | 'outlined' | 'filled';
 }
 
