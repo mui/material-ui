dfea5ba988f70716d5bec510846e20b8cd100e72
Seth Perry <seth.perry@logrhythm.com>
2019-04-10 12:47:49 -0600

    [docs] Add most Dialog TypeScript demos (#15271)
    * [docs]: adding most dialog typescript demos
    
    * alphabetizing SimpleDialog proptypes
    
    * fixing prop type order in js
    
    * adding dividers props to DialogContent
    
    * [docs] consistent order of steps
    
    * update ResponsiveDialog with new HOC props

--- docs/src/pages/demos/dialogs/CustomizedDialog.tsx
+++ docs/src/pages/demos/dialogs/CustomizedDialog.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
@@ -9,20 +9,27 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
 
-const styles = theme => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
-});
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      margin: 0,
+      padding: theme.spacing(2),
+    },
+    closeButton: {
+      position: 'absolute',
+      right: theme.spacing(1),
+      top: theme.spacing(1),
+      color: theme.palette.grey[500],
+    },
+  });
 
-const DialogTitle = withStyles(styles)(props => {
+export interface DialogTitleProps extends WithStyles<typeof styles> {
+  id: string;
+  children: React.ReactNode;
+  onClose: () => void;
+}
+
+const DialogTitle = withStyles(styles)((props: DialogTitleProps) => {
   const { children, classes, onClose } = props;
   return (
     <MuiDialogTitle disableTypography className={classes.root}>
