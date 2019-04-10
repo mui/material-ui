82a5bc61461a9f785acfb268f4caf903865d9788
sperry94 <setherperry@gmail.com>
2019-04-09 18:57:04 -0600

    updating List tsx test file

--- packages/material-ui/src/List/List.spec.tsx
+++ packages/material-ui/src/List/List.spec.tsx
@@ -5,8 +5,8 @@ import List from '@material-ui/core/List';
   <List component="div" />;
   <List
     component="div"
-    onChange={e => {
-      e.currentTarget; // $ExpectType EventTarget & HTMLUListElement
+    onChange={(e: React.FormEvent<HTMLDivElement>) => {
+      e.currentTarget;
     }}
   />;
 }
