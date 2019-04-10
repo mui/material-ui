2bb800c909f344ec12f2ff48b31c8d12c29189b3
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-04-10 12:39:58 +0200

    Matt review

--- packages/material-ui/src/Paper/Paper.js
+++ packages/material-ui/src/Paper/Paper.js
@@ -73,7 +73,7 @@ Paper.propTypes = {
   component: PropTypes.elementType,
   /**
    * Shadow depth, corresponds to `dp` in the spec.
-   * It's accepting values between 0 and 24 inclusive.
+   * It accepts values between 0 and 24 inclusive.
    */
   elevation: PropTypes.number,
   /**
