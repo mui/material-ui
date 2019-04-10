9a9b4e774c3c8f68de05f36ca3e5ab9cb8cfa0b6
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-04-10 10:43:54 +0200

    [test] Keep original issue reference

--- packages/material-ui/src/FormControl/FormControl.spec.tsx
+++ packages/material-ui/src/FormControl/FormControl.spec.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import FormControl from '@material-ui/core/FormControl';
 
+// custom host
+// https://github.com/mui-org/material-ui/issues/13744
 {
   <FormControl component="fieldset" className="hello-world" />;
 }
--- packages/material-ui/src/List/List.spec.tsx
+++ packages/material-ui/src/List/List.spec.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import List from '@material-ui/core/List';
 
+// custom host
+// https://github.com/mui-org/material-ui/issues/13746
 {
   <List component="div" />;
   <List
