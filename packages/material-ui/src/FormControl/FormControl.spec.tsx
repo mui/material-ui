dd04aeb99edec6a0d73b0f4d34e3b9500400c70d
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-04-10 10:50:31 +0200

    [test] Test remove redundant type test
    Already tested in the docs

--- packages/material-ui/src/FormControl/FormControl.spec.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import FormControl from '@material-ui/core/FormControl';
-
-// custom host
-// https://github.com/mui-org/material-ui/issues/13744
-{
-  <FormControl component="fieldset" className="hello-world" />;
-}
--- packages/material-ui/src/FormLabel/FormLabel.spec.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import FormLabel from '@material-ui/core/FormLabel';
-
-{
-  <FormLabel component="legend" />;
-}
