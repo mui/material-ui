a4b92ba276cabdb064fc34c2b8fbe1bba9fc9415
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-04-12 11:57:51 +0200

    [test] Don't memoize mode
    - Interfers with other tests
    - also polishes createMount

--- packages/material-ui/src/test-utils/createMount.js
+++ packages/material-ui/src/test-utils/createMount.js
@@ -3,48 +3,54 @@ import ReactDOM from 'react-dom';
 import * as PropTypes from 'prop-types';
 import { mount as enzymeMount } from 'enzyme';
 
+/**
+ * Can't just mount <React.Fragment>{node}</React.Fragment>
+ * because that swallows wrapper.setProps
+ *
+ * why class component:
+ * https://github.com/airbnb/enzyme/issues/2043
+ */
+// eslint-disable-next-line react/prefer-stateless-function
+class Mode extends React.Component {
+  static propTypes = {
+    /**
+     * this is essentially children. However we can't use children because then
+     * using `wrapper.setProps({ children })` would work differently if this component
+     * would be the root.
+     */
+    __element: PropTypes.element.isRequired,
+    __strict: PropTypes.bool.isRequired,
+  };
+
+  render() {
+    const { __element, __strict, ...other } = this.props;
+    const Component = __strict ? React.StrictMode : React.Fragment;
+
+    return <Component>{React.cloneElement(__element, other)}</Component>;
+  }
+}
+
+Mode.propTypes = {
+  /**
+   * this is essentially children. However we can't use children because then
+   * using `wrapper.setProps({ children })` would work differently if this component
+   * would be the root.
+   */
+  __element: PropTypes.element.isRequired,
+  __strict: PropTypes.bool.isRequired,
+};
+
 // Generate an enhanced mount function.
 export default function createMount(options = {}) {
-  const { mount = enzymeMount, strict: strictOption, ...enzymeOptions } = options;
+  const { mount = enzymeMount, strict: globalStrict, ...globalEnzymeOptions } = options;
 
   const attachTo = window.document.createElement('div');
   attachTo.className = 'app';
   attachTo.setAttribute('id', 'app');
   window.document.body.insertBefore(attachTo, window.document.body.firstChild);
 
-  /**
-   * Can't just mount <React.Fragment>{node}</React.Fragment>
-   * because that swallows wrapper.setProps
-   *
-   * why class component:
-   * https://github.com/airbnb/enzyme/issues/2043
-   */
-
-  class Mode extends React.PureComponent {
-    static propTypes = {
-      /**
-       * this is essentially children. However we can't use children because then
-       * using `wrapper.setProps({ children })` would work differently if this component
-       * would be the root.
-       */
-      __element: PropTypes.element.isRequired,
-      __strict: PropTypes.bool.isRequired,
-    };
-
-    render() {
-      const { __element, __strict, ...other } = this.props;
-      const Component = __strict ? React.StrictMode : React.Fragment;
-
-      return <Component>{React.cloneElement(__element, other)}</Component>;
-    }
-  }
-
   const mountWithContext = function mountWithContext(node, localOptions = {}) {
-    const {
-      disableUnnmount = false,
-      strict: localStrictOption = strictOption,
-      ...localEnzymeOptions
-    } = localOptions;
+    const { disableUnnmount = false, strict = globalStrict, ...localEnzymeOptions } = localOptions;
 
     if (!disableUnnmount) {
       ReactDOM.unmountComponentAtNode(attachTo);
@@ -52,18 +58,11 @@ export default function createMount(options = {}) {
 
     // some tests require that no other components are in the tree
     // e.g. when doing .instance(), .state() etc.
-    return mount(
-      localStrictOption == null ? (
-        node
-      ) : (
-        <Mode __element={node} __strict={Boolean(localStrictOption)} />
-      ),
-      {
-        attachTo,
-        ...enzymeOptions,
-        ...localEnzymeOptions,
-      },
-    );
+    return mount(strict == null ? node : <Mode __element={node} __strict={Boolean(strict)} />, {
+      attachTo,
+      ...globalEnzymeOptions,
+      ...localEnzymeOptions,
+    });
   };
 
   mountWithContext.attachTo = attachTo;
