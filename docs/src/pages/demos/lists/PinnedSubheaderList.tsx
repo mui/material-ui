c568c98df6bbede6054cfc324f43582dad174cb8
unknown <jztang8@gmail.com>
2019-04-13 14:29:27 -0400

    Add Typescript demo for PinnedSubheaderList component

---
+++ docs/src/pages/demos/lists/PinnedSubheaderList.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+      position: 'relative',
+      overflow: 'auto',
+      maxHeight: 300,
+    },
+    listSection: {
+      backgroundColor: 'inherit',
+    },
+    ul: {
+      backgroundColor: 'inherit',
+      padding: 0,
+    },
+  });
+
+export interface PinnedSubheaderListProps extends WithStyles<typeof styles> {}
+
+function PinnedSubheaderList(props: PinnedSubheaderListProps) {
+  const { classes } = props;
+
+  return (
+    <List className={classes.root} subheader={<li />}>
+      {[0, 1, 2, 3, 4].map(sectionId => (
+        <li key={`section-${sectionId}`} className={classes.listSection}>
+          <ul className={classes.ul}>
+            <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
+            {[0, 1, 2].map(item => (
+              <ListItem key={`item-${sectionId}-${item}`}>
+                <ListItemText primary={`Item ${item}`} />
+              </ListItem>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </List>
+  );
+}
+
+PinnedSubheaderList.propTypes = {
+  classes: PropTypes.object.isRequired,
+} as any;
+
+export default withStyles(styles)(PinnedSubheaderList);
