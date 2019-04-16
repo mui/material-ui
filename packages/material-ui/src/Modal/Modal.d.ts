c615a3321b2a61a9f969e08804307cf775e86221
Mark Jones <nsmarkejones@gmail.com>
2019-04-16 18:09:46 +0100

    Using unknown type and string union for reason

--- packages/material-ui/src/Modal/Modal.d.ts
+++ packages/material-ui/src/Modal/Modal.d.ts
@@ -20,7 +20,10 @@ export interface ModalProps
   keepMounted?: boolean;
   manager?: ModalManager;
   onBackdropClick?: React.ReactEventHandler<{}>;
-  onClose?: (event: React.SyntheticEvent<any>, reason: string) => void;
+  onClose?: (
+    event: React.SyntheticEvent<unknown>,
+    reason: 'backdropClick' | 'escapeKeyDown',
+  ) => void;
   onEscapeKeyDown?: React.ReactEventHandler<{}>;
   onRendered?: PortalProps['onRendered'];
   open: boolean;
