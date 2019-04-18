ccc466ad4cef38d66c235071bfe9152fbf0450e4
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-04-19 01:42:44 +0200

    [example] Add preact-next example (#15401)

---
+++ examples/preact-next/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
+import App from './App';
+import theme from './theme';
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.querySelector('#root'),
+);
