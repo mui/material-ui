6c91129ab4cea84d55740d6cb9b637f0b17c1803
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-04-19 18:03:53 +0200

    migrate to hooks

--- docs/src/pages/demos/tables/MaterialTableDemo.js
+++ docs/src/pages/demos/tables/MaterialTableDemo.js
@@ -1,68 +1,63 @@
-import React, { Component } from "react";
-import MaterialTable from "material-table";
+import React from 'react';
+import MaterialTable from 'material-table';
 
-export default class MaterialTableDemo extends Component {
-  constructor(props) {
-    super(props);
+export default function MaterialTableDemo() {
+  const [state, setState] = React.useState({
+    columns: [
+      { title: 'Name', field: 'name' },
+      { title: 'Surname', field: 'surname' },
+      { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+      {
+        title: 'Birth Place',
+        field: 'birthCity',
+        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+      },
+    ],
+    data: [
+      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+      {
+        name: 'Zerya Betül',
+        surname: 'Baran',
+        birthYear: 2017,
+        birthCity: 34,
+      },
+    ],
+  });
 
-    this.state = {
-      columns: [
-        { title: "Name", field: "name" },
-        { title: "Surname", field: "surname" },
-        { title: "Birth Year", field: "birthYear", type: "numeric" },
-        {
-          title: "Birth Place",
-          field: "birthCity",
-          lookup: { 34: "İstanbul", 63: "Şanlıurfa" }
-        }
-      ],
-      data: [
-        { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
-        {
-          name: "Zerya Betül",
-          surname: "Baran",
-          birthYear: 2017,
-          birthCity: 34
-        }
-      ]
-    };
-  }
-
-  render() {
-    return (
-      <MaterialTable
-        title="Editable Example"
-        columns={this.state.columns}
-        data={this.state.data}
-        editable={{
-          onRowAdd: newData =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                const data = this.state.data;
-                data.push(newData);
-                this.setState({ data }, () => resolve());
-              }, 600);
-            }),
-          onRowUpdate: (newData, oldData) =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                const data = this.state.data;
-                const index = data.indexOf(oldData);
-                data[index] = newData;
-                this.setState({ data }, () => resolve());
-              }, 600);
-            }),
-          onRowDelete: oldData =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                let data = this.state.data;
-                const index = data.indexOf(oldData);
-                data.splice(index, 1);
-                this.setState({ data }, () => resolve());
-              }, 600);
-            })
-        }}
-      />
-    );
-  }
+  return (
+    <MaterialTable
+      title="Editable Example"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.push(newData);
+              setState({ ...state, data });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data[data.indexOf(oldData)] = newData;
+              setState({ ...state, data });
+            }, 600);
+          }),
+        onRowDelete: oldData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.splice(data.indexOf(oldData), 1);
+              setState({ ...state, data });
+            }, 600);
+          }),
+      }}
+    />
+  );
 }
