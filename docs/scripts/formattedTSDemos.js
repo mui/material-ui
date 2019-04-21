c4f282a80da263c4a69a8ed7839ebf1c46010647
merceyz <merceyz@users.noreply.github.com>
2019-04-22 00:19:24 +0200

    Refactored formattedTSDemos

--- docs/scripts/formattedTSDemos.js
+++ docs/scripts/formattedTSDemos.js
@@ -1,55 +1,101 @@
+/* eslint-disable no-console */
+/* eslint-disable func-names */
+/* eslint-disable no-continue */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
+
 /**
  * Transpiles and formats TS demos.
  * Can be used to verify that JS and TS demos are equivalent. No introduced change
  * would indicate equivalence.
  */
-const childProcess = require('child_process');
+
+/**
+ * List of demos to ignore when transpiling
+ * Example: "app-bar/BottomAppBar.tsx"
+ */
+const ignoreList = [];
+
 const fse = require('fs-extra');
 const path = require('path');
+const babel = require('@babel/core');
 const prettier = require('prettier');
-const util = require('util');
+const os = require('os');
 
-const exec = util.promisify(childProcess.exec);
+const babelConfig = {
+  presets: ['@babel/preset-typescript'],
+  plugins: ['unwrap-createStyles'],
+  generatorOpts: { retainLines: true },
+  babelrc: false,
+  configFile: false,
+};
 
-async function getUnstagedGitFiles() {
-  const { stdout } = await exec('git diff --name-only');
-  const list = stdout.trim();
+const prettierConfig = prettier.resolveConfig(process.cwd(), {
+  config: path.join(__dirname, '../../prettier.config.js'),
+});
 
-  if (list === '') {
-    // "".split(" ") => [""]
-    return [];
+const watchMode = process.argv.some(arg => arg === '--watch');
+
+async function getFiles(root, callback) {
+  const files = [];
+  const folderItem = await fse.readdir(root);
+
+  for (const name of folderItem) {
+    const filePath = path.join(root, name);
+    const stat = await fse.stat(filePath);
+
+    if (stat.isFile()) {
+      if (!filePath.endsWith('.tsx')) continue;
+      if (ignoreList.some(ignorePath => filePath.endsWith(path.normalize(ignorePath)))) continue;
+      files.push(filePath);
+    } else if (stat.isDirectory()) {
+      files.push(...(await getFiles(filePath, callback)));
+    }
   }
 
-  return list.split('\n');
+  return files;
 }
 
 function fixBabelGeneratorIssues(source) {
-  return source.replace(/\/>\n\n/g, '/>\n').replace(/,\n\n/g, ',\n');
+  return source
+    .replace(/\/>(\r\n|\r|\n)(\r\n|\r|\n)/g, `/>${os.EOL}`)
+    .replace(/,(\r\n|\r|\n)(\r\n|\r|\n)/g, `,${os.EOL}`);
 }
 
-exec('yarn docs:typescript')
-  .then(() => {
-    const prettierConfigPath = path.join(__dirname, '../../prettier.config.js');
-    const prettierConfig = prettier.resolveConfig(process.cwd(), { config: prettierConfigPath });
+async function transpileFile(filePath) {
+  try {
+    const { code } = await babel.transformFileAsync(filePath, babelConfig);
+    const prettified = prettier.format(code, { ...(await prettierConfig), filepath: filePath });
+    const formatted = fixBabelGeneratorIssues(prettified);
 
-    return Promise.all([getUnstagedGitFiles(), prettierConfig]);
-  })
-  .then(([changedDemos, prettierConfig]) =>
-    Promise.all(
-      changedDemos.map(filename => {
-        const filepath = path.join(process.cwd(), filename);
-
-        return fse.readFile(filepath).then(source => {
-          const prettified = prettier.format(source.toString(), { ...prettierConfig, filepath });
-          const formatted = fixBabelGeneratorIssues(prettified);
-
-          return fse.writeFile(filepath, formatted);
-        });
-      }),
-    ),
-  )
-  .catch(err => {
-    // eslint-disable-next-line no-console
+    await fse.writeFile(filePath.replace('.tsx', '.js'), formatted);
+    return true;
+  } catch (err) {
     console.error(err);
-    process.exit(1);
-  });
+    if (!watchMode) process.exit(1);
+    return false;
+  }
+}
+
+(async () => {
+  const tsxFiles = await getFiles(path.join(__dirname, '../src/pages'));
+
+  for (const filePath of tsxFiles) {
+    if (watchMode) {
+      fse.watchFile(filePath, async () => {
+        const successful = await transpileFile(filePath);
+        if (watchMode && successful) {
+          console.log('Successfully transpiled: %s', filePath);
+        }
+      });
+    }
+    await transpileFile(filePath);
+  }
+
+  if (watchMode) {
+    console.log('Watching files...');
+  }
+})().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
