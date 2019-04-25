38ae6525348b185be5e45997b065075f8cde2af0
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-04-25 21:48:38 +0200

    Switch to terser

--- packages/material-ui/scripts/rollup.config.js
+++ packages/material-ui/scripts/rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 import nodeGlobals from 'rollup-plugin-node-globals';
-import { uglify } from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 
 const input = './src/index.js';
@@ -72,7 +72,7 @@ export default [
       nodeGlobals(), // Wait for https://github.com/cssinjs/jss/pull/893
       replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
       sizeSnapshot({ snapshotPath: 'size-snapshot.json' }),
-      uglify(),
+      terser(),
     ],
   },
 ];
