86f219d42c34da1cda8503031c6b1870602f6316
merceyz <merceyz@users.noreply.github.com>
2019-04-26 18:37:33 +0200

    Set default theme for makeStyles

--- packages/material-ui/src/styles/makeStyles.d.ts
+++ packages/material-ui/src/styles/makeStyles.d.ts
@@ -1,3 +1,12 @@
-import { makeStyles } from '@material-ui/styles';
+import { Theme as DefaultTheme } from './createMuiTheme';
+import { StylesHook } from '@material-ui/styles/makeStyles';
+import { Styles, WithStylesOptions } from '@material-ui/styles/withStyles';
 
-export default makeStyles;
+export default function makeStyles<
+  Theme = DefaultTheme,
+  Props extends {} = {},
+  ClassKey extends string = string
+>(
+  styles: Styles<Theme, Props, ClassKey>,
+  options?: WithStylesOptions<Theme>,
+): StylesHook<Styles<Theme, Props, ClassKey>>;
