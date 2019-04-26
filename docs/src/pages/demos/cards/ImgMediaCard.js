90b5a42c7c68aee39f99cc09223ddfeb321fbb0f
merceyz <merceyz@users.noreply.github.com>
2019-04-26 23:42:59 +0200

    [docs] Migrate ImgMediaCard to hooks

--- docs/src/pages/demos/cards/ImgMediaCard.js
+++ docs/src/pages/demos/cards/ImgMediaCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -9,14 +8,15 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const styles = {
+const useStyles = makeStyles({
   card: {
     maxWidth: 345,
   },
-};
+});
 
-function ImgMediaCard(props) {
-  const { classes } = props;
+function ImgMediaCard() {
+  const classes = useStyles();
+
   return (
     <Card className={classes.card}>
       <CardActionArea>
@@ -49,8 +49,4 @@ function ImgMediaCard(props) {
   );
 }
 
-ImgMediaCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(ImgMediaCard);
+export default ImgMediaCard;
