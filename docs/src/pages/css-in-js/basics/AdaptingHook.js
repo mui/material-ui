d60f60188a347fa8d593fc710bf5ec5b4d31c0bc
merceyz <merceyz@users.noreply.github.com>
2019-04-26 19:28:53 +0200

    [docs] Fix error in AdaptingHook

--- docs/src/pages/css-in-js/basics/AdaptingHook.js
+++ docs/src/pages/css-in-js/basics/AdaptingHook.js
@@ -4,14 +4,14 @@ import { makeStyles } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
-  root: {
-    background: props =>
+  root: props => ({
+    background:
       props.color === 'red'
         ? 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
         : 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
     border: 0,
     borderRadius: 3,
-    boxShadow: props =>
+    boxShadow:
       props.color === 'red'
         ? '0 3px 5px 2px rgba(255, 105, 135, .3)'
         : '0 3px 5px 2px rgba(33, 203, 243, .3)',
@@ -19,7 +19,7 @@ const useStyles = makeStyles({
     height: 48,
     padding: '0 30px',
     margin: 8,
-  },
+  }),
 });
 
 function MyButton(props) {
