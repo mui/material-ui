1e98ca775b7e0559dbcd2b8b93be33909e2c4f81
merceyz <merceyz@users.noreply.github.com>
2019-04-27 02:44:16 +0200

    Revert "[docs] Fix error in AdaptingHook"
    This reverts commit d60f60188a347fa8d593fc710bf5ec5b4d31c0bc.

--- docs/src/pages/css-in-js/basics/AdaptingHook.tsx
+++ docs/src/pages/css-in-js/basics/AdaptingHook.tsx
@@ -9,14 +9,14 @@ interface Props {
 }
 
 const useStyles = makeStyles({
-  root: (props: Props) => ({
-    background:
+  root: {
+    background: (props: Props) =>
       props.color === 'red'
         ? 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
         : 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
     border: 0,
     borderRadius: 3,
-    boxShadow:
+    boxShadow: (props: Props) =>
       props.color === 'red'
         ? '0 3px 5px 2px rgba(255, 105, 135, .3)'
         : '0 3px 5px 2px rgba(33, 203, 243, .3)',
@@ -24,7 +24,7 @@ const useStyles = makeStyles({
     height: 48,
     padding: '0 30px',
     margin: 8,
-  }),
+  },
 });
 
 function MyButton(props: Props & Omit<ButtonProps, 'color'>) {
