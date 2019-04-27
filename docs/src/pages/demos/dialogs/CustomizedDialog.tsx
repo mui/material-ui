854c734fd4297f2d4c6844788f911123fb339e03
merceyz <merceyz@users.noreply.github.com>
2019-04-27 02:05:07 +0200

    [docs] Fix CustomizedDialog

---
+++ docs/src/pages/demos/dialogs/CustomizedDialog.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      margin: 0,
+      padding: theme.spacing(2),
+    },
+    closeButton: {
+      position: 'absolute',
+      right: theme.spacing(1),
+      top: theme.spacing(1),
+      color: theme.palette.grey[500],
+    },
+    content: {
+      padding: theme.spacing(2),
+    },
+    actions: {
+      margin: 0,
+      padding: theme.spacing(1),
+    },
+  }),
+);
+
+interface DialogTitleProps {
+  children: React.ReactNode;
+  handleClose?(): void;
+}
+
+function DialogTitle(props: DialogTitleProps) {
+  const classes = useStyles();
+
+  return (
+    <MuiDialogTitle disableTypography className={classes.root}>
+      <Typography variant="h6">{props.children}</Typography>
+      {props.handleClose && (
+        <IconButton aria-label="Close" className={classes.closeButton} onClick={props.handleClose}>
+          <CloseIcon />
+        </IconButton>
+      )}
+    </MuiDialogTitle>
+  );
+}
+
+DialogTitle.propTypes = {
+  children: PropTypes.element.isRequired,
+  handleClose: PropTypes.func,
+} as any;
+
+function CustomizedDialogDemo() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  function handleClickOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
+  return (
+    <div>
+      <Button variant="outlined" color="secondary" onClick={handleClickOpen}>
+        Open dialog
+      </Button>
+      <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        <DialogTitle>Modal title</DialogTitle>
+        <DialogContent className={classes.content} dividers>
+          <Typography gutterBottom>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+          </Typography>
+          <Typography gutterBottom>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+            lacus vel augue laoreet rutrum faucibus dolor auctor.
+          </Typography>
+          <Typography gutterBottom>
+            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
+            scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
+            auctor fringilla.
+          </Typography>
+        </DialogContent>
+        <DialogActions className={classes.actions}>
+          <Button onClick={handleClose} color="primary">
+            Save changes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default CustomizedDialogDemo;
