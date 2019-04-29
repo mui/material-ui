e4a19286d625da292fe2376bc30925659f1a7475
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-04-29 13:50:16 +0200

    Remove instanceof error checks
    Forgot they won't work that well across windows

--- packages/material-ui/src/ButtonBase/focusVisible.js
+++ packages/material-ui/src/ButtonBase/focusVisible.js
@@ -1,5 +1,3 @@
-import warning from 'warning';
-
 // based on https://github.com/WICG/focus-visible/blob/v4.1.5/src/focus-visible.js
 
 let hadKeyboardEvent = true;
@@ -98,16 +96,8 @@ export function isFocusVisible(event) {
   } catch (error) {
     // browsers not implementing :focus-visible will throw a SyntaxError
     // we use our own heuristic for those browsers
-
     // rethrow might be better if it's not the expected error but do we really
     // want to crash if focus-visible malfunctioned?
-    warning(
-      error instanceof SyntaxError,
-      [
-        'Material-UI: focus-visible heuristic threw an unexpected error. ',
-        'Check caught errors in the debugger for more information.',
-      ].join(''),
-    );
   }
 
   // no need for validFocusTarget check. the user does that by attaching it to
