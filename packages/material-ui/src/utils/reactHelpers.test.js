67e3085626eae9ec21b57ca4c0588634d2421a08
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-04-30 08:23:42 +0200

    [useForkRef] Improve test for various ref branchings

--- packages/material-ui/src/utils/reactHelpers.test.js
+++ packages/material-ui/src/utils/reactHelpers.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { assert } from 'chai';
 import { spy } from 'sinon';
 import PropTypes from 'prop-types';
+import consoleErrorMock from 'test/utils/consoleErrorMock';
 import { isMuiElement, setRef, useForkRef } from './reactHelpers';
 import { Input, ListItemSecondaryAction, SvgIcon } from '..';
 import { mount } from 'enzyme';
@@ -63,6 +64,14 @@ describe('utils/reactHelpers.js', () => {
   });
 
   describe('useForkRef', () => {
+    beforeEach(() => {
+      consoleErrorMock.spy();
+    });
+
+    afterEach(() => {
+      consoleErrorMock.reset();
+    });
+
     it('returns a single ref-setter function that forks the ref to its inputs', () => {
       function Component(props) {
         const { innerRef } = props;
@@ -83,6 +92,44 @@ describe('utils/reactHelpers.js', () => {
       mount(<Component innerRef={outerRef} />);
 
       assert.strictEqual(outerRef.current.textContent, 'has a ref');
+      assert.strictEqual(consoleErrorMock.callCount(), 0);
+    });
+
+    it('forks if only one of the branches requires a ref', () => {
+      const Component = React.forwardRef(function Component(props, ref) {
+        const [hasRef, setHasRef] = React.useState(false);
+        const handleOwnRef = React.useCallback(() => setHasRef(true), []);
+        const handleRef = useForkRef(handleOwnRef, ref);
+
+        return <div ref={handleRef}>{String(hasRef)}</div>;
+      });
+
+      const wrapper = mount(<Component />);
+
+      assert.strictEqual(wrapper.containsMatchingElement(<div>true</div>), true);
+      assert.strictEqual(consoleErrorMock.callCount(), 0);
+    });
+
+    it('does nothing if none of the forked branches requires a ref', () => {
+      const Outer = React.forwardRef(function Outer(props, ref) {
+        const { children } = props;
+        const handleRef = useForkRef(children.ref, ref);
+
+        return React.cloneElement(children, { ref: handleRef });
+      });
+
+      Outer.propTypes = { children: PropTypes.element.isRequired };
+
+      function Inner() {
+        return <div />;
+      }
+
+      mount(
+        <Outer>
+          <Inner />
+        </Outer>,
+      );
+      assert.strictEqual(consoleErrorMock.callCount(), 0);
     });
   });
 });
