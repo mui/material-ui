aa7a67cd152717b31b27519c2c1b21ebe0eaac48
cvanem <chris@greenlinkservices.com>
2019-05-01 16:42:35 -0600

    Update typings

--- packages/material-ui/src/useScrollTrigger/useScrollTrigger.d.ts
+++ packages/material-ui/src/useScrollTrigger/useScrollTrigger.d.ts
@@ -11,11 +11,11 @@ export type OnTriggerEval = (
   options?: TriggerOptions,
 ) => boolean;
 
-export type UseScrollTriggerProps = {
+export interface UseScrollTriggerProps {
   onTriggerEval?: OnTriggerEval;
   initialOptions?: TriggerOptions;
-};
+}
 
 export default function useScrollTrigger(
   props?: UseScrollTriggerProps,
-): [boolean, React.Dispatch<any>];
+): [boolean, React.Dispatch<any>, React.Dispatch<React.SetStateAction<{}>>];
