8601a6830174543ee74fa9f915c039c54c7a1880
Chris <merceyz@users.noreply.github.com>
2019-05-02 09:17:02 +0200

    [docs] Add DelayingApperance TypeScript demo (#15551)

--- docs/src/pages/demos/progress/DelayingAppearance.tsx
+++ docs/src/pages/demos/progress/DelayingAppearance.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Fade from '@material-ui/core/Fade';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  button: {
-    margin: theme.spacing(2),
-  },
-  placeholder: {
-    height: 40,
-  },
-}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    button: {
+      margin: theme.spacing(2),
+    },
+    placeholder: {
+      height: 40,
+    },
+  }),
+);
 
 function DelayingAppearance() {
   const classes = useStyles();
   const [loading, setLoading] = React.useState(false);
   const [query, setQuery] = React.useState('idle');
-  const timerRef = React.useRef();
+  const timerRef = React.useRef<number>();
 
   React.useEffect(
     () => () => {
