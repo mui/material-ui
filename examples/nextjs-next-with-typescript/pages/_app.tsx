97ea28d178f7717fe450a74cd757952671794633
Jan Potoms <potoms.jan@gmail.com>
2019-05-04 15:45:37 +0200

    Start preparing some changes for next.js@canary

--- examples/nextjs-next-with-typescript/pages/_app.tsx
+++ examples/nextjs-next-with-typescript/pages/_app.tsx
@@ -9,7 +9,7 @@ class MyApp extends App {
   componentDidMount() {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
+    if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
   }
--- examples/nextjs-next-with-typescript/pages/_document.tsx
+++ examples/nextjs-next-with-typescript/pages/_document.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/styles';
-import flush from 'styled-jsx/server';
 import theme from '../src/theme';
 
 class MyDocument extends Document {
@@ -56,24 +55,24 @@ MyDocument.getInitialProps = async ctx => {
 
   // Render app and page and get the context of the page with collected side effects.
   const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
 
-  ctx.renderPage = () =>
-    originalRenderPage({
+  const {
+    styles,
+    ...initialProps
+  } = await Document.getInitialProps({
+    ...ctx,
+    renderPage: () => ctx.renderPage({
       enhanceApp: App => props => sheets.collect(<App {...props} />),
-    });
-
-  const initialProps = await Document.getInitialProps(ctx);
+    })
+  });
 
   return {
     ...initialProps,
     // Styles fragment is rendered after the app and page rendering finish.
-    styles: (
-      <React.Fragment>
-        {sheets.getStyleElement()}
-        {flush() || null}
-      </React.Fragment>
-    ),
+    styles: [
+      sheets.getStyleElement(),
+      ...(styles || [])
+    ],
   };
 };
 
