35e33da121bb195d2d255132c52837b6f6a8daee
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-05-08 17:09:12 +0200

    Make prompt repeatable

--- docs/scripts/componentsAffected.js
+++ docs/scripts/componentsAffected.js
@@ -1,9 +1,18 @@
-/* eslint-disable no-console */
+/* eslint-disable no-console, no-restricted-syntax */
 import * as inquirer from 'inquirer';
 import * as path from 'path';
 import { findComponents } from '../src/modules/utils/find';
 import getDependencyGraph from '../src/modules/utils/getDependencyGraph';
 import getApparentProps from '../src/modules/utils/getApparentProps';
+
+function* readAnswers() {
+  while (true) {
+    yield inquirer.prompt([
+      { message: 'What component changed (name):', name: 'component' },
+      { message: 'What prop changed (name):', name: 'prop' },
+    ]);
+  }
+}
 
 async function run() {
   console.log('Building meta data. This might take a while.');
@@ -20,26 +29,27 @@ async function run() {
   // input feels unresponsive
   const componentsApparentProps = await getApparentProps(components, dependencyGraph);
 
-  const { component: changedComponentName, prop: changedPropName } = await inquirer.prompt([
-    { message: 'What component changed (name):', name: 'component' },
-    { message: 'What prop changed (name):', name: 'prop' },
-  ]);
+  for await (const answer of readAnswers()) {
+    const { component: changedComponentName, prop: changedPropName } = answer;
 
-  const affectedComponents = Array.from(componentsApparentProps.entries())
-    .filter(([, apparentProps]) => {
-      return Object.entries(apparentProps).some(
-        ([propName, prop]) =>
-          propName === changedPropName && prop.implementedBy === changedComponentName,
-      );
-    })
-    .map(([componentName]) => componentName)
-    .sort((a, b) => a.localeCompare(b));
+    const affectedComponents = Array.from(componentsApparentProps.entries())
+      .filter(([, apparentProps]) => {
+        return Object.entries(apparentProps).some(
+          ([propName, prop]) =>
+            propName === changedPropName && prop.implementedBy === changedComponentName,
+        );
+      })
+      .map(([componentName]) => componentName)
+      .sort((a, b) => a.localeCompare(b));
 
-  console.log(
-    `Changing the prop '${changedPropName}' in '${changedComponentName}' affects the following components:\n${affectedComponents
-      .map(name => `  - ${name}`)
-      .join('\n')}`,
-  );
+    console.log(
+      `Changing the prop '${changedPropName}' in '${changedComponentName}' affects the following components:\n${affectedComponents
+        .map(name => `  - ${name}`)
+        .join('\n')}`,
+    );
+
+    console.log('\nCTRL+C to stop program\n');
+  }
 }
 
 run().catch(error => {
