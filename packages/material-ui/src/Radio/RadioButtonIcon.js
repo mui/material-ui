cde15763acb7fb8fc2182fa169d4dc2f92ec9496
Dave Caruso <dave@davecode.me>
2019-05-12 15:58:43 -0400

    [Radio] Run prettier and change prop type.

--- packages/material-ui/src/Radio/RadioButtonIcon.js
+++ packages/material-ui/src/Radio/RadioButtonIcon.js
@@ -10,12 +10,12 @@ export const styles = {
   root: {
     position: 'relative',
     width: '24px',
-    height: '24px'
+    height: '24px',
   },
   icon: {
     position: 'absolute',
     left: 0,
-    top: 0
+    top: 0,
   },
 };
 
@@ -23,26 +23,23 @@ export const styles = {
  * @ignore - internal component.
  */
 function RadioButtonIcon(props) {
-  const {
-    checked,
-    classes,
-    className,
-    ...other
-  } = props;
+  const { checked, classes, className, ...other } = props;
 
-  return <div className={clsx(classes.root, className)} {...other} >
-    <Zoom in={checked} timeout={100}>
-      <RadioButtonCheckedIcon className={classes.icon} />
-    </Zoom>
-    <RadioButtonUncheckedIcon className={classes.icon} />
-  </div>;
+  return (
+    <div className={clsx(classes.root, className)} {...other}>
+      <Zoom in={checked} timeout={100}>
+        <RadioButtonCheckedIcon className={classes.icon} />
+      </Zoom>
+      <RadioButtonUncheckedIcon className={classes.icon} />
+    </div>
+  );
 }
 
 RadioButtonIcon.propTypes = {
   /**
    * If `true`, the component is checked.
    */
-  checked: PropTypes.bool.isRequired,
+  checked: PropTypes.bool,
   /**
    * Override or extend the styles applied to the component.
    * See [CSS API](#css) below for more details.
@@ -52,6 +49,6 @@ RadioButtonIcon.propTypes = {
    * @ignore
    */
   className: PropTypes.string,
-}
+};
 
 export default withStyles(styles, { name: 'MuiRadioButtonIcon' })(RadioButtonIcon);
