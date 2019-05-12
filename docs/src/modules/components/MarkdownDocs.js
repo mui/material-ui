e4e62597c98e5845a772f8759749aa3f9f2b8fde
Matthew Brookes <github@nospam.33m.co>
2019-05-12 11:15:34 +0100

    Align edit button text with TOC title

--- docs/src/modules/components/MarkdownDocs.js
+++ docs/src/modules/components/MarkdownDocs.js
@@ -37,7 +37,6 @@ const styles = theme => ({
     alignItems: 'flex-end',
   },
   markdownElement: {
-    marginTop: theme.spacing(2),
     marginBottom: theme.spacing(2),
     padding: theme.spacing(0, 1),
   },
