2b5546b422fe8514c4215d1d16f8ae384b293078
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-05-14 22:22:17 +0200

    [docs] Use pickers from material-ui namespace (#15691)
    * [docs] Use pickers from material-ui namespace
    
    * Fix picker types

--- docs/src/pages/components/pickers/MaterialUIPickers.tsx
+++ docs/src/pages/components/pickers/MaterialUIPickers.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import DateFnsUtils from '@date-io/date-fns';
-import { MuiPickersUtilsProvider, TimePicker, DatePicker } from 'material-ui-pickers';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 
 const useStyles = makeStyles(
   createStyles({
@@ -15,23 +19,25 @@ const useStyles = makeStyles(
 
 function MaterialUIPickers() {
   // The first commit of Material-UI
-  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+  const [selectedDate, setSelectedDate] = React.useState<Date | null>(
+    new Date('2014-08-18T21:11:54'),
+  );
   const classes = useStyles();
 
-  function handleDateChange(date: Date) {
+  function handleDateChange(date: Date | null) {
     setSelectedDate(date);
   }
 
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <Grid container className={classes.grid} justify="space-around">
-        <DatePicker
+        <KeyboardDatePicker
           margin="normal"
           label="Date picker"
           value={selectedDate}
           onChange={handleDateChange}
         />
-        <TimePicker
+        <KeyboardTimePicker
           margin="normal"
           label="Time picker"
           value={selectedDate}
