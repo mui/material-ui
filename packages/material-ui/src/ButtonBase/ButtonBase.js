12f50c00fb3e0abd5224a423fcadbafe45c27a3a
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-05-17 10:57:51 +0200

    Move useEnhancedEffect outside of scope
    Makes it actually invariant once loaded

--- packages/material-ui/src/ButtonBase/ButtonBase.js
+++ packages/material-ui/src/ButtonBase/ButtonBase.js
@@ -47,6 +47,7 @@ export const styles = {
   focusVisible: {},
 };
 
+const useEnhancedEffect = typeof window !== 'undefined' ? React.useLayoutEffect : React.useEffect;
 /**
  * https://github.com/facebook/react/issues/14099#issuecomment-440013892
  *
@@ -54,7 +55,6 @@ export const styles = {
  */
 function useEventCallback(fn) {
   const ref = React.useRef(fn);
-  const useEnhancedEffect = typeof window !== 'undefined' ? React.useLayoutEffect : React.useEffect;
   useEnhancedEffect(() => {
     ref.current = fn;
   });
