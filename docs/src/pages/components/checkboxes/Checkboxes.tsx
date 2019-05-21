cb05bb98a34b84d4b2689f589ff3266c9fb14700
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-05-21 16:07:51 +0200

    [docs] Fix severe a11y issues with checkbox demos

--- docs/src/pages/components/checkboxes/Checkboxes.tsx
+++ docs/src/pages/components/checkboxes/Checkboxes.tsx
@@ -14,23 +14,61 @@ function Checkboxes() {
 
   return (
     <div>
-      <Checkbox checked={state.checkedA} onChange={handleChange('checkedA')} value="checkedA" />
+      <Checkbox
+        checked={state.checkedA}
+        onChange={handleChange('checkedA')}
+        value="checkedA"
+        inputProps={{
+          'aria-label': 'primary checkbox',
+        }}
+      />
       <Checkbox
         checked={state.checkedB}
         onChange={handleChange('checkedB')}
         value="checkedB"
         color="primary"
+        inputProps={{
+          'aria-label': 'secondary checkbox',
+        }}
       />
-      <Checkbox value="checkedC" />
-      <Checkbox disabled value="checkedD" />
-      <Checkbox disabled checked value="checkedE" />
+      <Checkbox
+        value="checkedC"
+        inputProps={{
+          'aria-label': 'uncontrolled-checkbox',
+        }}
+      />
+      <Checkbox
+        disabled
+        value="checkedD"
+        inputProps={{
+          'aria-label': 'disabled checkbox',
+        }}
+      />
+      <Checkbox
+        disabled
+        checked
+        value="checkedE"
+        inputProps={{
+          'aria-label': 'disabled checked checkbox',
+        }}
+      />
       <Checkbox
         checked={state.checkedF}
         onChange={handleChange('checkedF')}
         value="checkedF"
         indeterminate
+        inputProps={{
+          'aria-label': 'indeterminate checkbox',
+        }}
       />
-      <Checkbox defaultChecked color="default" value="checkedG" />
+      <Checkbox
+        defaultChecked
+        color="default"
+        value="checkedG"
+        inputProps={{
+          'aria-label': 'checkbox with default color',
+        }}
+      />
     </div>
   );
 }
