9ea6cbbf290b400c34508d7b8d250575a5c1c1f3
Walter Corrales <corraleswalter@live.com>
2019-05-23 13:36:05 -0600

    [Test] Introducing Callable AlertDialog

--- packages/material-ui/src/AlertDialog/AlertDialog.test.js
+++ packages/material-ui/src/AlertDialog/AlertDialog.test.js
@@ -3,13 +3,13 @@ import React from 'react';
 import AlertDialog from './AlertDialog';
 
 describe('show dialog', () => {
-    it('show dialog...', () => {
-        expect(AlertDialog.show('MyAppTitle', 'MyMessage')).toBe(Promise);
+    it('show dialog confirmable...', () => {
+        AlertDialog.show('MyAppTitle', 'MyMessage');
     });
 });
 
 describe('show dialog confirm', () => {
     it('show dialog confirmable...', () => {
-        expect(AlertDialog.show('MyAppTitle', 'MyMessage', { confirmable: true })).toBe(Promise);
+        AlertDialog.show('MyAppTitle', 'MyMessage', { confirmable: true });
     });
 });
