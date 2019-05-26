19088271709777847069ae6cbaa9ccb351e2a1c9
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2019-05-26 20:04:44 +0100

    Update packages/material-ui/src/TableSortLabel/TableSortLabel.d.ts
    Co-Authored-By: Sebastian Silbermann <silbermann.sebastian@gmail.com>

--- packages/material-ui/src/TableSortLabel/TableSortLabel.d.ts
+++ packages/material-ui/src/TableSortLabel/TableSortLabel.d.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { ExtendButtonBase } from '../ButtonBase';
 import { SvgIconProps } from '../SvgIcon';
-import { SimplifiedPropsOf } from '@material-ui/core/OverridableComponent';
+import { SimplifiedPropsOf } from '../OverridableComponent';
 
 declare const TableSortLabel: ExtendButtonBase<{
   props: {
