54ebb9c54fee2237e26f16e8494634ce47abdb8a
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-05-27 11:05:57 +0200

    [Box] Don't warn on array system props

--- packages/material-ui-system/src/responsivePropType.js
+++ packages/material-ui-system/src/responsivePropType.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const responsivePropType =
   process.env.NODE_ENV !== 'production'
-    ? PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.object])
+    ? PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.object, PropTypes.array])
     : {};
 
 export default responsivePropType;
