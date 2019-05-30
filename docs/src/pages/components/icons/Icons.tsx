93414e898c2b1194fb5b04c8af5407e6c4fc944f
Simon Goldin <Simon.Goldin+github@gmail.com>
2019-05-30 11:30:20 -0400

    added Icons TypeScript demo

---
+++ docs/src/pages/components/icons/Icons.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+import Icon from '@material-ui/core/Icon';
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'flex-end',
+    },
+    icon: {
+      margin: theme.spacing(2),
+    },
+    iconHover: {
+      margin: theme.spacing(2),
+      '&:hover': {
+        color: red[800],
+      },
+    },
+  });
+
+export interface Props extends WithStyles<typeof styles> {}
+
+const Icons = (props: Props) => {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Icon className={classes.icon}>add_circle</Icon>
+      <Icon className={classes.icon} color="primary">
+        add_circle
+      </Icon>
+      <Icon className={classes.icon} color="secondary">
+        add_circle
+      </Icon>
+      <Icon className={classes.icon} color="action">
+        add_circle
+      </Icon>
+      <Icon className={classes.iconHover} color="error" style={{ fontSize: 30 }}>
+        add_circle
+      </Icon>
+      <Icon className={classes.icon} color="disabled" fontSize="large">
+        add_circle
+      </Icon>
+    </div>
+  );
+};
+
+export default withStyles(styles)(Icons);
