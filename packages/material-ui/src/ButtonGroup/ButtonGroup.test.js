932bf3927ca50c1149ac71c99ab5b94607dd78ef
Matthew Brookes <github@nospam.33m.co>
2019-06-01 00:03:51 +0100

    Revise tests

--- packages/material-ui/src/ButtonGroup/ButtonGroup.test.js
+++ packages/material-ui/src/ButtonGroup/ButtonGroup.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { assert } from 'chai';
 import {
   createMount,
-  createShallow,
-  // describeConformance,
+  describeConformance,
   getClasses,
 } from '@material-ui/core/test-utils';
 import Button from '../Button';
@@ -11,12 +10,10 @@ import ButtonGroup from './ButtonGroup';
 
 describe('<ButtonGroup />', () => {
   let mount;
-  let shallow;
   let classes;
 
   before(() => {
     mount = createMount({ strict: true });
-    shallow = createShallow({ dive: true });
     classes = getClasses(
       <ButtonGroup>
         <Button>Hello World</Button>
@@ -28,183 +25,179 @@ describe('<ButtonGroup />', () => {
     mount.cleanUp();
   });
 
-  // describeConformance(<ButtonGroup><Button>Conformance?</Button></ButtonGroup>, () => ({
-  //   classes,
-  //   mount,
-  //   refInstanceof: window.HTMLDivElement,
-  //   skip: ['componentProp'],
-  // }));
+  describeConformance(<ButtonGroup><Button>Conformance?</Button></ButtonGroup>, () => ({
+    classes,
+    inheritComponent: 'div',
+    mount,
+    refInstanceof: window.HTMLDivElement,
+    testComponentPropWith: 'span',
+  }));
 
-  it('should render with the root class but no others', () => {
-    const wrapper = shallow(
+  it('renders with the root class but no others', () => {
+    const wrapper = mount(
       <ButtonGroup>
         <Button>Hello World</Button>
       </ButtonGroup>,
     );
-    assert.strictEqual(wrapper.hasClass(classes.root), true);
-    assert.strictEqual(wrapper.hasClass(classes.contained), false);
-    assert.strictEqual(wrapper.hasClass(classes.fullWidth), false);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.grouped), true);
+    const buttonGroup = wrapper.find('div')
+    assert.strictEqual(buttonGroup.hasClass(classes.root), true);
+    assert.strictEqual(buttonGroup.hasClass(classes.contained), false);
+    assert.strictEqual(buttonGroup.hasClass(classes.fullWidth), false);
   });
 
+  // it('overwrites the root class', () => {
+  //   const rootClassName = 'testRootClassName';
+  //   const wrapper = mount(
+  //     <ButtonGroup classes={{ badge: rootClassName }}>
+  //       <Button>Hello World</Button>
+  //     </ButtonGroup>,
+  //   );
+
+  //   assert.strictEqual(findBG(wrapper).hasClass(rootClassName), true);
+  // });
+
   it('should render an outlined button', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <ButtonGroup>
         <Button>Hello World</Button>
       </ButtonGroup>,
     );
-    assert.strictEqual(wrapper.childAt(0).props().variant, 'outlined');
-    assert.strictEqual(wrapper.childAt(0).props().color, 'default');
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.grouped), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedOutlined), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedOutlinedPrimary), false);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedOutlinedSecondary), false);
+    const button = wrapper.find('button');
+    assert.strictEqual(button.hasClass('MuiButton-outlined'), true);
+    assert.strictEqual(button.hasClass(classes.grouped), true);
+    assert.strictEqual(button.hasClass(classes.groupedOutlined), true);
+    assert.strictEqual(button.hasClass(classes.groupedOutlinedPrimary), false);
+    assert.strictEqual(button.hasClass(classes.groupedOutlinedSecondary), false);
   });
 
   it('should render an outlined primary button', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <ButtonGroup color="primary">
         <Button>Hello World</Button>
       </ButtonGroup>,
     );
-    assert.strictEqual(wrapper.childAt(0).props().variant, 'outlined');
-    assert.strictEqual(wrapper.childAt(0).props().color, 'primary');
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.grouped), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedOutlined), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedOutlinedPrimary), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedOutlinedSecondary), false);
+    const button = wrapper.find('div').find('button');
+    assert.strictEqual(button.hasClass('MuiButton-outlinedPrimary'), true);
+    assert.strictEqual(button.hasClass(classes.grouped), true);
+    assert.strictEqual(button.hasClass(classes.groupedOutlined), true);
+    assert.strictEqual(button.hasClass(classes.groupedOutlinedPrimary), true);
+    assert.strictEqual(button.hasClass(classes.groupedOutlinedSecondary), false);
   });
 
   it('should render an outlined secondary button', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <ButtonGroup color="secondary">
         <Button>Hello World</Button>
       </ButtonGroup>,
     );
-    assert.strictEqual(wrapper.childAt(0).props().variant, 'outlined');
-    assert.strictEqual(wrapper.childAt(0).props().color, 'secondary');
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.grouped), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedOutlined), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedOutlinedPrimary), false);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedOutlinedSecondary), true);
+    const button = wrapper.find('button');
+    assert.strictEqual(button.hasClass('MuiButton-outlinedSecondary'), true);
+    assert.strictEqual(button.hasClass(classes.grouped), true);
+    assert.strictEqual(button.hasClass(classes.groupedOutlined), true);
+    assert.strictEqual(button.hasClass(classes.groupedOutlinedPrimary), false);
+    assert.strictEqual(button.hasClass(classes.groupedOutlinedSecondary), true);
   });
 
   it('should render a contained button', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <ButtonGroup variant="contained">
         <Button>Hello World</Button>
       </ButtonGroup>,
     );
-    assert.strictEqual(wrapper.childAt(0).props().variant, 'contained');
-    assert.strictEqual(wrapper.childAt(0).props().color, 'default');
-    assert.strictEqual(wrapper.hasClass(classes.contained), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.grouped), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedContained), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedContainedPrimary), false);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedContainedSecondary), false);
+    const button = wrapper.find('button');
+    assert.strictEqual(button.hasClass('MuiButton-contained'), true);
+    assert.strictEqual(button.hasClass(classes.grouped), true);
+    assert.strictEqual(button.hasClass(classes.groupedContained), true);
+    assert.strictEqual(button.hasClass(classes.groupedContainedPrimary), false);
+    assert.strictEqual(button.hasClass(classes.groupedContainedSecondary), false);
   });
 
   it('should render a contained primary button', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <ButtonGroup variant="contained" color="primary">
         <Button>Hello World</Button>
       </ButtonGroup>,
     );
-    assert.strictEqual(wrapper.childAt(0).props().variant, 'contained');
-    assert.strictEqual(wrapper.childAt(0).props().color, 'primary');
-    assert.strictEqual(wrapper.hasClass(classes.contained), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.grouped), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedContained), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedContainedPrimary), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedContainedSecondary), false);
+    const button = wrapper.find('button');
+    assert.strictEqual(button.hasClass('MuiButton-containedPrimary'), true);
+    assert.strictEqual(button.hasClass(classes.grouped), true);
+    assert.strictEqual(button.hasClass(classes.groupedContained), true);
+    assert.strictEqual(button.hasClass(classes.groupedContainedPrimary), true);
+    assert.strictEqual(button.hasClass(classes.groupedContainedSecondary), false);
   });
 
   it('should render a contained secondary button', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <ButtonGroup variant="contained" color="secondary">
         <Button>Hello World</Button>
       </ButtonGroup>,
     );
-    assert.strictEqual(wrapper.childAt(0).props().variant, 'contained');
-    assert.strictEqual(wrapper.childAt(0).props().color, 'secondary');
-    assert.strictEqual(wrapper.hasClass(classes.contained), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.grouped), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedContained), true);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedContainedPrimary), false);
-    assert.strictEqual(wrapper.childAt(0).hasClass(classes.groupedContainedSecondary), true);
+    const button = wrapper.find('button');
+    assert.strictEqual(button.hasClass('MuiButton-containedSecondary'), true);
+    assert.strictEqual(button.hasClass(classes.grouped), true);
+    assert.strictEqual(button.hasClass(classes.groupedContained), true);
+    assert.strictEqual(button.hasClass(classes.groupedContainedPrimary), false);
+    assert.strictEqual(button.hasClass(classes.groupedContainedSecondary), true);
   });
 
   it('should render a small button', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <ButtonGroup size="small">
         <Button>Hello World</Button>
       </ButtonGroup>,
     );
-    assert.strictEqual(wrapper.childAt(0).props().size, 'small');
+    const button = wrapper.find('button');
+    assert.strictEqual(button.hasClass('MuiButton-sizeSmall'), true);
   });
 
   it('should render a large button', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <ButtonGroup size="large">
         <Button>Hello World</Button>
       </ButtonGroup>,
     );
-    assert.strictEqual(wrapper.childAt(0).props().size, 'large');;
+    const button = wrapper.find('button');
+    assert.strictEqual(button.hasClass('MuiButton-sizeLarge'), true);
   });
 
-  it('Button should have a ripple by default', () => {
-    const wrapper = shallow(
+  it('should have a ripple by default', () => {
+    const wrapper = mount(
       <ButtonGroup>
         <Button>Hello World</Button>
       </ButtonGroup>,
     );
-    assert.strictEqual(wrapper.childAt(0).props().disableRipple, false);
+    assert.strictEqual(wrapper.find('TouchRipple').exists(), true);
   });
 
-  it('should pass disableRipple to Button', () => {
-    const wrapper = shallow(
+  it('can disable the ripple', () => {
+    const wrapper = mount(
       <ButtonGroup disableRipple>
         <Button>Hello World</Button>
       </ButtonGroup>,
     );
-    assert.strictEqual(wrapper.childAt(0).props().disableRipple, true);
-  });
-
-  it('Button should have a focusRipple by default', () => {
-    const wrapper = shallow(
-      <ButtonGroup>
-        <Button>Hello World</Button>
-      </ButtonGroup>,
-    );
-    assert.strictEqual(wrapper.childAt(0).props().disableFocusRipple, false);
-  });
-
-  it('should pass disableFocusRipple to Button', () => {
-    const wrapper = shallow(
-      <ButtonGroup disableFocusRipple>
-        <Button>Hello World</Button>
-      </ButtonGroup>,
-    );
-    assert.strictEqual(wrapper.childAt(0).props().disableFocusRipple, true);
+    assert.strictEqual(wrapper.find('TouchRipple').exists(), false);
   });
 
   it('should not be fullWidth by default', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <ButtonGroup>
         <Button>Hello World</Button>
       </ButtonGroup>,
     );
+    const button = wrapper.find('button');
     assert.strictEqual(wrapper.hasClass(classes.fullWidth), false);
-    assert.strictEqual(wrapper.childAt(0).props().fullWidth, false);
+    assert.strictEqual(button.hasClass('MuiButton-fullWidth'), false);
   });
 
   it('should pass fullWidth to Button', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <ButtonGroup fullWidth>
         <Button>Hello World</Button>
       </ButtonGroup>,
     );
-    assert.strictEqual(wrapper.hasClass(classes.fullWidth), true);
-    assert.strictEqual(wrapper.childAt(0).props().fullWidth, true);
+    const buttonGroup = wrapper.find('div')
+    const button = wrapper.find('button');
+    assert.strictEqual(buttonGroup.hasClass(classes.fullWidth), true);
+    assert.strictEqual(button.hasClass('MuiButton-fullWidth'), true);
   });
 });
