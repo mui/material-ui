90c2d35f387e5b066ad617228c1cbd1db080e9e1
Peter Henson Valdes II <petev1991@gmail.com>
2019-06-01 16:05:35 -0700

    [example] Fix ssr example to work on Windows (#15949)
    * ssr start script updated to work on windows
    
    * fix CSS mismatch
    
    * script order fix for windows
    
    * use npm-run-all
    
    * fix eslint

--- examples/ssr/server.js
+++ examples/ssr/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles';
 import App from './App';
 import theme from './theme';
@@ -31,6 +32,8 @@ function handleRender(req, res) {
   const html = ReactDOMServer.renderToString(
     sheets.collect(
       <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
         <App />
       </ThemeProvider>,
     ),
@@ -51,4 +54,7 @@ app.use('/build', express.static('build'));
 app.use(handleRender);
 
 const port = 3000;
-app.listen(port);
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Listening on ${port}`);
+});
