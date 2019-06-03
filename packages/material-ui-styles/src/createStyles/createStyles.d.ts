f4dc7bbf32b3c9655e12b73419e5e55b9a6c0532
Kristoffer K <merceyz@users.noreply.github.com>
2019-06-03 13:57:07 +0200

    [styles] Fix createStyles for TypeScript v3.5 (#15990)
    * [styles] Fix createStyles for TypeScript v3.5
    
    * Change link

--- packages/material-ui-styles/src/createStyles/createStyles.d.ts
+++ packages/material-ui-styles/src/createStyles/createStyles.d.ts
@@ -8,6 +8,9 @@ import { StyleRules } from '@material-ui/styles/withStyles';
  * @param styles a set of style mappings
  * @returns the same styles that were passed in
  */
-export default function createStyles<C extends string, P extends object>(
+// For TypeScript v3.5 P has to extend {} instead of object
+// See https://github.com/mui-org/material-ui/issues/15942
+// and https://github.com/microsoft/TypeScript/issues/31735
+export default function createStyles<C extends string, P extends {}>(
   styles: StyleRules<P, C>,
 ): StyleRules<P, C>;
