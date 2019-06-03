6cf7cea0b1e5d8df76dd64a48518e10dcbbbc861
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-06-03 20:13:45 +0200

    [docs] Fix IE 11 crash in material-table demos

--- docs/src/pages/components/tables/MaterialTableDemo.js
+++ docs/src/pages/components/tables/MaterialTableDemo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MaterialTable from 'material-table';
+import Promise from 'core-js-pure/features/promise'; // required for IE 11
 
 export default function MaterialTableDemo() {
   const [state, setState] = React.useState({
