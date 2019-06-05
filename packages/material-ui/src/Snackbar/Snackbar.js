9c353a4574fb22c8ee0c8371698f90fca7576f2c
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-06-05 17:46:24 +0200

    [Snackbar] Fix wrong event call (#16070)

--- packages/material-ui/src/Snackbar/Snackbar.js
+++ packages/material-ui/src/Snackbar/Snackbar.js
@@ -149,7 +149,9 @@ const Snackbar = React.forwardRef(function Snackbar(props, ref) {
   );
 
   React.useEffect(() => {
-    if (open) setAutoHideTimer();
+    if (open) {
+      setAutoHideTimer();
+    }
 
     return () => {
       clearTimeout(timerAutoHide.current);
@@ -203,7 +205,7 @@ const Snackbar = React.forwardRef(function Snackbar(props, ref) {
   };
 
   React.useEffect(() => {
-    if (!disableWindowBlurListener) {
+    if (!disableWindowBlurListener && open) {
       window.addEventListener('focus', handleResume);
       window.addEventListener('blur', handlePause);
 
@@ -214,7 +216,7 @@ const Snackbar = React.forwardRef(function Snackbar(props, ref) {
     }
 
     return undefined;
-  }, [disableWindowBlurListener, handleResume]);
+  }, [disableWindowBlurListener, handleResume, open]);
 
   // So we only render active snackbars.
   if (!open && exited) {
