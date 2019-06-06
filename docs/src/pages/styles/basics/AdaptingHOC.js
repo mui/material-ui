c53e625c12105b09946986e033b6fd65c8e74648
Aditya Shankar <49206592+aditya1906@users.noreply.github.com>
2019-06-06 08:44:11 +0530

    [docs] use string template in AdaptingHOC.js

--- docs/src/pages/styles/basics/AdaptingHOC.js
+++ docs/src/pages/styles/basics/AdaptingHOC.js
@@ -6,15 +6,14 @@ import Button from '@material-ui/core/Button';
 const styles = {
   root: {
     background: props =>
-      props.color === 'red'
-        ? 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
-        : 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+      `linear-gradient(45deg, ${props.color === 'red'
+        ? '#FE6B8B 30%, #FF8E53' : '#2196F3 30%, #21CBF3'}
+      90%)`,
     border: 0,
     borderRadius: 3,
     boxShadow: props =>
-      props.color === 'red'
-        ? '0 3px 5px 2px rgba(255, 105, 135, .3)'
-        : '0 3px 5px 2px rgba(33, 203, 243, .3)',
+      `0 3px 5px 2px rgba(${props.color === 'red'
+          ? '255, 105, 135, .3' : '33, 203, 243, .3'})`,
     color: 'white',
     height: 48,
     padding: '0 30px',
