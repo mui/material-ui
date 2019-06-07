785a6841e85a070258251fa2dbdde6adfc7bae68
Matthew Brookes <github@nospam.33m.co>
2019-06-07 23:16:37 +0100

    remove stray commented code

--- docs/src/pages/components/tables/CustomPaginationActionsTable.js
+++ docs/src/pages/components/tables/CustomPaginationActionsTable.js
@@ -18,7 +18,6 @@ const useStyles1 = makeStyles(theme => ({
   root: {
     flexShrink: 0,
     color: theme.palette.text.secondary,
-    // marginLeft: theme.spacing(2.5),
   },
 }));
 
--- docs/src/pages/components/tables/CustomPaginationActionsTable.tsx
+++ docs/src/pages/components/tables/CustomPaginationActionsTable.tsx
@@ -19,7 +19,6 @@ const useStyles1 = makeStyles((theme: Theme) =>
     root: {
       flexShrink: 0,
       color: theme.palette.text.secondary,
-      // marginLeft: theme.spacing(2.5),
     },
   }),
 );
