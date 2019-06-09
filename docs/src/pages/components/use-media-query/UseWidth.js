71ee401bf4397fdec5774443c6389c93b4582e42
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-06-09 17:16:06 +0200

    [docs] Revert unreleased changes to the useMediaQuery API

--- docs/src/pages/components/use-media-query/UseWidth.js
+++ docs/src/pages/components/use-media-query/UseWidth.js
@@ -11,10 +11,11 @@ import { createMuiTheme } from '@material-ui/core/styles';
 function useWidth() {
   const theme = useTheme();
   const keys = [...theme.breakpoints.keys].reverse();
-  const queries = useMediaQuery(keys.map(key => theme.breakpoints.only(key)));
   return (
-    queries.reduce((output, matches, index) => {
-      return !output && matches ? keys[index] : output;
+    keys.reduce((output, key) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const matches = useMediaQuery(theme.breakpoints.only(key));
+      return !output && matches ? key : output;
     }, null) || 'xs'
   );
 }
