b0655d9f99e3578818d1e6fa4e33d91440f72219
Bernard <bernardwang@users.noreply.github.com>
2019-06-09 14:51:19 -0500

    [docs] Fix typo in the Gatsby example (#16130)

--- examples/gatsby/src/components/Link.js
+++ examples/gatsby/src/components/Link.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import MuiLink from '@material-ui/core/Link';
-import { Link as GastsbyLink } from 'gatsby';
+import { Link as GatsbyLink } from 'gatsby';
 
 const Link = React.forwardRef(function Link(props, ref) {
-  return <MuiLink component={GastsbyLink} ref={ref} {...props} />;
+  return <MuiLink component={GatsbyLink} ref={ref} {...props} />;
 });
 
 export default Link;
