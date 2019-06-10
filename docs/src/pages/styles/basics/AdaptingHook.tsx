c349f1d9f99238616da0c6fab3cb66ec47b8210b
Kristoffer K <merceyz@users.noreply.github.com>
2019-06-10 09:10:33 +0200

    [docs] Add AdaptingHook TypeScript demo (#16131)
    * [docs] Add typescript version of AdaptingHook
    
    * [docs] Update duplicated demo

--- docs/src/pages/styles/basics/AdaptingHook.tsx
+++ docs/src/pages/styles/basics/AdaptingHook.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles, createStyles } from '@material-ui/styles';
 import Button, { ButtonProps as MuiButtonProps } from '@material-ui/core/Button';
 import { Omit } from '@material-ui/types';
 
@@ -8,24 +8,26 @@ interface Props {
   color: 'red' | 'blue';
 }
 
-const useStyles = makeStyles({
-  root: {
-    background: (props: Props) =>
-      props.color === 'red'
-        ? 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
-        : 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: (props: Props) =>
-      props.color === 'red'
-        ? '0 3px 5px 2px rgba(255, 105, 135, .3)'
-        : '0 3px 5px 2px rgba(33, 203, 243, .3)',
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-    margin: 8,
-  },
-});
+const useStyles = makeStyles(
+  createStyles({
+    root: {
+      background: (props: Props) =>
+        props.color === 'red'
+          ? 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
+          : 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: (props: Props) =>
+        props.color === 'red'
+          ? '0 3px 5px 2px rgba(255, 105, 135, .3)'
+          : '0 3px 5px 2px rgba(33, 203, 243, .3)',
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+      margin: 8,
+    },
+  }),
+);
 
 function MyButton(props: Props & Omit<MuiButtonProps, keyof Props>) {
   const { color, ...other } = props;
