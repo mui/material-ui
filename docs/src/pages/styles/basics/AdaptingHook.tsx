f04efa330a8c94fc18c57764697e5456cfcd8866
merceyz <merceyz@users.noreply.github.com>
2019-06-10 02:04:15 +0200

    [docs] Add typescript version of AdaptingHook

---
+++ docs/src/pages/styles/basics/AdaptingHook.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles, createStyles } from '@material-ui/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(
+  createStyles({
+    root: {
+      background: (props: MyButtonProps) =>
+        props.color === 'red'
+          ? 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
+          : 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: (props: MyButtonProps) =>
+        props.color === 'red'
+          ? '0 3px 5px 2px rgba(255, 105, 135, .3)'
+          : '0 3px 5px 2px rgba(33, 203, 243, .3)',
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+      margin: 8,
+    },
+  }),
+);
+
+interface MyButtonProps {
+  color: 'red' | 'blue';
+  children: React.ReactNode;
+}
+
+function MyButton(props: MyButtonProps) {
+  const { color, ...other } = props;
+  const classes = useStyles(props);
+  return <Button className={classes.root} {...other} />;
+}
+
+MyButton.propTypes = {
+  color: PropTypes.oneOf(['red', 'blue']).isRequired,
+};
+
+export default function AdaptingHook() {
+  return (
+    <React.Fragment>
+      <MyButton color="red">Red</MyButton>
+      <MyButton color="blue">Blue</MyButton>
+    </React.Fragment>
+  );
+}
