5e83ee90c91aa022cd1cce0e4f39effefd7443a7
Aditya <adityashnkr5@gmail.com>
2019-06-12 10:59:11 +0530

    Prettify WhiteSpace.js

--- docs/src/pages/system/display/WhiteSpace.js
+++ docs/src/pages/system/display/WhiteSpace.js
@@ -4,12 +4,10 @@ import Box from '@material-ui/core/Box';
 function WhiteSpace() {
   return (
     <div>
-      <Box component="div" overflow="scroll" whiteSpace="nowrap" bgcolor="background.paper">
-        Lorem Ipsum is simply dummy text of the printing.
+      <Box component="div" whiteSpace="nowrap" bgcolor="background.paper">
         Lorem Ipsum is simply dummy text of the printing.
       </Box>
       <Box component="div" whiteSpace="normal" bgcolor="background.paper">
-        Lorem Ipsum is simply dummy text of the printing.
         Lorem Ipsum is simply dummy text of the printing.
       </Box>
     </div>
