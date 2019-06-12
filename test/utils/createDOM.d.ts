2f1ab43f9b203443c778072e2af4bafd77695843
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-06-12 22:36:41 +0200

    Focus global types

--- test/utils/createDOM.d.ts
+++ test/utils/createDOM.d.ts
@@ -1,5 +1,5 @@
 // there's probably a broader solution e.g. levering DOMWindow from 'jsodm'
-// interface Window extends DOMWindow doesn't work because jsdom typings use 
+// interface Window extends DOMWindow doesn't work because jsdom typings use
 // triple slash directives. Technical dom.lib.d.ts should already have these properties
 interface Window {
   HTMLParagraphElement: HTMLParagraphElement;
