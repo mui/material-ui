31942545fdb511f274776eb8de14543a1b102c6d
merceyz <merceyz@users.noreply.github.com>
2019-06-13 03:09:32 +0200

    Upgrade babel-plugin-tester

--- packages/babel-plugin-unwrap-createStyles/test/unwrapCreateStyles.test.js
+++ packages/babel-plugin-unwrap-createStyles/test/unwrapCreateStyles.test.js
@@ -5,6 +5,7 @@ import unwrapCreateStylesPlugin from '../src';
 pluginTester({
   plugin: unwrapCreateStylesPlugin,
   pluginName: 'unwrapCreateStylesPlugin',
+  endOfLine: 'preserve',
   fixtures: path.join(__dirname, '__fixtures__'),
   babelOptions: {
     root: __dirname,
