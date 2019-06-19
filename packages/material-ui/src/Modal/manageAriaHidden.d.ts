6609918b58b53e336cc3f60fec2c896cd2bf2627
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-06-19 16:32:36 +0200

    try to simplify the logic

--- packages/material-ui/src/Modal/manageAriaHidden.d.ts
+++
@@ -1,8 +0,0 @@
-export function ariaHidden(node: Node, show: boolean): never;
-export function ariaHiddenSiblings(
-  container: Element,
-  mountNode: Node,
-  currentNode: Node,
-  nodesToExclude: Node[],
-  show: boolean,
-): never;
