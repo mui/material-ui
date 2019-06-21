305e057123eacc5d003ada1378967793dcd3a2e4
merceyz <merceyz@users.noreply.github.com>
2019-06-21 15:33:40 +0200

    components/click-away-listener/ClickAway

--- docs/src/pages/components/click-away-listener/ClickAway.tsx
+++ docs/src/pages/components/click-away-listener/ClickAway.tsx
@@ -1,30 +1,32 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import { grey } from '@material-ui/core/colors';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    position: 'relative',
-  },
-  paper: {
-    position: 'absolute',
-    top: 36,
-    right: 0,
-    left: 0,
-  },
-  fake: {
-    backgroundColor: grey[200],
-    height: theme.spacing(1),
-    margin: theme.spacing(2),
-    // Selects every two elements among any group of siblings.
-    '&:nth-child(2n)': {
-      marginRight: theme.spacing(3),
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      position: 'relative',
     },
-  },
-}));
+    paper: {
+      position: 'absolute',
+      top: 36,
+      right: 0,
+      left: 0,
+    },
+    fake: {
+      backgroundColor: grey[200],
+      height: theme.spacing(1),
+      margin: theme.spacing(2),
+      // Selects every two elements among any group of siblings.
+      '&:nth-child(2n)': {
+        marginRight: theme.spacing(3),
+      },
+    },
+  }),
+);
 
 export default function ClickAway() {
   const [open, setOpen] = React.useState(false);
