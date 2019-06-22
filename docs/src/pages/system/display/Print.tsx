4baa830f312514e76a0f568e5b24314aae843fba
merceyz <merceyz@users.noreply.github.com>
2019-06-22 16:54:16 +0200

    system/display/Print

---
+++ docs/src/pages/system/display/Print.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+
+export default function Print() {
+  return (
+    <div style={{ width: '100%' }}>
+      <Box display="block" displayPrint="none" m={1}>
+        Screen Only (Hide on print only)
+      </Box>
+      <Box display="none" displayPrint="block" m={1}>
+        Print Only (Hide on screen only)
+      </Box>
+    </div>
+  );
+}
