bc691a5a378c431b9498c6ac4b3df99cb3833110
merceyz <merceyz@users.noreply.github.com>
2019-06-22 15:46:58 +0200

    guides/right-to-left/RtlOptOut

---
+++ docs/src/pages/guides/right-to-left/RtlOptOut.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      marginTop: theme.spacing(4),
+      marginRight: theme.spacing(2),
+    },
+    affected: {
+      textAlign: 'right',
+    },
+    unaffected: {
+      flip: false,
+      textAlign: 'right',
+    },
+  }),
+);
+
+export default function RtlOptOut() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.affected}>Affected</div>
+      <div className={classes.unaffected}>Unaffected</div>
+    </div>
+  );
+}
