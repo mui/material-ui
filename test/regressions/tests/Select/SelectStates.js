634f5c1ddfd16fe69a1c0223697eebac64febd24
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-06-22 12:01:34 +0200

    Reduce test flakyness

--- test/regressions/tests/Select/SelectStates.js
+++ test/regressions/tests/Select/SelectStates.js
@@ -5,7 +5,7 @@ import Select from '@material-ui/core/Select';
 function SelectOverflow() {
   return (
     <div style={{ height: 130, width: 100 }}>
-      <Select open value="first">
+      <Select MenuProps={{ transitionDuration: 0 }} open value="first">
         <MenuItem value="first">First</MenuItem>
         <MenuItem autoFocus value="second">
           Second
