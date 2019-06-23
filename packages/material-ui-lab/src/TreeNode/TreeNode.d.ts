5e77244eedce621bd512ce3b7d11df730e77a757
joshwooding <12938082+joshwooding@users.noreply.github.com>
2019-06-23 16:05:43 +0100

    Prettier

--- packages/material-ui-lab/src/TreeNode/TreeNode.d.ts
+++ packages/material-ui-lab/src/TreeNode/TreeNode.d.ts
@@ -3,11 +3,11 @@ import { StandardProps } from '@material-ui/core';
 
 export interface TreeViewProps
   extends StandardProps<React.HTMLAttributes<HTMLDivElement>, TreeViewClassKey> {
-    collapseIcon?: React.ReactNode;
-    expandIcon?: React.ReactNode;
-    icon?: React.ReactNode;
-    label?: React.ReactNode;
-    nodeId: string;
+  collapseIcon?: React.ReactNode;
+  expandIcon?: React.ReactNode;
+  icon?: React.ReactNode;
+  label?: React.ReactNode;
+  nodeId: string;
 }
 
 export type TreeViewClassKey = 'root' | 'group' | 'content' | 'iconContainer' | 'label';
--- packages/material-ui-lab/src/TreeView/TreeView.d.ts
+++ packages/material-ui-lab/src/TreeView/TreeView.d.ts
@@ -3,10 +3,10 @@ import { StandardProps } from '@material-ui/core';
 
 export interface TreeViewProps
   extends StandardProps<React.HTMLAttributes<HTMLDivElement>, TreeViewClassKey> {
-    defaultChildIcon?: React.ReactNode;
-    defaultCollapseIcon?: React.ReactNode;
-    defaultEndIcon?: React.ReactNode;
-    defaultExpandIcon?: React.ReactNode;
+  defaultChildIcon?: React.ReactNode;
+  defaultCollapseIcon?: React.ReactNode;
+  defaultEndIcon?: React.ReactNode;
+  defaultExpandIcon?: React.ReactNode;
 }
 
 export type TreeViewClassKey = 'root';
