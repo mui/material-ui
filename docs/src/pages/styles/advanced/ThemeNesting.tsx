e0810a6ca14f08d390f623927095038225d9b3a3
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-06-24 11:26:26 +0200

    Prefer ThemeProvider<ThemeType>

--- docs/src/pages/styles/advanced/ThemeNesting.tsx
+++ docs/src/pages/styles/advanced/ThemeNesting.tsx
@@ -34,7 +34,7 @@ function DeepChild() {
 export default function ThemeNesting() {
   return (
     <div>
-      <ThemeProvider
+      <ThemeProvider<MyTheme>
         theme={{
           background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
           boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
@@ -43,7 +43,7 @@ export default function ThemeNesting() {
         <DeepChild />
         <br />
         <br />
-        <ThemeProvider
+        <ThemeProvider<MyTheme>
           theme={outerTheme => ({
             ...outerTheme,
             background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
