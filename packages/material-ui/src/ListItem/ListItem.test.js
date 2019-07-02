10df4bef9dcab40e14b432c858056e37eeeddf52
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-07-02 12:29:57 +0000

    [ListItem] testing-library

--- packages/material-ui/src/ListItem/ListItem.test.js
+++ packages/material-ui/src/ListItem/ListItem.test.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { assert } from 'chai';
+import { expect } from 'chai';
 import PropTypes from 'prop-types';
-import { getClasses, createMount, findOutermostIntrinsic } from '@material-ui/core/test-utils';
+import { getClasses, createMount } from '@material-ui/core/test-utils';
 import describeConformance from '../test-utils/describeConformance';
 import consoleErrorMock from 'test/utils/consoleErrorMock';
+import {
+  act,
+  cleanup,
+  createClientRender,
+  fireEvent,
+  queries,
+} from 'test/utils/createClientRender';
 import ListItemText from '../ListItemText';
 import ListItemSecondaryAction from '../ListItemSecondaryAction';
 import ListItem from './ListItem';
-import ButtonBase from '../ButtonBase';
 import ListContext from '../List/ListContext';
 
 const NoContent = React.forwardRef(() => {
@@ -16,12 +22,17 @@ const NoContent = React.forwardRef(() => {
 
 describe('<ListItem />', () => {
   let mount;
+  const render = createClientRender({ strict: false });
   let classes;
 
   before(() => {
     classes = getClasses(<ListItem />);
     // StrictModeViolation: uses ButtonBase
     mount = createMount({ strict: false });
+  });
+
+  afterEach(() => {
+    cleanup();
   });
 
   after(() => {
@@ -36,34 +47,31 @@ describe('<ListItem />', () => {
   }));
 
   it('should render with gutters classes', () => {
-    const wrapper = mount(<ListItem className="woofListItem" />);
-    const listItem = wrapper.find('li');
-    assert.strictEqual(listItem.hasClass(classes.gutters), true);
+    const { getByRole } = render(<ListItem />);
+    expect(getByRole('listitem')).to.have.class(classes.gutters);
   });
 
   it('should render with the selected class', () => {
-    const wrapper = mount(<ListItem selected />);
-    const listItem = wrapper.find('li');
-    assert.strictEqual(listItem.hasClass(classes.selected), true);
+    const { getByRole } = render(<ListItem selected />);
+    expect(getByRole('listitem')).to.have.class(classes.selected);
   });
 
   it('should disable the gutters', () => {
-    const wrapper = mount(<ListItem disableGutters />);
-    const listItem = wrapper.find('li');
-    assert.strictEqual(listItem.hasClass(classes.gutters), false);
+    const { getByRole } = render(<ListItem disableGutters />);
+    expect(getByRole('listitem')).not.to.have.class(classes.gutters);
   });
 
   describe('prop: button', () => {
     it('renders a div', () => {
-      const wrapper = mount(<ListItem button />);
-      assert.strictEqual(findOutermostIntrinsic(wrapper).type(), 'div');
+      const { container } = render(<ListItem button />);
+      expect(container.firstChild).to.have.property('nodeName', 'DIV');
     });
   });
 
   describe('context: dense', () => {
     it('should forward the context', () => {
       let context = null;
-      const wrapper = mount(
+      const { setProps } = render(
         <ListItem>
           <ListContext.Consumer>
             {options => {
@@ -72,71 +80,77 @@ describe('<ListItem />', () => {
           </ListContext.Consumer>
         </ListItem>,
       );
-      assert.strictEqual(context.dense, false);
-      wrapper.setProps({ dense: true });
-      assert.strictEqual(context.dense, true);
+      expect(context).to.have.property('dense', false);
+      setProps({ dense: true });
+      expect(context).to.have.property('dense', true);
     });
   });
 
   describe('secondary action', () => {
     it('should wrap with a container', () => {
-      const wrapper = mount(
+      const { getByRole } = render(
         <ListItem>
           <ListItemText primary="primary" />
           <ListItemSecondaryAction />
         </ListItem>,
       );
-      const listItem = findOutermostIntrinsic(wrapper);
-      assert.strictEqual(listItem.hasClass(classes.container), true);
-      assert.strictEqual(wrapper.find('li > div').exists(), true);
+      const listItem = getByRole('listitem');
+
+      expect(listItem).to.have.class(classes.container);
+      expect(listItem.querySelector(`div.${classes.root}`)).to.be.ok;
     });
 
     it('should accept a component property', () => {
-      const wrapper = mount(
+      const { getByRole } = render(
         <ListItem component="span">
           <ListItemText primary="primary" />
           <ListItemSecondaryAction />
         </ListItem>,
       );
-      const listItem = findOutermostIntrinsic(wrapper);
-      assert.strictEqual(listItem.hasClass(classes.container), true);
-      assert.strictEqual(wrapper.find('li > span').exists(), true);
+      const listItem = getByRole('listitem');
+
+      expect(listItem).to.have.class(classes.container);
+      expect(listItem.querySelector(`span.${classes.root}`)).to.be.ok;
     });
 
     it('should accept a button property', () => {
-      const wrapper = mount(
+      const { getByRole } = render(
         <ListItem button>
           <ListItemText primary="primary" />
           <ListItemSecondaryAction />
         </ListItem>,
       );
-      const listItem = findOutermostIntrinsic(wrapper);
-      assert.strictEqual(listItem.hasClass(classes.container), true);
-      assert.strictEqual(listItem.childAt(0).type(), ButtonBase);
+      const listItem = getByRole('listitem');
+
+      expect(listItem).to.have.class(classes.container);
+      expect(queries.getByRole(listItem, 'button')).to.be.ok;
     });
 
     it('should accept a ContainerComponent property', () => {
-      const wrapper = mount(
-        <ListItem ContainerComponent="div">
+      const { getByRole } = render(
+        <ListItem ContainerComponent="div" ContainerProps={{ role: 'listitem' }}>
           <ListItemText primary="primary" />
           <ListItemSecondaryAction />
         </ListItem>,
       );
-      const listItem = wrapper.find('div').first();
-      assert.strictEqual(listItem.hasClass(classes.container), true);
-      assert.strictEqual(wrapper.find('div > div').exists(), true);
+      const listItem = getByRole('listitem');
+
+      expect(listItem).to.have.property('nodeName', 'DIV');
+      expect(listItem).to.have.class(classes.container);
+      expect(listItem.querySelector(`div.${classes.root}`)).to.be.ok;
     });
 
     it('should allow customization of the wrapper', () => {
-      const wrapper = mount(
-        <ListItem ContainerProps={{ className: 'bubu' }}>
+      const { getByRole } = render(
+        <ListItem ContainerProps={{ className: 'bubu', role: 'listitem' }}>
           <ListItemText primary="primary" />
           <ListItemSecondaryAction />
         </ListItem>,
       );
-      const listItem = findOutermostIntrinsic(wrapper);
-      assert.strictEqual(listItem.hasClass(classes.container), true);
-      assert.strictEqual(listItem.hasClass('bubu'), true);
+      const listItem = getByRole('listitem');
+
+      expect(listItem).to.have.class(classes.container);
+      expect(listItem).to.have.class('bubu');
     });
 
     describe('warnings', () => {
@@ -150,25 +164,24 @@ describe('<ListItem />', () => {
       });
 
       it('warns if it cant detect the secondary action properly', () => {
-        mount(
+        render(
           <ListItem>
             <ListItemSecondaryAction>I should have come last :(</ListItemSecondaryAction>
             <ListItemText>My position doesn not matter.</ListItemText>
           </ListItem>,
         );
 
-        assert.strictEqual(consoleErrorMock.callCount(), 1);
-        assert.include(
-          consoleErrorMock.args()[0][0],
+        expect(consoleErrorMock.callCount()).to.equal(1);
+        expect(consoleErrorMock.args()[0][0]).to.include(
           'Warning: Failed prop type: Material-UI: you used an element',
         );
       });
 
       it('should warn (but not error) with autoFocus with a function component with no content', () => {
-        mount(<ListItem component={NoContent} autoFocus />);
-        assert.strictEqual(consoleErrorMock.callCount(), 1);
-        assert.include(
-          consoleErrorMock.args()[0][0],
+        render(<ListItem component={NoContent} autoFocus />);
+
+        expect(consoleErrorMock.callCount()).to.equal(1);
+        expect(consoleErrorMock.args()[0][0]).to.include(
           'Warning: Material-UI: unable to set focus to a ListItem whose component has not been rendered.',
         );
       });
@@ -177,13 +190,18 @@ describe('<ListItem />', () => {
 
   describe('prop: focusVisibleClassName', () => {
     it('should merge the class names', () => {
-      const wrapper = mount(<ListItem button focusVisibleClassName="focusVisibleClassName" />);
-      const listItem = wrapper.find(ButtonBase);
-      assert.strictEqual(listItem.props().component, 'div');
-      assert.strictEqual(
-        listItem.props().focusVisibleClassName,
-        `${classes.focusVisible} focusVisibleClassName`,
+      const { getByRole } = render(
+        <ListItem button focusVisibleClassName="focusVisibleClassName" />,
       );
+      const button = getByRole('button');
+
+      act(() => {
+        fireEvent.keyDown(document.activeElement || document.body, { key: 'Tab' });
+        button.focus();
+      });
+
+      expect(button).to.have.class('focusVisibleClassName');
+      expect(button).to.have.class(classes.focusVisible);
     });
   });
 });
