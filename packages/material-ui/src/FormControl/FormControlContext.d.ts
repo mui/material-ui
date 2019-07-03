8e90ac7ffaeaf92811d7e6b691b7d3739f5d3e81
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-07-03 15:22:41 +0000

    Fix dtslint failure

--- packages/material-ui/src/FormControl/FormControlContext.d.ts
+++ packages/material-ui/src/FormControl/FormControlContext.d.ts
@@ -1,6 +1,9 @@
 import { Context } from 'react';
 import { FormControlProps } from './FormControl';
 
+// shut off automatic exporting
+export {};
+
 type ContextFromPropsKey = 'disabled' | 'error' | 'margin' | 'required' | 'variant';
 
 export interface FormControl extends Pick<FormControlProps, ContextFromPropsKey> {
