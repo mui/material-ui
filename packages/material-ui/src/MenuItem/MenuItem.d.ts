143124dc944f61c9f4456ea3cd7e4a3e6cebd668
Kristoffer K <merceyz@users.noreply.github.com>
2019-07-04 10:46:12 +0200

    [MenuItem] Fix type deceleration not using MenuItemClassKey (#16358)
    * [core] use declared MenuItemClassKey
    
    * Import omit

--- packages/material-ui/src/MenuItem/MenuItem.d.ts
+++ packages/material-ui/src/MenuItem/MenuItem.d.ts
@@ -1,14 +1,22 @@
 import { ListItemTypeMap } from '../ListItem';
 import { OverridableComponent, OverrideProps } from '../OverridableComponent';
 import { ExtendButtonBase } from '../ButtonBase';
+import { Omit } from '@material-ui/types';
 
 export type MenuItemClassKey = 'root' | 'gutters' | 'selected';
 
-declare const MenuItem: OverridableComponent<ListItemTypeMap<{ button: false }, 'li'>> &
-  ExtendButtonBase<ListItemTypeMap<{ button?: true }, 'li'>>;
+export type MenuItemTypeMap<P, D extends React.ElementType> = Omit<
+  ListItemTypeMap<P, D>,
+  'classKey'
+> & {
+  classKey: MenuItemClassKey;
+};
+
+declare const MenuItem: OverridableComponent<MenuItemTypeMap<{ button: false }, 'li'>> &
+  ExtendButtonBase<MenuItemTypeMap<{ button?: true }, 'li'>>;
 
 export type MenuItemProps<D extends React.ElementType = 'li', P = {}> = OverrideProps<
-  ListItemTypeMap<P, D>,
+  MenuItemTypeMap<P, D>,
   D
 >;
 
