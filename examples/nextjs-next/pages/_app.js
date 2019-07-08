1dc997642a1a16d6a7487737791b274ab74f4525
Jan Potoms <potoms.jan@gmail.com>
2019-07-08 14:01:30 +0200

    remove .parentNode check

--- examples/nextjs-next/pages/_app.js
+++ examples/nextjs-next/pages/_app.js
@@ -9,7 +9,7 @@ class MyApp extends App {
   componentDidMount() {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles && jssStyles.parentNode) {
+    if (jssStyles) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
   }
