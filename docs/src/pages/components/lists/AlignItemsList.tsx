3557d2642cc3fa689ae5a2a00e3570fe7616db0f
William Malone <wmalone@alteryx.com>
2019-07-10 22:02:51 -0600

    Updated additional alt

--- docs/src/pages/components/lists/AlignItemsList.tsx
+++ docs/src/pages/components/lists/AlignItemsList.tsx
@@ -72,7 +72,7 @@ export default function AlignItemsList() {
       <Divider variant="inset" component="li" />
       <ListItem alignItems="flex-start">
         <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/3.jpg" />
+          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
         </ListItemAvatar>
         <ListItemText
           primary="Oui Oui"
