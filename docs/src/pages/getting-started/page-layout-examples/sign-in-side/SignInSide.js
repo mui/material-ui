0d1944f05fa3b5889278fc3946d0e4b08a73ffcf
Jay <wangjiping@gmail.com>
2019-07-12 13:06:05 -0400

    Update SignInSide.js
    fix TypeScript check error.

--- docs/src/pages/getting-started/page-layout-examples/sign-in-side/SignInSide.js
+++ docs/src/pages/getting-started/page-layout-examples/sign-in-side/SignInSide.js
@@ -61,7 +61,7 @@ export default function SignInSide() {
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+      <Grid item xs={12} sm={8} md={5} component={() => <Paper square elevation={6} />}>
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
