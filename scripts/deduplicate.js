e51c9251ad0e629a9a6fba611c60bde55c806d34
merceyz <merceyz@users.noreply.github.com>
2019-07-17 17:31:35 +0200

    Remove postinstall check, remove function wrapper

--- scripts/deduplicate.js
+++ scripts/deduplicate.js
@@ -5,60 +5,38 @@ const path = require('path');
 const fs = require('fs');
 const deduplicate = require('yarn-deduplicate');
 
-function deduplicatePackages() {
-  return new Promise(resolve => {
-    const lockFile = path.resolve(__dirname, '../yarn.lock');
-    const yarnlock = fs.readFileSync(lockFile, 'utf8');
+const lockFile = path.resolve(__dirname, '../yarn.lock');
+const yarnlock = fs.readFileSync(lockFile, 'utf8');
 
-    const duplicates = deduplicate.listDuplicates(yarnlock);
-    if (duplicates.length === 0) {
-      console.log('No duplicated packages found');
-      resolve();
-      return;
-    }
-
-    console.log(
-      `${duplicates.length} duplicated package(s) found\n${duplicates
-        .map(x => `  ${x}`)
-        .join('\n')}`,
-    );
-
-    if (process.env.CI) {
-      console.error(
-        [
-          `Error: There are currently ${duplicates.length} duplicated package(s).`,
-          `To deduplicate run "yarn deduplicate"`,
-        ].join('\n'),
-      );
-      process.exit(1);
-    }
-
-    console.log('Deduplicating package(s)');
-    fs.writeFileSync(lockFile, deduplicate.fixDuplicates(yarnlock));
-
-    const yarn = spawn('yarn', {
-      shell: true,
-      stdio: 'inherit',
-      cwd: path.resolve(__dirname, '..'),
-      env: { NOYARNPOSTINSTALL: 1 },
-    });
-
-    yarn.on('close', code => {
-      if (code !== 0) {
-        process.exit(code);
-      }
-
-      resolve();
-    });
-  });
+const duplicates = deduplicate.listDuplicates(yarnlock);
+if (duplicates.length === 0) {
+  console.log('No duplicated packages found');
+  process.exit(0);
 }
 
-async function run() {
-  if (process.env.NOYARNPOSTINSTALL) {
-    return;
-  }
+console.log(
+  `${duplicates.length} duplicated package(s) found\n${duplicates.map(x => `  ${x}`).join('\n')}`,
+);
 
-  await deduplicatePackages();
+if (process.env.CI) {
+  console.error(
+    [
+      `Error: There are currently ${duplicates.length} duplicated package(s).`,
+      `To deduplicate run "yarn deduplicate"`,
+    ].join('\n'),
+  );
+  process.exit(1);
 }
 
-run();
+console.log('Deduplicating package(s)');
+fs.writeFileSync(lockFile, deduplicate.fixDuplicates(yarnlock));
+
+const yarn = spawn('yarn', {
+  shell: true,
+  stdio: 'inherit',
+  cwd: path.resolve(__dirname, '..'),
+});
+
+yarn.on('close', code => {
+  process.exit(code);
+});
