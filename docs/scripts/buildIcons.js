3830eb7509d405edc6eef51b7d02d2ff104f2a54
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-07-18 15:03:49 +0200

    Move docs:icons to docs workspace

--- docs/scripts/buildIcons.js
+++ docs/scripts/buildIcons.js
@@ -4,8 +4,8 @@ const path = require('path');
 const gm = require('gm');
 
 const SIZES = [48, 70, 96, 150, 152, 192, 256, 310, 384, 512];
-const INPUT_ICON = path.join(__dirname, '../../static/logo.png');
-const OUTPUT_DIR = path.join(__dirname, '../../static/icons');
+const INPUT_ICON = path.join(__dirname, '../static/logo.png');
+const OUTPUT_DIR = path.join(__dirname, '../static/icons');
 
 console.log('Generating Icons');
 
