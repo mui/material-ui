fa0edab87760ace797ea8405d7684f7060db00fb
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-07-18 15:14:05 +0200

    Remove legacy docs-* babel environments

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,3 @@
-const bpmr = require('babel-plugin-module-resolver');
-
-function resolvePath(sourcePath, currentFile, opts) {
-  if (sourcePath === 'markdown') {
-    const base = currentFile.substring(__dirname.length).slice(0, -3);
-    return `${__dirname}/docs/src/${base}/`;
-  }
-
-  return bpmr.resolvePath(sourcePath, currentFile, opts);
-}
-
 let defaultPresets;
 
 // We release a ES version of Material-UI.
@@ -86,50 +75,6 @@ module.exports = {
             },
           },
         ],
-      ],
-    },
-    'docs-development': {
-      presets: ['next/babel', '@zeit/next-typescript/babel'],
-      plugins: [
-        'babel-plugin-preval',
-        [
-          'babel-plugin-module-resolver',
-          {
-            alias: {
-              ...defaultAlias,
-              '@material-ui/docs': './packages/material-ui-docs/src',
-              docs: './docs',
-              modules: './modules',
-              pages: './pages',
-            },
-            transformFunctions: ['require', 'require.context'],
-            resolvePath,
-          },
-        ],
-      ],
-    },
-    'docs-production': {
-      presets: ['next/babel', '@zeit/next-typescript/babel'],
-      plugins: [
-        'babel-plugin-preval',
-        [
-          'babel-plugin-module-resolver',
-          {
-            alias: {
-              ...defaultAlias,
-              '@material-ui/docs': './packages/material-ui-docs/src',
-              docs: './docs',
-              modules: './modules',
-              pages: './pages',
-            },
-            transformFunctions: ['require', 'require.context'],
-            resolvePath,
-          },
-        ],
-        'babel-plugin-transform-react-constant-elements',
-        'babel-plugin-transform-dev-warning',
-        ['babel-plugin-react-remove-properties', { properties: ['data-mui-test'] }],
-        ['babel-plugin-transform-react-remove-prop-types', { mode: 'remove' }],
       ],
     },
     esm: {
