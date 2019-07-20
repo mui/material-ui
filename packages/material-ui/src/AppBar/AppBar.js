4d20e1d909659f403503e8cec85c96b25fa22665
merceyz <merceyz@users.noreply.github.com>
2019-07-20 23:46:24 +0200

    AppBar

--- packages/material-ui/src/AppBar/AppBar.js
+++ packages/material-ui/src/AppBar/AppBar.js
@@ -93,12 +93,12 @@ AppBar.propTypes = {
   /**
    * The content of the component.
    */
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
   /**
    * Override or extend the styles applied to the component.
    * See [CSS API](#css) below for more details.
    */
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object,
   /**
    * @ignore
    */
@@ -106,13 +106,13 @@ AppBar.propTypes = {
   /**
    * The color of the component. It supports those theme colors that make sense for this component.
    */
-  color: PropTypes.oneOf(['inherit', 'primary', 'secondary', 'default']),
+  color: PropTypes.oneOf(['default', 'inherit', 'primary', 'secondary']),
   /**
    * The positioning type. The behavior of the different options is described
    * [in the MDN web docs](https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Positioning).
    * Note: `sticky` is not universally supported and will fall back to `static` when unavailable.
    */
-  position: PropTypes.oneOf(['fixed', 'absolute', 'sticky', 'static', 'relative']),
+  position: PropTypes.oneOf(['absolute', 'fixed', 'relative', 'static', 'sticky']),
 };
 
 export default withStyles(styles, { name: 'MuiAppBar' })(AppBar);
