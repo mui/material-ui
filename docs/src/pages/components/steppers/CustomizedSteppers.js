7ae62129729f99a3da74d3bd8ab6a96fc5cbb856
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-07-27 21:49:23 +0200

    [Stepper] Add cutomization example (#16769)

--- docs/src/pages/components/steppers/CustomizedSteppers.js
+++ docs/src/pages/components/steppers/CustomizedSteppers.js
@@ -1,11 +1,84 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
+import Check from '@material-ui/icons/Check';
 import StepConnector from '@material-ui/core/StepConnector';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+
+const QontoConnector = withStyles({
+  alternativeLabel: {
+    top: 10,
+    left: 'calc(-50% + 16px)',
+    right: 'calc(50% + 16px)',
+  },
+  active: {
+    '& $line': {
+      borderColor: '#784af4',
+    },
+  },
+  completed: {
+    '& $line': {
+      borderColor: '#784af4',
+    },
+  },
+  disabled: {
+    '& $line': {
+      borderColor: '#eaeaf0',
+    },
+  },
+  line: {
+    borderTopWidth: 3,
+    borderRadius: 1,
+  },
+})(StepConnector);
+
+const useQontoStepIconStyles = makeStyles({
+  root: {
+    color: '#eaeaf0',
+    display: 'flex',
+    height: 22,
+    alignItems: 'center',
+  },
+  active: {
+    color: '#784af4',
+  },
+  circle: {
+    width: 8,
+    height: 8,
+    borderRadius: '50%',
+    backgroundColor: 'currentColor',
+  },
+  completed: {
+    color: '#784af4',
+    zIndex: 1,
+    fontSize: 18,
+  },
+});
+
+function QontoStepIcon(props) {
+  const classes = useQontoStepIconStyles();
+  const { active, completed } = props;
+
+  return (
+    <div
+      className={clsx(classes.root, {
+        [classes.active]: active,
+      })}
+    >
+      {completed ? <Check className={classes.completed} /> : <div className={classes.circle} />}
+    </div>
+  );
+}
+
+QontoStepIcon.propTypes = {
+  active: PropTypes.bool,
+  completed: PropTypes.bool,
+};
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -17,24 +90,6 @@ const useStyles = makeStyles(theme => ({
   instructions: {
     marginTop: theme.spacing(1),
     marginBottom: theme.spacing(1),
-  },
-  connectorActive: {
-    '& $connectorLine': {
-      borderColor: theme.palette.secondary.main,
-    },
-  },
-  connectorCompleted: {
-    '& $connectorLine': {
-      borderColor: theme.palette.primary.main,
-    },
-  },
-  connectorDisabled: {
-    '& $connectorLine': {
-      borderColor: theme.palette.grey[100],
-    },
-  },
-  connectorLine: {
-    transition: theme.transitions.create('border-color'),
   },
 }));
 
@@ -57,7 +112,7 @@ function getStepContent(step) {
 
 export default function CustomizedSteppers() {
   const classes = useStyles();
-  const [activeStep, setActiveStep] = React.useState(0);
+  const [activeStep, setActiveStep] = React.useState(1);
   const steps = getSteps();
 
   function handleNext() {
@@ -72,30 +127,12 @@ export default function CustomizedSteppers() {
     setActiveStep(0);
   }
 
-  const connector = (
-    <StepConnector
-      classes={{
-        active: classes.connectorActive,
-        completed: classes.connectorCompleted,
-        disabled: classes.connectorDisabled,
-        line: classes.connectorLine,
-      }}
-    />
-  );
-
   return (
     <div className={classes.root}>
-      <Stepper activeStep={activeStep} connector={connector}>
+      <Stepper alternativeLabel activeStep={activeStep} connector={<QontoConnector />}>
         {steps.map(label => (
           <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      <Stepper alternativeLabel activeStep={activeStep} connector={connector}>
-        {steps.map(label => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
+            <StepLabel StepIconComponent={QontoStepIcon}>{label}</StepLabel>
           </Step>
         ))}
       </Stepper>
