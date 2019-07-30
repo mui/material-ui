20b694ffe567f951da528af5ab37cbf0f5da3355
Matthew Brookes <github@nospam.33m.co>
2019-07-30 18:15:45 +0100

    tidy up the demo a bit

--- docs/src/pages/components/tree-view/FileSystemNavigator.js
+++ docs/src/pages/components/tree-view/FileSystemNavigator.js
@@ -1,22 +1,40 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import TreeView from '@material-ui/lab/TreeView';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import TreeItem from '@material-ui/lab/TreeItem';
 
+const useStyles = makeStyles({
+  root: {
+    height: 216,
+  },
+});
+
+function handleClick(event) {
+  event.preventDefault();
+  alert('You clicked a leaf node.');
+}
+
 export default function FileSystemNavigator() {
+  const classes = useStyles();
+
   return (
-    <TreeView defaultCollapseIcon={<ExpandMoreIcon />} defaultExpandIcon={<ChevronRightIcon />}>
-      <TreeItem nodeId="1" label="Applications: ">
-        <TreeItem nodeId="2" label="Calendar : app" />
-        <TreeItem nodeId="3" label="Chrome : app" />
-        <TreeItem nodeId="4" label="Webstorm : app" />
+    <TreeView
+      className={classes.root}
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpandIcon={<ChevronRightIcon />}
+    >
+      <TreeItem nodeId="1" label="Applications">
+        <TreeItem nodeId="2" label="Calendar" onClick={handleClick} />
+        <TreeItem nodeId="3" label="Chrome" onClick={handleClick} />
+        <TreeItem nodeId="4" label="Webstorm" onClick={handleClick} />
       </TreeItem>
-      <TreeItem nodeId="5" label="Documents: ">
-        <TreeItem nodeId="6" label="Material-UI : app">
-          <TreeItem nodeId="7" label="src : ">
-            <TreeItem nodeId="8" label="index : js" />
-            <TreeItem nodeId="9" label="tree-view : js" />
+      <TreeItem nodeId="5" label="Documents">
+        <TreeItem nodeId="6" label="Material-UI">
+          <TreeItem nodeId="7" label="src" >
+            <TreeItem nodeId="8" label="index.js" onClick={handleClick} />
+            <TreeItem nodeId="9" label="tree-view.js" onClick={handleClick} />
           </TreeItem>
         </TreeItem>
       </TreeItem>
