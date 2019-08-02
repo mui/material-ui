8daf46efc2e29dcc66623626112daa7fd962a582
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-08-02 15:52:04 +0200

    Remove createStyles when no theme prop is used
    Fix the inconsistency, we had 18 occurences with the helper and 26 without.

--- docs/src/pages/components/avatars/ImageAvatars.tsx
+++ docs/src/pages/components/avatars/ImageAvatars.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles(
-  createStyles({
-    avatar: {
-      margin: 10,
-    },
-    bigAvatar: {
-      margin: 10,
-      width: 60,
-      height: 60,
-    },
-  }),
-);
+const useStyles = makeStyles({
+  avatar: {
+    margin: 10,
+  },
+  bigAvatar: {
+    margin: 10,
+    width: 60,
+    height: 60,
+  },
+});
 
 export default function ImageAvatars() {
   const classes = useStyles();
