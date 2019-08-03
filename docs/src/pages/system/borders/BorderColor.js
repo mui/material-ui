6d5b01397ceb2b7941682bbffc3cdb6601ca4663
Roger Clotet <clotet89@gmail.com>
2019-08-04 00:52:57 +0200

    [core] Avoid !important in borderColor prop

--- docs/src/pages/system/borders/BorderColor.js
+++ docs/src/pages/system/borders/BorderColor.js
@@ -2,28 +2,21 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 
-const inner = (
-  <Box bgcolor="background.paper" m={1} border={1} style={{ width: '5rem', height: '5rem' }} />
-);
+const defaultProps = {
+  bgcolor: 'background.paper',
+  m: 1,
+  border: 1,
+  style: { width: '5rem', height: '5rem' },
+};
 
 function BorderColor() {
   return (
     <Grid container justify="center">
-      <Box borderColor="primary.main" clone>
-        {inner}
-      </Box>
-      <Box borderColor="secondary.main" clone>
-        {inner}
-      </Box>
-      <Box borderColor="error.main" clone>
-        {inner}
-      </Box>
-      <Box borderColor="grey.500" clone>
-        {inner}
-      </Box>
-      <Box borderColor="text.primary" clone>
-        {inner}
-      </Box>
+      <Box borderColor="primary.main" {...defaultProps} />
+      <Box borderColor="secondary.main" {...defaultProps} />
+      <Box borderColor="error.main" {...defaultProps} />
+      <Box borderColor="grey.500" {...defaultProps} />
+      <Box borderColor="text.primary" {...defaultProps} />
     </Grid>
   );
 }
