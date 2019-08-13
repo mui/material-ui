03e21909622dcb79bf6f1331de06536a8d14c0b7
megos <gmegos.27@gmail.com>
2019-08-14 06:13:16 +0900

    [Dialog] Add support for a Dialog without a DialogTitle (#16980)
    * Fix dialog content padding without title
    
    * even closer to spec

--- packages/material-ui/src/DialogContent/DialogContent.js
+++ packages/material-ui/src/DialogContent/DialogContent.js
@@ -7,12 +7,17 @@ export const styles = theme => ({
   /* Styles applied to the root element. */
   root: {
     flex: '1 1 auto',
-    padding: '8px 24px',
     WebkitOverflowScrolling: 'touch', // Add iOS momentum scrolling.
     overflowY: 'auto',
+    padding: '8px 24px',
+    '&:first-child': {
+      // dialog without title
+      paddingTop: 20,
+    },
   },
   /* Styles applied to the root element if `dividers={true}`. */
   dividers: {
+    padding: '16px 24px',
     borderTop: `1px solid ${theme.palette.divider}`,
     borderBottom: `1px solid ${theme.palette.divider}`,
   },
