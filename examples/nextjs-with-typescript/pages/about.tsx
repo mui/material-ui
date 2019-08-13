82ea09a0ceeca6d08d73c220f2ecb4c0c55e98c3
Cole Marsteller <cgm32547@gmail.com>
2019-08-13 16:22:18 -0700

    [example] Remove unused MuiLink declaration

--- examples/nextjs-with-typescript/pages/about.tsx
+++ examples/nextjs-with-typescript/pages/about.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import MuiLink from '@material-ui/core/Link';
 import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 
