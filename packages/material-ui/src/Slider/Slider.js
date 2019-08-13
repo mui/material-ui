807fb7de5afc648ec1e86ed6e9f417d048708281
Astrantia <32885386+Astrantia@users.noreply.github.com>
2019-08-13 12:36:17 +0000

    Fix Slider.js for Firefox
    As suggested by @oliviertassinari in https://github.com/mui-org/material-ui/issues/16983#issuecomment-520814056

--- packages/material-ui/src/Slider/Slider.js
+++ packages/material-ui/src/Slider/Slider.js
@@ -544,7 +544,11 @@ const Slider = React.forwardRef(function Slider(props, ref) {
   const handleMouseEnter = useEventCallback(event => {
     // If the slider was being interacted with but the mouse went off the window
     // and then re-entered while unclicked then end the interaction.
-    if (event.buttons === 0) {
+    //
+    // In Firefox, the event can be triggered when a new DOM node is inserted and hovered.
+    // We need to make sure that the relatedTarget (The EventTarget the pointing device exited from)
+    // is not null (it should be the html element)
+    if (event.buttons === 0 && event.relatedTarget !== null) {
       handleTouchEnd(event);
     }
   });
