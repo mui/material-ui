b6f67a18d1bd776ced2660bd74f27f24a694efd3
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-08-19 16:26:34 +0200

    [Modal] Add transition documentation

--- docs/src/pages/components/modal/SimpleModal.js
+++ docs/src/pages/components/modal/SimpleModal.js
@@ -24,7 +24,7 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 4),
+    padding: theme.spacing(2, 4, 3),
   },
 }));
 
