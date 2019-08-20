e785d276e6319f7b34f05399627b26423b86b4a8
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-08-20 11:18:56 +0200

    [Divider] Add vertical support (#17063)

--- packages/material-ui/src/Divider/Divider.js
+++ packages/material-ui/src/Divider/Divider.js
@@ -33,6 +33,11 @@ export const styles = theme => ({
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
   },
+  /* Styles applied to the root element if `orientation="vertical"`. */
+  vertical: {
+    height: '100%',
+    width: 1,
+  },
 });
 
 const Divider = React.forwardRef(function Divider(props, ref) {
@@ -42,6 +47,7 @@ const Divider = React.forwardRef(function Divider(props, ref) {
     className,
     component: Component = 'hr',
     light = false,
+    orientation = 'horizontal',
     role = Component !== 'hr' ? 'separator' : undefined,
     variant = 'fullWidth',
     ...other
@@ -52,10 +58,10 @@ const Divider = React.forwardRef(function Divider(props, ref) {
       className={clsx(
         classes.root,
         {
-          [classes.inset]: variant === 'inset',
-          [classes.middle]: variant === 'middle',
+          [classes[variant]]: variant !== 'fullWidth',
           [classes.absolute]: absolute,
           [classes.light]: light,
+          [classes.vertical]: orientation === 'vertical',
         },
         className,
       )}
@@ -89,6 +95,10 @@ Divider.propTypes = {
    * If `true`, the divider will have a lighter color.
    */
   light: PropTypes.bool,
+  /**
+   * The divider orientation.
+   */
+  orientation: PropTypes.oneOf(['horizontal', 'vertical']),
   /**
    * @ignore
    */
