19247f023a240f35265d0fd33e2208d5e82ac6ca
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-08-21 10:53:22 +0200

    [Modal] Add transition documentation (#17059)
    * [Modal] Add transition documentation
    
    * Update docs/src/pages/components/modal/modal.md
    
    Co-Authored-By: Matt <github@nospam.33m.co>
    
    * Update docs/src/pages/components/modal/modal.md
    
    Co-Authored-By: Matt <github@nospam.33m.co>
    
    * Update docs/src/pages/components/modal/modal.md
    
    Co-Authored-By: Matt <github@nospam.33m.co>
    
    * Matt review

--- docs/src/pages/components/modal/SimpleModal.tsx
+++ docs/src/pages/components/modal/SimpleModal.tsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme: Theme) =>
       backgroundColor: theme.palette.background.paper,
       border: '2px solid #000',
       boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 4),
+      padding: theme.spacing(2, 4, 3),
     },
   }),
 );
