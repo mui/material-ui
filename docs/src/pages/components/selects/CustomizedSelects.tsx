d6ee575dfb058cae04410ed4be643eb850989eca
Behnam Mohammadi <itten@live.com>
2019-08-23 03:15:47 +0430

    CustomizedSelects update with JSS rule

--- docs/src/pages/components/selects/CustomizedSelects.tsx
+++ docs/src/pages/components/selects/CustomizedSelects.tsx
@@ -18,7 +18,7 @@ const BootstrapInput = withStyles((theme: Theme) =>
       borderRadius: 4,
       position: 'relative',
       backgroundColor: theme.palette.background.paper,
-      border: '1px solid #ced4da',
+      border: [[1, 'solid', '#ced4da']],
       fontSize: 16,
       padding: '10px 26px 10px 12px',
       transition: theme.transitions.create(['border-color', 'box-shadow']),
