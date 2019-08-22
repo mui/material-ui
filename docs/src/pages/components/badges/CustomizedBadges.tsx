2062eaa24d6e7019c216a88035b64dd29a9703b8
Behnam Mohammadi <itten@live.com>
2019-08-23 02:43:41 +0430

    Update style with jss structure

--- docs/src/pages/components/badges/CustomizedBadges.tsx
+++ docs/src/pages/components/badges/CustomizedBadges.tsx
@@ -9,9 +9,13 @@ const StyledBadge = withStyles((theme: Theme) => ({
     top: '50%',
     right: -3,
     // The border color match the background color.
-    border: `2px solid ${
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[900]
-    }`,
+    border: [
+      [
+        2,
+        'solid',
+        theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[900],
+      ],
+    ],
   },
 }))(Badge);
 
