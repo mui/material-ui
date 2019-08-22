8befa62ea10389015b6afedd5a9b5da8731278b1
Behnam Mohammadi <itten@live.com>
2019-08-23 03:14:59 +0430

    SpringModal update with JSS rule

--- docs/src/pages/components/modal/SpringModal.tsx
+++ docs/src/pages/components/modal/SpringModal.tsx
@@ -15,7 +15,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     paper: {
       backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
+      border: [[2, 'solid', '#000']],
       boxShadow: theme.shadows[5],
       padding: theme.spacing(2, 4, 3),
     },
