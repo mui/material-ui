96f9d9d150da59793376f4589728048cd553600b
egerardus <51415416+egerardus@users.noreply.github.com>
2019-08-24 13:34:32 -0700

    Create ScrollableTable.js
    Then example code for scrollable table rows with a fixed header and toolbar.

---
+++ docs/src/pages/components/tables/ScrollableTable.js
@@ -0,0 +1,394 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList';
+
+const columns = [
+  { id: 'name', label: 'Name', minWidth: 200 },
+  { id: 'code', label: 'ISO Code' },
+  { id: 'population', 
+    label: 'Population', 
+    minWidth: 120, 
+    align: 'right', 
+    format: v => v.toLocaleString() 
+  },
+  { id: 'size', 
+    label: 'Size (km\u00b2)', 
+    minWidth: 120, 
+    align: 'right', 
+    format: v => v.toLocaleString() 
+  },
+  { id: 'density', 
+    label: 'Density', 
+    minWidth: 120, 
+    align: 'right', 
+    format: v => v.toFixed(2) 
+  },
+];
+
+const rows = [
+  createData('India', 'IN', 1324171354, 3287263),
+  createData('China', 'CN', 1403500365, 9596961),
+  createData('Italy', 'IT', 60483973, 301340),
+  createData('United States', 'US', 327167434, 9833520),
+  createData('Canada', 'CA', 37602103, 9984670),
+  createData('Australia', 'AU', 25475400, 7692024),
+  createData('Germany', 'DE', 83019200, 357578),
+  createData('Ireland', 'IE', 4857000, 70273),
+  createData('Mexico', 'MX', 126577691, 1972550),
+  createData('Japan', 'JP', 126317000, 377973),
+  createData('France', 'FR', 67022000, 640679),
+  createData('United Kingdom', 'GB', 67545757, 242495),
+  createData('Russia', 'RU', 146793744, 17098246),
+  createData('Nigeria', 'NG', 200962417, 923768),
+  createData('Brazil', 'BR', 210147125, 8515767)
+];
+
+function createData(name, code, population, size) {
+  const density = (population / size);
+  return { name, code, population, size, density };
+}
+
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
+
+function ScrollableTableHead(props) {
+  const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+  const createSortHandler = property => event => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell className={classes.headerCell} padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{ 'aria-label': 'select all' }}
+          />
+        </TableCell>
+        {columns.map(column => (
+          <TableCell
+            className={classes.headerCell}
+            key={column.id}
+            align={column.align}
+            style={{ width: column.width, minWidth: column.minWidth}}
+            sortDirection={orderBy === column.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === column.id}
+              direction={order}
+              onClick={createSortHandler(column.id)}
+            >
+              {column.label}
+              {orderBy === column.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+ScrollableTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const useToolbarStyles = makeStyles(theme => ({
+  root: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+  },
+  spacer: {
+    flex: '1 1 100%',
+  },
+  title: {
+    flex: '0 0 auto',
+  },
+  paper: {
+    flex: '1 1 100%',
+    display: 'flex',
+    zIndex: 120,
+    position: 'relative',
+    alignItems: 'center',
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    paddingLeft: theme.spacing(3),
+    paddingRight: theme.spacing(3),
+    justifyContent: 'space-between',
+  },
+}));
+
+const ScrollableTableToolbar = props => {
+  const classes = useToolbarStyles();
+  const { numSelected } = props;
+
+  return (
+    <Fragment>
+      {numSelected === 0 ? (
+        <Toolbar>
+          <div className={classes.title}>
+            <Typography variant="h6" id="tableTitle">
+              Countries
+            </Typography>
+          </div>
+          <div className={classes.spacer} />
+          <div>
+            <Tooltip title="Filter list">
+              <IconButton aria-label="filter list">
+                <FilterListIcon />
+              </IconButton>
+            </Tooltip>
+          </div>
+        </Toolbar>
+      ) : (
+        <Paper 
+          className={classes.paper}
+          elevation={1}>
+          <div className={classes.title}>
+            <Typography color="inherit" variant="subtitle1">
+              {numSelected} selected
+            </Typography>
+          </div>
+          <div className={classes.spacer} />
+          <div>
+            <Tooltip title="Delete">
+              <IconButton aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
+            </Tooltip>
+          </div>
+        </Paper>
+      )
+    }
+    </Fragment>
+  );
+}
+
+ScrollableTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    height: '100vh',
+    marginTop: theme.spacing(8),
+  },
+  bbar: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0
+  },
+  tableWrapper: {
+    position: 'relative',
+    height: '100%',
+    maxHeight: 'calc(100% - 184px)',
+    overflow: 'auto',
+  },
+  tableBody: {
+    width: '100%',
+  },
+  headerCell: {
+    position: 'sticky',
+    top: 0,
+    left: 0,
+    backgroundColor: theme.palette.background.default,
+    zIndex: 100,
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+}));
+
+export default function ScrollableTable() {
+  const classes = useStyles();
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('column1');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  function handleRequestSort(event, property) {
+    const isDesc = orderBy === property && order === 'desc';
+    setOrder(isDesc ? 'asc' : 'desc');
+    setOrderBy(property);
+  }
+
+  function handleSelectAllClick(event) {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((row, idx) => idx);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  }
+
+  function handleClick(event, dataIndex) {
+    const selectedIndex = selected.indexOf(dataIndex);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, dataIndex);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  }
+
+  function handleChangePage(event, newPage) {
+    setPage(newPage);
+  }
+
+  function handleChangeRowsPerPage(event) {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  }
+
+  const isSelected = dataIndex => selected.indexOf(dataIndex) !== -1;
+
+
+  return (
+    <div className={classes.root}>
+      <ScrollableTableToolbar numSelected={selected.length} />
+      <div className={classes.tableWrapper}>
+        <Table
+          role="grid"
+          aria-labelledby="tableTitle"
+          size="medium"
+        >
+          <ScrollableTableHead
+            classes={classes}
+            numSelected={selected.length}
+            order={order}
+            orderBy={orderBy}
+            onSelectAllClick={handleSelectAllClick}
+            onRequestSort={handleRequestSort}
+            rowCount={rows.length}
+          />
+          <TableBody>
+            {stableSort(rows, getSorting(order, orderBy))
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                const dataIndex = rows.indexOf(row);
+                const isItemSelected = isSelected(dataIndex);
+                const labelId = `scrollable-table-checkbox-${dataIndex}`;
+
+                return (
+                  <TableRow
+                    hover
+                    onClick={event => handleClick(event, dataIndex)}
+                    role="checkbox"
+                    aria-checked={isItemSelected}
+                    tabIndex={-1}
+                    key={dataIndex}
+                    selected={isItemSelected}
+                  >
+                    <TableCell padding="checkbox">
+                      <Checkbox
+                        color="primary"
+                        checked={isItemSelected}
+                        inputProps={{ 'aria-labelledby': labelId }}
+                      />
+                    </TableCell>
+                    {columns.map(column => (
+                      <TableCell 
+                        key={column.id} 
+                        align={column.align}
+                      >
+                        {column.format ? (
+                          column.format(row[column.id])
+                        ) : (
+                          row[column.id]
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </div>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        className={classes.bbar}
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        backIconButtonProps={{
+          'aria-label': 'previous page',
+        }}
+        nextIconButtonProps={{
+          'aria-label': 'next page',
+        }}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </div>
+  );
+}
