5f96448e14743281f8e6dd656503936e496d3d2d
Yoshihide Shiono <saltyshiomix@users.noreply.github.com>
2019-08-27 01:15:53 +0900

    [examples] Fix warnings Container in _app.js with Next.js (#17181)

--- examples/nextjs-with-typescript/pages/_app.tsx
+++ examples/nextjs-with-typescript/pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -18,7 +18,7 @@ class MyApp extends App {
     const { Component, pageProps } = this.props;
 
     return (
-      <Container>
+      <React.Fragment>
         <Head>
           <title>My page</title>
         </Head>
@@ -27,7 +27,7 @@ class MyApp extends App {
           <CssBaseline />
           <Component {...pageProps} />
         </ThemeProvider>
-      </Container>
+      </React.Fragment>
     );
   }
 }
