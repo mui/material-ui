4bf64db871f7d4cb19c6026eec81eb8f6b7ee321
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-08-27 15:47:53 +0200

    Sebastian review

--- packages/material-ui-utils/src/refType.js
+++ packages/material-ui-utils/src/refType.js
@@ -1,10 +1,5 @@
 import PropTypes from 'prop-types';
 
-const refType = PropTypes.oneOfType([
-  PropTypes.func,
-  PropTypes.shape({
-    current: PropTypes.object,
-  }),
-]);
+const refType = PropTypes.oneOfType([PropTypes.func, PropTypes.PropTypes.object]);
 
 export default refType;
