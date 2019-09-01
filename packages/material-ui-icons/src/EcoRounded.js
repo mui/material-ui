e904c1f9e6a3f05027dd91285e806debbc417168
Cole Marsteller <cgm32547@gmail.com>
2019-08-31 17:19:18 -0700

    [icons] add material.io Eco icon

---
+++ packages/material-ui-icons/src/EcoRounded.js
@@ -0,0 +1,13 @@
+import React from "react";
+import createSvgIcon from "./utils/createSvgIcon";
+
+export default createSvgIcon(
+  <React.Fragment>
+    <path fill="none" d="M0 0h24v24H0V0z" />
+    <path
+      d="M6.05,8.05c-2.73,2.73-2.73,7.15-0.02,9.88c1.47-3.4,4.09-6.24,7.36-7.93c-2.77,2.34-4.71,5.61-5.39,9.32
+  c2.6,1.23,5.8,0.78,7.95-1.37c2.99-2.99,3.83-11.14,4.01-13.38c0.02-0.31-0.23-0.56-0.53-0.53C17.19,4.22,9.04,5.06,6.05,8.05z"
+    />
+  </React.Fragment>,
+  "EcoRounded"
+);
