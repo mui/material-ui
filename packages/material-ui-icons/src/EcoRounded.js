ff5829c0c5179b171c62f1859c1a0f5cd1e0ea12
Cole Marsteller <cgm32547@gmail.com>
2019-08-31 20:17:40 -0700

    [icons] Refresh material icons

---
+++ packages/material-ui-icons/src/EcoRounded.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import createSvgIcon from './utils/createSvgIcon';
+
+export default createSvgIcon(
+  <path d="M6.05 8.05c-2.73 2.73-2.73 7.15-.02 9.88 1.47-3.4 4.09-6.24 7.36-7.93-2.77 2.34-4.71 5.61-5.39 9.32 2.6 1.23 5.8.78 7.95-1.37 2.99-2.99 3.83-11.14 4.01-13.38.02-.31-.23-.56-.53-.53-2.24.18-10.39 1.02-13.38 4.01z" />
+, 'EcoRounded');
