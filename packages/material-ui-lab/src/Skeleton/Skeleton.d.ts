5bb044c220d69c0511677a2a16adb35cbe38d325
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-09-03 10:20:51 +0200

    Apply to Grid and Skeleton

--- packages/material-ui-lab/src/Skeleton/Skeleton.d.ts
+++ packages/material-ui-lab/src/Skeleton/Skeleton.d.ts
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { OverridableComponent, SimplifiedPropsOf } from '@material-ui/core/OverridableComponent';
+import { OverridableComponent, OverrideProps } from '@material-ui/core/OverridableComponent';
 
-declare const Skeleton: OverridableComponent<{
-  props: {
+export interface SkeletonTypeMap<P = {}, D extends React.ElementType = 'hr'> {
+  props: P & {
     disableAnimate?: boolean;
     height?: number | string;
     variant?: 'text' | 'rect' | 'circle';
@@ -10,10 +10,15 @@ declare const Skeleton: OverridableComponent<{
   };
   defaultComponent: 'div';
   classKey: SkeletonClassKey;
-}>;
+}
+
+declare const Skeleton: OverridableComponent<SkeletonTypeMap>;
 
 export type SkeletonClassKey = 'root' | 'text' | 'rect' | 'circle' | 'animate';
 
-export type SkeletonProps = SimplifiedPropsOf<typeof Skeleton>;
+export type SkeletonProps<
+  D extends React.ElementType = SkeletonTypeMap['defaultComponent'],
+  P = {}
+> = OverrideProps<SkeletonTypeMap<P, D>, D>;
 
 export default Skeleton;
