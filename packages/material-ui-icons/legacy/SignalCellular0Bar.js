22d97a6f47825d9339db585a7e878fe58f602c66
Cole Marsteller <cgm32547@gmail.com>
2019-09-04 16:29:02 -0700

    fix import path

--- packages/material-ui-icons/legacy/SignalCellular0Bar.js
+++ packages/material-ui-icons/legacy/SignalCellular0Bar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import createSvgIcon from '../src/utils/createSvgIcon';
+import createSvgIcon from './utils/createSvgIcon';
 
 export default createSvgIcon(
   <path fillOpacity=".3" d="M2 22h20V2z" />
