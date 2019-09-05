b329ffad71874c24441e508bd50b115860e6a114
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-09-05 14:51:36 +0200

    remove dead path

--- packages/material-ui-icons/legacy/SignalCellular1BarTwoTone.js
+++ packages/material-ui-icons/legacy/SignalCellular1BarTwoTone.js
@@ -2,5 +2,5 @@ import React from 'react';
 import createSvgIcon from './utils/createSvgIcon';
 
 export default createSvgIcon(
-  <React.Fragment><path fill="none" d="M0 0h24v24H0V0z" /><path fillOpacity=".3" d="M2 22h20V2L2 22z" /><path d="M12 12L2 22h10V12z" /></React.Fragment>
+  <React.Fragment><path fillOpacity=".3" d="M2 22h20V2L2 22z" /><path d="M12 12L2 22h10V12z" /></React.Fragment>
 , 'SignalCellular1BarTwoTone');
