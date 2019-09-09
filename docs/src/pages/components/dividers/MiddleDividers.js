251b9289a802a48a012b8393e44362e8fcd94e15
Andrew Scales <ascales@pronvest.com>
2019-09-09 13:35:23 -0400

    spelling

--- docs/src/pages/components/dividers/MiddleDividers.js
+++ docs/src/pages/components/dividers/MiddleDividers.js
@@ -45,7 +45,7 @@ export default function MiddleDividers() {
           </Grid>
         </Grid>
         <Typography color="textSecondary" variant="body2">
-          Handmade, artisinal toothbrush with natural fibers will keep your teeth clean while
+          Handmade, artisanal toothbrush with natural fibers will keep your teeth clean while
           keeping plastic out of landfills.
         </Typography>
       </div>
