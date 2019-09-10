fc4a74d68ad2a34924f358a8c62439eae4477cf4
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-09-10 09:30:13 +0200

    Add test for focus handler

--- packages/material-ui/src/TextField/TextField.spec.tsx
+++ packages/material-ui/src/TextField/TextField.spec.tsx
@@ -37,3 +37,31 @@ import TextField from '@material-ui/core/TextField';
     <TextField variant="outlined" InputProps={{ classes: { notchedOutline: 'notched-outline' } }} />
   );
 }
+
+// https://github.com/mui-org/material-ui/issues/17369#issuecomment-529622304
+function FocusHandlerTest() {
+  const inputHandler = React.useCallback((event: React.FocusEvent<HTMLInputElement>) => {}, []);
+  // undesired failure but using discriminate unions or generics has bad type interop
+  // $ExpectError
+  const input = <TextField onFocus={inputHandler} />;
+
+  // this or a generic `HTMLElement` is probably closer to what we actually use.
+  const valueHandler = React.useCallback((event: React.FocusEvent<{ value: string }>) => {}, []);
+  const textfield = <TextField onFocus={valueHandler} />;
+
+  // sound narrowing with runtime overhead
+  const genericHandler = React.useCallback((event: React.FocusEvent<HTMLElement>) => {
+    if (event.currentTarget instanceof HTMLInputElement) {
+      console.assert(event.currentTarget.value === 'foo');
+    }
+  }, []);
+  const element = <TextField onFocus={genericHandler} />;
+
+  const fieldHandler = React.useCallback(
+    (ev?: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {},
+    [],
+  );
+  const field = <TextField onFocus={fieldHandler} />;
+
+  return null;
+}
