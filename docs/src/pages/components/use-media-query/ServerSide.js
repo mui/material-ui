44dcb5473df7b14c44221284919186e88f209405
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-09-15 14:53:42 +0200

    [docs] Improve useMediaQuery SSR docs

--- docs/src/pages/components/use-media-query/ServerSide.js
+++ docs/src/pages/components/use-media-query/ServerSide.js
@@ -11,17 +11,8 @@ function MyComponent() {
 
 export default function ServerSide() {
   const ssrMatchMedia = query => ({
-    // Use https://github.com/ericf/css-mediaquery as ponyfill.
     matches: mediaQuery.match(query, {
       // The estimated CSS width of the browser.
-      // For the sake of this demo, we are using a fixed value.
-      //
-      // In production, you can leverage:
-      //
-      // - Client hints. You can ask the client to send your server its width.
-      // Be aware that this feature is not supported everywhere: https://caniuse.com/#search=client%20hint.
-      // - User-agent. You can parse the user agent of the client, then convert the data to a
-      // is mobile or is desktop variable, and finally, guess the most likely screen width of the client.
       width: 800,
     }),
   });
@@ -30,6 +21,7 @@ export default function ServerSide() {
     <ThemeProvider
       theme={{
         props: {
+          // Change the default options of useMediaQuery
           MuiUseMediaQuery: { ssrMatchMedia },
         },
       }}
