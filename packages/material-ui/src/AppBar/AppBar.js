4fd354021a3234165c909e8995cfa85a0790f908
Vadim Goy <vadim.goy@smoozeedev.com>
2019-09-18 11:15:15 +0300

    fix for tests

--- packages/material-ui/src/AppBar/AppBar.js
+++ packages/material-ui/src/AppBar/AppBar.js
@@ -115,7 +115,7 @@ AppBar.propTypes = {
   /**
    * The color of the component. It supports those theme colors that make sense for this component.
    */
-  color: PropTypes.oneOf(['default', 'inherit', 'primary', 'secondary']),
+  color: PropTypes.oneOf(['default', 'inherit', 'primary', 'secondary', 'error']),
   /**
    * The positioning type. The behavior of the different options is described
    * [in the MDN web docs](https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Positioning).
