47328d5721cb7ee07f2ec2620feff6dffe7fdd36
croraf <vrafaeli@msn.com>
2019-09-22 20:10:04 +0200

    Move GOVERNANCE.md and ROADMAP.md files

--- docs/pages/discover-more/governance.js
+++ docs/pages/discover-more/governance.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
-import markdown from '../../../GOVERNANCE.md';
 
-function Page() {
-  return <MarkdownDocs markdown={markdown} markdownLocation="/GOVERNANCE.md" />;
+const req = require.context('docs/src/pages/discover-more/governance', false, /\.(md|js|tsx)$/);
+const reqSource = require.context(
+  '!raw-loader!../../src/pages/discover-more/governance',
+  false,
+  /\.(js|tsx)$/,
+);
+const reqPrefix = 'pages/discover-more/governance';
+
+export default function Page() {
+  return <MarkdownDocs req={req} reqSource={reqSource} reqPrefix={reqPrefix} />;
 }
-
-export default Page;
