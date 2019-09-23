50cb92168fbd8c12fb78db8348be68e279b5abdb
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-09-23 14:10:56 +0200

    Remove elevation for bootstrap customization

--- docs/src/pages/components/buttons/CustomizedButtons.js
+++ docs/src/pages/components/buttons/CustomizedButtons.js
@@ -29,6 +29,7 @@ const BootstrapButton = withStyles({
     '&:hover': {
       backgroundColor: '#0069d9',
       borderColor: '#0062cc',
+      boxShadow: 'none',
     },
     '&:active': {
       boxShadow: 'none',
