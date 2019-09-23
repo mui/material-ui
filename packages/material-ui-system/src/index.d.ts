1f1bd34b5e7e97ca479c5457661a22c060031d1c
Alex Bukurov <abukurov@hwdtech.ru>
2019-09-23 12:48:03 +0600

    [system] Improve style function typings

--- packages/material-ui-system/src/index.d.ts
+++ packages/material-ui-system/src/index.d.ts
@@ -161,7 +161,7 @@ export interface StyleOptions<PropKey, Theme extends object> {
 }
 export function style<PropKey extends string, Theme extends object>(
   options: StyleOptions<PropKey, Theme>,
-): StyleFunction<{ [K in PropKey]: unknown } & { theme: Theme }>;
+): StyleFunction<{ [K in PropKey]?: unknown } & { theme: Theme }>;
 
 // typography.js
 export const fontFamily: SimpleStyleFunction<'fontFamily'>;
