794c5c85fe119ac6391e71d358354fa975b676ed
Lucas <lucas_s.s@hotmail.com>
2019-10-02 05:20:42 -0300

    replace ImgMediaCard demo to styled components

--- docs/src/pages/components/cards/ImgMediaCard.js
+++ docs/src/pages/components/cards/ImgMediaCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import styled from 'styled-components';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -8,17 +8,13 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
-  card: {
-    maxWidth: 345,
-  },
-});
+const StyledCard = styled(Card)`
+  max-width: 345px;
+`;
 
 export default function ImgMediaCard() {
-  const classes = useStyles();
-
   return (
-    <Card className={classes.card}>
+    <StyledCard>
       <CardActionArea>
         <CardMedia
           component="img"
@@ -45,6 +41,6 @@ export default function ImgMediaCard() {
           Learn More
         </Button>
       </CardActions>
-    </Card>
+    </StyledCard>
   );
 }
