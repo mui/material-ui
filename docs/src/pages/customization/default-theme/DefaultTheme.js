f1df5089c5c33fb6d5d194ae241a3187daab41d9
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-10-02 09:57:05 +0200

    [docs] Replace react-inspect with custom TreeView implementation

--- docs/src/pages/customization/default-theme/DefaultTheme.js
+++ docs/src/pages/customization/default-theme/DefaultTheme.js
@@ -2,10 +2,149 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import url from 'url';
-import Inspector from 'react-inspector';
-import { withStyles, createMuiTheme, useTheme } from '@material-ui/core/styles';
+import ExpandIcon from '@material-ui/icons/ExpandMore';
+import CollapseIcon from '@material-ui/icons/ChevronRight';
+import TreeView from '@material-ui/lab/TreeView';
+import TreeItem from '@material-ui/lab/TreeItem';
+import clsx from 'clsx';
+import { makeStyles, withStyles, createMuiTheme, useTheme } from '@material-ui/core/styles';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
+
+/**
+ * @type {React.Context<keyof object>}
+ */
+const ObjectContext = React.createContext('$ROOT');
+
+/**
+ * @param {unknown} value
+ */
+function useType(value) {
+  if (Array.isArray(value)) {
+    return 'array';
+  }
+  if (value === null) {
+    return 'null';
+  }
+
+  return typeof value;
+}
+
+/**
+ *
+ * @param {unknown} value
+ * @param {ReturnType<typeof useType>} type
+ */
+function useLabel(value, type) {
+  switch (type) {
+    case 'array':
+      return `Array(${value.length})`;
+    case 'null':
+      return 'null';
+    case 'undefined':
+      return 'undefined';
+    case 'function':
+      return `f ${value.name}()`;
+    case 'object':
+      return 'Object';
+    case 'string':
+      return `"${value}"`;
+    case 'symbol':
+      return `Symbol(${String(value)})`;
+    case 'bigint':
+    case 'boolean':
+    case 'number':
+    default:
+      return String(value);
+  }
+}
+
+const useTreeLabelStyles = makeStyles({
+  objectKey: {
+    color: 'rgb(227, 110, 236)',
+  },
+  objectValue: {},
+  'type-function': {
+    fontStyle: 'italic',
+  },
+  'type-string': {
+    color: 'rgb(233, 63, 59)',
+  },
+  'type-boolean': {
+    color: 'rgb(153, 128, 255);',
+  },
+  'type-number': {
+    color: 'rgb(153, 128, 255);',
+  },
+});
+
+function TreeLabel({ objectKey, objectValue }) {
+  const type = useType(objectValue);
+  const label = useLabel(objectValue, type);
+  const classes = useTreeLabelStyles();
+
+  return (
+    <React.Fragment>
+      <span className={classes.objectKey}>{objectKey}: </span>
+      <span className={clsx(classes.objectValue, classes[`type-${type}`])}>{label}</span>
+    </React.Fragment>
+  );
+}
+TreeLabel.propTypes = { objectKey: PropTypes.any, objectValue: PropTypes.any };
+
+function ObjectTreeItem(props) {
+  const { objectKey, objectValue } = props;
+
+  const keyPrefix = React.useContext(ObjectContext);
+
+  if (
+    (objectValue !== null && typeof objectValue === 'object') ||
+    typeof objectValue === 'function'
+  ) {
+    const children = Object.keys(objectValue).map(key => {
+      return <ObjectTreeItem key={key} objectKey={key} objectValue={objectValue[key]} />;
+    });
+
+    if (objectKey === undefined) {
+      return (
+        <TreeView defaultCollapseIcon={<ExpandIcon />} defaultExpandIcon={<CollapseIcon />}>
+          {children}
+        </TreeView>
+      );
+    }
+
+    return (
+      <TreeItem
+        nodeId={`${keyPrefix}-${objectKey}`}
+        label={<TreeLabel objectKey={objectKey} objectValue={objectValue} />}
+      >
+        <ObjectContext.Provider value={`${keyPrefix}-${objectKey}`}>
+          {children}
+        </ObjectContext.Provider>
+      </TreeItem>
+    );
+  }
+
+  return (
+    <TreeItem
+      nodeId={`${keyPrefix}-${objectKey}`}
+      label={<TreeLabel objectKey={objectKey} objectValue={objectValue} />}
+    />
+  );
+}
+ObjectTreeItem.propTypes = { objectKey: PropTypes.any, objectValue: PropTypes.any };
+
+function Inspector(props) {
+  const { data } = props;
+
+  return <ObjectTreeItem objectValue={data} />;
+}
+
+Inspector.propTypes = {
+  data: PropTypes.any,
+  /* expandLevel: PropTypes.number,
+  expandPaths: PropTypes.arrayOf(PropTypes.string), */
+};
 
 const styles = theme => ({
   root: {
