86b70bd8eaf517ddaf483bdf83f9af62b6804840
Lucas Silva <lucas.silva@avecbrasil.com.br>
2019-10-07 11:41:42 -0300

    replace styles to makeStyles

--- docs/src/pages/customization/components/ClassNames.js
+++ docs/src/pages/customization/components/ClassNames.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-import { withStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 // We can inject some CSS into the DOM.
-const styles = {
-  root: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    borderRadius: 3,
-    border: 0,
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  },
-};
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      borderRadius: 3,
+      border: 0,
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    },
+  }),
+);
 
-function ClassNames(props) {
-  const { classes, children, className, ...other } = props;
+export default function ClassNames(props) {
+  const { children, className, ...other } = props;
+  const classes = useStyles();
 
   return (
     <Button className={clsx(classes.root, className)} {...other}>
@@ -32,5 +35,3 @@ ClassNames.propTypes = {
   classes: PropTypes.object.isRequired,
   className: PropTypes.string,
 };
-
-export default withStyles(styles)(ClassNames);
