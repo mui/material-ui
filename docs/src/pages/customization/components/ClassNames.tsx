e327fb14137c1d100e680d24dc5245cf2690cd0e
Lucas Silva <lucas.silva@avecbrasil.com.br>
2019-10-07 11:20:57 -0300

    refactor to use styles

--- docs/src/pages/customization/components/ClassNames.tsx
+++ docs/src/pages/customization/components/ClassNames.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import clsx from 'clsx';
-import { withStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import Button, { ButtonProps } from '@material-ui/core/Button';
 
 interface Props extends ButtonProps {
   children: React.ReactNode;
-  classes: { root: string };
   className: string;
 }
 
 // We can inject some CSS into the DOM.
-const styles = {
-  root: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    borderRadius: 3,
-    border: 0,
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  },
-};
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      borderRadius: 3,
+      border: 0,
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    },
+  }),
+);
 
-function ClassNames(props: Props) {
-  const { classes, children, className, ...other } = props;
+export default function ClassNames(props: Props) {
+  const { children, className, ...other } = props;
+  const classes = useStyles();
 
   return (
     <Button className={clsx(classes.root, className)} {...other}>
@@ -32,4 +34,4 @@ function ClassNames(props: Props) {
   );
 }
 
-export default withStyles(styles)(ClassNames);
+// export default withStyles(styles)(ClassNames);
