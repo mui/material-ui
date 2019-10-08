83a5f73b01c98a22231962c0f1ce5b2a9f38223a
Ganesh (SGAT) <limatgans@gmail.com>
2019-10-08 16:43:19 +0530

    [docs] Migrate Globals demo to TypeScript  (#17785)

---
+++ docs/src/pages/customization/globals/GlobalCss.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const theme = createMuiTheme({
+  overrides: {
+    // Style sheet name ⚛️
+    MuiButton: {
+      // Name of the rule
+      text: {
+        // Some CSS
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      },
+    },
+  },
+});
+
+function OverridesCss() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Button>Overrides CSS</Button>
+    </ThemeProvider>
+  );
+}
+
+export default OverridesCss;
