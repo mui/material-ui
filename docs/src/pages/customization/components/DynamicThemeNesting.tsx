2b47520f8ebfaca2324e2b17d03ef1a4f1413108
limatgans <limatgans@gmail.com>
2019-10-08 17:13:06 +0530

    [docs] Migrated Customization/Components to ts

---
+++ docs/src/pages/customization/components/DynamicThemeNesting.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { blue } from '@material-ui/core/colors';
+import Switch from '@material-ui/core/Switch';
+
+const defaultTheme = createMuiTheme();
+
+export default function DynamicThemeNesting() {
+  const [color, setColor] = React.useState('default');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setColor(event.target.checked ? 'blue' : 'default');
+  };
+
+  return (
+    <React.Fragment>
+      <FormControlLabel
+        control={
+          <Switch
+            checked={color === 'blue'}
+            onChange={handleChange}
+            color="primary"
+            value="dynamic-class-name"
+          />
+        }
+        label="Blue"
+      />
+      <ThemeProvider
+        theme={
+          color === 'blue'
+            ? {
+                ...defaultTheme,
+                palette: {
+                  ...defaultTheme.palette,
+                  secondary: {
+                    main: blue[500],
+                    contrastText: '#fff',
+                  },
+                },
+              }
+            : defaultTheme
+        }
+      >
+        <Button variant="contained" color="secondary">
+          {'Theme nesting'}
+        </Button>
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
