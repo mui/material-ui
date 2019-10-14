fc1bff6ce243aa4256ddc5d31919adbf7a78274d
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-10-14 13:36:07 +0200

    Recover ariaHidden but deprecate it

--- test/utils/init.d.ts
+++ test/utils/init.d.ts
@@ -3,6 +3,12 @@
 declare namespace Chai {
   interface Assertion {
     /**
+     * checks if the element in question is considered aria-hidden
+     * Does not replace accessibility check as that requires display/visibility/layout
+     * @deprecated Use `inaccessible` + `visible` instead
+     */
+    ariaHidden: Assertion;
+    /**
      * checks if the element is inaccessible
      */
     inaccessible: Assertion;
