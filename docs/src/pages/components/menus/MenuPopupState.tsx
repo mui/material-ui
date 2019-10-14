35898e707f387c0c34b8160bd5dbc0653ff9fa09
programistka <iwona@programistka.com>
2019-10-14 09:17:14 +0200

    [docs] Add Typescript demos for MenuPopupState

---
+++ docs/src/pages/components/menus/MenuPopupState.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+
+function MenuPopupState() {
+  return (
+    <PopupState variant="popover" popupId="demo-popup-menu">
+      {popupState => (
+        <React.Fragment>
+          <Button variant="contained" {...bindTrigger(popupState)}>
+            Open Menu
+          </Button>
+          <Menu {...bindMenu(popupState)}>
+            <MenuItem onClick={popupState.close}>Cake</MenuItem>
+            <MenuItem onClick={popupState.close}>Death</MenuItem>
+          </Menu>
+        </React.Fragment>
+      )}
+    </PopupState>
+  );
+}
+
+export default MenuPopupState;
