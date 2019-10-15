d3690132cd1638933f752723810f8567f9b9fe26
Matthew Brookes <github@nospam.33m.co>
2019-10-15 18:57:36 +0100

    Fix it - again!

--- docs/src/modules/components/Demo.js
+++ docs/src/modules/components/Demo.js
@@ -281,9 +281,10 @@ function Demo(props) {
       /export default .*(\n {2}return \(\n|\n {2}return )(.*?)(\n {2}\);\n}|;\n})/s,
     );
     // Just the match, or the full source if no match, so as not to break the Collapse transition.
-    jsx = jsx ? jsx[2] : demoData.raw;
+    jsx = jsx ? jsx[2] : code;
     // Number of leading spaces on the first line
     const indentSize = jsx.match(/^ */)[0].length;
+    // Remove leading spaces from each line
     return jsx.split(/\n/).reduce((acc, line) => `${acc}${line.slice(indentSize)}\n`, '');
   }
 
