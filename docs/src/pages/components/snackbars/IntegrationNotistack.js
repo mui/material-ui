a0a755ebd5c300e2038dcdf0fe416b84a93bfa52
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-10-16 19:14:44 +0200

    [docs] Write down the imperative API benefit
    This remembers me: https://dev.to/dmtrkovalenko/the-neatest-way-to-handle-alert-dialogs-in-react-1aoe

--- docs/src/pages/components/snackbars/IntegrationNotistack.js
+++ docs/src/pages/components/snackbars/IntegrationNotistack.js
@@ -11,13 +11,13 @@ function MyApp() {
 
   const handleClickVariant = variant => () => {
     // variant could be success, error, warning, info, or default
-    enqueueSnackbar('This is a warning message!', { variant });
+    enqueueSnackbar('This is a success message!', { variant });
   };
 
   return (
     <React.Fragment>
       <Button onClick={handleClick}>Show snackbar</Button>
-      <Button onClick={handleClickVariant('warning')}>Show warning snackbar</Button>
+      <Button onClick={handleClickVariant('success')}>Show success snackbar</Button>
     </React.Fragment>
   );
 }
