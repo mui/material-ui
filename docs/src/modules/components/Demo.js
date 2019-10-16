9bb461489b5612d54ba1f3068864492fd6a1dffb
Matthew Brookes <github@nospam.33m.co>
2019-10-16 22:58:53 +0100

    further fiddling

--- docs/src/modules/components/Demo.js
+++ docs/src/modules/components/Demo.js
@@ -280,17 +280,23 @@ function Demo(props) {
     let jsx = code.match(
       /export default .*(\n {2}return \(\n|\n {2}return )(.*?)(\n {2}\);\n}|;\n})/s,
     );
-    // Just the match, or the full source if no match, so as not to break the Collapse transition.
-    jsx = jsx ? jsx[2] : code;
-    // Number of leading spaces on the first line
-    const indentSize = jsx.match(/^ */)[0].length;
+    /* Just the match, otherwise the full source if no match or disabled,
+    so as not to break the Collapse transition. */
+    jsx = jsx && demoOptions.defaultCodeOpen !== false ? jsx[2] : code;
+
     // Remove leading spaces from each line
-    return jsx.split(/\n/).reduce((acc, line) => `${acc}${line.slice(indentSize)}\n`, '');
+    return jsx.split(/\n/).reduce(
+      (acc, line) =>
+        `${acc}${line.slice(
+          // Number of leading spaces on the first line
+          jsx.match(/^ */)[0].length,
+        )}\n`,
+      '',
+    );
   }
 
   const jsx = jsxOnly();
-  const showPreview =
-    demoOptions.defaultCodeOpen !== false && jsx !== demoData.raw && jsx.split(/\n/).length <= 20;
+  const showPreview = jsx !== demoData.raw && jsx.split(/\n/).length <= 20;
 
   let showCodeLabel;
   if (codeOpen) {
