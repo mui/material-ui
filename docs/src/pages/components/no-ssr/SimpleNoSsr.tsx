42ba28edc121b2a47fc6c14cc960376b9048d61a
Ganesh <ganesh_r@f22labs.com>
2019-10-17 07:27:19 +0530

    [docs]:add ts demos for SimpleNoSsr and FrameDeferring

---
+++ docs/src/pages/components/no-ssr/SimpleNoSsr.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { WithStyles } from '@material-ui/styles';
+import NoSsr from '@material-ui/core/NoSsr';
+import Button from '@material-ui/core/Button';
+
+const styles = (theme: any) => ({
+  button: {
+    display: 'block',
+    margin: theme.spacing(2),
+  },
+});
+
+export interface iProps extends WithStyles<typeof styles> {}
+
+function SimpleNoSsr(props: iProps) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <Button className={classes.button} variant="contained" color="primary">
+        Server & Client
+      </Button>
+      <NoSsr>
+        <Button className={classes.button} variant="contained" color="secondary">
+          Client only
+        </Button>
+      </NoSsr>
+    </div>
+  );
+}
+
+export default withStyles(styles)(SimpleNoSsr);
