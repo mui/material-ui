8070047b701a372236696ededc728ca677b0be6e
yishai <yishaib@gmail.com>
2019-10-20 13:51:39 +0300

    yarn docs:typescript:formatted

--- docs/src/pages/components/checkboxes/FormControlLabelPosition.js
+++ docs/src/pages/components/checkboxes/FormControlLabelPosition.js
@@ -6,16 +6,10 @@ import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 
 function FormControlLabelPosition() {
-  const [value, setValue] = React.useState('female');
-
-  const handleChange = event => {
-    setValue(event.target.value);
-  };
-
   return (
     <FormControl component="fieldset">
-      <FormLabel component="legend">labelPlacement</FormLabel>
-      <FormGroup aria-label="position" name="position" value={value} onChange={handleChange} row>
+      <FormLabel component="legend">Label Placement</FormLabel>
+      <FormGroup aria-label="position" row>
         <FormControlLabel
           value="top"
           control={<Checkbox color="primary" />}
