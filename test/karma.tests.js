ee330c0e9e1b11fcf679f3728546ecdb1e223726
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-10-22 18:57:59 +0200

    Satisfy react environment

--- test/karma.tests.js
+++ test/karma.tests.js
@@ -1,3 +1,6 @@
+// https://reactjs.org/docs/javascript-environment-requirements.html
+import 'core-js/es6/map';
+import 'core-js/es6/set';
 // https://github.com/airbnb/enzyme/issues/1792
 import 'core-js/modules/es6.array.from';
 
