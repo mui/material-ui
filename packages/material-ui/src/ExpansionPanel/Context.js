2c8d5a007ad1d40c10a77ab23f267fe0c9d418b0
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-10-29 14:35:15 +0100

    fix: "no suitable component definition found"

--- packages/material-ui/src/ExpansionPanel/Context.js
+++ packages/material-ui/src/ExpansionPanel/Context.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 /**
+ * @ignore - internal component.
  * @type {React.Context<{} | {expanded: boolean, disabled: boolean, toggle: () => void}>}
  */
 const Context = React.createContext({});
