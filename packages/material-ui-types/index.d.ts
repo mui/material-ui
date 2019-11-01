9b9b6d60fa6d5f0ee5e66150b9132ea255cd73cb
Andrew Casey <andrew.casey@microsoft.com>
2019-10-31 20:35:47 -0700

    Restore string check and trailing newline

--- packages/material-ui-types/index.d.ts
+++ packages/material-ui-types/index.d.ts
@@ -64,8 +64,14 @@ export type And<A, B, C = true> = (A | B | C) extends true ? true : false;
  * @internal
  *
  * check if a type is `{}`
+ *
+ * 1. false if the given type has any members
+ * 2. false if the type is `object` which is the only other type with no members
+ *  {} is a top type so e.g. `string extends {}` but not `string extends object`
+ * 3. false if the given type is `unknown`
  */
 export type IsEmptyInterface<T> = And<
   {} extends T ? true : false,
+  string extends T ? true : false,
   unknown extends T ? false : true
 >;
