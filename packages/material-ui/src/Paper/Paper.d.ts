7440461725a92d20a576fc8434fda2bd991fa99f
James Cuénod <jcuenod@users.noreply.github.com>
2019-11-07 09:04:55 -0600

    [docs] Improve demo clarity by using form elements (#18241)

--- packages/material-ui/src/Paper/Paper.d.ts
+++ packages/material-ui/src/Paper/Paper.d.ts
@@ -3,7 +3,7 @@ import { StandardProps } from '..';
 
 export interface PaperProps
   extends StandardProps<React.HTMLAttributes<HTMLDivElement>, PaperClassKey> {
-  component?: React.ElementType<React.HTMLAttributes<HTMLDivElement>>;
+  component?: React.ElementType<React.HTMLAttributes<HTMLElement>>;
   elevation?: number;
   square?: boolean;
 }
