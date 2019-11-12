f0e7461d160c4337bdddac4b41c8773067baccff
merceyz <merceyz@users.noreply.github.com>
2019-11-12 19:37:30 +0100

    [lab] Export everything

--- packages/material-ui-lab/src/index.d.ts
+++ packages/material-ui-lab/src/index.d.ts
@@ -1,11 +1,32 @@
 export { default as Autocomplete } from './Autocomplete';
+export * from './Autocomplete';
+
 export { default as Rating } from './Rating';
+export * from './Rating';
+
 export { default as Skeleton } from './Skeleton';
+export * from './Skeleton';
+
 export { default as SpeedDial } from './SpeedDial';
+export * from './SpeedDial';
+
 export { default as SpeedDialAction } from './SpeedDialAction';
+export * from './SpeedDialAction';
+
 export { default as SpeedDialIcon } from './SpeedDialIcon';
+export * from './SpeedDialIcon';
+
 export { default as ToggleButton } from './ToggleButton';
+export * from './ToggleButton';
+
 export { default as ToggleButtonGroup } from './ToggleButtonGroup';
-export { default as TreeView } from './TreeView';
+export * from './ToggleButtonGroup';
+
 export { default as TreeItem } from './TreeItem';
-export { default as useAutocomplete, createFilterOptions } from './useAutocomplete';
+export * from './TreeItem';
+
+export { default as TreeView } from './TreeView';
+export * from './TreeView';
+
+export { default as useAutocomplete } from './useAutocomplete';
+export * from './useAutocomplete';
