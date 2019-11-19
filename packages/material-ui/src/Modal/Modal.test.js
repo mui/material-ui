d1aae91f602766638a1a1da2fbb2d4a5f4dc56b6
Andreas Heim <andreas@moz.com>
2019-11-19 04:33:23 -0800

    [Modal] Prefer to lock scroll on body than html element (#18445)

--- packages/material-ui/src/Modal/Modal.test.js
+++ packages/material-ui/src/Modal/Modal.test.js
@@ -632,11 +632,11 @@ describe('<Modal />', () => {
       };
 
       const wrapper = mount(<TestCase open={false} />);
-      assert.strictEqual(document.body.parentElement.style.overflow, '');
+      assert.strictEqual(document.body.style.overflow, '');
       wrapper.setProps({ open: true });
-      assert.strictEqual(document.body.parentElement.style.overflow, 'hidden');
+      assert.strictEqual(document.body.style.overflow, 'hidden');
       wrapper.setProps({ open: false });
-      assert.strictEqual(document.body.parentElement.style.overflow, '');
+      assert.strictEqual(document.body.style.overflow, '');
     });
 
     it('should open and close with Transitions', done => {
@@ -661,17 +661,17 @@ describe('<Modal />', () => {
 
       let wrapper;
       const onEntered = () => {
-        assert.strictEqual(document.body.parentElement.style.overflow, 'hidden');
+        assert.strictEqual(document.body.style.overflow, 'hidden');
         wrapper.setProps({ open: false });
       };
 
       const onExited = () => {
-        assert.strictEqual(document.body.parentElement.style.overflow, '');
+        assert.strictEqual(document.body.style.overflow, '');
         done();
       };
 
       wrapper = mount(<TestCase onEntered={onEntered} onExited={onExited} open={false} />);
-      assert.strictEqual(document.body.parentElement.style.overflow, '');
+      assert.strictEqual(document.body.style.overflow, '');
       wrapper.setProps({ open: true });
     });
   });
@@ -723,23 +723,23 @@ describe('<Modal />', () => {
 
       let wrapper;
       const onEntered = () => {
-        assert.strictEqual(document.body.parentElement.style.overflow, 'hidden');
+        assert.strictEqual(document.body.style.overflow, 'hidden');
         wrapper.setProps({ open: false });
       };
 
       const onExited = () => {
-        assert.strictEqual(document.body.parentElement.style.overflow, '');
+        assert.strictEqual(document.body.style.overflow, '');
         done();
       };
 
       const onExiting = () => {
-        assert.strictEqual(document.body.parentElement.style.overflow, 'hidden');
+        assert.strictEqual(document.body.style.overflow, 'hidden');
       };
 
       wrapper = mount(
         <TestCase onEntered={onEntered} onExiting={onExiting} onExited={onExited} open={false} />,
       );
-      assert.strictEqual(document.body.parentElement.style.overflow, '');
+      assert.strictEqual(document.body.style.overflow, '');
       wrapper.setProps({ open: true });
     });
 
@@ -766,23 +766,23 @@ describe('<Modal />', () => {
 
       let wrapper;
       const onEntered = () => {
-        assert.strictEqual(document.body.parentElement.style.overflow, 'hidden');
+        assert.strictEqual(document.body.style.overflow, 'hidden');
         wrapper.setProps({ open: false });
       };
 
       const onExited = () => {
-        assert.strictEqual(document.body.parentElement.style.overflow, '');
+        assert.strictEqual(document.body.style.overflow, '');
         done();
       };
 
       const onExiting = () => {
-        assert.strictEqual(document.body.parentElement.style.overflow, '');
+        assert.strictEqual(document.body.style.overflow, '');
       };
 
       wrapper = mount(
         <TestCase onEntered={onEntered} onExiting={onExiting} onExited={onExited} open={false} />,
       );
-      assert.strictEqual(document.body.parentElement.style.overflow, '');
+      assert.strictEqual(document.body.style.overflow, '');
       wrapper.setProps({ open: true });
     });
   });
