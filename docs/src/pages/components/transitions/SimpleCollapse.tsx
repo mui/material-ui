c98613ae2b5ea7b68c491eaa3ff729444dc8cd2d
Weslen do Nascimento <weslen-7316@outlook.com>
2019-11-20 11:56:15 -0300

    [Collapse] Add a warn when collapsedHeight misses units (#18461)

--- docs/src/pages/components/transitions/SimpleCollapse.tsx
+++ docs/src/pages/components/transitions/SimpleCollapse.tsx
@@ -50,7 +50,7 @@ export default function SimpleCollapse() {
             </svg>
           </Paper>
         </Collapse>
-        <Collapse in={checked} collapsedHeight="40px">
+        <Collapse in={checked} collapsedHeight={40}>
           <Paper elevation={4} className={classes.paper}>
             <svg className={classes.svg}>
               <polygon points="0,100 50,00, 100,100" className={classes.polygon} />
