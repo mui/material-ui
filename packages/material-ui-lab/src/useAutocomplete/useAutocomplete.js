aed1f38f17aeb25f29b9948c6134c8ed6364f1c6
gyeongmin kim <gyeongmin251@gmail.com>
2019-11-22 03:11:48 +0900

    [Autocomplete] Ignore object keys in default filter (#18480)

--- packages/material-ui-lab/src/useAutocomplete/useAutocomplete.js
+++ packages/material-ui-lab/src/useAutocomplete/useAutocomplete.js
@@ -11,12 +11,30 @@ function stripDiacritics(string) {
     : string;
 }
 
+function defaultStringify(value) {
+  if (value == null) {
+    return '';
+  }
+
+  if (typeof value === 'string') {
+    return value;
+  }
+
+  if (typeof value === 'object') {
+    return Object.keys(value)
+      .map(key => value[key])
+      .join(' ');
+  }
+
+  return JSON.stringify(value);
+}
+
 export function createFilterOptions(config = {}) {
   const {
     ignoreAccents = true,
     ignoreCase = true,
     matchFrom = 'any',
-    stringify = JSON.stringify,
+    stringify = defaultStringify,
     trim = false,
   } = config;
 
