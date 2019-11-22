3b8c5cdd85a3c526dfab29df9f6dd8f8df4ece57
Angelk90 <angelo.k90@hotmail.it>
2019-11-22 19:17:14 +0100

    [core] Add localization itIT

--- packages/material-ui/src/locale/index.d.ts
+++ packages/material-ui/src/locale/index.d.ts
@@ -2,6 +2,7 @@ export const deDE: object;
 export const enUS: object;
 export const esES: object;
 export const frFR: object;
+export const itIT: object;
 export const jaJP: object;
 export const ptBR: object;
 export const ruRU: object;
