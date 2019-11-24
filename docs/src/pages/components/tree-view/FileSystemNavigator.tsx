0915b61896f688ec613839f32dba5138b7df27c2
joshwooding <12938082+joshwooding@users.noreply.github.com>
2019-11-24 15:13:12 +0000

    prettier

--- docs/src/pages/components/tree-view/FileSystemNavigator.tsx
+++ docs/src/pages/components/tree-view/FileSystemNavigator.tsx
@@ -28,6 +28,7 @@ export default function FileSystemNavigator() {
         <TreeItem nodeId="4" label="Webstorm" />
       </TreeItem>
       <TreeItem nodeId="5" label="Documents">
+        <TreeItem nodeId="10" label="OSS" />
         <TreeItem nodeId="6" label="Material-UI">
           <TreeItem nodeId="7" label="src">
             <TreeItem nodeId="8" label="index.js" />
