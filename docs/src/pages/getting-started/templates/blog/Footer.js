e2f36acafec225e95f9c77ac080989a5e9de355a
Matthew Brookes <github@nospam.33m.co>
2019-11-30 19:08:15 +0000

    [docs] Break up blog template into smaller sections

---
+++ docs/src/pages/getting-started/templates/blog/Footer.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    // marginTop: theme.spacing(8),
+    padding: theme.spacing(6, 0),
+  },
+}));
+
+export default function Footer(props) {
+  const classes = useStyles();
+  const { description, title } = props;
+
+  return (
+    <footer className={classes.footer}>
+      <Container maxWidth="lg">
+        <Typography variant="h6" align="center" gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          {description}
+        </Typography>
+        <Copyright />
+      </Container>
+    </footer>
+  );
+}
+
+Footer.propTypes = {
+  description: PropTypes.string,
+  title: PropTypes.string,
+};
---
+++ docs/src/pages/getting-started/templates/blog/Sidebar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles(theme => ({
+  sidebarAboutBox: {
+    padding: theme.spacing(2),
+    backgroundColor: theme.palette.grey[200],
+  },
+  sidebarSection: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+export default function Sidebar(props) {
+  const classes = useStyles();
+  const { archives, description, social, title } = props;
+
+  return (
+    <Grid item xs={12} md={4}>
+      <Paper elevation={0} className={classes.sidebarAboutBox}>
+        <Typography variant="h6" gutterBottom>
+          {title}
+        </Typography>
+        <Typography>{description}</Typography>
+      </Paper>
+      <Typography variant="h6" gutterBottom className={classes.sidebarSection}>
+        Archives
+      </Typography>
+      {archives.map(archive => (
+        <Link display="block" variant="body1" href={archive.url} key={archive.title}>
+          {archive.title}
+        </Link>
+      ))}
+      <Typography variant="h6" gutterBottom className={classes.sidebarSection}>
+        Social
+      </Typography>
+      {social.map(network => (
+        <Link display="block" variant="body1" href="#" key={network}>
+          {network}
+        </Link>
+      ))}
+    </Grid>
+  );
+}
+
+Sidebar.propTypes = {
+  archives: PropTypes.array,
+  description: PropTypes.string,
+  social: PropTypes.array,
+  title: PropTypes.string,
+};
