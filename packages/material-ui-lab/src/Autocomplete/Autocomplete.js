3470b7b83715d4663dc7277eddf3245b6537c92b
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-12-17 08:37:02 +0100

    yarn proptypes

--- packages/material-ui-lab/src/Autocomplete/Autocomplete.js
+++ packages/material-ui-lab/src/Autocomplete/Autocomplete.js
@@ -508,7 +508,7 @@ Autocomplete.propTypes = {
   /**
    * Force the visibility display of the popup icon.
    */
-  forcePopupIcon: PropTypes.oneOf([true, false ,'auto']),
+  forcePopupIcon: PropTypes.oneOfType([PropTypes.oneOf(['auto']), PropTypes.bool]),
   /**
    * If `true`, the Autocomplete is free solo, meaning that the user input is not bound to provided options.
    */
