df67e5b757aa226258373a704da7be8267c7e6ba
Jeong Woo Chang <inspired.jw@gmail.com>
2019-12-22 03:23:17 -0800

    [I10n] Add koKR translations (Korean)

--- packages/material-ui/src/locale/index.d.ts
+++ packages/material-ui/src/locale/index.d.ts
@@ -8,6 +8,7 @@ export const frFR: object;
 export const idID: object;
 export const itIT: object;
 export const jaJP: object;
+export const koKR: object;
 export const nlNL: object;
 export const plPL: object;
 export const ptBR: object;
