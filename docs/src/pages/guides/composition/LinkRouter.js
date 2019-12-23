d434dbb620dbe5fc8c27ccad8f5416cdc1a602f7
Olivier Tassinari <olivier.tassinari@gmail.com>
2019-12-23 12:26:15 +0100

    Use ref instead of innerRef
    Available since https://github.com/ReactTraining/react-router/pull/6914, react-router v5.1.0.

--- docs/src/pages/guides/composition/LinkRouter.js
+++ docs/src/pages/guides/composition/LinkRouter.js
@@ -4,23 +4,19 @@ import { MemoryRouter as Router } from 'react-router';
 import { Link as RouterLink } from 'react-router-dom';
 import Link from '@material-ui/core/Link';
 
-// The use of React.forwardRef will no longer be required for react-router-dom v6.
-// See https://github.com/ReactTraining/react-router/issues/6056
-const Link1 = React.forwardRef((props, ref) => <RouterLink innerRef={ref} {...props} />);
-
-const Link2 = React.forwardRef((props, ref) => (
-  <RouterLink innerRef={ref} to="/getting-started/installation/" {...props} />
+const LinkBehavior = React.forwardRef((props, ref) => (
+  <RouterLink ref={ref} to="/getting-started/installation/" {...props} />
 ));
 
 export default function LinkRouter() {
   return (
     <Router>
       <div>
-        <Link component={Link1} to="/">
+        <Link component={RouterLink} to="/">
           With prop forwarding
         </Link>
         <br />
-        <Link component={Link2}>Without prop forwarding</Link>
+        <Link component={LinkBehavior}>Without prop forwarding</Link>
       </div>
     </Router>
   );
