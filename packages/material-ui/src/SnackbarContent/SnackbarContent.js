6def062a63b3f5e28083299ac67238ae5c0c59ec
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2019-12-26 11:14:44 -0500

    Update packages/material-ui/src/SnackbarContent/SnackbarContent.js
    Co-Authored-By: Olivier Tassinari <olivier.tassinari@gmail.com>

--- packages/material-ui/src/SnackbarContent/SnackbarContent.js
+++ packages/material-ui/src/SnackbarContent/SnackbarContent.js
@@ -61,7 +61,7 @@ const SnackbarContent = React.forwardRef(function SnackbarContent(props, ref) {
 
 SnackbarContent.propTypes = {
   /**
-   * The action to display. It renders after the message, at the end of the alert.
+   * The action to display. It renders after the message, at the end of the snackbar.
    */
   action: PropTypes.node,
   /**
