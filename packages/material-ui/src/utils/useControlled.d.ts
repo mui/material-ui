064c6487bd314abfbe033d604cc63c5430b4af5d
Matheus Wichman <matheushw@outlook.com>
2020-01-07 16:41:01 +0000

    [core] Add types

---
+++ packages/material-ui/src/utils/useControlled.d.ts
@@ -0,0 +1,22 @@
+export interface UseControlledProps {
+  /**
+   * This prop contains the component value when it's controlled.
+   */
+  controlled: any;
+  /**
+   * The default value.
+   */
+  default: any;
+  /**
+   * The component name displayed in warnings.
+   */
+  name: string;
+}
+
+export default function useControlled(
+  props: UseControlledProps,
+): {
+  value: any;
+  setValue: () => void;
+  isControlled: boolean;
+};
