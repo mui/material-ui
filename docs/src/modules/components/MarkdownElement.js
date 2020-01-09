467807c28857084955743fd2a94065b80d4dd316
Matheus Wichman <matheushw@outlook.com>
2020-01-09 13:11:53 -0300

    Use palette.divider color

--- docs/src/modules/components/MarkdownElement.js
+++ docs/src/modules/components/MarkdownElement.js
@@ -237,7 +237,7 @@ const styles = theme => ({
       '& .prop-default': {
         fontSize: 13,
         fontFamily: 'Consolas, "Liberation Mono", Menlo, monospace',
-        borderBottom: `1px dotted ${theme.palette.text.disabled}`,
+        borderBottom: `1px dotted ${theme.palette.divider}`,
       },
     },
     '& td': {
