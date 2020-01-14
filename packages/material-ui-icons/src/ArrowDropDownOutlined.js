bb970428dbc391f2f917a885bd9b9bc2c2f9bd00
Abraão Zaidan <abraao.zaidan@gmail.com>
2020-01-14 14:29:14 -0300

    [icons] ArrowDropUpOutlined and ArrowDropDownOutlined improved
    There was no difference between ArrowDropUpOutlined and ArrowDropUp. The same problem occurred between ArrowDropDownOutlined and ArrowDropDown.

--- packages/material-ui-icons/src/ArrowDropDownOutlined.js
+++ packages/material-ui-icons/src/ArrowDropDownOutlined.js
@@ -2,5 +2,5 @@ import React from 'react';
 import createSvgIcon from './utils/createSvgIcon';
 
 export default createSvgIcon(
-  <path d="M7 10l5 5 5-5H7z" />
+  <path d="m12 14-5-5h10zm-0.041-0.88 3.5-3.5h-7z" />
 , 'ArrowDropDownOutlined');
--- packages/material-ui-icons/src/ArrowDropUpOutlined.js
+++ packages/material-ui-icons/src/ArrowDropUpOutlined.js
@@ -2,5 +2,5 @@ import React from 'react';
 import createSvgIcon from './utils/createSvgIcon';
 
 export default createSvgIcon(
-  <path d="M7 14l5-5 5 5H7z" />
+  <path d="m12 9-5 5h10l-5-5zm-0.041 0.88 3.5 3.5h-7l3.5-3.5z" />
 , 'ArrowDropUpOutlined');
