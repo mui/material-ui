02411c89e5bc1c8b632c8f4e8768558fd4a3fa9a
Benjamin Swerdlow <swerdlowbenjamin@gmail.com>
2020-01-17 19:17:33 -0800

    Using prettier
    Switched single quotes to double quotes

--- docs/src/pages/components/backdrop/SimpleBackdrop.js
+++ docs/src/pages/components/backdrop/SimpleBackdrop.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import Button from "@material-ui/core/Button";
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
