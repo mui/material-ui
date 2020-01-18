18dc9f7f49bac6e61bc9694a7ead3cafc2023ce2
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2020-01-18 12:42:28 -0800

    [Alert] Improve Transition demo (#19283)

--- docs/src/pages/components/alert/TransitionAlerts.tsx
+++ docs/src/pages/components/alert/TransitionAlerts.tsx
@@ -17,7 +17,7 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-export default function ActionAlerts() {
+export default function TransitionAlerts() {
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
 
@@ -42,6 +42,7 @@ export default function ActionAlerts() {
         </Alert>
       </Collapse>
       <Button
+        disabled={open}
         variant="outlined"
         onClick={() => {
           setOpen(true);
