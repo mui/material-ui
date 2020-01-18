7ce09a879c3a9b79a64a532a498269190bc69a07
merceyz <merceyz@users.noreply.github.com>
2020-01-18 13:42:51 +0100

    chore(copy-files): remove createModulePackages

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -14,42 +14,6 @@ async function includeFileInBuild(file) {
   console.log(`Copied ${sourcePath} to ${targetPath}`);
 }
 
-/**
- * Puts a package.json into every immediate child directory of rootDir.
- * That package.json contains information about esm for bundlers so that imports
- * like import Typography from '@material-ui/core/Typography' are tree-shakeable.
- *
- * It also tests that an this import can be used in typescript by checking
- * if an index.d.ts is present at that path.
- *
- * @param {string} rootDir
- */
-async function createModulePackages({ from, to }) {
-  const directoryPackages = glob.sync('*/index.js', { cwd: from }).map(path.dirname);
-
-  await Promise.all(
-    directoryPackages.map(async directoryPackage => {
-      const packageJson = {
-        sideEffects: false,
-        module: path.join('../esm', directoryPackage, 'index.js'),
-        typings: './index.d.ts',
-      };
-      const packageJsonPath = path.join(to, directoryPackage, 'package.json');
-
-      const [typingsExist] = await Promise.all([
-        fse.exists(path.join(to, directoryPackage, 'index.d.ts')),
-        fse.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2)),
-      ]);
-
-      if (!typingsExist) {
-        throw new Error(`index.d.ts for ${directoryPackage} is missing`);
-      }
-
-      return packageJsonPath;
-    }),
-  );
-}
-
 async function typescriptCopy({ from, to }) {
   if (!(await fse.exists(to))) {
     console.warn(`path ${to} does not exists`);
@@ -70,7 +34,7 @@ async function createPackageFile() {
     ...packageDataOther,
     private: false,
     main: './index.js',
-    module: './esm/index.js',
+    module: './index.mjs',
     typings: './index.d.ts',
   };
   const targetPath = path.resolve(buildPath, './package.json');
@@ -96,6 +60,8 @@ async function addLicense(packageData) {
   await Promise.all(
     [
       './index.js',
+      './index.mjs',
+      // TODO: Remove next line in v5
       './esm/index.js',
       './umd/material-ui.development.js',
       './umd/material-ui.production.min.js',
@@ -130,8 +96,6 @@ async function run() {
 
     // TypeScript
     await typescriptCopy({ from: srcPath, to: buildPath });
-
-    await createModulePackages({ from: srcPath, to: buildPath });
   } catch (err) {
     console.error(err);
     process.exit(1);
