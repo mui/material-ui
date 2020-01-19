9268a5e78195f645baed8b8e674b07a406d19291
Matthew Brookes <github@nospam.33m.co>
2020-01-19 22:49:33 +0000

    no transition, disable ripple

--- packages/material-ui-lab/src/PaginationItem/PaginationItem.js
+++ packages/material-ui-lab/src/PaginationItem/PaginationItem.js
@@ -18,9 +18,6 @@ const styles = theme => ({
     height: 32,
     margin: '0 3px',
     color: theme.palette.text.primary,
-    transition: theme.transitions.create('background-color', {
-      duration: theme.transitions.duration.short,
-    }),
     '&:hover, &:focus': {
       backgroundColor: fade(theme.palette.action.active, theme.palette.action.hoverOpacity),
       // Reset on touch devices, it doesn't add specificity
@@ -276,6 +273,7 @@ const PaginationItem = React.forwardRef(function PaginationItem(props, ref) {
           [classes.selected]: selected,
           [classes[`size${capitalize(size)}`]]: size !== 'medium',
         })}
+        disableRipple
         {...other}
       >
         {type === 'page' && page}
