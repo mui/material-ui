8652b8e5d178361e783ee96bc0c8378d4be6f280
Rostislav Bobrovsky <rostislav.bobrovsky@gmail.com>
2020-01-21 12:42:38 +0200

    [Radio] Test disabled state with FormControl

--- packages/material-ui/src/Radio/Radio.test.js
+++ packages/material-ui/src/Radio/Radio.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 import { getClasses, createMount } from '@material-ui/core/test-utils';
 import describeConformance from '@material-ui/core/test-utils/describeConformance';
+import { createClientRender } from 'test/utils/createClientRender';
+import FormControl from '../FormControl';
 import Radio from './Radio';
 import IconButton from '../IconButton';
 
 describe('<Radio />', () => {
+  const render = createClientRender();
   let classes;
   let mount;
 
@@ -45,6 +48,52 @@ describe('<Radio />', () => {
     it('should render a checked icon', () => {
       const wrapper = mount(<Radio checked />);
       assert.strictEqual(wrapper.find('svg[data-mui-test="RadioButtonCheckedIcon"]').length, 1);
+    });
+  });
+
+  describe('with FormControl', () => {
+    describe('enabled', () => {
+      it('should not have the disabled class', () => {
+        const { getByTestId } = render(
+          <FormControl>
+            <Radio data-testid="root" />
+          </FormControl>,
+        );
+
+        expect(getByTestId('root')).not.to.have.class(classes.disabled);
+      });
+
+      it('should be overridden by props', () => {
+        const { getByTestId } = render(
+          <FormControl>
+            <Radio data-testid="root" disabled />
+          </FormControl>,
+        );
+
+        expect(getByTestId('root')).to.have.class(classes.disabled);
+      });
+    });
+
+    describe('disabled', () => {
+      it('should have the disabled class', () => {
+        const { getByTestId } = render(
+          <FormControl disabled>
+            <Radio data-testid="root" />
+          </FormControl>,
+        );
+
+        expect(getByTestId('root')).to.have.class(classes.disabled);
+      });
+
+      it('should be overridden by props', () => {
+        const { getByTestId } = render(
+          <FormControl disabled>
+            <Radio data-testid="root" disabled={false} />
+          </FormControl>,
+        );
+
+        expect(getByTestId('root')).not.to.have.class(classes.disabled);
+      });
     });
   });
 });
