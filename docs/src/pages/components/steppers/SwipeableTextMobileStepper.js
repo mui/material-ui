50ad6b9ad8f558d5004bebc1bf26c0b98471b2f8
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-01-21 10:07:30 +0100

    [docs] Inline TextMobileStepper images

--- docs/src/pages/components/steppers/SwipeableTextMobileStepper.js
+++ docs/src/pages/components/steppers/SwipeableTextMobileStepper.js
@@ -14,28 +14,23 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 const tutorialSteps = [
   {
     label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+    imgPath: '/static/images/steppers/golden-gate.jpeg',
   },
   {
     label: 'Bird',
-    imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+    imgPath: '/static/images/steppers/bird.jpeg',
   },
   {
     label: 'Bali, Indonesia',
-    imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
+    imgPath: '/static/images/steppers/bali-garden.jpeg',
   },
   {
     label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
+    imgPath: '/static/images/steppers/neonbrand.jpeg',
   },
   {
     label: 'Goč, Serbia',
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+    imgPath: '/static/images/steppers/serbia-nature.jpeg',
   },
 ];
 
