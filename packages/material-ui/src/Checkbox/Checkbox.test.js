3b176f0710bb46385cd2382b2c38c024e467cf91
Rostislav Bobrovsky <rostislav.bobrovsky@gmail.com>
2020-01-21 12:38:03 +0200

    [Checkbox] Test disabled state with FormControl

--- packages/material-ui/src/Checkbox/Checkbox.test.js
+++ packages/material-ui/src/Checkbox/Checkbox.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 import { getClasses, createMount } from '@material-ui/core/test-utils';
 import describeConformance from '../test-utils/describeConformance';
+import { createClientRender } from 'test/utils/createClientRender';
 import Checkbox from './Checkbox';
+import FormControl from '../FormControl';
 import IconButton from '../IconButton';
 
+
 describe('<Checkbox />', () => {
+  const render = createClientRender();
   let classes;
   let mount;
 
@@ -36,6 +40,52 @@ describe('<Checkbox />', () => {
     it('should render an indeterminate icon', () => {
       const wrapper = mount(<Checkbox indeterminate />);
       assert.strictEqual(wrapper.find('svg[data-mui-test="IndeterminateCheckBoxIcon"]').length, 1);
+    });
+  });
+
+  describe('with FormControl', () => {
+    describe('enabled', () => {
+      it('should not have the disabled class', () => {
+        const { getByTestId } = render(
+          <FormControl>
+            <Checkbox data-testid="root" />
+          </FormControl>,
+        );
+
+        expect(getByTestId('root')).not.to.have.class(classes.disabled);
+      });
+
+      it('should be overridden by props', () => {
+        const { getByTestId } = render(
+          <FormControl>
+            <Checkbox data-testid="root" disabled />
+          </FormControl>,
+        );
+
+        expect(getByTestId('root')).to.have.class(classes.disabled);
+      });
+    });
+
+    describe('disabled', () => {
+      it('should have the disabled class', () => {
+        const { getByTestId } = render(
+          <FormControl disabled>
+            <Checkbox data-testid="root" />
+          </FormControl>,
+        );
+
+        expect(getByTestId('root')).to.have.class(classes.disabled);
+      });
+
+      it('should be overridden by props', () => {
+        const { getByTestId } = render(
+          <FormControl disabled>
+            <Checkbox data-testid="root" disabled={false} />
+          </FormControl>,
+        );
+
+        expect(getByTestId('root')).not.to.have.class(classes.disabled);
+      });
     });
   });
 });
