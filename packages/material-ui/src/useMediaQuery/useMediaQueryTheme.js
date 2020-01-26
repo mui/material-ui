143e908fd747c9c1ffe6f1f63d62bc37c24db396
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-01-26 21:53:30 +0100

    [core] Prefer early export default for consistency

--- packages/material-ui/src/useMediaQuery/useMediaQueryTheme.js
+++ packages/material-ui/src/useMediaQuery/useMediaQueryTheme.js
@@ -1,8 +1,6 @@
 import useMediaQuery from './useMediaQuery';
 
 // TODO to deprecate in v4.x and remove in v5
-function useMediaQueryTheme(...args) {
+export default function useMediaQueryTheme(...args) {
   return useMediaQuery(...args);
 }
-
-export default useMediaQueryTheme;
