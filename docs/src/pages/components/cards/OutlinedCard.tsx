20cd5cbf600bf973c8481c57f7cdca446fa0a892
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-01-26 21:39:49 +0100

    [docs] Use style root convention

--- docs/src/pages/components/cards/OutlinedCard.tsx
+++ docs/src/pages/components/cards/OutlinedCard.tsx
@@ -7,7 +7,7 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
-  card: {
+  root: {
     minWidth: 275,
   },
   bullet: {
@@ -28,7 +28,7 @@ export default function OutlinedCard() {
   const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <Card className={classes.card} variant="outlined">
+    <Card className={classes.root} variant="outlined">
       <CardContent>
         <Typography className={classes.title} color="textSecondary" gutterBottom>
           Word of the Day
