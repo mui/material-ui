5142a9857b853c7d69e1ae5201a39737e77f4817
Matthew Brookes <github@nospam.33m.co>
2020-01-27 23:15:31 +0000

    Link to source tweet

--- docs/src/modules/components/HomeQuotes.js
+++ docs/src/modules/components/HomeQuotes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
+import Link from 'docs/src/modules/components/Link';
 import Card from '@material-ui/core/Card';
-import Grid from '@material-ui/core/Grid';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 import TwitterIcon from '@material-ui/icons/Twitter';
@@ -17,6 +19,7 @@ const quotes = [
     avatar: 'https://pbs.twimg.com/profile_images/1134188599170215936/9CUB-yeB_400x400.jpg',
     name: 'Aumit Leon',
     username: '@aumitleon',
+    tweet: 'https://twitter.com/aumitleon/status/1210396946566963200',
     quote:
       'Material-UI continues to blow my mind how easily I can put together really aesthetic and functional components and minimize overhead.',
   },
@@ -24,6 +27,7 @@ const quotes = [
     avatar: 'https://pbs.twimg.com/profile_images/1169043984561389568/pEdFvVIW_400x400.jpg',
     name: 'Derek Shanks',
     username: '@fragileglass',
+    tweet: 'https://twitter.com/fragileglass/status/1205256087290753025',
     quote:
       'It’s a game changer with how nicely it works with React. It’s made working with React so much more enjoyable. Everything is configurable and predictable. Bootstrap was killing me. It was hijacking my whole project.',
   },
@@ -31,6 +35,7 @@ const quotes = [
     avatar: 'https://pbs.twimg.com/profile_images/1220819548523331584/3T1G8g1q_400x400.jpg',
     name: 'Mohamed EL AYADI',
     username: '@MohamedELAYAD19',
+    tweet: "https://twitter.com/MohamedELAYAD19/status/1208118580430229504",
     quote:
       "Such a great library. I have used Material-UI for the last two years as the main react ui library in my projects (in more than 4 companies!) and I find that it's really great! A lot of good work and dedication are put in there. Salute to the team!",
   },
@@ -45,6 +50,7 @@ const quotes = [
     avatar: 'https://pbs.twimg.com/profile_images/849731047625502720/nudIAz2B_400x400.jpg',
     name: 'Rodrigo Ciprian',
     username: '@rodrigocipriani',
+    tweet: 'https://twitter.com/rodrigocipriani/status/1215578130217340929',
     quote:
       'I always use Material-UI, it is really awesome, and it have a looot of very good and easy to use components.',
   },
@@ -52,6 +58,7 @@ const quotes = [
     avatar: 'https://pbs.twimg.com/profile_images/1129370929409056769/Zkwjy9_I_400x400.jpg',
     name: 'Samantha Durrant',
     username: '@SamDurrant_',
+    tweet: '',
     quote:
       'Began coding out the front end of my app today. Used MaterialUI for the first time and love how easy it is to make things look nice. It’s also really cool to see all the hard work building out the backend of my app come to life in the front end!',
   },
@@ -87,6 +94,9 @@ const useQuoteStyles = makeStyles(theme => ({
     flexDirection: 'column',
     height: '100%',
   },
+  cardAction: {
+    height: '100%'
+  },
   avatar: {
     width: 48,
     height: 48,
@@ -104,41 +114,59 @@ const useQuoteStyles = makeStyles(theme => ({
   },
 }));
 
+const HomeQuoteLink = React.forwardRef((props, ref) => {
+  return (
+    <Link
+      data-ga-event-category="quote"
+      data-ga-event-action="click"
+      data-ga-event-label="home-link"
+      target="_blank"
+      rel="noopener nofollow"
+      naked
+      ref={ref}
+      {...props}
+    />
+  );
+});
+
 function HomeQuote(props) {
-  const { avatar, quote, name, userName } = props;
+  const { avatar, href, name, quote, userName } = props;
   const classes = useQuoteStyles();
 
   return (
     <Card variant="outlined" className={classes.card}>
-      <CardContent>
-        <Grid container spacing={3}>
-          <Grid item>
-            <Avatar src={avatar} alt={name} className={classes.avatar} />
+      <CardActionArea component={HomeQuoteLink} href={href} className={classes.cardAction}>
+        <CardContent>
+          <Grid container spacing={3}>
+            <Grid item>
+              <Avatar src={avatar} alt={name} className={classes.avatar} />
+            </Grid>
+            <Grid item>
+              <Typography variant="h6" color="textPrimary" className={classes.name}>
+                {name}
+              </Typography>
+              <Typography variant="subtitle2" color="textSecondary">
+                {userName}
+              </Typography>
+            </Grid>
+            <Grid item className={classes.twitter}>
+              <TwitterIcon />
+            </Grid>
           </Grid>
-          <Grid item>
-            <Typography variant="h6" color="textPrimary" className={classes.name}>
-              {name}
-            </Typography>
-            <Typography variant="subtitle2" color="textSecondary">
-              {userName}
-            </Typography>
-          </Grid>
-          <Grid item className={classes.twitter}>
-            <TwitterIcon />
-          </Grid>
-        </Grid>
-      </CardContent>
-      <CardContent className={classes.quote}>
-        <Typography color="textPrimary" variant="body2">
-          {quote}
-        </Typography>
-      </CardContent>
+        </CardContent>
+        <CardContent className={classes.quote}>
+          <Typography color="textPrimary" variant="body2">
+            {quote}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
 
 HomeQuote.propTypes = {
   avatar: PropTypes.string,
+  href: PropTypes.string,
   name: PropTypes.string,
   quote: PropTypes.string,
   userName: PropTypes.string,
@@ -171,6 +199,7 @@ function HomeQuotes(props) {
                 <Grid item xs={12} md={4} key={quote.username}>
                   <HomeQuote
                     avatar={quote.avatar}
+                    href={quote.tweet}
                     name={quote.name}
                     userName={quote.username}
                     quote={quote.quote}
