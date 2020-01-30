95d9c78cd08e97a5a0cb121b306d116b2343a9be
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-01-30 10:54:39 +0100

    [docs] Improve Style Library Interoperability (#19457)

--- docs/src/pages/guides/interoperability/StyledComponentsDeep.js
+++ docs/src/pages/guides/interoperability/StyledComponentsDeep.js
@@ -3,26 +3,23 @@ import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
 import NoSsr from '@material-ui/core/NoSsr';
 
-const StyledButton = styled(({ color, ...other }) => <Button {...other} />)`
-  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
-  border: 0;
-  color: white;
-  height: 48px;
-  padding: 0 30px;
-  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
-
+const StyledButton = styled(Button)`
+  background-color: #6772e5;
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  padding: 7px 14px;
+  &:hover {
+    background-color: #5469d4;
+  }
   & .MuiButton-label {
-    color: ${props => props.color};
+    color: #fff;
   }
 `;
 
 export default function StyledComponentsDeep() {
   return (
     <NoSsr>
-      <div>
-        <Button>Material-UI</Button>
-        <StyledButton color="papayawhip">Styled Components</StyledButton>
-      </div>
+      <Button>Default</Button>
+      <StyledButton>Customized</StyledButton>
     </NoSsr>
   );
 }
