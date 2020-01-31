5f990d4e218aed69f46bcf606cd5d816e8b99d0f
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-01-31 20:05:40 +0100

    [icons] Synchronize components with Google (#19485)

--- packages/material-ui-icons/src/AirplanemodeInactiveSharp.js
+++ packages/material-ui-icons/src/AirplanemodeInactiveSharp.js
@@ -2,5 +2,5 @@ import React from 'react';
 import createSvgIcon from './utils/createSvgIcon';
 
 export default createSvgIcon(
-  <path d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5v3.11l8.65 8.65L21 16zm-2.27 5l1.41-1.41L4.41 3.86 3 5.27l4.99 4.99L2 14v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-3.73z" />
+  <path d="M10.5 7.67V3.5c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5V9l8.5 5v2l-4.49-1.32-7.01-7.01zm9.28 14.94l1.41-1.41-7.69-7.7-3.94-3.94-6.75-6.75-1.42 1.41 6.38 6.38L2 14v2l8.5-2.5V19L8 20.5V22l4-1 4 1v-1.5L13.5 19v-2.67l6.28 6.28z" />
 , 'AirplanemodeInactiveSharp');
