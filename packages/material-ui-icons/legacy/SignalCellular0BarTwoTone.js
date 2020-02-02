0607707da2e017b2ac36cb00c2217245d3a42395
timmydoza <timmydoza@gmail.com>
2020-02-02 02:32:33 -0700

    [icons] Remove extraneous path (#19483)

--- packages/material-ui-icons/legacy/SignalCellular0BarTwoTone.js
+++ packages/material-ui-icons/legacy/SignalCellular0BarTwoTone.js
@@ -2,5 +2,5 @@ import React from 'react';
 import createSvgIcon from './utils/createSvgIcon';
 
 export default createSvgIcon(
-  <React.Fragment><path fillOpacity=".3" d="M2 22h20V2L2 22z" /></React.Fragment>
+  <path fillOpacity=".3" d="M2 22h20V2L2 22z" />
 , 'SignalCellular0BarTwoTone');
--- packages/material-ui-icons/src/SystemUpdateAlt.js
+++ packages/material-ui-icons/src/SystemUpdateAlt.js
@@ -2,5 +2,5 @@ import React from 'react';
 import createSvgIcon from './utils/createSvgIcon';
 
 export default createSvgIcon(
-  <React.Fragment><path fill="none" d="M0 .5h24v24H0z" /><path d="M12 16.5l4-4h-3v-9h-2v9H8l4 4zm9-13h-6v1.99h6v14.03H3V5.49h6V3.5H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2v-14c0-1.1-.9-2-2-2z" /></React.Fragment>
+  <path d="M12 16.5l4-4h-3v-9h-2v9H8l4 4zm9-13h-6v1.99h6v14.03H3V5.49h6V3.5H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2v-14c0-1.1-.9-2-2-2z" />
 , 'SystemUpdateAlt');
