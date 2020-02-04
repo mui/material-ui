3ba2e9b56e5ab50881d73e0e6260ad3fd41c9f31
Ahmad Reza <hungry.dev39@gmail.com>
2020-02-04 21:20:11 +0700

    prettier

--- docs/src/pages/components/tables/EnhancedTable.tsx
+++ docs/src/pages/components/tables/EnhancedTable.tsx
@@ -70,7 +70,9 @@ function getComparator<Key extends keyof any>(
   order: Order,
   orderBy: Key,
 ): (a: { [key in Key]: number | string }, b: { [key in Key]: number | string }) => number {
-  return order === 'desc' ? (a, b) => descendingComparator(a, b, orderBy) : (a, b) => -descendingComparator(a, b, orderBy);
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
 }
 
 function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
