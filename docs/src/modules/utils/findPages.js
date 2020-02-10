6a8d015186405f71a55c72683ed491cf01dc3657
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-02-10 20:50:59 +0100

    fix: api pages not showing up

--- docs/src/modules/utils/findPages.js
+++ docs/src/modules/utils/findPages.js
@@ -1,7 +1,20 @@
 import { findPages } from 'docs/src/modules/utils/find';
+const api = require('docs/static/api.json');
 
-const pages = findPages({
-  front: true,
-});
+const pages = [
+  {
+    children: Object.keys(api.components).map(componentName => {
+      return {
+        pathname: `/api/${componentName}`,
+        title: componentName,
+      };
+    }),
+    pathname: '/api',
+  },
+].concat(
+  findPages({
+    front: true,
+  }),
+);
 
 export default pages;
