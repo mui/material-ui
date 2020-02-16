45c4d783bab8e78a2a3af9c06ec20f241e6cd605
Ahmad Reza <hungry.dev39@gmail.com>
2020-02-16 11:15:48 +0700

    prettier

--- packages/material-ui-lab/src/useAutocomplete/useAutocomplete.js
+++ packages/material-ui-lab/src/useAutocomplete/useAutocomplete.js
@@ -446,7 +446,9 @@ export default function useAutocomplete(props) {
           console.error(
             [
               'Material-UI: the `getOptionSelected` method of useAutocomplete do not handle the arguments correctly.',
-              `The component expects a single value to match a given option but found ${matches.length} matches`,
+              `The component expects a single value to match a given option but found ${
+                matches.length
+              } matches.`,
             ].join('\n'),
           );
         }
