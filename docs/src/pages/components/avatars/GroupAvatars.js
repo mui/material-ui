0422f4641c15aa856bc4c9f2ab11223c85eb52af
Gavin Fynbo <gavfynbo@gmail.com>
2020-02-25 19:19:04 -0800

    Fix docs import

--- docs/src/pages/components/avatars/GroupAvatars.js
+++ docs/src/pages/components/avatars/GroupAvatars.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
-import AvatarGroup from '@material-ui/core/AvatarGroup';
+import AvatarGroup from '@material-ui/lab/AvatarGroup';
 
 export default function GroupAvatars() {
   return (
