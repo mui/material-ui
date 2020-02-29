4548eefcfc664e4b6f4aa76a91f1116e8f0e6b43
Diego Fernandez <56129093+dfernandez-asapp@users.noreply.github.com>
2020-02-29 07:21:02 -0300

    [RadioGroup] Random default name (#19890)

--- packages/material-ui/src/RadioGroup/RadioGroup.test.js
+++ packages/material-ui/src/RadioGroup/RadioGroup.test.js
@@ -85,6 +85,18 @@ describe('<RadioGroup />', () => {
     assert.strictEqual(findRadio(wrapper, 'one').props().checked, true);
   });
 
+  it('should have a default name', () => {
+    const wrapper = mount(
+      <RadioGroup>
+        <Radio value="zero" />
+        <Radio value="one" />
+      </RadioGroup>,
+    );
+
+    assert.match(findRadio(wrapper, 'zero').props().name, /^mui-radiogroup-[0-9]+/);
+    assert.match(findRadio(wrapper, 'one').props().name, /^mui-radiogroup-[0-9]+/);
+  });
+
   describe('imperative focus()', () => {
     it('should focus the first non-disabled radio', () => {
       const actionsRef = React.createRef();
@@ -294,6 +306,16 @@ describe('<RadioGroup />', () => {
 
         setProps({ value: 'one' });
         expect(radioGroupRef.current).to.have.property('value', 'one');
+      });
+
+      it('should have a default name from the instance', () => {
+        const radioGroupRef = React.createRef();
+        const { setProps } = render(<RadioGroupControlled ref={radioGroupRef} />);
+
+        expect(radioGroupRef.current.name).to.match(/^mui-radiogroup-[0-9]+/);
+
+        setProps({ name: 'anotherGroup' });
+        expect(radioGroupRef.current).to.have.property('name', 'anotherGroup');
       });
     });
 
