49c5d2bc2db49005d74bda1cf3eb4e49df1be4b1
pvdstel <pvdstel@users.noreply.github.com>
2020-03-02 14:50:38 +0100

    Add types for PaginationItem

--- packages/material-ui-lab/src/PaginationItem/PaginationItem.d.ts
+++ packages/material-ui-lab/src/PaginationItem/PaginationItem.d.ts
@@ -1,2 +1,43 @@
-declare const PaginationItem: any;
+import * as React from 'react';
+import { OverridableComponent, OverrideProps } from '@material-ui/core/OverridableComponent';
+
+export interface PaginationItemTypeMap<P = {}, D extends React.ElementType = 'div'> {
+    props: P & {
+        color?: 'standard' | 'primary' | 'secondary';
+        disabled?: boolean;
+        page?: number;
+        selected?: boolean;
+        shape?: 'round' | 'rounded';
+        size?: 'small' | 'medium' | 'large';
+        type?: 'page' | 'first' | 'last' | 'next' | 'previous' | 'start-ellipsis' | 'end-ellipsis';
+        variant?: 'text' | 'outlined';
+    };
+    defaultComponent: D;
+    classKey: PaginationItemClassKey;
+}
+
+declare const PaginationItem: OverridableComponent<PaginationItemTypeMap>;
+
+export type PaginationItemClassKey =
+    | 'root'
+    | 'page'
+    | 'sizeSmall'
+    | 'sizeLarge'
+    | 'textPrimary'
+    | 'textSecondary'
+    | 'outlined'
+    | 'outlinedPrimary'
+    | 'outlinedSecondary'
+    | 'rounded'
+    | 'ellipsis'
+    | 'focusVisible'
+    | 'disabled'
+    | 'selected'
+    | 'icon';
+
+export type PaginationItemProps<
+    D extends React.ElementType = PaginationItemTypeMap['defaultComponent'],
+    P = {}
+> = OverrideProps<PaginationItemTypeMap<P, D>, D>;
+
 export default PaginationItem;
