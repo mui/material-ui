f8bcc018a32dee0330a55f4463e10c7cf5474859
pvdstel <pvdstel@users.noreply.github.com>
2020-03-02 14:17:39 +0100

    Add types for Pagination

--- packages/material-ui-lab/src/Pagination/Pagination.d.ts
+++ packages/material-ui-lab/src/Pagination/Pagination.d.ts
@@ -1,2 +1,29 @@
-declare const Pagination: any;
-export default Pagination;
+import * as React from 'react';
+import { StandardProps } from '@material-ui/core';
+
+export interface PaginationProps
+    extends StandardProps<React.HTMLAttributes<HTMLElement>, PaginationClassKey, 'onChange'> {
+    boundaryCount?: number;
+    color?: 'default' | 'primary' | 'secondary';
+    count?: number;
+    defaultPage?: number;
+    disabled?: boolean;
+    getItemAriaLabel?: (type: string | undefined, page: number, selected: boolean) => string;
+    hideNextButton?: boolean;
+    hidePrevButton?: boolean;
+    onChange?: (event: React.ChangeEvent<{}>, page: number) => void;
+    page?: number;
+    renderItem?: (params: object) => React.ReactNode;
+    shape?: 'round' | 'rounded';
+    showFirstButton?: boolean;
+    showLastButton?: boolean;
+    siblingCount?: number;
+    size?: 'small' | 'medium' | 'large';
+    variant?: 'text' | 'outlined';
+}
+
+export type PaginationClassKey =
+    | 'root'
+    | 'ul';
+
+export default function Pagination(props: PaginationProps): JSX.Element;
