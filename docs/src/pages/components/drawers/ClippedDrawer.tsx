031fd304adcc0ac0b0ec5bb15f587857727f23f2
smerriman18 <smerriman18@gmail.com>
2020-03-02 09:35:58 -0600

    Update ClippedDrawer.tsx

--- docs/src/pages/components/drawers/ClippedDrawer.tsx
+++ docs/src/pages/components/drawers/ClippedDrawer.tsx
@@ -34,6 +34,7 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
       padding: theme.spacing(3),
     },
+    // necessary for content to be below app bar
     toolbar: theme.mixins.toolbar,
   }),
 );
