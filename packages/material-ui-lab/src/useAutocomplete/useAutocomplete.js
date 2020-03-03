5d25ccf7cee894315d67ff1a4c7dbea8a7a7a8c3
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-03-03 23:01:04 +0100

    Update packages/material-ui-lab/src/useAutocomplete/useAutocomplete.js

--- packages/material-ui-lab/src/useAutocomplete/useAutocomplete.js
+++ packages/material-ui-lab/src/useAutocomplete/useAutocomplete.js
@@ -703,7 +703,7 @@ export default function useAutocomplete(props) {
 
   const handleBlur = event => {
     // Ignore the event when using the scrollbar with IE 11
-    if (listboxRef.current != null && document.activeElement === listboxRef.current.parentElement) {
+    if (listboxRef.current !== null && document.activeElement === listboxRef.current.parentElement) {
       inputRef.current.focus();
       return;
     }
