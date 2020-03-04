dcfcc542aee672055a7c035ea00473bf4e67515a
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-03-04 21:58:31 +0100

    don't silence "unwrapped in act()"-warnings

--- test/utils/createClientRender.js
+++ test/utils/createClientRender.js
@@ -79,9 +79,10 @@ export function createClientRender(globalOptions = {}) {
   const { strict: globalStrict } = globalOptions;
 
   afterEach(async () => {
-    await act(async () => {
-      await cleanup();
-    });
+    // If this issues an act() warning you probably didn't
+    // wait for an async event in your test (or didn't wrap it in act() at all).
+    // please wait for every update in your test and make appropriate assertions
+    await cleanup();
   });
 
   return function configuredClientRender(element, options = {}) {
