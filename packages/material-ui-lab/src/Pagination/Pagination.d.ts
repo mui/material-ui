0e9f1c732e694743c7f0c94e4d98a1a128f924c9
pvdstel <pvdstel@users.noreply.github.com>
2020-03-06 11:29:42 +0100

    Move Pagination documentation to usePagination
    Pagination now extends UsePagination;
    inherited props have been removed.

--- packages/material-ui-lab/src/Pagination/Pagination.d.ts
+++ packages/material-ui-lab/src/Pagination/Pagination.d.ts
@@ -1,29 +1,14 @@
 import * as React from 'react';
 import { StandardProps } from '@material-ui/core';
-import { UsePaginationItem } from './usePagination';
+import { UsePaginationItem, UsePaginationProps } from './usePagination';
 
 export interface PaginationProps
-  extends StandardProps<React.HTMLAttributes<HTMLElement>, PaginationClassKey, 'onChange'> {
-  /**
-   * Number of always visible pages at the beginning and end.
-   */
-  boundaryCount?: number;
+  extends UsePaginationProps,
+    StandardProps<React.HTMLAttributes<HTMLElement>, PaginationClassKey, 'children' | 'onChange'> {
   /**
    * The active color.
    */
   color?: 'default' | 'primary' | 'secondary';
-  /**
-   * The total number of pages.
-   */
-  count?: number;
-  /**
-   * The page selected by default when the component is uncontrolled.
-   */
-  defaultPage?: number;
-  /**
-   * If `true`, the pagination component will be disabled.
-   */
-  disabled?: boolean;
   /**
    * Accepts a function which returns a string value that provides a user-friendly name for the current page.
    *
@@ -40,25 +25,6 @@ export interface PaginationProps
     selected: boolean,
   ) => string;
   /**
-   * If `true`, hide the next-page button.
-   */
-  hideNextButton?: boolean;
-  /**
-   * If `true`, hide the previous-page button.
-   */
-  hidePrevButton?: boolean;
-  /**
-   * Callback fired when the page is changed.
-   *
-   * @param {object} event The event source of the callback.
-   * @param {number} page The page selected.
-   */
-  onChange?: (event: React.ChangeEvent<unknown>, page: number) => void;
-  /**
-   * The current page.
-   */
-  page?: number;
-  /**
    * The pagination item component to render.
    *
    * @param {object} params The props to spread on the component.
@@ -69,18 +35,6 @@ export interface PaginationProps
    * The shape of the pagination items.
    */
   shape?: 'round' | 'rounded';
-  /**
-   * If `true`, show the first-page button.
-   */
-  showFirstButton?: boolean;
-  /**
-   * If `true`, show the last-page button.
-   */
-  showLastButton?: boolean;
-  /**
-   * Number of always visible pages before and after the current page.
-   */
-  siblingCount?: number;
   /**
    * The size of the pagination component.
    */
--- packages/material-ui-lab/src/Pagination/usePagination.d.ts
+++ packages/material-ui-lab/src/Pagination/usePagination.d.ts
@@ -1,17 +1,56 @@
 import * as React from 'react';
 
 export interface UsePaginationProps {
+  /**
+   * Number of always visible pages at the beginning and end.
+   */
   boundaryCount?: number;
+  /**
+   * The name of the component where this hook is used.
+   */
   componentName?: string;
+  /**
+   * The total number of pages.
+   */
   count?: number;
+  /**
+   * The page selected by default when the component is uncontrolled.
+   */
   defaultPage?: number;
+  /**
+   * If `true`, the pagination component will be disabled.
+   */
   disabled?: boolean;
+  /**
+   * If `true`, hide the next-page button.
+   */
   hideNextButton?: boolean;
+  /**
+   * If `true`, hide the previous-page button.
+   */
   hidePrevButton?: boolean;
-  onChange?: (event: React.ChangeEvent<{}>, page: number) => void;
+  /**
+   * Callback fired when the page is changed.
+   *
+   * @param {object} event The event source of the callback.
+   * @param {number} page The page selected.
+   */
+  onChange?: (event: React.ChangeEvent<unknown>, page: number) => void;
+  /**
+   * The current page.
+   */
   page?: number;
+  /**
+   * If `true`, show the first-page button.
+   */
   showFirstButton?: boolean;
+  /**
+   * If `true`, show the last-page button.
+   */
   showLastButton?: boolean;
+  /**
+   * Number of always visible pages before and after the current page.
+   */
   siblingCount?: number;
 }
 
