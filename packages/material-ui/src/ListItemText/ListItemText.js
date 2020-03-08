32db727c3d6c605971e9b89ced1d691c63808a5c
psdr03 <psdelarosa@gmail.com>
2020-03-08 14:17:59 +0800

    ListItemText - Add display block
    Fixes ListItemText with secondaryTypographyProps rendering on same line as primary.

--- packages/material-ui/src/ListItemText/ListItemText.js
+++ packages/material-ui/src/ListItemText/ListItemText.js
@@ -52,6 +52,7 @@ const ListItemText = React.forwardRef(function ListItemText(props, ref) {
         variant={dense ? 'body2' : 'body1'}
         className={classes.primary}
         component="span"
+        display="block"
         {...primaryTypographyProps}
       >
         {primary}
@@ -66,6 +67,7 @@ const ListItemText = React.forwardRef(function ListItemText(props, ref) {
         variant="body2"
         className={classes.secondary}
         color="textSecondary"
+        display="block"
         {...secondaryTypographyProps}
       >
         {secondary}
