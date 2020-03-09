9e320a4e0c70b424df3e6f1b2f6d66b7f2b05d33
Gabriel Santerre <gabriel.santerre@potloc.com>
2020-03-09 11:26:41 -0400

    [lab][Skeleton] Fix SkeletonClassKey type

--- packages/material-ui-lab/src/Skeleton/Skeleton.d.ts
+++ packages/material-ui-lab/src/Skeleton/Skeleton.d.ts
@@ -14,7 +14,7 @@ export interface SkeletonTypeMap<P = {}, D extends React.ElementType = 'span'> {
 
 declare const Skeleton: OverridableComponent<SkeletonTypeMap>;
 
-export type SkeletonClassKey = 'root' | 'text' | 'rect' | 'circle' | 'animate';
+export type SkeletonClassKey = 'root' | 'text' | 'rect' | 'circle' | 'pulse' | 'wave';
 
 export type SkeletonProps<
   D extends React.ElementType = SkeletonTypeMap['defaultComponent'],
