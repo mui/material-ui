da24c16b499fd61f0a66b40bb090ba3a0c6e05dd
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-03-09 11:37:08 +0100

    test: fix chrome 49 tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -79,6 +79,11 @@ module.exports = function setKarmaConfig(config) {
       },
       resolve: {
         alias: {
+          // yarn alias for `ansi-styles@3`
+          // @testing-library/dom -> pretty-format@25 -> ansi-styles@4
+          // which uses Object.entries which isn't implemented in all browsers
+          // we support
+          'ansi-styles': require.resolve('ansi-styles-v3'),
           // https://github.com/sinonjs/sinon/issues/1951
           // use the cdn main field. Neither module nor main are supported for browserbuilds
           sinon: 'sinon/pkg/sinon.js',
