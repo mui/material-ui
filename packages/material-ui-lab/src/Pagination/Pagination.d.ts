51b2b927cf31b59f16dbe7d2a51ef1ee111845cc
Paul <pvdstel@users.noreply.github.com>
2020-03-11 11:11:53 +0100

    [Pagination] Add TypeScript types (#19933)

--- packages/material-ui-lab/src/Pagination/Pagination.d.ts
+++ packages/material-ui-lab/src/Pagination/Pagination.d.ts
@@ -1,2 +1,50 @@
-declare const Pagination: any;
-export default Pagination;
+import * as React from 'react';
+import { StandardProps } from '@material-ui/core';
+import { UsePaginationItem, UsePaginationProps } from './usePagination';
+
+export interface PaginationProps
+  extends UsePaginationProps,
+    StandardProps<React.HTMLAttributes<HTMLElement>, PaginationClassKey, 'children' | 'onChange'> {
+  /**
+   * The active color.
+   */
+  color?: 'default' | 'primary' | 'secondary';
+  /**
+   * Accepts a function which returns a string value that provides a user-friendly name for the current page.
+   *
+   * For localization purposes, you can use the provided [translations](/guides/localization/).
+   *
+   * @param {string} type The link or button type to format ('page' | 'first' | 'last' | 'next' | 'previous'). Defaults to 'page'.
+   * @param {number} page The page number to format.
+   * @param {bool} selected If true, the current page is selected.
+   * @returns {string}
+   */
+  getItemAriaLabel?: (
+    type: 'page' | 'first' | 'last' | 'next' | 'previous',
+    page: number,
+    selected: boolean,
+  ) => string;
+  /**
+   * Render the item.
+   *
+   * @param {object} params The props to spread on a PaginationItem.
+   * @returns {ReactNode}
+   */
+  renderItem?: (params: object) => React.ReactNode;
+  /**
+   * The shape of the pagination items.
+   */
+  shape?: 'round' | 'rounded';
+  /**
+   * The size of the pagination component.
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * The variant to use.
+   */
+  variant?: 'text' | 'outlined';
+}
+
+export type PaginationClassKey = 'root' | 'ul';
+
+export default function Pagination(props: PaginationProps): JSX.Element;
--- packages/material-ui-lab/src/PaginationItem/PaginationItem.d.ts
+++ packages/material-ui-lab/src/PaginationItem/PaginationItem.d.ts
@@ -1,2 +1,68 @@
-declare const PaginationItem: any;
+import * as React from 'react';
+import { OverridableComponent, OverrideProps } from '@material-ui/core/OverridableComponent';
+import { UsePaginationItem } from '../Pagination/usePagination';
+
+export interface PaginationItemTypeMap<P = {}, D extends React.ElementType = 'div'> {
+  props: P & {
+    /**
+     * The active color.
+     */
+    color?: 'standard' | 'primary' | 'secondary';
+    /**
+     * If `true`, the item will be disabled.
+     */
+    disabled?: boolean;
+    /**
+     * The current page number.
+     */
+    page?: number;
+    /**
+     * If `true` the pagination item is selected.
+     */
+    selected?: boolean;
+    /**
+     * The shape of the pagination item.
+     */
+    shape?: 'round' | 'rounded';
+    /**
+     * The size of the pagination item.
+     */
+    size?: 'small' | 'medium' | 'large';
+    /**
+     * The type of pagination item.
+     */
+    type?: UsePaginationItem['type'];
+    /**
+     * The pagination item variant.
+     */
+    variant?: 'text' | 'outlined';
+  };
+  defaultComponent: D;
+  classKey: PaginationItemClassKey;
+}
+
+declare const PaginationItem: OverridableComponent<PaginationItemTypeMap>;
+
+export type PaginationItemClassKey =
+  | 'root'
+  | 'page'
+  | 'sizeSmall'
+  | 'sizeLarge'
+  | 'textPrimary'
+  | 'textSecondary'
+  | 'outlined'
+  | 'outlinedPrimary'
+  | 'outlinedSecondary'
+  | 'rounded'
+  | 'ellipsis'
+  | 'focusVisible'
+  | 'disabled'
+  | 'selected'
+  | 'icon';
+
+export type PaginationItemProps<
+  D extends React.ElementType = PaginationItemTypeMap['defaultComponent'],
+  P = {}
+> = OverrideProps<PaginationItemTypeMap<P, D>, D>;
+
 export default PaginationItem;
