4b7be4f40e924bce55b4f6e2455198de56edd8fa
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-03-11 22:12:41 +0100

    [docs] Display color preview in IntelliSense

--- packages/material-ui/src/colors/common.d.ts
+++ packages/material-ui/src/colors/common.d.ts
@@ -1,7 +1,12 @@
-export interface CommonColors {
-  black: string;
-  white: string;
-}
+declare const common: {
+  /**
+   * Preview: ![common black](https://material-ui.com/static/colors-preview/common-black-24x24.svg)
+   */
+  black: '#000';
+  /**
+   * Preview: ![common white](https://material-ui.com/static/colors-preview/common-white-24x24.svg)
+   */
+  white: '#fff';
+};
 
-declare const common: CommonColors;
 export default common;
