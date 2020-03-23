35999bb83666e721d756e54f8f9f829612e8ed2c
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-03-23 10:24:34 +0100

    [core] Format all ts files (#20233)

--- scripts/prettier.js
+++ scripts/prettier.js
@@ -24,7 +24,7 @@ function runPrettier(options) {
     .filter((notEmpty) => notEmpty);
 
   const files = glob
-    .sync('**/*.{js,tsx,d.ts}', { ignore: ['**/node_modules/**', ...ignoredFiles] })
+    .sync('**/*.{js,tsx,ts}', { ignore: ['**/node_modules/**', ...ignoredFiles] })
     .filter((f) => !changedFiles || changedFiles.has(f));
 
   if (!files.length) {
