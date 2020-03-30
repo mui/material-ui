92a6d9288df1c3dbbece4da16d201d2ef3c04741
Roy Art <royart@me.com>
2020-03-30 16:53:27 +0200

    [Card] Fix TypeScript not recognizing "component" prop (#20179)

--- packages/material-ui/src/CardHeader/CardHeader.d.ts
+++ packages/material-ui/src/CardHeader/CardHeader.d.ts
@@ -2,17 +2,28 @@ import * as React from 'react';
 import { TypographyProps } from '../Typography';
 import { OverridableComponent, OverrideProps } from '../OverridableComponent';
 
-export interface CardHeaderTypeMap<P = {}, D extends React.ElementType = 'div'> {
-  props: P & {
+export interface CardHeaderTypeMap<
+  Props = {},
+  DefaultComponent extends React.ElementType = 'div',
+  TitleTypographyComponent extends React.ElementType = 'span',
+  SubheaderTypographyComponent extends React.ElementType = 'span'
+> {
+  props: Props & {
     action?: React.ReactNode;
     avatar?: React.ReactNode;
     disableTypography?: boolean;
     subheader?: React.ReactNode;
-    subheaderTypographyProps?: Partial<TypographyProps>;
+    subheaderTypographyProps?: TypographyProps<
+      SubheaderTypographyComponent,
+      { component?: SubheaderTypographyComponent }
+    >;
     title?: React.ReactNode;
-    titleTypographyProps?: Partial<TypographyProps>;
+    titleTypographyProps?: TypographyProps<
+      TitleTypographyComponent,
+      { component?: TitleTypographyComponent }
+    >;
   };
-  defaultComponent: D;
+  defaultComponent: DefaultComponent;
   classKey: CardHeaderClassKey;
 }
 /**
@@ -25,13 +36,51 @@ export interface CardHeaderTypeMap<P = {}, D extends React.ElementType = 'div'> 
  *
  * - [CardHeader API](https://material-ui.com/api/card-header/)
  */
-declare const CardHeader: OverridableComponent<CardHeaderTypeMap>;
+declare const CardHeader: OverridableCardHeader;
+
+export interface OverridableCardHeader extends OverridableComponent<CardHeaderTypeMap> {
+  <
+    DefaultComponent extends React.ElementType = CardHeaderTypeMap['defaultComponent'],
+    Props = {},
+    TitleTypographyComponent extends React.ElementType = 'span',
+    SubheaderTypographyComponent extends React.ElementType = 'span'
+  >(
+    props: CardHeaderPropsWithComponent<
+      DefaultComponent,
+      Props,
+      TitleTypographyComponent,
+      SubheaderTypographyComponent
+    >,
+  ): JSX.Element;
+}
 
 export type CardHeaderClassKey = 'root' | 'avatar' | 'action' | 'content' | 'title' | 'subheader';
 
 export type CardHeaderProps<
-  D extends React.ElementType = CardHeaderTypeMap['defaultComponent'],
-  P = {}
-> = OverrideProps<CardHeaderTypeMap<P, D>, D>;
+  DefaultComponent extends React.ElementType = CardHeaderTypeMap['defaultComponent'],
+  Props = {},
+  TitleTypographyComponent extends React.ElementType = 'span',
+  SubheaderTypographyComponent extends React.ElementType = 'span'
+> = OverrideProps<
+  CardHeaderTypeMap<
+    Props,
+    DefaultComponent,
+    TitleTypographyComponent,
+    SubheaderTypographyComponent
+  >,
+  DefaultComponent
+>;
+
+export type CardHeaderPropsWithComponent<
+  DefaultComponent extends React.ElementType = CardHeaderTypeMap['defaultComponent'],
+  Props = {},
+  TitleTypographyComponent extends React.ElementType = 'span',
+  SubheaderTypographyComponent extends React.ElementType = 'span'
+> = { component?: DefaultComponent } & CardHeaderProps<
+  DefaultComponent,
+  Props,
+  TitleTypographyComponent,
+  SubheaderTypographyComponent
+>;
 
 export default CardHeader;
