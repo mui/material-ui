013a1ef77f285b950616076e110189ee65fafa11
Minh Nguyen <minhnguyenxx@gmail.com>
2020-03-31 11:12:20 +0100

    [core] Export core utils modules from barrel
    Fixes #20353.

--- packages/material-ui/src/index.d.ts
+++ packages/material-ui/src/index.d.ts
@@ -56,6 +56,8 @@ import * as colors from './colors';
 export { colors };
 export * from './styles';
 
+export * from './utils';
+
 export { default as AppBar } from './AppBar';
 export * from './AppBar';
 
