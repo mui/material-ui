44f8fb89a502a632a5f15eb539e7594734650711
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-04-06 18:59:05 +0200

    reduce size?

--- packages/material-ui/src/utils/useId.js
+++ packages/material-ui/src/utils/useId.js
@@ -3,18 +3,15 @@ import * as React from 'react';
 /**
  * Private module reserved for @material-ui/x packages.
  */
-export default function useId(idProp) {
-  const [defaultId, setDefaultId] = React.useState();
-  const id = idProp || defaultId;
+export default function useId(idOverride) {
+  const [id, setId] = React.useState(idOverride);
   React.useEffect(() => {
-    if (defaultId) {
-      return;
+    if (idOverride == null) {
+      // Fallback to this default id when possible.
+      // Use the random value for client-side rendering only.
+      // We can't use it server-side.
+      setId(`mui-${Math.round(Math.random() * 1e5)}`);
     }
-
-    // Fallback to this default id when possible.
-    // Use the random value for client-side rendering only.
-    // We can't use it server-side.
-    setDefaultId(`mui-${Math.round(Math.random() * 1e5)}`);
-  }, [defaultId]);
+  }, [idOverride]);
   return id;
 }
