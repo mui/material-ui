b34a65699ab0dc865003b576cf8e5b171f7cb01f
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-04-14 11:48:55 +0200

    sort alphanumeric

--- test/utils/init.d.ts
+++ test/utils/init.d.ts
@@ -3,13 +3,6 @@
 declare namespace Chai {
   interface Assertion {
     /**
-     * checks if the accessible name computation (according to `accname` spec)
-     * matches the expectation.
-     * @see https://www.w3.org/TR/accname-1.2/
-     * @param name
-     */
-    toHaveAccessibleName(name: string): Assertion;
-    /**
      * checks if the element in question is considered aria-hidden
      * Does not replace accessibility check as that requires display/visibility/layout
      * @deprecated Use `inaccessible` + `visible` instead
@@ -19,6 +12,13 @@ declare namespace Chai {
      * checks if the element is inaccessible
      */
     toBeInaccessible(): Assertion;
+    /**
+     * checks if the accessible name computation (according to `accname` spec)
+     * matches the expectation.
+     * @see https://www.w3.org/TR/accname-1.2/
+     * @param name
+     */
+    toHaveAccessibleName(name: string): Assertion;
     /**
      * checks if the element is focused
      */
