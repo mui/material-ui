b70eb0e48c82ccf7c8cb924473dad320f5dd6e02
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-04-16 23:13:43 +0200

    temp: build all locales

--- docs/next.config.js
+++ docs/next.config.js
@@ -155,7 +155,8 @@ module.exports = {
     }
 
     // We want to speed-up the build of pull requests.
-    if (process.env.PULL_REQUEST === 'true') {
+    // TODO: revert. Only flipped for debugging
+    if (process.env.PULL_REQUEST === 'false') {
       // eslint-disable-next-line no-console
       console.log('Considering only English for SSR');
       traverse(pages, 'en');
