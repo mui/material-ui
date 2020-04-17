5089f6dfc46dbf183177515b17426b88c4a637f2
Ankita Singh <ankita.singh@gcpat.com>
2020-04-17 19:15:11 -0400

    Adding typescript test for Pagination Buttons Demo

---
+++ docs/src/pages/components/pagination/PaginationButtons.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Pagination from '@material-ui/lab/Pagination';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+export default function PaginationButtons() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Pagination count={10} showFirstButton showLastButton />
+      <Pagination count={10} hidePrevButton hideNextButton />
+    </div>
+  );
+}
