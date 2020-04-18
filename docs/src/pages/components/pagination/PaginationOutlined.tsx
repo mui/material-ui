7240f3f02868584c63edd318de17f02ba85671d0
Ankita Singh <ankita.singh@gcpat.com>
2020-04-17 20:06:14 -0400

    Added typescript test for Outlined Pagination

---
+++ docs/src/pages/components/pagination/PaginationOutlined.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Pagination from '@material-ui/lab/Pagination';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+export default function PaginationOutlined() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Pagination count={10} variant="outlined" />
+      <Pagination count={10} variant="outlined" color="primary" />
+      <Pagination count={10} variant="outlined" color="secondary" />
+      <Pagination count={10} variant="outlined" disabled />
+    </div>
+  );
+}
