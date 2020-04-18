ed7bc7552089dc37e8147da3a44ae29016a6cddf
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-04-18 13:15:09 +0200

    yarn proptypes

--- packages/material-ui-lab/src/Autocomplete/Autocomplete.js
+++ packages/material-ui-lab/src/Autocomplete/Autocomplete.js
@@ -637,8 +637,11 @@ Autocomplete.propTypes = {
    */
   getOptionLabel: PropTypes.func,
   /**
-   * Used to determine if an option is selected.
+   * Used to determine if an option is selected, considering the current value.
    * Uses strict equality by default.
+   * @param {T} option The option to test.
+   * @param {T} value The value to test against.
+   * @return {boolean}
    */
   getOptionSelected: PropTypes.func,
   /**
