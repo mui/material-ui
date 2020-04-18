4e6dd3eb99f7daa4f4ab0a21f00237711e34c80a
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-04-18 12:00:49 +0200

    add createStyles for function

--- docs/src/pages/components/pagination/PaginationControlled.tsx
+++ docs/src/pages/components/pagination/PaginationControlled.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Pagination from '@material-ui/lab/Pagination';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > * + *': {
-      marginTop: theme.spacing(2),
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      '& > * + *': {
+        marginTop: theme.spacing(2),
+      },
     },
-  },
-}));
+  }),
+);
 
 export default function PaginationControlled() {
   const classes = useStyles();
