4352ab7826db66fe3a5045c1464dc233a75f280e
Ankita Singh <ankita.singh@gcpat.com>
2020-04-17 20:26:41 -0400

    Added typescript test for Pagination ranges

---
+++ docs/src/pages/components/pagination/PaginationRanges.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Pagination from '@material-ui/lab/Pagination';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+export default function PaginationRanges() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Pagination count={11} defaultPage={6} siblingCount={0} />
+      <Pagination count={11} defaultPage={6} /> {/* Default ranges */}
+      <Pagination count={11} defaultPage={6} siblingCount={0} boundaryCount={2} />
+      <Pagination count={11} defaultPage={6} boundaryCount={2} />
+    </div>
+  );
+}
