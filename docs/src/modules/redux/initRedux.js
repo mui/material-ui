9af3e271139706e1e35f1139af793226b3bc1f63
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-04-19 12:40:00 +0200

    [docs] Improve the display of notifications

--- docs/src/modules/redux/initRedux.js
+++ docs/src/modules/redux/initRedux.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import optionsReducer from 'docs/src/modules/redux/optionsReducer';
+import notificationsReducer from 'docs/src/modules/redux/notificationsReducer';
 import { createLogger } from 'redux-logger';
 
 // Get the Redux DevTools extension and fallback to a no-op function
@@ -30,6 +31,7 @@ export default function create(initialState) {
   return createStore(
     combineReducers({
       options: optionsReducer,
+      notifications: notificationsReducer,
     }),
     initialState, // Hydrate the store with server-side data
     compose(applyMiddleware(...middleware), devtools),
