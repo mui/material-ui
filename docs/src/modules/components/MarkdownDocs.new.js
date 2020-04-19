c6dabe3ced1a8243f503f7d20e0e6c42a2779656
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-04-19 14:33:23 +0200

    chore: extract MarkdownElement

--- docs/src/modules/components/MarkdownDocs.new.js
+++ docs/src/modules/components/MarkdownDocs.new.js
@@ -19,6 +19,7 @@ import Link from 'docs/src/modules/components/Link';
 import { SOURCE_CODE_ROOT_URL } from 'docs/src/modules/constants';
 import Demo from 'docs/src/modules/components/Demo';
 import AppTableOfContents from './AppTableOfContents.new';
+import MarkdownElement from './MarkdownElement.new';
 
 function flattenPages(pages, current = []) {
   return pages.reduce((items, item) => {
@@ -84,185 +85,6 @@ const styles = (theme) => ({
     textTransform: 'none',
     fontWeight: theme.typography.fontWeightRegular,
   },
-  markdownElement: {
-    ...theme.typography.body1,
-    color: theme.palette.text.primary,
-    wordBreak: 'break-word',
-    '& .anchor-link': {
-      marginTop: -96, // Offset for the anchor.
-      position: 'absolute',
-    },
-    '& pre': {
-      margin: theme.spacing(3, 0),
-      padding: theme.spacing(2),
-      backgroundColor: '#272c34',
-      direction: 'ltr',
-      borderRadius: theme.shape.borderRadius,
-      overflow: 'auto',
-      WebkitOverflowScrolling: 'touch', // iOS momentum scrolling.
-      maxWidth: 'calc(100vw - 32px)',
-      [theme.breakpoints.up('md')]: {
-        maxWidth: 'calc(100vw - 32px - 16px)',
-      },
-    },
-    '& code': {
-      lineHeight: 1.4,
-      display: 'inline-block',
-      fontFamily: 'Consolas, "Liberation Mono", Menlo, Courier, monospace',
-      WebkitFontSmoothing: 'subpixel-antialiased',
-      padding: '0 3px',
-      color: theme.palette.text.primary,
-      backgroundColor:
-        theme.palette.type === 'light' ? 'rgba(255, 229, 100, 0.2)' : 'rgba(255, 229, 100, 0.2)',
-      fontSize: 14,
-      borderRadius: 2,
-    },
-    '& code[class*="language-"]': {
-      backgroundColor: '#272c34',
-      color: '#fff',
-      // Avoid layout jump after hydration (style injected by prism)
-      lineHeight: 1.5,
-    },
-    '& p code, & ul code, & pre code': {
-      fontSize: 14,
-    },
-    '& .token.operator': {
-      background: 'transparent',
-    },
-    '& h1': {
-      ...theme.typography.h3,
-      fontSize: 40,
-      margin: '16px 0',
-    },
-    '& .description': {
-      ...theme.typography.h5,
-      margin: '0 0 40px',
-    },
-    '& h2': {
-      ...theme.typography.h4,
-      fontSize: 30,
-      margin: '40px 0 16px',
-    },
-    '& h3': {
-      ...theme.typography.h5,
-      margin: '40px 0 16px',
-    },
-    '& h4': {
-      ...theme.typography.h6,
-      margin: '32px 0 16px',
-    },
-    '& h5': {
-      ...theme.typography.subtitle2,
-      margin: '32px 0 16px',
-    },
-    '& p, & ul, & ol': {
-      marginTop: 0,
-      marginBottom: 16,
-    },
-    '& ul': {
-      paddingLeft: 30,
-    },
-    '& h1, & h2, & h3, & h4': {
-      '& code': {
-        fontSize: 'inherit',
-        lineHeight: 'inherit',
-        // Remove scroll on small screens.
-        wordBreak: 'break-all',
-      },
-      '& .anchor-link-style': {
-        // To prevent the link to get the focus.
-        display: 'none',
-      },
-      '&:hover .anchor-link-style': {
-        display: 'inline-block',
-        padding: '0 8px',
-        color: theme.palette.text.secondary,
-        '&:hover': {
-          color: theme.palette.text.primary,
-        },
-        '& svg': {
-          width: '0.7em',
-          height: '0.7em',
-          fill: 'currentColor',
-        },
-      },
-    },
-    '& table': {
-      // Trade display table for scroll overflow
-      display: 'block',
-      wordBreak: 'normal',
-      width: '100%',
-      overflowX: 'auto',
-      WebkitOverflowScrolling: 'touch', // iOS momentum scrolling.
-      borderCollapse: 'collapse',
-      marginBottom: '16px',
-      borderSpacing: 0,
-      overflow: 'hidden',
-      '& .prop-name': {
-        fontSize: 13,
-        fontFamily: 'Consolas, "Liberation Mono", Menlo, monospace',
-      },
-      '& .required': {
-        color: theme.palette.type === 'light' ? '#006500' : '#a5ffa5',
-      },
-      '& .prop-type': {
-        fontSize: 13,
-        fontFamily: 'Consolas, "Liberation Mono", Menlo, monospace',
-        color: theme.palette.type === 'light' ? '#932981' : '#ffb6ec',
-      },
-      '& .prop-default': {
-        fontSize: 13,
-        fontFamily: 'Consolas, "Liberation Mono", Menlo, monospace',
-        borderBottom: `1px dotted ${theme.palette.divider}`,
-      },
-    },
-    '& td': {
-      ...theme.typography.body2,
-      borderBottom: `1px solid ${theme.palette.divider}`,
-      padding: 16,
-      color: theme.palette.text.primary,
-    },
-    '& td code': {
-      fontSize: 13,
-      lineHeight: 1.6,
-    },
-    '& th': {
-      fontSize: 14,
-      lineHeight: theme.typography.pxToRem(24),
-      fontWeight: theme.typography.fontWeightMedium,
-      color: theme.palette.text.primary,
-      whiteSpace: 'pre',
-      borderBottom: `1px solid ${theme.palette.divider}`,
-      padding: 16,
-    },
-    '& blockquote': {
-      borderLeft: '5px solid #ffe564',
-      backgroundColor: 'rgba(255,229,100,0.2)',
-      padding: '4px 24px',
-      margin: '24px 0',
-      '& p': {
-        marginTop: '16px',
-      },
-    },
-    '& a, & a code': {
-      // Style taken from the Link component
-      color: theme.palette.secondary.main,
-      textDecoration: 'none',
-      '&:hover': {
-        textDecoration: 'underline',
-      },
-    },
-    '& img, video': {
-      maxWidth: '100%',
-    },
-    '& hr': {
-      height: 1,
-      margin: theme.spacing(6, 0),
-      border: 'none',
-      flexShrink: 0,
-      backgroundColor: theme.palette.divider,
-    },
-  },
 });
 
 function MarkdownDocs(props) {
@@ -309,20 +131,19 @@ function MarkdownDocs(props) {
               <path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z" />
             </symbol>
           </svg>
-          {rendered.map((someUnnamedVariable, index) => {
-            if (typeof someUnnamedVariable === 'string') {
-              const renderedMarkdown = someUnnamedVariable;
+          {rendered.map((renderedMarkdownOrDemo, index) => {
+            if (typeof renderedMarkdownOrDemo === 'string') {
+              const renderedMarkdown = renderedMarkdownOrDemo;
               return (
-                <div
+                <MarkdownElement
                   key={index}
-                  className={clsx(classes.markdownElement, 'markdown-body', 'markdownElement')}
                   // eslint-disable-next-line react/no-danger
                   dangerouslySetInnerHTML={{ __html: renderedMarkdown }}
                 />
               );
             }
 
-            const demoOptions = someUnnamedVariable;
+            const demoOptions = renderedMarkdownOrDemo;
             const name = demoOptions.demo;
             const demo = demos?.[name];
             if (demo === undefined) {
