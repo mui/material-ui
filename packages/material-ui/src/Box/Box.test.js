4ed0066e91a9fc17ab886920757f833c2170ab65
marcosvega91 <marcomoretti0103@gmail.com>
2020-04-24 16:04:17 +0200

    [Box] migrate to testing library

--- packages/material-ui/src/Box/Box.test.js
+++ packages/material-ui/src/Box/Box.test.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import { assert } from 'chai';
+import { expect } from 'chai';
+import { createClientRender } from 'test/utils/createClientRender';
 import { createMount } from '@material-ui/core/test-utils';
 import describeConformance from '@material-ui/core/test-utils/describeConformance';
 import Box from './Box';
 
 describe('<Box />', () => {
   let mount;
-
+  const render = createClientRender();
   before(() => {
     mount = createMount({ strict: true });
   });
@@ -21,22 +22,25 @@ describe('<Box />', () => {
     refInstanceof: window.HTMLDivElement,
   }));
 
-  const testChildren = <div className="unique">Hello World</div>;
+  const testChildren = (
+    <div data-testid="child" className="unique">
+      Hello World
+    </div>
+  );
 
   it('renders children and box content', () => {
-    const wrapper = mount(
+    const { container, getByTestId } = render(
       <Box component="span" m={1}>
         {testChildren}
       </Box>,
     );
-
-    assert.strictEqual(wrapper.contains(testChildren), true);
-    assert.strictEqual(wrapper.find('span').length, 1);
+    expect(container.firstChild).contain(getByTestId('child'));
+    expect(container.querySelectorAll('span').length).to.equal(1);
   });
 
   it('does not forward style props as DOM attributes', () => {
     const elementRef = React.createRef();
-    mount(
+    render(
       <Box
         color="primary.main"
         fontFamily="Comic Sans"
@@ -46,8 +50,8 @@ describe('<Box />', () => {
     );
 
     const { current: element } = elementRef;
-    assert.strictEqual(element.getAttribute('color'), null);
-    assert.strictEqual(element.getAttribute('font-family'), null);
-    assert.strictEqual(element.getAttribute('font-size'), null);
+    expect(element.getAttribute('color')).to.equal(null);
+    expect(element.getAttribute('font-family')).to.equal(null);
+    expect(element.getAttribute('font-size')).to.equal(null);
   });
 });
