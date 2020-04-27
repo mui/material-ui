20dad90cd63c266b08756a33b25455b8c864556e
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-04-28 00:06:33 +0200

    Update GroupAvatars.tsx

--- docs/src/pages/components/avatars/GroupAvatars.tsx
+++ docs/src/pages/components/avatars/GroupAvatars.tsx
@@ -4,7 +4,7 @@ import AvatarGroup from '@material-ui/lab/AvatarGroup';
 
 export default function GroupAvatars() {
   return (
-    <AvatarGroup max={3}>
+    <AvatarGroup max={4}>
       <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
       <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
       <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
