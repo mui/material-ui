ebb16892ffd07dfd81ec791ac543a9e5b900b3d3
corporateanon <corporateanon@gmail.com>
2020-04-29 14:25:49 +0300

    prettier

--- examples/nextjs/src/SSRProvider.js
+++ examples/nextjs/src/SSRProvider.js
@@ -5,26 +5,26 @@ import Head from 'next/head';
 const isServer = !process.browser;
 
 const SSRProvider = ({ children }) => {
-    if (isServer) {
-        const sheets = new ServerStyleSheets();
-        const output = sheets.collect(children);
-        const html = renderToString(output);
-        return (
-            <>
-                <Head>{sheets.getStyleElement()}</Head>
-                <div id="ssr-content" dangerouslySetInnerHTML={{ __html: html }} />
-            </>
-        );
-    } else {
-        return (
-            <>
-                <Head>
-                    <style id="jss-server-side"></style>
-                </Head>
-                <div id="ssr-content">{children}</div>
-            </>
-        );
-    }
+  if (isServer) {
+    const sheets = new ServerStyleSheets();
+    const output = sheets.collect(children);
+    const html = renderToString(output);
+    return (
+      <>
+        <Head>{sheets.getStyleElement()}</Head>
+        <div id="ssr-content" dangerouslySetInnerHTML={{ __html: html }} />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Head>
+          <style id="jss-server-side"></style>
+        </Head>
+        <div id="ssr-content">{children}</div>
+      </>
+    );
+  }
 };
 
 export default SSRProvider;
