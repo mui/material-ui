6a0b34566cfadbeb8e266806a17e3bdae050e8dd
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-04-29 19:34:04 +0200

    fix: remove variation selector

--- docs/src/modules/utils/parseMarkdown.js
+++ docs/src/modules/utils/parseMarkdown.js
@@ -181,7 +181,7 @@ ${headers.components
 
             const headingText = headingHtml
               .replace(
-                /([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+                /([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])\uFE0F?/g,
                 '',
               ) // remove emojis
               .replace(/<\/?[^>]+(>|$)/g, '') // remove HTML
