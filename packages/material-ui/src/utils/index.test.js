de3ae621ba8489fe77a17f75f7afce7f32418f73
marcosvega91 <marcomoretti0103@gmail.com>
2020-04-30 09:10:03 +0200

    Remove assert import

--- packages/material-ui/src/utils/index.test.js
+++ packages/material-ui/src/utils/index.test.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { expect, assert } from 'chai';
+import { expect } from 'chai';
 import { spy } from 'sinon';
 import PropTypes from 'prop-types';
 import consoleErrorMock from 'test/utils/consoleErrorMock';
