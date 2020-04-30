af986d0dc3257ef5fb7d73e572801aae1a306ca2
marcosvega91 <marcomoretti0103@gmail.com>
2020-04-30 09:15:32 +0200

    codemod assert-->expect

--- packages/material-ui-codemod/src/v0.15.0/import-path.test.js
+++ packages/material-ui-codemod/src/v0.15.0/import-path.test.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { assert } from 'chai';
+import { expect } from 'chai';
 import jscodeshift from 'jscodeshift';
 import transform from './import-path';
 
@@ -22,12 +22,7 @@ describe('@material-ui/codemod', () => {
         );
 
         const expected = read('./import-path.test/expected.js');
-
-        assert.strictEqual(
-          trim(actual),
-          trim(expected),
-          'The transformed version should be correct',
-        );
+        expect(trim(actual)).to.equal(trim(expected), 'The transformed version should be correct');
       });
     });
   });
