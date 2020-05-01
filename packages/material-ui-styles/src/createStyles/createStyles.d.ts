65d268c5141aab34c7f24e796023206cb3fd28de
Vladimir <vzh.inbox@gmail.com>
2020-05-01 14:15:17 +0600

    Remove default type

--- packages/material-ui-styles/src/createStyles/createStyles.d.ts
+++ packages/material-ui-styles/src/createStyles/createStyles.d.ts
@@ -11,6 +11,6 @@ import { StyleRules } from '@material-ui/styles/withStyles';
 // For TypeScript v3.5 Props has to extend {} instead of object
 // See https://github.com/mui-org/material-ui/issues/15942
 // and https://github.com/microsoft/TypeScript/issues/31735
-export default function createStyles<ClassKey extends string, Props extends {} = any>(
+export default function createStyles<ClassKey extends string, Props extends {}>(
   styles: StyleRules<Props, ClassKey>
 ): StyleRules<Props, ClassKey>;
