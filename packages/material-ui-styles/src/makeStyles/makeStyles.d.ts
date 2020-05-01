e4c0eb2640354e8b4184d1dec7e291bea68cffb1
Vladimir <vzh.inbox@gmail.com>
2020-05-01 13:32:30 +0600

    Fix incorrect infering when using both theme and props

--- packages/material-ui-styles/src/makeStyles/makeStyles.d.ts
+++ packages/material-ui-styles/src/makeStyles/makeStyles.d.ts
@@ -7,21 +7,21 @@ import {
 import { Omit } from '@material-ui/types';
 import { DefaultTheme } from '../defaultTheme';
 
+type MakeStylesHook<
+  Props extends {} = any,
+  ClassKey extends string = string
+> = undefined extends Props
+  ? (props?: Pick<StyledComponentProps<ClassKey>, 'classes'>) => ClassNameMap<ClassKey>
+  : (props: Props & Pick<StyledComponentProps<ClassKey>, 'classes'>) => ClassNameMap<ClassKey>;
+
 /**
- * `makeStyles` where the passed `styles` do not depend on props
- */
-export default function makeStyles<Theme = DefaultTheme, ClassKey extends string = string>(
-  style: Styles<Theme, {}, ClassKey>,
-  options?: Omit<WithStylesOptions<Theme>, 'withTheme'>
-): (props?: any) => ClassNameMap<ClassKey>;
-/**
- * `makeStyles` where the passed `styles` do depend on props
+ * `makeStyles` where the passed `styles` can depend on props
  */
 export default function makeStyles<
   Theme = DefaultTheme,
-  Props extends {} = {},
+  Props extends {} = any,
   ClassKey extends string = string
 >(
   styles: Styles<Theme, Props, ClassKey>,
-  options?: Omit<WithStylesOptions<Theme>, 'withTheme'>
-): (props: Props & Pick<StyledComponentProps<ClassKey>, 'classes'>) => ClassNameMap<ClassKey>;
+  options?: Omit<WithStylesOptions<Theme>, 'withTheme'>,
+): MakeStylesHook<Props, ClassKey>;
