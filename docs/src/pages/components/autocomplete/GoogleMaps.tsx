fb0cb08d69c3c8b61f49b6eb576e15df17b9152d
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-05-10 20:03:11 +0200

    [Autocompete] Fix Google Map demo warnings

--- docs/src/pages/components/autocomplete/GoogleMaps.tsx
+++ docs/src/pages/components/autocomplete/GoogleMaps.tsx
@@ -45,6 +45,7 @@ interface PlaceType {
 
 export default function GoogleMaps() {
   const classes = useStyles();
+  const [value, setValue] = React.useState<PlaceType | null>(null);
   const [inputValue, setInputValue] = React.useState('');
   const [options, setOptions] = React.useState<PlaceType[]>([]);
   const loaded = React.useRef(false);
@@ -60,10 +61,6 @@ export default function GoogleMaps() {
 
     loaded.current = true;
   }
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-  };
 
   const fetch = React.useMemo(
     () =>
@@ -84,20 +81,30 @@ export default function GoogleMaps() {
     }
 
     if (inputValue === '') {
-      setOptions([]);
+      setOptions(value ? [value] : []);
       return undefined;
     }
 
     fetch({ input: inputValue }, (results?: PlaceType[]) => {
       if (active) {
-        setOptions(results || []);
+        let newOptions = [] as PlaceType[];
+
+        if (value) {
+          newOptions = [value];
+        }
+
+        if (results) {
+          newOptions = [...newOptions, ...results];
+        }
+
+        setOptions(newOptions);
       }
     });
 
     return () => {
       active = false;
     };
-  }, [inputValue, fetch]);
+  }, [value, inputValue, fetch]);
 
   return (
     <Autocomplete
@@ -108,14 +115,17 @@ export default function GoogleMaps() {
       options={options}
       autoComplete
       includeInputInList
+      filterSelectedOptions
+      value={value}
+      onChange={(event: any, newValue: PlaceType | null) => {
+        setOptions(newValue ? [newValue, ...options] : options);
+        setValue(newValue);
+      }}
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
       renderInput={(params) => (
-        <TextField
-          {...params}
-          label="Add a location"
-          variant="outlined"
-          fullWidth
-          onChange={handleChange}
-        />
+        <TextField {...params} label="Add a location" variant="outlined" fullWidth />
       )}
       renderOption={(option) => {
         const matches = option.structured_formatting.main_text_matched_substrings;
