a8902de0fea0ad2637137ab08546d9a829b293dd
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-05-12 20:13:31 +0200

    [core] Allow undefined coefficient in emphasize (#21007)

--- packages/material-ui/src/styles/colorManipulator.d.ts
+++ packages/material-ui/src/styles/colorManipulator.d.ts
@@ -11,7 +11,7 @@ export function decomposeColor(color: string): ColorObject;
 export function recomposeColor(color: ColorObject): string;
 export function getContrastRatio(foreground: string, background: string): number;
 export function getLuminance(color: string): number;
-export function emphasize(color: string, coefficient: number): string;
+export function emphasize(color: string, coefficient?: number): string;
 export function fade(color: string, value: number): string;
 export function darken(color: string, coefficient: number): string;
 export function lighten(color: string, coefficient: number): string;
