c426551a144b5cbe425b7d4b09dd6e525d034088
Mike Wyatt <wyatt.mike@gmail.com>
2020-05-17 18:17:51 -0300

    demos

---
+++ docs/src/pages/components/skeleton/Typography.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import Skeleton from '@material-ui/lab/Skeleton';
+import Grid from '@material-ui/core/Grid';
+
+function TypographyDemo(props) {
+  const { loading = false } = props;
+
+  const variants = [
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'subtitle1',
+    'subtitle2',
+    'body1',
+    'body2',
+    'caption',
+  ];
+
+  return (
+    <div>
+      {variants.map((variant) => {
+        return <Typography variant={variant}>{loading ? <Skeleton /> : variant}</Typography>;
+      })}
+    </div>
+  );
+}
+
+TypographyDemo.propTypes = {
+  loading: PropTypes.bool,
+};
+
+export default function SkeletonTypography() {
+  return (
+    <Grid container spacing={8}>
+      <Grid item xs>
+        <TypographyDemo loading />
+      </Grid>
+      <Grid item xs>
+        <TypographyDemo />
+      </Grid>
+    </Grid>
+  );
+}
--- docs/src/pages/components/skeleton/skeleton.md
+++ docs/src/pages/components/skeleton/skeleton.md
@@ -39,3 +39,17 @@ By default, the skeleton pulsate, but you can change the animation for a wave or
 ## Facebook example
 
 {{"demo": "pages/components/skeleton/Facebook.js", "defaultCodeOpen": false, "bg": true}}
+
+## Inferring width and height
+
+In addition to accepting `width` and `height` props, `Skeleton` can also infer
+those in other ways. It already works well when it comes to `Typography`, as
+its height is set using `rem` units.
+
+{{"demo": "pages/components/skeleton/Typography.js", "defaultCodeOpen": false}}
+
+But when it comes to other components, you may not want to repeat the width and
+height. In these instances, you can pass children to `Skeleton` and it will
+infer its width and height from them.
+
+{{"demo": "pages/components/skeleton/Children.js", "defaultCodeOpen": false}}
