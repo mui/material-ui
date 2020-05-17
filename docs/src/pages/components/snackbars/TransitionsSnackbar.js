afd485346345d319c062b84e07d97a6e4d41e20c
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2020-05-17 11:10:01 +0100

    [Snackbar] Fix double click issue on demos (#21059)

--- docs/src/pages/components/snackbars/TransitionsSnackbar.js
+++ docs/src/pages/components/snackbars/TransitionsSnackbar.js
@@ -43,6 +43,7 @@ export default function TransitionsSnackbar() {
         onClose={handleClose}
         TransitionComponent={state.Transition}
         message="I love snacks"
+        key={state.Transition.name}
       />
     </div>
   );
