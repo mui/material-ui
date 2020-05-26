2c27d09f235f6f5dea98c568a52e4d20e1ba8756
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-05-26 15:07:33 +0300

    Generate docs

--- packages/material-ui/src/Unstable_TrapFocus/TrapFocus.d.ts
+++ packages/material-ui/src/Unstable_TrapFocus/TrapFocus.d.ts
@@ -9,7 +9,12 @@ export interface TrapFocusProps {
   disableEnforceFocus?: boolean;
   disableRestoreFocus?: boolean;
 }
-
+/**
+ * Utility component that locks focus inside the component.
+ * API:
+ *
+ * - [TrapFocus API](https://material-ui.com/api/trap-focus/)
+ */
 declare const TrapFocus: React.ComponentType<TrapFocusProps>;
 
 export default TrapFocus;
