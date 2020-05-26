d23b822996a09e1bc125e03f1ee1ad1fc80fb7d2
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-05-26 13:23:03 +0200

    fix lint

---
+++ packages/material-ui-utils/test/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rules: {
+    // auto generated imports do no add a newline
+    'import/newline-after-import': 'off',
+    // for testing purposes
+    'no-useless-concat': 'off',
+  },
+};
