e5bf6ec8fd4c9c11a7e5979fa5935ec712dd1879
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-05-29 12:17:17 +0200

    fix: pre-transpile template literals in loose mode

--- packages/material-ui-utils/src/index.js
+++ packages/material-ui-utils/src/index.js
@@ -14,11 +14,16 @@ export { default as refType } from './refType';
  * @param {number} code
  */
 export function formatMuiErrorMessage(code) {
-  let url = `https://material-ui.com/error-decoder/${code}/?`;
+  // Apply babel-plugin-transform-template-literals in loose mode
+  // loose mode is safe iff we're concatenating primitives
+  // see https://babeljs.io/docs/en/babel-plugin-transform-template-literals#loose
+  /* eslint-disable prefer-template */
+  let url = 'https://material-ui.com/error-decoder/' + code + '/?';
   for (let i = 1; i < arguments.length; i += 1) {
     // rest params over-transpile for this case
     // eslint-disable-next-line prefer-rest-params
-    url += `args[]=${encodeURIComponent(arguments[i])}&`;
+    url += 'args[]=' + encodeURIComponent(arguments[i]) + '&';
   }
-  return `Minified Material-UI error #${code}; visit ${url} for the full message.`;
+  return 'Minified Material-UI error #' + code + '; visit ' + url + ' for the full message.';
+  /* eslint-enable prefer-template */
 }
