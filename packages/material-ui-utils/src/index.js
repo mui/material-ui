4208903f690f462c94f0a5ae67fe08900c87262c
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-05-29 13:26:03 +0200

    fix: webpack can't tree-shake a "mixed-barell"
    mixed-barell means it's part re-export part actual export
    webpack keeps all the re-exports around even if you only import
    the actual export.
    
    Maybe something is off with side-effect detection

--- packages/material-ui-utils/src/index.js
+++ packages/material-ui-utils/src/index.js
@@ -3,27 +3,8 @@ export { default as deepmerge } from './deepmerge';
 export { default as elementAcceptingRef } from './elementAcceptingRef';
 export { default as elementTypeAcceptingRef } from './elementTypeAcceptingRef';
 export { default as exactProp } from './exactProp';
+export { default as formatMuiErrorMessage } from './formatMuiErrorMessage';
 export { default as getDisplayName } from './getDisplayName';
 export { default as HTMLElementType } from './HTMLElementType';
 export { default as ponyfillGlobal } from './ponyfillGlobal';
 export { default as refType } from './refType';
-
-/**
- * WARNING: Don't import this directly.
- * Use `MuiError` from `@material-ui/utils/macros/MuiError.macro` instead.
- * @param {number} code
- */
-export function formatMuiErrorMessage(code) {
-  // Apply babel-plugin-transform-template-literals in loose mode
-  // loose mode is safe iff we're concatenating primitives
-  // see https://babeljs.io/docs/en/babel-plugin-transform-template-literals#loose
-  /* eslint-disable prefer-template */
-  let url = 'https://material-ui.com/error-decoder/' + code + '/?';
-  for (let i = 1; i < arguments.length; i += 1) {
-    // rest params over-transpile for this case
-    // eslint-disable-next-line prefer-rest-params
-    url += 'args[]=' + encodeURIComponent(arguments[i]) + '&';
-  }
-  return 'Minified Material-UI error #' + code + '; visit ' + url + ' for the full message.';
-  /* eslint-enable prefer-template */
-}
