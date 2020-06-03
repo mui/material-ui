15967902457f8342be9f6c7945f5126ede5bd6e2
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-06-03 20:16:45 +0200

    [test] Type-test event handlers on ListItem (#21298)

--- packages/material-ui/src/ListItem/ListItem.spec.tsx
+++ packages/material-ui/src/ListItem/ListItem.spec.tsx
@@ -19,3 +19,14 @@ function BooleanButtonTest() {
     return <ListItem>Editable? No</ListItem>;
   }
 }
+
+// verify that https://github.com/mui-org/material-ui/issues/19756 already worked.
+function MouseEnterTest() {
+  function handleMouseEnter(event: React.MouseEvent<HTMLLIElement>) {}
+  <ListItem onMouseEnter={handleMouseEnter} />;
+
+  function handleMouseEnterButton(event: React.MouseEvent<HTMLDivElement>) {}
+  // $ExpectError
+  <ListItem onMouseEnter={handleMouseEnterButton} />; // desired: missing property button
+  <ListItem button onMouseEnter={handleMouseEnterButton} />;
+}
