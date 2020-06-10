fa7701df47583e6eca1257f1300b05b24a23f332
Marija Najdova <mnajdova@gmail.com>
2020-06-10 21:54:44 +0200

    fix width change when changing state

--- packages/material-ui/src/LoadingButton/LoadingButton.js
+++ packages/material-ui/src/LoadingButton/LoadingButton.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import clsx from 'clsx';
 import withStyles from '../styles/withStyles';
+import capitalize from '../utils/capitalize';
 import Button from '../Button';
 import CircularProgress from '../CircularProgress';
 
@@ -9,6 +10,36 @@ export const styles = () => ({
   root: {},
   /* Styles applied to the root element if `loading={true}`. */
   loading: {},
+  /* Styles applied to the loadingIndicator element. */
+  loadingIndicator: {
+    position: 'absolute',
+    visibility: 'visible',
+  },
+  /* Styles applied to the loadingIndicator element if `loadingIndicatorPosition="center"`. */
+  loadingIndicatorCenter: {
+    left: '50%',
+    transform: 'translate(-50%)',
+  },
+  /* Styles applied to the loadingIndicator element if `loadingIndicatorPosition="start"`. */
+  loadingIndicatorStart: {
+    left: 10,
+  },
+  /* Styles applied to the loadingIndicator element if `loadingIndicatorPosition="end"`. */
+  loadingIndicatorEnd: {
+    right: 10,
+  },
+  /* Styles applied to the endIcon element if `loading={true}` and `loadingIndicatorPosition="end"`. */
+  endIconLoadingEnd: {
+    visibility: 'hidden'
+  },
+  /* Styles applied to the startIcon element if `loading={true}` and `loadingIndicatorPosition="start"`. */
+  startIconLoadingStart: {
+    visibility: 'hidden'
+  },
+  /* Styles applied to the label element if `loading={true}` and `loadingIndicatorPosition="center"`. */
+  labelLoadingCenter: {
+    visibility: 'hidden',
+  },
 });
 
 const LoadingButton = React.forwardRef(function LoadingButton(props, ref) {
@@ -18,8 +49,6 @@ const LoadingButton = React.forwardRef(function LoadingButton(props, ref) {
     disabled = false,
     loading = false,
     loadingIndicatorPosition = 'start',
-    startIcon,
-    endIcon,
     children,
     loadingIndicator = <CircularProgress color="inherit" size={16} />,
     ...other
@@ -34,13 +63,17 @@ const LoadingButton = React.forwardRef(function LoadingButton(props, ref) {
         },
         className,
       )}
-      startIcon={loading && loadingIndicatorPosition === 'start' ? loadingIndicator : startIcon}
-      endIcon={loading && loadingIndicatorPosition === 'end' ? loadingIndicator : endIcon}
       disabled={disabled || loading}
       ref={ref}
+      classes={{
+        startIcon: classes[`startIcon${loading ? 'Loading' : ''}${capitalize(loadingIndicatorPosition)}`],
+        endIcon: classes[`endIcon${loading ? 'Loading' : ''}${capitalize(loadingIndicatorPosition)}`],
+        label: classes[`label${loading ? 'Loading' : ''}${capitalize(loadingIndicatorPosition)}`]
+      }}
       {...other}
     >
-      {loading && loadingIndicatorPosition === 'center' ? loadingIndicator : children}
+      {loading && <div className={clsx(classes.loadingIndicator, classes[`loadingIndicator${capitalize(loadingIndicatorPosition)}`])}>{loadingIndicator}</div>}
+      {children}
     </Button>
   );
 });
