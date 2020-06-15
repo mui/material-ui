3fdd3107c6f72b1eb405d576fba2edb75709f1ee
Marija Najdova <mnajdova@gmail.com>
2020-06-15 21:33:56 +0200

    converted example to use styled-components

--- docs/src/pages/customization/transitions/TransitionHover.tsx
+++ docs/src/pages/customization/transitions/TransitionHover.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import styled, { ThemeProvider } from 'styled-components';
+import NoSsr from '@material-ui/core/NoSsr';
+import {
+  createMuiTheme,
+  ThemeProvider as MuiThemeProvider,
+} from '@material-ui/core/styles';
+import { deepPurple } from '@material-ui/core/colors';
 import Avatar from '@material-ui/core/Avatar';
 
-const useStyles = makeStyles((theme) => ({
-  avatar: {
-    cursor: 'pointer',
-    color: theme.palette.primary.contrastText,
-    backgroundColor: theme.palette.primary.main,
-    transition: theme.transitions.create(['background-color', 'transform'], {
-      duration: theme.transitions.duration.standard,
-    }),
-    '&:hover': {
-      backgroundColor: theme.palette.secondary.main,
-      transform: 'scale(1.3)',
-    },
-  },
-}));
+const customTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: deepPurple[500],
+    }
+  }
+})
+
+const Styledvatar = styled(Avatar)`
+  ${({ theme }) => `
+  cursor: pointer;
+  background-color: ${theme.palette.primary.main};
+  transition: ${theme.transitions.create(['background-color', 'transform'], {
+    duration: theme.transitions.duration.standard,
+  })};
+  &:hover {
+    background-color: ${theme.palette.secondary.main};
+    transform: scale(1.3);
+  }
+  `}
+`;
 
 export default function TransitionHover() {
-  const classes = useStyles();
-
-  return <Avatar className={classes.avatar}>OP</Avatar>;
+  return (
+    <NoSsr>
+      <MuiThemeProvider theme={customTheme}>
+        <ThemeProvider theme={customTheme}>
+          <Styledvatar>OP</Styledvatar>
+        </ThemeProvider>
+      </MuiThemeProvider>
+    </NoSsr>
+  );
 }
