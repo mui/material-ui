564000f17101669725d00bd4ee0e04206519b85d
Marija Najdova <mnajdova@gmail.com>
2020-06-25 18:15:14 +0200

    reverted all expansion panel files, disabled tests

---
+++ packages/material-ui/src/ExpansionPanelActions/ExpansionPanelActions.d.ts
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { StandardProps } from '..';
+
+export interface ExpansionPanelActionsProps
+  extends StandardProps<React.HTMLAttributes<HTMLDivElement>, ExpansionPanelActionsClassKey> {
+  /**
+   * The content of the component.
+   */
+  children?: React.ReactNode;
+  /**
+   * If `true`, the actions do not have additional margin.
+   */
+  disableSpacing?: boolean;
+}
+
+export type ExpansionPanelActionsClassKey = 'root' | 'spacing';
+
+/**
+ *
+ * API:
+ *
+ * - [ExpansionPanelActions API](https://material-ui.com/api/expansion-panel-actions/)
+ */
+export default function ExpansionPanelActions(props: ExpansionPanelActionsProps): JSX.Element;
