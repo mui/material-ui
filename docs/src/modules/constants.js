2f55359dcc8d65e2536be3960890b1e6a2493ae9
Matt <github@nospam.33m.co>
2020-06-27 18:16:07 +0100

    Remove in-context translation code

--- docs/src/modules/constants.js
+++ docs/src/modules/constants.js
@@ -9,7 +9,7 @@ const ACTION_TYPES = {
 };
 
 // Valid languages to server-side render in production
-const LANGUAGES = ['en', 'zh', 'ru', 'pt', 'es', 'fr', 'de', 'ja', 'aa'];
+const LANGUAGES = ['en', 'zh', 'ru', 'pt', 'es', 'ja'];
 
 // Server side rendered languages
 const LANGUAGES_SSR = ['en', 'zh', 'ru', 'pt', 'es'];
@@ -38,14 +38,6 @@ const LANGUAGES_LABEL = [
   {
     code: 'es',
     text: 'Español',
-  },
-  {
-    code: 'fr',
-    text: 'Français',
-  },
-  {
-    code: 'de',
-    text: 'Deutsch',
   },
   {
     code: 'ja',
