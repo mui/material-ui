ed33f7570377a7ee58579b90a77c68b9c0a56b95
Matt <github@nospam.33m.co>
2020-06-28 22:16:33 +0100

    fix Global CSS example

--- docs/src/pages/customization/globals/GlobalCss.tsx
+++ docs/src/pages/customization/globals/GlobalCss.tsx
@@ -7,7 +7,7 @@ const theme = createMuiTheme({
     // Style sheet name ⚛️
     MuiButton: {
       // Name of the rule
-      text: {
+      textPrimary: {
         // Some CSS
         background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
         borderRadius: 3,
