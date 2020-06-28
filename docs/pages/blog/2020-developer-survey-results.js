d5ddb66dca7fc11914dcf74b6b3e8f072aeffa08
Marija Najdova <mnajdova@gmail.com>
2020-06-28 13:18:22 +0200

    [blog] Post survey results 2020 (#21555)

---
+++ docs/pages/blog/2020-developer-survey-results.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TopLayoutBlog from 'docs/src/modules/components/TopLayoutBlog';
+import { prepareMarkdown } from 'docs/src/modules/utils/parseMarkdown';
+
+const pageFilename = 'blog/2020-developer-survey-results';
+const requireRaw = require.context('!raw-loader!./', false, /2020-developer-survey-results\.md$/);
+
+export default function Page({ docs }) {
+  return <TopLayoutBlog docs={docs} />;
+}
+
+Page.getInitialProps = () => {
+  const { demos, docs } = prepareMarkdown({ pageFilename, requireRaw });
+  return { demos, docs };
+};
