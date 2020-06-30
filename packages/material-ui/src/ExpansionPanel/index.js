3306fed2bf08e040473eed8f266175c87562e4ef
Marija Najdova <mnajdova@gmail.com>
2020-06-30 20:06:11 +0200

    removed exports for ExpansionPanel

--- packages/material-ui/src/ExpansionPanel/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Use `Accordion` instead. This component will be removed in v5.
- *
- * @deprecated
- */
-export { default } from '../Accordion';
--- packages/material-ui/src/ExpansionPanelActions/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Use `AccordionActions` instead. This component will be removed in v5.
- *
- * @deprecated
- */
-export { default } from '../AccordionActions';
