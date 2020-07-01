1bad752c145dd89649278a7f12a510cbb065798b
Jakub Figlak <jakub.figlak@gmail.com>
2020-07-01 19:46:33 +0200

    [Localization] remove keyword type after ComponentsPropsList import

--- packages/material-ui/src/locale/index.ts
+++ packages/material-ui/src/locale/index.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import type { ComponentsPropsList } from '../styles/props';
+import { ComponentsPropsList } from '../styles/props';
 
 export interface Localization {
   props?: {
