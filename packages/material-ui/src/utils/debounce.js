e66a5f247fd570fe6be5a5a100b44a56ab223290
Minh Nguyen <minhnguyenxx@gmail.com>
2020-07-07 15:29:44 +0100

    [core] Simplify debounce (#21666)

--- packages/material-ui/src/utils/debounce.js
+++ packages/material-ui/src/utils/debounce.js
@@ -3,10 +3,8 @@
 export default function debounce(func, wait = 166) {
   let timeout;
   function debounced(...args) {
-    // eslint-disable-next-line consistent-this
-    const that = this;
     const later = () => {
-      func.apply(that, args);
+      func.apply(this, args);
     };
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
