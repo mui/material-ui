49a309477e1223e93f9fa6cd8cbd488f913e237d
Ken Gregory <kjg@storis.com>
2020-07-12 07:35:31 -0400

    [List] Fix secondary action position when disableGutters={true}  (#21732)

---
+++ docs/src/pages/components/lists/GutterlessList.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import CommentIcon from '@material-ui/icons/Comment';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+  }),
+);
+
+export default function GutterlessList() {
+  const classes = useStyles();
+
+  return (
+    <List className={classes.root}>
+      {[1, 2, 3].map((value) => (
+        <ListItem key={value} disableGutters>
+          <ListItemText primary={`Line item ${value}`} />
+          <ListItemSecondaryAction>
+            <CommentIcon color="action" />
+          </ListItemSecondaryAction>
+        </ListItem>
+      ))}
+    </List>
+  );
+}
