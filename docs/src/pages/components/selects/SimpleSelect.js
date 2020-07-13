0d641e63af6e8e5feaa806f4dc8cddf469b12a07
Christian Jöcker <kontakt@cjoecker.de>
2020-07-13 11:38:38 +0200

    Refactor selects order

--- docs/src/pages/components/selects/SimpleSelect.js
+++ docs/src/pages/components/selects/SimpleSelect.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
