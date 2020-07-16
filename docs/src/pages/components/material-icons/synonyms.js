a9380cedaf05828a7c408cd31c26db60cc8949b0
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-07-16 12:24:08 +0200

    consistency

--- docs/src/pages/components/material-icons/synonyms.js
+++ docs/src/pages/components/material-icons/synonyms.js
@@ -332,7 +332,7 @@ const synonyms = {
   GitHub: 'brand code',
   GolfCourse: 'flag',
   GpsFixed: 'location',
-  Google: 'Brand logo',
+  Google: 'brand logo',
   Grade: 'likes rated rating shape star',
   GraphicEq: 'audio equalizer',
   GridOn: 'sheet',
