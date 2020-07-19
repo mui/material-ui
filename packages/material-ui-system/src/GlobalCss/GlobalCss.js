09bce372b4c146b0d137e168ac7b29f44e14ccee
Marija Najdova <mnajdova@gmail.com>
2020-07-19 14:56:34 +0200

    prettier

--- packages/material-ui-system/src/GlobalCss/GlobalCss.js
+++ packages/material-ui-system/src/GlobalCss/GlobalCss.js
@@ -6,17 +6,20 @@ import typography from './typography';
 import positions from './positions';
 import displays from './displays';
 
-const GlobalCss = withStyles((theme) => {
-  return {
-    '@global': {
-      ...spacings(theme),
-      ...colors(theme),
-      ...elevations(theme),
-      ...typography(theme),
-      ...positions(theme),
-      ...displays(theme),
-    },
-  };
-}, { index: 0 })(() => null);
+const GlobalCss = withStyles(
+  (theme) => {
+    return {
+      '@global': {
+        ...spacings(theme),
+        ...colors(theme),
+        ...elevations(theme),
+        ...typography(theme),
+        ...positions(theme),
+        ...displays(theme),
+      },
+    };
+  },
+  { index: 0 },
+)(() => null);
 
 export default GlobalCss;
