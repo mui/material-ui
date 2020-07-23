5fd29d5cf3f9ed701fa72906d07b1c570fe672b1
Matt <github@nospam.33m.co>
2020-07-23 01:48:02 +0100

    [framer] Fix Avatar imageFile & imageUrl

--- framer/Material-UI.framerfx/code/Avatar.tsx
+++ framer/Material-UI.framerfx/code/Avatar.tsx
@@ -10,20 +10,20 @@ interface Props {
   backgroundColor?: string;
   textColor?: string;
   icon?: string;
-  imageFile?: string;
-  imageUrl?: string;
+  avatarImageFile?: string;
+  avatarImageUrl?: string;
   label?: string;
   width?: number;
   height?: number;
 }
 
-const defaultProps: Props = {
+const defaultProps: Props =  {
   variant: 'circle',
   backgroundColor: '#4154af',
   textColor: undefined,
   icon: 'face',
-  imageFile: '',
-  imageUrl: 'https://i.pravatar.cc/300',
+  avatarImageFile: '',
+  avatarImageUrl: 'https://i.pravatar.cc/300',
   label: 'MB',
   width: 40,
   height: 40,
@@ -34,8 +34,8 @@ export const Avatar: React.SFC<Props> = (props: Props) => {
     backgroundColor,
     height,
     icon,
-    imageFile,
-    imageUrl,
+    avatarImageFile: imageFile,
+    avatarImageUrl: imageUrl,
     label,
     textColor,
     width,
@@ -49,44 +49,41 @@ export const Avatar: React.SFC<Props> = (props: Props) => {
       {icon === '' ? label : <Icon icon={icon} />}
     </MuiAvatar>
   );
-};
+}
 
 Avatar.defaultProps = defaultProps;
 
 addPropertyControls(Avatar, {
-  variant: {
-    type: ControlType.Enum,
-    title: 'Variant',
-    options: ['circle', 'rounded', 'square'],
-  },
-  backgroundColor: {
-    type: ControlType.Color,
-    title: 'Background color',
-  },
-  textColor: {
-    type: ControlType.Color,
-    title: 'Text color',
-  },
-  icon: {
-    type: ControlType.String,
-    title: 'Icon',
-  },
-  imageFile: {
-    type: ControlType.Image,
-    title: 'Image File',
-    hidden: function hidden(props) {
-      return props.primaryAction && props.primaryAction !== 'avatar';
-    },
-  },
-  imageUrl: {
-    type: ControlType.String,
-    title: 'Image URL',
-    hidden: function hidden(props) {
-      return props.imageFile !== '' || (props.primaryAction && props.primaryAction !== 'avatar');
-    },
-  },
-  label: {
-    type: ControlType.String,
-    title: 'Label',
-  },
+variant: {
+  type: ControlType.Enum,
+  title: 'Variant',
+  options: ['circle', 'rounded', 'square'],
+},
+backgroundColor: {
+  type: ControlType.Color,
+  title: 'Background color',
+},
+textColor: {
+  type: ControlType.Color,
+  title: 'Text color',
+},
+icon: {
+  type: ControlType.String,
+  title: 'Icon',
+},
+avatarImageFile: {
+  type: ControlType.Image,
+  title: 'Avatar Image File',
+},
+avatarImageUrl: {
+  type: ControlType.String,
+  title: 'Avatar Image URL',
+  hidden: function hidden(props) {
+          return props.avatarImageFile !== '';
+        },
+},
+label: {
+  type: ControlType.String,
+  title: 'Label',
+},
 });
