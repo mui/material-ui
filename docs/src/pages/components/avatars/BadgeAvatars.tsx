a9340a47badd19f88e03371a438fe2c52b9be475
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-08-07 14:21:01 +0200

    [Badge] Add overlap circular and rectangular (#22076)

--- docs/src/pages/components/avatars/BadgeAvatars.tsx
+++ docs/src/pages/components/avatars/BadgeAvatars.tsx
@@ -61,7 +61,7 @@ export default function BadgeAvatars() {
   return (
     <div className={classes.root}>
       <StyledBadge
-        overlap="circle"
+        overlap="circular"
         anchorOrigin={{
           vertical: 'bottom',
           horizontal: 'right',
@@ -71,7 +71,7 @@ export default function BadgeAvatars() {
         <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
       </StyledBadge>
       <Badge
-        overlap="circle"
+        overlap="circular"
         anchorOrigin={{
           vertical: 'bottom',
           horizontal: 'right',
