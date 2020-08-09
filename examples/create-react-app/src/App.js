def522442adbf6aab2db9da680e2ff11939efc31
fandok <mr.parker0@gmail.com>
2020-08-09 12:22:19 +0700

    [examples/cra] add browserslist, update App.js html format

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -5,29 +5,26 @@ import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
 import ProTip from './ProTip';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+const Copyright = () => (
+  <Typography variant="body2" color="textSecondary" align="center">
+    Copyright &copy;
+    <Link color="inherit" href="https://material-ui.com/">
+      Your Website
+    </Link>
+    {new Date().getFullYear()}.
+  </Typography>
+);
 
-export default function App() {
-  return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v4-beta example
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+const App = () => (
+  <Container maxWidth="sm">
+    <Box my={4}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Create React App v4-beta example
+      </Typography>
+      <ProTip />
+      <Copyright />
+    </Box>
+  </Container>
+);
+
+export default App;
