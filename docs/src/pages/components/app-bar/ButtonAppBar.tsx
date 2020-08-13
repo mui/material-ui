53cf934b2c97a12f029a1b7ab4a9c36099c269dd
brackenl <31513722+brackenl@users.noreply.github.com>
2020-08-13 12:58:11 +0100

    Update ButtonAppBar.tsx
    This example refers to "MenuIcon" rather than "Menu" which seems to be outdated

--- docs/src/pages/components/app-bar/ButtonAppBar.tsx
+++ docs/src/pages/components/app-bar/ButtonAppBar.tsx
@@ -5,7 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -29,7 +29,7 @@ export default function ButtonAppBar() {
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+            <Menu />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             News
