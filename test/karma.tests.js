e43f4d17f071b45f43ddb612a8cb91689aea1136
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-08-14 13:07:17 +0200

    [test] Setup infra for tests or implementation in TypeScript

--- test/karma.tests.js
+++ test/karma.tests.js
@@ -5,12 +5,20 @@ import './utils/init';
 const integrationContext = require.context(
   '../packages/material-ui/test/integration',
   true,
-  /\.test\.js$/,
+  /\.test\.(js|ts|tsx)$/,
 );
 integrationContext.keys().forEach(integrationContext);
 
-const coreUnitContext = require.context('../packages/material-ui/src/', true, /\.test\.js$/);
+const coreUnitContext = require.context(
+  '../packages/material-ui/src/',
+  true,
+  /\.test\.(js|ts|tsx)$/,
+);
 coreUnitContext.keys().forEach(coreUnitContext);
 
-const labUnitContext = require.context('../packages/material-ui-lab/src/', true, /\.test\.js$/);
+const labUnitContext = require.context(
+  '../packages/material-ui-lab/src/',
+  true,
+  /\.test\.(js|ts|tsx)$/,
+);
 labUnitContext.keys().forEach(labUnitContext);
