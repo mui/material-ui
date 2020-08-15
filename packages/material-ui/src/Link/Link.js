1fd983abd9c26d3d751f99d2efbfa2e5ab063786
Marija Najdova <mnajdova@gmail.com>
2020-08-15 18:16:30 +0200

    [Link] Custom variant (#22204)
    [Link] Custom variant (#22204)

--- packages/material-ui/src/Link/Link.js
+++ packages/material-ui/src/Link/Link.js
@@ -172,21 +172,24 @@ Link.propTypes = {
   /**
    * Applies the theme typography styles.
    */
-  variant: PropTypes.oneOf([
-    'body1',
-    'body2',
-    'button',
-    'caption',
-    'h1',
-    'h2',
-    'h3',
-    'h4',
-    'h5',
-    'h6',
-    'inherit',
-    'overline',
-    'subtitle1',
-    'subtitle2',
+  variant: PropTypes /* @typescript-to-proptypes-ignore */.oneOfType([
+    PropTypes.oneOf([
+      'body1',
+      'body2',
+      'button',
+      'caption',
+      'h1',
+      'h2',
+      'h3',
+      'h4',
+      'h5',
+      'h6',
+      'inherit',
+      'overline',
+      'subtitle1',
+      'subtitle2',
+    ]),
+    PropTypes.string,
   ]),
 };
 
