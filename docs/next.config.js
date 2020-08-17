f46dedfcf8a546b6e6f46ea3157e2d63ce911602
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-08-17 10:53:54 +0200

    fix warning

--- docs/next.config.js
+++ docs/next.config.js
@@ -134,7 +134,6 @@ module.exports = {
       },
     };
   },
-  exportTrailingSlash: true,
   trailingSlash: true,
   // Next.js provides a `defaultPathMap` argument, we could simplify the logic.
   // However, we don't in order to prevent any regression in the `findPages()` method.
