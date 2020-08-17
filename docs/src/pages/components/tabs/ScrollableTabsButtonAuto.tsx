9f211af716c0d32bb8d6354f5ea74a6c40ada5e6
Marcelo Gheiler <marcelogheiler@gmail.com>
2020-08-17 11:04:22 -0400

    fix lint issue

--- docs/src/pages/components/tabs/ScrollableTabsButtonAuto.tsx
+++ docs/src/pages/components/tabs/ScrollableTabsButtonAuto.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
