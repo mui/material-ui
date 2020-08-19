579dd49ddf3b2f8c4462a3e3dc892395d8e7e51e
Avi <pireads@gmail.com>
2020-08-19 10:43:18 -0700

    Re-enable disable check on keydown

--- packages/material-ui/src/Slider/Slider.js
+++ packages/material-ui/src/Slider/Slider.js
@@ -452,9 +452,9 @@ const Slider = React.forwardRef(function Slider(props, ref) {
   const isRtl = theme.direction === 'rtl';
 
   const handleKeyDown = useEventCallback((event) => {
-    // if (disabled) {
-    //   return;
-    // }
+    if (disabled) {
+      return;
+    }
 
     const index = Number(event.currentTarget.getAttribute('data-index'));
     const value = values[index];
@@ -589,10 +589,6 @@ const Slider = React.forwardRef(function Slider(props, ref) {
   };
 
   const handleTouchMove = useEventCallback((nativeEvent) => {
-    // if (disabled) {
-    //   return;
-    // }
-
     const finger = trackFinger(nativeEvent, touchId);
 
     if (!finger) {
