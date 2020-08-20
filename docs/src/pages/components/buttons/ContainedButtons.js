4c4373759fcfec965a11b80e30836584dd6b5d29
Marija Najdova <mnajdova@gmail.com>
2020-08-20 20:52:35 +0200

    reverted contained button changes

--- docs/src/pages/components/buttons/ContainedButtons.js
+++ docs/src/pages/components/buttons/ContainedButtons.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { ThemeProvider } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,38 +10,21 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const theme = createMuiTheme({
-  components: {
-    MuiButton: {
-      overrides: {
-        disabled: {
-          color: 'red'
-        }
-      }
-    }
-  }
-})
-
 export default function ContainedButtons() {
   const classes = useStyles();
 
   return (
-    <ThemeProvider theme={theme}>
-      <div className={classes.root}>
-        <Button variant="contained">Primary</Button>
-        <Button variant="contained" color="secondary">
-          Secondary
-        </Button>
-        <Button variant="contained" disabled>
-          Disabled
-        </Button>
-        <Button variant="contained" href="#contained-buttons">
-          Link
-        </Button>
-        <Button variant="contained" disabled>
-          Link
-        </Button>
-      </div>
-    </ThemeProvider>
+    <div className={classes.root}>
+      <Button variant="contained">Primary</Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" href="#contained-buttons">
+        Link
+      </Button>
+    </div>
   );
 }
