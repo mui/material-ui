c1690c96e0b97991e58f7fb77f505ea650c33401
Andrew Rothbart <rothbart@gmail.com>
2020-08-20 13:12:40 -0700

    justifyContent

--- docs/src/pages/premium-themes/onepirate/modules/views/AppFooter.js
+++ docs/src/pages/premium-themes/onepirate/modules/views/AppFooter.js
@@ -83,6 +83,7 @@ export default function AppFooter() {
             <Grid
               container
               direction="column"
+              justifyContent="flex-end"
               className={classes.iconsWrapper}
               spacing={2}
             >
