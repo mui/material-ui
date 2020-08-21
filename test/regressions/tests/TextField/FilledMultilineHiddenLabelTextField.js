d40ae0e0018ef63801bd4d1d0938e43eb371d00a
Fakeha Rahman <fakeha126@gmail.com>
2020-08-21 15:12:38 +0530

    added changes

--- test/regressions/tests/TextField/FilledMultilineHiddenLabelTextField.js
+++ test/regressions/tests/TextField/FilledMultilineHiddenLabelTextField.js
@@ -2,5 +2,16 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
 export default function FilledMultilineHiddenLabelTextField() {
-  return <TextField variant="filled" multiline rows={3} hiddenLabel />;
+  return (
+    <div>
+      <TextField
+        variant="filled"
+        defaultValue="Multiline Default Value"
+        multiline
+        rows={1}
+        hiddenLabel
+      />
+      <TextField variant="filled" defaultValue="Default Value" rows={1} hiddenLabel />
+    </div>
+  );
 }
