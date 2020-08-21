e4b3c6d16b1a1aa2e8ed62286032ad16724ba16c
Marco Moretti <marcomoretti0103@gmail.com>
2020-08-21 09:02:58 +0200

    [colorManipulator] add support for CSS Color Module Level 4 (#20790)

--- packages/material-ui/src/styles/colorManipulator.d.ts
+++ packages/material-ui/src/styles/colorManipulator.d.ts
@@ -2,6 +2,7 @@ export type ColorFormat = 'rgb' | 'rgba' | 'hsl' | 'hsla';
 export interface ColorObject {
   type: ColorFormat;
   values: [number, number, number] | [number, number, number, number];
+  colorSpace?: 'srgb' | 'display-p3' | 'a98-rgb' | 'prophoto-rgb' | 'rec-2020';
 }
 
 export function hexToRgb(hex: string): string;
--- packages/material-ui/src/styles/colorManipulator.test.js
+++ packages/material-ui/src/styles/colorManipulator.test.js
@@ -33,6 +33,16 @@ describe('utils/colorManipulator', () => {
       ).to.equal('rgba(255, 255, 255, 0.5)');
     });
 
+    it('converts a decomposed CSS4 color object to a string` ', () => {
+      expect(
+        recomposeColor({
+          type: 'color',
+          colorSpace: 'display-p3',
+          values: [0.5, 0.3, 0.2],
+        }),
+      ).to.equal('color(display-p3 0.5 0.3 0.2)');
+    });
+
     it('converts a decomposed hsl color object to a string` ', () => {
       expect(
         recomposeColor({
@@ -115,6 +125,25 @@ describe('utils/colorManipulator', () => {
       expect(values).to.deep.equal([100, 50, 25, 0.5]);
     });
 
+    it('converts CSS4 color with color space display-3', () => {
+      const { type, values, colorSpace } = decomposeColor('color(display-p3 0 1 0)');
+      expect(type).to.equal('color');
+      expect(colorSpace).to.equal('display-p3');
+      expect(values).to.deep.equal([0, 1, 0]);
+    });
+
+    it('converts an alpha CSS4 color with color space display-3', () => {
+      const { type, values, colorSpace } = decomposeColor('color(display-p3 0 1 0 /0.4)');
+      expect(type).to.equal('color');
+      expect(colorSpace).to.equal('display-p3');
+      expect(values).to.deep.equal([0, 1, 0, 0.4]);
+    });
+
+    it('should throw error with inexistent color color space', () => {
+      const decimposeWithError = () => decomposeColor('color(foo 0 1 0)');
+      expect(decimposeWithError).to.throw();
+    });
+
     it('idempotent', () => {
       const output1 = decomposeColor('hsla(100, 50%, 25%, 0.5)');
       const output2 = decomposeColor(output1);
@@ -125,6 +154,7 @@ describe('utils/colorManipulator', () => {
       const decomposed = decomposeColor('#111111f8');
       expect(decomposed).to.deep.equal({
         type: 'rgba',
+        colorSpace: undefined,
         values: [17, 17, 17, 0.973],
       });
     });
@@ -156,6 +186,7 @@ describe('utils/colorManipulator', () => {
     it('returns a valid luminance for rgb black', () => {
       expect(getLuminance('rgba(0, 0, 0)')).to.equal(0);
       expect(getLuminance('rgb(0, 0, 0)')).to.equal(0);
+      expect(getLuminance('color(display-p3 0 0 0)')).to.equal(0);
     });
 
     it('returns a valid luminance for rgb white', () => {
@@ -182,6 +213,10 @@ describe('utils/colorManipulator', () => {
       expect(getLuminance(hsl)).to.equal(getLuminance(rgb));
     });
 
+    it('returns a valid luminance from an CSS4 color', () => {
+      expect(getLuminance('color(display-p3 1 1 0.1)')).to.equal(0.929);
+    });
+
     it('throw on invalid colors', () => {
       expect(() => {
         getLuminance('black');
@@ -205,11 +240,27 @@ describe('utils/colorManipulator', () => {
     it('darkens a light rgb color with the coefficient 0.15 by default', () => {
       expect(emphasize('rgb(250, 240, 230)')).to.equal(darken('rgb(250, 240, 230)', 0.15));
     });
+
+    it('lightens a dark CSS4 color with the coefficient 0.15 by default', () => {
+      expect(emphasize('color(display-p3 0.1 0.1 0.1)')).to.equal(
+        lighten('color(display-p3 0.1 0.1 0.1)', 0.15),
+      );
+    });
+
+    it('darkens a light CSS4 color with the coefficient 0.15 by default', () => {
+      expect(emphasize('color(display-p3 1 1 0.1)')).to.equal(
+        darken('color(display-p3 1 1 0.1)', 0.15),
+      );
+    });
   });
 
   describe('fade', () => {
     it('converts an rgb color to an rgba color with the value provided', () => {
       expect(fade('rgb(1, 2, 3)', 0.4)).to.equal('rgba(1, 2, 3, 0.4)');
+    });
+
+    it('updates an CSS4 color with the alpha value provided', () => {
+      expect(fade('color(display-p3 1 2 3)', 0.4)).to.equal('color(display-p3 1 2 3 /0.4)');
     });
 
     it('updates an rgba color with the alpha value provided', () => {
@@ -283,6 +334,14 @@ describe('utils/colorManipulator', () => {
     it("doesn't modify hsl colors when l is 0%", () => {
       expect(darken('hsl(0, 50%, 0%)', 0.5)).to.equal('hsl(0, 50%, 0%)');
     });
+
+    it('darkens CSS4 color red by 50% when coefficient is 0.5', () => {
+      expect(darken('color(display-p3 1 0 0)', 0.5)).to.equal('color(display-p3 0.5 0 0)');
+    });
+
+    it("doesn't modify CSS4 color when coefficient is 0", () => {
+      expect(darken('color(display-p3 1 0 0)', 0)).to.equal('color(display-p3 1 0 0)');
+    });
   });
 
   describe('lighten', () => {
@@ -336,6 +395,14 @@ describe('utils/colorManipulator', () => {
 
     it("doesn't modify hsl colors when `l` is 100%", () => {
       expect(lighten('hsl(0, 50%, 100%)', 0.5)).to.equal('hsl(0, 50%, 100%)');
+    });
+
+    it('lightens CSS4 color red by 50% when coefficient is 0.5', () => {
+      expect(lighten('color(display-p3 1 0 0)', 0.5)).to.equal('color(display-p3 1 0.5 0.5)');
+    });
+
+    it("doesn't modify CSS4 color when coefficient is 0", () => {
+      expect(lighten('color(display-p3 1 0 0)', 0)).to.equal('color(display-p3 1 0 0)');
     });
   });
 });
