abb125016ce23d34a9c3ba3885830393d8a3a3a5
Matt <github@nospam.33m.co>
2020-08-21 20:37:47 +0100

    [core] Updated config to use tab indents

--- prettier.config.js
+++ prettier.config.js
@@ -1,21 +1,22 @@
 module.exports = {
-  printWidth: 100,
-  singleQuote: true,
-  trailingComma: 'all',
-  overrides: [
-    {
-      files: '*.d.ts',
-      options: {
-        // This is needed for TypeScript 3.2 support
-        trailingComma: 'es5',
-      },
-    },
-    {
-      files: ['docs/**/*.md', 'docs/src/pages/**/*.{js,tsx}'],
-      options: {
-        // otherwise code blocks overflow on the docs website
-        printWidth: 80,
-      },
-    },
-  ],
+	printWidth: 100,
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+	overrides: [
+		{
+			files: '*.d.ts',
+			options: {
+				// This is needed for TypeScript 3.2 support
+				trailingComma: 'es5',
+			},
+		},
+		{
+			files: ['docs/**/*.md', 'docs/src/pages/**/*.{js,tsx}'],
+			options: {
+				// otherwise code blocks overflow on the docs website
+				printWidth: 80,
+			},
+		},
+	],
 };
