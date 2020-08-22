d1e46862de8ec803a5e2172f05c13f0e31eb6b5a
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-08-22 16:06:46 +0200

    [docs] Resolve .tsx first

--- docs/next.config.js
+++ docs/next.config.js
@@ -84,6 +84,14 @@ module.exports = {
     return {
       ...config,
       plugins,
+      resolve: {
+        ...config.resolve,
+        // resolve .tsx first
+        extensions: [
+          '.tsx',
+          ...config.resolve.extensions.filter((extension) => extension !== '.tsx'),
+        ],
+      },
       node: {
         fs: 'empty',
       },
