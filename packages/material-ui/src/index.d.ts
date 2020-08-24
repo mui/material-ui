c3d890ce484886c7b6a57339cab7aed64778b173
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-08-24 13:18:57 +0200

    Reintroduce implicit className/style for now

--- packages/material-ui/src/index.d.ts
+++ packages/material-ui/src/index.d.ts
@@ -31,7 +31,7 @@ export type StandardProps<C, ClassKey extends string, Removals extends keyof C =
 /**
  * @private ONLY USE FROM WITHIN mui-org/material-ui
  *
- * Helper type for internal components that are decorated with `withStyles`.
+ * Internal helper type for conform (describeConformance) components that are decorated with `withStyles
  * However, we don't declare classes on this type.
  * It is recommended to declare them manually with an interface so that each class can have a separate JSDOC.
  */
@@ -42,6 +42,9 @@ export type InternalStandardProps<C, Removals extends keyof C = never> = Omit<
   // each component declares it's classes in a separate interface for proper JSDOC
   StyledComponentProps<never> & {
     ref?: C extends { ref?: infer RefType } ? RefType : React.Ref<unknown>;
+    // TODO: Remove implicit props. Up to each component.
+    className?: string;
+    style?: React.CSSProperties;
   };
 
 export type PaletteType = 'light' | 'dark';
