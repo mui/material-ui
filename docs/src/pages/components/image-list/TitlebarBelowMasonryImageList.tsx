a12ccfaafb34628115967c76bb9a301a209ffff8
Matt <github@nospam.33m.co>
2020-09-08 00:03:02 +0100

    image order

--- docs/src/pages/components/image-list/TitlebarBelowMasonryImageList.tsx
+++ docs/src/pages/components/image-list/TitlebarBelowMasonryImageList.tsx
@@ -36,9 +36,9 @@ const itemData = [
     author: 'swabdesign',
   },
   {
-    img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
-    title: 'Kitchen',
-    author: 'Christian Mackie',
+    img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
+    title: 'Books',
+    author: 'Pavel Nekoranec',
   },
   {
     img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
@@ -46,19 +46,19 @@ const itemData = [
     author: 'Charles Deluvio',
   },
   {
-    img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
-    title: 'Books',
-    author: 'Pavel Nekoranec',
+    img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
+    title: 'Kitchen',
+    author: 'Christian Mackie',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
+    title: 'Blinds',
+    author: 'Darren Richardson',
   },
   {
     img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
     title: 'Chairs',
     author: 'Taylor Simpson',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
-    title: 'Candle',
-    author: 'Fi Bell',
   },
   {
     img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
@@ -81,13 +81,13 @@ const itemData = [
     author: 'Douglas Sheppard',
   },
   {
+    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
+    title: 'Candle',
+    author: 'Fi Bell',
+  },
+  {
     img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
     title: 'Coffee table',
     author: 'Hutomo Abrianto',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
-    title: 'Blinds',
-    author: 'Darren Richardson',
   },
 ];
