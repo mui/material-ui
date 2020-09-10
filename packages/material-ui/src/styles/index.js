c7d5c6620b4cfcc7c3de37973d2c751a984e03aa
Matt Hammond <32389848+madmanwithabike@users.noreply.github.com>
2020-09-10 23:57:24 +0100

    [theme] Allow spacing and border radius with CSS unit (#22530)

--- packages/material-ui/src/styles/index.js
+++ packages/material-ui/src/styles/index.js
@@ -1,8 +1,9 @@
+export { default as adaptV4Theme } from './adaptV4Theme';
 export * from './colorManipulator';
 export { default as createMuiTheme } from './createMuiTheme';
-export { default as adaptV4Theme } from './adaptV4Theme';
 export { default as unstable_createMuiStrictModeTheme } from './createMuiStrictModeTheme';
 export { default as createStyles } from './createStyles';
+export { getUnit as unstable_getUnit, toUnitless as unstable_toUnitless } from './cssUtils';
 export { default as makeStyles } from './makeStyles';
 export { default as responsiveFontSizes } from './responsiveFontSizes';
 export { default as styled } from './styled';
