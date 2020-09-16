c9438c2f650b3788fd13df1128e3bb5b5fc057a3
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-09-16 23:30:08 +0200

    fix demo

--- docs/src/pages/components/slider/CustomizedSlider.tsx
+++ docs/src/pages/components/slider/CustomizedSlider.tsx
@@ -204,6 +204,7 @@ export default function CustomizedSlider() {
       <div className={classes.margin} />
       <Typography gutterBottom>Tooltip value label</Typography>
       <Slider
+        valueLabelDisplay="auto"
         ValueLabelComponent={ValueLabelComponent}
         aria-label="custom thumb label"
         defaultValue={20}
