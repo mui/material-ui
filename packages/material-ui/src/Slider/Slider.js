0cf297b19fe2f1ed3d92f7d022a97fa3f1eceea9
Marija Najdova <mnajdova@gmail.com>
2020-09-16 09:59:13 +0200

    refactorings

--- packages/material-ui/src/Slider/Slider.js
+++ packages/material-ui/src/Slider/Slider.js
@@ -5,37 +5,7 @@ import { fade, lighten, darken } from '../styles/colorManipulator';
 import capitalize from '../utils/capitalize';
 import SliderBase from './SliderBase';
 import styled from '@material-ui/styled-engine';
-
-const getStyleOverrides = (name, theme) => {
-  let styleOverrides = {};
-
-  if (
-    theme &&
-    theme.components &&
-    theme.components[name] &&
-    theme.components[name].styleOverrides
-  ) {
-    styleOverrides = theme.components[name].styleOverrides;
-  }
-
-  return styleOverrides;
-};
-
-const getVariantStyles = (name, theme) => {
-  let variants = [];
-  if (theme && theme.components && theme.components[name] && theme.components[name].variants) {
-    variants = theme.components[name].variants;
-  }
-
-  const variantsStyles = {};
-
-  variants.forEach((definition) => {
-    const key = propsToClassKey(definition.props);
-    variantsStyles[key] = definition.style;
-  });
-
-  return variantsStyles;
-};
+import { getStyleOverrides, getVariantStyles } from '../styles/muiStyled';
 
 const overridesResolver = (props, styles, name) => {
   const {
