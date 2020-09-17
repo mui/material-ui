f4bba69afff58cab53124e710b6de563abb81951
Marija Najdova <mnajdova@gmail.com>
2020-09-17 08:27:23 +0200

    framer build

--- framer/Material-UI.framerfx/code/Slider.tsx
+++ framer/Material-UI.framerfx/code/Slider.tsx
@@ -3,14 +3,6 @@ import { addPropertyControls, ControlType } from 'framer';
 import MuiSlider from '@material-ui/core/Slider';
 
 interface Props {
-  color: 'primary' | 'secondary';
-  disabled: boolean;
-  max: number;
-  min: number;
-  orientation: 'horizontal' | 'vertical';
-  step: number;
-  track: 'inverted' | 'normal' | false;
-  valueLabelDisplay: 'auto' | 'off' | 'on';
   width: number | string;
   height: number;
 }
@@ -21,53 +13,10 @@ export function Slider(props: Props): JSX.Element {
 }
 
 Slider.defaultProps = {
-  color: 'primary' as 'primary',
-  disabled: false,
-  max: 100,
-  min: 0,
-  orientation: 'horizontal' as 'horizontal',
-  step: 1,
-  track: 'normal' as 'normal',
-  valueLabelDisplay: 'off' as 'off',
   width: 160,
   height: 24,
 };
 
 addPropertyControls(Slider, {
-  color: {
-    type: ControlType.Enum,
-    title: 'Color',
-    options: ['primary', 'secondary'],
-  },
-  disabled: {
-    type: ControlType.Boolean,
-    title: 'Disabled',
-  },
-  max: {
-    type: ControlType.Number,
-    title: 'Max',
-  },
-  min: {
-    type: ControlType.Number,
-    title: 'Min',
-  },
-  orientation: {
-    type: ControlType.Enum,
-    title: 'Orientation',
-    options: ['horizontal', 'vertical'],
-  },
-  step: {
-    type: ControlType.Number,
-    title: 'Step',
-  },
-  track: {
-    type: ControlType.Enum,
-    title: 'Track',
-    options: ['inverted', 'normal', false],
-  },
-  valueLabelDisplay: {
-    type: ControlType.Enum,
-    title: 'Value label display',
-    options: ['auto', 'off', 'on'],
-  },
+
 });
