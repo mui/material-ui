238f8a991d70048078189807270fb5a001f2b831
Daniel <daniel.elliott.lewis@gmail.com>
2020-09-21 23:31:07 +1000

    Update icons via `yarn src:download & yarn src:icons`

---
+++ packages/material-ui-icons/src/AlignVerticalCenterSharp.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import createSvgIcon from './utils/createSvgIcon';
+
+export default createSvgIcon(
+  <path d="M22 11h-5V6h-3v5h-4V3H7v8H1.84v2H7v8h3v-8h4v5h3v-5h5z" />
+, 'AlignVerticalCenterSharp');
---
+++ packages/material-ui-icons/src/VerticalDistributeRounded.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import createSvgIcon from './utils/createSvgIcon';
+
+export default createSvgIcon(
+  <path d="M22 3c0 .55-.45 1-1 1H3c-.55 0-1-.45-1-1s.45-1 1-1h18c.55 0 1 .45 1 1zM7 12c0 .83.67 1.5 1.5 1.5h7c.83 0 1.5-.67 1.5-1.5s-.67-1.5-1.5-1.5h-7c-.83 0-1.5.67-1.5 1.5zm-5 9c0 .55.45 1 1 1h18c.55 0 1-.45 1-1s-.45-1-1-1H3c-.55 0-1 .45-1 1z" />
+, 'VerticalDistributeRounded');
