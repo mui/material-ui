6dd874c5e67e6c2190933f556e9a4ecd1aa8dcbf
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-09-22 17:56:56 +0200

    [docs] Add live demo with DataGrid

--- docs/next.config.js
+++ docs/next.config.js
@@ -67,9 +67,11 @@ module.exports = {
 
       config.externals = [
         (context, request, callback) => {
-          const hasDependencyOnRepoPackages = ['notistack', '@material-ui/pickers'].includes(
-            request,
-          );
+          const hasDependencyOnRepoPackages = [
+            'notistack',
+            '@material-ui/pickers',
+            '@material-ui/data-grid',
+          ].includes(request);
 
           if (hasDependencyOnRepoPackages) {
             return callback(null);
@@ -96,7 +98,7 @@ module.exports = {
           // transpile 3rd party packages with dependencies in this repository
           {
             test: /\.(js|mjs|jsx)$/,
-            include: /node_modules(\/|\\)(notistack|@material-ui(\/|\\)pickers)/,
+            include: /node_modules(\/|\\)(notistack|@material-ui(\/|\\)(pickers|data-grid))/,
             use: {
               loader: 'babel-loader',
               options: {
