24d8ba88f91c2b19461e25ad01a63b14fd89a528
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-09-23 17:34:42 +0200

    add performance pages

---
+++ docs/pages/performance/slider-emotion.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import NoSsr from '@material-ui/core/NoSsr';
+import SliderStyled from '@material-ui/lab/SliderStyled';
+
+const data = {
+  name: 'Frozen yoghurt',
+  calories: 159,
+  fat: 6.0,
+  carbs: 24,
+  protein: 4.0,
+};
+
+const rows = Array.from(new Array(500)).map(() => data);
+
+export default function SliderEmotion() {
+  return (
+    <NoSsr defer>
+      <div style={{ width: 300 }}>
+        {rows.map((row, index) => (
+          <SliderStyled value={20} key={index} />
+        ))}
+      </div>
+    </NoSsr>
+  );
+}
