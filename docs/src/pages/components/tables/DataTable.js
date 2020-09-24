27cf73143c795cee6a5303104fa296d22835238e
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-09-24 13:32:25 +0200

    fix wrong name

--- docs/src/pages/components/tables/DataTable.js
+++ docs/src/pages/components/tables/DataTable.js
@@ -34,7 +34,7 @@ const rows = [
   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
-export default function DataGridDemo() {
+export default function DataTable() {
   return (
     <div style={{ height: 400, width: '100%' }}>
       <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
