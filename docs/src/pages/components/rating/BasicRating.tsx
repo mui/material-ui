a94c3d70849fcc43a20c1eab2c5a05d3ecb29a61
Matt <github@nospam.33m.co>
2020-09-25 18:53:48 +0100

    prettier

--- docs/src/pages/components/rating/BasicRating.tsx
+++ docs/src/pages/components/rating/BasicRating.tsx
@@ -19,20 +19,20 @@ export default function BasicRating() {
 
   return (
     <div className={classes.root}>
-        <Typography component="legend">Controlled</Typography>
-        <Rating
-          name="simple-controlled"
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
-        />
-        <Typography component="legend">Read only</Typography>
-        <Rating name="read-only" value={value} readOnly />
-        <Typography component="legend">Disabled</Typography>
-        <Rating name="disabled" value={value} disabled />
-        <Typography component="legend">No rating given</Typography>
-        <Rating name="no-value" value={null} />
+      <Typography component="legend">Controlled</Typography>
+      <Rating
+        name="simple-controlled"
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      />
+      <Typography component="legend">Read only</Typography>
+      <Rating name="read-only" value={value} readOnly />
+      <Typography component="legend">Disabled</Typography>
+      <Rating name="disabled" value={value} disabled />
+      <Typography component="legend">No rating given</Typography>
+      <Rating name="no-value" value={null} />
     </div>
   );
 }
--- docs/src/pages/components/rating/CustomizedRating.tsx
+++ docs/src/pages/components/rating/CustomizedRating.tsx
@@ -67,26 +67,26 @@ export default function CustomizedRating() {
 
   return (
     <div className={classes.root}>
-        <Typography component="legend">Custom icon and color</Typography>
-        <StyledRating
-          name="customized-color"
-          defaultValue={2}
-          getLabelText={(value: number) =>
-            `${value} Heart${value !== 1 ? 's' : ''}`
-          }
-          precision={0.5}
-          icon={<FavoriteIcon fontSize="inherit" />}
-          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
-        />
-        <Typography component="legend">10 stars</Typography>
-        <Rating name="customized-10" defaultValue={2} max={10} />
-        <Typography component="legend">Custom icon set</Typography>
-        <Rating
-          name="customized-icons"
-          defaultValue={2}
-          getLabelText={(value: number) => customIcons[value].label}
-          IconContainerComponent={IconContainer}
-        />
+      <Typography component="legend">Custom icon and color</Typography>
+      <StyledRating
+        name="customized-color"
+        defaultValue={2}
+        getLabelText={(value: number) =>
+          `${value} Heart${value !== 1 ? 's' : ''}`
+        }
+        precision={0.5}
+        icon={<FavoriteIcon fontSize="inherit" />}
+        emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+      />
+      <Typography component="legend">10 stars</Typography>
+      <Rating name="customized-10" defaultValue={2} max={10} />
+      <Typography component="legend">Custom icon set</Typography>
+      <Rating
+        name="customized-icons"
+        defaultValue={2}
+        getLabelText={(value: number) => customIcons[value].label}
+        IconContainerComponent={IconContainer}
+      />
     </div>
   );
 }
