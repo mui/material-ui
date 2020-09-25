42da0f9377575623a37c97f2e8db1155d3ef8e69
Matt <github@nospam.33m.co>
2020-09-25 18:01:20 +0100

    simplify BasicRating

--- docs/src/pages/components/rating/BasicRating.js
+++ docs/src/pages/components/rating/BasicRating.js
@@ -1,35 +1,36 @@
 import * as React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Rating from '@material-ui/core/Rating';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > legend': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
 
 export default function BasicRating() {
+  const classes = useStyles();
   const [value, setValue] = React.useState(2);
 
   return (
-    <div>
-      <Box component="fieldset" mb={3} borderColor="transparent">
-        <Typography component="legend">Controlled</Typography>
-        <Rating
-          name="simple-controlled"
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
-        />
-      </Box>
-      <Box component="fieldset" mb={3} borderColor="transparent">
-        <Typography component="legend">Read only</Typography>
-        <Rating name="read-only" value={value} readOnly />
-      </Box>
-      <Box component="fieldset" mb={3} borderColor="transparent">
-        <Typography component="legend">Disabled</Typography>
-        <Rating name="disabled" value={value} disabled />
-      </Box>
-      <Box component="fieldset" mb={3} borderColor="transparent">
-        <Typography component="legend">No rating given</Typography>
-        <Rating name="no-value" value={null} />
-      </Box>
+    <div className={classes.root}>
+      <Typography component="legend">Controlled</Typography>
+      <Rating
+        name="simple-controlled"
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      />
+      <Typography component="legend">Read only</Typography>
+      <Rating name="read-only" value={value} readOnly />
+      <Typography component="legend">Disabled</Typography>
+      <Rating name="disabled" value={value} disabled />
+      <Typography component="legend">No rating given</Typography>
+      <Rating name="no-value" value={null} />
     </div>
   );
 }
--- docs/src/pages/components/rating/BasicRating.tsx
+++ docs/src/pages/components/rating/BasicRating.tsx
@@ -1,14 +1,24 @@
 import * as React from 'react';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Rating from '@material-ui/core/Rating';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      '& > legend': {
+        marginTop: theme.spacing(2),
+      },
+    },
+  }),
+);
 
 export default function BasicRating() {
+  const classes = useStyles();
   const [value, setValue] = React.useState<number | null>(2);
 
   return (
-    <div>
-      <Box component="fieldset" mb={3} borderColor="transparent">
+    <div className={classes.root}>
         <Typography component="legend">Controlled</Typography>
         <Rating
           name="simple-controlled"
@@ -17,19 +27,12 @@ export default function BasicRating() {
             setValue(newValue);
           }}
         />
-      </Box>
-      <Box component="fieldset" mb={3} borderColor="transparent">
         <Typography component="legend">Read only</Typography>
         <Rating name="read-only" value={value} readOnly />
-      </Box>
-      <Box component="fieldset" mb={3} borderColor="transparent">
         <Typography component="legend">Disabled</Typography>
         <Rating name="disabled" value={value} disabled />
-      </Box>
-      <Box component="fieldset" mb={3} borderColor="transparent">
         <Typography component="legend">No rating given</Typography>
         <Rating name="no-value" value={null} />
-      </Box>
     </div>
   );
 }
