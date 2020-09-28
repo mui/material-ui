583d557105ac190c26fbd63a46d4b6d04242b0f1
Marija Najdova <mnajdova@gmail.com>
2020-09-28 09:41:34 +0200

    added example project

---
+++ examples/styled-components-as-styled-engine/src/demo.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Slider from "@material-ui/lab/SliderStyled";
+import { Typography } from "@material-ui/core";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Typography>Styled slider powered by styled-components</Typography>
+      <Slider
+        defaultValue={30}
+        aria-labelledby="primary-slider"
+        color="primary"
+      />
+      <Slider
+        defaultValue={30}
+        aria-labelledby="secondary-slider"
+        color="secondary"
+      />
+      <Slider defaultValue={30} disabled />
+      <Slider
+        defaultValue={30}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+        step={10}
+        marks
+        min={10}
+        max={110}
+      />
+    </div>
+  );
+}
---
+++ examples/styled-components-as-styled-engine/src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+import Demo from "./demo";
+
+ReactDOM.render(<Demo />, document.getElementById("app"));
