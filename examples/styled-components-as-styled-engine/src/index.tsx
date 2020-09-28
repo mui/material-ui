22e0700e99dd45d602d4cd1971311e7e7ddfaf7b
Marija Najdova <mnajdova@gmail.com>
2020-09-28 11:47:12 +0200

    prettier

--- examples/styled-components-as-styled-engine/src/index.tsx
+++ examples/styled-components-as-styled-engine/src/index.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
-import Demo from "./demo";
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Demo from './demo';
 
-ReactDOM.render(<Demo />, document.getElementById("app"));
+ReactDOM.render(<Demo />, document.getElementById('app'));
