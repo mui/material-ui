56876d8ae06b4bfc346736741c3583a07f628519
Marija Najdova <mnajdova@gmail.com>
2020-09-29 08:48:59 +0200

    removed unreferenced aria-labelledby

--- examples/create-react-app-with-styled-components-engine/src/App.js
+++ examples/create-react-app-with-styled-components-engine/src/App.js
@@ -6,12 +6,11 @@ export default function App() {
   return (
     <div>
       <Typography>Styled slider powered by styled-components</Typography>
-      <Slider defaultValue={30} aria-labelledby="primary-slider" color="primary" />
-      <Slider defaultValue={30} aria-labelledby="secondary-slider" color="secondary" />
+      <Slider defaultValue={30} color="primary" />
+      <Slider defaultValue={30} color="secondary" />
       <Slider defaultValue={30} disabled />
       <Slider
         defaultValue={30}
-        aria-labelledby="discrete-slider"
         valueLabelDisplay="auto"
         step={10}
         marks
