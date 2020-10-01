1e7339d610213ecc2f0c21ae69a1f005489d7478
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-10-01 14:11:34 +0200

    Fix failing initMatchers test

--- test/utils/initMatchers.test.js
+++ test/utils/initMatchers.test.js
@@ -44,12 +44,12 @@ describe('custom matchers', () => {
         'Could not match the following console.error calls. ' +
           "Make sure previous actions didn't call console.error by wrapping them in expect(() => {}).not.toErrorDev(): \n\n" +
           '  - "expected message"\n' +
-          '    at Context.<anonymous>',
+          '    at Context.', // `Context.it` in node 10.x, `Context.<anonymous>` in later node version
       );
       // check that the top stackframe points to this test
       // if this test is moved to another file the next assertion fails
       expect(caughtError.stack).to.match(
-        /- "expected message"\s+at Context\.<anonymous> \(.+\/initMatchers\.test\.js:\d+:\d+\)/,
+        /- "expected message"\s+at Context\.(<anonymous>|it) \(.+\/initMatchers\.test\.js:\d+:\d+\)/,
       );
     });
 
