a001588131f6609310834637640513e9ba9932c2
Marija Najdova <mnajdova@gmail.com>
2020-10-01 11:49:30 +0200

    fixed docs generation

--- packages/material-ui/src/styles/ThemeProvider.d.ts
+++ packages/material-ui/src/styles/ThemeProvider.d.ts
@@ -4,6 +4,15 @@ export interface ThemeProviderProps<Theme = DefaultTheme> {
   children?: React.ReactNode;
   theme: Partial<Theme> | ((outerTheme: Theme) => Theme);
 }
+
+/**
+ * This component takes a `theme` prop.
+ * It makes the `theme` available down the React tree thanks to React context.
+ * This component should preferably be used at **the root of your component tree**.
+ * API:
+ *
+ * - [ThemeProvider API](https://material-ui.com/api/theme-provider/)
+ */
 export default function ThemeProvider<T = DefaultTheme>(
   props: ThemeProviderProps<T>
 ): React.ReactElement<ThemeProviderProps<T>>;
