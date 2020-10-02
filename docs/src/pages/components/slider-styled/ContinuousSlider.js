74dc451854f9c751959059345203418ecfc687f1
Marija Najdova <mnajdova@gmail.com>
2020-10-02 14:35:02 +0200

    wip

--- docs/src/pages/components/slider-styled/ContinuousSlider.js
+++ docs/src/pages/components/slider-styled/ContinuousSlider.js
@@ -5,12 +5,18 @@ import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/lab/SliderStyled';
 import VolumeDown from '@material-ui/icons/VolumeDown';
 import VolumeUp from '@material-ui/icons/VolumeUp';
+import { ThemeProvider as SCThemeProvider } from 'styled-components';
+import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming';
+import { createMuiTheme } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles({
   root: {
     width: 200,
   },
 });
+
+const theme = createMuiTheme();
 
 export default function ContinuousSlider() {
   const classes = useStyles();
@@ -21,29 +27,66 @@ export default function ContinuousSlider() {
   };
 
   return (
-    <div className={classes.root}>
-      <Typography id="continuous-slider" gutterBottom>
-        Volume
-      </Typography>
-      <Grid container spacing={2}>
-        <Grid item>
-          <VolumeDown />
-        </Grid>
-        <Grid item xs>
-          <Slider
-            value={value}
-            onChange={handleChange}
-            aria-labelledby="continuous-slider"
-          />
-        </Grid>
-        <Grid item>
-          <VolumeUp />
-        </Grid>
-      </Grid>
-      <Typography id="disabled-slider" gutterBottom>
-        Disabled slider
-      </Typography>
-      <Slider disabled defaultValue={30} aria-labelledby="disabled-slider" />
-    </div>
+    <SCThemeProvider theme={theme}>
+      <EmotionThemeProvider theme={theme}>
+        <div className={classes.root}>
+          <Typography id="continuous-slider" gutterBottom>
+            Volume
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid item>
+              <VolumeDown />
+            </Grid>
+            <Grid item xs>
+              <Slider
+                value={value}
+                onChange={handleChange}
+                aria-labelledby="continuous-slider"
+              />
+            </Grid>
+            <Grid item>
+              <VolumeUp />
+            </Grid>
+          </Grid>
+          <Typography id="disabled-slider" gutterBottom>
+            Disabled slider
+          </Typography>
+          <Slider disabled defaultValue={30} aria-labelledby="disabled-slider" />
+          <Typography gutterBottom>
+            Regular Box
+          </Typography>
+          <Box component="span" p={3}>
+            <Slider
+              value={value}
+              onChange={handleChange}
+              aria-labelledby="continuous-slider"
+            />
+          </Box>
+          <Typography gutterBottom>
+            Cloned Box
+          </Typography>
+          <Box component="span" color="secondary.main" clone>
+            <Slider
+              value={value}
+              onChange={handleChange}
+              aria-labelledby="continuous-slider"
+            />
+          </Box>
+          <Typography gutterBottom>
+            Children as fn Box
+          </Typography>
+          <Box color="secondary.main">
+            {({className}) => (
+              <Slider
+                value={value}
+                onChange={handleChange}
+                aria-labelledby="continuous-slider"
+                className={className}
+              />
+            )}
+          </Box>
+        </div>
+      </EmotionThemeProvider>
+    </SCThemeProvider>
   );
 }
--- packages/material-ui/src/Box/Box.js
+++ packages/material-ui/src/Box/Box.js
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import {
   borders,
   compose,
@@ -12,7 +13,7 @@ import {
   typography,
   css,
 } from '@material-ui/system';
-import styled from '../styles/styled';
+import muiStyled from '../styles/muiStyled';
 
 export const styleFunction = css(
   compose(
@@ -29,9 +30,149 @@ export const styleFunction = css(
   ),
 );
 
+function omit(input, fields) {
+  const output = {};
+
+  Object.keys(input).forEach((prop) => {
+    if (fields.indexOf(prop) === -1) {
+      output[prop] = input[prop];
+    }
+  });
+
+  return output;
+}
+
+const boxProps = [
+  'border',
+  'borderTop',
+  'borderRight',
+  'borderBottom',
+  'borderLeft',
+  'borderColor',
+  'borderRadius',
+  'display',
+  'displayPrint',
+  'overflow',
+  'textOverflow',
+  'visibility',
+  'whiteSpace',
+  'flexBasis',
+  'flexDirection',
+  'flexWrap',
+  'justifyContent',
+  'alignItems',
+  'alignContent',
+  'order',
+  'flex',
+  'flexGrow',
+  'flexShrink',
+  'alignSelf',
+  'justifyItems',
+  'justifySelf',
+  'gridGap',
+  'gridColumnGap',
+  'gridRowGap',
+  'gridColumn',
+  'gridRow',
+  'gridAutoFlow',
+  'gridAutoColumns',
+  'gridAutoRows',
+  'gridTemplateColumns',
+  'gridTemplateRows',
+  'gridTemplateAreas',
+  'gridArea',
+  'zIndex',
+  'position',
+  'top',
+  'right',
+  'bottom',
+  'left',
+  'bgcolor',
+  'color',
+  'boxShadow',
+  'width',
+  'maxWidth',
+  'minWidth',
+  'height',
+  'maxHeight',
+  'minHeight',
+  'sizeWidth',
+  'sizeHeight',
+  'boxSizing',
+  'm',
+  'mt',
+  'mr',
+  'mb',
+  'ml',
+  'mx',
+  'my',
+  'p',
+  'pt',
+  'pr',
+  'pb',
+  'pl',
+  'px',
+  'py',
+  'margin',
+  'marginTop',
+  'marginRight',
+  'marginBottom',
+  'marginLeft',
+  'marginX',
+  'marginY',
+  'padding',
+  'paddingTop',
+  'paddingRight',
+  'paddingBottom',
+  'paddingLeft',
+  'paddingX',
+  'paddingY',
+  'fontFamily',
+  'fontSize',
+  'fontStyle',
+  'fontWeight',
+  'letterSpacing',
+  'lineHeight',
+  'textAlign',
+];
+
 /**
  * @ignore - do not document.
  */
-const Box = styled('div')(styleFunction, { name: 'MuiBox' });
+const BoxRoot = React.forwardRef(function StyledComponent(props, ref) {
+  const {
+    children,
+    clone,
+    className,
+    component: Component = "div",
+    ...other
+  } = props;
+
+  const spread = omit(other, boxProps);
+
+  if (clone) {
+    return React.cloneElement(children, {
+      className: clsx(children.prop?.className, className),
+      ...spread,
+    });
+  }
+
+  if (typeof children === 'function') {
+    return children({ className, ...spread });
+  }
+
+  return (
+    <Component ref={ref} className={className} {...spread}>
+      {children}
+    </Component>
+  );
+});
+
+const shouldForwardProp = prop => boxProps.indexOf(prop) === -1;
+
+/**
+ * @ignore - do not document.
+ */
+const Box = muiStyled(BoxRoot, { shouldForwardProp }, { muiName: 'MuiBox' })(styleFunction);
 
 export default Box;
