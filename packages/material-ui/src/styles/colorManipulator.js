99a42ce99c07296ce446a5908424fa69b5af90a2
Marija Najdova <mnajdova@gmail.com>
2020-10-03 13:25:50 +0200

    Update packages/material-ui/src/styles/colorManipulator.js
    Co-authored-by: Matt <github@nospam.33m.co>

--- packages/material-ui/src/styles/colorManipulator.js
+++ packages/material-ui/src/styles/colorManipulator.js
@@ -230,7 +230,7 @@ export function fade(color, value) {
 
 /**
  * Set the absolute transparency of a color.
- * Any existing alpha values are overwritten.
+ * Any existing alpha value is overwritten.
  *
  * @param {string} color - CSS color, i.e. one of: #nnn, #nnnnnn, rgb(), rgba(), hsl(), hsla()
  * @param {number} value - value to set the alpha channel to in the range 0 -1
