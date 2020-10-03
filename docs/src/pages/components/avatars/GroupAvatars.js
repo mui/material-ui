9ac8a2ac727b3e52cc2bd876c3de88f9a296d4a3
Jade Suntoro <jadesuntoro@Jades-MacBook-Air.local>
2020-10-03 09:39:45 +0800

    revert documentation example to original

--- docs/src/pages/components/avatars/GroupAvatars.js
+++ docs/src/pages/components/avatars/GroupAvatars.js
@@ -1,45 +1,15 @@
 import * as React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import AvatarGroup from '@material-ui/lab/AvatarGroup';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
-}));
-
 export default function GroupAvatars() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <AvatarGroup max={4}>
-        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-        <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-        <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-        <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
-        <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
-      </AvatarGroup>
-      <AvatarGroup max={4} variant="rounded">
-        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-        <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-        <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-        <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
-        <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
-      </AvatarGroup>
-      <AvatarGroup max={4} variant="square">
-        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-        <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-        <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-        <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
-        <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
-      </AvatarGroup>
-    </div>
+    <AvatarGroup max={4}>
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+      <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+      <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+      <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+    </AvatarGroup>
   );
 }
