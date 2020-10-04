e77b20edcc02120ef7db4ce7abd4c59ffe6e1a37
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-10-04 19:42:56 +0200

    fix ci

--- packages/material-ui/src/Hidden/Hidden.test.js
+++ packages/material-ui/src/Hidden/Hidden.test.js
@@ -9,7 +9,7 @@ describe('<Hidden />', () => {
 
   describe('prop: implementation', () => {
     it('should use HiddenJs by default', () => {
-      const { container } = render(<Hidden>{child}</Hidden>);
+      const { container } = render(<Hidden width="sm">{child}</Hidden>);
       // JS implementation doesn't requires wrapping <div />
       expect(container.firstChild).to.have.tagName('span');
     });
