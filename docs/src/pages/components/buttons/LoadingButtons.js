47fb1cfb3f1e59d367f893fd500468a9ddb209b3
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-10-04 19:43:09 +0200

    [docs] Document LoadingButton behavior

--- docs/src/pages/components/buttons/LoadingButtons.js
+++ docs/src/pages/components/buttons/LoadingButtons.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import LoadingButton from '@material-ui/lab/LoadingButton';
 import SaveIcon from '@material-ui/icons/Save';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    flexGrow: 1,
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'center',
     '& button': {
       margin: theme.spacing(1),
     },
@@ -14,22 +21,48 @@ const useStyles = makeStyles((theme) => ({
 export default function LoadingButtons() {
   const classes = useStyles();
 
+  const [pending, setPending] = React.useState(true);
+  function handleClick() {
+    setPending(true);
+  }
+
   return (
     <div className={classes.root}>
-      <LoadingButton variant="outlined" pending>
-        Submit
-      </LoadingButton>
-      <LoadingButton variant="outlined" pending pendingIndicator="Loading...">
-        Fetch data
-      </LoadingButton>
-      <LoadingButton
-        variant="outlined"
-        pending
-        pendingPosition="start"
-        startIcon={<SaveIcon />}
-      >
-        Save
-      </LoadingButton>
+      <FormControlLabel
+        control={
+          <Switch
+            checked={pending}
+            onChange={(event, checked) => setPending(checked)}
+          />
+        }
+        label="Pending"
+      />
+      <div className={classes.buttons}>
+        <LoadingButton
+          onClick={handleClick}
+          pending={pending}
+          variant="outlined"
+        >
+          Submit
+        </LoadingButton>
+        <LoadingButton
+          onClick={handleClick}
+          pending={pending}
+          pendingIndicator="Loading..."
+          variant="outlined"
+        >
+          Fetch data
+        </LoadingButton>
+        <LoadingButton
+          onClick={handleClick}
+          pending={pending}
+          pendingPosition="start"
+          startIcon={<SaveIcon />}
+          variant="outlined"
+        >
+          Save
+        </LoadingButton>
+      </div>
     </div>
   );
 }
