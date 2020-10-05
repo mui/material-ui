726f2bacfdbb7466c473172a8c7b3f5eab5bfe70
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-10-05 19:35:55 +0200

    [Button] Fix invalid type value (#22883)

--- packages/material-ui/src/Button/Button.test.js
+++ packages/material-ui/src/Button/Button.test.js
@@ -367,4 +367,14 @@ describe('<Button />', () => {
       expect(markup.text()).to.equal('Hello World');
     });
   });
+
+  it('should automatically change the button to an anchor element when href is provided', () => {
+    const { container } = render(<Button href="https://google.com">Hello</Button>);
+    const button = container.firstChild;
+
+    expect(button).to.have.property('nodeName', 'A');
+    expect(button).not.to.have.attribute('role');
+    expect(button).not.to.have.attribute('type');
+    expect(button).to.have.attribute('href', 'https://google.com');
+  });
 });
