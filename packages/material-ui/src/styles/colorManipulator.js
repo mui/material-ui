d6dbef31fd823a64a810555727a06f018dc31de8
Marija Najdova <mnajdova@gmail.com>
2020-10-05 10:00:20 +0200

    Update packages/material-ui/src/styles/colorManipulator.js
    Co-authored-by: Matt <github@nospam.33m.co>

--- packages/material-ui/src/styles/colorManipulator.js
+++ packages/material-ui/src/styles/colorManipulator.js
@@ -218,7 +218,7 @@ export function emphasize(color, coefficient = 0.15) {
  * Set the absolute transparency of a color.
  * Any existing alpha values are overwritten.
  * @param {string} color - CSS color, i.e. one of: #nnn, #nnnnnn, rgb(), rgba(), hsl(), hsla(), color()
- * @param {number} value - value to set the alpha channel to in the range 0 -1
+ * @param {number} value - value to set the alpha channel to in the range 0 - 1
  * @returns {string} A CSS color string. Hex input values are returned as rgb
  */
 export function alpha(color, value) {
