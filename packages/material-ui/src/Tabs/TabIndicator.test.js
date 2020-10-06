2e818756212c21146d56b73b4f279e41df0d8b4b
Ivan Semochkin <righiy90@gmail.com>
2020-10-06 17:02:31 +0300

    Revert changes for the TabIndicator

--- packages/material-ui/src/Tabs/TabIndicator.test.js
+++ packages/material-ui/src/Tabs/TabIndicator.test.js
@@ -1,44 +1,41 @@
 import * as React from 'react';
 import { expect } from 'chai';
-import { getClasses, createClientRender } from 'test/utils';
+import { createShallow, getClasses } from 'test/utils';
 import TabIndicator from './TabIndicator';
 
 describe('<TabIndicator />', () => {
-  const render = createClientRender();
+  let shallow;
   let classes;
   const defaultProps = {
     direction: 'left',
     orientation: 'horizontal',
     color: 'secondary',
   };
-  const style = { left: '1px', width: '2px' };
+  const style = { left: 1, width: 2 };
 
   before(() => {
+    shallow = createShallow({ dive: true });
     classes = getClasses(<TabIndicator {...defaultProps} />);
   });
 
   it('should render with the root class', () => {
-    const { container } = render(<TabIndicator {...defaultProps} />);
-
-    expect(container.firstChild).to.have.tagName('span');
-    expect(container.firstChild).to.have.class(classes.root);
+    const wrapper = shallow(<TabIndicator {...defaultProps} />);
+    expect(wrapper.name()).to.equal('span');
+    expect(wrapper.hasClass(classes.root)).to.equal(true);
   });
 
   describe('prop: style', () => {
     it('should be applied on the root element', () => {
-      const { container } = render(<TabIndicator {...defaultProps} style={style} />);
-
-      expect(container.firstChild.style).to.have.property('left', '1px');
-      expect(container.firstChild.style).to.have.property('width', '2px');
+      const wrapper = shallow(<TabIndicator {...defaultProps} style={style} />);
+      expect(wrapper.props().style).to.equal(style);
     });
   });
 
   describe('prop: className', () => {
     it('should append the className on the root element', () => {
-      const { container } = render(<TabIndicator {...defaultProps} className="foo" />);
-
-      expect(container.firstChild).to.have.tagName('span');
-      expect(container.firstChild).to.have.class('foo');
+      const wrapper = shallow(<TabIndicator {...defaultProps} className="foo" />);
+      expect(wrapper.name()).to.equal('span');
+      expect(wrapper.hasClass('foo')).to.equal(true);
     });
   });
 });
