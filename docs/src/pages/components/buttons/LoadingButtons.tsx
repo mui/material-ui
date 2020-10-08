22cf5d3b79809586b7c5caf077d19cacafbbe32f
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-10-08 11:29:05 +0200

    Showcase behavior in transition demo instead of in the visual demo

--- docs/src/pages/components/buttons/LoadingButtons.tsx
+++ docs/src/pages/components/buttons/LoadingButtons.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import LoadingButton from '@material-ui/lab/LoadingButton';
 import SaveIcon from '@material-ui/icons/Save';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
-  },
-  buttons: {
-    display: 'flex',
-    justifyContent: 'center',
     '& button': {
       margin: theme.spacing(1),
     },
@@ -21,48 +14,22 @@ const useStyles = makeStyles((theme) => ({
 export default function LoadingButtons() {
   const classes = useStyles();
 
-  const [pending, setPending] = React.useState(true);
-  function handleClick() {
-    setPending(true);
-  }
-
   return (
     <div className={classes.root}>
-      <FormControlLabel
-        control={
-          <Switch
-            checked={pending}
-            onChange={(event, checked) => setPending(checked)}
-          />
-        }
-        label="Pending"
-      />
-      <div className={classes.buttons}>
-        <LoadingButton
-          onClick={handleClick}
-          pending={pending}
-          variant="outlined"
-        >
-          Submit
-        </LoadingButton>
-        <LoadingButton
-          onClick={handleClick}
-          pending={pending}
-          pendingIndicator="Loading..."
-          variant="outlined"
-        >
-          Fetch data
-        </LoadingButton>
-        <LoadingButton
-          onClick={handleClick}
-          pending={pending}
-          pendingPosition="start"
-          startIcon={<SaveIcon />}
-          variant="outlined"
-        >
-          Save
-        </LoadingButton>
-      </div>
+      <LoadingButton pending variant="outlined">
+        Submit
+      </LoadingButton>
+      <LoadingButton pending pendingIndicator="Loading..." variant="outlined">
+        Fetch data
+      </LoadingButton>
+      <LoadingButton
+        pending
+        pendingPosition="start"
+        startIcon={<SaveIcon />}
+        variant="outlined"
+      >
+        Save
+      </LoadingButton>
     </div>
   );
 }
