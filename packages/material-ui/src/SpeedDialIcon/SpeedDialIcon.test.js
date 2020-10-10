b84503d6e6e53e187caf44a98a013a142b34ad1e
Jeferson S. Brito <30840709+jeferson-sb@users.noreply.github.com>
2020-10-10 13:10:53 -0300

    [test] Migrate SpeedDialIcon to react-testing-library (#22965)

--- packages/material-ui/src/SpeedDialIcon/SpeedDialIcon.test.js
+++ packages/material-ui/src/SpeedDialIcon/SpeedDialIcon.test.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { expect } from 'chai';
-import { getClasses, findOutermostIntrinsic, createMount, describeConformance } from 'test/utils';
+import { getClasses, createMount, createClientRender, describeConformance } from 'test/utils';
 import Icon from '@material-ui/core/Icon';
 import SpeedDialIcon from './SpeedDialIcon';
 
 describe('<SpeedDialIcon />', () => {
   const mount = createMount();
+  const render = createClientRender();
   let classes;
   const icon = <Icon>font_icon</Icon>;
 
@@ -22,57 +23,50 @@ describe('<SpeedDialIcon />', () => {
   }));
 
   it('should render the Add icon by default', () => {
-    const wrapper = mount(<SpeedDialIcon />);
-    expect(findOutermostIntrinsic(wrapper).find('svg[data-testid="AddIcon"]').length).to.equal(1);
+    const { getAllByTestId } = render(<SpeedDialIcon />);
+    expect(getAllByTestId('AddIcon').length).to.equal(1);
   });
 
   it('should render an Icon', () => {
-    const wrapper = mount(<SpeedDialIcon icon={icon} />);
-    const iconWrapper = findOutermostIntrinsic(wrapper).childAt(0);
-    expect(iconWrapper.find(Icon).length).to.equal(1);
+    const { container } = render(<SpeedDialIcon icon={icon} />);
+    expect(container.firstChild.querySelector('span')).to.have.class(classes.icon);
   });
 
   it('should render an openIcon', () => {
-    const wrapper = mount(<SpeedDialIcon openIcon={icon} />);
-    const iconWrapper = findOutermostIntrinsic(wrapper).childAt(0);
-    expect(iconWrapper.find(Icon).length).to.equal(1);
+    const { container } = render(<SpeedDialIcon openIcon={icon} />);
+    expect(container.firstChild.querySelector('span')).to.have.class(classes.openIcon);
   });
 
   it('should render the icon with the icon class', () => {
-    const wrapper = mount(<SpeedDialIcon />);
-    const iconWrapper = findOutermostIntrinsic(wrapper).childAt(0);
-    expect(iconWrapper.hasClass(classes.icon)).to.equal(true);
-    expect(iconWrapper.hasClass(classes.iconOpen)).to.equal(false);
-    expect(iconWrapper.hasClass(classes.iconWithOpenIconOpen)).to.equal(false);
+    const { container } = render(<SpeedDialIcon />);
+    expect(container.querySelector('svg')).to.have.class(classes.icon);
+    expect(container.querySelector('svg')).to.not.have.class(classes.iconOpen);
+    expect(container.querySelector('svg')).to.not.have.class(classes.iconWithOpenIconOpen);
   });
 
   it('should render the icon with the icon and iconOpen classes', () => {
-    const wrapper = mount(<SpeedDialIcon open />);
-    const iconWrapper = findOutermostIntrinsic(wrapper).childAt(0);
-    expect(iconWrapper.hasClass(classes.icon)).to.equal(true);
-    expect(iconWrapper.hasClass(classes.iconOpen)).to.equal(true);
-    expect(iconWrapper.hasClass(classes.iconWithOpenIconOpen)).to.equal(false);
+    const { container } = render(<SpeedDialIcon open />);
+    expect(container.querySelector('svg')).to.have.class(classes.icon);
+    expect(container.querySelector('svg')).to.have.class(classes.iconOpen);
+    expect(container.querySelector('svg')).to.not.have.class(classes.iconWithOpenIconOpen);
   });
 
   it('should render the icon with the icon, iconOpen iconWithOpenIconOpen classes', () => {
-    const wrapper = mount(<SpeedDialIcon open openIcon={icon} />);
-    const iconWrapper = findOutermostIntrinsic(wrapper).childAt(1);
-    expect(iconWrapper.hasClass(classes.icon)).to.equal(true);
-    expect(iconWrapper.hasClass(classes.iconOpen)).to.equal(true);
-    expect(iconWrapper.hasClass(classes.iconWithOpenIconOpen)).to.equal(true);
+    const { container } = render(<SpeedDialIcon open openIcon={icon} />);
+    expect(container.querySelector('svg')).to.have.class(classes.icon);
+    expect(container.querySelector('svg')).to.have.class(classes.iconOpen);
+    expect(container.querySelector('svg')).to.have.class(classes.iconWithOpenIconOpen);
   });
 
   it('should render the openIcon with the openIcon class', () => {
-    const wrapper = mount(<SpeedDialIcon openIcon={icon} />);
-    const iconWrapper = findOutermostIntrinsic(wrapper).childAt(0);
-    expect(iconWrapper.hasClass(classes.openIcon)).to.equal(true);
-    expect(iconWrapper.hasClass(classes.openIconOpen)).to.equal(false);
+    const { container } = render(<SpeedDialIcon openIcon={icon} />);
+    expect(container.firstChild.querySelector('span')).to.have.class(classes.openIcon);
+    expect(container.firstChild.querySelector('span')).to.not.have.class(classes.openIconOpen);
   });
 
   it('should render the openIcon with the openIcon, openIconOpen classes', () => {
-    const wrapper = mount(<SpeedDialIcon open openIcon={icon} />);
-    const iconWrapper = findOutermostIntrinsic(wrapper).childAt(0);
-    expect(iconWrapper.hasClass(classes.openIcon)).to.equal(true);
-    expect(iconWrapper.hasClass(classes.openIconOpen)).to.equal(true);
+    const { container } = render(<SpeedDialIcon openIcon={icon} open />);
+    expect(container.firstChild.querySelector('span')).to.have.class(classes.openIcon);
+    expect(container.firstChild.querySelector('span')).to.have.class(classes.openIconOpen);
   });
 });
