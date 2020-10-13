853523c59d7bcf9b5845f2d58a8b13eacf3145a5
Tubalevião <alvaro.prata.filho@gmail.com>
2020-10-13 00:29:32 -0400

    Fixing typo

--- packages/material-ui/src/useAutocomplete/useAutocomplete.js
+++ packages/material-ui/src/useAutocomplete/useAutocomplete.js
@@ -1008,7 +1008,7 @@ export default function useAutocomplete(props) {
         'data-option-index': index,
         'aria-disabled': disabled,
         'aria-selected': selected,
-        'data-focus': index ? false : true,
+        'data-focus': index ? 'false' : 'true',
       };
     },
     id,
