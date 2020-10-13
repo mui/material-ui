b81f58884f3bec7cf198e3597ed88b5c623d5259
Marija Najdova <mnajdova@gmail.com>
2020-10-13 11:48:31 +0200

    styled-components, emotion, global

--- docs/src/pages/guides/interoperability/EmotionTheme.tsx
+++ docs/src/pages/guides/interoperability/EmotionTheme.tsx
@@ -1,17 +1,17 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
-import Button from '@material-ui/core/Button';
-import { ThemeProvider } from 'emotion-theming';
+import Slider from '@material-ui/lab/SliderStyled';
 import {
   createMuiTheme,
   ThemeProvider as MuiThemeProvider,
   darken,
 } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 
 const customTheme = createMuiTheme({
   palette: {
     primary: {
-      main: '#6772e5',
+      main: '#20b2aa',
     },
   },
 });
@@ -19,30 +19,17 @@ const customTheme = createMuiTheme({
 export default function EmotionTheme() {
   return (
     <MuiThemeProvider theme={customTheme}>
-      <ThemeProvider theme={customTheme}>
-        <Button>Default</Button>
-        <Button
+      <Box width={300}>
+        <Slider
+          defaultValue={30}
           css={(theme) => css`
-            background-color: ${theme.palette.primary.main};
-            color: #fff;
-            box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11),
-              0 1px 3px rgba(0, 0, 0, 0.08);
-            padding: 4px 10px;
-            font-size: 13px;
-
-            &:hover {
-              background-color: ${darken(theme.palette.primary.main, 0.2)};
-            }
-
-            ${theme.breakpoints.up('sm')} {
-              font-size: 14px;
-              padding: 7px 14px;
+            color: ${theme.palette.primary.main};
+            :hover {
+              color: ${darken(theme.palette.primary.main, 0.2)};
             }
           `}
-        >
-          Customized
-        </Button>
-      </ThemeProvider>
+        />
+      </Box>
     </MuiThemeProvider>
   );
 }
