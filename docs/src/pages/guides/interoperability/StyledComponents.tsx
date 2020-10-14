1261e18fa7dff54bf8fb832fae6daf7991bb7f52
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-10-14 20:29:31 +0200

    [core] Lint CSS (#22976)

--- docs/src/pages/guides/interoperability/StyledComponents.tsx
+++ docs/src/pages/guides/interoperability/StyledComponents.tsx
@@ -8,6 +8,7 @@ const StyledButton = styled(Button)`
   color: #fff;
   box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
   padding: 7px 14px;
+
   &:hover {
     background-color: #5469d4;
   }
