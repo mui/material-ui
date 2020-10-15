dc34cbca2154b6da376e3ada52018fcf06bc6cd7
Lorenz Henk <lorenz.henk@cybertec.at>
2020-10-15 20:11:32 +0200

    Add demo for link underline

---
+++ docs/src/pages/components/links/UnderlineLink.js
@@ -0,0 +1,32 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > * + *': {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
+
+export default function Links() {
+  const classes = useStyles();
+  const preventDefault = (event) => event.preventDefault();
+
+  return (
+    <Typography className={classes.root}>
+      <Link href="#" onClick={preventDefault} underline="none">
+        {'underline="none"'}
+      </Link>
+      <Link href="#" onClick={preventDefault} underline="hover">
+        {'underline="hover"'}
+      </Link>
+      <Link href="#" onClick={preventDefault} underline="always">
+        {'underline="always"'}
+      </Link>
+    </Typography>
+  );
+}
