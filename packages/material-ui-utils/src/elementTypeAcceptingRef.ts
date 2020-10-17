7101993a78db68c848b139f096565b62f28d86f9
eps1lon <silbermann.sebastian@gmail.com>
2020-10-17 18:09:58 +0200

    [core] Don't warn about non-ref-forwarding components on the server

--- packages/material-ui-utils/src/elementTypeAcceptingRef.ts
+++ packages/material-ui-utils/src/elementTypeAcceptingRef.ts
@@ -18,7 +18,14 @@ function elementTypeAcceptingRef(
   const propValue = props[propName];
   const safePropName = propFullName || propName;
 
-  if (propValue == null) {
+  if (
+    propValue == null ||
+    // When server-side rendering React doesn't warn either.
+    // This is not an accurate check for SSR.
+    // This is only in place for emotion compat.
+    // TODO: Revisit once https://github.com/facebook/react/issues/20047 is resolved.
+    typeof window === 'undefined'
+  ) {
     return null;
   }
 
