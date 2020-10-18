3e7284c8e9b87ae56b1eb7651e9ad5941c812542
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-10-18 18:29:04 +0200

    [utils] Fix types of chainPropTypes

--- packages/material-ui-utils/src/chainPropTypes.ts
+++ packages/material-ui-utils/src/chainPropTypes.ts
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 
-export default function chainPropTypes(
-  propType1: PropTypes.Validator<unknown>,
-  propType2: PropTypes.Validator<unknown>,
-): PropTypes.Validator<unknown> {
+export default function chainPropTypes<A, B>(
+  propType1: PropTypes.Validator<A>,
+  propType2: PropTypes.Validator<B>,
+): PropTypes.Validator<A & B> {
   if (process.env.NODE_ENV === 'production') {
     return () => null;
   }
--- packages/material-ui/src/Grid/Grid.spec.tsx
+++ packages/material-ui/src/Grid/Grid.spec.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-function responsiveTest() {
+function ResponsiveTest() {
   <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square />;
 }
