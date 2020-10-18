61800cdee1a6a4281e9c371f14b0930bc09a29a6
Vikas Raj <vikasraj1911@gmail.com>
2020-10-19 03:19:32 +0530

    [examples] Updated nextjs-typescript example to use @material-ui/core@next (#23119)

--- examples/nextjs-with-typescript/pages/_app.tsx
+++ examples/nextjs-with-typescript/pages/_app.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { CacheProvider } from '@emotion/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import createCache from '@emotion/cache';
 import theme from '../src/theme';
+
+export const cache = createCache();
 
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
@@ -17,7 +21,7 @@ export default function MyApp(props: AppProps) {
   }, []);
 
   return (
-    <React.Fragment>
+    <CacheProvider value={cache}>
       <Head>
         <title>My page</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
@@ -27,6 +31,6 @@ export default function MyApp(props: AppProps) {
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
-    </React.Fragment>
+    </CacheProvider>
   );
 }
