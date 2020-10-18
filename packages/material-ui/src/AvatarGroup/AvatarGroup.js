dcfae5c5bbd0d7515bbe2647811ee376bfa858dd
Matt <github@nospam.33m.co>
2020-10-18 15:08:10 +0100

    fix import, docs:api

--- packages/material-ui/src/AvatarGroup/AvatarGroup.js
+++ packages/material-ui/src/AvatarGroup/AvatarGroup.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import { isFragment } from 'react-is';
 import clsx from 'clsx';
+import { chainPropTypes } from '@material-ui/utils';
 import { withStyles } from '../styles';
 import Avatar from '../Avatar';
-import { chainPropTypes } from '../utils';
 
 const SPACINGS = {
   small: -16,
