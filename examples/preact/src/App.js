881512a0b3edd01f282d4405f814c38b7f4e3dd6
Marija Najdova <mnajdova@gmail.com>
2020-10-19 16:05:33 +0200

    [examples] Update preact to use the @material-ui/core@next  (#23154)

--- examples/preact/src/App.js
+++ examples/preact/src/App.js
@@ -2,28 +2,15 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
 import ProTip from './ProTip';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import Copyright from './Copyright';
 
 export default function App() {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Preact v4-beta example
+          Preact v5-alpha example
         </Typography>
         <ProTip />
         <Copyright />
