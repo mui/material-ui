c382d44bc9f0ada66f854eac2e728963bb9db921
Marija Najdova <mnajdova@gmail.com>
2020-10-22 15:39:32 +0200

    refactored and added TODO

--- packages/material-ui-styled-engine-sc/src/index.js
+++ packages/material-ui-styled-engine-sc/src/index.js
@@ -1,15 +1,18 @@
 import scStyled from 'styled-components';
 
 export default function styled(tag, options) {
-  let scStyledPrepared = scStyled(tag);
+  let scStyledPrepared;
 
   if (options) {
     scStyledPrepared = scStyled(tag).withConfig({
       displayName: options.label,
       shouldForwardProp: options.shouldForwardProp,
     });
+  } else {
+    scStyledPrepared = scStyled(tag);
   }
 
+  // TODO: This should not be required once we solve the warning `You have illegal escape sequence in your template literal`
   return (styles) => {
     return scStyledPrepared(...styles);
   };
