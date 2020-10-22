ed4ef798e99d33d8504171361e9cb89db0d076aa
eps1lon <silbermann.sebastian@gmail.com>
2020-10-22 14:35:38 +0200

    Add warning to webpackBaseConfig to prevent over-drying

--- webpackBaseConfig.js
+++ webpackBaseConfig.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+// WARNING: Use this module only as an inspiration.
+// Cherry-pick the parts you need and inline them in the webpack.config you need.
 // This module isn't used to build the documentation. We use Next.js for that.
 // This module is used by the visual regression tests to run the demos and by eslint-plugin-import.
 module.exports = {
