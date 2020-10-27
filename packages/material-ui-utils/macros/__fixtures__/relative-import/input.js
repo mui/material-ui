c625415734ded91e0206afc8c23c24df0d9dc29b
eps1lon <silbermann.sebastian@gmail.com>
2020-10-27 11:21:18 +0100

    Add compat for usage in utils itself

---
+++ packages/material-ui-utils/macros/__fixtures__/relative-import/input.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-useless-path-segments -- macro requirement
+import MuiError from '../../../macros/MuiError.macro';
+
+throw new MuiError('Material-UI: Expected valid input target.\n' + 'Did you use `inputComponent`');
