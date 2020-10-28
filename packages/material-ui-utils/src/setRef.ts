623a89e192e92c5ce5d8cb5751355b2041db9ea9
Marija Najdova <mnajdova@gmail.com>
2020-10-28 19:24:07 +0100

    Update packages/material-ui-utils/src/setRef.ts
    Co-authored-by: Matt <github@nospam.33m.co>

--- packages/material-ui-utils/src/setRef.ts
+++ packages/material-ui-utils/src/setRef.ts
@@ -6,7 +6,7 @@ import * as React from 'react';
  * passes {value} to {ref}
  *
  * WARNING: Be sure to only call this inside a callback that is passed as a ref.
- * Otherwise make sure to cleanup previous {ref} if it changes. See
+ * Otherwise, make sure to cleanup the previous {ref} if it changes. See
  * https://github.com/mui-org/material-ui/issues/13539
  *
  * useful if you want to expose the ref of an inner component to the public api
