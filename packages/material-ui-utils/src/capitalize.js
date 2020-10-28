c9e7ba0be0a510dde62b997c89730a1a31dc3712
Marija Najdova <mnajdova@gmail.com>
2020-10-28 19:21:00 +0100

    Update packages/material-ui-utils/src/capitalize.js
    Co-authored-by: Matt <github@nospam.33m.co>

--- packages/material-ui-utils/src/capitalize.js
+++ packages/material-ui-utils/src/capitalize.js
@@ -1,7 +1,7 @@
 import MuiError from '../macros/MuiError.macro';
 // It should to be noted that this function isn't equivalent to `text-transform: capitalize`.
 //
-// A strict capitalization should uppercase the first letter of each word a the sentence.
+// A strict capitalization should uppercase the first letter of each word in the sentence.
 // We only handle the first word.
 export default function capitalize(string) {
   if (typeof string !== 'string') {
