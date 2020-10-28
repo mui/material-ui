e0bc92167885df6b488711a136e0ad4cb3208817
Marija Najdova <mnajdova@gmail.com>
2020-10-28 19:25:17 +0100

    Update packages/material-ui-utils/src/setRef.ts
    Co-authored-by: Matt <github@nospam.33m.co>

--- packages/material-ui-utils/src/setRef.ts
+++ packages/material-ui-utils/src/setRef.ts
@@ -11,7 +11,7 @@ import * as React from 'react';
  *
  * Useful if you want to expose the ref of an inner component to the public API
  * while still using it inside the component.
- * @param ref a ref callback or ref object if anything falsy this is a no-op
+ * @param ref A ref callback or ref object. If anything falsy, this is a no-op.
  */
 export default function setRef<T>(
   ref: React.MutableRefObject<T | null> | ((instance: T | null) => void) | null | undefined,
