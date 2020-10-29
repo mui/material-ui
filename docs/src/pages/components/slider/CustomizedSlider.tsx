43711300500db3e5d8f8122309dc341184b7bcfc
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-10-29 22:17:07 +0100

    increase CSS specificity :/

--- docs/src/pages/components/slider/CustomizedSlider.tsx
+++ docs/src/pages/components/slider/CustomizedSlider.tsx
@@ -73,8 +73,10 @@ const IOSSlider = styled(Slider)({
   '& .MuiSlider-valueLabel': {
     left: 'calc(-50% + 12px)',
     top: -22,
-    '& *': {
+    '& .MuiSlider-valueLabelCircle': {
       background: 'transparent',
+    },
+    '& .MuiSlider-valueLabelLabel': {
       color: '#000',
     },
   },
