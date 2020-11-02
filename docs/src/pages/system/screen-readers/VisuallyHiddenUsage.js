538939b4acbd3cb0f55f185aad9525e917345253
Marija Najdova <mnajdova@gmail.com>
2020-11-02 11:44:49 +0100

    visuallyHidden & shadows examples converted

--- docs/src/pages/system/screen-readers/VisuallyHiddenUsage.js
+++ docs/src/pages/system/screen-readers/VisuallyHiddenUsage.js
@@ -1,21 +1,16 @@
 import * as React from 'react';
 import Link from '@material-ui/core/Link';
 import { visuallyHidden } from '@material-ui/system';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-  // TODO fix #20379.
-  span: visuallyHidden,
-});
+import Box from '@material-ui/core/Box';
 
 export default function VisuallyHiddenUsage() {
-  const classes = useStyles();
-
   return (
     <Link href="#foo">
       Read more
       {/* always visually hidden because the parent is focusable element */}
-      <span className={classes.span}>about how to visually hide elements</span>
+      <Box component="span" sx={{ ...visuallyHidden }}>
+        about how to visually hide elements
+      </Box>
     </Link>
   );
 }
