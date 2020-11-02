f447fd88a8766f055c4b6e074f91acbe54698f35
Marija Najdova <mnajdova@gmail.com>
2020-11-02 18:29:47 +0100

    Update docs/scripts/buildApi.ts

--- docs/scripts/buildApi.ts
+++ docs/scripts/buildApi.ts
@@ -134,7 +134,7 @@ async function annotateComponentDefinition(context: {
       }
 
       const { leadingComments } = node;
-      const jsdocBlock = leadingComments !== null ? leadingComments[0] : null;
+      const jsdocBlock = leadingComments != null ? leadingComments[0] : null;
       if (leadingComments !== null && leadingComments.length > 1) {
         throw new Error('Should only have a single leading jsdoc block');
       }
