81910b42ed26f58e0a4d15836d5c6388e9548c41
Marija Najdova <mnajdova@gmail.com>
2020-11-02 16:25:56 +0100

    buildApi CSS changes wip

--- docs/scripts/buildApi.ts
+++ docs/scripts/buildApi.ts
@@ -192,6 +192,71 @@ async function annotateComponentDefinition(context: {
   writeFileSync(typesFilename, typesSourceNew, { encoding: 'utf8' });
 }
 
+function trimComment(comment: string) {
+  let i = 0;
+  for (; i < comment.length; i++) {
+    if (comment[i] !== '*' && comment[i] !== ' ') {
+      break;
+    }
+  }
+  return comment.substr(i, comment.length - 1);
+}
+
+async function updateStylesDefinition(context: { api: ReactApi; component: { filename: string } }) {
+  const { api, component } = context;
+
+  const typesFilename = component.filename.replace(/\.js$/, '.d.ts');
+  const typesSource = readFileSync(typesFilename, { encoding: 'utf8' });
+  const typesAST = await babel.parseAsync(typesSource, {
+    configFile: false,
+    filename: typesFilename,
+    presets: [require.resolve('@babel/preset-typescript')],
+  });
+  if (typesAST === null) {
+    throw new Error('No AST returned from babel.');
+  }
+
+  if (api.styles.classes.length === 0) {
+    const parts = component.filename.split('\\');
+    const componentName = parts[parts.length - 1].replace(/\.js$/, '');
+
+    typesAST.program.body.forEach((node) => {
+      const name = node.type === 'ExportNamedDeclaration' ? node?.declaration?.id?.name : undefined;
+      if (name === `${componentName}ClassKey` && node.declaration.typeAnnotation.types) {
+        const classes = node.declaration.typeAnnotation.types.map(
+          (node: babel.types.TSLiteralType) => node.literal.value,
+        );
+
+        const nodeLeadingComments = node.declaration.typeAnnotation.leadingComments || [];
+
+        node.declaration.typeAnnotation.types.forEach(
+          (typeNode: babel.types.TSLiteralType, idx: number) => {
+            let leadingComments = typeNode.leadingComments;
+            if (idx === 0) {
+              if (leadingComments) {
+                leadingComments = leadingComments.concat(nodeLeadingComments);
+              } else {
+                leadingComments = nodeLeadingComments;
+              }
+            }
+            if (leadingComments) {
+              for (let i = 0; i < leadingComments.length; i++) {
+                if (leadingComments[i].end + 6 === typeNode.literal.start) {
+                  api.styles.descriptions[typeNode.literal.value as string] = trimComment(
+                    leadingComments[i].value,
+                  );
+                }
+              }
+            }
+            return '';
+          },
+        );
+        api.styles.classes = classes;
+      }
+    });
+  }
+}
+
 async function annotateClassesDefinition(context: {
   api: ReactApi;
   component: { filename: string };
@@ -227,7 +292,6 @@ async function annotateClassesDefinition(context: {
       }
     },
   });
-
   if (end === null || start === 0) {
     // Some components actually don't implement this prop.
     return;
@@ -379,6 +443,11 @@ async function buildDocs(options: {
   // Relative location in the file system.
   reactAPI.filename = componentObject.filename.replace(workspaceRoot, '');
   reactAPI.inheritance = getInheritance(testInfo, src);
+
+  await updateStylesDefinition({
+    api: reactAPI,
+    component: componentObject,
+  });
 
   let markdown;
   try {
