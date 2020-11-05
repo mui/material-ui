28eb893e8d31e2efd22160ece4edee401d63e640
Marija Najdova <mnajdova@gmail.com>
2020-11-05 20:43:28 +0100

    formatted

--- docs/src/pages/system/basics/BreakpointsAsObject.js
+++ docs/src/pages/system/basics/BreakpointsAsObject.js
@@ -3,18 +3,10 @@ import Box from '@material-ui/core/Box';
 
 export default function BreakpointsAsObject() {
   return (
-    <Box
-      sx={{
-        width: {
-          sx: 100,
-          sm: 200,
-          md: 300,
-          lg: 400,
-          xl: 500,
-        },
-      }}
-    >
-      This box has a responsive width.
-    </Box>
+    <div>
+      <Box sx={{ width: { sx: 100, sm: 200, md: 300, lg: 400, xl: 500 } }}>
+        This box has a responsive width.
+      </Box>
+    </div>
   );
 }
--- docs/src/pages/system/basics/ValueAsFunction.js
+++ docs/src/pages/system/basics/ValueAsFunction.js
@@ -3,14 +3,16 @@ import Box from '@material-ui/core/Box';
 
 export default function ValueAsFunction() {
   return (
-    <Box
-      sx={{
-        p: 1,
-        border: 1,
-        borderColor: (theme) => theme.palette.primary.main,
-      }}
-    >
-      Border color with theme value.
-    </Box>
+    <div>
+      <Box
+        sx={{
+          p: 1,
+          border: 1,
+          borderColor: (theme) => theme.palette.primary.main,
+        }}
+      >
+        Border color with theme value.
+      </Box>
+    </div>
   );
 }
