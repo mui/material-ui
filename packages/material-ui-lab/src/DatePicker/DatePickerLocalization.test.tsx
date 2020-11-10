d071640c78776529b22868d009521addf39519de
eps1lon <silbermann.sebastian@gmail.com>
2020-11-10 23:30:27 +0100

    Revert previous changes

--- packages/material-ui-lab/src/DatePicker/DatePickerLocalization.test.tsx
+++ packages/material-ui-lab/src/DatePicker/DatePickerLocalization.test.tsx
@@ -15,17 +15,15 @@ describe('<DatePicker /> localization', () => {
   // TODO
   // eslint-disable-next-line mocha/no-skipped-tests
   it.skip('datePicker localized format for year view', () => {
-    const value = adapterToUse.date('2018-01-01T00:00:00.000');
     render(
       <MobileDatePicker
         renderInput={(params) => <TextField {...params} />}
-        value={value}
+        value={adapterToUse.date('2018-01-01T00:00:00.000')}
         onChange={() => {}}
         views={['year']}
       />,
     );
 
-    expect(screen.getByRole('textbox')).to.have.value(value.getFullYear().toString());
     expect(screen.getByRole('textbox')).to.have.value('2018');
 
     fireEvent.click(screen.getByLabelText(/Choose date/));
