c505ebbc8622ba2032b464c8cbc40439560c77fe
Matt <github@nospam.33m.co>
2020-11-11 16:01:50 +0000

    Use getInitialProps for now

--- docs/pages/api-docs/accordion.js
+++ docs/pages/api-docs/accordion.js
@@ -3,7 +3,11 @@ import ApiDocs from 'docs/src/modules/components/ApiDocs';
 import mapApiTranslations, { parsePropsMarkdown } from 'docs/src/modules/utils/mapApiTranslations';
 import jsonPageContent from './accordion.json';
 
-export async function getStaticProps() {
+export default function Page({ pageContent }) {
+  return <ApiDocs pageContent={pageContent} />;
+}
+
+Page.getInitialProps = async (ctx) => {
   const req1 = require.context('docs/translations', false, /component-descriptions.*.json$/);
   const req2 = require.context('docs/translations', false, /prop-descriptions.*.json$/);
   const req3 = require.context('docs/translations', false, /class-descriptions.*.json$/);
@@ -19,11 +23,6 @@ export async function getStaticProps() {
     classDescriptions,
   };
 
-  return {
-    props: { pageContent },
-  };
+  return { pageContent };
 }
 
-export default function Page({ pageContent }) {
-  return <ApiDocs pageContent={pageContent} />;
-}
