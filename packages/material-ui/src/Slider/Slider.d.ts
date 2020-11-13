6e76e8c57961475f0655a4d6784b09cd5bc4f1af
Marija Najdova <mnajdova@gmail.com>
2020-11-13 13:42:14 +0100

    fixes

--- packages/material-ui/src/Slider/Slider.d.ts
+++ packages/material-ui/src/Slider/Slider.d.ts
@@ -17,7 +17,20 @@ export type SliderTypeMap<
 }>;
 
 type SliderRootProps = NonNullable<SliderTypeMap['props']['componentsProps']>['root'];
+type SliderMarkProps = NonNullable<SliderTypeMap['props']['componentsProps']>['mark'];
+type SliderMarkLabelProps = NonNullable<SliderTypeMap['props']['componentsProps']>['markLabel'];
+type SliderRailProps = NonNullable<SliderTypeMap['props']['componentsProps']>['rail'];
+type SliderTrackProps = NonNullable<SliderTypeMap['props']['componentsProps']>['track'];
+type SliderThumbProps = NonNullable<SliderTypeMap['props']['componentsProps']>['thumb'];
+type SliderValueLabel = NonNullable<SliderTypeMap['props']['componentsProps']>['valueLabel'];
+
 export const SliderRoot: React.FC<SliderRootProps>;
+export const SliderMark: React.FC<SliderMarkProps>;
+export const SliderMarkLabel: React.FC<SliderMarkLabelProps>;
+export const SliderRail: React.FC<SliderRailProps>;
+export const SliderTrack: React.FC<SliderTrackProps>;
+export const SliderThumb: React.FC<SliderThumbProps>;
+export const SliderValueLabel: React.FC<SliderValueLabel>;
 
 /**
  *
