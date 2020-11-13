558d25fd6cebeb11e291a2db870269c6ac3aacd2
Marija Najdova <mnajdova@gmail.com>
2020-11-13 23:22:01 +0100

    [Box] Deprecate css prop in favor of sx (#23480)

--- packages/material-ui/src/Box/Box.js
+++ packages/material-ui/src/Box/Box.js
@@ -10,11 +10,11 @@ import {
   sizing,
   spacing,
   typography,
-  css,
+  styleFunctionSx,
 } from '@material-ui/system';
 import styled from '../styles/styled';
 
-export const styleFunction = css(
+export const styleFunction = styleFunctionSx(
   compose(
     borders,
     display,
