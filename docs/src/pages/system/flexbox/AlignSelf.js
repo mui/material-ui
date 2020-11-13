e7ad1bc01b0d2857d60fb490b7f5fd3c096a51c7
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-11-14 00:05:33 +0100

    [docs] Improve formatting of the system

--- docs/src/pages/system/flexbox/AlignSelf.js
+++ docs/src/pages/system/flexbox/AlignSelf.js
@@ -15,9 +15,7 @@ export default function AlignSelf() {
         }}
       >
         <Box sx={{ p: 1, bgcolor: 'grey.300' }}>Item 1</Box>
-        <Box sx={{ p: 1, bgcolor: 'grey.300', alignSelf: 'flex-end' }}>
-          Item 2
-        </Box>
+        <Box sx={{ p: 1, bgcolor: 'grey.300', alignSelf: 'flex-end' }}>Item 2</Box>
         <Box sx={{ p: 1, bgcolor: 'grey.300' }}>Item 3</Box>
       </Box>
     </div>
--- docs/src/pages/system/typography/TextAlignment.js
+++ docs/src/pages/system/typography/TextAlignment.js
@@ -6,8 +6,8 @@ export default function TextAlignment() {
   return (
     <Typography component="div">
       <Box sx={{ textAlign: 'justify', m: 1 }}>
-        Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit
-        amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus.
+        Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet
+        fermentum. Donec sed odio operae, eu vulputate felis rhoncus.
       </Box>
       <Box sx={{ textAlign: 'left', m: 1 }}>Left aligned text.</Box>
       <Box sx={{ textAlign: 'center', m: 1 }}>Center aligned text.</Box>
