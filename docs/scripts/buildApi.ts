d40375a30b57cb5e2dfaf94ee6497a856d84e5f1
Matt <github@nospam.33m.co>
2020-11-15 15:04:48 +0000

    better <code> replacement

--- docs/scripts/buildApi.ts
+++ docs/scripts/buildApi.ts
@@ -337,7 +337,7 @@ function extractClassConditions(classDescriptions: { [key: string]: { [key: stri
         if (conditions) {
           classConditions[componentName][className] = {
             description: classDescription.replace(stylesRegex, '$1{{conditions}}.'),
-            conditions: conditions[2].replace('`', '<code>').replace('`', '</code>'),
+            conditions: conditions[2].replace(/`(.*?)`/g, '<code>$1</code>'),
           };
         } else {
           classConditions[componentName][className] = { description: classDescription };
