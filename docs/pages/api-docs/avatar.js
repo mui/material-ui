7fbaefa35812340e2e674d078308966cb3be523f
Matt <github@nospam.33m.co>
2020-11-16 21:14:02 +0000

    [docs] API pages i18n (#23214)

--- docs/pages/api-docs/avatar.js
+++ docs/pages/api-docs/avatar.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
-import { prepareMarkdown } from 'docs/src/modules/utils/parseMarkdown';
+import ApiDocs from 'docs/src/modules/components/ApiDocs';
+import mapApiTranslations, { parsePropsMarkdown } from 'docs/src/modules/utils/mapApiTranslations';
+import jsonPageContent from './avatar.json';
 
-const pageFilename = 'api/avatar';
-const requireRaw = require.context('!raw-loader!./', false, /\/avatar\.md$/);
-
-export default function Page({ docs }) {
-  return <MarkdownDocs docs={docs} />;
+export default function Page({ pageContent }) {
+  return <ApiDocs pageContent={pageContent} />;
 }
 
-Page.getInitialProps = () => {
-  const { demos, docs } = prepareMarkdown({ pageFilename, requireRaw });
-  return { demos, docs };
+Page.getInitialProps = async () => {
+  const req1 = require.context('docs/translations', false, /component-descriptions.*.json$/);
+  const req2 = require.context('docs/translations', false, /prop-descriptions.*.json$/);
+  const req3 = require.context('docs/translations', false, /class-descriptions.*.json$/);
+
+  const componentDescription = mapApiTranslations(req1, 'Avatar');
+  const propDescriptions = parsePropsMarkdown(mapApiTranslations(req2, 'Avatar'));
+  const classDescriptions = mapApiTranslations(req3, 'Avatar');
+
+  const pageContent = {
+    ...jsonPageContent,
+    componentDescription,
+    propDescriptions,
+    classDescriptions,
+  };
+
+  return { pageContent };
 };
--- docs/pages/api-docs/button-base.js
+++ docs/pages/api-docs/button-base.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
-import { prepareMarkdown } from 'docs/src/modules/utils/parseMarkdown';
+import ApiDocs from 'docs/src/modules/components/ApiDocs';
+import mapApiTranslations, { parsePropsMarkdown } from 'docs/src/modules/utils/mapApiTranslations';
+import jsonPageContent from './button-base.json';
 
-const pageFilename = 'api/button-base';
-const requireRaw = require.context('!raw-loader!./', false, /\/button-base\.md$/);
-
-export default function Page({ docs }) {
-  return <MarkdownDocs docs={docs} />;
+export default function Page({ pageContent }) {
+  return <ApiDocs pageContent={pageContent} />;
 }
 
-Page.getInitialProps = () => {
-  const { demos, docs } = prepareMarkdown({ pageFilename, requireRaw });
-  return { demos, docs };
+Page.getInitialProps = async () => {
+  const req1 = require.context('docs/translations', false, /component-descriptions.*.json$/);
+  const req2 = require.context('docs/translations', false, /prop-descriptions.*.json$/);
+  const req3 = require.context('docs/translations', false, /class-descriptions.*.json$/);
+
+  const componentDescription = mapApiTranslations(req1, 'ButtonBase');
+  const propDescriptions = parsePropsMarkdown(mapApiTranslations(req2, 'ButtonBase'));
+  const classDescriptions = mapApiTranslations(req3, 'ButtonBase');
+
+  const pageContent = {
+    ...jsonPageContent,
+    componentDescription,
+    propDescriptions,
+    classDescriptions,
+  };
+
+  return { pageContent };
 };
