654419efe0aae027bdb6c27e177c7ba1118b3375
Marija Najdova <mnajdova@gmail.com>
2020-11-18 11:14:07 +0100

    broke circular dep

--- packages/material-ui-unstyled/src/SliderUnstyled/sliderClasses.ts
+++ packages/material-ui-unstyled/src/SliderUnstyled/sliderClasses.ts
@@ -1,19 +1,35 @@
-import { SliderUnstyledTypeMap } from './SliderUnstyled';
+interface SliderClasses {
+  root: string;
+  active: string;
+  focusVisible: string;
+  colorPrimary: string;
+  colorSecondary: string;
+  disabled: string;
+  marked: string;
+  vertical: string;
+  trackInverted: string;
+  trackFalse: string;
+  rail: string;
+  track: string;
+  mark: string;
+  markActive: string;
+  markLabel: string;
+  markLabelActive: string;
+  thumb: string;
+  thumbPrimary: string;
+  thumbSecondary: string;
+  valueLabel: string;
+  valueLabelOffset: string;
+  valueLabelOpen: string;
+  valueLabelCircle: string;
+  valueLabelLabel: string;
+}
 
 const getUtilityClass = (name: string): string => {
   return `MuiSlider-${name}`;
 };
 
-const sliderClasses: NonNullable<
-  SliderUnstyledTypeMap['props']['classes'] & {
-    thumbPrimary: string;
-    thumbSecondary: string;
-    valueLabelOffset: string;
-    valueLabelOpen: string;
-    valueLabelCircle: string;
-    valueLabelLabel: string;
-  }
-> = {
+const sliderClasses: SliderClasses = {
   root: getUtilityClass('root'),
   active: 'Mui-active',
   focusVisible: 'Mui-focusVisible',
