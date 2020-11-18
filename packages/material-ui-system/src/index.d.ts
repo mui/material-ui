82532dfa43845e8e27c4457b1ad58ab17b196af1
Marija Najdova <mnajdova@gmail.com>
2020-11-18 20:22:31 +0100

    Update packages/material-ui-system/src/index.d.ts
    Co-authored-by: Matt <github@nospam.33m.co>

--- packages/material-ui-system/src/index.d.ts
+++ packages/material-ui-system/src/index.d.ts
@@ -289,7 +289,7 @@ export interface CSSProperties
 export type CSSPseudoSelectorProps = { [K in CSS.Pseudos]?: SystemStyleObject };
 
 /**
- * CSS as a plain object that is compatible with CSS-in-JS libaries.
+ * CSS as a plain object that is compatible with CSS-in-JS libraries.
  * Copied directly from [emotion](https://github.com/emotion-js/emotion/blob/ca3ad1c1dcabf78a95b55cc2dc94cad1998a3196/packages/serialize/types/index.d.ts#L45) types
  */
 export interface CSSObject
