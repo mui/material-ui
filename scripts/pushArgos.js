ca7b8728dd47c278089f644670809cc8e5d2999a
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-11-18 23:40:18 +0100

    Update scripts/pushArgos.js

--- scripts/pushArgos.js
+++ scripts/pushArgos.js
@@ -23,7 +23,7 @@ const screenshotsBase = 'test/regressions/screenshots/chrome';
 
 async function run() {
   let screenshots = await glob(`${screenshotsBase}/**/*`);
-  screenshots = chunk(screenshots, Math.ceil(screenshots.length / 3));
+  screenshots = chunk(screenshots, Math.ceil(screenshots.length / 2));
 
   await Promise.all(
     screenshots.map((chunks, chunkIndex) =>
