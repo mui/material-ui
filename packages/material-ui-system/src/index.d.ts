a9685d6972a6b1fe0b101e8567c49829f926c9c5
Marija Najdova <mnajdova@gmail.com>
2020-11-18 16:36:38 +0100

    comments

--- packages/material-ui-system/src/index.d.ts
+++ packages/material-ui-system/src/index.d.ts
@@ -295,7 +295,7 @@ export interface CSSProperties
 export type CSSPseudoSelectorProps = { [K in CSS.Pseudos]?: SystemStyleObject };
 
 /**
- * CSS as POJO that is compatible with CSS-in-JS libaries.
+ * CSS as a plain object that is compatible with CSS-in-JS libaries.
  * Copied directly from [emotion](https://github.com/emotion-js/emotion/blob/ca3ad1c1dcabf78a95b55cc2dc94cad1998a3196/packages/serialize/types/index.d.ts#L45) types
  */
 export interface CSSObject
