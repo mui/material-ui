b9ea0104a3ebc2cd1e669794c52743ca81923558
TiavinaMichael <ralainirinatiavina@gmail.com>
2020-11-19 10:41:59 +0300

    initial commit

--- examples/nextjs-with-typescript/pages/index.tsx
+++ examples/nextjs-with-typescript/pages/index.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import Image from '../src/Image';
 
 export default function Index() {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <div>
+      <h1>Image Component With Layout Fixed</h1>
+      <Image layout="fixed" height={300} />
+  </div>
   );
 }
