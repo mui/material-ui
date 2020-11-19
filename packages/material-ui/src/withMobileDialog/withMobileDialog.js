90bf9d7e50ee79e45b27a82f5c5097e1ce7681a5
Reece Dunham <me@rdil.rocks>
2020-11-19 05:43:41 -0500

    [Dialog] Add deprecation warning for withMobileDialog (#23570)

--- packages/material-ui/src/withMobileDialog/withMobileDialog.js
+++ packages/material-ui/src/withMobileDialog/withMobileDialog.js
@@ -2,12 +2,25 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import withWidth, { isWidthDown } from '../withWidth';
 
+let warnedOnce = false;
+
 /**
  * Dialog will responsively be full screen *at or below* the given breakpoint
  * (defaults to 'sm' for mobile devices).
  * Notice that this Higher-order Component is incompatible with server-side rendering.
  */
 const withMobileDialog = (options = {}) => (Component) => {
+  if (process.env.NODE_ENV !== 'production') {
+    if (!warnedOnce) {
+      console.warn(
+        [
+          'Material-UI: The `withMobileDialog` function is deprecated.',
+          'Head to https://material-ui.com/r/migration-v4/#dialog for a migration path.',
+        ].join('\n'),
+      );
+      warnedOnce = true;
+    }
+  }
   const { breakpoint = 'sm' } = options;
 
   function WithMobileDialog(props) {
