81099268bc2e83139a5563172ce8ad97b3397397
Ricky Lippmann <3674067+ldrick@users.noreply.github.com>
2020-11-23 20:52:11 +0100

    [styles] Add support for TypeScript 4.1 (#23692)

--- packages/material-ui-styles/src/makeStyles/makeStyles.d.ts
+++ packages/material-ui-styles/src/makeStyles/makeStyles.d.ts
@@ -7,21 +7,13 @@ import {
 import { Omit } from '@material-ui/types';
 import { DefaultTheme } from '../defaultTheme';
 
-/**
- * `makeStyles` where the passed `styles` do not depend on props
- */
-export default function makeStyles<Theme = DefaultTheme, ClassKey extends string = string>(
-  style: Styles<Theme, {}, ClassKey>,
-  options?: Omit<WithStylesOptions<Theme>, 'withTheme'>
-): (props?: any) => ClassNameMap<ClassKey>;
-/**
- * `makeStyles` where the passed `styles` do depend on props
- */
 export default function makeStyles<
   Theme = DefaultTheme,
-  Props extends {} = {},
+  Props extends object = {},
   ClassKey extends string = string
 >(
   styles: Styles<Theme, Props, ClassKey>,
   options?: Omit<WithStylesOptions<Theme>, 'withTheme'>
-): (props: Props) => ClassNameMap<ClassKey>;
+): keyof Props extends never // `makeStyles` where the passed `styles` do not depend on props
+  ? (props?: any) => ClassNameMap<ClassKey> // `makeStyles` where the passed `styles` do depend on props
+  : (props: Props) => ClassNameMap<ClassKey>;
