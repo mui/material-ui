6cfb98f09437bccbe60463b7fba6d7896793dc50
Marija Najdova <mnajdova@gmail.com>
2020-11-23 12:44:28 +0100

    moved height & width to sx

--- docs/src/pages/system/basics/Demo.tsx
+++ docs/src/pages/system/basics/Demo.tsx
@@ -20,12 +20,12 @@ export default function Demo() {
       <Box
         component="img"
         sx={{
+          height: 233,
+          width: 350,
           maxHeight: { xs: 233, md: 167 },
           maxWidth: { xs: 350, md: 250 },
         }}
         alt="The house from the offer."
-        height="233"
-        width="350"
         src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
       />
       <Box
