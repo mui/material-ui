8b416e206c390e7f35b6f6c814c9d50a1f559201
Bob Ippolito <bob@redivi.com>
2020-11-25 04:36:05 -0800

    [styles] Update mergeClasses types to more closely match its implementation (#23705)

--- packages/material-ui-styles/src/mergeClasses/mergeClasses.test.js
+++ packages/material-ui-styles/src/mergeClasses/mergeClasses.test.js
@@ -15,4 +15,28 @@ describe('mergeClasses', () => {
       root: 'foo bar',
     });
   });
+
+  it('should allow newClasses to be partial', () => {
+    const output = mergeClasses({
+      baseClasses: {
+        root: 'foo',
+        child: 'baz',
+      },
+      newClasses: {
+        root: 'bar',
+      },
+    });
+    expect(output).to.deep.equal({
+      root: 'foo bar',
+      child: 'baz',
+    });
+  });
+
+  it('should allow newClasses to be optional', () => {
+    const baseClasses = {
+      root: 'foo',
+    };
+    expect(mergeClasses({ baseClasses })).to.equal(baseClasses);
+    expect(mergeClasses({ baseClasses, newClasses: null })).to.equal(baseClasses);
+  });
 });
