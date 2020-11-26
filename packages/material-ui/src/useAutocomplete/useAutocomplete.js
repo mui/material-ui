fb80e2049f636fac22193ce5b6ba0e788c2d5b2c
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-11-26 10:40:36 +0100

    fix eslint

--- packages/material-ui/src/useAutocomplete/useAutocomplete.js
+++ packages/material-ui/src/useAutocomplete/useAutocomplete.js
@@ -470,7 +470,6 @@ export default function useAutocomplete(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
     // Only sync the highlighted index when the option switch between empty and not
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     filteredOptions.length,
     // Don't sync the highlighted index with the value when multiple
     // eslint-disable-next-line react-hooks/exhaustive-deps
