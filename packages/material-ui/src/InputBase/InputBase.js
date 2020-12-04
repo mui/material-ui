ef9debb7d3391812f45ee260578923b68cc1d790
Mayra Lucero Garcia Ramírez <ml17ft@gmail.com>
2020-12-04 08:15:42 -0600

    Update packages/material-ui/src/InputBase/InputBase.js
    Co-authored-by: Marija Najdova <mnajdova@gmail.com>

--- packages/material-ui/src/InputBase/InputBase.js
+++ packages/material-ui/src/InputBase/InputBase.js
@@ -203,6 +203,7 @@ const InputBase = React.forwardRef(function InputBase(props, ref) {
     ...other
   } = props;
 
+
   const value = inputPropsProp.value != null ? inputPropsProp.value : valueProp;
   const { current: isControlled } = React.useRef(value != null);
 
