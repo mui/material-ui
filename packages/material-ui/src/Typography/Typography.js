8bdd48cb3b2c3071945352628e0e2a06a7e146ba
Danail H <hadjiatanasov@gmail.com>
2020-12-04 16:00:10 +0100

    First draft for typography migration to emotion

--- packages/material-ui/src/Typography/Typography.js
+++ packages/material-ui/src/Typography/Typography.js
@@ -1,106 +1,44 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { useThemeVariants } from '@material-ui/styles';
-import withStyles from '../styles/withStyles';
-import capitalize from '../utils/capitalize';
+import experimentalStyled from '../styles/experimentalStyled';
 
-export const styles = (theme) => ({
-  /* Styles applied to the root element. */
-  root: {
-    margin: 0,
-  },
-  /* Styles applied to the root element if `variant="body2"`. */
-  body2: theme.typography.body2,
-  /* Styles applied to the root element if `variant="body1"`. */
-  body1: theme.typography.body1,
-  /* Styles applied to the root element if `variant="caption"`. */
-  caption: theme.typography.caption,
-  /* Styles applied to the root element if `variant="button"`. */
-  button: theme.typography.button,
-  /* Styles applied to the root element if `variant="h1"`. */
-  h1: theme.typography.h1,
-  /* Styles applied to the root element if `variant="h2"`. */
-  h2: theme.typography.h2,
-  /* Styles applied to the root element if `variant="h3"`. */
-  h3: theme.typography.h3,
-  /* Styles applied to the root element if `variant="h4"`. */
-  h4: theme.typography.h4,
-  /* Styles applied to the root element if `variant="h5"`. */
-  h5: theme.typography.h5,
-  /* Styles applied to the root element if `variant="h6"`. */
-  h6: theme.typography.h6,
-  /* Styles applied to the root element if `variant="subtitle1"`. */
-  subtitle1: theme.typography.subtitle1,
-  /* Styles applied to the root element if `variant="subtitle2"`. */
-  subtitle2: theme.typography.subtitle2,
-  /* Styles applied to the root element if `variant="overline"`. */
-  overline: theme.typography.overline,
-  /* Styles applied to the root element if `variant="inherit"`. */
-  inherit: {},
-  /* Styles applied to the root element if `align="left"`. */
-  alignLeft: {
-    textAlign: 'left',
-  },
-  /* Styles applied to the root element if `align="center"`. */
-  alignCenter: {
-    textAlign: 'center',
-  },
-  /* Styles applied to the root element if `align="right"`. */
-  alignRight: {
-    textAlign: 'right',
-  },
-  /* Styles applied to the root element if `align="justify"`. */
-  alignJustify: {
-    textAlign: 'justify',
-  },
-  /* Styles applied to the root element if `nowrap={true}`. */
-  noWrap: {
+const getTextColor = (color, palette) => {
+  if (color === 'textPrimary' || color === 'textSecondary') {
+    return palette.text[color.split('text').pop().toLowerCase()];
+  }
+
+  if (color === 'inherit' || color === 'initial') {
+    return color;
+  }
+
+  return palette[color].main;
+};
+
+export const TypographyRoot = experimentalStyled(
+  'span',
+  {},
+  { muiName: 'MuiTypography' },
+)((props) => ({
+  margin: 0,
+  ...(props.styleProps.variant && props.theme.typography[props.styleProps.variant]),
+  ...(props.styleProps.textAlign && {
+    textAlign: props.styleProps.textAlign
+  }),
+  ...(props.styleProps.noWrap && {
     overflow: 'hidden',
     textOverflow: 'ellipsis',
     whiteSpace: 'nowrap',
-  },
-  /* Styles applied to the root element if `gutterBottom={true}`. */
-  gutterBottom: {
-    marginBottom: '0.35em',
-  },
-  /* Styles applied to the root element if `paragraph={true}`. */
-  paragraph: {
+  }),
+  ...(props.styleProps.paragraph && {
     marginBottom: 16,
-  },
-  /* Styles applied to the root element if `color="inherit"`. */
-  colorInherit: {
-    color: 'inherit',
-  },
-  /* Styles applied to the root element if `color="primary"`. */
-  colorPrimary: {
-    color: theme.palette.primary.main,
-  },
-  /* Styles applied to the root element if `color="secondary"`. */
-  colorSecondary: {
-    color: theme.palette.secondary.main,
-  },
-  /* Styles applied to the root element if `color="textPrimary"`. */
-  colorTextPrimary: {
-    color: theme.palette.text.primary,
-  },
-  /* Styles applied to the root element if `color="textSecondary"`. */
-  colorTextSecondary: {
-    color: theme.palette.text.secondary,
-  },
-  /* Styles applied to the root element if `color="error"`. */
-  colorError: {
-    color: theme.palette.error.main,
-  },
-  /* Styles applied to the root element if `display="inline"`. */
-  displayInline: {
-    display: 'inline',
-  },
-  /* Styles applied to the root element if `display="block"`. */
-  displayBlock: {
-    display: 'block',
-  },
-});
+  }),
+  ...(props.styleProps.color && {
+    color: getTextColor(props.styleProps.color, props.theme.palette),
+  }),
+  ...(props.styleProps.display && {
+    display: props.styleProps.display,
+  }),
+}));
 
 const defaultVariantMapping = {
   h1: 'h1',
@@ -132,20 +70,20 @@ const Typography = React.forwardRef(function Typography(props, ref) {
     ...other
   } = props;
 
-  const themeVariantsClasses = useThemeVariants(
-    {
-      ...props,
-      align,
-      color,
-      display,
-      gutterBottom,
-      noWrap,
-      paragraph,
-      variant,
-      variantMapping,
-    },
-    'MuiTypography',
-  );
+  const stateAndProps = {
+    ...props,
+    align,
+    classes,
+    className,
+    color,
+    component,
+    display,
+    gutterBottom,
+    noWrap,
+    paragraph,
+    variant,
+    variantMapping,
+  };
 
   const Component =
     component ||
@@ -153,22 +91,10 @@ const Typography = React.forwardRef(function Typography(props, ref) {
     'span';
 
   return (
-    <Component
-      className={clsx(
-        classes.root,
-        classes[variant],
-        {
-          [classes[`color${capitalize(color)}`]]: color !== 'initial',
-          [classes.noWrap]: noWrap,
-          [classes.gutterBottom]: gutterBottom,
-          [classes.paragraph]: paragraph,
-          [classes[`align${capitalize(align)}`]]: align !== 'inherit',
-          [classes[`display${capitalize(display)}`]]: display !== 'initial',
-        },
-        themeVariantsClasses,
-        className,
-      )}
+    <TypographyRoot
+      as={Component}
       ref={ref}
+      styleProps={stateAndProps}
       {...other}
     />
   );
@@ -282,4 +208,4 @@ Typography.propTypes = {
   variantMapping: PropTypes /* @typescript-to-proptypes-ignore */.object,
 };
 
-export default withStyles(styles, { name: 'MuiTypography' })(Typography);
+export default Typography;
