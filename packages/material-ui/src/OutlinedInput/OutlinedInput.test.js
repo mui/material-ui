2ef57e553de3812b912a83dcec712914d804681c
Mayra Lucero Garcia Ramírez <ml17ft@gmail.com>
2020-12-04 08:33:04 -0600

    Apply suggestions from code review
    Co-authored-by: Marija Najdova <mnajdova@gmail.com>

--- packages/material-ui/src/OutlinedInput/OutlinedInput.test.js
+++ packages/material-ui/src/OutlinedInput/OutlinedInput.test.js
@@ -29,9 +29,9 @@ describe('<OutlinedInput />', () => {
     expect(container.querySelector('.notched-outlined')).not.to.equal(null);
   });
 
-  it('should add the classes when size is small', () => {
-    const { container } = render(<OutlinedInput size="small" id="smInput" />);
+  it('should add the inputMarginDense class when size is small', () => {
+    const { container } = render(<OutlinedInput size="small" />);
 
-    expect(container.querySelector('input')).to.have.class(classes.inputMarginDense);
+    expect(container.firstChild).to.have.class(classes.inputMarginDense);
   });
 });
