bf72902260041ef13038348851a6a4199594a1d7
Jens Ox <github@jens-ox.de>
2020-12-04 19:22:30 +0100

    roll back old changes

--- packages/eslint-plugin-material-ui/src/index.js
+++ packages/eslint-plugin-material-ui/src/index.js
@@ -4,5 +4,4 @@ module.exports.rules = {
   'docgen-ignore-before-comment': require('./rules/docgen-ignore-before-comment'),
   'no-hardcoded-labels': require('./rules/no-hardcoded-labels'),
   'rules-of-use-theme-variants': require('./rules/rules-of-use-theme-variants'),
-  'warn-unused-classes': require('./rules/warn-unused-classes')
 };
