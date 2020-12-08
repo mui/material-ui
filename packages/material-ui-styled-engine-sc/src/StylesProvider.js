2b241749e16164bc99004f7fac707bf168f4271c
Marija Najdova <mnajdova@gmail.com>
2020-12-08 19:50:48 +0100

    lint

--- packages/material-ui-styled-engine-sc/src/StylesProvider.js
+++ packages/material-ui-styled-engine-sc/src/StylesProvider.js
@@ -1,8 +1,4 @@
-import * as React from 'react';
-
 // Dumb component that does nothin, but complies with the API exported from @material-ui/styled-engine
-export function StylesProvider(props) {
-  return <>{props.children}</>;
-}
+const StylesProvider = ({ children }) => children;
 
 export default StylesProvider;
--- packages/material-ui-styled-engine/src/StylesProvider.js
+++ packages/material-ui-styled-engine/src/StylesProvider.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
 
@@ -8,5 +9,12 @@ export const cache = createCache({ key: 'css', prepend: true });
 export function StylesProvider(props) {
   return <CacheProvider value={cache}>{props.children}</CacheProvider>;
 }
+
+StylesProvider.propTypes = {
+  /**
+   * Your component tree.
+   */
+  children: PropTypes.node,
+};
 
 export default StylesProvider;
