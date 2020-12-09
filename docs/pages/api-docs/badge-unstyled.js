616f0899c3585551b7a95da308ed4b1a97496a0f
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-12-09 12:20:39 +0100

    yarn docs:api

--- docs/pages/api-docs/badge-unstyled.js
+++ docs/pages/api-docs/badge-unstyled.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
-import { prepareMarkdown } from 'docs/src/modules/utils/parseMarkdown';
+import * as React from 'react';
+import ApiPage from 'docs/src/modules/components/ApiPage';
+import mapApiPageTranslations from 'docs/src/modules/utils/mapApiPageTranslations';
+import jsonPageContent from './badge-unstyled.json';
 
-const pageFilename = 'api/badge-unstyled';
-const requireRaw = require.context('!raw-loader!./', false, /\/badge-unstyled\.md$/);
-
-export default function Page({ docs }) {
-  return <MarkdownDocs docs={docs} />;
+export default function Page(props) {
+  const { descriptions, pageContent } = props;
+  return <ApiPage descriptions={descriptions} pageContent={pageContent} />;
 }
 
 Page.getInitialProps = () => {
-  const { demos, docs } = prepareMarkdown({ pageFilename, requireRaw });
-  return { demos, docs };
+  const req = require.context(
+    'docs/translations/api-docs/badge-unstyled',
+    false,
+    /badge-unstyled.*.json$/,
+  );
+  const descriptions = mapApiPageTranslations(req);
+
+  return {
+    descriptions,
+    pageContent: jsonPageContent,
+  };
 };
