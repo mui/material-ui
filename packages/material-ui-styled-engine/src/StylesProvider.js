0f82a97e024db427c2620638db1875b79e877f78
Marija Najdova <mnajdova@gmail.com>
2020-12-09 16:09:16 +0100

    prettier

--- packages/material-ui-styled-engine/src/StylesProvider.js
+++ packages/material-ui-styled-engine/src/StylesProvider.js
@@ -3,15 +3,19 @@ import PropTypes from 'prop-types';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
 
-// Cache with option to prepend emotion's style tag 
+// Cache with option to prepend emotion's style tag
 export const cache = createCache({ key: 'css', prepend: true });
 
 export function StylesProvider({ injectFirst }) {
-  return injectFirst ? <CacheProvider value={cache}>{props.children}</CacheProvider> : props.children;
+  return injectFirst ? (
+    <CacheProvider value={cache}>{props.children}</CacheProvider>
+  ) : (
+    props.children
+  );
 }
 
 StylesProvider.propTypes = {
-    /**
+  /**
    * Your component tree.
    */
   children: PropTypes.node,
