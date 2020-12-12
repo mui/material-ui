e8314cc80c84e195446fc6e3724ad2e5067d7410
Jalen Massey <jmassey@digitalprimates.net>
2020-12-12 14:17:24 -0600

    ignoring ts to proptypes for the List

--- packages/material-ui/src/List/List.js
+++ packages/material-ui/src/List/List.js
@@ -82,7 +82,7 @@ List.propTypes = {
    * The component used for the root node.
    * Either a string to use a HTML element or a component.
    */
-  component: PropTypes.elementType,
+  component: PropTypes /* @typescript-to-proptypes-ignore */.elementType,
   /**
    * If `true`, compact vertical padding designed for keyboard and mouse input is used for
    * the list and list items.
