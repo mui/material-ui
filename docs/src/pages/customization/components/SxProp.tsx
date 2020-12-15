dcabdf7490be7a9dc39d527d8551371152f69f7d
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-12-15 23:36:25 +0100

    keep each section focusing on showing one thing

--- docs/src/pages/customization/components/SxProp.tsx
+++ docs/src/pages/customization/components/SxProp.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Slider from '@material-ui/core/Slider';
-import { alpha } from '@material-ui/core/styles';
 
 export default function SxProp() {
   return (
@@ -9,16 +8,6 @@ export default function SxProp() {
       sx={{
         width: 300,
         color: 'success.main',
-        '& .MuiSlider-thumb': {
-          '&:hover, &.Mui-focusVisible': {
-            boxShadow: (theme) =>
-              `0px 0px 0px 8px ${alpha(theme.palette.success.main, 0.16)}`,
-          },
-          '&.Mui-active': {
-            boxShadow: (theme) =>
-              `0px 0px 0px 14px ${alpha(theme.palette.success.main, 0.16)}`,
-          },
-        },
       }}
     />
   );
