d09e3d085e3a880f4122467783e2697698f19f18
Jan Silhan <jsilhan@oriflame.com>
2020-12-21 17:26:56 +0100

    fix: Theme augmentation.

--- packages/material-ui-lab/src/themeAugmentation/components.d.ts
+++ packages/material-ui-lab/src/themeAugmentation/components.d.ts
@@ -46,6 +46,102 @@ export interface LabComponents {
     defaultProps?: ComponentsProps['MuiTreeView'];
     styleOverrides?: ComponentsOverrides['MuiTreeView'];
   };
+  MuiClock?: {
+    defaultProps?: ComponentsProps['MuiClock'];
+    styleOverrides?: ComponentsOverrides['MuiClock'];
+  };
+  MuiClockNumber?: {
+    defaultProps?: ComponentsProps['MuiClockNumber'];
+    styleOverrides?: ComponentsOverrides['MuiClockNumber'];
+  };
+  MuiClockPointer?: {
+    defaultProps?: ComponentsProps['MuiClockPointer'];
+    styleOverrides?: ComponentsOverrides['MuiClockPointer'];
+  };
+  MuiDatePickerToolbar?: {
+    defaultProps?: ComponentsProps['MuiDatePickerToolbar'];
+    styleOverrides?: ComponentsOverrides['MuiDatePickerToolbar'];
+  };
+  MuiDateTimePickerTabs?: {
+    defaultProps?: ComponentsProps['MuiDateTimePickerTabs'];
+    styleOverrides?: ComponentsOverrides['MuiDateTimePickerTabs'];
+  };
+  MuiDateTimePickerToolbar?: {
+    defaultProps?: ComponentsProps['MuiDateTimePickerToolbar'];
+    styleOverrides?: ComponentsOverrides['MuiDateTimePickerToolbar'];
+  };
+  MuiDayPicker?: {
+    defaultProps?: ComponentsProps['MuiDayPicker'];
+    styleOverrides?: ComponentsOverrides['MuiDayPicker'];
+  };
+  MuiMonthPicker?: {
+    defaultProps?: ComponentsProps['MuiMonthPicker'];
+    styleOverrides?: ComponentsOverrides['MuiMonthPicker'];
+  };
+  MuiPicker?: {
+    defaultProps?: ComponentsProps['MuiPicker'];
+    styleOverrides?: ComponentsOverrides['MuiPicker'];
+  };
+  MuiPickersArrowSwitcher?: {
+    defaultProps?: ComponentsProps['MuiPickersArrowSwitcher'];
+    styleOverrides?: ComponentsOverrides['MuiPickersArrowSwitcher'];
+  };
+  MuiPickersCalendar?: {
+    defaultProps?: ComponentsProps['MuiPickersCalendar'];
+    styleOverrides?: ComponentsOverrides['MuiPickersCalendar'];
+  };
+  MuiPickersCalendarHeader?: {
+    defaultProps?: ComponentsProps['MuiPickersCalendarHeader'];
+    styleOverrides?: ComponentsOverrides['MuiPickersCalendarHeader'];
+  };
+  MuiPickersCalendarSkeleton?: {
+    defaultProps?: ComponentsProps['MuiPickersCalendarSkeleton'];
+    styleOverrides?: ComponentsOverrides['MuiPickersCalendarSkeleton'];
+  };
+  MuiPickersDay?: {
+    defaultProps?: ComponentsProps['MuiPickersDay'];
+    styleOverrides?: ComponentsOverrides['MuiPickersDay'];
+  };
+  MuiPickersFadeTransition?: {
+    defaultProps?: ComponentsProps['MuiPickersFadeTransition'];
+    styleOverrides?: ComponentsOverrides['MuiPickersFadeTransition'];
+  };
+  MuiPickersModalDialog?: {
+    defaultProps?: ComponentsProps['MuiPickersModalDialog'];
+    styleOverrides?: ComponentsOverrides['MuiPickersModalDialog'];
+  };
+  MuiPickersMonth?: {
+    defaultProps?: ComponentsProps['MuiPickersMonth'];
+    styleOverrides?: ComponentsOverrides['MuiPickersMonth'];
+  };
+  MuiPickersPopper?: {
+    defaultProps?: ComponentsProps['MuiPickersPopper'];
+    styleOverrides?: ComponentsOverrides['MuiPickersPopper'];
+  };
+  MuiPickersSlideTransition?: {
+    defaultProps?: ComponentsProps['MuiPickersSlideTransition'];
+    styleOverrides?: ComponentsOverrides['MuiPickersSlideTransition'];
+  };
+  MuiPickersToolbar?: {
+    defaultProps?: ComponentsProps['MuiPickersToolbar'];
+    styleOverrides?: ComponentsOverrides['MuiPickersToolbar'];
+  };
+  MuiPickersToolbarButton?: {
+    defaultProps?: ComponentsProps['MuiPickersToolbarButton'];
+    styleOverrides?: ComponentsOverrides['MuiPickersToolbarButton'];
+  };
+  MuiPickersToolbarText?: {
+    defaultProps?: ComponentsProps['MuiPickersToolbarText'];
+    styleOverrides?: ComponentsOverrides['MuiPickersToolbarText'];
+  };
+  MuiPickersYear?: {
+    defaultProps?: ComponentsProps['MuiPickersYear'];
+    styleOverrides?: ComponentsOverrides['MuiPickersYear'];
+  };
+  MuiYearPicker?: {
+    defaultProps?: ComponentsProps['MuiYearPicker'];
+    styleOverrides?: ComponentsOverrides['MuiYearPicker'];
+  };
 }
 
 declare module '@material-ui/core/styles/components' {
