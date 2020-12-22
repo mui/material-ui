f1389e83a6be5ae6b3c4b60e0e0aa3dcbb0ce72d
Marija Najdova <mnajdova@gmail.com>
2020-12-22 11:47:45 +0100

    [system] Improve the SxProp typings structure, by splitting them in a separate module. (#24090)

---
+++ packages/material-ui-system/src/styleFunctionSx/index.d.ts
@@ -0,0 +1,6 @@
+export { default } from './styleFunctionSx';
+
+export * from './styleFunctionSx';
+export * from './AliasesCSSProperties';
+export * from './OverwriteCSSProperties';
+export * from './StandardCssProperties';
