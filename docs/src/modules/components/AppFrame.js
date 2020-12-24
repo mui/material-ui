21d753b48cc19acf924be608eea6a69248e36cb0
Marija Najdova <mnajdova@gmail.com>
2020-12-24 15:05:46 +0100

    typo

--- docs/src/modules/components/AppFrame.js
+++ docs/src/modules/components/AppFrame.js
@@ -302,7 +302,7 @@ function AppFrame(props) {
 AppFrame.propTypes = {
   children: PropTypes.node.isRequired,
   classes: PropTypes.object.isRequired,
-  disableDrawer: PropTypes.boolean,
+  disableDrawer: PropTypes.bool,
 };
 
 export default withStyles(styles)(AppFrame);
