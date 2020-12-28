8b4c50c755ac02a3f4cd4733a8986f2ccbe6b984
eps1lon <silbermann.sebastian@gmail.com>
2020-12-28 18:47:52 +0100

    fix es module interop with prop-types

--- packages/material-ui-unstyled/src/SliderUnstyled/SliderValueLabelUnstyled.js
+++ packages/material-ui-unstyled/src/SliderUnstyled/SliderValueLabelUnstyled.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import sliderUnstyledClasses from './sliderUnstyledClasses';
 
