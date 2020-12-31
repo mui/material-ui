05590bcdcb2571186383cf721be313689a7fab63
Marija Najdova <mnajdova@gmail.com>
2020-12-31 17:56:16 +0100

    refactored typings based on latest emotion

---
+++ packages/material-ui/src/styles/experimentalStyled.spec.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { experimentalStyled as styled } from '@material-ui/core/styles';
+
+const Box = styled('div')(({ theme }) => ({
+  color: theme.palette.primary.main,
+}));
+
+
+const SimpleBox = styled('div')``;
+
+function SxTestSimpleBox() {
+  <SimpleBox sx={{ p: [2, 3, 4] }} />;
+}
+
+function SxTest() {
+  <Box sx={{ p: [2, 3, 4] }} />;
+}
+
+function WorksWithNoTheme() {
+  <Box />;
+}
