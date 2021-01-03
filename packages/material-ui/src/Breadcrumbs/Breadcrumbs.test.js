c19917c0bee7ddc1980ec48891c13297965c5542
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-01-03 18:09:23 +0100

    [core] Batch small changes (#24224)

--- packages/material-ui/src/Breadcrumbs/Breadcrumbs.test.js
+++ packages/material-ui/src/Breadcrumbs/Breadcrumbs.test.js
@@ -65,7 +65,7 @@ describe('<Breadcrumbs />', () => {
   it('should expand when `BreadcrumbCollapsed` is clicked', () => {
     const { getAllByRole, getByRole, getByText } = render(
       <Breadcrumbs>
-        <span tabIndex="-1">first</span>
+        <span tabIndex={-1}>first</span>
         <span>second</span>
         <span>third</span>
         <span>fourth</span>
