3f51909422b4c8c8dece50d114d39bceeddd1cba
Engr-Asad-Hussain <asad.h1998@yahoo.com>
2021-01-05 20:27:15 +0500

    Update ButtonAppBar.js file

--- docs/src/pages/components/app-bar/ButtonAppBar.js
+++ docs/src/pages/components/app-bar/ButtonAppBar.js
@@ -5,7 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from '@material-ui/core/Menu';
 
 const useStyles = makeStyles((theme) => ({
   root: {
