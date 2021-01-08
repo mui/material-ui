88762ccbca2ce60bf1e94273a58e6e1d84fdfec7
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-01-08 23:09:14 +0100

    yarn docs:api

--- packages/material-ui/src/Global/Global.d.ts
+++ packages/material-ui/src/Global/Global.d.ts
@@ -1,4 +1,9 @@
 import { GlobalProps } from '@material-ui/styled-engine';
 import { Omit } from '@material-ui/types';
-
+/**
+ *
+ * API:
+ *
+ * - [Global API](https://material-ui.com/api/global/)
+ */
 export default function Global(props: Omit<GlobalProps, 'defaultTheme'>): React.ReactElement;
