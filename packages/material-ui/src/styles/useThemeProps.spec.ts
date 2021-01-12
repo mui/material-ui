1b92f7fa4f39625dc3ad4a0a6852b989ddd4503c
Marija Najdova <mnajdova@gmail.com>
2021-01-12 16:49:07 +0100

    typings

---
+++ packages/material-ui/src/styles/useThemeProps.spec.ts
@@ -0,0 +1,14 @@
+import { Theme, useThemeProps } from '@material-ui/core/styles';
+import { SliderProps } from '@material-ui/core/Slider';
+
+{
+  const props = useThemeProps<Theme, SliderProps, 'MuiSlider'>({ props: { color: 'primary' }, name: 'MuiSlider' });
+
+  // additional props are valid
+  props.isRtl;
+  props.theme;
+
+  // component's props are valid
+  props.track;
+  props.valueLabelDisplay;
+}
