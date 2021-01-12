18792a4302908f7810b50baf887adbcf0016edd8
Marija Najdova <mnajdova@gmail.com>
2021-01-12 16:14:12 +0100

    prettier

--- docs/src/pages/customization/how-to-customize/GlobalCssOverride.tsx
+++ docs/src/pages/customization/how-to-customize/GlobalCssOverride.tsx
@@ -4,8 +4,8 @@ import GlobalStyles from '@material-ui/core/GlobalStyles';
 export default function GlobalCssOverride() {
   return (
     <React.Fragment>
-      <GlobalStyles styles={{ h1: { color: 'grey' }}} />
+      <GlobalStyles styles={{ h1: { color: 'grey' } }} />
       <h1>Grey h1 element</h1>
     </React.Fragment>
-  )
+  );
 }
