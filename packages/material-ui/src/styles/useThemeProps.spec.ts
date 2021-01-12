a4e7f4bf2dc34373679ffd56cc4dca46ee53e6cf
Marija Najdova <mnajdova@gmail.com>
2021-01-12 18:38:34 +0100

    lint

--- packages/material-ui/src/styles/useThemeProps.spec.ts
+++ packages/material-ui/src/styles/useThemeProps.spec.ts
@@ -2,6 +2,7 @@ import { Theme, unstable_useThemeProps as useThemeProps } from '@material-ui/cor
 import { SliderProps } from '@material-ui/core/Slider';
 
 {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const props = useThemeProps<Theme, SliderProps, 'MuiSlider'>({
     props: { color: 'primary' },
     name: 'MuiSlider',
