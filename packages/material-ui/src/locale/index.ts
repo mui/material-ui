b2bb0f1e1fce134ad242cf70c2326612a14c5d6c
Lukas Elmer <lukas.elmer@gmail.com>
2021-01-15 19:47:06 +0100

    [l10n] Add de-CH locale

--- packages/material-ui/src/locale/index.ts
+++ packages/material-ui/src/locale/index.ts
@@ -401,6 +401,77 @@ export const csCZ: Localization = {
   },
 };
 
+export const deCH: Localization = {
+  components: {
+    MuiBreadcrumbs: {
+      defaultProps: {
+        expandText: 'Pfad anzeigen',
+      },
+    },
+    MuiTablePagination: {
+      defaultProps: {
+        getItemAriaLabel: (type) => {
+          if (type === 'first') {
+            return 'Zur ersten Seite';
+          }
+          if (type === 'last') {
+            return 'Zur letzten Seite';
+          }
+          if (type === 'next') {
+            return 'Zur nächsten Seite';
+          }
+          // if (type === 'previous') {
+          return 'Zur vorherigen Seite';
+        },
+        labelRowsPerPage: 'Zeilen pro Seite:',
+        labelDisplayedRows: ({ from, to, count }) =>
+          `${from}-${to} von ${count !== -1 ? count : `mehr als ${to}`}`,
+      },
+    },
+    MuiRating: {
+      defaultProps: {
+        getLabelText: (value) => `${value} ${value !== 1 ? 'Sterne' : 'Stern'}`,
+        emptyLabelText: 'Keine Wertung',
+      },
+    },
+    MuiAutocomplete: {
+      defaultProps: {
+        clearText: 'Leeren',
+        closeText: 'Schliessen',
+        loadingText: 'Wird geladen…',
+        noOptionsText: 'Keine Optionen',
+        openText: 'Öffnen',
+      },
+    },
+    MuiAlert: {
+      defaultProps: {
+        closeText: 'Schliessen',
+      },
+    },
+    MuiPagination: {
+      defaultProps: {
+        'aria-label': 'Navigation via Seitennummerierung',
+        getItemAriaLabel: (type, page, selected) => {
+          if (type === 'page') {
+            return `${selected ? '' : 'Gehe zu '}Seite ${page}`;
+          }
+          if (type === 'first') {
+            return 'Zur ersten Seite';
+          }
+          if (type === 'last') {
+            return 'Zur letzten Seite';
+          }
+          if (type === 'next') {
+            return 'Zur nächsten Seite';
+          }
+          // if (type === 'previous') {
+          return 'Zur vorherigen Seite';
+        },
+      },
+    },
+  },
+};
+
 export const deDE: Localization = {
   components: {
     MuiBreadcrumbs: {
