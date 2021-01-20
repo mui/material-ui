44bf6e9140abbbf091806aeb3ecb26ba994e0205
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2021-01-20 10:10:59 -0800

    Revert accidental changes

--- packages/material-ui/src/Accordion/Accordion.js
+++ packages/material-ui/src/Accordion/Accordion.js
@@ -40,71 +40,73 @@ const AccordionRoot = experimentalStyled(
     slot: 'Root',
     overridesResolver,
   },
-)(({ theme, styleProps }) => ({
-  /* Styles applied to the root element. */
-  position: 'relative',
-  transition: theme.transitions.create(['margin'], {
+)(({ theme, styleProps }) => {
+  const transition = {
     duration: theme.transitions.duration.shortest,
-  }),
-  overflowAnchor: 'none', // Keep the same scrolling position
-  '&:before': {
-    position: 'absolute',
-    left: 0,
-    top: -1,
-    right: 0,
-    height: 1,
-    content: '""',
-    opacity: 1,
-    backgroundColor: theme.palette.divider,
-    transition: theme.transitions.create(['opacity', 'background-color'], {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  '&:first-child': {
+  };
+
+  return {
+    /* Styles applied to the root element. */
+    position: 'relative',
+    transition: theme.transitions.create(['margin'], transition),
+    overflowAnchor: 'none', // Keep the same scrolling position
     '&:before': {
-      display: 'none',
+      position: 'absolute',
+      left: 0,
+      top: -1,
+      right: 0,
+      height: 1,
+      content: '""',
+      opacity: 1,
+      backgroundColor: theme.palette.divider,
+      transition: theme.transitions.create(['opacity', 'background-color'], transition),
     },
-  },
-  /* Styles applied to the root element if `expanded={true}`. */
-  [`&.${accordionClasses.expanded}`]: {
-    margin: '16px 0',
     '&:first-child': {
-      marginTop: 0,
-    },
-    '&:last-child': {
-      marginBottom: 0,
-    },
-    '&:before': {
-      opacity: 0,
-    },
-    '& + &': {
       '&:before': {
         display: 'none',
       },
     },
-  },
-  /* Styles applied to the root element if `disabled={true}`. */
-  ...(styleProps.disabled && {
-    backgroundColor: theme.palette.action.disabledBackground,
-  }),
-  /* Styles applied to the root element unless `square={true}`. */
-  ...(!styleProps.square && {
-    borderRadius: 0,
-    '&:first-child': {
-      borderTopLeftRadius: theme.shape.borderRadius,
-      borderTopRightRadius: theme.shape.borderRadius,
-    },
-    '&:last-child': {
-      borderBottomLeftRadius: theme.shape.borderRadius,
-      borderBottomRightRadius: theme.shape.borderRadius,
-      // Fix a rendering issue on Edge
-      '@supports (-ms-ime-align: auto)': {
-        borderBottomLeftRadius: 0,
-        borderBottomRightRadius: 0,
+    /* Styles applied to the root element if `expanded={true}`. */
+    [`&.${accordionClasses.expanded}`]: {
+      margin: '16px 0',
+      '&:first-child': {
+        marginTop: 0,
+      },
+      '&:last-child': {
+        marginBottom: 0,
+      },
+      '&:before': {
+        opacity: 0,
+      },
+      '& + &': {
+        '&:before': {
+          display: 'none',
+        },
       },
     },
-  }),
-}));
+    /* Styles applied to the root element if `disabled={true}`. */
+    [`&.${accordionClasses.disabled}`]: {
+      backgroundColor: theme.palette.action.disabledBackground,
+    },
+    /* Styles applied to the root element unless `square={true}`. */
+    ...(!styleProps.square && {
+      borderRadius: 0,
+      '&:first-child': {
+        borderTopLeftRadius: theme.shape.borderRadius,
+        borderTopRightRadius: theme.shape.borderRadius,
+      },
+      '&:last-child': {
+        borderBottomLeftRadius: theme.shape.borderRadius,
+        borderBottomRightRadius: theme.shape.borderRadius,
+        // Fix a rendering issue on Edge
+        '@supports (-ms-ime-align: auto)': {
+          borderBottomLeftRadius: 0,
+          borderBottomRightRadius: 0,
+        },
+      },
+    }),
+  };
+});
 
 const AccordionRegion = experimentalStyled(
   'div',
