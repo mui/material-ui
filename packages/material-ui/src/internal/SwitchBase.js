b171deada699bc07bf2704dacfa0234d99da0f89
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2021-01-23 02:48:52 -0800

    The classes prop is no longer required

--- packages/material-ui/src/internal/SwitchBase.js
+++ packages/material-ui/src/internal/SwitchBase.js
@@ -208,7 +208,7 @@ SwitchBase.propTypes = {
    * Override or extend the styles applied to the component.
    * See [CSS API](#css) below for more details.
    */
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object,
   /**
    * @ignore
    */
