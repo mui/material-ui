86abbccd147bc87808be276a4891e5b5090c1694
Victor Casas <victor.casas@outlook.cl>
2021-01-24 12:26:26 -0300

    feat: Migrate to emotion

--- packages/material-ui/src/Card/Card.js
+++ packages/material-ui/src/Card/Card.js
@@ -1,21 +1,59 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
+import { deepmerge } from '@material-ui/utils';
+import { unstable_composeClasses as composeClasses } from '@material-ui/unstyled';
+import experimentalStyled from '../styles/experimentalStyled';
+import useThemeProps from '../styles/useThemeProps';
+import { getCardUtilityClass } from './cardClasses';
 import Paper from '../Paper';
-import withStyles from '../styles/withStyles';
 
-export const styles = {
-  /* Styles applied to the root element. */
-  root: {
-    overflow: 'hidden',
-  },
+const overridesResolver = (props, styles) => {
+  const { styleProps } = props;
+
+  return deepmerge(styles.root || {}, { ...styleProps });
 };
 
-const Card = React.forwardRef(function Card(props, ref) {
-  const { classes, className, raised = false, ...other } = props;
+const useUtilityClasses = (styleProps) => {
+  const { classes } = styleProps;
+
+  const slots = {
+    root: [
+      'root',
+    ],
+  };
+
+  return composeClasses(slots, getCardUtilityClass, classes);
+};
+
+const CardRoot = experimentalStyled(
+  'div',
+  {},
+  {
+    name: 'MuiCard',
+    slot: 'Root',
+    overridesResolver,
+  },
+)(() => {
+  return {
+    /* Styles applied to the root element. */
+    overflow: 'hidden',
+  };
+});
+
+const Card = React.forwardRef(function Card(inProps, ref) {
+  const props = useThemeProps({ props: inProps, name: 'MuiCard' });
+
+  const { className, raised = false, ...other } = props;
+
+  const styleProps = { ...props }
+
+  const classes = useUtilityClasses(styleProps)
 
   return (
-    <Paper
+    <CardRoot
+      as={Paper}
+      styleProps={styleProps}
       className={clsx(classes.root, className)}
       elevation={raised ? 8 : 1}
       ref={ref}
@@ -46,6 +84,10 @@ Card.propTypes = {
    * @default false
    */
   raised: PropTypes.bool,
+  /**
+  * The system prop that allows defining system overrides as well as additional CSS styles.
+  */
+  sx: PropTypes.object,
 };
 
-export default withStyles(styles, { name: 'MuiCard' })(Card);
+export default Card;
---
+++ packages/material-ui/src/Card/cardClasses.js
@@ -0,0 +1,11 @@
+import { generateUtilityClass, generateUtilityClasses } from '@material-ui/unstyled';
+
+export function getCardUtilityClass(slot) {
+  return generateUtilityClass('MuiCard', slot);
+}
+
+const cardClasses = generateUtilityClasses('MuiCard', [
+  'root',
+]);
+
+export default cardClasses;
