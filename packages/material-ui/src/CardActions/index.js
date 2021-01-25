85534535dfd4d9138ce69bbec40cd8db57f2aab9
Marija Najdova <mnajdova@gmail.com>
2021-01-25 11:51:56 +0100

    [CardActions] Fix wrong classes export name (#24609)

--- packages/material-ui/src/CardActions/index.js
+++ packages/material-ui/src/CardActions/index.js
@@ -1,3 +1,3 @@
 export { default } from './CardActions';
-export { default as cardClasses } from './cardActionsClasses';
+export { default as cardActionsClasses } from './cardActionsClasses';
 export * from './cardActionsClasses';
