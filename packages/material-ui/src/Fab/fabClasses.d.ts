e9cee8a828d756f0bc8eff8ee79ad82a2ff5f651
Sean Campbell <sean.campbell13@gmail.com>
2021-01-25 11:27:16 -0500

    Fab to emotion

---
+++ packages/material-ui/src/Fab/fabClasses.d.ts
@@ -0,0 +1,19 @@
+export interface FabClasses {
+  root: string;
+  label: string;
+  primary: string;
+  secondary: string;
+  extended: string;
+  circular: string;
+  focusVisible: string;
+  disabled: string;
+  colorInherit: string;
+  sizeSmall: string;
+  sizeMedium: string;
+}
+
+declare const fabClasses: FabClasses;
+
+export function getFabUtilityClass(slot: string): string;
+
+export default fabClasses;
