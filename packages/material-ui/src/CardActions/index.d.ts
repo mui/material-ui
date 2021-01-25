2918ce51923cd4069fb52846b521918817a6b38c
Víctor Casas <57921784+vicasas@users.noreply.github.com>
2021-01-25 05:16:40 -0300

    [Card] Migrate CardActions to emotion (#24604)

--- packages/material-ui/src/CardActions/index.d.ts
+++ packages/material-ui/src/CardActions/index.d.ts
@@ -1,2 +1,4 @@
 export { default } from './CardActions';
 export * from './CardActions';
+export { default as cardContentClasses } from './cardActionsClasses';
+export * from './cardActionsClasses';
