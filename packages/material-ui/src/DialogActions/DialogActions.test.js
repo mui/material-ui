d94af9006828e26bb2452df7afb870ecfb4ab198
Victor Casas <victor.casas@outlook.cl>
2021-01-25 11:56:15 -0300

    feat: Migrate to emotion

--- packages/material-ui/src/DialogActions/DialogActions.test.js
+++ packages/material-ui/src/DialogActions/DialogActions.test.js
@@ -1,20 +1,18 @@
 import * as React from 'react';
-import { getClasses, createMount, describeConformance } from 'test/utils';
+import { createMount, describeConformance } from 'test/utils';
 import DialogActions from './DialogActions';
+import classes from './dialogActionsClasses';
 
 describe('<DialogActions />', () => {
   const mount = createMount();
-  let classes;
-
-  before(() => {
-    classes = getClasses(<DialogActions />);
-  });
 
   describeConformance(<DialogActions />, () => ({
     classes,
     inheritComponent: 'div',
     mount,
     refInstanceof: window.HTMLDivElement,
-    skip: ['componentProp'],
+    muiName: 'MuiDialogActions',
+    testVariantProps: { disableSpacing: true },
+    skip: ['componentProp', 'componentsProp'],
   }));
 });
