d84753bea37e45a1558aee75b928bcb7d4388a28
Marija Najdova <mnajdova@gmail.com>
2021-01-26 14:10:09 +0100

    prettier

--- packages/material-ui/src/FilledInput/filledInputClasses.js
+++ packages/material-ui/src/FilledInput/filledInputClasses.js
@@ -1,16 +1,9 @@
-import {
-  generateUtilityClasses,
-  generateUtilityClass,
-} from '@material-ui/unstyled';
+import { generateUtilityClasses, generateUtilityClass } from '@material-ui/unstyled';
 
 export function getFilledInputUtilityClass(slot) {
   return generateUtilityClass('MuiFilledInput', slot);
 }
 
-const filledInputClasses = generateUtilityClasses('MuiFilledInput', [
-  'root',
-  'underline',
-  'input',
-]);
+const filledInputClasses = generateUtilityClasses('MuiFilledInput', ['root', 'underline', 'input']);
 
 export default filledInputClasses;
