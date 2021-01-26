4c130ff5356cd211d09ed08764617f71665644d5
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-01-26 13:25:56 +0100

    add test case

--- packages/material-ui-lab/src/StaticDateRangePicker/StaticDateRangePicker.test.tsx
+++ packages/material-ui-lab/src/StaticDateRangePicker/StaticDateRangePicker.test.tsx
@@ -35,4 +35,22 @@ describe('<StaticDateRangePicker />', () => {
       ),
     ).to.have.length(31);
   });
+
+  it('should render the correct a11y tree structure', () => {
+    render(
+      <StaticDateRangePicker
+        renderInput={defaultRangeRenderInput}
+        onChange={() => {}}
+        value={[
+          adapterToUse.date('2018-01-01T00:00:00.000'),
+          adapterToUse.date('2018-01-31T00:00:00.000'),
+        ]}
+      />,
+    );
+
+    // It should follow https://www.w3.org/TR/wai-aria-practices/examples/dialog-modal/datepicker-dialog.html
+    expect(
+      document.querySelector('[role="grid"] > [role="row"] [role="cell"] > button'),
+    ).to.have.text('1');
+  });
 });
