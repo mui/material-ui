5f5625624dbbb3232d2d7da9ba735c78fb4a4371
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-01-27 15:41:45 +0100

    fix CI

--- packages/material-ui/src/Link/Link.test.js
+++ packages/material-ui/src/Link/Link.test.js
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { expect } from 'chai';
 import { spy } from 'sinon';
 import { createMount, act, createClientRender, fireEvent, describeConformanceV5 } from 'test/utils';
-import Link from './Link';
-import Typography, { typographyClasses } from '../Typography';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
 import classes from './linkClasses';
 
 function focusVisible(element) {
