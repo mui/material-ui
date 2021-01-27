32d3a0da89cc6e4d8deca05fe5399f85a30099a0
Sean Campbell <sean.campbell13@gmail.com>
2021-01-27 12:09:02 -0500

    TableBody to emotion

---
+++ packages/material-ui/src/TableBody/tableBodyClasses.d.ts
@@ -0,0 +1,9 @@
+export interface TableBodyClasses {
+  root: string;
+}
+
+declare const tableBodyClasses: TableBodyClasses;
+
+export function getTableBodyUtilityClass(slot: string): string;
+
+export default tableBodyClasses;
