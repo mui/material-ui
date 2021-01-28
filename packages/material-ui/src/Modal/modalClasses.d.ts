dcdc9743aff4a688d15b68548463ff82f8cb0e80
Sean Campbell <sean.campbell13@gmail.com>
2021-01-28 08:50:00 -0500

    remove file

--- packages/material-ui/src/Modal/modalClasses.d.ts
+++
@@ -1,9 +0,0 @@
-import { generateUtilityClass, generateUtilityClasses } from '@material-ui/unstyled';
-
-export function getChipUtilityClass(slot) {
-  return generateUtilityClass('MuiChip', slot);
-}
-
-const chipClasses = generateUtilityClasses('MuiChip', ['root']);
-
-export default chipClasses;
