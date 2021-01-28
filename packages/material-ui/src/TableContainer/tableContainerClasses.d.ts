a8324a854d614b62b9a2f410f2362423697b0070
Sean Campbell <natac13@users.noreply.github.com>
2021-01-28 02:35:26 -0500

    [Table] Migrate TableContainer to emotion (#24666)

---
+++ packages/material-ui/src/TableContainer/tableContainerClasses.d.ts
@@ -0,0 +1,9 @@
+export interface TableContainerClasses {
+  root: string;
+}
+
+declare const tableContainerClasses: TableContainerClasses;
+
+export function getTableContainerUtilityClass(slot: string): string;
+
+export default tableContainerClasses;
