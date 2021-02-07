dc05110cb3afde2abb2fac2009618e98171667c3
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-02-08 00:28:30 +0100

    [Pickers] Enable YearPicker documentation

--- docs/src/pagesApi.js
+++ docs/src/pagesApi.js
@@ -146,5 +146,6 @@ module.exports = [
   { pathname: '/api-docs/tree-view' },
   { pathname: '/api-docs/typography' },
   { pathname: '/api-docs/unstable-trap-focus' },
+  { pathname: '/api-docs/year-picker' },
   { pathname: '/api-docs/zoom' },
 ];
