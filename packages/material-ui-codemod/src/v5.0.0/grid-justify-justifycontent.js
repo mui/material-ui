5f7b75553c0225772411ff5f6174b0c47bdfdb63
Matt <github@nospam.33m.co>
2021-02-08 00:07:31 +0000

    refactor grid-justify-JustifyContent to use renameProps

--- packages/material-ui-codemod/src/v5.0.0/grid-justify-justifycontent.js
+++ packages/material-ui-codemod/src/v5.0.0/grid-justify-justifycontent.js
@@ -1,18 +1,16 @@
+import renameProps from '../util/renameProps';
+
 /**
  * @param {import('jscodeshift').FileInfo} file
  * @param {import('jscodeshift').API} api
  */
 export default function transformer(file, api) {
   const j = api.jscodeshift;
+  const root = j(file.source);
 
-  return j(file.source)
-    .findJSXElements('Grid')
-    .forEach((path) => {
-      path.node.openingElement.attributes.forEach((node) => {
-        if (node.type === 'JSXAttribute' && node.name.name === 'justify') {
-          node.name.name = 'justifyContent';
-        }
-      });
-    })
-    .toSource();
+  return renameProps({
+    root,
+    componentName: 'Grid',
+    props: { justify: 'justifyContent' },
+  }).toSource();
 }
