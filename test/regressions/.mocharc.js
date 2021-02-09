ffd340b2a8c5203d2002fc191ce34d763a6ab442
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-02-09 23:52:47 +0100

    [test] Increase timeout to 4000ms for screenshots

--- test/regressions/.mocharc.js
+++ test/regressions/.mocharc.js
@@ -1,5 +1,6 @@
 module.exports = {
   recursive: true,
+  timeout: (process.env.CIRCLECI === 'true' ? 4 : 2) * 1000, // Circle CI has low-performance CPUs.
   reporter: 'dot',
   require: [require.resolve('@babel/register')],
 };
