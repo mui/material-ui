098eb9eace1a880b21831cbe5c13d1acfc82b35b
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-02-14 22:07:12 +0100

    use box

--- docs/src/pages/components/modal/NestedModal.tsx
+++ docs/src/pages/components/modal/NestedModal.tsx
@@ -1,47 +1,32 @@
 import * as React from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import Modal from '@material-ui/core/Modal';
 
-const useStyles = makeStyles((theme: Theme) => {
-  const sharedModalStyles: React.CSSProperties = {
-    position: 'absolute',
-    maxWidth: '100%',
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-    top: `50%`,
-    left: `50%`,
-    transform: `translate(-50%, -50%)`,
-  };
-
-  return createStyles({
-    parentModal: {
-      ...sharedModalStyles,
-      width: '400px',
-    },
-    childModal: {
-      ...sharedModalStyles,
-      width: '200px',
-    },
-  });
-});
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
 
 function ChildModal() {
-  const classes = useStyles();
-
   const [open, setOpen] = React.useState(false);
-
   const handleOpen = () => {
     setOpen(true);
   };
-
   const handleClose = () => {
     setOpen(false);
   };
 
   return (
-    <div>
+    <React.Fragment>
       <button type="button" onClick={handleOpen}>
         Open Child Modal
       </button>
@@ -52,7 +37,7 @@ function ChildModal() {
         aria-labelledby="child-modal-title"
         aria-describedby="child-modal-description"
       >
-        <div className={classes.childModal}>
+        <Box sx={{ ...style, width: 200 }}>
           <h2 id="child-modal-title">Text in a child modal</h2>
           <p id="child-modal-description">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -60,21 +45,17 @@ function ChildModal() {
           <button type="button" onClick={handleClose}>
             Close Child Modal
           </button>
-        </div>
+        </Box>
       </Modal>
-    </div>
+    </React.Fragment>
   );
 }
 
-export default function ParentModal() {
-  const classes = useStyles();
-
+export default function NestedModal() {
   const [open, setOpen] = React.useState(false);
-
   const handleOpen = () => {
     setOpen(true);
   };
-
   const handleClose = () => {
     setOpen(false);
   };
@@ -90,13 +71,13 @@ export default function ParentModal() {
         aria-labelledby="parent-modal-title"
         aria-describedby="parent-modal-description"
       >
-        <div className={classes.parentModal}>
+        <Box sx={{ ...style, width: 400 }}>
           <h2 id="parent-modal-title">Text in a modal</h2>
           <p id="parent-modal-description">
             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
           </p>
           <ChildModal />
-        </div>
+        </Box>
       </Modal>
     </div>
   );
