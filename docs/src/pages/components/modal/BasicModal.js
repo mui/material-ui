e1145163cb245c27afae8e6afa0a0ef8a287d010
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-02-14 22:02:46 +0100

    use box

--- docs/src/pages/components/modal/BasicModal.js
+++ docs/src/pages/components/modal/BasicModal.js
@@ -1,23 +1,22 @@
 import * as React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import Modal from '@material-ui/core/Modal';
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: 'absolute',
-    top: `50%`,
-    left: `50%`,
-    transform: `translate(-50%, -50%)`,
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
 
 export default function BasicModal() {
-  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
 
   const handleOpen = () => {
@@ -39,12 +38,12 @@ export default function BasicModal() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <div className={classes.paper}>
+        <Box sx={style}>
           <h2 id="modal-modal-title">Text in a modal</h2>
           <p id="modal-modal-description">
             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
           </p>
-        </div>
+        </Box>
       </Modal>
     </div>
   );
