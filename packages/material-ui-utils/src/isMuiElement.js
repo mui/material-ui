18e81686ad6eee080a1ec1b22ac9c3c7168eb00a
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-02-14 21:12:47 +0100

    [utils] Fix isMuiElement types (#24936)

--- packages/material-ui-utils/src/isMuiElement.js
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-export default function isMuiElement(element, muiNames) {
-  return React.isValidElement(element) && muiNames.indexOf(element.type.muiName) !== -1;
-}
