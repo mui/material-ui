397f6d6d6a0678a522972fd9a65e048733721b4e
eps1lon <silbermann.sebastian@gmail.com>
2021-02-15 18:08:05 +0100

    [test] debug slow listChangedFiles

--- scripts/listChangedFiles.test.js
+++ scripts/listChangedFiles.test.js
@@ -8,17 +8,31 @@ const writeFileAsync = promisify(fs.writeFile);
 const rimrafAsync = promisify(rimraf);
 
 describe('listChangedFiles', () => {
+  // This test oftentimes times out after 4s but it's unclear why considering `listChangedFiles` is used in CI tasks that only take <1s.
+  const noop = () => {};
+  // eslint-disable-next-line no-console
+  const time = process.env.CI ? console.time : noop;
+  // eslint-disable-next-line no-console
+  const timeLog = process.env.CI ? console.timeLog : noop;
+  // eslint-disable-next-line no-console
+  const timeEnd = process.env.CI ? console.timeEnd : noop;
   it('should detect changes', async () => {
+    time();
     const changesBeforeAdd = await listChangedFiles({ branch: 'next' });
+    timeLog();
     const testFile = 'someTestFile.yml';
     try {
       await writeFileAsync(testFile, 'console.log("hello");');
+      timeLog();
       const changesAfterAdd = await listChangedFiles({ branch: 'next' });
+      timeLog();
       expect(changesBeforeAdd).not.to.contain(testFile);
       expect(changesAfterAdd).to.contain(testFile);
       expect(changesAfterAdd.size - changesBeforeAdd.size).to.equal(1);
+      timeLog();
     } finally {
       await rimrafAsync(testFile);
+      timeEnd();
     }
   });
 });
