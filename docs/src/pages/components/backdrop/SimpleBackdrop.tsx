b36062e6ac9f96622c7aa35456b064ce8148df39
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-02-18 01:25:09 +0100

    polish backdrop docs

--- docs/src/pages/components/backdrop/SimpleBackdrop.tsx
+++ docs/src/pages/components/backdrop/SimpleBackdrop.tsx
@@ -2,19 +2,8 @@ import * as React from 'react';
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Button from '@material-ui/core/Button';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    backdrop: {
-      zIndex: theme.zIndex.drawer + 1,
-      color: '#fff',
-    },
-  }),
-);
 
 export default function SimpleBackdrop() {
-  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const handleClose = () => {
     setOpen(false);
@@ -25,10 +14,12 @@ export default function SimpleBackdrop() {
 
   return (
     <div>
-      <Button variant="outlined" onClick={handleToggle}>
-        Show backdrop
-      </Button>
-      <Backdrop className={classes.backdrop} open={open} onClick={handleClose}>
+      <Button onClick={handleToggle}>Show backdrop</Button>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        onClick={handleClose}
+      >
         <CircularProgress color="inherit" />
       </Backdrop>
     </div>
