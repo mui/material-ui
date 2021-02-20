f77cf86f85b00a6fafce01c59fd45a68bf4b0410
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-02-20 12:19:44 +0100

    logging all the tests in the CI allows to spot slow tests

--- test/regressions/.mocharc.js
+++ test/regressions/.mocharc.js
@@ -2,6 +2,5 @@ module.exports = {
   recursive: true,
   slow: 500,
   timeout: (process.env.CIRCLECI === 'true' ? 4 : 2) * 1000, // Circle CI has low-performance CPUs.
-  reporter: 'dot',
   require: [require.resolve('@babel/register')],
 };
