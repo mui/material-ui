1c34d9bbb5f3218969a651c95b6ece7af55b46d1
eps1lon <silbermann.sebastian@gmail.com>
2021-02-24 18:17:42 +0100

    yarn docs:typescript:formatted

--- docs/src/pages/components/date-range-picker/StaticDateRangePickerDemo.js
+++ docs/src/pages/components/date-range-picker/StaticDateRangePickerDemo.js
@@ -5,7 +5,7 @@ import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
 import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
 import Box from '@material-ui/core/Box';
 
-export default function StaticDateRangePickerExample() {
+export default function StaticDateRangePickerDemo() {
   const [value, setValue] = React.useState([null, null]);
 
   return (
