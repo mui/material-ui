6edc6bb2a4de8c28e9ff043523cb40013c863d11
Lucas Breitembach Rodrigues <lucas.rodrigues@bancointer.com.br>
2021-02-25 14:52:36 -0300

    added test for dateTimePicker

--- packages/material-ui-lab/src/DateTimePicker/DateTimePicker.test.tsx
+++ packages/material-ui-lab/src/DateTimePicker/DateTimePicker.test.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import TextField from '@material-ui/core/TextField';
 import DateTimePicker from '@material-ui/lab/DateTimePicker';
-import { createPickerRender } from '../internal/pickers/test-utils';
+import { expect } from 'chai';
+import {  createPickerRender, getByMuiTest } from '../internal/pickers/test-utils';
 
 describe('<DateTimePicker />', () => {
   const render = createPickerRender();
 
-  // TODO: Write tests for responsive pickers. This test should be removed after adding actual tests.
   it('renders without crashing', () => {
     render(
       <DateTimePicker
@@ -16,4 +16,46 @@ describe('<DateTimePicker />', () => {
       />,
     );
   });
+
+  it('should be render seconds on view', () => {
+    const date = new Date(2021, 10, 20, 10, 1, 22)
+    render(
+      <DateTimePicker
+        renderInput={(params) => <TextField {...params} />}
+        onChange={() => {}}
+        open={Boolean(true)}
+        views={['seconds']}
+        value={date} 
+      />);
+      expect(getByMuiTest('seconds')).to.have.text('22');
+      
+  });
+
+  it('should not be render seconds by default', () => {
+    const date = new Date(2021, 10, 20, 10, 1, 22)
+    render(
+      <DateTimePicker
+        renderInput={(params) => <TextField {...params} />}
+        onChange={() => {}}
+        open={Boolean(true)}
+        value={date} 
+      />);
+      expect(() => getByMuiTest('seconds')).throw('Unable to find an element');
+  });
+
+  it('should be render date and time by default', () => {
+    const date = new Date(2021, 10, 20, 10, 1, 22)
+    render(
+      <DateTimePicker
+        renderInput={(params) => <TextField {...params} />}
+        onChange={() => {}}
+        open={Boolean(true)}
+        value={date} 
+      />);
+      expect(getByMuiTest('hours')).to.have.text('10')
+      expect(getByMuiTest('minutes')).to.have.text('01')
+      expect(getByMuiTest('datetimepicker-toolbar-year')).to.have.text('2021')
+      expect(getByMuiTest('datetimepicker-toolbar-date')).to.have.text('Nov 20')
+  });
+
 });
