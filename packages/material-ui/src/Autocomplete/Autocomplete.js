8cc9fbb3b7349cc11820b24524078665e315b139
Daniel Sallai <daniel_sallai@epam.com>
2021-02-26 20:46:03 +0100

    [AutoComplete] prevent closing on no option click

--- packages/material-ui/src/Autocomplete/Autocomplete.js
+++ packages/material-ui/src/Autocomplete/Autocomplete.js
@@ -644,7 +644,14 @@ const Autocomplete = React.forwardRef(function Autocomplete(inProps, ref) {
               </AutocompleteLoading>
             ) : null}
             {groupedOptions.length === 0 && !freeSolo && !loading ? (
-              <AutocompleteNoOptions className={classes.noOptions} styleProps={styleProps}>
+              <AutocompleteNoOptions
+                className={classes.noOptions}
+                styleProps={styleProps}
+                role="presentation"
+                onMouseDown={(event) => {
+                  event.preventDefault();
+                }}
+              >
                 {noOptionsText}
               </AutocompleteNoOptions>
             ) : null}
