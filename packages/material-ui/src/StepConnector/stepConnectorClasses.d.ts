d2f27732c6d475850ab3042b532c8c3997ebd8cc
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-02-28 22:57:55 +0100

    no duplication

--- packages/material-ui/src/StepConnector/stepConnectorClasses.d.ts
+++ packages/material-ui/src/StepConnector/stepConnectorClasses.d.ts
@@ -1,17 +1,6 @@
-export interface StepConnectorClasses {
-  root: string;
-  horizontal: string;
-  vertical: string;
-  alternativeLabel: string;
-  active: string;
-  completed: string;
-  disabled: string;
-  line: string;
-  lineHorizontal: string;
-  lineVertical: string;
-}
+import { StepConnectorClasskey } from './StepConnector';
 
-declare const stepConnectorClasses: StepConnectorClasses;
+declare const stepConnectorClasses: Record<StepConnectorClasskey, string>;
 
 export function getStepConnectorUtilityClass(slot: string): string;
 
