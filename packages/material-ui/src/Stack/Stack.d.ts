3e8a707bfa532599dd41e6ce272e86589ceec3c8
Travis Arnold <ftntravis@gmail.com>
2021-03-03 18:14:12 -0800

    remove StackSpacing type
    Co-authored-by: Olivier Tassinari <olivier.tassinari@gmail.com>

--- packages/material-ui/src/Stack/Stack.d.ts
+++ packages/material-ui/src/Stack/Stack.d.ts
@@ -3,7 +3,6 @@ import { SystemProps, SxProps } from '@material-ui/system';
 import { OverrideProps, OverridableComponent } from '../OverridableComponent';
 import { Theme } from '../styles/createMuiTheme';
 
-export type StackSpacing = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
 export interface StackTypeMap<P = {}, D extends React.ElementType = 'div'> {
   props: P &
