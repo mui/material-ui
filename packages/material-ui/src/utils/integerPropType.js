b00b9a98bf55c92859c5fa47501e7ffaa5a7036e
fayzzzm <m.fayzov1@gmail.com>
2021-03-07 04:04:45 +0500

    Has written integerCustomType and tests.

---
+++ packages/material-ui/src/utils/integerPropType.js
@@ -0,0 +1,38 @@
+// IE 11 support
+function ponyfillIsInteger(x) {
+  // eslint-disable-next-line no-restricted-globals
+  return typeof x === 'number' && isFinite(x) && Math.floor(x) === x;
+}
+
+const isInteger = Number.isInteger || ponyfillIsInteger;
+
+function requiredInteger(props, propName, componentName, location, propFullName) {
+  const propValue = props[propName];
+
+  if (propValue == null || !isInteger(propValue)) {
+    const propType = typeof propValue;
+    return new RangeError(
+      `Invalid ${location} \`${propFullName}\` of type \`${propType}\` supplied to \`${componentName}\`, expected \`integer\`.`);
+  }
+
+  return null;
+}
+
+function validator(props, propName, ...rest) {
+  const propValue = props[propName];
+
+  if (propValue === undefined) {
+    return null;
+  }
+  
+  return requiredInteger(props, propName, ...rest);
+}
+
+function validatorNoop() {
+  return null;
+}
+
+validator.isRequired = requiredInteger;
+validatorNoop.isRequired = validatorNoop;
+
+export default process.env.NODE_ENV === 'production' ? validatorNoop : validator;
---
+++ packages/material-ui/src/utils/integerPropType.test.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { expect } from 'chai';
+
+import integerPropType  from './integerPropType';
+
+function callValidator(
+  validator,
+  { props },
+  propName = '',
+  componentName = '',
+  location = '',
+  propFullName = '',
+) {
+  return validator(props, propName, componentName, location, propFullName);
+}
+
+describe('integer', () => {
+  function assertPasses(validator, element, propName) {
+    expect(callValidator(validator, element, propName, 'integer test')).to.equal(null);
+  }
+
+  function assertFails(validator, element, propName) {
+    expect(callValidator(validator, element, propName, 'integer test')).to.be.instanceOf(Error);
+  }
+
+  it('Passes on undefined but failes on null value', () => {
+    assertPasses(integerPropType, <div a={undefined} />, 'a');
+    assertFails(integerPropType, <div a={null} />, 'a');
+  });
+
+  it('Passes on zero', () => assertPasses(integerPropType, <div a={0} />, 'a'));
+
+  it('Passes on positive numbers', () => {
+    assertPasses(integerPropType, <div a={1} />, 'a');
+    assertPasses(integerPropType, <div a={42} />, 'a');
+  });
+
+  it('Passes with coversion before passing', () => {
+      assertPasses(integerPropType, <div a={parseInt(1.1, 10)} />, "a");
+      assertPasses(integerPropType, <div a={Math.floor(1.1)} />, "a");
+      // eslint-disable-next-line no-bitwise
+      assertPasses(integerPropType, <div a={1.1 | 0}/>, "a")
+  })
+
+  it('Passes on negative numbers', () => {
+    assertPasses(integerPropType, <div a={-1} />, 'a');
+    assertPasses(integerPropType, <div a={-42} />, 'a');
+  });
+
+  it('Fails on non-integers', () => {
+    assertFails(integerPropType, <div a={1.5} />, 'a');
+    assertFails(integerPropType, <div a={1.999999} />, 'a');
+    assertFails(integerPropType, <div a={(0.1 + 0.2) * 10} />, 'a');
+    assertFails(integerPropType, <div a={'a message'} />, 'a');
+    assertFails(integerPropType, <div a={false} />, 'a');
+    assertFails(integerPropType, <div a={null} />, 'a');
+    assertFails(integerPropType, <div a={[]} />, 'a');
+  });
+
+  it('Fails on number edge cases', () => {
+    assertFails(integerPropType, <div a={-Infinity} />, 'a');
+    assertFails(integerPropType, <div a={Infinity} />, 'a');
+    assertFails(integerPropType, <div a={NaN} />, 'a');
+  });
+
+  describe('isRequired', () => {
+    it('Passes when not required', () => assertPasses(integerPropType, <div />, 'a'));
+
+    it('Fails when required', () => assertFails(integerPropType.isRequired, <div />, 'a'));
+  });
+});
