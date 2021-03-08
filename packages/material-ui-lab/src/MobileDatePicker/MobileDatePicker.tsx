a5b52c59c5393bfa4e25cd287b767cac02098b89
eps1lon <silbermann.sebastian@gmail.com>
2021-03-08 13:28:03 +0100

    [MobileDatePicker] Inline `makeWrapper` call

--- packages/material-ui-lab/src/MobileDatePicker/MobileDatePicker.tsx
+++ packages/material-ui-lab/src/MobileDatePicker/MobileDatePicker.tsx
@@ -16,10 +16,16 @@ import { ParsableDate } from '../internal/pickers/constants/prop-types';
 import { MuiPickersAdapter } from '../internal/pickers/hooks/useUtils';
 import { parsePickerInputValue } from '../internal/pickers/date-utils';
 import { KeyboardDateInput } from '../internal/pickers/KeyboardDateInput';
-import { makeWrapperComponent } from '../internal/pickers/wrappers/makeWrapperComponent';
 import { PureDateInput } from '../internal/pickers/PureDateInput';
 import { usePickerState, PickerStateValueManager } from '../internal/pickers/hooks/usePickerState';
 import { AllSharedPickerProps } from '../internal/pickers/Picker/SharedPickerProps';
+import { BasePickerProps } from '../internal/pickers/typings/BasePicker';
+import { ResponsiveWrapperProps } from '../internal/pickers/wrappers/ResponsiveWrapper';
+import {
+  StaticWrapperProps,
+  DateInputPropsLike,
+  WrapperProps,
+} from '../internal/pickers/wrappers/WrapperProps';
 
 type AllPickerProps<T, TWrapper extends SomeWrapper = SomeWrapper> = T &
   AllSharedPickerProps &
@@ -36,11 +42,63 @@ type TWrapper = typeof Wrapper;
 const name = 'MuiMobileDatePicker';
 type T = BaseDatePickerProps<unknown>;
 const { DefaultToolbarComponent, useInterceptProps, useValidation } = datePickerConfig;
+const KeyboardDateInputComponent = KeyboardDateInput;
+const PureDateInputComponent = PureDateInput;
 
-const WrapperComponent = makeWrapperComponent(Wrapper, {
-  KeyboardDateInputComponent: KeyboardDateInput,
-  PureDateInputComponent: PureDateInput,
-});
+interface WithWrapperProps {
+  children: React.ReactNode;
+  DateInputProps: DateInputPropsLike;
+  wrapperProps: Omit<WrapperProps, 'DateInputProps'>;
+}
+
+function MobileDatePickerWrapper(
+  props: Partial<BasePickerProps<any, any>> &
+    WithWrapperProps &
+    ResponsiveWrapperProps &
+    StaticWrapperProps,
+) {
+  const {
+    disableCloseOnSelect,
+    cancelText,
+    clearable,
+    clearText,
+    DateInputProps,
+    DialogProps,
+    displayStaticWrapperAs,
+    inputFormat,
+    okText,
+    onAccept,
+    onChange,
+    onClose,
+    onOpen,
+    open,
+    PopperProps,
+    todayText,
+    value,
+    wrapperProps,
+    ...other
+  } = props;
+
+  const TypedWrapper = Wrapper as SomeWrapper;
+
+  return (
+    <TypedWrapper
+      clearable={clearable}
+      clearText={clearText}
+      DialogProps={DialogProps}
+      PopperProps={PopperProps}
+      okText={okText}
+      todayText={todayText}
+      cancelText={cancelText}
+      DateInputProps={DateInputProps}
+      KeyboardDateInputComponent={KeyboardDateInputComponent}
+      PureDateInputComponent={PureDateInputComponent}
+      displayStaticWrapperAs={displayStaticWrapperAs}
+      {...wrapperProps}
+      {...other}
+    />
+  );
+}
 
 /**
  *
@@ -69,7 +127,11 @@ const MobileDatePicker = React.forwardRef(function MobileDatePicker<TDate>(
   const AllDateInputProps = { ...inputProps, ...other, ref, validationError };
 
   return (
-    <WrapperComponent wrapperProps={wrapperProps} DateInputProps={AllDateInputProps} {...other}>
+    <MobileDatePickerWrapper
+      wrapperProps={wrapperProps}
+      DateInputProps={AllDateInputProps}
+      {...other}
+    >
       <Picker
         {...pickerProps}
         toolbarTitle={props.label || props.toolbarTitle}
@@ -77,7 +139,7 @@ const MobileDatePicker = React.forwardRef(function MobileDatePicker<TDate>(
         DateInputProps={AllDateInputProps}
         {...other}
       />
-    </WrapperComponent>
+    </MobileDatePickerWrapper>
   );
 }) as DatePickerGenericComponent<TWrapper>;
 
