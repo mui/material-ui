43c7839e5bbad839c6ef5cb0fb97eb638e5490ef
cong <zhicong.tay@gmail.com>
2021-03-09 22:18:28 +0800

    fix typescript check

--- packages/material-ui/src/InputAdornment/index.d.ts
+++ packages/material-ui/src/InputAdornment/index.d.ts
@@ -1,4 +1,4 @@
+export * from './InputAdornment';
 export { default } from './InputAdornment';
-
 export { default as inputAdornmentClasses } from './inputAdornmentClasses';
 export * from './inputAdornmentClasses';
