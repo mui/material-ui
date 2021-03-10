c45fe82a5e9f335f0ea3869ef75c80bc48dc3399
Victor Casas <victor.casas@outlook.cl>
2021-03-10 18:36:46 -0300

    fix: Remove comment eslint

--- docs/src/pages/components/links/UnderlineLink.tsx
+++ docs/src/pages/components/links/UnderlineLink.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import * as React from 'react';
 import { experimentalStyled as styled } from '@material-ui/core/styles';
