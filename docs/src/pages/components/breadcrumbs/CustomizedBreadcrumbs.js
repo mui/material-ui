210593006c9b4238e7c3eda11a21e4d133fa6c7c
Victor Casas <victor.casas@outlook.cl>
2021-03-10 18:19:29 -0300

    docs: Migrate customized breadcrumbs demo to emotion

--- docs/src/pages/components/breadcrumbs/CustomizedBreadcrumbs.js
+++ docs/src/pages/components/breadcrumbs/CustomizedBreadcrumbs.js
@@ -1,25 +1,23 @@
 import * as React from 'react';
-import { emphasize, withStyles } from '@material-ui/core/styles';
+import { emphasize, experimentalStyled as styled } from '@material-ui/core/styles';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Chip from '@material-ui/core/Chip';
 import HomeIcon from '@material-ui/icons/Home';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const StyledBreadcrumb = withStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.grey[100],
-    height: theme.spacing(3),
-    color: theme.palette.grey[800],
-    fontWeight: theme.typography.fontWeightRegular,
-    '&:hover, &:focus': {
-      backgroundColor: theme.palette.grey[300],
-    },
-    '&:active': {
-      boxShadow: theme.shadows[1],
-      backgroundColor: emphasize(theme.palette.grey[300], 0.12),
-    },
+const StyledBreadcrumb = styled(Chip)(({ theme }) => ({
+  backgroundColor: theme.palette.grey[100],
+  height: theme.spacing(3),
+  color: theme.palette.grey[800],
+  fontWeight: theme.typography.fontWeightRegular,
+  '&:hover, &:focus': {
+    backgroundColor: theme.palette.grey[300],
   },
-}))(Chip); // TypeScript only: need a type cast here because https://github.com/Microsoft/TypeScript/issues/26591
+  '&:active': {
+    boxShadow: theme.shadows[1],
+    backgroundColor: emphasize(theme.palette.grey[300], 0.12),
+  },
+})); // TypeScript only: need a type cast here because https://github.com/Microsoft/TypeScript/issues/26591
 
 function handleClick(event) {
   event.preventDefault();
