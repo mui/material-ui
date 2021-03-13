3c369a277e37cfde8c6bbc644b7c652abea80443
Victor Casas <victor.casas@outlook.cl>
2021-03-13 13:01:55 -0300

    docs: Migrate grid whit multiple breakpoint demo to emotion

--- docs/src/pages/components/grid/FullWidthGrid.js
+++ docs/src/pages/components/grid/FullWidthGrid.js
@@ -1,47 +1,48 @@
 import * as React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
-
 export default function FullWidthGrid() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
+          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
+            xs=12
+          </Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
+            xs=12 sm=6
+          </Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
+            xs=12 sm=6
+          </Paper>
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
+            xs=6 sm=3
+          </Paper>
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
+            xs=6 sm=3
+          </Paper>
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
+            xs=6 sm=3
+          </Paper>
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
+            xs=6 sm=3
+          </Paper>
         </Grid>
       </Grid>
-    </div>
+    </Box>
   );
 }
