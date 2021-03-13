443a7a3b32bb4f8d3b59a48f57f65d5180ac84d4
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-03-14 00:10:24 +0100

    give a try to the styled API

--- docs/src/pages/components/grid/CenteredGrid.js
+++ docs/src/pages/components/grid/CenteredGrid.js
@@ -1,46 +1,39 @@
 import * as React from 'react';
-import Box from '@material-ui/core/Box';
+import { experimentalStyled as styled } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export default function CenteredGrid() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
-            xs=12
-          </Paper>
+          <Item>xs=12</Item>
         </Grid>
         <Grid item xs={6}>
-          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
-            xs=6
-          </Paper>
+          <Item>xs=6</Item>
         </Grid>
         <Grid item xs={6}>
-          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
-            xs=6
-          </Paper>
+          <Item>xs=6</Item>
         </Grid>
         <Grid item xs={3}>
-          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
-            xs=3
-          </Paper>
+          <Item>xs=3</Item>
         </Grid>
         <Grid item xs={3}>
-          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
-            xs=3
-          </Paper>
+          <Item>xs=3</Item>
         </Grid>
         <Grid item xs={3}>
-          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
-            xs=3
-          </Paper>
+          <Item>xs=3</Item>
         </Grid>
         <Grid item xs={3}>
-          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
-            xs=3
-          </Paper>
+          <Item>xs=3</Item>
         </Grid>
       </Grid>
     </Box>
