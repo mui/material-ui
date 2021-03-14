a05a4e7865eae9e1dc2afa2f4c2c8101328fb6aa
Victor Casas <victor.casas@outlook.cl>
2021-03-14 09:24:49 -0300

    refactor: Using api styled for AutoGrid demo

--- docs/src/pages/components/grid/AutoGrid.tsx
+++ docs/src/pages/components/grid/AutoGrid.tsx
@@ -1,44 +1,39 @@
 import * as React from 'react';
+import { experimentalStyled as styled } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export default function AutoGrid() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={3}>
         <Grid item xs>
-          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
-            xs
-          </Paper>
+          <Item>xs</Item>
         </Grid>
         <Grid item xs>
-          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
-            xs
-          </Paper>
+          <Item>xs</Item>
         </Grid>
         <Grid item xs>
-          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
-            xs
-          </Paper>
+          <Item>xs</Item>
         </Grid>
       </Grid>
       <br />
       <Grid container spacing={3}>
         <Grid item xs>
-          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
-            xs
-          </Paper>
+          <Item>xs</Item>
         </Grid>
         <Grid item xs={6}>
-          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
-            xs=6
-          </Paper>
+          <Item>xs=6</Item>
         </Grid>
         <Grid item xs>
-          <Paper sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
-            xs
-          </Paper>
+          <Item>xs</Item>
         </Grid>
       </Grid>
     </Box>
