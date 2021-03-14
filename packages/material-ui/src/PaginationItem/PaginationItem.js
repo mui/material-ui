c805c8baf62fe30cacf3f3654d85d0f40ffd31f3
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-03-14 14:49:38 +0100

    fix ci

--- packages/material-ui/src/PaginationItem/PaginationItem.js
+++ packages/material-ui/src/PaginationItem/PaginationItem.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-import { deepmerge, integerPropType } from '@material-ui/utils';
+import { deepmerge } from '@material-ui/utils';
 import { unstable_composeClasses as composeClasses } from '@material-ui/unstyled';
 import useThemeProps from '../styles/useThemeProps';
 import paginationItemClasses, { getPaginationItemUtilityClass } from './paginationItemClasses';
