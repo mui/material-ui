e452c7eee56c61261d87318624ef48164345441d
Victor Casas <victor.casas@outlook.cl>
2021-03-14 09:15:07 -0300

    Helper component is defined outside the render method

--- docs/src/pages/components/grid/NestedGrid.tsx
+++ docs/src/pages/components/grid/NestedGrid.tsx
@@ -3,29 +3,29 @@ import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-export default function NestedGrid() {
-  function FormRow() {
-    return (
-      <React.Fragment>
-        <Grid item xs={4}>
-          <Paper sx={{ p: 1, textAlign: 'center', color: 'text.secondary' }}>
-            item
-          </Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper sx={{ p: 1, textAlign: 'center', color: 'text.secondary' }}>
-            item
-          </Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper sx={{ p: 1, textAlign: 'center', color: 'text.secondary' }}>
-            item
-          </Paper>
-        </Grid>
-      </React.Fragment>
-    );
-  }
+function FormRow() {
+  return (
+    <React.Fragment>
+      <Grid item xs={4}>
+        <Paper sx={{ p: 1, textAlign: 'center', color: 'text.secondary' }}>
+          item
+        </Paper>
+      </Grid>
+      <Grid item xs={4}>
+        <Paper sx={{ p: 1, textAlign: 'center', color: 'text.secondary' }}>
+          item
+        </Paper>
+      </Grid>
+      <Grid item xs={4}>
+        <Paper sx={{ p: 1, textAlign: 'center', color: 'text.secondary' }}>
+          item
+        </Paper>
+      </Grid>
+    </React.Fragment>
+  );
+}
 
+export default function NestedGrid() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={1}>
