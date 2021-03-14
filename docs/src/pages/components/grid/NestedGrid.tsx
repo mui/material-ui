1af5f0e44b026846215188c1c234021315c5f97a
Victor Casas <victor.casas@outlook.cl>
2021-03-14 09:38:09 -0300

    refactor: Using api styled for NestedGrid demo

--- docs/src/pages/components/grid/NestedGrid.tsx
+++ docs/src/pages/components/grid/NestedGrid.tsx
@@ -1,25 +1,26 @@
 import * as React from 'react';
+import { experimentalStyled as styled } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function FormRow() {
   return (
     <React.Fragment>
       <Grid item xs={4}>
-        <Paper sx={{ p: 1, textAlign: 'center', color: 'text.secondary' }}>
-          item
-        </Paper>
+        <Item>item</Item>
       </Grid>
       <Grid item xs={4}>
-        <Paper sx={{ p: 1, textAlign: 'center', color: 'text.secondary' }}>
-          item
-        </Paper>
+        <Item>item</Item>
       </Grid>
       <Grid item xs={4}>
-        <Paper sx={{ p: 1, textAlign: 'center', color: 'text.secondary' }}>
-          item
-        </Paper>
+        <Item>item</Item>
       </Grid>
     </React.Fragment>
   );
