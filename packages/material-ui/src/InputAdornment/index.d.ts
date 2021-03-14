5be4cd9c6c5ae5cd1eff7f112d350a0a321210f1
kayuapi <kayuapi@users.noreply.github.com>
2021-03-15 03:46:49 +0800

    [TextField] Migrate InputAdornment to emotion (#25279)

--- packages/material-ui/src/InputAdornment/index.d.ts
+++ packages/material-ui/src/InputAdornment/index.d.ts
@@ -1,2 +1,5 @@
-export { default } from './InputAdornment';
 export * from './InputAdornment';
+export { default } from './InputAdornment';
+
+export { default as inputAdornmentClasses } from './inputAdornmentClasses';
+export * from './inputAdornmentClasses';
