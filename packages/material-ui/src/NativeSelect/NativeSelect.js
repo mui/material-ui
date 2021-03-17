533dc743794fd7e4f49d516f8a107787b54ac314
Christiaan Baartse <cbaartse@leviy.com>
2021-03-17 12:21:16 +0100

    [NativeSelect] Fix focus background when variant="outlined" (#25392)

--- packages/material-ui/src/NativeSelect/NativeSelect.js
+++ packages/material-ui/src/NativeSelect/NativeSelect.js
@@ -53,13 +53,16 @@ export const styles = (theme) => ({
   /* Styles applied to the select component if `variant="outlined"`. */
   outlined: {
     borderRadius: theme.shape.borderRadius,
+    '&:focus': {
+      borderRadius: theme.shape.borderRadius, // Reset the reset for Chrome style
+    },
     '&&': {
       paddingRight: 32,
     },
   },
   /* Styles applied to the select component `selectMenu` class. */
   selectMenu: {
-    height: 'auto', // Resets for multpile select with chips
+    height: 'auto', // Resets for multiple select with chips
     minHeight: '1.4375em', // Required for select\text-field height consistency
     textOverflow: 'ellipsis',
     whiteSpace: 'nowrap',
