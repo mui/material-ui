edef55c5ca7f2c1dd8532a5caf1efd5691938e77
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-03-17 22:21:24 +0100

    fix tests

--- packages/material-ui/src/Stack/Stack.test.js
+++ packages/material-ui/src/Stack/Stack.test.js
@@ -1,13 +1,17 @@
 import * as React from 'react';
-import { createMount, describeConformanceV5 } from 'test/utils';
+import { createMount, createClientRender, describeConformanceV5 } from 'test/utils';
 import Stack from '@material-ui/core/Stack';
 
 describe('<Stack />', () => {
+  const render = createClientRender();
   const mount = createMount();
 
   describeConformanceV5(<Stack />, () => ({
+    render,
+    inheritComponent: 'div',
     mount,
     refInstanceof: window.HTMLDivElement,
     muiName: 'MuiStack',
+    skip: ['componentProp', 'componentsProp', 'rootClass', 'themeVariants', 'themeStyleOverrides'],
   }));
 });
