23d1154801e047d66497a19a747f54840e6b9f1b
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-03-18 19:44:21 +0100

    fix focus visible regression

--- docs/src/pages/components/radio-buttons/CustomizedRadios.tsx
+++ docs/src/pages/components/radio-buttons/CustomizedRadios.tsx
@@ -13,7 +13,7 @@ const Icon = styled('span')({
   boxShadow: 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
   backgroundColor: '#f5f8fa',
   backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
-  '.root.Mui-focusVisible &': {
+  '.Mui-focusVisible &': {
     outline: '2px auto rgba(19,124,189,.6)',
     outlineOffset: 2,
   },
