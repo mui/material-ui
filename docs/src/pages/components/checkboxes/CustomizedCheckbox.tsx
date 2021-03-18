b5f9577c4b2237442ff8d00c294d9d5ac94b4c66
Victor Casas <victor.casas@outlook.cl>
2021-03-18 10:22:05 -0300

    Fix focusVisible

--- docs/src/pages/components/checkboxes/CustomizedCheckbox.tsx
+++ docs/src/pages/components/checkboxes/CustomizedCheckbox.tsx
@@ -9,14 +9,14 @@ const Icon = styled('span')({
   boxShadow: 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
   backgroundColor: '#f5f8fa',
   backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
-  '.root.Mui-focusVisible &': {
+  '.Mui-focusVisible &': {
     outline: '2px auto rgba(19,124,189,.6)',
     outlineOffset: 2,
   },
-  'input:hover ~ &': {
+  '& input:hover ~ &': {
     backgroundColor: '#ebf1f5',
   },
-  'input:disabled ~ &': {
+  '& input:disabled ~ &': {
     boxShadow: 'none',
     background: 'rgba(206,217,224,.5)',
   },
@@ -35,7 +35,7 @@ const CheckedIcon = styled(Icon)({
       "1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E\")",
     content: '""',
   },
-  'input:hover ~ &': {
+  '& input:hover ~ &': {
     backgroundColor: '#106ba3',
   },
 });
