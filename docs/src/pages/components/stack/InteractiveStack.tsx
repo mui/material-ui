4ebb2dbd72220e2ef725b3a105ff40eac7add57e
Travis Arnold <ftntravis@gmail.com>
2021-03-17 17:45:47 -0700

    use shorthand values
    Co-authored-by: Olivier Tassinari <olivier.tassinari@gmail.com>

--- docs/src/pages/components/stack/InteractiveStack.tsx
+++ docs/src/pages/components/stack/InteractiveStack.tsx
@@ -47,7 +47,7 @@ export default function InteractiveStack() {
           </Paper>
         ))}
       </Stack>
-      <Paper sx={{ padding: 2 }}>
+      <Paper sx={{ p: 2 }}>
         <Grid container spacing={3}>
           <Grid item xs={12}>
             <FormControl component="fieldset">
