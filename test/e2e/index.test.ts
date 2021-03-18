2b591acddb8304460715cc8806cc407e55da43c7
eps1lon <silbermann.sebastian@gmail.com>
2021-03-18 16:16:34 +0100

    Move userEvent.tab tests to e2e

--- test/e2e/index.test.ts
+++ test/e2e/index.test.ts
@@ -21,4 +21,55 @@ describe('e2e', () => {
   after(async () => {
     await browser.close();
   });
+
+  describe('<TrapFocus />', () => {
+    it('should loop the tab key', () => {
+      render(
+        <TrapFocus {...defaultProps} open>
+          <div tabIndex={-1} data-testid="root">
+            <div>Title</div>
+            <button type="button">x</button>
+            <button type="button">cancel</button>
+            <button type="button">ok</button>
+          </div>
+        </TrapFocus>,
+      );
+      expect(screen.getByTestId('root')).toHaveFocus();
+
+      userEvent.tab();
+      expect(screen.getByText('x')).toHaveFocus();
+      userEvent.tab();
+      expect(screen.getByText('cancel')).toHaveFocus();
+      userEvent.tab();
+      expect(screen.getByText('ok')).toHaveFocus();
+      userEvent.tab();
+      expect(screen.getByText('x')).toHaveFocus();
+
+      initialFocus.focus();
+      expect(screen.getByTestId('root')).toHaveFocus();
+      screen.getByText('x').focus();
+      userEvent.tab({ shift: true });
+      expect(screen.getByText('ok')).toHaveFocus();
+    });
+
+    it('should focus on first focus element after last has received a tab click', async () => {
+      render(
+        <TrapFocus {...defaultProps} open>
+          <div tabIndex={-1} data-testid="root">
+            <div>Title</div>
+            <button type="button">x</button>
+            <button type="button">cancel</button>
+            <button type="button">ok</button>
+          </div>
+        </TrapFocus>,
+      );
+
+      userEvent.tab();
+      expect(screen.getByText('x')).toHaveFocus();
+      userEvent.tab();
+      expect(screen.getByText('cancel')).toHaveFocus();
+      userEvent.tab();
+      expect(screen.getByText('ok')).toHaveFocus();
+    });
+  });
 });
