956abd018aacee07c44cc44029198cc6d2326dc3
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-03-20 18:58:13 +0100

    enable inline-preview

--- docs/src/pages/components/dividers/VerticalDividers.tsx
+++ docs/src/pages/components/dividers/VerticalDividers.tsx
@@ -4,38 +4,34 @@ import FormatAlignCenterIcon from '@material-ui/icons/FormatAlignCenter';
 import FormatAlignRightIcon from '@material-ui/icons/FormatAlignRight';
 import FormatBoldIcon from '@material-ui/icons/FormatBold';
 import FormatItalicIcon from '@material-ui/icons/FormatItalic';
-import FormatUnderlinedIcon from '@material-ui/icons/FormatUnderlined';
-import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import Divider from '@material-ui/core/Divider';
 
 export default function VerticalDividers() {
   return (
-    <div>
-      <Grid
-        sx={{
-          width: 'fit-content',
-          border: (theme) => `1px solid ${theme.palette.divider}`,
-          borderRadius: 1,
-          bgcolor: 'background.paper',
-          color: 'text.secondary',
-          '& svg': {
-            m: 1.5,
-          },
-          '& hr': {
-            mx: 0.5,
-          },
-        }}
-        container
-        alignItems="center"
-      >
-        <FormatAlignLeftIcon />
-        <FormatAlignCenterIcon />
-        <FormatAlignRightIcon />
-        <Divider orientation="vertical" flexItem />
-        <FormatBoldIcon />
-        <FormatItalicIcon />
-        <FormatUnderlinedIcon />
-      </Grid>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        width: 'fit-content',
+        border: (theme) => `1px solid ${theme.palette.divider}`,
+        borderRadius: 1,
+        bgcolor: 'background.paper',
+        color: 'text.secondary',
+        '& svg': {
+          m: 1.5,
+        },
+        '& hr': {
+          mx: 0.5,
+        },
+      }}
+    >
+      <FormatAlignLeftIcon />
+      <FormatAlignCenterIcon />
+      <FormatAlignRightIcon />
+      <Divider orientation="vertical" flexItem />
+      <FormatBoldIcon />
+      <FormatItalicIcon />
+    </Box>
   );
 }
