ae44d60422451838e7985e847b0434bd5851e4d4
Jan Sedloň <jansedlon@icloud.com>
2021-03-20 21:28:05 +0100

    [Snackbar] Fix prop type error for 'key' prop (#25304)

--- packages/typescript-to-proptypes/src/generator.ts
+++ packages/typescript-to-proptypes/src/generator.ts
@@ -161,6 +161,12 @@ export function generate(component: t.Component, options: GenerateOptions = {}):
     }
 
     if (propType.type === 'any') {
+      // key isn't a prop like the others, see
+      // https://github.com/mui-org/material-ui/issues/25304
+      if (context.propTypeDefinition.name === 'key') {
+        return '() => null';
+      }
+
       return `${importedName}.any`;
     }
 
