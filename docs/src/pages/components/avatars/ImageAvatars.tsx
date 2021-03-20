dd5c945cd322a795771584621c2180ef2eabdc78
Victor Casas <victor.casas@outlook.cl>
2021-03-20 10:57:13 -0300

    feat: Use Stack to demo image avatar

--- docs/src/pages/components/avatars/ImageAvatars.tsx
+++ docs/src/pages/components/avatars/ImageAvatars.tsx
@@ -1,19 +1,13 @@
 import * as React from 'react';
-import Box from '@material-ui/core/Box';
 import Avatar from '@material-ui/core/Avatar';
+import Stack from '@material-ui/core/Stack';
 
 export default function ImageAvatars() {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        // TODO Replace with Stack
-        '& > :not(style) + :not(style)': { ml: 2 },
-      }}
-    >
+    <Stack direction="row" spacing={2}>
       <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
       <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
       <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-    </Box>
+    </Stack>
   );
 }
