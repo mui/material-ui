e42b902376823b9ad90070a4e2ef44199a72b36d
Victor Casas <victor.casas@outlook.cl>
2021-03-20 11:27:01 -0300

    feat: Use Stack to demo variants skeleton

--- docs/src/pages/components/skeleton/Variants.tsx
+++ docs/src/pages/components/skeleton/Variants.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
-import Box from '@material-ui/core/Box';
 import Skeleton from '@material-ui/core/Skeleton';
+import Stack from '@material-ui/core/Stack';
 
 export default function Variants() {
   return (
-    <Box
-      sx={{
-        // TODO Replace with Stack
-        '& > :not(style) + :not(style)': { mt: 1 },
-      }}
-    >
+    <Stack spacing={1}>
       <Skeleton variant="text" />
       <Skeleton variant="circular" width={40} height={40} />
       <Skeleton variant="rectangular" width={210} height={118} />
-    </Box>
+    </Stack>
   );
 }
