a555f053703fef8f08e6b51f2ba82d609727ad35
Víctor Casas <57921784+vicasas@users.noreply.github.com>
2021-03-26 08:53:54 -0300

    [docs] Migrate Portal demos to emotion (#25505)

--- docs/src/pages/components/portal/SimplePortal.js
+++ docs/src/pages/components/portal/SimplePortal.js
@@ -1,17 +1,8 @@
 import * as React from 'react';
+import Box from '@material-ui/core/Box';
 import Portal from '@material-ui/core/Portal';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  alert: {
-    padding: theme.spacing(1),
-    margin: theme.spacing(1, 0),
-    border: '1px solid',
-  },
-}));
 
 export default function SimplePortal() {
-  const classes = useStyles();
   const [show, setShow] = React.useState(false);
   const container = React.useRef(null);
 
@@ -24,15 +15,15 @@ export default function SimplePortal() {
       <button type="button" onClick={handleClick}>
         {show ? 'Unmount children' : 'Mount children'}
       </button>
-      <div className={classes.alert}>
+      <Box sx={{ p: 1, my: 1, border: '1px solid' }}>
         It looks like I will render here.
         {show ? (
           <Portal container={container.current}>
             <span>But I actually render here!</span>
           </Portal>
         ) : null}
-      </div>
-      <div className={classes.alert} ref={container} />
+      </Box>
+      <Box sx={{ p: 1, my: 1, border: '1px solid' }} ref={container} />
     </div>
   );
 }
