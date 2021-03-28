18acc95c2818a4e09d2fb85560ca6db8385f1401
Jan Silhan <jsilhan@oriflame.com>
2021-03-28 22:52:45 +0200

    test: Update test covformance.

--- packages/material-ui/src/ScopedCssBaseline/ScopedCssBaseline.test.js
+++ packages/material-ui/src/ScopedCssBaseline/ScopedCssBaseline.test.js
@@ -1,17 +1,20 @@
 import * as React from 'react';
-import { createMount, describeConformanceV5 } from 'test/utils';
+import { createMount, describeConformanceV5, createClientRender } from 'test/utils';
 import classes from './scopedCssBaselineClasses';
 import ScopedCssBaseline from './ScopedCssBaseline';
 
 describe('<ScopedCssBaseline />', () => {
+  const render = createClientRender();
   const mount = createMount();
 
   describeConformanceV5(<ScopedCssBaseline />, () => ({
     classes,
     inheritComponent: 'div',
     mount,
+    render,
     muiName: 'MuiScopedCssBaseline',
     refInstanceof: window.HTMLDivElement,
+    testComponentPropWith: 'span',
     skip: ['componentProp'],
   }));
 });
