ec50405c8d0c75cf06e9b4e09f4dfee050853bd0
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-03-29 22:11:24 +0200

    yarn docs:typescript:formatted

--- docs/src/pages/components/slider/MinimumDistanceSlider.js
+++ docs/src/pages/components/slider/MinimumDistanceSlider.js
@@ -12,27 +12,35 @@ const minDistance = 10;
 export default function MinimumDistanceSlider() {
   const [value1, setValue1] = React.useState([20, 37]);
 
-  const handleChange1 = (event, newValue) => {
-    if (typeof newValue !== 'number' && newValue[1] - newValue[0] >= minDistance) {
-      setValue1(newValue);
+  const handleChange1 = (event, newValue, activeThumb) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (activeThumb === 0) {
+      setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
+    } else {
+      setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
     }
   };
 
   const [value2, setValue2] = React.useState([20, 37]);
 
   const handleChange2 = (event, newValue, activeThumb) => {
-    if (typeof newValue !== 'number') {
-      if (newValue[1] - newValue[0] < minDistance) {
-        if (activeThumb === 0) {
-          const clamped = Math.min(newValue[0], 100 - minDistance);
-          setValue2([clamped, clamped + minDistance]);
-        } else {
-          const clamped = Math.max(newValue[1], minDistance);
-          setValue2([clamped - minDistance, clamped]);
-        }
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (newValue[1] - newValue[0] < minDistance) {
+      if (activeThumb === 0) {
+        const clamped = Math.min(newValue[0], 100 - minDistance);
+        setValue2([clamped, clamped + minDistance]);
       } else {
-        setValue2(newValue);
+        const clamped = Math.max(newValue[1], minDistance);
+        setValue2([clamped - minDistance, clamped]);
       }
+    } else {
+      setValue2(newValue);
     }
   };
 
