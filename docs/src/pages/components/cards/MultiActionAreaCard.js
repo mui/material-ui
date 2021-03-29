6353b30122df40fbfb55199b16d84d3d00e0e5c3
Victor Casas <victor.casas@outlook.cl>
2021-03-29 19:41:55 -0300

    docs: Migrate MultiActionAreaCard demo to emotion

--- docs/src/pages/components/cards/MultiActionAreaCard.js
+++ docs/src/pages/components/cards/MultiActionAreaCard.js
@@ -1,28 +1,16 @@
 import * as React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { Button, CardActionArea, CardActions } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-});
-
 export default function MultiActionAreaCard() {
-  const classes = useStyles();
-
   return (
-    <Card className={classes.root}>
+    <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
-          className={classes.media}
+          sx={{ height: 140 }}
           image="/static/images/cards/contemplative-reptile.jpg"
           title="Contemplative Reptile"
         />
