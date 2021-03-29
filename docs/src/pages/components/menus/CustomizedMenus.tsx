6c921cf54ef9b02da0c5df02da3611fe8f2f6615
Victor Casas <victor.casas@outlook.cl>
2021-03-29 14:06:10 -0300

    style: prettier

--- docs/src/pages/components/menus/CustomizedMenus.tsx
+++ docs/src/pages/components/menus/CustomizedMenus.tsx
@@ -28,30 +28,30 @@ const StyledMenu = styled((props: MenuProps) => (
 ))(({ theme }) => ({
   '& .MuiPaper-root': {
     borderRadius: 6,
-  marginTop: theme.spacing(1),
-  minWidth: 180,
-  color:
-    theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-  boxShadow:
-    'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-  '& .MuiList-root': {
-    padding: '4px 0',
-  },
-  '& .MuiListItem-root': {
-    ...theme.typography.body1 as CSS.Properties,
-    '& .MuiSvgIcon-root': {
-      fontSize: 18,
-      color: theme.palette.text.secondary,
-      marginRight: theme.spacing(1.5),
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiList-root': {
+      padding: '4px 0',
     },
-    '&:active': {
-      backgroundColor: alpha(
-        theme.palette.primary.main,
-        theme.palette.action.selectedOpacity,
-      ),
+    '& .MuiListItem-root': {
+      ...(theme.typography.body1 as CSS.Properties),
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
     },
   },
-  }
 }));
 
 export default function CustomizedMenus() {
