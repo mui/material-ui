52f42dbabf48e014ec86c462d40492f47f07b306
Victor Casas <victor.casas@outlook.cl>
2021-03-29 19:39:54 -0300

    docs: Migrate OutlinedCard demo to emotion

--- docs/src/pages/components/cards/OutlinedCard.tsx
+++ docs/src/pages/components/cards/OutlinedCard.tsx
@@ -1,42 +1,31 @@
 import * as React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
-
 export default function OutlinedCard() {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      •
+    </Box>
+  );
 
   return (
-    <Card className={classes.root} variant="outlined">
+    <Card sx={{ minWidth: 275 }} variant="outlined">
       <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
+        <Typography sx={{ fontSize: 14 }} color="textSecondary" gutterBottom>
           Word of the Day
         </Typography>
         <Typography variant="h5" component="div">
           be{bull}nev{bull}o{bull}lent
         </Typography>
-        <Typography className={classes.pos} color="textSecondary">
+        <Typography sx={{ mb: 1.5 }} color="textSecondary">
           adjective
         </Typography>
         <Typography variant="body2" component="p">
