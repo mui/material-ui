45e0b20357370b8bed21879f92a92f7420110536
Victor Casas <victor.casas@outlook.cl>
2021-03-29 19:54:47 -0300

    fix: arrow

--- docs/src/pages/components/cards/RecipeReviewCard.js
+++ docs/src/pages/components/cards/RecipeReviewCard.js
@@ -17,7 +17,7 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const ExpandMore = styled((props) => <IconButton {...props} />)(
   ({ theme, expand }) => ({
-    transform: expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
     marginLeft: 'auto',
     transition: theme.transitions.create('transform', {
       duration: theme.transitions.duration.shortest,
--- docs/src/pages/components/cards/RecipeReviewCard.tsx
+++ docs/src/pages/components/cards/RecipeReviewCard.tsx
@@ -21,7 +21,7 @@ interface ExpandMoreProps extends IconButtonProps {
 
 const ExpandMore = styled((props: ExpandMoreProps) => <IconButton {...props} />)(
   ({ theme, expand }) => ({
-    transform: expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
     marginLeft: 'auto',
     transition: theme.transitions.create('transform', {
       duration: theme.transitions.duration.shortest,
