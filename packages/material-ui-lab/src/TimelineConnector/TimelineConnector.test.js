3c57b2e07db6c956c2d9a77491efdc53c893cc6e
Victor Casas <victor.casas@outlook.cl>
2021-04-08 08:30:06 -0400

    test: Remove skip themeVariants

--- packages/material-ui-lab/src/TimelineConnector/TimelineConnector.test.js
+++ packages/material-ui-lab/src/TimelineConnector/TimelineConnector.test.js
@@ -14,6 +14,6 @@ describe('<TimelineConnector />', () => {
     mount,
     muiName: 'MuiTimelineConnector',
     refInstanceof: window.HTMLSpanElement,
-    skip: ['componentProp', 'componentsProp', 'themeVariants'],
+    skip: ['componentProp', 'componentsProp'],
   }));
 });
