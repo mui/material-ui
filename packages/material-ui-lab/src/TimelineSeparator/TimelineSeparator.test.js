71b3449e78def5665598046b5fae6e3c703f6bb5
Victor Casas <victor.casas@outlook.cl>
2021-04-08 09:14:28 -0400

    test: Update

--- packages/material-ui-lab/src/TimelineSeparator/TimelineSeparator.test.js
+++ packages/material-ui-lab/src/TimelineSeparator/TimelineSeparator.test.js
@@ -14,6 +14,6 @@ describe('<TimelineSeparator />', () => {
     mount,
     muiName: 'MuiTimelineSeparator',
     refInstanceof: window.HTMLDivElement,
-    skip: ['componentProp', 'componentsProp'],
+    skip: ['componentProp', 'componentsProp', 'themeVariants'],
   }));
 });
