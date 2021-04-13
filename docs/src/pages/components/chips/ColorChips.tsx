c0f414464290c7b2c36cf67d4efeae90c8e061f7
Victor Casas <victor.casas@outlook.cl>
2021-04-13 08:21:48 -0400

    docs: Chip

---
+++ docs/src/pages/components/chips/ColorChips.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Chip from '@material-ui/core/Chip';
+import Stack from '@material-ui/core/Stack';
+
+export default function BasicChips() {
+  return (
+    <Stack direction="row" spacing={1}>
+      <Chip label="Default" />
+      <Chip label="Primary" color="primary" />
+      <Chip label="Secondary" color="secondary" variant="outlined" />
+    </Stack>
+  );
+}
---
+++ docs/src/pages/components/chips/SizesChips.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Chip from '@material-ui/core/Chip';
+import Stack from '@material-ui/core/Stack';
+
+export default function IconChips() {
+  return (
+    <Stack direction="row" spacing={1}>
+      <Chip label="Normal" />
+      <Chip label="Small" size="small" variant="outlined" />
+    </Stack>
+  );
+}
