f12b287f5a5cea5e05a295f7db1aabd6853033e8
Marija Najdova <mnajdova@gmail.com>
2021-04-13 18:02:23 +0200

    prettier

--- test/regressions/fixtures/TextField/FilledHiddenLabelInputAdornment.js
+++ test/regressions/fixtures/TextField/FilledHiddenLabelInputAdornment.js
@@ -1,19 +1,19 @@
-import * as React from "react";
-import Box from "@material-ui/core/Box";
-import TextField from "@material-ui/core/TextField";
-import InputAdornment from "@material-ui/core/InputAdornment";
+import * as React from 'react';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 export default function FilledHiddenLabelInputAdornment() {
   return (
     <Box
       component="form"
       sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" }
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
       }}
       noValidate
       autoComplete="off"
     >
-      <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
         <label htmlFor="filled-hidden-label-small">Hidden label</label>
         <TextField
           hiddenLabel
@@ -22,7 +22,7 @@ export default function FilledHiddenLabelInputAdornment() {
           variant="filled"
           size="small"
           InputProps={{
-            startAdornment: <InputAdornment position="start">Kg</InputAdornment>
+            startAdornment: <InputAdornment position="start">Kg</InputAdornment>,
           }}
         />
       </Box>
