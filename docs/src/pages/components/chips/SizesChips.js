6678c993b83ca2b36a2ecf7cb6b7a8f8f052c1c5
Victor Casas <victor.casas@outlook.cl>
2021-04-13 10:42:30 -0400

    fix: files names

--- docs/src/pages/components/chips/SizesChips.js
+++ docs/src/pages/components/chips/SizesChips.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Chip from '@material-ui/core/Chip';
 import Stack from '@material-ui/core/Stack';
 
-export default function IconChips() {
+export default function SizesChips() {
   return (
     <Stack direction="row" spacing={1}>
       <Chip label="Small" size="small" />
