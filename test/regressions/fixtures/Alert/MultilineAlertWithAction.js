1ec6d251d191af647ac8537c367538db4d32cded
Marija Najdova <mnajdova@gmail.com>
2021-04-14 16:31:09 +0200

    prettier

--- test/regressions/fixtures/Alert/MultilineAlertWithAction.js
+++ test/regressions/fixtures/Alert/MultilineAlertWithAction.js
@@ -1,7 +1,7 @@
-import * as React from "react";
-import Button from "@material-ui/core/Button";
-import Typogrpahy from "@material-ui/core/Typography";
-import Alert from "@material-ui/core/Alert";
+import * as React from 'react';
+import Button from '@material-ui/core/Button';
+import Typogrpahy from '@material-ui/core/Typography';
+import Alert from '@material-ui/core/Alert';
 
 export default function MultilineAlertWithAction() {
   return (
