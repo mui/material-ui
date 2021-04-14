2a58b3080faa1f89184e87b3330df061a72498f8
Marija Najdova <mnajdova@gmail.com>
2021-04-14 10:36:22 +0200

    simplified visual test

--- test/regressions/fixtures/TextField/FilledHiddenLabelInputAdornment.js
+++ test/regressions/fixtures/TextField/FilledHiddenLabelInputAdornment.js
@@ -1,31 +1,17 @@
 import * as React from 'react';
-import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 
 export default function FilledHiddenLabelInputAdornment() {
   return (
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
+    <TextField
+      hiddenLabel
+      id="filled-hidden-label"
+      defaultValue="Value"
+      variant="filled"
+      InputProps={{
+        startAdornment: <InputAdornment position="start">Kg</InputAdornment>,
       }}
-      noValidate
-      autoComplete="off"
-    >
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <label htmlFor="filled-hidden-label-small">Hidden label</label>
-        <TextField
-          hiddenLabel
-          id="filled-hidden-label-small"
-          defaultValue="Small"
-          variant="filled"
-          size="small"
-          InputProps={{
-            startAdornment: <InputAdornment position="start">Kg</InputAdornment>,
-          }}
-        />
-      </Box>
-    </Box>
+    />
   );
 }
