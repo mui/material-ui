e793fda276f7554b9da1b4cc6949102b8ebc29f2
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-04-17 19:51:18 +0200

    push one step further

--- packages/material-ui/src/Select/SelectInput.js
+++ packages/material-ui/src/Select/SelectInput.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import MuiError from '@material-ui/utils/macros/MuiError.macro';
 import { unstable_composeClasses as composeClasses } from '@material-ui/unstyled';
-import { refType, deepmerge } from '@material-ui/utils';
+import { refType } from '@material-ui/utils';
 import ownerDocument from '../utils/ownerDocument';
 import capitalize from '../utils/capitalize';
 import Menu from '../Menu/Menu';
@@ -17,14 +17,15 @@ import selectClasses, { getSelectUtilitiyClasses } from './selectClasses';
 
 const overridesResolver = (props, styles) => {
   const { styleProps } = props;
-  return deepmerge(
-    {
-      [`&.${selectClasses.select}`]: styles.select,
-      [`&.${selectClasses.selectMenu}`]: styles.selectMenu,
-      [`&.${selectClasses[styleProps.variant]}`]: styles[styleProps.variant],
+  return {
+    [`&.${selectClasses.select}`]: {
+      // TODO v5: remove `root` and `selectMenu`
+      ...styles.root,
+      ...styles.select,
+      ...styles.selectMenu,
+      ...styles[styleProps.variant],
     },
-    styles.root || {},
-  );
+  };
 };
 
 const SelectRoot = experimentalStyled(
@@ -32,6 +33,7 @@ const SelectRoot = experimentalStyled(
   {},
   { name: 'MuiSelect', slot: 'Root', overridesResolver },
 )(nativeSelectRootStyles, {
+  // Win specificity over the input base
   [`&.${selectClasses.selectMenu}`]: {
     height: 'auto', // Resets for multiple select with chips
     minHeight: '1.4375em', // Required for select\text-field height consistency
@@ -43,13 +45,11 @@ const SelectRoot = experimentalStyled(
 
 const iconOverridesResolver = (props, styles) => {
   const { styleProps } = props;
-  return deepmerge(
-    {
-      ...(styleProps.variant && styles[`icon${capitalize(styleProps.variant)}`]),
-      ...(styleProps.open && styles.iconOpen),
-    },
-    styles.icon || {},
-  );
+  return {
+    ...styles.icon,
+    ...(styleProps.variant && styles[`icon${capitalize(styleProps.variant)}`]),
+    ...(styleProps.open && styles.iconOpen),
+  };
 };
 
 const SelectIcon = experimentalStyled(
