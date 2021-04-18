ca65eb878fb608d28473c75b77c9a9747518ecc0
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-04-18 22:31:42 +0200

    enable inline-previews and add a unstyled h2

--- docs/src/pages/components/trap-focus/DisableEnforceFocus.js
+++ docs/src/pages/components/trap-focus/DisableEnforceFocus.js
@@ -1,32 +1,34 @@
 import * as React from 'react';
+import Box from '@material-ui/core/Box';
 import TrapFocus from '@material-ui/core/Unstable_TrapFocus';
 
 export default function DisableEnforceFocus() {
   const [open, setOpen] = React.useState(false);
+
   return (
-    <div>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
       <button type="button" onClick={() => setOpen(true)}>
         Open
       </button>
-      <br />
       {open && (
         <TrapFocus disableEnforceFocus open>
-          <div tabIndex={-1}>
-            <h3>Quick form</h3>
+          <Box tabIndex={-1} sx={{ mt: 1, p: 1 }}>
             <label>
               First name: <input type="text" />
-            </label>
-            <br />
-            <label>
-              Last name: <input type="text" />
             </label>
             <br />
             <button type="button" onClick={() => setOpen(false)}>
               Close
             </button>
-          </div>
+          </Box>
         </TrapFocus>
       )}
-    </div>
+    </Box>
   );
 }
