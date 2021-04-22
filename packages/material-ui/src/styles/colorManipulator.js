d579749e7a8efd52a38742555dd5c45cb67df4c1
Matheus Wichman <matheushw@outlook.com>
2021-04-22 14:44:17 -0300

    [theme] Remove fade color helper

--- packages/material-ui/src/styles/colorManipulator.js
+++ packages/material-ui/src/styles/colorManipulator.js
@@ -237,36 +237,6 @@ export function alpha(color, value) {
   return recomposeColor(color);
 }
 
-let warnedOnce = false;
-
-/**
- * Set the absolute transparency of a color.
- * Any existing alpha values are overwritten.
- *
- * @param {string} color - CSS color, i.e. one of: #nnn, #nnnnnn, rgb(), rgba(), hsl(), hsla()
- * @param {number} value - value to set the alpha channel to in the range 0 -1
- * @returns {string} A CSS color string. Hex input values are returned as rgb
- *
- * @deprecated
- * Use `import { alpha } from '@material-ui/core/styles'` instead.
- */
-export function fade(color, value) {
-  if (process.env.NODE_ENV !== 'production') {
-    if (!warnedOnce) {
-      warnedOnce = true;
-      console.error(
-        [
-          'Material-UI: The `fade` color utility was renamed to `alpha` to better describe its functionality.',
-          '',
-          "You should use `import { alpha } from '@material-ui/core/styles'`",
-        ].join('\n'),
-      );
-    }
-  }
-
-  return alpha(color, value);
-}
-
 /**
  * Darkens a color.
  * @param {string} color - CSS color, i.e. one of: #nnn, #nnnnnn, rgb(), rgba(), hsl(), hsla(), color()
