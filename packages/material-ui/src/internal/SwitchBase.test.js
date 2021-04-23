bacce0c651e3c3b049107e70efe0cb995f483318
eps1lon <silbermann.sebastian@gmail.com>
2021-04-23 10:23:54 +0200

    Test click not setProps

--- packages/material-ui/src/internal/SwitchBase.test.js
+++ packages/material-ui/src/internal/SwitchBase.test.js
@@ -194,23 +194,29 @@ describe('<SwitchBase />', () => {
     });
 
     it('should call onChange when controlled', () => {
-      const checked = true;
-      const handleChange = spy();
-      const { getByRole, setProps } = render(
-        <SwitchBase
-          icon="unchecked"
-          checkedIcon="checked"
-          type="checkbox"
-          checked={checked}
-          onChange={handleChange}
-        />,
-      );
+      const defaultChecked = true;
+      function ControlledSwichBase() {
+        const [checked, setChecked] = React.useState(defaultChecked);
 
-      getByRole('checkbox').click();
-      setProps({ checked: false });
+        return (
+          <SwitchBase
+            icon="unchecked"
+            checkedIcon="checked"
+            type="checkbox"
+            checked={checked}
+            onChange={(event) => setChecked(event.target.checked)}
+          />
+        );
+      }
 
-      expect(handleChange.callCount).to.equal(1);
-      expect(handleChange.firstCall.args[0].target.checked).to.equal(!checked);
+      const { getByRole } = render(<ControlledSwichBase />);
+      const checkbox = getByRole('checkbox');
+
+      act(() => {
+        checkbox.click();
+      });
+
+      expect(checkbox).to.have.property('checked', !defaultChecked);
     });
 
     it('should not change checkbox state when event is default prevented', () => {
