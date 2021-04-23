6bd85dca791fbfb7e238d5535b8a61abce5fd0d0
eps1lon <silbermann.sebastian@gmail.com>
2021-04-23 11:09:45 +0200

    Makure passive listeners are removed

--- packages/material-ui/src/ClickAwayListener/ClickAwayListener.tsx
+++ packages/material-ui/src/ClickAwayListener/ClickAwayListener.tsx
@@ -79,16 +79,32 @@ function ClickAwayListener(props: ClickAwayListenerProps): JSX.Element {
     const doc = ownerDocument(nodeRef.current);
     function activate() {
       activatedRef.current = true;
-      doc.removeEventListener('mousedown', activate, { capture: true });
-      doc.removeEventListener('touchstart', activate, { capture: true });
+      doc.removeEventListener('mousedown', activate, {
+        capture: true,
+        // @ts-expect-error https://github.com/mui-org/material-ui/pull/25741#discussion_r619060154
+        passive: true,
+      });
+      doc.removeEventListener('touchstart', activate, {
+        capture: true,
+        // @ts-expect-error https://github.com/mui-org/material-ui/pull/25741#discussion_r619060154
+        passive: true,
+      });
     }
 
     doc.addEventListener('mousedown', activate, { capture: true, passive: true });
     doc.addEventListener('touchstart', activate, { capture: true, passive: true });
 
     return () => {
-      doc.removeEventListener('mousedown', activate, { capture: true });
-      doc.removeEventListener('touchstart', activate, { capture: true });
+      doc.removeEventListener('mousedown', activate, {
+        capture: true,
+        // @ts-expect-error https://github.com/mui-org/material-ui/pull/25741#discussion_r619060154
+        passive: true,
+      });
+      doc.removeEventListener('touchstart', activate, {
+        capture: true,
+        // @ts-expect-error https://github.com/mui-org/material-ui/pull/25741#discussion_r619060154
+        passive: true,
+      });
       activatedRef.current = false;
     };
   }, []);
