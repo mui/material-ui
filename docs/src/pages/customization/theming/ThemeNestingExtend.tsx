c8a8165b219f7cb87bed602189cfbcd17788dffd
Victor Casas <victor.casas@outlook.cl>
2021-04-27 14:09:13 -0400

    Fix argos

--- docs/src/pages/customization/theming/ThemeNestingExtend.tsx
+++ docs/src/pages/customization/theming/ThemeNestingExtend.tsx
@@ -14,7 +14,7 @@ const outerTheme = createMuiTheme({
 export default function ThemeNestingExtend() {
   return (
     <ThemeProvider theme={outerTheme}>
-      <Checkbox defaultChecked />
+      <Checkbox defaultChecked color="secondary" />
       <ThemeProvider
         theme={(theme: Theme) =>
           createMuiTheme({
@@ -28,7 +28,7 @@ export default function ThemeNestingExtend() {
           })
         }
       >
-        <Checkbox defaultChecked color="primary" />
+        <Checkbox defaultChecked />
         <Checkbox defaultChecked color="secondary" />
       </ThemeProvider>
     </ThemeProvider>
