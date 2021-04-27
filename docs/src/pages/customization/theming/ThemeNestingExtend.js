9b4c4c82d75f8e72f7734b2e73c4b6d3d06c8aa6
Victor Casas <victor.casas@outlook.cl>
2021-04-27 14:32:06 -0400

    yarn docs:typescript:formatted

--- docs/src/pages/customization/theming/ThemeNestingExtend.js
+++ docs/src/pages/customization/theming/ThemeNestingExtend.js
@@ -14,7 +14,7 @@ const outerTheme = createMuiTheme({
 export default function ThemeNestingExtend() {
   return (
     <ThemeProvider theme={outerTheme}>
-      <Checkbox defaultChecked />
+      <Checkbox defaultChecked color="secondary" />
       <ThemeProvider
         theme={(theme) =>
           createMuiTheme({
@@ -28,7 +28,7 @@ export default function ThemeNestingExtend() {
           })
         }
       >
-        <Checkbox defaultChecked color="primary" />
+        <Checkbox defaultChecked />
         <Checkbox defaultChecked color="secondary" />
       </ThemeProvider>
     </ThemeProvider>
