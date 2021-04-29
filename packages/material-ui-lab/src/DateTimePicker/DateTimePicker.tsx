df0dad99d57336c88c8f1e951b6c0007136b0a48
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2021-04-29 23:46:56 +0200

    [internal][pickers] Minimal types for defaultizing in useInterceptProps (#26063)

--- packages/material-ui-lab/src/DateTimePicker/DateTimePicker.tsx
+++ packages/material-ui-lab/src/DateTimePicker/DateTimePicker.tsx
@@ -78,7 +78,9 @@ export interface BaseDateTimePickerProps<TDate>
   views?: readonly DateTimePickerView[];
 }
 
-function useInterceptProps({
+type InterceptedProps<Props> = Props & { inputFormat: string };
+
+function useInterceptProps<Props extends BaseDateTimePickerProps<unknown>>({
   ampm,
   inputFormat,
   maxDate: __maxDate = defaultMaxDate,
@@ -91,7 +93,7 @@ function useInterceptProps({
   orientation = 'portrait',
   views = ['year', 'day', 'hours', 'minutes'],
   ...other
-}: BaseDateTimePickerProps<unknown>) {
+}: Props): InterceptedProps<Props> {
   const utils = useUtils();
   const minTime = useParsedDate(__minTime);
   const maxTime = useParsedDate(__maxTime);
@@ -126,7 +128,7 @@ function useInterceptProps({
       '12h': utils.formats.keyboardDateTime12h,
       '24h': utils.formats.keyboardDateTime24h,
     }),
-    ...other,
+    ...(other as Props),
   };
 }
 
