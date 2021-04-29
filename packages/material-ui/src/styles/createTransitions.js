9f8e573bb0fe866750a387b98c5919b4be48b5b1
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-04-29 16:26:29 +0200

    fix the issue

--- packages/material-ui/src/styles/createTransitions.js
+++ packages/material-ui/src/styles/createTransitions.js
@@ -1,7 +1,46 @@
-import { duration, easing, getAutoHeightDuration } from './transitions';
+// Follow https://material.google.com/motion/duration-easing.html#duration-easing-natural-easing-curves
+// to learn the context in which each easing should be used.
+export const easing = {
+  // This is the most common easing curve.
+  easeInOut: 'cubic-bezier(0.4, 0, 0.2, 1)',
+  // Objects enter the screen at full velocity from off-screen and
+  // slowly decelerate to a resting point.
+  easeOut: 'cubic-bezier(0.0, 0, 0.2, 1)',
+  // Objects leave the screen at full velocity. They do not decelerate when off-screen.
+  easeIn: 'cubic-bezier(0.4, 0, 1, 1)',
+  // The sharp curve is used by objects that may return to the screen at any time.
+  sharp: 'cubic-bezier(0.4, 0, 0.6, 1)',
+};
+
+// Follow https://material.io/guidelines/motion/duration-easing.html#duration-easing-common-durations
+// to learn when use what timing
+export const duration = {
+  shortest: 150,
+  shorter: 200,
+  short: 250,
+  // most basic recommended timing
+  standard: 300,
+  // this is to be used in complex animations
+  complex: 375,
+  // recommended when something is entering screen
+  enteringScreen: 225,
+  // recommended when something is leaving screen
+  leavingScreen: 195,
+};
 
 function formatMs(milliseconds) {
   return `${Math.round(milliseconds)}ms`;
+}
+
+function getAutoHeightDuration(height) {
+  if (!height) {
+    return 0;
+  }
+
+  const constant = height / 36;
+
+  // https://www.wolframalpha.com/input/?i=(4+%2B+15+*+(x+%2F+36+)+**+0.25+%2B+(x+%2F+36)+%2F+5)+*+10
+  return Math.round((4 + 15 * constant ** 0.25 + constant / 5) * 10);
 }
 
 export default function createTransitions(inputTransitions) {
--- packages/material-ui/src/styles/index.js
+++ packages/material-ui/src/styles/index.js
@@ -7,7 +7,7 @@ export { getUnit as unstable_getUnit, toUnitless as unstable_toUnitless } from '
 export { default as makeStyles } from './makeStyles';
 export { default as responsiveFontSizes } from './responsiveFontSizes';
 export { default as styled } from './styled';
-export { duration, easing } from './transitions';
+export { duration, easing } from './createTransitions';
 export { default as useTheme } from './useTheme';
 export { default as unstable_useThemeProps } from './useThemeProps';
 export { default as withStyles } from './withStyles';
