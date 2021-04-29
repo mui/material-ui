c93d63897d6e0e55b5b6fae66e72d0c0bd14f763
siriwatknp <siriwatkunaporn@gmail.com>
2021-04-29 22:23:04 +0700

    migrate to emotion

--- packages/material-ui-lab/src/ClockPicker/ClockNumber.tsx
+++ packages/material-ui-lab/src/ClockPicker/ClockNumber.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
 import clsx from 'clsx';
-import { MuiStyles, StyleRules, WithStyles, withStyles } from '@material-ui/core/styles';
+import {
+  experimentalStyled,
+  unstable_useThemeProps as useThemeProps,
+  Theme,
+} from '@material-ui/core/styles';
+import { SxProps } from '@material-ui/system';
+import {
+  unstable_composeClasses as composeClasses,
+  generateUtilityClass,
+  generateUtilityClasses,
+} from '@material-ui/unstyled';
 import { CLOCK_WIDTH, CLOCK_HOUR_WIDTH } from './shared';
 
-export interface ClockNumberProps {
+type SpanProps = JSX.IntrinsicElements['span'];
+export interface ClockNumberProps extends SpanProps {
+  classes?: Partial<typeof clockNumberClasses>;
   disabled: boolean;
   index: number;
   inner: boolean;
@@ -11,45 +23,81 @@ export interface ClockNumberProps {
   selected: boolean;
   // TODO: spread to a `span`. What are the implications (generic role etc.)
   'aria-label': string;
+  sx: SxProps<Theme>;
 }
 
-export type ClockNumberClassKey = 'root' | 'selected' | 'disabled' | 'inner';
+export type ClockNumberClassKey = keyof typeof clockNumberClasses;
 
-export const styles: MuiStyles<ClockNumberClassKey> = (theme): StyleRules<ClockNumberClassKey> => ({
-  root: {
-    width: CLOCK_HOUR_WIDTH,
-    height: CLOCK_HOUR_WIDTH,
-    position: 'absolute',
-    left: `calc((100% - ${CLOCK_HOUR_WIDTH}px) / 2)`,
-    display: 'inline-flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: '50%',
-    color: theme.palette.text.primary,
-    '&:focused': {
-      backgroundColor: theme.palette.background.paper,
-    },
-    '&$selected': {
-      color: theme.palette.primary.contrastText,
-    },
-    '&$disabled': {
-      pointerEvents: 'none',
-      color: theme.palette.text.disabled,
+export function getClockNumberUtilityClass(slot: string) {
+  return generateUtilityClass('MuiClockNumber', slot);
+}
+
+export const clockNumberClasses = generateUtilityClasses('MuiClockNumber', [
+  'root',
+  'selected',
+  'disabled',
+  'inner',
+]);
+
+const useUtilityClasses = (styleProps: ClockNumberProps) => {
+  const { inner, classes } = styleProps;
+
+  const slots = {
+    root: ['root', inner && 'inner'],
+  };
+
+  return composeClasses(slots, getClockNumberUtilityClass, classes);
+};
+
+const ClockNumberRoot = experimentalStyled(
+  'span',
+  {},
+  {
+    name: 'MuiClockNumber',
+    slot: 'Root',
+    overridesResolver: (props, styles: Record<ClockNumberClassKey, object>) => {
+      const { styleProps } = props;
+      return {
+        ...styles.root,
+        ...(styleProps.inner && styles.inner),
+      };
     },
   },
-  selected: {},
-  disabled: {},
-  inner: {
-    ...theme.typography.body2,
+)(({ theme, styleProps = {} }) => ({
+  width: CLOCK_HOUR_WIDTH,
+  height: CLOCK_HOUR_WIDTH,
+  position: 'absolute',
+  left: `calc((100% - ${CLOCK_HOUR_WIDTH}px) / 2)`,
+  display: 'inline-flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: '50%',
+  color: theme.palette.text.primary,
+  '&:focused': {
+    backgroundColor: theme.palette.background.paper,
+  },
+  [`&.${clockNumberClasses.selected}`]: {
+    color: theme.palette.primary.contrastText,
+  },
+  [`&.${clockNumberClasses.disabled}`]: {
+    pointerEvents: 'none',
+    color: theme.palette.text.disabled,
+  },
+  ...(!!styleProps.inner && {
+    ...(theme.typography.body2 as React.CSSProperties),
     color: theme.palette.text.secondary,
-  },
-});
+  }),
+}));
 
 /**
  * @ignore - internal component.
  */
-const ClockNumber: React.FC<ClockNumberProps & WithStyles<typeof styles>> = (props) => {
-  const { classes, disabled, index, inner, label, selected, ...other } = props;
+function ClockNumber(inProps: ClockNumberProps) {
+  const props = useThemeProps({ props: inProps, name: 'MuiClockNumber' });
+  const { className, disabled, index, inner, label, selected, theme, ...other } = props;
+  const styleProps = { ...props };
+
+  const classes = useUtilityClasses(styleProps);
 
   const angle = ((index % 12) / 12) * Math.PI * 2 - Math.PI / 2;
   const length = ((CLOCK_WIDTH - CLOCK_HOUR_WIDTH - 2) / 2) * (inner ? 0.65 : 1);
@@ -61,20 +109,16 @@ const ClockNumber: React.FC<ClockNumberProps & WithStyles<typeof styles>> = (pro
   };
 
   return (
-    <span
-      className={clsx(classes.root, {
-        [classes.selected]: selected,
-        [classes.disabled]: disabled,
-        [classes.inner]: inner,
-      })}
+    <ClockNumberRoot
+      className={clsx(classes.root, className)}
       style={transformStyle}
+      styleProps={styleProps}
+      theme={theme as Theme}
       {...other}
     >
       {label}
-    </span>
+    </ClockNumberRoot>
   );
-};
+}
 
-export default withStyles(styles, { name: 'MuiClockNumber' })(ClockNumber) as (
-  props: ClockNumberProps,
-) => JSX.Element;
+export default ClockNumber;
