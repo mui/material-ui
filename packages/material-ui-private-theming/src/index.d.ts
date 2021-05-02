611dce59a1cda1804f2837879b650a8e19d5f72c
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-05-02 21:25:08 +0200

    forgot to revert these two

--- packages/material-ui-private-theming/src/index.d.ts
+++ packages/material-ui-private-theming/src/index.d.ts
@@ -1,7 +1,7 @@
 export { default as ThemeProvider } from './ThemeProvider';
 export * from './ThemeProvider';
 
-export { default as useTheme, ThemeContext } from './useTheme';
+export { default as useTheme } from './useTheme';
 export * from './useTheme';
 
 export * from './defaultTheme';
--- packages/material-ui-private-theming/src/index.js
+++ packages/material-ui-private-theming/src/index.js
@@ -1,4 +1,4 @@
 export { default as ThemeProvider } from './ThemeProvider';
 export * from './ThemeProvider';
 
-export { default as useTheme, ThemeContext } from './useTheme';
+export { default as useTheme } from './useTheme';
