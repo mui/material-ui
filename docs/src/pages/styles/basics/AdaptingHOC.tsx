3645784bfbbe8ffec6f8b240eb95bb0d513cd3f0
Marija Najdova <mnajdova@gmail.com>
2021-05-04 12:45:52 +0200

    init

--- docs/src/pages/styles/basics/AdaptingHOC.tsx
+++ docs/src/pages/styles/basics/AdaptingHOC.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { withStyles, WithStyles } from '@material-ui/core/styles';
+import { withStyles, WithStyles } from '@material-ui/styles';
 import Button, { ButtonProps } from '@material-ui/core/Button';
 
 const styles = {
