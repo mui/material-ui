8dc2512d28863ca0669ec02a4c6e64f2262952af
Victor Casas <victor.casas@outlook.cl>
2021-05-07 18:35:13 -0400

    fix color switch

--- docs/src/pages/components/switches/ColorSwitches.js
+++ docs/src/pages/components/switches/ColorSwitches.js
@@ -4,13 +4,13 @@ import { green } from '@material-ui/core/colors';
 import Switch from '@material-ui/core/Switch';
 
 const GreenSwitch = styled(Switch)(({ theme }) => ({
-  '&.MuiSwitch-root > .Mui-checked': {
+  '& > .MuiSwitch-switchBase.Mui-checked': {
     color: green[600],
     '&:hover': {
       backgroundColor: alpha(green[600], theme.palette.action.hoverOpacity),
     },
   },
-  '&.MuiSwitch-root > .Mui-checked + .MuiSwitch-track': {
+  '& > .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
     backgroundColor: green[600],
   },
 }));
