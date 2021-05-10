c54c0daa1ba676fd7e24244c26d8b76492598c31
siriwatknp <siriwatkunaporn@gmail.com>
2021-05-10 17:21:15 +0700

    remove classes file

--- packages/material-ui-lab/src/MonthPicker/pickersMonthClasses.ts
+++
@@ -1,9 +0,0 @@
-import { generateUtilityClass, generateUtilityClasses } from '@material-ui/unstyled';
-
-export function getPickersMonthUtilityClass(slot: string) {
-  return generateUtilityClass('MuiPickersMonth', slot);
-}
-
-const pickersMonthClasses = generateUtilityClasses('MuiPickersMonth', ['root', 'selected']);
-
-export default pickersMonthClasses;
