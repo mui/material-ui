699ac18c27f88ade852096e89e18e39e2c81f522
siriwatknp <siriwatkunaporn@gmail.com>
2021-05-12 14:29:34 +0700

    simplify for internal component

--- packages/material-ui-lab/src/internal/pickers/PickersToolbarText.tsx
+++ packages/material-ui-lab/src/internal/pickers/PickersToolbarText.tsx
@@ -2,42 +2,14 @@ import * as React from 'react';
 import clsx from 'clsx';
 import Typography, { TypographyProps } from '@material-ui/core/Typography';
 import { experimentalStyled } from '@material-ui/core/styles';
-import {
-  unstable_composeClasses as composeClasses,
-  generateUtilityClass,
-  generateUtilityClasses,
-} from '@material-ui/unstyled';
-import { ExtendMui } from './typings/helpers';
+import { generateUtilityClasses } from '@material-ui/unstyled';
 
-export interface PickersToolbarTextProps extends ExtendMui<TypographyProps> {
-  classes?: {
-    root?: string;
-    selected?: string;
-  };
+export interface PickersToolbarTextProps extends Omit<TypographyProps, 'classes'> {
   selected?: boolean;
   value: React.ReactNode;
 }
 
-export type PickersToolbarTextClassKey = keyof NonNullable<PickersToolbarTextProps['classes']>;
-
-export function getPickersToolbarTextUtilityClass(slot: string) {
-  return generateUtilityClass('PrivatePickersToolbarText', slot);
-}
-
-export const pickersToolbarTextClasses = generateUtilityClasses<PickersToolbarTextClassKey>(
-  'PrivatePickersToolbarText',
-  ['root', 'selected'],
-);
-
-const useUtilityClasses = (styleProps: PickersToolbarTextProps) => {
-  const { selected, classes } = styleProps;
-
-  const slots = {
-    root: ['root', selected && 'selected'],
-  };
-
-  return composeClasses(slots, getPickersToolbarTextUtilityClass, classes);
-};
+const classes = generateUtilityClasses('PrivatePickersToolbarText', ['selected']);
 
 const PickersToolbarTextRoot = experimentalStyled(
   Typography,
@@ -46,7 +18,7 @@ const PickersToolbarTextRoot = experimentalStyled(
 )<{ component?: React.ElementType }>(({ theme }) => ({
   transition: theme.transitions.create('color'),
   color: theme.palette.text.secondary,
-  [`&.${pickersToolbarTextClasses.selected}`]: {
+  [`&.${classes.selected}`]: {
     color: theme.palette.text.primary,
   },
 }));
@@ -54,17 +26,12 @@ const PickersToolbarTextRoot = experimentalStyled(
 const PickersToolbarText = React.forwardRef<HTMLSpanElement, PickersToolbarTextProps>(
   function PickersToolbarText(props, ref) {
     const { className, selected, value, ...other } = props;
-    // TODO: convert to simple assignment after the type error in defaultPropsHandler.js:60:6 is fixed
-    const styleProps = { ...props };
-
-    const classes = useUtilityClasses(styleProps);
 
     return (
       <PickersToolbarTextRoot
         ref={ref}
-        className={clsx(classes.root, className)}
+        className={clsx({ [classes.selected]: selected }, className)}
         component="span"
-        styleProps={styleProps}
         {...other}
       >
         {value}
