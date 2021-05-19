84c2c25a53e55b4a9d2e46fa1efdcc8bc06ef986
Marija Najdova <mnajdova@gmail.com>
2021-05-19 14:49:11 +0200

    wip

--- packages/material-ui/src/styles/index.d.ts
+++ packages/material-ui/src/styles/index.d.ts
@@ -38,15 +38,6 @@ export {
 export { default as useTheme } from './useTheme';
 export { default as unstable_useThemeProps } from './useThemeProps';
 export * from './useThemeProps';
-export {
-  default as withStyles,
-  MuiStyles,
-  Styles,
-  WithStyles,
-  StyleRules,
-  StyleRulesCallback,
-  StyledComponentProps,
-} from './withStyles';
 export { default as experimentalStyled, CreateMUIStyled, CSSObject } from './experimentalStyled';
 export { default as ThemeProvider } from './ThemeProvider';
 export {
