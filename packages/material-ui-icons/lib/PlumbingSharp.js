7d5dc27c6748b881e0ce94aa2ca1614daafc0ca1
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2021-05-20 15:06:16 +0200

    [icons] Use array children instead of React fragments (#26309)

--- packages/material-ui-icons/lib/PlumbingSharp.js
+++ packages/material-ui-icons/lib/PlumbingSharp.js
@@ -2,25 +2,19 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
 
-var React = _interopRequireWildcard(require("react"));
-
 var _createSvgIcon = _interopRequireDefault(require("./utils/createSvgIcon"));
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-var _default = (0, _createSvgIcon.default)( /*#__PURE__*/(0, _jsxRuntime.jsxs)(React.Fragment, {
-  children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
-    d: "m16.16 5.64 3.54 3.54c1.17-1.17 1.17-3.07 0-4.24L16.16 1.4l-4.24 4.24 2.12 2.12 2.12-2.12zM4.842 12.7081l3.5355-3.5355 2.1213 2.1213-3.5355 3.5355z"
-  }), /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
-    d: "m15.45 7.76-1.41 1.41-4.25-4.24-2.12 2.12 4.24 4.24-8.49 8.49 2.83 2.83L16.86 12l.71.71 1.41-1.41-3.53-3.54z"
-  })]
-}), 'PlumbingSharp');
+var _default = (0, _createSvgIcon.default)([/*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
+  d: "m16.16 5.64 3.54 3.54c1.17-1.17 1.17-3.07 0-4.24L16.16 1.4l-4.24 4.24 2.12 2.12 2.12-2.12zM4.842 12.7081l3.5355-3.5355 2.1213 2.1213-3.5355 3.5355z"
+}, "0"), /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
+  d: "m15.45 7.76-1.41 1.41-4.25-4.24-2.12 2.12 4.24 4.24-8.49 8.49 2.83 2.83L16.86 12l.71.71 1.41-1.41-3.53-3.54z"
+}, "1")], 'PlumbingSharp');
 
 exports.default = _default;
--- packages/material-ui-icons/lib/esm/MuseumOutlined.js
+++ packages/material-ui-icons/lib/esm/MuseumOutlined.js
@@ -1,11 +1,7 @@
-import * as React from 'react';
 import createSvgIcon from './utils/createSvgIcon';
 import { jsx as _jsx } from "react/jsx-runtime";
-import { jsxs as _jsxs } from "react/jsx-runtime";
-export default createSvgIcon( /*#__PURE__*/_jsxs(React.Fragment, {
-  children: [/*#__PURE__*/_jsx("path", {
-    d: "M22 11V9L12 2 2 9v2h2v9H2v2h20v-2h-2v-9h2zm-4 9H6V9h12v11z"
-  }), /*#__PURE__*/_jsx("path", {
-    d: "m10 14 2 3 2-3v4h2v-7h-2l-2 3-2-3H8v7h2z"
-  })]
-}), 'MuseumOutlined');
+export default createSvgIcon([/*#__PURE__*/_jsx("path", {
+  d: "M22 11V9L12 2 2 9v2h2v9H2v2h20v-2h-2v-9h2zm-4 9H6V9h12v11z"
+}, "0"), /*#__PURE__*/_jsx("path", {
+  d: "m10 14 2 3 2-3v4h2v-7h-2l-2 3-2-3H8v7h2z"
+}, "1")], 'MuseumOutlined');
