4da97e085749333707a766f261507eea6d2b3617
Marija Najdova <mnajdova@gmail.com>
2021-05-28 15:57:55 +0200

    add typing from createTheme

--- packages/material-ui-system/src/createTheme/index.d.ts
+++ packages/material-ui-system/src/createTheme/index.d.ts
@@ -36,3 +36,12 @@ export interface Theme {
   zIndex: Record<string, any>;
   unstable_strictMode?: boolean;
 }
+
+/**
+ * Generate a theme base on the options received.
+ * @param options Takes an incomplete theme object and adds the missing parts.
+ * @param args Deep merge the arguments with the about to be returned theme.
+ * @returns A complete, ready to use theme object.
+ */
+ export default function createTheme(options?: ThemeOptions, ...args: object[]): Theme;
+ 
