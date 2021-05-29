36c6ecdb964b7459c5c875fff125dce539e10b17
Victor Casas <victor.casas@outlook.cl>
2021-05-29 11:28:13 -0400

    Migrate pro to emotion

--- docs/src/pages/landing/Pro.js
+++ docs/src/pages/landing/Pro.js
@@ -1,43 +1,36 @@
 import * as React from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/styles';
+import { experimentalStyled as styled } from '@material-ui/core/styles';
 import Link from 'docs/src/modules/components/Link';
 import { useTranslate } from 'docs/src/modules/utils/i18n';
 
-const useStyles = makeStyles(
-  (theme) => ({
-    root: {
-      padding: theme.spacing(1, 2, 1, 2),
-      right: 0,
-      left: 0,
-      color: theme.palette.common.white,
-      backgroundColor: '#626980',
-      position: 'relative',
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'flex',
-        top: 100,
-        left: 'auto',
-        position: 'absolute',
-        borderBottomLeftRadius: 36 / 2,
-        borderTopLeftRadius: 36 / 2,
-      },
-    },
-  }),
-  { name: 'Pro' },
-);
+const StyledLink = styled(Link)(({ theme }) => ({
+  padding: theme.spacing(1, 2, 1, 2),
+  right: 0,
+  left: 0,
+  color: theme.palette.common.white,
+  backgroundColor: '#626980',
+  position: 'relative',
+  display: 'none',
+  [theme.breakpoints.up('sm')]: {
+    display: 'flex',
+    top: 100,
+    left: 'auto',
+    position: 'absolute',
+    borderBottomLeftRadius: 36 / 2,
+    borderTopLeftRadius: 36 / 2,
+  },
+}));
 
 export default function Pro() {
-  const classes = useStyles();
   const t = useTranslate();
 
   return (
-    <Link
+    <StyledLink
       variant="body2"
-      className={clsx(classes.root, 'mui-fixed')}
+      className="mui-fixed"
       href="/getting-started/support/#professional-support-premium"
     >
       {t('getProfessionalSupport')}
-    </Link>
+    </StyledLink>
   );
 }
