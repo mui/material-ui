7dfa90f3890b1db587a412e750f5dfcb5b1ec529
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2021-05-31 13:06:37 +0200

    [pickers] Remove `allowKeyboardControl` (#26451)

--- packages/material-ui-lab/src/StaticDateTimePicker/StaticDateTimePicker.tsx
+++ packages/material-ui-lab/src/StaticDateTimePicker/StaticDateTimePicker.tsx
@@ -88,11 +88,6 @@ StaticDateTimePicker.propTypes /* remove-proptypes */ = {
    */
   acceptRegex: PropTypes.instanceOf(RegExp),
   /**
-   * Enables keyboard listener for moving between days in calendar.
-   * Defaults to `true` unless the `ClockPicker` is used inside a `Static*` picker component.
-   */
-  allowKeyboardControl: PropTypes.bool,
-  /**
    * If `true`, `onChange` is fired on click even if the same date is selected.
    * @default false
    */
@@ -107,6 +102,10 @@ StaticDateTimePicker.propTypes /* remove-proptypes */ = {
    * @default false
    */
   ampmInClock: PropTypes.bool,
+  /**
+   * @ignore
+   */
+  autoFocus: PropTypes.bool,
   /**
    * className applied to the root component.
    */
