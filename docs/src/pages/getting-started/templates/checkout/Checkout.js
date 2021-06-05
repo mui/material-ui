e7b6aa66d0646ebfc93da411c1aaef162def3f8b
Victor Casas <victor.casas@outlook.cl>
2021-06-05 11:42:53 -0400

    Fix style checkout

--- docs/src/pages/getting-started/templates/checkout/Checkout.js
+++ docs/src/pages/getting-started/templates/checkout/Checkout.js
@@ -73,7 +73,7 @@ function CheckoutContent() {
         </Toolbar>
       </AppBar>
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-        <Paper variant="outlined" sx={{ my: [3, 6], p: [2, 3] }}>
+        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
           <Typography component="h1" variant="h4" align="center">
             Checkout
           </Typography>
