b1518634d54b38fac8e9c7dd219d930aacc5e1fd
siriwatknp <siriwatkunaporn@gmail.com>
2021-06-06 21:04:33 +0700

    revert the code

--- docs/src/modules/components/MarkdownElement.js
+++ docs/src/modules/components/MarkdownElement.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/styles';
-import { createTheme, alpha } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 
 const styles = (theme) => ({
   root: {
@@ -36,9 +36,7 @@ const styles = (theme) => ({
       padding: '0 3px',
       color: theme.palette.text.primary,
       backgroundColor:
-        theme.palette.mode === 'light'
-          ? 'rgba(255, 229, 100, 0.2)'
-          : alpha(theme.palette.primary.main, 0.08),
+        theme.palette.mode === 'light' ? 'rgba(255, 229, 100, 0.2)' : 'rgba(255, 229, 100, 0.2)',
       fontSize: '.85em',
       borderRadius: 2,
     },
@@ -172,8 +170,7 @@ const styles = (theme) => ({
     },
     '& a, & a code': {
       // Style taken from the Link component
-      color:
-        theme.palette.mode === 'dark' ? theme.palette.primary.main : theme.palette.primary.dark,
+      color: theme.palette.primary.main,
       textDecoration: 'none',
       '&:hover': {
         textDecoration: 'underline',
