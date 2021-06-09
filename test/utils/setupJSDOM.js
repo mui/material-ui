49fbdb0696932f1a90a3d126e445f35575f445df
eps1lon <silbermann.sebastian@gmail.com>
2021-06-09 17:24:58 +0200

    mock scheduler for test:unit

--- test/utils/setupJSDOM.js
+++ test/utils/setupJSDOM.js
@@ -1,7 +1,17 @@
 const testingLibrary = require('@testing-library/dom');
 const Mocha = require('mocha');
+const Module = require('module');
 const createDOM = require('./createDOM');
 const { createMochaHooks } = require('./mochaHooks');
+
+const requireActual = Module.prototype.require;
+
+Module.prototype.require = function mockRequire(moduleId) {
+  if (moduleId === 'scheduler') {
+    return requireActual.call(this, 'scheduler/unstable_mock');
+  }
+  return requireActual.call(this, moduleId);
+};
 
 // Enable missing act warnings: https://github.com/facebook/react/blob/v16.13.1/packages/react-reconciler/src/ReactFiberHooks.js#L965
 // TODO: Revisit once https://github.com/facebook/react/issues/15439 is resolved.
