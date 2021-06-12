8f7b501f7e76828dcc78a56c52eca489c9e32451
Víctor Casas <57921784+vicasas@users.noreply.github.com>
2021-06-12 00:39:35 -0400

    [docs] Migrate onepirate premium template to emotion part1 (#26671)

--- docs/src/pages/premium-themes/onepirate/modules/components/AppBar.tsx
+++ docs/src/pages/premium-themes/onepirate/modules/components/AppBar.tsx
@@ -1,16 +1,8 @@
 import * as React from 'react';
-import { Theme } from '@material-ui/core/styles';
-import { withStyles, WithStyles } from '@material-ui/styles';
 import MuiAppBar, { AppBarProps } from '@material-ui/core/AppBar';
 
-const styles = (theme: Theme) => ({
-  root: {
-    color: theme.palette.common.white,
-  },
-});
-
-function AppBar(props: WithStyles<typeof styles> & AppBarProps) {
+function AppBar(props: AppBarProps) {
   return <MuiAppBar elevation={0} position="fixed" {...props} />;
 }
 
-export default withStyles(styles)(AppBar);
+export default AppBar;
