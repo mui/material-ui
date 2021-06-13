601253b395fc72832d7943496e884eab0b931a21
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-06-13 17:59:10 +0200

    even simpler

--- docs/src/pages/components/text-fields/LayoutTextFields.js
+++ docs/src/pages/components/text-fields/LayoutTextFields.js
@@ -2,47 +2,36 @@ import * as React from 'react';
 import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 
+function RedBar() {
+  return (
+    <Box
+      sx={{
+        height: 20,
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'light'
+            ? 'rgba(255, 0, 0, 0.1)'
+            : 'rgb(255 132 132 / 25%)',
+      }}
+    />
+  );
+}
+
 export default function LayoutTextFields() {
   return (
     <Box
       sx={{
         display: 'flex',
-        flexWrap: 'wrap',
-        '& .MuiTextField-root:first-of-type': { m: 1, mb: 2 },
-        '& .MuiTextField-root:not(:first-of-type)': { mx: 1, width: '25ch' },
+        flexDirection: 'column',
+        '& .MuiTextField-root': { width: '25ch' },
       }}
     >
-      <TextField
-        id="outlined-full-width"
-        label="Label"
-        placeholder="Placeholder"
-        helperText="Full width!"
-        fullWidth
-        margin="normal"
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
-      <TextField
-        label="None"
-        id="outlined-margin-none"
-        defaultValue="Default Value"
-        helperText="Some important text"
-      />
-      <TextField
-        label="Dense"
-        id="outlined-margin-dense"
-        defaultValue="Default Value"
-        helperText="Some important text"
-        margin="dense"
-      />
-      <TextField
-        label="Normal"
-        id="outlined-margin-normal"
-        defaultValue="Default Value"
-        helperText="Some important text"
-        margin="normal"
-      />
+      <RedBar />
+      <TextField label={'margin="none"'} id="margin-none" />
+      <RedBar />
+      <TextField label={'margin="dense"'} id="margin-dense" margin="dense" />
+      <RedBar />
+      <TextField label={'margin="normal"'} id="margin-normal" margin="normal" />
+      <RedBar />
     </Box>
   );
 }
