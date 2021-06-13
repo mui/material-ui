db059ca64978b6d687f2b4a59b4514b3dda14f90
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-06-14 00:50:13 +0200

    try first section without prevent default

--- docs/src/pages/guides/routing/ButtonDemo.js
+++ docs/src/pages/guides/routing/ButtonDemo.js
@@ -1,16 +1,10 @@
 import * as React from 'react';
 import Button from '@material-ui/core/Button';
 
-function preventDefault(event) {
-  event.preventDefault();
-}
-
 export default function ButtonDemo() {
   return (
-    <div role="presentation" onClick={preventDefault}>
-      <Button href="/" variant="contained">
-        Link
-      </Button>
-    </div>
+    <Button href="/" variant="contained">
+      Link
+    </Button>
   );
 }
