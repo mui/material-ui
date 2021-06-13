0d31f7acf8584ef3756a690ffc2e40e2289bc948
Victor Casas <victor.casas@outlook.cl>
2021-06-13 09:33:10 -0400

    Small demo layout

--- docs/src/pages/components/text-fields/LayoutTextFields.tsx
+++ docs/src/pages/components/text-fields/LayoutTextFields.tsx
@@ -8,118 +8,41 @@ export default function LayoutTextFields() {
       sx={{
         display: 'flex',
         flexWrap: 'wrap',
-        '& > :not(style)': { mb: 1 },
-        '& .MuiTextField-root:first-of-type': { m: 1 },
+        '& .MuiTextField-root:first-of-type': { m: 1, mb: 2 },
         '& .MuiTextField-root:not(:first-of-type)': { mx: 1, width: '25ch' },
       }}
     >
-      <div>
-        <TextField
-          id="outlined-full-width"
-          label="Label"
-          placeholder="Placeholder"
-          helperText="Full width!"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField
-          label="None"
-          id="outlined-margin-none"
-          defaultValue="Default Value"
-          helperText="Some important text"
-        />
-        <TextField
-          label="Dense"
-          id="outlined-margin-dense"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          margin="dense"
-        />
-        <TextField
-          label="Normal"
-          id="outlined-margin-normal"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          margin="normal"
-        />
-      </div>
-      <div>
-        <TextField
-          id="filled-full-width"
-          label="Label"
-          placeholder="Placeholder"
-          helperText="Full width!"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          label="None"
-          id="filled-margin-none"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="filled"
-        />
-        <TextField
-          label="Dense"
-          id="filled-margin-dense"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          margin="dense"
-          variant="filled"
-        />
-        <TextField
-          label="Normal"
-          id="filled-margin-normal"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          margin="normal"
-          variant="filled"
-        />
-      </div>
-      <div>
-        <TextField
-          id="standard-full-width"
-          label="Label"
-          placeholder="Placeholder"
-          helperText="Full width!"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="standard"
-        />
-        <TextField
-          label="None"
-          id="margin-none"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="standard"
-        />
-        <TextField
-          label="Dense"
-          id="margin-dense"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          margin="dense"
-          variant="standard"
-        />
-        <TextField
-          label="Normal"
-          id="margin-normal"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          margin="normal"
-          variant="standard"
-        />
-      </div>
+      <TextField
+        id="outlined-full-width"
+        label="Label"
+        placeholder="Placeholder"
+        helperText="Full width!"
+        fullWidth
+        margin="normal"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <TextField
+        label="None"
+        id="outlined-margin-none"
+        defaultValue="Default Value"
+        helperText="Some important text"
+      />
+      <TextField
+        label="Dense"
+        id="outlined-margin-dense"
+        defaultValue="Default Value"
+        helperText="Some important text"
+        margin="dense"
+      />
+      <TextField
+        label="Normal"
+        id="outlined-margin-normal"
+        defaultValue="Default Value"
+        helperText="Some important text"
+        margin="normal"
+      />
     </Box>
   );
 }
