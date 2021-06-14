4b23fcec78f958dff9cb12e944e5c00ae78fb21b
Marija Najdova <mnajdova@gmail.com>
2021-06-14 11:16:58 +0200

    fix argos, merged props

--- packages/material-ui-system/src/useThemeProps/useThemeProps.js
+++ packages/material-ui-system/src/useThemeProps/useThemeProps.js
@@ -4,5 +4,5 @@ import useTheme from '../useTheme';
 export default function useThemeProps({ props, name, defaultTheme }) {
   const theme = useTheme(defaultTheme);
   const mergedProps = getThemeProps({ theme, name, props });
-  return { ...mergedProps };
+  return mergedProps;
 }
