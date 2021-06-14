e3087cddef62a75172d904635274d931ccafe306
siriwatknp <siriwatkunaporn@gmail.com>
2021-06-14 11:21:29 +0700

    move to local styles

--- docs/src/modules/components/AppFrame.js
+++ docs/src/modules/components/AppFrame.js
@@ -85,6 +85,9 @@ const styles = (theme) => ({
   },
   root: {
     display: 'flex',
+    ...(theme.palette.mode === 'dark' && {
+      backgroundColor: '#222',
+    }),
   },
   grow: {
     flex: '1 1 auto',
