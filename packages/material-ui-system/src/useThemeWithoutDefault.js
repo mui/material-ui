5fe43e4f7e5d2e4693074ac78755b345f1e23be7
Marija Najdova <mnajdova@gmail.com>
2021-06-17 18:39:12 +0200

    remove styled-engine context

--- packages/material-ui-system/src/useThemeWithoutDefault.js
+++ packages/material-ui-system/src/useThemeWithoutDefault.js
@@ -1,15 +1,11 @@
-import * as React from 'react';
 import { useTheme as muiUseTheme } from '@material-ui/private-theming';
-import { ThemeContext } from '@material-ui/styled-engine';
 
 function isObjectEmpty(obj) {
   return Object.keys(obj).length === 0;
 }
 
 function useTheme(defaultTheme = null) {
-  const muiContextTheme = muiUseTheme();
-  const styledEngineContextTheme = React.useContext(ThemeContext);
-  const contextTheme = muiContextTheme || styledEngineContextTheme;
+  const contextTheme = muiUseTheme();
   return !contextTheme || isObjectEmpty(contextTheme) ? defaultTheme : contextTheme;
 }
 
