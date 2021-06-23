e82ae193832dba7a018d037329dcd839feffc2ca
siriwatknp <siriwatkunaporn@gmail.com>
2021-06-23 18:08:52 +0700

    revert the code

--- docs/src/pages/getting-started/templates/blog/FeaturedPost.js
+++ docs/src/pages/getting-started/templates/blog/FeaturedPost.js
@@ -6,7 +6,6 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Hidden from '@material-ui/core/Hidden';
 
 function FeaturedPost(props) {
   const { post } = props;
@@ -29,13 +28,11 @@ function FeaturedPost(props) {
               Continue reading...
             </Typography>
           </CardContent>
-          <Hidden smDown>
-            <CardMedia
-              sx={{ width: 160 }}
-              image={post.image}
-              title={post.imageText}
-            />
-          </Hidden>
+          <CardMedia
+            sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
+            image={post.image}
+            title={post.imageText}
+          />
         </Card>
       </CardActionArea>
     </Grid>
--- docs/src/pages/premium-themes/paperbase/Header.js
+++ docs/src/pages/premium-themes/paperbase/Header.js
@@ -5,7 +5,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import HelpIcon from '@material-ui/icons/Help';
-import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
 import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
@@ -26,18 +25,16 @@ function Header(props) {
       <AppBar color="primary" position="sticky" elevation={0}>
         <Toolbar>
           <Grid container spacing={1} alignItems="center">
-            <Hidden smUp>
-              <Grid item>
-                <IconButton
-                  color="inherit"
-                  aria-label="open drawer"
-                  onClick={onDrawerToggle}
-                  edge="start"
-                >
-                  <MenuIcon />
-                </IconButton>
-              </Grid>
-            </Hidden>
+            <Grid sx={{ display: { sm: 'none', xs: 'block' } }} item>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={onDrawerToggle}
+                edge="start"
+              >
+                <MenuIcon />
+              </IconButton>
+            </Grid>
             <Grid item xs />
             <Grid item>
               <Link
