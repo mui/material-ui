598ce4ccf8471a3b00dcc7ff2ec07bd2b8dca7e5
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-06-27 00:22:16 +0200

    [core] Apply @tag convention

--- packages/material-ui/src/styles/styled.d.ts
+++ packages/material-ui/src/styles/styled.d.ts
@@ -3,7 +3,6 @@ import { Theme } from './createTheme';
 
 /**
  * Custom styled utility that has a default MUI theme.
- *
  * @param tag HTML tag or component that should serve as base.
  * @param options Styled options for the created component.
  * @returns React component that has styles attached to it.
