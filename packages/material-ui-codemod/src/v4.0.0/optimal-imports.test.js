22cf9bcc84b2e141197a85ef4307019125a51a2d
Michał Dudak <michaldudak@users.noreply.github.com>
2021-06-28 10:30:08 +0200

    [core] Fix tests on Windows (#26931)
    * Ignore line endings in codemod tests
    * Fix envinfo tests on Windows
    * Fix errors related to path separators

--- packages/material-ui-codemod/src/v4.0.0/optimal-imports.test.js
+++ packages/material-ui-codemod/src/v4.0.0/optimal-imports.test.js
@@ -3,13 +3,14 @@ import path from 'path';
 import { expect } from 'chai';
 import jscodeshift from 'jscodeshift';
 import transform from './optimal-imports';
+import readFile from '../util/readFile';
 
 function trim(str) {
   return str ? str.replace(/^\s+|\s+$/, '') : '';
 }
 
 function read(fileName) {
-  return fs.readFileSync(path.join(__dirname, fileName), 'utf8').toString();
+  return readFile(path.join(__dirname, fileName));
 }
 
 describe('@material-ui/codemod', () => {
