89e33e1660d5de5a016fff8f019d192b6df54e6b
siriwatknp <siriwatkunaporn@gmail.com>
2021-06-29 21:54:33 +0700

    fix adaptV4Theme

--- packages/material-ui-codemod/src/v5.0.0/adapter-v4.js
+++ packages/material-ui-codemod/src/v5.0.0/adapter-v4.js
@@ -12,16 +12,16 @@ export default function transformer(file, api) {
    */
   utils.processImportFrom('@material-ui/core/styles', (nodes) => {
     nodes.forEach(({ node }) => {
-      utils.insertImportSpecifier(node, 'adapterV4Theme');
+      utils.insertImportSpecifier(node, 'adaptV4Theme');
     });
   });
 
-  function isNotAdapterV4ThemeArg(node) {
+  function isNotadaptV4ThemeArg(node) {
     return (
       node.arguments.length &&
       (node.arguments[0].type !== 'CallExpression' ||
         (node.arguments[0].type === 'CallExpression' &&
-          node.arguments[0].callee.name !== 'adapterV4Theme'))
+          node.arguments[0].callee.name !== 'adaptV4Theme'))
     );
   }
 
@@ -30,8 +30,8 @@ export default function transformer(file, api) {
    */
   utils.processCallExpression('createMuiTheme', (nodes) => {
     nodes.forEach(({ node }) => {
-      if (isNotAdapterV4ThemeArg(node)) {
-        node.arguments = [j.callExpression(j.identifier('adapterV4Theme'), node.arguments)];
+      if (isNotadaptV4ThemeArg(node)) {
+        node.arguments = [j.callExpression(j.identifier('adaptV4Theme'), node.arguments)];
       }
     });
   });
@@ -41,8 +41,8 @@ export default function transformer(file, api) {
    */
   utils.processCallExpression('createTheme', (nodes) => {
     nodes.forEach(({ node }) => {
-      if (isNotAdapterV4ThemeArg(node)) {
-        node.arguments = [j.callExpression(j.identifier('adapterV4Theme'), node.arguments)];
+      if (isNotadaptV4ThemeArg(node)) {
+        node.arguments = [j.callExpression(j.identifier('adaptV4Theme'), node.arguments)];
       }
     });
   });
--- packages/material-ui-codemod/src/v5.0.0/adapter-v4.test/expected.js
+++ packages/material-ui-codemod/src/v5.0.0/adapter-v4.test/expected.js
@@ -1,7 +1,7 @@
-import { createMuiTheme, createTheme, adapterV4Theme } from '@material-ui/core/styles';
+import { createMuiTheme, createTheme, adaptV4Theme } from '@material-ui/core/styles';
 
 const theme1 = createMuiTheme();
 const theme2 = createTheme();
 
-const theme3 = createMuiTheme(adapterV4Theme({ palette: { primary: { main: '#ff5252' } } }));
-const theme4 = createTheme(adapterV4Theme({ palette: { primary: { main: '#ff5252' } } }));
+const theme3 = createMuiTheme(adaptV4Theme({ palette: { primary: { main: '#ff5252' } } }));
+const theme4 = createTheme(adaptV4Theme({ palette: { primary: { main: '#ff5252' } } }));
