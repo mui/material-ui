b2c6b9e6a5b481ef9821cbd9ba95a456cf74566a
siriwatknp <siriwatkunaporn@gmail.com>
2021-06-29 15:14:25 +0700

    add prepend true to emotion cache

--- packages/material-ui-codemod/src/v5.0.0/styled-engine-provider.js
+++ packages/material-ui-codemod/src/v5.0.0/styled-engine-provider.js
@@ -2,7 +2,7 @@
  * @param {import('jscodeshift').FileInfo} file
  * @param {import('jscodeshift').API} api
  */
-export default function transformer(file, api, options) {
+export default function transformer(file, api, options = {}) {
   const j = api.jscodeshift;
   const root = j(file.source);
   const printOptions = options.printOptions || { quote: 'single' };
