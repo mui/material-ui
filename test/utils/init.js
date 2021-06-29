6e36be2bd144a2d2e7c4797d1810f3add43894c3
eps1lon <silbermann.sebastian@gmail.com>
2021-06-29 13:02:16 +0200

    f testing library

--- test/utils/init.js
+++ test/utils/init.js
@@ -1,6 +1,5 @@
 import enzyme from 'enzyme';
 import Adapter from '@eps1lon/enzyme-adapter-react-17';
-import * as React from 'react';
 import * as testingLibrary from '@testing-library/react/pure';
 import './initMatchers';
 
@@ -14,11 +13,3 @@ const defaultHidden = !process.env.CI;
 // adds verbosity for something that might be confusing
 console.warn(`${defaultHidden ? 'including' : 'excluding'} inaccessible elements by default`);
 testingLibrary.configure({ defaultHidden });
-
-// FIXME: Move to testing-library/react
-if (React.version.startsWith('18')) {
-  testingLibrary.configure({
-    asyncWrapper: React.unstable_act,
-    eventWrapper: React.unstable_act,
-  });
-}
