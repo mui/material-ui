361bdb6ee3566e73d0a548bf0d77bb7cdd4366f6
eps1lon <silbermann.sebastian@gmail.com>
2021-06-30 21:31:51 +0200

    [test] Update tests with latest state of StrictMode compatibility

--- packages/material-ui-lab/src/MobileDateTimePicker/MobileDateTimePicker.test.tsx
+++ packages/material-ui-lab/src/MobileDateTimePicker/MobileDateTimePicker.test.tsx
@@ -17,6 +17,7 @@ describe('<MobileDateTimePicker />', () => {
     clock.restore();
   });
 
+  // StrictModeViolation: Uses CalendarPicker
   const render = createPickerRender({ strict: false });
 
   it('opens dialog on textField click for Mobile mode', () => {
