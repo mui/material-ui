6dcaeaf15539738cb6779a13025383b3be871d97
Victor Casas <victor.casas@outlook.cl>
2021-06-30 09:05:29 -0400

    Use div

--- test/regressions/fixtures/Autocomplete/StandardAutocomplete.js
+++ test/regressions/fixtures/Autocomplete/StandardAutocomplete.js
@@ -1,17 +1,16 @@
 import * as React from 'react';
-import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/core/Autocomplete';
 
 export default function StandardAutocomplete() {
   return (
-    <Box sx={{ width: 300 }}>
+    <div style={{ width: 300 }}>
       <Autocomplete
         options={[]}
         renderInput={(params) => (
           <TextField {...params} label="Standard autocomplete" variant="standard" />
         )}
       />
-    </Box>
+    </div>
   );
 }
