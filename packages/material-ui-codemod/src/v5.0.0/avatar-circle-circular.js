e69a254333270630c6a86525a7acdb1695959096
siriwatknp <siriwatkunaporn@gmail.com>
2021-07-01 14:06:30 +0700

    use const

--- packages/material-ui-codemod/src/v5.0.0/avatar-circle-circular.js
+++ packages/material-ui-codemod/src/v5.0.0/avatar-circle-circular.js
@@ -5,7 +5,7 @@
 export default function transformer(file, api) {
   const j = api.jscodeshift;
 
-  let source = j(file.source)
+  const source = j(file.source)
     .findJSXElements('Avatar')
     .forEach((path) => {
       path.node.openingElement.attributes.forEach((node) => {
--- packages/material-ui-codemod/src/v5.0.0/badge-overlap-value.js
+++ packages/material-ui-codemod/src/v5.0.0/badge-overlap-value.js
@@ -5,7 +5,7 @@
 export default function transformer(file, api) {
   const j = api.jscodeshift;
 
-  let source = j(file.source)
+  const source = j(file.source)
     .findJSXElements('Badge')
 
     .forEach((path) => {
