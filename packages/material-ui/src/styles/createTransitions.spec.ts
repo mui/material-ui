14a0a8b3a5770790419aa34e12b1718263150062
siriwatknp <siriwatkunaporn@gmail.com>
2021-07-02 11:58:17 +0700

    fix spec

--- packages/material-ui/src/styles/createTransitions.spec.ts
+++ packages/material-ui/src/styles/createTransitions.spec.ts
@@ -1,5 +1,8 @@
-import createTransitions from './createTransitions';
+import { createTheme, Theme } from '@material-ui/core/styles';
 
-createTransitions();
-createTransitions({ easing: { easeInOut: 'cubic-bezier(0.4, 0, 0.2, 1)' } });
-createTransitions({ duration: { short: 250 } });
+{
+  const transitions = createTheme().transitions;
+  transitions.create('all');
+  transitions.create('all', { easing: 'cubic-bezier(0.4, 0, 0.2, 1)' });
+  transitions.create('all', { duration: 250 });
+}
