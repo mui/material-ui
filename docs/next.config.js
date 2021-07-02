9e7d5c019615a0f5951e6769256600c21ba0ba79
eps1lon <silbermann.sebastian@gmail.com>
2021-07-02 22:29:30 +0200

    REVERT LATER deploy all locales

--- docs/next.config.js
+++ docs/next.config.js
@@ -194,7 +194,8 @@ module.exports = {
 
     // We want to speed-up the build of pull requests.
     // For crowdin PRs we want to build all locales for testing.
-    if (process.env.PULL_REQUEST === 'true' && !l10nPRInNetlify && !vercelDeploy) {
+    // FIXME: REVERT BEFORE MERGING to `=== 'true'`
+    if (process.env.PULL_REQUEST === 'false' && !l10nPRInNetlify && !vercelDeploy) {
       // eslint-disable-next-line no-console
       console.log('Considering only English for SSR');
       traverse(pages, 'en');
