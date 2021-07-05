37309ca3492661664227914c0c644c33f6ed7aa4
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-07-05 15:18:02 +0200

    leading dot

--- packages/material-ui-unstyled/src/utils/isHostComponent.ts
+++ packages/material-ui-unstyled/src/utils/isHostComponent.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 
 /**
- * Determines if a given element is a DOM element name (i.e. not a React component)
+ * Determines if a given element is a DOM element name (i.e. not a React component).
  */
 function isHostComponent(element: React.ElementType) {
   return typeof element === 'string';
