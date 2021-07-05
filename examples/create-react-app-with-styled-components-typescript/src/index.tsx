a392c8c3b3c45304185dbc4e3b3eed2d6acf2a5e
Marija Najdova <mnajdova@gmail.com>
2021-07-05 19:21:10 +0200

    updated examples

--- examples/create-react-app-with-styled-components-typescript/src/index.tsx
+++ examples/create-react-app-with-styled-components-typescript/src/index.tsx
@@ -5,11 +5,10 @@ import { StyledEngineProvider } from '@material-ui/core/styles';
 import App from './App';
 
 ReactDOM.render(
-  // TODO v5: remove once migration to emotion is completed
-  <StyledEngineProvider injectFirst>
+  <>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
     <App />
-  </StyledEngineProvider>,
+  </>,
   document.getElementById('root'),
 );
