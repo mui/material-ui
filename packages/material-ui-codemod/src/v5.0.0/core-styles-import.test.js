43b3d05ea242e94eb3bbe2580622d1f9ae3c85cc
siriwatknp <siriwatkunaporn@gmail.com>
2021-07-06 01:14:26 +0700

    add core-styles-import codemod

---
+++ packages/material-ui-codemod/src/v5.0.0/core-styles-import.test.js
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import path from 'path';
+import { expect } from 'chai';
+import jscodeshift from 'jscodeshift';
+import transform from './core-styles-import';
+import readFile from '../util/readFile';
+
+function read(fileName) {
+  return readFile(path.join(__dirname, fileName));
+}
+
+describe('@material-ui/codemod', () => {
+  describe('v5.0.0', () => {
+    describe('core-styles-import', () => {
+      it('transforms as needed', () => {
+        const actual = transform(
+          {
+            source: read('./core-styles-import.test/actual.js'),
+            path: require.resolve('./core-styles-import.test/actual.js'),
+          },
+          { jscodeshift: jscodeshift },
+          {},
+        );
+
+        const expected = read('./core-styles-import.test/expected.js');
+        expect(actual).to.equal(expected, 'The transformed version should be correct');
+      });
+
+      it('should be idempotent', () => {
+        const actual = transform(
+          {
+            source: read('./core-styles-import.test/expected.js'),
+            path: require.resolve('./core-styles-import.test/expected.js'),
+          },
+          { jscodeshift: jscodeshift },
+          {},
+        );
+
+        const expected = read('./core-styles-import.test/expected.js');
+        expect(actual).to.equal(expected, 'The transformed version should be correct');
+      });
+    });
+  });
+});
