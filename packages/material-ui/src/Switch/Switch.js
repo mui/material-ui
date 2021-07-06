ca9e81b98b0850e26167f070d94cd078cc796160
Michał Dudak <michal@dudak.me>
2021-07-06 17:06:54 +0200

    Use useSwitch in Switch implementation

--- packages/material-ui/src/Switch/Switch.js
+++ packages/material-ui/src/Switch/Switch.js
@@ -2,119 +2,115 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-import { refType } from '@material-ui/utils';
-import { unstable_composeClasses as composeClasses } from '@material-ui/unstyled';
-import { alpha, darken, lighten } from '@material-ui/system';
-import capitalize from '../utils/capitalize';
-import SwitchBase from '../internal/SwitchBase';
-import useThemeProps from '../styles/useThemeProps';
-import styled from '../styles/styled';
+import { SwitchUnstyled, unstable_composeClasses as composeClasses } from '@material-ui/unstyled';
+import { alpha, darken, lighten, useThemeProps, styled } from '@material-ui/system';
+import { capitalize } from '@material-ui/core/utils';
+import useFormControl from '../FormControl/useFormControl';
 import switchClasses, { getSwitchUtilityClass } from './switchClasses';
+import TouchRipple from '../ButtonBase/TouchRipple';
+import useTouchRipple from '../useTouchRipple';
 
 const useUtilityClasses = (styleProps) => {
-  const { classes, edge, size, color, checked, disabled } = styleProps;
+  const { classes, edge, size, color, checked, disabled, focusVisible } = styleProps;
 
   const slots = {
-    root: ['root', edge && `edge${capitalize(edge)}`, `size${capitalize(size)}`],
+    root: [
+      edge && `edge${capitalize(edge)}`,
+      `size${capitalize(size)}`,
+      `color${capitalize(color)}`,
+    ],
     switchBase: [
       'switchBase',
       `color${capitalize(color)}`,
+      focusVisible && 'focusVisible',
       checked && 'checked',
       disabled && 'disabled',
     ],
-    thumb: ['thumb'],
     track: ['track'],
-    input: ['input'],
   };
 
   const composedClasses = composeClasses(slots, getSwitchUtilityClass, classes);
 
   return {
-    ...classes, // forward the disabled and checked classes to the SwitchBase
     ...composedClasses,
   };
 };
 
-const SwitchRoot = styled('span', {
+const SwitchTrack = styled('span', {
   name: 'MuiSwitch',
-  slot: 'Root',
-  overridesResolver: (props, styles) => {
-    const { styleProps } = props;
-
-    return [
-      styles.root,
-      styleProps.edge && styles[`edge${capitalize(styleProps.edge)}`],
-      styles[`size${capitalize(styleProps.size)}`],
-    ];
-  },
-})(({ styleProps }) => ({
-  /* Styles applied to the root element. */
-  display: 'inline-flex',
-  width: 34 + 12 * 2,
-  height: 14 + 12 * 2,
-  overflow: 'hidden',
-  padding: 12,
-  boxSizing: 'border-box',
-  position: 'relative',
-  flexShrink: 0,
-  zIndex: 0, // Reset the stacking context.
-  verticalAlign: 'middle', // For correct alignment with the text.
-  '@media print': {
-    colorAdjust: 'exact',
-  },
-  /* Styles applied to the root element if `edge="start"`. */
-  ...(styleProps.edge === 'start' && {
-    marginLeft: -8,
+  slot: 'Track',
+  overridesResolver: (props, styles) => styles.track,
+})(({ theme }) => ({
+  height: '100%',
+  width: '100%',
+  borderRadius: 14 / 2,
+  zIndex: -1,
+  transition: theme.transitions.create(['opacity', 'background-color'], {
+    duration: theme.transitions.duration.shortest,
   }),
-  /* Styles applied to the root element if `edge="end"`. */
-  ...(styleProps.edge === 'end' && {
-    marginRight: -8,
-  }),
-  ...(styleProps.size === 'small' && {
-    width: 40,
-    height: 24,
-    padding: 7,
-    [`& .${switchClasses.thumb}`]: {
-      width: 16,
-      height: 16,
-    },
-    [`& .${switchClasses.switchBase}`]: {
-      padding: 4,
-      [`&.${switchClasses.checked}`]: {
-        transform: 'translateX(16px)',
-      },
-    },
-  }),
+  backgroundColor:
+    theme.palette.mode === 'light' ? theme.palette.common.black : theme.palette.common.white,
+  opacity: theme.palette.mode === 'light' ? 0.38 : 0.3,
 }));
 
-const SwitchSwitchBase = styled(SwitchBase, {
+const SwitchBase = styled('span', {
   name: 'MuiSwitch',
   slot: 'SwitchBase',
   overridesResolver: (props, styles) => {
     const { styleProps } = props;
 
-    return [
-      styles.switchBase,
-      styles.input,
-      styleProps.color !== 'default' && styles[`color${capitalize(styleProps.color)}`],
-    ];
+    return {
+      ...styles.switchBase,
+      ...styles.input,
+      ...(styleProps.color !== 'default' && styles[`color${capitalize(styleProps.color)}`]),
+    };
   },
 })(
-  ({ theme }) => ({
-    /* Styles applied to the internal `SwitchBase` component's `root` class. */
+  ({ theme, styleProps }) => ({
     position: 'absolute',
     top: 0,
     left: 0,
-    zIndex: 1, // Render above the focus ripple.
+    zIndex: 1,
     color: theme.palette.mode === 'light' ? theme.palette.common.white : theme.palette.grey[300],
     transition: theme.transitions.create(['left', 'transform'], {
       duration: theme.transitions.duration.shortest,
     }),
+    padding: 9,
+    borderRadius: '50%',
+    ...(styleProps.edge === 'start' && {
+      marginLeft: styleProps.size === 'small' ? -3 : -12,
+    }),
+    ...(styleProps.edge === 'end' && {
+      marginRight: styleProps.size === 'small' ? -3 : -12,
+    }),
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    boxSizing: 'border-box',
+    WebkitTapHighlightColor: 'transparent',
+    backgroundColor: 'transparent',
+    outline: 0,
+    border: 0,
+    margin: 0,
+    cursor: 'pointer',
+    userSelect: 'none',
+    verticalAlign: 'middle',
+    MozAppearance: 'none',
+    WebkitAppearance: 'none',
+    textDecoration: 'none',
+    '&::-moz-focus-inner': {
+      borderStyle: 'none',
+    },
+    '@media print': {
+      colorAdjust: 'exact',
+    },
     [`&.${switchClasses.checked}`]: {
       transform: 'translateX(20px)',
     },
     [`&.${switchClasses.disabled}`]: {
       color: theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[600],
+      pointerEvents: 'none',
+      cursor: 'default',
     },
     [`&.${switchClasses.checked} + .${switchClasses.track}`]: {
       opacity: 0.5,
@@ -122,21 +118,14 @@ const SwitchSwitchBase = styled(SwitchBase, {
     [`&.${switchClasses.disabled} + .${switchClasses.track}`]: {
       opacity: theme.palette.mode === 'light' ? 0.12 : 0.2,
     },
-    [`& .${switchClasses.input}`]: {
-      /* Styles applied to the internal SwitchBase component's input element. */
-      left: '-100%',
-      width: '300%',
-    },
   }),
   ({ theme, styleProps }) => ({
     '&:hover': {
       backgroundColor: alpha(theme.palette.action.active, theme.palette.action.hoverOpacity),
-      // Reset on touch devices, it doesn't add specificity
       '@media (hover: none)': {
         backgroundColor: 'transparent',
       },
     },
-    /* Styles applied to the internal SwitchBase component element unless `color="default"`. */
     ...(styleProps.color !== 'default' && {
       [`&.${switchClasses.checked}`]: {
         color: theme.palette[styleProps.color].main,
@@ -163,30 +152,62 @@ const SwitchSwitchBase = styled(SwitchBase, {
   }),
 );
 
-const SwitchTrack = styled('span', {
+const SwitchRoot = styled('span', {
   name: 'MuiSwitch',
-  slot: 'Track',
-  overridesResolver: (props, styles) => styles.track,
-})(({ theme }) => ({
-  /* Styles applied to the track element. */
-  height: '100%',
-  width: '100%',
-  borderRadius: 14 / 2,
-  zIndex: -1,
-  transition: theme.transitions.create(['opacity', 'background-color'], {
-    duration: theme.transitions.duration.shortest,
+  slot: 'Root',
+  overridesResolver: (props, styles) => {
+    const { styleProps } = props;
+
+    return {
+      ...styles.root,
+      ...(styleProps.edge && styles[`edge${capitalize(styleProps.edge)}`]),
+      ...styles[`size${capitalize(styleProps.size)}`],
+      ...styles.input,
+      ...(styleProps.color !== 'default' && styles[`color${capitalize(styleProps.color)}`]),
+    };
+  },
+})(({ styleProps }) => ({
+  display: 'inline-flex',
+  width: 34 + 12 * 2,
+  height: 14 + 12 * 2,
+  overflow: 'hidden',
+  padding: 12,
+  boxSizing: 'border-box',
+  position: 'relative',
+  flexShrink: 0,
+  zIndex: 0,
+  verticalAlign: 'middle',
+  '@media print': {
+    colorAdjust: 'exact',
+  },
+  ...(styleProps.edge === 'start' && {
+    marginLeft: -8,
   }),
-  backgroundColor:
-    theme.palette.mode === 'light' ? theme.palette.common.black : theme.palette.common.white,
-  opacity: theme.palette.mode === 'light' ? 0.38 : 0.3,
+  ...(styleProps.edge === 'end' && {
+    marginRight: -8,
+  }),
+  ...(styleProps.size === 'small' && {
+    width: 40,
+    height: 24,
+    padding: 7,
+    [`& .${switchClasses.thumb}`]: {
+      width: 16,
+      height: 16,
+    },
+    [`& .${switchClasses.switchBase}`]: {
+      padding: 4,
+      [`&.${switchClasses.checked}`]: {
+        transform: 'translateX(16px)',
+      },
+    },
+  }),
 }));
 
-const SwitchThumb = styled('span', {
+const DefaultSwitchThumb = styled('span', {
   name: 'MuiSwitch',
   slot: 'Thumb',
   overridesResolver: (props, styles) => styles.thumb,
 })(({ theme }) => ({
-  /* Styles used to create the thumb passed to the internal `SwitchBase` component `icon` prop. */
   boxShadow: theme.shadows[1],
   backgroundColor: 'currentColor',
   width: 20,
@@ -194,36 +215,206 @@ const SwitchThumb = styled('span', {
   borderRadius: '50%',
 }));
 
+const SwitchInput = styled('input', {
+  name: 'MuiSwitch',
+  slot: 'Input',
+  skipSx: true,
+})({
+  cursor: 'inherit',
+  position: 'absolute',
+  opacity: 0,
+  width: '300%',
+  height: '100%',
+  top: 0,
+  left: '-100%',
+  margin: 0,
+  padding: 0,
+  zIndex: 1,
+});
+
+/* eslint-disable react/prop-types */
+
+const SwitchThumb = ({ isChecked, icon, checkedIcon, className }) => {
+  if (!isChecked && icon) {
+    return icon;
+  }
+
+  if (isChecked && checkedIcon) {
+    return checkedIcon;
+  }
+
+  return <DefaultSwitchThumb className={className} />;
+};
+
+const SwitchLayout = React.forwardRef((props, ref) => {
+  const {
+    className,
+    disableRipple,
+    disableTouchRipple,
+    disableFocusRipple,
+    styleProps,
+    TouchRippleProps,
+    children,
+    onFocus,
+    onBlur,
+    ...other
+  } = props;
+
+  const { checked, disabled, focusVisible } = styleProps;
+
+  const rippleRef = React.useRef(null);
+
+  const { enableTouchRipple, getRippleHandlers } = useTouchRipple({
+    rippleRef,
+    focusVisible,
+    disabled,
+    disableRipple,
+    disableTouchRipple,
+    disableFocusRipple,
+  });
+
+  const rippleHandlers = getRippleHandlers({
+    onBlur,
+  });
+
+  if (process.env.NODE_ENV !== 'production') {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    React.useEffect(() => {
+      if (enableTouchRipple && !rippleRef.current) {
+        console.error(
+          [
+            'Material-UI: The `component` prop provided to ButtonBase is invalid.',
+            'Please make sure the children prop is rendered in this custom component.',
+          ].join('\n'),
+        );
+      }
+    }, [enableTouchRipple]);
+  }
+
+  const classes = useUtilityClasses({
+    ...styleProps,
+    checked,
+    disabled,
+    focusVisible,
+  });
+
+  return (
+    <SwitchRoot
+      {...other}
+      className={clsx(className, classes.root)}
+      ref={ref}
+      styleProps={styleProps}
+    >
+      <SwitchBase
+        className={classes.switchBase}
+        styleProps={styleProps}
+        {...rippleHandlers}
+        onFocus={onFocus}
+      >
+        {children}
+        {enableTouchRipple && <TouchRipple ref={rippleRef} center {...TouchRippleProps} />}
+      </SwitchBase>
+      <SwitchTrack className={classes.track} />
+    </SwitchRoot>
+  );
+});
+
+/* eslint-enable react/prop-types */
+
 const Switch = React.forwardRef(function Switch(inProps, ref) {
   const props = useThemeProps({ props: inProps, name: 'MuiSwitch' });
-  const { className, color = 'primary', edge = false, size = 'medium', sx, ...other } = props;
+
+  const {
+    checked: checkedProp,
+    checkedIcon,
+    className,
+    color = 'primary',
+    disabled: disabledProp,
+    disableFocusRipple = false,
+    disableRipple = false,
+    disableTouchRipple = false,
+    edge = false,
+    icon,
+    inputProps,
+    onBlur,
+    onFocus,
+    size = 'medium',
+    TouchRippleProps,
+    ...other
+  } = props;
+
+  const muiFormControl = useFormControl();
+
+  const handleFocus = (event) => {
+    onFocus?.(event);
+
+    if (muiFormControl && muiFormControl.onFocus) {
+      muiFormControl.onFocus(event);
+    }
+  };
+
+  const handleBlur = (event) => {
+    onBlur?.(event);
+
+    if (muiFormControl && muiFormControl.onBlur) {
+      muiFormControl.onBlur(event);
+    }
+  };
+
+  let disabled = disabledProp;
+
+  if (muiFormControl) {
+    if (typeof disabled === 'undefined') {
+      disabled = muiFormControl.disabled;
+    }
+  }
 
   const styleProps = {
     ...props,
     color,
     edge,
     size,
+    disableFocusRipple,
+    disableRipple,
+    disableTouchRipple,
   };
 
-  const classes = useUtilityClasses(styleProps);
-  const icon = <SwitchThumb className={classes.thumb} styleProps={styleProps} />;
+  const components = {
+    Root: SwitchLayout,
+    Input: SwitchInput,
+    Thumb: SwitchThumb,
+  };
+
+  const componentsProps = {
+    root: {
+      className,
+      styleProps,
+      disableFocusRipple,
+      disableRipple,
+      disableTouchRipple,
+      onBlur: handleBlur,
+      onFocus: handleFocus,
+      TouchRippleProps,
+    },
+    input: {
+      styleProps,
+      ...inputProps,
+    },
+    thumb: {
+      icon,
+      checkedIcon,
+      defaultThumbClassName: switchClasses.thumb,
+    },
+  };
 
   return (
-    <SwitchRoot className={clsx(classes.root, className)} sx={sx} styleProps={styleProps}>
-      <SwitchSwitchBase
-        type="checkbox"
-        icon={icon}
-        checkedIcon={icon}
-        ref={ref}
-        styleProps={styleProps}
-        {...other}
-        classes={{
-          ...classes,
-          root: classes.switchBase,
-        }}
-      />
-      <SwitchTrack className={classes.track} styleProps={styleProps} />
-    </SwitchRoot>
+    <SwitchUnstyled
+      ref={ref}
+      components={components}
+      componentsProps={componentsProps}
+      disabled={disabled}
+      {...other}
+    />
   );
 });
 
@@ -252,9 +443,14 @@ Switch.propTypes /* remove-proptypes */ = {
    * The color of the component. It supports those theme colors that make sense for this component.
    * @default 'primary'
    */
-  color: PropTypes /* @typescript-to-proptypes-ignore */.oneOfType([
-    PropTypes.oneOf(['default', 'primary', 'secondary', 'error', 'info', 'success', 'warning']),
-    PropTypes.string,
+  color: PropTypes.oneOf([
+    'default',
+    'error',
+    'info',
+    'primary',
+    'secondary',
+    'success',
+    'warning',
   ]),
   /**
    * The default checked state. Use when the component is not controlled.
@@ -265,9 +461,19 @@ Switch.propTypes /* remove-proptypes */ = {
    */
   disabled: PropTypes.bool,
   /**
+   * If `true`, the  keyboard focus ripple is disabled.
+   * @default false
+   */
+  disableFocusRipple: PropTypes.bool,
+  /**
    * If `true`, the ripple effect is disabled.
    */
   disableRipple: PropTypes.bool,
+  /**
+   * If `true`, the touch ripple effect is disabled.
+   * @default false
+   */
+  disableTouchRipple: PropTypes.bool,
   /**
    * If given, uses a negative margin to counteract the padding on one
    * side (this is often helpful for aligning the left or right
@@ -291,7 +497,16 @@ Switch.propTypes /* remove-proptypes */ = {
   /**
    * Pass a ref to the `input` element.
    */
-  inputRef: refType,
+  inputRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({
+      current: PropTypes.any.isRequired,
+    }),
+  ]),
+  /**
+   * @ignore
+   */
+  onBlur: PropTypes.func,
   /**
    * Callback fired when the state is changed.
    *
@@ -301,6 +516,10 @@ Switch.propTypes /* remove-proptypes */ = {
    */
   onChange: PropTypes.func,
   /**
+   * @ignore
+   */
+  onFocus: PropTypes.func,
+  /**
    * If `true`, the `input` element is required.
    */
   required: PropTypes.bool,
@@ -309,14 +528,15 @@ Switch.propTypes /* remove-proptypes */ = {
    * `small` is equivalent to the dense switch styling.
    * @default 'medium'
    */
-  size: PropTypes /* @typescript-to-proptypes-ignore */.oneOfType([
-    PropTypes.oneOf(['medium', 'small']),
-    PropTypes.string,
-  ]),
+  size: PropTypes.oneOf(['medium', 'small']),
   /**
    * The system prop that allows defining system overrides as well as additional CSS styles.
    */
   sx: PropTypes.object,
+  /**
+   * Props applied to the `TouchRipple` element.
+   */
+  TouchRippleProps: PropTypes.object,
   /**
    * The value of the component. The DOM API casts this to a string.
    * The browser uses "on" as the default value.
