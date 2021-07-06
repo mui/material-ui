cf778b099ae0c22c16a6d24f31f6d5a56709a5d4
Marija Najdova <mnajdova@gmail.com>
2021-07-06 10:33:12 +0200

    Update index.js

--- examples/create-react-app-with-styled-components/src/index.js
+++ examples/create-react-app-with-styled-components/src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <>
+  <React.Fragment>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
     <App />
-  </>,
+  </React.Fragment>,
   document.getElementById('root'),
 );
 
