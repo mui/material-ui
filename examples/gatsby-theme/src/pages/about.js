53f4bc261028baa3083036fac54197bda149562b
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-07-07 17:40:23 +0200

    review

--- examples/gatsby-theme/src/pages/about.js
+++ examples/gatsby-theme/src/pages/about.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { StyledEngineProvider } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
