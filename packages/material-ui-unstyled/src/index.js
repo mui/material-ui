a7da4023dba24bf0a104890b49284092830c784e
Michał Dudak <michal@dudak.me>
2021-07-12 16:54:18 +0200

    Fixing the build

--- packages/material-ui-unstyled/src/index.js
+++ packages/material-ui-unstyled/src/index.js
@@ -7,23 +7,19 @@ export * from './BadgeUnstyled';
 export { default as FormControlUnstyled } from './FormControlUnstyled';
 export * from './FormControlUnstyled';
 
-export { default as ModalUnstyled } from './ModalUnstyled';
-export * from './ModalUnstyled';
-
 export { default as SliderUnstyled } from './SliderUnstyled';
 export * from './SliderUnstyled';
 
 export { default as SwitchUnstyled } from './SwitchUnstyled';
 export * from './SwitchUnstyled';
 
+export { default as ModalUnstyled } from './ModalUnstyled';
+export * from './ModalUnstyled';
+
 export { default as Portal } from './Portal';
 
 export { default as Unstable_TrapFocus } from './Unstable_TrapFocus';
-
 export { default as generateUtilityClass } from './generateUtilityClass';
-
 export { default as generateUtilityClasses } from './generateUtilityClasses';
-
 export { default as unstable_composeClasses } from './composeClasses';
-
 export * from './utils';
