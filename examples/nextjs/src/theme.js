91791c71b323d775eabc8b850863d037ff1a9c5a
Martin Vandersteen <vandersteenmartin@gmail.com>
2021-07-13 10:35:10 +0200

    Edit createTheme import path to fix error #18377
    Original import path would raise error `Attempted import error: 'ThemeProvider' is not exported from '@material-ui/core/styles'`.
    Removing to `/core` part fixes it as mentioned in https://github.com/mui-org/material-ui/issues/18377 !

--- examples/nextjs/src/theme.js
+++ examples/nextjs/src/theme.js
@@ -1,4 +1,4 @@
-import { createTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/styles';
 import { red } from '@material-ui/core/colors';
 
 // Create a theme instance.
