91d2f45e5009f606f9ea445d1a7e81175cd1e3c5
Abolfazl Panbehkar <41692768+Panbehkar@users.noreply.github.com>
2021-07-13 11:43:33 +0430

    Update CustomStyles.tsx
    Fix error `Invalid module name in augmentation, module '@material-ui/core/styles/createTheme' cannot be found.ts`

--- docs/src/pages/customization/theming/CustomStyles.tsx
+++ docs/src/pages/customization/theming/CustomStyles.tsx
@@ -9,7 +9,7 @@ import {
 } from '@material-ui/core/styles';
 import { orange } from '@material-ui/core/colors';
 
-declare module '@material-ui/core/styles/createTheme' {
+declare module '@material-ui/core/styles/createMuiTheme' {
   interface Theme {
     status: {
       danger: string;
