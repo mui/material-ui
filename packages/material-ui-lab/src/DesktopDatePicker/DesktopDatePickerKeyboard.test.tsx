07cf2f6ce3b96a3a43895a75fd666983420f39a2
eps1lon <silbermann.sebastian@gmail.com>
2021-07-14 11:39:22 +0200

    Enable enter-keydown opens datepicker test
    same pattern as e.g. PickersDay

--- packages/material-ui-lab/src/DesktopDatePicker/DesktopDatePickerKeyboard.test.tsx
+++ packages/material-ui-lab/src/DesktopDatePicker/DesktopDatePickerKeyboard.test.tsx
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import { spy, useFakeTimers } from 'sinon';
 import { isWeekend } from 'date-fns';
 import TextField from '@material-ui/core/TextField';
-import { fireEvent, screen, act } from 'test/utils';
+import { fireEvent, screen } from 'test/utils';
 import DesktopDatePicker, { DesktopDatePickerProps } from '@material-ui/lab/DesktopDatePicker';
 import { adapterToUse, createPickerRender } from '../internal/pickers/test-utils';
 import { MakeOptional } from '../internal/pickers/typings/helpers';
@@ -200,20 +200,18 @@ describe('<DesktopDatePicker /> keyboard interactions', () => {
     });
   });
 
-  // TODO
-  // eslint-disable-next-line mocha/no-skipped-tests
-  it.skip('Opens calendar by keydown on the open button', () => {
+  it('Opens calendar by keydown on the open button', () => {
     render(<TestKeyboardDatePicker />);
     const openButton = screen.getByLabelText(/choose date/i);
 
-    act(() => {
-      openButton.focus();
-    });
+    // A native button implies Enter and Space keydown behavior
+    // These keydown events only trigger click behavior if they're trusted (programmatically dispatched events aren't trusted).
+    // If this breaks, make sure to add tests for
+    // - fireEvent.keyDown(targetDay, { key: 'Enter' })
+    // - fireEvent.keyUp(targetDay, { key: 'Space' })
+    expect(openButton.tagName).to.equal('BUTTON');
 
-    fireEvent.keyDown(openButton, {
-      key: 'Enter',
-      keyCode: 13,
-    });
+    fireEvent.click(openButton);
 
     expect(screen.queryByRole('dialog')).toBeVisible();
   });
