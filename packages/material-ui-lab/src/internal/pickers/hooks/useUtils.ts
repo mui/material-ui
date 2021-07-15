3c95b618a58629d5dcde16f8b49ae6b76031b522
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2021-07-15 14:14:52 +0200

    [pickers] Minify error when LocalizationProvider is missing (#27295)

--- packages/material-ui-lab/src/internal/pickers/hooks/useUtils.ts
+++ packages/material-ui-lab/src/internal/pickers/hooks/useUtils.ts
@@ -1,22 +1,18 @@
 import * as React from 'react';
+import MuiError from '@material-ui/utils/macros/MuiError.macro';
 import { MuiPickersAdapterContext } from '../../../LocalizationProvider';
 
 // Required for babel https://github.com/vercel/next.js/issues/7882. Replace with `export type` in future
 export type MuiPickersAdapter<TDate = unknown> =
   import('../../../LocalizationProvider/LocalizationProvider').MuiPickersAdapter<TDate>;
 
-// TODO uncomment when syntax will be allowed by next babel
-function checkUtils(utils: MuiPickersAdapter | null) /* :asserts utils is MuiPickersAdapter */ {
-  if (!utils) {
-    throw new Error(
+export function useUtils<T = unknown>() {
+  const utils = React.useContext(MuiPickersAdapterContext);
+  if (utils === null) {
+    throw new MuiError(
       'Can not find utils in context. It looks like you forgot to wrap your component in LocalizationProvider, or pass dateAdapter prop directly.',
     );
   }
-}
-
-export function useUtils<T = unknown>() {
-  const utils = React.useContext(MuiPickersAdapterContext);
-  checkUtils(utils);
 
   return utils as MuiPickersAdapter<T>;
 }
