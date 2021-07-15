fdb2f179f4ed7a989552af9f79c8344a466bf762
Serena Lin <71234575+serenalin121@users.noreply.github.com>
2021-07-15 13:46:29 -0700

    [Accordion, Collapse] Fix failed proptype error (#27307)

--- packages/material-ui/src/Accordion/Accordion.js
+++ packages/material-ui/src/Accordion/Accordion.js
@@ -174,11 +174,13 @@ Accordion.propTypes = {
    */
   classes: chainPropTypes(PropTypes.object, (props) => {
     if (props.classes && props.classes.focused) {
-      throw new Error([
-        'Material-UI: the classes.focused key is deprecated.',
-        'Use `classes.focusedVisible` instead',
-        'The name of the pseudo-class was changed for consistency.',
-      ]).join('\n');
+      throw new Error(
+        [
+          'Material-UI: the classes.focused key is deprecated.',
+          'Use `classes.focusedVisible` instead',
+          'The name of the pseudo-class was changed for consistency.',
+        ].join('\n'),
+      );
     }
 
     return null;
