5ff68d84d105d7c6bde998b2f50c41915e2d3b6f
Goodness Chris-Ugari <goodnesschrisugari@yahoo.com>
2021-07-15 21:30:57 +0100

    Fixed Popper is not closing when Autocomplete is disabled
    This is a fix for issue #26378

--- packages/material-ui/src/useAutocomplete/useAutocomplete.js
+++ packages/material-ui/src/useAutocomplete/useAutocomplete.js
@@ -67,6 +67,7 @@ export default function useAutocomplete(props) {
     autoHighlight = false,
     autoSelect = false,
     blurOnSelect = false,
+    disabled,
     clearOnBlur = !props.freeSolo,
     clearOnEscape = false,
     componentName = 'useAutocomplete',
@@ -957,6 +958,10 @@ export default function useAutocomplete(props) {
 
       return acc;
     }, []);
+  }
+  
+  if (disabled && focused){
+    handleBlur();
   }
 
   return {
