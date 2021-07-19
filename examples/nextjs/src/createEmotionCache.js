de4a5af40dcac0698e9953bbddb43debb1f5c1ad
Marija Najdova <mnajdova@gmail.com>
2021-07-19 16:18:26 +0200

    Update examples/nextjs/src/createEmotionCache.js

--- examples/nextjs/src/createEmotionCache.js
+++ examples/nextjs/src/createEmotionCache.js
@@ -1,9 +1,5 @@
 import createCache from '@emotion/cache';
 
 export default function createEmotionCache() {
-  const cache = createCache({ key: 'css' });
-  // compat is important. It prevents emotion to render invalid HTML.
-  // See https://github.com/mui-org/material-ui/issues/26561
-  cache.compat = true;
-  return cache;
+  return createCache({ key: 'css' });
 }
