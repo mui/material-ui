fe529af21192a6b71f012fcfebb6a961c2d9cc88
siriwatknp <siriwatkunaporn@gmail.com>
2021-07-20 14:06:54 +0700

    add references section

--- docs/pages/branding/home.tsx
+++ docs/pages/branding/home.tsx
@@ -1,8 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
 import * as React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppHeader from 'docs/src/layouts/AppHeader';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 import Hero from 'docs/src/components/home/Hero';
+import References from 'docs/src/components/home/References';
 import brandingTheme from 'docs/src/modules/brandingTheme';
 
 export default function Home() {
@@ -11,6 +16,16 @@ export default function Home() {
       <CssBaseline />
       <AppHeader />
       <Hero />
+      <Container sx={{ py: 8 }}>
+        <References />
+        <Typography color="grey.600" maxWidth={360} textAlign="center" mx="auto" mt={4}>
+          From startups to Fortune 500s, the world's best product teams leverage{' '}
+          <Box component="span" color="primary.main">
+            <b>MUI</b>
+          </Box>{' '}
+          to build their UIs.
+        </Typography>
+      </Container>
     </ThemeProvider>
   );
 }
