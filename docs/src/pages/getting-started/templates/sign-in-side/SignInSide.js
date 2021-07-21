8c699aefbc586aa45412b1eb1c695b9c5a4c7f92
Arun kumar mahto <76945986+arunkr741@users.noreply.github.com>
2021-07-21 16:57:34 +0530

    Update SignInSide.js

--- docs/src/pages/getting-started/templates/sign-in-side/SignInSide.js
+++ docs/src/pages/getting-started/templates/sign-in-side/SignInSide.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
+
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
