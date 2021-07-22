1d2a7bfb6a00897ef2b8de2882b43b9e28cdba91
Marija Najdova <mnajdova@gmail.com>
2021-07-22 13:48:35 +0200

    Showcase failing private imports

--- packages/material-ui-codemod/src/v5.0.0/private-imports.test/actual.js
+++ packages/material-ui-codemod/src/v5.0.0/private-imports.test/actual.js
@@ -3,3 +3,7 @@ import Tab from '@material-ui/core/Tab';
 import MuiTabs from '@material-ui/core/Tabs';
 import createTheme from '@material-ui/core/styles/createTheme';
 import yellow from "@material-ui/core/colors/yellow";
+// just testing that these get ignored
+import TableContext from '@material-ui/core/Table/TableContext';
+import TabScrollButton from '@material-ui/core/Tabs/TabScrollButton';
+import SwitchBase from '@material-ui/core/internal/SwitchBase';
--- packages/material-ui-codemod/src/v5.0.0/private-imports.test/expected.js
+++ packages/material-ui-codemod/src/v5.0.0/private-imports.test/expected.js
@@ -3,3 +3,7 @@ import Tab from '@material-ui/core/Tab';
 import MuiTabs from '@material-ui/core/Tabs';
 import { yellow } from '@material-ui/core/colors';
 import { createTheme } from '@material-ui/core/styles';
+// just testing that these get ignored
+import TableContext from '@material-ui/core/Table/TableContext';
+import TabScrollButton from '@material-ui/core/Tabs/TabScrollButton';
+import SwitchBase from '@material-ui/core/internal/SwitchBase';
