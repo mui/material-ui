cc1a73de49c20403931ce36b0bfd4c60640c4d5e
siriwatknp <siriwatkunaporn@gmail.com>
2021-07-23 15:50:39 +0700

    add ThemeDatePicker

---
+++ docs/src/pages/components/date-picker/ThemeDatePicker.js
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import { ThemeProvider, createTheme, useTheme } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
+import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
+import StaticDatePicker from '@material-ui/lab/StaticDatePicker';
+
+const primary = {
+  50: '#F0F7FF',
+  100: '#C2E0FF',
+  200: '#80BFFF',
+  300: '#66B2FF',
+  400: '#3399FF',
+  500: '#007FFF',
+  600: '#0072E5',
+  700: '#0059B2',
+  800: '#004C99',
+  900: '#003A75',
+};
+
+export default function ThemeDatePicker() {
+  const [value, setValue] = React.useState(new Date());
+  const globalTheme = useTheme();
+  const mode = globalTheme.palette.mode;
+  const theme = React.useMemo(() => {
+    const baseTheme = createTheme({
+      palette: {
+        mode,
+        primary,
+        background: {
+          paper: mode === 'dark' ? primary[800] : '#fff',
+        },
+      },
+      shape: {
+        borderRadius: 10,
+      },
+      spacing: 10,
+      typography: {
+        fontFamily: [
+          '"PlusJakartaSans"',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'sans-serif',
+        ].join(','),
+        button: {
+          textTransform: 'initial',
+        },
+      },
+    });
+    return createTheme(baseTheme, {
+      components: {
+        MuiButtonBase: {
+          defaultProps: {
+            disableTouchRipple: true,
+          },
+        },
+        MuiIconButton: {
+          styleOverrides: {
+            root: {
+              color: mode === 'dark' ? primary[300] : primary[500],
+            },
+          },
+        },
+        MuiCalendarPicker: {
+          styleOverrides: {
+            root: {
+              '& .MuiTypography-caption': {
+                color: mode === 'dark' ? '#96A3B0' : '#AAB4BE',
+              },
+            },
+          },
+        },
+        MuiPickersDay: {
+          styleOverrides: {
+            root: {
+              color: mode === 'dark' ? primary[200] : '#5A6978',
+              fontWeight: 500,
+            },
+            today: {
+              '&:not(.Mui-selected)': {
+                borderColor: baseTheme.palette.primary.main,
+              },
+            },
+          },
+        },
+      },
+    });
+  }, [mode]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Box
+          sx={{
+            '& > div': {
+              border: '1px solid',
+              borderColor: mode === 'dark' ? '#132F4C' : '#E5E8EC',
+              borderRadius: 1,
+            },
+          }}
+        >
+          <StaticDatePicker
+            displayStaticWrapperAs="desktop"
+            value={value}
+            onChange={(newValue) => {
+              setValue(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </Box>
+      </LocalizationProvider>
+    </ThemeProvider>
+  );
+}
