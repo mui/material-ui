6727acb37178fec378a548b44db78373b2940784
Michał Dudak <michal@dudak.me>
2021-07-23 14:16:57 +0200

    [Autocomplete] Explain how the loading prop works

--- packages/material-ui/src/Autocomplete/Autocomplete.js
+++ packages/material-ui/src/Autocomplete/Autocomplete.js
@@ -853,6 +853,7 @@ Autocomplete.propTypes /* remove-proptypes */ = {
   ListboxProps: PropTypes.object,
   /**
    * If `true`, the component is in a loading state.
+   * This shows the `loadingText` in place of suggestions (only if there are no suggestions to show, e.g. `options` are empty).
    * @default false
    */
   loading: PropTypes.bool,
