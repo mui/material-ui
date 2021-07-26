31225509505bd1d17a0cb1a2384426ede6f8e805
Ahmed Hakim <me@hakim.ninja>
2021-07-26 11:44:25 +0200

    fix removing the injected css
    after migrating to nextjs v11 most of SSR components got broken

--- examples/nextjs/pages/_app.js
+++ examples/nextjs/pages/_app.js
@@ -11,7 +11,7 @@ export default function MyApp(props) {
   React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
+    if (jssStyles && !process.browser) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
