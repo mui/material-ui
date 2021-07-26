fa632e623fd8972f09d37e4e18b54353d90e8916
siriwatknp <siriwatkunaporn@gmail.com>
2021-07-26 15:30:47 +0700

    add demos to hero

--- docs/src/components/home/Hero.tsx
+++ docs/src/components/home/Hero.tsx
@@ -7,6 +7,16 @@ import Typography from '@material-ui/core/Typography';
 import KeyboardArrowRightRounded from '@material-ui/icons/KeyboardArrowRightRounded';
 import ContentCopyRounded from '@material-ui/icons/ContentCopyRounded';
 import GradientText from 'docs/src/components/GradientText';
+import PlayerCard from 'docs/src/pages/components/cards/PlayerCard';
+import TaskCard from 'docs/src/pages/components/cards/TaskCard';
+import NotificationCard from 'docs/src/pages/components/cards/NotificationCard';
+import CustomizedChip from 'docs/src/pages/components/chips/CustomizedChip';
+import ThemeDatePicker from 'docs/src/pages/components/date-picker/ThemeDatePicker';
+import ThemeSlider from 'docs/src/pages/components/slider/ThemeSlider';
+import FolderTable from 'docs/src/pages/components/tables/FolderTable';
+import ThemeTabs from 'docs/src/pages/components/tabs/ThemeTabs';
+import ThemeTimeline from 'docs/src/pages/components/timeline/ThemeTimeline';
+import ViewToggleButton from 'docs/src/pages/components/toggle-button/ViewToggleButton';
 
 export default function Hero() {
   return (
@@ -73,6 +83,7 @@ export default function Hero() {
             sx={{ maxHeight: '100%', display: { xs: 'none', md: 'initial' } }}
           >
             <Box
+              aria-hidden="true"
               sx={{
                 bgcolor: 'grey.50',
                 minWidth: 2000,
@@ -81,7 +92,92 @@ export default function Hero() {
                 maxHeight: { lg: 700, xl: 1000 },
                 borderBottomLeftRadius: 10,
               }}
-            />
+            >
+              <Box
+                sx={{
+                  maxWidth: 1000,
+                  py: 3,
+                  px: 5,
+                  display: 'grid',
+                  gap: 3,
+                  gridTemplateColumns: 'repeat(auto-fit, minmax(60px, 1fr))',
+                  gridAutoRows: '60px',
+                  gridAutoFlow: 'dense',
+                  alignTracks: 'center',
+                  '& > *': {
+                    justifySelf: 'center',
+                    alignSelf: 'center',
+                    opacity: 0.6,
+                    transition: '0.4s',
+                    '&:hover': {
+                      opacity: 1,
+                    },
+                  },
+                  '& *': {
+                    fontFamily: [
+                      '"PlusJakartaSans"',
+                      '-apple-system',
+                      'BlinkMacSystemFont',
+                      'sans-serif',
+                    ].join(','),
+                  },
+                }}
+              >
+                <Box sx={{ gridRow: 'span 4', gridColumn: 'span 4' }}>
+                  <TaskCard />
+                </Box>
+                <Box
+                  sx={{
+                    gridRow: 'span 3',
+                    gridColumn: 'span 1',
+                    ml: -2,
+                    py: 2,
+                    bgcolor: 'background.paper',
+                    borderRadius: 5,
+                  }}
+                >
+                  <ThemeSlider />
+                </Box>
+                <Box sx={{ gridRow: 'span 3', gridColumn: 'span 3' }}>
+                  <FolderTable />
+                </Box>
+                <Box sx={{ gridRow: 'span 4', gridColumn: 'span 1' }} />
+                <Box sx={{ gridColumn: 'span 3' }}>
+                  <ViewToggleButton />
+                </Box>
+                <Box sx={{ gridRow: 'span 1', gridColumn: 'span 4' }}>
+                  <ThemeTabs />
+                </Box>
+                <Box sx={{ gridRow: 'span 3', gridColumn: 'span 3' }}>
+                  <ThemeTimeline />
+                </Box>
+                <Box sx={{ gridRow: 'span 3', gridColumn: 'span 3' }} />
+                <Box sx={{ gridRow: 'span 2', gridColumn: 'span 4' }}>
+                  <PlayerCard />
+                </Box>
+                <Box sx={{ gridRow: 'span 1', gridColumn: 'span 4' }}>
+                  <CustomizedChip />
+                </Box>
+                <Box
+                  sx={{
+                    gridRow: 'span 5',
+                    gridColumn: 'span 4',
+                    '&&': { justifySelf: 'flex-start', alignSelf: 'flex-start' },
+                  }}
+                >
+                  <ThemeDatePicker />
+                </Box>
+                <Box
+                  sx={{
+                    gridRow: 'span 2',
+                    gridColumn: 'span 4',
+                    '&&': { alignSelf: 'flex-start' },
+                  }}
+                >
+                  <NotificationCard />
+                </Box>
+              </Box>
+            </Box>
           </Grid>
         </Grid>
       </Container>
