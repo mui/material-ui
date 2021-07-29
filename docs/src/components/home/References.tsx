da38926731929830a6207c3c7b8b1ec2d3dc7a0e
siriwatknp <siriwatkunaporn@gmail.com>
2021-07-29 13:11:10 +0700

    fix references

--- docs/src/components/home/References.tsx
+++ docs/src/components/home/References.tsx
@@ -10,26 +10,66 @@ const References = () => {
       justifyContent="center"
       sx={{
         textAlign: 'center',
-        '& img': { maxWidth: '100%', px: 1 },
+        '& > div': {
+          px: 1,
+        },
+        '& img': { maxWidth: '100%', height: 'auto' },
       }}
     >
       <Grid item xs={4} md={2}>
-        <img src="/static/branding/companies/coursera.svg" alt="Coursera logo" />
+        <div>
+          <img
+            width="102"
+            height="16"
+            src="/static/branding/companies/coursera.svg"
+            alt="Coursera logo"
+          />
+        </div>
       </Grid>
       <Grid item xs={4} md={2}>
-        <img src="/static/branding/companies/amazon.svg" alt="Amazon logo" />
+        <div>
+          <img
+            width="92"
+            height="28"
+            src="/static/branding/companies/amazon.svg"
+            alt="Amazon logo"
+          />
+        </div>
       </Grid>
       <Grid item xs={4} md={2}>
-        <img src="/static/branding/companies/nasa.svg" alt="Nasa logo" />
+        <div>
+          <img width="64" height="53" src="/static/branding/companies/nasa.svg" alt="Nasa logo" />
+        </div>
       </Grid>
       <Grid item xs={4} md={2}>
-        <img src="/static/branding/companies/netflix.svg" alt="Netflix logo" />
+        <div>
+          <img
+            width="88"
+            height="24"
+            src="/static/branding/companies/netflix.svg"
+            alt="Netflix logo"
+          />
+        </div>
       </Grid>
       <Grid item xs={4} md={2}>
-        <img src="/static/branding/companies/unity.svg" alt="Unity logo" />
+        <div>
+          <img
+            width="110"
+            height="40"
+            src="/static/branding/companies/unity.svg"
+            alt="Unity logo"
+          />
+        </div>
       </Grid>
       <Grid item xs={4} md={2}>
-        <img src="/static/branding/companies/shutterstock.svg" alt="Shutterstock logo" />
+        <div>
+          <img
+            width="138"
+            height="21"
+            src="/static/branding/companies/shutterstock.svg"
+            alt="Shutterstock logo"
+          />
+        </div>
       </Grid>
     </Grid>
   );
