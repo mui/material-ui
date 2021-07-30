mui/material-ui

commit 032cde1aa876fb9254264a8789e0be04efca6528

[MenuList] Include disabled items in keyboard navigation (#19967)

packages/material-ui/src/MenuList/MenuList.js
@@ -46,7 +46,14 @@ function textCriteriaMatches(nextFocus, textCriteria) {
   return text.indexOf(textCriteria.keys.join('')) === 0;
 }
 
-function moveFocus(list, currentFocus, disableListWrap, traversalFunction, textCriteria) {
+function moveFocus(
+  list,
+  currentFocus,
+  disableListWrap,
+  disabledItemsFocusable,
+  traversalFunction,
+  textCriteria,
+) {
   let wrappedOnce = false;
   let nextFocus = traversalFunction(list, currentFocus, currentFocus ? disableListWrap : false);
 
@@ -54,25 +61,28 @@ function moveFocus(list, currentFocus, disableListWrap, traversalFunction, textC
     // Prevent infinite loop.
     if (nextFocus === list.firstChild) {
       if (wrappedOnce) {
-        return false;
+        return;
       }
       wrappedOnce = true;
     }
-    // Move to the next element.
+
+    // Same logic as useAutocomplete.js
+    const nextFocusDisabled = disabledItemsFocusable
+      ? false
+      : nextFocus.disabled || nextFocus.getAttribute('aria-disabled') === 'true';
+
     if (
       !nextFocus.hasAttribute('tabindex') ||
-      nextFocus.disabled ||
-      nextFocus.getAttribute('aria-disabled') === 'true' ||
-      !textCriteriaMatches(nextFocus, textCriteria)
+      !textCriteriaMatches(nextFocus, textCriteria) ||
+      nextFocusDisabled
     ) {
+      // Move to the next element.
       nextFocus = traversalFunction(list, nextFocus, disableListWrap);
     } else {
       nextFocus.focus();
-      return true;
+      return;
     }
   }
-
-  return false;
 }
 
 const useEnhancedEffect = typeof window === 'undefined' ? React.useEffect : React.useLayoutEffect;
@@ -92,8 +102,9 @@ const MenuList = React.forwardRef(function MenuList(props, ref) {
     autoFocusItem = false,
     children,
     className,
-    onKeyDown,
+    disabledItemsFocusable = false,
     disableListWrap = false,
+    onKeyDown,
     variant = 'selectedMenu',
     ...other
   } = props;
@@ -145,16 +156,16 @@ const MenuList = React.forwardRef(function MenuList(props, ref) {
     if (key === 'ArrowDown') {
       // Prevent scroll of the page
       event.preventDefault();
-      moveFocus(list, currentFocus, disableListWrap, nextItem);
+      moveFocus(list, currentFocus, disableListWrap, disabledItemsFocusable, nextItem);
     } else if (key === 'ArrowUp') {
       event.preventDefault();
-      moveFocus(list, currentFocus, disableListWrap, previousItem);
+      moveFocus(list, currentFocus, disableListWrap, disabledItemsFocusable, previousItem);
     } else if (key === 'Home') {
       event.preventDefault();
-      moveFocus(list, null, disableListWrap, nextItem);
+      moveFocus(list, null, disableListWrap, disabledItemsFocusable, nextItem);
     } else if (key === 'End') {
       event.preventDefault();
-      moveFocus(list, null, disableListWrap, previousItem);
+      moveFocus(list, null, disableListWrap, disabledItemsFocusable, previousItem);
     } else if (key.length === 1) {
       const criteria = textCriteriaRef.current;
       const lowerKey = key.toLowerCase();
@@ -175,7 +186,8 @@ const MenuList = React.forwardRef(function MenuList(props, ref) {
         currentFocus && !criteria.repeating && textCriteriaMatches(currentFocus, criteria);
       if (
         criteria.previousKeyMatched &&
-        (keepFocusOnCurrent || moveFocus(list, currentFocus, false, nextItem, criteria))
+        (keepFocusOnCurrent ||
+          moveFocus(list, currentFocus, false, disabledItemsFocusable, nextItem, criteria))
       ) {
         event.preventDefault();
       } else {
@@ -280,6 +292,10 @@ MenuList.propTypes = {
    * @ignore
    */
   className: PropTypes.string,
+  /**
+   * If `true`, will allow focus on disabled items.
+   */
+  disabledItemsFocusable: PropTypes.bool,
   /**
    * If `true`, the menu items will not wrap focus.
    */

packages/material-ui/test/integration/MenuList.test.js
@@ -73,7 +73,7 @@ describe('<MenuList> integration', () => {
       expect(menuitems[2]).to.have.property('tabIndex', -1);
     });
 
-    it('should select the secont item when pressing down if the first item is selected', () => {
+    it('should select the second item when pressing down if the first item is selected', () => {
       const { getAllByRole } = render(
         <MenuList autoFocusItem>
           <MenuItem selected>Menu Item 1</MenuItem>
@@ -325,37 +325,22 @@ describe('<MenuList> integration', () => {
     const menuitems = getAllByRole('menuitem');
 
     fireEvent.keyDown(document.activeElement, { key: 'ArrowDown' });
-
     expect(menuitems[0]).to.have.focus;
-
     fireEvent.keyDown(document.activeElement, { key: 'ArrowDown' });
-
     expect(menuitems[1]).to.have.focus;
-
     fireEvent.keyDown(document.activeElement, { key: 'ArrowDown' });
-
     expect(menuitems[3]).to.have.focus;
-
     fireEvent.keyDown(document.activeElement, { key: 'ArrowDown' });
-
     expect(menuitems[0]).to.have.focus;
 
     // and ArrowUp again
-
     fireEvent.keyDown(document.activeElement, { key: 'ArrowUp' });
-
     expect(menuitems[3]).to.have.focus;
-
     fireEvent.keyDown(document.activeElement, { key: 'ArrowUp' });
-
     expect(menuitems[1]).to.have.focus;
-
     fireEvent.keyDown(document.activeElement, { key: 'ArrowUp' });
-
     expect(menuitems[0]).to.have.focus;
-
     fireEvent.keyDown(document.activeElement, { key: 'ArrowUp' });
-
     expect(menuitems[3]).to.have.focus;
   });
 
@@ -371,23 +356,14 @@ describe('<MenuList> integration', () => {
     const menuitems = getAllByRole('menuitem');
 
     fireEvent.keyDown(document.activeElement, { key: 'ArrowDown' });
-
     expect(menuitems[1]).to.have.focus;
-
     fireEvent.keyDown(document.activeElement, { key: 'ArrowDown' });
-
     expect(menuitems[1]).to.have.focus;
-
     fireEvent.keyDown(document.activeElement, { key: 'ArrowDown' });
-
     expect(menuitems[1]).to.have.focus;
-
     fireEvent.keyDown(document.activeElement, { key: 'ArrowUp' });
-
     expect(menuitems[1]).to.have.focus;
-
     fireEvent.keyDown(document.activeElement, { key: 'ArrowUp' });
-
     expect(menuitems[1]).to.have.focus;
   });
 
@@ -403,24 +379,39 @@ describe('<MenuList> integration', () => {
     const menu = getByRole('menu');
 
     fireEvent.keyDown(document.activeElement, { key: 'Home' });
-
     expect(menu).to.have.focus;
-
     fireEvent.keyDown(document.activeElement, { key: 'ArrowDown' });
-
     expect(menu).to.have.focus;
-
     fireEvent.keyDown(document.activeElement, { key: 'ArrowDown' });
-
     expect(menu).to.have.focus;
-
     fireEvent.keyDown(document.activeElement, { key: 'End' });
-
     expect(menu).to.have.focus;
-
     fireEvent.keyDown(document.activeElement, { key: 'ArrowUp' });
-
     expect(menu).to.have.focus;
+  });
+
+  it('should allow focus on disabled items when disabledItemsFocusable=true', () => {
+    const { getAllByRole } = render(
+      <MenuList autoFocus disabledItemsFocusable>
+        <MenuItem disabled>Menu Item 1</MenuItem>
+        <MenuItem disabled>Menu Item 2</MenuItem>
+        <MenuItem disabled>Menu Item 3</MenuItem>
+        <MenuItem disabled>Menu Item 4</MenuItem>
+      </MenuList>,
+    );
+
+    const menuitems = getAllByRole('menuitem');
+
+    fireEvent.keyDown(document.activeElement, { key: 'Home' });
+    expect(menuitems[0]).to.have.focus;
+    fireEvent.keyDown(document.activeElement, { key: 'ArrowDown' });
+    expect(menuitems[1]).to.have.focus;
+    fireEvent.keyDown(document.activeElement, { key: 'ArrowDown' });
+    expect(menuitems[2]).to.have.focus;
+    fireEvent.keyDown(document.activeElement, { key: 'End' });
+    expect(menuitems[3]).to.have.focus;
+    fireEvent.keyDown(document.activeElement, { key: 'ArrowUp' });
+    expect(menuitems[2]).to.have.focus;
   });
 
   describe('MenuList text-based keyboard controls', () => {

docs/src/pages/components/autocomplete/LimitTags.js
@@ -21,7 +21,7 @@ export default function LimitTags() {
       <Autocomplete
         multiple
         limitTags={2}
-        id="tags-standard"
+        id="limit-tags"
         options={top100Films}
         getOptionLabel={(option) => option.title}
         defaultValue={[top100Films[13], top100Films[12], top100Films[11]]}

docs/src/pages/components/autocomplete/LimitTags.tsx
@@ -23,7 +23,7 @@ export default function LimitTags() {
       <Autocomplete
         multiple
         limitTags={2}
-        id="tags-standard"
+        id="limit-tags"
         options={top100Films}
         getOptionLabel={(option) => option.title}
         defaultValue={[top100Films[13], top100Films[12], top100Films[11]]}

packages/material-ui-lab/src/Autocomplete/Autocomplete.js
@@ -249,6 +249,7 @@ const Autocomplete = React.forwardRef(function Autocomplete(props, ref) {
     disableClearable = false,
     disableCloseOnSelect = false,
     disabled = false,
+    disabledItemsFocusable = false,
     disableListWrap = false,
     disablePortal = false,
     filterOptions,
@@ -575,6 +576,10 @@ Autocomplete.propTypes = {
    * If `true`, the input will be disabled.
    */
   disabled: PropTypes.bool,
+  /**
+   * If `true`, will allow focus on disabled items.
+   */
+  disabledItemsFocusable: PropTypes.bool,
   /**
    * If `true`, the list box in the popup will not wrap focus.
    */

packages/material-ui-lab/src/useAutocomplete/useAutocomplete.d.ts
@@ -67,6 +67,10 @@ export interface UseAutocompleteCommonProps<T> {
    */
   disableCloseOnSelect?: boolean;
   /**
+   * If `true`, will allow focus on disabled items.
+   */
+  disabledItemsFocusable?: boolean;
+  /**
    * If `true`, the list box in the popup will not wrap focus.
    */
   disableListWrap?: boolean;
@@ -181,6 +185,10 @@ export type AutocompleteInputChangeReason = 'input' | 'reset' | 'clear';
 
 export interface UseAutocompleteMultipleProps<T> extends UseAutocompleteCommonProps<T> {
   /**
+   * The default input value. Use when the component is not controlled.
+   */
+  defaultValue?: T[];
+  /**
    * If `true`, `value` must be an array and the menu will support multiple selections.
    */
   multiple: true;
@@ -191,10 +199,6 @@ export interface UseAutocompleteMultipleProps<T> extends UseAutocompleteCommonPr
    * You can customize the equality behavior with the `getOptionSelected` prop.
    */
   value?: T[];
-  /**
-   * The default input value. Use when the component is not controlled.
-   */
-  defaultValue?: T[];
   /**
    * Callback fired when the value changes.
    *
@@ -212,6 +216,10 @@ export interface UseAutocompleteMultipleProps<T> extends UseAutocompleteCommonPr
 
 export interface UseAutocompleteSingleProps<T> extends UseAutocompleteCommonProps<T> {
   /**
+   * The default input value. Use when the component is not controlled.
+   */
+  defaultValue?: T;
+  /**
    * If `true`, `value` must be an array and the menu will support multiple selections.
    */
   multiple?: false;
@@ -222,10 +230,6 @@ export interface UseAutocompleteSingleProps<T> extends UseAutocompleteCommonProp
    * You can customize the equality behavior with the `getOptionSelected` prop.
    */
   value?: T | null;
-  /**
-   * The default input value. Use when the component is not controlled.
-   */
-  defaultValue?: T;
   /**
    * Callback fired when the value changes.
    *

packages/material-ui-lab/src/useAutocomplete/useAutocomplete.js
@@ -73,6 +73,7 @@ export default function useAutocomplete(props) {
     defaultValue = props.multiple ? [] : null,
     disableClearable = false,
     disableCloseOnSelect = false,
+    disabledItemsFocusable = false,
     disableListWrap = false,
     filterOptions = defaultFilterOptions,
     filterSelectedOptions = false,
@@ -312,12 +313,12 @@ export default function useAutocomplete(props) {
       const option = listboxRef.current.querySelector(`[data-option-index="${nextFocus}"]`);
 
       // Same logic as MenuList.js
-      if (
-        option &&
-        (!option.hasAttribute('tabindex') ||
-          option.disabled ||
-          option.getAttribute('aria-disabled') === 'true')
-      ) {
+      const nextFocusDisabled = disabledItemsFocusable
+        ? false
+        : option && (option.disabled || option.getAttribute('aria-disabled') === 'true');
+
+      if ((option && !option.hasAttribute('tabindex')) || nextFocusDisabled) {
+        // Move to the next element.
         nextFocus += direction === 'next' ? 1 : -1;
       } else {
         return nextFocus;
@@ -641,9 +642,16 @@ export default function useAutocomplete(props) {
           break;
         }
         if (highlightedIndexRef.current !== -1 && popupOpen) {
+          const option = filteredOptions[highlightedIndexRef.current];
+          const disabled = getOptionDisabled ? getOptionDisabled(option) : false;
+
+          if (disabled) {
+            return;
+          }
+
           // We don't want to validate the form.
           event.preventDefault();
-          selectNewValue(event, filteredOptions[highlightedIndexRef.current], 'select-option');
+          selectNewValue(event, option, 'select-option');
 
           // Move the selection to the end.
           if (autoComplete) {
@@ -1011,6 +1019,10 @@ useAutocomplete.propTypes = {
    * If `true`, the popup won't close when a value is selected.
    */
   disableCloseOnSelect: PropTypes.bool,
+  /**
+   * If `true`, will allow focus on disabled items.
+   */
+  disabledItemsFocusable: PropTypes.bool,
   /**
    * If `true`, the list box in the popup will not wrap focus.
    */

packages/material-ui/src/ButtonBase/ButtonBase.js
@@ -158,11 +158,8 @@ const ButtonBase = React.forwardRef(function ButtonBase(props, ref) {
     },
     false,
   );
-  const handleFocus = useEventCallback((event) => {
-    if (disabled) {
-      return;
-    }
 
+  const handleFocus = useEventCallback((event) => {
     // Fix for https://github.com/facebook/react/issues/7769
     if (!buttonRef.current) {
       buttonRef.current = event.currentTarget;
@@ -215,7 +212,12 @@ const ButtonBase = React.forwardRef(function ButtonBase(props, ref) {
     }
 
     // Keyboard accessibility for non interactive elements
-    if (event.target === event.currentTarget && isNonNativeButton() && event.key === 'Enter') {
+    if (
+      event.target === event.currentTarget &&
+      isNonNativeButton() &&
+      event.key === 'Enter' &&
+      !disabled
+    ) {
       event.preventDefault();
       if (onClick) {
         onClick(event);

commit 53be5a3b10cdc83a92d1c2a39c5cae4500c4dd52

[MenuList] Fix text navigation for v4 (#27513)
Fixes bug introduced by #19967

packages/material-ui/src/MenuList/MenuList.js
@@ -61,7 +61,7 @@ function moveFocus(
     // Prevent infinite loop.
     if (nextFocus === list.firstChild) {
       if (wrappedOnce) {
-        return;
+        return false;
       }
       wrappedOnce = true;
     }
@@ -80,9 +80,10 @@ function moveFocus(
       nextFocus = traversalFunction(list, nextFocus, disableListWrap);
     } else {
       nextFocus.focus();
-      return;
+      return true;
     }
   }
+  return false;
 }
 
 const useEnhancedEffect = typeof window === 'undefined' ? React.useEffect : React.useLayoutEffect;

packages/material-ui/test/integration/MenuList.test.js
@@ -519,13 +519,14 @@ describe('<MenuList> integration', () => {
     it('matches rapidly typed text', () => {
       render(
         <MenuList autoFocus>
+          <MenuItem>War</MenuItem>
           <MenuItem>Worm</MenuItem>
           <MenuItem>Ordinary</MenuItem>
         </MenuList>,
       );
 
       fireEvent.keyDown(screen.getByRole('menu'), { key: 'W' });
-      fireEvent.keyDown(screen.getByText('Worm'), { key: 'o' });
+      fireEvent.keyDown(screen.getByText('War'), { key: 'o' });
 
       expect(screen.getByText('Worm')).toHaveFocus();
     });