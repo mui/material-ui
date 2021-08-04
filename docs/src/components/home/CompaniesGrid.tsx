8bfc463027d33773861a1a06c2c1e2c50a46fd74
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-08-04 16:23:09 +0200

    remove one level of DOM nesting

--- docs/src/components/home/CompaniesGrid.tsx
+++ docs/src/components/home/CompaniesGrid.tsx
@@ -5,65 +5,59 @@ export const CORE_COMPANIES = [
   {
     src: '/static/branding/companies/coursera.svg',
     alt: 'Coursera logo',
-    width: '102',
-    height: '16',
+    width: 102,
+    height: 16,
   },
   {
     src: '/static/branding/companies/amazon.svg',
     alt: 'Amazon logo',
-    width: '92',
-    height: '28',
+    width: 92,
+    height: 28,
   },
   {
     src: '/static/branding/companies/nasa.svg',
     alt: 'Nasa logo',
-    width: '64',
-    height: '53',
+    width: 64,
+    height: 53,
   },
   {
     src: '/static/branding/companies/netflix.svg',
     alt: 'Netflix logo',
-    width: '88',
-    height: '24',
+    width: 88,
+    height: 24,
   },
   {
     src: '/static/branding/companies/unity.svg',
     alt: 'Unity logo',
-    width: '110',
-    height: '40',
+    width: 110,
+    height: 40,
   },
   {
     src: '/static/branding/companies/shutterstock.svg',
     alt: 'Shutterstock logo',
-    width: '138',
-    height: '21',
+    width: 138,
+    height: 21,
   },
 ];
 
-const CompaniesGrid = ({ data }: { data: Array<JSX.IntrinsicElements['img']> }) => {
+export default function CompaniesGrid({ data }: { data: Array<JSX.IntrinsicElements['img']> }) {
   return (
-    <Grid
-      container
-      spacing={2}
-      alignItems="center"
-      justifyContent="center"
-      sx={{
-        textAlign: 'center',
-        '& > div': {
-          px: 1,
-        },
-        '& img': { maxWidth: '100%', height: 'auto' },
-      }}
-    >
+    <Grid container spacing={2}>
       {data.map((imgProps) => (
-        <Grid key={imgProps.src} item xs={4} md={2}>
-          <div>
-            <img alt={imgProps.alt} loading="lazy" {...imgProps} />
-          </div>
+        <Grid
+          key={imgProps.src}
+          item
+          xs={4}
+          md={2}
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <img alt={imgProps.alt} loading="lazy" {...imgProps} />
         </Grid>
       ))}
     </Grid>
   );
-};
-
-export default CompaniesGrid;
+}
