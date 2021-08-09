fb5260c5398f8918fd9af90a7ad4c501dc6b810b
Tarandeep Singh <peripona@yahoo.com>
2021-08-09 19:56:09 +0200

    switch to rowsMin
    It seems like minRows was a typo or changed to rowsMin but documentation not updated. 
    I was implementing this is project and found not working. thought to fix this here to.

--- docs/src/pages/components/textarea-autosize/MinHeightTextarea.js
+++ docs/src/pages/components/textarea-autosize/MinHeightTextarea.js
@@ -2,5 +2,5 @@ import React from 'react';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 
 export default function MinHeightTextarea() {
-  return <TextareaAutosize aria-label="minimum height" minRows={3} placeholder="Minimum 3 rows" />;
+  return <TextareaAutosize aria-label="minimum height" rowsMin={3} placeholder="Minimum 3 rows" />;
 }
