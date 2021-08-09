64fd452cf51b3ff67ec2af7be6b3d379e7675038
Ignacio Tonoli <ignacio@tonoli.tech>
2021-08-09 10:39:44 +0200

    Remove typescipt children mission error

--- packages/material-ui/src/ClickAwayListener/ClickAwayListener.tsx
+++ packages/material-ui/src/ClickAwayListener/ClickAwayListener.tsx
@@ -26,7 +26,7 @@ export interface ClickAwayListenerProps {
   /**
    * The wrapped element.
    */
-  children: React.ReactElement;
+  children?: React.ReactElement;
   /**
    * If `true`, the React tree is ignored and only the DOM tree is considered.
    * This prop changes how portaled elements are handled.
