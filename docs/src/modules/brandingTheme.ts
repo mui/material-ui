3057944de0ac134162dc4a785720bd51417c6d92
siriwatknp <siriwatkunaporn@gmail.com>
2021-08-14 11:41:04 +0700

    fix switch thumb styles

--- docs/src/modules/brandingTheme.ts
+++ docs/src/modules/brandingTheme.ts
@@ -398,6 +398,7 @@ export function getThemedComponents(theme: Theme) {
             height: 20,
             width: 20,
             padding: 0,
+            color: '#fff',
             '&.Mui-checked + .MuiSwitch-track': {
               opacity: 1,
             },
