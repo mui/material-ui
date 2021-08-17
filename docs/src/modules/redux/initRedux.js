e93f76579c3ed9c08464fa639cfb2d81782cad6d
eps1lon <silbermann.sebastian@gmail.com>
2021-08-17 13:45:02 +0200

    Move notifications from global into local state

--- docs/src/modules/redux/initRedux.js
+++ docs/src/modules/redux/initRedux.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import optionsReducer from 'docs/src/modules/redux/optionsReducer';
-import notificationsReducer from 'docs/src/modules/redux/notificationsReducer';
 import { createLogger } from 'redux-logger';
 
 // Get the Redux DevTools extension and fallback to a no-op function
@@ -31,7 +30,6 @@ export default function create(initialState) {
   return createStore(
     combineReducers({
       options: optionsReducer,
-      notifications: notificationsReducer,
     }),
     initialState, // Hydrate the store with server-side data
     compose(applyMiddleware(...middleware), devtools),
