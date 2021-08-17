d165bfc7ce24d1decc18c76da41862f5e5d43346
siriwatknp <siriwatkunaporn@gmail.com>
2021-08-17 11:39:35 +0700

    add overline to CoreHero

--- docs/src/components/productCore/CoreHero.tsx
+++ docs/src/components/productCore/CoreHero.tsx
@@ -4,12 +4,25 @@ import Typography from '@material-ui/core/Typography';
 import GradientText from 'docs/src/components/typography/GradientText';
 import GetStartedButtons from 'docs/src/components/home/GetStartedButtons';
 import HeroContainer from 'docs/src/layouts/HeroContainer';
+import IconImage from 'docs/src/components/icon/IconImage';
 
 export default function Hero() {
   return (
     <HeroContainer
       left={
         <Box sx={{ textAlign: { xs: 'center', md: 'left' } }}>
+          <Typography
+            fontWeight="bold"
+            variant="body2"
+            color={(theme) => (theme.palette.mode === 'dark' ? 'primary.400' : 'primary.600')}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              '& > *': { mr: 1, width: 28, height: 28 },
+            }}
+          >
+            <IconImage name="product-core" /> Advanced
+          </Typography>
           <Typography variant="h1" sx={{ my: 2, maxWidth: 500 }}>
             Ready to use, <br />
             <GradientText>forever free</GradientText>,<br /> components.
