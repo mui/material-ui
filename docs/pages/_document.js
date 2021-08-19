501d7733df2e74d2c157a1ccee7c4924df849bec
siriwatknp <siriwatkunaporn@gmail.com>
2021-08-19 15:13:14 +0700

    preload font and remove unused fonts

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -78,7 +78,12 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600&family=IBM+Plex+Sans:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
-          <link href="/static/fonts/Plusjakartasans.css" rel="stylesheet" />
+          <link
+            rel="preload"
+            href="/static/fonts/PlusJakartaSans-ExtraBold.woff2"
+            as="font"
+            type="font/woff2"
+          />
         </Head>
         <body>
           <Main />
