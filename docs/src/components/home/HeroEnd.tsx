30379d9e3b4baa328c29f160ffc74c05db6a8ed6
siriwatknp <siriwatkunaporn@gmail.com>
2021-08-20 14:24:49 +0700

    lazy load HeroEnd

--- docs/src/components/home/HeroEnd.tsx
+++ docs/src/components/home/HeroEnd.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
+import dynamic from 'next/dynamic';
+import { useInView } from 'react-intersection-observer';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Link from 'docs/src/modules/components/Link';
-import GradientText from 'docs/src/components/typography/GradientText';
-import KeyboardArrowRightRounded from '@material-ui/icons/KeyboardArrowRightRounded';
-import ROUTES from 'docs/src/route';
-import GetStartedButtons from 'docs/src/components/home/GetStartedButtons';
+
+const StartToday = dynamic(() => import('./StartToday'));
 
 const HeroEnd = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0,
+  });
   return (
     <Box
+      ref={ref}
       sx={{
         background: (theme) =>
           theme.palette.mode === 'dark'
@@ -20,79 +21,7 @@ const HeroEnd = () => {
             : `linear-gradient(180deg, ${theme.palette.grey[50]} 0%, #FFFFFF 100%)`,
       }}
     >
-      <Container sx={{ py: { xs: 4, sm: 6, md: 8 } }}>
-        <Grid container spacing={{ xs: 2, md: 4 }} alignItems="center">
-          <Grid item xs={12} sm={6} md={6} sx={{ mb: { md: 4 } }}>
-            <Typography
-              color={(theme) => (theme.palette.mode === 'dark' ? 'primary.400' : 'primary.600')}
-              fontWeight="bold"
-              variant="body2"
-              sx={{ mb: 1 }}
-            >
-              Go live!
-            </Typography>
-            <Typography variant="h2" sx={{ maxWidth: 460, mb: 1 }}>
-              Start building with <GradientText>MUI</GradientText> today!
-            </Typography>
-            <Typography color="text.secondary" sx={{ mb: 2, maxWidth: 450 }}>
-              Check it for yourself, give it a try and share with us what you&apos;ve built!
-            </Typography>
-            <GetStartedButtons
-              sx={{ '&& > *': { minWidth: 'clamp(0px, (426px - 100%) * 999 ,100%)' } }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} md={6} container spacing={2}>
-            <Grid item xs={12} md={6}>
-              <Paper
-                component={Link}
-                href={ROUTES.showcase}
-                noLinkStyle
-                variant="outlined"
-                sx={{ p: 2, height: '100%' }}
-              >
-                <Typography variant="body2" fontWeight="bold" sx={{ mb: 0.5 }}>
-                  Showcases
-                </Typography>
-                <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
-                  See more projects and companies out there relying on MUI.
-                </Typography>
-                <Typography
-                  color={(theme) => (theme.palette.mode === 'dark' ? 'primary.400' : 'primary.600')}
-                  variant="body2"
-                  fontWeight="bold"
-                >
-                  Learn more{' '}
-                  <KeyboardArrowRightRounded fontSize="small" sx={{ verticalAlign: 'middle' }} />
-                </Typography>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Paper
-                component={Link}
-                href={ROUTES.blog}
-                noLinkStyle
-                variant="outlined"
-                sx={{ p: 2, height: '100%' }}
-              >
-                <Typography variant="body2" fontWeight="bold" sx={{ mb: 0.5 }}>
-                  Blog
-                </Typography>
-                <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
-                  Check behind the scenes and news from the company.
-                </Typography>
-                <Typography
-                  color={(theme) => (theme.palette.mode === 'dark' ? 'primary.400' : 'primary.600')}
-                  variant="body2"
-                  fontWeight="bold"
-                >
-                  Learn more{' '}
-                  <KeyboardArrowRightRounded fontSize="small" sx={{ verticalAlign: 'middle' }} />
-                </Typography>
-              </Paper>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Container>
+      <Container sx={{ py: { xs: 4, sm: 6, md: 8 } }}>{inView && <StartToday />}</Container>
     </Box>
   );
 };
