b7b0cc502763e328fdf39974aa9b564af99f662a
Benny Joo <sldisek783@gmail.com>
2021-08-20 15:59:21 +0100

    Add test

--- packages/material-ui-system/src/createStyled.test.js
+++ packages/material-ui-system/src/createStyled.test.js
@@ -1,7 +1,12 @@
+import * as React from 'react';
 import { expect } from 'chai';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+import { createClientRender } from 'test/utils';
 import createStyled from './createStyled';
 
 describe('createStyled', () => {
+  const render = createClientRender();
+
   describe('displayName', () => {
     // These tests rely on implementation details (namely `displayName`)
     // Ideally we'd just test if the proper name appears in a React warning.
@@ -42,6 +47,59 @@ describe('createStyled', () => {
       const SomeMuiComponent = styled(() => null)({});
 
       expect(SomeMuiComponent).to.have.property('displayName', 'Styled(Component)');
+    });
+  });
+
+  describe('styles', () => {
+    it('styles of pseudo classes of variants are merged', () => {
+      const theme = createTheme({
+        components: {
+          MuiButton: {
+            variants: [
+              {
+                props: { variant: 'contained' },
+                style: {
+                  '&.Mui-disabled': {
+                    opacity: 0.65,
+                  },
+                },
+              },
+              {
+                props: { variant: 'contained', color: 'primary' },
+                style: {
+                  '&.Mui-disabled': {
+                    color: '#fff',
+                    backgroundColor: '#f00',
+                  },
+                },
+              },
+            ],
+          },
+        },
+      });
+      const styled = createStyled({});
+      const Button = styled('button', {
+        shouldForwardProp: (prop) => prop !== 'color' && prop !== 'contained',
+        name: 'MuiButton',
+        slot: 'Root',
+        overridesResolver: (props, styles) => styles.root,
+      })({
+        display: 'flex',
+      });
+
+      const { container } = render(
+        <ThemeProvider theme={theme}>
+          <Button color="primary" variant="contained" className="Mui-disabled">
+            Hello
+          </Button>
+        </ThemeProvider>,
+      );
+
+      expect(container.getElementsByTagName('button')[0]).toHaveComputedStyle({
+        color: 'rgb(255, 255, 255)',
+        backgroundColor: 'rgb(255, 0, 0)',
+        opacity: '0.65',
+      });
     });
   });
 });
