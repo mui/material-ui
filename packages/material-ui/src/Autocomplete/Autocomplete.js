a0427f2a28e456f9aac75e17a4c2a84c0dc50029
vedadeepta <pal.veda.deepta@gmail.com>
2021-08-23 21:08:27 +0530

    [Autocomplete]: Add verbose warning for defaultValue

--- packages/material-ui/src/Autocomplete/Autocomplete.js
+++ packages/material-ui/src/Autocomplete/Autocomplete.js
@@ -718,7 +718,17 @@ Autocomplete.propTypes /* remove-proptypes */ = {
    * The default value. Use when the component is not controlled.
    * @default props.multiple ? [] : null
    */
-  defaultValue: PropTypes.any,
+  defaultValue: chainPropTypes(PropTypes.any, (props) => {
+    if (props.multiple && props.defaultValue !== undefined && !Array.isArray(props.defaultValue)) {
+      return new Error(
+        [
+          'Material-UI: The Autocomplete expects the `defaultValue` prop to be an array or undefined.',
+          `However, ${props.defaultValue} was provided.`,
+        ].join('\n'),
+      );
+    }
+    return null;
+  }),
   /**
    * If `true`, the input can't be cleared.
    * @default false
