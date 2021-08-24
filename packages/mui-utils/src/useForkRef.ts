e962ed032dac7baafa656431e26ac16cb320641b
Michał Dudak <michal@dudak.me>
2021-08-24 13:59:41 +0200

    [core] Make useForkRef variadic

--- packages/mui-utils/src/useForkRef.ts
+++ packages/mui-utils/src/useForkRef.ts
@@ -1,22 +1,23 @@
 import * as React from 'react';
 import setRef from './setRef';
 
-export default function useForkRef<InstanceA, InstanceB>(
-  refA: React.Ref<InstanceA> | null | undefined,
-  refB: React.Ref<InstanceB> | null | undefined,
-): React.Ref<InstanceA & InstanceB> | null {
+export default function useForkRef<Instance>(
+  ...refs: Array<React.Ref<Instance> | null | undefined>
+): React.Ref<Instance> | null {
   /**
    * This will create a new function if the ref props change and are defined.
    * This means react will call the old forkRef with `null` and the new forkRef
    * with the ref. Cleanup naturally emerges from this behavior.
    */
   return React.useMemo(() => {
-    if (refA == null && refB == null) {
+    if (refs.every((ref) => ref == null)) {
       return null;
     }
     return (refValue) => {
-      setRef(refA, refValue);
-      setRef(refB, refValue);
+      refs.forEach((ref) => {
+        setRef(ref, refValue);
+      });
     };
-  }, [refA, refB]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [...refs]);
 }
