ed8129665ad1f21d971f71e0dca2ffce4eb4eea7
Marija Najdova <mnajdova@gmail.com>
2021-08-31 13:26:57 +0200

    Use React.memo

--- packages/material-ui/src/InputBase/InputBase.js
+++ packages/material-ui/src/InputBase/InputBase.js
@@ -212,6 +212,16 @@ export const InputBaseComponent = styled('input', {
     }),
   };
 });
+
+const InputGlobalStyles = React.memo(() => (
+  <GlobalStyles
+    styles={{
+      '@keyframes mui-auto-fill': { from: { display: 'block' } },
+      '@keyframes mui-auto-fill-cancel': { from: { display: 'block' } },
+    }}
+  />
+));
+
 /**
  * `InputBase` contains as few styles as possible.
  * It aims to be a simple building block for creating an input.
@@ -483,12 +493,7 @@ const InputBase = React.forwardRef(function InputBase(inProps, ref) {
 
   return (
     <React.Fragment>
-      <GlobalStyles
-        styles={{
-          '@keyframes mui-auto-fill': { from: {} },
-          '@keyframes mui-auto-fill-cancel': { from: {} },
-        }}
-      />
+      <InputGlobalStyles />
       <Root
         {...rootProps}
         {...(!isHostComponent(Root) && {
