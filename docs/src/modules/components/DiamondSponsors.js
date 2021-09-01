9af0a5365e167d07b37df518827b08cb68bfe64f
danilo leal <daniloleal09@gmail.com>
2021-09-01 11:32:22 -0300

    changing to svg

--- docs/src/modules/components/DiamondSponsors.js
+++ docs/src/modules/components/DiamondSponsors.js
@@ -74,9 +74,9 @@ export default function DiamondSponsors(props) {
           }}
         >
           <img
-            width="150"
-            height="42"
-            src={`/static/sponsors/octopus-${theme.palette.mode}.png`}
+            width="137"
+            height="30"
+            src={`/static/sponsors/octopus-${theme.palette.mode}.svg`}
             alt="octopus"
             title="Repeatable, reliable deployments"
             loading="lazy"
