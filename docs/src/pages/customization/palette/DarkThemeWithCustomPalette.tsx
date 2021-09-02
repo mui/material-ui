168e19e51abdf1eb14d5f602cb99c90eb7119e12
Marija Najdova <mnajdova@gmail.com>
2021-09-02 12:52:37 +0200

    Add PaletteMode type

--- docs/src/pages/customization/palette/DarkThemeWithCustomPalette.tsx
+++ docs/src/pages/customization/palette/DarkThemeWithCustomPalette.tsx
@@ -3,10 +3,10 @@ import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
-import { ThemeProvider, useTheme, createTheme } from '@mui/material/styles';
+import { ThemeProvider, useTheme, createTheme, PaletteMode } from '@mui/material/styles';
 import { amber, deepOrange, grey } from '@mui/material/colors';
 
-const getDesignTokens = (mode = 'light') => ({
+const getDesignTokens = (mode: PaletteMode = 'light') => ({
   palette: {
     primary: {
       ...amber,
