877b5db1884beebd6643e771be8092de7d92dd0e
danilo leal <daniloleal09@gmail.com>
2021-09-03 10:10:00 -0300

    add transition to the design kit links

--- docs/src/components/home/DesignKits.tsx
+++ docs/src/components/home/DesignKits.tsx
@@ -24,6 +24,8 @@ const Image = styled('img')(({ theme }) => ({
   borderColor: theme.palette.mode === 'dark' ? theme.palette.grey[800] : theme.palette.grey[400],
   borderRadius: theme.shape.borderRadius,
   objectFit: 'cover',
+  transitionProperty: 'all',
+  transitionDuration: '150ms',
   boxShadow:
     theme.palette.mode === 'dark'
       ? '0px 4px 20px rgba(0, 0, 0, 0.6)'
