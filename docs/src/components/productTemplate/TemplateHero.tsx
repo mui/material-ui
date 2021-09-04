06e038e43e0ef99474e8e84c1e250f75aed375c7
Siriwat K <siriwatkunaporn@gmail.com>
2021-09-04 10:06:01 +0700

    Update docs/src/components/productTemplate/TemplateHero.tsx

--- docs/src/components/productTemplate/TemplateHero.tsx
+++ docs/src/components/productTemplate/TemplateHero.tsx
@@ -37,7 +37,7 @@ export default function TemplateHero() {
             templates for your project
           </Typography>
           <Typography color="text.secondary" sx={{ mb: 3, maxWidth: 500 }}>
-            A collection of 4.5 average rating templates, selected and curated by MUI's team of
+            A collection of 4.5 average rating templates, selected and curated by MUI&apos;s team of
             maintainers to get your projects up and running today.
           </Typography>
           <Button
