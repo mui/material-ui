376c00d42237a01e39a6f975d83d37be0682f8bc
siriwatknp <siriwatkunaporn@gmail.com>
2021-09-06 18:19:53 +0700

    add chart and sparkline

--- docs/src/components/productX/XComponents.tsx
+++ docs/src/components/productX/XComponents.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Fade from '@mui/material/Fade';
 import Grid from '@mui/material/Grid';
+import Chip from '@mui/material/Chip';
 import Typography from '@mui/material/Typography';
 import Section from 'docs/src/layouts/Section';
 import SectionHeadline from 'docs/src/components/typography/SectionHeadline';
@@ -18,6 +20,9 @@ import XDateRangeDemo from 'docs/src/components/productX/XDateRangeDemo';
 import XTreeViewDemo from 'docs/src/components/productX/XTreeViewDemo';
 import More from 'docs/src/components/action/More';
 import ROUTES from 'docs/src/route';
+import EmailSubscribe from 'docs/src/components/footer/EmailSubscribe';
+import Frame from 'docs/src/components/action/Frame';
+import { brandingDarkTheme } from 'docs/src/modules/brandingTheme';
 
 const DEMOS = ['Data Grid', 'Date Picker', 'Tree View', 'Sparkline', 'Charts'];
 
@@ -73,6 +78,74 @@ export default function XComponents() {
             <Fade in timeout={500}>
               <Box>
                 <XTreeViewDemo />
+              </Box>
+            </Fade>
+          )}
+          {(demo === DEMOS[3] || demo === DEMOS[4]) && (
+            <Fade in timeout={500}>
+              <Box sx={{ height: '100%' }}>
+                <Frame sx={{ height: '100%' }}>
+                  <Frame.Demo sx={{ p: 2, flexGrow: 1 }}>
+                    <Box sx={{ textAlign: 'right', mb: 1 }}>
+                      <Chip variant="outlined" label="PNG Preview" size="small" />
+                    </Box>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        overflow: 'auto',
+                        '& img': {
+                          maxHeight: 160,
+                          maxWidth: 240,
+                          '&:not(:first-of-type)': { marginLeft: 2 },
+                        },
+                      }}
+                    >
+                      {demo === DEMOS[3] && (
+                        <React.Fragment>
+                          <img
+                            alt="sparkline 1"
+                            src="/static/branding/mui-x/sparkline1.png"
+                            loading="lazy"
+                            height="160"
+                          />
+                          <img
+                            alt="sparkline 2"
+                            src="/static/branding/mui-x/sparkline2.png"
+                            loading="lazy"
+                            height="160"
+                          />
+                        </React.Fragment>
+                      )}
+                      {demo === DEMOS[4] && (
+                        <React.Fragment>
+                          <img
+                            alt="chart 1"
+                            src="/static/branding/mui-x/chart1.png"
+                            loading="lazy"
+                            height="132"
+                          />
+                          <img
+                            alt="chart 2"
+                            src="/static/branding/mui-x/chart2.png"
+                            loading="lazy"
+                            height="132"
+                          />
+                        </React.Fragment>
+                      )}
+                    </Box>
+                  </Frame.Demo>
+                  <ThemeProvider theme={brandingDarkTheme}>
+                    <Frame.Info>
+                      <Typography fontWeight="bold">Coming soon!</Typography>
+                      <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
+                        Subscribe to our newsletter to get first-hand info about the development and
+                        release of new components.
+                      </Typography>
+                      <EmailSubscribe sx={{ '& > div': { maxWidth: 'initial' } }} />
+                    </Frame.Info>
+                  </ThemeProvider>
+                </Frame>
               </Box>
             </Fade>
           )}
