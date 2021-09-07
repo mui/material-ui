ac93054930650f4a1f7ea9ab0948c5ec76eeee4b
siriwatknp <siriwatkunaporn@gmail.com>
2021-09-07 16:35:56 +0700

    use env variable

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -2,9 +2,7 @@
 import { LicenseInfo } from '@mui/x-data-grid-pro';
 
 // Remove the license warning from demonstration purposes
-LicenseInfo.setLicenseKey(
-  '0f94d8b65161817ca5d7f7af8ac2f042T1JERVI6TVVJLVN0b3J5Ym9vayxFWFBJUlk9MTY1NDg1ODc1MzU1MCxLRVlWRVJTSU9OPTE=',
-);
+LicenseInfo.setLicenseKey(process.env.NEXT_PUBLIC_MUI_LICENSE);
 
 import 'docs/src/modules/components/bootstrap';
 // --- Post bootstrap -----
