02b694fd538570d2493ef91a56aa3c5bb8594fa6
danilo leal <daniloleal09@gmail.com>
2021-09-10 17:07:33 -0300

    alert demo minor tweak

--- docs/src/components/home/MaterialDesignComponents.tsx
+++ docs/src/components/home/MaterialDesignComponents.tsx
@@ -290,7 +290,7 @@ export function buildTheme(theme: Theme): ThemeOptions {
           },
           filled: {
             backgroundColor:
-              theme.palette.mode === 'dark' ? theme.palette.primaryDark[500] : theme.palette.primaryDark[700],
+              theme.palette.mode === 'dark' ? theme.palette.primaryDark[800] : theme.palette.primaryDark[700],
             '& .MuiAlert-icon': {
               color: theme.palette.primary[100],
             },
