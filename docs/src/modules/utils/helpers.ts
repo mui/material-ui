a11434ba73f2561d0abf111f61bd8cc0d0c9fd54
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-09-16 18:17:06 +0200

    [docs] Fix the notification display logic

--- docs/src/modules/utils/helpers.ts
+++ docs/src/modules/utils/helpers.ts
@@ -228,7 +228,7 @@ export function getDependencies(
 export function getCookie(name: string): string | undefined {
   // `process.browser` is set by nextjs where we only use `getCookie`
   // but this file is imported from nodejs scripts so TypeScript complains for that environment.
-  if ((process as any).browser) {
+  if (typeof document !== 'undefined') {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) {
