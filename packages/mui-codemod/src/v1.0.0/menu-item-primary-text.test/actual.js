8e103168286253a62d15e6341a6e1939ed81000c
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2021-09-18 12:15:45 +0300

    [codemod] Add MenuItem v.1.0.0 transform for primaryText propery

---
+++ packages/mui-codemod/src/v1.0.0/menu-item-primary-text.test/actual.js
@@ -0,0 +1,24 @@
+<div>
+  <MenuItem
+    onClick={() => {
+      analytics('Clicked Menu > Progress');
+    }}
+    primaryText="My Progress"
+    rightIcon={<ProgressIcon/>}
+  />
+  <MenuItem
+    onClick={() => {
+      analytics('Clicked Menu > Progress2');
+    }}
+    primaryText={true ? "My Progress" : "Not progress"}
+    rightIcon={<ProgressIcon/>}
+  />
+  <MenuItem
+    onClick={() => {
+      analytics('Clicked Menu > Progress');
+    }}
+    rightIcon={<ProgressIcon/>}
+  >
+    Already changed
+  </MenuItem>
+</div>
