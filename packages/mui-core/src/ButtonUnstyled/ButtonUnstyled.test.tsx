16a5fdb84b0d66fb41d713bb2210a330ed79f542
Michał Dudak <michal@dudak.me>
2021-09-20 11:13:36 +0200

    [ButtonUnstyled] Don't set redundant role=button

--- packages/mui-core/src/ButtonUnstyled/ButtonUnstyled.test.tsx
+++ packages/mui-core/src/ButtonUnstyled/ButtonUnstyled.test.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { createMount, createClientRender, describeConformanceUnstyled } from 'test/utils';
 import ButtonUnstyled, { buttonUnstyledClasses } from '@mui/core/ButtonUnstyled';
+import { expect } from 'chai';
 
 describe('<ButtonUnstyled />', () => {
   const mount = createMount();
@@ -19,4 +20,19 @@ describe('<ButtonUnstyled />', () => {
       },
     },
   }));
+
+  describe('role attribute', () => {
+    it('is set when the root component is an HTML element other than a button', () => {
+      const { getByRole } = render(<ButtonUnstyled component="span" />);
+      expect(getByRole('button')).not.to.equal(null);
+    });
+
+    it('is set when the root component is an component that renders an HTML component other than a button', () => {
+      const WrappedSpan = (props: React.HTMLAttributes<HTMLSpanElement>) => (
+        <span role={props.role} />
+      );
+      const { getByRole } = render(<ButtonUnstyled component={WrappedSpan} />);
+      expect(getByRole('button')).not.to.equal(null);
+    });
+  });
 });
