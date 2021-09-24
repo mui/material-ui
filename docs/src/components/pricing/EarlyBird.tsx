ae41143811ee91530a3c4b8547bfd08ada93a556
danilo leal <daniloleal09@gmail.com>
2021-09-24 10:01:43 -0300

    polishment

--- docs/src/components/pricing/EarlyBird.tsx
+++ docs/src/components/pricing/EarlyBird.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Link from 'docs/src/modules/components/Link';
 import KeyboardArrowRightRounded from '@mui/icons-material/KeyboardArrowRightRounded';
@@ -9,7 +10,7 @@ import KeyboardArrowRightRounded from '@mui/icons-material/KeyboardArrowRightRou
 export default function EarlyBird() {
   return (
     <Container sx={{ pt: 2, pb: { xs: 2, sm: 4, md: 8 } }} id="EarlyBird">
-      <Box
+      <Stack
         sx={{
           borderRadius: 1,
           p: 2,
@@ -19,7 +20,7 @@ export default function EarlyBird() {
             theme.palette.mode === 'dark' ? 'primaryDark.500' : 'primary.100',
           display: 'flex',
           flexDirection: {
-            xs: 'column-reverse',
+            xs: 'column',
             sm: 'row',
           },
           justifyContent: 'space-between',
@@ -29,7 +30,7 @@ export default function EarlyBird() {
           },
         }}
       >
-        <div>
+        <Box>
           <Typography fontWeight="bold" sx={{ mb: 0.5 }}>
             🐦&nbsp;&nbsp;Early bird special!
           </Typography>
@@ -39,19 +40,24 @@ export default function EarlyBird() {
             and updates for one year. The early bird special is available for a limited time, so
             don&apos;t miss this opportunity!
           </Typography>
-          <Button
-            component={Link}
-            noLinkStyle
-            href="https://material-ui.com/store/items/material-ui-pro/"
-            variant="contained"
-            fullWidth
-            endIcon={<KeyboardArrowRightRounded />}
-            sx={{ py: 1, mt: 2, width: { xs: '100%', md: '20%' } }}
-          >
-            Buy now
-          </Button>
-        </div>
-      </Box>
+        </Box>
+        <Button
+          component={Link}
+          noLinkStyle
+          href="https://material-ui.com/store/items/material-ui-pro/"
+          variant="contained"
+          fullWidth
+          endIcon={<KeyboardArrowRightRounded />}
+          sx={{
+            py: 1,
+            ml: { xs: 0, sm: 2 },
+            mt: { xs: 3, sm: 0 },
+            width: { xs: '100%', sm: '50%', md: '20%' },
+          }}
+        >
+          Buy now
+        </Button>
+      </Stack>
     </Container>
   );
 }
