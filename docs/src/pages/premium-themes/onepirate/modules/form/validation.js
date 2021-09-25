940f230b44bd995eb652b2cc57363072b163f375
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-09-25 11:36:15 +0200

    polish

--- docs/src/pages/premium-themes/onepirate/modules/form/validation.js
+++ docs/src/pages/premium-themes/onepirate/modules/form/validation.js
@@ -1,6 +1,12 @@
-// Simplified method of validator/lib/isEmail
+/**
+ * This is a simplified logic.
+ * Consider using `import isEmail from 'validator/lib/isEmail'` from
+ * https://github.com/validatorjs/validator.js/blob/7376945b4ce028b65955ae57b8fccbbf3fe58467/src/lib/isEmail.js
+ * for a more robust version.
+ */
 function isEmail(string) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFaa-zA-Z\-0-9]+\.)+[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFaa-zA-Z]{2,}))$/;
+  const re =
+    /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
   return re.test(string);
 }
 
