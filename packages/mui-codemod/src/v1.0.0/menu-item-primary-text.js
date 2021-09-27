71715b7a3a1b19f48eb20ad0ac953a6889e28f56
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2021-09-27 18:39:44 +0300

    [codemod] Fix linter warnings of v1.0.0 MenuItem transform

--- packages/mui-codemod/src/v1.0.0/menu-item-primary-text.js
+++ packages/mui-codemod/src/v1.0.0/menu-item-primary-text.js
@@ -10,18 +10,18 @@ export default function transformer(file, api, options) {
   return j(file.source)
     .findJSXElements('MenuItem')
     .forEach((path) => {
-      if (!path.node.openingElement.selfClosing) return;
+      if (!path.node.openingElement.selfClosing) {
+        return;
+      }
 
       const attributes = path.node.openingElement.attributes;
-      attributes.some((node, index) => {
+      attributes.forEach((node, index) => {
         if (node.type === 'JSXAttribute' && node.name.name === 'primaryText') {
           delete attributes[index];
 
           path.node.openingElement.selfClosing = false;
           path.node.children = [node.value];
           path.node.closingElement = j.jsxClosingElement(path.node.openingElement.name);
-
-          return true;
         }
       });
     })
