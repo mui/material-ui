2f2a12a72fb80f5c215ed9e69ff433e3ee275b78
siriwatknp <siriwatkunaporn@gmail.com>
2021-10-04 11:48:41 +0700

    fix wrong method

--- packages/mui-system/src/createDesignSystem/cssVars/camelize.test.ts
+++ packages/mui-system/src/createDesignSystem/cssVars/camelize.test.ts
@@ -3,6 +3,6 @@ import camelize from './camelize';
 
 describe('camelize', () => {
   it('support kebab-case', () => {
-    expect(camelize('color-scheme')).to.be('colorScheme');
+    expect(camelize('color-scheme')).to.equal('colorScheme');
   });
 });
