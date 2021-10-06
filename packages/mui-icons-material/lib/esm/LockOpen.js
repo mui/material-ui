96852e61aac51fd2e4f7c69cc80d6eebac861163
Adam Fitzgibbon <fitzgamer@hotmail.com>
2021-10-05 23:31:43 -0700

    updated default LockOpen icon to correctly be the filled variant

--- packages/mui-icons-material/lib/esm/LockOpen.js
+++ packages/mui-icons-material/lib/esm/LockOpen.js
@@ -1,5 +1,8 @@
 import createSvgIcon from './utils/createSvgIcon';
-import { jsx as _jsx } from "react/jsx-runtime";
-export default createSvgIcon( /*#__PURE__*/_jsx("path", {
-  d: "M12 17c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm6-9h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6h1.9c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm0 12H6V10h12v10z"
-}), 'LockOpen');
+import { jsx as _jsx } from 'react/jsx-runtime';
+export default createSvgIcon(
+  /*#__PURE__*/ _jsx('path', {
+    d: 'M6 20zm6-7c1.1 0 2 .9 2 2s-.9 2-2 2s-2-.9-2-2s.9-2 2-2zM18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6h2c0-1.66 1.34-3 3-3s3 1.34 3 3v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zzz',
+  }),
+  'LockOpen',
+);
