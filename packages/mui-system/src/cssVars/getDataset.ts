9445b07c493735bc16acecc92417b19bd55573f8
siriwatknp <siriwatkunaporn@gmail.com>
2021-10-13 12:11:54 +0700

    remove unused getDataset

--- packages/mui-system/src/cssVars/getDataset.ts
+++
@@ -1,7 +0,0 @@
-// reference: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset#name_conversion
-// Note: this is not the same as camelCase()
-export default function getDataset(dataAttribute: string) {
-  return dataAttribute
-    .toLowerCase()
-    .replace(/([\u002d][a-z])/g, (m, chr) => chr.substr(1).toUpperCase());
-}
