7f7ba28459ffd29a2debd769357bb688f3b59654
eps1lon <silbermann.sebastian@gmail.com>
2021-10-19 19:51:26 +0200

    [docs] Use Suspense on the server for docs search

--- docs/src/modules/components/AppFrame.js
+++ docs/src/modules/components/AppFrame.js
@@ -53,20 +53,10 @@ export function NextNProgressBar() {
 
 const AppSearch = React.lazy(() => import('docs/src/modules/components/AppSearch'));
 export function DeferredAppSearch() {
-  const [mounted, setMounted] = React.useState(false);
-  React.useEffect(() => {
-    setMounted(true);
-  }, []);
-
   return (
-    <React.Fragment>
-      {/* Suspense isn't supported for SSR yet */}
-      {mounted ? (
-        <React.Suspense fallback={null}>
-          <AppSearch />
-        </React.Suspense>
-      ) : null}
-    </React.Fragment>
+    <React.Suspense fallback={null}>
+      <AppSearch />
+    </React.Suspense>
   );
 }
 
