f30e03e2710c357ce7e9cee08c471471b98d99a1
eps1lon <silbermann.sebastian@gmail.com>
2021-10-20 16:00:04 +0200

    [NProgressBar] Use GlobalStyles instead of withStyles

--- packages/mui-docs/src/NProgressBar/NProgressBar.js
+++ packages/mui-docs/src/NProgressBar/NProgressBar.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import NProgress from 'nprogress';
-import { withStyles } from '@mui/styles';
-import { createTheme } from '@mui/material/styles';
+import GlobalStyles from '@mui/material/GlobalStyles';
 import NoSsr from '@mui/core/NoSsr';
 import { exactProp } from '@mui/utils';
 
@@ -25,66 +24,59 @@ const styles = (theme) => {
   }
 
   return {
-    '@global': {
-      '#nprogress': {
-        direction: 'ltr',
-        pointerEvents: 'none',
+    '#nprogress': {
+      direction: 'ltr',
+      pointerEvents: 'none',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      height: 2,
+      zIndex: theme.zIndex.tooltip,
+      backgroundColor:
+        theme.palette.mode === 'dark' ? theme.palette.primary[700] : theme.palette.primary[200],
+      '& .nprogress-bar': {
         position: 'fixed',
+        backgroundColor: theme.nprogress.color,
         top: 0,
         left: 0,
         right: 0,
         height: 2,
-        zIndex: theme.zIndex.tooltip,
-        backgroundColor:
-          theme.palette.mode === 'dark' ? theme.palette.primary[700] : theme.palette.primary[200],
-        '& .nprogress-bar': {
-          position: 'fixed',
-          backgroundColor: theme.nprogress.color,
-          top: 0,
-          left: 0,
-          right: 0,
-          height: 2,
-        },
-        '& .nprogress-bar > div': {
-          position: 'absolute',
-          top: 0,
-          height: 2,
-          boxShadow: `${theme.nprogress.color} 1px 0 6px 1px`,
-          borderRadius: '100%',
-          animation: 'mui-nprogress-pulse 2s ease-out 0s infinite',
-        },
-        '& .nprogress-bar > div:first-child': {
-          opacity: 0.6,
-          width: 20,
-          right: 0,
-          clip: 'rect(-6px,22px,14px,10px)',
-        },
-        '& .nprogress-bar > div:last-child': {
-          opacity: 0.6,
-          width: 180,
-          right: -80,
-          clip: 'rect(-6px,90px,14px,-6px)',
-        },
       },
-      '@keyframes mui-nprogress-pulse': {
-        '30%': {
-          opacity: 0.6,
-        },
-        '60%': {
-          opacity: 0,
-        },
-        to: {
-          opacity: 0.6,
-        },
+      '& .nprogress-bar > div': {
+        position: 'absolute',
+        top: 0,
+        height: 2,
+        boxShadow: `${theme.nprogress.color} 1px 0 6px 1px`,
+        borderRadius: '100%',
+        animation: 'mui-nprogress-pulse 2s ease-out 0s infinite',
+      },
+      '& .nprogress-bar > div:first-child': {
+        opacity: 0.6,
+        width: 20,
+        right: 0,
+        clip: 'rect(-6px,22px,14px,10px)',
+      },
+      '& .nprogress-bar > div:last-child': {
+        opacity: 0.6,
+        width: 180,
+        right: -80,
+        clip: 'rect(-6px,90px,14px,-6px)',
+      },
+    },
+    '@keyframes mui-nprogress-pulse': {
+      '30%': {
+        opacity: 0.6,
+      },
+      '60%': {
+        opacity: 0,
+      },
+      to: {
+        opacity: 0.6,
       },
     },
   };
 };
-
-const defaultTheme = createTheme();
-const GlobalStyles = withStyles(styles, { defaultTheme, flip: false, name: 'MuiNProgressBar' })(
-  () => null,
-);
 
 /**
  * Elegant and ready-to-use wrapper on top of https://github.com/rstacruz/nprogress/.
@@ -94,7 +86,7 @@ function NProgressBar(props) {
   return (
     <NoSsr>
       {props.children}
-      <GlobalStyles />
+      <GlobalStyles styles={styles} />
     </NoSsr>
   );
 }
