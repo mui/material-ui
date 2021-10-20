6d310209244eea6b994fe165665456115e5f18bf
eps1lon <silbermann.sebastian@gmail.com>
2021-10-20 16:35:07 +0200

    object-based -> template string

--- packages/mui-docs/src/NProgressBar/NProgressBar.js
+++ packages/mui-docs/src/NProgressBar/NProgressBar.js
@@ -23,59 +23,60 @@ const styles = (theme) => {
     );
   }
 
-  return {
-    '#nprogress': {
-      direction: 'ltr',
-      pointerEvents: 'none',
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      right: 0,
-      height: 2,
-      zIndex: theme.zIndex.tooltip,
-      backgroundColor:
-        theme.palette.mode === 'dark' ? theme.palette.primary[700] : theme.palette.primary[200],
-      '& .nprogress-bar': {
-        position: 'fixed',
-        backgroundColor: theme.nprogress.color,
-        top: 0,
-        left: 0,
-        right: 0,
-        height: 2,
-      },
-      '& .nprogress-bar > div': {
-        position: 'absolute',
-        top: 0,
-        height: 2,
-        boxShadow: `${theme.nprogress.color} 1px 0 6px 1px`,
-        borderRadius: '100%',
-        animation: 'mui-nprogress-pulse 2s ease-out 0s infinite',
-      },
-      '& .nprogress-bar > .nprogress-bar-first': {
+  return `
+    #nprogress {
+      direction: ltr;
+      pointer-events: none;
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      height: 2px;
+      z-index: ${theme.zIndex.tooltip};
+      background-color: ${
+        theme.palette.mode === 'dark' ? theme.palette.primary[700] : theme.palette.primary[200]
+      };
+      & .nprogress-bar {
+        position: fixed;
+        background-color: ${theme.nprogress.color};
+        top: 0;
+        left: 0;
+        right: 0;
+        height: 2px;
+      }
+      & .nprogress-bar > div {
+        position: absolute;
+        top: 0;
+        height: 2px;
+        box-shadow: ${theme.nprogress.color} 1px 0 6px 1px;
+        border-radius: 100%;
+        animation: mui-nprogress-pulse 2s ease-out 0s infinite;
+      }
+      & .nprogress-bar > .nprogress-bar-first {
+        opacity: 0.6;
+        width: 20px;
+        right: 0;
+        clip: rect(-6px,22px,14px,10px);
+      }
+      & .nprogress-bar > nprogress-bar-last {
+        opacity: 0.6;
+        width: 180px;
+        right: -80px;
+        clip: rect(-6px,90px,14px,-6px);
+      }
+    }
+    @keyframes mui-nprogress-pulse {
+      30% {
         opacity: 0.6,
-        width: 20,
-        right: 0,
-        clip: 'rect(-6px,22px,14px,10px)',
-      },
-      '& .nprogress-bar > nprogress-bar-last': {
-        opacity: 0.6,
-        width: 180,
-        right: -80,
-        clip: 'rect(-6px,90px,14px,-6px)',
-      },
-    },
-    '@keyframes mui-nprogress-pulse': {
-      '30%': {
-        opacity: 0.6,
-      },
-      '60%': {
+      }
+      60% {
         opacity: 0,
-      },
-      to: {
+      }
+      to {
         opacity: 0.6,
-      },
-    },
-  };
+      }
+    }
+  `;
 };
 
 /**
