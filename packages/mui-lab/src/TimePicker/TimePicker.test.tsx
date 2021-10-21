24db1e142b5a908d9983b2c619f8f7a34e24c970
Adam Fitzgibbon <fitzgamer@hotmail.com>
2021-10-21 00:03:23 -0700

    added tests for the hook!

--- packages/mui-lab/src/TimePicker/TimePicker.test.tsx
+++ packages/mui-lab/src/TimePicker/TimePicker.test.tsx
@@ -1,10 +1,16 @@
+/* eslint-disable material-ui/mui-name-matches-component-name */
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
-import { describeConformance } from 'test/utils';
+import { createClientRender, describeConformance } from 'test/utils';
 import TimePicker from './TimePicker';
 import { wrapPickerMount } from '../internal/pickers/test-utils';
+import { useTimePickerDefaultizedProps } from './shared';
+import { DesktopTimePickerProps, LocalizationProvider } from '..';
+import { expect } from 'chai';
+import AdapterDateFns from '../AdapterDateFns';
 
 describe('<TimePicker />', () => {
+  const render = createClientRender();
   describeConformance(
     <TimePicker
       onChange={() => {}}
@@ -29,4 +35,44 @@ describe('<TimePicker />', () => {
       ],
     }),
   );
+
+  describe('shared functions', () => {
+    describe('prop: mask', () => {
+      const ChildComponent = ({ mask, ampm }: { mask?: string; ampm?: boolean }) => {
+        const { mask: outputMask } = useTimePickerDefaultizedProps(
+          { mask, ampm } as DesktopTimePickerProps<unknown>,
+          'MuiDesktopTimePicker',
+        );
+
+        return <div id="mask">{outputMask}</div>;
+      };
+
+      const TestComponent = ({ mask, ampm }: { mask?: string; ampm?: boolean }) => (
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <ChildComponent mask={mask} ampm={ampm} />
+        </LocalizationProvider>
+      );
+      it('uses the mask prop if passed', () => {
+        const inputMask = '__:__ _M';
+        const { container } = render(<TestComponent mask={inputMask} />);
+
+        const mask = container.querySelector('#mask')?.textContent;
+        expect(mask).to.equal(inputMask.toLowerCase());
+      });
+
+      it('defaults to an ampm mask if no mask prop is passed and ampm is passed as true', () => {
+        const { container } = render(<TestComponent ampm />);
+
+        const mask = container.querySelector('#mask')?.textContent;
+        expect(mask).to.equal('__:__ _m');
+      });
+
+      it('defaults to a 24 hour mask if no mask prop is passed and ampm is passed as false', () => {
+        const { container } = render(<TestComponent ampm={false} />);
+
+        const mask = container.querySelector('#mask')?.textContent;
+        expect(mask).to.equal('__:__');
+      });
+    });
+  });
 });
