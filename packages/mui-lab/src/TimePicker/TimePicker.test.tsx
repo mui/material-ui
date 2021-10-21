e88e1667c74c6e76f6d0fff84c2fa0a38a5d589b
Adam Fitzgibbon <fitzgamer@hotmail.com>
2021-10-21 00:16:04 -0700

    fix linting error

--- packages/mui-lab/src/TimePicker/TimePicker.test.tsx
+++ packages/mui-lab/src/TimePicker/TimePicker.test.tsx
@@ -2,11 +2,11 @@
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import { createClientRender, describeConformance } from 'test/utils';
+import { expect } from 'chai';
 import TimePicker from './TimePicker';
 import { wrapPickerMount } from '../internal/pickers/test-utils';
 import { useTimePickerDefaultizedProps } from './shared';
 import { DesktopTimePickerProps, LocalizationProvider } from '..';
-import { expect } from 'chai';
 import AdapterDateFns from '../AdapterDateFns';
 
 describe('<TimePicker />', () => {
