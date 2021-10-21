a32e19a52ea2cc2087f6be99534d18383004e91c
wimukti <wimukthi.18@cse.mrt.ac.lk>
2021-10-21 11:52:00 +0530

    Added two seperate sections for enable color on dark code

--- docs/src/pages/components/app-bar/EnableColorOnDarkAppBar.tsx
+++ docs/src/pages/components/app-bar/EnableColorOnDarkAppBar.tsx
@@ -6,65 +6,21 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
 
 export default function EnableColorOnDarkAppBar() {
-  const [themeMode, setThemeMode] = React.useState<'light' | 'dark'>('dark');
-  const [enableColorOnDarkMode, setEnableColorOnDarkMode] =
-    React.useState<boolean>(true);
-
   const theme = createTheme({
     palette: {
-      mode: themeMode,
+      mode: 'dark',
       primary: {
-        main: '#FF0000',
-      },
-    },
-    components: {
-      MuiAppBar: {
-        defaultProps: {
-          enableColorOnDark: enableColorOnDarkMode,
-        },
+        main: '#1976d2',
       },
     },
   });
 
-  const themeModeHandleChange = () => {
-    setThemeMode(themeMode === 'dark' ? 'light' : 'dark');
-  };
-
-  const handleChange = () => {
-    setEnableColorOnDarkMode(!enableColorOnDarkMode);
-  };
-
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ flexGrow: 1 }}>
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={enableColorOnDarkMode}
-                onChange={handleChange}
-                aria-label="enable color on dark mode switch"
-              />
-            }
-            label={
-              enableColorOnDarkMode
-                ? 'Disable Color on Dark'
-                : 'Enable Color on Dark'
-            }
-          />
-        </FormGroup>
-        <AppBar
-          color="primary"
-          position="static"
-          enableColorOnDark={enableColorOnDarkMode}
-        >
+        <AppBar position="static" enableColorOnDark sx={{ mb: theme.spacing(2) }}>
           <Toolbar variant="dense">
             <IconButton
               edge="start"
@@ -80,22 +36,28 @@ export default function EnableColorOnDarkAppBar() {
               component="div"
               sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
             >
-              MUI
+              enableColorOnDark
             </Typography>
-            <div>
-              {theme.palette.mode} mode
-              <IconButton
-                sx={{ ml: 1 }}
-                onClick={themeModeHandleChange}
-                color="inherit"
-              >
-                {theme.palette.mode === 'dark' ? (
-                  <Brightness7Icon />
-                ) : (
-                  <Brightness4Icon />
-                )}
-              </IconButton>
-            </div>
+          </Toolbar>
+        </AppBar>
+        <AppBar color="primary" position="static">
+          <Toolbar variant="dense">
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
+              default
+            </Typography>
           </Toolbar>
         </AppBar>
       </Box>
