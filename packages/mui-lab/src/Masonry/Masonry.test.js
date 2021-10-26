0ddf877997b6537ad57ee9a92a583cd20c136587
Benny Joo <sldisek783@gmail.com>
2021-10-26 10:35:03 +0100

    [Masonry] Add tests for unordered responsive prop values

--- packages/mui-lab/src/Masonry/Masonry.test.js
+++ packages/mui-lab/src/Masonry/Masonry.test.js
@@ -213,4 +213,99 @@ describe('<Masonry />', () => {
       });
     });
   });
+
+  describe('prop: columns', () => {
+    it('should generate correct responsive styles regardless of breakpoints order', () => {
+      const columns = { sm: 5, md: 7, xs: 3 };
+      const spacing = 1;
+      expect(
+        getStyle({
+          ownerState: {
+            columns,
+            spacing,
+            maxColumnHeight,
+          },
+          theme,
+        }),
+      ).to.deep.equal({
+        width: '100%',
+        display: 'flex',
+        flexFlow: 'column wrap',
+        alignContent: 'space-between',
+        boxSizing: 'border-box',
+        '& > *': {
+          boxSizing: 'border-box',
+          margin: parseToNumber(theme.spacing(spacing)) / 2,
+        },
+        margin: -(parseToNumber(theme.spacing(spacing)) / 2),
+        height: maxColumnHeight + parseToNumber(theme.spacing(spacing)),
+        [`@media (min-width:${theme.breakpoints.values.xs}px)`]: {
+          '& > *': {
+            width: `calc(${(100 / columns.xs).toFixed(2)}% - ${theme.spacing(spacing)})`,
+          },
+        },
+        [`@media (min-width:${theme.breakpoints.values.sm}px)`]: {
+          '& > *': {
+            width: `calc(${(100 / columns.sm).toFixed(2)}% - ${theme.spacing(spacing)})`,
+          },
+        },
+        [`@media (min-width:${theme.breakpoints.values.md}px)`]: {
+          '& > *': {
+            width: `calc(${(100 / columns.md).toFixed(2)}% - ${theme.spacing(spacing)})`,
+          },
+        },
+      });
+    });
+  });
+
+  describe('prop: spacing', () => {
+    it('should generate correct responsive styles regardless of breakpoints order', () => {
+      const columns = 4;
+      const spacing = { sm: 2, md: 3, xs: 1 };
+      expect(
+        getStyle({
+          ownerState: {
+            columns,
+            spacing,
+            maxColumnHeight,
+          },
+          theme,
+        }),
+      ).to.deep.equal({
+        width: '100%',
+        display: 'flex',
+        flexFlow: 'column wrap',
+        alignContent: 'space-between',
+        boxSizing: 'border-box',
+        '& > *': {
+          boxSizing: 'border-box',
+          width: `calc(${(100 / columns).toFixed(2)}% - 0px)`,
+        },
+        [`@media (min-width:${theme.breakpoints.values.xs}px)`]: {
+          '& > *': {
+            margin: parseToNumber(theme.spacing(spacing.xs)) / 2,
+            width: `calc(${(100 / columns).toFixed(2)}% - ${theme.spacing(spacing.xs)})`,
+          },
+          margin: -(parseToNumber(theme.spacing(spacing.xs)) / 2),
+          height: maxColumnHeight + parseToNumber(theme.spacing(spacing.xs)),
+        },
+        [`@media (min-width:${theme.breakpoints.values.sm}px)`]: {
+          '& > *': {
+            margin: parseToNumber(theme.spacing(spacing.sm)) / 2,
+            width: `calc(${(100 / columns).toFixed(2)}% - ${theme.spacing(spacing.sm)})`,
+          },
+          margin: -(parseToNumber(theme.spacing(spacing.sm)) / 2),
+          height: maxColumnHeight + parseToNumber(theme.spacing(spacing.sm)),
+        },
+        [`@media (min-width:${theme.breakpoints.values.md}px)`]: {
+          '& > *': {
+            margin: parseToNumber(theme.spacing(spacing.md)) / 2,
+            width: `calc(${(100 / columns).toFixed(2)}% - ${theme.spacing(spacing.md)})`,
+          },
+          margin: -(parseToNumber(theme.spacing(spacing.md)) / 2),
+          height: maxColumnHeight + parseToNumber(theme.spacing(spacing.md)),
+        },
+      });
+    });
+  });
 });
