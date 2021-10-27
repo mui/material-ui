7dd740f3abeb6b31bb8a61afa47d9caf94de25b2
Benny Joo <sldisek783@gmail.com>
2021-10-27 11:11:02 +0100

    [system] Add tests for breakpoints.js

--- packages/mui-system/src/breakpoints.test.js
+++ packages/mui-system/src/breakpoints.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import breakpoints from './breakpoints';
+import breakpoints, { computeBreakpointsBase, resolveBreakpointValues } from './breakpoints';
 import style from './style';
 
 const textColor = style({
@@ -8,6 +8,8 @@ const textColor = style({
 });
 
 describe('breakpoints', () => {
+  const themeBreakpoints = { xs: 100, sm: 200, md: 300, lg: 400, xl: 500 };
+
   it('should work', () => {
     const palette = breakpoints(textColor);
 
@@ -24,6 +26,73 @@ describe('breakpoints', () => {
       '@media (min-width:600px)': {
         color: 'blue',
       },
+    });
+  });
+
+  describe('function: computeBreakpointsBase', () => {
+    it('compute base for breakpoint values of array type', () => {
+      const columns = [1, 2, 3];
+      const base = computeBreakpointsBase(columns, themeBreakpoints);
+      expect(base).to.deep.equal({ xs: true, sm: true, md: true });
+    });
+
+    it('compute base for breakpoint values of object type', () => {
+      const columns = { xs: 1, sm: 2, md: 3 };
+      const base = computeBreakpointsBase(columns, themeBreakpoints);
+      expect(base).to.deep.equal({ xs: true, sm: true, md: true });
+    });
+
+    it('return empty object for fixed value', () => {
+      const columns = 3;
+      const base = computeBreakpointsBase(columns, themeBreakpoints);
+      expect(base).to.deep.equal({});
+    });
+  });
+
+  describe('function: resolveBreakpointValues', () => {
+    it('resolve breakpoint values for prop of array type', () => {
+      const columns = [1, 2, 3];
+      const values = resolveBreakpointValues({ values: columns, breakpoints: themeBreakpoints });
+      expect(values).to.deep.equal({ xs: 1, sm: 2, md: 3 });
+    });
+
+    it('resolve breakpoint values for prop of object type', () => {
+      const columns = { xs: 1, sm: 2, md: 3 };
+      const values = resolveBreakpointValues({ values: columns, breakpoints: themeBreakpoints });
+      expect(values).to.deep.equal({ xs: 1, sm: 2, md: 3 });
+    });
+
+    it('resolve breakpoint values for unordered prop of object type', () => {
+      const columns = { sm: 2, md: 3, xs: 1 };
+      const values = resolveBreakpointValues({ values: columns, breakpoints: themeBreakpoints });
+      expect(values).to.deep.equal({ xs: 1, sm: 2, md: 3 });
+    });
+
+    it('return prop as it is for prop of fixed value', () => {
+      const columns = 3;
+      const values = resolveBreakpointValues({ values: columns, breakpoints: themeBreakpoints });
+      expect(values).to.equal(3);
+    });
+
+    it('resolve breakpoint values for prop of array type given custom base', () => {
+      const columns = [1, 2, 3];
+      const customBase = { xs: true, sm: true, md: true, lg: true };
+      const values = resolveBreakpointValues({ values: columns, base: customBase });
+      expect(values).to.deep.equal({ xs: 1, sm: 2, md: 3, lg: 3 });
+    });
+
+    it('resolve breakpoint values for prop of object type given custom base', () => {
+      const columns = { xs: 1, sm: 2, md: 3 };
+      const customBase = { xs: true, sm: true, md: true, lg: true };
+      const values = resolveBreakpointValues({ values: columns, base: customBase });
+      expect(values).to.deep.equal({ xs: 1, sm: 2, md: 3, lg: 3 });
+    });
+
+    it('resolve breakpoint values for unordered prop of object type given custom base', () => {
+      const columns = { sm: 2, md: 3, xs: 1 };
+      const customBase = { xs: true, sm: true, md: true, lg: true };
+      const values = resolveBreakpointValues({ values: columns, base: customBase });
+      expect(values).to.deep.equal({ xs: 1, sm: 2, md: 3, lg: 3 });
     });
   });
 });
