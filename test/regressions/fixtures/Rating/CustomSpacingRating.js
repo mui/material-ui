209b4d8fec2f78a5cf1ff67dc700eab7bbc4f6b9
nikita <nikita.bobers@gmail.com>
2021-10-28 23:25:14 +0300

    Fix regression test

--- test/regressions/fixtures/Rating/CustomSpacingRating.js
+++ test/regressions/fixtures/Rating/CustomSpacingRating.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import Rating from '@mui/material/Rating';
 
-export default function FocusVisibleRating() {
+export default function CustomSpacingRating() {
   return (
     <Rating
-      name="no-value"
-      value={null}
+      name="half-rating"
+      defaultValue={1}
+      precision={0.5}
       sx={{
         '.MuiRating-decimal': { marginLeft: 1, marginRight: 1 },
       }}
