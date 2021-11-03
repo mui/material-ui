aaed4adab7368a2a8d01103c093a73b5782f8151
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-11-03 16:48:19 +0100

    run scripts

--- packages/mui-material/src/ScopedCssBaseline/ScopedCssBaseline.js
+++ packages/mui-material/src/ScopedCssBaseline/ScopedCssBaseline.js
@@ -79,7 +79,9 @@ ScopedCssBaseline.propTypes /* remove-proptypes */ = {
    */
   component: PropTypes.elementType,
   /**
-   * @ignore
+   * Enable `color-scheme` css property to use `theme.palette.mode`.
+   * For more details, check out https://developer.mozilla.org/en-US/docs/Web/CSS/color-scheme
+   * For browser support, check out https://caniuse.com/?search=color-scheme
    */
   enableColorScheme: PropTypes.bool,
 };
