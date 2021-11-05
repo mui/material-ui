f95074871095d4ae24b4bf96da40552df32dd50c
siriwatknp <siriwatkunaporn@gmail.com>
2021-11-05 08:17:49 +0700

    run proptypes

--- packages/mui-joy/src/Switch/Switch.tsx
+++ packages/mui-joy/src/Switch/Switch.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { unstable_composeClasses as composeClasses } from '@mui/core';
 import { useSwitch } from '@mui/core/SwitchUnstyled';
@@ -68,6 +69,39 @@ const SwitchRoot = styled('span', {
   };
 });
 
+SwitchRoot.propTypes /* remove-proptypes */ = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // |     To update them edit TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * @ignore
+   */
+  children: PropTypes.node,
+  /**
+   * @ignore
+   */
+  ownerState: PropTypes.shape({
+    checked: PropTypes.bool,
+    classes: PropTypes.object,
+    className: PropTypes.string,
+    color: PropTypes.oneOf(['brand', 'neutral']),
+    component: PropTypes.elementType,
+    defaultChecked: PropTypes.bool,
+    disabled: PropTypes.bool,
+    onBlur: PropTypes.func,
+    onChange: PropTypes.func,
+    onFocus: PropTypes.func,
+    onFocusVisible: PropTypes.func,
+    readOnly: PropTypes.bool,
+    required: PropTypes.bool,
+  }).isRequired,
+  /**
+   * @ignore
+   */
+  theme: PropTypes.object,
+} as any;
+
 const SwitchInput = styled('input', {
   name: 'JoySwitch',
   slot: 'Input',
@@ -85,6 +119,35 @@ const SwitchInput = styled('input', {
   cursor: 'pointer',
 }));
 
+SwitchInput.propTypes /* remove-proptypes */ = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // |     To update them edit TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * @ignore
+   */
+  children: PropTypes.node,
+  /**
+   * @ignore
+   */
+  ownerState: PropTypes.shape({
+    checked: PropTypes.bool,
+    classes: PropTypes.object,
+    className: PropTypes.string,
+    color: PropTypes.oneOf(['brand', 'neutral']),
+    component: PropTypes.elementType,
+    defaultChecked: PropTypes.bool,
+    disabled: PropTypes.bool,
+    onBlur: PropTypes.func,
+    onChange: PropTypes.func,
+    onFocus: PropTypes.func,
+    onFocusVisible: PropTypes.func,
+    readOnly: PropTypes.bool,
+    required: PropTypes.bool,
+  }).isRequired,
+} as any;
+
 const SwitchTrack = styled('span', {
   name: 'JoySwitch',
   slot: 'Track',
@@ -98,6 +161,36 @@ const SwitchTrack = styled('span', {
   backgroundColor: 'currentColor',
   borderRadius: 'var(--joy-Switch-track-radius)',
 }));
+
+SwitchTrack.propTypes /* remove-proptypes */ = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // |     To update them edit TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * @ignore
+   */
+  children: PropTypes.node,
+  /**
+   * @ignore
+   */
+  ownerState: PropTypes.shape({
+    checked: PropTypes.bool,
+    classes: PropTypes.object,
+    className: PropTypes.string,
+    color: PropTypes.oneOf(['brand', 'neutral']),
+    component: PropTypes.elementType,
+    defaultChecked: PropTypes.bool,
+    disabled: PropTypes.bool,
+    focusVisible: PropTypes.bool.isRequired,
+    onBlur: PropTypes.func,
+    onChange: PropTypes.func,
+    onFocus: PropTypes.func,
+    onFocusVisible: PropTypes.func,
+    readOnly: PropTypes.bool,
+    required: PropTypes.bool,
+  }).isRequired,
+} as any;
 
 const SwitchThumb = styled('span', {
   name: 'JoySwitch',
@@ -117,6 +210,35 @@ const SwitchThumb = styled('span', {
     left: 'calc(50% + var(--joy-Switch-track-width) / 2 - var(--joy-Switch-thumb-width) / 2 - var(--joy-Switch-thumb-offset))',
   },
 }));
+
+SwitchThumb.propTypes /* remove-proptypes */ = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // |     To update them edit TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * @ignore
+   */
+  children: PropTypes.node,
+  /**
+   * @ignore
+   */
+  ownerState: PropTypes.shape({
+    checked: PropTypes.bool,
+    classes: PropTypes.object,
+    className: PropTypes.string,
+    color: PropTypes.oneOf(['brand', 'neutral']),
+    component: PropTypes.elementType,
+    defaultChecked: PropTypes.bool,
+    disabled: PropTypes.bool,
+    onBlur: PropTypes.func,
+    onChange: PropTypes.func,
+    onFocus: PropTypes.func,
+    onFocusVisible: PropTypes.func,
+    readOnly: PropTypes.bool,
+    required: PropTypes.bool,
+  }).isRequired,
+} as any;
 
 const Switch = React.forwardRef<HTMLSpanElement, SwitchProps>(function Switch(inProps, ref) {
   const props = inProps;
@@ -171,5 +293,66 @@ const Switch = React.forwardRef<HTMLSpanElement, SwitchProps>(function Switch(in
     </SwitchRoot>
   );
 });
+
+Switch.propTypes /* remove-proptypes */ = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // |     To update them edit TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * If `true`, the component is checked.
+   */
+  checked: PropTypes.bool,
+  /**
+   * @ignore
+   */
+  children: PropTypes.node,
+  /**
+   * Class name applied to the root element.
+   */
+  className: PropTypes.string,
+  /**
+   * The component used for the Root slot.
+   * Either a string to use a HTML element or a component.
+   * This is equivalent to `components.Root`. If both are provided, the `component` is used.
+   */
+  component: PropTypes.elementType,
+  /**
+   * The default checked state. Use when the component is not controlled.
+   */
+  defaultChecked: PropTypes.bool,
+  /**
+   * If `true`, the component is disabled.
+   */
+  disabled: PropTypes.bool,
+  /**
+   * @ignore
+   */
+  onBlur: PropTypes.func,
+  /**
+   * Callback fired when the state is changed.
+   *
+   * @param {React.ChangeEvent<HTMLInputElement>} event The event source of the callback.
+   * You can pull out the new value by accessing `event.target.value` (string).
+   * You can pull out the new checked state by accessing `event.target.checked` (boolean).
+   */
+  onChange: PropTypes.func,
+  /**
+   * @ignore
+   */
+  onFocus: PropTypes.func,
+  /**
+   * @ignore
+   */
+  onFocusVisible: PropTypes.func,
+  /**
+   * If `true`, the component is read only.
+   */
+  readOnly: PropTypes.bool,
+  /**
+   * If `true`, the `input` element is required.
+   */
+  required: PropTypes.bool,
+} as any;
 
 export default Switch;
