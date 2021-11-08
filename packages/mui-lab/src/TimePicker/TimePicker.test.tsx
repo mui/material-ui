e5efb0bd0ff9f1b46dfbbc0f4fc655f8e827b71f
Adam Fitzgibbon <fitzgamer@hotmail.com>
2021-11-07 18:31:26 -0800

    fix unused imports

--- packages/mui-lab/src/TimePicker/TimePicker.test.tsx
+++ packages/mui-lab/src/TimePicker/TimePicker.test.tsx
@@ -1,16 +1,11 @@
 /* eslint-disable material-ui/mui-name-matches-component-name */
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
-import { createClientRender, describeConformance } from 'test/utils';
-import { expect } from 'chai';
+import { describeConformance } from 'test/utils';
 import TimePicker from './TimePicker';
 import { wrapPickerMount } from '../internal/pickers/test-utils';
-import { useTimePickerDefaultizedProps } from './shared';
-import { DesktopTimePickerProps, LocalizationProvider } from '..';
-import AdapterDateFns from '../AdapterDateFns';
 
 describe('<TimePicker />', () => {
-  const render = createClientRender();
   describeConformance(
     <TimePicker
       onChange={() => {}}
