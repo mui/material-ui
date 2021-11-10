4c2d225cdada3c81ee2484f1c45752f7cc1d718e
Marija Najdova <mnajdova@gmail.com>
2021-11-10 11:24:57 +0100

    [theme] Fix types for variants' style and cover with tests

--- packages/mui-material/src/styles/variants.d.ts
+++ packages/mui-material/src/styles/variants.d.ts
@@ -1,9 +1,10 @@
-import { CSSInterpolation } from '@mui/system';
+import { Interpolation } from '@mui/system';
+import { Theme } from './createTheme';
 import { ComponentsPropsList } from './props';
 
 export type ComponentsVariants = {
   [Name in keyof ComponentsPropsList]?: Array<{
     props: Partial<ComponentsPropsList[Name]>;
-    style: CSSInterpolation;
+    style: Interpolation<{ theme: Theme }>;
   }>;
 };
