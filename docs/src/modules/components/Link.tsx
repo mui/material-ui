833f66ebf489d8272a3aff3147c21e92b7e89bba
Olivier Tassinari <olivier.tassinari@gmail.com>
2021-11-12 12:01:41 +0100

    [docs] Fix link locale handling (#29624)

--- docs/src/modules/components/Link.tsx
+++ docs/src/modules/components/Link.tsx
@@ -86,10 +86,6 @@ const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(function Link(props,
     return <MuiLink className={className} href={href} ref={ref} {...other} />;
   }
 
-  if (noLinkStyle) {
-    return <NextLinkComposed className={className} ref={ref} to={href} {...other} />;
-  }
-
   let linkAs = linkAsProp || (href as string);
   if (
     userLanguage !== 'en' &&
@@ -98,6 +94,12 @@ const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(function Link(props,
     href.indexOf('/blog') !== 0
   ) {
     linkAs = `/${userLanguage}${linkAs}`;
+  }
+
+  if (noLinkStyle) {
+    return (
+      <NextLinkComposed className={className} ref={ref} to={href} linkAs={linkAs} {...other} />
+    );
   }
 
   return (
