b092f6bfb248820abdb9b9750b07033f7501169b
siriwatknp <siriwatkunaporn@gmail.com>
2021-11-16 15:23:22 +0700

    fix duplicate key

--- docs/src/route.ts
+++ docs/src/route.ts
@@ -15,7 +15,6 @@ const ROUTES = {
   theming: '/customization/theming/',
   documentation: '/getting-started/usage/',
   communityHelp: '/getting-started/support/#community-help-free',
-  dataGridDocs: '/components/data-grid/getting-started/',
   blog: 'https://medium.com/material-ui',
   showcase: '/discover-more/showcase',
   roadmap: 'https://github.com/mui-org/material-ui-x/projects/1',
