0e6426bc4566ab252609de2f39d398b1a8b677aa
Marija Najdova <mnajdova@gmail.com>
2021-11-18 14:27:58 +0100

    Remove unused imports

--- packages/mui-base/src/TabsListUnstyled/TabsListUnstyled.tsx
+++ packages/mui-base/src/TabsListUnstyled/TabsListUnstyled.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-import { unstable_useForkRef as useForkRef } from '@mui/utils';
 import { OverridableComponent } from '@mui/types';
 import composeClasses from '../composeClasses';
 import { appendOwnerState } from '../utils';
--- packages/mui-material-next/src/Tabs/TabsList.js
+++ packages/mui-material-next/src/Tabs/TabsList.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import { styled } from '@mui/material/styles';
-import { unstable_useForkRef as useForkRef } from '@mui/utils';
 import { useTabsList } from '@mui/base/TabsListUnstyled';
 
 const FlexContainer = styled('div', {
