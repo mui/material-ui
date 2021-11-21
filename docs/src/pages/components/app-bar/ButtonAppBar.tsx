f79779d0a5cc67ef403f8004971a910b1719ec11
Kazi Abdur Rakib <57065763+karakib2k18@users.noreply.github.com>
2021-11-21 20:46:22 +0600

    add margin on news

--- docs/src/pages/components/app-bar/ButtonAppBar.tsx
+++ docs/src/pages/components/app-bar/ButtonAppBar.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
 
 export default function ButtonAppBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -22,22 +22,16 @@ export default function ButtonAppBar() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
+        <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, mt: 1 }}>
             News
           </Typography>
           <Button color="inherit">Login</Button>
         </Box>
-        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1, mt: 1 }}>
             News
           </Typography>
@@ -52,25 +46,25 @@ export default function ButtonAppBar() {
             <MenuIcon />
           </IconButton>
 
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
             <Menu
               id="menu-appbar"
               anchorEl={anchorEl}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorEl)}
               onClose={handleClose}
               sx={{
-                display: { xs: 'block', sm: 'none' },
-                mt: '32px',
-                px: '20px',
+                display: { xs: "block", sm: "none" },
+                mt: "32px",
+                px: "20px",
               }}
             >
               <MenuItem>
@@ -80,8 +74,8 @@ export default function ButtonAppBar() {
                   noWrap
                   component="div"
                   sx={{
-                    display: 'flex',
-                    alignItems: 'center',
+                    display: "flex",
+                    alignItems: "center",
                   }}
                 >
                   login
