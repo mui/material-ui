58e0b0ce21f983f0fcc316773f14cc3c4cff07e2
Kazi Abdur Rakib <57065763+karakib2k18@users.noreply.github.com>
2021-11-21 17:14:31 +0600

    javascript formatted

--- docs/src/pages/components/app-bar/ButtonAppBar.js
+++ docs/src/pages/components/app-bar/ButtonAppBar.js
@@ -3,11 +3,11 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
 
 export default function ButtonAppBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -19,125 +19,86 @@ export default function ButtonAppBar() {
   const handleClose = () => {
     setAnchorEl(null);
   };
-
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Container maxWidth="xl">
+    <Box>
+      <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+        <AppBar position="static">
           <Toolbar>
-            <Typography variant="h6" noWrap component="div">
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               News
             </Typography>
-            <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-              <div>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={Boolean(anchorEl)}
-                  onClose={handleClose}
-                  sx={{
-                    display: { xs: 'block', sm: 'none' },
-                    mt: '32px',
-                    px: '20px',
-                  }}
-                >
-                  <MenuItem>
-                    <Typography
-                      onClick={handleClose}
-                      variant="h6"
-                      noWrap
-                      component="div"
-                      sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        m: 1,
-                      }}
-                    >
-                      Home
-                    </Typography>
-                  </MenuItem>
-                  <Box>
-                    <MenuItem>
-                      <Typography
-                        onClick={handleClose}
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          m: 1,
-                        }}
-                      >
-                        login
-                      </Typography>
-                    </MenuItem>
-                  </Box>
-                </Menu>
-              </div>
-            </Box>
-
-            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleMenu}
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          <div>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              sx={{
+                display: { xs: 'block', sm: 'none' },
+                mt: '32px',
+                px: '20px',
+              }}
+            >
               <MenuItem>
                 <Typography
+                  onClick={handleClose}
                   variant="h6"
                   noWrap
                   component="div"
                   sx={{
                     display: 'flex',
                     alignItems: 'center',
-                    p: 1,
                     m: 1,
                   }}
                 >
-                  Home
+                  login
                 </Typography>
               </MenuItem>
-            </Box>
-            <Box sx={{ flexGrow: 1 }}>
-              <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                <MenuItem>
-                  <Typography
-                    variant="h6"
-                    noWrap
-                    component="div"
-                    sx={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      p: 1,
-                      m: 1,
-                    }}
-                  >
-                    Login
-                  </Typography>
-                </MenuItem>
-              </Box>
-            </Box>
-            <Box>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-                sx={{ display: { xs: 'block', sm: 'none' } }}
-              >
-                <MenuIcon />
-              </IconButton>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
+            </Menu>
+          </div>
+        </Box>
+      </Box>
     </Box>
   );
 }
