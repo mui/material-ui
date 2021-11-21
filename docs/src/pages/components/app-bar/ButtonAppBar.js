44c386c02c39f999dff163fe8f2fb61372f17880
Kazi Abdur Rakib <57065763+karakib2k18@users.noreply.github.com>
2021-11-21 13:31:17 +0600

    responsive ButtonAppBar.js  for all devices
    I created a responsive menubar for all users. It's so useful for beginners. Most of the beginners become upset when they can't able make a responsive navbar properly.

--- docs/src/pages/components/app-bar/ButtonAppBar.js
+++ docs/src/pages/components/app-bar/ButtonAppBar.js
@@ -1,31 +1,160 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
 
 export default function ButtonAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
+        <Container maxWidth="xl">
+          <Toolbar>
+            <Typography variant="h6" noWrap component="div">
+              LOGO
+            </Typography>
+
+            {/* responsive AppBar for mobile  start */}
+
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <div>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                  }}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                  sx={{
+                    display: { xs: "block", sm: "none" },
+                    mt: "32px",
+                    px: "20px"
+                  }}
+                >
+                  <MenuItem>
+                    <Typography
+                      onClick={handleClose}
+                      variant="h6"
+                      noWrap
+                      component="div"
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        m: 1
+                      }}
+                    >
+                      Home
+                    </Typography>
+                  </MenuItem>
+
+                  <Box></Box>
+                  <Box>
+                    {/* NAVBAR FOR DESKTOP */}
+
+                    <MenuItem>
+                      <Typography
+                        onClick={handleClose}
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        // onClick={}
+                        sx={{
+                          display: "flex",
+                          alignItems: "center",
+                          m: 1
+                        }}
+                      >
+                        login
+                      </Typography>
+                    </MenuItem>
+                  </Box>
+                </Menu>
+              </div>
+            </Box>
+
+            {/*  */}
+            {/* for mobile responsive APPbar end here */}
+            {/*  */}
+            {/*  */}
+            {/* NAVBAR FOR DESKTOP START HERE */}
+            {/*  */}
+
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <MenuItem>
+                <Typography
+                  variant="h6"
+                  noWrap
+                  component="div"
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    p: 1,
+                    m: 1
+                  }}
+                >
+                  Home
+                </Typography>
+              </MenuItem>
+            </Box>
+            <Box sx={{ flexGrow: 1 }}></Box>
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <MenuItem>
+                <Typography
+                  variant="h6"
+                  noWrap
+                  component="div"
+                  // onClick={}
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    p: 1,
+                    m: 1
+                  }}
+                >
+                  Login
+                </Typography>
+              </MenuItem>
+            </Box>
+ {/* NAVBAR FOR DESKTOP  END*/}
+
+ {/* Menu dropdown icon*/}
+            <Box>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+                sx={{ display: { xs: "block", sm: "none" } }}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   );
