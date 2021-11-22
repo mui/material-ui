a6f611a23649b5a3d0648910d835f45cf78af7a6
Marija Najdova <mnajdova@gmail.com>
2021-11-22 11:07:42 +0100

    prettier

--- packages/mui-material/src/Box/Box.spec.tsx
+++ packages/mui-material/src/Box/Box.spec.tsx
@@ -1,23 +1,23 @@
 import * as React from 'react';
-import Box from "@mui/material/Box";
+import Box from '@mui/material/Box';
 
 function ThemeValuesCanBeSpread() {
   <Box
     sx={(theme) => ({
       ...theme.typography.body1,
-      color: theme.palette.primary.main
+      color: theme.palette.primary.main,
     })}
   />;
   <Box
     sx={(theme) => ({
       ...theme.mixins.toolbar,
-      color: theme.palette.primary.main
+      color: theme.palette.primary.main,
     })}
   />;
   <Box
     sx={(theme) => ({
       ...theme.mixins.toolbar,
-      color: 'primary.main'
+      color: 'primary.main',
     })}
   />;
 }
