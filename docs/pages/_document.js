f0851e6321131fd39931fca8372b1437e9f6002d
siriwatknp <siriwatkunaporn@gmail.com>
2021-11-23 15:59:39 +0700

    revert

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -8,7 +8,6 @@ import { pathnameToLanguage } from 'docs/src/modules/utils/helpers';
 import createEmotionCache from 'docs/src/createEmotionCache';
 import { getMetaThemeColor } from 'docs/src/modules/brandingTheme';
 import GlobalStyles from '@mui/material/GlobalStyles';
-import { getInitColorSchemeScript } from '@mui/joy/styles';
 
 // You can find a benchmark of the available CSS minifiers under
 // https://github.com/GoalSmashers/css-minification-benchmark
@@ -135,7 +134,6 @@ export default class MyDocument extends Document {
           />
         </Head>
         <body>
-          {getInitColorSchemeScript({ enableSystem: true })}
           <Main />
           <script
             // eslint-disable-next-line react/no-danger
