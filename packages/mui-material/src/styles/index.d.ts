68fcb7da54cde06f4d91588683bd38463f4e6488
Marija Najdova <mnajdova@gmail.com>
2021-11-23 10:18:59 +0100

    Rename unstable_ to experimental_

--- packages/mui-material/src/styles/index.d.ts
+++ packages/mui-material/src/styles/index.d.ts
@@ -55,7 +55,7 @@ export {
   lighten,
   ColorFormat,
   ColorObject,
-  unstable_sx,
+  experimental_sx,
 } from '@mui/system';
 export { default as useTheme } from './useTheme';
 export { default as useThemeProps } from './useThemeProps';
