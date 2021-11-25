298ad07702fd95743294188b0c0daa6c42156fab
Marija Najdova <mnajdova@gmail.com>
2021-11-25 13:30:10 +0100

    Add touch ripple for showcasing when it works.

--- examples/nextjs-with-styled-components-typescript/pages/index.tsx
+++ examples/nextjs-with-styled-components-typescript/pages/index.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import TouchRipple from '@mui/material/ButtonBase/TouchRipple';
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 import ProTip from '../src/ProTip';
 import Link from '../src/Link';
-import Copyright from '../src/Copyright';
 
-const CustomButton = styled(Button)`
-  background-color: red;
+const CustomButton = styled(Button)({
+  backgroundColor: 'red'
+});
+
+const LeftAlignedTypography = styled(Typography)`
+  text-align: left;
+`;
+
+const CustomTouchRipple = styled(TouchRipple)`
+  opacity: 0.5;
+  background: red;
 `;
 
 export default function Index() {
@@ -23,8 +32,17 @@ export default function Index() {
           Go to the about page
         </Link>
         <ProTip />
-        <CustomButton>Custom button</CustomButton>
-        <Copyright />
+        <CustomButton>This is not working as styles are defined with fn</CustomButton>
+        <Typography textAlign={"right"}>This should be aligned right.</Typography>
+        <LeftAlignedTypography textAlign={"right"}>This will be aligned left, no matter what! (not working as the styles are defined with fn)</LeftAlignedTypography>
+        <div style={{ width: 200, height: 200, position: 'relative', border: "2px dashed grey"}}>
+          <CustomTouchRipple />
+          This override is always working, as the TouchRipple is defined using ``
+        </div>
+        <div style={{ width: 200, height: 200, position: 'relative', border: "2px dashed grey"}}>
+          <TouchRipple />
+          This is the default Touch Ripple
+        </div>
       </Box>
     </Container>
   );
