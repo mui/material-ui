e773c93edc5a666c4b2feba099bd4f5bf57d2464
Takahiro Sugiura <sugiura@spesden.com>
2021-11-29 19:21:44 +0900

    run proptypes and docs:api

--- packages/mui-material/src/Stack/Stack.js
+++ packages/mui-material/src/Stack/Stack.js
@@ -165,7 +165,7 @@ Stack.propTypes /* remove-proptypes */ = {
    * The system prop, which allows defining system overrides as well as additional CSS styles.
    */
   sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object])),
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
     PropTypes.func,
     PropTypes.object,
   ]),
