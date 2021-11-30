241299c300630bfca2c7a743faaf2eecd3b51a43
huydhoang <huydhoang@protonmail.com>
2021-12-01 00:59:15 +0700

    Update nextjs-typescript-example

---
+++ examples/nextjs-with-typescript/src/pages/index.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import type { NextPage } from 'next';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '../components/Link';
+import ProTip from '../components/ProTip';
+import Copyright from '../components/Copyright';
+
+const Home: NextPage = () => {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          MUI v5 + Next.js with TypeScript example
+        </Typography>
+        <Link href="/about" color="secondary">
+          Go to the about page
+        </Link>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+};
+
+export default Home;
