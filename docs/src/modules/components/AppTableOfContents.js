d2328aedc951aa0a024ca077d8d2a9e5ef70f81a
danilo leal <daniloleal09@gmail.com>
2021-12-01 09:51:58 -0300

    refine toc card

--- docs/src/modules/components/AppTableOfContents.js
+++ docs/src/modules/components/AppTableOfContents.js
@@ -219,35 +219,52 @@ export default function AppTableOfContents(props) {
           data-ga-event-label="table-contents"
           sx={(theme) => ({
             mb: 2,
+            p: 1,
             display: 'flex',
             flexDirection: 'column',
-            p: 1,
+            alignItems: 'start',
+            background:
+              theme.palette.mode === 'dark'
+                ? `linear-gradient(90deg, ${theme.palette.primary[900]}, ${theme.palette.primary[600]} 120%)`
+                : `linear-gradient(-90deg, ${theme.palette.primary[700]}, ${theme.palette.primary[500]} 120%)`,
             borderRadius: 1,
-            border: '1px solid',
-            borderColor: theme.palette.mode === 'dark' ? 'primaryDark.600' : 'grey.300',
+            transitionProperty: 'all',
+            transitionTiming: 'cubic-bezier(0.4, 0, 0.2, 1)',
+            transitionDuration: '200ms',
             '&:hover, &:focus-visible': {
-              borderColor: theme.palette.mode === 'dark' ? 'primaryDark.400' : 'grey.500',
-            },
-            '& > img': {
-              width: '100%',
-              height: 'auto',
-              aspectRatio: 3 / 2,
-              borderRadius: 0.5,
+              boxShadow:
+                theme.palette.mode === 'dark'
+                  ? '1px 1px 20px 0 rgb(2 2 2 / 50%)'
+                  : '1px 1px 20px 0 rgb(90 105 120 / 30%)',
             },
           })}
         >
-          <img
-            src="/static/images/survey/table-contents-banner.png"
-            alt="2021 MUI developer survey"
-            width="600"
-            height="400"
-          />
+          <Typography component="span" variant="body1" fontWeight="bold" sx={{ color: '#fff' }}>
+            2021 MUI&nbsp;&nbsp;🚀
+            <br />
+          </Typography>
           <Typography
             component="span"
             variant="body2"
-            sx={{ mt: 1, color: 'text.primary', textAlign: 'center' }}
+            fontWeight="medium"
+            sx={{ color: 'primary.50' }}
           >
-            Help us shape the future of MUI! 🚀
+            Developer survey
+            <br />
+          </Typography>
+          <Typography
+            component="span"
+            variant="caption"
+            fontWeight="normal"
+            sx={(theme) => ({
+              mt: 1,
+              pt: 1,
+              color: 'primary.50',
+              borderTop: 1,
+              borderColor: 'primary.400',
+            })}
+          >
+            Help us shape the future of MUI! &#8594;
           </Typography>
         </Link>
       </NoSsr>
