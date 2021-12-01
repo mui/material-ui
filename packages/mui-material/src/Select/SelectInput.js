65a856fb5c4bfc2e7586cab863a3580bde23c753
Domino987 <domino051@gmail.com>
2021-12-01 22:30:30 +0100

    Reduce to simple join

--- packages/mui-material/src/Select/SelectInput.js
+++ packages/mui-material/src/Select/SelectInput.js
@@ -418,7 +418,7 @@ const SelectInput = React.forwardRef(function SelectInput(props, ref) {
 
   if (computeDisplay) {
     if (multiple) {
-      display = displayMultiple.reduce((prev, curr) => [prev, ', ', curr], '');
+      display = displayMultiple.join(', ');
     } else {
       display = displaySingle;
     }
