b6e5b3c4433b5e23655e8bb97c7bb4c392d10974
huydhoang <huydhoang@protonmail.com>
2021-12-02 00:15:38 +0700

    Move createEmotionCache to utils

--- examples/nextjs-with-typescript/src/pages/_app.tsx
+++ examples/nextjs-with-typescript/src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import theme from '../../styles/theme';
-import createEmotionCache from '../lib/createEmotionCache';
+import createEmotionCache from '../utils/createEmotionCache';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
