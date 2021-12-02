5cfb444eaa7f6c2814237b3c570dc199c2c709f3
Marija Najdova <mnajdova@gmail.com>
2021-12-02 10:52:39 +0100

    Fixes, restructured folders

--- examples/nextjs-with-typescript/src/pages/_app.tsx
+++ examples/nextjs-with-typescript/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
-import theme from '../../styles/theme';
+import theme from '../styles/theme';
 import createEmotionCache from '../utils/createEmotionCache';
 
 // Client-side cache, shared for the whole session of the user in the browser.
--- examples/nextjs-with-typescript/src/pages/_document.tsx
+++ examples/nextjs-with-typescript/src/pages/_document.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
-import theme from '../../styles/theme';
-import createEmotionCache from '../lib/createEmotionCache';
+import theme from '../styles/theme';
+import createEmotionCache from '../utils/createEmotionCache';
 
 export default class MyDocument extends Document {
   render() {
