17cb63752c83ff505328530cbb68e48666596e1e
Michaël De Boey <info@michaeldeboey.be>
2021-12-06 01:19:59 +0100

    Update remix.config.js

--- examples/remix-with-typescript/remix.config.js
+++ examples/remix-with-typescript/remix.config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   appDirectory: 'app',
-  browserBuildDirectory: 'public/build',
+  assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
   serverBuildDirectory: 'build',
   devServerPort: 8002,
