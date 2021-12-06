766911435e545220c7f215a7d00b82b990734d9a
Marija Najdova <mnajdova@gmail.com>
2021-12-06 09:57:02 +0100

    [GlobalStyles] Fix theme type

---
+++ packages/mui-material/src/GlobalStyles/GlobalStyles.spec.tsx
@@ -0,0 +1,9 @@
+import GlobalStyles from '@mui/material/GlobalStyles';
+
+function TestThemeType() {
+  <GlobalStyles
+    styles={(theme) => ({
+      [theme.breakpoints.up('sm')]: { color: theme.palette.primary.main },
+    })}
+  />;
+}
