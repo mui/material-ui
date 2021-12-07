96c3dff6409cd75254e5eab95c4d4a83246dcf74
Marija Najdova <mnajdova@gmail.com>
2021-12-07 13:16:00 +0100

    [useSlider] Add hook (WIP)

--- packages/mui-base/src/SliderUnstyled/index.d.ts
+++ packages/mui-base/src/SliderUnstyled/index.d.ts
@@ -4,5 +4,8 @@ export * from './SliderUnstyled';
 export { default as SliderValueLabelUnstyled } from './SliderValueLabelUnstyled';
 export * from './SliderValueLabelUnstyled';
 
+export { default as useSlider } from './useSlider';
+export * from './useSlider';
+
 export { default as sliderUnstyledClasses } from './sliderUnstyledClasses';
 export * from './sliderUnstyledClasses';
