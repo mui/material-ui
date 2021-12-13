881ef2989bb29f8c8852505aba36ec95788510c8
danilo leal <daniloleal09@gmail.com>
2021-12-13 13:16:12 -0300

    table unstyled customized demo

---
+++ docs/src/pages/components/tables/TableCustomized.js
@@ -0,0 +1,155 @@
+import * as React from 'react';
+import { styled } from '@mui/system';
+import TablePaginationUnstyled from '@mui/base/TablePaginationUnstyled';
+
+function createData(name, calories, fat) {
+  return { name, calories, fat };
+}
+
+const rows = [
+  createData('Cupcake', 305, 3.7),
+  createData('Donut', 452, 25.0),
+  createData('Eclair', 262, 16.0),
+  createData('Frozen yoghurt', 159, 6.0),
+  createData('Gingerbread', 356, 16.0),
+  createData('Honeycomb', 408, 3.2),
+  createData('Ice cream sandwich', 237, 9.0),
+  createData('Jelly Bean', 375, 0.0),
+  createData('KitKat', 518, 26.0),
+  createData('Lollipop', 392, 0.2),
+  createData('Marshmallow', 318, 0),
+  createData('Nougat', 360, 19.0),
+  createData('Oreo', 437, 18.0),
+].sort((a, b) => (a.calories < b.calories ? -1 : 1));
+
+const blue = {
+  50: '#F0F7FF',
+  100: '#C2E0FF',
+  200: '#A5D8FF',
+  300: '#66B2FF',
+  400: '#3399FF',
+  main: '#007FFF', // contrast 3.83:1
+  500: '#007FFF',
+  600: '#0072E5',
+  700: '#0059B2',
+  800: '#004C99',
+  900: '#003A75',
+};
+const grey = {
+  50: '#F3F6F9',
+  100: '#EAEEF3',
+  200: '#E5E8EC',
+  300: '#D7DCE1',
+  400: '#BFC7CF',
+  500: '#AAB4BE',
+  600: '#7F8E9D',
+  700: '#46505A', // contrast 8.21:1
+  800: '#2F3A45', // contrast 11.58:1
+  900: '#20262D',
+};
+
+const Root = styled('div')`
+  table {
+    font-family: IBM Plex Sans, sans-serif;
+    font-size: 0.875rem;
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  td,
+  th {
+    border: 1px solid ${grey[100]};
+    text-align: left;
+    padding: 5px;
+    &:hover {
+      background-color: ${grey[50]};
+    }
+  }
+
+  th {
+    background-color: ${grey[100]};
+  }
+`;
+const CustomTablePagination = styled(TablePaginationUnstyled)`
+  & .MuiTablePaginationUnstyled-toolbar {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+  }
+`;
+
+export default function UnstyledTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <Root>
+      <table style={{ minWidth: 500 }} aria-label="custom pagination table">
+        <thead>
+          <tr>
+            <th>Dessert</th>
+            <th>Calories</th>
+            <th>Fat</th>
+          </tr>
+        </thead>
+        <tbody>
+          {(rowsPerPage > 0
+            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row) => (
+            <tr key={row.name}>
+              <td>{row.name}</td>
+              <td style={{ width: 160 }} align="right">
+                {row.calories}
+              </td>
+              <td style={{ width: 160 }} align="right">
+                {row.fat}
+              </td>
+            </tr>
+          ))}
+
+          {emptyRows > 0 && (
+            <tr style={{ height: 41 * emptyRows }}>
+              <td colSpan={3} />
+            </tr>
+          )}
+        </tbody>
+        <tfoot>
+          <tr>
+            <CustomTablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              componentsProps={{
+                select: {
+                  'aria-label': 'rows per page',
+                },
+                actions: {
+                  showFirstButton: true,
+                  showLastButton: true,
+                },
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </tr>
+        </tfoot>
+      </table>
+    </Root>
+  );
+}
