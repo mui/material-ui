c86a3d7a5a16bc6deb3ef976cf85901267195bd9
ShuPink <60080324+ShuPink@users.noreply.github.com>
2021-12-15 13:07:41 +0800

    added cssmodule injection order correction comments to nextjs examples

--- examples/nextjs/src/createEmotionCache.js
+++ examples/nextjs/src/createEmotionCache.js
@@ -1,5 +1,8 @@
 import createCache from '@emotion/cache';
 
 export default function createEmotionCache() {
+  // If using css-modules, to correct the injection order
+  // add prepend: true to the createCache options
+  // https://mui.com/guides/interoperability/#css-injection-order-3
   return createCache({ key: 'css' });
 }
