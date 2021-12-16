a9a4982745cbdb8fab982524303485b17a0f5b55
ShuPink <60080324+ShuPink@users.noreply.github.com>
2021-12-16 09:38:42 +0800

    Make prepend default in nextjs examples

--- examples/nextjs/src/createEmotionCache.js
+++ examples/nextjs/src/createEmotionCache.js
@@ -1,8 +1,5 @@
 import createCache from '@emotion/cache';
 
 export default function createEmotionCache() {
-  // If using css-modules, to correct the injection order
-  // add prepend: true to the createCache options
-  // https://mui.com/guides/interoperability/#css-injection-order-3
-  return createCache({ key: 'css' });
+  return createCache({ key: 'css', prepend: true });
 }
