e0e80ed09b5030c3ee62f81dbfbd00227ae74b5e
siriwatknp <siriwatkunaporn@gmail.com>
2021-12-24 15:53:04 +0700

    point translation to new urls

--- docs/src/modules/utils/replaceMarkdownLinks.test.ts
+++ docs/src/modules/utils/replaceMarkdownLinks.test.ts
@@ -1,10 +1,51 @@
 import { expect } from 'chai';
 import replaceMarkdownLinks, {
+  replaceMaterialLinks,
   replaceAPILinks,
   replaceComponentLinks,
 } from './replaceMarkdownLinks';
 
 describe('replaceMarkdownLinks', () => {
+  it('replace material related links', () => {
+    expect(
+      replaceMaterialLinks(`
+      <ul>
+      <a href="/guides/minimizing-bundle-size/">reading this guide on minimizing bundle size</a>
+      <a href="/customization/theme-components/#default-props">default props</a>
+      <a data-no-markdown-link="true" tabindex="0" href="/getting-started/usage/">Get started</a>
+      <li><a href="/discover-more/related-projects/">Tree view</a></li>
+      </ul>
+    `),
+    ).to.equal(`
+      <ul>
+      <a href="/material/guides/minimizing-bundle-size/">reading this guide on minimizing bundle size</a>
+      <a href="/material/customization/theme-components/#default-props">default props</a>
+      <a data-no-markdown-link="true" tabindex="0" href="/material/getting-started/usage/">Get started</a>
+      <li><a href="/material/discover-more/related-projects/">Tree view</a></li>
+      </ul>
+    `);
+  });
+
+  it('should not change if links have been updated', () => {
+    expect(
+      replaceMaterialLinks(`
+      <ul>
+      <a href="/material/guides/minimizing-bundle-size/">reading this guide on minimizing bundle size</a>
+      <a href="/material/customization/theme-components/#default-props">default props</a>
+      <a data-no-markdown-link="true" tabindex="0" href="/material/getting-started/usage/">Get started</a>
+      <li><a href="/material/discover-more/related-projects/">Tree view</a></li>
+      </ul>
+    `),
+    ).to.equal(`
+      <ul>
+      <a href="/material/guides/minimizing-bundle-size/">reading this guide on minimizing bundle size</a>
+      <a href="/material/customization/theme-components/#default-props">default props</a>
+      <a data-no-markdown-link="true" tabindex="0" href="/material/getting-started/usage/">Get started</a>
+      <li><a href="/material/discover-more/related-projects/">Tree view</a></li>
+      </ul>
+    `);
+  });
+
   it('replace correct component links', () => {
     expect(
       replaceComponentLinks(`
@@ -115,6 +156,10 @@ describe('replaceMarkdownLinks', () => {
     <li><a href="/api/data-grid/data-grid-pro/">DataGridPro</a></li>
     <li><a href="/styles/api/">Styles</a></li>
     <li><a href="/system/basics/">System</a></li>
+    <a href="/guides/minimizing-bundle-size/">reading this guide on minimizing bundle size</a>
+    <a href="/customization/theme-components/#default-props">default props</a>
+    <a data-no-markdown-link="true" tabindex="0" href="/getting-started/usage/">Get started</a>
+    <li><a href="/discover-more/related-projects/">Tree view</a></li>
     </ul>
     `,
         '/material/react-buttons',
@@ -134,8 +179,29 @@ describe('replaceMarkdownLinks', () => {
     <li><a href="/x/api/mui-data-grid/data-grid-pro/">DataGridPro</a></li>
     <li><a href="/styles/api/">Styles</a></li>
     <li><a href="/system/basics/">System</a></li>
+    <a href="/material/guides/minimizing-bundle-size/">reading this guide on minimizing bundle size</a>
+    <a href="/material/customization/theme-components/#default-props">default props</a>
+    <a data-no-markdown-link="true" tabindex="0" href="/material/getting-started/usage/">Get started</a>
+    <li><a href="/material/discover-more/related-projects/">Tree view</a></li>
     </ul>
     `);
+  });
+
+  it('should work with json', () => {
+    const json = {
+      importDifference:
+        'You can learn about the difference by <a href="/guides/minimizing-bundle-size/">reading this guide on minimizing bundle size</a>.',
+    };
+    expect(replaceMarkdownLinks(JSON.stringify(json), '/material/api/')).to.equal(
+      '{"importDifference":"You can learn about the difference by <a href=\\"/material/guides/minimizing-bundle-size/\\">reading this guide on minimizing bundle size</a>."}',
+    );
+    const json2 = {
+      styleOverrides:
+        'The name <code>{{componentStyles.name}}</code> can be used when providing <a href="/customization/theme-components/#default-props">default props</a> or <a href="/customization/theme-components/#global-style-overrides">style overrides</a> in the theme.',
+    };
+    expect(replaceMarkdownLinks(JSON.stringify(json2), '/material/api/')).to.equal(
+      '{"styleOverrides":"The name <code>{{componentStyles.name}}</code> can be used when providing <a href=\\"/material/customization/theme-components/#default-props\\">default props</a> or <a href=\\"/material/customization/theme-components/#global-style-overrides\\">style overrides</a> in the theme."}',
+    );
   });
 
   it('does nothing for old routes', () => {
@@ -147,6 +213,10 @@ describe('replaceMarkdownLinks', () => {
       <li><a href="/components/buttons/">Buttons</a></li>
       <li><a href="/components/tree-view/">Tree view</a></li>
       <li><a href="/components/data-grid/demo/">Demo</a></li>
+      <a href="/guides/minimizing-bundle-size/">reading this guide on minimizing bundle size</a>
+      <a href="/customization/theme-components/#default-props">default props</a>
+      <a data-no-markdown-link="true" tabindex="0" href="/getting-started/usage/">Get started</a>
+      <li><a href="/discover-more/related-projects/">Tree view</a></li>
       </ul>
     `,
         '/components/buttons/',
@@ -157,6 +227,10 @@ describe('replaceMarkdownLinks', () => {
       <li><a href="/components/buttons/">Buttons</a></li>
       <li><a href="/components/tree-view/">Tree view</a></li>
       <li><a href="/components/data-grid/demo/">Demo</a></li>
+      <a href="/guides/minimizing-bundle-size/">reading this guide on minimizing bundle size</a>
+      <a href="/customization/theme-components/#default-props">default props</a>
+      <a data-no-markdown-link="true" tabindex="0" href="/getting-started/usage/">Get started</a>
+      <li><a href="/discover-more/related-projects/">Tree view</a></li>
       </ul>
     `);
   });
