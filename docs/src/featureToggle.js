20e56b91a24405f094c15729f7dd0959a5d3b977
siriwatknp <siriwatkunaporn@gmail.com>
2021-12-25 17:19:51 +0700

    update feature toggle to commonjs

--- docs/src/featureToggle.js
+++ docs/src/featureToggle.js
@@ -1,8 +1,6 @@
-const FEATURE_TOGGLE = {
+module.exports = {
   nav_products: true,
   enable_product_scope: false,
   enable_website_banner: false,
   enable_blog_index: process.env.NODE_ENV !== 'production' || process.env.PULL_REQUEST === 'true',
 };
-
-export default FEATURE_TOGGLE;
