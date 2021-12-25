ff0cd0f760a1d550fdeb6a01267ab3b11bc442a1
Noam Honig <noam.honig@gmail.com>
2021-12-25 09:23:30 +0200

    fixed the tsx and ran yarn prettier yarn docs:typescript:formatted

--- docs/src/pages/guides/right-to-left/Direction.js
+++ docs/src/pages/guides/right-to-left/Direction.js
@@ -1,12 +1,12 @@
-import * as React from "react";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import TextField from "@mui/material/TextField";
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
 import rtlPlugin from 'stylis-plugin-rtl';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
 
 const theme = createTheme({
-  direction: "rtl" // Both here and <body dir="rtl">
+  direction: 'rtl', // Both here and <body dir="rtl">
 });
 // Create rtl cache
 const cacheRtl = createCache({
