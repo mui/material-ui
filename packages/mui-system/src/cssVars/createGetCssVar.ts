7153702c1cde1bdfaa4565e34e1c1cc2e08c6479
siriwatknp <siriwatkunaporn@gmail.com>
2022-01-07 14:55:50 +0700

    add comment

--- packages/mui-system/src/cssVars/createGetCssVar.ts
+++ packages/mui-system/src/cssVars/createGetCssVar.ts
@@ -1,3 +1,7 @@
+/**
+ * The benefit of this function is to help developers get CSS var from theme without specifying the whole variable
+ * and they does not need to remember the prefix (defined once).
+ */
 export default function createGetCssVar<T extends string = string>(prefix: string = '') {
   function appendVar(...vars: string[]): string {
     if (!vars.length) {
@@ -6,6 +10,7 @@ export default function createGetCssVar<T extends string = string>(prefix: strin
     return `, var(--${prefix ? `${prefix}-` : ''}${vars[0]}${appendVar(...vars.slice(1))})`;
   }
 
+  // AdditionalVars makes `getCssVar` less strict, so it can be use like this `getCssVar('non-mui-variable')` without type error.
   const getCssVar = <AdditionalVars extends string = never>(
     field: T | AdditionalVars,
     ...vars: (T | AdditionalVars)[]
