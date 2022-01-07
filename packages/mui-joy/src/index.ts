e81f306e58df74c983e15718eb3f4bdf51896054
Siriwat K <siriwatkunaporn@gmail.com>
2022-01-07 16:49:12 +0700

    [Joy] Add `Typography` component (#30489)

--- packages/mui-joy/src/index.ts
+++ packages/mui-joy/src/index.ts
@@ -4,5 +4,8 @@ export * from './styles';
 export { default as Button } from './Button';
 export * from './Button';
 
+export { default as Typography } from './Typography';
+export * from './Typography';
+
 export { default as Switch } from './Switch';
 export * from './Switch';
