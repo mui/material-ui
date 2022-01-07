6d52177403d526db96e748c73d0f87168677ffbc
siriwatknp <siriwatkunaporn@gmail.com>
2022-01-07 14:36:48 +0700

    rename to getCssVar

--- packages/mui-system/src/cssVars/createGetCssVar.ts
+++ packages/mui-system/src/cssVars/createGetCssVar.ts
@@ -1,4 +1,4 @@
-export default function createGetThemeVar<T extends string = string>(prefix: string = '') {
+export default function createGetCssVar<T extends string = string>(prefix: string = '') {
   function appendVar(...vars: string[]): string {
     if (!vars.length) {
       return '';
@@ -6,8 +6,8 @@ export default function createGetThemeVar<T extends string = string>(prefix: str
     return `, var(--${prefix ? `${prefix}-` : ''}${vars[0]}${appendVar(...vars.slice(1))})`;
   }
 
-  const getThemeVar = (field: T, ...vars: T[]) => {
+  const getCssVar = (field: T, ...vars: T[]) => {
     return `var(--${prefix ? `${prefix}-` : ''}${field}${appendVar(...vars)})`;
   };
-  return getThemeVar;
+  return getCssVar;
 }
