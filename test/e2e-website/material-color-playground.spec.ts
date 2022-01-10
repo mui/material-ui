7e248643d8d3c2231511de02728fb4b283baa179
siriwatknp <siriwatkunaporn@gmail.com>
2022-01-10 11:34:25 +0700

    add comment

--- test/e2e-website/material-color-playground.spec.ts
+++ test/e2e-website/material-color-playground.spec.ts
@@ -6,10 +6,10 @@ const test = base.extend<TestFixture>({});
 test('should be able to change color without crash', async ({ page }) => {
   await page.goto('/customization/color/#playground', { waitUntil: 'networkidle' });
 
-  await page.fill('#primary', '');
+  await page.fill('#primary', ''); // clear the input
   await page.type('#primary', '#e91e63');
 
-  await page.fill('#secondary', '');
+  await page.fill('#secondary', ''); // clear the input
   await page.type('#secondary', '#ffc400');
 
   await page.click('button:has-text("Set Docs Colors")');
