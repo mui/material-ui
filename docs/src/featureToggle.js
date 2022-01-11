468228ee14d5175da853faa6cfd3488d17053791
siriwatknp <siriwatkunaporn@gmail.com>
2022-01-11 15:02:03 +0700

    cleanup old files

--- docs/src/featureToggle.js
+++ docs/src/featureToggle.js
@@ -5,5 +5,5 @@ module.exports = {
   // TODO: cleanup once migration is done
   enable_product_scope: true, // related to new structure change
   enable_redirects: false, // related to new structure change
-  enable_mui_base_scope: false,
+  enable_mui_base_scope: false, // will be enabled after the migration
 };
