c1e3f62a9ed6d3a26a6a7f8d3e99ac4f8cbdb187
Siriwat K <siriwatkunaporn@gmail.com>
2022-01-11 17:08:24 +0700

    [docs] Add products identifier and drawer (#30283)

--- docs/src/featureToggle.js
+++ docs/src/featureToggle.js
@@ -5,4 +5,5 @@ module.exports = {
   // TODO: cleanup once migration is done
   enable_product_scope: false, // related to new structure change
   enable_redirects: false, // related to new structure change
+  enable_mui_base_scope: false, // will be enabled after the migration
 };
