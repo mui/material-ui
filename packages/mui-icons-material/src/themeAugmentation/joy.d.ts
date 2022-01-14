65fdc7b30ab5e77b353daa39bacb6f10b6fce49e
siriwatknp <siriwatkunaporn@gmail.com>
2022-01-14 16:17:54 +0700

    add themeAugmentation

---
+++ packages/mui-icons-material/src/themeAugmentation/joy.d.ts
@@ -0,0 +1,30 @@
+declare module '@mui/icons-material/utils/SvgIcon' {
+  import { JoyTheme } from '@mui/joy/styles';
+
+  interface SvgIconThemeOverrides extends JoyTheme {}
+
+  interface SvgIconPropsSizeOverrides {
+    small: false;
+    medium: false;
+    large: false;
+    xs: true;
+    sm: true;
+    md: true;
+    lg: true;
+    xl: true;
+    xl2: true;
+    xl3: true;
+    xl4: true;
+    xl5: true;
+    xl6: true;
+  }
+
+  interface SvgIconPropsColorOverrides {
+    secondary: false;
+    action: false;
+    disabled: false;
+  }
+}
+
+// disable automatic export
+export {};
---
+++ packages/mui-icons-material/src/themeAugmentation/material.d.ts
@@ -0,0 +1,8 @@
+declare module '@mui/icons-material/utils/SvgIcon' {
+  import { Theme } from '@mui/material/styles';
+
+  interface SvgIconThemeOverrides extends Theme {}
+}
+
+// disable automatic export
+export {};
