d8bf23226893117c058c5139d54978c9ed8348e5
siriwatknp <siriwatkunaporn@gmail.com>
2022-01-16 12:57:38 +0700

    support styleOverridesCallback for Joy

--- packages/mui-joy/src/styles/components.d.ts
+++ packages/mui-joy/src/styles/components.d.ts
@@ -4,19 +4,45 @@ import { ButtonProps } from '../Button/ButtonProps';
 import { ButtonClassKey } from '../Button/buttonClasses';
 import { SwitchProps } from '../Switch/SwitchProps';
 import { SwitchClassKey } from '../Switch/switchClasses';
+import { TypographyProps } from '../Typography/TypographyProps';
+import { TypographyClassKey } from '../Typography/typographyClasses';
+import { SvgIconProps } from '../SvgIcon/SvgIconProps';
+import { SvgIconClassKey } from '../SvgIcon/svgIconClasses';
 
-export type OverridesStyleRules<ClassKey extends string = string> = Record<
-  ClassKey,
-  CSSInterpolation
+export type OverridesStyleRules<
+  ClassKey extends string = string,
+  ComponentProps = Record<string, unknown>,
+  Theme = unknown,
+> = Partial<
+  Record<
+    Exclude<ClassKey, GlobalStateSlot>,
+    | CSSInterpolation
+    | ((
+        // Record<string, unknown> is for other props that the slot receive internally
+        // Documenting all ownerStates could be a huge work, let's wait until we have a real needs from developers.
+        props: {
+          ownerState: ComponentProps & Record<string, unknown>;
+          theme: Theme;
+        } & Record<string, unknown>,
+      ) => CSSInterpolation)
+  >
 >;
 
-export interface Components {
+export interface Components<Theme = unknown> {
   MuiButton?: {
     defaultProps?: Partial<ButtonProps>;
-    styleOverrides?: Partial<OverridesStyleRules<Exclude<ButtonClassKey, GlobalStateSlot>>>;
+    styleOverrides?: OverridesStyleRules<ButtonClassKey, ButtonProps, Theme>;
   };
   MuiSwitch?: {
     defaultProps?: Partial<SwitchProps>;
-    styleOverrides?: Partial<OverridesStyleRules<Exclude<SwitchClassKey, GlobalStateSlot>>>;
+    styleOverrides?: OverridesStyleRules<SwitchClassKey, SwitchProps, Theme>;
+  };
+  MuiTypography?: {
+    defaultProps?: Partial<TypographyProps>;
+    styleOverrides?: OverridesStyleRules<TypographyClassKey, TypographyProps, Theme>;
+  };
+  MuiSvgIcon?: {
+    defaultProps?: Partial<SvgIconProps>;
+    styleOverrides?: OverridesStyleRules<SvgIconClassKey, SvgIconProps, Theme>;
   };
 }
