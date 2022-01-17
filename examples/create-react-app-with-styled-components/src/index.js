d20480e03636f2d9b27b331f91fc2b7a70fb3f0c
gitdittay <dt@bspowk.com>
2022-01-17 10:32:57 +0000

    unreg serv worker

--- examples/create-react-app-with-styled-components/src/index.js
+++ examples/create-react-app-with-styled-components/src/index.js
@@ -17,4 +17,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
