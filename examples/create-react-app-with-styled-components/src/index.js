845e74794d12c55c5a4c379d2fdcc7e912d918af
gitdittay <dt@bspowk.com>
2022-01-17 10:30:17 +0000

    first staged setting, manifest, index commits

--- examples/create-react-app-with-styled-components/src/index.js
+++ examples/create-react-app-with-styled-components/src/index.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
-import CssBaseline from '@mui/material/CssBaseline';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <React.Fragment>
@@ -16,4 +17,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
