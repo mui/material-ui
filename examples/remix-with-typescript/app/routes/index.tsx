be3d49e835c597d280494fe41bf0e34a8beab8d3
Marija Najdova <mnajdova@gmail.com>
2022-01-20 11:27:55 +0100

    Revert changes for broken link

--- examples/remix-with-typescript/app/routes/index.tsx
+++ examples/remix-with-typescript/app/routes/index.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import type { MetaFunction } from 'remix';
 import { Link } from 'remix';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
 
 // https://remix.run/api/conventions#meta
 export const meta: MetaFunction = () => {
@@ -16,16 +14,13 @@ export const meta: MetaFunction = () => {
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
   return (
-    <Stack>
+    <React.Fragment>
       <Typography variant="h4" component="h1" gutterBottom>
         Remix with TypeScript example
       </Typography>
       <Link to="/about" color="secondary">
         Go to the about page
       </Link>
-      <Button variant="contained" component={Link} to="/404">
-        Go to the 404 page (bug reproduce)
-      </Button>
-    </Stack>
+    </React.Fragment>
   );
 }
