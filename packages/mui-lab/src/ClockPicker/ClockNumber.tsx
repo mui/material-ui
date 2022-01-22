db94898e084158c5aa55a93c38f2b37a1f33bd8a
Ali Sasani <sasani@vistamehr.net>
2022-01-23 01:10:40 +0330

    add font for clock numbers

--- packages/mui-lab/src/ClockPicker/ClockNumber.tsx
+++ packages/mui-lab/src/ClockPicker/ClockNumber.tsx
@@ -30,6 +30,7 @@ const ClockNumberRoot = styled('span', { skipSx: true })<{ ownerState: ClockNumb
     alignItems: 'center',
     borderRadius: '50%',
     color: theme.palette.text.primary,
+    fontFamily: theme.typography.fontFamily,
     '&:focused': {
       backgroundColor: theme.palette.background.paper,
     },
