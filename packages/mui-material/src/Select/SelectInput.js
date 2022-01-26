8cf69dee9b10ed468b10a5f725785de210c963db
Jack Works <jackworks@protonmail.com>
2022-01-26 16:26:45 +0800

    [TextField] Remove usage of dangerouslySetInnerHTML (#30776)

--- packages/mui-material/src/Select/SelectInput.js
+++ packages/mui-material/src/Select/SelectInput.js
@@ -499,8 +499,7 @@ const SelectInput = React.forwardRef(function SelectInput(props, ref) {
         {/* So the vertical align positioning algorithm kicks in. */}
         {isEmpty(display) ? (
           // notranslate needed while Google Translate will not fix zero-width space issue
-          // eslint-disable-next-line react/no-danger
-          <span className="notranslate" dangerouslySetInnerHTML={{ __html: '&#8203;' }} />
+          <span className="notranslate">&#8203;</span>
         ) : (
           display
         )}
