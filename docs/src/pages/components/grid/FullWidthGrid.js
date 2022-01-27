3e515fb5684cadc81c21a3f932e9d0b62fdfa0bd
Shubham Chandra Mishra <84576053+scmishra-cse@users.noreply.github.com>
2022-01-27 15:41:37 +0530

    Update FullWidthGrid.js
    In line 11 the comma at the end of line is throwing an error.

--- docs/src/pages/components/grid/FullWidthGrid.js
+++ docs/src/pages/components/grid/FullWidthGrid.js
@@ -8,7 +8,7 @@ const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: 'center',
-  color: theme.palette.text.secondary,
+  color: theme.palette.text.secondary
 }));
 
 export default function FullWidthGrid() {
