7e6ea1d84aa28f7380983b4af6aa94c9d3af3f79
siriwatknp <siriwatkunaporn@gmail.com>
2022-01-28 17:57:37 +0700

    fix variant type to be augmentable

--- packages/mui-joy/src/styles/types/variants.ts
+++ packages/mui-joy/src/styles/types/variants.ts
@@ -19,12 +19,48 @@ export type ContextualOverrideKeys = OverridableStringUnion<
 
 type State = 'Hover' | 'Active' | 'Disabled';
 
-type VariantKey = VariantProp | 'containedOverrides' | `${VariantProp}${State}`;
-
 export type DefaultVariantKey = DefaultVariantProp | `${DefaultVariantProp}${State}`;
 
-export type Variants = {
-  [k in VariantKey]: Record<ColorPaletteProp, CSSObject>;
-} & {
-  [k in ContextualOverrideKeys]: Record<Exclude<ColorPaletteProp, 'context'>, CSSObject>;
-};
+type BaseContextOverrides = Record<Exclude<ColorPaletteProp, 'context'>, CSSObject>;
+
+export interface VariantText extends Record<ColorPaletteProp, CSSObject> {}
+export interface VariantTextHover extends Record<ColorPaletteProp, CSSObject> {}
+export interface VariantTextActive extends Record<ColorPaletteProp, CSSObject> {}
+export interface VariantTextDisabled extends Record<ColorPaletteProp, CSSObject> {}
+
+export interface VariantOutlined extends Record<ColorPaletteProp, CSSObject> {}
+export interface VariantOutlinedHover extends Record<ColorPaletteProp, CSSObject> {}
+export interface VariantOutlinedActive extends Record<ColorPaletteProp, CSSObject> {}
+export interface VariantOutlinedDisabled extends Record<ColorPaletteProp, CSSObject> {}
+
+export interface VariantLight extends Record<ColorPaletteProp, CSSObject> {}
+export interface VariantLightHover extends Record<ColorPaletteProp, CSSObject> {}
+export interface VariantLightActive extends Record<ColorPaletteProp, CSSObject> {}
+export interface VariantLightDisabled extends Record<ColorPaletteProp, CSSObject> {}
+
+export interface VariantContained extends Record<ColorPaletteProp, CSSObject> {}
+export interface VariantContainedHover extends Record<ColorPaletteProp, CSSObject> {}
+export interface VariantContainedActive extends Record<ColorPaletteProp, CSSObject> {}
+export interface VariantContainedDisabled extends Record<ColorPaletteProp, CSSObject> {}
+
+export interface Variants extends BaseContextOverrides {
+  text: VariantText;
+  textHover: VariantTextHover;
+  textActive: VariantTextActive;
+  textDisabled: VariantTextDisabled;
+
+  outlined: VariantOutlined;
+  outlinedHover: VariantOutlinedHover;
+  outlinedActive: VariantOutlinedActive;
+  outlinedDisabled: VariantOutlinedDisabled;
+
+  light: VariantLight;
+  lightHover: VariantLightHover;
+  lightActive: VariantLightActive;
+  lightDisabled: VariantLightDisabled;
+
+  contained: VariantContained;
+  containedHover: VariantContainedHover;
+  containedActive: VariantContainedActive;
+  containedDisabled: VariantContainedDisabled;
+}
