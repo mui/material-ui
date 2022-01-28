81e1dc7ba438ad4f9cd6a4d5e2d34d016f78f98d
Michał Dudak <michal@dudak.me>
2022-01-28 13:43:21 +0100

    [SelectUnstyled] Create unstyled select (+ hook) (#30113)
    Co-authored-by: Siriwat K <siriwatkunaporn@gmail.com>

---
+++ docs/src/pages/components/selects/UnstyledSelectObjectValues.js
@@ -0,0 +1,140 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import SelectUnstyled, { selectUnstyledClasses } from '@mui/base/SelectUnstyled';
+import OptionUnstyled, { optionUnstyledClasses } from '@mui/base/OptionUnstyled';
+import PopperUnstyled from '@mui/base/PopperUnstyled';
+import { styled } from '@mui/system';
+
+const StyledButton = styled('button')`
+  font-family: IBM Plex Sans, sans-serif;
+  font-size: 0.875rem;
+  box-sizing: border-box;
+  min-height: calc(1.5em + 22px);
+  min-width: 200px;
+  background: #fff;
+  border: 1px solid #ccc;
+  border-radius: 0.75em;
+  margin: 0.5em;
+  padding: 10px;
+  text-align: left;
+  line-height: 1.5;
+  color: #000;
+
+  &.${selectUnstyledClasses.focusVisible} {
+    outline: 4px solid rgba(100, 100, 100, 0.3);
+  }
+
+  &.${selectUnstyledClasses.expanded} {
+    border-radius: 0.75em 0.75em 0 0;
+
+    &::after {
+      content: '▴';
+    }
+  }
+
+  &::after {
+    content: '▾';
+    float: right;
+  }
+`;
+
+const StyledListbox = styled('ul')`
+  font-family: IBM Plex Sans, sans-serif;
+  font-size: 0.875rem;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  background-color: #fff;
+  min-width: 200px;
+  border: 1px solid #ccc;
+  border-top: none;
+  color: #000;
+`;
+
+const StyledOption = styled(OptionUnstyled)`
+  list-style: none;
+  padding: 4px 10px;
+  margin: 0;
+  border-bottom: 1px solid #ddd;
+  cursor: default;
+
+  &:last-of-type {
+    border-bottom: none;
+  }
+
+  &.${optionUnstyledClasses.disabled} {
+    color: #888;
+  }
+
+  &.${optionUnstyledClasses.selected} {
+    background-color: rgba(25, 118, 210, 0.08);
+  }
+
+  &.${optionUnstyledClasses.highlighted} {
+    background-color: #16d;
+    color: #fff;
+  }
+
+  &.${optionUnstyledClasses.highlighted}.${optionUnstyledClasses.selected} {
+    background-color: #05e;
+    color: #fff;
+  }
+
+  &:hover:not(.${optionUnstyledClasses.disabled}) {
+    background-color: #39e;
+  }
+`;
+
+const StyledPopper = styled(PopperUnstyled)`
+  z-index: 1;
+`;
+
+function CustomSelect(props) {
+  const components = {
+    Root: StyledButton,
+    Listbox: StyledListbox,
+    Popper: StyledPopper,
+    ...props.components,
+  };
+
+  return <SelectUnstyled {...props} components={components} />;
+}
+
+CustomSelect.propTypes = {
+  /**
+   * The components used for each slot inside the Select.
+   * Either a string to use a HTML element or a component.
+   * @default {}
+   */
+  components: PropTypes.shape({
+    Listbox: PropTypes.elementType,
+    Popper: PropTypes.elementType,
+    Root: PropTypes.elementType,
+  }),
+};
+
+const characters = [
+  { name: 'Frodo', race: 'Hobbit' },
+  { name: 'Sam', race: 'Hobbit' },
+  { name: 'Merry', race: 'Hobbit' },
+  { name: 'Gandalf', race: 'Maia' },
+  { name: 'Gimli', race: 'Dwarf' },
+];
+
+export default function UnstyledSelectObjectValues() {
+  const [character, setCharacter] = React.useState(characters[0]);
+  return (
+    <div>
+      <CustomSelect value={character} onChange={setCharacter}>
+        {characters.map((c) => (
+          <StyledOption key={c.name} value={c}>
+            {c.name}
+          </StyledOption>
+        ))}
+      </CustomSelect>
+
+      <p>Selected character:</p>
+      <pre>{JSON.stringify(character, null, 2)}</pre>
+    </div>
+  );
+}
