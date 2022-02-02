db1d0d7e9c9c3c212a9cb8919645171abbcc8063
danilo leal <daniloleal09@gmail.com>
2022-02-02 13:18:57 -0300

    tweaks to the switch and input - initiating the select

--- docs/src/pages/components/selects/UnstyledSelectSimple.js
+++ docs/src/pages/components/selects/UnstyledSelectSimple.js
@@ -4,23 +4,56 @@ import OptionUnstyled, { optionUnstyledClasses } from '@mui/base/OptionUnstyled'
 import PopperUnstyled from '@mui/base/PopperUnstyled';
 import { styled } from '@mui/system';
 
-const StyledButton = styled('button')`
+const blue = {
+  50: '#F0F7FF',
+  100: '#C2E0FF',
+  200: '#99CCF3',
+  300: '#66B2FF',
+  400: '#3399FF',
+  500: '#007FFF',
+  600: '#0072E5',
+  700: '#0059B2',
+  800: '#004C99',
+  900: '#003A75',
+};
+
+const grey = {
+  50: '#F3F6F9',
+  100: '#E7EBF0',
+  200: '#E0E3E7',
+  300: '#CDD2D7',
+  400: '#B2BAC2',
+  500: '#A0AAB4',
+  600: '#6F7E8C',
+  700: '#3E5060',
+  800: '#2D3843',
+  900: '#1A2027',
+};
+
+const StyledButton = styled('button')(
+  ({ theme }) => `
   font-family: IBM Plex Sans, sans-serif;
   font-size: 0.875rem;
   box-sizing: border-box;
   min-height: calc(1.5em + 22px);
   min-width: 200px;
-  background: #fff;
-  border: 1px solid #ccc;
+  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[300]};
   border-radius: 0.75em;
   margin: 0.5em;
   padding: 10px;
   text-align: left;
   line-height: 1.5;
-  color: #000;
+  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+
+  &:hover {
+    background: ${theme.palette.mode === 'dark' ? '' : grey[100]};
+    border-color: ${theme.palette.mode === 'dark' ? grey[700] : grey[400]};
+  }
 
   &.${selectUnstyledClasses.focusVisible} {
-    outline: 4px solid rgba(100, 100, 100, 0.3);
+    outline: 2px solid ${theme.palette.mode === 'dark' ? blue[400] : blue[200]};
+    outline-offset: 2px;
   }
 
   &.${selectUnstyledClasses.expanded} {
@@ -35,54 +68,65 @@ const StyledButton = styled('button')`
     content: '▾';
     float: right;
   }
-`;
+  `,
+);
 
-const StyledListbox = styled('ul')`
+const StyledListbox = styled('ul')(
+  ({ theme }) => `
   font-family: IBM Plex Sans, sans-serif;
   font-size: 0.875rem;
   box-sizing: border-box;
   padding: 0;
   margin: 0;
-  background-color: #fff;
   min-width: 200px;
-  border: 1px solid #ccc;
+  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[300]};
+  border-radius: 0 0 0.75em 0.75em;
   border-top: none;
-  color: #000;
-`;
+  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  overflow: auto;
+  outline: 2px solid ${theme.palette.mode === 'dark' ? blue[400] : blue[200]};
+  outline-offset: 2px;
+  `,
+);
 
-const StyledOption = styled(OptionUnstyled)`
+const StyledOption = styled(OptionUnstyled)(
+  ({ theme }) => `
   list-style: none;
-  padding: 4px 10px;
+  padding: 5px 10px;
   margin: 0;
-  border-bottom: 1px solid #ddd;
+  border-bottom: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[300]};
   cursor: default;
 
   &:last-of-type {
     border-bottom: none;
   }
 
-  &.${optionUnstyledClasses.disabled} {
-    color: #888;
-  }
-
   &.${optionUnstyledClasses.selected} {
-    background-color: rgba(25, 118, 210, 0.08);
+    background-color: ${blue[500]};
+    color: #fff;
   }
 
   &.${optionUnstyledClasses.highlighted} {
-    background-color: #16d;
-    color: #fff;
+    background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
+    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
   }
 
   &.${optionUnstyledClasses.highlighted}.${optionUnstyledClasses.selected} {
-    background-color: #05e;
+    background-color: ${blue[600]};
     color: #fff;
   }
 
-  &:hover:not(.${optionUnstyledClasses.disabled}) {
-    background-color: #39e;
+  &.${optionUnstyledClasses.disabled} {
+    color: ${theme.palette.mode === 'dark' ? grey[700] : grey[400]};
   }
-`;
+
+  &:hover:not(.${optionUnstyledClasses.disabled}) {
+    background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
+    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  }
+  `,
+);
 
 const StyledPopper = styled(PopperUnstyled)`
   z-index: 1;
@@ -105,6 +149,9 @@ export default function UnstyledSelectSimple() {
       <StyledOption value={10}>Ten</StyledOption>
       <StyledOption value={20}>Twenty</StyledOption>
       <StyledOption value={30}>Thirty</StyledOption>
+      <StyledOption value={40} disabled>
+        Forty
+      </StyledOption>
     </CustomSelect>
   );
 }
