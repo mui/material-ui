65ad7e0c6fa9adfdd9e404957c44b725f4cde804
danilo leal <daniloleal09@gmail.com>
2022-02-03 11:44:48 -0300

    fix one demo broken on mobile

--- docs/src/pages/components/selects/UnstyledSelectRichOptions.js
+++ docs/src/pages/components/selects/UnstyledSelectRichOptions.js
@@ -30,7 +30,7 @@ const StyledButton = styled('button')(
   font-size: 0.875rem;
   box-sizing: border-box;
   min-height: calc(1.5em + 22px);
-  min-width: 480px;
+  min-width: 320px;
   background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
   border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[300]};
   border-radius: 0.75em;
@@ -74,7 +74,7 @@ const StyledListbox = styled('ul')(
   box-sizing: border-box;
   padding: 0;
   margin: 10px 0;
-  min-width: 480px;
+  min-width: 320px;
   max-height: 400px;
   background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
   border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[300]};
