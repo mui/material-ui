6a89eba2edbf3170d652741e5720289830fb3137
danilo leal <daniloleal09@gmail.com>
2022-02-08 15:41:15 -0300

    typescript formatted

--- docs/data/material/components/stack/DividerStack.tsx
+++ docs/data/material/components/stack/DividerStack.tsx
@@ -6,6 +6,7 @@ import { styled } from '@mui/material/styles';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary,
