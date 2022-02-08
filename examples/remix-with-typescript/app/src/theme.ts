f52a1b46516b9189f761a991ebb60f32302c71b7
nnecec <nnecec@outlook.com>
2022-02-08 23:00:26 +0800

    [examples] Fix import ThemeProvider from correct package in remix-with-typescript

--- examples/remix-with-typescript/app/src/theme.ts
+++ examples/remix-with-typescript/app/src/theme.ts
@@ -14,6 +14,13 @@ const theme = createTheme({
       main: red.A400,
     },
   },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        color: "secondary"
+      }
+    }
+  }
 });
 
 export default theme;
