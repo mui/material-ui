691becec90199fb2751681958e6520b58e45aa0c
danilo leal <daniloleal09@gmail.com>
2022-02-09 18:28:17 -0300

    change "demo with bg" background color

--- docs/src/modules/components/Demo.js
+++ docs/src/modules/components/Demo.js
@@ -112,9 +112,11 @@ const DemoRoot = styled('div', {
   }),
   /* Prepare the background to display an inner elevation. */
   ...(bg === true && {
-    padding: theme.spacing(3),
+    padding: theme.spacing(4),
     backgroundColor:
-      theme.palette.mode === 'dark' ? theme.palette.grey[900] : theme.palette.grey[100],
+      theme.palette.mode === 'dark'
+        ? alpha(theme.palette.grey[800], 0.15)
+        : theme.palette.grey[100],
   }),
   ...(hiddenToolbar && {
     paddingTop: theme.spacing(2),
