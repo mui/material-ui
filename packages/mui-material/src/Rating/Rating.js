a89a64505ef8b20b4ddaa2dfe20892ee6261a838
LukasGerm <lukas@lukasgermerott.de>
2022-02-12 12:19:41 +0100

    [Rating] Removed focusing bug

--- packages/mui-material/src/Rating/Rating.js
+++ packages/mui-material/src/Rating/Rating.js
@@ -452,7 +452,7 @@ const Rating = React.forwardRef(function Rating(inProps, ref) {
     const newFocus = parseFloat(event.target.value);
     setState((prev) => ({
       hover: prev.hover,
-      focus: newFocus,
+      focus: valueProp ? newFocus : prev.focus,
     }));
   };
 
