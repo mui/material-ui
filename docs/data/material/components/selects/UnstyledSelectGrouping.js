ef13f324df89a8deb4ad867eb5a00d956a988d3f
Michał Dudak <michal@dudak.me>
2022-02-17 11:05:38 +0100

    Proptypes

--- docs/data/material/components/selects/UnstyledSelectGrouping.js
+++ docs/data/material/components/selects/UnstyledSelectGrouping.js
@@ -166,7 +166,7 @@ CustomSelect.propTypes = {
    */
   components: PropTypes.shape({
     Listbox: PropTypes.elementType,
-    Popper: PropTypes.elementType,
+    Popper: PropTypes.func,
     Root: PropTypes.elementType,
   }),
 };
