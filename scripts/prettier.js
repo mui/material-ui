008e343ecb4df117e6710f9d240355ad6f78ea4a
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2022-02-21 10:24:37 +0100

    Add html, prisma

--- scripts/prettier.js
+++ scripts/prettier.js
@@ -48,7 +48,7 @@ function runPrettier(options) {
     });
 
   const files = glob
-    .sync('**/*.{js,jsx,md,tsx,ts,json,prisma}', {
+    .sync('**/*.{js,jsx,md,tsx,ts,json,html,prisma}', {
       cwd: workspaceRoot,
       gitignore: true,
       ignore: [
