2ee27b0e1011d7a36dae93a6ecc2ee9072ea6ebc
Siriwat K <siriwatkunaporn@gmail.com>
2022-02-21 14:36:22 +0700

    Apply suggestions from code review

--- test/regressions/fixtures/CssVarsProvider/CssVarsProviderJoy.js
+++ test/regressions/fixtures/CssVarsProvider/CssVarsProviderJoy.js
@@ -3,6 +3,7 @@ import { CssVarsProvider } from '@mui/joy/styles';
 import Box from '@mui/joy/Box';
 import Button from '@mui/joy/Button';
 
+// All buttons should look the same, otherwise, it means that multiple instances with different prefixes do not work.
 export default function VariantColorJoy() {
   return (
     <Box sx={{ display: 'flex', gap: 2 }}>
