dd12ab7adbd620b5d7a8b60d629f5d25ba2ce668
danilo leal <daniloleal09@gmail.com>
2022-03-02 09:58:35 -0300

    Olivier's review

--- docs/src/components/banner/TableOfContentsBanner.tsx
+++ docs/src/components/banner/TableOfContentsBanner.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Link from 'docs/src/modules/components/Link';
-// import ROUTES from 'docs/src/route';
 import FEATURE_TOGGLE from 'docs/src/featureToggle';
 import { alpha } from '@mui/material/styles';
 
 export default function TableOfContentsBanner() {
   return FEATURE_TOGGLE.enable_toc_banner ? (
     <Link
-      href="https://www.change.org/p/%D0%BE%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%B8%D1%82%D1%8C-%D0%B2%D0%BE%D0%B9%D0%BD%D1%83-%D1%81-%D1%83%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%BE%D0%B9-2ce0a2d7-b957-4e23-981a-c67a26e2b0b7?cs_tk=AtzoDie-d7wPH5wkJmIAAXicyyvNyQEABF8BvDWGKBrzpzmlPrhKFp7sRDI%3D" // Fix me!
+      href="https://ukraine.ua/news/stand-with-ukraine/" // Fix me!
       target="_blank"
       sx={(theme) => ({
         mb: 2,
@@ -40,7 +39,7 @@ export default function TableOfContentsBanner() {
       })}
     >
       <Typography component="span" variant="caption" fontWeight="500" color="text.secondary">
-        ✋&#160;&#160;MUI stands in solidarity with the Ukranian people against the Russian
+        🇺🇦&#160;&#160;MUI stands in solidarity with the Ukrainian people against the Russian
         invasion.
         <br />
       </Typography>
