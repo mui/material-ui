48d5501ab072961f16df782823d4b3a2dc1f7df5
siriwatknp <siriwatkunaporn@gmail.com>
2022-03-02 13:47:42 +0700

    replace any with {}

--- packages/mui-base/src/FormControlUnstyled/useFormControl.ts
+++ packages/mui-base/src/FormControlUnstyled/useFormControl.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import FormControlUnstyledContext, { FormControlUnstyledState } from './FormControlContext';
 
-export default function useFormControlUnstyled<ExtraContextProps = any>() {
+export default function useFormControlUnstyled<ExtraContextProps = {}>() {
   return React.useContext(FormControlUnstyledContext) as
     | (FormControlUnstyledState & ExtraContextProps)
     | undefined;
