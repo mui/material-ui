d40b521317446d760268564e0746dd8666e48d1e
siriwatknp <siriwatkunaporn@gmail.com>
2022-03-07 09:54:11 +0700

    base: add mockup button page

--- docs/data/base/pages.ts
+++ docs/data/base/pages.ts
@@ -7,6 +7,17 @@ const pages = [
     children: [{ pathname: '/base/getting-started/installation' }],
   },
   {
+    pathname: '/base/react-',
+    title: 'Components',
+    icon: 'ToggleOnIcon',
+    children: [
+      {
+        pathname: '/base/react-button-unstyled',
+        title: 'Button',
+      },
+    ],
+  },
+  {
     title: 'Component API',
     pathname: '/base/api',
     icon: 'CodeIcon',
