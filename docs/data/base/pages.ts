760c44f5ec4db9a100478732af0c8ebc28e7cffb
Michał Dudak <michal@dudak.me>
2022-03-07 15:49:58 +0100

    Move SelectUnstyled docs to the Base space

--- docs/data/base/pages.ts
+++ docs/data/base/pages.ts
@@ -14,7 +14,10 @@ const pages = [
       {
         pathname: '/base/components/inputs',
         subheader: 'inputs',
-        children: [{ pathname: '/base/react-button', title: 'Button' }],
+        children: [
+          { pathname: '/base/react-button', title: 'Button' },
+          { pathname: '/base/react-select', title: 'Select' },
+        ],
       },
       {
         pathname: '/base/components/navigation',
