f48c9a9a212f7ec54593cb414bd57d82b15f23fe
Siriwat K <siriwatkunaporn@gmail.com>
2022-03-10 09:50:38 +0700

    [Joy] Add `TextField` component (#31299)

--- packages/mui-joy/src/TextField/textFieldClasses.ts
+++ packages/mui-joy/src/TextField/textFieldClasses.ts
@@ -1,22 +1,14 @@
 import { generateUtilityClass, generateUtilityClasses } from '@mui/base';
 
-export interface InputClasses {
+export interface TextFieldClasses {
   /** Styles applied to the root element. */
   root: string;
-  /** Styles applied to the input element. */
-  input: string;
-  /** Styles applied to the root element if the component is a descendant of `FormControl`. */
-  formControl: string;
-  /** Styles applied to the root element if `startAdornment` is provided. */
-  adornedStart: string;
-  /** Styles applied to the root element if `endAdornment` is provided. */
-  adornedEnd: string;
-  /** Styles applied to the root element if the component is focused. */
-  focused: string;
   /** Styles applied to the root element if `disabled={true}`. */
   disabled: string;
   /** State class applied to the root element if `error={true}`. */
   error: string;
+  /** Styles applied to the root element if the component is focused. */
+  focused: string;
   /** Styles applied to the root element if `color="primary"`. */
   colorPrimary: string;
   /** Styles applied to the root element if `color="neutral"`. */
@@ -45,21 +37,17 @@ export interface InputClasses {
   fullWidth: string;
 }
 
-export type InputClassKey = keyof InputClasses;
+export type TextFieldClassKey = keyof TextFieldClasses;
 
-export function getInputUtilityClass(slot: string): string {
-  return generateUtilityClass('JoyInput', slot);
+export function getTextFieldUtilityClass(slot: string): string {
+  return generateUtilityClass('MuiTextField', slot);
 }
 
-const inputClasses: InputClasses = generateUtilityClasses('JoyInput', [
+const textFieldClasses: TextFieldClasses = generateUtilityClasses('MuiTextField', [
   'root',
-  'input',
-  'formControl',
-  'focused',
   'disabled',
   'error',
-  'adornedStart',
-  'adornedEnd',
+  'focused',
   'colorPrimary',
   'colorNeutral',
   'colorDanger',
@@ -75,4 +63,4 @@ const inputClasses: InputClasses = generateUtilityClasses('JoyInput', [
   'fullWidth',
 ]);
 
-export default inputClasses;
+export default textFieldClasses;
