9ba7e903e9f4316919ad7a1b1f0d5caf74f1ee67
Patrik Affentranger <patrik.affentranger@hbf.com.au>
2022-03-10 13:16:19 +0800

    [Stepper] Export useStepperContext

--- packages/mui-material/src/Stepper/index.js
+++ packages/mui-material/src/Stepper/index.js
@@ -2,3 +2,6 @@ export { default } from './Stepper';
 
 export { default as stepperClasses } from './stepperClasses';
 export * from './stepperClasses';
+
+export { default as StepperContext } from './StepperContext';
+export * from './StepperContext';
