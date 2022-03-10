cc0e053747c72d8b21c66a70a1abfaef9f8b5ec7
Michał Dudak <michal@dudak.me>
2022-03-10 13:01:14 +0100

    Reorder fields

--- packages/mui-material/src/styles/zIndex.js
+++ packages/mui-material/src/styles/zIndex.js
@@ -2,13 +2,13 @@
 // like global values in the browser.
 const zIndex = {
   mobileStepper: 1000,
+  fab: 1050,
   speedDial: 1050,
   appBar: 1100,
   drawer: 1200,
   modal: 1300,
   snackbar: 1400,
   tooltip: 1500,
-  fab: 1050,
 };
 
 export default zIndex;
