b64be8718cc029eee8c862081da8907a85d0fc47
theCuriousOne <theCuriousOne@users.noreply.github.com>
2022-03-10 13:56:30 +0100

    [l10n] Add Macedonian translation

--- packages/mui-material/src/locale/index.ts
+++ packages/mui-material/src/locale/index.ts
@@ -2113,6 +2113,85 @@ export const kzKZ: Localization = {
   },
 };
 
+// Macedonian - Македонски
+export const mkMK: Localization = {
+  components: {
+    MuiBreadcrumbs: {
+      defaultProps: {
+        expandText: 'Прикажи патека',
+      },
+    },
+    MuiTablePagination: {
+      defaultProps: {
+        getItemAriaLabel: (type) => {
+          if (type === 'first') {
+            return 'Оди на прва страница';
+          }
+          if (type === 'last') {
+            return 'Оди на последна страница';
+          }
+          if (type === 'next') {
+            return 'Оди на следна страница';
+          }
+          // if (type === 'previous') {
+          return 'Оди на предходна страница';
+        },
+        labelRowsPerPage: 'Редови по страница:',
+        labelDisplayedRows: ({ from, to, count }) =>
+          `${from}–${to} од ${count !== -1 ? count : `повеќе од ${to}`}`,
+      },
+    },
+    MuiRating: {
+      defaultProps: {
+        getLabelText: (value) => {
+          const lastDigit = value % 10;
+          const lastTwoDigits = value % 100;
+          if ([2, 3, 4].includes(lastDigit) && ![12, 13, 14].includes(lastTwoDigits)) {
+            return 'Ѕвезда';
+          }
+          return 'Ѕвезда';
+        },
+        emptyLabelText: 'Празно',
+      },
+    },
+    MuiAutocomplete: {
+      defaultProps: {
+        clearText: 'Избриши',
+        closeText: 'Затвори',
+        loadingText: 'Се презема',
+        noOptionsText: 'Нема опција',
+        openText: 'Отвори',
+      },
+    },
+    MuiAlert: {
+      defaultProps: {
+        closeText: 'Затвори',
+      },
+    },
+    MuiPagination: {
+      defaultProps: {
+        'aria-label': 'Навигација низ страници',
+        getItemAriaLabel: (type, page, selected) => {
+          if (type === 'page') {
+            return `${selected ? '' : 'Оди на '}страница ${page}`;
+          }
+          if (type === 'first') {
+            return 'Оди на прва страница';
+          }
+          if (type === 'last') {
+            return 'Оди на последна страница';
+          }
+          if (type === 'next') {
+            return 'Оди на следна страница';
+          }
+          // if (type === 'previous') {
+          return 'Оди на предходна страница';
+        },
+      },
+    },
+  },
+};
+
 export const nbNO: Localization = {
   components: {
     MuiBreadcrumbs: {
