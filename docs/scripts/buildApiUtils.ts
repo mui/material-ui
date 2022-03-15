8fb73407e4b70d744b8128211b876edb2a153f36
siriwatknp <siriwatkunaporn@gmail.com>
2022-03-15 16:21:09 +0700

    add PopperUnstyled to migrated components

--- docs/scripts/buildApiUtils.ts
+++ docs/scripts/buildApiUtils.ts
@@ -102,7 +102,12 @@ export type ComponentInfo = {
   skipApiGeneration?: boolean;
 };
 
-const migratedBaseComponents = ['ButtonUnstyled', 'MenuUnstyled', 'MenuItemUnstyled'];
+const migratedBaseComponents = [
+  'ButtonUnstyled',
+  'MenuUnstyled',
+  'MenuItemUnstyled',
+  'PopperUnstyled',
+];
 
 export const getGenericComponentInfo = (filename: string): ComponentInfo => {
   const { name } = extractPackageFile(filename);
