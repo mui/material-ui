4e4914f6798e445e74a38b57992881aee1d3154f
Paul Hachmang <paul@reachdigital.nl>
2022-03-16 11:46:13 +0100

    chore: `yarn proptypes` ran

--- packages/mui-joy/src/SvgIcon/SvgIcon.tsx
+++ packages/mui-joy/src/SvgIcon/SvgIcon.tsx
@@ -125,7 +125,6 @@ SvgIcon.propTypes /* remove-proptypes */ = {
     PropTypes.oneOf(['danger', 'info', 'inherit', 'neutral', 'primary', 'success', 'warning']),
     PropTypes.string,
   ]),
-
   /**
    * The component used for the root node.
    * Either a string to use a HTML element or a component.
