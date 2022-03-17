8f56327b69df3ed5b99a959cd449317beb1c5a4e
Michał Dudak <michal@dudak.me>
2022-03-17 09:28:41 +0100

    Add more demos

---
+++ docs/data/base/components/slider/DiscreteSliderMarks.tsx
@@ -0,0 +1,131 @@
+import * as React from 'react';
+import { styled, alpha, Box } from '@mui/system';
+import SliderUnstyled from '@mui/base/SliderUnstyled';
+
+const StyledSlider = styled(SliderUnstyled)(
+  ({ theme }) => `
+  color: ${theme.palette.mode === 'light' ? '#1976d2' : '#90caf9'};
+  height: 4px;
+  width: 100%;
+  padding: 13px 0;
+  display: inline-block;
+  position: relative;
+  cursor: pointer;
+  touch-action: none;
+  -webkit-tap-highlight-color: transparent;
+  opacity: 0.75;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  & .MuiSlider-rail {
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 4px;
+    border-radius: 2px;
+    background-color: currentColor;
+    opacity: 0.38;
+  }
+
+  & .MuiSlider-track {
+    display: block;
+    position: absolute;
+    height: 4px;
+    border-radius: 2px;
+    background-color: currentColor;
+  }
+
+  & .MuiSlider-thumb {
+    position: absolute;
+    width: 14px;
+    height: 14px;
+    margin-left: -6px;
+    margin-top: -5px;
+    box-sizing: border-box;
+    border-radius: 50%;
+    outline: 0;
+    border: 2px solid currentColor;
+    background-color: #fff;
+  }
+
+  & .MuiSlider-mark {
+    position: absolute;
+    width: 4px;
+    height: 4px;
+    border-radius: 2px;
+    background-color: currentColor;
+    top: 50%;
+    opacity: 0.7;
+    transform: translateX(-50%);
+  }
+
+  & .MuiSlider-markActive {
+    background-color: #fff;
+  }
+
+  & .MuiSlider-markLabel {
+    font-family: IBM Plex Sans;
+    font-size: 14px;
+    position: absolute;
+    white-space: nowrap;
+    top: 30px;
+    -webkit-transform: translateX(-50%);
+    -moz-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+  }
+
+  :hover,
+  &.Mui-focusVisible {
+    box-shadow: 0 0 0 0.25rem ${alpha(
+      theme.palette.mode === 'light' ? '#1976d2' : '#90caf9',
+      0.15,
+    )};
+  }
+
+  &.Mui-active {
+    box-shadow: 0 0 0 0.25rem ${alpha(
+      theme.palette.mode === 'light' ? '#1976d2' : '#90caf9',
+      0.3,
+    )};
+  }
+`,
+);
+
+const marks = [
+  {
+    value: 0,
+    label: '0°C',
+  },
+  {
+    value: 20,
+    label: '20°C',
+  },
+  {
+    value: 37,
+    label: '37°C',
+  },
+  {
+    value: 100,
+    label: '100°C',
+  },
+];
+
+function valuetext(value: number) {
+  return `${value}°C`;
+}
+
+export default function DiscreteSliderMarks() {
+  return (
+    <Box sx={{ width: 300 }}>
+      <StyledSlider
+        aria-label="Temperature"
+        defaultValue={37}
+        getAriaValueText={valuetext}
+        marks={marks}
+      />
+    </Box>
+  );
+}
--- docs/data/base/components/slider/slider.md
+++ docs/data/base/components/slider/slider.md
@@ -11,9 +11,8 @@ packageName: '@mui/base'
 
 <p class="description">Sliders allow users to make selections from a range of values.</p>
 
-Sliders reflect a range of values along a bar, from which users may select a single value. They are ideal for adjusting settings such as volume, brightness, or applying image filters.
-
-- 📦 [5.6 kB gzipped](https://bundlephobia.com/package/@mui/base@latest)
+Sliders show a range of values along a bar, from which users may select a single value.
+They are ideal for adjusting settings such as volume, brightness, or applying image filters.
 
 {{"component": "modules/components/ComponentLinkHeader.js", "design": false}}
 
@@ -21,4 +20,41 @@ Sliders reflect a range of values along a bar, from which users may select a sin
 import SliderUnstyled from '@mui/base/SliderUnstyled';
 ```
 
-{{"demo": "UnstyledSlider.js"}}
+{{"demo": "UnstyledSlider.js", "defaultCodeOpen": false}}
+
+## Discrete sliders
+
+Discrete sliders snap to the nearest step.
+You can generate a mark for each step with `marks={true}`.
+
+{{"demo": "DiscreteSlider.js"}}
+
+### Custom marks
+
+You can have custom marks by providing a rich array to the `marks` prop.
+
+{{"demo": "DiscreteSliderMarks.js"}}
+
+### Restricted values
+
+You can restrict the selectable values to those provided with the `marks` prop with `step={null}`.
+
+{{"demo": "DiscreteSliderValues.js"}}
+
+## Range slider
+
+The slider can be used to set the start and end of a range by supplying an array of values to the `value` or `defaultValue` prop.
+
+{{"demo": "RangeSlider.js"}}
+
+## Accessibility
+
+(WAI-ARIA: https://www.w3.org/TR/wai-aria-practices/#slider)
+
+The component handles most of the work necessary to make it accessible.
+However, you need to make sure that:
+
+- Each thumb has a user-friendly label (`aria-label`, `aria-labelledby` or `getAriaLabel` prop).
+- Each thumb has a user-friendly text for its current value.
+  This is not required if the value matches the semantics of the label.
+  You can change the name with the `getAriaValueText` or `aria-valuetext` prop.
