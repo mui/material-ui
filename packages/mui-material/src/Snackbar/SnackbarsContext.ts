5bdbcdc3448f6b1d3a3c4705e3adba4fa761f769
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2022-03-24 18:03:28 +0530

    feat: ShowSnackbarProps

--- packages/mui-material/src/Snackbar/SnackbarsContext.ts
+++ packages/mui-material/src/Snackbar/SnackbarsContext.ts
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import type { SnackbarProps } from './Snackbar';
 
+type ShowSnackbarProps = Omit<SnackbarProps, 'open'>;
+
 export interface SnackbarsContextProps {
-  showSnackbar(props: SnackbarProps): void;
+  showSnackbar(props: ShowSnackbarProps): void;
 }
 
 const SnackbarsContext = React.createContext<SnackbarsContextProps | undefined>(undefined);
