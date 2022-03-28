7b068ec51f5d96537ce82fca72ea25982a14c94f
delangle <flaviendelangle@gmail.com>
2022-03-28 12:05:59 +0200

    Work

--- packages/mui-lab/src/ClockPicker/ClockPicker.spec.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import ClockPicker from '@mui/lab/ClockPicker';
-
-// External components are generic
-<ClockPicker<Date> view="hours" date={null} onChange={(date) => date?.getDate()} />;
