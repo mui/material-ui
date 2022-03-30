ac08a3a599db2ae6eaabc31bd61df64e44884ae7
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2022-03-30 08:54:08 +0200

    [core] Fix TS 4.7 compat issues (#32041)

--- packages/material-ui/src/TableBody/TableBody.d.ts
+++ packages/material-ui/src/TableBody/TableBody.d.ts
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { OverridableComponent, OverrideProps } from '../OverridableComponent';
+import { OverridableComponent, OverrideProps, OverridableTypeMap } from '../OverridableComponent';
 
-export interface TableBodyTypeMap<P = {}, D extends React.ElementType = 'tbody'> {
+export interface TableBodyTypeMap<P extends {} = {}, D extends React.ElementType = 'tbody'>
+  extends OverridableTypeMap {
   props: P;
   defaultComponent: D;
   classKey: TableBodyClassKey;
@@ -22,7 +23,7 @@ export type TableBodyClassKey = 'root';
 
 export type TableBodyProps<
   D extends React.ElementType = TableBodyTypeMap['defaultComponent'],
-  P = {}
+  P extends {} = {}
 > = OverrideProps<TableBodyTypeMap<P, D>, D>;
 
 export default TableBody;
