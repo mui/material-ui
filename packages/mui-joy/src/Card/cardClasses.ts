abcb6512a32287db5d522b11da856a9abf40e928
siriwatknp <siriwatkunaporn@gmail.com>
2022-03-31 19:13:15 +0700

    add tests for Card

--- packages/mui-joy/src/Card/cardClasses.ts
+++ packages/mui-joy/src/Card/cardClasses.ts
@@ -3,6 +3,26 @@ import { generateUtilityClass, generateUtilityClasses } from '@mui/base';
 export interface CardClasses {
   /** Styles applied to the root element. */
   root: string;
+  /** Styles applied to the root element if `color="primary"`. */
+  colorPrimary: string;
+  /** Styles applied to the root element if `color="neutral"`. */
+  colorNeutral: string;
+  /** Styles applied to the root element if `color="danger"`. */
+  colorDanger: string;
+  /** Styles applied to the root element if `color="info"`. */
+  colorInfo: string;
+  /** Styles applied to the root element if `color="success"`. */
+  colorSuccess: string;
+  /** Styles applied to the root element if `color="warning"`. */
+  colorWarning: string;
+  /** Styles applied to the root element if `variant="text"`. */
+  variantText: string;
+  /** Styles applied to the root element if `variant="outlined"`. */
+  variantOutlined: string;
+  /** Styles applied to the root element if `variant="light"`. */
+  variantLight: string;
+  /** Styles applied to the root element if `variant="contained"`. */
+  variantContained: string;
   /** Styles applied to the root element if `size="sm"`. */
   sizeSm: string;
   /** Styles applied to the root element if `size="md"`. */
@@ -19,6 +39,16 @@ export function getCardUtilityClass(slot: string): string {
 
 const cardClasses: CardClasses = generateUtilityClasses('MuiCard', [
   'root',
+  'colorPrimary',
+  'colorNeutral',
+  'colorDanger',
+  'colorInfo',
+  'colorSuccess',
+  'colorWarning',
+  'variantText',
+  'variantOutlined',
+  'variantLight',
+  'variantContained',
   'sizeSm',
   'sizeMd',
   'sizeLg',
