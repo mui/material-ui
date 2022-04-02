585c4a3850936d7ddcd60ed4ad063996415e0f3a
Siriwat K <siriwatkunaporn@gmail.com>
2022-04-02 16:10:21 +0700

    [Joy] Add `Card` components (#32027)

---
+++ packages/mui-joy/src/AspectRatio/AspectRatio.tsx
@@ -0,0 +1,172 @@
+import * as React from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { unstable_composeClasses as composeClasses } from '@mui/base';
+import { OverridableComponent } from '@mui/types';
+import { unstable_capitalize as capitalize } from '@mui/utils';
+import { useThemeProps } from '../styles';
+import styled from '../styles/styled';
+import { getAspectRatioUtilityClass } from './aspectRatioClasses';
+import { AspectRatioProps, AspectRatioTypeMap } from './AspectRatioProps';
+
+const useUtilityClasses = (ownerState: AspectRatioProps) => {
+  const { variant, color } = ownerState;
+  const slots = {
+    root: [
+      'root',
+      variant && `variant${capitalize(variant)}`,
+      color && `color${capitalize(color)}`,
+    ],
+  };
+
+  return composeClasses(slots, getAspectRatioUtilityClass, {});
+};
+
+const AspectRatioRoot = styled('div', {
+  name: 'MuiAspectRatio',
+  slot: 'Root',
+  overridesResolver: (props, styles) => styles.root,
+})<{ ownerState: AspectRatioProps }>(({ theme, ownerState }) => {
+  const min = typeof ownerState.min === 'number' ? `${ownerState.min}px` : ownerState.min;
+  const max = typeof ownerState.max === 'number' ? `${ownerState.max}px` : ownerState.max;
+  return [
+    {
+      position: 'relative',
+      borderRadius: 'var(--AspectRatio-radius)',
+      height: 0,
+      paddingBottom: `clamp(${min || '0px'}, calc(100% / (${ownerState.ratio})), ${
+        max || '9999px'
+      })`,
+      // use data-attribute instead of :first-child to support zero config SSR (emotion)
+      '& > [data-first-child]': {
+        borderRadius: 'var(--AspectRatio-radius)',
+        boxSizing: 'border-box',
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        objectFit: ownerState.objectFit,
+        overflow: 'hidden',
+        margin: 0,
+        padding: 0,
+        '& > img': {
+          // support art-direction that uses <picture><img /></picture>
+          width: '100%',
+          height: '100%',
+          objectFit: ownerState.objectFit,
+        },
+      },
+    },
+    theme.variants[ownerState.variant!]?.[ownerState.color!],
+  ];
+});
+
+const AspectRatio = React.forwardRef(function AspectRatio(inProps, ref) {
+  const props = useThemeProps<typeof inProps & AspectRatioProps>({
+    props: inProps,
+    name: 'MuiAspectRatio',
+  });
+
+  const {
+    className,
+    component = 'div',
+    children,
+    ratio = '16 / 9',
+    min,
+    max,
+    objectFit = 'cover',
+    color = 'neutral',
+    variant = 'light',
+    ...other
+  } = props;
+
+  const ownerState = {
+    ...props,
+    component,
+    min,
+    max,
+    objectFit,
+    ratio,
+    color,
+    variant,
+  };
+
+  const classes = useUtilityClasses(ownerState);
+
+  return (
+    <AspectRatioRoot
+      as={component}
+      ownerState={ownerState}
+      className={clsx(classes.root, className)}
+      ref={ref}
+      {...other}
+    >
+      {React.Children.map(children, (child, index) =>
+        index === 0 && React.isValidElement(child)
+          ? React.cloneElement(child, { 'data-first-child': '' })
+          : child,
+      )}
+    </AspectRatioRoot>
+  );
+}) as OverridableComponent<AspectRatioTypeMap>;
+
+AspectRatio.propTypes /* remove-proptypes */ = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // |     To update them edit TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * Used to render icon or text elements inside the AspectRatio if `src` is not set.
+   * This can be an element, or just a string.
+   */
+  children: PropTypes.node,
+  /**
+   * @ignore
+   */
+  className: PropTypes.string,
+  /**
+   * The color of the component. It supports those theme colors that make sense for this component.
+   * @default 'neutral'
+   */
+  color: PropTypes.oneOf(['danger', 'info', 'neutral', 'primary', 'success', 'warning']),
+  /**
+   * The component used for the root node.
+   * Either a string to use a HTML element or a component.
+   */
+  component: PropTypes.elementType,
+  /**
+   * The maximum calculated height of the element (not the CSS height).
+   */
+  max: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  /**
+   * The minimum calculated height of the element (not the CSS height).
+   */
+  min: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  /**
+   * The CSS object-fit value of the first-child.
+   */
+  objectFit: PropTypes.oneOf([
+    '-moz-initial',
+    'contain',
+    'cover',
+    'fill',
+    'inherit',
+    'initial',
+    'none',
+    'revert',
+    'scale-down',
+    'unset',
+  ]),
+  /**
+   * The aspect-ratio of the element. The current implementation uses padding instead of the CSS aspect-ratio due to browser support.
+   * https://caniuse.com/?search=aspect-ratio
+   * @default '16 / 9'
+   */
+  ratio: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  /**
+   * The variant to use.
+   * @default 'light'
+   */
+  variant: PropTypes.oneOf(['contained', 'light', 'outlined', 'text']),
+} as any;
+
+export default AspectRatio;
--- packages/mui-joy/src/Card/Card.test.js
+++ packages/mui-joy/src/Card/Card.test.js
@@ -2,27 +2,27 @@ import * as React from 'react';
 import { expect } from 'chai';
 import { createRenderer, describeConformance } from 'test/utils';
 import { ThemeProvider } from '@mui/joy/styles';
-import Sheet, { sheetClasses as classes } from '@mui/joy/Sheet';
+import Card, { cardClasses as classes } from '@mui/joy/Card';
 import { unstable_capitalize as capitalize } from '@mui/utils';
 
-describe('<Sheet />', () => {
+describe('<Card />', () => {
   const { render } = createRenderer();
 
-  describeConformance(<Sheet />, () => ({
+  describeConformance(<Card />, () => ({
     classes,
     inheritComponent: 'div',
     render,
     ThemeProvider,
-    muiName: 'MuiSheet',
+    muiName: 'MuiCard',
     refInstanceof: window.HTMLDivElement,
-    testComponentPropWith: 'header',
+    testComponentPropWith: 'li',
     testVariantProps: { variant: 'contained' },
     skip: ['classesRoot', 'componentsProp'],
   }));
 
   describe('prop: variant', () => {
     it('text by default', () => {
-      const { getByTestId } = render(<Sheet data-testid="root">Hello World</Sheet>);
+      const { getByTestId } = render(<Card data-testid="root">Hello World</Card>);
 
       expect(getByTestId('root')).to.have.class(classes.variantText);
     });
@@ -30,9 +30,9 @@ describe('<Sheet />', () => {
     ['text', 'outlined', 'light', 'contained'].forEach((variant) => {
       it(`should render ${variant}`, () => {
         const { getByTestId } = render(
-          <Sheet data-testid="root" variant={variant}>
+          <Card data-testid="root" variant={variant}>
             Hello World
-          </Sheet>,
+          </Card>,
         );
 
         expect(getByTestId('root')).to.have.class(classes[`variant${capitalize(variant)}`]);
@@ -42,7 +42,7 @@ describe('<Sheet />', () => {
 
   describe('prop: color', () => {
     it('adds a neutral class by default', () => {
-      const { getByTestId } = render(<Sheet data-testid="root">Hello World</Sheet>);
+      const { getByTestId } = render(<Card data-testid="root">Hello World</Card>);
 
       expect(getByTestId('root')).to.have.class(classes.colorNeutral);
     });
@@ -50,9 +50,9 @@ describe('<Sheet />', () => {
     ['primary', 'success', 'info', 'danger', 'neutral', 'warning'].forEach((color) => {
       it(`should render ${color}`, () => {
         const { getByTestId } = render(
-          <Sheet data-testid="root" color={color}>
+          <Card data-testid="root" color={color}>
             Hello World
-          </Sheet>,
+          </Card>,
         );
 
         expect(getByTestId('root')).to.have.class(classes[`color${capitalize(color)}`]);
@@ -60,26 +60,25 @@ describe('<Sheet />', () => {
     });
   });
 
-  describe('prop: elevation', () => {
-    it('undefined by default', () => {
-      const { getByTestId } = render(<Sheet data-testid="root">Hello World</Sheet>);
+  it('can change size', () => {
+    const { container, rerender } = render(<Card />);
 
-      expect(getByTestId('root')).not.to.have.class(classes.elevationXs);
-      expect(getByTestId('root')).not.to.have.class(classes.elevationSm);
-      expect(getByTestId('root')).not.to.have.class(classes.elevationMd);
-      expect(getByTestId('root')).not.to.have.class(classes.elevationLg);
-      expect(getByTestId('root')).not.to.have.class(classes.elevationXl);
-    });
-    ['xs', 'sm', 'md', 'lg', 'xl'].forEach((elevation) => {
-      it(`should render ${elevation}`, () => {
-        const { getByTestId } = render(
-          <Sheet data-testid="root" elevation={elevation}>
-            Hello World
-          </Sheet>,
-        );
+    expect(container.firstChild).to.have.class(classes.sizeMd);
 
-        expect(getByTestId('root')).to.have.class(classes[`elevation${capitalize(elevation)}`]);
-      });
-    });
+    rerender(<Card size="lg" />);
+
+    expect(container.firstChild).to.have.class(classes.sizeLg);
+  });
+
+  it('add data-attribute to the first and last child', () => {
+    const { container } = render(
+      <Card>
+        <div>First</div>
+        <div>Second</div>
+        <div>Third</div>
+      </Card>,
+    );
+    expect(container.querySelector('[data-first-child]')).to.have.text('First');
+    expect(container.querySelector('[data-last-child]')).to.have.text('Third');
   });
 });
