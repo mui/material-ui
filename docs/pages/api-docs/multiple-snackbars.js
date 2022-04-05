e7170e91d4f688b91f665265cf0e6a09c133fd42
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2022-04-05 15:11:10 +0530

    yarn prettier

--- docs/pages/api-docs/multiple-snackbars.js
+++ docs/pages/api-docs/multiple-snackbars.js
@@ -9,7 +9,11 @@ export default function Page(props) {
 }
 
 Page.getInitialProps = () => {
-  const req = require.context('docs/translations/api-docs/multiple-snackbars', false, /multiple-snackbars.*.json$/);
+  const req = require.context(
+    'docs/translations/api-docs/multiple-snackbars',
+    false,
+    /multiple-snackbars.*.json$/,
+  );
   const descriptions = mapApiPageTranslations(req);
 
   return {
