a6c2a445c3f8c21a36ab18c96aecf1b23f3c67eb
Olivier Tassinari <olivier.tassinari@gmail.com>
2022-04-07 12:25:49 +0200

    Keep the npm KPI up to date

--- docs/src/components/home/MuiStatistics.tsx
+++ docs/src/components/home/MuiStatistics.tsx
@@ -4,7 +4,7 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
 const data = [
-  { title: '2.8M', metadata: 'Weekly downloads on npm' },
+  { title: '3.2M', metadata: 'Weekly downloads on npm' },
   { title: '76k', metadata: 'Stars on GitHub' },
   { title: '2.4k', metadata: 'Open-source contributors' },
   { title: '17k', metadata: 'Followers on Twitter' },
