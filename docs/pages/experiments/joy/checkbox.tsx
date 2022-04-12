bdd9f373396c847cbc3911f3eb162c77f5943c95
siriwatknp <siriwatkunaporn@gmail.com>
2022-04-12 22:01:25 +0700

    add complex switch examples

--- docs/pages/experiments/joy/checkbox.tsx
+++ docs/pages/experiments/joy/checkbox.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import Avatar from '@mui/joy/Avatar';
 import Box from '@mui/joy/Box';
 import Button from '@mui/joy/Button';
 import Checkbox from '@mui/joy/Checkbox';
 import List from '@mui/joy/List';
 import ListItem from '@mui/joy/ListItem';
+import Sheet from '@mui/joy/Sheet';
 import Typography from '@mui/joy/Typography';
 import { CssVarsProvider, useColorScheme } from '@mui/joy/styles';
 import Moon from '@mui/icons-material/DarkMode';
@@ -132,6 +134,129 @@ export default function JoyCheckbox() {
               }
             />
           </Box>
+        </Box>
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+            gap: 2,
+          }}
+        >
+          <Sheet sx={{ boxShadow: 'md', p: 2 }}>
+            <Typography
+              id="filter-status"
+              sx={{
+                textTransform: 'uppercase',
+                fontSize: 'xs',
+                letterSpacing: 'md',
+                fontWeight: 'md',
+                color: 'text.secondary',
+                mb: 1,
+              }}
+            >
+              Filter status
+            </Typography>
+            <Box role="group" aria-labelledby="filter-status">
+              <List
+                sx={{
+                  '--List-item-radius': '4px',
+                  '& .MuiCheckbox-root': { position: 'initial' },
+                  '[data-mui-color-scheme="light"] &': {
+                    '--joy-palette-neutral-lightBg': 'var(--joy-palette-neutral-50)',
+                  },
+                }}
+              >
+                <ListItem variant="light" color="danger">
+                  <Checkbox label="Declined Payment" color="danger" checked />
+                  <Typography color="inherit" sx={{ ml: 'auto' }}>
+                    8
+                  </Typography>
+                </ListItem>
+                <ListItem variant="light" color="warning">
+                  <Checkbox label="Delivery Error" color="warning" checked />
+                  <Typography color="inherit" sx={{ ml: 'auto' }}>
+                    24
+                  </Typography>
+                </ListItem>
+                <ListItem variant="light">
+                  <Checkbox label="Wrong Amount" color="neutral" />
+                </ListItem>
+                <ListItem variant="light">
+                  <Checkbox label="Wrong Address" color="neutral" />
+                </ListItem>
+                <ListItem variant="light">
+                  <Checkbox label="Wrong UX Solution" color="neutral" />
+                </ListItem>
+              </List>
+            </Box>
+            <Button variant="text" color="warning" size="sm" sx={{ px: 1.5, mt: 1 }}>
+              Clear All
+            </Button>
+          </Sheet>
+          <Sheet variant="outlined" sx={{ boxShadow: 'sm', p: 2, borderRadius: '8px' }}>
+            <Typography
+              id="member"
+              sx={{
+                textTransform: 'uppercase',
+                fontSize: 'xs',
+                letterSpacing: 'md',
+                fontWeight: 'md',
+                color: 'text.secondary',
+                mb: 1,
+              }}
+            >
+              Team members
+            </Typography>
+            <Box role="group" aria-labelledby="member">
+              <List
+                sx={{
+                  '--List-item-radius': '4px',
+                  '& .MuiCheckbox-root': {
+                    position: 'initial',
+                    mr: 'auto',
+                    alignItems: 'center',
+                    '--Checkbox-gap': '12px',
+                  },
+                  '[data-mui-color-scheme="light"] &': {
+                    '--joy-palette-neutral-lightBg': 'var(--joy-palette-neutral-50)',
+                  },
+                }}
+              >
+                <ListItem>
+                  <Checkbox label="Friedrich Oberbrunner" />
+                  <Avatar aria-hidden="true" src="/static/images/avatar/1.jpg" />
+                </ListItem>
+                <ListItem variant="light" color="primary">
+                  <Checkbox
+                    label={
+                      <React.Fragment>
+                        Adeline O&apos;Reilly
+                        <Box
+                          component="span"
+                          aria-hidden="true"
+                          sx={{ display: 'block', fontSize: 'sm', color: 'primary.500' }}
+                        >
+                          This user was picked
+                        </Box>
+                      </React.Fragment>
+                    }
+                    checked
+                  />
+                  <Avatar aria-hidden="true" src="/static/images/avatar/2.jpg" />
+                </ListItem>
+                <ListItem>
+                  <Checkbox label="Fernando Pidrillio" color="neutral" />
+                  <Avatar aria-hidden="true" src="/static/images/avatar/3.jpg" />
+                </ListItem>
+                <ListItem>
+                  <Checkbox label="Anonymous User" color="neutral" />
+                  <Avatar aria-hidden="true" variant="contained">
+                    AU
+                  </Avatar>
+                </ListItem>
+              </List>
+            </Box>
+          </Sheet>
         </Box>
       </Box>
     </CssVarsProvider>
