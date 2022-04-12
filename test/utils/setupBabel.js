a6e5e45805ab6ec68a12924576ee1688a48fd55f
Michał Dudak <michal@dudak.me>
2022-04-12 11:37:10 +0200

    [test] Fix running unit tests on Windows

--- test/utils/setupBabel.js
+++ test/utils/setupBabel.js
@@ -3,5 +3,5 @@ require('@babel/register')({
   // We have to apply `babel-plugin-module-resolve` to the files in `@mui/x-date-pickers`.
   // Otherwise we can't import `@mui/material` from `@mui/x-date-pickers` in `yarn test:unit`.
   // TODO: Remove once the lab do not export the pickers
-  ignore: [/node_modules(\/|\\)(?!.*(@mui(\/|\\)x-date-pickers(\/|\\)([a-zA-Z/])+)\.js)/],
+  ignore: [/node_modules(\/|\\)(?!.*(@mui(\/|\\)x-date-pickers(\/|\\)([a-zA-Z/\\])+)\.js)/],
 });
