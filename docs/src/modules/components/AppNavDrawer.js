26daf4c270b10a67dd4c0d5d4847df7328bf03ed
Olivier Tassinari <olivier.tassinari@gmail.com>
2022-04-16 13:18:04 +0200

    [core] Remove unecessary div (#32237)

--- docs/src/modules/components/AppNavDrawer.js
+++ docs/src/modules/components/AppNavDrawer.js
@@ -323,9 +323,6 @@ PersistScroll.propTypes = {
   slot: PropTypes.string.isRequired,
 };
 
-// https://github.com/philipwalton/flexbugs#3-min-height-on-a-flex-container-wont-apply-to-its-flex-items
-const ToolbarIE11 = styled('div')({ display: 'flex' });
-
 const ToolbarDiv = styled('div')(({ theme }) => ({
   padding: theme.spacing(1.45, 2),
   paddingRight: 0,
@@ -346,10 +343,11 @@ const StyledDrawer = styled(Drawer)(({ theme }) => ({
   },
 }));
 
-const SwipeableDrawerPaperComponent = styled('div')(({ theme }) => {
+const AppNavPaperComponent = styled('div')(({ theme }) => {
   return {
     width: 'var(--MuiDocs-navDrawer-width)',
     boxShadow: 'none',
+    paddingBottom: theme.spacing(5),
     [theme.breakpoints.up('xs')]: {
       borderRadius: '0px 10px 10px 0px',
     },
@@ -439,7 +437,7 @@ function reduceChildRoutes(context) {
 // So: <SwipeableDrawer disableBackdropTransition={false} />
 const iOS = typeof navigator !== 'undefined' && /iPad|iPhone|iPod/.test(navigator.userAgent);
 
-function AppNavDrawer(props) {
+export default function AppNavDrawer(props) {
   const { className, disablePermanent, mobileOpen, onClose, onOpen } = props;
   const { activePage, pages } = React.useContext(PageContext);
   const router = useRouter();
@@ -552,105 +550,103 @@ function AppNavDrawer(props) {
 
     return (
       <React.Fragment>
-        <ToolbarIE11>
-          <ToolbarDiv>
-            <NextLink href="/" passHref onClick={onClose}>
-              <Box
-                component="a"
-                aria-label={t('goToHome')}
-                sx={{
-                  pr: '12px',
-                  mr: '4px',
-                  borderRight: isProductScoped ? '1px solid' : '0px',
-                  borderColor: (theme) =>
-                    theme.palette.mode === 'dark'
-                      ? alpha(theme.palette.primary[100], 0.08)
-                      : theme.palette.grey[200],
-                }}
-              >
-                <SvgMuiLogo width={30} />
-              </Box>
-            </NextLink>
-            {!isProductScoped &&
-              renderVersionSelector(
-                [
-                  { text: `v${process.env.LIB_VERSION}`, current: true },
-                  { text: 'v4', href: `https://v4.mui.com${languagePrefix}/` },
-                  {
-                    text: 'View all versions',
-                    href: `https://mui.com${languagePrefix}/versions/`,
-                  },
-                ],
-                { mr: 2 },
-              )}
-            {asPathWithoutLang.startsWith('/material-ui/') && (
-              <ProductIdentifier
-                name="Material UI"
-                metadata="MUI Core"
-                versionSelector={renderVersionSelector([
-                  { text: `v${materialPkgJson.version}`, current: true },
-                  {
-                    text: 'v4',
-                    href: `https://v4.mui.com${languagePrefix}/getting-started/installation/`,
-                  },
-                  {
-                    text: 'View all versions',
-                    href: `https://mui.com${languagePrefix}/versions/`,
-                  },
-                ])}
-              />
+        <ToolbarDiv>
+          <NextLink href="/" passHref onClick={onClose}>
+            <Box
+              component="a"
+              aria-label={t('goToHome')}
+              sx={{
+                pr: '12px',
+                mr: '4px',
+                borderRight: isProductScoped ? '1px solid' : '0px',
+                borderColor: (theme) =>
+                  theme.palette.mode === 'dark'
+                    ? alpha(theme.palette.primary[100], 0.08)
+                    : theme.palette.grey[200],
+              }}
+            >
+              <SvgMuiLogo width={30} />
+            </Box>
+          </NextLink>
+          {!isProductScoped &&
+            renderVersionSelector(
+              [
+                { text: `v${process.env.LIB_VERSION}`, current: true },
+                { text: 'v4', href: `https://v4.mui.com${languagePrefix}/` },
+                {
+                  text: 'View all versions',
+                  href: `https://mui.com${languagePrefix}/versions/`,
+                },
+              ],
+              { mr: 2 },
             )}
-            {asPathWithoutLang.startsWith('/system/') && FEATURE_TOGGLE.enable_system_scope && (
-              <ProductIdentifier
-                name="MUI System"
-                metadata="MUI Core"
-                versionSelector={renderVersionSelector([
-                  { text: `v${systemPkgJson.version}`, current: true },
-                  { text: 'v4', href: `https://v4.mui.com${languagePrefix}/system/basics/` },
-                  {
-                    text: 'View all versions',
-                    href: `https://mui.com${languagePrefix}/versions/`,
-                  },
-                ])}
-              />
-            )}
-            {asPathWithoutLang.startsWith('/base/') && (
-              <ProductIdentifier
-                name="MUI Base"
-                metadata="MUI Core"
-                versionSelector={renderVersionSelector([
-                  { text: `v${basePkgJson.version}`, current: true },
-                ])}
-              />
-            )}
-            {asPathWithoutLang.startsWith('/x/advanced-components') && (
-              <ProductIdentifier name="Advanced components" metadata="MUI X" />
-            )}
-            {(asPathWithoutLang.startsWith('/x/react-data-grid') ||
-              asPathWithoutLang.startsWith('/x/api/data-grid')) && (
-              <ProductIdentifier
-                name="Data Grid"
-                metadata="MUI X"
-                versionSelector={renderVersionSelector([
-                  // DATA_GRID_VERSION is set from the X repo
-                  { text: `v${process.env.DATA_GRID_VERSION}`, current: true },
-                  { text: 'v4', href: `https://v4.mui.com${languagePrefix}/components/data-grid/` },
-                ])}
-              />
-            )}
-            {(asPathWithoutLang.startsWith('/x/react-date-pickers') ||
-              asPathWithoutLang.startsWith('/x/api/date-pickers')) && (
-              <ProductIdentifier
-                name="Date pickers"
-                metadata="MUI X"
-                versionSelector={renderVersionSelector([
-                  // DATE_PICKERS_VERSION is set from the X repo
-                  { text: `v${process.env.DATE_PICKERS_VERSION}`, current: true },
-                ])}
-              />
-            )}
-          </ToolbarDiv>
-        </ToolbarIE11>
+          {asPathWithoutLang.startsWith('/material-ui/') && (
+            <ProductIdentifier
+              name="Material UI"
+              metadata="MUI Core"
+              versionSelector={renderVersionSelector([
+                { text: `v${materialPkgJson.version}`, current: true },
+                {
+                  text: 'v4',
+                  href: `https://v4.mui.com${languagePrefix}/getting-started/installation/`,
+                },
+                {
+                  text: 'View all versions',
+                  href: `https://mui.com${languagePrefix}/versions/`,
+                },
+              ])}
+            />
+          )}
+          {asPathWithoutLang.startsWith('/system/') && FEATURE_TOGGLE.enable_system_scope && (
+            <ProductIdentifier
+              name="MUI System"
+              metadata="MUI Core"
+              versionSelector={renderVersionSelector([
+                { text: `v${systemPkgJson.version}`, current: true },
+                { text: 'v4', href: `https://v4.mui.com${languagePrefix}/system/basics/` },
+                {
+                  text: 'View all versions',
+                  href: `https://mui.com${languagePrefix}/versions/`,
+                },
+              ])}
+            />
+          )}
+          {asPathWithoutLang.startsWith('/base/') && (
+            <ProductIdentifier
+              name="MUI Base"
+              metadata="MUI Core"
+              versionSelector={renderVersionSelector([
+                { text: `v${basePkgJson.version}`, current: true },
+              ])}
+            />
+          )}
+          {asPathWithoutLang.startsWith('/x/advanced-components') && (
+            <ProductIdentifier name="Advanced components" metadata="MUI X" />
+          )}
+          {(asPathWithoutLang.startsWith('/x/react-data-grid') ||
+            asPathWithoutLang.startsWith('/x/api/data-grid')) && (
+            <ProductIdentifier
+              name="Data Grid"
+              metadata="MUI X"
+              versionSelector={renderVersionSelector([
+                // DATA_GRID_VERSION is set from the X repo
+                { text: `v${process.env.DATA_GRID_VERSION}`, current: true },
+                { text: 'v4', href: `https://v4.mui.com${languagePrefix}/components/data-grid/` },
+              ])}
+            />
+          )}
+          {(asPathWithoutLang.startsWith('/x/react-date-pickers') ||
+            asPathWithoutLang.startsWith('/x/api/date-pickers')) && (
+            <ProductIdentifier
+              name="Date pickers"
+              metadata="MUI X"
+              versionSelector={renderVersionSelector([
+                // DATE_PICKERS_VERSION is set from the X repo
+                { text: `v${process.env.DATE_PICKERS_VERSION}`, current: true },
+              ])}
+            />
+          )}
+        </ToolbarDiv>
         <Divider
           sx={{
             borderColor: (theme) =>
@@ -661,7 +657,6 @@ function AppNavDrawer(props) {
         />
         <DiamondSponsors spot="drawer" />
         {navItems}
-        <Box sx={{ height: 40 }} />
       </React.Fragment>
     );
   }, [activePage, pages, onClose, languagePrefix, t, anchorEl, setAnchorEl, router.asPath]);
@@ -680,11 +675,7 @@ function AppNavDrawer(props) {
           }}
           PaperProps={{
             className: 'algolia-drawer',
-            component: SwipeableDrawerPaperComponent,
-            sx: {
-              background: (theme) =>
-                theme.palette.mode === 'dark' ? theme.palette.primaryDark[900] : '#FFF',
-            },
+            component: AppNavPaperComponent,
           }}
         >
           <PersistScroll slot="swipeable" enabled={mobileOpen}>
@@ -696,15 +687,7 @@ function AppNavDrawer(props) {
         <StyledDrawer
           variant="permanent"
           PaperProps={{
-            component: SwipeableDrawerPaperComponent,
-            sx: {
-              background: (theme) =>
-                theme.palette.mode === 'dark' ? theme.palette.primaryDark[900] : '#fff',
-              borderColor: (theme) =>
-                theme.palette.mode === 'dark'
-                  ? alpha(theme.palette.primary[100], 0.08)
-                  : theme.palette.grey[100],
-            },
+            component: AppNavPaperComponent,
           }}
           open
         >
@@ -724,5 +707,3 @@ AppNavDrawer.propTypes = {
   onClose: PropTypes.func.isRequired,
   onOpen: PropTypes.func.isRequired,
 };
-
-export default AppNavDrawer;
