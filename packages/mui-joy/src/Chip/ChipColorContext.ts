f456d394249dbc261bb0db91ad1c3da3125eae83
siriwatknp <siriwatkunaporn@gmail.com>
2022-04-18 22:00:56 +0700

    rename context to include other props

--- packages/mui-joy/src/Chip/ChipColorContext.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { ChipProps } from './ChipProps';
-
-const ChipColorContext = React.createContext<ChipProps['color']>(undefined);
-
-export default ChipColorContext;
