55030803475dee16f63d54fb03f224d88bbdca2b
Benny Joo <sldisek783@gmail.com>
2022-04-20 11:47:24 +0100

    Do not use relative import in type test

--- packages/mui-material/src/DialogTitle/DialogTitle.spec.tsx
+++ packages/mui-material/src/DialogTitle/DialogTitle.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import DialogTitle from './DialogTitle';
+import { DialogTitle } from '@mui/material';
 
 function DialogTitleTest() {
   <DialogTitle component="h4" />;
