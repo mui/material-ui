ef34770fbb29ad41932f5c94b4073b3bf056007f
Benny Joo <sldisek783@gmail.com>
2022-04-20 11:24:41 +0100

    Add type test

---
+++ packages/mui-material/src/DialogTitle/DialogTitle.spec.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import DialogTitle from './DialogTitle';
+
+function DialogTitleTest() {
+  <DialogTitle component="h4" />;
+  <DialogTitle component="button" />;
+  <DialogTitle component="p" />;
+}
