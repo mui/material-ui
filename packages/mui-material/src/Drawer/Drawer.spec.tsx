ec2f328c295646ddcec98d62791570abace9fd6f
Benny Joo <sldisek783@gmail.com>
2022-04-21 09:00:28 +0100

    [Drawer] Add type test for passing component to PaperProps

---
+++ packages/mui-material/src/Drawer/Drawer.spec.tsx
@@ -0,0 +1,8 @@
+import { Drawer, Paper } from '@mui/material';
+import * as React from 'react';
+
+function PaperPropsTest() {
+  <Drawer PaperProps={{ component: Paper }} variant="permanent">
+    Drawer content
+  </Drawer>;
+}
