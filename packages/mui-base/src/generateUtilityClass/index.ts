e0c32edbafeb07d0ce85eb0ae13d53229006f856
Marija Najdova <mnajdova@gmail.com>
2022-04-29 10:32:07 +0200

    Fix the build

--- packages/mui-base/src/generateUtilityClass/index.ts
+++ packages/mui-base/src/generateUtilityClass/index.ts
@@ -1,1 +1,2 @@
-export { generateUtilityClass as default, GlobalStateSlot } from '@mui/private-classnames';
+export { generateUtilityClass as default } from '@mui/private-classnames';
+export type { GlobalStateSlot } from '@mui/private-classnames';
