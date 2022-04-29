7e418fe384330211367c639228b8946997c316f8
Marija Najdova <mnajdova@gmail.com>
2022-04-29 10:41:13 +0200

    prettier & lint

--- packages/mui-private-classnames/src/index.js
+++ packages/mui-private-classnames/src/index.js
@@ -1,6 +1,3 @@
 export { default as unstable_composeClasses } from './composeClasses';
-
 export { default as generateUtilityClass } from './generateUtilityClass';
-
 export { default as generateUtilityClasses } from './generateUtilityClasses';
-export * from './generateUtilityClasses';
