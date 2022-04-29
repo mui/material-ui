48a8606b6ab606742ba98053abb6f1bc4f190b5b
Marija Najdova <mnajdova@gmail.com>
2022-04-29 13:28:29 +0200

    [examples] Update to use React 18's createRoot

--- examples/create-react-app-with-styled-components-typescript/src/index.tsx
+++ examples/create-react-app-with-styled-components-typescript/src/index.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement!);
+
+root.render(
   <React.Fragment>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
     <App />
   </React.Fragment>,
-  document.getElementById('root'),
 );
--- examples/create-react-app-with-styled-components/src/index.js
+++ examples/create-react-app-with-styled-components/src/index.js
@@ -1,16 +1,19 @@
 import * as React from 'react';
+import { createRoot } from 'react-dom/client';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <React.Fragment>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
     <App />
   </React.Fragment>,
-  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
