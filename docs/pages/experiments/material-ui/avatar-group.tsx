8ec0c0b2b9145f00b8689633ebf9ef00fd224588
Victor Casas <victor.casas@outlook.cl>
2022-04-30 15:39:23 -0400

    Rename component

--- docs/pages/experiments/material-ui/avatar-group.tsx
+++ docs/pages/experiments/material-ui/avatar-group.tsx
@@ -37,7 +37,7 @@ const ColorSchemePicker = () => {
   );
 };
 
-export default function CssVarsTemplate() {
+export default function AvatarGroupCssVars() {
   return (
     <CssVarsProvider>
       <CssBaseline />
