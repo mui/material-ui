a6754b58d21e7f8467e78d417a8cc11da321d1ce
siriwatknp <siriwatkunaporn@gmail.com>
2022-04-30 10:30:11 +0700

    export VariantOverride APIs

--- packages/mui-joy/src/styles/index.ts
+++ packages/mui-joy/src/styles/index.ts
@@ -7,3 +7,4 @@ export * from './ThemeProvider';
 export { default as useThemeProps } from './useThemeProps';
 export { sx as experimental_sx } from './styleFunctionSx';
 export { default as extendTheme } from './extendTheme';
+export { VariantOverrideProvider, useVariantOverride } from './VariantOverride';
