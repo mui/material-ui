e050ad8401acaccb293fa732404f5516f8dfc0b9
siriwatknp <siriwatkunaporn@gmail.com>
2022-05-02 11:31:24 +0700

    explicitly export types

--- packages/mui-joy/src/styles/defaultTheme.ts
+++ packages/mui-joy/src/styles/defaultTheme.ts
@@ -1,5 +1,48 @@
+import { deepmerge } from '@mui/utils';
 import extendTheme from './extendTheme';
+import type { ThemeInput, ColorSystemInput } from './extendTheme';
+import type { Theme, RuntimeColorSystem } from './types';
 
-const defaultTheme = extendTheme();
+export const getThemeWithVars = (
+  themeInput?: Omit<ThemeInput, 'colorSchemes'> & ColorSystemInput,
+): Theme => {
+  const {
+    colorSchemes,
+    breakpoints,
+    spacing,
+    getCssVar,
+    palette: paletteInput,
+    ...scales
+  } = extendTheme(themeInput);
+  const colorSchemePalette = deepmerge(
+    colorSchemes[paletteInput?.colorScheme || 'light'].palette,
+    paletteInput,
+  );
+  const {
+    mode = 'light',
+    colorScheme = 'light',
+    ...palette
+  } = colorSchemePalette as RuntimeColorSystem['palette'];
+
+  const defaultTheme = {
+    ...scales,
+    colorSchemes: {
+      ...colorSchemes,
+      [colorScheme]: palette,
+    },
+    palette: {
+      ...palette,
+      mode,
+      colorScheme,
+    },
+    breakpoints,
+    spacing,
+    getCssVar,
+    vars: { ...scales, palette },
+  };
+  return defaultTheme;
+};
+
+const defaultTheme = getThemeWithVars();
 
 export default defaultTheme;
