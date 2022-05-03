075ffdc47cfab438fe765ff1c6c89b6808c41e0e
siriwatknp <siriwatkunaporn@gmail.com>
2022-05-03 14:30:15 +0700

    add joy button page

--- docs/data/joy/pages.ts
+++ docs/data/joy/pages.ts
@@ -1,3 +1,16 @@
-const pages: Array<any> = [];
+const pages: Array<any> = [
+  {
+    pathname: '/joy-ui/react-',
+    title: 'Components',
+    icon: 'ToggleOnIcon',
+    children: [
+      {
+        pathname: '/joy-ui/components/inputs',
+        subheader: 'inputs',
+        children: [{ pathname: '/joy-ui/react-button' }],
+      },
+    ],
+  },
+];
 
 export default pages;
