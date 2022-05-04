2b69d220b2954a57bd04c028d44f347325dab411
Haneen Mahdin <haneenmahdin@gmail.com>
2022-05-04 16:28:20 +0530

    [MobileStepper] Add support for CSS vars

---
+++ docs/pages/experiments/material-ui/mobile-stepper.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  useColorScheme,
+} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Moon from '@mui/icons-material/DarkMode';
+import Sun from '@mui/icons-material/LightMode';
+import { MobileStepper } from '@mui/material';
+
+const ColorSchemePicker = () => {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <Button
+      variant="outlined"
+      onClick={() => {
+        if (mode === 'light') {
+          setMode('dark');
+        } else {
+          setMode('light');
+        }
+      }}
+    >
+      {mode === 'light' ? <Moon /> : <Sun />}
+    </Button>
+  );
+};
+
+export default function CssVarsTemplate() {
+  const [activeStep, setStep] = React.useState(3);
+
+  const handleIncrease = () => setStep(activeStep + 1);
+  const handleDecrease = () => setStep(activeStep - 1);
+
+  return (
+    <CssVarsProvider>
+      <CssBaseline />
+      <Container sx={{ my: 5 }}>
+        <Box sx={{ pb: 2 }}>
+          <ColorSchemePicker />
+        </Box>
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(256px, 1fr))',
+            gridAutoRows: 'minmax(160px, auto)',
+            gap: 2,
+            '& > div': {
+              placeSelf: 'center',
+            },
+          }}
+        >
+          <Box>Mobile Stepper</Box>
+
+          <Box>
+            <MobileStepper
+              steps={20}
+              activeStep={activeStep}
+              backButton={<Button onClick={handleDecrease}>Decrement</Button>}
+              nextButton={<Button onClick={handleIncrease}>Increment</Button>}
+            />
+          </Box>
+        </Box>
+      </Container>
+    </CssVarsProvider>
+  );
+}
