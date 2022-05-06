85410f37c8b0efe1766f5cf369a275286148eede
José Rodolfo Freitas <joserodolfo.freitas@gmail.com>
2022-05-06 20:49:06 +0200

    Simplifying Update docs/src/components/pricing/FAQ.tsx
    Co-authored-by: Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>

--- docs/src/components/pricing/FAQ.tsx
+++ docs/src/components/pricing/FAQ.tsx
@@ -90,10 +90,9 @@ const faqData = [
     summary: 'Am I allowed to use the product after the update entitlement expires?',
     detail: (
       <React.Fragment>
-        Yes. You're allowed to continue using any version of the product in production environments,
-        even after the entitlement expires. However, you will be required to renew your license if
-        you wish to continue development, and to access subsequently released functionality and
-        updates, as well as technical support.
+        Yes. 
+        You can continue to use the product in production environments after the entitlement expires.
+        But you will need to renew your license to continue development, update for new features, or gain access to technical support.
         <br />
         <br />
         To renew your license, please{' '}
