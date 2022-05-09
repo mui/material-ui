8f614b2912f7aca34d2cf3055c626f694584d86a
Ali Sasani <alisasanibab@gmail.com>
2022-05-09 18:50:32 +0430

    add tests to check activeChannel values

--- packages/mui-material/src/styles/experimental_extendTheme.test.js
+++ packages/mui-material/src/styles/experimental_extendTheme.test.js
@@ -85,6 +85,9 @@ describe('experimental_extendTheme', () => {
 
     expect(theme.colorSchemes.light.palette.grey.darkChannel).to.equal('97 97 97');
     expect(theme.colorSchemes.dark.palette.grey.darkChannel).to.equal('97 97 97');
+
+    expect(theme.colorSchemes.dark.palette.action.activeChannel).to.equal('255 255 255');
+    expect(theme.colorSchemes.light.palette.action.activeChannel).to.equal('0 0 0');
   });
 
   it('should change grey darkChannel', () => {
