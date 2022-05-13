518771fdb0200a2f473f18619cbb76cb4dc317b7
siriwatknp <siriwatkunaporn@gmail.com>
2022-05-13 18:00:56 +0700

    fix build api script

--- docs/scripts/ApiBuilders/ComponentApiBuilder.ts
+++ docs/scripts/ApiBuilders/ComponentApiBuilder.ts
@@ -498,8 +498,6 @@ const attachPropsTable = (reactApi: ReactApi) => {
   reactApi.propsTable = componentProps;
 };
 
-const systemComponents = ['Container', 'Box'];
-
 /**
  * - Build react component (specified filename) api by lookup at its definition (.d.ts or ts)
  *   and then generate the API page + json data
@@ -518,6 +516,7 @@ const generateComponentApi = async (componentInfo: ComponentInfo, program: ttp.t
     getDemos,
     readFile,
     skipApiGeneration,
+    isSystemComponent,
   } = componentInfo;
 
   const { shouldSkip, spread, EOL, src } = readFile();
@@ -528,36 +527,49 @@ const generateComponentApi = async (componentInfo: ComponentInfo, program: ttp.t
 
   let reactApi: ReactApi;
 
-  if (systemComponents.includes(name)) {
-    reactApi = docgenParse(
-      src,
-      (ast) => {
-        let node;
-        astTypes.visit(ast, {
-          visitVariableDeclaration: (variablePath) => {
-            const definitions: any[] = [];
-            if (variablePath.node.declarations) {
-              variablePath
-                .get('declarations')
-                .each((declarator: any) => definitions.push(declarator.get('init')));
-            }
-
-            definitions.forEach((definition) => {
-              const definitionName = definition.value.callee.name;
-
-              if (definitionName === `create${name}`) {
-                node = definition;
+  if (isSystemComponent) {
+    try {
+      reactApi = docgenParse(
+        src,
+        (ast) => {
+          let node;
+          astTypes.visit(ast, {
+            visitVariableDeclaration: (variablePath) => {
+              const definitions: any[] = [];
+              if (variablePath.node.declarations) {
+                variablePath
+                  .get('declarations')
+                  .each((declarator: any) => definitions.push(declarator.get('init')));
               }
-            });
-            return false;
-          },
-        });
 
-        return node;
-      },
-      defaultHandlers,
-      { filename },
-    );
+              definitions.forEach((definition) => {
+                if (definition.value?.callee) {
+                  const definitionName = definition.value.callee.name;
+
+                  if (definitionName === `create${name}`) {
+                    node = definition;
+                  }
+                }
+              });
+              return false;
+            },
+          });
+
+          return node;
+        },
+        defaultHandlers,
+        { filename },
+      );
+    } catch (error) {
+      // fallback to default logic if there is no `create*` definition.
+      if ((error as Error).message === 'No suitable component definition found.') {
+        reactApi = docgenParse(src, null, defaultHandlers.concat(muiDefaultPropsHandler), {
+          filename,
+        });
+      } else {
+        throw error;
+      }
+    }
   } else {
     reactApi = docgenParse(src, null, defaultHandlers.concat(muiDefaultPropsHandler), { filename });
   }
