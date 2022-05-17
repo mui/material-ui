305240907755c89b4c0fc8f13d03b885ddc5cfa9
siriwatknp <siriwatkunaporn@gmail.com>
2022-05-17 12:02:33 +0700

    add blank pages

---
+++ docs/pages/experiments/joy/example-ui/email.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function EmailExample() {
+  return <div>Email example</div>;
+}
---
+++ docs/pages/experiments/joy/example-ui/files.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function FileExample() {
+  return <div>File example</div>;
+}
