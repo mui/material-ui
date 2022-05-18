f6e023c6b43a3e4cbb255b249b0266bcb57a993a
danilo leal <67129314+danilo-leal@users.noreply.github.com>
2022-05-17 22:14:22 -0300

    starting to set up the list component

--- docs/pages/experiments/joy/example-ui/email.tsx
+++ docs/pages/experiments/joy/example-ui/email.tsx
@@ -3,6 +3,13 @@ import { GlobalStyles } from '@mui/system';
 import { CssVarsProvider, Theme } from '@mui/joy/styles';
 import Box from '@mui/joy/Box';
 import Sheet from '@mui/joy/Sheet';
+import Typography from '@mui/joy/Typography';
+import List from '@mui/joy/List';
+import ListItem from '@mui/joy/ListItem';
+import ListItemButton from '@mui/joy/ListItemButton';
+import ListItemDecorator from '@mui/joy/ListItemDecorator';
+import ListItemContent from '@mui/joy/ListItemContent';
+import InboxIcon from '@mui/icons-material/Inbox';
 
 export default function EmailExample() {
   return (
@@ -21,7 +28,7 @@ export default function EmailExample() {
           gridTemplateColumns: {
             xs: '0px 0px 1fr',
             sm: '64px minmax(200px, 1fr) minmax(500px, 1fr)',
-            md: 'minmax(160px, 220px) minmax(200px, 351px) minmax(700px, 1fr)',
+            md: 'minmax(160px, 260px) minmax(200px, 480px) minmax(700px, 1fr)',
           },
           gridTemplateRows: '64px 1fr',
           minHeight: '100vh',
@@ -48,7 +55,48 @@ export default function EmailExample() {
             p: 2,
           }}
         >
-          Navigation
+          <Box sx={{ mb: 2 }}>
+            <Typography
+              color="neutral.500"
+              fontWeight={700}
+              sx={{ fontSize: '10px', textTransform: 'uppercase', letterSpacing: '.1rem' }}
+            >
+              Browse
+            </Typography>
+          </Box>
+
+          <List
+            sx={{
+              '--List-item-radius': '8px',
+              '--List-decorator-width': '32px',
+              '& .MuiListItemButton-root': { p: '8px' },
+            }}
+          >
+            <ListItem>
+              <ListItemButton variant="soft" color="primary">
+                <ListItemDecorator sx={{ color: 'inherit' }}>
+                  <InboxIcon fontSize="small" />
+                </ListItemDecorator>
+                <ListItemContent>Inbox</ListItemContent>
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton>
+                <ListItemDecorator sx={{ color: 'inherit' }}>
+                  <InboxIcon fontSize="small" />
+                </ListItemDecorator>
+                <ListItemContent>Inbox</ListItemContent>
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton>
+                <ListItemDecorator sx={{ color: 'inherit' }}>
+                  <InboxIcon fontSize="small" />
+                </ListItemDecorator>
+                <ListItemContent>Inbox</ListItemContent>
+              </ListItemButton>
+            </ListItem>
+          </List>
         </Box>
         <Box
           className="Inbox"
