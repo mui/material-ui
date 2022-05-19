ae33abab12418a2eee0683a9a8ae290d51411041
danilo leal <67129314+danilo-leal@users.noreply.github.com>
2022-05-19 00:18:59 -0300

    simplify demo

--- docs/data/material/components/multiple-snackbars/CustomizedSnackbars.js
+++ docs/data/material/components/multiple-snackbars/CustomizedSnackbars.js
@@ -32,9 +32,6 @@ function MyApp() {
       >
         Open success snackbar
       </Button>
-      <Alert severity="error">This is an error message!</Alert>
-      <Alert severity="warning">This is a warning message!</Alert>
-      <Alert severity="info">This is an information message!</Alert>
       <Alert severity="success">This is a success message!</Alert>
     </Stack>
   );
