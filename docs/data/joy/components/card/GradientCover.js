1a854cdc5f1cd98398f00110088e042791880422
danilo leal <67129314+danilo-leal@users.noreply.github.com>
2022-05-19 02:58:19 -0300

    gradient overal design tweak

--- docs/data/joy/components/card/GradientCover.js
+++ docs/data/joy/components/card/GradientCover.js
@@ -4,36 +4,30 @@ import Card from '@mui/joy/Card';
 import CardCover from '@mui/joy/CardCover';
 import CardContent from '@mui/joy/CardContent';
 import Typography from '@mui/joy/Typography';
-import StarBorder from '@mui/icons-material/StarBorder';
-import LocationOn from '@mui/icons-material/LocationOnOutlined';
+import LocationOnRoundedIcon from '@mui/icons-material/LocationOnRounded';
 
 export default function GradientCover() {
   return (
-    <Card size="lg" sx={{ minHeight: '300px', minWidth: 240 }}>
+    <Card sx={{ minHeight: '280px', minWidth: 320 }}>
       <CardCover>
         <img
-          src="https://images.unsplash.com/photo-1525630558331-067c957817a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2250&q=80"
+          src="https://images.unsplash.com/photo-1542773998-9325f0a098d7?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3271"
           alt=""
         />
       </CardCover>
       <CardCover
         sx={{
           background:
-            'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 100px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 200px)',
+            'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)',
         }}
       />
       <CardContent sx={{ justifyContent: 'flex-end' }}>
         <Typography level="h2" fontSize="lg" color="#fff" mb={1}>
-          The Beach
+          Yosemite National Park
         </Typography>
-        <Box sx={{ display: 'flex', gap: 2 }}>
-          <Typography startDecorator={<LocationOn />} color="neutral.300">
-            Tarifa, Spain
-          </Typography>
-          <Typography startDecorator={<StarBorder />} color="neutral.300">
-            4.8
-          </Typography>
-        </Box>
+        <Typography startDecorator={<LocationOnRoundedIcon />} color="neutral.300">
+          California, USA
+        </Typography>
       </CardContent>
     </Card>
   );
