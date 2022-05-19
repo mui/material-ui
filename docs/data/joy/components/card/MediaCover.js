f11cd7ec1c66fc762873d045ce1312942acbec5d
danilo leal <67129314+danilo-leal@users.noreply.github.com>
2022-05-19 02:53:38 -0300

    media card design tweak

--- docs/data/joy/components/card/MediaCover.js
+++ docs/data/joy/components/card/MediaCover.js
@@ -11,25 +11,25 @@ export default function BasicCard() {
       component="ul"
       sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', p: 0, m: 0 }}
     >
-      <Card component="li" sx={{ minWidth: 200, flexGrow: 1 }}>
+      <Card component="li" sx={{ minWidth: 300, flexGrow: 1 }}>
         <CardCover>
           <img
-            src="https://images.unsplash.com/photo-1483683804023-6ccdb62f86ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2835&q=80"
+            src="https://images.unsplash.com/photo-1502657877623-f66bf489d236?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369"
             alt=""
           />
         </CardCover>
         <CardContent sx={{ justifyContent: 'center', gap: 1 }}>
           <Typography
-            typography="h2"
+            level="h6"
+            fontWeight="lg"
             color="#fff"
-            mt={{ xs: 12, sm: 16 }}
-            sx={{ textShadow: '0 0 8px rgba(255,255,255,1)' }}
+            mt={{ xs: 12, sm: 18 }}
           >
-            IMAGE
+            Image
           </Typography>
         </CardContent>
       </Card>
-      <Card component="li" sx={{ minWidth: 200, flexGrow: 1 }}>
+      <Card component="li" sx={{ minWidth: 300, flexGrow: 1 }}>
         <CardCover>
           <video
             autoPlay
@@ -45,12 +45,12 @@ export default function BasicCard() {
         </CardCover>
         <CardContent sx={{ justifyContent: 'center', gap: 1 }}>
           <Typography
-            typography="h2"
+            level="h6"
+            fontWeight="lg"
             color="#fff"
-            mt={{ xs: 12, sm: 16 }}
-            sx={{ textShadow: '0 0 8px rgba(255,255,255,1)' }}
+            mt={{ xs: 12, sm: 18 }}
           >
-            VIDEO
+            Video
           </Typography>
         </CardContent>
       </Card>
