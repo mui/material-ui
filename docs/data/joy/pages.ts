23c8b9f2fd7f1c417c5b8b06cd01f647b4db26f0
danilo leal <67129314+danilo-leal@users.noreply.github.com>
2022-05-20 00:09:36 -0300

    add the page

--- docs/data/joy/pages.ts
+++ docs/data/joy/pages.ts
@@ -3,6 +3,7 @@ const pages = [
     pathname: '/joy-ui/getting-started',
     icon: 'DescriptionIcon',
     children: [
+      { pathname: '/joy-ui/getting-started/overview' },
       { pathname: '/joy-ui/getting-started/quick-start' },
       { pathname: '/joy-ui/getting-started/tutorial' },
       { pathname: '/joy-ui/getting-started/principles' },
