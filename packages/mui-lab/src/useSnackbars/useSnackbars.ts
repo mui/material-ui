834bf996837777ae65f99948c41b35f9d0286e5b
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2022-05-24 13:22:26 +0530

    refactor: improve stacked snackbars context error message and use MuiError

--- packages/mui-lab/src/useSnackbars/useSnackbars.ts
+++ packages/mui-lab/src/useSnackbars/useSnackbars.ts
@@ -1,11 +1,16 @@
 import * as React from 'react';
+import MuiError from '@mui/utils/macros/MuiError.macro';
 import SnackbarsContext, { SnackbarsContextProps } from '../SnackbarsProvider/SnackbarsContext';
 
 export default function useSnackbars(): SnackbarsContextProps {
   const context = React.useContext(SnackbarsContext);
 
   if (context === undefined) {
-    throw new Error('MUI: useSnackbars must be within a SnackbarsProvider');
+    throw new MuiError(
+      'MUI: useSnackbars must be within a SnackbarsProvider. ' +
+        'Make sure the component where you are using the useSnackbars hook is rendered within the SnackbarsProvider context. ' +
+        'See https://mui.com/material-ui/react-stacked-snackbars/#usage for more info.',
+    );
   }
 
   return context;
