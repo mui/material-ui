610c9944535e27213e4822ceb18d309a3dade354
danilo leal <67129314+danilo-leal@users.noreply.github.com>
2022-05-31 00:27:48 -0300

    fix lint

--- docs/pages/experiments/joy/example-ui/files.tsx
+++ docs/pages/experiments/joy/example-ui/files.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import * as React from 'react';
 import { GlobalStyles } from '@mui/system';
 import { CssVarsProvider, useColorScheme } from '@mui/joy/styles';
