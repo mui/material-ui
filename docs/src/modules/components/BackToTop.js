c447d77863970a1f80e582d20c7b12246a1c4bde
danilo leal <67129314+danilo-leal@users.noreply.github.com>
2022-06-02 03:24:55 -0300

    styling adjustments

--- docs/src/modules/components/BackToTop.js
+++ docs/src/modules/components/BackToTop.js
@@ -22,35 +22,44 @@ export default function BackToTop() {
     <Fade in={trigger}>
       <Tooltip title="Scroll to top">
         <Fab
-          sx={{
+          sx={(theme) => ({
             position: 'fixed',
             bottom: 24,
             right: 24,
             zIndex: 10,
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'dark' ? theme.palette.grey[400] : theme.palette.grey[300],
-            boxShadow: (theme) =>
-              `0px 4px 20px ${
-                theme.palette.mode === 'dark' ? 'rgba(0, 0, 0, 0.5)' : 'rgba(170, 180, 190, 0.3)'
-              }`,
+            backgroundColor:
+              theme.palette.mode === 'dark'
+                ? theme.palette.primaryDark[400]
+                : theme.palette.primary[100],
+            boxShadow: `0px 4px 20px ${
+              theme.palette.mode === 'dark' ? 'rgba(0, 0, 0, 0.5)' : 'rgba(170, 180, 190, 0.3)'
+            }`,
             '&:hover': {
-              backgroundColor: (theme) =>
-                theme.palette.mode === 'dark' ? theme.palette.grey[300] : theme.palette.grey[400],
+              backgroundColor:
+                theme.palette.mode === 'dark'
+                  ? theme.palette.primaryDark[500]
+                  : theme.palette.primary[200],
             },
             '&:active': {
-              boxShadow: (theme) =>
-                `0px 4px 20px ${
-                  theme.palette.mode === 'dark' ? 'rgba(0, 0, 0, 0.7)' : 'rgba(170, 180, 190, 0.6)'
-                }`,
+              boxShadow: `0px 4px 20px ${
+                theme.palette.mode === 'dark' ? 'rgba(0, 0, 0, 0.7)' : 'rgba(170, 180, 190, 0.6)'
+              }`,
             },
-          }}
+          })}
           size="small"
           aria-label={t('backToTop')}
           onClick={handleClick}
           data-ga-event-category="docs"
           data-ga-event-action="click-back-to-top"
         >
-          <KeyboardArrowUpRoundedIcon />
+          <KeyboardArrowUpRoundedIcon
+            sx={{
+              color: (theme) =>
+                theme.palette.mode === 'dark'
+                  ? theme.palette.primary[200]
+                  : theme.palette.primary[800],
+            }}
+          />
         </Fab>
       </Tooltip>
     </Fade>
