c87334f2e4b4862828132f7e3e9f266e85d28ed4
siriwatknp <siriwatkunaporn@gmail.com>
2022-06-03 22:46:21 +0700

    fix all components

--- packages/mui-joy/src/FormHelperText/formHelperTextClasses.ts
+++ packages/mui-joy/src/FormHelperText/formHelperTextClasses.ts
@@ -1,4 +1,4 @@
-import { generateUtilityClass, generateUtilityClasses } from '@mui/base';
+import { generateUtilityClass, generateUtilityClasses } from '../className';
 
 export interface FormHelperTextClasses {
   /** Styles applied to the root element. */
