2650f4800a794fa2745fe81b965f3f865b33b299
Flavien DELANGLE <flaviendelangle@gmail.com>
2022-06-04 22:00:13 +0200

    [pickers] Add deprecations when importing pickers from the lab (#32950)

--- packages/mui-lab/src/PickersDay/index.ts
+++ packages/mui-lab/src/PickersDay/index.ts
@@ -1,6 +1,3 @@
-export {
-  PickersDay as default,
-  pickersDayClasses,
-  getPickersDayUtilityClass,
-} from '@mui/x-date-pickers/PickersDay';
+export { default } from './PickersDay';
+export { pickersDayClasses, getPickersDayUtilityClass } from '@mui/x-date-pickers/PickersDay';
 export type { PickersDayClassKey, PickersDayProps } from '@mui/x-date-pickers/PickersDay';
