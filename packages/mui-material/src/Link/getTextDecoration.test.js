6fded2b11f258357b300dfe71632562944a95181
siriwatknp <siriwatkunaporn@gmail.com>
2022-06-09 16:23:40 +0700

    fix tests to apply 'vars' to theme

--- packages/mui-material/src/Link/getTextDecoration.test.js
+++ packages/mui-material/src/Link/getTextDecoration.test.js
@@ -58,47 +58,60 @@ describe('getTextDecoration', () => {
   describe('CSS variables', () => {
     const theme = extendTheme();
     theme.palette = theme.colorSchemes.light.palette;
-    theme.vars = theme.colorSchemes.light;
-    theme.vars.palette.primary.mainChannel = 'var(--palette-primary-main)';
-    theme.vars.palette.secondary.mainChannel = 'var(--palette-secondary-main)';
-    theme.vars.palette.text.primaryChannel = 'var(--palette-text-primary)';
-    theme.vars.palette.text.secondaryChannel = 'var(--palette-text-secondary)';
-    theme.vars.palette.error.mainChannel = 'var(--palette-error-main)';
+    theme.vars = {
+      palette: {
+        primary: {
+          mainChannel: 'var(--palette-primary-mainChannel)',
+        },
+        secondary: {
+          mainChannel: 'var(--palette-secondary-mainChannel)',
+        },
+        text: {
+          primaryChannel: 'var(--palette-text-primaryChannel)',
+          secondaryChannel: 'var(--palette-text-secondaryChannel)',
+        },
+        error: {
+          mainChannel: 'var(--palette-error-mainChannel)',
+        },
+      },
+    };
     // in the application, the value will be CSS variable: `rgba(var(--the-color-channel) / 0.4)`
     it('deprecated color', () => {
       expect(getTextDecoration({ theme, ownerState: { color: 'primary' } })).to.equal(
-        'rgba(var(--palette-primary-main) / 0.4)',
+        'rgba(var(--palette-primary-mainChannel) / 0.4)',
       );
       expect(getTextDecoration({ theme, ownerState: { color: 'textPrimary' } })).to.equal(
-        'rgba(var(--palette-text-primary) / 0.4)',
+        'rgba(var(--palette-text-primaryChannel) / 0.4)',
       );
       expect(getTextDecoration({ theme, ownerState: { color: 'secondary' } })).to.equal(
-        'rgba(var(--palette-secondary-main) / 0.4)',
+        'rgba(var(--palette-secondary-mainChannel) / 0.4)',
       );
       expect(getTextDecoration({ theme, ownerState: { color: 'textSecondary' } })).to.equal(
-        'rgba(var(--palette-text-secondary) / 0.4)',
+        'rgba(var(--palette-text-secondaryChannel) / 0.4)',
       );
       expect(getTextDecoration({ theme, ownerState: { color: 'error' } })).to.equal(
-        'rgba(var(--palette-error-main) / 0.4)',
+        'rgba(var(--palette-error-mainChannel) / 0.4)',
       );
     });
 
     it('system color', () => {
       expect(getTextDecoration({ theme, ownerState: { color: 'primary.main' } })).to.equal(
-        'rgba(var(--palette-primary-main) / 0.4)',
+        'rgba(var(--palette-primary-mainChannel) / 0.4)',
       );
       expect(getTextDecoration({ theme, ownerState: { color: 'text.primary' } })).to.equal(
-        'rgba(var(--palette-text-primary) / 0.4)',
+        'rgba(var(--palette-text-primaryChannel) / 0.4)',
       );
       expect(getTextDecoration({ theme, ownerState: { color: 'secondary.main' } })).to.equal(
-        'rgba(var(--palette-secondary-main) / 0.4)',
+        'rgba(var(--palette-secondary-mainChannel) / 0.4)',
       );
       expect(getTextDecoration({ theme, ownerState: { color: 'text.secondary' } })).to.equal(
-        'rgba(var(--palette-text-secondary) / 0.4)',
+        'rgba(var(--palette-text-secondaryChannel) / 0.4)',
       );
       expect(getTextDecoration({ theme, ownerState: { color: 'error.main' } })).to.equal(
-        'rgba(var(--palette-error-main) / 0.4)',
+        'rgba(var(--palette-error-mainChannel) / 0.4)',
       );
+
+      // it fallback to use `theme.palette.grey.500` with alpha() if it could not find channel color
       expect(getTextDecoration({ theme, ownerState: { color: 'grey.500' } })).to.equal(
         'rgba(158, 158, 158, 0.4)',
       );
