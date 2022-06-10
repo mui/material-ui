d579e804ef9de677a5b671354d1631c3792ba15b
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2022-06-10 14:30:55 +0530

    add some props

--- packages/mui-base/src/SnackbarUnstyled/SnackbarUnstyled.tsx
+++ packages/mui-base/src/SnackbarUnstyled/SnackbarUnstyled.tsx
@@ -1,34 +1,75 @@
 import * as React from 'react';
 import clsx from 'clsx';
+import { unstable_useEventCallback as useEventCallback } from '@mui/utils';
 import ClickAwayListener from '../ClickAwayListener';
-import { SnackbarUnstyledProps } from './SnackbarUnstyled.types';
+import { SnackbarCloseReason, SnackbarUnstyledProps } from './SnackbarUnstyled.types';
 
 const SnackbarUnstyled = (props: SnackbarUnstyledProps) => {
   const {
+    autoHideDuration = null,
     children,
+    className,
     ClickAwayListenerProps,
     component,
     components = {},
     componentsProps = {},
+    onBlur,
     onClose,
-    className,
+    resumeHideDuration,
     ...other
   } = props;
+
+  const timerAutoHide = React.useRef<ReturnType<typeof setTimeout>>();
 
   const Root = component || components.Root || 'div';
 
   const TransitionComponent = components.Transition;
 
-  const rootProps = {
-    ...other,
-    ...componentsProps.root,
-    className: clsx(className, componentsProps.root?.className),
-  };
+  const handleClose = useEventCallback(
+    (event: Event | React.SyntheticEvent<any, Event> | null, reason: SnackbarCloseReason) => {
+      if (onClose) {
+        onClose(event, reason);
+      }
+    },
+  );
+
+  const setAutoHideTimer = useEventCallback((autoHideDurationParam: number) => {
+    if (!onClose || autoHideDurationParam == null) {
+      return;
+    }
+
+    clearTimeout(timerAutoHide.current);
+    timerAutoHide.current = setTimeout(() => {
+      handleClose(null, 'timeout');
+    }, autoHideDurationParam);
+  });
 
   const handleClickAway = (event: React.SyntheticEvent<any> | Event) => {
     if (onClose) {
       onClose(event, 'clickaway');
     }
+  };
+
+  // Restart the timer when the user is no longer interacting with the Snackbar
+  // or when the window is shown back.
+  const handleResume = React.useCallback(() => {
+    if (autoHideDuration != null) {
+      setAutoHideTimer(resumeHideDuration != null ? resumeHideDuration : autoHideDuration * 0.5);
+    }
+  }, [autoHideDuration, resumeHideDuration, setAutoHideTimer]);
+
+  const handleBlur = (event: React.FocusEvent) => {
+    if (onBlur) {
+      onBlur(event);
+    }
+    handleResume();
+  };
+
+  const rootProps = {
+    onBlur: handleBlur,
+    ...other,
+    ...componentsProps.root,
+    className: clsx(className, componentsProps.root?.className),
   };
 
   return (
--- packages/mui-base/src/SnackbarUnstyled/SnackbarUnstyled.types.ts
+++ packages/mui-base/src/SnackbarUnstyled/SnackbarUnstyled.types.ts
@@ -4,6 +4,14 @@ import { ClickAwayListenerProps } from '../ClickAwayListener';
 export type SnackbarCloseReason = 'timeout' | 'clickaway' | 'escapeKeyDown';
 
 export interface SnackbarUnstyledProps<TransitionType = {}> {
+  /**
+   * The number of milliseconds to wait before automatically calling the
+   * `onClose` function. `onClose` should then set the state of the `open`
+   * prop to hide the Snackbar. This behavior is disabled by default with
+   * the `null` value.
+   * @default null
+   */
+  autoHideDuration?: number | null;
   className?: string;
   children?: React.ReactElement<any, any>;
   /**
@@ -33,6 +41,7 @@ export interface SnackbarUnstyledProps<TransitionType = {}> {
     root?: React.HTMLAttributes<HTMLDivElement>;
     transition?: TransitionType;
   };
+  onBlur?: React.FocusEventHandler;
   /**
    * Callback fired when the component requests to be closed.
    * Typically `onClose` is used to set state in the parent component,
@@ -43,5 +52,12 @@ export interface SnackbarUnstyledProps<TransitionType = {}> {
    * @param {React.SyntheticEvent<any> | Event} event The event source of the callback.
    * @param {string} reason Can be: `"timeout"` (`autoHideDuration` expired), `"clickaway"`, or `"escapeKeyDown"`.
    */
-  onClose?: (event: React.SyntheticEvent<any> | Event, reason: SnackbarCloseReason) => void;
+  onClose?: (event: React.SyntheticEvent<any> | Event | null, reason: SnackbarCloseReason) => void;
+  /**
+   * The number of milliseconds to wait before dismissing after user interaction.
+   * If `autoHideDuration` prop isn't specified, it does nothing.
+   * If `autoHideDuration` prop is specified but `resumeHideDuration` isn't,
+   * we default to `autoHideDuration / 2` ms.
+   */
+  resumeHideDuration?: number;
 }
