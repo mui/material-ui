6007697c2c0ad4d40c630e7e1348122f65cc4297
siriwatknp <siriwatkunaporn@gmail.com>
2022-06-13 17:19:00 +0700

    use .tsx

--- packages/mui-material/src/styles/CssVarsProvider.tsx
+++ packages/mui-material/src/styles/CssVarsProvider.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { unstable_createCssVarsProvider as createCssVarsProvider } from '@mui/system';
-import experimental_extendTheme from './experimental_extendTheme';
+import experimental_extendTheme, { SupportedColorScheme, Theme } from './experimental_extendTheme';
 import createTypography from './createTypography';
 
 const defaultTheme = experimental_extendTheme();
@@ -9,7 +9,7 @@ const {
   CssVarsProvider: Experimental_CssVarsProvider,
   useColorScheme,
   getInitColorSchemeScript,
-} = createCssVarsProvider({
+} = createCssVarsProvider<SupportedColorScheme, Theme>({
   theme: defaultTheme,
   attribute: 'data-mui-color-scheme',
   modeStorageKey: 'mui-mode',
@@ -18,7 +18,7 @@ const {
     light: 'light',
     dark: 'dark',
   },
-  prefix: 'md',
+  prefix: 'mui',
   resolveTheme: (theme) => {
     const newTheme = {
       ...theme,
