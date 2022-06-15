9404b2d4c52819789cd082751d2bc073393aa483
siriwatknp <siriwatkunaporn@gmail.com>
2022-06-15 12:57:16 +0700

    remove base related

--- packages/mui-joy/src/className/index.ts
+++ packages/mui-joy/src/className/index.ts
@@ -1,14 +1,6 @@
 import { unstable_generateUtilityClass, unstable_generateUtilityClasses } from '@mui/utils';
 
-/**
- * Caution! this module must not include unstyled components import from `@mui/base`, otherwise, it will break the ClassNameGenerator.
- * ❌ import { ... } from '@mui/base';
- * ✅ import { ... } from '@mui/base/utils'; // must be specific base module
- *
- * Issue: https://github.com/mui/material-ui/issues/30011#issuecomment-1024993401
- */
-
-export { unstable_ClassNameGenerator } from '@mui/base/className';
+export { unstable_ClassNameGenerator } from '@mui/utils';
 
 export const generateUtilityClass = (componentName: string, slot: string) =>
   unstable_generateUtilityClass(componentName, slot, 'Joy');
