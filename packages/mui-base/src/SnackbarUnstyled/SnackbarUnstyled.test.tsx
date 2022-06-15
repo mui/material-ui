06a8e065f4bce82947bd9f827b8c3e982a80c0ea
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2022-06-15 11:53:10 +0530

    add SnackbarUnstyled describeConformanceUnstyled

---
+++ packages/mui-base/src/SnackbarUnstyled/SnackbarUnstyled.test.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { createRenderer, createMount, describeConformanceUnstyled } from 'test/utils';
+import SnackbarUnstyled, { snackbarUnstyledClasses as classes } from '@mui/base/SnackbarUnstyled';
+
+describe('<SnackbarUnstyled />', () => {
+  const { render } = createRenderer();
+  const mount = createMount();
+
+  describeConformanceUnstyled(
+    <SnackbarUnstyled open>
+      <div />
+    </SnackbarUnstyled>,
+    () => ({
+      classes,
+      inheritComponent: 'div',
+      render,
+      mount,
+      refInstanceof: window.HTMLDivElement,
+      muiName: 'BaseSnackbar',
+      slots: {
+        root: {
+          expectedClassName: classes.root,
+        },
+      },
+      skip: ['componentsPropsCallbacks', 'ownerStatePropagation'], // not implemented yet
+    }),
+  );
+});
