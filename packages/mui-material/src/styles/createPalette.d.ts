f043f3a51bf5e4198d49b40f3101099b3c83b228
gin1314 <eugene.santos@smartchartsfx.com>
2022-06-17 18:25:59 +0800

    [SpeedDialAction] Add support for CSS variables (#32608)

--- packages/mui-material/src/styles/createPalette.d.ts
+++ packages/mui-material/src/styles/createPalette.d.ts
@@ -167,6 +167,9 @@ export interface PaletteWithChannels {
   SnackbarContent: {
     bg: string;
   };
+  SpeedDialAction: {
+    fabHoverBg: string;
+  };
   StepConnector: {
     border: string;
   };
