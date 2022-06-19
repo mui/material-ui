7d4a4a37addd6edbcd97f92ae4df35f66058ffa2
siriwatknp <siriwatkunaporn@gmail.com>
2022-06-19 09:09:44 +0700

    add theme augmentation

--- packages/mui-material/src/styles/experimental_extendTheme.d.ts
+++ packages/mui-material/src/styles/experimental_extendTheme.d.ts
@@ -1,12 +1,9 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { ThemeOptions as SystemThemeOptions, Theme as SystemTheme } from '@mui/system';
 import { OverridableStringUnion } from '@mui/types';
-import { Mixins, MixinsOptions } from './createMixins';
+import { ThemeOptions, Theme } from './createTheme';
 import { Palette, PaletteOptions } from './createPalette';
-import { Typography, TypographyOptions } from './createTypography';
 import { Shadows } from './shadows';
-import { Transitions, TransitionsOptions } from './createTransitions';
-import { ZIndex, ZIndexOptions } from './zIndex';
+import { ZIndex } from './zIndex';
 import { Components } from './components';
 
 /**
@@ -67,45 +64,180 @@ export type Overlays = [
   string | undefined,
 ];
 
-export interface ThemeOptions extends Omit<SystemThemeOptions, 'zIndex'> {
-  mixins?: MixinsOptions;
-  components?: Components<BaseTheme>;
-  colorSchemes?: Record<
-    SupportedColorScheme,
-    {
-      palette?: PaletteOptions;
-      opacity?: Partial<Opacity>;
-      overlays?: Overlays;
-    }
-  >;
-  shadows?: Shadows;
-  transitions?: TransitionsOptions;
-  typography?: TypographyOptions | ((palette: Palette) => TypographyOptions);
-  zIndex?: ZIndexOptions;
-  unstable_strictMode?: boolean;
+export interface PaletteCommonChannel {
+  background: string;
+  backgroundChannel: string;
+  onBackground: string;
+  onBackgroundChannel: string;
 }
 
-interface BaseTheme extends SystemTheme {
-  mixins: Mixins;
-  palette: Palette;
+export interface PaletteColorChannel {
+  mainChannel: string;
+  lightChannel: string;
+  darkChannel: string;
+  contrastTextChannel: string;
+}
+
+export interface PaletteActionChannel {
+  activeChannel: string;
+  selectedChannel: string;
+}
+
+export interface PaletteTextChannel {
+  primaryChannel: string;
+  secondaryChannel: string;
+  disabledChannel: string;
+}
+
+export interface PaletteAppBar {
+  defaultBg: string;
+  darkBg: string;
+  darkColor: string;
+}
+
+export interface PaletteAvatar {
+  defaultBg: string;
+}
+
+export interface PaletteChip {
+  defaultBorder: string;
+  defaultAvatarColor: string;
+  defaultIconColor: string;
+}
+
+export interface PaletteFilledInput {
+  bg: string;
+  hoverBg: string;
+  disabledBg: string;
+}
+
+export interface PaletteLinearProgress {
+  primaryBg: string;
+  secondaryBg: string;
+  errorBg: string;
+  infoBg: string;
+  successBg: string;
+  warningBg: string;
+}
+
+export interface PaletteSlider {
+  primaryTrack: string;
+  secondaryTrack: string;
+  errorTrack: string;
+  infoTrack: string;
+  successTrack: string;
+  warningTrack: string;
+}
+
+export interface PaletteSnackbarContent {
+  bg: string;
+}
+
+export interface PaletteStepConnector {
+  border: string;
+}
+
+export interface PaletteStepContent {
+  border: string;
+}
+
+export interface PaletteSwitch {
+  defaultColor: string;
+  defaultDisabledColor: string;
+  primaryDisabledColor: string;
+  secondaryDisabledColor: string;
+  errorDisabledColor: string;
+  infoDisabledColor: string;
+  successDisabledColor: string;
+  warningDisabledColor: string;
+}
+
+export interface PaletteTableCell {
+  border: string;
+}
+
+export interface PaletteTooltip {
+  bg: string;
+}
+
+export interface ColorSystemOptions {
+  palette?: PaletteOptions & {
+    common?: Partial<PaletteCommonChannel>;
+    primary?: Partial<PaletteColorChannel>;
+    secondary?: Partial<PaletteColorChannel>;
+    error?: Partial<PaletteColorChannel>;
+    info?: Partial<PaletteColorChannel>;
+    success?: Partial<PaletteColorChannel>;
+    text?: Partial<PaletteTextChannel>;
+    dividerChannel?: Partial<string>;
+    action?: Partial<PaletteActionChannel>;
+    AppBar?: Partial<PaletteAppBar>;
+    Chip?: Partial<PaletteChip>;
+    FilledInput?: Partial<PaletteFilledInput>;
+    LinearProgress?: Partial<PaletteLinearProgress>;
+    Slider?: Partial<PaletteSlider>;
+    SnackbarContent?: Partial<PaletteSnackbarContent>;
+    StepConnector?: Partial<PaletteStepConnector>;
+    StepContent?: Partial<PaletteStepContent>;
+    Switch?: Partial<PaletteSwitch>;
+    TableCell?: Partial<PaletteTableCell>;
+    Tooltip?: Partial<PaletteTooltip>;
+  };
+  opacity?: Partial<Opacity>;
+  overlays?: Overlays;
+}
+
+export interface ColorSystem {
+  palette: Palette & {
+    common: PaletteCommonChannel;
+    primary: PaletteColorChannel;
+    secondary: PaletteColorChannel;
+    error: PaletteColorChannel;
+    info: PaletteColorChannel;
+    success: PaletteColorChannel;
+    text: PaletteTextChannel;
+    dividerChannel: string;
+    action: PaletteActionChannel;
+    AppBar: PaletteAppBar;
+    Chip: PaletteChip;
+    FilledInput: PaletteFilledInput;
+    LinearProgress: PaletteLinearProgress;
+    Slider: PaletteSlider;
+    SnackbarContent: PaletteSnackbarContent;
+    StepConnector: PaletteStepConnector;
+    StepContent: PaletteStepContent;
+    Switch: PaletteSwitch;
+    TableCell: PaletteTableCell;
+    Tooltip: PaletteTooltip;
+  };
   opacity: Opacity;
   overlays: Overlays;
-  shadows: Shadows;
-  transitions: Transitions;
-  typography: Typography;
-  zIndex: ZIndex;
-  unstable_strictMode?: boolean;
-  colorSchemes: Record<string, { palette: Palette }>;
 }
 
-// shut off automatic exporting for the `BaseTheme` above
+export interface CssVarsThemeOptions extends Omit<ThemeOptions, 'palette' | 'components'> {
+  components?: Components<Omit<CssVarsTheme, 'components'>>;
+  colorSchemes?: Partial<Record<SupportedColorScheme, ColorSystemOptions>>;
+}
+
+interface ThemeVars extends ColorSystem {
+  shadows: Shadows;
+  zIndex: ZIndex;
+  shape: Theme['shape'];
+}
+
+// shut off automatic exporting for the `ThemeVars` above
 export {};
 
-/**
- * Our [TypeScript guide on theme customization](https://mui.com/material-ui/guides/typescript/#customization-of-theme) explains in detail how you would add custom properties.
- */
-export interface Theme extends BaseTheme {
-  components?: Components<BaseTheme>;
+export interface CssVarsTheme extends Omit<Theme, 'palette' | 'components'>, ColorSystem {
+  components?: Components<Omit<CssVarsTheme, 'components'>>;
+  colorSchemes: Record<SupportedColorScheme, ColorSystem>;
+  prefix: string;
+  vars: ThemeVars;
+  getCssVar: <CustomVar extends string = never>(
+    field: string | CustomVar,
+    ...vars: Array<string | CustomVar>
+  ) => string;
+  getColorSchemeSelector: (colorScheme: SupportedColorScheme) => string;
 }
 
 /**
@@ -114,4 +246,7 @@ export interface Theme extends BaseTheme {
  * @param args Deep merge the arguments with the about to be returned theme.
  * @returns A complete, ready-to-use theme object.
  */
-export default function experimental_extendTheme(options?: ThemeOptions, ...args: object[]): Theme;
+export default function experimental_extendTheme(
+  options?: CssVarsThemeOptions,
+  ...args: object[]
+): CssVarsTheme;
