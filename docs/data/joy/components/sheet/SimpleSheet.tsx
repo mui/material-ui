85884dcb2fc78ac8bfe69f3c0604a6801bc8a157
Benny Joo <sldisek783@gmail.com>
2022-06-20 13:48:49 +0900

    Re-add Danilo's demo

---
+++ docs/data/joy/components/sheet/SimpleSheet.tsx
@@ -0,0 +1,10 @@
+import Sheet from '@mui/joy/Sheet';
+import * as React from 'react';
+
+export default function SimpleSheet() {
+  return (
+    <Sheet variant="outlined" sx={{ p: 4 }}>
+      Hello world!
+    </Sheet>
+  );
+}
