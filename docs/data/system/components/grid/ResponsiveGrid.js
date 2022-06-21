fae9d2ee0e48eccdeaf067ae9692d99795f3e940
siriwatknp <siriwatkunaporn@gmail.com>
2022-06-21 14:47:41 +0700

    run docs:typescript:formatted

--- docs/data/system/components/grid/ResponsiveGrid.js
+++ docs/data/system/components/grid/ResponsiveGrid.js
@@ -17,7 +17,7 @@ export default function ResponsiveGrid() {
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         {Array.from(Array(6)).map((_, index) => (
-          <Grid xs={2} sm={4} md={4} key={index}>
+          <Grid xs={2} sm={4} key={index}>
             <Item>{index + 1}</Item>
           </Grid>
         ))}
