a0aedd226f53bf4353b7540cdc53867670d4deb6
Marija Najdova <mnajdova@gmail.com>
2022-06-22 10:33:00 +0200

    prettier

--- packages/mui-material/src/Alert/Alert.js
+++ packages/mui-material/src/Alert/Alert.js
@@ -71,7 +71,9 @@ const AlertRoot = styled(Paper, {
       }),
     ...(color &&
       ownerState.variant === 'filled' && {
-        color: theme.palette.getContrastText(theme.palette.mode === 'dark' ? theme.palette[color].dark : theme.palette[color].main),
+        color: theme.palette.getContrastText(
+          theme.palette.mode === 'dark' ? theme.palette[color].dark : theme.palette[color].main,
+        ),
         fontWeight: theme.typography.fontWeightMedium,
         backgroundColor:
           theme.palette.mode === 'dark' ? theme.palette[color].dark : theme.palette[color].main,
