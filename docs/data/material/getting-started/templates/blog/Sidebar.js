2c885b158f812a5f3b3860c1344b54d744f6282d
siriwatknp <siriwatkunaporn@gmail.com>
2022-06-24 08:01:26 +0700

    fix style

--- docs/data/material/getting-started/templates/blog/Sidebar.js
+++ docs/data/material/getting-started/templates/blog/Sidebar.js
@@ -11,7 +11,7 @@ function Sidebar(props) {
 
   return (
     <Grid item xs={12} md={4}>
-      <Paper elevation={0} sx={{ p: 2, bgcolor: 'grey.200' }}>
+      <Paper variant="outlined" sx={{ p: 2 }}>
         <Typography variant="h6" gutterBottom>
           {title}
         </Typography>
--- docs/data/material/getting-started/templates/blog/Sidebar.tsx
+++ docs/data/material/getting-started/templates/blog/Sidebar.tsx
@@ -23,7 +23,7 @@ export default function Sidebar(props: SidebarProps) {
 
   return (
     <Grid item xs={12} md={4}>
-      <Paper elevation={0} sx={{ p: 2, bgcolor: 'grey.200' }}>
+      <Paper variant="outlined" sx={{ p: 2 }}>
         <Typography variant="h6" gutterBottom>
           {title}
         </Typography>
