145623539e25e953156728ff93c14c8418323467
Henrique Holtz <henrique_holtz@hotmail.com>
2022-06-24 16:35:22 -0300

    remove invalid prop "loading" at packages/mui-base/src/AutocompleteUnstyled/useAutocomplete.d.ts
    Co-authored-by: Marija Najdova <mnajdova@gmail.com>

--- packages/mui-base/src/AutocompleteUnstyled/useAutocomplete.d.ts
+++ packages/mui-base/src/AutocompleteUnstyled/useAutocomplete.d.ts
@@ -280,11 +280,6 @@ export interface UseAutocompleteProps<
     * @default false
     */
     readOnly?: boolean;
-   /**
-    * If `true`, the component is in a loading state. This shows the loadingText in place of suggestions (only if there are no suggestions to show, e.g. options are empty).
-    * @default false
-    */
-    loading?: boolean;
 }
 
 export type AutocompleteHighlightChangeReason = 'keyboard' | 'mouse' | 'auto';
