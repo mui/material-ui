ca8c2f7e0d92ac785127de73de6246141417534f
siriwatknp <siriwatkunaporn@gmail.com>
2022-06-25 15:18:35 +0700

    improve iOS switch

--- docs/data/joy/components/switch/ExampleIosSwitch.js
+++ docs/data/joy/components/switch/ExampleIosSwitch.js
@@ -14,14 +14,14 @@ export default function ExampleIosSwitch() {
         '--Switch-track-width': '51px',
         '--Switch-track-height': '31px',
         '--Switch-track-background': theme.vars.palette.background.level3,
+        [`& .${switchClasses.thumb}`]: {
+          transition: 'width 0.2s, left 0.2s',
+        },
         '&:hover': {
           '--Switch-track-background': theme.vars.palette.background.level3,
         },
         '&:active': {
           '--Switch-thumb-width': '32px',
-          [`& .${switchClasses.thumb}`]: {
-            transition: 'width 0.2s, left 0.2s',
-          },
         },
         [`&.${switchClasses.checked}`]: {
           '--Switch-track-background': 'rgb(48 209 88)',
