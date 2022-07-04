c54cfac28f63bf18024d0236eeb06e65d0124f73
siriwatknp <siriwatkunaporn@gmail.com>
2022-07-04 17:30:31 +0700

    add code copy hotkey

--- docs/src/modules/components/CodeEditor.js
+++ docs/src/modules/components/CodeEditor.js
@@ -8,6 +8,7 @@ import prism from '@mui/markdown/prism';
 import MarkdownElement from 'docs/src/modules/components/MarkdownElement';
 import CodeCopyButton from 'docs/src/modules/components/CodeCopyButton';
 import { useTranslate } from 'docs/src/modules/utils/i18n';
+import { useCodeCopy } from 'docs/src/modules/utils/CodeCopy';
 
 const Wrapper = styled(MarkdownElement)(({ theme }) => ({
   position: 'relative',
@@ -18,9 +19,8 @@ const Wrapper = styled(MarkdownElement)(({ theme }) => ({
     marginTop: theme.spacing(0),
   },
   borderRadius: theme.shape.borderRadius,
-  outlineOffset: '-1px',
   '&:hover': {
-    outline: `2px solid ${
+    outline: `3px solid ${
       theme.palette.mode === 'dark' ? theme.palette.primaryDark['500'] : theme.palette.primary.light
     }`,
     '& .MuiCode-copy': {
@@ -31,14 +31,14 @@ const Wrapper = styled(MarkdownElement)(({ theme }) => ({
     },
   },
   '&:focus-within': {
-    outline: `2px solid ${
+    outline: `3px solid ${
       theme.palette.mode === 'dark' ? theme.palette.primaryDark.main : theme.palette.primary.main
     }`,
   },
   '& pre': {
     maxHeight: 'initial',
   },
-  '& > pre': {
+  '& > .MuiCode-root > pre': {
     margin: '0 auto',
     padding: 0,
     maxHeight: 'min(68vh, 1000px)',
@@ -74,6 +74,7 @@ const StyledEditor = styled(Editor)(({ theme }) => ({
 const CodeEditor = ({ language = 'jsx', value, onChange, copyButtonProps, ...rest }) => {
   const t = useTranslate();
   const wrapperRef = React.useRef(null);
+  const handlers = useCodeCopy();
 
   React.useEffect(() => {
     if (wrapperRef.current) {
@@ -103,36 +104,38 @@ const CodeEditor = ({ language = 'jsx', value, onChange, copyButtonProps, ...res
         }
       }}
     >
-      <pre>
-        <StyledEditor
-          padding={20}
-          highlight={(code) => prism(code, language)}
-          {...rest}
-          value={value}
-          onValueChange={onChange}
+      <div className="MuiCode-root" {...handlers}>
+        <pre>
+          <StyledEditor
+            padding={20}
+            highlight={(code) => prism(code, language)}
+            {...rest}
+            value={value}
+            onValueChange={onChange}
+          />
+        </pre>
+        <Box
+          aria-live="polite"
+          tabIndex={0}
+          sx={{
+            color: 'white',
+            position: 'absolute',
+            top: '4px',
+            right: '20px',
+            fontSize: '12px',
+            outline: 'none',
+            '&:not(:focus)': visuallyHidden,
+          }}
+          dangerouslySetInnerHTML={{
+            __html: t('editorHint'),
+          }}
         />
-      </pre>
-      <Box
-        aria-live="polite"
-        tabIndex={0}
-        sx={{
-          color: 'white',
-          position: 'absolute',
-          top: '4px',
-          right: '20px',
-          fontSize: '12px',
-          outline: 'none',
-          '&:not(:focus)': visuallyHidden,
-        }}
-        dangerouslySetInnerHTML={{
-          __html: t('editorHint'),
-        }}
-      />
-      <CodeCopyButton
-        {...copyButtonProps}
-        code={value}
-        onKeyDown={(event) => event.stopPropagation()}
-      />
+        <CodeCopyButton
+          {...copyButtonProps}
+          code={value}
+          onKeyDown={(event) => event.stopPropagation()}
+        />
+      </div>
     </Wrapper>
   );
 };
