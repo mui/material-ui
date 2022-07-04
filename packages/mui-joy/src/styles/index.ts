16a298b42ba7c35f5c5823c5fdc590ec9617db43
siriwatknp <siriwatkunaporn@gmail.com>
2022-07-04 13:31:58 +0700

    fix export types

--- packages/mui-joy/src/styles/index.ts
+++ packages/mui-joy/src/styles/index.ts
@@ -63,5 +63,5 @@ export * from './ThemeProvider';
 export { default as useThemeProps } from './useThemeProps';
 export { sx as experimental_sx } from './styleFunctionSx';
 export { default as extendTheme, createGetCssVar } from './extendTheme';
-export type { ThemeInput } from './extendTheme';
+export type { CssVarsThemeOptions } from './extendTheme';
 export { default as StyledEngineProvider } from './StyledEngineProvider';
