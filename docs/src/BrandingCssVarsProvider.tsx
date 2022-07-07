25cee16b360ce19eafef3e8d8df772c65af81c7c
siriwatknp <siriwatkunaporn@gmail.com>
2022-07-07 09:27:26 +0700

    create BrandingCssVarsProvider

---
+++ docs/src/BrandingCssVarsProvider.tsx
@@ -0,0 +1,185 @@
+import * as React from 'react';
+import {
+  alpha,
+  Experimental_CssVarsProvider as CssVarsProvider,
+  experimental_extendTheme as extendTheme,
+  PaletteColorOptions,
+} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { NextNProgressBar } from 'docs/src/modules/components/AppFrame';
+import { blue, blueDark, grey, error, success, warning } from 'docs/src/modules/brandingTheme';
+
+const systemFont = [
+  '-apple-system',
+  'BlinkMacSystemFont',
+  '"Segoe UI"',
+  'Roboto',
+  '"Helvetica Neue"',
+  'Arial',
+  'sans-serif',
+  '"Apple Color Emoji"',
+  '"Segoe UI Emoji"',
+  '"Segoe UI Symbol"',
+];
+
+declare module '@mui/material/styles' {
+  interface TypeText {
+    display1: string;
+    display2: string;
+    h5: string;
+  }
+
+  interface PaletteOptions {
+    primaryDark: PaletteColorOptions;
+  }
+}
+
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: blue,
+        primaryDark: blueDark,
+        common: {
+          black: '#1D1D1D',
+        },
+        divider: grey[100],
+        text: {
+          primary: grey[900],
+          secondary: grey[700],
+          display2: 'var(--mui-palette-primaryDark-700)',
+          h5: 'var(--mui-palette-primary-main)',
+        },
+        grey,
+        error,
+        success: {
+          ...success,
+          main: '#1AA251', // contrast 3.31:1
+        },
+        warning,
+      },
+    },
+    dark: {
+      palette: {
+        primary: {
+          ...blue,
+          main: blue[400],
+        },
+        primaryDark: blueDark,
+        background: {
+          default: blueDark[800],
+          paper: blueDark[900],
+        },
+        common: {
+          black: '#1D1D1D',
+        },
+        divider: alpha(blue[100], 0.08),
+        text: {
+          primary: '#fff',
+          secondary: grey[400],
+          display1: 'var(--mui-palette-primaryDark-900)',
+          display2: 'var(--mui-palette-grey-100)',
+          h5: 'var(--mui-palette-primary-300)',
+        },
+        grey,
+        error,
+        success: {
+          ...success,
+          main: '#1DB45A', // contrast 6.17:1 (blueDark.800)
+        },
+        warning,
+      },
+    },
+  },
+  shape: {
+    borderRadius: 10,
+  },
+  spacing: 10,
+  typography: {
+    fontFamily: ['"IBM Plex Sans"', ...systemFont].join(','),
+    fontFamilyCode: ['Consolas', 'Menlo', 'Monaco', 'Andale Mono', 'Ubuntu Mono', 'monospace'].join(
+      ',',
+    ),
+    fontFamilySystem: systemFont.join(','),
+    fontWeightSemiBold: 600,
+    fontWeightExtraBold: 800,
+    h1: {
+      fontFamily: ['"PlusJakartaSans-ExtraBold"', ...systemFont].join(','),
+      fontSize: 'clamp(2.625rem, 1.2857rem + 3.5714vw, 4rem)',
+      fontWeight: 800,
+      lineHeight: 78 / 70,
+      color: 'var(--mui-palette-text-display1)',
+    },
+    h2: {
+      fontFamily: ['"PlusJakartaSans-ExtraBold"', ...systemFont].join(','),
+      fontSize: 'clamp(1.5rem, 0.9643rem + 1.4286vw, 2.25rem)',
+      fontWeight: 800,
+      lineHeight: 44 / 36,
+      color: 'var(--mui-palette-text-display2)',
+    },
+    h3: {
+      fontFamily: ['"PlusJakartaSans-Bold"', ...systemFont].join(','),
+      fontSize: '2.25rem', // 36px
+      lineHeight: 44 / 36,
+      letterSpacing: 0.2,
+    },
+    h4: {
+      fontFamily: ['"PlusJakartaSans-Bold"', ...systemFont].join(','),
+      fontSize: '1.75rem', // 28px
+      lineHeight: 42 / 28,
+      letterSpacing: 0.2,
+    },
+    h5: {
+      fontFamily: ['"PlusJakartaSans-Bold"', ...systemFont].join(','),
+      fontSize: '1.5rem', // 24px
+      lineHeight: 36 / 24,
+      letterSpacing: 0.1,
+      color: 'var(--mui-palette-text-h5)',
+    },
+    h6: {
+      fontSize: '1.25rem', // 20px
+      lineHeight: 30 / 20,
+    },
+    button: {
+      textTransform: 'initial',
+      fontWeight: 700,
+      letterSpacing: 0,
+    },
+    subtitle1: {
+      fontSize: '1.125rem', // 18px
+      lineHeight: 24 / 18,
+      letterSpacing: 0,
+      fontWeight: 500,
+    },
+    body1: {
+      fontSize: '1rem', // 16px
+      lineHeight: 24 / 16,
+      letterSpacing: 0,
+    },
+    body2: {
+      fontSize: '0.875rem', // 14px
+      lineHeight: 21 / 14,
+      letterSpacing: 0,
+    },
+    caption: {
+      display: 'inline-block',
+      fontSize: '0.75rem', // 12px
+      lineHeight: 18 / 12,
+      letterSpacing: 0,
+      fontWeight: 700,
+    },
+    allVariants: {
+      scrollMarginTop: 'calc(var(--MuiDocs-header-height) + 32px)',
+    },
+  },
+});
+
+export default function BrandingCssVarsProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <CssVarsProvider theme={theme}>
+      <CssBaseline />
+      <NextNProgressBar />
+      {children}
+    </CssVarsProvider>
+  );
+}
--- docs/src/modules/brandingTheme.ts
+++ docs/src/modules/brandingTheme.ts
@@ -28,12 +28,14 @@ declare module '@mui/material/styles/createTypography' {
     fontWeightSemiBold?: number;
     fontWeightExtraBold?: number;
     fontFamilyCode?: string;
+    fontFamilySystem?: string;
   }
 
   interface Typography {
     fontWeightSemiBold: number;
     fontWeightExtraBold: number;
     fontFamilyCode: string;
+    fontFamilySystem: string;
   }
 }
 
@@ -72,7 +74,7 @@ export const blueDark = {
   800: '#001E3C',
   900: '#0A1929',
 };
-const grey = {
+export const grey = {
   50: '#F3F6F9',
   100: '#E7EBF0',
   200: '#E0E3E7',
@@ -83,6 +85,44 @@ const grey = {
   700: '#3E5060', // vs white bg: WCAG 8.3 AAA, APCA 88.7 Best for text
   800: '#2D3843', // vs white bg: WCAG 11.9 AAA, APCA 97.3 Best for text
   900: '#1A2027',
+};
+export const error = {
+  50: '#FFF0F1',
+  100: '#FFDBDE',
+  200: '#FFBDC2',
+  300: '#FF99A2',
+  400: '#FF7A86',
+  500: '#FF505F',
+  main: '#EB0014', // contrast 4.63:1
+  600: '#EB0014',
+  700: '#C70011',
+  800: '#94000D',
+  900: '#570007',
+};
+export const success = {
+  50: '#E9FBF0',
+  100: '#C6F6D9',
+  200: '#9AEFBC',
+  300: '#6AE79C',
+  400: '#3EE07F',
+  500: '#21CC66',
+  600: '#1DB45A',
+  700: '#1AA251',
+  800: '#178D46',
+  900: '#0F5C2E',
+};
+export const warning = {
+  50: '#FFF9EB',
+  100: '#FFF3C1',
+  200: '#FFECA1',
+  300: '#FFDC48', // vs blueDark900: WCAG 10.4 AAA, APCA 72 Ok for text
+  400: '#F4C000', // vs blueDark900: WCAG 6.4 AA normal, APCA 48 Only large text
+  500: '#DEA500', // vs blueDark900: WCAG 8 AAA normal, APCA 58 Only large text
+  main: '#DEA500',
+  600: '#D18E00', // vs blueDark900: WCAG 6.4 AA normal, APCA 48 Only large text
+  700: '#AB6800', // vs white bg: WCAG 4.4 AA large, APCA 71 Ok for text
+  800: '#8C5800', // vs white bg: WCAG 5.9 AAA large, APCA 80 Best for text
+  900: '#5A3600', // vs white bg: WCAG 10.7 AAA, APCA 95 Best for text
 };
 // context on the Advanced Perceptual Contrast Algorithm (APCA) used above here: https://github.com/w3c/wcag/issues/695
 
@@ -141,50 +181,17 @@ export const getDesignTokens = (mode: 'light' | 'dark') =>
         },
       }),
       grey,
-      error: {
-        50: '#FFF0F1',
-        100: '#FFDBDE',
-        200: '#FFBDC2',
-        300: '#FF99A2',
-        400: '#FF7A86',
-        500: '#FF505F',
-        main: '#EB0014', // contrast 4.63:1
-        600: '#EB0014',
-        700: '#C70011',
-        800: '#94000D',
-        900: '#570007',
-      },
+      error,
       success: {
-        50: '#E9FBF0',
-        100: '#C6F6D9',
-        200: '#9AEFBC',
-        300: '#6AE79C',
-        400: '#3EE07F',
-        500: '#21CC66',
-        600: '#1DB45A',
+        ...success,
         ...(mode === 'dark' && {
           main: '#1DB45A', // contrast 6.17:1 (blueDark.800)
         }),
         ...(mode === 'light' && {
           main: '#1AA251', // contrast 3.31:1
         }),
-        700: '#1AA251',
-        800: '#178D46',
-        900: '#0F5C2E',
       },
-      warning: {
-        50: '#FFF9EB',
-        100: '#FFF3C1',
-        200: '#FFECA1',
-        300: '#FFDC48', // vs blueDark900: WCAG 10.4 AAA, APCA 72 Ok for text
-        400: '#F4C000', // vs blueDark900: WCAG 6.4 AA normal, APCA 48 Only large text
-        500: '#DEA500', // vs blueDark900: WCAG 8 AAA normal, APCA 58 Only large text
-        main: '#DEA500',
-        600: '#D18E00', // vs blueDark900: WCAG 6.4 AA normal, APCA 48 Only large text
-        700: '#AB6800', // vs white bg: WCAG 4.4 AA large, APCA 71 Ok for text
-        800: '#8C5800', // vs white bg: WCAG 5.9 AAA large, APCA 80 Best for text
-        900: '#5A3600', // vs white bg: WCAG 10.7 AAA, APCA 95 Best for text
-      },
+      warning,
     },
     shape: {
       borderRadius: 10,
