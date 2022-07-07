6720a669c937723461058c74dd4d7e1b1645d1e9
siriwatknp <siriwatkunaporn@gmail.com>
2022-07-07 14:16:07 +0700

    use @mui/system

--- examples/base-cra-typescript/src/App.tsx
+++ examples/base-cra-typescript/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import styled from '@emotion/styled';
+import { styled } from '@mui/system';
 import { ButtonUnstyled, buttonUnstyledClasses } from '@mui/base';
 
 const Button = styled(ButtonUnstyled)`
