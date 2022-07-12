203d6b3a7f225d1cf80bed60c01f29ce73e1cfcf
Marija Najdova <mnajdova@gmail.com>
2022-07-12 10:22:14 +0200

    Update examples/tailwind-css/src/index.tsx

--- examples/tailwind-css/src/index.tsx
+++ examples/tailwind-css/src/index.tsx
@@ -9,6 +9,9 @@ import reportWebVitals from './reportWebVitals';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
 
+
+// All `Portal`-related components need to have the the main app wrapper element as a container
+// so that the are in the subtree under the element used in the `important` option of the Tailwind's config.
 const theme = createTheme({
   components: {
     MuiPopover: {
