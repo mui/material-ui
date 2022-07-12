d1d1654deb0018d4cdc587e6e73c59e03f5bbeb3
Siriwat K <siriwatkunaporn@gmail.com>
2022-07-12 08:36:12 +0100

    [Grid] Export new grid as unstable (#33479)

--- packages/mui-material/src/Unstable_Grid2/grid2Classes.ts
+++ packages/mui-material/src/Unstable_Grid2/grid2Classes.ts
@@ -1,10 +1,10 @@
 import { generateUtilityClass, generateUtilityClasses } from '@mui/base';
 import { GridClasses } from '@mui/system/Unstable_Grid';
 
-export type GridClassKey = keyof GridClasses;
+export type Grid2ClassKey = keyof GridClasses;
 
-export function getGridUtilityClass(slot: string): string {
-  return generateUtilityClass('JoyGrid', slot);
+export function getGrid2UtilityClass(slot: string): string {
+  return generateUtilityClass('MuiGrid2', slot);
 }
 
 const SPACINGS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as const;
@@ -12,7 +12,7 @@ const DIRECTIONS = ['column-reverse', 'column', 'row-reverse', 'row'] as const;
 const WRAPS = ['nowrap', 'wrap-reverse', 'wrap'] as const;
 const GRID_SIZES = ['auto', true, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] as const;
 
-const gridClasses: GridClasses = generateUtilityClasses('JoyGrid', [
+const grid2Classes: GridClasses = generateUtilityClasses('MuiGrid2', [
   'root',
   'container',
   'item',
@@ -33,4 +33,4 @@ const gridClasses: GridClasses = generateUtilityClasses('JoyGrid', [
   ...GRID_SIZES.map((size) => `grid-xl-${size}` as const),
 ]);
 
-export default gridClasses;
+export default grid2Classes;
