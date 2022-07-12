fa159307c0a3348eabc2d81900e6104328def4f3
siriwatknp <siriwatkunaporn@gmail.com>
2022-07-12 10:34:27 +0700

    export Grid2 from Material UI

---
+++ packages/mui-material/src/Grid2/Grid2.tsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import { createGrid } from '@mui/system/Unstable_Grid';
+import { OverridableComponent } from '@mui/types';
+import { styled, useThemeProps } from '../styles';
+import { Grid2TypeMap } from './Grid2Props';
+
+const Grid = createGrid({
+  createStyledComponent: styled('div', {
+    name: 'MuiGrid2',
+    overridesResolver: (props, styles) => styles.root,
+  }),
+  componentName: 'MuiGrid2',
+  useThemeProps: (inProps) => useThemeProps({ props: inProps, name: 'MuiGrid2' }),
+}) as OverridableComponent<Grid2TypeMap>;
+
+Grid.propTypes /* remove-proptypes */ = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // |     To update them edit TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * The content of the component.
+   */
+  children: PropTypes.node,
+  /**
+   * @ignore
+   */
+  sx: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+} as any;
+
+export default Grid;
