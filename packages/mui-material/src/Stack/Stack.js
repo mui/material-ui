a5fd654c14f600f821db1bdbe222b0db67ae8b4d
Benny Joo <sldisek783@gmail.com>
2022-07-17 13:50:13 +0100

    Set default flex direction to column

--- packages/mui-material/src/Stack/Stack.js
+++ packages/mui-material/src/Stack/Stack.js
@@ -44,6 +44,7 @@ const getSideFromDirection = (direction) => {
 export const style = ({ ownerState, theme }) => {
   let styles = {
     display: 'flex',
+    flexDirection:'column',
     ...handleBreakpoints(
       { theme },
       resolveBreakpointValues({
