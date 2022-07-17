d4c92ecde6a10e0724589c465534a2e809992198
Benny Joo <sldisek783@gmail.com>
2022-07-17 14:03:49 +0100

    Use given direction prop if it is row

--- packages/mui-material/src/Stack/Stack.js
+++ packages/mui-material/src/Stack/Stack.js
@@ -44,7 +44,7 @@ const getSideFromDirection = (direction) => {
 export const style = ({ ownerState, theme }) => {
   let styles = {
     display: 'flex',
-    flexDirection: 'column',
+    flexDirection: ownerState.direction === 'row' ? 'row' : 'column',
     ...handleBreakpoints(
       { theme },
       resolveBreakpointValues({
