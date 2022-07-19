0eba77d1dd50648de78c51b06996acdf77cacd3e
Gerda <106991024+gerdadesign@users.noreply.github.com>
2022-07-19 16:46:49 -0400

    Add YouTube link to footer

--- docs/src/layouts/AppFooter.tsx
+++ docs/src/layouts/AppFooter.tsx
@@ -8,6 +8,7 @@ import Stack from '@mui/material/Stack';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 import SvgMuiLogo from 'docs/src/icons/SvgMuiLogo';
 import EmailSubscribe from 'docs/src/components/footer/EmailSubscribe';
 import ROUTES from 'docs/src/route';
@@ -178,6 +179,17 @@ export default function AppFooter() {
             >
               <LinkedInIcon fontSize="small" />
             </IconButton>
+            <IconButton
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.youtube.com/channel/UCUdh2wVTrd7hErLo9Rh0HsQ"
+              aria-label="YouTube"
+              title="YouTube"
+              size="small"
+            >
+              <YouTubeIcon fontSize="small" />
+            </IconButton>
+            
           </Stack>
         </Box>
       </Box>
