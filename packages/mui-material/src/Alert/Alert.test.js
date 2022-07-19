b858412bc0aa125057fbd57427be75bf45d5cd29
Jake Stewart <jake.stewart@collibra.com>
2022-07-19 16:49:13 -0500

    [Alert] Add `components` and `componentsProps` props to allow close action overrides (#33230)

--- packages/mui-material/src/Alert/Alert.test.js
+++ packages/mui-material/src/Alert/Alert.test.js
@@ -4,6 +4,8 @@ import { createRenderer, describeConformance, screen } from 'test/utils';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Alert, { alertClasses as classes } from '@mui/material/Alert';
 import Paper, { paperClasses } from '@mui/material/Paper';
+import { iconButtonClasses } from '@mui/material/IconButton';
+import { svgIconClasses } from '@mui/material/SvgIcon';
 
 describe('<Alert />', () => {
   const { render } = createRenderer();
@@ -62,6 +64,76 @@ describe('<Alert />', () => {
           </ThemeProvider>,
         ),
       ).not.to.throw();
+    });
+  });
+
+  describe('prop: components', () => {
+    it('should override the default icon used in the close action', () => {
+      const MyCloseIcon = () => <div data-testid="closeIcon">X</div>;
+
+      render(
+        <Alert onClose={() => {}} components={{ CloseIcon: MyCloseIcon }}>
+          Hello World
+        </Alert>,
+      );
+
+      expect(screen.getByTestId('closeIcon')).toBeVisible();
+    });
+
+    it('should override the default button used in the close action', () => {
+      const MyCloseButton = () => <button data-testid="closeButton">X</button>;
+
+      render(
+        <Alert onClose={() => {}} components={{ CloseButton: MyCloseButton }}>
+          Hello World
+        </Alert>,
+      );
+
+      expect(screen.getByTestId('closeButton')).toBeVisible();
+    });
+  });
+
+  describe('prop: componentsProps', () => {
+    it('should apply the props on the close IconButton component', () => {
+      render(
+        <Alert
+          onClose={() => {}}
+          componentsProps={{
+            closeButton: {
+              'data-testid': 'closeButton',
+              size: 'large',
+              className: 'my-class',
+            },
+          }}
+        >
+          Hello World
+        </Alert>,
+      );
+
+      const closeIcon = screen.getByTestId('closeButton');
+      expect(closeIcon).to.have.class(iconButtonClasses.sizeLarge);
+      expect(closeIcon).to.have.class('my-class');
+    });
+
+    it('should apply the props on the close SvgIcon component', () => {
+      render(
+        <Alert
+          onClose={() => {}}
+          componentsProps={{
+            closeIcon: {
+              'data-testid': 'closeIcon',
+              fontSize: 'large',
+              className: 'my-class',
+            },
+          }}
+        >
+          Hello World
+        </Alert>,
+      );
+
+      const closeIcon = screen.getByTestId('closeIcon');
+      expect(closeIcon).to.have.class(svgIconClasses.fontSizeLarge);
+      expect(closeIcon).to.have.class('my-class');
     });
   });
 });
