0357892078afa4101f55c3100cefaf3d4e1d4f07
Marija Najdova <mnajdova@gmail.com>
2022-07-21 12:57:31 +0200

    Apply the changes

--- scripts/use-react-version.js
+++ scripts/use-react-version.js
@@ -17,48 +17,88 @@ const exec = promisify(childProcess.exec);
 
 // packages published from the react monorepo using the same version
 const reactPackageNames = ['react', 'react-dom', 'react-is', 'react-test-renderer', 'scheduler'];
+const devDependenciesPackageNames = ['@mnajdova/enzyme-adapter-react-18', '@testing-library/react'];
 
-async function main(options) {
-  const { distTag } = options;
-  if (typeof distTag !== 'string') {
-    throw new TypeError(`expected distTag: string but got '${distTag}'`);
+// if we need to support more versions we will need to add new mapping here
+const additionalVersionsMappings = {
+  17: {
+    '@mnajdova/enzyme-adapter-react-18': 'npm:@eps1lon/enzyme-adapter-react-17',
+    '@testing-library/react': '^12.1.0',
+  },
+};
+
+async function main(version) {
+  if (typeof version !== 'string') {
+    throw new TypeError(`expected version: string but got '${version}'`);
   }
 
-  if (distTag === 'stable') {
-    console.log('nothing to do with stable');
+  if (version === 'stable') {
+    console.log('Nothing to do with stable');
     return;
   }
 
   const packageJsonPath = path.resolve(__dirname, '../package.json');
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, { encoding: 'utf8' }));
 
+  const { stdout: v } = await exec(`npm view --json react@${version} version`);
+  const majorVersion = JSON.parse(v).split('.')[0];
+
   await Promise.all(
     reactPackageNames.map(async (reactPackageName) => {
-      const { stdout: versions } = await exec(`npm dist-tag ls ${reactPackageName} ${distTag}`);
+      const { stdout: versions } = await exec(`npm dist-tag ls ${reactPackageName} ${version}`);
       const tagMapping = versions.split('\n').find((mapping) => {
-        return mapping.startsWith(`${distTag}: `);
+        return mapping.startsWith(`${version}: `);
       });
+
+      let packageVersion = null;
+
       if (tagMapping === undefined) {
-        throw new Error(`Could not find '${distTag}' in "${versions}"`);
+        // Some specific version is being requested
+        if (majorVersion) {
+          packageVersion = version;
+          if (reactPackageName === 'scheduler') {
+            // get the scheduler version from the react-dom's dependencies entry
+            const { stdout: reactDOMDependenciesString } = await exec(
+              `npm view --json react-dom@${version} dependencies`,
+            );
+            packageVersion = JSON.parse(reactDOMDependenciesString).scheduler;
+          }
+        } else {
+          throw new Error(`Could not find '${version}' in "${versions}"`);
+        }
+      } else {
+        packageVersion = tagMapping.replace(`${version}: `, '');
       }
 
-      const version = tagMapping.replace(`${distTag}: `, '');
-
-      packageJson.resolutions[reactPackageName] = version;
+      packageJson.resolutions[reactPackageName] = packageVersion;
     }),
   );
 
-  // TODO: What is this doing?
+  // At this moment all dist tags reference React 18 version, so we don't need
+  // to update these dependencies unless an older version is used, or when the
+  // next/experimental dist tag reference to a future version of React
   // packageJson.devDependencies['@mnajdova/enzyme-adapter-react-18'] =
   //   'npm:@mnajdova/enzyme-adapter-react-next';
-  packageJson.devDependencies['@testing-library/react'] = 'alpha';
+  // packageJson.devDependencies['@testing-library/react'] = 'alpha';
+
+  if (additionalVersionsMappings[majorVersion]) {
+    devDependenciesPackageNames.forEach((packageName) => {
+      if (!additionalVersionsMappings[majorVersion][packageName]) {
+        throw new Error(
+          `Version ${majorVersion} does not have version defined for the ${packageName}`,
+        );
+      }
+      packageJson.devDependencies[packageName] =
+        additionalVersionsMappings[majorVersion][packageName];
+    });
+  }
 
   // add newline for clean diff
   fs.writeFileSync(packageJsonPath, `${JSON.stringify(packageJson, null, 2)}${os.EOL}`);
 }
 
-const [distTag = process.env.REACT_DIST_TAG] = process.argv.slice(2);
-main({ distTag }).catch((error) => {
+const [version = process.env.REACT_VERSION] = process.argv.slice(2);
+main(version).catch((error) => {
   console.error(error);
   process.exit(1);
 });
