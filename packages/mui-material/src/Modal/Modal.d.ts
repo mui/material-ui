c879d9fabd3db29939fd1db21cdf859ccdbfb945
Benny Joo <sldisek783@gmail.com>
2022-07-25 11:02:59 +0100

    Fix lint error

--- packages/mui-material/src/Modal/Modal.d.ts
+++ packages/mui-material/src/Modal/Modal.d.ts
@@ -9,7 +9,8 @@ export type ModalTypeMap<D extends React.ElementType = 'div', P = {}> = ExtendMo
   props: P & {
     /**
      * A backdrop component. This prop enables custom backdrop rendering.
-     * @deprecated Use `components.Backdrop` instead. While this prop currently works, it will be removed in the next major version. Use the `components.Backdrop` prop to make your application ready for the next version of Material UI.
+     * @deprecated Use `components.Backdrop` instead. While this prop currently works, it will be removed in the next major version. 
+     * Use the `components.Backdrop` prop to make your application ready for the next version of Material UI.
      * @default styled(Backdrop, {
      *   name: 'MuiModal',
      *   slot: 'Backdrop',
