5c7d3d1666c73cccd252be5621e48aa906aeb879
Benny Joo <sldisek783@gmail.com>
2022-08-01 05:09:46 +0100

    Add Breadcrumbs playground demo

---
+++ docs/data/joy/components/breadcrumbs/BreadcrumbsUsage.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Link from '@mui/joy/Link';
+import Typography from '@mui/joy/Typography';
+import Breadcrumbs from '@mui/joy/Breadcrumbs';
+import JoyUsageDemo from 'docs/src/modules/components/JoyUsageDemo';
+
+export default function BreadcrumbsUsage() {
+  return (
+    <JoyUsageDemo
+      componentName="Breadcrumbs"
+      data={[
+        {
+          propName: 'size',
+          knob: 'radio',
+          options: ['sm', 'md', 'lg'],
+          defaultValue: 'md',
+        },
+        {
+          propName: 'separator',
+          knob: 'input',
+        },
+      ]}
+      renderDemo={(props) => (
+        <Breadcrumbs {...props}>
+          <Link underline="hover" color="neutral" href="/" fontSize="inherit">
+            MUI
+          </Link>
+          <Link
+            underline="hover"
+            color="neutral"
+            href="/joy-ui/getting-started/installation/"
+            fontSize="inherit"
+          >
+            Joy
+          </Link>
+          <Typography fontSize="inherit">Breadcrumbs</Typography>
+        </Breadcrumbs>
+      )}
+    />
+  );
+}
