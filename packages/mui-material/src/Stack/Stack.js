bd4ea359642e581ad22b1ea440077565c7c4c56b
Marija Najdova <mnajdova@gmail.com>
2022-08-02 16:11:23 +0200

    [Stack} Replace with createStack

--- packages/mui-material/src/Stack/Stack.js
+++ packages/mui-material/src/Stack/Stack.js
@@ -1,144 +1,16 @@
-import * as React from 'react';
+/* eslint-disable material-ui/mui-name-matches-component-name */
 import PropTypes from 'prop-types';
-import {
-  createUnarySpacing,
-  getValue,
-  handleBreakpoints,
-  mergeBreakpointsInOrder,
-  unstable_extendSxProp as extendSxProp,
-  unstable_resolveBreakpointValues as resolveBreakpointValues,
-} from '@mui/system';
-import { deepmerge } from '@mui/utils';
+import { createStack } from '@mui/system';
 import styled from '../styles/styled';
 import useThemeProps from '../styles/useThemeProps';
 
-/**
- * Return an array with the separator React element interspersed between
- * each React node of the input children.
- *
- * > joinChildren([1,2,3], 0)
- * [1,0,2,0,3]
- */
-function joinChildren(children, separator) {
-  const childrenArray = React.Children.toArray(children).filter(Boolean);
-
-  return childrenArray.reduce((output, child, index) => {
-    output.push(child);
-
-    if (index < childrenArray.length - 1) {
-      output.push(React.cloneElement(separator, { key: `separator-${index}` }));
-    }
-
-    return output;
-  }, []);
-}
-
-const getSideFromDirection = (direction) => {
-  return {
-    row: 'Left',
-    'row-reverse': 'Right',
-    column: 'Top',
-    'column-reverse': 'Bottom',
-  }[direction];
-};
-
-export const style = ({ ownerState, theme }) => {
-  let styles = {
-    display: 'flex',
-    flexDirection: 'column',
-    ...handleBreakpoints(
-      { theme },
-      resolveBreakpointValues({
-        values: ownerState.direction,
-        breakpoints: theme.breakpoints.values,
-      }),
-      (propValue) => ({
-        flexDirection: propValue,
-      }),
-    ),
-  };
-
-  if (ownerState.spacing) {
-    const transformer = createUnarySpacing(theme);
-
-    const base = Object.keys(theme.breakpoints.values).reduce((acc, breakpoint) => {
-      if (
-        (typeof ownerState.spacing === 'object' && ownerState.spacing[breakpoint] != null) ||
-        (typeof ownerState.direction === 'object' && ownerState.direction[breakpoint] != null)
-      ) {
-        acc[breakpoint] = true;
-      }
-      return acc;
-    }, {});
-
-    const directionValues = resolveBreakpointValues({
-      values: ownerState.direction,
-      base,
-    });
-
-    const spacingValues = resolveBreakpointValues({
-      values: ownerState.spacing,
-      base,
-    });
-
-    if (typeof directionValues === 'object') {
-      Object.keys(directionValues).forEach((breakpoint, index, breakpoints) => {
-        const directionValue = directionValues[breakpoint];
-        if (!directionValue) {
-          const previousDirectionValue =
-            index > 0 ? directionValues[breakpoints[index - 1]] : 'column';
-          directionValues[breakpoint] = previousDirectionValue;
-        }
-      });
-    }
-
-    const styleFromPropValue = (propValue, breakpoint) => {
-      return {
-        '& > :not(style) + :not(style)': {
-          margin: 0,
-          [`margin${getSideFromDirection(
-            breakpoint ? directionValues[breakpoint] : ownerState.direction,
-          )}`]: getValue(transformer, propValue),
-        },
-      };
-    };
-    styles = deepmerge(styles, handleBreakpoints({ theme }, spacingValues, styleFromPropValue));
-  }
-
-  styles = mergeBreakpointsInOrder(theme.breakpoints, styles);
-
-  return styles;
-};
-
-const StackRoot = styled('div', {
-  name: 'MuiStack',
-  slot: 'Root',
-  overridesResolver: (props, styles) => {
-    return [styles.root];
-  },
-})(style);
-
-const Stack = React.forwardRef(function Stack(inProps, ref) {
-  const themeProps = useThemeProps({ props: inProps, name: 'MuiStack' });
-  const props = extendSxProp(themeProps);
-  const {
-    component = 'div',
-    direction = 'column',
-    spacing = 0,
-    divider,
-    children,
-    ...other
-  } = props;
-  const ownerState = {
-    direction,
-    spacing,
-  };
-
-  return (
-    <StackRoot as={component} ownerState={ownerState} ref={ref} {...other}>
-      {divider ? joinChildren(children, divider) : children}
-    </StackRoot>
-  );
+const Stack = createStack({
+  createStyledComponent: styled('div', {
+    name: 'MuiStack',
+    slot: 'Root',
+    overridesResolver: (props, styles) => [styles.root],
+  }),
+  useThemeProps: (inProps) => useThemeProps({ props: inProps, name: 'MuiStack' }),
 });
 
 Stack.propTypes /* remove-proptypes */ = {
@@ -150,11 +22,6 @@ Stack.propTypes /* remove-proptypes */ = {
    * The content of the component.
    */
   children: PropTypes.node,
-  /**
-   * The component used for the root node.
-   * Either a string to use a HTML element or a component.
-   */
-  component: PropTypes.elementType,
   /**
    * Defines the `flex-direction` style property.
    * It is applied for all screen sizes.
