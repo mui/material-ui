2a683dc72dcd29f1bcf1a952d8ab94ef3d783720
siriwatknp <siriwatkunaporn@gmail.com>
2022-08-08 18:50:51 +0700

    add custom stylis plugins

--- docs/src/createEmotionCache.ts
+++ docs/src/createEmotionCache.ts
@@ -1,6 +1,21 @@
+/* eslint-disable default-case */
 import createCache from '@emotion/cache';
+import { Element, RULESET } from 'stylis';
+
+function globalSelector(element: Element) {
+  switch (element.type) {
+    case RULESET:
+      element.props = (element.props as string[]).map((value: any) => {
+        if (value.match(/(:where|:has)\(/)) {
+          value = value.replace(/\.[^:]+(:where|:has)/, '$1');
+          return value;
+        }
+        return value;
+      });
+  }
+}
 
 export default function createEmotionCache() {
   // TODO remove prepend: true once JSS is out
-  return createCache({ key: 'css', prepend: true });
+  return createCache({ key: 'css', prepend: true, stylisPlugins: [globalSelector] });
 }
