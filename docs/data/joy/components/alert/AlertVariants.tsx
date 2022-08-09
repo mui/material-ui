b8ff128014098ec4e849c831b9edbeefb24e6030
danilo leal <67129314+danilo-leal@users.noreply.github.com>
2022-08-09 00:52:28 -0300

    fix variants demo

--- docs/data/joy/components/alert/AlertVariants.tsx
+++ docs/data/joy/components/alert/AlertVariants.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import Box from '@mui/joy/Box';
-import Alert from '@mui/joy/Button';
+import Alert from '@mui/joy/Alert';
 
 export default function AlertVariants() {
   return (
-    <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
+    <Box sx={{ display: 'flex', gap: 2, width: '100%', flexDirection: 'column' }}>
       <Alert variant="solid">Solid</Alert>
       <Alert variant="soft">Soft</Alert>
       <Alert variant="outlined">Outlined</Alert>
