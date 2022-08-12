4f0c612058f3dbda1f6153830be93835e57bdd48
Bharat Kashyap <bharatkashyap@outlook.com>
2022-08-12 19:19:52 +0800

    [docs] SectionHeadline configurable for Toolpad

--- docs/src/components/typography/SectionHeadline.tsx
+++ docs/src/components/typography/SectionHeadline.tsx
@@ -2,20 +2,33 @@ import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 
+interface SectionHeadlineTheme {
+  overlineColor?: string;
+  titleColor?: string;
+  descriptionColor?: string;
+}
+
 export default function SectionHeadline({
   overline,
   title,
   description,
+  localTheme,
 }: {
   overline: React.ReactNode;
   title: React.ReactNode;
   description?: React.ReactNode;
+  localTheme?: (mode?: string) => SectionHeadlineTheme;
 }) {
   const globalTheme = useTheme();
   const mode = globalTheme.palette.mode;
-  const overlineColor = mode === 'dark' ? 'primary.300' : 'primary.600';
-  const titleColor = mode === 'dark' ? 'grey.100' : 'primaryDark.900';
-  const descriptionColor = mode === 'dark' ? 'grey.500' : 'grey.800';
+  const defaultOverlineColor = mode === 'dark' ? 'primary.300' : 'primary.600';
+  const defaultTitleColor = mode === 'dark' ? 'grey.100' : 'primaryDark.900';
+  const defaultDescriptionColor = mode === 'dark' ? 'grey.500' : 'grey.800';
+
+  const overlineColor: string = localTheme?.(mode)?.overlineColor ?? defaultOverlineColor;
+  const titleColor: string = localTheme?.(mode)?.titleColor ?? defaultTitleColor;
+  const descriptionColor: string = localTheme?.(mode)?.descriptionColor ?? defaultDescriptionColor;
+
   return (
     <React.Fragment>
       <Typography
