6bf8f66d950c7c4257fc9f67ca838406a68ed3e9
danilo leal <67129314+danilo-leal@users.noreply.github.com>
2022-08-17 03:56:28 -0300

    fix lint again

--- docs/data/base/components/table-pagination/UnstyledPaginationIntroduction.js
+++ docs/data/base/components/table-pagination/UnstyledPaginationIntroduction.js
@@ -145,10 +145,6 @@ export default function UnstyledTable() {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
