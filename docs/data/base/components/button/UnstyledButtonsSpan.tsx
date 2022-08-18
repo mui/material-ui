04504f85dbf3b3e9e9d221c82cd56bb9b26e3972
danilo leal <67129314+danilo-leal@users.noreply.github.com>
2022-08-18 01:25:14 -0300

    make the rest of Button's demos consistent

--- docs/data/base/components/button/UnstyledButtonsSpan.tsx
+++ docs/data/base/components/button/UnstyledButtonsSpan.tsx
@@ -19,12 +19,11 @@ const CustomButton = styled(ButtonUnstyled)`
   font-size: 0.875rem;
   background-color: ${blue[500]};
   padding: 12px 24px;
-  border-radius: 8px;
+  border-radius: 12px;
   color: white;
   transition: all 150ms ease;
   cursor: pointer;
   border: none;
-
   &:hover {
     background-color: ${blue[600]};
   }
