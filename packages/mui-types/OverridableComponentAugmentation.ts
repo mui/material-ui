0059bf45f5735545ab88a7a8a1b09c690476ebd5
Marija Najdova <mnajdova@gmail.com>
2022-08-22 15:57:48 +0200

    remove unnecessary comments

--- packages/mui-types/OverridableComponentAugmentation.ts
+++ packages/mui-types/OverridableComponentAugmentation.ts
@@ -7,10 +7,6 @@ declare module '@mui/types' {
    * Adjusts valid props based on the type of `component`.
    */
   export interface OverridableComponent<M extends OverridableTypeMap> {
-    // If you make any changes to this interface, please make sure to update the
-    // `OverridableComponent` type in `mui-material/src/OverridableComponent.d.ts` as well.
-    // Also, there are types in MUI Base that have a similar shape to this interface
-    // (e.g. SelectUnstyledType, OptionUnstyledType, etc.).
     <C extends React.ElementType>(
       props: {
         /**
