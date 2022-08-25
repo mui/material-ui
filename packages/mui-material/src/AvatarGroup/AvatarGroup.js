95e24e3348d7a9d86ceb0d28acc0ecb39922f9ff
Paul Schreiber <paul@techmatters.org>
2022-08-25 12:14:26 -0400

    fix: remove generated component prop

--- packages/mui-material/src/AvatarGroup/AvatarGroup.js
+++ packages/mui-material/src/AvatarGroup/AvatarGroup.js
@@ -171,11 +171,6 @@ AvatarGroup.propTypes /* remove-proptypes */ = {
    */
   className: PropTypes.string,
   /**
-   * The component used for the root node.
-   * Either a string to use a HTML element or a component.
-   */
-  component: PropTypes.elementType,
-  /**
    * The props used for each slot inside the AvatarGroup.
    * @default {}
    */
