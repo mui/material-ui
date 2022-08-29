9eded78bc2778e262efef3d550f59163487e2069
Andrew <andrew@mui.com>
2022-08-29 12:41:52 +0200

    allow to pass options to `mousePress` function

--- test/utils/userEvent.ts
+++ test/utils/userEvent.ts
@@ -1,24 +1,24 @@
 import * as React from 'react';
 import { click, mouseDown, mouseUp, keyDown, keyUp } from './fireDiscreteEvent';
-import { act, fireEvent } from './createRenderer';
+import { act, fireEvent, FireFunction } from './createRenderer';
 
 export function touch(target: Element): void {
   fireEvent.touchStart(target);
   fireEvent.touchEnd(target);
 }
 
-export function mousePress(target: Element): void {
+export const mousePress: (...args: Parameters<FireFunction>) => void = (target, options) => {
   if (React.version.startsWith('18')) {
-    fireEvent.mouseDown(target);
-    fireEvent.mouseUp(target);
-    fireEvent.click(target);
+    fireEvent.mouseDown(target, options);
+    fireEvent.mouseUp(target, options);
+    fireEvent.click(target, options);
   } else {
-    mouseDown(target);
-    mouseUp(target);
-    click(target);
+    mouseDown(target, options);
+    mouseUp(target, options);
+    click(target, options);
     act(() => {});
   }
-}
+};
 
 export function keyPress(target: Element, options: { key: string }): void {
   if (React.version.startsWith('18')) {
