8793c28ba4ae6169be96bd04d11ab0e5f4ce77f7
Paul Schreiber <paul@techmatters.org>
2022-08-30 16:09:43 -0400

    fix: allow component overriding for Stepper

--- packages/mui-material/src/Stepper/Stepper.d.ts
+++ packages/mui-material/src/Stepper/Stepper.d.ts
@@ -29,6 +29,11 @@ export interface StepperProps extends StandardProps<PaperProps> {
    */
   classes?: Partial<StepperClasses>;
   /**
+   * The component used for the root node.
+   * Either a string to use a HTML element or a component.
+   */
+  component?: React.ElementType;
+  /**
    * An element to be placed between each step.
    * @default <StepConnector />
    */
