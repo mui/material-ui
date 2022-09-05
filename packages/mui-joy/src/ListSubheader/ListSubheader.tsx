e9155f4cc4db606e28cadb50a4a2c8e22f66cd77
siriwatknp <siriwatkunaporn@gmail.com>
2022-09-05 14:44:31 +0700

    run proptypes

--- packages/mui-joy/src/ListSubheader/ListSubheader.tsx
+++ packages/mui-joy/src/ListSubheader/ListSubheader.tsx
@@ -104,18 +104,28 @@ ListSubheader.propTypes /* remove-proptypes */ = {
    */
   children: PropTypes.node,
   /**
-   * Override or extend the styles applied to the component.
-   */
-  classes: PropTypes.object,
-  /**
    * @ignore
    */
   className: PropTypes.string,
+  /**
+   * The color of the component. It supports those theme colors that make sense for this component.
+   * @default 'neutral'
+   */
+  color: PropTypes.oneOf(['danger', 'info', 'neutral', 'primary', 'success', 'warning']),
   /**
    * The component used for the root node.
    * Either a string to use a HTML element or a component.
    */
   component: PropTypes.elementType,
+  /**
+   * @ignore
+   */
+  id: PropTypes.string,
+  /**
+   * If `true`, the component has sticky position (with top = 0).
+   * @default false
+   */
+  sticky: PropTypes.bool,
   /**
    * The system prop that allows defining system overrides as well as additional CSS styles.
    */
@@ -124,6 +134,11 @@ ListSubheader.propTypes /* remove-proptypes */ = {
     PropTypes.func,
     PropTypes.object,
   ]),
+  /**
+   * The variant to use.
+   * @default 'plain'
+   */
+  variant: PropTypes.oneOf(['outlined', 'plain', 'soft', 'solid']),
 } as any;
 
 export default ListSubheader;
