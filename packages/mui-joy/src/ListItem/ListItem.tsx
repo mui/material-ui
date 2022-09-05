797e5598a1e7dd94696cfbc155e52a627a88efa3
siriwatknp <siriwatkunaporn@gmail.com>
2022-09-05 14:08:54 +0700

    run proptypes

--- packages/mui-joy/src/ListItem/ListItem.tsx
+++ packages/mui-joy/src/ListItem/ListItem.tsx
@@ -237,10 +237,6 @@ ListItem.propTypes /* remove-proptypes */ = {
    */
   children: PropTypes.node,
   /**
-   * Override or extend the styles applied to the component.
-   */
-  classes: PropTypes.object,
-  /**
    * @ignore
    */
   className: PropTypes.string,
