fa1288d88a4a70af506bd0b206ada0dc5e3df42b
Kátia Bernardes <katiabernardes2@hotmail.com>
2022-09-06 15:45:15 -0300

    refactor: update lint

--- docs/pages/base/api/focus-trap.js
+++ docs/pages/base/api/focus-trap.js
@@ -9,7 +9,7 @@ export default function Page(props) {
 }
 
 Page.getInitialProps = () => {
-  const req = require.context('docs/translations/api-docs/trap-focus', false, /trap-focus.*.json$/);
+  const req = require.context('docs/translations/api-docs/focus-trap', false, /focus-trap.*.json$/);
   const descriptions = mapApiPageTranslations(req);
 
   return {
