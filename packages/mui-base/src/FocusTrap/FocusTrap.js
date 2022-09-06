355d0521c6d32cfc57e41adff41076d775acd9d2
Kátia Bernardes <katiabernardes2@hotmail.com>
2022-09-06 13:48:02 -0300

    refactor: update TrapFocus name on missing files.

--- packages/mui-base/src/FocusTrap/FocusTrap.js
+++ packages/mui-base/src/FocusTrap/FocusTrap.js
@@ -351,26 +351,26 @@ FocusTrap.propTypes /* remove-proptypes */ = {
    */
   children: elementAcceptingRef,
   /**
-   * If `true`, the trap focus will not automatically shift focus to itself when it opens, and
+   * If `true`, the focus trap will not automatically shift focus to itself when it opens, and
    * replace it to the last focused element when it closes.
-   * This also works correctly with any trap focus children that have the `disableAutoFocus` prop.
+   * This also works correctly with any focus trap children that have the `disableAutoFocus` prop.
    *
-   * Generally this should never be set to `true` as it makes the trap focus less
+   * Generally this should never be set to `true` as it makes the focus trap less
    * accessible to assistive technologies, like screen readers.
    * @default false
    */
   disableAutoFocus: PropTypes.bool,
   /**
-   * If `true`, the trap focus will not prevent focus from leaving the trap focus while open.
+   * If `true`, the focus trap will not prevent focus from leaving the focus trap while open.
    *
-   * Generally this should never be set to `true` as it makes the trap focus less
+   * Generally this should never be set to `true` as it makes the focus trap less
    * accessible to assistive technologies, like screen readers.
    * @default false
    */
   disableEnforceFocus: PropTypes.bool,
   /**
-   * If `true`, the trap focus will not restore focus to previously focused element once
-   * trap focus is hidden or unmounted.
+   * If `true`, the focus trap will not restore focus to previously focused element once
+   * focus trap is hidden or unmounted.
    * @default false
    */
   disableRestoreFocus: PropTypes.bool,
@@ -384,7 +384,7 @@ FocusTrap.propTypes /* remove-proptypes */ = {
    * This prop extends the `open` prop.
    * It allows to toggle the open state without having to wait for a rerender when changing the `open` prop.
    * This prop should be memoized.
-   * It can be used to support multiple trap focus mounted at the same time.
+   * It can be used to support multiple focus trap mounted at the same time.
    * @default function defaultIsEnabled() {
    *   return true;
    * }
