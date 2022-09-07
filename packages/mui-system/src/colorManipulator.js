99a9d46b6dfea3c18a8ae8d8acbaefb9884c7dbe
Mark Daniel <83738282+Mark-777-0@users.noreply.github.com>
2022-09-06 22:18:55 -0400

    Fixing Missing Radix, and Nested Ternaries
    Signed-off-by: Mark Daniel <83738282+Mark-777-0@users.noreply.github.com>

--- packages/mui-system/src/colorManipulator.js
+++ packages/mui-system/src/colorManipulator.js
@@ -26,21 +26,36 @@ function clamp(value, min = 0, max = 1) {
  */
  function rgbToHsl(color) {
   const rgb = color.replace(/[^\d,]/g, '').split(',')
-  const r= parseInt(rgb[0]) / 255
-  const g= parseInt(rgb[1]) /255
-  const b= parseInt(rgb[2]) / 255
+  const r= parseInt(rgb[0],10) / 255
+  const g= parseInt(rgb[1],10) /255
+  const b= parseInt(rgb[2],10) / 255
   const l = Math.max(r, g, b);
   const s = l - Math.min(r, g, b);
-  const h = s
-    ? l === r
-      ? (g - b) / s
-      : l === g
-      ? 2 + (b - r) / s
-      : 4 + (r - g) / s
-    : 0;
+  let h
+
+  if (s) {
+    if (l === r){
+      h=(g - b) / s
+    } else if (l === g) {
+      h=2 + (b - r) / s
+    } else{
+      h=4 + (r - g) / s
+    }} else {
+    h=0
+   }
+  
+    let sFinal
+    if (s){
+      if (l <= 0.5){
+        sFinal =Math.round(100 *  s / (2 * l - s))
+      } else {
+        sFinal =Math.round(100 * s / (2 - (2 * l - s)))
+      }
+    } else {
+      sFinal = 0
+    }
   
     const hFinal = Math.round(60 * h < 0 ? 60 * h + 360 : 60 * h);
-    const sFinal = Math.round(100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0))
     const lFinal = Math.round((100 * (2 * l - s)) / 2)
 
   return `hsl(${hFinal}, ${sFinal}%, ${lFinal}%)` ;
