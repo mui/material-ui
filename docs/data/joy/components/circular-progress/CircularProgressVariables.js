8f2fcb27615365ef1783595aa4901c4b22ff1a6c
siriwatknp <siriwatkunaporn@gmail.com>
2022-09-08 23:51:38 +0700

    simplify variable demo

--- docs/data/joy/components/circular-progress/CircularProgressVariables.js
+++ docs/data/joy/components/circular-progress/CircularProgressVariables.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import JoyVariablesDemo from 'docs/src/modules/components/JoyVariablesDemo';
-import Box from '@mui/joy/Box';
 import CircularProgress from '@mui/joy/CircularProgress';
 
 export default function CircularProgressVariables() {
@@ -10,30 +9,18 @@ export default function CircularProgressVariables() {
       data={[
         {
           var: '--CircularProgress-size',
-          defaultValue: '85px',
+          defaultValue: '40px',
         },
         {
           var: '--CircularProgress-track-thickness',
-          defaultValue: '10px',
+          defaultValue: '6px',
         },
         {
           var: '--CircularProgress-progress-thickness',
-          defaultValue: '10px',
+          defaultValue: '6px',
         },
       ]}
-      renderDemo={(sx) => (
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            gap: 4,
-          }}
-        >
-          <CircularProgress sx={sx} />
-          <CircularProgress variant="soft" color="neutral" sx={sx} />
-        </Box>
-      )}
+      renderDemo={(sx) => <CircularProgress sx={sx} />}
     />
   );
 }
