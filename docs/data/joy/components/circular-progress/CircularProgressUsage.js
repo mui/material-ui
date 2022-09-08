a91d5cc87a99bfbac9bf697a69e580ab7b71fb85
Benny Joo <sldisek783@gmail.com>
2022-09-08 12:07:32 +0100

    Set default value to empty string for value prop in playground demo

--- docs/data/joy/components/circular-progress/CircularProgressUsage.js
+++ docs/data/joy/components/circular-progress/CircularProgressUsage.js
@@ -32,7 +32,7 @@ export default function CircularProgressUsage() {
         {
           propName: 'value',
           knob: 'input',
-          defaultValue: 0,
+          defaultValue: "",
         },
       ]}
       renderDemo={(props) => <CircularProgress {...props} />}
