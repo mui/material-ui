804dbf50b6ce7cf7fb7aecca987062e637d305fb
Paul Schreiber <paul@techmatters.org>
2022-09-09 11:48:04 -0400

    fix: use h1 for dialog titles

--- packages/mui-material/src/DialogTitle/DialogTitle.js
+++ packages/mui-material/src/DialogTitle/DialogTitle.js
@@ -41,7 +41,7 @@ const DialogTitle = React.forwardRef(function DialogTitle(inProps, ref) {
 
   return (
     <DialogTitleRoot
-      component="h2"
+      component="h1"
       className={clsx(classes.root, className)}
       ownerState={ownerState}
       ref={ref}
