4799180cf9aaad2c7d8df84c407577f5aad28261
Matheus Wichman <matheushw@outlook.com>
2022-09-13 11:15:01 -0300

    Try redirect to stable docs

--- docs/pages/blog/first-look-at-joy.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import TopLayoutBlog from 'docs/src/modules/components/TopLayoutBlog';
-import { docs } from './first-look-at-joy.md?@mui/markdown';
-
-export default function Page() {
-  return <TopLayoutBlog docs={docs} />;
-}
