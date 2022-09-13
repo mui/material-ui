4888e8fb8e95ff310808d400be4b359aeb9670a9
Kátia Bernardes <katiabernardes2@hotmail.com>
2022-09-13 15:56:43 -0300

    refactor: move useMenuItem to its own folder.

--- docs/data/base/components/menu/UseMenu.js
+++ docs/data/base/components/menu/UseMenu.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import { useMenu, MenuUnstyledContext } from '@mui/base/MenuUnstyled';
-import { useMenuItem } from '@mui/base/MenuItemUnstyled';
+import useMenuItem from '@mui/base/useMenuItem';
 import { GlobalStyles } from '@mui/system';
 import clsx from 'clsx';
 
