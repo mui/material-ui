23e1fe66facaf47f01081e2b18090fedb17c88b5
Kátia Bernardes <katiabernardes2@hotmail.com>
2022-09-13 13:52:36 -0300

    refactor: update useFormControlUnstyledContext exports.

--- packages/mui-base/src/useFormControlUnstyledContext/index.ts
+++ packages/mui-base/src/useFormControlUnstyledContext/index.ts
@@ -1,1 +1,3 @@
 export { default as useFormControlUnstyledContext } from './useFormControlUnstyledContext';
+export { default } from './useFormControlUnstyledContext';
+export * from './useFormControlUnstyledContext';
