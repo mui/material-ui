5fb9aba269df1aead59d97cdeeb04a964dd2c9fa
Kátia Bernardes <katiabernardes2@hotmail.com>
2022-09-13 10:32:21 -0300

    refactor: move useInput to its own folder.

--- packages/mui-base/src/InputUnstyled/index.ts
+++ packages/mui-base/src/InputUnstyled/index.ts
@@ -2,9 +2,5 @@ export { default } from './InputUnstyled';
 
 export * from './InputUnstyled.types';
 
-export { default as useInput } from './useInput';
-
-export * from './useInput.types';
-
 export { default as inputUnstyledClasses } from './inputUnstyledClasses';
 export * from './inputUnstyledClasses';
