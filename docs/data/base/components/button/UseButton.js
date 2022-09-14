623d5248089108b54b542f59d949976fb7b5d173
Kátia Bernardes <katiabernardes2@hotmail.com>
2022-09-14 10:37:21 -0300

    refactor: move useButton to its own folder.

--- docs/data/base/components/button/UseButton.js
+++ docs/data/base/components/button/UseButton.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import useButton from '@mui/base/useButton';
-
 import { styled } from '@mui/system';
 import Stack from '@mui/material/Stack';
 
