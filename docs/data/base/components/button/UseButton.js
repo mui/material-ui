7be16397008b722ca73712704096af7bfe2e74c5
Kátia Bernardes <katiabernardes2@hotmail.com>
2022-09-14 15:35:38 -0300

    refactor: update lint

--- docs/data/base/components/button/UseButton.js
+++ docs/data/base/components/button/UseButton.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import useButton from '@mui/base/useButton';
+
 import { styled } from '@mui/system';
 import Stack from '@mui/material/Stack';
 
