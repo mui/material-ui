8e7c873e3985e7585695af21ebc9fc808777e848
Kátia Bernardes <katiabernardes2@hotmail.com>
2022-09-14 09:58:03 -0300

    refactor: move useTabs to its own folder.

--- packages/mui-base/src/index.d.ts
+++ packages/mui-base/src/index.d.ts
@@ -22,6 +22,8 @@ export * from './useTabPanel';
 
 export * from './useTabsList';
 
+export * from './useTabs';
+
 export { default as BadgeUnstyled } from './BadgeUnstyled';
 
 export { default as ButtonUnstyled } from './ButtonUnstyled';
