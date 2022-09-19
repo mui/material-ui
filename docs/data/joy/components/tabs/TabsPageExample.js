f16228ec834a2ee7fce592c2665c248a151d26f3
siriwatknp <siriwatkunaporn@gmail.com>
2022-09-19 18:11:58 +0700

    update demos

--- docs/data/joy/components/tabs/TabsPageExample.js
+++ docs/data/joy/components/tabs/TabsPageExample.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/joy/Box';
 import Chip from '@mui/joy/Chip';
+import Divider from '@mui/joy/Divider';
 import Tabs from '@mui/joy/Tabs';
 import TabList from '@mui/joy/TabList';
 import Tab, { tabClasses } from '@mui/joy/Tab';
@@ -82,15 +83,7 @@ export default function TabsPageExample() {
           </Tab>
           <Tab>Search library</Tab>
         </TabList>
-        <Box
-          sx={(theme) => ({
-            '--bg': theme.vars.palette.background.level3,
-            height: '1px',
-            background: 'var(--bg)',
-            boxShadow: '0 0 0 100vmax var(--bg)',
-            clipPath: 'inset(0 -100vmax)',
-          })}
-        />
+        <Divider inset="fullscreen" />
         <Box
           sx={(theme) => ({
             '--bg': theme.vars.palette.background.level1,
