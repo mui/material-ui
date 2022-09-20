e5b655e2bbed7ef923516be451bfc65d516e7f51
siriwatknp <siriwatkunaporn@gmail.com>
2022-09-20 18:58:13 +0700

    add comment

--- packages/mui-joy/src/ListDivider/ListDivider.tsx
+++ packages/mui-joy/src/ListDivider/ListDivider.tsx
@@ -14,6 +14,7 @@ const useUtilityClasses = (ownerState: ListDividerOwnerState) => {
   const slots = {
     root: [
       'root',
+      // `insetContext` class is already produced by Divider
       ownerState.inset && ownerState.inset !== 'context' && `inset${capitalize(ownerState.inset)}`,
     ],
   };
