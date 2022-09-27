8ed270124aff07d91cde8bb9f2a4f90d078017fa
siriwatknp <siriwatkunaporn@gmail.com>
2022-09-27 10:38:48 +0700

    add alignSelf start

--- packages/mui-joy/src/FormLabel/FormLabel.tsx
+++ packages/mui-joy/src/FormLabel/FormLabel.tsx
@@ -23,7 +23,8 @@ const FormLabelRoot = styled('label', {
   overridesResolver: (props, styles) => styles.root,
 })<{ ownerState: FormLabelProps }>(({ theme }) => ({
   WebkitTapHighlightColor: 'transparent',
-  display: 'flex',
+  alignSelf: 'flex-start', // to not fill the block space. It seems like a bug when clicking on empty space (within the label area), even though it is not.
+  display: 'inline-flex',
   alignItems: 'center',
   flexWrap: 'wrap',
   userSelect: 'none',
