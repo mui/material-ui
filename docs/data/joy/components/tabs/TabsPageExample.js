5abd65ac19a5e4426a772c7b1cf54a656808efd3
danilo leal <67129314+danilo-leal@users.noreply.github.com>
2022-09-27 02:31:32 -0300

    yarn docs:typescript:formatted

--- docs/data/joy/components/tabs/TabsPageExample.js
+++ docs/data/joy/components/tabs/TabsPageExample.js
@@ -17,7 +17,6 @@ export default function TabsPageExample() {
         bgcolor: 'background.body',
         flexGrow: 1,
         m: -3,
-        p: 3,
         overflowX: 'hidden',
         borderRadius: 'md',
       }}
@@ -26,11 +25,15 @@ export default function TabsPageExample() {
         aria-label="Pipeline"
         value={index}
         onChange={(event, value) => setIndex(value)}
-        sx={{ '--Tabs-gap': '0px', maxWidth: 400, mx: 'auto' }}
+        sx={{ '--Tabs-gap': '0px' }}
       >
         <TabList
           variant="plain"
           sx={{
+            width: '100%',
+            maxWidth: 400,
+            mx: 'auto',
+            pt: 2,
             alignSelf: 'flex-start',
             [`& .${tabClasses.root}`]: {
               bgcolor: 'transparent',
@@ -97,36 +100,36 @@ export default function TabsPageExample() {
             background: 'var(--bg)',
             boxShadow: '0 0 0 100vmax var(--bg)',
             clipPath: 'inset(0 -100vmax)',
-            px: 1.5,
+            px: 4,
             py: 2,
           })}
         >
           <TabPanel value={0}>
             <Typography
-              level="h1"
+              level="h2"
               component="div"
-              fontSize="xl2"
+              fontSize="lg"
               mb={2}
-              textColor="text.secondary"
+              textColor="text.primary"
             >
-              Deals
+              Deals panel
             </Typography>
           </TabPanel>
           <TabPanel value={1}>
             <Typography
-              level="h1"
+              level="h2"
               component="div"
-              fontSize="xl2"
+              fontSize="lg"
               mb={2}
-              textColor="text.secondary"
+              textColor="text.primary"
             >
-              Library
+              Library panel
             </Typography>
           </TabPanel>
           <TabPanel value={2}>
             <Input
               autoFocus
-              placeholder="type a library..."
+              placeholder="Type in third panel..."
               startDecorator={<SearchRounded />}
             />
           </TabPanel>
