262e93407d15a295726f109f01158cc8d42d8c4b
Marija Najdova <mnajdova@gmail.com>
2022-10-03 13:27:39 +0200

    Use same coding style as other demos

--- docs/data/material/components/autocomplete/RenderGroup.tsx
+++ docs/data/material/components/autocomplete/RenderGroup.tsx
@@ -3,22 +3,20 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import { styled, lighten, darken } from '@mui/system';
 
-const GroupHeader = styled('div')(({ theme }) => {
-  return {
-    position: 'sticky',
-    top: '-8px',
-    padding: '4px 10px',
-    color: theme.palette.primary.main,
-    backgroundColor:
-      theme.palette.mode === 'light'
-        ? lighten(theme.palette.primary.light, 0.85)
-        : darken(theme.palette.primary.main, 0.8),
-  };
-});
+const GroupHeader = styled('div')(({ theme }) => ({
+  position: 'sticky',
+  top: '-8px',
+  padding: '4px 10px',
+  color: theme.palette.primary.main,
+  backgroundColor:
+    theme.palette.mode === 'light'
+      ? lighten(theme.palette.primary.light, 0.85)
+      : darken(theme.palette.primary.main, 0.8),
+}));
 
-const GroupItems = styled('ul')(`
-  padding: 0;
-`);
+const GroupItems = styled('ul')({
+  padding: 0,
+});
 
 export default function RenderGroup() {
   const options = top100Films.map((option) => {
