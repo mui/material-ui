91635f7734fec12f6ba29ca5276f8a321ae8f94d
Olivier Tassinari <olivier.tassinari@gmail.com>
2022-10-04 21:20:35 +0200

    might be clearer

--- docs/src/components/pricing/PricingTable.tsx
+++ docs/src/components/pricing/PricingTable.tsx
@@ -737,7 +737,7 @@ const premiumData: Record<string, React.ReactNode> = {
   'mui-x-development': <Info value="1 year" />,
   'mui-x-updates': <Info value="1 year" />,
   // Support
-  'core-support': pending,
+  'core-support': <Info value={pending} metadata="Eventually" />,
   'x-support': <Info value={yes} metadata="Priority over Pro" />,
   'tech-advisory': pending,
   'support-duration': <Info value="1 year" />,
