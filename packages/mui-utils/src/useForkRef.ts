0a3f43ae6aed88ce8466b2c80b5f23b8c6c9027d
Michał Dudak <michal@dudak.me>
2022-10-05 11:18:23 +0200

    Change parameter name

--- packages/mui-utils/src/useForkRef.ts
+++ packages/mui-utils/src/useForkRef.ts
@@ -3,7 +3,7 @@ import setRef from './setRef';
 
 export default function useForkRef<Instance>(
   ...refs: Array<React.Ref<Instance> | undefined>
-): React.Ref<Instance> {
+): React.RefCallback<Instance> | null {
   /**
    * This will create a new function if the refs passed to this hook change and are all defined.
    * This means react will call the old forkRef with `null` and the new forkRef
@@ -13,9 +13,10 @@ export default function useForkRef<Instance>(
     if (refs.every((ref) => ref == null)) {
       return null;
     }
-    return (refValue) => {
+
+    return (instance) => {
       refs.forEach((ref) => {
-        setRef(ref, refValue);
+        setRef(ref, instance);
       });
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
