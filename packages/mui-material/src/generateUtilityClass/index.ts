61babab2576a1aaa74466c04d89a08e73381bb44
Michał Dudak <michal@dudak.me>
2022-10-10 13:29:19 +0200

    Do not generate product-specific classes

--- packages/mui-material/src/generateUtilityClass/index.ts
+++ packages/mui-material/src/generateUtilityClass/index.ts
@@ -1,17 +1,2 @@
-import { unstable_generateUtilityClass as generateUtilityClass } from '@mui/utils';
-
+export { unstable_generateUtilityClass as default } from '@mui/utils';
 export type { GlobalStateSlot } from '@mui/utils';
-export const ProductClass = 'Mui-material';
-
-export default function generateBaseUtilityClass(
-  componentName: string,
-  slot: string,
-  globalStatePrefix = 'Mui',
-) {
-  const slotClass = generateUtilityClass(componentName, slot, globalStatePrefix);
-  if (slot === 'root') {
-    return `${ProductClass} ${slotClass}`;
-  }
-
-  return slotClass;
-}
