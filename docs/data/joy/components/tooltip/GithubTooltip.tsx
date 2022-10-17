8941fd6981ddc0478097a32ab8a5bf7652affc0d
siriwatknp <siriwatkunaporn@gmail.com>
2022-10-17 14:04:04 +0700

    fix github tooltip

--- docs/data/joy/components/tooltip/GithubTooltip.tsx
+++ docs/data/joy/components/tooltip/GithubTooltip.tsx
@@ -4,13 +4,14 @@ import Tooltip from '@mui/joy/Tooltip';
 import Typography from '@mui/joy/Typography';
 import Link from '@mui/joy/Link';
 import Chip from '@mui/joy/Chip';
-import IconButton from '@mui/joy/IconButton';
 import AdjustIcon from '@mui/icons-material/Adjust';
 
 export default function GithubTooltip() {
   return (
     <Tooltip
       placement="top-end"
+      variant="outlined"
+      arrow
       title={
         <Box
           sx={{
@@ -21,60 +22,48 @@ export default function GithubTooltip() {
             p: 1,
           }}
         >
-          <Link
-            underline="none"
-            href="#common-examples"
-            variant="plain"
-            color="neutral"
+          <Typography
             fontSize="sm"
             textColor="grey"
-            endDecorator={
-              <Typography fontSize="sm" textColor="grey">
-                on Feb 25
-              </Typography>
+            startDecorator={
+              <Link
+                underline="always"
+                href="#common-examples"
+                color="neutral"
+                fontSize="sm"
+              >
+                mui/material-ui
+              </Link>
             }
-            sx={{ '--Link-gap': '0.5rem', pr: 0, pl: 2 }}
           >
-            mui/material-ui
-          </Link>
-          <Box sx={{ display: 'flex', gap: 0.5, width: '100%' }}>
+            on Feb 25
+          </Typography>
+          <Box sx={{ display: 'flex', gap: 1, width: '100%', mt: 1 }}>
+            <AdjustIcon color="success" />
             <Box>
-              <IconButton size="lg" variant="plain" color="success">
-                <AdjustIcon />
-              </IconButton>
-            </Box>
-            <Box>
-              <Typography sx={{ fontWeight: 'bold' }}>
-                {'[system] grey is no more recognized as color with the sx prop'}
+              <Typography fontWeight="lg" fontSize="sm">
+                [system] grey is no more recognized as color with the sx prop
               </Typography>
-              <Typography textColor="grey" sx={{ mb: 1 }}>
-                {
-                  'Duplicates I have searched the existing issues Latest version I have tested the ...'
-                }
+              <Typography textColor="text.secondary" fontSize="sm" sx={{ mb: 1 }}>
+                Duplicates I have searched the existing issues Latest version I have
+                tested the …
               </Typography>
-              <Chip size="sm" color="danger">
+              <Chip size="sm" color="danger" sx={{ fontWeight: 'lg' }}>
                 bug 🐛
               </Chip>
-              <Chip size="sm" color="info" sx={{ ml: 1 }}>
+              <Chip size="sm" color="info" sx={{ ml: 1, fontWeight: 'lg' }}>
                 package: system
               </Chip>
             </Box>
           </Box>
         </Box>
       }
-      variant="outlined"
-      size="lg"
-      arrow
     >
       <Link
         href="#common-examples"
         underline="none"
-        startDecorator={
-          <IconButton variant="plain" color="success">
-            <AdjustIcon />
-          </IconButton>
-        }
-        sx={{ '--Link-gap': '0.5rem', pr: 0, pl: 1, fontWeight: 'bold' }}
+        startDecorator={<AdjustIcon color="success" />}
+        sx={{ fontWeight: 'lg' }}
       >
         [system] grey is no more recognized as color with the sx prop
       </Link>
