44e3181bfeb8a0ccafb2ffbe5c16fda78db478a9
siriwatknp <siriwatkunaporn@gmail.com>
2022-10-21 13:22:52 +0700

    run proptypes

--- packages/mui-joy/src/Autocomplete/Autocomplete.tsx
+++ packages/mui-joy/src/Autocomplete/Autocomplete.tsx
@@ -816,7 +816,7 @@ Autocomplete.propTypes /* remove-proptypes */ = {
    */
   popupIcon: PropTypes.node,
   /**
-   * If `true`, the component becomes readonly. It is also supported for multiple tags where the tag cannot be deleted.
+   * If `true`, the component becomes read-only. It is also supported in multiple tags where the tag cannot be deleted.
    * @default false
    */
   readOnly: PropTypes.bool,
