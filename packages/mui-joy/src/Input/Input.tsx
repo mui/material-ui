42cc8ac590b8b9140a7b04b2daaa92013798b0ba
siriwatknp <siriwatkunaporn@gmail.com>
2022-10-21 16:07:27 +0700

    fix input variables

--- packages/mui-joy/src/Input/Input.tsx
+++ packages/mui-joy/src/Input/Input.tsx
@@ -70,6 +70,7 @@ const InputRoot = styled('div', {
       '--Input-decorator-childRadius':
         'max(var(--Input-radius) - var(--_Input-paddingBlock), min(var(--_Input-paddingBlock) / 2, var(--Input-radius) / 2))',
       '--Button-minHeight': 'var(--Input-decorator-childHeight)',
+      '--Chip-minHeight': 'var(--Input-decorator-childHeight)',
       '--IconButton-size': 'var(--Input-decorator-childHeight)',
       '--Button-radius': 'var(--Input-decorator-childRadius)',
       '--IconButton-radius': 'var(--Input-decorator-childRadius)',
