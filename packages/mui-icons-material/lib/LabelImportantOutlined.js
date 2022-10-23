ea34a5f70089ba235605c7e8b6982a5a5aa73bcd
Vikrant Guleria <vguleria1108@gmail.com>
2022-10-23 20:50:51 +0530

    [icons]: Fix LabelImportantOutlined icon

--- packages/mui-icons-material/lib/LabelImportantOutlined.js
+++ packages/mui-icons-material/lib/LabelImportantOutlined.js
@@ -12,7 +12,7 @@ var _createSvgIcon = _interopRequireDefault(require("./utils/createSvgIcon"));
 var _jsxRuntime = require("react/jsx-runtime");
 
 var _default = (0, _createSvgIcon.default)( /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
-  d: "M4 18.99h11c.67 0 1.27-.32 1.63-.83L21 12l-4.37-6.16C16.27 5.33 15.67 5 15 5H4l5 7-5 6.99z"
+  d: 'M16.63 5.84C16.27 5.33 15.67 5 15 5H4l5 7-5 6.99h11c.67 0 1.27-.32 1.63-.83L21 12l-4.37-6.16zM15 17H7.89l3.57-5-3.57-5H15l3.55 5L15 17z',
 }), 'LabelImportantOutlined');
 
 exports.default = _default;
--- packages/mui-icons-material/lib/esm/LabelImportantOutlined.js
+++ packages/mui-icons-material/lib/esm/LabelImportantOutlined.js
@@ -1,5 +1,5 @@
 import createSvgIcon from './utils/createSvgIcon';
 import { jsx as _jsx } from "react/jsx-runtime";
 export default createSvgIcon( /*#__PURE__*/_jsx("path", {
-  d: "M4 18.99h11c.67 0 1.27-.32 1.63-.83L21 12l-4.37-6.16C16.27 5.33 15.67 5 15 5H4l5 7-5 6.99z"
+  d: 'M16.63 5.84C16.27 5.33 15.67 5 15 5H4l5 7-5 6.99h11c.67 0 1.27-.32 1.63-.83L21 12l-4.37-6.16zM15 17H7.89l3.57-5-3.57-5H15l3.55 5L15 17z',
 }), 'LabelImportantOutlined');
