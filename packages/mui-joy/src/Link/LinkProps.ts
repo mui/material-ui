2a77a330230dfd534225a914b957cd591e7802c3
Daher Alfawares <daher.alfawares@live.com>
2022-10-31 06:28:53 -0700

    [core] Enforce import * as React (#34878)

--- packages/mui-joy/src/Link/LinkProps.ts
+++ packages/mui-joy/src/Link/LinkProps.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { OverridableStringUnion, OverrideProps } from '@mui/types';
 import { SlotComponentProps } from '@mui/base/utils';
 import {
