66e68093da5505cbf8fa2b2c6cf6eb37960e0ba2
Petro Silenius <petro.silenius@gmail.com>
2022-10-31 21:26:58 +0200

    Set font display to 'swap'

--- examples/nextjs/src/theme.js
+++ examples/nextjs/src/theme.js
@@ -2,7 +2,12 @@ import { Roboto } from '@next/font/google';
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
-export const roboto = Roboto({ weight: '400', subsets: ['latin'], fallback: [ 'Helvetica', 'Arial', 'sans-serif'] });
+export const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: [ 'Helvetica', 'Arial', 'sans-serif']
+});
 
 // Create a theme instance.
 const theme = createTheme({
