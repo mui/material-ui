7d084d3da597ce1713bf01e5faa9bced475f6c3e
Sylvia Favello <90116354+sfavello@users.noreply.github.com>
2022-10-31 09:03:32 -0700

    erase unnecessary code

--- packages/mui-base/src/AutocompleteUnstyled/useAutocomplete.js
+++ packages/mui-base/src/AutocompleteUnstyled/useAutocomplete.js
@@ -852,7 +852,7 @@ export default function useAutocomplete(props) {
           break;
         case 'Delete':
           if (multiple && !readOnly && inputValue === '' && value.length > 0 && focusedTag !== -1) {
-            const index = focusedTag === -1 ? value.length - 1 : focusedTag;
+            const index = focusedTag;
             const newValue = value.slice();
             newValue.splice(index, 1);
             handleValue(event, newValue, 'removeOption', {
