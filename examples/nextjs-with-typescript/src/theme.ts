52801f74743b91df0635531b5c3fcfd01d3905a9
Petro Silenius <petro.silenius@gmail.com>
2022-11-01 20:51:19 +0200

    Use multiple font weights added in next 13.0.1

--- examples/nextjs-with-typescript/src/theme.ts
+++ examples/nextjs-with-typescript/src/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
 export const roboto = Roboto({
-  weight: '400',
+  weight: ['300','400','500','700'],
   subsets: ['latin'],
   display: 'swap',
   fallback: [ 'Helvetica', 'Arial', 'sans-serif']
