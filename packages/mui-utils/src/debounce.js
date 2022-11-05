1f33c44983aac9b61e22258360a37b88ebe7d077
Sanjaiyan Parthipan <parthipankalayini@gmail.com>
2022-11-05 12:39:56 +0530

    Adding requestAnimationFrame for optimized style calculations 🧮

--- packages/mui-utils/src/debounce.js
+++ packages/mui-utils/src/debounce.js
@@ -1,17 +1,41 @@
+export const requestAnimationFrameMUI =
+  (typeof self !== "undefined" &&
+    self.requestAnimationFrame &&
+    self.requestAnimationFrame.bind(window)) ||
+  function (cb) {
+    let start = Date.now();
+    return setTimeout(function () {
+      cb({
+        didTimeout: false,
+        timeRemaining: function () {
+          return Math.max(0, 50 - (Date.now() - start));
+        },
+      });
+    }, 166);
+  };
+
+export const cancelAnimationFrameMUI =
+  (typeof self !== "undefined" &&
+    self.cancelAnimationFrame &&
+    self.cancelAnimationFrame.bind(window)) ||
+  function (id) {
+    return clearTimeout(id);
+  };
+
 // Corresponds to 10 frames at 60 Hz.
 // A few bytes payload overhead when lodash/debounce is ~3 kB and debounce ~300 B.
-export default function debounce(func, wait = 166) {
+export default function debounce(func) {
   let timeout;
   function debounced(...args) {
     const later = () => {
       func.apply(this, args);
     };
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
+    cancelAnimationFrameMUI(timeout);
+    timeout = requestAnimationFrameMUI(later);
   }
 
   debounced.clear = () => {
-    clearTimeout(timeout);
+    cancelAnimationFrameMUI(timeout);
   };
 
   return debounced;
