cf67a3140c9a735a48291949cf1dd1adf6d014f5
Pratik Dev <87067570+PratikDev@users.noreply.github.com>
2022-11-08 23:20:28 +0600

    Update BasicTextFields.js
    added state so all fields have synced values. used FormControl here, so that all fields have placeholder's floating animation when one is changed
    
    Signed-off-by: Pratik Dev <87067570+PratikDev@users.noreply.github.com>

--- docs/data/material/components/text-fields/BasicTextFields.js
+++ docs/data/material/components/text-fields/BasicTextFields.js
@@ -1,8 +1,16 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
 
 export default function BasicTextFields() {
+
+  const [value, setValue] = React.useState('');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <Box
       component="form"
@@ -12,9 +20,34 @@ export default function BasicTextFields() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+      
+      <FormControl variant="standard">
+        <TextField
+         id="outlined-basic"
+         label="Outlined"
+         variant="outlined"
+         value={value}
+         onChange={handleChange} />
+      </FormControl>
+
+      <FormControl variant="standard">
+        <TextField
+         id="filled-basic"
+         label="Filled"
+         variant="filled"
+         value={value}
+         onChange={handleChange} />
+      </FormControl>
+
+      <FormControl variant="standard">
+        <TextField
+         id="standard-basic"
+         label="Standard"
+         variant="standard"
+         value={value}
+         onChange={handleChange} />
+      </FormControl>
+
     </Box>
   );
 }
