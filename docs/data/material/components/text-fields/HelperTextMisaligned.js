ff2cd58bd528adf388682e2ef15d785a16d974a0
Pratik Dev <87067570+PratikDev@users.noreply.github.com>
2022-11-09 19:59:28 +0600

    Update HelperTextMisaligned.js
    applied eslint and prettier
    
    Signed-off-by: Pratik Dev <87067570+PratikDev@users.noreply.github.com>

--- docs/data/material/components/text-fields/HelperTextMisaligned.js
+++ docs/data/material/components/text-fields/HelperTextMisaligned.js
@@ -1,21 +1,20 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 export default function HelperTextMisaligned() {
+  const [value, setValue] = React.useState("");
 
-const [value, setValue] = React.useState('');
-
-const handleChange = (event) => {
-  setValue(event.target.value);
-}
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
   return (
     <Box
       sx={{
-        display: 'flex',
-        alignItems: 'center',
-        '& > :not(style)': { m: 1 },
+        display: "flex",
+        alignItems: "center",
+        "& > :not(style)": { m: 1 },
       }}
     >
       <TextField
@@ -25,9 +24,9 @@ const handleChange = (event) => {
         value={value}
         onChange={handleChange}
       />
-      <TextField 
-        id="demo-helper-text-misaligned-no-helper" 
-        label="Name" 
+      <TextField
+        id="demo-helper-text-misaligned-no-helper"
+        label="Name"
         value={value}
         onChange={handleChange}
       />
