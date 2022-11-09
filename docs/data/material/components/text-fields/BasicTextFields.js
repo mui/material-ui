24f09cd9dd2a34155c0c4c0991ab0f801682b3c8
Pratik Dev <87067570+PratikDev@users.noreply.github.com>
2022-11-09 19:55:39 +0600

    Update BasicTextFields.js
    applied eslint and prettier
    
    Signed-off-by: Pratik Dev <87067570+PratikDev@users.noreply.github.com>

--- docs/data/material/components/text-fields/BasicTextFields.js
+++ docs/data/material/components/text-fields/BasicTextFields.js
@@ -1,10 +1,9 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 export default function BasicTextFields() {
-
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState("");
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -14,33 +13,34 @@ export default function BasicTextFields() {
     <Box
       component="form"
       sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
+        "& > :not(style)": { m: 1, width: "25ch" },
       }}
       noValidate
       autoComplete="off"
     >
+      <TextField
+        id="outlined-basic"
+        label="Outlined"
+        variant="outlined"
+        value={value}
+        onChange={handleChange}
+      />
 
-        <TextField
-         id="outlined-basic"
-         label="Outlined"
-         variant="outlined"
-         value={value}
-         onChange={handleChange} />
+      <TextField
+        id="filled-basic"
+        label="Filled"
+        variant="filled"
+        value={value}
+        onChange={handleChange}
+      />
 
-        <TextField
-         id="filled-basic"
-         label="Filled"
-         variant="filled"
-         value={value}
-         onChange={handleChange} />
-
-        <TextField
-         id="standard-basic"
-         label="Standard"
-         variant="standard"
-         value={value}
-         onChange={handleChange} />
-
+      <TextField
+        id="standard-basic"
+        label="Standard"
+        variant="standard"
+        value={value}
+        onChange={handleChange}
+      />
     </Box>
   );
 }
