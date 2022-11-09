494810c550eee155a01cc075095c52ef5b444440
Pratik Dev <87067570+PratikDev@users.noreply.github.com>
2022-11-09 20:00:59 +0600

    Update LayoutTextFields.js
    applied eslint and prettier
    
    Signed-off-by: Pratik Dev <87067570+PratikDev@users.noreply.github.com>

--- docs/data/material/components/text-fields/LayoutTextFields.js
+++ docs/data/material/components/text-fields/LayoutTextFields.js
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 function RedBar() {
   return (
@@ -8,52 +8,51 @@ function RedBar() {
       sx={{
         height: 20,
         backgroundColor: (theme) =>
-          theme.palette.mode === 'light'
-            ? 'rgba(255, 0, 0, 0.1)'
-            : 'rgb(255 132 132 / 25%)',
+          theme.palette.mode === "light"
+            ? "rgba(255, 0, 0, 0.1)"
+            : "rgb(255 132 132 / 25%)",
       }}
     />
   );
 }
 
 export default function LayoutTextFields() {
+  const [value, setValue] = React.useState("");
 
-const [value, setValue] = React.useState('')
-
-const handleChange = (event) => {
+  const handleChange = (event) => {
     setValue(event.target.value);
   };
 
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        '& .MuiTextField-root': { width: '25ch' },
+        display: "flex",
+        flexDirection: "column",
+        "& .MuiTextField-root": { width: "25ch" },
       }}
     >
       <RedBar />
-      <TextField 
-        label={'margin="none"'} 
-        id="margin-none" 
+      <TextField
+        label={'margin="none"'}
+        id="margin-none"
         value={value}
-        onChange={handleChange} 
+        onChange={handleChange}
       />
       <RedBar />
-      <TextField 
-        label={'margin="dense"'} 
-        id="margin-dense" 
-        margin="dense" 
+      <TextField
+        label={'margin="dense"'}
+        id="margin-dense"
+        margin="dense"
         value={value}
-        onChange={handleChange} 
+        onChange={handleChange}
       />
       <RedBar />
-      <TextField 
-        label={'margin="normal"'} 
-        id="margin-normal" 
-        margin="normal" 
+      <TextField
+        label={'margin="normal"'}
+        id="margin-normal"
+        margin="normal"
         value={value}
-        onChange={handleChange} 
+        onChange={handleChange}
       />
       <RedBar />
     </Box>
