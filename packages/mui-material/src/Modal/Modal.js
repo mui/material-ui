f77ebcdb51bc975425fd06e8ab75ee3b534fbff4
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2022-11-14 14:04:54 +0530

    yarn proptypes && yarn docs:api

--- packages/mui-material/src/Modal/Modal.js
+++ packages/mui-material/src/Modal/Modal.js
@@ -203,7 +203,7 @@ Modal.propTypes /* remove-proptypes */ = {
    * @default {}
    */
   componentsProps: PropTypes.shape({
-    backdrop: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+    backdrop: PropTypes.object,
     root: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
   }),
   /**
