f4110cd8db18306cd4dc576dca98f53a6cbdc24d
iamxukai <85668115+iamxukai@users.noreply.github.com>
2022-11-14 12:01:18 +0800

    Fix test

--- packages/mui-utils/src/composeClasses/composeClasses.ts
+++ packages/mui-utils/src/composeClasses/composeClasses.ts
@@ -1,6 +1,7 @@
 export default function composeClasses<ClassKey extends string>(
   slots: Record<ClassKey, ReadonlyArray<string | false | undefined | null>>,
   getUtilityClass: (slot: string) => string,
+  classes: Record<string, string> | undefined,
 ): Record<ClassKey, string> {
   const output: Record<ClassKey, string> = {} as any;
 
