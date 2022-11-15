2849eb1435d15ee86760ef1cf089040f17f7f417
danilo leal <67129314+danilo-leal@users.noreply.github.com>
2022-11-15 03:07:57 -0300

    reduce blur
    Signed-off-by: danilo leal <67129314+danilo-leal@users.noreply.github.com>

--- docs/src/layouts/AppHeader.tsx
+++ docs/src/layouts/AppHeader.tsx
@@ -24,7 +24,7 @@ const Header = styled('header')(({ theme }) => [
     top: 0,
     transition: theme.transitions.create('top'),
     zIndex: theme.zIndex.appBar,
-    backdropFilter: 'blur(20px)',
+    backdropFilter: 'blur(8px)',
     boxShadow: `inset 0px -1px 1px ${(theme.vars || theme).palette.grey[100]}`,
     backgroundColor: 'rgba(255,255,255,0.8)',
   },
