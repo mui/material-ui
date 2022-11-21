0393080ce6d8e8ed50685eae9bdaea2ce2dc9f6a
Marija Najdova <mnajdova@gmail.com>
2022-11-21 12:43:10 +0100

    lint

--- packages/mui-system/src/styleFunctionSx/index.d.ts
+++ packages/mui-system/src/styleFunctionSx/index.d.ts
@@ -7,7 +7,6 @@ export * from './StandardCssProperties';
 
 export { default as extendSxProp } from './extendSxProp';
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export { default as unstable_defaultSxConfig } from './defaultSxConfig';
 export * from './defaultSxConfig';
 
