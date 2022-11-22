76e4765310f5867b063c3bdf0dec70f7bd24cb9e
Matheus Wichman <matheushw@outlook.com>
2022-11-22 13:43:50 -0300

    Prevent matching variables starting with 'import'

--- docs/packages/markdown/extractImports.js
+++ docs/packages/markdown/extractImports.js
@@ -1,5 +1,5 @@
 const importModuleRegexp =
-  /^\s*import(?:["'\s]*(?:[\w*{}\n, ]+)from\s*)?["'\s]*([^"'{}$\s]+)["'\s].*/gm;
+  /^\s*import (?:["'\s]*(?:[\w*{}\n, ]+)from\s*)?["'\s]*([^"'{}$\s]+)["'\s].*/gm;
 
 function extractImports(code) {
   return (code.match(importModuleRegexp) || []).map((x) => x.replace(importModuleRegexp, '$1'));
