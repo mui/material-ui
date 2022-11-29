ab13d5b203c60b8c834c5e9710d52ddb008a1481
Marija Najdova <mnajdova@gmail.com>
2022-11-29 11:59:43 +0100

    Add more tests

--- packages/mui-joy/src/styles/styleFunctionSx.spec.tsx
+++ packages/mui-joy/src/styles/styleFunctionSx.spec.tsx
@@ -8,3 +8,15 @@ const Test = styled('div')(({ theme }) =>
     m: 2,
   }),
 );
+
+// Can be used in when styles are defined as arrays
+const TestArray = styled('div')(
+  ({ theme }) => theme.unstable_sx({ color: 'primary.100' }),
+  ({ theme }) => theme.unstable_sx({ mt: 2 }),
+);
+
+// Can be used inside pseudo elements
+const TestPseudo = styled('div')(({ theme }) => ({
+  ...theme.unstable_sx({ color: 'primary.100' }),
+  '&:hover': theme.unstable_sx({ color: 'primary.700' }),
+}));
