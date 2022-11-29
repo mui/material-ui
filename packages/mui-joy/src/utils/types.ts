98c630f24f0ba0d78f5bd51158b617c0fbc987b5
siriwatknp <siriwatkunaporn@gmail.com>
2022-11-29 12:17:14 +0700

    Fix types for Alert

--- packages/mui-joy/src/utils/types.ts
+++ packages/mui-joy/src/utils/types.ts
@@ -1,14 +1,49 @@
 import { SxProps } from '../styles/types';
 
-type SlotComponentCommonProps = {
+type SlotCommonProps = {
   component?: React.ElementType;
   sx?: SxProps;
 };
 
-export type SlotComponentProps<TSlotComponent extends React.ElementType, TOverrides, TOwnerState> =
-  | (Partial<React.ComponentPropsWithRef<TSlotComponent>> & TOverrides & SlotComponentCommonProps)
+// The `Record<string, unknown>` is to allow arbitrary attributes, e.g. data-*.
+export type RootSlotProps<TSlotComponent extends React.ElementType, TOverrides, TOwnerState> =
+  | (React.ComponentPropsWithRef<TSlotComponent> &
+      TOverrides &
+      SlotCommonProps &
+      TOwnerState &
+      Record<string, unknown>)
   | ((
       ownerState: TOwnerState,
-    ) => Partial<React.ComponentPropsWithRef<TSlotComponent>> &
+    ) => Record<string, unknown> &
+      React.ComponentPropsWithRef<TSlotComponent> &
       TOverrides &
-      SlotComponentCommonProps);
+      SlotCommonProps &
+      TOwnerState &
+      Record<string, unknown>);
+
+export type OtherSlotProps<TSlotComponent extends React.ElementType, TOverrides, TOwnerState> =
+  | (React.ComponentPropsWithRef<TSlotComponent> &
+      TOverrides &
+      SlotCommonProps &
+      Record<string, unknown>)
+  | ((
+      ownerState: TOwnerState,
+    ) => Record<string, unknown> &
+      React.ComponentPropsWithRef<TSlotComponent> &
+      TOverrides &
+      SlotCommonProps &
+      Record<string, unknown>);
+
+/**
+ * Use T to make sure that K contains all of the keys of T
+ *
+ * @example CreateSlotsAndSlotProps<'root' | 'decorator', { root: ..., decorator: ... }>
+ */
+export type CreateSlotsAndSlotProps<T extends string, K extends Record<T, any>> = {
+  slots?: {
+    [P in keyof K]?: React.ElementType;
+  };
+  slotProps?: {
+    [P in keyof K]?: K[P];
+  };
+};
