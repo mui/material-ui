dcd142559d6a4e2d4b7573aa8b9a5471febaba4a
Benny Joo <sldisek783@gmail.com>
2022-12-06 15:24:49 +0000

    Add css grid demo

---
+++ docs/data/joy/components/grid/CSSGrid.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { styled } from '@mui/joy/styles';
+import Box from '@mui/joy/Box';
+import Sheet from '@mui/joy/Sheet';
+
+const Item = styled(Sheet)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.vars.palette.neutral[500],
+}));
+
+export default function CSSGrid() {
+  return (
+    <Box sx={{ width: 1 }}>
+      <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
+        <Box gridColumn="span 8">
+          <Item>xs=8</Item>
+        </Box>
+        <Box gridColumn="span 4">
+          <Item>xs=4</Item>
+        </Box>
+        <Box gridColumn="span 4">
+          <Item>xs=4</Item>
+        </Box>
+        <Box gridColumn="span 8">
+          <Item>xs=8</Item>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
