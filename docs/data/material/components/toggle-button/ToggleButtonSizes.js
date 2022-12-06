2c4a1f3699e42fb2b62995b61d05a789b4fa4078
Arman <mail@arman.io>
2022-12-06 18:00:21 +0100

    [docs] Refactor ToggleGroupSizes example
    Signed-off-by: Arman <mail@arman.io>

--- docs/data/material/components/toggle-button/ToggleButtonSizes.js
+++ docs/data/material/components/toggle-button/ToggleButtonSizes.js
@@ -3,9 +3,9 @@ import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
 import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
 import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
 import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
-import Box from '@mui/material/Box';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import Stack from '@mui/material/Stack';
 
 export default function ToggleButtonSizes() {
   const [alignment, setAlignment] = React.useState('left');
@@ -36,15 +36,7 @@ export default function ToggleButtonSizes() {
   };
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        // TODO Replace with Stack
-        '& > :not(style) + :not(style)': { mt: 2 },
-      }}
-    >
+    <Stack spacing={2} alignItems="center">
       <ToggleButtonGroup size="small" {...control} aria-label="Small sizes">
         {children}
       </ToggleButtonGroup>
@@ -54,6 +46,6 @@ export default function ToggleButtonSizes() {
       <ToggleButtonGroup size="large" {...control} aria-label="Large sizes">
         {children}
       </ToggleButtonGroup>
-    </Box>
+    </Stack>
   );
 }
