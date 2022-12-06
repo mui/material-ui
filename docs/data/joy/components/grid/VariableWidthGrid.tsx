4217298dc547f9877bb171b4189a037084a0012d
Benny Joo <sldisek783@gmail.com>
2022-12-06 15:26:00 +0000

    Add variable width grid demo

---
+++ docs/data/joy/components/grid/VariableWidthGrid.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { styled } from '@mui/joy/styles';
+import Box from '@mui/joy/Box';
+import Sheet from '@mui/joy/Sheet';
+import Grid from '@mui/joy/Grid';
+
+const Item = styled(Sheet)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.vars.palette.neutral[500],
+}));
+
+export default function VariableWidthGrid() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3}>
+        <Grid xs="auto">
+          <Item>variable width content</Item>
+        </Grid>
+        <Grid xs={6}>
+          <Item>xs=6</Item>
+        </Grid>
+        <Grid xs>
+          <Item>xs</Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
