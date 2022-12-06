959db9a2444fbf7f180303b23a0b0579928ade10
Benny Joo <sldisek783@gmail.com>
2022-12-06 14:26:23 +0000

    Add responsive stack demo

---
+++ docs/data/joy/components/stack/ResponsiveStack.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Box from '@mui/joy/Box';
+import Sheet from '@mui/joy/Sheet';
+import Stack from '@mui/joy/Stack';
+import { styled } from '@mui/joy/styles';
+
+const Item = styled(Sheet)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.vars.palette.neutral[500],
+}));
+
+export default function ResponsiveStack() {
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Stack
+        direction={{ xs: 'column', sm: 'row' }}
+        spacing={{ xs: 1, sm: 2, md: 4 }}
+        justifyContent="center"
+      >
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+      </Stack>
+    </Box>
+  );
+}
