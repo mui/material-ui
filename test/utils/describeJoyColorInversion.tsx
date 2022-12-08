7b8b1cecb585f0d0b18201897a7b300df013d5ab
siriwatknp <siriwatkunaporn@gmail.com>
2022-12-08 18:03:29 +0700

    handle non root slot

--- test/utils/describeJoyColorInversion.tsx
+++ test/utils/describeJoyColorInversion.tsx
@@ -2,8 +2,14 @@ import * as React from 'react';
 import { expect } from 'chai';
 import { ThemeProvider } from '@mui/joy/styles';
 import Sheet from '@mui/joy/Sheet';
-import { createRenderer } from './createRenderer';
+import { createRenderer, MuiRenderResult } from './createRenderer';
 
+/**
+ * Test suite for checking the color inversion conformance for a component.
+ *
+ * Specify the `data-testid="test-element"` to the slot that apply `colorContext` className
+ * otherwise the root slot will be used as a test element.
+ */
 export default function describeJoyColorInversion(
   element: React.ReactElement,
   options: {
@@ -12,21 +18,25 @@ export default function describeJoyColorInversion(
   },
 ) {
   const { render } = createRenderer();
+  const getTestElement = (result: MuiRenderResult) => {
+    const { container, queryByTestId } = result;
+    return queryByTestId('test-element') ?? container.firstChild?.firstChild;
+  };
   describe('Joy Color Inversion', () => {
     describe('Feature enabled', () => {
       it('implicit color value', () => {
-        const { container } = render(
+        const result = render(
           <ThemeProvider>
             <Sheet invertedColors variant="solid" color="primary">
               {element}
             </Sheet>
           </ThemeProvider>,
         );
-        expect(container.firstChild?.firstChild).to.have.class(options.classes.colorContext);
+        expect(getTestElement(result)).to.have.class(options.classes.colorContext);
       });
 
       it('implicit color with theme default color', () => {
-        const { getByTestId } = render(
+        const result = render(
           <ThemeProvider
             theme={{
               components: {
@@ -38,26 +48,24 @@ export default function describeJoyColorInversion(
               },
             }}
           >
-            {React.cloneElement(element, { color: 'success', 'data-testid': 'element' })}
-            <Sheet data-testid="sheet" invertedColors variant="solid" color="primary">
+            <Sheet invertedColors variant="solid" color="primary">
               {React.cloneElement(element)}
             </Sheet>
           </ThemeProvider>,
         );
-        expect(getByTestId('element')).to.have.class(options.classes.colorSuccess);
-        expect(getByTestId('sheet')?.firstChild).to.have.class(options.classes.colorContext);
+        expect(getTestElement(result)).to.have.class(options.classes.colorContext);
       });
     });
 
     it('should use instance color', () => {
-      const { container } = render(
+      const result = render(
         <ThemeProvider>
           <Sheet invertedColors variant="solid" color="primary">
             {React.cloneElement(element, { color: 'primary' })}
           </Sheet>
         </ThemeProvider>,
       );
-      expect(container.firstChild?.firstChild).to.have.class(options.classes.colorPrimary);
+      expect(getTestElement(result)).to.have.class(options.classes.colorPrimary);
     });
   });
 }
