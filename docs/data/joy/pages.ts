7072b5438437302249be4af3c07747859e59c5be
Benny Joo <sldisek783@gmail.com>
2022-12-08 11:32:11 +0000

    Place grid under layout section

--- docs/data/joy/pages.ts
+++ docs/data/joy/pages.ts
@@ -68,11 +68,7 @@ const pages = [
       {
         pathname: '/joy-ui/components/surfaces',
         subheader: 'surfaces',
-        children: [
-          { pathname: '/joy-ui/react-card' },
-          { pathname: '/joy-ui/react-sheet' },
-          { pathname: '/joy-ui/react-grid' },
-        ],
+        children: [{ pathname: '/joy-ui/react-card' }, { pathname: '/joy-ui/react-sheet' }],
       },
       {
         pathname: '/joy-ui/components/navigation',
@@ -87,7 +83,7 @@ const pages = [
       {
         pathname: '/joy-ui/components/layout',
         subheader: 'layout',
-        children: [{ pathname: '/joy-ui/react-stack' }],
+        children: [{ pathname: '/joy-ui/react-grid' }, { pathname: '/joy-ui/react-stack' }],
       },
       {
         pathname: '/joy-ui/components/utils',
