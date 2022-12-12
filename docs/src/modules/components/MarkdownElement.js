b1a6ec58ff52d4ca7ae2a9b042eee6c405c78e7f
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2022-12-12 09:13:29 -0300

    fixing the fallback color tokens

--- docs/src/modules/components/MarkdownElement.js
+++ docs/src/modules/components/MarkdownElement.js
@@ -257,7 +257,7 @@ const Root = styled('div')(
       '&.MuiCallout-error': {
         color: `var(--muidocs-palette-error-900, ${lightTheme.palette.error[900]})`,
         backgroundColor: `var(--muidocs-palette-error-50, ${lightTheme.palette.error[50]})`,
-        borderColor: `var(--muidocs-palette-error-100, ${lightTheme.palette.error[200]})`,
+        borderColor: `var(--muidocs-palette-error-100, ${lightTheme.palette.error[100]})`,
         '& strong': {
           color: `var(--muidocs-palette-error-800, ${lightTheme.palette.error[800]})`,
         },
@@ -272,7 +272,7 @@ const Root = styled('div')(
       '&.MuiCallout-info': {
         color: `var(--muidocs-palette-primary-900, ${lightTheme.palette.primary[900]})`,
         backgroundColor: `var(--muidocs-palette-grey-50, ${lightTheme.palette.grey[50]})`,
-        borderColor: `var(--muidocs-palette-grey-100, ${lightTheme.palette.grey[200]})`,
+        borderColor: `var(--muidocs-palette-grey-200, ${lightTheme.palette.grey[200]})`,
         '& strong': {
           color: `var(--muidocs-palette-primary-800, ${lightTheme.palette.primary[800]})`,
         },
@@ -295,7 +295,7 @@ const Root = styled('div')(
       '&.MuiCallout-warning': {
         color: `var(--muidocs-palette-grey-900, ${lightTheme.palette.grey[900]})`,
         backgroundColor: alpha(lightTheme.palette.warning[50], 0.5),
-        borderColor: `var(--muidocs-palette-warning-800, ${alpha(
+        borderColor: `var(--muidocs-palette-warning-600, ${alpha(
           lightTheme.palette.warning[600],
           0.3,
         )})`,
@@ -508,7 +508,7 @@ const Root = styled('div')(
         '&.MuiCallout-error': {
           color: `var(--muidocs-palette-error-50, ${darkTheme.palette.error[50]})`,
           backgroundColor: alpha(darkTheme.palette.error[700], 0.15),
-          borderColor: `var(--muidocs-palette-error-800, ${alpha(
+          borderColor: `var(--muidocs-palette-error-600, ${alpha(
             lightTheme.palette.error[600],
             0.3,
           )})`,
@@ -516,7 +516,7 @@ const Root = styled('div')(
             color: `var(--muidocs-palette-error-100, ${darkTheme.palette.error[100]})`,
           },
           '& a': {
-            color: `var(--muidocs-palette-error-100, ${darkTheme.palette.error[200]})`,
+            color: `var(--muidocs-palette-error-200, ${darkTheme.palette.error[200]})`,
           },
         },
         '&.MuiCallout-info': {
@@ -524,18 +524,18 @@ const Root = styled('div')(
           backgroundColor: alpha(darkTheme.palette.grey[700], 0.2),
           borderColor: `var(--muidocs-palette-primary-800, ${darkTheme.palette.grey[800]})`,
           '& strong': {
-            color: `var(--muidocs-palette-primary-100, ${darkTheme.palette.primary[200]})`,
+            color: `var(--muidocs-palette-primary-200, ${darkTheme.palette.primary[200]})`,
           },
         },
         '&.MuiCallout-success': {
           color: `var(--muidocs-palette-success-50, ${darkTheme.palette.success[50]})`,
           backgroundColor: alpha(darkTheme.palette.success[700], 0.15),
-          borderColor: `var(--muidocs-palette-success-800, ${alpha(
+          borderColor: `var(--muidocs-palette-success-600, ${alpha(
             lightTheme.palette.success[600],
             0.3,
           )})`,
           '& strong': {
-            color: `var(--muidocs-palette-success-100, ${darkTheme.palette.success[200]})`,
+            color: `var(--muidocs-palette-success-200, ${darkTheme.palette.success[200]})`,
           },
           '& a': {
             color: `var(--muidocs-palette-success-100, ${darkTheme.palette.success[100]})`,
@@ -544,12 +544,12 @@ const Root = styled('div')(
         '&.MuiCallout-warning': {
           color: `var(--muidocs-palette-warning-50, ${darkTheme.palette.warning[50]})`,
           backgroundColor: alpha(darkTheme.palette.warning[700], 0.15),
-          borderColor: `var(--muidocs-palette-warning-800, ${alpha(
+          borderColor: `var(--muidocs-palette-warning-600, ${alpha(
             darkTheme.palette.warning[600],
             0.3,
           )})`,
           '& strong': {
-            color: `var(--muidocs-palette-warning-800, ${darkTheme.palette.warning[200]})`,
+            color: `var(--muidocs-palette-warning-200, ${darkTheme.palette.warning[200]})`,
           },
           '& a': {
             color: `var(--muidocs-palette-warning-100, ${darkTheme.palette.warning[100]})`,
