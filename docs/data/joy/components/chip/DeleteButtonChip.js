e4af0ca4e95911a858a598335cafbd4c1ecd37bf
sai6855 <60743144+sai6855@users.noreply.github.com>
2022-12-14 14:08:17 +0530

    ran yarn docs:typescript:formatted

--- docs/data/joy/components/chip/DeleteButtonChip.js
+++ docs/data/joy/components/chip/DeleteButtonChip.js
@@ -4,20 +4,32 @@ import ChipDelete from '@mui/joy/ChipDelete';
 import * as React from 'react';
 
 export default function DeleteButtonChip() {
+  const handleDelete = () => {
+    console.log('onDelete is called');
+  };
   return (
     <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
       <Chip
         size="sm"
         variant="outlined"
         color="danger"
-        endDecorator={<ChipDelete />}
+        endDecorator={<ChipDelete onDelete={handleDelete} />}
       >
         Remove
       </Chip>
-      <Chip variant="soft" color="danger" endDecorator={<ChipDelete />}>
+      <Chip
+        variant="soft"
+        color="danger"
+        endDecorator={<ChipDelete onDelete={handleDelete} />}
+      >
         Delete
       </Chip>
-      <Chip size="lg" variant="solid" color="danger" endDecorator={<ChipDelete />}>
+      <Chip
+        size="lg"
+        variant="solid"
+        color="danger"
+        endDecorator={<ChipDelete onDelete={handleDelete} />}
+      >
         Delete
       </Chip>
     </Box>
