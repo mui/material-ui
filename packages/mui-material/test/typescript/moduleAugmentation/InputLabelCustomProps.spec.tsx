80b9d24df53af5d94bb926ae6d62f3c865e8be2e
mickaelaustoni <mickael.austoni@gmail.com>
2022-12-15 08:52:15 +0100

    change path import

--- packages/mui-material/test/typescript/moduleAugmentation/InputLabelCustomProps.spec.tsx
+++ packages/mui-material/test/typescript/moduleAugmentation/InputLabelCustomProps.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { InputLabel } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
 
 declare module '@mui/material/InputLabel' {
   interface InputLabelPropsSizeOverrides {
