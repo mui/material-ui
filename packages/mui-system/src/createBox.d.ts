29d84c64645281ab3c80e4ef45331f1f1f98d8f8
Marija Najdova <mnajdova@gmail.com>
2022-12-19 12:18:40 +0100

    lint issue

--- packages/mui-system/src/createBox.d.ts
+++ packages/mui-system/src/createBox.d.ts
@@ -1,5 +1,5 @@
-import { BoxTypeMap } from './Box';
 import { OverridableComponent } from '@mui/types';
+import { BoxTypeMap } from './Box';
 import { Theme as SystemTheme } from './createTheme';
 
 export default function createBox<
