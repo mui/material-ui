7dfa7920d10949fed4fcd37b28fa1cd7f847f1e9
Benny Joo <sldisek783@gmail.com>
2022-12-21 14:08:33 -0500

    Make styling more stable

--- docs/data/material/components/autocomplete/GoogleMaps.js
+++ docs/data/material/components/autocomplete/GoogleMaps.js
@@ -7,7 +7,6 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import parse from 'autosuggest-highlight/parse';
 import throttle from 'lodash/throttle';
-import { ListItem } from '@mui/material';
 
 // This key was created specifically for the demo in mui.com.
 // You need to create a new one for your application.
@@ -116,13 +115,7 @@ export default function GoogleMaps() {
       )}
       renderOption={(props, option) => {
         const matches = option.structured_formatting.main_text_matched_substrings;
-        if (!matches) {
-          return (
-            <ListItem sx={{ color: 'text.secondary', pt: '6px', pb: '6px' }}>
-              No options
-            </ListItem>
-          );
-        }
+
         const parts = parse(
           option.structured_formatting.main_text,
           matches.map((match) => [match.offset, match.offset + match.length]),
@@ -131,13 +124,13 @@ export default function GoogleMaps() {
         return (
           <li {...props}>
             <Grid container alignItems="center">
-              <Grid item>
+              <Grid item xs={2}>
                 <Box
                   component={LocationOnIcon}
                   sx={{ color: 'text.secondary', mr: 2 }}
                 />
               </Grid>
-              <Grid item xs>
+              <Grid item xs={10}>
                 {parts.map((part, index) => (
                   <span
                     key={index}
@@ -150,7 +143,11 @@ export default function GoogleMaps() {
                   </span>
                 ))}
 
-                <Typography variant="body2" color="text.secondary">
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{ wordWrap: 'break-word' }}
+                >
                   {option.structured_formatting.secondary_text}
                 </Typography>
               </Grid>
