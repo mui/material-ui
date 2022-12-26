3be15fb93e8c751ee6e872c035351158707e0573
sai6855 <60743144+sai6855@users.noreply.github.com>
2022-12-26 15:28:17 +0530

    change name

--- packages/mui-base/src/SliderUnstyled/SliderValueLabelUnstyled.tsx
+++ packages/mui-base/src/SliderUnstyled/SliderValueLabelUnstyled.tsx
@@ -4,8 +4,8 @@ import clsx from 'clsx';
 import sliderUnstyledClasses from './sliderUnstyledClasses';
 import { SliderValueLabelUnstyledProps } from './SliderValueLabelUnstyled.types';
 
-const useValueLabelClasses = (props: SliderValueLabelUnstyledProps) => {
-  const { open } = props;
+const useValueLabelClasses = (ownerState: SliderValueLabelUnstyledProps) => {
+  const { open } = ownerState;
   const utilityClasses = {
     offset: clsx({
       [sliderUnstyledClasses.valueLabelOpen]: open,
