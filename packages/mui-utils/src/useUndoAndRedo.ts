12a84d1996d170d617ea06e11d8e22ea99b2b7c7
Benny Joo <sldisek783@gmail.com>
2022-12-26 13:39:04 -0500

    Build [useUndoAndRedo] hook

---
+++ packages/mui-utils/src/useUndoAndRedo.ts
@@ -0,0 +1,55 @@
+import isEqual from 'lodash/isEqual';
+import { useState } from 'react';
+
+const useUndoAndRedo = <T>(initialValue?: T) => {
+  const [values, setValues] = useState<T[]>(initialValue !== undefined ? [initialValue] : []);
+  const [index, setIndex] = useState(initialValue !== undefined ? 0 : -1);
+
+  const currentValue = values[index];
+
+  const setValue = (value: T) => {
+    if (isEqual(currentValue, value)) {
+      return;
+    }
+
+    setValues((prev) => [...prev, value]);
+    setIndex((prev) => prev + 1);
+  };
+
+  const clearHistory = () => {
+    setIndex(0);
+    setValues(initialValue !== undefined ? [initialValue] : []);
+  };
+
+  const undo = (jumps: number = 1): T | undefined => {
+    if (values.length > 0 && index > 0) {
+      const destination = index - jumps;
+      const newIndex = Math.max(destination, 0);
+      setIndex(newIndex);
+      return values[newIndex];
+    }
+    return undefined;
+  };
+
+  const redo = (jumps: number = 1): T | undefined => {
+    if (values.length > 0 && index < values.length - 1) {
+      const destination = index + jumps;
+      const newIndex = Math.min(destination, values.length - 1);
+      setIndex(newIndex);
+      return values[newIndex];
+    }
+    return undefined;
+  };
+
+  return {
+    value: currentValue,
+    storeValue: setValue,
+    clearHistory,
+    currIndex: index,
+    lastIndex: values.length - 1,
+    undo,
+    redo,
+  };
+};
+
+export default useUndoAndRedo;
