acfaedcace8554c339d7b37a3a22e9896ac38a35
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2022-12-27 11:06:24 -0600

    post merge formatting

--- docs/data/joy/components/avatar/MaxAndTotalAvatars.tsx
+++ docs/data/joy/components/avatar/MaxAndTotalAvatars.tsx
@@ -40,12 +40,10 @@ export default function GroupedAvatars() {
     ],
     total: 24,
   };
-
   const { avatars, surplus } = clampAvatars(dataFromTheServer.people, {
     max: 5,
     total: dataFromTheServer.total,
   });
-  
   return (
     <AvatarGroup>
       {avatars.map((avatar) => (
