10a4d4862a60860962e036892e92842cbab9b338
Olivier Tassinari <olivier.tassinari@gmail.com>
2023-01-01 20:38:16 +0100

    [core] Fix the product license reference name

--- scripts/copyFiles.mjs
+++ scripts/copyFiles.mjs
@@ -123,7 +123,7 @@ async function prepend(file, string) {
 }
 
 async function addLicense(packageData) {
-  const license = `/** @license MUI v${packageData.version}
+  const license = `/** @license MUI Core v${packageData.version}
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
