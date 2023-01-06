edb03ac1da66109d8f9f25d33f689ac5439c3052
Benny Joo <sldisek783@gmail.com>
2023-01-06 11:00:27 -0500

    improve codemod to turn TextField into FormControl / FormLabel / FormHelperText / Input

--- packages/mui-codemod/src/v5.0.0/joy-text-field-to-input.js
+++ packages/mui-codemod/src/v5.0.0/joy-text-field-to-input.js
@@ -15,28 +15,103 @@ export default function transformer(file, api, options) {
         node.source.value = '@mui/joy/Input';
       }
 
-      // Process only Joy UI components
-      return sourceVal.startsWith('@mui/joy');
+      return [
+        '@mui/joy', // Process only Joy UI components
+        '@mui/joy/TextField', // Filter default imports of components other than TextField
+      ].includes(sourceVal);
     })
     .forEach((path) => {
       path.node.specifiers.forEach((elementNode) => {
-        if (elementNode.imported?.name === 'TextField') {
-          elementNode.imported.name = 'Input';
-        }
-
-        let newElementName;
-        root.findJSXElements(elementNode.local.name).forEach((elementPath) => {
-          if (elementPath.node.type !== 'JSXElement') {
-            return;
+        if (
+          (elementNode.type === 'ImportSpecifier' && elementNode.imported?.name === 'TextField') ||
+          elementNode.type === 'ImportDefaultSpecifier'
+        ) {
+          if (elementNode.imported?.name === 'TextField') {
+            elementNode.imported.name = 'Input';
           }
-          newElementName = elementPath.node.openingElement.name.name.replaceAll(
-            'TextField',
-            'Input',
-          );
-          elementPath.node.openingElement.name.name = newElementName;
-        });
 
-        elementNode.local.name = newElementName;
+          let newElementName;
+          root.findJSXElements(elementNode.local.name).forEach((elementPath) => {
+            if (elementPath.node.type !== 'JSXElement') {
+              return;
+            }
+            newElementName = elementPath.node.openingElement.name.name.replaceAll(
+              'TextField',
+              'Input',
+            );
+            elementPath.node.openingElement.name.name = newElementName;
+
+            const formControlAttributeNodes = [];
+            const inputAttributeNodes = [];
+            let formLabelValue;
+            let formHelperTextValue;
+
+            elementPath.node.openingElement.attributes.forEach((attributeNode) => {
+              if (attributeNode.type !== 'JSXAttribute') {
+                return;
+              }
+              const attributeName = attributeNode.name.name;
+              switch (attributeName) {
+                case 'size':
+                  formControlAttributeNodes.push(attributeNode);
+                  break;
+
+                case 'label':
+                  formLabelValue = attributeNode.value.value;
+                  break;
+
+                case 'helperText':
+                  formHelperTextValue = attributeNode.value.value;
+                  break;
+
+                default:
+              }
+              if (!['size', 'label', 'helperText'].includes(attributeName)) {
+                inputAttributeNodes.push(attributeNode);
+              }
+            });
+
+            elementPath.node.openingElement.attributes = inputAttributeNodes;
+
+            if (formControlAttributeNodes.length > 0 || formLabelValue || formHelperTextValue) {
+              const formControlIdentifier = j.jsxIdentifier('FormControl');
+              const childrenOfFormControl = [];
+
+              if (formLabelValue) {
+                const formLabelIdentifier = j.jsxIdentifier('FormLabel');
+                const formLabelElement = j.jsxElement(
+                  j.jsxOpeningElement(formLabelIdentifier),
+                  j.jsxClosingElement(formLabelIdentifier),
+                  [j.jsxText(formLabelValue)],
+                );
+                childrenOfFormControl.push(formLabelElement, j.jsxText('\n'));
+              }
+
+              childrenOfFormControl.push(elementPath.node, j.jsxText('\n'));
+
+              if (formHelperTextValue) {
+                const formHelperTextIdentifier = j.jsxIdentifier('FormHelperText');
+                const formHelperTextElement = j.jsxElement(
+                  j.jsxOpeningElement(formHelperTextIdentifier),
+                  j.jsxClosingElement(formHelperTextIdentifier),
+                  [j.jsxText(formHelperTextValue)],
+                );
+                childrenOfFormControl.push(formHelperTextElement);
+              }
+
+              elementPath.replace(
+                j.jsxElement(
+                  j.jsxOpeningElement(formControlIdentifier, formControlAttributeNodes),
+                  j.jsxClosingElement(formControlIdentifier),
+                  [j.jsxText('\n'), ...childrenOfFormControl, j.jsxText('\n')],
+                ),
+              );
+            }
+          });
+          if (newElementName) {
+            elementNode.local.name = newElementName;
+          }
+        }
       });
     });
 
