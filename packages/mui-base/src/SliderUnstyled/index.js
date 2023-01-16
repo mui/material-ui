8e35e07ff51417794c26b51adaddf2484a6f42ae
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2023-01-16 18:19:27 +0530

    remove files

--- packages/mui-base/src/SliderUnstyled/index.js
+++
@@ -1,4 +0,0 @@
-export { default } from './SliderUnstyled';
-export { default as sliderUnstyledClasses } from './sliderUnstyledClasses';
-export { default as useSlider } from './useSlider';
-export * from './sliderUnstyledClasses';
