7f12d1e233fe4487f71c6d6a947d4a9d910e0245
Vadim Ilinchik <ivp.dev@outlook.com>
2023-01-19 22:44:57 +0300

    fix useinput ref

---
+++ packages/mui-base/src/InputUnstyled/useInput.test.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { createRenderer } from 'test/utils';
+import { expect } from 'chai';
+import useInput from './useInput';
+import { useSlotProps } from '../utils';
+
+describe('useInput', () => {
+  const { render } = createRenderer();
+  describe('params: inputRef', () => {
+    it('should be able to attach input ref passed through params', () => {
+      const inputRef = React.createRef<HTMLInputElement>();
+
+      function Input() {
+        const { getInputProps } = useInput({
+          inputRef,
+        });
+        return <input id="test" {...getInputProps()} />;
+      }
+
+      expect(() => {
+        render(<Input />);
+      }).not.toErrorDev();
+
+      expect(inputRef.current!.id).to.equal('test');
+    });
+
+    it('refs provided by forwarded prop and internal input ref', () => {
+      const outerInputRef = React.createRef<HTMLInputElement>();
+      const innerInputRef = React.createRef<HTMLInputElement>();
+
+      const Wrapper = ({ children }: React.PropsWithChildren) => {
+        return React.cloneElement(children as React.ReactElement, { ref: outerInputRef });
+      };
+
+      const Input = React.forwardRef(function Input(
+        props: React.InputHTMLAttributes<HTMLInputElement>,
+        forwardedRef: React.ForwardedRef<HTMLInputElement>,
+      ) {
+        const { getInputProps } = useInput({
+          inputRef: innerInputRef,
+        });
+
+        const inputProps = useSlotProps({
+          elementType: 'input',
+          getSlotProps: getInputProps,
+          externalSlotProps: props,
+          additionalProps: {
+            ref: forwardedRef,
+          },
+          ownerState: {},
+        });
+        return <input {...inputProps} />;
+      });
+
+      expect(() => {
+        render(
+          <Wrapper>
+            <Input id="test" />
+          </Wrapper>,
+        );
+      }).not.toErrorDev();
+
+      expect(outerInputRef.current!.id).to.equal('test');
+      expect(innerInputRef.current!.id).to.equal('test');
+    });
+  });
+});
