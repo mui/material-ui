7c85d0a231d3126b4c7724180037205f9eece585
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-01-19 11:43:54 +0530

    used partial utility instead of rewriting

--- packages/mui-utils/src/usePreviousProps.ts
+++ packages/mui-utils/src/usePreviousProps.ts
@@ -1,15 +1,11 @@
 import * as React from 'react';
 
-type PartialProps<T> = {
-  [Property in keyof T]?: T[Property];
-};
-
 const usePreviousProps = <T>(value: T) => {
   const ref = React.useRef<T | {}>({});
   React.useEffect(() => {
     ref.current = value;
   });
-  return ref.current as PartialProps<T>;
+  return ref.current as Partial<T>;
 };
 
 export default usePreviousProps;
