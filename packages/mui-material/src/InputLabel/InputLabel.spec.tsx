76c6e439a3793139123f4613abd66fe1df38dd7e
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-01-19 18:42:03 +0530

    added more tests

--- packages/mui-material/src/InputLabel/InputLabel.spec.tsx
+++ packages/mui-material/src/InputLabel/InputLabel.spec.tsx
@@ -1,4 +1,23 @@
 import * as React from 'react';
 import { InputLabel } from '@mui/material';
 
-<InputLabel component="legend" />;
+const InputLabelTest = () => {
+  const CustomComponent: React.FC<{ prop1: string; prop2: number }> = function CustomComponent() {
+    return <div />;
+  };
+  return (
+    <div>
+      <InputLabel component='legend' />
+ 
+      {/* @ts-expect-error */}
+      <InputLabel component="a" incorrectAttribute="url" />
+      {/* @ts-expect-error */}
+      <InputLabel component="div" href="url" />
+      <InputLabel component={CustomComponent} prop1="1" prop2={12} />
+      {/* @ts-expect-error */}
+      <InputLabel component={CustomComponent} prop1="1" />
+      {/* @ts-expect-error */}
+      <InputLabel component={CustomComponent} prop1="1" prop2="12" />
+    </div>
+  );
+};
