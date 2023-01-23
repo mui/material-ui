72586ed5c051626a9b2be1ea373e2631e6255312
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-01-23 22:14:58 +0530

    migrated code to use Extend

--- packages/mui-material/src/InputLabel/InputLabel.d.ts
+++ packages/mui-material/src/InputLabel/InputLabel.d.ts
@@ -2,77 +2,72 @@ import { OverridableComponent, OverrideProps } from '@mui/material/OverridableCo
 import * as React from 'react';
 import { SxProps } from '@mui/system';
 import { OverridableStringUnion } from '@mui/types';
-import { FormLabelProps, FormLabelBaseProps } from '../FormLabel';
+import { FormLabelProps, ExtendFormLabelTypeMap } from '../FormLabel';
 import { Theme } from '../styles';
 import { InputLabelClasses } from './inputLabelClasses';
-import { InternalStandardProps as StandardProps } from '..';
 
 export interface InputLabelPropsSizeOverrides {}
 
-interface BaseProps extends Omit<StandardProps<FormLabelProps>, keyof FormLabelBaseProps> {
-  /**
-   * The content of the component.
-   */
-  children?: React.ReactNode;
-  /**
-   * Override or extend the styles applied to the component.
-   */
-  classes?: Partial<InputLabelClasses>;
-  /**
-   * The color of the component.
-   * It supports both default and custom theme colors, which can be added as shown in the
-   * [palette customization guide](https://mui.com/material-ui/customization/palette/#adding-new-colors).
-   */
-  color?: FormLabelProps['color'];
-  /**
-   * If `true`, the transition animation is disabled.
-   * @default false
-   */
-  disableAnimation?: boolean;
-  /**
-   * If `true`, the component is disabled.
-   */
-  disabled?: boolean;
-  /**
-   * If `true`, the label is displayed in an error state.
-   */
-  error?: boolean;
-  /**
-   * If `true`, the `input` of this label is focused.
-   */
-  focused?: boolean;
-  /**
-   * If `dense`, will adjust vertical spacing. This is normally obtained via context from
-   * FormControl.
-   */
-  margin?: 'dense';
-  /**
-   * if `true`, the label will indicate that the `input` is required.
-   */
-  required?: boolean;
-  /**
-   * If `true`, the label is shrunk.
-   */
-  shrink?: boolean;
-  /**
-   * The size of the component.
-   * @default 'normal'
-   */
-  size?: OverridableStringUnion<'small' | 'normal', InputLabelPropsSizeOverrides>;
-  /**
-   * The system prop that allows defining system overrides as well as additional CSS styles.
-   */
-  sx?: SxProps<Theme>;
-  /**
-   * The variant to use.
-   */
-  variant?: 'standard' | 'outlined' | 'filled';
-}
-
-export interface InputLabelTypeMap<P = {}, D extends React.ElementType = 'label'> {
-  props: P & BaseProps;
+export type InputLabelTypeMap<
+  P = {},
+  D extends React.ElementType = 'label',
+> = ExtendFormLabelTypeMap<{
+  props: P & {
+    /**
+     * The content of the component.
+     */
+    children?: React.ReactNode;
+    /**
+     * Override or extend the styles applied to the component.
+     */
+    classes?: Partial<InputLabelClasses>;
+    color?: FormLabelProps['color'];
+    /**
+     * If `true`, the transition animation is disabled.
+     * @default false
+     */
+    disableAnimation?: boolean;
+    /**
+     * If `true`, the component is disabled.
+     */
+    disabled?: boolean;
+    /**
+     * If `true`, the label is displayed in an error state.
+     */
+    error?: boolean;
+    /**
+     * If `true`, the `input` of this label is focused.
+     */
+    focused?: boolean;
+    /**
+     * If `dense`, will adjust vertical spacing. This is normally obtained via context from
+     * FormControl.
+     */
+    margin?: 'dense';
+    /**
+     * if `true`, the label will indicate that the `input` is required.
+     */
+    required?: boolean;
+    /**
+     * If `true`, the label is shrunk.
+     */
+    shrink?: boolean;
+    /**
+     * The size of the component.
+     * @default 'normal'
+     */
+    size?: OverridableStringUnion<'small' | 'normal', InputLabelPropsSizeOverrides>;
+    /**
+     * The system prop that allows defining system overrides as well as additional CSS styles.
+     */
+    sx?: SxProps<Theme>;
+    /**
+     * The variant to use.
+     */
+    variant?: 'standard' | 'outlined' | 'filled';
+  };
   defaultComponent: D;
-}
+}>;
 
 /**
  *
