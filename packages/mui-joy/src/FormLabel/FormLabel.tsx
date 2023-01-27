e624bc61e16567ecc796052e95c8f3cfe12dbb48
siriwatknp <siriwatkunaporn@gmail.com>
2023-01-27 10:03:58 +0700

    revert FormLabel

--- packages/mui-joy/src/FormLabel/FormLabel.tsx
+++ packages/mui-joy/src/FormLabel/FormLabel.tsx
@@ -30,7 +30,7 @@ const FormLabelRoot = styled('label', {
   userSelect: 'none',
   fontFamily: theme.vars.fontFamily.body,
   fontSize: `var(--FormLabel-fontSize, ${theme.vars.fontSize.sm})`,
-  fontWeight: theme.vars.fontWeight.lg,
+  fontWeight: theme.vars.fontWeight.md,
   lineHeight: theme.vars.lineHeight.md,
   color: `var(--FormLabel-color, ${theme.vars.palette.text.primary})`,
   margin: 'var(--FormLabel-margin, 0px)',
