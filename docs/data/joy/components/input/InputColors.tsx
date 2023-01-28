d6b4bb830318a87bee09d2e8f5f2cdd40401621b
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2023-01-27 19:01:45 -0800

    some edits to joy ui input doc

--- docs/data/joy/components/input/InputColors.tsx
+++ docs/data/joy/components/input/InputColors.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Box from '@mui/joy/Box';
 import Input from '@mui/joy/Input';
 
+
 export default function InputColors() {
   return (
     <Box
@@ -16,6 +17,8 @@ export default function InputColors() {
       <Input placeholder="Type in here…" variant="outlined" color="primary" />
       <Input placeholder="Type in here…" variant="outlined" color="neutral" />
       <Input placeholder="Type in here…" variant="outlined" color="danger" />
+      <Input placeholder="Type in here…" variant="outlined" color="info" />
+      <Input placeholder="Type in here…" variant="outlined" color="success" />
       <Input placeholder="Type in here…" variant="outlined" color="warning" />
     </Box>
   );
