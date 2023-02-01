975b8168e6b5742a96561a30fa98656ddd7f2fe5
siriwatknp <siriwatkunaporn@gmail.com>
2023-02-01 14:50:37 +0700

    revert to original

--- dangerfile.js
+++ dangerfile.js
@@ -1,6 +1,6 @@
 // inspire by reacts dangerfile
 // danger has to be the first thing required!
-const { danger, markdown, message } = require('danger');
+const { danger, markdown } = require('danger');
 const { exec } = require('child_process');
 const { loadComparison } = require('./scripts/sizeSnapshot');
 
@@ -166,9 +166,43 @@ async function reportBundleSize() {
   }
 }
 
+function addDeployPreviewUrls() {
+  /**
+   * The incoming path from danger does not start with `/`
+   * e.g. ['docs/data/joy/components/button/button.md']
+   */
+  function formatFileToLink(path) {
+    return path
+      .replace('docs/data/', '')
+      .replace('material/', 'material-ui/')
+      .replace('joy/', 'joy-ui/')
+      .replace('components/', 'react-')
+      .replace(/\/[^/]+\.md$/, '/');
+  }
+
+  const netlifyPreview = `https://deploy-preview-${danger.github.pr.number}--material-ui.netlify.app/`;
+
+  const files = [...danger.git.created_files, ...danger.git.modified_files];
+
+  // limit to the first 5 docs
+  const docs = files
+    .filter((file) => file.startsWith('docs/data') && file.endsWith('.md'))
+    .slice(0, 5);
+
+  markdown(`
+## Netlify deploy preview
+
+${docs
+  .map((path) => {
+    const formattedUrl = formatFileToLink(path);
+    return `- [${formattedUrl}](${netlifyPreview}${formattedUrl})`;
+  })
+  .join('\n')}
+`);
+}
+
 async function run() {
-  const netlifyPreview = `https://deploy-preview-${process.env.CIRCLE_PR_NUMBER}--material-ui.netlify.app/`;
-  message(`Netlify deploy preview: <a href="${netlifyPreview}">${netlifyPreview}</a>`);
+  addDeployPreviewUrls();
 
   switch (dangerCommand) {
     case 'prepareBundleSizeReport':
