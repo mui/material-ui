b365431f382ed2487d7867151740e53a035981de
Michał Dudak <michal@dudak.me>
2023-02-01 22:45:27 +0100

    Revert #27299 (but leave its tests)

--- packages/mui-material/src/Select/SelectInput.js
+++ packages/mui-material/src/Select/SelectInput.js
@@ -350,7 +350,7 @@ const SelectInput = React.forwardRef(function SelectInput(props, ref) {
     }
   }
 
-  const items = childrenArray.map((child, index, arr) => {
+  const items = childrenArray.map((child) => {
     if (!React.isValidElement(child)) {
       return null;
     }
@@ -391,26 +391,6 @@ const SelectInput = React.forwardRef(function SelectInput(props, ref) {
       foundMatch = true;
     }
 
-    if (child.props.value === undefined) {
-      return React.cloneElement(child, {
-        'aria-readonly': true,
-        role: 'option',
-      });
-    }
-
-    const isFirstSelectableElement = () => {
-      if (value) {
-        return selected;
-      }
-      const firstSelectableElement = arr.find(
-        (item) => item?.props?.value !== undefined && item.props.disabled !== true,
-      );
-      if (child === firstSelectableElement) {
-        return true;
-      }
-      return selected;
-    };
-
     return React.cloneElement(child, {
       'aria-selected': selected ? 'true' : 'false',
       onClick: handleItemClick(child),
@@ -427,10 +407,7 @@ const SelectInput = React.forwardRef(function SelectInput(props, ref) {
         }
       },
       role: 'option',
-      selected:
-        arr[0]?.props?.value === undefined || arr[0]?.props?.disabled === true
-          ? isFirstSelectableElement()
-          : selected,
+      selected,
       value: undefined, // The value is most likely not a valid HTML attribute.
       'data-value': child.props.value, // Instead, we provide it as a data attribute.
     });
