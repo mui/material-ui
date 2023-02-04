f76fa09f087e6229715d9a023b7fa24532d05e6c
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-02-04 17:09:17 +0900

    initial commit

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,33 +1,11 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
+import React, { Fragment } from "react";
 
-function Copyright() {
+const App = () => {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
+    <Fragment>
+      <div>Hi !</div>
+    </Fragment>
   );
-}
+};
 
-export default function App() {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App example
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+export default App;
