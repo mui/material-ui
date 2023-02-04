32c93cbbe9df158d4b65d49d9dda685cb7fa42ea
sai chand <fayazceg@gmail.com>
2023-02-04 07:31:20 +0000

    [useTabsList] Add explicit return type

--- packages/mui-base/src/TabsListUnstyled/useTabsList.types.ts
+++ packages/mui-base/src/TabsListUnstyled/useTabsList.types.ts
@@ -18,3 +18,32 @@ export type UseTabsListRootSlotProps<TOther = {}> = TOther & {
   ref: React.Ref<any>;
   onKeyDown?: React.KeyboardEventHandler<HTMLElement>;
 };
+
+export interface UseTabsListReturnValue {
+  /**
+   * The direction of the text.
+   * @default 'false'
+   */
+  isRtl: boolean;
+  /**
+   * The component orientation (layout flow direction).
+   * @default 'horizontal'
+   */
+  orientation: 'horizontal' | 'vertical';
+  /**
+   * The value of the currently selected `Tab`.
+   */
+  value: string | number | false;
+  processChildren: () =>
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>[]
+    | null
+    | undefined;
+  /**
+   * Resolver for the root slot's props.
+   * @param externalProps props for the root slot
+   * @returns props that should be spread on the root slot
+   */
+  getRootProps: <TOther extends Record<string, any> = {}>(
+    externalProps?: TOther,
+  ) => UseTabsListRootSlotProps<TOther>;
+}
