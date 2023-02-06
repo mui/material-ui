a4d817e732295c46bffc43c3d34ab09fa90cd52f
Jose Rodolfo Freitas <joserodolfo.freitas@gmail.com>
2023-02-06 13:38:20 +0100

    Prevent broken link on main docs

--- docs/src/route.ts
+++ docs/src/route.ts
@@ -31,7 +31,7 @@ const ROUTES = {
   goldSponsor: '/material-ui/discover-more/backers/#gold',
   store: 'https://mui.com/store/',
   advancedComponents: '/x/introduction/',
-  advancedComponentsWhatsNew: '/x/whats-new',
+  advancedComponentsWhatsNew: 'https://next.mui.com/x/whats-new/',
   toolpadDocs: '/toolpad/getting-started/overview/',
   dataGridSpace: '/x/react-data-grid/getting-started/',
   dataGridDocs: '/x/react-data-grid/getting-started/',
