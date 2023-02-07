e2d52cdb2f5e4e246180143d8090132920c9cc93
Benny Joo <sldisek783@gmail.com>
2023-02-07 12:12:16 +0000

    Revert exporting ColorInversion

--- packages/mui-joy/src/styles/index.ts
+++ packages/mui-joy/src/styles/index.ts
@@ -71,11 +71,7 @@ export { default as styled } from './styled';
 export { default as ThemeProvider } from './ThemeProvider';
 export * from './ThemeProvider';
 export { default as useThemeProps } from './useThemeProps';
-export {
-  default as ColorInversion,
-  ColorInversionProvider,
-  useColorInversion,
-} from './ColorInversion';
+export { ColorInversionProvider, useColorInversion } from './ColorInversion';
 export { default as extendTheme, createGetCssVar } from './extendTheme';
 export type { CssVarsThemeOptions } from './extendTheme';
 export { default as StyledEngineProvider } from './StyledEngineProvider';
