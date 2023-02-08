c791f7f39c8ae4797c6e702cc6f32ee39f8c962f
Lukas <llukas.tyla@gmail.com>
2023-02-08 13:50:41 +0000

    Add example usage of documented `@template`

--- packages/mui-joy/src/Autocomplete/Autocomplete.tsx
+++ packages/mui-joy/src/Autocomplete/Autocomplete.tsx
@@ -922,6 +922,7 @@ Autocomplete.propTypes /* remove-proptypes */ = {
    * Used to determine the disabled state for a given option.
    *
    * @param {Value} option The option to test.
+   * @template Value The option shape. Will be the same shape as an item of the options.
    * @returns {boolean}
    */
   getOptionDisabled: PropTypes.func,
