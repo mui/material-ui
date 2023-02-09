5e0141c6d9410763473cecc3f038e78ac810464d
Benny Joo <sldisek783@gmail.com>
2023-02-09 11:42:25 +0000

    address code review comments

--- packages/mui-joy/src/CssBaseline/CssBaseline.tsx
+++ packages/mui-joy/src/CssBaseline/CssBaseline.tsx
@@ -88,6 +88,7 @@ CssBaseline.propTypes /* remove-proptypes */ = {
   children: PropTypes.node,
   /**
    * Disable `color-scheme` CSS property.
+   *
    * For more details, check out https://developer.mozilla.org/en-US/docs/Web/CSS/color-scheme
    * For browser support, check out https://caniuse.com/?search=color-scheme
    * @default false
--- packages/mui-joy/src/CssBaseline/CssBaselineProps.ts
+++ packages/mui-joy/src/CssBaseline/CssBaselineProps.ts
@@ -5,6 +5,7 @@ export interface CssBaselineProps {
   children?: React.ReactNode;
   /**
    * Disable `color-scheme` CSS property.
+   *
    * For more details, check out https://developer.mozilla.org/en-US/docs/Web/CSS/color-scheme
    * For browser support, check out https://caniuse.com/?search=color-scheme
    * @default false
