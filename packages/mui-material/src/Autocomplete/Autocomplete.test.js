a835e6a1be6e4f512cc61440c9798a8c2c089ee1
Dawid Rusnak <dawid@drcode.pl>
2023-02-13 11:07:08 +0100

    add unit test for getOptionLabel contract in Autocomplete component

--- packages/mui-material/src/Autocomplete/Autocomplete.test.js
+++ packages/mui-material/src/Autocomplete/Autocomplete.test.js
@@ -2418,6 +2418,26 @@ describe('<Autocomplete />', () => {
       });
       expect(textbox).to.have.property('value', 'a');
     });
+
+    it('should not call getOptionLabel with unknown values when computing highlight', () => {
+      const { getByRole } = render(
+        <Autocomplete
+          openOnFocus
+          autoHighlight
+          options={[
+            { property: { name: 'one' } },
+            { property: { name: 'two' } },
+            { property: { name: 'three' } },
+          ]}
+          getOptionLabel={(option) => option.property.name}
+          renderInput={(params) => <TextField {...params} />}
+        />,
+      );
+
+      expect(() => {
+        fireEvent.focus(getByRole('combobox'));
+      }).not.to.throw();
+    });
   });
 
   describe('prop: fullWidth', () => {
