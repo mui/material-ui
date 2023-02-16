33fa30aeafe536f536dd1be3300a4b7a81dce019
sai chand <fayazceg@gmail.com>
2023-02-16 17:24:25 +0000

    [Avatar] Fix

--- packages/mui-material/src/Avatar/Avatar.test.js
+++ packages/mui-material/src/Avatar/Avatar.test.js
@@ -3,6 +3,7 @@ import { expect } from 'chai';
 import { createRenderer, fireEvent, describeConformance } from 'test/utils';
 import { spy } from 'sinon';
 import Avatar, { avatarClasses as classes } from '@mui/material/Avatar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CancelIcon from '../internal/svg-icons/Cancel';
 
 describe('<Avatar />', () => {
@@ -211,5 +212,29 @@ describe('<Avatar />', () => {
 
       expect(avatar).to.have.class(classes.colorDefault);
     });
+  });
+
+  it('should not error when ownerState is used in styleOverrides', () => {
+    const theme = createTheme({
+      components: {
+        MuiAvatar: {
+          styleOverrides: {
+            root: ({ ownerState }) => ({
+              ...(ownerState.variant === 'rounded' && {
+                color: 'red',
+              }),
+            }),
+          },
+        },
+      },
+    });
+
+    expect(() =>
+      render(
+        <ThemeProvider theme={theme}>
+          <Avatar variant="rounded" />
+        </ThemeProvider>,
+      ),
+    ).not.toErrorDev();
   });
 });
