7cc2421d5a05eb15cd00d70f5a66e76ff7fccaa3
Olivier Tassinari <olivier.tassinari@gmail.com>
2023-02-18 20:06:49 +0100

    consistency in linebreak

--- packages/mui-base/src/AutocompleteUnstyled/useAutocomplete.d.ts
+++ packages/mui-base/src/AutocompleteUnstyled/useAutocomplete.d.ts
@@ -357,99 +357,82 @@ export interface UseAutoCompleteReturnValue<
    * @returns props that should be spread on the root slot
    */
   getRootProps: (externalProps?: any) => React.HTMLAttributes<HTMLDivElement>;
-
   /**
    * Resolver for the input element's props.
    * @returns props that should be spread on the input element
    */
   getInputProps: () => React.InputHTMLAttributes<HTMLInputElement>;
-
   /**
    * Resolver for the input label element's props.
    * @returns props that should be spread on the input label element
    */
   getInputLabelProps: () => Omit<React.HTMLAttributes<HTMLLabelElement>, 'color'>;
-
   /**
    * Resolver for the `clear` button element's props.
    * @returns props that should be spread on the *clear* button element
    */
   getClearProps: () => React.HTMLAttributes<HTMLButtonElement>;
-
   /**
    * Resolver for the popup icon's props.
    * @returns props that should be spread on the popup icon
    */
   getPopupIndicatorProps: () => React.HTMLAttributes<HTMLButtonElement>;
-
   /**
    * A tag props getter.
    */
   getTagProps: AutocompleteGetTagProps;
-
   /**
    * Resolver for the listbox component's props.
    * @returns props that should be spread on the listbox component
    */
   getListboxProps: () => React.HTMLAttributes<HTMLUListElement>;
-
   /**
    * Resolver for the rendered option element's props.
    * @param renderedOption option rendered on the Autocomplete
    * @returns props that should be spread on the li element
    */
   getOptionProps: (renderedOption: RenderedOption<T>) => React.HTMLAttributes<HTMLLIElement>;
-
   /**
    * Id for the Autocomplete.
    */
   id: string;
-
   /**
    * The input value.
    */
   inputValue: string;
-
   /**
    * The value of the autocomplete.
    */
   value: AutocompleteValue<T, Multiple, DisableClearable, FreeSolo>;
-
   /**
    * If `true`, the component has valid values selected.
    * @default false
    */
   dirty: boolean;
-
   /**
    * If `true`, the popup is open on the component.
    * @default false
    */
   popupOpen: boolean;
-
   /**
    * If `true`, the component is focused.
    * @default false
    */
   focused: boolean;
-
   /**
    * An HTML element that is used to set the position of the component.
    */
   anchorEl: null | HTMLElement;
-
   /**
    * Setter for the component `anchorEl`.
    * @returns function for setting `anchorEl`
    */
   setAnchorEl: () => void;
-
   /**
    * Index of the focused tag for the component.
    * @default -1
    */
   focusedTag: number;
-
   /**
    * The options to render. It's either `T[]` or `AutocompleteGroupedOption<T>[]` if the groupBy prop is provided.
    */
