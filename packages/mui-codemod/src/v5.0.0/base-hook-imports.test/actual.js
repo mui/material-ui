5bf5e65fbd62a3980284b53a16d21f25b08e108e
Benny Joo <sldisek783@gmail.com>
2023-02-22 15:04:46 +0000

    organise imports

--- packages/mui-codemod/src/v5.0.0/base-hook-imports.test/actual.js
+++ packages/mui-codemod/src/v5.0.0/base-hook-imports.test/actual.js
@@ -2,12 +2,6 @@ import { useAutocomplete } from '@mui/base/AutocompleteUnstyled';
 import { useBadge } from '@mui/base/BadgeUnstyled';
 import { useButton } from '@mui/base/ButtonUnstyled';
 import { useInput } from '@mui/base/InputUnstyled';
-import {
-  useSelect,
-  SelectOption,
-  SelectChild,
-  isOptionGroup,
-  SelectOptionGroup,
-} from '@mui/base/SelectUnstyled';
+import { useSelect, SelectOption, SelectChild, isOptionGroup, SelectOptionGroup } from '@mui/base/SelectUnstyled';
 import { UseOptionReturnValue } from '@mui/base/OptionUnstyled';
 import useListbox from '@mui/base/ListboxUnstyled/useListbox';
