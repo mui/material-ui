375788d826ff161b90df7f8efc79ee71e780395c
Benny Joo <sldisek783@gmail.com>
2023-02-22 11:29:30 +0000

    improve codemod script

--- packages/mui-codemod/src/v5.0.0/rename-css-variables.js
+++ packages/mui-codemod/src/v5.0.0/rename-css-variables.js
@@ -2,6 +2,71 @@ const capitalize = (str) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
+const JoyComponents = [
+  'Alert',
+  'AspectRatio',
+  'Autocomplete',
+  'AutocompleteListbox',
+  'AutocompleteOption',
+  'Avatar',
+  'AvatarGroup',
+  'Badge',
+  'Box',
+  'Breadcrumbs',
+  'Button',
+  'Card',
+  'CardContent',
+  'CardCover',
+  'CardOverflow',
+  'Checkbox',
+  'Chip',
+  'ChipDelete',
+  'CircularProgress',
+  'Container',
+  'CssBaseline',
+  'Divider',
+  'FormControl',
+  'FormHelperText',
+  'FormLabel',
+  'Grid',
+  'IconButton',
+  'Input',
+  'LinearProgress',
+  'Link',
+  'List',
+  'ListDivider',
+  'ListItem',
+  'ListItemButton',
+  'ListItemContent',
+  'ListItemDecorator',
+  'ListSubheader',
+  'Menu',
+  'MenuItem',
+  'MenuList',
+  'Modal',
+  'ModalClose',
+  'ModalDialog',
+  'Option',
+  'Radio',
+  'RadioGroup',
+  'ScopedCssBaseline',
+  'Select',
+  'Sheet',
+  'Slider',
+  'Stack',
+  'SvgIcon',
+  'Switch',
+  'Tab',
+  'Table',
+  'TabList',
+  'TabPanel',
+  'Tabs',
+  'Textarea',
+  'TextField',
+  'Tooltip',
+  'Typography',
+];
+
 /**
  * @param {import('jscodeshift').FileInfo} file
  */
@@ -9,13 +74,19 @@ export default function transformer(file) {
   return (
     file.source
       // from `--<Component>-<slot>-<property>` to `--<Component>-<slot><Property>`
-      .replace(/--([a-zA-Z]+)-([a-zA-Z]+)-([a-zA-Z]+)/gm, (_, capture1, capture2, capture3) => {
-        // turn `--List-item-...` and `--List-divider-...` to `--ListItem-...` and `--ListDivider-...` 
-        if (capture1 === 'List' && ['divider', 'item'].includes(capture2)) {
-          return `--${capture1}${capitalize(capture2)}-${capture3}`;
-        }
-        return `--${capture1}-${capture2}${capitalize(capture3)}`;
-      })
+      .replace(
+        /--([a-zA-Z]+)-([a-zA-Z]+)-([a-zA-Z]+)/gm,
+        (matched, capture1, capture2, capture3) => {
+          if (!JoyComponents.includes(capture1)) {
+            return matched;
+          }
+          // turn `--List-item-...` and `--List-divider-...` to `--ListItem-...` and `--ListDivider-...`
+          if (capture1 === 'List' && ['divider', 'item'].includes(capture2)) {
+            return `--${capture1}${capitalize(capture2)}-${capture3}`;
+          }
+          return `--${capture1}-${capture2}${capitalize(capture3)}`;
+        },
+      )
       // from `--internal-...` to `--unstable_...`
       .replace(/--internal-/gm, '--unstable_')
   );
