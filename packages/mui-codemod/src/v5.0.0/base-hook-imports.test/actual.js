601ccc4ba7ba0aef6e3c19661556d9f58d13a844
Benny Joo <sldisek783@gmail.com>
2023-02-22 15:01:58 +0000

    Add codemod tests

---
+++ packages/mui-codemod/src/v5.0.0/base-hook-imports.test/actual.js
@@ -0,0 +1,13 @@
+import { useAutocomplete } from '@mui/base/AutocompleteUnstyled';
+import { useBadge } from '@mui/base/BadgeUnstyled';
+import { useButton } from '@mui/base/ButtonUnstyled';
+import { useInput } from '@mui/base/InputUnstyled';
+import {
+  useSelect,
+  SelectOption,
+  SelectChild,
+  isOptionGroup,
+  SelectOptionGroup,
+} from '@mui/base/SelectUnstyled';
+import { UseOptionReturnValue } from '@mui/base/OptionUnstyled';
+import useListbox from '@mui/base/ListboxUnstyled/useListbox';
---
+++ packages/mui-codemod/src/v5.0.0/base-hook-imports.test/expected.js
@@ -0,0 +1,7 @@
+import useAutocomplete from '@mui/base/useAutocomplete';
+import useBadge from '@mui/base/useBadge';
+import useButton from '@mui/base/useButton';
+import useInput from '@mui/base/useInput';
+import useSelect, { SelectOption, SelectChild, isOptionGroup, SelectOptionGroup } from '@mui/base/useSelect';
+import { UseOptionReturnValue } from '@mui/base/useOption';
+import useListbox from '@mui/base/useListbox';
