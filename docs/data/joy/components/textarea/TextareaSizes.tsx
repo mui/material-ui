4cf618d88d1169e37e2419d3ae800841493d56e2
varunmulay22 <varunmulay22@gmail.com>
2023-02-28 20:46:29 +0530

    reverted to label upon discussion

--- docs/data/joy/components/textarea/TextareaSizes.tsx
+++ docs/data/joy/components/textarea/TextareaSizes.tsx
@@ -5,9 +5,9 @@ import Textarea from '@mui/joy/Textarea';
 export default function TextareaSizes() {
   return (
     <Box sx={{ display: 'flex', gap: 2, alignItems: 'center', flexWrap: 'wrap' }}>
-      <Textarea size="sm" label="Size" placeholder="Small" />
-      <Textarea size="md" label="Size" placeholder="Medium" />
-      <Textarea size="lg" label="Size" placeholder="Large" />
+      <Textarea size="sm" name="Size" placeholder="Small" />
+      <Textarea size="md" name="Size" placeholder="Medium" />
+      <Textarea size="lg" name="Size" placeholder="Large" />
     </Box>
   );
 }
