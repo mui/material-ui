6afd201f0a9f838d1110e2c6b3bb0ad78e4e14db
varunmulay22 <varunmulay22@gmail.com>
2023-03-01 17:20:38 +0530

    part modals converted

---
+++ docs/data/joy/components/modal/LayoutModalDialog.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Button from '@mui/joy/Button';
+import Stack from '@mui/joy/Stack';
+import Modal from '@mui/joy/Modal';
+import ModalClose from '@mui/joy/ModalClose';
+import ModalDialog, { ModalDialogProps } from '@mui/joy/ModalDialog';
+import Typography from '@mui/joy/Typography';
+
+export default function LayoutModalDialog() {
+  const [open, setOpen] = React.useState<ModalDialogProps['layout'] | undefined>(undefined);
+  return (
+    <React.Fragment>
+      <Stack direction="row" spacing={1}>
+        <Button variant="outlined" color="neutral" onClick={() => setOpen('center')}>
+          Center
+        </Button>
+        <Button
+          variant="outlined"
+          color="neutral"
+          onClick={() => setOpen('fullscreen')}
+        >
+          Full screen
+        </Button>
+      </Stack>
+      <Modal open={!!open} onClose={() => setOpen(undefined)}>
+        <ModalDialog
+          aria-labelledby="layout-modal-title"
+          aria-describedby="layout-modal-description"
+          layout={open || undefined}
+        >
+          <ModalClose />
+          <Typography id="layout-modal-title" component="h2">
+            Modal Dialog
+          </Typography>
+          <Typography id="layout-modal-description" textColor="text.tertiary">
+            This is a <code>{open}</code> modal dialog. Press <code>esc</code> to
+            close it.
+          </Typography>
+        </ModalDialog>
+      </Modal>
+    </React.Fragment>
+  );
+}
---
+++ docs/data/joy/components/modal/NestedModals.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import Button from '@mui/joy/Button';
+import Modal from '@mui/joy/Modal';
+import ModalDialog from '@mui/joy/ModalDialog';
+import Typography from '@mui/joy/Typography';
+
+type NestedModalsProps = {
+  random?: boolean;
+}
+
+function randomBetween(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export default function NestedModals({ random }: NestedModalsProps): JSX.Element {
+  const [open, setOpen] = React.useState<boolean>(false);
+  return (
+    <React.Fragment>
+      <Button variant="outlined" color="neutral" onClick={() => setOpen(true)}>
+        Open modal
+      </Button>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <ModalDialog
+          aria-labelledby="nested-modal-title"
+          aria-describedby="nested-modal-description"
+          layout="center"
+          {...(random && {
+            sx: {
+              top: `${randomBetween(25, 75)}%`,
+              left: `${randomBetween(25, 75)}%`,
+            },
+          })}
+        >
+          <Typography id="nested-modal-title" component="h2">
+            Infinite modals
+          </Typography>
+          <Typography id="nested-modal-description" textColor="text.tertiary">
+            Welcome to the infinite nested modals.
+          </Typography>
+          <NestedModals random />
+        </ModalDialog>
+      </Modal>
+    </React.Fragment>
+  );
+}
