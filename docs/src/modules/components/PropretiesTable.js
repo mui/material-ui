9b124cfe4f1f8efcdf6b246614a2f75e48d00fd1
Marija Najdova <mnajdova@gmail.com>
2023-03-03 20:04:50 +0100

    merge conflict correctly resolved

--- docs/src/modules/components/PropretiesTable.js
+++ docs/src/modules/components/PropretiesTable.js
@@ -53,14 +53,8 @@ export default function PropertiesTable(props) {
         </thead>
         <tbody>
           {Object.entries(properties).map(([propName, propData]) => {
-            let typeName = propData.type.name;
-            typeName = typeName
-              .replace(/&/g, '&amp;')
-              .replace(/</g, '&lt;')
-              .replace(/>/g, '&gt')
-              .replace(/"/g, '&quot;')
-              .replace(/'/g, '&#39;');
-            const propDefault = propData.default || (propData.type.name === 'bool' && 'false');
+            const typeName = propData.type.description || propData.type.name;
+            const propDefault = propData.default;
             return (
               propData.description !== '@ignore' && (
                 <tr key={propName}>
