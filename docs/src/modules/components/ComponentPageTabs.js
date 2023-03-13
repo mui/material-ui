dd38235c2c19d0eee2f4c84cefb360294bc080ba
Marija Najdova <mnajdova@gmail.com>
2023-03-13 12:26:34 +0100

    Update docs/src/modules/components/ComponentPageTabs.js
    Co-authored-by: Olivier Tassinari <olivier.tassinari@gmail.com>
    Signed-off-by: Marija Najdova <mnajdova@gmail.com>

--- docs/src/modules/components/ComponentPageTabs.js
+++ docs/src/modules/components/ComponentPageTabs.js
@@ -64,7 +64,7 @@ export default function ComponentPageTabs(props) {
           zIndex: 1000,
         }}
       >
-        <Tab as={Link} shallow href={`${pathname}demos`} label="Demos" value="demos" />
+        <Tab component={Link} shallow href={`${pathname}demos`} label="Demos" value="demos" />
         <Tab
           as={Link}
           shallow
