d4b77c1eb5d99926861b77acaad55bcdb57dc9fc
siriwatknp <siriwatkunaporn@gmail.com>
2023-03-13 11:03:56 +0700

    refactor createVariantStyle and tests

--- packages/mui-joy/src/styles/variantUtils.ts
+++ packages/mui-joy/src/styles/variantUtils.ts
@@ -6,7 +6,7 @@ import {
   DefaultColorPalette,
   PaletteVariant,
 } from './types/colorSystem';
-import { VariantKey, Variants } from './types/variants';
+import { Variants } from './types/variants';
 
 export const isVariantPalette = (colorPalette: string | number | Record<string, any>) =>
   colorPalette &&
@@ -27,6 +27,67 @@ const assignCss = (target: Record<string, string>, variantVar: string, value: st
   if (variantVar.includes('Border')) {
     target.borderColor = value;
   }
+};
+
+/**
+ *
+ * @param name variant name
+ * @example 'plain'
+ *
+ * @param palette object that contains palette tokens
+ * @example { primary: { plainColor: '', plainHoverColor: '', ...tokens }, ...other palete }
+ *
+ * @param getCssVar a function that receive variant token and return a CSS variable
+ *
+ * result will be the stylesheet based on the palette tokens
+ * @example {
+ *   color: '--token',
+ *   backgroundColor: '--token',
+ *   '--variant-borderWidth': '0px',
+ * }
+ * @example {
+ *   cursor: 'pointer',
+ *   color: '--token',
+ *   backgroundColor: '--token',
+ *   '--variant-borderWidth': '1px',
+ * }
+ * @example {
+ *   pointerEvents: 'none',
+ *   cursor: 'default',
+ *   color: '--token',
+ *   backgroundColor: '--token',
+ *   '--variant-borderWidth': '0px',
+ * }
+ */
+export const createVariantStyle = (
+  source: Record<string, any>,
+  key: string,
+  value: string | undefined | null,
+) => {
+  if (value) {
+    if (key.includes('Disabled')) {
+      source.pointerEvents = 'none';
+      source.cursor = 'default';
+    }
+    if (key.match(/(Hover|Active|Disabled)/)) {
+      assignCss(source as any, key, value);
+    } else {
+      // initial state
+      if (!source['--variant-borderWidth']) {
+        // important to prevent inheritance, otherwise the children will have the wrong styles e.g.
+        //   <Card variant="outlined">
+        //     <Typography variant="soft">
+        source['--variant-borderWidth'] = '0px';
+      }
+      if (key.includes('Border')) {
+        source['--variant-borderWidth'] = '1px';
+        source.border = 'var(--variant-borderWidth) solid';
+      }
+      // border color should come later
+      assignCss(source as any, key, value);
+    }
+  }
+  return source;
 };
 
 export const createVariants = (
@@ -80,7 +141,7 @@ export const createVariants = (
               const variantName = matches[1] as keyof Variants;
               result[variantName] = result[variantName] || {};
               result[variantName][color] = result[variantName][color] || {};
-              const css: CSSObject = result[variantName][color];
+
               let defaultValue;
               const cssVarMatches = (value as string).match(/^var\(--(.*)\)$/);
               if ((color as string) !== 'context') {
@@ -103,100 +164,13 @@ export const createVariants = (
                 cssValue = value.toString();
               }
 
-              if (variantVar.includes('Disabled')) {
-                css.pointerEvents = 'none';
-                css.cursor = 'default';
-              }
-              if (variantVar.match(/(Hover|Active|Disabled)/)) {
-                assignCss(css as any, variantVar, cssValue);
-              } else {
-                // initial state
-                if (!css['--variant-borderWidth']) {
-                  // important to prevent inheritance, otherwise the children will have the wrong styles e.g.
-                  //   <Card variant="outlined">
-                  //     <Typography variant="soft">
-                  css['--variant-borderWidth'] = '0px';
-                }
-                if (variantVar.includes('Border')) {
-                  css['--variant-borderWidth'] = '1px';
-                  css.border = 'var(--variant-borderWidth) solid';
-                }
-                // border color should come later
-                assignCss(css as any, variantVar, cssValue);
-              }
+              createVariantStyle(result[variantName][color], variantVar, cssValue);
             }
           }
         },
       );
     }
   });
-  return result;
-};
-
-/**
- *
- * @param name variant name
- * @example 'plain'
- *
- * @param palette object that contains palette tokens
- * @example { primary: { plainColor: '', plainHoverColor: '', ...tokens }, ...other palete }
- *
- * @param getCssVar a function that receive variant token and return a CSS variable
- *
- * result will be the stylesheet based on the palette tokens
- * @example {
- *   color: '--token',
- *   backgroundColor: '--token',
- *   '--variant-borderWidth': '0px',
- * }
- * @example {
- *   cursor: 'pointer',
- *   color: '--token',
- *   backgroundColor: '--token',
- *   '--variant-borderWidth': '1px',
- * }
- * @example {
- *   pointerEvents: 'none',
- *   cursor: 'default',
- *   color: '--token',
- *   backgroundColor: '--token',
- *   '--variant-borderWidth': '0px',
- * }
- */
-export const createVariantStyle = (
-  name: string,
-  palette: Partial<PaletteVariant> | undefined,
-  getCssVar?: (variantVar: keyof PaletteVariant) => string,
-) => {
-  const result: CSSObject = {};
-  (Object.entries(palette || {}) as Array<[keyof PaletteVariant, string]>).forEach(
-    ([variantVar, value]) => {
-      if (variantVar.match(new RegExp(`${name}(color|bg|border)`, 'i')) && !!value) {
-        const cssVar = getCssVar ? getCssVar(variantVar) : value;
-        if (variantVar.includes('Disabled')) {
-          result.pointerEvents = 'none';
-          result.cursor = 'default';
-        }
-        if (variantVar.match(/(Hover|Active|Disabled)/)) {
-          assignCss(result as any, variantVar, cssVar);
-        } else {
-          // initial state
-          if (!result['--variant-borderWidth']) {
-            // important to prevent inheritance, otherwise the children will have the wrong styles e.g.
-            //   <Card variant="outlined">
-            //     <Typography variant="soft">
-            result['--variant-borderWidth'] = '0px';
-          }
-          if (variantVar.includes('Border')) {
-            result['--variant-borderWidth'] = '1px';
-            result.border = 'var(--variant-borderWidth) solid';
-          }
-          // border color should come later
-          assignCss(result as any, variantVar, cssVar);
-        }
-      }
-    },
-  );
   return result;
 };
 
@@ -212,60 +186,60 @@ const createPrefixVar = (cssVarPrefix: string | undefined | null) => {
 };
 
 // It's used only in extendTheme, so it's safe to always include default values
-export const createVariant = (variant: VariantKey, theme?: ThemeFragment) => {
-  let result = {} as Record<DefaultColorPalette | 'context', CSSObject>;
-  if (theme) {
-    const { getCssVar, palette } = theme;
-    Object.entries(palette).forEach((entry) => {
-      const [color, colorPalette] = entry as [
-        Exclude<DefaultColorPalette, 'context'>,
-        string | number | Record<string, any>,
-      ];
-      if (isVariantPalette(colorPalette) && typeof colorPalette === 'object') {
-        result = {
-          ...result,
-          [color]: createVariantStyle(variant, colorPalette, (variantVar) =>
-            getCssVar(`palette-${color}-${variantVar}`, palette[color][variantVar]),
-          ),
-        };
-      }
-    });
-  }
+// export const createVariant = (variant: VariantKey, theme?: ThemeFragment) => {
+//   let result = {} as Record<DefaultColorPalette | 'context', CSSObject>;
+//   if (theme) {
+//     const { getCssVar, palette } = theme;
+//     Object.entries(palette).forEach((entry) => {
+//       const [color, colorPalette] = entry as [
+//         Exclude<DefaultColorPalette, 'context'>,
+//         string | number | Record<string, any>,
+//       ];
+//       if (isVariantPalette(colorPalette) && typeof colorPalette === 'object') {
+//         result = {
+//           ...result,
+//           [color]: createVariantStyle(variant, colorPalette, (variantVar) =>
+//             getCssVar(`palette-${color}-${variantVar}`, palette[color][variantVar]),
+//           ),
+//         };
+//       }
+//     });
+//   }
 
-  result.context = createVariantStyle(variant, {
-    plainColor: 'var(--variant-plainColor)',
-    plainHoverColor: `var(--variant-plainHoverColor)`,
-    plainHoverBg: 'var(--variant-plainHoverBg)',
-    plainActiveBg: 'var(--variant-plainActiveBg)',
-    plainDisabledColor: 'var(--variant-plainDisabledColor)',
+//   result.context = createVariantStyle(variant, {
+//     plainColor: 'var(--variant-plainColor)',
+//     plainHoverColor: `var(--variant-plainHoverColor)`,
+//     plainHoverBg: 'var(--variant-plainHoverBg)',
+//     plainActiveBg: 'var(--variant-plainActiveBg)',
+//     plainDisabledColor: 'var(--variant-plainDisabledColor)',
 
-    outlinedColor: 'var(--variant-outlinedColor)',
-    outlinedBorder: 'var(--variant-outlinedBorder)',
-    outlinedHoverColor: `var(--variant-outlinedHoverColor)`,
-    outlinedHoverBorder: `var(--variant-outlinedHoverBorder)`,
-    outlinedHoverBg: `var(--variant-outlinedHoverBg)`,
-    outlinedActiveBg: `var(--variant-outlinedActiveBg)`,
-    outlinedDisabledColor: `var(--variant-outlinedDisabledColor)`,
-    outlinedDisabledBorder: `var(--variant-outlinedDisabledBorder)`,
+//     outlinedColor: 'var(--variant-outlinedColor)',
+//     outlinedBorder: 'var(--variant-outlinedBorder)',
+//     outlinedHoverColor: `var(--variant-outlinedHoverColor)`,
+//     outlinedHoverBorder: `var(--variant-outlinedHoverBorder)`,
+//     outlinedHoverBg: `var(--variant-outlinedHoverBg)`,
+//     outlinedActiveBg: `var(--variant-outlinedActiveBg)`,
+//     outlinedDisabledColor: `var(--variant-outlinedDisabledColor)`,
+//     outlinedDisabledBorder: `var(--variant-outlinedDisabledBorder)`,
 
-    softColor: 'var(--variant-softColor)',
-    softBg: 'var(--variant-softBg)',
-    softHoverColor: 'var(--variant-softHoverColor)',
-    softHoverBg: 'var(--variant-softHoverBg)',
-    softActiveBg: 'var(--variant-softActiveBg)',
-    softDisabledColor: 'var(--variant-softDisabledColor)',
-    softDisabledBg: 'var(--variant-softDisabledBg)',
+//     softColor: 'var(--variant-softColor)',
+//     softBg: 'var(--variant-softBg)',
+//     softHoverColor: 'var(--variant-softHoverColor)',
+//     softHoverBg: 'var(--variant-softHoverBg)',
+//     softActiveBg: 'var(--variant-softActiveBg)',
+//     softDisabledColor: 'var(--variant-softDisabledColor)',
+//     softDisabledBg: 'var(--variant-softDisabledBg)',
 
-    solidColor: 'var(--variant-solidColor)',
-    solidBg: 'var(--variant-solidBg)',
-    solidHoverColor: 'var(--variant-solidHoverColor)',
-    solidHoverBg: 'var(--variant-solidHoverBg)',
-    solidActiveBg: 'var(--variant-solidActiveBg)',
-    solidDisabledColor: 'var(--variant-solidDisabledColor)',
-    solidDisabledBg: 'var(--variant-solidDisabledBg)',
-  });
-  return result;
-};
+//     solidColor: 'var(--variant-solidColor)',
+//     solidBg: 'var(--variant-solidBg)',
+//     solidHoverColor: 'var(--variant-solidHoverColor)',
+//     solidHoverBg: 'var(--variant-solidHoverBg)',
+//     solidActiveBg: 'var(--variant-solidActiveBg)',
+//     solidDisabledColor: 'var(--variant-solidDisabledColor)',
+//     solidDisabledBg: 'var(--variant-solidDisabledBg)',
+//   });
+//   return result;
+// };
 
 export const createSoftInversion = (
   theme: ThemeFragment & {
