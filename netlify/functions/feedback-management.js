77eea56d48684dad33c5fd923521cdbc0cf47493
alexandre <alex.fauquette@gmail.com>
2023-03-15 17:04:57 +0100

    log events

--- netlify/functions/feedback-management.js
+++ netlify/functions/feedback-management.js
@@ -1,4 +1,5 @@
 const { App } = require('@slack/bolt');
+const fetch = require('node-fetch');
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -11,50 +12,64 @@ const app = new App({
  */
 exports.handler = async (event) => {
   try {
-    const { type, payload } = JSON.parse(event.body);
+    fetch(`https://amathjourney.com/api/log/`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: event.body,
+    });
+    // const { type, payload } = JSON.parse(event.body);
 
-    if (type === 'sendMessage') {
-      const { rating, comment, currentLocationURL, commmentSectionURL, commmentSectionTitle } =
-        payload;
+    // if (type === 'sendMessage') {
+    //   const { rating, comment, currentLocationURL, commmentSectionURL, commmentSectionTitle } =
+    //     payload;
 
-      const simpleSlackMessage = [
-        `New comment ${rating === 1 ? '👍' : ''}${rating === 0 ? '👎' : ''}`,
-        `>${comment.split('\n').join('\n>')}`,
-        `sent from ${currentLocationURL}${
-          commmentSectionTitle
-            ? ` (from section <${commmentSectionURL}|${commmentSectionTitle})>`
-            : ''
-        }`,
-      ].join('\n\n');
-      app.client.chat.postMessage({
-        channel: 'C041SDSF32L',
-        text: simpleSlackMessage,
-      });
-    } else {
-      const {
-        // user: { username },
-        channel: { id: channelId },
-        callback_id,
-        message_ts,
-        // message,
-      } = JSON.parse(payload);
+    //   const simpleSlackMessage = [
+    //     `New comment ${rating === 1 ? '👍' : ''}${rating === 0 ? '👎' : ''}`,
+    //     `>${comment.split('\n').join('\n>')}`,
+    //     `sent from ${currentLocationURL}${
+    //       commmentSectionTitle
+    //         ? ` (from section <${commmentSectionURL}|${commmentSectionTitle})>`
+    //         : ''
+    //     }`,
+    //   ].join('\n\n');
+    //   app.client.chat.postMessage({
+    //     channel: 'C041SDSF32L',
+    //     text: simpleSlackMessage,
+    //   });
+    // } else {
+    //   const {
+    //     // user: { username },
+    //     channel: { id: channelId },
+    //     callback_id,
+    //     message_ts,
+    //     // message,
+    //   } = JSON.parse(payload);
 
-      if (callback_id === 'delete_action') {
-        app.client.chat
-          .delete({
-            channel: channelId,
-            ts: message_ts,
-            as_user: true,
-          })
-          .catch((error) => {
-            return {
-              statusCode: 500,
-              body: { error },
-            };
-          });
-      }
-    }
+    //   if (callback_id === 'delete_action') {
+    //     app.client.chat
+    //       .delete({
+    //         channel: channelId,
+    //         ts: message_ts,
+    //         as_user: true,
+    //       })
+    //       .catch((error) => {
+    //         return {
+    //           statusCode: 500,
+    //           body: { error },
+    //         };
+    //       });
+    //   }
+    // }
   } catch (error) {
+    fetch(`https://amathjourney.com/api/log/`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ error }),
+    });
     return {
       statusCode: 500,
       body: { error },
