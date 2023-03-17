81fc82747329f598cbd8d20a6a4626f08f7e1c3d
Benny Joo <sldisek783@gmail.com>
2023-03-17 07:40:42 -0400

    update api docs

--- packages/mui-base/src/ButtonUnstyled/index.ts
+++ packages/mui-base/src/ButtonUnstyled/index.ts
@@ -1,6 +1,4 @@
 export { default } from './ButtonUnstyled';
-export {
-  default as buttonUnstyledClasses,
-} from './buttonUnstyledClasses';
+export { default as buttonUnstyledClasses } from './buttonUnstyledClasses';
 export * from './buttonUnstyledClasses';
 export * from './ButtonUnstyled.types';
