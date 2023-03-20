814f960ac28582a6adee840476f58015c7da2acc
Benny Joo <sldisek783@gmail.com>
2023-03-20 08:53:06 -0400

    [Base] Add JSDoc comments for classes of Base components

--- packages/mui-base/src/TabsListUnstyled/tabsListUnstyledClasses.ts
+++ packages/mui-base/src/TabsListUnstyled/tabsListUnstyledClasses.ts
@@ -2,8 +2,11 @@ import generateUtilityClass from '../generateUtilityClass';
 import generateUtilityClasses from '../generateUtilityClasses';
 
 export interface TabsListUnstyledClasses {
+  /** Class name applied to the root element. */
   root: string;
+  /** State class applied to the root `div` element if `orientation='horizontal'`. */
   horizontal: string;
+  /** State class applied to the root `div` element if `orientation='vertical'`. */
   vertical: string;
 }
 
