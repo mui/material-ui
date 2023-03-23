6bcb01ebe148c52a6ee53073bdb3a4d5e6730ce1
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2023-03-23 16:14:49 +0530

    use named import for babel-plugin-tester

--- packages/mui-utils/macros/MuiError.macro.test.js
+++ packages/mui-utils/macros/MuiError.macro.test.js
@@ -1,4 +1,4 @@
-import pluginTester from 'babel-plugin-tester';
+import { pluginTester } from 'babel-plugin-tester';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as os from 'os';
