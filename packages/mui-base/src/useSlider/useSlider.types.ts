9ec499ad214233f731e90ed5b7107a00e11d27d2
varunmulay22 <varunmulay22@gmail.com>
2023-03-24 12:03:13 +0530

    added jsDoc descriptions

--- packages/mui-base/src/useSlider/useSlider.types.ts
+++ packages/mui-base/src/useSlider/useSlider.types.ts
@@ -86,24 +86,72 @@ export interface AxisProps<T extends Axis> {
 }
 
 export interface UseSliderReturnValue {
+  /**
+   * If `true`, the slider will be active.
+   */
   active: number;
+  /**
+   * The orientation of the slider.
+   */
   axis: Axis;
+  /**
+   * The extra props for the axis. You can override the existing props or add new ones.
+   */
   axisProps: { [key in Axis]: AxisProps<key> };
+  /**
+   * The orientation of the slider.
+   */
   dragging: boolean;
+  /**
+   * The index of the thumb which is focused on the slider.
+   */
   focusedThumbIndex: number;
+  /**
+   * Resolver for the hidden input slot's props.
+   * @param externalProps props for the hidden input slot
+   * @returns props that should be spread on the hidden input slot
+   */
   getHiddenInputProps: <TOther extends EventHandlers = {}>(
     otherHandlers?: TOther,
   ) => UseSliderHiddenInputProps<TOther>;
+  /**
+   * Resolver for the root slot's props.
+   * @param externalProps props for the root slot
+   * @returns props that should be spread on the root slot
+   */
   getRootProps: <TOther extends EventHandlers = {}>(
     otherHandlers?: TOther,
   ) => UseSliderRootSlotProps<TOther>;
+  /**
+   * Resolver for the thumb slot's props.
+   * @param externalProps props for the thumb slot
+   * @returns props that should be spread on the thumb slot
+   */
   getThumbProps: <TOther extends EventHandlers = {}>(
     otherHandlers?: TOther,
   ) => UseSliderThumbSlotProps<TOther>;
+  /**
+   * Marks indicate predetermined values to which the user can move the slider. If true the marks are spaced according the value of the step prop. If an array, it should contain objects with value and an optional label keys.
+   */
   marks: Mark[];
+  /**
+   * The thumb index for the current value when in hover state.
+   */
   open: number;
+  /**
+   * The range of the slider.
+   */
   range: boolean;
+  /**
+   * The track leap for the current value of the slider.
+   */
   trackLeap: number;
+  /**
+   * The track offset for the current value of the slider.
+   */
   trackOffset: number;
+  /**
+   * The possible values of the slider.
+   */
   values: number[];
 }
