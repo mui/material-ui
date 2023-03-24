7465ccc38d59b79b1aec056e1b1cd04f99dde92a
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2023-03-24 15:32:11 +0530

    refactor useSlider paramaters type

--- packages/mui-base/src/useSlider/useSlider.types.ts
+++ packages/mui-base/src/useSlider/useSlider.types.ts
@@ -2,22 +2,104 @@ import * as React from 'react';
 import { EventHandlers } from '../utils';
 
 export interface UseSliderParameters {
+  /**
+   * The id of the element containing a label for the slider.
+   */
   'aria-labelledby'?: string;
+  /**
+   * The default value. Use when the component is not controlled.
+   */
   defaultValue?: number | number[];
+  /**
+   * If `true`, the component is disabled.
+   * @default false
+   */
   disabled?: boolean;
+  /**
+   * If `true`, the active thumb doesn't swap when moving pointer over a thumb while dragging another thumb.
+   * @default false
+   */
   disableSwap?: boolean;
+  /**
+   * If `true` the Slider will be rendered right-to-left (with the lowest value on the right-hand side).
+   * @default false
+   */
   isRtl?: boolean;
+  /**
+   * Marks indicate predetermined values to which the user can move the slider.
+   * If `true` the marks are spaced according the value of the `step` prop.
+   * If an array, it should contain objects with `value` and an optional `label` keys.
+   * @default false
+   */
   marks?: boolean | Mark[];
+  /**
+   * The maximum allowed value of the slider.
+   * Should not be equal to min.
+   * @default 100
+   */
   max?: number;
+  /**
+   * The minimum allowed value of the slider.
+   * Should not be equal to max.
+   * @default 0
+   */
   min?: number;
+  /**
+   * Name attribute of the hidden `input` element.
+   */
   name?: string;
+  /**
+   * Callback function that is fired when the slider's value changed.
+   *
+   * @param {Event} event The event source of the callback.
+   * You can pull out the new value by accessing `event.target.value` (any).
+   * **Warning**: This is a generic event not a change event.
+   * @param {number | number[]} value The new value.
+   * @param {number} activeThumb Index of the currently moved thumb.
+   */
   onChange?: (event: Event, value: number | number[], activeThumb: number) => void;
+  /**
+   * Callback function that is fired when the `mouseup` is triggered.
+   *
+   * @param {React.SyntheticEvent | Event} event The event source of the callback. **Warning**: This is a generic event not a change event.
+   * @param {number | number[]} value The new value.
+   */
   onChangeCommitted?: (event: React.SyntheticEvent | Event, value: number | number[]) => void;
+  /**
+   * The component orientation.
+   * @default 'horizontal'
+   */
   orientation?: 'horizontal' | 'vertical';
+  /**
+   * The ref attached to the root of the Slider.
+   */
   ref: React.Ref<any>;
+  /**
+   * A transformation function, to change the scale of the slider.
+   * @param {any} x
+   * @returns {any}
+   * @default function Identity(x) {
+   *   return x;
+   * }
+   */
   scale?: (value: number) => number;
+  /**
+   * The granularity with which the slider can step through values. (A "discrete" slider.)
+   * The `min` prop serves as the origin for the valid values.
+   * We recommend (max - min) to be evenly divisible by the step.
+   *
+   * When step is `null`, the thumb can only be slid onto marks provided with the `marks` prop.
+   * @default 1
+   */
   step?: number | null;
+  /**
+   * Tab index attribute of the hidden `input` element.
+   */
   tabIndex?: number;
+  /**
+   * The value of the slider.
+   * For ranged sliders, provide an array with two values.
+   */
   value?: number | number[];
 }
 
