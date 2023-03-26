1a22c614176f435a67a0bde6bcd5456281c71f95
Olivier Tassinari <olivier.tassinari@gmail.com>
2023-03-27 01:22:48 +0200

    merge, simpler

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -34,7 +34,9 @@ if (process.env.NODE_ENV === 'production') {
 const PRODUCTION_GA =
   process.env.DEPLOY_ENV === 'production' || process.env.DEPLOY_ENV === 'staging';
 
+// TODO remove https://support.google.com/analytics/answer/11986666
 const GOOGLE_ANALYTICS_ID = PRODUCTION_GA ? 'UA-106598593-2' : 'UA-106598593-3';
+
 const GOOGLE_ANALYTICS_ID_V4 = PRODUCTION_GA ? 'G-5NXDQLC2ZK' : 'G-XJ83JQEK7J';
 
 export default class MyDocument extends Document {
@@ -155,25 +157,11 @@ export default class MyDocument extends Document {
             // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{
               __html: `
-                window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-                window.ga('create','${GOOGLE_ANALYTICS_ID}',{
-                  sampleRate: ${PRODUCTION_GA ? 80 : 100},
-                });
-              `,
-            }}
-          />
-          {/**
-           * A better alternative to <script async>, to delay its execution
-           * https://developer.chrome.com/blog/script-component/
-           */}
-          <Script
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID_V4}`}
-          />
-          <script
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+window.ga('create','${GOOGLE_ANALYTICS_ID}',{
+  sampleRate: ${PRODUCTION_GA ? 80 : 100},
+});
+
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 window.gtag = gtag;
@@ -183,6 +171,14 @@ gtag('config', '${GOOGLE_ANALYTICS_ID_V4}', {
 });
 `,
             }}
+          />
+          {/**
+           * A better alternative to <script async>, to delay its execution
+           * https://developer.chrome.com/blog/script-component/
+           */}
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID_V4}`}
           />
           <NextScript />
         </body>
