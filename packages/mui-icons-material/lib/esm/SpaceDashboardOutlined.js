ae9b0e3e425bf7868203b307aec387ebff46c9ca
Michał Dudak <michal@dudak.me>
2023-03-29 15:10:08 +0200

    [icons] Do not ignore popular icons (#36608)

---
+++ packages/mui-icons-material/lib/esm/SpaceDashboardOutlined.js
@@ -0,0 +1,5 @@
+import createSvgIcon from './utils/createSvgIcon';
+import { jsx as _jsx } from "react/jsx-runtime";
+export default createSvgIcon( /*#__PURE__*/_jsx("path", {
+  d: "M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM5 19V5h6v14H5zm14 0h-6v-7h6v7zm0-9h-6V5h6v5z"
+}), 'SpaceDashboardOutlined');
