0ab8bdfb817d9c1eb5a279ec6be3396dcf632603
siriwatknp <siriwatkunaporn@gmail.com>
2023-03-30 00:16:29 +0700

    fix types

--- packages/mui-system/src/GlobalStyles/GlobalStyles.tsx
+++ packages/mui-system/src/GlobalStyles/GlobalStyles.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import { GlobalStyles as MuiGlobalStyles, Interpolation } from '@mui/styled-engine';
 import useTheme from '../useTheme';
 import { Theme as SystemTheme } from '../createTheme';
 
 export interface GlobalStylesProps<Theme = SystemTheme> {
   styles: Interpolation<Theme>;
-  defaultTheme?: Theme;
+  defaultTheme?: object;
   identifier?: string;
 }
 
-export default function GlobalStyles<Theme = SystemTheme>({
+function GlobalStyles<Theme = SystemTheme>({
   styles,
   identifier,
-  defaultTheme = {} as Theme,
+  defaultTheme = {},
 }: GlobalStylesProps<Theme>) {
   const upperTheme = useTheme(defaultTheme);
 
@@ -23,3 +24,33 @@ export default function GlobalStyles<Theme = SystemTheme>({
 
   return <MuiGlobalStyles styles={globalStyles as any} />;
 }
+
+GlobalStyles.propTypes /* remove-proptypes */ = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // |     To update them edit TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * @ignore
+   */
+  defaultTheme: PropTypes.any,
+  /**
+   * @ignore
+   */
+  identifier: PropTypes.string,
+  /**
+   * @ignore
+   */
+  styles: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.number,
+    PropTypes.object,
+    PropTypes.shape({
+      __emotion_styles: PropTypes.any.isRequired,
+    }),
+    PropTypes.string,
+    PropTypes.bool,
+  ]),
+} as any;
+
+export default GlobalStyles;
