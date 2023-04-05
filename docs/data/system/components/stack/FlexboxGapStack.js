2f09fd17da7c7facce57554eeccb33383fb0697d
Olivier Tassinari <olivier.tassinari@gmail.com>
2023-04-05 23:13:47 +0200

    [docs] Sync Stack between projects

--- docs/data/system/components/stack/FlexboxGapStack.js
+++ docs/data/system/components/stack/FlexboxGapStack.js
@@ -1,25 +1,24 @@
 import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import { styled, Stack } from '@mui/system';
+import Stack from '@mui/system/Stack';
+import Box from '@mui/system/Box';
+import { styled } from '@mui/system';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+const Item = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#262B32' : '#fff',
   padding: theme.spacing(1),
   textAlign: 'center',
+  borderRadius: 4,
+  flexGrow: 1,
 }));
 
 export default function FlexboxGapStack() {
   return (
-    <Stack
-      spacing={{ xs: 1, sm: 2 }}
-      direction="row"
-      useFlexGap
-      flexWrap="wrap"
-      sx={{ width: 200, '& > *': { flexGrow: 1 } }}
-    >
-      <Item>Item 1</Item>
-      <Item>Item 2</Item>
-      <Item>Long content</Item>
-    </Stack>
+    <Box sx={{ width: 200 }}>
+      <Stack spacing={{ xs: 1, sm: 2 }} direction="row" useFlexGap flexWrap="wrap">
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Long content</Item>
+      </Stack>
+    </Box>
   );
 }
