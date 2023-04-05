001a4c090b082247ae31c4dd4d24128141fdf0f1
Benny Joo <sldisek783@gmail.com>
2023-04-05 12:16:23 +0100

    revert slots/slotProps typing in Box

--- packages/mui-joy/src/Box/BoxProps.ts
+++ packages/mui-joy/src/Box/BoxProps.ts
@@ -1,28 +1,12 @@
 import { OverrideProps } from '@mui/types';
 import { BoxTypeMap } from '@mui/system';
 import { Theme } from '../styles/types';
-import { SlotProps, CreateSlotsAndSlotProps } from '../utils/types';
 
 export type BoxSlot = 'root';
-
-export interface BoxSlots {
-  /**
-   * The component that renders the root.
-   * @default 'div'
-   */
-  root: React.ElementType;
-}
-
-export type BoxSlotsAndSlotProps = CreateSlotsAndSlotProps<
-  BoxSlots,
-  {
-    root: SlotProps<'div', {}, BoxOwnerState>;
-  }
->;
 
 export type BoxProps<
   D extends React.ElementType = BoxTypeMap['defaultComponent'],
   P = {},
-> = OverrideProps<BoxTypeMap<P & BoxSlotsAndSlotProps, D, Theme>, D>;
+> = OverrideProps<BoxTypeMap<P, D, Theme>, D>;
 
 export interface BoxOwnerState extends BoxProps {}
