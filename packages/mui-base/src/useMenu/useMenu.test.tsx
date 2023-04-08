fe2f664ff278e7b54ae422b620d78cd25b2c2517
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-04-08 22:59:54 +0530

    test name

--- packages/mui-base/src/useMenu/useMenu.test.tsx
+++ packages/mui-base/src/useMenu/useMenu.test.tsx
@@ -7,34 +7,32 @@ import MenuItemUnstyled, { MenuItemMetadata } from '@mui/base/MenuItemUnstyled';
 
 describe('useMenu', () => {
   const { render } = createRenderer();
-  describe('prop: id', () => {
-    it('propagates it to the root element', () => {
-      let menuItemsData: Record<string, MenuItemMetadata> | undefined;
-      function Menu({ options }: { options: Array<string> }) {
-        const ref = React.createRef();
-        const { getListboxProps, contextValue, menuItems } = useMenu({ listboxRef: ref });
-        menuItemsData = menuItems;
-        return (
-          <ul {...getListboxProps()}>
-            {
-              <MenuUnstyledContext.Provider value={contextValue}>
-                {options.map((option) => (
-                  <MenuItemUnstyled value={option} label={option} key={option} />
-                ))}
-              </MenuUnstyledContext.Provider>
-            }
-          </ul>
-        );
-      }
-      const extractLables = () => Object.values(menuItemsData!).map((data) => data.label);
+  it('should register options in correct order', () => {
+    let menuItemsData: Record<string, MenuItemMetadata> | undefined;
+    function Menu({ options }: { options: Array<string> }) {
+      const ref = React.createRef();
+      const { getListboxProps, contextValue, menuItems } = useMenu({ listboxRef: ref });
+      menuItemsData = menuItems;
+      return (
+        <ul {...getListboxProps()}>
+          {
+            <MenuUnstyledContext.Provider value={contextValue}>
+              {options.map((option) => (
+                <MenuItemUnstyled value={option} label={option} key={option} />
+              ))}
+            </MenuUnstyledContext.Provider>
+          }
+        </ul>
+      );
+    }
+    const extractLables = () => Object.values(menuItemsData!).map((data) => data.label);
 
-      const mount = render(<Menu options={['1', '2', '3', '4', '5']} />);
+    const mount = render(<Menu options={['1', '2', '3', '4', '5']} />);
 
-      mount.setProps({ options: ['1', '5'] });
-      expect(extractLables()).deep.equal(['1', '5']);
+    mount.setProps({ options: ['1', '5'] });
+    expect(extractLables()).deep.equal(['1', '5']);
 
-      mount.setProps({ options: ['1', '2', '3', '4', '5'] });
-      expect(extractLables()).deep.equal(['1', '2', '3', '4', '5']);
-    });
+    mount.setProps({ options: ['1', '2', '3', '4', '5'] });
+    expect(extractLables()).deep.equal(['1', '2', '3', '4', '5']);
   });
 });
