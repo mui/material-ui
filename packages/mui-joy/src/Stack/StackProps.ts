e913291df726ea854e76ab1129ea4379386ce8ea
Olivier Tassinari <olivier.tassinari@gmail.com>
2023-04-08 12:13:10 +0200

    fix 301

--- packages/mui-joy/src/Stack/StackProps.ts
+++ packages/mui-joy/src/Stack/StackProps.ts
@@ -29,7 +29,7 @@ export interface StackTypeMap<P = {}, D extends React.ElementType = 'div'> {
     /**
      * If `true`, the CSS flexbox `gap` is used instead of applying `margin` to children.
      *
-     * While CSS `gap` removes the [known limitations](https://mui.com/joy-ui/react-stack#limitations),
+     * While CSS `gap` removes the [known limitations](https://mui.com/joy-ui/react-stack/#limitations),
      * it is not fully supported in some browsers. We recommend checking https://caniuse.com/?search=flex%20gap before using this flag.
      *
      * To enable this flag globally, follow the [theme's default props](https://mui.com/joy-ui/customization/themed-components/#default-props) configuration.
