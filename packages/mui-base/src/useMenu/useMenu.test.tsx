9b24fe4a94b261b88bcc101ae123d834022b7029
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-04-09 13:57:56 +0530

    update test

--- packages/mui-base/src/useMenu/useMenu.test.tsx
+++ packages/mui-base/src/useMenu/useMenu.test.tsx
@@ -1,38 +1,68 @@
 import * as React from 'react';
 import { expect } from 'chai';
-import { createRenderer } from 'test/utils';
+import { act, createRenderer, fireEvent } from 'test/utils';
 import useMenu from '@mui/base/useMenu';
 import { MenuUnstyledContext } from '@mui/base/MenuUnstyled';
-import MenuItemUnstyled, { MenuItemMetadata } from '@mui/base/MenuItemUnstyled';
+import MenuItemUnstyled from '@mui/base/MenuItemUnstyled';
 
 describe('useMenu', () => {
   const { render } = createRenderer();
   it('should register options in correct order', () => {
-    let menuItemsData: Record<string, MenuItemMetadata> | undefined;
-    function Menu({ options }: { options: Array<string> }) {
+    function Menu() {
       const ref = React.createRef();
-      const { getListboxProps, contextValue, menuItems } = useMenu({ listboxRef: ref });
-      menuItemsData = menuItems;
+      const { getListboxProps, contextValue } = useMenu({ listboxRef: ref });
+      const [options, setOptions] = React.useState(['1', '2', '3', '4', '5']);
       return (
-        <ul {...getListboxProps()}>
-          {
-            <MenuUnstyledContext.Provider value={contextValue}>
-              {options.map((option) => (
-                <MenuItemUnstyled value={option} label={option} key={option} />
-              ))}
-            </MenuUnstyledContext.Provider>
-          }
-        </ul>
+        <React.Fragment>
+          <button onClick={() => setOptions(['5', '6', '7', '8', '9'])}>update options</button>
+          <ul {...getListboxProps()}>
+            {
+              <MenuUnstyledContext.Provider value={contextValue}>
+                {options.map((option) => (
+                  <MenuItemUnstyled value={option} label={option} key={option} />
+                ))}
+              </MenuUnstyledContext.Provider>
+            }
+          </ul>
+        </React.Fragment>
       );
     }
-    const extractLables = () => Object.values(menuItemsData!).map((data) => data.label);
 
-    const mount = render(<Menu options={['1', '2', '3', '4', '5']} />);
+    const mount = render(<Menu />);
 
-    mount.setProps({ options: ['1', '5'] });
-    expect(extractLables()).deep.equal(['1', '5']);
+    const { getByRole } = mount;
 
-    mount.setProps({ options: ['1', '2', '3', '4', '5'] });
-    expect(extractLables()).deep.equal(['1', '2', '3', '4', '5']);
+    const menu = getByRole('menu');
+    const button = getByRole('button');
+    const options = menu.querySelectorAll('li')!;
+
+    act(() => {
+      menu.focus();
+    });
+
+    fireEvent.keyDown(menu, { key: 'ArrowDown' });
+    expect(document.activeElement).to.equal(options[1]);
+    fireEvent.keyDown(options[1], { key: 'ArrowDown' });
+    expect(document.activeElement).to.equal(options[2]);
+    fireEvent.keyDown(options[2], { key: 'ArrowDown' });
+    expect(document.activeElement).to.equal(options[3]);
+    fireEvent.keyDown(options[3], { key: 'ArrowDown' });
+    expect(document.activeElement).to.equal(options[4]);
+
+    fireEvent.click(button);
+
+    act(() => {
+      getByRole('menu').focus();
+    });
+    const updatedOptions = menu.querySelectorAll('li');
+
+    fireEvent.keyDown(getByRole('menu'), { key: 'ArrowDown' });
+    expect(document.activeElement).to.equal(updatedOptions[1]);
+    fireEvent.keyDown(updatedOptions[1], { key: 'ArrowDown' });
+    expect(document.activeElement).to.equal(updatedOptions[2]);
+    fireEvent.keyDown(updatedOptions[2], { key: 'ArrowDown' });
+    expect(document.activeElement).to.equal(updatedOptions[3]);
+    fireEvent.keyDown(updatedOptions[3], { key: 'ArrowDown' });
+    expect(document.activeElement).to.equal(updatedOptions[4]);
   });
 });
