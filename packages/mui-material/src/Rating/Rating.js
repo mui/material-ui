c2a2929ba3fa46782324ca7146a2c0a18ea3ebaf
Nicolas Theodarus <nicolas.theodarus@check24.de>
2023-04-11 09:28:04 +0200

    run yarn proptypes

--- packages/mui-material/src/Rating/Rating.js
+++ packages/mui-material/src/Rating/Rating.js
@@ -674,7 +674,7 @@ Rating.propTypes /* remove-proptypes */ = {
   /**
    * The component containing the icon.
    * @default function IconContainer(props) {
-   *   const { value, ...other } = props;
+   *   const { value, isActive, ...other } = props;
    *   return <span {...other} />;
    * }
    */
