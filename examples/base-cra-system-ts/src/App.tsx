3b99881d4728ca27bb77ba63f0b25d946d0dc8a0
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-04-12 19:03:49 -0500

    improved startup page

--- examples/base-cra-system-ts/src/App.tsx
+++ examples/base-cra-system-ts/src/App.tsx
@@ -1,34 +1,50 @@
 import * as React from 'react';
-import { styled } from '@mui/system';
-import { ButtonUnstyled, buttonUnstyledClasses } from '@mui/base';
+import { Box, Stack, styled } from '@mui/system';
 
-const Button = styled(ButtonUnstyled)`
-  padding-inline: 1rem;
-  padding-block: 0.5rem;
-  font-size: 1rem;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif,
-    'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-  border-radius: 4px;
-  border: 2px solid rgba(0 0 0 / 0.5);
-  background-color: initial;
-  &:hover {
-    background-color: rgba(0 0 0 / 0.08);
-    border-color: rgba(0 0 0 / 0.72);
-  }
-  &:active {
-    background-color: rgba(0 0 0 / 0.12);
-    transform: translateY(2px);
-  }
-  &.${buttonUnstyledClasses.focusVisible} {
-    outline: 3px solid rgba(0 0 0 / 0.3);
-    border-color: rgba(0 0 0 / 0.72);
-  }
-  &.${buttonUnstyledClasses.disabled} {
-    opacity: 0.6;
-    cursor: not-allowed;
-  }
-`;
+const Item = styled('div')({
+  border: '1px solid #000000',
+  padding: '20px',
+  borderRadius: '4px',
+});
 
-export default function App() {
-  return <Button>Hello from Base UI</Button>;
+export default function RowAndColumnSpacing() {
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        maxWidth: '800px',
+        margin: 'auto',
+        padding: '15px',
+        fontFamily: 'sans-serif',
+        lineHeight: '1.5',
+      }}
+    >
+      <div>
+        <h1>Base UI + CRA + MUI System scaffold (TypeScript)</h1>
+      </div>
+
+      <Stack spacing={2} sx={{ marginBottom: '20px' }}>
+        <Item>
+          <a href="https://mui.com/base/getting-started/overview/">Base UI</a> is a library of
+          unstyled React UI components which includes prebuilt components with production-ready
+          functionality, along with low-level hooks for transferring that functionality to other
+          components.
+        </Item>
+        <Item>
+          <a href="https://create-react-app.dev/">Create React App</a> is a framework for quickly
+          creating a new React project without the need to configure complex build tools or
+          development environments.
+        </Item>
+        <Item>
+          <a href="https://mui.com/system/getting-started/overview/">MUI System</a> is a set of CSS
+          utilities for building custom designs more efficiently when working with MUI component
+          libraries. This startup page uses the MUI System `styled()` and `sx` utilities. (Note: MUI
+          System, in turn, relies on <a href="https://emotion.sh/docs/introduction">Emotion</a>.)
+        </Item>
+      </Stack>
+      <span>
+        Created with 💙 by <a href="https://mui.com">MUI</a>.
+      </span>
+    </Box>
+  );
 }
