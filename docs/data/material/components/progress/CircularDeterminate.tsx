d406664fc1fb17fc25be36d2634e5fe1348486a1
apoznanski <71227648+apoznanski@users.noreply.github.com>
2023-04-18 19:36:50 -0400

    progress percent announced every 2.5 seconds

--- docs/data/material/components/progress/CircularDeterminate.tsx
+++ docs/data/material/components/progress/CircularDeterminate.tsx
@@ -4,16 +4,29 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 export default function CircularDeterminate() {
   const [progress, setProgress] = React.useState(0);
-
+  const srOnlyStyle: React.CSSProperties = {
+    position: 'absolute',
+    width: '1px',
+    height: '1px',
+    padding: '0',
+    margin: '-1px',
+    overflow: 'hidden',
+    clip: 'rect(0,0,0,0)',
+    whiteSpace: 'nowrap',
+    border: '0'
+  };
+  
   React.useEffect(() => {
     const timer = setInterval(() => {
-      setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 10));
-    }, 800);
+      setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 25));
+    }, 2500);
 
     return () => {
       clearInterval(timer);
     };
   }, []);
+
+  const progressDescription = `${progress}% progress`;
 
   return (
     <Stack spacing={2} direction="row">
@@ -21,7 +34,19 @@ export default function CircularDeterminate() {
       <CircularProgress variant="determinate" value={50} />
       <CircularProgress variant="determinate" value={75} />
       <CircularProgress variant="determinate" value={100} />
-      <CircularProgress variant="determinate" value={progress} />
+      <div aria-describedby='progress-description' aria-live="polite" >
+        <CircularProgress 
+        variant="determinate" 
+        value={progress}
+        aria-valuemin={0}
+        aria-valuemax={100}
+        aria-valuetext={progressDescription}
+        />
+        <div id="progress-description" style={srOnlyStyle}>
+          {progressDescription}
+        </div>
+      </div>
+
     </Stack>
   );
 }
