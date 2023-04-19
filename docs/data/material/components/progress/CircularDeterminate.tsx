58c6a7bf78ebae7f838aa40d28b64eccf17247a0
apoznanski <71227648+apoznanski@users.noreply.github.com>
2023-04-19 14:30:54 -0400

    add button to run demo

--- docs/data/material/components/progress/CircularDeterminate.tsx
+++ docs/data/material/components/progress/CircularDeterminate.tsx
@@ -2,21 +2,47 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import CircularProgress from '@mui/material/CircularProgress';
 import { visuallyHidden } from 'packages/mui-utils/src';
+import Button from '@mui/material/Button';
 
 export default function CircularDeterminate() {
   const [progress, setProgress] = React.useState(0);
-  
+  const [isRunning, setIsRunning] = React.useState(false);
+
   React.useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 25));
-    }, 2500);
+    let timer: NodeJS.Timeout | null = null;
+
+    if (isRunning) {
+      timer = setInterval(() => {
+        setProgress((prevProgress) => {
+          const nextProgress = prevProgress + 25;
+          if (nextProgress >= 100) {
+            setIsRunning(false);
+            return 100;
+          } else {
+            return nextProgress;
+          }
+        });
+      }, 2000);
+    }
 
     return () => {
-      clearInterval(timer);
+      if (timer) {
+        clearInterval(timer);
+      }
     };
-  }, []);
+  }, [isRunning]);
 
   const progressDescription = `${progress}% progress`;
+
+  const handleStart = () => {
+    setIsRunning(true);
+    setProgress(0);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+    setProgress(0);
+  };
 
   return (
     <Stack spacing={2} direction="row">
@@ -24,18 +50,29 @@ export default function CircularDeterminate() {
       <CircularProgress variant="determinate" value={50} />
       <CircularProgress variant="determinate" value={75} />
       <CircularProgress variant="determinate" value={100} />
-      <div aria-describedby='progress-description' aria-live="polite" >
-        <CircularProgress 
-        variant="determinate" 
-        value={progress}
-        aria-valuemin={0}
-        aria-valuemax={100}
+      <div aria-describedby="progress-description" aria-live="polite">
+        <CircularProgress
+          variant="determinate"
+          value={progress}
+          aria-valuemin={0}
+          aria-valuemax={100}
         />
         <div id="progress-description" style={visuallyHidden}>
           {progressDescription}
         </div>
       </div>
-
+      <Stack spacing={2} direction="row">
+        {!isRunning && (
+          <Button onClick={handleStart} variant="outlined">
+            Start
+          </Button>
+        )}
+        {isRunning && (
+          <Button onClick={handleStop} variant="outlined">
+            Stop
+          </Button>
+        )}
+      </Stack>
     </Stack>
   );
 }
