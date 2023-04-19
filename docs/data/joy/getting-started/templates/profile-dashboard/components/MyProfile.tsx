d594a582cbdd440fd06c60bf03bc9f7eddf298e8
siriwatknp <siriwatkunaporn@gmail.com>
2023-04-19 11:34:50 +0700

    extract components

--- docs/data/joy/getting-started/templates/profile-dashboard/components/MyProfile.tsx
+++ docs/data/joy/getting-started/templates/profile-dashboard/components/MyProfile.tsx
@@ -1,19 +1,9 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import * as React from 'react';
-import AspectRatio from '@mui/joy/AspectRatio';
 import Avatar from '@mui/joy/Avatar';
-import Autocomplete from '@mui/joy/Autocomplete';
-import AutocompleteOption from '@mui/joy/AutocompleteOption';
 import Box from '@mui/joy/Box';
 import Button from '@mui/joy/Button';
-import Card from '@mui/joy/Card';
-import CardContent from '@mui/joy/CardContent';
 import Chip, { chipClasses } from '@mui/joy/Chip';
 import Divider from '@mui/joy/Divider';
-import IconButton from '@mui/joy/IconButton';
-import Link from '@mui/joy/Link';
-import ListItemDecorator from '@mui/joy/ListItemDecorator';
-import LinearProgress from '@mui/joy/LinearProgress';
 import FormControl from '@mui/joy/FormControl';
 import FormLabel from '@mui/joy/FormLabel';
 import FormHelperText from '@mui/joy/FormHelperText';
@@ -27,6 +17,10 @@ import Typography from '@mui/joy/Typography';
 import Tabs from '@mui/joy/Tabs';
 import TabList from '@mui/joy/TabList';
 import Tab, { tabClasses } from '@mui/joy/Tab';
+import DropZone from './DropZone';
+import FileUpload from './FileUpload';
+import CountrySelector from './CountrySelector';
+import EditorToolbar from './EditorToolbar';
 
 export default function MyProfile() {
   return (
@@ -56,18 +50,17 @@ export default function MyProfile() {
             position: 'sticky',
             top: 'calc(var(--Tab-height) - var(--main-paddingTop) - (var(--_shadow-height) / 2))',
             zIndex: 1,
-          }}
-        >
-          <Box
-            sx={{
+            '&::before': {
+              content: '""',
+              display: 'block',
               position: 'relative',
               zIndex: 1,
               height: 'var(--_shadow-height)',
               background:
                 'radial-gradient(closest-side, rgba(0 0 0 / 0.12), transparent 100%)',
-            }}
-          />
-        </Box>
+            },
+          }}
+        />
         <TabList
           variant="plain"
           size="sm"
@@ -76,8 +69,7 @@ export default function MyProfile() {
             '--ListItem-minHeight': 'var(--Tab-height)',
             '--Chip-minHeight': '20px',
             bgcolor: 'background.body',
-            borderBottom: '1px solid',
-            borderBottomColor: 'divider',
+            boxShadow: `inset 0 -1px 0 0 ${theme.vars.palette.divider}`,
             position: 'sticky',
             top: 'calc(-1 * var(--main-paddingTop))',
             zIndex: 10,
@@ -142,7 +134,7 @@ export default function MyProfile() {
             gridTemplateColumns: {
               xs: '100%',
               sm: 'minmax(120px, 30%) 1fr',
-              lg: '280px 1fr 208px',
+              lg: '280px 1fr minmax(120px, 208px)',
             },
             columnGap: { xs: 2, sm: 3, md: 4 },
             rowGap: { xs: 2, sm: 2.5 },
@@ -217,41 +209,7 @@ export default function MyProfile() {
               src="/static/images/avatar/1.jpg"
               sx={{ '--Avatar-size': '64px' }}
             />
-            <Card
-              variant="outlined"
-              sx={{
-                borderRadius: 'sm',
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 1,
-                alignItems: 'center',
-                px: 3,
-                flexGrow: 1,
-              }}
-            >
-              <Box sx={{ p: 1, bgcolor: 'background.level1', borderRadius: '50%' }}>
-                <Box
-                  sx={{
-                    width: 32,
-                    height: 32,
-                    borderRadius: '50%',
-                    bgcolor: 'background.level2',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <i data-feather="upload-cloud" />
-                </Box>
-              </Box>
-              <Typography level="body2" textAlign="center">
-                <Link component="button" overlay>
-                  Click to upload
-                </Link>{' '}
-                or drag and drop
-                <br /> SVG, PNG, JPG or GIF (max. 800x400px)
-              </Typography>
-            </Card>
+            <DropZone />
           </Box>
 
           <Divider role="presentation" />
@@ -263,36 +221,7 @@ export default function MyProfile() {
 
           <Divider role="presentation" />
 
-          <FormControl sx={{ display: { sm: 'contents' } }}>
-            <FormLabel>Contry</FormLabel>
-            <Autocomplete
-              defaultValue={{ label: 'Thailand', code: 'TH' }}
-              options={[
-                { label: 'United States', code: 'US' },
-                { label: 'Thailand', code: 'TH' },
-              ]}
-              renderOption={(props, option) => (
-                <AutocompleteOption {...props}>
-                  <ListItemDecorator>
-                    <AspectRatio
-                      ratio="1"
-                      sx={{ minWidth: 20, borderRadius: '50%' }}
-                    >
-                      <img
-                        loading="lazy"
-                        width="20"
-                        src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
-                        srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
-                        alt=""
-                      />
-                    </AspectRatio>
-                  </ListItemDecorator>
-                  {option.label}
-                </AutocompleteOption>
-              )}
-              slotProps={{}}
-            />
-          </FormControl>
+          <CountrySelector />
 
           <Divider role="presentation" />
 
@@ -321,42 +250,7 @@ export default function MyProfile() {
             <FormHelperText>Write a short introduction</FormHelperText>
           </Box>
           <Box>
-            <Box sx={{ display: 'flex', gap: 0.5 }}>
-              <Select defaultValue="1" sx={{ minWidth: 160 }}>
-                <Option value="1">Normal text</Option>
-                <Option value="2" sx={{ fontFamily: 'code' }}>
-                  Code text
-                </Option>
-              </Select>
-              <IconButton
-                variant="plain"
-                color="neutral"
-                sx={{ '--Icon-fontSize': '16px' }}
-              >
-                <i data-feather="bold" />
-              </IconButton>
-              <IconButton
-                variant="plain"
-                color="neutral"
-                sx={{ '--Icon-fontSize': '16px' }}
-              >
-                <i data-feather="italic" />
-              </IconButton>
-              <IconButton
-                variant="plain"
-                color="neutral"
-                sx={{ '--Icon-fontSize': '16px' }}
-              >
-                <i data-feather="link-2" />
-              </IconButton>
-              <IconButton
-                variant="plain"
-                color="neutral"
-                sx={{ '--Icon-fontSize': '16px' }}
-              >
-                <i data-feather="list" />
-              </IconButton>
-            </Box>
+            <EditorToolbar />
             <Textarea
               minRows={4}
               sx={{ mt: 1.5 }}
@@ -374,184 +268,27 @@ export default function MyProfile() {
             <FormHelperText>Share a few snippets of your work.</FormHelperText>
           </Box>
           <Stack useFlexGap spacing={1.5}>
-            <Card
-              variant="outlined"
-              sx={{
-                borderRadius: 'sm',
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 1,
-                alignItems: 'center',
-                px: 3,
-                flexGrow: 1,
-              }}
-            >
-              <Box sx={{ p: 1, bgcolor: 'background.level1', borderRadius: '50%' }}>
-                <Box
-                  sx={{
-                    width: 32,
-                    height: 32,
-                    borderRadius: '50%',
-                    bgcolor: 'background.level2',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <i data-feather="upload-cloud" />
-                </Box>
-              </Box>
-              <Typography level="body2" textAlign="center">
-                <Link component="button" overlay>
-                  Click to upload
-                </Link>{' '}
-                or drag and drop
-                <br /> SVG, PNG, JPG or GIF (max. 800x400px)
-              </Typography>
-            </Card>
-            <Card
-              variant="outlined"
-              orientation="horizontal"
-              sx={{
-                gap: 1.5,
-                alignItems: 'flex-start',
-                borderColor: 'primary.500',
-              }}
-            >
-              <AspectRatio
-                ratio="1"
-                variant="soft"
-                color="primary"
-                sx={{
-                  minWidth: 32,
-                  borderRadius: '50%',
-                  '--Icon-fontSize': '16px',
-                }}
-              >
-                <div>
-                  <i data-feather="file" />
-                </div>
-              </AspectRatio>
-              <CardContent>
-                <Typography fontSize="sm">Tech design requirements.pdf</Typography>
-                <Typography level="body3">200 KB</Typography>
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                  <LinearProgress
-                    value={100}
-                    determinate
-                    variant="plain"
-                    sx={{ bgcolor: 'neutral.softBg' }}
-                  />
-                  <Typography fontSize="xs">100%</Typography>
-                </Box>
-              </CardContent>
-              <AspectRatio
-                ratio="1"
-                variant="solid"
-                color="primary"
-                sx={{
-                  minWidth: 20,
-                  borderRadius: '50%',
-                  '--Icon-fontSize': '14px',
-                }}
-              >
-                <div>
-                  <i data-feather="check" />
-                </div>
-              </AspectRatio>
-            </Card>
-            <Card
-              variant="outlined"
-              orientation="horizontal"
-              sx={{
-                gap: 1.5,
-                alignItems: 'flex-start',
-              }}
-            >
-              <AspectRatio
-                ratio="1"
-                variant="soft"
-                color="primary"
-                sx={{
-                  minWidth: 32,
-                  borderRadius: '50%',
-                  '--Icon-fontSize': '16px',
-                }}
-              >
-                <div>
-                  <i data-feather="film" />
-                </div>
-              </AspectRatio>
-              <CardContent>
-                <Typography fontSize="sm">
-                  Dashboard prototype recording.mp4
-                </Typography>
-                <Typography level="body3">16 MB</Typography>
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                  <LinearProgress
-                    variant="plain"
-                    value={40}
-                    determinate
-                    sx={{ bgcolor: 'neutral.softBg' }}
-                  />
-                  <Typography fontSize="xs">40%</Typography>
-                </Box>
-              </CardContent>
-              <IconButton
-                variant="plain"
-                color="neutral"
-                size="sm"
-                sx={{ mt: -1, mr: -1 }}
-              >
-                <i data-feather="trash-2" />
-              </IconButton>
-            </Card>
-            <Card
-              variant="outlined"
-              orientation="horizontal"
-              sx={{
-                gap: 1.5,
-                alignItems: 'flex-start',
-              }}
-            >
-              <AspectRatio
-                ratio="1"
-                variant="soft"
-                color="primary"
-                sx={{
-                  minWidth: 32,
-                  borderRadius: '50%',
-                  '--Icon-fontSize': '16px',
-                }}
-              >
-                <div>
-                  <i data-feather="upload-cloud" />
-                </div>
-              </AspectRatio>
-              <CardContent>
-                <Typography fontSize="sm">
-                  Dashboard prototype recording.mp4
-                </Typography>
-                <Typography level="body3">16 MB</Typography>
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                  <LinearProgress
-                    variant="plain"
-                    value={80}
-                    determinate
-                    sx={{ bgcolor: 'neutral.softBg' }}
-                  />
-                  <Typography fontSize="xs">80%</Typography>
-                </Box>
-              </CardContent>
-              <IconButton
-                variant="plain"
-                color="neutral"
-                size="sm"
-                sx={{ mt: -1, mr: -1 }}
-              >
-                <i data-feather="trash-2" />
-              </IconButton>
-            </Card>
+            <DropZone />
+
+            <FileUpload
+              fileName="Tech design requirements.pdf"
+              fileSize="200 KB"
+              progress={100}
+            />
+
+            <FileUpload
+              icon={<i data-feather="film" />}
+              fileName="Dashboard prototype recording.mp4"
+              fileSize="16 MB"
+              progress={40}
+            />
+
+            <FileUpload
+              icon={<i data-feather="upload-cloud" />}
+              fileName="Dashboard prototype FINAL.fig"
+              fileSize="4.2 MB"
+              progress={80}
+            />
           </Stack>
 
           <Divider role="presentation" />
