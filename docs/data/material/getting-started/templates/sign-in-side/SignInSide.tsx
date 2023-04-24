0eb137a0787a81d118769c1fdedad831ac2600b6
naved <naved@qburst.com>
2023-04-24 15:52:58 +0530

    Image added to sign in side page of react-template

--- docs/data/material/getting-started/templates/sign-in-side/SignInSide.tsx
+++ docs/data/material/getting-started/templates/sign-in-side/SignInSide.tsx
@@ -48,7 +48,7 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
+            backgroundImage: 'url(https://source.unsplash.com/random/?signin/)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
