8583bdfe2fd6e3082139655b426a569339853991
Hassan Marzooq <marzooq703@gmail.com>
2023-04-24 15:25:59 +0530

    Added Hover Effect.js with hover Animation effect

---
+++ docs/data/material/components/paper/HoverEffect.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+
+export default function SimplePaper() {
+  const [isHovered, setIsHovered] = React.useState(false);
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        textAlign: 'center',
+        '& > :not(style)': {
+          m: 1,
+          p: 2,
+          width: 128,
+          height: 128,
+        },
+      }}
+    >
+      <Paper
+        elevation={isHovered ? 12 : 2}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {isHovered ? 'Hovering' : 'Not hovering'}
+      </Paper>
+    </Box>
+  );
+}
