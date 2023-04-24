044d2467e4b6eb2b76f9a551534e6c606079e1b1
navedqb <109583873+navedqb@users.noreply.github.com>
2023-04-24 16:32:30 +0530

    Checks fixed.
    Signed-off-by: navedqb <109583873+navedqb@users.noreply.github.com>

--- docs/data/material/getting-started/templates/blog/Blog.tsx
+++ docs/data/material/getting-started/templates/blog/Blog.tsx
@@ -33,7 +33,7 @@ const mainFeaturedPost = {
   title: 'Title of a longer featured blog post',
   description:
     "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-  image: 'https://source.unsplash.com/random',
+  image: 'https://source.unsplash.com/random/?blog/',
   imageText: 'main image description',
   linkText: 'Continue reading…',
 };
