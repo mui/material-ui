74afda92b02593d98f3e4df9b1c563b28ec833a2
Albert Yu <albert@albertyu.co>
2023-04-24 20:56:26 +0800

    Add base-vite-tailwind example repo

---
+++ examples/base-vite-tailwind/src/App.jsx
@@ -0,0 +1,36 @@
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+function App() {
+  return (
+    <div>
+      <h1>Base UI + Vite.js + Tailwind CSS</h1>
+
+      <ul>
+        <li>
+          <a href="https://mui.com/base/getting-started/overview/">Base UI</a> is a library of
+          unstyled React UI components which includes prebuilt components with production-ready
+          functionality, along with low-level hooks for transferring that functionality to other
+          components.
+        </li>
+
+        <li>
+          <a href="https://vitejs.dev/">Vite</a> is a build tool that aims to provide a faster and
+          leaner development experience for modern web projects, consisting of a dev server and a
+          build command.
+        </li>
+
+        <li>
+          <a href="https://tailwindcss.com/">Tailwind CSS</a> is a utility-first CSS framework that
+          provides low-level CSS classes that can be composed to build custom UI designs.
+        </li>
+      </ul>
+
+      <span>
+        Created with 💙 by <a href="https://mui.com">MUI</a>.
+      </span>
+    </div>
+  );
+}
+
+export default App;
