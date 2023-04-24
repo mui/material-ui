mui/material-ui

commit 62713bc20336ebcc5c86f8b51b5d7b2acdaff067

[docs] Allows to customize menu with any icon (#36408)

docs/src/modules/components/AppNavDrawerItem.js
@@ -7,35 +7,7 @@ import Collapse from '@mui/material/Collapse';
 import Chip from '@mui/material/Chip';
 import { shouldHandleLinkClick } from 'docs/src/modules/components/MarkdownLinks';
 import Link from 'docs/src/modules/components/Link';
-import ArticleRoundedIcon from '@mui/icons-material/ArticleRounded';
-import ToggleOffRoundedIcon from '@mui/icons-material/ToggleOffRounded';
-import CodeRoundedIcon from '@mui/icons-material/CodeRounded';
-import HandymanRoundedIcon from '@mui/icons-material/HandymanRounded';
-import EditRoundedIcon from '@mui/icons-material/EditRounded';
-import VisibilityRoundedIcon from '@mui/icons-material/VisibilityRounded';
-import InvertColorsRoundedIcon from '@mui/icons-material/InvertColorsRounded';
-import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
-import BookRoundedIcon from '@mui/icons-material/BookRounded';
-import ChromeReaderModeRoundedIcon from '@mui/icons-material/ChromeReaderModeRounded';
-import TableViewRoundedIcon from '@mui/icons-material/TableViewRounded';
-import ScienceIcon from '@mui/icons-material/Science';
-import DateRangeRounded from '@mui/icons-material/DateRangeRounded';
-
-const iconsMap = {
-  DescriptionIcon: ArticleRoundedIcon,
-  ToggleOnIcon: ToggleOffRoundedIcon,
-  CodeIcon: CodeRoundedIcon,
-  BuildIcon: HandymanRoundedIcon,
-  CreateIcon: EditRoundedIcon,
-  VisibilityIcon: VisibilityRoundedIcon,
-  StyleIcon: InvertColorsRoundedIcon,
-  AddIcon: AddCircleRoundedIcon,
-  BookIcon: BookRoundedIcon,
-  ReaderIcon: ChromeReaderModeRoundedIcon,
-  TableViewIcon: TableViewRoundedIcon,
-  ExperimentIcon: ScienceIcon,
-  DatePickerIcon: DateRangeRounded,
-};
+import standardNavIcons from './AppNavIcons';
 
 const Item = styled(
   function Item({ component: Component = 'div', ...props }) {
@@ -255,9 +227,8 @@ export default function AppNavDrawerItem(props) {
     }
   };
 
-  const hasIcon = icon && iconsMap[icon];
-  const IconComponent = hasIcon ? iconsMap[icon] : null;
-  const iconProps = hasIcon ? { fontSize: 'small', color: 'primary' } : {};
+  const hasIcon = icon && (typeof icon !== 'string' || !!standardNavIcons[icon]);
+  const IconComponent = typeof icon === 'string' ? standardNavIcons[icon] : icon;
   const iconElement = hasIcon ? (
     <Box
       component="span"
@@ -270,7 +241,7 @@ export default function AppNavDrawerItem(props) {
         py: '2px',
       }}
     >
-      <IconComponent {...iconProps} />
+      <IconComponent fontSize="small" color="primary" />
     </Box>
   ) : null;
 

docs/data/base/pages.ts
@@ -1,9 +1,10 @@
+import standardNavIcons from 'docs/src/modules/components/AppNavIcons';
 import pagesApi from 'docs/data/base/pagesApi';
 
 const pages = [
   {
     pathname: '/base/getting-started',
-    icon: 'DescriptionIcon',
+    icon: standardNavIcons.DescriptionIcon,
     children: [
       { pathname: '/base/getting-started/overview', title: 'Overview' },
       { pathname: '/base/getting-started/installation', title: 'Installation' },
@@ -14,7 +15,7 @@ const pages = [
   {
     pathname: '/base/react-',
     title: 'Components',
-    icon: 'ToggleOnIcon',
+    icon: standardNavIcons.ToggleOnIcon,
     children: [
       {
         pathname: '/base/components/inputs',
@@ -75,13 +76,13 @@ const pages = [
   {
     title: 'APIs',
     pathname: '/base/api',
-    icon: 'CodeIcon',
+    icon: standardNavIcons.CodeIcon,
     children: pagesApi,
   },
   {
     pathname: '/base/guides',
     title: 'How-to guides',
-    icon: 'VisibilityIcon',
+    icon: standardNavIcons.VisibilityIcon,
     children: [
       {
         pathname: '/base/guides/working-with-tailwind-css',

docs/data/joy/pages.ts
@@ -1,9 +1,10 @@
+import standardNavIcons from 'docs/src/modules/components/AppNavIcons';
 import pagesApi from './pagesApi';
 
 const pages = [
   {
     pathname: '/joy-ui/getting-started',
-    icon: 'DescriptionIcon',
+    icon: standardNavIcons.DescriptionIcon,
     children: [
       { pathname: '/joy-ui/getting-started/overview' },
       { pathname: '/joy-ui/getting-started/installation' },
@@ -25,7 +26,7 @@ const pages = [
   {
     pathname: '/joy-ui/react-',
     title: 'Components',
-    icon: 'ToggleOnIcon',
+    icon: standardNavIcons.ToggleOnIcon,
     children: [
       {
         pathname: '/joy-ui/components/inputs',
@@ -104,12 +105,12 @@ const pages = [
   {
     title: 'Component API',
     pathname: '/joy-ui/api',
-    icon: 'CodeIcon',
+    icon: standardNavIcons.CodeIcon,
     children: pagesApi,
   },
   {
     pathname: '/joy-ui/customization',
-    icon: 'CreateIcon',
+    icon: standardNavIcons.CreateIcon,
     children: [
       { pathname: '/joy-ui/customization/approaches' },
       { pathname: '/joy-ui/customization/dark-mode' },
@@ -137,7 +138,7 @@ const pages = [
   {
     pathname: '/joy-ui/guides',
     title: 'How-to guides',
-    icon: 'VisibilityIcon',
+    icon: standardNavIcons.VisibilityIcon,
     children: [
       {
         pathname: '/joy-ui/guides/overriding-component-structure',

docs/data/material/pages.ts
@@ -1,10 +1,11 @@
+import standardNavIcons from 'docs/src/modules/components/AppNavIcons';
 import pagesApi from 'docs/data/material/pagesApi';
 import { MuiPage } from 'docs/src/MuiPage';
 
 const pages: MuiPage[] = [
   {
     pathname: '/material-ui/getting-started',
-    icon: 'DescriptionIcon',
+    icon: standardNavIcons.DescriptionIcon,
     children: [
       { pathname: '/material-ui/getting-started/overview' },
       { pathname: '/material-ui/getting-started/installation' },
@@ -22,7 +23,7 @@ const pages: MuiPage[] = [
   {
     pathname: '/material-ui/react-',
     title: 'Components',
-    icon: 'ToggleOnIcon',
+    icon: standardNavIcons.ToggleOnIcon,
     children: [
       {
         pathname: '/material-ui/components/inputs',
@@ -151,12 +152,12 @@ const pages: MuiPage[] = [
   {
     title: 'Component API',
     pathname: '/material-ui/api',
-    icon: 'CodeIcon',
+    icon: standardNavIcons.CodeIcon,
     children: pagesApi,
   },
   {
     pathname: '/material-ui/customization',
-    icon: 'CreateIcon',
+    icon: standardNavIcons.CreateIcon,
     children: [
       {
         pathname: '/material-ui/customization/theme',
@@ -182,7 +183,7 @@ const pages: MuiPage[] = [
   {
     pathname: '/material-ui/guides',
     title: 'How-to guides',
-    icon: 'VisibilityIcon',
+    icon: standardNavIcons.VisibilityIcon,
     children: [
       { pathname: '/material-ui/guides/api', title: 'API design approach' },
       { pathname: '/material-ui/guides/understand-mui-packages', title: 'Understand MUI packages' },
@@ -208,7 +209,7 @@ const pages: MuiPage[] = [
   {
     pathname: '/material-ui/experimental-api',
     title: 'Experimental APIs',
-    icon: 'ExperimentIcon',
+    icon: standardNavIcons.ExperimentIcon,
     children: [
       {
         pathname: '/material-ui/experimental-api/classname-generator',
@@ -235,7 +236,7 @@ const pages: MuiPage[] = [
   },
   {
     pathname: '/material-ui/discover-more',
-    icon: 'AddIcon',
+    icon: standardNavIcons.AddIcon,
     children: [
       { pathname: '/material-ui/discover-more/showcase' },
       { pathname: '/material-ui/discover-more/related-projects' },
@@ -250,7 +251,7 @@ const pages: MuiPage[] = [
   {
     pathname: '/material-ui/migration',
     title: 'Migration',
-    icon: 'BookIcon',
+    icon: standardNavIcons.BookIcon,
     children: [
       {
         pathname: '/material-ui/migration/migration-grid-v2',
@@ -295,7 +296,7 @@ const pages: MuiPage[] = [
   {
     pathname: 'https://mui.com/store/?utm_source=docs&utm_medium=referral&utm_campaign=sidenav',
     title: 'Templates',
-    icon: 'ReaderIcon',
+    icon: standardNavIcons.ReaderIcon,
   },
 ];
 

docs/data/system/pages.ts
@@ -1,9 +1,10 @@
+import standardNavIcons from 'docs/src/modules/components/AppNavIcons';
 import pagesApi from 'docs/data/system/pagesApi';
 
 const pages = [
   {
     pathname: '/system/getting-started',
-    icon: 'DescriptionIcon',
+    icon: standardNavIcons.DescriptionIcon,
     children: [
       { pathname: '/system/getting-started/overview' },
       { pathname: '/system/getting-started/installation' },
@@ -14,7 +15,7 @@ const pages = [
   },
   {
     pathname: '/style-utilities',
-    icon: 'BuildIcon',
+    icon: standardNavIcons.BuildIcon,
     children: [
       { pathname: '/system/properties' },
       { pathname: '/system/borders' },
@@ -34,7 +35,7 @@ const pages = [
   {
     pathname: '/system/react-',
     title: 'Components',
-    icon: 'ToggleOnIcon',
+    icon: standardNavIcons.ToggleOnIcon,
     children: [
       { pathname: '/system/react-box', title: 'Box' },
       { pathname: '/system/react-container', title: 'Container' },
@@ -45,13 +46,13 @@ const pages = [
   {
     title: 'Component API',
     pathname: '/system/api',
-    icon: 'CodeIcon',
+    icon: standardNavIcons.CodeIcon,
     children: pagesApi,
   },
   {
     pathname: '/system/experimental-api',
     title: 'Experimental APIs',
-    icon: 'ExperimentIcon',
+    icon: standardNavIcons.ExperimentIcon,
     children: [
       {
         pathname: '/system/experimental-api/configure-the-sx-prop',
@@ -63,7 +64,7 @@ const pages = [
     pathname: '/system/styles',
     title: 'Styles',
     legacy: true,
-    icon: 'StyleIcon',
+    icon: standardNavIcons.StyleIcon,
     children: [
       { pathname: '/system/styles/basics' },
       { pathname: '/system/styles/advanced' },

docs/src/MuiPage.ts
@@ -1,9 +1,11 @@
+import * as React from 'react';
+
 export interface MuiPage {
   pathname: string;
   query?: object;
   children?: MuiPage[];
   disableDrawer?: boolean;
-  icon?: string;
+  icon?: string | React.ComponentType;
   /**
    * Indicates if the pages are regarding some legacy API.
    */

docs/src/modules/components/AppNavIcons.ts
@@ -1,0 +1,32 @@
+/// <reference path="../../../types/icons.d.ts"/>
+import ArticleRoundedIcon from '@mui/icons-material/ArticleRounded';
+import ToggleOffRoundedIcon from '@mui/icons-material/ToggleOffRounded';
+import CodeRoundedIcon from '@mui/icons-material/CodeRounded';
+import HandymanRoundedIcon from '@mui/icons-material/HandymanRounded';
+import EditRoundedIcon from '@mui/icons-material/EditRounded';
+import VisibilityRoundedIcon from '@mui/icons-material/VisibilityRounded';
+import InvertColorsRoundedIcon from '@mui/icons-material/InvertColorsRounded';
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
+import BookRoundedIcon from '@mui/icons-material/BookRounded';
+import ChromeReaderModeRoundedIcon from '@mui/icons-material/ChromeReaderModeRounded';
+import TableViewRoundedIcon from '@mui/icons-material/TableViewRounded';
+import ScienceIcon from '@mui/icons-material/Science';
+import DateRangeRounded from '@mui/icons-material/DateRangeRounded';
+
+const standardNavIcons = {
+  DescriptionIcon: ArticleRoundedIcon,
+  ToggleOnIcon: ToggleOffRoundedIcon,
+  CodeIcon: CodeRoundedIcon,
+  BuildIcon: HandymanRoundedIcon,
+  CreateIcon: EditRoundedIcon,
+  VisibilityIcon: VisibilityRoundedIcon,
+  StyleIcon: InvertColorsRoundedIcon,
+  AddIcon: AddCircleRoundedIcon,
+  BookIcon: BookRoundedIcon,
+  ReaderIcon: ChromeReaderModeRoundedIcon,
+  TableViewIcon: TableViewRoundedIcon,
+  ExperimentIcon: ScienceIcon,
+  DatePickerIcon: DateRangeRounded,
+};
+
+export default standardNavIcons;

docs/src/pages.ts
@@ -1,10 +1,11 @@
 import pagesApi from 'docs/src/pagesApi';
 import type { MuiPage, OrderedMuiPage } from 'docs/src/MuiPage';
+import standardNavIcons from 'docs/src/modules/components/AppNavIcons';
 
 const pages: readonly MuiPage[] = [
   {
     pathname: '/getting-started',
-    icon: 'DescriptionIcon',
+    icon: standardNavIcons.DescriptionIcon,
     children: [
       { pathname: '/getting-started/installation' },
       { pathname: '/getting-started/usage' },
@@ -19,7 +20,7 @@ const pages: readonly MuiPage[] = [
   },
   {
     pathname: '/components',
-    icon: 'ToggleOnIcon',
+    icon: standardNavIcons.ToggleOnIcon,
     children: [
       {
         pathname: '/components/inputs',
@@ -165,7 +166,7 @@ const pages: readonly MuiPage[] = [
   {
     title: 'Component API',
     pathname: '/api-docs',
-    icon: 'CodeIcon',
+    icon: standardNavIcons.CodeIcon,
     children: [
       ...pagesApi,
       {
@@ -207,7 +208,7 @@ const pages: readonly MuiPage[] = [
   },
   {
     pathname: '/system',
-    icon: 'BuildIcon',
+    icon: standardNavIcons.BuildIcon,
     children: [
       { pathname: '/system/basics' },
       { pathname: '/system/properties' },
@@ -230,7 +231,7 @@ const pages: readonly MuiPage[] = [
   },
   {
     pathname: '/customization-root',
-    icon: 'CreateIcon',
+    icon: standardNavIcons.CreateIcon,
     children: [
       {
         pathname: '/customization',
@@ -257,7 +258,7 @@ const pages: readonly MuiPage[] = [
   {
     pathname: '/guides',
     title: 'How-to guides',
-    icon: 'VisibilityIcon',
+    icon: standardNavIcons.VisibilityIcon,
     children: [
       { pathname: '/guides/api', title: 'API design approach' },
       { pathname: '/guides/understand-mui-packages', title: 'Understand MUI packages' },
@@ -282,7 +283,7 @@ const pages: readonly MuiPage[] = [
   {
     pathname: '/styles',
     title: 'Styles',
-    icon: 'StyleIcon',
+    icon: standardNavIcons.StyleIcon,
     legacy: true,
     children: [
       { pathname: '/styles/basics' },
@@ -292,7 +293,7 @@ const pages: readonly MuiPage[] = [
   },
   {
     pathname: '/discover-more',
-    icon: 'AddIcon',
+    icon: standardNavIcons.AddIcon,
     children: [
       { pathname: '/discover-more/showcase' },
       { pathname: '/discover-more/related-projects' },
@@ -307,7 +308,7 @@ const pages: readonly MuiPage[] = [
   {
     pathname: 'https://mui.com/store/',
     title: 'Templates',
-    icon: 'ReaderIcon',
+    icon: standardNavIcons.ReaderIcon,
     linkProps: {
       'data-ga-event-category': 'store',
       'data-ga-event-action': 'click',
@@ -316,7 +317,7 @@ const pages: readonly MuiPage[] = [
   },
   { pathname: '/versions', inSideNav: false },
   { pathname: '/', inSideNav: false, disableDrawer: true },
-  { pathname: '/blog', title: 'Blog', icon: 'BookIcon' },
+  { pathname: '/blog', title: 'Blog', icon: standardNavIcons.BookIcon },
 ];
 
 export type { MuiPage, OrderedMuiPage };

commit 36b9e1ebb27eeb5a4fdfea78b17c20cd2577deeb

[docs] Fix console error introduced by #36408

docs/src/modules/components/AppNavDrawerItem.js
@@ -285,7 +285,7 @@ AppNavDrawerItem.propTypes = {
   comingSoon: PropTypes.bool,
   depth: PropTypes.number.isRequired,
   href: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-  icon: PropTypes.string,
+  icon: PropTypes.elementType,
   legacy: PropTypes.bool,
   linkProps: PropTypes.object,
   newFeature: PropTypes.bool,