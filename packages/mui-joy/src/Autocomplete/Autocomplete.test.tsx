cbcc5292688cff3c4355035074cbf13354780888
Pham Duc Binh <binh1298@gmail.com>
2023-04-25 17:09:46 +0700

    fix: adjust unit test to the new reason props for Joy Autocomplete

--- packages/mui-joy/src/Autocomplete/Autocomplete.test.tsx
+++ packages/mui-joy/src/Autocomplete/Autocomplete.test.tsx
@@ -1860,7 +1860,7 @@ describe('Joy <Autocomplete />', () => {
 
       expect(handleInputChange.callCount).to.equal(1);
       expect(handleInputChange.args[0][1]).to.equal(options[0].name);
-      expect(handleInputChange.args[0][2]).to.equal('reset');
+      expect(handleInputChange.args[0][2]).to.equal('selectOption');
     });
   });
 
