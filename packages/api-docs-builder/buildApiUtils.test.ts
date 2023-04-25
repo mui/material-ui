41845e681ffbb5754a1f348ab97fe34b15b59661
Benny Joo <sldisek783@gmail.com>
2023-04-25 10:01:05 +0100

    Remove occurrences of unstyled from button doc

--- packages/api-docs-builder/buildApiUtils.test.ts
+++ packages/api-docs-builder/buildApiUtils.test.ts
@@ -128,7 +128,7 @@ describe('buildApiUtils', () => {
         path.join(process.cwd(), `/packages/mui-base/src/Button/Button.tsx`),
       );
       sinon.assert.match(info, {
-        name: 'Unstyled Button',
+        name: 'Button',
         apiPathname: '/base/react-button/components-api/#button',
         muiName: 'MuiButton',
         apiPagesDirectory: sinon.match((value) =>
@@ -176,7 +176,7 @@ describe('buildApiUtils', () => {
       if (existed) {
         expect(info.getDemos()).to.deep.equal([
           {
-            demoPageTitle: 'Unstyled Button',
+            demoPageTitle: 'Button',
             demoPathname: '/base/react-button/#hook',
           },
         ]);
