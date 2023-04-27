fed960e9cf07937cac65ae2195c8ad7fe1478dbf
Benny Joo <sldisek783@gmail.com>
2023-04-27 10:44:42 +0100

    [Switch][base] Drop component prop

--- docs/data/base/components/switch/UnstyledSwitchIntroduction.tsx
+++ docs/data/base/components/switch/UnstyledSwitchIntroduction.tsx
@@ -7,10 +7,34 @@ export default function UnstyledSwitches() {
 
   return (
     <div>
-      <Switch component={Root} {...label} defaultChecked />
-      <Switch component={Root} {...label} />
-      <Switch component={Root} {...label} defaultChecked disabled />
-      <Switch component={Root} {...label} disabled />
+      <Switch
+        slots={{
+          root: Root,
+        }}
+        {...label}
+        defaultChecked
+      />
+      <Switch
+        slots={{
+          root: Root,
+        }}
+        {...label}
+      />
+      <Switch
+        slots={{
+          root: Root,
+        }}
+        {...label}
+        defaultChecked
+        disabled
+      />
+      <Switch
+        slots={{
+          root: Root,
+        }}
+        {...label}
+        disabled
+      />
     </div>
   );
 }
