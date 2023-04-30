04bbb3e20fff5d16b9285acc3bc545d1f1403747
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-04-30 22:19:37 +0530

    [Accordion] Support component type

--- packages/mui-material/src/Accordion/Accordion.spec.tsx
+++ packages/mui-material/src/Accordion/Accordion.spec.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Accordion from '@mui/material/Accordion';
+import { expectType } from '@mui/types';
 
 function testOnChange() {
   function handleAccordionChange(event: React.SyntheticEvent, tabsValue: unknown) {}
@@ -15,3 +16,37 @@ function testOnChange() {
     <div />
   </Accordion>;
 }
+
+const CustomComponent: React.FC<{ prop1: string; prop2: number }> = function CustomComponent() {
+  return <div />;
+};
+
+const requiredProps = {
+  children: <div />,
+};
+
+const AccordionComponentTest = () => {
+  return (
+    <div>
+      <Accordion {...requiredProps} />
+      <Accordion {...requiredProps} component="legend" />
+      <Accordion
+        {...requiredProps}
+        component="legend"
+        onClick={(event) => {
+          expectType<React.MouseEvent<HTMLLegendElement, MouseEvent>, typeof event>(event);
+        }}
+      />
+
+      {/* @ts-expect-error */}
+      <Accordion {...requiredProps} component="a" incorrectAttribute="url" />
+      {/* @ts-expect-error */}
+      <Accordion {...requiredProps} component="div" href="url" />
+      <Accordion {...requiredProps} component={CustomComponent} prop1="1" prop2={12} />
+      {/* @ts-expect-error */}
+      <Accordion {...requiredProps} component={CustomComponent} prop1="1" />
+      {/* @ts-expect-error */}
+      <Accordion {...requiredProps} component={CustomComponent} prop1="1" prop2="12" />
+    </div>
+  );
+};
