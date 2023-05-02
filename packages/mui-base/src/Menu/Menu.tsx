0d3d927a5eaa45dccd3e650b854b13e5b14f5cc0
Benny Joo <sldisek783@gmail.com>
2023-05-02 11:21:20 +0100

    [Popper][base] Drop component prop (#37084)

--- packages/mui-base/src/Menu/Menu.tsx
+++ packages/mui-base/src/Menu/Menu.tsx
@@ -146,7 +146,7 @@ Menu.propTypes /* remove-proptypes */ = {
    * The props used for each slot inside the Menu.
    * @default {}
    */
-  slotProps: PropTypes.shape({
+  slotProps: PropTypes /* @typescript-to-proptypes-ignore */.shape({
     listbox: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
     root: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
   }),
