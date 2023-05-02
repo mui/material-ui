c751fbe3bbc2ca801b60e48d516922f0c3c764f4
Amal Mathew <amal.mathew@qburst.com>
2023-05-02 18:12:16 +0530

    Improve Paperbase demo

--- docs/src/pages/premium-themes/paperbase/Header.tsx
+++ docs/src/pages/premium-themes/paperbase/Header.tsx
@@ -13,6 +13,8 @@ import Tabs from '@mui/material/Tabs';
 import Toolbar from '@mui/material/Toolbar';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Content from './Content';
 
 const lightColor = 'rgba(255, 255, 255, 0.7)';
 
@@ -20,8 +22,49 @@ interface HeaderProps {
   onDrawerToggle: () => void;
 }
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <React.Fragment>
+      {value === index && (
+        <Box sx={{ backgroundColor: '#eaeff1', flex: 1, paddingTop: '24px' }}>
+          <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+          >
+            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+          </div>
+        </Box>
+      )}
+    </React.Fragment>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 export default function Header(props: HeaderProps) {
   const { onDrawerToggle } = props;
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (_: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   return (
     <React.Fragment>
@@ -106,13 +149,48 @@ export default function Header(props: HeaderProps) {
         </Toolbar>
       </AppBar>
       <AppBar component="div" position="static" elevation={0} sx={{ zIndex: 0 }}>
-        <Tabs value={0} textColor="inherit">
-          <Tab label="Users" />
-          <Tab label="Sign-in method" />
-          <Tab label="Templates" />
-          <Tab label="Usage" />
+        <Tabs value={value} onChange={handleChange} textColor="inherit">
+          <Tab label="Users" {...a11yProps(0)} />
+          <Tab label="Sign-in method" {...a11yProps(1)} />
+          <Tab label="Templates" {...a11yProps(2)} />
+          <Tab label="Usage" {...a11yProps(3)} />
         </Tabs>
       </AppBar>
+      <TabPanel value={value} index={0}>
+        <Box component="main">
+          <Content />
+        </Box>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <img
+          src="/static/images/buttons/google-signin-dark.png"
+          style={{ width: '200px', display: 'block', marginBottom: '10px' }}
+          alt="Google Signin button"
+        />
+        <img
+          src="/static/images/buttons/linkedin-signin-dark.png"
+          style={{ width: '200px', display: 'block' }}
+          alt="Linkedin Signin button"
+        />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <img
+          src="/static/images/templates/dashboard.png"
+          alt="Dashboard page"
+          style={{ width: '500px' }}
+        />
+        <img
+          src="/static/images/templates/sign-in.png"
+          alt="Login page"
+          style={{ width: '500px', marginLeft: '20px' }}
+        />
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        <p style={{ margin: '0' }}>
+          <a href="/material-ui/getting-started/usage">Learn the basics</a> of
+          working with Material UI components.
+        </p>
+      </TabPanel>
     </React.Fragment>
   );
 }
