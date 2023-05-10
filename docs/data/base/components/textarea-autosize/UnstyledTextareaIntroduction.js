b7ab6e3dff5da82e0f04905fb6c841e84786a4df
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-05-10 04:38:04 -0300

    [docs] Stray design tweaks to Base UI demos (#37003)

--- docs/data/base/components/textarea-autosize/UnstyledTextareaIntroduction.js
+++ docs/data/base/components/textarea-autosize/UnstyledTextareaIntroduction.js
@@ -37,7 +37,9 @@ export default function EmptyTextarea() {
     color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
     background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
     border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-    box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+    box-shadow: 0px 2px 24px ${
+      theme.palette.mode === 'dark' ? blue[900] : blue[100]
+    };
   
     &:hover {
       border-color: ${blue[400]};
@@ -45,7 +47,7 @@ export default function EmptyTextarea() {
   
     &:focus {
       border-color: ${blue[400]};
-      box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[500] : blue[200]};
+      box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[600] : blue[200]};
     }
   
     // firefox
@@ -55,11 +57,5 @@ export default function EmptyTextarea() {
   `,
   );
 
-  return (
-    <StyledTextarea
-      aria-label="minimum height"
-      minRows={3}
-      placeholder="Minimum 3 rows"
-    />
-  );
+  return <StyledTextarea aria-label="empty textarea" placeholder="Empty" />;
 }
