20130af71a942cfc0c6b3b6fe251c5a4fbed04ee
siriwatknp <siriwatkunaporn@gmail.com>
2023-05-10 18:40:34 +0700

    update docs

--- docs/data/material/components/icons/SvgIconChildren.js
+++ docs/data/material/components/icons/SvgIconChildren.js
@@ -4,14 +4,7 @@ import SvgIcon from '@mui/material/SvgIcon';
 export default function SvgIconChildren() {
   return (
     <SvgIcon>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
+      <svg fill="none" strokeWidth={1.5} stroke="currentColor">
         <path
           strokeLinecap="round"
           strokeLinejoin="round"
