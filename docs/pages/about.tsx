f693147a941aabbf8bdf8353b5a2937d2c26811d
Rich Bustos <92274722+richbustos@users.noreply.github.com>
2023-05-11 11:26:41 -0700

    [website] Add Rich to the 'About' page (#37221)
    Signed-off-by: Marija Najdova <mnajdova@gmail.com>
    Co-authored-by: Rich Bustos <richbustos@Kristies-MacBook-Pro.local>
    Co-authored-by: Marija Najdova <mnajdova@gmail.com>
    Co-authored-by: Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>

--- docs/pages/about.tsx
+++ docs/pages/about.tsx
@@ -453,6 +453,15 @@ const teamMembers: Array<Profile> = [
     about: 'Open-source tinkerer',
     github: 'romgrk',
   },
+  {
+    src: '/static/branding/about/rich.png',
+    name: 'Rich Bustos',
+    title: 'Developer Advocate',
+    location: 'Daly City, CA, USA',
+    locationCountry: 'us',
+    about: 'I enjoy collecting sports cards and practicing jiu-jitsu',
+    github: 'richbustos',
+  },
 ];
 
 const contributors = [
