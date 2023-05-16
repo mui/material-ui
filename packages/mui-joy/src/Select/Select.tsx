31ef9b7c26c9608327f590ae3bece1eea36218e2
Akash Agarwal <akash191095@gmail.com>
2023-05-16 17:28:05 +0530

    [Select] [joy] handle long text content

--- packages/mui-joy/src/Select/Select.tsx
+++ packages/mui-joy/src/Select/Select.tsx
@@ -20,7 +20,7 @@ import { ListOwnerState } from '../List';
 import FormControlContext from '../FormControl/FormControlContext';
 
 function defaultRenderSingleValue<TValue>(selectedOption: SelectOption<TValue> | null) {
-  return selectedOption?.label ?? '';
+  return <span>{selectedOption?.label}</span> ?? '';
 }
 
 function defaultFormValueProvider<TValue>(selectedOption: SelectOption<TValue> | null) {
@@ -198,6 +198,12 @@ const SelectButton = styled('button', {
   fontSize: 'inherit',
   color: 'inherit',
   alignSelf: 'stretch',
+  // handle long text
+  overflow: 'hidden',
+  '& > *': {
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
   // make children horizontally aligned
   display: 'flex',
   alignItems: 'center',
@@ -205,7 +211,6 @@ const SelectButton = styled('button', {
   fontFamily: 'inherit',
   cursor: 'pointer',
   whiteSpace: 'nowrap',
-  overflow: 'auto',
   ...((ownerState.value === null || ownerState.value === undefined) && {
     opacity: 'var(--Select-placeholderOpacity)',
   }),
