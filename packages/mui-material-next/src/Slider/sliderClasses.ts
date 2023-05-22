aa9a69532017336c5fbcca7803fe9d7af22d5f40
Diego Andai <diego@mui.com>
2023-05-22 17:29:46 -0400

    [Material You] fix generateUtilityClass import

--- packages/mui-material-next/src/Slider/sliderClasses.ts
+++ packages/mui-material-next/src/Slider/sliderClasses.ts
@@ -1,5 +1,7 @@
-import { unstable_generateUtilityClasses as generateUtilityClasses } from '@mui/utils';
-import generateUtilityClass from '../generateUtilityClass';
+import {
+  unstable_generateUtilityClasses as generateUtilityClasses,
+  unstable_generateUtilityClass as generateUtilityClass,
+} from '@mui/utils';
 
 export interface SliderClasses {
   /** Styles applied to the root element. */
