6d9587718c12aad02a1bd491ee18d7cbecdb404d
Salman Mohammed <mohdsalmanper@gmail.com>
2023-05-23 06:05:31 -0400

    included next font with mui5 theme.

--- examples/material-next-app-router-ts/src/components/Theme/ThemeRegistry/theme.ts
+++ examples/material-next-app-router-ts/src/components/Theme/ThemeRegistry/theme.ts
@@ -2,7 +2,19 @@
 
 import { createTheme, ThemeOptions } from '@mui/material/styles';
 
-// When needed::: first argument is needed if you have your enterprise theme, and second argument is to override your enterprise theme.
-const defaultTheme = createTheme({}, {} satisfies ThemeOptions);
+import GoogleInterFont from '@/app/fonts/fonts';
+
+// When needed::: first argument is needed if you have common enterprise theme, and second argument is to override your enterprise theme.
+// apply fonts to all other typography options like headings, subtitles, etc...
+const defaultTheme = createTheme(
+  {
+    typography: {
+      fontFamily: GoogleInterFont.style.fontFamily,
+      body1: { fontFamily: GoogleInterFont.style.fontFamily },
+      body2: { fontFamily: GoogleInterFont.style.fontFamily },
+    },
+  },
+  {} satisfies ThemeOptions,
+);
 
 export default defaultTheme;
