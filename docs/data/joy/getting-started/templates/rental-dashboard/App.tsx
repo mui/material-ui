91af78c968842a0b6db1654c33e1ef423ab5c43a
Steve Ernstberger <steve32285@gmail.com>
2023-05-25 10:04:16 -0400

    move into grid

--- docs/data/joy/getting-started/templates/rental-dashboard/App.tsx
+++ docs/data/joy/getting-started/templates/rental-dashboard/App.tsx
@@ -61,121 +61,130 @@ export default function JoyOrderDashboardTemplate() {
         <Header />
         <FirstSidebar />
         {/* <SecondSidebar /> */}
-        <Grid container spacing={2}>
-          <Grid xs={12} md={6}>
-            main
+        <Grid container spacing={2} sx={{ flexGrow: 1 }}>
+          <Grid xs={12} md={8}>
+            <Box
+              component="main"
+              className="MainContent"
+              sx={(theme) => ({
+                px: {
+                  xs: 2,
+                  md: 6,
+                },
+                pt: {
+                  xs: `calc(${theme.spacing(2)} + var(--Header-height))`,
+                  sm: `calc(${theme.spacing(2)} + var(--Header-height))`,
+                  md: 3,
+                },
+                pb: {
+                  xs: 2,
+                  sm: 2,
+                  md: 3,
+                },
+                flex: 1,
+                display: 'flex',
+                flexDirection: 'column',
+                minWidth: 0,
+                height: '100dvh',
+                gap: 1,
+              })}
+            >
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  my: 1,
+                  gap: 1,
+                  flexWrap: 'wrap',
+                  '& > *': {
+                    minWidth: 'clamp(0px, (500px - 100%) * 999, 100%)',
+                    flexGrow: 1,
+                  },
+                }}
+              >
+                <div>
+                  <Typography level="h1" fontSize="xl4">
+                    232 stays in Melbourne
+                  </Typography>
+                  <Typography level="body1" color="neutral">
+                    Book your next stay at one of our properties.
+                  </Typography>
+                </div>
+                <Box sx={{ flex: 999 }} />
+                <Box sx={{ display: 'flex', gap: 1, '& > *': { flexGrow: 1 } }}>
+                  <Button variant="outlined" color="neutral">
+                    Shared
+                  </Button>
+                  <Button
+                    variant="solid"
+                    color="primary"
+                    startDecorator={<i data-feather="star" />}
+                  >
+                    Save search
+                  </Button>
+                  <ColorSchemeToggle
+                    sx={{ ml: 'auto', display: { xs: 'none', md: 'inline-flex' } }}
+                  />
+                </Box>
+              </Box>
+              <Divider />
+              <div>
+                <Stack spacing={2} direction="row">
+                  <div>
+                    <CountrySelector />
+                  </div>
+                  <div>Jan 6 - Jan 13</div>
+
+                  <FormControl>
+                    <FormLabel>Price</FormLabel>
+                    <Input
+                      startDecorator="$"
+                      type="number"
+                      placeholder="Any price"
+                    />
+                  </FormControl>
+                  <Button
+                    variant="outlined"
+                    color="neutral"
+                    startDecorator={<i data-feather="filter" />}
+                  >
+                    More filters
+                  </Button>
+                </Stack>
+                <Stack spacing={2} direction="row">
+                  <FormControl sx={{ flex: 1 }}>
+                    {/* todo: remove label but make sure its still accessible */}
+                    <FormLabel>Search</FormLabel>
+                    <Input
+                      placeholder="Search"
+                      startDecorator={<i data-feather="search" />}
+                    />
+                  </FormControl>
+                  <Button variant="outlined" color="neutral">
+                    Clear
+                  </Button>
+                  <Button variant="solid" color="primary">
+                    Search
+                  </Button>
+                </Stack>
+              </div>
+              <Stack spacing={2} direction="row">
+                <div>Sort by date</div>
+                <div>Sort by price</div>
+              </Stack>
+              <RentalCard />
+              <RentalCard />
+              <RentalCard />
+              <RentalCard />
+              <Divider />
+              Pagination
+            </Box>
           </Grid>
-          <Grid xs={12} md={6}>
-            map
+          <Grid xs={12} md={3}>
+            <Divider orientation="vertical" />
+            <Box sx={{ backgroundColor: 'red', height: 500 }}>map</Box>
           </Grid>
         </Grid>
-        <Box
-          component="main"
-          className="MainContent"
-          sx={(theme) => ({
-            px: {
-              xs: 2,
-              md: 6,
-            },
-            pt: {
-              xs: `calc(${theme.spacing(2)} + var(--Header-height))`,
-              sm: `calc(${theme.spacing(2)} + var(--Header-height))`,
-              md: 3,
-            },
-            pb: {
-              xs: 2,
-              sm: 2,
-              md: 3,
-            },
-            flex: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            minWidth: 0,
-            height: '100dvh',
-            gap: 1,
-          })}
-        >
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              my: 1,
-              gap: 1,
-              flexWrap: 'wrap',
-              '& > *': {
-                minWidth: 'clamp(0px, (500px - 100%) * 999, 100%)',
-                flexGrow: 1,
-              },
-            }}
-          >
-            <div>
-              <Typography level="h1" fontSize="xl4">
-                232 stays in Melbourne
-              </Typography>
-              <Typography level="body1" color="neutral">
-                Book your next stay at one of our properties.
-              </Typography>
-            </div>
-            <Box sx={{ flex: 999 }} />
-            <Box sx={{ display: 'flex', gap: 1, '& > *': { flexGrow: 1 } }}>
-              <Button variant="outlined" color="neutral">
-                Shared
-              </Button>
-              <Button
-                variant="solid"
-                color="primary"
-                startDecorator={<i data-feather="star" />}
-              >
-                Save search
-              </Button>
-              <ColorSchemeToggle
-                sx={{ ml: 'auto', display: { xs: 'none', md: 'inline-flex' } }}
-              />
-            </Box>
-          </Box>
-          <Divider />
-          <div>
-            <Stack spacing={2} direction="row">
-              <div>
-                <CountrySelector />
-              </div>
-              <div>Jan 6 - Jan 13</div>
-
-              <FormControl>
-                <FormLabel>Price</FormLabel>
-                <Input startDecorator="$" type="number" placeholder="Any price" />
-              </FormControl>
-              <Button
-                variant="outlined"
-                color="neutral"
-                startDecorator={<i data-feather="filter" />}
-              >
-                More filters
-              </Button>
-            </Stack>
-            <Stack spacing={2} direction="row">
-              <FormControl sx={{ flex: 1 }}>
-                {/* todo: remove label but make sure its still accessible */}
-                <FormLabel>Search</FormLabel>
-                <Input
-                  placeholder="Search"
-                  startDecorator={<i data-feather="search" />}
-                />
-              </FormControl>
-              <Button variant="outlined" color="neutral">
-                Clear
-              </Button>
-              <Button variant="solid" color="primary">
-                Search
-              </Button>
-            </Stack>
-          </div>
-          <Stack spacing={2} direction="row">
-            <div>Sort by date</div>
-            <div>Sort by price</div>
-          </Stack>
-          <RentalCard />
-        </Box>
       </Box>
     </CssVarsProvider>
   );
