8c55e8527fc431a0e7de97533cd6a61b5c20ce66
Steve Ernstberger <steve32285@gmail.com>
2023-06-02 09:49:15 -0400

    created conversations pane

---
+++ docs/data/joy/getting-started/templates/messages/components/ConversationsPane.tsx
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import Stack from '@mui/joy/Stack';
+import Sheet from '@mui/joy/Sheet';
+import Typography from '@mui/joy/Typography';
+import { Box, Chip, IconButton, Input } from '@mui/joy';
+import List from '@mui/joy/List';
+import ConversationListItem from './ConversationListItem';
+
+export default function ConversationsPane() {
+  return (
+    <Sheet
+      sx={{
+        // backgroundColor: 'background.body',
+        // backgroundColor: 'blue',
+        borderRight: '1px solid',
+        borderColor: 'divider',
+        height: '100dvh',
+        overflowY: 'auto',
+      }}
+    >
+      <Stack
+        direction="row"
+        spacing={2}
+        alignItems="center"
+        justifyContent="space-between"
+        // sx={{ backgroundColor: 'pink' }}
+        py={3}
+        px={2.5}
+      >
+        <Typography
+          level="h1"
+          fontSize="xl2"
+          endDecorator={
+            <Chip
+              variant="outlined"
+              color="neutral"
+              size="sm"
+              sx={{
+                '--Chip-radius': '6px',
+              }}
+            >
+              40
+            </Chip>
+          }
+        >
+          Messages
+        </Typography>
+
+        <IconButton variant="outlined" aria-label="edit" color="neutral">
+          <i data-feather="edit" />
+        </IconButton>
+      </Stack>
+      <Box
+        // sx={{ backgroundColor: 'red' }}
+        px={2}
+        pb={1.5}
+      >
+        <Input
+          startDecorator={<i data-feather="search" />}
+          placeholder="Search"
+          aria-label="Search"
+        />
+      </Box>
+      {/* <Typography>conversations list</Typography> */}
+      <List
+        sx={{
+          // bgcolor: 'background.body',
+          minWidth: '100%',
+          '--ListItemDecorator-size': '48px',
+          '--ListItem-paddingLeft': '1.5rem',
+          '--ListItem-paddingRight': '1rem',
+          py: 0,
+        }}
+      >
+        <ConversationListItem selected />
+        <ConversationListItem />
+        <ConversationListItem />
+        <ConversationListItem />
+        <ConversationListItem />
+        <ConversationListItem />
+        <ConversationListItem />
+        <ConversationListItem />
+        <ConversationListItem />
+      </List>
+    </Sheet>
+  );
+}
--- docs/data/joy/getting-started/templates/messages/components/MyMessages.tsx
+++ docs/data/joy/getting-started/templates/messages/components/MyMessages.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import Grid from '@mui/joy/Grid';
-import Stack from '@mui/joy/Stack';
 import Sheet from '@mui/joy/Sheet';
-import Typography from '@mui/joy/Typography';
-import { Chip, IconButton, Input } from '@mui/joy';
-import List from '@mui/joy/List';
-import ConversationListItem from './ConversationListItem';
 import MessagesPane from './MessagesPane';
+import ConversationsPane from './ConversationsPane';
 
 const messages = [
   {
@@ -100,64 +96,7 @@ export default function MyProfile() {
     >
       <Grid container>
         <Grid xs={12} md={4}>
-          <Sheet
-            sx={{
-              // backgroundColor: 'background.body',
-              // backgroundColor: 'blue',
-              borderRight: '1px solid',
-              borderColor: 'divider',
-              height: '100dvh',
-              overflowY: 'auto',
-            }}
-          >
-            <Stack
-              direction="row"
-              spacing={2}
-              alignItems="center"
-              justifyContent="space-between"
-            >
-              <Typography
-                level="h1"
-                fontSize="xl2"
-                endDecorator={
-                  <Chip variant="outlined" color="neutral" size="sm">
-                    40
-                  </Chip>
-                }
-              >
-                Messages
-              </Typography>
-
-              <IconButton variant="outlined" aria-label="edit" color="neutral">
-                <i data-feather="edit" />
-              </IconButton>
-            </Stack>
-            <Input
-              startDecorator={<i data-feather="search" />}
-              placeholder="Search"
-              aria-label="Search"
-            />
-            {/* <Typography>conversations list</Typography> */}
-            <List
-              sx={{
-                // bgcolor: 'background.body',
-                minWidth: '100%',
-                '--ListItemDecorator-size': '48px',
-                '--ListItem-paddingLeft': '1.5rem',
-                '--ListItem-paddingRight': '1rem',
-              }}
-            >
-              <ConversationListItem selected />
-              <ConversationListItem />
-              <ConversationListItem />
-              <ConversationListItem />
-              <ConversationListItem />
-              <ConversationListItem />
-              <ConversationListItem />
-              <ConversationListItem />
-              <ConversationListItem />
-            </List>
-          </Sheet>
+          <ConversationsPane />
         </Grid>
         <Grid xs={12} md={8}>
           <MessagesPane messages={messages} />
