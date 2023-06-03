eecc30f1f93c05de7e1fd84c5cd59a7c0d1cae41
Jannis Hell <Primajin@users.noreply.github.com>
2023-06-03 16:26:55 +0200

    add same Grid from Positioned tooltips example

--- docs/data/material/components/snackbars/PositionedSnackbar.tsx
+++ docs/data/material/components/snackbars/PositionedSnackbar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Snackbar, { SnackbarOrigin } from '@mui/material/Snackbar';
 
@@ -24,54 +25,34 @@ export default function PositionedSnackbar() {
 
   const buttons = (
     <React.Fragment>
-      <Button
-        onClick={handleClick({
-          vertical: 'top',
-          horizontal: 'center',
-        })}
-      >
-        Top-Center
-      </Button>
-      <Button
-        onClick={handleClick({
-          vertical: 'top',
-          horizontal: 'left',
-        })}
-      >
-        Top-Left
-      </Button>
-      <Button
-        onClick={handleClick({
-          vertical: 'bottom',
-          horizontal: 'left',
-        })}
-      >
-        Bottom-Left
-      </Button>
-      <Button
-        onClick={handleClick({
-          vertical: 'bottom',
-          horizontal: 'center',
-        })}
-      >
-        Bottom-Center
-      </Button>
-      <Button
-        onClick={handleClick({
-          vertical: 'bottom',
-          horizontal: 'right',
-        })}
-      >
-        Bottom-Right
-      </Button>
-      <Button
-        onClick={handleClick({
-          vertical: 'top',
-          horizontal: 'right',
-        })}
-      >
-        Top-Right
-      </Button>
+      <Grid container justifyContent="center">
+        <Grid item>
+          <Button onClick={handleClick({ vertical: 'top', horizontal: 'left' })}>
+            Top-Left
+          </Button>
+          <Button onClick={handleClick({ vertical: 'top', horizontal: 'center' })}>
+            Top-Center
+          </Button>
+          <Button onClick={handleClick({ vertical: 'top', horizontal: 'right' })}>
+            Top-Right
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid container justifyContent="center">
+        <Grid item>
+          <Button onClick={handleClick({ vertical: 'bottom', horizontal: 'left' })}>
+            Bottom-Left
+          </Button>
+          <Button
+            onClick={handleClick({ vertical: 'bottom', horizontal: 'center' })}
+          >
+            Bottom-Center
+          </Button>
+          <Button onClick={handleClick({ vertical: 'bottom', horizontal: 'right' })}>
+            Bottom-Right
+          </Button>
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 
