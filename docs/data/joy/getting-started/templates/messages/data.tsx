e31fcb0cb169801289e6a27e37af2693f2e009ca
Steve Ernstberger <steve32285@gmail.com>
2023-06-07 16:09:54 -0500

    added more data

--- docs/data/joy/getting-started/templates/messages/data.tsx
+++ docs/data/joy/getting-started/templates/messages/data.tsx
@@ -138,30 +138,50 @@ export const chats: ChatProps[] = [
     messages: [
       {
         id: '1',
-        content: 'Hey, how are you?',
-        timestamp: '6 mins ago',
+        content: 'Hi Olivia, I am currently working on the project.',
+        timestamp: 'Wednesday 9:00am',
         sender: users[0],
       },
       {
         id: '2',
-        content: 'I’m good, how about you?',
-        timestamp: '5 mins ago',
+        content: 'That sounds great, Mabel! Keep up the good work.',
+        timestamp: 'Wednesday 9:10am',
         sender: 'You',
       },
       {
         id: '3',
-        timestamp: 'Just now',
-        sender: 'You',
-        content: 'Sure thing, I’ll have a look today. They’re looking great!',
+        timestamp: 'Wednesday 11:30am',
+        sender: users[0],
+        content: 'I will send the draft by end of the day.',
       },
       {
         id: '4',
-        timestamp: 'now',
-        sender: users[0],
-        content: 'Typing...',
+        timestamp: 'Wednesday 2:00pm',
+        sender: 'You',
+        content: 'Sure, I will be waiting for it.',
       },
       {
         id: '5',
+        timestamp: 'Wednesday 4:30pm',
+        sender: users[0],
+        content: 'Just a heads up, I am about to send the draft.',
+      },
+      {
+        id: '6',
+        content:
+          'Thanks Olivia! Almost there. I’ll work on making those changes you suggested and will shoot it over.',
+        timestamp: 'Thursday 10:16am',
+        sender: users[0],
+      },
+      {
+        id: '7',
+        content:
+          'Hey Olivia, I’ve finished with the requirements doc! I made some notes in the gdoc as well for Phoenix to look over.',
+        timestamp: 'Thursday 11:40am',
+        sender: users[0],
+      },
+      {
+        id: '3',
         timestamp: 'Thursday 11:40am',
         sender: users[0],
         content: 'Tech requirements.pdf',
@@ -171,6 +191,30 @@ export const chats: ChatProps[] = [
           size: '1.2 MB',
         },
       },
+      {
+        id: '8',
+        timestamp: 'Thursday 11:41am',
+        sender: 'You',
+        content: 'Awesome! Thanks. I’ll look at this today.',
+      },
+      {
+        id: '9',
+        timestamp: 'Thursday 11:44am',
+        sender: users[0],
+        content: 'No rush though — we still have to wait for Lana’s designs.',
+      },
+      {
+        id: '10',
+        timestamp: 'Today 2:20pm',
+        sender: users[0],
+        content: 'Hey Olivia, can you please review the latest design when you can?',
+      },
+      {
+        id: '11',
+        timestamp: 'Just now',
+        sender: 'You',
+        content: 'Sure thing, I’ll have a look today. They’re looking great!',
+      },
     ],
   },
   {
@@ -179,11 +223,40 @@ export const chats: ChatProps[] = [
     messages: [
       {
         id: '1',
-        content:
-          'Hey Olivia, I’ve finished with the requirements doc! I made some notes in the gdoc as well for Phoenix to look over.',
-        timestamp: '5 mins ago',
+        content: 'Hi Olivia, I am thinking about taking a vacation.',
+        timestamp: 'Wednesday 9:00am',
         sender: users[1],
-        unread: true,
+      },
+      {
+        id: '2',
+        content:
+          'That sounds like a great idea, Katherine! Any idea where you want to go?',
+        timestamp: 'Wednesday 9:05am',
+        sender: 'You',
+      },
+      {
+        id: '3',
+        content: 'I am considering a trip to the beach.',
+        timestamp: 'Wednesday 9:30am',
+        sender: users[1],
+      },
+      {
+        id: '4',
+        content: 'The beach sounds perfect this time of year!',
+        timestamp: 'Wednesday 9:35am',
+        sender: 'You',
+      },
+      {
+        id: '5',
+        content: 'Yes, I agree. It will be a much-needed break.',
+        timestamp: 'Wednesday 10:00am',
+        sender: users[1],
+      },
+      {
+        id: '6',
+        content: 'Make sure to take lots of pictures!',
+        timestamp: 'Wednesday 10:05am',
+        sender: 'You',
       },
     ],
   },
@@ -193,8 +266,7 @@ export const chats: ChatProps[] = [
     messages: [
       {
         id: '1',
-        content:
-          'Hey Olivia, I’ve finished with the requirements doc! I made some notes in the gdoc as well for Phoenix to look over.',
+        content: 'Hey!',
         timestamp: '5 mins ago',
         sender: users[2],
         unread: true,
@@ -208,10 +280,36 @@ export const chats: ChatProps[] = [
       {
         id: '1',
         content:
-          'Hey Olivia, I’ve finished with the requirements doc! I made some notes in the gdoc as well for Phoenix to look over.',
-        timestamp: '5 mins ago',
+          'Hey Olivia, I was thinking about doing some home improvement work.',
+        timestamp: 'Wednesday 9:00am',
         sender: users[3],
-        unread: true,
+      },
+      {
+        id: '2',
+        content:
+          'That sounds interesting! What kind of improvements are you considering?',
+        timestamp: 'Wednesday 9:05am',
+        sender: 'You',
+      },
+      {
+        id: '3',
+        content: 'I am planning to repaint the walls and replace the old furniture.',
+        timestamp: 'Wednesday 9:15am',
+        sender: users[3],
+      },
+      {
+        id: '4',
+        content:
+          'That will definitely give your house a fresh look. Do you need help with anything?',
+        timestamp: 'Wednesday 9:20am',
+        sender: 'You',
+      },
+      {
+        id: '5',
+        content:
+          'I might need some help with picking the right paint colors. Can we discuss this over the weekend?',
+        timestamp: 'Wednesday 9:30am',
+        sender: users[3],
       },
     ],
   },
@@ -222,8 +320,7 @@ export const chats: ChatProps[] = [
     messages: [
       {
         id: '1',
-        content:
-          'Hey Olivia, I’ve finished with the requirements doc! I made some notes in the gdoc as well for Phoenix to look over.',
+        content: 'Sup',
         timestamp: '5 mins ago',
         sender: users[4],
         unread: true,
@@ -236,10 +333,9 @@ export const chats: ChatProps[] = [
     messages: [
       {
         id: '1',
-        content:
-          'Hey Olivia, I’ve finished with the requirements doc! I made some notes in the gdoc as well for Phoenix to look over.',
+        content: 'Heyo',
         timestamp: '5 mins ago',
-        sender: users[5],
+        sender: 'You',
         unread: true,
       },
     ],
