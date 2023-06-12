15e9c2b8d4c4660f6d38900fd90861ad9b32a30a
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-06-12 12:24:47 -0300

    remove unused import

--- docs/src/modules/components/BaseUIComponents.js
+++ docs/src/modules/components/BaseUIComponents.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
