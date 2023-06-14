b81d0c53fd744ce18013ce6566bab8eddc8ad881
dvkam <82422481+dvkam@users.noreply.github.com>
2023-06-14 20:02:51 +0200

    Update examples/base-vite-react-tailwind-ts/src/main.tsx
    Co-authored-by: Olivier Tassinari <olivier.tassinari@gmail.com>
    Signed-off-by: dvkam <82422481+dvkam@users.noreply.github.com>

--- examples/base-vite-react-tailwind-ts/src/main.tsx
+++ examples/base-vite-react-tailwind-ts/src/main.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
