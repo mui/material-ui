183fc3f52cd81e2700384a8ae7ca74542d3772ad
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2023-06-14 19:09:13 +0530

    fix imports and rename files

--- packages/mui-material/test/typescript/moduleAugmentation/TooltipSlotSxProps.spec.tsx
+++ packages/mui-material/test/typescript/moduleAugmentation/TooltipSlotSxProps.spec.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import { PaletteColor, Tooltip, Button } from '@mui/material';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import { PaletteColor } from '@mui/material/styles';
 
-declare module '@mui/material' {
+declare module '@mui/material/styles' {
   interface Palette {
     custom: PaletteColor;
   }
