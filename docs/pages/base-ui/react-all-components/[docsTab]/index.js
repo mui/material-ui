4b51d0df2dc373df4086726b16677b368cdc53c4
MUK-Dev <manian1221@gmail.com>
2023-06-18 18:49:02 +0500

    [docs] running yarn docs:api adds one import which is unused so ignoring that for now

--- docs/pages/base-ui/react-all-components/[docsTab]/index.js
+++ docs/pages/base-ui/react-all-components/[docsTab]/index.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import MarkdownDocs from 'docs/src/modules/components/MarkdownDocsV2';
 import AppFrame from 'docs/src/modules/components/AppFrame';
 import * as pageProps from 'docs/data/base/components/all-components/all-components.md?@mui/markdown';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import mapApiPageTranslations from 'docs/src/modules/utils/mapApiPageTranslations';
 
 export default function Page(props) {
   const { userLanguage, ...other } = props;
