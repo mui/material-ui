45298e54287a22f8bc7bbd5ae3c9c9e92b1722b4
Steve Ernstberger <steve32285@gmail.com>
2023-06-18 20:53:59 -0600

    [docs][joy] Add a messages template (#37546)

--- docs/data/joy/getting-started/templates/messages/components/ColorSchemeToggle.tsx
+++ docs/data/joy/getting-started/templates/messages/components/ColorSchemeToggle.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { useColorScheme } from '@mui/joy/styles';
-import GlobalStyles from '@mui/joy/GlobalStyles';
 import IconButton, { IconButtonProps } from '@mui/joy/IconButton';
-import Sheet from '@mui/joy/Sheet';
-import MuiLogo from './MuiLogo';
-import { toggleSidebar } from '../utils';
 
-function ColorSchemeToggle({ onClick, sx, ...props }: IconButtonProps) {
+export default function ColorSchemeToggle({
+  onClick,
+  sx,
+  ...props
+}: IconButtonProps) {
   const { mode, setMode } = useColorScheme();
   const [mounted, setMounted] = React.useState(false);
   React.useEffect(() => {
@@ -26,6 +26,7 @@ function ColorSchemeToggle({ onClick, sx, ...props }: IconButtonProps) {
   }
   return (
     <IconButton
+      id="toggle-mode"
       size="sm"
       variant="outlined"
       color="neutral"
@@ -53,46 +54,5 @@ function ColorSchemeToggle({ onClick, sx, ...props }: IconButtonProps) {
       <i data-feather="moon" />
       <i data-feather="sun" />
     </IconButton>
-  );
-}
-
-export default function Header() {
-  return (
-    <Sheet
-      sx={{
-        display: { xs: 'flex', md: 'none' },
-        alignItems: 'center',
-        position: 'fixed',
-        top: 0,
-        width: '100vw',
-        height: 'var(--Header-height)',
-        zIndex: 9995,
-        py: 1,
-        px: 2,
-        gap: 1,
-        boxShadow: 'sm',
-      }}
-    >
-      <GlobalStyles
-        styles={(theme) => ({
-          ':root': {
-            '--Header-height': '52px',
-            [theme.breakpoints.up('md')]: {
-              '--Header-height': '0px',
-            },
-          },
-        })}
-      />
-      <IconButton
-        onClick={() => toggleSidebar()}
-        variant="outlined"
-        color="neutral"
-        size="sm"
-      >
-        <i data-feather="menu" />
-      </IconButton>
-      <MuiLogo variant="plain" sx={{ boxShadow: 'none', mr: 'auto' }} />
-      <ColorSchemeToggle id={undefined} />
-    </Sheet>
   );
 }
