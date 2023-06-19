2fe889da4539bb50d27b06c9cade058833abe6a7
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-06-19 15:41:06 -0400

    remove system and tailwind examples

--- examples/base-cra-system-ts/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement!);
-
-root.render(<App />);
