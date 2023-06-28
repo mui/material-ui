1f07c5775099018be1847b6f4eb39a7ee78258a4
zanivan <victorzanivan@gmail.com>
2023-06-28 14:12:18 -0300

    added Box with background.body

--- docs/pages/experiments/joy/global-variants.tsx
+++ docs/pages/experiments/joy/global-variants.tsx
@@ -61,158 +61,160 @@ function ColorSchemePicker() {
 export default function GlobalVariantsPage() {
   return (
     <CssVarsProvider defaultMode="system">
-      <Box
-        sx={{
-          position: 'sticky',
-          top: 0,
-          minHeight: 56,
-          borderBottom: '1px solid',
-          borderColor: 'neutral.outlinedBorder',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          bgcolor: 'background.body',
-        }}
-      >
-        <ColorSchemePicker />
-      </Box>
+      <Box sx={{ bgcolor: 'background.body' }}>
+        <Box
+          sx={{
+            position: 'sticky',
+            top: 0,
+            minHeight: 56,
+            borderBottom: '1px solid',
+            borderColor: 'neutral.outlinedBorder',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            bgcolor: 'background.body',
+          }}
+        >
+          <ColorSchemePicker />
+        </Box>
 
-      <Container
-        sx={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(5, 1fr)',
-          '& > div': {
-            justifySelf: 'center',
-            padding: '1rem',
-          },
-        }}
-      >
-        {(['plain', 'outlined', 'soft', 'solid'] as const).map((variant) =>
-          (['primary', 'neutral', 'danger', 'success', 'warning'] as const).map((color) => (
-            <div>
-              <Button key={`${variant}-${color}`} variant={variant} color={color}>
-                Button
-              </Button>
-              <Button
-                key={`${variant}-${color}`}
-                variant={variant}
-                color={color}
-                disabled
-                sx={{ ml: '0.5rem' }}
-              >
-                Disabled
-              </Button>
-            </div>
-          )),
-        )}
-      </Container>
-      <Container
-        sx={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(5, 1fr)',
-          '& > div': {
-            justifyItems: 'center',
-            padding: '1rem',
-          },
-        }}
-      >
-        {(['plain', 'outlined', 'soft', 'solid'] as const).map((variant) =>
-          (['primary', 'neutral', 'danger', 'success', 'warning'] as const).map((color) => (
-            <div>
-              <Select
-                key={`${variant}-${color}`}
-                placeholder="select one"
-                variant={variant}
-                color={color}
-              >
-                <Option value="1">Option 1</Option>
-                <Option value="2">Option 2</Option>
-                <Option value="3">Option 3</Option>
-              </Select>
-            </div>
-          )),
-        )}
-      </Container>
-      <Container
-        maxWidth="xl"
-        sx={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fill, minmax(min(100%, 360px), 1fr))',
-          '& > div': {
-            justifyItems: 'center',
-            padding: '1rem',
-          },
-        }}
-      >
-        {(['soft', 'solid'] as const).map((variant) =>
-          (['primary', 'neutral', 'danger', 'success', 'warning'] as const).map((color) => (
-            <div>
-              <Card variant={variant} color={color} invertedColors>
-                <Chip size="sm" variant="outlined" onClick={() => {}}>
-                  MOST POPULAR
-                </Chip>
-                {/* TODO: level="text-lg" */}
-                <Typography variant="soft" level="h2" fontSize="xl2" sx={{ borderRadius: 'xs' }}>
-                  Unlimited
-                </Typography>
-                <Divider inset="none" />
-                <List
-                  component="div"
-                  size="sm"
-                  sx={{
-                    display: 'grid',
-                    gridTemplateColumns: '1fr 1fr',
-                    '--ListItem-radius': '20px',
-                  }}
+        <Container
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(5, 1fr)',
+            '& > div': {
+              justifySelf: 'center',
+              padding: '1rem',
+            },
+          }}
+        >
+          {(['plain', 'outlined', 'soft', 'solid'] as const).map((variant) =>
+            (['primary', 'neutral', 'danger', 'success', 'warning'] as const).map((color) => (
+              <div>
+                <Button key={`${variant}-${color}`} variant={variant} color={color}>
+                  Button
+                </Button>
+                <Button
+                  key={`${variant}-${color}`}
+                  variant={variant}
+                  color={color}
+                  disabled
+                  sx={{ ml: '0.5rem' }}
                 >
-                  <ListItemButton variant="soft">
-                    <ListItemDecorator>
-                      <Check />
-                    </ListItemDecorator>
-                    Virtual Credit Cards
-                  </ListItemButton>
-                  <ListItemButton>
-                    <ListItemDecorator>
-                      <Check />
-                    </ListItemDecorator>
-                    Financial Analytics
-                  </ListItemButton>
-                  <ListItemButton>
-                    <ListItemDecorator>
-                      <Check />
-                    </ListItemDecorator>
-                    Checking Account
-                  </ListItemButton>
-                  <ListItemButton>
-                    <ListItemDecorator>
-                      <Check />
-                    </ListItemDecorator>
-                    API Integration
-                  </ListItemButton>
-                  <ListItemButton>
-                    <ListItemDecorator>
-                      <Check />
-                    </ListItemDecorator>
-                    Cancel Anytime
-                  </ListItemButton>
-                </List>
-                <Divider inset="none" />
-                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                  <Typography level="h4" sx={{ mr: 'auto' }}>
-                    5.990€{' '}
-                    <Typography fontSize="sm" textColor="text.tertiary">
-                      / month
-                    </Typography>
+                  Disabled
+                </Button>
+              </div>
+            )),
+          )}
+        </Container>
+        <Container
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(5, 1fr)',
+            '& > div': {
+              justifyItems: 'center',
+              padding: '1rem',
+            },
+          }}
+        >
+          {(['plain', 'outlined', 'soft', 'solid'] as const).map((variant) =>
+            (['primary', 'neutral', 'danger', 'success', 'warning'] as const).map((color) => (
+              <div>
+                <Select
+                  key={`${variant}-${color}`}
+                  placeholder="select one"
+                  variant={variant}
+                  color={color}
+                >
+                  <Option value="1">Option 1</Option>
+                  <Option value="2">Option 2</Option>
+                  <Option value="3">Option 3</Option>
+                </Select>
+              </div>
+            )),
+          )}
+        </Container>
+        <Container
+          maxWidth="xl"
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(min(100%, 360px), 1fr))',
+            '& > div': {
+              justifyItems: 'center',
+              padding: '1rem',
+            },
+          }}
+        >
+          {(['soft', 'solid'] as const).map((variant) =>
+            (['primary', 'neutral', 'danger', 'success', 'warning'] as const).map((color) => (
+              <div>
+                <Card variant={variant} color={color} invertedColors>
+                  <Chip size="sm" variant="outlined" onClick={() => {}}>
+                    MOST POPULAR
+                  </Chip>
+                  {/* TODO: level="text-lg" */}
+                  <Typography variant="soft" level="h2" fontSize="xl2" sx={{ borderRadius: 'xs' }}>
+                    Unlimited
                   </Typography>
-                  <Button endDecorator={<KeyboardArrowRight />} sx={{ borderRadius: '40px' }}>
-                    Start now
-                  </Button>
-                </Box>
-              </Card>
-            </div>
-          )),
-        )}
-      </Container>
+                  <Divider inset="none" />
+                  <List
+                    component="div"
+                    size="sm"
+                    sx={{
+                      display: 'grid',
+                      gridTemplateColumns: '1fr 1fr',
+                      '--ListItem-radius': '20px',
+                    }}
+                  >
+                    <ListItemButton variant="soft">
+                      <ListItemDecorator>
+                        <Check />
+                      </ListItemDecorator>
+                      Virtual Credit Cards
+                    </ListItemButton>
+                    <ListItemButton>
+                      <ListItemDecorator>
+                        <Check />
+                      </ListItemDecorator>
+                      Financial Analytics
+                    </ListItemButton>
+                    <ListItemButton>
+                      <ListItemDecorator>
+                        <Check />
+                      </ListItemDecorator>
+                      Checking Account
+                    </ListItemButton>
+                    <ListItemButton>
+                      <ListItemDecorator>
+                        <Check />
+                      </ListItemDecorator>
+                      API Integration
+                    </ListItemButton>
+                    <ListItemButton>
+                      <ListItemDecorator>
+                        <Check />
+                      </ListItemDecorator>
+                      Cancel Anytime
+                    </ListItemButton>
+                  </List>
+                  <Divider inset="none" />
+                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Typography level="h4" sx={{ mr: 'auto' }}>
+                      5.990€{' '}
+                      <Typography fontSize="sm" textColor="text.tertiary">
+                        / month
+                      </Typography>
+                    </Typography>
+                    <Button endDecorator={<KeyboardArrowRight />} sx={{ borderRadius: '40px' }}>
+                      Start now
+                    </Button>
+                  </Box>
+                </Card>
+              </div>
+            )),
+          )}
+        </Container>
+      </Box>
     </CssVarsProvider>
   );
 }
