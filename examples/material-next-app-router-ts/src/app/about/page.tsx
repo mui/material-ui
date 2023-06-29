3ed7d9449098a98c04ed73d925bf7a312dcf33f4
Albert Yu <albert@albertyu.co>
2023-06-29 22:25:10 +0800

    Update app router example repo

--- examples/material-next-app-router-ts/src/app/about/page.tsx
+++ examples/material-next-app-router-ts/src/app/about/page.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Copyright from '@/components/CopyRight';
-import ProTip from '@/components/ProTip';
+import Box from '@mui/material/Box';
 import Link from 'next/link';
+import ProTip from '@/components/ProTip';
+import Copyright from '@/components/CopyRight';
 
-export default function HomePage() {
+export default function AboutPage() {
   return (
     <Container maxWidth="lg">
       <Box
@@ -22,9 +22,9 @@ export default function HomePage() {
           Material UI - Next.js example using App Router in TypeScript
         </Typography>
         <Typography variant="h4" component="h3" gutterBottom>
-          This the Home Page
+          This the About Page
         </Typography>
-        <Link href="/about">Go to the about page</Link>
+        <Link href="/">Go to the main page</Link>
         <ProTip />
         <Copyright />
       </Box>
--- examples/material-next-app-router-ts/src/components/ThemeRegistry/theme.ts
+++ examples/material-next-app-router-ts/src/components/ThemeRegistry/theme.ts
@@ -1,19 +1,13 @@
-import { createTheme } from '@mui/material/styles';
+'use client';
 
+import { createTheme } from '@mui/material/styles';
+import { green } from '@mui/material/colors';
+
+// When needed::: first argument is needed if you have common enterprise theme, and second argument is to override your enterprise theme.
+// apply fonts to all other typography options like headings, subtitles, etc...
 const defaultTheme = createTheme({
   palette: {
-    mode: 'light',
-  },
-  components: {
-    MuiAlert: {
-      styleOverrides: {
-        root: ({ ownerState }) => ({
-          ...(ownerState.severity === 'info' && {
-            backgroundColor: '#60a5fa',
-          }),
-        }),
-      },
-    },
+    primary: green,
   },
 });
 
