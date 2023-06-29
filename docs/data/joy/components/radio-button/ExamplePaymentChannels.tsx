184193beb930567be438eaf6bbc2dc4ae70b9dcd
zanivan <victorzanivan@gmail.com>
2023-06-29 16:42:27 -0300

    tweaks on radio demos

--- docs/data/joy/components/radio-button/ExamplePaymentChannels.tsx
+++ docs/data/joy/components/radio-button/ExamplePaymentChannels.tsx
@@ -23,17 +23,17 @@ export default function ExamplePaymentChannels() {
       >
         <Typography
           id="example-payment-channel-label"
-          level="body-xs"
-          textTransform="uppercase"
+          level="title-md"
+          textColor={'text.secondary'}
           fontWeight="xl"
-          sx={{ letterSpacing: '0.1em' }}
         >
           Pay with
         </Typography>
         <Switch
           component="label"
           size="sm"
-          endDecorator="Row view"
+          startDecorator="List"
+          endDecorator="Row"
           checked={orientation === 'horizontal'}
           onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
             setOrientation(event.target.checked ? 'horizontal' : 'vertical')
