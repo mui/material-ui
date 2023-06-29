193523383f7fa477318ea458de6fb4ecf7977bac
Diego Andai <diego@mui.com>
2023-06-29 16:22:34 -0400

    Restrain useSliderElementsOverlap to 2 elements

--- packages/mui-material-next/src/Slider/useSliderElementsOverlap.ts
+++ packages/mui-material-next/src/Slider/useSliderElementsOverlap.ts
@@ -2,9 +2,11 @@ import * as React from 'react';
 import { Axis } from '@mui/base';
 import { unstable_useEnhancedEffect as useEnhancedEffect } from '@mui/utils';
 
-function getSliderElementsOverlap(elements: HTMLElement[], axis: Axis, margin: number) {
-  if (elements.length === 2) {
-    const [firstElement, secondElement] = elements;
+type SliderElements = [HTMLElement?, HTMLElement?];
+
+function getSliderElementsOverlap(elements: SliderElements, axis: Axis, margin: number) {
+  const [firstElement, secondElement] = elements;
+  if (firstElement && secondElement) {
     const firstElementRect = firstElement.getBoundingClientRect();
     const secondElementRect = secondElement.getBoundingClientRect();
     if (axis === 'horizontal') {
@@ -19,11 +21,11 @@ function getSliderElementsOverlap(elements: HTMLElement[], axis: Axis, margin: n
 }
 
 function useElementsOverlap(axis: Axis, margin: number = 0) {
-  const elementsRefList = React.useRef<HTMLElement[]>([]);
+  const elementsRefList = React.useRef<SliderElements>([]);
   const [elementsOverlap, setThumbsOverlap] = React.useState(false);
 
   const setRef = React.useCallback((elementIndex: number, ref: HTMLElement) => {
-    if (!!ref && elementsRefList.current[elementIndex] !== ref) {
+    if (elementIndex <= 1 && !!ref && elementsRefList.current[elementIndex] !== ref) {
       elementsRefList.current[elementIndex] = ref;
     }
   }, []);
@@ -67,6 +69,9 @@ export default function useSliderElementsOverlap(axis: Axis) {
     getIsOverlapping: getIsValueLabelOverlapping,
     onMove: onValueLabelMove,
   } = useElementsOverlap(axis, -12);
+  // ValueLabel -12px margin is required due to how its "inverted water drop"
+  // shape is built with CSS. Might want to allow this to be configurable
+  // in the future.
 
   const onThumbMoved = React.useCallback(() => {
     onThumbMove();
