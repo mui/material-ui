156c0a33d2bf2b4f6bda27f10394ddbf60634365
Albert Yu <albert@albertyu.co>
2023-06-30 14:07:58 +0800

    Build out RootLayout with Material UI components

--- examples/material-next-app-router-ts/src/app/about/page.tsx
+++ examples/material-next-app-router-ts/src/app/about/page.tsx
@@ -1,32 +1,22 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from 'next/link';
-import ProTip from '@/components/ProTip';
-import Copyright from '@/components/CopyRight';
+import Typography from '@mui/material/Typography';
 
 export default function AboutPage() {
   return (
-    <Container maxWidth="lg">
+    <Container>
       <Box
         sx={{
-          my: 4,
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
         }}
       >
-        <Typography variant="h4" component="h3" gutterBottom>
-          Material UI - Next.js example using App Router in TypeScript
+        <Typography variant="body1" gutterBottom>
+          About Page
         </Typography>
-        <Typography variant="h4" component="h3" gutterBottom>
-          This the About Page
-        </Typography>
-        <Link href="/">Go to the main page</Link>
-        <ProTip />
-        <Copyright />
       </Box>
     </Container>
   );
