49b78fc1746c21902c4593b14bb2989a05e152b2
Albert Yu <albert@albertyu.co>
2023-06-30 14:07:58 +0800

    Build out RootLayout with Material UI components

--- examples/material-next-app-router-ts/src/app/layout.tsx
+++ examples/material-next-app-router-ts/src/app/layout.tsx
@@ -1,5 +1,18 @@
 import * as React from 'react';
 import { Inter } from 'next/font/google';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
 
 export const metadata = {
@@ -9,11 +22,72 @@ export const metadata = {
 
 const inter = Inter({ subsets: ['latin'] });
 
+const DRAWER_WIDTH = 240;
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeRegistry>{children}</ThemeRegistry>
+        <ThemeRegistry>
+          <AppBar
+            position="fixed"
+            sx={{ width: `calc(100% - ${DRAWER_WIDTH}px)`, ml: `${DRAWER_WIDTH}px` }}
+          >
+            <Toolbar>
+              <Typography variant="h6" noWrap component="div">
+                Next.js 13 + Material UI
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            sx={{
+              width: DRAWER_WIDTH,
+              flexShrink: 0,
+              '& .MuiDrawer-paper': {
+                width: DRAWER_WIDTH,
+                boxSizing: 'border-box',
+              },
+            }}
+            variant="permanent"
+            anchor="left"
+          >
+            <Toolbar />
+            <Divider />
+            <List>
+              {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+            <Divider />
+            <List>
+              {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Drawer>
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              bgcolor: 'background.default',
+              ml: `${DRAWER_WIDTH}px`,
+              mt: ['48px', '56px', '64px'],
+              p: 3,
+            }}
+          >
+            {children}
+          </Box>
+        </ThemeRegistry>
       </body>
     </html>
   );
--- examples/material-next-app-router-ts/src/app/page.tsx
+++ examples/material-next-app-router-ts/src/app/page.tsx
@@ -2,31 +2,21 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Copyright from '@/components/CopyRight';
-import ProTip from '@/components/ProTip';
-import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <Container maxWidth="lg">
+    <Container>
       <Box
         sx={{
-          my: 4,
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
         }}
       >
-        <Typography variant="h4" component="h3" gutterBottom>
-          Material UI - Next.js example using App Router in TypeScript
+        <Typography variant="body1" gutterBottom>
+          Home Page
         </Typography>
-        <Typography variant="h4" component="h3" gutterBottom>
-          This the Home Page
-        </Typography>
-        <Link href="/about">Go to the about page</Link>
-        <ProTip />
-        <Copyright />
       </Box>
     </Container>
   );
