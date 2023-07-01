513562a041d0894789015d23979c551b8d034516
Albert Yu <albert@albertyu.co>
2023-07-01 22:06:18 +0800

    Start Base UI example

--- examples/base-next-app-router-tailwind-ts/src/app/layout.tsx
+++ examples/base-next-app-router-tailwind-ts/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { IBM_Plex_Sans } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 
-const font = IBM_Plex_Sans({ weight: ['400', '500', '700'], subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
