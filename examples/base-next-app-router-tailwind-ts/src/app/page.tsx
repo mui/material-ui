574fff060e28b0bc2b15ccc72d498a9c9c4f2af9
Albert Yu <albert@albertyu.co>
2023-07-01 22:06:18 +0800

    Start Base UI example

---
+++ examples/base-next-app-router-tailwind-ts/src/app/page.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Hello World
+    </main>
+  );
+}
