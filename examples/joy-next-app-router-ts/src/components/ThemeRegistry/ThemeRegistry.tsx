4a5633f4cdc124ce45777382eabe1057d9fa47f6
Albert Yu <albert@albertyu.co>
2023-07-01 18:59:39 +0800

    Add Joy UI with Next 13 App Router example

--- examples/joy-next-app-router-ts/src/components/ThemeRegistry/ThemeRegistry.tsx
+++ examples/joy-next-app-router-ts/src/components/ThemeRegistry/ThemeRegistry.tsx
@@ -3,12 +3,11 @@ import * as React from 'react';
 import { CssVarsProvider } from '@mui/joy/styles';
 import CssBaseline from '@mui/joy/CssBaseline';
 import NextAppDirEmotionCacheProvider from './EmotionCache';
-import theme from './theme';
 
 export default function ThemeRegistry({ children }: { children: React.ReactNode }) {
   return (
     <NextAppDirEmotionCacheProvider options={{ key: 'joy' }}>
-      <CssVarsProvider theme={theme}>
+      <CssVarsProvider>
         <CssBaseline />
         {children}
       </CssVarsProvider>
