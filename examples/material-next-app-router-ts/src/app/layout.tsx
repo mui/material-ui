03d5b61a4d3b67c0a97876b6a87361ac5a10bd8c
Albert Yu <albert@albertyu.co>
2023-07-01 19:55:14 +0800

    Update material example

--- examples/material-next-app-router-ts/src/app/layout.tsx
+++ examples/material-next-app-router-ts/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Inter } from 'next/font/google';
+import { Roboto } from 'next/font/google';
 import Link from 'next/link';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -26,7 +26,7 @@ export const metadata = {
   description: 'Next.js App Router + Material UI v5',
 };
 
-const inter = Inter({ subsets: ['latin'] });
+const roboto = Roboto({ weight: ['300', '400', '500', '700'], subsets: ['latin'] });
 
 const DRAWER_WIDTH = 240;
 
@@ -45,7 +45,7 @@ const PLACEHOLDER_LINKS = [
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <ThemeRegistry>
           <AppBar position="fixed" sx={{ zIndex: 2000 }}>
             <Toolbar sx={{ backgroundColor: 'background.paper' }}>
