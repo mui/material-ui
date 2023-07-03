19d947e54ef51e07f59e6b64fe870c121eedbb86
siriwatknp <siriwatkunaporn@gmail.com>
2023-07-03 15:36:09 +0700

    update TabsUsage

--- docs/data/joy/components/tabs/TabsUsage.js
+++ docs/data/joy/components/tabs/TabsUsage.js
@@ -39,8 +39,28 @@ export default function TabsUsage() {
           defaultValue: 'horizontal',
         },
         {
+          propName: 'indicatorPlacement',
+          knob: 'radio',
+          options: ['top', 'bottom', 'left', 'right'],
+          codeBlockDisplay: false,
+        },
+        {
           formLabel: 'TabList disable underline',
           propName: 'disableUnderline',
+          knob: 'switch',
+          defaultValue: false,
+          codeBlockDisplay: false,
+        },
+        {
+          formLabel: 'Disable Tab indicator',
+          propName: 'disableIndicator',
+          knob: 'switch',
+          defaultValue: false,
+          codeBlockDisplay: false,
+        },
+        {
+          formLabel: 'Tab indicator inset',
+          propName: 'indicatorInset',
           knob: 'switch',
           defaultValue: false,
           codeBlockDisplay: false,
@@ -56,17 +76,52 @@ export default function TabsUsage() {
           `<TabList${props.disableUnderline ? ` disableUnderline` : ''}>
     <Tab${props.variant ? ` variant="${props.variant}"` : ''}${
             props.color ? ` color="${props.color}"` : ''
+          }${
+            props.indicatorPlacement
+              ? ` indicatorPlacement="${props.indicatorPlacement}"`
+              : ''
+          }${props.disableIndicator ? ` disableIndicator` : ''}${
+            props.indicatorInset ? ` indicatorInset` : ''
           }>...</Tab>
   </TabList>
   <TabPanel>...</TabPanel>`,
         )
       }
-      renderDemo={({ color, variant, disableUnderline, ...props }) => (
+      renderDemo={({
+        color,
+        variant,
+        disableUnderline,
+        disableIndicator,
+        indicatorPlacement,
+        indicatorInset,
+        ...props
+      }) => (
         <Tabs {...props} value={index} onChange={(event, value) => setIndex(value)}>
           <TabList disableUnderline={disableUnderline}>
-            <Tab {...(index === 0 && { color, variant })}>Tab A</Tab>
-            <Tab {...(index === 1 && { color, variant })}>Tab B</Tab>
-            <Tab {...(index === 2 && { color, variant })}>Tab C</Tab>
+            <Tab
+              indicatorInset={indicatorInset}
+              indicatorPlacement={indicatorPlacement}
+              disableIndicator={disableIndicator}
+              {...(index === 0 && { color, variant })}
+            >
+              Tab A
+            </Tab>
+            <Tab
+              indicatorInset={indicatorInset}
+              indicatorPlacement={indicatorPlacement}
+              disableIndicator={disableIndicator}
+              {...(index === 1 && { color, variant })}
+            >
+              Tab B
+            </Tab>
+            <Tab
+              indicatorInset={indicatorInset}
+              indicatorPlacement={indicatorPlacement}
+              disableIndicator={disableIndicator}
+              {...(index === 2 && { color, variant })}
+            >
+              Tab C
+            </Tab>
           </TabList>
           <TabPanel value={0}>Content of Tab A</TabPanel>
           <TabPanel value={1}>Content of Tab B</TabPanel>
