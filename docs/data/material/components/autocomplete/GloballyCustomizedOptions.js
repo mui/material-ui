3229583b3831e252c2ef615982867c4e7f8a3a04
Nicolas Theodarus <nicolastheodarus@gmail.com>
2023-07-03 16:14:35 +0200

    improve docs

--- docs/data/material/components/autocomplete/GloballyCustomizedOptions.js
+++ docs/data/material/components/autocomplete/GloballyCustomizedOptions.js
@@ -5,7 +5,7 @@ import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import { createTheme, useTheme, ThemeProvider } from '@mui/material/styles';
 
-// Theme.js
+// Theme.ts
 const customTheme = (outerTheme) =>
   createTheme({
     palette: {
