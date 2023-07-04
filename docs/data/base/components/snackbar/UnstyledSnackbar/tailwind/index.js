e8a054383ea7b69e34353be008523c6fdeb40058
Marija Najdova <mnajdova@gmail.com>
2023-07-04 14:58:53 +0200

    Remove unnecessary React.Fragment

--- docs/data/base/components/snackbar/UnstyledSnackbar/tailwind/index.js
+++ docs/data/base/components/snackbar/UnstyledSnackbar/tailwind/index.js
@@ -29,26 +29,24 @@ export default function UseSnackbar() {
   };
 
   return (
-    <React.Fragment>
-      <div className={`${isDarkMode ? 'dark' : ''}`}>
-        <button
-          className="text-sm box-border rounded-xl font-semibold px-3.5 py-2.5 leading-normal bg-transparent border border-solid border-slate-200 dark:border-slate-800 text-slate-900 dark:text-slate-100 hover: bg-slate-50 hover:dark:bg-slate-800 hover:border-slate-300 dark:hover:border-slate-600 focus-visible:border-purple-500 focus-visible:hover:border-purple-500 focus-visible:dark:hover:border-purple-500 focus-visible:outline-0 focus-visible:shadow-outline-purple"
-          type="button"
-          onClick={handleOpen}
-        >
-          Open snackbar
-        </button>
-        {open ? (
-          <ClickAwayListener onClickAway={onClickAway}>
-            <div
-              className="fixed z-50 flex right-4 bottom-4 left-auto justify-start rounded-lg bg-slate-50 dark:bg-slate-900 p-3 leading-normal animation-appear transition-transform border border-solid border-slate-200 dark:border-slate-700 min-w-snackbar max-w-snackbar shadow-md text-slate-900 dark:text-slate-50"
-              {...getRootProps()}
-            >
-              Hello World
-            </div>
-          </ClickAwayListener>
-        ) : null}
-      </div>
-    </React.Fragment>
+    <div className={`${isDarkMode ? 'dark' : ''}`}>
+      <button
+        className="text-sm box-border rounded-xl font-semibold px-3.5 py-2.5 leading-normal bg-transparent border border-solid border-slate-200 dark:border-slate-800 text-slate-900 dark:text-slate-100 hover: bg-slate-50 hover:dark:bg-slate-800 hover:border-slate-300 dark:hover:border-slate-600 focus-visible:border-purple-500 focus-visible:hover:border-purple-500 focus-visible:dark:hover:border-purple-500 focus-visible:outline-0 focus-visible:shadow-outline-purple"
+        type="button"
+        onClick={handleOpen}
+      >
+        Open snackbar
+      </button>
+      {open ? (
+        <ClickAwayListener onClickAway={onClickAway}>
+          <div
+            className="fixed z-50 flex right-4 bottom-4 left-auto justify-start rounded-lg bg-slate-50 dark:bg-slate-900 p-3 leading-normal animation-appear transition-transform border border-solid border-slate-200 dark:border-slate-700 min-w-snackbar max-w-snackbar shadow-md text-slate-900 dark:text-slate-50"
+            {...getRootProps()}
+          >
+            Hello World
+          </div>
+        </ClickAwayListener>
+      ) : null}
+    </div>
   );
 }
