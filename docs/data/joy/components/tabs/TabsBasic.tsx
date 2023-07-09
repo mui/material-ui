24c7f606d129cbc7996d597070fbe2ca8eae3949
Nicolas Theodarus <nicolastheodarus@gmail.com>
2023-07-09 18:10:52 +0200

    docs

--- docs/data/joy/components/tabs/TabsBasic.tsx
+++ docs/data/joy/components/tabs/TabsBasic.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
-import Tabs from '@mui/joy/Tabs';
+import ScrollableTabs from '@mui/joy/ScrollableTabs';
 import TabList from '@mui/joy/TabList';
 import Tab from '@mui/joy/Tab';
 import TabPanel from '@mui/joy/TabPanel';
 
 export default function TabsBasic() {
   return (
-    <Tabs aria-label="Basic tabs" defaultValue={0} sx={{ borderRadius: 'lg' }}>
+    <ScrollableTabs
+      aria-label="Basic tabs"
+      defaultValue={0}
+      sx={{ borderRadius: 'lg', width: '300px' }}
+    >
       <TabList>
         <Tab>First tab</Tab>
         <Tab>Second tab</Tab>
         <Tab>Third tab</Tab>
+        <Tab>Fourth tab</Tab>
+        <Tab>Fifth tab</Tab>
+        <Tab>Sixth tab</Tab>
       </TabList>
       <TabPanel value={0} sx={{ p: 2 }}>
         <b>First</b> tab panel
@@ -21,6 +28,15 @@ export default function TabsBasic() {
       <TabPanel value={2} sx={{ p: 2 }}>
         <b>Third</b> tab panel
       </TabPanel>
-    </Tabs>
+      <TabPanel value={3} sx={{ p: 2 }}>
+        <b>Fourth</b> tab panel
+      </TabPanel>
+      <TabPanel value={4} sx={{ p: 2 }}>
+        <b>Fifth</b> tab panel
+      </TabPanel>
+      <TabPanel value={5} sx={{ p: 2 }}>
+        <b>Sixth</b> tab panel
+      </TabPanel>
+    </ScrollableTabs>
   );
 }
