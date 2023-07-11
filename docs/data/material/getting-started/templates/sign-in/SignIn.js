f1645112de59e3a4f98f43e02c896eb7976e0959
Arshad Siddiqui <101931160+Arshad-Siddiqui@users.noreply.github.com>
2023-07-11 10:06:47 +0100

    Update Signin.js to match
    Signed-off-by: Arshad Siddiqui <101931160+Arshad-Siddiqui@users.noreply.github.com>

--- docs/data/material/getting-started/templates/sign-in/SignIn.js
+++ docs/data/material/getting-started/templates/sign-in/SignIn.js
@@ -11,7 +11,6 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Copyright(props) {
   return (
@@ -26,10 +25,6 @@ function Copyright(props) {
   );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme();
-
 export default function SignIn() {
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -41,7 +36,6 @@ export default function SignIn() {
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
@@ -107,6 +101,5 @@ export default function SignIn() {
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
-    </ThemeProvider>
   );
 }
