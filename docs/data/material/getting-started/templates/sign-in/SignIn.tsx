0269543d92ced4153e825398da4a70ce47607976
Arshad Siddiqui <101931160+Arshad-Siddiqui@users.noreply.github.com>
2023-07-11 10:05:47 +0100

    Stop Signin template from resetting the theme
    Signed-off-by: Arshad Siddiqui <101931160+Arshad-Siddiqui@users.noreply.github.com>

--- docs/data/material/getting-started/templates/sign-in/SignIn.tsx
+++ docs/data/material/getting-started/templates/sign-in/SignIn.tsx
@@ -11,7 +11,6 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Copyright(props: any) {
   return (
@@ -26,9 +25,6 @@ function Copyright(props: any) {
   );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
-
 export default function SignIn() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -40,7 +36,6 @@ export default function SignIn() {
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
@@ -106,6 +101,5 @@ export default function SignIn() {
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
-    </ThemeProvider>
   );
 }
