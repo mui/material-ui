9aafdf47f4ff39ecc34ef6c7e861b671574420d8
Olivier Tassinari <olivier.tassinari@gmail.com>
2023-07-16 14:26:13 +0200

    [docs] Disable ad for onboarding pages

--- docs/pages/material-ui/getting-started/supported-platforms.js
+++ docs/pages/material-ui/getting-started/supported-platforms.js
@@ -3,5 +3,5 @@ import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
 import * as pageProps from 'docs/data/material/getting-started/supported-platforms/supported-platforms.md?@mui/markdown';
 
 export default function Page() {
-  return <MarkdownDocs {...pageProps} />;
+  return <MarkdownDocs {...pageProps} disableAd />;
 }
