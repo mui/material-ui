9ebd932d754a15f9b13a5566ac6c5e19d291ab22
Olivier Tassinari <olivier.tassinari@gmail.com>
2023-07-16 23:32:48 +0200

    these don't feel needed

--- docs/pages/material-ui/getting-started/faq.js
+++ docs/pages/material-ui/getting-started/faq.js
@@ -3,5 +3,5 @@ import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
 import * as pageProps from 'docs/data/material/getting-started/faq/faq.md?@mui/markdown';
 
 export default function Page() {
-  return <MarkdownDocs {...pageProps} disableAd />;
+  return <MarkdownDocs {...pageProps} />;
 }
