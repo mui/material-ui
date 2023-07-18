d8600093aa3fedac2f4a50f4931303aa9e1ef222
siriwatknp <siriwatkunaporn@gmail.com>
2023-07-18 15:06:43 +0800

    update demos

--- docs/data/joy/components/typography/DecoratorExamples.tsx
+++ docs/data/joy/components/typography/DecoratorExamples.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/joy/Box';
 import Typography from '@mui/joy/Typography';
+import SvgIcon from '@mui/joy/SvgIcon';
 import InfoOutlined from '@mui/icons-material/InfoOutlined';
 
 export default function DecoratorExamples() {
@@ -53,6 +54,32 @@ export default function DecoratorExamples() {
         sx={{ '--Typography-gap': '0.5rem', p: 1 }}
       >
         Simple alert using only Typography.
+      </Typography>
+
+      <Typography
+        color="success"
+        startDecorator={
+          <SvgIcon fontSize="md" color="inherit">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"
+              />
+            </svg>
+          </SvgIcon>
+        }
+      >
+        +8.2%{' '}
+        <Typography level="body-xs" sx={{ alignSelf: 'last baseline', ml: 1 }}>
+          Since last month
+        </Typography>
       </Typography>
     </Box>
   );
