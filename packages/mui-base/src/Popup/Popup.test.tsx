089db1b8f97be3b542aafd6cf3528605bfbec776
Michał Dudak <michal@mui.com>
2023-07-18 11:41:42 +0200

    Tests

---
+++ packages/mui-base/src/Popup/Popup.test.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { act, createRenderer, createMount, describeConformanceUnstyled } from 'test/utils';
+import Popup, { popupClasses, PopupProps } from '@mui/base/Popup';
+
+describe('<Popup />', () => {
+  const { render } = createRenderer();
+  const mount = createMount();
+
+  const defaultProps: PopupProps = {
+    anchor: () => document.createElement('div'),
+    children: <span>Hello World</span>,
+    open: true,
+  };
+
+  describeConformanceUnstyled(<Popup {...defaultProps} />, () => ({
+    inheritComponent: 'div',
+    render: async (...renderArgs) => {
+      const result = render(...renderArgs);
+      // flush microtasks (https://floating-ui.com/docs/react#testing)
+      await act(() => async () => {});
+      return result;
+    },
+    mount,
+    refInstanceof: window.HTMLDivElement,
+    skip: [
+      // https://github.com/facebook/react/issues/11565
+      'reactTestRenderer',
+      'componentProp',
+    ],
+    slots: {
+      root: {
+        expectedClassName: popupClasses.root,
+      },
+    },
+  }));
+});
