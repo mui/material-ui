64d354ba150a8153d62876a52f1e1f3eee7584e1
Andrew Cherniavskii <andrew@mui.com>
2023-07-18 11:28:31 +0200

    [website] Update MUI X landing page (#37966)

--- docs/src/components/productX/XHero.tsx
+++ docs/src/components/productX/XHero.tsx
@@ -117,8 +117,7 @@ export default function XHero() {
             components. We&apos;re kicking it off with the most powerful Data Grid on the market.
           </Typography>
           <GetStartedButtons
-            installation="npm install @mui/x-data-grid"
-            to={ROUTES.dataGridDocs}
+            to={ROUTES.advancedComponents}
             sx={{ justifyContent: { xs: 'center', md: 'flex-start' } }}
           />
         </Box>
