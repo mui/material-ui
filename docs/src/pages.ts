4111f324d615e7ef7554ade3b2b206f6e36a3e61
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-07-19 11:09:46 -0300

    testing side nav

--- docs/src/pages.ts
+++ docs/src/pages.ts
@@ -1,67 +1,31 @@
 import type { MuiPage, OrderedMuiPage } from 'docs/src/MuiPage';
-import standardNavIcons from 'docs/src/modules/components/AppNavIcons';
 
 const pages: readonly MuiPage[] = [
   { pathname: '/versions' },
+  { pathname: '/docs/callouts', title: 'Callouts' },
   {
-    pathname: 'https://mui.com/store/',
-    title: 'Templates',
-    icon: standardNavIcons.ReaderIcon,
-    linkProps: {
-      'data-ga-event-category': 'store',
-      'data-ga-event-action': 'click',
-      'data-ga-event-label': 'sidenav',
-    },
-  },
-  { pathname: '/blog', title: 'Blog', icon: standardNavIcons.BookIcon },
-  {
-    pathname: '/x/react-data-grid-group',
-    title: 'Data Grid',
+    pathname: '/about',
+    title: 'Main parent',
     children: [
-      { pathname: '/x/react-data-grid', title: 'Overview' },
-      { pathname: '/x/react-data-grid/demo' },
       {
-        pathname: '/x/react-data-grid/columns',
+        pathname: '/x/react-data-grid-group',
+        title: 'First-level child',
         children: [
-          { pathname: '/x/react-data-grid/column-definition' },
-          { pathname: '/x/react-data-grid/column-dimensions' },
-          { pathname: '/x/react-data-grid/column-visibility' },
-        ],
-      },
-      {
-        pathname: '/x/react-data-grid/rows',
-        children: [
-          { pathname: '/x/react-data-grid/row-definition' },
-          { pathname: '/x/react-data-grid/row-updates' },
-          { pathname: '/x/react-data-grid/row-height' },
-        ],
-      },
-      { pathname: '/x/react-data-grid/editing' },
-      {
-        pathname: '/x/api/data-grid-group',
-        title: 'API Reference',
-        children: [
-          { pathname: '/x/api/data-grid', title: 'Index' },
+          { pathname: '/x/react-data-grid/editing', title: 'Second-level child' },
+          { pathname: '/x/react-data-grid', title: 'Second-level child' },
           {
-            pathname: '/x/api/data-grid-components-group',
-            subheader: 'Components',
+            pathname: '/x/api/data-grid-group',
+            title: 'Second-level child parent',
             children: [
-              { pathname: '/x/api/data-grid/data-grid', title: 'DataGrid' },
+              { pathname: '/x/api/data-grid', title: 'Third-level child' },
               {
-                pathname: '/x/api/data-grid/data-grid-premium',
-                title: 'DataGridPremium',
-                plan: 'premium',
+                pathname: '/x/api/data-grid-components-group',
+                subheader: 'Subheader divider',
+                children: [
+                  { pathname: '/x/api/data-grid/data-grid', title: 'Fourth-level child' },
+                  { pathname: '/x/api/data-grid/grid-api', title: 'Fourth-level child' },
+                ],
               },
-              { pathname: '/x/api/data-grid/data-grid-pro', title: 'DataGridPro', plan: 'pro' },
-            ],
-          },
-          {
-            pathname: '/x/api/data-grid-interfaces-group',
-            subheader: 'Interfaces',
-            children: [
-              { pathname: '/x/api/data-grid/grid-api', title: 'GridApi' },
-              { pathname: '/x/api/data-grid/grid-cell-params', title: 'GridCellParams' },
-              { pathname: '/x/api/data-grid/grid-col-def', title: 'GridColDef' },
             ],
           },
         ],
