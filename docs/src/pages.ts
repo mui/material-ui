8c7dc59063a380c530ed89f7afaf48add0877e56
Andrew <andrew@mui.com>
2023-07-19 15:18:12 +0200

    add MUI X side nav structure to the experiment page

--- docs/src/pages.ts
+++ docs/src/pages.ts
@@ -14,6 +14,60 @@ const pages: readonly MuiPage[] = [
     },
   },
   { pathname: '/blog', title: 'Blog', icon: standardNavIcons.BookIcon },
+  {
+    pathname: '/x/react-data-grid-group',
+    title: 'Data Grid',
+    children: [
+      { pathname: '/x/react-data-grid', title: 'Overview' },
+      { pathname: '/x/react-data-grid/demo' },
+      {
+        pathname: '/x/react-data-grid/columns',
+        children: [
+          { pathname: '/x/react-data-grid/column-definition' },
+          { pathname: '/x/react-data-grid/column-dimensions' },
+          { pathname: '/x/react-data-grid/column-visibility' },
+        ],
+      },
+      {
+        pathname: '/x/react-data-grid/rows',
+        children: [
+          { pathname: '/x/react-data-grid/row-definition' },
+          { pathname: '/x/react-data-grid/row-updates' },
+          { pathname: '/x/react-data-grid/row-height' },
+        ],
+      },
+      { pathname: '/x/react-data-grid/editing' },
+      {
+        pathname: '/x/api/data-grid-group',
+        title: 'API Reference',
+        children: [
+          { pathname: '/x/api/data-grid', title: 'Index' },
+          {
+            pathname: '/x/api/data-grid-components-group',
+            subheader: 'Components',
+            children: [
+              { pathname: '/x/api/data-grid/data-grid', title: 'DataGrid' },
+              {
+                pathname: '/x/api/data-grid/data-grid-premium',
+                title: 'DataGridPremium',
+                plan: 'premium',
+              },
+              { pathname: '/x/api/data-grid/data-grid-pro', title: 'DataGridPro', plan: 'pro' },
+            ],
+          },
+          {
+            pathname: '/x/api/data-grid-interfaces-group',
+            subheader: 'Interfaces',
+            children: [
+              { pathname: '/x/api/data-grid/grid-api', title: 'GridApi' },
+              { pathname: '/x/api/data-grid/grid-cell-params', title: 'GridCellParams' },
+              { pathname: '/x/api/data-grid/grid-col-def', title: 'GridColDef' },
+            ],
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 export type { MuiPage, OrderedMuiPage };
