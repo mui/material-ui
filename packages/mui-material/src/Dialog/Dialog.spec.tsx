0b92b0a042ca2af559b2cd47d7bcc16d69c92529
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-07-21 13:20:11 +0530

    Revert "[core] Adds `component` prop to `OverrideProps` type (#35924)"
    This reverts commit 1b9c8ab5a7c0125fa411f46ffafba76f04fc6eae.

--- packages/mui-material/src/Dialog/Dialog.spec.tsx
+++ packages/mui-material/src/Dialog/Dialog.spec.tsx
@@ -1,19 +1,6 @@
 import * as React from 'react';
-import Dialog from '@mui/material/Dialog';
-import { PaperProps } from '@mui/material/Paper';
-import { expectType } from '@mui/types';
+import { Dialog } from '@mui/material';
 
-const paperProps: PaperProps<'span'> = {
-  component: 'span',
-  onClick: (event) => {
-    expectType<React.MouseEvent<HTMLSpanElement, MouseEvent>, typeof event>(event);
-  },
-};
-function Test() {
-  return (
-    <React.Fragment>
-      <Dialog open />;
-      <Dialog open PaperProps={paperProps} />;
-    </React.Fragment>
-  );
+function optionalChildrenTest() {
+  <Dialog open />;
 }
