eb63aa636f779f15787967c7291ed30488d7b633
Marija Najdova <mnajdova@gmail.com>
2023-07-24 18:37:57 +0200

    lint & prettier

--- packages/mui-base/src/utils/createSlot.ts
+++ packages/mui-base/src/utils/createSlot.ts
@@ -1,7 +1,13 @@
+/* eslint-disable react/prop-types */
 import * as React from 'react';
 
-export default function createSlot<ComponentType extends React.JSXElementConstructor<React.ComponentProps<ComponentType>>>(Component: ComponentType) {
-  return React.forwardRef<HTMLElement, React.ComponentProps<typeof Component>>(function Slot(props, ref) {
+export default function createSlot<
+  ComponentType extends React.JSXElementConstructor<React.ComponentProps<ComponentType>>,
+>(Component: ComponentType) {
+  return React.forwardRef<HTMLElement, React.ComponentProps<typeof Component>>(function Slot(
+    props,
+    ref,
+  ) {
     // @ts-ignore ownerState is added by the owner component
     const { ownerState, children, ...other } = props;
     return React.createElement(typeof Component, { ref, ...other }, children);
