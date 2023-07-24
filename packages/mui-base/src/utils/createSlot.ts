f78f4a7d4877d2a9dec2760b51da26f205af03b1
Marija Najdova <mnajdova@gmail.com>
2023-07-24 18:21:25 +0200

    change types

--- packages/mui-base/src/utils/createSlot.ts
+++ packages/mui-base/src/utils/createSlot.ts
@@ -1,9 +1,9 @@
 import * as React from 'react';
 
-export default function createSlot<P>(Component: React.JSXElementConstructor<P>) {
-  return React.forwardRef<HTMLElement, P>(function Slot(props, ref) {
+export default function createSlot<ComponentType extends React.JSXElementConstructor<React.ComponentProps<ComponentType>>>(Component: ComponentType) {
+  return React.forwardRef<HTMLElement, React.ComponentProps<typeof Component>>(function Slot(props, ref) {
     // @ts-ignore ownerState is added by the owner component
     const { ownerState, children, ...other } = props;
-    return React.createElement(Component, { ref, ...other }, children);
+    return React.createElement(typeof Component, { ref, ...other }, children);
   });
 }
