94a37c55facc6eb46fabbef95934a07989d0dad1
Marija Najdova <mnajdova@gmail.com>
2023-07-24 13:09:47 +0200

    Remove duplicate key

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -17,7 +17,6 @@ module.exports = {
         'outline-purple-light': '0 0 0 4px rgba(245, 208, 254, 0.25)',
         'outline-purple-xs': '0 0 0 1px rgba(192, 132, 252, 0.25)',
         'outline-switch': '0 0 1px 8px rgba(168, 85, 247, 0.35)',
-        'outline-switch': '0 0 1px 8px rgba(0, 0, 0, 0.25)',
       },
       cursor: {
         inherit: 'inherit',
