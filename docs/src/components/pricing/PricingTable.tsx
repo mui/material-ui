9b041434e6db442fdaaa98efb1b979b30b51b5df
alexandre <alex.fauquette@gmail.com>
2023-07-25 10:07:10 +0200

    prettier

--- docs/src/components/pricing/PricingTable.tsx
+++ docs/src/components/pricing/PricingTable.tsx
@@ -489,7 +489,11 @@ const rowHeaders: Record<string, React.ReactNode> = {
     <ColumnHead label="Column filters" nested href="/x/react-data-grid/filtering/" />
   ),
   'data-grid/filter-quick': (
-    <ColumnHead label="Quick filter (Search)" nested href="/x/react-data-grid/filtering/quick-filter/" />
+    <ColumnHead
+      label="Quick filter (Search)"
+      nested
+      href="/x/react-data-grid/filtering/quick-filter/"
+    />
   ),
   'data-grid/header-filters': (
     <ColumnHead label="Header filters" nested href="/x/react-data-grid/filtering/header-filters/" />
