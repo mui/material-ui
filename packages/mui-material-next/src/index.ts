e35f9ae7694beef810c04f36368a936b630d8f12
Diego Andai <diego@mui.com>
2023-07-26 13:06:09 -0400

    [Chip][material-next] Copy chip component from material (#38053)

--- packages/mui-material-next/src/index.ts
+++ packages/mui-material-next/src/index.ts
@@ -6,6 +6,9 @@ export * from './Badge';
 export { default as Button } from './Button';
 export * from './Button';
 
+export { default as Chip } from './Chip';
+export * from './Chip';
+
 export { default as Input } from './Input';
 
 export { default as Slider } from './Slider';
