2dced6c94af3bdd6bf9fef68149d1d72f8100673
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-07-28 17:27:57 +0530

    add demo

--- docs/data/joy/customization/theme-typography/CustomFontFamily.js
+++ docs/data/joy/customization/theme-typography/CustomFontFamily.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { CssVarsProvider, extendTheme } from '@mui/joy/styles';
-import Box from '@mui/joy/Box';
 import CssBaseline from '@mui/joy/CssBaseline';
 import Typography from '@mui/joy/Typography';
 
@@ -8,23 +7,14 @@ const customTheme = extendTheme({
   fontFamily: {
     body: 'SF Pro Text',
   },
-  cssVarPrefix: 'joy',
+  cssVarPrefix: 'font',
 });
 
-function TypographySystem() {
-  return (
-    <Box>
-      <Typography>smksmkms</Typography>
-    </Box>
-  );
-}
-
 export default function CustomFontFamily() {
-  console.log(customTheme.vars.fontFamily);
   return (
     <CssVarsProvider theme={customTheme}>
       <CssBaseline />
-      <TypographySystem />
+      <Typography>Hello I am SF Pro Text</Typography>
     </CssVarsProvider>
   );
 }
