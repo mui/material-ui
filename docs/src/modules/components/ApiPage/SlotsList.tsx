ba6060cd43e55fd4b4388ce4c3057b77e08f0b81
alexandre <alex.fauquette@gmail.com>
2023-08-01 10:25:06 +0200

    split dedicated styling

--- docs/src/modules/components/ApiPage/SlotsList.tsx
+++ docs/src/modules/components/ApiPage/SlotsList.tsx
@@ -1,13 +1,54 @@
 /* eslint-disable react/no-danger */
 import * as React from 'react';
+import { alpha, styled } from '@mui/material/styles';
+import {
+  brandingDarkTheme as darkTheme,
+  brandingLightTheme as lightTheme,
+} from 'docs/src/modules/brandingTheme';
 import { useTranslate } from 'docs/src/modules/utils/i18n';
 import ApiItem from './ApiItem';
+
+const StyledApiItem = styled(ApiItem)(
+  ({ theme }) => ({
+    '.slot-classname': {
+      ...theme.typography.body2,
+      fontWeight: theme.typography.fontWeightSemiBold,
+      code: {
+        ...theme.typography.caption,
+        fontFamily: theme.typography.fontFamilyCode,
+        fontWeight: theme.typography.fontWeightRegular,
+        padding: '1px 4px',
+        border: '1px solid',
+        borderColor: alpha(darkTheme.palette.primary[100], 0.5),
+        backgroundColor: `var(--muidocs-palette-primary-50, ${lightTheme.palette.primary[50]})`,
+      },
+    },
+    '& .slot-description-title': {
+      ...theme.typography.body2,
+      fontWeight: theme.typography.fontWeightSemiBold,
+      paddingRight: 5,
+      whiteSpace: 'nowrap',
+    },
+  }),
+  ({ theme }) => ({
+    [`:where(${theme.vars ? '[data-mui-color-scheme="dark"]' : '.mode-dark'}) &`]: {
+      '& .slot-classname': {
+        color: `var(--muidocs-palette-grey-300, ${darkTheme.palette.grey[300]})`,
+        code: {
+          borderColor: alpha(darkTheme.palette.primary[800], 0.4),
+          backgroundColor: alpha(darkTheme.palette.primary[900], 0.4),
+        },
+      },
+    },
+  }),
+);
 
 export type SlotsListProps = {
   componentSlots: { class: string; name: string; default: string }[];
   slotDescriptions: { [key: string]: string };
   componentName?: string;
 };
+
 export default function SlotsList(props: SlotsListProps) {
   const { componentSlots, slotDescriptions, componentName } = props;
   const t = useTranslate();
@@ -17,7 +58,7 @@ export default function SlotsList(props: SlotsListProps) {
     <div className="MuiApi-slot-list">
       {componentSlots.map(({ class: className, name, default: defaultValue }) => {
         return (
-          <ApiItem
+          <StyledApiItem
             id={`${hashPrefix}slots-${className}`}
             key={className}
             description={defaultValue}
@@ -26,14 +67,14 @@ export default function SlotsList(props: SlotsListProps) {
             type="slots"
           >
             {className && (
-              <p className="prop-list-default-props">
+              <p className="slot-classname">
                 <span>{t('api-docs.globalClass')}:</span>{' '}
                 <code dangerouslySetInnerHTML={{ __html: className }} />
               </p>
             )}
             {slotDescriptions[name] && (
-              <p className="prop-list-default-props">
-                <span>{t('api-docs.description')}:</span>{' '}
+              <p>
+                <span className="slot-description-title">{t('api-docs.description')}:</span>{' '}
                 <span
                   dangerouslySetInnerHTML={{
                     __html: slotDescriptions[name] || '',
@@ -41,7 +82,7 @@ export default function SlotsList(props: SlotsListProps) {
                 />
               </p>
             )}
-          </ApiItem>
+          </StyledApiItem>
         );
       })}
     </div>
--- docs/src/modules/components/PropertiesTable.js
+++ docs/src/modules/components/PropertiesTable.js
@@ -1,6 +1,11 @@
 /* eslint-disable react/no-danger */
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import { alpha, styled } from '@mui/material/styles';
+import {
+  brandingDarkTheme as darkTheme,
+  brandingLightTheme as lightTheme,
+} from 'docs/src/modules/brandingTheme';
 import Alert from '@mui/material/Alert';
 import { useTranslate } from 'docs/src/modules/utils/i18n';
 import ApiItem from './ApiPage/ApiItem';
@@ -17,6 +22,81 @@ const additionalPropsInfoText = {
   'joy-variant':
     'To learn how to add your own variants, check out <a href="/joy-ui/customization/themed-components/#extend-variants">Themed components—Extend variants</a>.',
 };
+
+const StyledApiItem = styled(ApiItem)(
+  ({ theme }) => ({
+    '& .prop-list-additional-info': {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 8,
+      '& .prop-list-title': {
+        paddingRight: 5,
+        whiteSpace: 'nowrap',
+        p: {
+          ...theme.typography.body2,
+          margin: 0,
+          fontWeight: theme.typography.fontWeightSemiBold,
+        },
+      },
+      '&>div': { display: 'flex', alignItems: 'baseline' },
+    },
+    '& .prop-list-default-props': {
+      ...theme.typography.body2,
+      fontWeight: theme.typography.fontWeightSemiBold,
+      code: {
+        ...theme.typography.caption,
+        fontFamily: theme.typography.fontFamilyCode,
+        fontWeight: theme.typography.fontWeightRegular,
+        padding: '1px 4px',
+        border: '1px solid',
+        borderColor: alpha(darkTheme.palette.primary[100], 0.5),
+        backgroundColor: `var(--muidocs-palette-primary-50, ${lightTheme.palette.primary[50]})`,
+      },
+    },
+    '& .prop-list-signature': {
+      p: {
+        ...theme.typography.body2,
+        fontWeight: theme.typography.fontWeightSemiBold,
+        marginBottom: 8,
+      },
+      ul: {
+        paddingLeft: 24,
+        marginTop: 2,
+        marginBottom: 0,
+      },
+      '&>code': {
+        borderRadius: 8,
+        padding: 12,
+        width: '100%',
+        marginBottom: 8,
+        color: `var(--muidocs-palette-grey-900, ${lightTheme.palette.grey[50]})`,
+        border: '1px solid',
+        borderColor: `var(--muidocs-palette-primaryDark-700, ${lightTheme.palette.primaryDark[700]})`,
+        backgroundColor: `var(--muidocs-palette-primaryDark-800, ${lightTheme.palette.primaryDark[800]})`,
+      },
+    },
+    marginBottom: 36,
+  }),
+  ({ theme }) => ({
+    [`:where(${theme.vars ? '[data-mui-color-scheme="dark"]' : '.mode-dark'}) &`]: {
+      '& .prop-list-additional-info': {
+        '& .prop-list-title': {
+          p: {
+            color: `var(--muidocs-palette-grey-50, ${darkTheme.palette.grey[50]})`,
+          },
+        },
+      },
+
+      '& .prop-list-default-props': {
+        color: `var(--muidocs-palette-grey-300, ${darkTheme.palette.grey[300]})`,
+        code: {
+          borderColor: alpha(darkTheme.palette.primary[800], 0.4),
+          backgroundColor: alpha(darkTheme.palette.primary[900], 0.4),
+        },
+      },
+    },
+  }),
+);
 
 export default function PropertiesTable(props) {
   const {
@@ -40,7 +120,7 @@ export default function PropertiesTable(props) {
           const signature = propData.signature?.type;
 
           return (
-            <ApiItem
+            <StyledApiItem
               key={propName}
               id={`${hashPrefix}prop-${propName}`}
               title={propName}
@@ -192,7 +272,7 @@ export default function PropertiesTable(props) {
                   </div>
                 )}
               </div>
-            </ApiItem>
+            </StyledApiItem>
           );
         })}
     </div>
