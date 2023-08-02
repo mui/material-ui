54dd346ac4ecd26febdfa90bf1cf0aec0a741457
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-08-02 10:01:31 -0300

    a bit slower!

--- docs/src/modules/components/AppNavDrawerItem.js
+++ docs/src/modules/components/AppNavDrawerItem.js
@@ -330,7 +330,7 @@ export default function AppNavDrawerItem(props) {
         {planned && <Chip label="Planned" sx={sxChip('grey')} />}
       </Item>
       {expandable ? (
-        <Collapse in={open} timeout={200} unmountOnExit>
+        <Collapse in={open} timeout={250} unmountOnExit>
           {children}
         </Collapse>
       ) : (
