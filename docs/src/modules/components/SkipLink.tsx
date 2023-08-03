9fcc9e6ccd589ad14710cb9258b3bed8fa421b06
Olivier Tassinari <olivier.tassinari@gmail.com>
2023-08-03 18:10:48 +0200

    [docs-infra] Fix skip to content design

--- docs/src/modules/components/SkipLink.tsx
+++ docs/src/modules/components/SkipLink.tsx
@@ -5,21 +5,36 @@ import { useTranslate } from 'docs/src/modules/utils/i18n';
 
 const StyledLink = styled(MuiLink)(({ theme }) => ({
   position: 'fixed',
-  padding: theme.spacing(1),
-  background: (theme.vars || theme).palette.background.paper,
+  padding: theme.spacing(1, 2),
+  backgroundColor: (theme.vars || theme).palette.secondary[700],
+  color: '#fff',
+  outlineOffset: 2,
+  '&:hover': {
+    color: '#fff',
+  },
+  borderRadius: theme.shape.borderRadius,
+  left: theme.spacing(2),
+  zIndex: theme.zIndex.tooltip + 1,
+  top: theme.spacing(-10),
   transition: theme.transitions.create('top', {
     easing: theme.transitions.easing.easeIn,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  left: theme.spacing(2),
-  top: theme.spacing(-10),
-  zIndex: theme.zIndex.tooltip + 1,
   '&:focus': {
     top: theme.spacing(2),
     transition: theme.transitions.create('top', {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
+  },
+  '@media (prefers-reduced-motion: reduce)': {
+    top: theme.spacing(2),
+    transition: theme.transitions.create('opacity'),
+    opacity: 0,
+    '&:focus': {
+      opacity: 1,
+      transition: theme.transitions.create('opacity'),
+    },
   },
   '@media print': {
     display: 'none',
@@ -29,9 +44,5 @@ const StyledLink = styled(MuiLink)(({ theme }) => ({
 export default function SkipLink() {
   const t = useTranslate();
 
-  return (
-    <StyledLink color="secondary" href="#main-content">
-      {t('appFrame.skipToContent')}
-    </StyledLink>
-  );
+  return <StyledLink href="#main-content">{t('appFrame.skipToContent')}</StyledLink>;
 }
