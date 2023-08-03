8107514fc8a787a808cce624b181fcb2ada2b8f4
zanivan <victorzanivan@gmail.com>
2023-08-03 15:05:14 -0300

    Tweaks on the box-shadow

--- docs/data/base/components/input/UnstyledInputIntroduction/css/index.tsx
+++ docs/data/base/components/input/UnstyledInputIntroduction/css/index.tsx
@@ -64,7 +64,7 @@ function Styles() {
         color: ${isDarkMode ? grey[300] : grey[900]};
         background: ${isDarkMode ? grey[900] : '#fff'};
         border: 1px solid ${isDarkMode ? grey[700] : grey[200]};
-        box-shadow: 0px 2px 24px ${isDarkMode ? cyan[900] : cyan[100]};
+        box-shadow: 0px 2px 24px ${isDarkMode ? cyan[800] : cyan[100]};
       
         &:hover {
           border-color: ${cyan[400]};
