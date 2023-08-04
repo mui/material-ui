92f1cbf6671bcd39b141566fe8755add9166b6ab
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-08-04 15:39:02 -0500

    adds material ui nextjs example in js

---
+++ examples/material-ui-nextjs/next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/random',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
