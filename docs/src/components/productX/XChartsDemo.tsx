45556f5035cb206b2f1ab9d2cf8fcf01b6b40913
alexandre <alex.fauquette@gmail.com>
2023-08-07 14:40:10 +0200

    refinement

--- docs/src/components/productX/XChartsDemo.tsx
+++ docs/src/components/productX/XChartsDemo.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-// import Grid from '@mui/material/Grid';
 import Chip from '@mui/material/Chip';
 import { alpha } from '@mui/material/styles';
-// import { shouldForwardProp } from '@mui/system';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
@@ -24,32 +22,6 @@ export default function XChartsDemo() {
                 borderRadius: 1,
                 bgcolor: '#fff',
               },
-              '& > div > div > div > div': {
-                flexGrow: 1,
-              },
-              '& .MuiTypography-subtitle1': {
-                fontSize: '0.875rem',
-              },
-              '& .MuiTypography-caption': {
-                width: 28,
-                height: 32,
-              },
-              '& .MuiPickersSlideTransition-root': {
-                minWidth: 258,
-                minHeight: 238,
-              },
-              '& [role="row"]': {
-                margin: '4px 0',
-              },
-              '& .MuiDateRangePickerDay-root': {
-                lineHeight: 0,
-                margin: 0,
-              },
-              '& .MuiPickersDay-root': {
-                width: 28,
-                height: 28,
-                fontWeight: 400,
-              },
             },
             (theme) =>
               theme.applyDarkStyles({
@@ -59,38 +31,40 @@ export default function XChartsDemo() {
               }),
           ]}
         >
-          <LineChart
-            sx={{
-              '& .MuiMarkElement-root': { display: 'none' },
-              '& .MuiMarkElement-root.MuiMarkElement-highlighted': { display: 'block' },
-            }}
-            margin={{ top: 10, bottom: 50, left: 40, right: 20 }}
-            series={[
-              { data: [35, 44, 24, 34], stack: 'total', area: true },
-              { data: [51, 6, 49, 30], stack: 'total', area: true },
-              { data: [5, 15, 30, 50], stack: 'total', area: true },
-              { data: [60, 50, 15, 10], stack: 'total', area: true },
-            ]}
-            xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'point' }]}
-            colors={mangoFusionPalette}
-            height={250}
-          />
-          <BarChart
-            sx={{
-              '& .MuiMarkElement-root': { display: 'none' },
-              '& .MuiMarkElement-root.MuiMarkElement-highlighted': { display: 'block' },
-            }}
-            margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
-            series={[
-              { data: [35, 44, 24, 34] },
-              { data: [51, 6, 49, 30] },
-              { data: [15, 25, 30, 50] },
-              { data: [60, 50, 15, 25] },
-            ]}
-            xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}
-            height={200}
-            colors={mangoFusionPalette}
-          />
+          <Box sx={{ py: 1 }}>
+            <LineChart
+              sx={{
+                '& .MuiMarkElement-root': { display: 'none' },
+                '& .MuiMarkElement-root.MuiMarkElement-highlighted': { display: 'block' },
+              }}
+              margin={{ top: 10, bottom: 50, left: 40, right: 20 }}
+              series={[
+                { data: [35, 44, 24, 34], stack: 'total', area: true },
+                { data: [51, 6, 49, 30], stack: 'total', area: true },
+                { data: [5, 15, 30, 50], stack: 'total', area: true },
+                { data: [60, 50, 15, 10], stack: 'total', area: true },
+              ]}
+              xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'point' }]}
+              colors={mangoFusionPalette}
+              height={250}
+            />
+            <BarChart
+              sx={{
+                '& .MuiMarkElement-root': { display: 'none' },
+                '& .MuiMarkElement-root.MuiMarkElement-highlighted': { display: 'block' },
+              }}
+              margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
+              series={[
+                { data: [35, 44, 24, 34] },
+                { data: [51, 6, 49, 30] },
+                { data: [15, 25, 30, 50] },
+                { data: [60, 50, 15, 25] },
+              ]}
+              xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}
+              height={200}
+              colors={mangoFusionPalette}
+            />
+          </Box>
         </Paper>
       </Frame.Demo>
       <Frame.Info data-mui-color-scheme="dark">
