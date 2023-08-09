f37a5826742fec5e8d3045cb8f04212f78918d09
Brijesh Bittu <brijeshb42@gmail.com>
2023-08-09 19:27:46 +0530

    [zero][app] Add jsx syntax plugin for intermediate imports

--- apps/zero-runtime-vite-app/vite.config.ts
+++ apps/zero-runtime-vite-app/vite.config.ts
@@ -21,6 +21,9 @@ export default defineConfig({
       themeArgs: {
         theme,
       },
+      babelOptions: {
+        plugins: ['@babel/plugin-syntax-jsx'],
+      },
     }),
     reactPlugin(),
     splitVendorChunkPlugin(),
