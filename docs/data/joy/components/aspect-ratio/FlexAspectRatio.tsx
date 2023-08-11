0c0109f14b6ae8d12c8c503a2cdca5bdefd4317f
siriwatknp <siriwatkunaporn@gmail.com>
2023-08-11 21:43:41 +0700

    add demo

---
+++ docs/data/joy/components/aspect-ratio/FlexAspectRatio.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import AspectRatio from '@mui/joy/AspectRatio';
+import Button from '@mui/joy/Button';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import CardActions from '@mui/joy/CardActions';
+import Typography from '@mui/joy/Typography';
+import Stack from '@mui/joy/Stack';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Switch from '@mui/joy/Switch';
+
+export default function FlexAspectRatio() {
+  const [flex, setFlex] = React.useState(false);
+  return (
+    <Stack spacing={2} alignItems="center">
+      <FormControl orientation="horizontal">
+        <FormLabel>Flex</FormLabel>
+        <Switch checked={flex} onChange={(event) => setFlex(event.target.checked)} />
+      </FormControl>
+      <Card orientation="horizontal" sx={{ resize: 'horizontal', overflow: 'auto' }}>
+        <AspectRatio ratio="21/9" flex={flex} sx={{ flexBasis: 200 }}>
+          <Typography level="h1" component="div">
+            21 / 9
+          </Typography>
+        </AspectRatio>
+        <CardContent>
+          <Typography level="body-xs">20 APR 2023</Typography>
+          <Typography level="h2" component="div">
+            Widget Title
+          </Typography>
+          <Typography>
+            Lorem ipsum is placeholder text commonly used in the graphic.
+          </Typography>
+          <CardActions buttonFlex="none">
+            <Button variant="soft" color="neutral">
+              Learn more
+            </Button>
+          </CardActions>
+        </CardContent>
+      </Card>
+    </Stack>
+  );
+}
