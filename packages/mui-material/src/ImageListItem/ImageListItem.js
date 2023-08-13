f11de7d965350115cec46b8472a0f031cf5a1ba1
aravitz <aviravitz+1@gmail.com>
2023-08-13 11:28:28 +0300

    ImageItemList fix incorrect (below) rendering

--- packages/mui-material/src/ImageListItem/ImageListItem.js
+++ packages/mui-material/src/ImageListItem/ImageListItem.js
@@ -37,6 +37,7 @@ const ImageListItemRoot = styled('li', {
 })(({ ownerState }) => ({
   display: 'block',
   position: 'relative',
+  overflow: 'hidden',
   ...(ownerState.variant === 'standard' && {
     // For titlebar under list item
     display: 'flex',
