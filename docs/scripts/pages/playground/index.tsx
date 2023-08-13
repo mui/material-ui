515c3a70d417d1b7465817a11c3cdff0ccd86e70
omriklein <omrik2007@gmail.com>
2023-08-13 13:10:02 +0300

    remove file

--- docs/scripts/pages/playground/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export default function Playground() {
-  return (
-    <div>A playground for a fast iteration development cycle in isolation outside of git.</div>
-  );
-}
