7dc775d4086aeebf866ff5c8e9e5b19a87644072
Marija Najdova <mnajdova@gmail.com>
2023-08-15 15:30:13 +0200

    Badge demo update

--- docs/data/base/components/badge/UnstyledBadgeIntroduction/tailwind/index.tsx
+++ docs/data/base/components/badge/UnstyledBadgeIntroduction/tailwind/index.tsx
@@ -1,77 +1,63 @@
 import * as React from 'react';
-import { styled, Box } from '@mui/system';
-import { Badge, badgeClasses } from '@mui/base/Badge';
+import clsx from 'clsx';
+import { Badge as BaseBadge, BadgeProps } from '@mui/base/Badge';
+import { useTheme } from '@mui/system';
 
-const blue = {
-  100: '#DAECFF',
-  500: '#007FFF',
-  900: '#003A75',
-};
-
-const grey = {
-  200: '#d0d7de',
-  700: '#424a53',
-};
-
-function BadgeContent() {
-  return (
-    <Box
-      component="span"
-      sx={{
-        width: 40,
-        height: 40,
-        borderRadius: '12px',
-        background: (theme) =>
-          theme.palette.mode === 'dark' ? grey[700] : grey[200],
-        display: 'inline-block',
-        verticalAlign: 'middle',
-      }}
-    />
-  );
+function useIsDarkMode() {
+  const theme = useTheme();
+  return theme.palette.mode === 'dark';
 }
 
 export default function UnstyledBadgeIntroduction() {
+  // Replace this with your app logic for determining dark mode
+  const isDarkMode = useIsDarkMode();
+
   return (
-    <StyledBadge badgeContent={5}>
-      <BadgeContent />
-    </StyledBadge>
+    <div className={isDarkMode ? 'dark' : ''}>
+      <Badge badgeContent={5}>
+        <span className="w-10 h-10 rounded-xl bg-slate-300 dark:bg-slate-400 inline-block align-middle" />
+      </Badge>
+    </div>
   );
 }
 
-const StyledBadge = styled(Badge)(
-  ({ theme }) => `
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  font-size: 14px;
-  font-variant: tabular-nums;
-  list-style: none;
-  font-family: IBM Plex Sans, sans-serif;
-  position: relative;
-  display: inline-block;
-  line-height: 1;
+const resolveSlotProps = (fn: any, args: any) =>
+  typeof fn === 'function' ? fn(args) : fn;
 
-  & .${badgeClasses.badge} {
-    z-index: auto;
-    position: absolute;
-    top: 0;
-    right: 0;
-    min-width: 22px;
-    height: 22px;
-    padding: 0 6px;
-    color: #fff;
-    font-weight: 600;
-    font-size: 12px;
-    line-height: 22px;
-    white-space: nowrap;
-    text-align: center;
-    border-radius: 12px;
-    background: ${blue[500]};
-    box-shadow: 0px 2px 24px ${
-      theme.palette.mode === 'dark' ? blue[900] : blue[100]
-    };
-    transform: translate(50%, -50%);
-    transform-origin: 100% 0;
-  }
-  `,
-);
+const Badge = React.forwardRef<HTMLSpanElement, BadgeProps>((props, ref) => {
+  return (
+    <BaseBadge
+      ref={ref}
+      {...props}
+      slotProps={{
+        ...props.slotProps,
+        root: (ownerState) => {
+          const resolvedSlotProps = resolveSlotProps(
+            props.slotProps?.root,
+            ownerState,
+          );
+          return {
+            ...resolvedSlotProps,
+            className: clsx(
+              'box-border m-0 p-0 text-xs list-none relative inline-block leading-none',
+              resolvedSlotProps?.className,
+            ),
+          };
+        },
+        badge: (ownerState) => {
+          const resolvedSlotProps = resolveSlotProps(
+            props.slotProps?.badge,
+            ownerState,
+          );
+          return {
+            ...resolvedSlotProps,
+            className: clsx(
+              'z-auto absolute top-0 right-0 min-w-badge min-h-badge font-sans p-0 text-white font-semibold font-xs font-sans rounded-xl bg-purple-500 leading-5.5 whitespace-nowrap text-center translate-x-1/2 -translate-y-1/2 drop-shadow-lg origin-right',
+              resolvedSlotProps?.className,
+            ),
+          };
+        },
+      }}
+    />
+  );
+});
