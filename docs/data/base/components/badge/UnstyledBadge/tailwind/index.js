6415ae7aee401f2ed972f3a1ae2e9280a16eda53
Ali Sasani Babak <ababak@nuclera.com>
2023-08-15 14:28:22 +0100

    add css and tailwind demo for BadgeIntroduction and extract CustomBadge

--- docs/data/base/components/badge/UnstyledBadge/tailwind/index.js
+++ docs/data/base/components/badge/UnstyledBadge/tailwind/index.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import { Badge } from '@mui/base/Badge';
 import { useTheme } from '@mui/system';
+
+import clsx from 'clsx';
 
 function useIsDarkMode() {
   const theme = useTheme();
@@ -13,21 +16,68 @@ export default function UnstyledBadge() {
 
   return (
     <div className={isDarkMode ? 'dark' : ''}>
-      <Badge
-        slotProps={{
-          root: {
-            className:
-              'box-border m-0 p-0 text-xs list-none relative inline-block leading-none',
-          },
-          badge: {
-            className:
-              'z-auto absolute top-0 right-0 min-w-badge min-h-badge p-0 text-white font-semibold font-xs rounded-xl bg-purple-500 leading-5.5 whitespace-nowrap text-center translate-x-1/2 -translate-y-1/2 drop-shadow-lg origin-right',
-          },
-        }}
-        badgeContent={5}
-      >
+      <CustomBadge badgeContent={5}>
         <span className="w-10 h-10 rounded-xl bg-slate-300 dark:bg-slate-400 inline-block align-middle" />
-      </Badge>
+      </CustomBadge>
     </div>
   );
 }
+
+const resolveSlotProps = (fn, args) => (typeof fn === 'function' ? fn(args) : fn);
+
+const CustomBadge = React.forwardRef((props, ref) => {
+  // Replace this with your app logic for determining dark modes
+  const isDarkMode = useIsDarkMode();
+
+  return (
+    <Badge
+      ref={ref}
+      {...props}
+      className={clsx(isDarkMode ? 'dark' : '', props.className)}
+      slotProps={{
+        ...props.slotProps,
+        root: (ownerState) => {
+          const resolvedSlotProps = resolveSlotProps(
+            props.slotProps?.root,
+            ownerState,
+          );
+          return {
+            ...resolvedSlotProps,
+            className: clsx(
+              'box-border m-0 p-0 text-xs list-none relative inline-block leading-none',
+              resolvedSlotProps?.className,
+            ),
+          };
+        },
+        badge: (ownerState) => {
+          const resolvedSlotProps = resolveSlotProps(
+            props.slotProps?.badge,
+            ownerState,
+          );
+          return {
+            ...resolvedSlotProps,
+            className: clsx(
+              'z-auto absolute top-0 right-0 min-w-badge min-h-badge p-0 text-white font-semibold font-xs rounded-xl bg-purple-500 leading-5.5 whitespace-nowrap text-center translate-x-1/2 -translate-y-1/2 drop-shadow-lg origin-right',
+              resolvedSlotProps?.className,
+            ),
+          };
+        },
+      }}
+    />
+  );
+});
+
+CustomBadge.propTypes = {
+  /**
+   * @ignore
+   */
+  className: PropTypes.string,
+  /**
+   * The props used for each slot inside the Badge.
+   * @default {}
+   */
+  slotProps: PropTypes.shape({
+    badge: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+    root: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+  }),
+};
--- docs/data/base/components/badge/UnstyledBadge/tailwind/index.tsx
+++ docs/data/base/components/badge/UnstyledBadge/tailwind/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Badge } from '@mui/base/Badge';
 import { useTheme } from '@mui/system';
+import { BadgeProps } from '@mui/material';
+import clsx from 'clsx';
 
 function useIsDarkMode() {
   const theme = useTheme();
@@ -13,21 +15,54 @@ export default function UnstyledBadge() {
 
   return (
     <div className={isDarkMode ? 'dark' : ''}>
-      <Badge
-        slotProps={{
-          root: {
-            className:
-              'box-border m-0 p-0 text-xs list-none relative inline-block leading-none',
-          },
-          badge: {
-            className:
-              'z-auto absolute top-0 right-0 min-w-badge min-h-badge p-0 text-white font-semibold font-xs rounded-xl bg-purple-500 leading-5.5 whitespace-nowrap text-center translate-x-1/2 -translate-y-1/2 drop-shadow-lg origin-right',
-          },
-        }}
-        badgeContent={5}
-      >
+      <CustomBadge badgeContent={5}>
         <span className="w-10 h-10 rounded-xl bg-slate-300 dark:bg-slate-400 inline-block align-middle" />
-      </Badge>
+      </CustomBadge>
     </div>
   );
 }
+
+const resolveSlotProps = (fn: any, args: any) =>
+  typeof fn === 'function' ? fn(args) : fn;
+
+const CustomBadge = React.forwardRef<HTMLSpanElement, BadgeProps>((props, ref) => {
+  // Replace this with your app logic for determining dark modes
+  const isDarkMode = useIsDarkMode();
+
+  return (
+    <Badge
+      ref={ref}
+      {...props}
+      className={clsx(isDarkMode ? 'dark' : '', props.className)}
+      slotProps={{
+        ...props.slotProps,
+        root: (ownerState) => {
+          const resolvedSlotProps = resolveSlotProps(
+            props.slotProps?.root,
+            ownerState,
+          );
+          return {
+            ...resolvedSlotProps,
+            className: clsx(
+              'box-border m-0 p-0 text-xs list-none relative inline-block leading-none',
+              resolvedSlotProps?.className,
+            ),
+          };
+        },
+        badge: (ownerState) => {
+          const resolvedSlotProps = resolveSlotProps(
+            props.slotProps?.badge,
+            ownerState,
+          );
+          return {
+            ...resolvedSlotProps,
+            className: clsx(
+              'z-auto absolute top-0 right-0 min-w-badge min-h-badge p-0 text-white font-semibold font-xs rounded-xl bg-purple-500 leading-5.5 whitespace-nowrap text-center translate-x-1/2 -translate-y-1/2 drop-shadow-lg origin-right',
+              resolvedSlotProps?.className,
+            ),
+          };
+        },
+      }}
+    />
+  );
+});
