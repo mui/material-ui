bfe1a5bc9c75b29b09272ebd70234436b12bda0d
Marija Najdova <mnajdova@gmail.com>
2023-08-15 13:09:41 +0200

    extract custom component

--- docs/data/base/components/slider/UnstyledSliderIntroduction/tailwind/index.tsx
+++ docs/data/base/components/slider/UnstyledSliderIntroduction/tailwind/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useTheme } from '@mui/system';
-import { Slider } from '@mui/base/Slider';
+import { Slider, SliderProps } from '@mui/base/Slider';
+import clsx from 'clsx';
 
 function useIsDarkMode() {
   const theme = useTheme();
@@ -13,49 +14,82 @@ export default function UnstyledSliderIntroduction() {
 
   return (
     <div className={isDarkMode ? 'dark' : ''} style={{ width: 320 }}>
-      <Slider
-        slotProps={{
-          root: ({ disabled }) => ({
-            className: `h-1.5 w-full py-4 inline-block relative touch-none ${
-              disabled
-                ? 'opacity-50 cursor-default pointer-events-none text-slate-300 dark:text-slate-600'
-                : 'hover:opacity-100 cursor-pointer text-purple-500 dark:text-purple-400'
-            }`,
-          }),
-          rail: {
-            className: 'block absolute w-full h-1 rounded-sm bg-current opacity-40',
-          },
-          track: { className: 'block absolute h-1 rounded-sm bg-current' },
-          thumb: (_, { active, focused }) => ({
-            className: `absolute w-4 h-4 -ml-1.5 -mt-1.5 box-border rounded-full outline-0 border-3 border-solid border-current bg-white hover:shadow-outline-purple ${
-              focused || active ? 'shadow-outline-purple' : ''
-            }`,
-          }),
-        }}
-        defaultValue={50}
-      />
-      <Slider
-        slotProps={{
-          root: ({ disabled }) => ({
-            className: `h-1.5 w-full py-4 inline-block relative touch-none ${
-              disabled
-                ? 'opacity-50 cursor-default pointer-events-none text-slate-300 dark:text-slate-600'
-                : 'hover:opacity-100 cursor-pointer text-purple-500 dark:text-purple-400'
-            }`,
-          }),
-          rail: {
-            className: 'block absolute w-full h-1 rounded-sm bg-current opacity-40',
-          },
-          track: { className: 'block absolute h-1 rounded-sm bg-current' },
-          thumb: (_, { active, focused }) => ({
-            className: `absolute w-4 h-4 -ml-1.5 -mt-1.5 box-border rounded-full outline-0 border-3 border-solid border-current bg-white hover:shadow-outline-purple ${
-              focused || active ? 'shadow-outline-purple' : ''
-            }`,
-          }),
-        }}
-        defaultValue={10}
-        disabled
-      />
+      <CustomSlider defaultValue={50} />
+      <CustomSlider defaultValue={10} disabled />
     </div>
   );
 }
+
+const resolveSlotProps = (fn: any, args: any) =>
+  typeof fn === 'function' ? fn(args) : fn;
+
+const CustomSlider = React.forwardRef<HTMLSpanElement, SliderProps>((props, ref) => {
+  return (
+    <Slider
+      ref={ref}
+      {...props}
+      slotProps={{
+        ...props.slotProps,
+        root: (ownerState) => {
+          const resolvedSlotProps = resolveSlotProps(
+            props.slotProps?.root,
+            ownerState,
+          );
+          return {
+            ...resolvedSlotProps,
+            className: clsx(
+              `h-1.5 w-full py-4 inline-block relative touch-none ${
+                ownerState.disabled
+                  ? 'opacity-50 cursor-default pointer-events-none text-slate-300 dark:text-slate-600'
+                  : 'hover:opacity-100 cursor-pointer text-purple-500 dark:text-purple-400'
+              }`,
+              resolvedSlotProps?.className,
+            ),
+          };
+        },
+        rail: (ownerState) => {
+          const resolvedSlotProps = resolveSlotProps(
+            props.slotProps?.rail,
+            ownerState,
+          );
+          return {
+            ...resolvedSlotProps,
+            className: clsx(
+              'block absolute w-full h-1 rounded-sm bg-current opacity-40',
+              resolvedSlotProps?.className,
+            ),
+          };
+        },
+        track: (ownerState) => {
+          const resolvedSlotProps = resolveSlotProps(
+            props.slotProps?.track,
+            ownerState,
+          );
+
+          return {
+            ...resolvedSlotProps,
+            className: clsx(
+              'block absolute h-1 rounded-sm bg-current',
+              resolvedSlotProps?.className,
+            ),
+          };
+        },
+        thumb: (ownerState, { active, focused }) => {
+          const resolvedSlotProps = resolveSlotProps(
+            props.slotProps?.thumb,
+            ownerState,
+          );
+          return {
+            ...resolvedSlotProps,
+            className: clsx(
+              `absolute w-4 h-4 -ml-1.5 -mt-1.5 box-border rounded-full outline-0 border-3 border-solid border-current bg-white hover:shadow-outline-purple ${
+                focused || active ? 'shadow-outline-purple' : ''
+              }`,
+              resolvedSlotProps?.className,
+            ),
+          };
+        },
+      }}
+    />
+  );
+});
