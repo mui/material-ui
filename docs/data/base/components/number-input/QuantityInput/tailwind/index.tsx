ddf840bf3cc72ff863d91dabf65408fed3f5d046
Marija Najdova <mnajdova@gmail.com>
2023-08-15 17:07:23 +0200

    [docs] Use IBM Plex Sans in Tailwind CSS demos (#38464)

--- docs/data/base/components/number-input/QuantityInput/tailwind/index.tsx
+++ docs/data/base/components/number-input/QuantityInput/tailwind/index.tsx
@@ -15,21 +15,21 @@ const CustomNumberInput = React.forwardRef(function CustomNumberInput(
       slotProps={{
         root: {
           className:
-            'leading-snug text-slate-900 dark:text-slate-300 flex flex-row flex-nowrap justify-center items-center',
+            'leading-snug font-sans text-slate-900 dark:text-slate-300 flex flex-row flex-nowrap justify-center items-center',
         },
         input: {
           className:
-            'leading-snug text-sm text-inherit bg-white dark:bg-slate-900 border border-solid border-slate-300 dark:border-slate-600 rounded-[4px] my-0 mx-1 py-2.5 px-3 outline-0 min-w-0 w-16 text-center focus-visible:outline-0 hover:border-purple-500 dark:hover:border-purple-500 focus:border-purple-500 dark:focus:border-purple-500 focus:shadow-outline-purple dark:focus:shadow-outline-purple focus-visible:outline-none',
+            'leading-snug font-sans text-sm text-inherit bg-white dark:bg-slate-900 border border-solid border-slate-300 dark:border-slate-600 rounded-[4px] my-0 mx-1 py-2.5 px-3 outline-0 min-w-0 w-16 text-center focus-visible:outline-0 hover:border-purple-500 dark:hover:border-purple-500 focus:border-purple-500 dark:focus:border-purple-500 focus:shadow-outline-purple dark:focus:shadow-outline-purple focus-visible:outline-none',
         },
         incrementButton: {
           children: <AddIcon />,
           className:
-            'order-1 text-sm box-border leading-normal border-0 rounded-full bg-transparent w-10 h-10 flex flex-row flex-nowrap justify-center items-center transition-all duration-[120ms] focus-visible:outline-0 hover:cursor-pointer hover:bg-purple-100 dark:hover:bg-purple-800',
+            'order-1 text-sm font-sans box-border leading-normal border-0 rounded-full bg-transparent w-10 h-10 flex flex-row flex-nowrap justify-center items-center transition-all duration-[120ms] focus-visible:outline-0 hover:cursor-pointer hover:bg-purple-100 dark:hover:bg-purple-800',
         },
         decrementButton: {
           children: <RemoveIcon />,
           className:
-            'text-sm box-border leading-normal border-0 rounded-full bg-transparent w-10 h-10 flex flex-row flex-nowrap justify-center items-center transition-all duration-[120ms] focus-visible:outline-0 hover:cursor-pointer hover:bg-purple-100 dark:hover:bg-purple-800',
+            'text-sm font-sans box-border leading-normal border-0 rounded-full bg-transparent w-10 h-10 flex flex-row flex-nowrap justify-center items-center transition-all duration-[120ms] focus-visible:outline-0 hover:cursor-pointer hover:bg-purple-100 dark:hover:bg-purple-800',
         },
       }}
       {...props}
