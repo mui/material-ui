dd257e3626280ec74dca9716f83d3cb7bbc9ea0f
Diego Andai <diego@mui.com>
2023-08-25 11:48:16 -0400

    Don't destructure ownerState in style function

--- packages/mui-material/src/ButtonBase/ButtonBase.js
+++ packages/mui-material/src/ButtonBase/ButtonBase.js
@@ -32,47 +32,43 @@ export const ButtonBaseRoot = styled('button', {
   name: 'MuiButtonBase',
   slot: 'Root',
   overridesResolver: (props, styles) => styles.root,
-})(({ ownerState }) => {
-  const { isButton } = ownerState;
-
-  return {
-    display: 'inline-flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'relative',
-    boxSizing: 'border-box',
-    WebkitTapHighlightColor: 'transparent',
-    backgroundColor: 'transparent', // Reset default value
-    // We disable the focus ring for mouse, touch and keyboard users.
-    outline: 0,
-    border: 0,
-    margin: 0, // Remove the margin in Safari
-    borderRadius: 0,
-    padding: 0, // Remove the padding in Firefox
-    cursor: 'pointer',
-    verticalAlign: 'middle',
-    MozAppearance: 'none', // Reset
-    WebkitAppearance: 'none', // Reset
-    textDecoration: 'none',
-    // So we take precedent over the style of a native <a /> element.
-    color: 'inherit',
-    '&:active': {
-      userSelect: 'none',
-    },
-    '&::-moz-focus-inner': {
-      borderStyle: 'none', // Remove Firefox dotted outline.
-    },
-    [`&.${buttonBaseClasses.disabled}`]: {
-      cursor: 'default',
-      ...(!isButton && {
-        pointerEvents: 'none', // Disable link interactions
-      }),
-    },
-    '@media print': {
-      colorAdjust: 'exact',
-    },
-  };
-});
+})(({ ownerState }) => ({
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  position: 'relative',
+  boxSizing: 'border-box',
+  WebkitTapHighlightColor: 'transparent',
+  backgroundColor: 'transparent', // Reset default value
+  // We disable the focus ring for mouse, touch and keyboard users.
+  outline: 0,
+  border: 0,
+  margin: 0, // Remove the margin in Safari
+  borderRadius: 0,
+  padding: 0, // Remove the padding in Firefox
+  cursor: 'pointer',
+  verticalAlign: 'middle',
+  MozAppearance: 'none', // Reset
+  WebkitAppearance: 'none', // Reset
+  textDecoration: 'none',
+  // So we take precedent over the style of a native <a /> element.
+  color: 'inherit',
+  '&:active': {
+    userSelect: 'none',
+  },
+  '&::-moz-focus-inner': {
+    borderStyle: 'none', // Remove Firefox dotted outline.
+  },
+  [`&.${buttonBaseClasses.disabled}`]: {
+    cursor: 'default',
+    ...(!ownerState.isButton && {
+      pointerEvents: 'none', // Disable link interactions
+    }),
+  },
+  '@media print': {
+    colorAdjust: 'exact',
+  },
+}));
 
 /**
  * `ButtonBase` contains as few styles as possible.
