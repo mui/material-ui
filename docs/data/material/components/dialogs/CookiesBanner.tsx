50ea4fbbc525373e215c1102a218d259fa5072fb
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-08-30 09:30:40 -0300

    simplify demo

--- docs/data/material/components/dialogs/CookiesBanner.tsx
+++ docs/data/material/components/dialogs/CookiesBanner.tsx
@@ -8,7 +8,6 @@ import Toolbar from '@mui/material/Toolbar';
 import Box from '@mui/material/Box';
 import Dialog from '@mui/material/Dialog';
 import Typography from '@mui/material/Typography';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 interface Props {
   /**
@@ -49,28 +48,7 @@ export default function CookiesBanner(props: Props) {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
           tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
           enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
+          imperdiet.
         </Typography>
       </Box>
       <Dialog
@@ -90,36 +68,30 @@ export default function CookiesBanner(props: Props) {
             maxWidth: '100%',
             width: '100%',
             m: 0,
-            p: 1,
+            p: 2,
             borderRadius: 0,
-            border: 1,
+            borderTop: 1,
             borderColor: 'divider',
           },
         }}
       >
-        <Typography variant="h5" gutterBottom>
-          This website uses cookies
-        </Typography>
-        <Stack direction="row">
-          <Typography>
-            example.com relies on cookies to improve your experience. Cookies are
-            used to show you relevant content and to analyze our website traffic.
-          </Typography>
-          <Stack direction="column" gap={0.5} sx={{ width: '350px' }}>
+        <Stack direction="row" alignItems="center" gap={2}>
+          <div>
+            <Typography variant="h6" gutterBottom>
+              This website uses cookies
+            </Typography>
+            <Typography variant="body2">
+              example.com relies on cookies to improve your experience. Cookies are
+              used to show you relevant content and to analyze our website traffic.
+            </Typography>
+          </div>
+          <Stack direction="column" gap={0.5} sx={{ width: '300px' }}>
             <Button
               size="small"
               onClick={() => toggleDialog(false)}
               variant="contained"
             >
               Allow all
-            </Button>
-            <Button
-              size="small"
-              onClick={() => toggleDialog(false)}
-              variant="outlined"
-              endIcon={<ChevronRightIcon />}
-            >
-              Customize
             </Button>
             <Button
               size="small"
