0f30d94371a6c36453bd11cd9173729d36098e56
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-09-01 17:57:48 +0530

    add noinfer

--- packages/mui-material/src/Slider/Slider.d.ts
+++ packages/mui-material/src/Slider/Slider.d.ts
@@ -150,14 +150,14 @@ export interface SliderOwnProps<Value> {
    * @param {number | number[]} value The new value.
    * @param {number} activeThumb Index of the currently moved thumb.
    */
-  onChange?: (event: Event, value: Value, activeThumb: number) => void;
+  onChange?: (event: Event, value: NoInfer<Value>, activeThumb: number) => void;
   /**
    * Callback function that is fired when the `mouseup` is triggered.
    *
    * @param {React.SyntheticEvent | Event} event The event source of the callback. **Warning**: This is a generic event not a change event.
    * @param {number | number[]} value The new value.
    */
-  onChangeCommitted?: (event: React.SyntheticEvent | Event, value: Value) => void;
+  onChangeCommitted?: (event: React.SyntheticEvent | Event, value: NoInfer<Value>) => void;
   /**
    * The component orientation.
    * @default 'horizontal'
