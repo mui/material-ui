5082859c3bec94f3f1da55786e77b9b1096e658f
zanivan <victorzanivan@gmail.com>
2023-09-01 11:36:53 -0300

    Run yarn docs:typescript:formatted

--- docs/data/joy/components/circular-progress/CircularProgressCountUp.js
+++ docs/data/joy/components/circular-progress/CircularProgressCountUp.js
@@ -8,12 +8,17 @@ import { useCountUp } from 'use-count-up';
 
 export default function CircularProgressCountUp() {
   const [isLoading, setIsLoading] = React.useState(false);
+  const [buttonLabel, setButtonLabel] = React.useState('Start'); // Track button label
 
-  const { value: value1 } = useCountUp({
+  const { value: value1, reset: resetValue1 } = useCountUp({
     isCounting: isLoading,
     duration: 1,
     start: 0,
     end: 25,
+    onEnd: () => {
+      setIsLoading(false);
+      setButtonLabel('Reset'); // Change the button label when loading is complete
+    },
   });
 
   const { value: value2, reset } = useCountUp({
@@ -23,22 +28,38 @@ export default function CircularProgressCountUp() {
     end: 75,
   });
 
+  const handleButtonClick = () => {
+    if (isLoading) {
+      setIsLoading(false);
+      setButtonLabel('Start');
+      resetValue1(); // Reset the value
+    } else {
+      setIsLoading(true);
+      setButtonLabel('Reset');
+    }
+  };
+
   return (
-    <Box sx={{ display: 'flex', gap: 2, alignItems: 'center', flexWrap: 'wrap' }}>
+    <Box sx={{ display: 'flex', gap: 8, alignItems: 'center', flexWrap: 'wrap' }}>
       <Stack sx={{ gap: 2 }}>
         <CircularProgress size="lg" determinate value={value1}>
           <Typography>{value1}%</Typography>
         </CircularProgress>
-        <Button size="sm" onClick={() => setIsLoading(true)}>
-          Start
+        <Button
+          size="sm"
+          variant="outlined"
+          color="neutral"
+          onClick={handleButtonClick} // Use a single click handler
+        >
+          {buttonLabel}
         </Button>
       </Stack>
       <Stack sx={{ gap: 2 }}>
         <CircularProgress size="lg" determinate value={value2}>
           <Typography>{value2}%</Typography>
         </CircularProgress>
-        <Button size="sm" onClick={reset}>
-          Reset
+        <Button size="sm" variant="outlined" color="neutral" onClick={reset}>
+          Reload
         </Button>
       </Stack>
     </Box>
