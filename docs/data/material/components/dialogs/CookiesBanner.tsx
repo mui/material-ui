aefc6cd8d3747e51c66c4a7703fb9c25fef69e99
Olivier Tassinari <olivier.tassinari@gmail.com>
2023-09-03 01:48:40 +0200

    polish

--- docs/data/material/components/dialogs/CookiesBanner.tsx
+++ docs/data/material/components/dialogs/CookiesBanner.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Stack from '@mui/material/Stack';
 import TrapFocus from '@mui/material/Unstable_TrapFocus';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Paper from '@mui/material/Paper';
@@ -13,122 +13,96 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { CssBaseline } from '@mui/material';
 
-const Root = styled('div')(({ theme }) => ({
-  height: '100%',
-  backgroundColor: theme.palette.background.default,
-}));
-
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window?: () => Window;
-}
-
-export default function CookiesBanner(props: Props) {
-  const { window } = props;
-  const doc = window !== undefined ? window().document : null;
-
-  const [open, setOpen] = React.useState(false);
+export default function CookiesBanner() {
   const [bannerOpen, setBannerOpen] = React.useState(true);
-
-  const rootRef = React.useRef<HTMLDivElement | null>(null);
 
   const closeBanner = () => {
     setBannerOpen(false);
   };
 
-  React.useEffect(() => {
-    const contain = () => {
-      // sets the open prop of the TrapFocus to true if the active element is inside the cookie banner
-      if (rootRef.current && rootRef.current.contains(doc?.activeElement ?? null)) {
-        setOpen(true);
-      } else {
-        setOpen(false);
-      }
-    };
-
-    doc?.addEventListener('focusin', contain);
-
-    return () => {
-      doc?.removeEventListener('focusin', contain);
-    };
-  });
-
   return (
     <React.Fragment>
       <CssBaseline />
-      <Root sx={{ bgcolor: 'background.default', position: 'relative' }}>
-        <AppBar position="fixed">
-          <Toolbar>
-            <IconButton color="inherit">
-              <MenuIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Box sx={{ p: 1, pt: 10 }}>
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus
-            non enim praesent elementum facilisis leo vel. Risus at ultrices mi
-            tempus imperdiet.
-          </Typography>
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus
-            non enim praesent elementum facilisis leo vel. Risus at ultrices mi
-            tempus imperdiet.
-          </Typography>
-        </Box>
+      <AppBar position="fixed" component="nav">
+        <Toolbar>
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Container component="main" sx={{ pt: 3 }}>
+        <Toolbar />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+          imperdiet.
+        </Typography>
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+          imperdiet.
+        </Typography>
+      </Container>
+      <TrapFocus open disableAutoFocus disableEnforceFocus>
         <Fade appear={false} in={bannerOpen}>
-          <div>
-            <TrapFocus open={open} disableRestoreFocus>
-              <Paper
-                role="dialog"
-                aria-modal="false"
-                aria-label="Cookie banner"
-                ref={rootRef}
+          <Paper
+            role="dialog"
+            aria-modal="false"
+            aria-label="Cookie banner"
+            square
+            variant="outlined"
+            tabIndex={-1}
+            sx={{
+              position: 'fixed',
+              bottom: 0,
+              left: 0,
+              right: 0,
+              m: 0,
+              p: 2,
+              borderWidth: 0,
+              borderTopWidth: 1,
+            }}
+          >
+            <Stack
+              direction={{ xs: 'column', sm: 'row' }}
+              justifyContent="space-between"
+              gap={2}
+            >
+              <Box
                 sx={{
-                  position: 'absolute',
-                  bottom: 0,
-                  maxWidth: '100%',
-                  width: '100%',
-                  m: 0,
-                  p: 2,
-                  borderRadius: 0,
-                  borderTop: 1,
-                  borderColor: 'divider',
+                  flexShrink: 1,
+                  alignSelf: { xs: 'flex-start', sm: 'center' },
                 }}
               >
-                <Stack
-                  direction="row"
-                  alignItems="center"
-                  justifyContent="space-between"
-                  gap={2}
-                >
-                  <div>
-                    <Typography fontWeight="bold">
-                      This website uses cookies
-                    </Typography>
-                    <Typography variant="body2">
-                      example.com relies on cookies to improve your experience.
-                    </Typography>
-                  </div>
-                  <Stack direction="row" gap={1}>
-                    <Button size="small" onClick={closeBanner} variant="contained">
-                      Allow all
-                    </Button>
-                    <Button size="small" onClick={closeBanner} variant="text">
-                      Reject all
-                    </Button>
-                  </Stack>
-                </Stack>
-              </Paper>
-            </TrapFocus>
-          </div>
+                <Typography fontWeight="bold">This website uses cookies</Typography>
+                <Typography variant="body2">
+                  example.com relies on cookies to improve your experience.
+                </Typography>
+              </Box>
+              <Stack
+                gap={2}
+                direction={{
+                  xs: 'row-reverse',
+                  sm: 'row',
+                }}
+                sx={{
+                  flexShrink: 0,
+                  alignSelf: { xs: 'flex-end', sm: 'center' },
+                }}
+              >
+                <Button size="small" onClick={closeBanner} variant="contained">
+                  Allow all
+                </Button>
+                <Button size="small" onClick={closeBanner}>
+                  Reject all
+                </Button>
+              </Stack>
+            </Stack>
+          </Paper>
         </Fade>
-      </Root>
+      </TrapFocus>
     </React.Fragment>
   );
 }
