914d9adad7fed377d01ca2fa0c2243a7de2d1c76
anle9650 <andy.le@ucdenver.edu>
2023-09-03 00:22:48 -0600

    place icon at start

--- docs/data/material/components/buttons/InputFileUpload.tsx
+++ docs/data/material/components/buttons/InputFileUpload.tsx
@@ -22,7 +22,7 @@ export default function InputFileUpload() {
       tabIndex={-1}
       variant="outlined"
       color="primary"
-      endIcon={
+      startIcon={
         <SvgIcon>
           <svg
             xmlns="http://www.w3.org/2000/svg"
