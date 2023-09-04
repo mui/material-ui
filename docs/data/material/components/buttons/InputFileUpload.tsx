4d4b7b28086003b946ae7843900349b3085957cc
anle9650 <andy.le@ucdenver.edu>
2023-09-04 10:27:21 -0600

    use icon from package

--- docs/data/material/components/buttons/InputFileUpload.tsx
+++ docs/data/material/components/buttons/InputFileUpload.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
-import SvgIcon from '@mui/material/SvgIcon';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 const VisuallyHiddenInput = styled('input')`
   clip: rect(0 0 0 0);
@@ -20,23 +20,7 @@ export default function InputFileUpload() {
     <Button
       component="label"
       variant="contained"
-      startIcon={
-        <SvgIcon>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-            />
-          </svg>
-        </SvgIcon>
-      }
+      startIcon={<CloudUploadIcon />}
       href="#file-upload"
     >
       Upload a file
