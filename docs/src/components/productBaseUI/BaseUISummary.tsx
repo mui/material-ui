d834370de485964ce12251a01275e629f63c6b37
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-09-05 19:34:13 -0300

    summary section consistency with Base UI and tweaks

--- docs/src/components/productBaseUI/BaseUISummary.tsx
+++ docs/src/components/productBaseUI/BaseUISummary.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import StyleRoundedIcon from '@mui/icons-material/StyleRounded';
 import AccessibilityNewRounded from '@mui/icons-material/AccessibilityNewRounded';
 import PhishingRoundedIcon from '@mui/icons-material/PhishingRounded';
+import Section from 'docs/src/layouts/Section';
 import SectionHeadline from 'docs/src/components/typography/SectionHeadline';
 import GradientText from 'docs/src/components/typography/GradientText';
 import Link from 'docs/src/modules/components/Link';
@@ -36,7 +36,7 @@ const content = [
 
 export default function BaseUISummary() {
   return (
-    <Container sx={{ py: { xs: 6, sm: 10, md: 20 } }}>
+    <Section>
       <SectionHeadline
         alwaysCenter
         overline="Why Base UI"
@@ -48,71 +48,74 @@ export default function BaseUISummary() {
         }
         description="Base UI abstracts away the more frustrating aspects of UI development—like accessibility, cross-browser compatibility, and event handling—so you can skip ahead to design implementation."
       />
-      <Box sx={{ mt: 8 }}>
-        <Grid container spacing={3}>
-          {content.map(({ icon, title, description, link }) => (
-            <Grid key={title} item xs={12} md={4}>
-              <Paper
-                component={Link}
-                href={link}
-                noLinkStyle
-                variant="outlined"
-                sx={(theme) => ({
-                  p: 4,
-                  height: '100%',
+      <Grid container spacing={3} mt={2}>
+        {content.map(({ icon, title, description, link }) => (
+          <Grid key={title} item xs={12} md={4}>
+            <Paper
+              component={Link}
+              href={link}
+              noLinkStyle
+              variant="outlined"
+              sx={(theme) => ({
+                p: 4,
+                height: '100%',
+                background: `${(theme.vars || theme).palette.gradients.linearSubtle}`,
+                ...theme.applyDarkStyles({
+                  bgcolor: 'primaryDark.900',
                   background: `${(theme.vars || theme).palette.gradients.linearSubtle}`,
+                  borderColor: 'primaryDark.700',
+                }),
+              })}
+            >
+              <Box
+                sx={(theme) => ({
+                  width: 40,
+                  height: 40,
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: 1,
+                  border: '1px solid',
+                  borderColor: 'primary.200',
+                  bgcolor: 'primary.50',
+                  boxShadow:
+                    '0px 1px 6px 0px rgba(194, 224, 255, 1), 0px 2px 30px 0px rgba(234, 237, 241, 0.3) inset',
                   ...theme.applyDarkStyles({
-                    bgcolor: 'primaryDark.900',
-                    background: `${(theme.vars || theme).palette.gradients.linearSubtle}`,
-                    borderColor: 'primaryDark.700',
+                    borderColor: 'primary.400',
+                    bgcolor: 'primary.900',
+                    boxShadow:
+                      '0px 1px 6px 0px rgba(0, 89, 178, 1), 0px 2px 30px 0px rgba(0, 0, 0, 0.25) inset',
                   }),
                 })}
               >
-                <Box
-                  sx={(theme) => ({
-                    width: 40,
-                    height: 40,
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: 1,
-                    border: '1px solid',
-                    borderColor: 'primary.200',
-                    bgcolor: 'primary.50',
-                    boxShadow:
-                      '0px 1px 6px 0px rgba(194, 224, 255, 1), 0px 2px 30px 0px rgba(234, 237, 241, 0.3) inset',
-                    ...theme.applyDarkStyles({
-                      borderColor: 'primary.400',
-                      bgcolor: 'primary.900',
-                      boxShadow:
-                        '0px 1px 6px 0px rgba(0, 89, 178, 1), 0px 2px 30px 0px rgba(0, 0, 0, 0.25) inset',
-                    }),
-                  })}
-                >
-                  {icon}
-                </Box>
-                <Typography
-                  fontWeight="bold"
-                  component="h3"
-                  color="text.primary"
-                  variant="body2"
-                  mt={2}
-                  mb={0.5}
-                >
-                  {title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {description}
-                </Typography>
-              </Paper>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-      <Typography fontWeight={500} textAlign="center" mt={8} mb={2} fontSize="0.875rem">
+                {icon}
+              </Box>
+              <Typography
+                fontWeight="bold"
+                component="h3"
+                color="text.primary"
+                variant="body2"
+                mt={2}
+                mb={0.5}
+              >
+                {title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {description}
+              </Typography>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+      <Typography
+        variant="caption"
+        fontWeight="semiBold"
+        textAlign="center"
+        sx={{ width: '100%', mt: 8, mb: 2 }}
+      >
         Alternative to libraries such as:
       </Typography>
-      <Box sx={{ display: 'flex', gap: 2, justifyContent: 'center' }}>
+      <Box sx={{ display: 'flex', gap: 1, justifyContent: 'center' }}>
         <Box
           sx={(theme) => ({
             background: 'url(/static/branding/base-ui/radix.svg)',
@@ -146,6 +149,6 @@ export default function BaseUISummary() {
           height={37}
         />
       </Box>
-    </Container>
+    </Section>
   );
 }
--- docs/src/components/productJoyUI/JoyUISummary.tsx
+++ docs/src/components/productJoyUI/JoyUISummary.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
@@ -8,6 +7,7 @@ import InvertColorsRoundedIcon from '@mui/icons-material/InvertColorsRounded';
 import HandymanRoundedIcon from '@mui/icons-material/HandymanRounded';
 import ArticleRoundedIcon from '@mui/icons-material/ArticleRounded';
 import AccessibilityNewRounded from '@mui/icons-material/AccessibilityNewRounded';
+import Section from 'docs/src/layouts/Section';
 import SectionHeadline from 'docs/src/components/typography/SectionHeadline';
 import GradientText from 'docs/src/components/typography/GradientText';
 import Link from 'docs/src/modules/components/Link';
@@ -38,14 +38,14 @@ const content = [
     icon: <AccessibilityNewRounded fontSize="small" color="primary" />,
     title: 'Accessibility',
     description:
-      'Joy UI is built off of from MUI Base, which delivers several accessibility features from the get go.',
+      'Joy UI is built off of from Base UI, which delivers several built-in accessibility features.',
     link: '/base-ui/getting-started/quickstart/#components-and-hooks',
   },
 ];
 
 export default function JoyUISummary() {
   return (
-    <Container sx={{ py: { xs: 6, sm: 10, md: 20 } }}>
+    <Section>
       <SectionHeadline
         alwaysCenter
         overline="Why Joy UI"
@@ -57,71 +57,74 @@ export default function JoyUISummary() {
         }
         description="Joy UI components were extracted from Material UI, featuring the same robust engineering, and quickly trusted by many community members."
       />
-      <Box sx={{ mt: 8 }}>
-        <Grid container spacing={3}>
-          {content.map(({ icon, title, description, link }) => (
-            <Grid key={title} item xs={12} md={3}>
-              <Paper
-                component={Link}
-                href={link}
-                noLinkStyle
-                variant="outlined"
-                sx={(theme) => ({
-                  p: 4,
-                  height: '100%',
+      <Grid container spacing={3} mt={2}>
+        {content.map(({ icon, title, description, link }) => (
+          <Grid key={title} item xs={12} md={3}>
+            <Paper
+              component={Link}
+              href={link}
+              noLinkStyle
+              variant="outlined"
+              sx={(theme) => ({
+                p: 4,
+                height: '100%',
+                background: `${(theme.vars || theme).palette.gradients.linearSubtle}`,
+                ...theme.applyDarkStyles({
+                  bgcolor: 'primaryDark.900',
                   background: `${(theme.vars || theme).palette.gradients.linearSubtle}`,
+                  borderColor: 'primaryDark.700',
+                }),
+              })}
+            >
+              <Box
+                sx={(theme) => ({
+                  width: 40,
+                  height: 40,
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: 1,
+                  border: '1px solid',
+                  borderColor: 'primary.200',
+                  bgcolor: 'primary.50',
+                  boxShadow:
+                    '0px 1px 6px 0px rgba(194, 224, 255, 1), 0px 2px 30px 0px rgba(234, 237, 241, 0.3) inset',
                   ...theme.applyDarkStyles({
-                    bgcolor: 'primaryDark.900',
-                    background: `${(theme.vars || theme).palette.gradients.linearSubtle}`,
-                    borderColor: 'primaryDark.700',
+                    borderColor: 'primary.400',
+                    bgcolor: 'primary.900',
+                    boxShadow:
+                      '0px 1px 6px 0px rgba(0, 89, 178, 1), 0px 2px 30px 0px rgba(0, 0, 0, 0.25) inset',
                   }),
                 })}
               >
-                <Box
-                  sx={(theme) => ({
-                    width: 40,
-                    height: 40,
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: 1,
-                    border: '1px solid',
-                    borderColor: 'primary.200',
-                    bgcolor: 'primary.50',
-                    boxShadow:
-                      '0px 1px 6px 0px rgba(194, 224, 255, 1), 0px 2px 30px 0px rgba(234, 237, 241, 0.3) inset',
-                    ...theme.applyDarkStyles({
-                      borderColor: 'primary.400',
-                      bgcolor: 'primary.900',
-                      boxShadow:
-                        '0px 1px 6px 0px rgba(0, 89, 178, 1), 0px 2px 30px 0px rgba(0, 0, 0, 0.25) inset',
-                    }),
-                  })}
-                >
-                  {icon}
-                </Box>
-                <Typography
-                  fontWeight="bold"
-                  component="h3"
-                  color="text.primary"
-                  variant="body2"
-                  mt={2}
-                  mb={0.5}
-                >
-                  {title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {description}
-                </Typography>
-              </Paper>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-      <Typography fontWeight={500} textAlign="center" mt={8} mb={2} fontSize="0.875rem">
+                {icon}
+              </Box>
+              <Typography
+                fontWeight="bold"
+                component="h3"
+                color="text.primary"
+                variant="body2"
+                mt={2}
+                mb={0.5}
+              >
+                {title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {description}
+              </Typography>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+      <Typography
+        variant="caption"
+        fontWeight="semiBold"
+        textAlign="center"
+        sx={{ width: '100%', mt: 8, mb: 2 }}
+      >
         Alternative to libraries such as:
       </Typography>
-      <Box sx={{ display: 'flex', gap: 2, justifyContent: 'center' }}>
+      <Box sx={{ display: 'flex', gap: 1, justifyContent: 'center' }}>
         <Box
           sx={(theme) => ({
             background: 'url(/static/branding/joy-ui/chakra.svg)',
@@ -156,13 +159,16 @@ export default function JoyUISummary() {
         />
 
         <Box
-          sx={{
+          sx={(theme) => ({
             background: 'url(/static/branding/joy-ui/material-ui.svg)',
-          }}
-          width={120}
-          height={38}
+            ...theme.applyDarkStyles({
+              background: 'url(/static/branding/joy-ui/material-ui-dark.svg)',
+            }),
+          })}
+          width={121}
+          height={37}
         />
       </Box>
-    </Container>
+    </Section>
   );
 }
