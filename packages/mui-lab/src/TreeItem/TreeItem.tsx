67eb5ce17f51fdfdcdb71f55c69243d612e85c92
Michał Dudak <michal@mui.com>
2023-09-05 22:19:09 +0200

    Add missing api-docs-builder dependency

--- packages/mui-lab/src/TreeItem/TreeItem.tsx
+++ packages/mui-lab/src/TreeItem/TreeItem.tsx
@@ -37,7 +37,7 @@ const TreeItem = React.forwardRef(function DeprecatedTreeItem(
 TreeItem.propTypes /* remove-proptypes */ = {
   // ----------------------------- Warning --------------------------------
   // | These PropTypes are generated from the TypeScript type definitions |
-  // |     To update them edit TypeScript types and run "yarn proptypes"  |
+  // |     To update them edit TypeScript types and run "pnpm proptypes"  |
   // ----------------------------------------------------------------------
   /**
    * The content of the component.
--- packages/mui-lab/src/TreeView/TreeView.tsx
+++ packages/mui-lab/src/TreeView/TreeView.tsx
@@ -37,7 +37,7 @@ const TreeView = React.forwardRef(function DeprecatedTreeView(
 TreeView.propTypes /* remove-proptypes */ = {
   // ----------------------------- Warning --------------------------------
   // | These PropTypes are generated from the TypeScript type definitions |
-  // |     To update them edit TypeScript types and run "yarn proptypes"  |
+  // |     To update them edit TypeScript types and run "pnpm proptypes"  |
   // ----------------------------------------------------------------------
   /**
    * The content of the component.
