e9bd1830dc4feb4ee1eacbe02d9513ab5618cceb
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-09-05 16:47:35 -0500

    [docs][material-ui] Revise and split up "Styled engine" doc (#37774)
    Signed-off-by: Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
    Co-authored-by: Olivier Tassinari <olivier.tassinari@gmail.com>
    Co-authored-by: Brijesh Bittu <brijeshb42@gmail.com>

--- docs/pages/material-ui/guides/styled-components.js
+++ docs/pages/material-ui/guides/styled-components.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
-import * as pageProps from 'docs/data/material/guides/styled-engine/styled-engine.md?@mui/markdown';
+import * as pageProps from 'docs/data/material/guides/styled-components/styled-components.md?@mui/markdown';
 
 export default function Page() {
   return <MarkdownDocs {...pageProps} />;
