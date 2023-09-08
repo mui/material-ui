508ec0d1a359f0fd91ab4a3b082e2358866fd126
zanivan <victorzanivan@gmail.com>
2023-09-08 15:05:41 -0300

    Fix lint

--- docs/data/joy/getting-started/templates/messages/components/Header.tsx
+++ docs/data/joy/getting-started/templates/messages/components/Header.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import GlobalStyles from '@mui/joy/GlobalStyles';
 import IconButton from '@mui/joy/IconButton';
 import Sheet from '@mui/joy/Sheet';
-import MuiLogo from './MuiLogo';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import MuiLogo from './MuiLogo';
 import ColorSchemeToggle from './ColorSchemeToggle';
 import { toggleSidebar } from '../utils';
 
