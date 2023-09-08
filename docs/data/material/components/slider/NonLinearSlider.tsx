6f0f84b8f8b552ac3cc7a23b4f3067af7d36959c
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-09-08 18:48:00 +0530

    address diego review - remove unnecessary checks

--- docs/data/material/components/slider/NonLinearSlider.tsx
+++ docs/data/material/components/slider/NonLinearSlider.tsx
@@ -25,9 +25,7 @@ export default function NonLinearSlider() {
   const [value, setValue] = React.useState(10);
 
   const handleChange = (event: Event, newValue: number) => {
-    if (typeof newValue === 'number') {
-      setValue(newValue);
-    }
+    setValue(newValue);
   };
 
   return (
