95e41fd90e6eab800842ceb96e4f77c059b64f76
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2023-09-08 19:55:45 +0530

    [ButtonGroup] Determine first, last and middle buttons to support different elements with correct styling (#38520)

---
+++ test/regressions/fixtures/ButtonGroup/DifferentChildren.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Stack from '@mui/material/Stack';
+import Tooltip from '@mui/material/Tooltip';
+
+export default function DifferentChildren() {
+  return (
+    <Stack spacing={2}>
+      {/* It has one button with href which is rendered as anchor tag */}
+      <ButtonGroup variant="contained">
+        <Button href="##">Button 1</Button>
+        <Button>Button 2</Button>
+        <Button>Button 3</Button>
+      </ButtonGroup>
+
+      {/* With tooltip */}
+      <ButtonGroup>
+        <Tooltip title="tooltip">
+          <Button>Enabled</Button>
+        </Tooltip>
+        <Tooltip title="tooltip">
+          <span>
+            <Button disabled>Disabled</Button>
+          </span>
+        </Tooltip>
+        <Tooltip title="tooltip">
+          <span>
+            <Button disabled>Disabled</Button>
+          </span>
+        </Tooltip>
+      </ButtonGroup>
+
+      {/* Single button */}
+      <ButtonGroup>
+        <Button>Single Button</Button>
+      </ButtonGroup>
+    </Stack>
+  );
+}
