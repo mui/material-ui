5c1281b4001c17d1d59b4aaab8a7504f9e265299
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-09-11 08:28:05 -0300

    fix inifinite scrolling animation by putting back the duped container

--- docs/src/components/about/AboutHero.tsx
+++ docs/src/components/about/AboutHero.tsx
@@ -135,6 +135,12 @@ function PhotoGallery() {
           />
         ))}
       </ImageContainer>
+      <ImageContainer aria-hidden="true">
+        {/* aria-hidden is used here because this element is a copy from the above, meaning we want to hide it from screen readers. */}
+        {teamPhotos.map((item, index) => (
+          <Image key={index} src={item.img} alt={item.title} loading="lazy" />
+        ))}
+      </ImageContainer>
     </Box>
   );
 }
