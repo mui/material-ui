0fa7a0468192902d523225ab4f3280216b96cb0b
zanivan <victorzanivan@gmail.com>
2023-09-11 17:38:13 -0300

    Refined Modal demos

--- docs/data/base/components/modal/ServerModal.tsx
+++ docs/data/base/components/modal/ServerModal.tsx
@@ -29,16 +29,31 @@ export default function ServerModal() {
         aria-describedby="server-modal-description"
         container={() => rootRef.current}
       >
-        <Box sx={style}>
-          <h2 id="server-modal-title">Server-side modal</h2>
-          <span id="server-modal-description">
+        <ModalContent sx={style}>
+          <h3 id="server-modal-title" className="modal-title">
+            Server-side modal
+          </h3>
+          <span id="server-modal-description" className="modal-description">
             If you disable JavaScript, you will still see me.
           </span>
-        </Box>
+        </ModalContent>
       </StyledModal>
     </Box>
   );
 }
+
+const grey = {
+  50: '#f6f8fa',
+  100: '#eaeef2',
+  200: '#d0d7de',
+  300: '#afb8c1',
+  400: '#8c959f',
+  500: '#6e7781',
+  600: '#57606a',
+  700: '#424a53',
+  800: '#32383f',
+  900: '#24292f',
+};
 
 const StyledModal = styled(Modal)`
   position: fixed;
@@ -55,8 +70,39 @@ const StyledModal = styled(Modal)`
 const style = (theme: Theme) => ({
   position: 'relative',
   width: 400,
-  borderRadius: '12px',
-  padding: '16px 32px 24px 32px',
-  backgroundColor: theme.palette.mode === 'dark' ? '#132F4C' : 'white',
-  boxShadow: `0px 2px 8px ${theme.palette.mode === 'dark' ? '#000' : '#BCBCBC'}`,
 });
+
+const ModalContent = styled('div')(
+  ({ theme }) => `
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  overflow: hidden;
+  background-color: ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+  border-radius: 8px;
+  border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+  box-shadow: 0px 4px 12px ${
+    theme.palette.mode === 'dark' ? 'rgba(0,0,0, 0.50)' : 'rgba(0,0,0, 0.1)'
+  };
+  padding: 1rem;
+  color: ${theme.palette.mode === 'dark' ? grey[50] : grey[900]};
+  font-family: IBM Plex Sans, sans-serif;
+  font-weight: 500;
+  text-align: start;
+  position: relative;
+
+
+  & .modal-title {
+    margin: 0;
+    line-height: 1.5rem;
+    margin-right: 0.5rem;
+  }
+
+  & .modal-description {
+    margin: 0;
+    line-height: 1.5rem;
+    font-weight: 400;
+    color: ${theme.palette.mode === 'dark' ? grey[400] : grey[800]};
+  }
+  `,
+);
