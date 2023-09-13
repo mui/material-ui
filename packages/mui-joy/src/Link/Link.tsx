2252e1737a523efa207eed1fee1c715086c0a27e
Diego Andai <diego@mui.com>
2023-09-13 14:15:57 -0300

    Rever Joy Link change

--- packages/mui-joy/src/Link/Link.tsx
+++ packages/mui-joy/src/Link/Link.tsx
@@ -133,11 +133,9 @@ const LinkRoot = styled('a', {
               }),
             },
           }),
+      userSelect: 'none',
       MozAppearance: 'none', // Reset
       WebkitAppearance: 'none', // Reset
-      '&:active': {
-        userSelect: 'none',
-      },
       '&::-moz-focus-inner': {
         borderStyle: 'none', // Remove Firefox dotted outline.
       },
