0ff8890f403ecbea9fb5bdd062fb8b1f9217fbfe
siriwatknp <siriwatkunaporn@gmail.com>
2023-09-15 19:41:01 +0700

    skip visual regression

--- test/regressions/index.js
+++ test/regressions/index.js
@@ -32,6 +32,7 @@ const blacklist = [
   'docs-joy-tools/PaletteThemeViewer.png', // No need for theme tokens
   'docs-joy-tools/ShadowThemeViewer.png', // No need for theme tokens
   'docs-joy-customization-theme-typography/TypographyThemeViewer.png', // No need for theme tokens
+  'docs-joy-components-circular-progress/CircularProgressCountUp.png', // Flaky due to animation
   'docs-joy-components-divider/DividerChildPosition.png', // Needs interaction
   'docs-joy-components-linear-progress/LinearProgressCountUp.png', // Flaky due to animation
   'docs-base-guides-working-with-tailwind-css/PlayerFinal.png', // No public components
