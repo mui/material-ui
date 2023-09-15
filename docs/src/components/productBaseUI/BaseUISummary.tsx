e8e98dc92fb36e769efb396d8c5e6922b8671fd2
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-09-15 14:54:13 -0300

    [website] Create the `InfoCard` component (#38987)

--- docs/src/components/productBaseUI/BaseUISummary.tsx
+++ docs/src/components/productBaseUI/BaseUISummary.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import StyleRoundedIcon from '@mui/icons-material/StyleRounded';
@@ -9,7 +8,7 @@ import PhishingRoundedIcon from '@mui/icons-material/PhishingRounded';
 import Section from 'docs/src/layouts/Section';
 import SectionHeadline from 'docs/src/components/typography/SectionHeadline';
 import GradientText from 'docs/src/components/typography/GradientText';
-import Link from 'docs/src/modules/components/Link';
+import InfoCard from 'docs/src/components/action/InfoCard';
 
 const content = [
   {
@@ -48,71 +47,16 @@ export default function BaseUISummary() {
         }
         description="Base UI abstracts away the more frustrating aspects of UI development—like accessibility, cross-browser compatibility, and event handling—so you can skip ahead to design implementation."
       />
-      <Grid container spacing={3} mt={2}>
-        {content.map(({ icon, title, description, link }) => (
-          <Grid key={title} item xs={12} md={4}>
-            <Paper
-              component={Link}
-              href={link}
-              noLinkStyle
-              variant="outlined"
-              sx={(theme) => ({
-                p: 4,
-                height: '100%',
-                background: `${(theme.vars || theme).palette.gradients.linearSubtle}`,
-                ...theme.applyDarkStyles({
-                  bgcolor: 'primaryDark.900',
-                  background: `${(theme.vars || theme).palette.gradients.linearSubtle}`,
-                  borderColor: 'primaryDark.700',
-                }),
-              })}
-            >
-              <Box
-                sx={(theme) => ({
-                  width: 40,
-                  height: 40,
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  borderRadius: 1,
-                  border: '1px solid',
-                  borderColor: 'primary.200',
-                  bgcolor: 'primary.50',
-                  boxShadow:
-                    '0px 1px 6px 0px rgba(194, 224, 255, 1), 0px 2px 30px 0px rgba(234, 237, 241, 0.3) inset',
-                  ...theme.applyDarkStyles({
-                    borderColor: 'primary.400',
-                    bgcolor: 'primary.900',
-                    boxShadow:
-                      '0px 1px 6px 0px rgba(0, 89, 178, 1), 0px 2px 30px 0px rgba(0, 0, 0, 0.25) inset',
-                  }),
-                })}
-              >
-                {icon}
-              </Box>
-              <Typography
-                fontWeight="bold"
-                component="h3"
-                color="text.primary"
-                variant="body2"
-                mt={2}
-                mb={0.5}
-              >
-                {title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {description}
-              </Typography>
-            </Paper>
-          </Grid>
-        ))}
-      </Grid>
-      <Typography
-        variant="caption"
-        fontWeight="semiBold"
-        textAlign="center"
-        sx={{ width: '100%', mt: 8, mb: 2 }}
-      >
+      <Box sx={{ mt: 8 }}>
+        <Grid container spacing={3}>
+          {content.map(({ icon, title, description, link }) => (
+            <Grid key={title} item xs={12} md={4}>
+              <InfoCard link={link} title={title} icon={icon} description={description} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+      <Typography fontWeight={500} textAlign="center" mt={8} mb={2} fontSize="0.875rem">
         Alternative to libraries such as:
       </Typography>
       <Box sx={{ display: 'flex', gap: 1, justifyContent: 'center' }}>
