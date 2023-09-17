8ca4b4d1e5ebc95a5edb734a06ce77e825921b63
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-09-17 10:32:41 +0530

    argos fix

--- packages/mui-joy/src/Button/Button.tsx
+++ packages/mui-joy/src/Button/Button.tsx
@@ -157,8 +157,11 @@ export const getButtonStyles = ({
       },
       '&:active, &[aria-pressed="true"]':
         theme.variants[`${ownerState.variant!}Active`]?.[ownerState.color!],
-      ...(ownerState.disabled &&
-        theme.variants[`${ownerState.variant!}Disabled`]?.[ownerState.color!]),
+      '&:disabled':
+        theme.variants[`${ownerState.variant!}Disabled`]?.[ownerState.color!],
+      // ...(ownerState.disabled &&
+      //   theme.variants[`${ownerState.variant!}Disabled`]?.[ownerState.color!]),
+
       ...(ownerState.loadingPosition === 'center' && {
         // this has to come after the variant styles to take effect.
         [`&.${buttonClasses.loading}`]: {
