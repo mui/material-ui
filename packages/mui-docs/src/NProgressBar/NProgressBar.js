6a117405dcc03fd0f229b63109412cbaebc4b8eb
mnajdova <mnajdova@gmail.com>
2023-09-18 16:56:31 +0200

    wip

--- packages/mui-docs/src/NProgressBar/NProgressBar.js
+++ packages/mui-docs/src/NProgressBar/NProgressBar.js
@@ -4,7 +4,7 @@ import NProgress from 'nprogress';
 import { NoSsr } from '@mui/base/NoSsr';
 import { exactProp } from '@mui/utils';
 import GlobalStyles from '@mui/material/GlobalStyles';
-import { keyframes } from '@mui/material/styles';
+import { keyframes, css } from '@mui/material/styles';
 
 NProgress.configure({
   barSelector: '.nprogress-bar',
@@ -30,6 +30,10 @@ const muiNProgressPulse = keyframes`
   }
 `;
 
+const animation = css`
+  ${muiNProgressPulse} 2s ease-out 0s infinite
+`
+
 /**
  * Elegant and ready-to-use wrapper on top of https://github.com/rstacruz/nprogress/.
  * The implementation is highly inspired by the YouTube one.
@@ -38,52 +42,55 @@ function NProgressBar(props) {
   return (
     <NoSsr>
       {props.children}
-      <GlobalStyles
-        styles={(theme) => ({
-          '#nprogress': {
-            direction: 'ltr',
-            pointerEvents: 'none',
+      {/* Error: It seems you are interpolating a keyframe declaration (ikWivX) into an untagged string. This was supported in styled-components v3, but is not longer supported in v4 as keyframes are now injected on-demand. Please wrap your string in the css\`\` helper which ensures the styles are injected correctly. See https://www.styled-components.com/docs/api#css */}
+      {/* <GlobalStyles
+        styles={`
+          #nprogress {
+            direction: ltr;
+            pointerEvents: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            height: 2;
+            zIndex: ${(theme) => (theme.vars || theme).zIndex.tooltip};
+            backgroundColor: ${(theme) => (theme.vars || theme).palette.primary[200]};
+          }
+          :where([data-mui-color-scheme="light|dark"]) & {
+            #nprogress {
+              backgroundColor: ${theme => (theme.vars || theme).palette.primary[700]};
+            }
+          }
+          #nprogress .nprogress-bar {
             position: 'fixed',
+            backgroundColor: ${theme => (theme.vars || theme).palette.primary.main};
             top: 0,
             left: 0,
             right: 0,
             height: 2,
-            zIndex: (theme.vars || theme).zIndex.tooltip,
-            backgroundColor: (theme.vars || theme).palette.primary[200],
-            ...theme.applyDarkStyles({
-              backgroundColor: (theme.vars || theme).palette.primary[700],
-            }),
-            '& .nprogress-bar': {
-              position: 'fixed',
-              backgroundColor: (theme.vars || theme).palette.primary.main,
-              top: 0,
-              left: 0,
-              right: 0,
-              height: 2,
-            },
-            '& .nprogress-bar > div': {
-              position: 'absolute',
-              top: 0,
-              height: 2,
-              boxShadow: `${(theme.vars || theme).palette.primary.main} 1px 0 6px 1px`,
-              borderRadius: '100%',
-              animation: `${muiNProgressPulse} 2s ease-out 0s infinite`,
-            },
-            '& .nprogress-bar > div:first-of-type': {
-              opacity: 0.6,
-              width: 20,
-              right: 0,
-              clip: 'rect(-6px,22px,14px,10px)',
-            },
-            '& .nprogress-bar > div:last-of-type': {
-              opacity: 0.6,
-              width: 180,
-              right: -80,
-              clip: 'rect(-6px,90px,14px,-6px)',
-            },
-          },
-        })}
-      />
+          };
+          #nprogress .nprogress-bar > div {
+            position: 'absolute',
+            top: 0,
+            height: 2,
+            boxShadow: ${theme => (theme.vars || theme).palette.primary.main} 1px 0 6px 1px;
+            borderRadius: '100%';
+            animation: ${animation};
+          };
+          #nprogress .nprogress-bar > div:first-of-type {
+            opacity: 0.6,
+            width: 20,
+            right: 0,
+            clip: 'rect(-6px,22px,14px,10px)',
+          };
+          #nprogress .nprogress-bar > div:last-of-type {
+            opacity: 0.6,
+            width: 180,
+            right: -80,
+            clip: 'rect(-6px,90px,14px,-6px)',
+          };
+        }`}
+      /> */}
     </NoSsr>
   );
 }
