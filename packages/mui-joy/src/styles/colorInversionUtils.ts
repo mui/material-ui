5bfe6b6000b8e9b5f6e133008b2093a4a49f817e
siriwatknp <siriwatkunaporn@gmail.com>
2023-09-19 19:00:23 +0700

    use data-skip-inverted-colors

--- packages/mui-joy/src/styles/colorInversionUtils.ts
+++ packages/mui-joy/src/styles/colorInversionUtils.ts
@@ -15,7 +15,7 @@ const createPrefixVar = (cssVarPrefix: string | undefined | null) => {
 };
 
 export const INVERTED_COLORS_SELECTOR =
-  '& :not([data-inverted-colors="false"]):not([data-inverted-colors="false"] *)';
+  '& :not([data-skip-inverted-colors]):not([data-skip-inverted-colors] *)';
 
 /**
  *
