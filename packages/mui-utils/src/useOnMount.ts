cde630dc35171c55bfd5f8638baf9590fbacb501
Rom Grk <romgrk.cc@gmail.com>
2023-09-19 12:03:35 -0400

    lint

--- packages/mui-utils/src/useOnMount.ts
+++ packages/mui-utils/src/useOnMount.ts
@@ -6,6 +6,6 @@ const EMPTY = [] as unknown[];
  * A React.useEffect equivalent that runs once, when the component is mounted.
  */
 export default function useOnMount(fn: React.EffectCallback) {
-  // eslint-disable-next-lines react-hooks/exhaustive-deps
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   React.useEffect(fn, EMPTY);
 }
