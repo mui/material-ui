3cce61e3d48d667be5a9661039986f097061d935
Marco Falkenberg <mfal@users.noreply.github.com>
2023-09-20 21:58:43 +0200

    Use correct loading indicator component in code example
    Use Joys `<CircularProgress />` instead of Material UIs `<SendIcon />` in the loading indicator code example.
    
    Signed-off-by: Marco Falkenberg <mfal@users.noreply.github.com>

--- docs/data/joy/components/button/ButtonLoadingIndicator.js
+++ docs/data/joy/components/button/ButtonLoadingIndicator.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import Stack from '@mui/joy/Stack';
-import SendIcon from '@mui/icons-material/Send';
+import CircularProgress from '@mui/joy/CircularProgress';
 import Button from '@mui/joy/Button';
 
 export default function ButtonLoadingIndicator() {
   return (
     <Stack spacing={2} direction="row">
-      <Button loading endDecorator={<SendIcon />} variant="solid">
+      <Button loading endDecorator={<CircularProgress />} variant="solid">
         Send
       </Button>
       <Button loading loadingIndicator="Loading…" variant="outlined">
