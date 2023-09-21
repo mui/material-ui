1f4d0573c5406e5e062fa3c8a1cf1691b18eca86
Albert Yu <albert@albertyu.co>
2023-09-21 15:25:44 +0800

    Update proptypes

--- packages/mui-material-next/src/FormControl/FormControl.tsx
+++ packages/mui-material-next/src/FormControl/FormControl.tsx
@@ -289,6 +289,11 @@ FormControl.propTypes /* remove-proptypes */ = {
     PropTypes.string,
   ]),
   /**
+   * The component used for the root node.
+   * Either a string to use a HTML element or a component.
+   */
+  component: PropTypes.elementType,
+  /**
    * If `true`, the label, input and helper text should be displayed in a disabled state.
    * @default false
    */
