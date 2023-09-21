6f7babf4025ddbc521d513a047c00837f3fafb09
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-09-21 11:48:06 +0530

    fix

--- packages/mui-material/src/DialogActions/DialogActions.js
+++ packages/mui-material/src/DialogActions/DialogActions.js
@@ -33,7 +33,7 @@ const DialogActionsRoot = styled('div', {
   flex: '0 0 auto',
   ...(!ownerState.disableSpacing && {
     '& > :not(style)': {
-      margin: 0,
+      marginLeft: 0,
     },
     '& > :not(style) ~ :not(style)': {
       marginLeft: '8px',
