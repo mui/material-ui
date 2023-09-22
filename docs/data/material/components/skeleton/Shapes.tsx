dc635f77bc018ba7c62645a2e0b55080811a0e4f
Diego Andai <diego@mui.com>
2023-09-22 11:52:51 -0300

    Add missing box size to examples
    Signed-off-by: Diego Andai <diego@mui.com>

--- docs/data/material/components/skeleton/Shapes.tsx
+++ docs/data/material/components/skeleton/Shapes.tsx
@@ -6,8 +6,8 @@ export default function Shapes() {
   return (
     <Stack spacing={1}>
       <Skeleton shape="circular" size="box" width={40} height={40} />
-      <Skeleton shape="rectangular" width={210} height={60} />
-      <Skeleton shape="rounded" width={210} height={60} />
+      <Skeleton shape="rectangular" size="box" width={210} height={60} />
+      <Skeleton shape="rounded"size="box" width={210} height={60} />
     </Stack>
   );
 }
