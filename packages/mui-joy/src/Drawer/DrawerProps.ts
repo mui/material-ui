19c4d9861f9e6d65fc6df9acab66ed1baf6363af
siriwatknp <siriwatkunaporn@gmail.com>
2023-09-22 15:18:10 +0700

    fix dep cycle

--- packages/mui-joy/src/Drawer/DrawerProps.ts
+++ packages/mui-joy/src/Drawer/DrawerProps.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { OverridableStringUnion, OverrideProps } from '@mui/types';
 import { ColorPaletteProp, VariantProp, ApplyColorInversion } from '../styles/types';
-import { ModalOwnProps } from '../Modal';
+import { ModalOwnProps } from '../Modal/ModalProps';
 import { CreateSlotsAndSlotProps, SlotProps } from '../utils/types';
 
 export type DrawerSlot = 'root' | 'backdrop' | 'content';
