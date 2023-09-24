fa44be3c548f169fd456ff0ee27e7493ab07bc78
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2023-09-24 17:59:10 +0200

    Update docs/src/modules/components/AppLayoutDocsFooter.js
    Co-authored-by: Olivier Tassinari <olivier.tassinari@gmail.com>
    Signed-off-by: Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>

--- docs/src/modules/components/AppLayoutDocsFooter.js
+++ docs/src/modules/components/AppLayoutDocsFooter.js
@@ -109,7 +109,7 @@ async function postFeedbackOnSlack(data) {
     currentLocationURL: window.location.href,
     commmentSectionURL: `${window.location.origin}${window.location.pathname}#${commentedSection.hash}`,
     commmentSectionTitle: commentedSection.text,
-    githubRepo: process.env.SOURCE_CODE_REPO ?? 'https://github.com/mui/material-ui',
+    githubRepo: process.env.SOURCE_CODE_REPO,
   };
   if (!comment || comment.length < 10) {
     return 'ignored';
