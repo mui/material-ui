541c3fdea9beb384ef1a7fc17089f6cbcaf29462
zanivan <victorzanivan@gmail.com>
2023-09-27 14:48:24 -0300

    Change TriggerButton of Modal demos

--- docs/data/base/components/modal/TransitionsModal.tsx
+++ docs/data/base/components/modal/TransitionsModal.tsx
@@ -47,8 +47,12 @@ const Backdrop = React.forwardRef<HTMLDivElement, { open?: boolean }>(
 );
 
 const blue = {
+  200: '#99CCFF',
+  300: '#66B2FF',
+  400: '#3399FF',
   500: '#007FFF',
-  600: '#0072E6',
+  600: '#0072E5',
+  700: '#0066CC',
 };
 
 const grey = {
@@ -124,8 +128,9 @@ const ModalContent = styled('div')(
   `,
 );
 
-const TriggerButton = styled(Button)`
-  font-family: 'IBM Plex Sans', sans-serif;
+const TriggerButton = styled(Button)(
+  ({ theme }) => `
+  font-family: IBM Plex Sans, sans-serif;
   font-weight: 600;
   font-size: 0.875rem;
   line-height: 1.5;
@@ -135,14 +140,32 @@ const TriggerButton = styled(Button)`
   color: white;
   transition: all 150ms ease;
   cursor: pointer;
-  border: none;
+  border: 1px solid ${blue[500]};
+  box-shadow: 0 2px 1px ${
+    theme.palette.mode === 'dark' ? 'rgba(0, 0, 0, 0.5)' : 'rgba(45, 45, 60, 0.2)'
+  }, inset 0 1.5px 1px ${blue[400]}, inset 0 -2px 1px ${blue[600]};
 
   &:hover {
     background-color: ${blue[600]};
   }
 
-  &:focus-visible {
-    box-shadow: 0 3px 20px 0 rgb(61 71 82 / 0.1), 0 0 0 5px rgb(0 127 255 / 0.5);
+  &.active {
+    background-color: ${blue[700]};
+    box-shadow: none;
+  }
+
+  &.focusVisible {
+    box-shadow: 0 0 0 4px ${theme.palette.mode === 'dark' ? blue[300] : blue[200]};
     outline: none;
   }
-`;
+
+  &.disabled {
+    opacity: 0.4;
+    cursor: not-allowed;
+    box-shadow: none;
+    &:hover {
+      background-color: ${blue[500]};
+    }
+  }
+`,
+);
