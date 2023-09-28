92fe002923ad6a174ecbb607f67e7d77869a527f
Albert Yu <albert@albertyu.co>
2023-09-28 17:39:41 +0800

    Copy v5 FormHelperText

--- packages/mui-material-next/src/index.ts
+++ packages/mui-material-next/src/index.ts
@@ -18,6 +18,9 @@ export * from './FilledInput';
 export { default as FormControl } from './FormControl';
 export * from './FormControl';
 
+export { default as FormHelperText } from './FormHelperText';
+export * from './FormHelperText';
+
 export { default as FormLabel } from './FormLabel';
 export * from './FormLabel';
 
