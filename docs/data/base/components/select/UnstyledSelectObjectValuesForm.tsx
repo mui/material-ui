05318f38d8cba65d48e384abe000e1510f0a7a09
zanivan <victorzanivan@gmail.com>
2023-09-28 14:44:00 -0300

    Update Button style on Select demo

--- docs/data/base/components/select/UnstyledSelectObjectValuesForm.tsx
+++ docs/data/base/components/select/UnstyledSelectObjectValuesForm.tsx
@@ -109,9 +109,11 @@ const characters: Character[] = [
 const blue = {
   100: '#DAECFF',
   200: '#99CCF3',
+  300: '#66B2FF',
   400: '#3399FF',
   500: '#007FFF',
   600: '#0072E5',
+  700: '#0066CC',
   900: '#003A75',
 };
 
@@ -241,22 +243,44 @@ const Label = styled('label')(
   `,
 );
 
-const Button = styled('button')`
-  font-family: 'IBM Plex Sans', sans-serif;
+const Button = styled('button')(
+  ({ theme }) => `
+  font-family: IBM Plex Sans, sans-serif;
   font-weight: 600;
   font-size: 0.875rem;
+  line-height: 1.5;
   background-color: ${blue[500]};
   padding: 8px 16px;
-  line-height: 1.5;
   border-radius: 8px;
   color: white;
   transition: all 150ms ease;
   cursor: pointer;
-  border: none;
-  vertical-align: middle;
-  min-height: calc(1em + 25px);
+  border: 1px solid ${blue[500]};
+  box-shadow: 0 2px 1px ${
+    theme.palette.mode === 'dark' ? 'rgba(0, 0, 0, 0.5)' : 'rgba(45, 45, 60, 0.2)'
+  }, inset 0 1.5px 1px ${blue[400]}, inset 0 -2px 1px ${blue[600]};
 
   &:hover {
     background-color: ${blue[600]};
   }
-`;
+
+  &.active {
+    background-color: ${blue[700]};
+    box-shadow: none;
+  }
+
+  &.focusVisible {
+    box-shadow: 0 0 0 4px ${theme.palette.mode === 'dark' ? blue[300] : blue[200]};
+    outline: none;
+  }
+
+  &.disabled {
+    opacity: 0.4;
+    cursor: not-allowed;
+    box-shadow: none;
+    &:hover {
+      background-color: ${blue[500]};
+    }
+  }
+`,
+);
