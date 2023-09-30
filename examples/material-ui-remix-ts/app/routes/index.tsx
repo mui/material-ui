1805e97e8516f95fae0324590a9ee14ede612c3f
Yukiya Nakagawa <yukiya@monicle.co.jp>
2023-09-30 21:03:23 +0900

    [examples][remix-ts] Replace to Remix v2 MetaFunction
    https://remix.run/docs/en/main/start/v2#route-meta

--- examples/material-ui-remix-ts/app/routes/index.tsx
+++ examples/material-ui-remix-ts/app/routes/index.tsx
@@ -4,13 +4,11 @@ import { Link as RemixLink } from '@remix-run/react';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 
-// https://remix.run/api/conventions#meta
-export const meta: MetaFunction = () => {
-  return {
-    title: 'Remix Starter',
-    description: 'Welcome to remix!',
-  };
-};
+// https://remix.run/docs/en/main/route/meta
+export const meta: MetaFunction = () => [
+  { title: 'Remix Starter' },
+  { name: 'description', content: 'Welcome to remix!' },
+];
 
 // https://remix.run/docs/en/main/file-conventions/routes#basic-routes
 export default function Index() {
