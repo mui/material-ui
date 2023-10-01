b2ae353d82aceba2613d73fb87becc261e3df626
NmN03jain <njain5587@gmail.com>
2023-10-01 19:51:05 +0530

    Standard input not loading correctly #39157 Fixed this issue

--- docs/data/material/components/text-fields/BasicTextFields.js
+++ docs/data/material/components/text-fields/BasicTextFields.js
@@ -13,8 +13,27 @@ export default function BasicTextFields() {
       autoComplete="off"
     >
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <TextField
+        id="filled-basic"
+        label="Filled"
+        variant="filled"
+        sx={{ // Add custom styles for the filled variant
+          '& .MuiFilledInput-root': {
+            backgroundColor: 'inherit', // Set the background color to inherit
+          },
+        }}
+      />
+      <TextField
+        id="standard-basic"
+        label="Standard"
+        variant="standard"
+        sx={{ // Add custom styles for the standard variant
+          '& .MuiInputBase-root': {
+            border: '1px solid #e0e0e0', // Set the border for the standard variant
+          },
+        }}
+      />
     </Box>
   );
 }
+
