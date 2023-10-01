7794fc2b181655d054be1a16c1cbccf909e3f6d5
NmN03jain <njain5587@gmail.com>
2023-10-01 20:27:45 +0530

    Standard input not loading correctly #39157 fixed issue

--- docs/data/material/components/text-fields/BasicTextFields.js
+++ docs/data/material/components/text-fields/BasicTextFields.js
@@ -2,38 +2,28 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
+const filledTextFieldStyles = {
+  '& .MuiFilledInput-root': {
+    backgroundColor: 'inherit',
+  },
+};
+
+const standardTextFieldStyles = {
+  '& .MuiInputBase-root': {
+    border: '1px solid #e0e0e0',
+  },
+};
+
 export default function BasicTextFields() {
   return (
     <Box
       component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
       noValidate
       autoComplete="off"
     >
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField
-        id="filled-basic"
-        label="Filled"
-        variant="filled"
-        sx={{ // Add custom styles for the filled variant
-          '& .MuiFilledInput-root': {
-            backgroundColor: 'inherit', // Set the background color to inherit
-          },
-        }}
-      />
-      <TextField
-        id="standard-basic"
-        label="Standard"
-        variant="standard"
-        sx={{ // Add custom styles for the standard variant
-          '& .MuiInputBase-root': {
-            border: '1px solid #e0e0e0', // Set the border for the standard variant
-          },
-        }}
-      />
+      <TextField id="filled-basic" label="Filled" variant="filled" style={filledTextFieldStyles} />
+      <TextField id="standard-basic" label="Standard" variant="standard" style={standardTextFieldStyles} />
     </Box>
   );
 }
-
--- docs/data/material/components/text-fields/BasicTextFields.tsx
+++ docs/data/material/components/text-fields/BasicTextFields.tsx
@@ -2,19 +2,28 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
+const filledTextFieldStyles = {
+  '& .MuiFilledInput-root': {
+    backgroundColor: 'inherit',
+  },
+};
+
+const standardTextFieldStyles = {
+  '& .MuiInputBase-root': {
+    border: '1px solid #e0e0e0',
+  },
+};
+
 export default function BasicTextFields() {
   return (
     <Box
       component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
       noValidate
       autoComplete="off"
     >
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <TextField id="filled-basic" label="Filled" variant="filled" style={filledTextFieldStyles} />
+      <TextField id="standard-basic" label="Standard" variant="standard" style={standardTextFieldStyles} />
     </Box>
   );
 }
