6c70f798d1a420ce79017ace66c017912f756cb7
Victor Zanivan Monteiro <victorzanivan@gmail.com>
2023-10-02 01:03:50 -0300

    [docs][joy-ui] Refine the Rental dashboard template (#39059)

--- docs/data/joy/getting-started/templates/rental-dashboard/components/ColorSchemeToggle.tsx
+++ docs/data/joy/getting-started/templates/rental-dashboard/components/ColorSchemeToggle.tsx
@@ -1,48 +1,61 @@
 import * as React from 'react';
 import { useColorScheme } from '@mui/joy/styles';
-import { ListItem, ListItemButton, ListItemButtonProps } from '@mui/joy';
+import IconButton, { IconButtonProps } from '@mui/joy/IconButton';
+
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import LightModeIcon from '@mui/icons-material/LightMode';
 
 export default function ColorSchemeToggle({
   onClick,
   sx,
   ...props
-}: ListItemButtonProps) {
+}: IconButtonProps) {
   const { mode, setMode } = useColorScheme();
   const [mounted, setMounted] = React.useState(false);
   React.useEffect(() => {
     setMounted(true);
   }, []);
   if (!mounted) {
-    return <ListItemButton {...props} sx={sx} disabled />;
+    return (
+      <IconButton
+        size="sm"
+        variant="outlined"
+        color="neutral"
+        {...props}
+        sx={sx}
+        disabled
+      />
+    );
   }
-
   return (
-    <ListItem>
-      <ListItemButton
-        id="toggle-mode"
-        onClick={(event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-          if (mode === 'light') {
-            setMode('dark');
-          } else {
-            setMode('light');
-          }
-          onClick?.(event);
-        }}
-        sx={[
-          {
-            '& > *:first-child': {
-              display: mode === 'dark' ? 'none' : 'initial',
-            },
-            '& > *:last-child': {
-              display: mode === 'light' ? 'none' : 'initial',
-            },
+    <IconButton
+      id="toggle-mode"
+      size="sm"
+      variant="outlined"
+      color="neutral"
+      {...props}
+      onClick={(event) => {
+        if (mode === 'light') {
+          setMode('dark');
+        } else {
+          setMode('light');
+        }
+        onClick?.(event);
+      }}
+      sx={[
+        {
+          '& > *:first-of-type': {
+            display: mode === 'dark' ? 'none' : 'initial',
           },
-          ...(Array.isArray(sx) ? sx : [sx]),
-        ]}
-      >
-        <i data-feather="moon" />
-        <i data-feather="sun" />
-      </ListItemButton>
-    </ListItem>
+          '& > *:last-of-type': {
+            display: mode === 'light' ? 'none' : 'initial',
+          },
+        },
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
+    >
+      <DarkModeRoundedIcon />
+      <LightModeIcon />
+    </IconButton>
   );
 }
