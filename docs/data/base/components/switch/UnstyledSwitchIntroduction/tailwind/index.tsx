5ec669158444a6f92621013350bd89b4498538bd
zanivan <victorzanivan@gmail.com>
2023-10-03 15:15:21 -0300

    Improved contrast Switch Tailwind intro demo

--- docs/data/base/components/switch/UnstyledSwitchIntroduction/tailwind/index.tsx
+++ docs/data/base/components/switch/UnstyledSwitchIntroduction/tailwind/index.tsx
@@ -73,7 +73,7 @@ const Switch = React.forwardRef<HTMLSpanElement, SwitchProps>((props, ref) => {
           return {
             ...resolvedSlotProps,
             className: clsx(
-              `absolute block w-full h-full transition rounded-full border dark:border-gray-700 shadow-[inset_0_1_1_rgb(0_0_0_/_0.05)] dark:shadow-[inset_0_1_1_rgb(0_0_0_/_0.5)] ${
+              `absolute block w-full h-full transition rounded-full border border-solid outline-none border-slate-300 dark:border-gray-700 shadow-[inset_0_1_1_rgb(0_0_0_/_0.05)] dark:shadow-[inset_0_1_1_rgb(0_0_0_/_0.5)] ${
                 ownerState.checked
                   ? 'bg-purple-500'
                   : 'bg-slate-100 dark:bg-slate-900 hover:bg-slate-200'
@@ -90,7 +90,7 @@ const Switch = React.forwardRef<HTMLSpanElement, SwitchProps>((props, ref) => {
           return {
             ...resolvedSlotProps,
             className: clsx(
-              `block w-4 h-4 top-1 rounded-2xl border dark:border-gray-700 transition shadow-[0_1px_2px_rgb(0_0_0_/_0.1)] dark:shadow-[0_1px_2px_rgb(0_0_0_/_0.25)] ${
+              `block w-4 h-4 top-1 rounded-2xl border border-solid outline-none border-slate-300 dark:border-gray-700 transition shadow-[0_1px_2px_rgb(0_0_0_/_0.1)] dark:shadow-[0_1px_2px_rgb(0_0_0_/_0.25)] ${
                 ownerState.checked ? 'left-[17px]' : 'left-[4px]'
               }  ${
                 ownerState.focusVisible
