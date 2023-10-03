62d3b9669bbb1363ff7cf3009d9bfca8974c7b8f
Victor Zanivan Monteiro <victorzanivan@gmail.com>
2023-10-03 15:32:00 -0300

    [joy-ui][Templates] Update and standardize Sidemenus (#39271)

--- docs/data/joy/getting-started/templates/order-dashboard/components/Sidebar.tsx
+++ docs/data/joy/getting-started/templates/order-dashboard/components/Sidebar.tsx
@@ -168,11 +168,7 @@ export default function Sidebar() {
           </ListItem>
 
           <ListItem>
-            <ListItemButton
-              role="menuitem"
-              component="a"
-              href="/joy-ui/getting-started/templates/order-dashboard/"
-            >
+            <ListItemButton selected>
               <ShoppingCartRoundedIcon />
               <ListItemContent>
                 <Typography level="title-sm">Orders</Typography>
@@ -229,7 +225,6 @@ export default function Sidebar() {
 
           <ListItem nested>
             <Toggler
-              defaultExpanded
               renderToggle={({ open, setOpen }) => (
                 <ListItemButton onClick={() => setOpen(!open)}>
                   <GroupRoundedIcon />
@@ -244,7 +239,13 @@ export default function Sidebar() {
             >
               <List sx={{ gap: 0.5 }}>
                 <ListItem sx={{ mt: 0.5 }}>
-                  <ListItemButton selected>My profile</ListItemButton>
+                  <ListItemButton
+                    role="menuitem"
+                    component="a"
+                    href="/joy-ui/getting-started/templates/profile-dashboard/"
+                  >
+                    My profile
+                  </ListItemButton>
                 </ListItem>
                 <ListItem>
                   <ListItemButton>Create a new user</ListItemButton>
