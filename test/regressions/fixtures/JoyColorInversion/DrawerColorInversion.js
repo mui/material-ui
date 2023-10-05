90ffb817cc680eb796652176ae634eb5e413da09
siriwatknp <siriwatkunaporn@gmail.com>
2023-10-05 10:15:09 +0700

    add regression

---
+++ test/regressions/fixtures/JoyColorInversion/DrawerColorInversion.js
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+import Drawer from '@mui/joy/Drawer';
+import Button from '@mui/joy/Button';
+import List from '@mui/joy/List';
+import Divider from '@mui/joy/Divider';
+import ListItem from '@mui/joy/ListItem';
+import ListItemButton from '@mui/joy/ListItemButton';
+import Card from '@mui/joy/Card';
+
+export default function DrawerColorInversion() {
+  return (
+    <CssVarsProvider>
+      <CssBaseline />
+      <Drawer open color="primary" variant="solid" invertedColors>
+        <Box role="presentation">
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
+              <ListItem key={text}>
+                <ListItemButton>{text}</ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text) => (
+              <ListItem key={text}>
+                <ListItemButton>{text}</ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Card variant="soft">
+          <Typography level="title-lg" fontFamily="code">
+            •••• •••• •••• 1212
+          </Typography>
+          <Box sx={{ display: 'flex', gap: 2, justifyContent: 'space-between' }}>
+            <div>
+              <Typography fontSize="xs" fontFamily="code">
+                CARD NAME
+              </Typography>
+              <Typography level="title-sm" fontSize="sm">
+                JOHN DOE
+              </Typography>
+            </div>
+            <div>
+              <Typography fontSize="xs" textAlign="right" fontFamily="code">
+                EXPIRE
+              </Typography>
+              <Typography level="title-sm" fontSize="sm" textAlign="right">
+                07/25
+              </Typography>
+            </div>
+          </Box>
+        </Card>
+      </Drawer>
+    </CssVarsProvider>
+  );
+}
