db1a72198019d1e6b718f87e3e326c31301c43c7
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-10-05 20:18:52 -0300

    adjustments to the alert within the description column

--- docs/src/modules/components/ApiPage/table/PropertiesTable.tsx
+++ docs/src/modules/components/ApiPage/table/PropertiesTable.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import { styled, alpha } from '@mui/material/styles';
 import Alert from '@mui/material/Alert';
+import WarningRoundedIcon from '@mui/icons-material/WarningRounded';
 import { useTranslate } from 'docs/src/modules/utils/i18n';
 import {
   brandingDarkTheme as darkTheme,
@@ -54,7 +55,7 @@ const StyledTable = styled('table')(
       fontWeight: theme.typography.fontWeightRegular,
     },
     '& .MuiPropTable-description-column': {
-      // width: '40%',
+      width: '40%',
       paddingRight: 8,
       '& .prop-list-description': {
         marginBottom: 0,
@@ -62,6 +63,26 @@ const StyledTable = styled('table')(
       '& .prop-list-additional-description': {
         marginTop: 12,
         marginBottom: 0,
+      },
+      '& .refAlert': {
+        padding: '2px 12px',
+        marginTop: 12,
+        color: `var(--muidocs-palette-grey-900, ${lightTheme.palette.grey[900]})`,
+        backgroundColor: alpha(lightTheme.palette.warning[50], 0.5),
+        borderColor: `var(--muidocs-palette-warning-200, ${lightTheme.palette.warning[200]})`,
+        '& strong': {
+          color: `var(--muidocs-palette-warning-800, ${lightTheme.palette.warning[800]})`,
+        },
+        '&>svg': {
+          fill: `var(--muidocs-palette-warning-600, ${lightTheme.palette.warning[600]})`,
+        },
+        '& a': {
+          color: `var(--muidocs-palette-warning-800, ${lightTheme.palette.warning[800]})`,
+          textDecorationColor: alpha(lightTheme.palette.warning.main, 0.4),
+          '&:hover': {
+            textDecorationColor: 'inherit',
+          },
+        },
       },
     },
     '& .prop-list-signature': {
@@ -93,6 +114,22 @@ const StyledTable = styled('table')(
       '& .prop-list-signature': {
         '& .prop-list-title': {
           color: `var(--muidocs-palette-text-primary, ${darkTheme.palette.text.primary})`,
+        },
+      },
+      '& .MuiPropTable-description-column': {
+        '& .refAlert': {
+          color: `var(--muidocs-palette-warning-50, ${darkTheme.palette.warning[50]})`,
+          backgroundColor: alpha(darkTheme.palette.warning[700], 0.15),
+          borderColor: alpha(darkTheme.palette.warning[600], 0.3),
+          '& strong': {
+            color: `var(--muidocs-palette-warning-200, ${darkTheme.palette.warning[200]})`,
+          },
+          '&>svg': {
+            fill: `var(--muidocs-palette-warning-400, ${darkTheme.palette.warning[400]})`,
+          },
+          '& a': {
+            color: `var(--muidocs-palette-warning-100, ${darkTheme.palette.warning[100]})`,
+          },
         },
       },
     },
@@ -181,19 +218,16 @@ export default function PropertiesTable(props: PropertiesTableProps) {
                 {description && <PropDescription description={description} />}
                 {requiresRef && (
                   <Alert
+                    className="refAlert"
                     severity="warning"
-                    sx={{ mb: 1, py: 0 }}
-                    iconMapping={{
-                      warning: (
-                        <svg
-                          width="14"
-                          height="12"
-                          viewBox="0 0 14 12"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M1.98012 11.9997H12.0201C13.0468 11.9997 13.6868 10.8864 13.1735 9.99971L8.15345 1.32638C7.64012 0.43971 6.36012 0.43971 5.84679 1.32638L0.826788 9.99971C0.313455 10.8864 0.953455 11.9997 1.98012 11.9997ZM7.00012 7.33304C6.63345 7.33304 6.33345 7.03304 6.33345 6.66638V5.33304C6.33345 4.96638 6.63345 4.66638 7.00012 4.66638C7.36679 4.66638 7.66679 4.96638 7.66679 5.33304V6.66638C7.66679 7.03304 7.36679 7.33304 7.00012 7.33304ZM7.66679 9.99971H6.33345V8.66638H7.66679V9.99971Z" />
-                        </svg>
-                      ),
+                    icon={<WarningRoundedIcon fontSize="small" />}
+                    sx={{
+                      alignItems: 'center',
+                      '& .MuiAlert-icon': {
+                        height: 'fit-content',
+                        p: 0,
+                        mb: 0.5,
+                      },
                     }}
                   >
                     <span
