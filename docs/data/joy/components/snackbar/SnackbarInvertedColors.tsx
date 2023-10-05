240a825a5c6fbab1fd09a5bec5a68078ab12c120
zanivan <victorzanivan@gmail.com>
2023-10-05 15:48:37 -0300

    Fix imports

--- docs/data/joy/components/snackbar/SnackbarInvertedColors.tsx
+++ docs/data/joy/components/snackbar/SnackbarInvertedColors.tsx
@@ -1,48 +1,6 @@
 import * as React from 'react';
 import Snackbar from '@mui/joy/Snackbar';
-import IconButton from '@mui/joy/IconButton';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Typography from '@mui/joy/Typography';
 import Button from '@mui/joy/Button';
-import Check from '@mui/icons-material/Check';
-import Close from '@mui/icons-material/Close';
-
-function SnackbarStartDecorator() {
-  return (
-    <AspectRatio
-      variant="solid"
-      ratio="1"
-      sx={{
-        minWidth: 30,
-        borderRadius: '50%',
-        boxShadow: '0 2px 12px 0 rgb(0 0 0/0.2)',
-      }}
-    >
-      <div>
-        <Check />
-      </div>
-    </AspectRatio>
-  );
-}
-
-interface SnackbarEndDecoratorProps {
-  onClose: () => React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-function SnackbarEndDecorator(props: SnackbarEndDecoratorProps) {
-  return (
-    <IconButton
-      variant="plain"
-      sx={{
-        '--IconButton-size': '32px',
-        transform: 'translate(0.5rem, -0.5rem)',
-      }}
-      onClick={props.onClose}
-    >
-      <Close />
-    </IconButton>
-  );
-}
 
 export default function SnackbarInvertedColors() {
   const [open, setOpen] = React.useState(false);
