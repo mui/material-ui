90c0ae154308e5e5977b7aea7e2ceac8d77801be
siriwatknp <siriwatkunaporn@gmail.com>
2023-10-06 11:29:43 +0700

    add icons to position demo

--- docs/data/joy/components/snackbar/PositionedSnackbar.tsx
+++ docs/data/joy/components/snackbar/PositionedSnackbar.tsx
@@ -3,6 +3,12 @@ import Grid from '@mui/joy/Grid';
 import Box from '@mui/joy/Box';
 import Button from '@mui/joy/Button';
 import Snackbar, { SnackbarOrigin } from '@mui/joy/Snackbar';
+import NorthWestIcon from '@mui/icons-material/NorthWest';
+import NorthEastIcon from '@mui/icons-material/NorthEast';
+import NorthIcon from '@mui/icons-material/North';
+import SouthIcon from '@mui/icons-material/South';
+import SouthEastIcon from '@mui/icons-material/SouthEast';
+import SouthWestIcon from '@mui/icons-material/SouthWest';
 
 interface State extends SnackbarOrigin {
   open: boolean;
@@ -29,51 +35,59 @@ export default function PositionedSnackbar() {
       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
         <Button
           variant="plain"
+          startDecorator={<NorthIcon />}
           onClick={handleClick({ vertical: 'top', horizontal: 'center' })}
+          sx={{ flexDirection: 'column', gap: 1, '--Button-gap': 0 }}
         >
-          Top-center
+          Top Center
         </Button>
       </Box>
       <Grid container justifyContent="center">
         <Grid xs={6}>
           <Button
             variant="plain"
+            startDecorator={<NorthWestIcon />}
             onClick={handleClick({ vertical: 'top', horizontal: 'left' })}
           >
-            Top-left
+            Top Left
           </Button>
         </Grid>
         <Grid xs={6} textAlign="right" sx={{ mb: 2 }}>
           <Button
             variant="plain"
+            endDecorator={<NorthEastIcon />}
             onClick={handleClick({ vertical: 'top', horizontal: 'right' })}
           >
-            Top-right
+            Top Right
           </Button>
         </Grid>
         <Grid xs={6}>
           <Button
             variant="plain"
+            startDecorator={<SouthWestIcon />}
             onClick={handleClick({ vertical: 'bottom', horizontal: 'left' })}
           >
-            Bottom-left
+            Bottom Left
           </Button>
         </Grid>
         <Grid xs={6} textAlign="right">
           <Button
             variant="plain"
+            endDecorator={<SouthEastIcon />}
             onClick={handleClick({ vertical: 'bottom', horizontal: 'right' })}
           >
-            Bottom-right
+            Bottom Right
           </Button>
         </Grid>
       </Grid>
       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
         <Button
           variant="plain"
+          endDecorator={<SouthIcon />}
           onClick={handleClick({ vertical: 'bottom', horizontal: 'center' })}
+          sx={{ flexDirection: 'column', gap: 1, '--Button-gap': 0 }}
         >
-          Bottom-center
+          Bottom Center
         </Button>
       </Box>
     </React.Fragment>
