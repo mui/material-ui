c2c98e6223e18c97436eae2c97f264c2c71c8aca
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-10-09 08:28:26 -0300

    [docs][joy-ui] Revise the Color Inversion page (#39306)

--- docs/data/joy/main-features/color-inversion/ColorInversionAnyParent.js
+++ docs/data/joy/main-features/color-inversion/ColorInversionAnyParent.js
@@ -5,15 +5,9 @@ import Box from '@mui/joy/Box';
 import Button from '@mui/joy/Button';
 import Typography from '@mui/joy/Typography';
 
-function Stat({ description, value, sx, ...props }) {
+function Stat({ description, value }) {
   return (
-    <Box
-      {...props}
-      sx={[
-        { borderLeft: 3, borderColor: 'divider', px: 2, py: 0.5 },
-        ...(Array.isArray(sx) ? sx : [sx]),
-      ]}
-    >
+    <Box sx={{ borderLeft: 3, borderColor: 'divider', px: 2, py: 0.5 }}>
       <Typography level="h3" component="div">
         {value}
       </Typography>
@@ -26,16 +20,6 @@ function Stat({ description, value, sx, ...props }) {
 
 Stat.propTypes = {
   description: PropTypes.node,
-  /**
-   * The system prop that allows defining system overrides as well as additional CSS styles.
-   */
-  sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(
-      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool]),
-    ),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
   value: PropTypes.node,
 };
 
@@ -49,24 +33,20 @@ export default function ColorInversionAnyParent() {
           alignItems: 'center',
           rowGap: 2,
           columnGap: 8,
-          p: 2,
+          p: 4,
+          borderRadius: 'sm',
           background: (theme) =>
             `linear-gradient(45deg, ${theme.vars.palette.neutral[800]}, ${theme.vars.palette.neutral[600]})`,
-          borderRadius: 'sm',
         },
         applySolidInversion('neutral'),
       ]}
     >
       <div>
-        <Typography sx={{ my: 2 }}>
+        <Typography sx={{ mb: 2 }}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat.
+          tempor incididunt ut labore et dolore magna aliqua.
         </Typography>
-        <Button variant="soft" sx={{ mt: 1 }}>
-          Learn more
-        </Button>
+        <Button variant="soft">Learn more</Button>
       </div>
       <Box
         sx={{
@@ -78,8 +58,8 @@ export default function ColorInversionAnyParent() {
           gap: 3,
         }}
       >
-        <Stat value="4M" description="Weekly download on NPM" />
-        <Stat value="87k" description="Stars on Github" />
+        <Stat value="4M" description="Weekly downloads" />
+        <Stat value="87k" description="Stars on GitHub" />
         <Stat value="2.7k" description="Open source contributors" />
         <Stat value="18.4k" description="Followers on Twitter" />
       </Box>
