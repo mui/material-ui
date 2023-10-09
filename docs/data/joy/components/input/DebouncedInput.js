edf1baeb913fbbd1103d5d734a56a97d80b16f51
sai chand <60743144+sai6855@users.noreply.github.com>
2023-10-09 17:53:10 +0530

    [joy-ui][Input] Add debounce input demo (#39300)

---
+++ docs/data/joy/components/input/DebouncedInput.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Input from '@mui/joy/Input';
+import Typography from '@mui/joy/Typography';
+import Box from '@mui/joy/Box';
+
+function DebounceInput(props) {
+  const { handleDebounce, debounceTimeout, ...rest } = props;
+
+  const timerRef = React.useRef();
+
+  const handleChange = (event) => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+
+    timerRef.current = window.setTimeout(() => {
+      handleDebounce(event.target.value);
+    }, debounceTimeout);
+  };
+
+  return <Input {...rest} onChange={handleChange} />;
+}
+
+DebounceInput.propTypes = {
+  debounceTimeout: PropTypes.number.isRequired,
+  handleDebounce: PropTypes.func.isRequired,
+};
+
+export default function DebouncedInput() {
+  const [debouncedValue, setDebouncedValue] = React.useState('');
+  const handleDebounce = (value) => {
+    setDebouncedValue(value);
+  };
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+      <DebounceInput
+        placeholder="Type in here…"
+        debounceTimeout={1000}
+        handleDebounce={handleDebounce}
+      />
+      <Typography>Debounced input: {debouncedValue}</Typography>
+    </Box>
+  );
+}
