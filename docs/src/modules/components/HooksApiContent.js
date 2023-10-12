980dfb219235a745fd8ac0dc27065046dde6398c
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-10-12 11:28:54 -0300

    remove unnecessary divider on the Hooks API page

--- docs/src/modules/components/HooksApiContent.js
+++ docs/src/modules/components/HooksApiContent.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import kebabCase from 'lodash/kebabCase';
 import { exactProp } from '@mui/utils';
 import { useTranslate, useUserLanguage } from 'docs/src/modules/utils/i18n';
-import Divider from 'docs/src/modules/components/ApiDivider';
 import PropertiesTable from 'docs/src/modules/components/ApiPage/sections/PropertiesSection';
 import HighlightedCode from 'docs/src/modules/components/HighlightedCode';
 import MarkdownElement from 'docs/src/modules/components/MarkdownElement';
@@ -95,7 +94,6 @@ export default function HooksApiContent(props) {
             <path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z" />
           </symbol>
         </svg>
-        {idx < numberOfHooks - 1 && <Divider />}
       </React.Fragment>
     );
   });
