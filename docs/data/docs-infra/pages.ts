142114c3e983594e6f2b7661fe8904588a9e52a5
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-10-13 08:47:18 -0300

    [docs-infra] Add a min height to the layout component (#39416)

--- docs/data/docs-infra/pages.ts
+++ docs/data/docs-infra/pages.ts
@@ -14,8 +14,8 @@ const pages: readonly MuiPage[] = [
     children: [
       { pathname: '/experiments/docs/callouts' },
       { pathname: '/experiments/docs/codeblock' },
-      { pathname: '/experiments/docs/data-grid-premium', title: 'API DataGridPremium' },
       { pathname: '/experiments/docs/demos' },
+      { pathname: '/experiments/docs/data-grid-premium', title: 'API DataGridPremium' },
     ],
   },
   {
