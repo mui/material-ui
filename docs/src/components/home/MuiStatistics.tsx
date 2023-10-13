4cf9410fc25165eb62b1eff80395f7c070722f10
Ujjawal <ujjawalrachhoya100@gmail.com>
2023-10-13 14:23:21 +0530

    Show no of stars on testinomial part dynamically

--- docs/src/components/home/MuiStatistics.tsx
+++ docs/src/components/home/MuiStatistics.tsx
@@ -3,14 +3,36 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
-const data = [
-  { title: '4M', metadata: 'Weekly downloads on npm' },
-  { title: '87k', metadata: 'Stars on GitHub' },
-  { title: '2.7k', metadata: 'Open-source contributors' },
-  { title: '18.4k', metadata: 'Followers on Twitter' },
-];
+function formatNumber(n: number): string {
+  let formatted: string;
+  if (n >= 1000000) {
+    formatted = (n / 1000000).toFixed(1);
+    formatted += 'M';
+  } else if (n >= 1000) {
+    formatted = (n / 1000).toFixed(1);
+    formatted += 'K';
+  } else {
+    return n.toString();
+  }
+  return formatted;
+}
 
 export default function MuiStatistics() {
+  const [data, setData] = React.useState([{ title: '', metadata: '' }]);
+  React.useEffect(() => {
+    async function getStatistics() {
+      const api = 'https://api.github.com/repos/mui/material-ui';
+      let res: any = await fetch(api);
+      res = await res.json();
+      setData([
+        { title: '5M', metadata: 'Weekly downloads on npm' },
+        { title: formatNumber(res?.stargazers_count), metadata: 'Stars on GitHub' },
+        { title: '2.7k', metadata: 'Open-source contributors' },
+        { title: '18.4k', metadata: 'Followers on Twitter' },
+      ]);
+    }
+    getStatistics();
+  }, []);
   return (
     <Grid item xs={12} md={6} container spacing={4}>
       {data.map((item) => (
