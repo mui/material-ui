b1f833f3654fefed5ed7179510e709ce385580f6
alexandre <alex.fauquette@gmail.com>
2023-10-18 10:07:58 +0200

    fix scroll

--- docs/src/modules/components/ApiPage/sections/ToggleDisplayOption.tsx
+++ docs/src/modules/components/ApiPage/sections/ToggleDisplayOption.tsx
@@ -6,10 +6,12 @@ import ToggleButton, { ToggleButtonProps } from '@mui/material/ToggleButton';
 import CalendarViewDayRoundedIcon from '@mui/icons-material/CalendarViewDayRounded';
 import TableChartRoundedIcon from '@mui/icons-material/TableChartRounded';
 import ReorderRoundedIcon from '@mui/icons-material/ReorderRounded';
+import useEnhancedEffect from '@mui/utils/useEnhancedEffect';
 
 export type ApiDisplayOptions = 'collapsed' | 'expended' | 'table';
 
 const options: ApiDisplayOptions[] = ['collapsed', 'expended', 'table'];
+const DEFAULT_LAYOUT: ApiDisplayOptions = 'expended';
 
 export const API_LAYOUT_STORAGE_KEYS = {
   default: 'apiPage_default',
@@ -22,7 +24,7 @@ export const API_LAYOUT_STORAGE_KEYS = {
 const getRandomOption = () => {
   if (/Algolia Crawler/.test(navigator.userAgent) || /Googlebot/.test(navigator.userAgent)) {
     // When crawlers visit the page, they should not have to expand items
-    return 'expended';
+    return DEFAULT_LAYOUT;
   }
   // A default layout is saved in localstorage at first render to make sure all section start with the same layout.
   const savedDefaultOption = localStorage.getItem(
@@ -42,10 +44,39 @@ const getRandomOption = () => {
   return randomOption;
 };
 
+let neverHydrated = true;
+
+const getOption = (storageKey: string) => () => {
+  if (neverHydrated) {
+    return DEFAULT_LAYOUT;
+  }
+  try {
+    const savedOption = localStorage.getItem(storageKey);
+
+    if (savedOption !== null) {
+      return savedOption as ApiDisplayOptions;
+    }
+
+    const randomOption = getRandomOption();
+
+    return randomOption;
+  } catch (error) {
+    return DEFAULT_LAYOUT;
+  }
+};
+
 export function useApiPageOption(
   storageKey: string,
 ): [ApiDisplayOptions, (newOption: ApiDisplayOptions) => void] {
-  const [option, setOption] = React.useState(options[0]);
+  const [option, setOption] = React.useState(getOption(storageKey));
+  useEnhancedEffect(() => {
+    if (neverHydrated) {
+      if (document?.location.hash) {
+        document.getElementById(document?.location.hash)?.scrollIntoView();
+      }
+    }
+    neverHydrated = false;
+  }, []);
 
   const updateOption = React.useCallback(
     (newOption: ApiDisplayOptions) => {
@@ -58,21 +89,6 @@ export function useApiPageOption(
     },
     [storageKey],
   );
-
-  React.useEffect(() => {
-    try {
-      const savedOption = localStorage.getItem(storageKey);
-      if (savedOption !== null) {
-        setOption(savedOption as ApiDisplayOptions);
-        return;
-      }
-
-      const randomOption = getRandomOption();
-      updateOption(randomOption);
-    } catch (error) {
-      // do nothing
-    }
-  }, [storageKey, updateOption]);
 
   return [option, updateOption];
 }
