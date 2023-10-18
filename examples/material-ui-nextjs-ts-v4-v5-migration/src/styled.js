adeeea66a7c446220d6c9b6c48a0235931fdcbb4
Marija Najdova <mnajdova@gmail.com>
2023-10-18 14:05:33 +0200

    remove unrelated changes

--- examples/material-ui-nextjs-ts-v4-v5-migration/src/styled.js
+++
@@ -1,8 +0,0 @@
-import { createStyled } from '@mui/system';
-import { createTheme } from '@mui/material/styles';
-
-const theme = createTheme();
-
-const styled = createStyled({ defaultTheme: theme });
-
-export default styled;
