42b23f57ff8dd2731f355a71aaa3c0be8a81ab22
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-10-23 08:47:58 +0530

    add cssvarsprovider

--- docs/data/joy/getting-started/usage/ButtonUsage.js
+++ docs/data/joy/getting-started/usage/ButtonUsage.js
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import Button from '@mui/joy/Button';
+import { CssVarsProvider } from '@mui/joy/styles';
 
 export default function ButtonUsage() {
-  return <Button variant="solid">Hello world</Button>;
+  return (
+    <CssVarsProvider>
+      <Button variant="solid">Hello world</Button>{' '}
+    </CssVarsProvider>
+  );
 }
