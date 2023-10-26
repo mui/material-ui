e3beef99f0dcafb452b7a61c4959ee90bf4e6951
Blake Netzeband <blake.netzeband@gmail.com>
2023-10-26 10:32:10 -0600

    Refactored ComponentsVariants into a generic type by adding a Theme parameter. This change follows the paradigm defined in the ComponentsOverrides interface.

--- packages/mui-material/src/styles/variants.d.ts
+++ packages/mui-material/src/styles/variants.d.ts
@@ -1,8 +1,7 @@
 import { Interpolation } from '@mui/system';
-import { Theme } from './createTheme';
 import { ComponentsPropsList } from './props';
 
-export type ComponentsVariants = {
+export type ComponentsVariants<Theme = unknown> = {
   [Name in keyof ComponentsPropsList]?: Array<{
     props: Partial<ComponentsPropsList[Name]>;
     style: Interpolation<{ theme: Theme }>;
