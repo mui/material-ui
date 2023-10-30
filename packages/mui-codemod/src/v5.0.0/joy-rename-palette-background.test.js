0b762c59a6ed0d7e8a84f8cb4968e7d4f85a569f
siriwatknp <siriwatkunaporn@gmail.com>
2023-10-30 14:27:30 +0700

    add codemod

--- packages/mui-codemod/src/v5.0.0/joy-rename-palette-background.test.js
+++ packages/mui-codemod/src/v5.0.0/joy-rename-palette-background.test.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import { expect } from 'chai';
 import jscodeshift from 'jscodeshift';
-import transform from './joy-rename-background-palette';
+import transform from './joy-rename-palette-background';
 import readFile from '../util/readFile';
 
 function read(fileName) {
@@ -10,18 +10,18 @@ function read(fileName) {
 
 describe('@mui/codemod', () => {
   describe('v5.0.0', () => {
-    describe('joy-rename-background-palette', () => {
+    describe('joy-rename-palette-background', () => {
       it('transforms classname prefix from Joy to Mui', () => {
         const actual = transform(
           {
-            source: read('./joy-rename-background-palette.test/actual.js'),
-            path: require.resolve('./joy-rename-background-palette.test/actual.js'),
+            source: read('./joy-rename-palette-background.test/actual.js'),
+            path: require.resolve('./joy-rename-palette-background.test/actual.js'),
           },
           { jscodeshift },
           {},
         );
 
-        const expected = read('./joy-rename-background-palette.test/expected.js');
+        const expected = read('./joy-rename-palette-background.test/expected.js');
         expect(actual).to.equal(expected, 'The transformed version should be correct');
       });
     });
