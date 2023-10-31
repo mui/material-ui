d0817417460495379302073dd67dfcbe331471b1
alexandre <alex.fauquette@gmail.com>
2023-10-31 16:16:44 +0100

    bring back gradient

--- docs/src/modules/components/ApiPage/table/StyledTableContainer.tsx
+++ docs/src/modules/components/ApiPage/table/StyledTableContainer.tsx
@@ -4,6 +4,18 @@ import { brandingDarkTheme as darkTheme } from 'docs/src/modules/brandingTheme';
 
 const StyledTableContainer = styled('div')(
   ({ theme }) => ({
+    overflow: 'hidden',
+    '& table': {
+      borderRadius: '10px',
+      marginLeft: -1,
+      marginRight: -1,
+      background: [
+        'linear-gradient(to right, rgb(255, 255, 255) 5%, rgba(255, 255, 255, 0) 80%) local',
+        'linear-gradient(to right, rgba(255, 255, 255, 0), rgb(255, 255, 255) 100%) 100% center local',
+        'linear-gradient(to right, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0) 5%) scroll',
+        'linear-gradient(to left, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0) 5%) scroll',
+      ].join(', '),
+    },
     '&& th': {
       paddingTop: 8,
       paddingBottom: 8,
@@ -20,6 +32,12 @@ const StyledTableContainer = styled('div')(
   }),
   ({ theme }) => ({
     [`:where(${theme.vars ? '[data-mui-color-scheme="dark"]' : '.mode-dark'}) &`]: {
+      background: [
+        'linear-gradient(to right, rgb(10, 25, 41) 5%, rgba(10, 25, 41, 0)) local',
+        'linear-gradient(to right, rgba(10, 25, 41, 0), rgb(10, 25, 41) 100%) 100% center local',
+        'linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0) 5%) scroll',
+        'linear-gradient(to left, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0) 5%) scroll',
+      ].join(', '),
       '& tr:hover': {
         backgroundColor: alpha(darkTheme.palette.primaryDark[800], 0.5),
       },
