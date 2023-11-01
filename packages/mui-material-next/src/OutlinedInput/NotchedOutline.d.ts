4b0706c65936a847ad851c0fc015ae7b8f6e53f1
Albert Yu <albert@albertyu.co>
2023-11-01 21:38:23 +0800

    Copy v5 OutlinedInput

---
+++ packages/mui-material-next/src/OutlinedInput/NotchedOutline.d.ts
@@ -0,0 +1,19 @@
+import * as React from 'react';
+// TODO v6: port to material-next
+// eslint-disable-next-line no-restricted-imports
+import { InternalStandardProps as StandardProps } from '@mui/material';
+
+export interface NotchedOutlineProps
+  extends StandardProps<React.FieldsetHTMLAttributes<HTMLFieldSetElement>> {
+  disabled?: boolean;
+  error?: boolean;
+  focused?: boolean;
+  label?: React.ReactNode;
+  notched: boolean;
+}
+
+export type NotchedOutlineClassKey = keyof NonNullable<NotchedOutlineProps['classes']>;
+
+declare const NotchedOutline: React.JSXElementConstructor<NotchedOutlineProps>;
+
+export default NotchedOutline;
