e67ff65299e7d2ddf12cd0883c50d9ec2334c679
Olivier Tassinari <olivier.tassinari@gmail.com>
2023-11-01 18:51:34 +0100

    [docs-infra] Link to ScaffoldHub v2
    v3 was released https://www.scaffoldhub.io/ without Material UI support.

--- docs/src/modules/components/Ad.js
+++ docs/src/modules/components/Ad.js
@@ -39,7 +39,7 @@ const disableAd =
 const inHouseAds = [
   {
     name: 'scaffoldhub',
-    link: 'https://www.scaffoldhub.io/?partner=1',
+    link: 'https://v2.scaffoldhub.io/scaffolds/react-material-ui?partner=1',
     img: '/static/ads-in-house/scaffoldhub.png',
     description: '<b>ScaffoldHub</b>. Automate building your full-stack MUI web-app.',
   },
