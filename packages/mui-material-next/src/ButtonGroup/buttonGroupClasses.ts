59e8baa5dd26411b40de2af5ddc1f31d01a5fd0e
Lucas Hilgert <lhilgert@gmx.net>
2023-11-01 21:56:33 +0100

    add classes to buttonGroupClasses

--- packages/mui-material-next/src/ButtonGroup/buttonGroupClasses.ts
+++ packages/mui-material-next/src/ButtonGroup/buttonGroupClasses.ts
@@ -6,12 +6,16 @@ import {
 export interface ButtonGroupClasses {
   /** Styles applied to the root element. */
   root: string;
-  /** Styles applied to the root element if `variant="contained"`. */
-  contained: string;
-  /** Styles applied to the root element if `variant="outlined"`. */
-  outlined: string;
   /** Styles applied to the root element if `variant="text"`. */
   text: string;
+  /** Styles applied to the root element if `variant="outlined"`. */
+  outlined: string;
+  /** Styles applied to the root element if `variant="filled"`. */
+  filled: string;
+  /** Styles applied to the root element if `variant="filledTonal"`. */
+  filledTonal: string;
+  /** Styles applied to the root element if `variant="elevated"`. */
+  elevated: string;
   /** Styles applied to the root element if `disableElevation={true}`. */
   disableElevation: string;
   /** State class applied to the child elements if `disabled={true}`. */
@@ -38,6 +42,8 @@ export interface ButtonGroupClasses {
   groupedTextPrimary: string;
   /** Styles applied to the children if `variant="text"` and `color="secondary"`. */
   groupedTextSecondary: string;
+  /** Styles applied to the children if `variant="text"` and `color="tertiary"`. */
+  groupedTextTertiary: string;
   /** Styles applied to the children if `variant="outlined"`. */
   groupedOutlined: string;
   /** Styles applied to the children if `variant="outlined"` and `orientation="horizontal"`. */
@@ -48,16 +54,44 @@ export interface ButtonGroupClasses {
   groupedOutlinedPrimary: string;
   /** Styles applied to the children if `variant="outlined"` and `color="secondary"`. */
   groupedOutlinedSecondary: string;
-  /** Styles applied to the children if `variant="contained"`. */
-  groupedContained: string;
-  /** Styles applied to the children if `variant="contained"` and `orientation="horizontal"`. */
-  groupedContainedHorizontal: string;
-  /** Styles applied to the children if `variant="contained"` and `orientation="vertical"`. */
-  groupedContainedVertical: string;
-  /** Styles applied to the children if `variant="contained"` and `color="primary"`. */
-  groupedContainedPrimary: string;
-  /** Styles applied to the children if `variant="contained"` and `color="secondary"`. */
-  groupedContainedSecondary: string;
+  /** Styles applied to the children if `variant="outlined"` and `color="tertiary"`. */
+  groupedOutlinedTertiary: string;
+  /** Styles applied to the children if `variant="filled"`. */
+  groupedFilled: string;
+  /** Styles applied to the children if `variant="filled"` and `orientation="horizontal"`. */
+  groupedFilledHorizontal: string;
+  /** Styles applied to the children if `variant="filled"` and `orientation="vertical"`. */
+  groupedFilledVertical: string;
+  /** Styles applied to the children if `variant="filled"` and `color="primary"`. */
+  groupedFilledPrimary: string;
+  /** Styles applied to the children if `variant="filled"` and `color="secondary"`. */
+  groupedFilledSecondary: string;
+  /** Styles applied to the children if `variant="filled"` and `color="tertiary"`. */
+  groupedFilledTertiary: string;
+  /** Styles applied to the children if `variant="filledTonal"`. */
+  groupedFilledTonal: string;
+  /** Styles applied to the children if `variant="filledTonal"` and `orientation="horizontal"`. */
+  groupedFilledTonalHorizontal: string;
+  /** Styles applied to the children if `variant="filledTonal"` and `orientation="vertical"`. */
+  groupedFilledTonalVertical: string;
+  /** Styles applied to the children if `variant="filledTonal"` and `color="primary"`. */
+  groupedFilledTonalPrimary: string;
+  /** Styles applied to the children if `variant="filledTonal"` and `color="secondary"`. */
+  groupedFilledTonalSecondary: string;
+  /** Styles applied to the children if `variant="filledTonal"` and `color="tertiary"`. */
+  groupedFilledTonalTertiary: string;
+  /** Styles applied to the children if `variant="elevated"`. */
+  groupedElevated: string;
+  /** Styles applied to the children if `variant="elevated"` and `orientation="horizontal"`. */
+  groupedElevatedHorizontal: string;
+  /** Styles applied to the children if `variant="elevated"` and `orientation="vertical"`. */
+  groupedElevatedVertical: string;
+  /** Styles applied to the children if `variant="elevated"` and `color="primary"`. */
+  groupedElevatedPrimary: string;
+  /** Styles applied to the children if `variant="elevated"` and `color="secondary"`. */
+  groupedElevatedSecondary: string;
+  /** Styles applied to the children if `variant="elevated"` and `color="tertiary"`. */
+  groupedElevatedTertiary: string;
   /** Styles applied to the last button in the button group. */
   lastButton: string;
   /** Styles applied to buttons in the middle of the button group. */
@@ -72,9 +106,11 @@ export function getButtonGroupUtilityClass(slot: string): string {
 
 const buttonGroupClasses: ButtonGroupClasses = generateUtilityClasses('MuiButtonGroup', [
   'root',
-  'contained',
-  'outlined',
   'text',
+  'outlined',
+  'filled',
+  'filledTonal',
+  'elevated',
   'disableElevation',
   'disabled',
   'firstButton',
@@ -88,16 +124,31 @@ const buttonGroupClasses: ButtonGroupClasses = generateUtilityClasses('MuiButton
   'groupedTextVertical',
   'groupedTextPrimary',
   'groupedTextSecondary',
+  'groupedTextTertiary',
   'groupedOutlined',
   'groupedOutlinedHorizontal',
   'groupedOutlinedVertical',
   'groupedOutlinedPrimary',
   'groupedOutlinedSecondary',
-  'groupedContained',
-  'groupedContainedHorizontal',
-  'groupedContainedVertical',
-  'groupedContainedPrimary',
-  'groupedContainedSecondary',
+  'groupedOutlinedTertiary',
+  'groupedFilled',
+  'groupedFilledHorizontal',
+  'groupedFilledVertical',
+  'groupedFilledPrimary',
+  'groupedFilledSecondary',
+  'groupedFilledTertiary',
+  'groupedFilledTonal',
+  'groupedFilledTonalHorizontal',
+  'groupedFilledTonalVertical',
+  'groupedFilledTonalPrimary',
+  'groupedFilledTonalSecondary',
+  'groupedFilledTonalTertiary',
+  'groupedElevated',
+  'groupedElevatedHorizontal',
+  'groupedElevatedVertical',
+  'groupedElevatedPrimary',
+  'groupedElevatedSecondary',
+  'groupedElevatedTertiary',
   'lastButton',
   'middleButton',
 ]);
