a44b4f44cd1e632cad7ae562da0e40edea83f57f
siriwatknp <siriwatkunaporn@gmail.com>
2023-11-01 14:19:21 +0700

    fix stepClasses

--- packages/mui-joy/src/Step/stepClasses.ts
+++ packages/mui-joy/src/Step/stepClasses.ts
@@ -5,20 +5,10 @@ export interface StepClasses {
   root: string;
   /** Class name applied to the indicator element. */
   indicator: string;
-  /** Class name applied to the root element if `size="sm"`. */
-  sizeSm: string;
-  /** Class name applied to the root element if `size="md"`. */
-  sizeMd: string;
-  /** Class name applied to the root element if `size="lg"`. */
-  sizeLg: string;
   /** Class name applied to the root element if `orientation="horizontal"`. */
   horizontal: string;
   /** Class name applied to the root element if `orientation="vertical"`. */
   vertical: string;
-  /** Class name applied to the root element if `active={true}`. */
-  active: string;
-  /** Class name applied to the root element if `completed={true}`. */
-  completed: string;
 }
 
 export type StepClassKey = keyof StepClasses;
@@ -30,13 +20,8 @@ export function getStepUtilityClass(slot: string): string {
 const stepClasses: StepClasses = generateUtilityClasses('MuiStep', [
   'root',
   'indicator',
-  'sizeSm',
-  'sizeMd',
-  'sizeLg',
   'horizontal',
   'vertical',
-  'active',
-  'completed',
 ]);
 
 export default stepClasses;
