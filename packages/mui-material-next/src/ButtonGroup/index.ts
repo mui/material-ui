fb182a44d7272b71b3a9ceb38d8ba5dfe15a7c46
Lucas Hilgert <lhilgert@gmx.net>
2023-11-01 10:34:27 +0100

    add missing exports to index

--- packages/mui-material-next/src/ButtonGroup/index.ts
+++ packages/mui-material-next/src/ButtonGroup/index.ts
@@ -1,4 +1,12 @@
 'use client';
 export { default } from './ButtonGroup';
+export * from './ButtonGroup';
+
+export * from './ButtonGroup.types';
+
+export { default as ButtonGroupButtonContext } from './ButtonGroupButtonContext';
+
 export { default as buttonGroupClasses } from './buttonGroupClasses';
 export * from './buttonGroupClasses';
+
+export { default as ButtonGroupContext } from './ButtonGroupContext';
