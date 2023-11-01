99de694a9fd55b2ee73ffcc7d492d3feee4bc9ce
Lucas Hilgert <lhilgert@gmx.net>
2023-11-01 14:50:16 +0100

    add size prop to playground

--- docs/data/material/components/button-group/ButtonGroupMaterialYouPlayground.js
+++ docs/data/material/components/button-group/ButtonGroupMaterialYouPlayground.js
@@ -25,6 +25,11 @@ export default function ButtonGroupMaterialYouPlayground() {
           defaultValue: 'primary',
           knob: 'select',
           options: ['primary', 'secondary', 'tertiary'],
+        },{
+          propName: 'size',
+          defaultValue: 'medium',
+          knob: 'select',
+          options: ['small', 'medium', 'large'],
         },
         {
           propName: 'disabled',
