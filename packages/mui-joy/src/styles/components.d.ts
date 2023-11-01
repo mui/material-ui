b6906a61a31cd31127ae780b7fc4443f05680487
siriwatknp <siriwatkunaporn@gmail.com>
2023-11-01 14:24:33 +0700

    add spec themeing

--- packages/mui-joy/src/styles/components.d.ts
+++ packages/mui-joy/src/styles/components.d.ts
@@ -196,6 +196,13 @@ import { OptionProps, OptionOwnerState, OptionSlot } from '../Option/OptionProps
 import { SliderProps, SliderOwnerState, SliderSlot } from '../Slider/SliderProps';
 import { SnackbarProps, SnackbarOwnerState, SnackbarSlot } from '../Snackbar/SnackbarProps';
 import { StackProps, StackSlot } from '../Stack/StackProps';
+import { StepperProps, StepperOwnerState, StepperSlot } from '../Stepper/StepperProps';
+import { StepProps, StepOwnerState, StepSlot } from '../Step/StepProps';
+import {
+  StepIndicatorProps,
+  StepIndicatorOwnerState,
+  StepIndicatorSlot,
+} from '../StepIndicator/StepIndicatorProps';
 import { SvgIconProps, SvgIconOwnerState, SvgIconSlot } from '../SvgIcon/SvgIconProps';
 import { SwitchProps, SwitchOwnerState, SwitchSlot } from '../Switch/SwitchProps';
 import { TabProps, TabOwnerState, TabSlot } from '../Tab/TabProps';
@@ -450,6 +457,18 @@ export interface Components<Theme = unknown> {
   JoyStack?: {
     defaultProps?: Partial<StackProps>;
     styleOverrides?: StyleOverrides<StackSlot, StackProps, Theme>;
+  };
+  JoyStepper?: {
+    defaultProps?: Partial<StepperProps>;
+    styleOverrides?: StyleOverrides<StepperSlot, StepperOwnerState, Theme>;
+  };
+  JoyStep?: {
+    defaultProps?: Partial<StepProps>;
+    styleOverrides?: StyleOverrides<StepSlot, StepOwnerState, Theme>;
+  };
+  JoyStepIndicator?: {
+    defaultProps?: Partial<StepIndicatorProps>;
+    styleOverrides?: StyleOverrides<StepIndicatorSlot, StepIndicatorOwnerState, Theme>;
   };
   JoySwitch?: {
     defaultProps?: Partial<SwitchProps>;
