9f6d0dfb1a1df563eb6a2dc27e746bbf9ae300ff
zanivan <victorzanivan@gmail.com>
2023-11-01 13:17:26 -0300

    Beef filters up

--- docs/data/joy/getting-started/templates/team/App.tsx
+++ docs/data/joy/getting-started/templates/team/App.tsx
@@ -41,6 +41,90 @@ import Navigation from './components/Navigation';
 
 export default function TeamExample() {
   const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+  const peopleData = [
+    {
+      name: 'Andrew Smith',
+      position: 'UI Designer',
+      avatar2x: 'https://i.pravatar.cc/80?img=7',
+      companyData: [
+        {
+          role: 'Senior designer',
+          name: 'Dribbble',
+          logo: 'https://www.vectorlogo.zone/logos/dribbble/dribbble-icon.svg',
+          years: '2015-now',
+        },
+        {
+          role: 'Designer',
+          name: 'Pinterest',
+          logo: 'https://www.vectorlogo.zone/logos/pinterest/pinterest-icon.svg',
+          years: '2012-2015',
+        },
+      ],
+      skills: ['UI design', 'Illustration'],
+    },
+    {
+      name: 'John Doe',
+      position: 'Frontend Developer',
+      avatar2x: 'https://i.pravatar.cc/80?img=8',
+      companyData: [
+        {
+          role: 'UI Engineer',
+          name: 'Google',
+          logo: 'https://www.vectorlogo.zone/logos/google/google-icon.svg',
+          years: '2018-now',
+        },
+        {
+          role: 'Frontend Developer',
+          name: 'Amazon',
+          logo: 'https://www.vectorlogo.zone/logos/amazon/amazon-icon.svg',
+          years: '2015-2018',
+        },
+      ],
+      skills: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+      name: 'Alice Johnson',
+      position: 'Product Manager',
+      avatar2x: 'https://i.pravatar.cc/80?img=9',
+      companyData: [
+        {
+          role: 'Product Manager',
+          name: 'Microsoft',
+          logo: 'https://www.vectorlogo.zone/logos/microsoft/microsoft-icon.svg',
+          years: '2016-now',
+        },
+        {
+          role: 'Product Analyst',
+          name: 'IBM',
+          logo: 'https://www.vectorlogo.zone/logos/ibm/ibm-icon.svg',
+          years: '2013-2016',
+        },
+      ],
+      skills: ['Product Management', 'Market Analysis'],
+    },
+    {
+      name: 'Eva Brown',
+      position: 'Graphic Designer',
+      avatar2x: 'https://i.pravatar.cc/80?img=10',
+      companyData: [
+        {
+          role: 'Art Director',
+          name: 'Adobe',
+          logo: 'https://www.vectorlogo.zone/logos/adobe/adobe-icon.svg',
+          years: '2019-now',
+        },
+        {
+          role: 'Graphic Designer',
+          name: 'Apple',
+          logo: 'https://www.vectorlogo.zone/logos/apple/apple-icon.svg',
+          years: '2016-2019',
+        },
+      ],
+      skills: ['Graphic Design', 'Illustration'],
+    },
+  ];
+
   return (
     <CssVarsProvider disableTransitionOnChange>
       <CssBaseline />
@@ -231,7 +315,8 @@ export default function TeamExample() {
                       'Chonburi',
                     ]}
                   />
-                  <Box sx={{ mt: 3, display: 'flex', gap: 1 }}>
+                  <Box sx={{ mt: 3, display: 'flex', flexDirection: 'column' }}>
+                    <Typography level="title-sm">Range</Typography>
                     <Slider
                       size="sm"
                       variant="solid"
@@ -261,6 +346,47 @@ export default function TeamExample() {
                 </Box>
               </AccordionDetails>
             </Accordion>
+            <Accordion defaultExpanded>
+              <AccordionSummary>
+                <Typography level="title-sm">Years of Experience</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box sx={{ my: 2 }}>
+                  <Slider
+                    size="sm"
+                    valueLabelFormat={(value) => `${value} years`}
+                    defaultValue={[5, 10]}
+                    step={1}
+                    min={0}
+                    max={30}
+                    valueLabelDisplay="on"
+                  />
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+              <AccordionSummary>
+                <Typography level="title-sm">Languages Spoken</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box sx={{ my: 2 }}>
+                  <Autocomplete
+                    size="sm"
+                    multiple
+                    placeholder="Select languages"
+                    options={[
+                      'English',
+                      'French',
+                      'German',
+                      'Portuguese',
+                      'Spanish',
+                    ]}
+                    getOptionLabel={(option) => option}
+                    filterSelectedOptions
+                  />
+                </Box>
+              </AccordionDetails>
+            </Accordion>
           </AccordionGroup>
         </Layout.SidePane>
         <Layout.Main>
@@ -271,7 +397,7 @@ export default function TeamExample() {
               gap: 2,
             }}
           >
-            {[...Array(3)].map((_, index) => (
+            {peopleData.map((person, index) => (
               <Sheet
                 key={index}
                 component="li"
@@ -285,58 +411,44 @@ export default function TeamExample() {
                 <Box sx={{ display: 'flex', gap: 2 }}>
                   <Avatar
                     variant="outlined"
-                    src="https://i.pravatar.cc/40?img=7"
-                    srcSet="https://i.pravatar.cc/80?img=7 2x"
+                    src={person.avatar2x}
+                    srcSet={`${person.avatar2x} 2x`}
                     sx={{ borderRadius: '50%' }}
                   />
                   <div>
-                    <Typography level="title-md">Andrew Smith</Typography>
-                    <Typography level="body-xs">UI Designer</Typography>
+                    <Typography level="title-md">{person.name}</Typography>
+                    <Typography level="body-xs">{person.position}</Typography>
                   </div>
                 </Box>
                 <Divider component="div" sx={{ my: 2 }} />
                 <List sx={{ '--ListItemDecorator-size': '40px', gap: 2 }}>
-                  <ListItem sx={{ alignItems: 'flex-start' }}>
-                    <ListItemDecorator
-                      sx={{
-                        '&:before': {
-                          content: '""',
-                          position: 'absolute',
-                          height: '100%',
-                          width: '1px',
-                          bgcolor: 'divider',
-                          left: 'calc(var(--ListItem-paddingLeft) + 12px)',
-                          top: '50%',
-                        },
-                      }}
-                    >
-                      <Avatar
-                        src="https://www.vectorlogo.zone/logos/dribbble/dribbble-icon.svg"
-                        sx={{ '--Avatar-size': '24px' }}
-                      />
-                    </ListItemDecorator>
-                    <ListItemContent>
-                      <Typography level="title-sm">Senior designer</Typography>
-                      <Typography level="body-xs">Dribbble</Typography>
-                    </ListItemContent>
-                    <Typography level="body-xs">2015-now</Typography>
-                  </ListItem>
-                  <ListItem sx={{ alignItems: 'flex-start' }}>
-                    <ListItemDecorator>
-                      <Avatar
-                        src="https://www.vectorlogo.zone/logos/pinterest/pinterest-icon.svg"
+                  {person.companyData.map((company, companyIndex) => (
+                    <ListItem key={companyIndex} sx={{ alignItems: 'flex-start' }}>
+                      <ListItemDecorator
                         sx={{
-                          backgroundColor: 'background.body',
-                          '--Avatar-size': '24px',
+                          '&:before': {
+                            content: '""',
+                            position: 'absolute',
+                            height: '100%',
+                            width: '1px',
+                            bgcolor: 'divider',
+                            left: 'calc(var(--ListItem-paddingLeft) + 12px)',
+                            top: '50%',
+                          },
                         }}
-                      />
-                    </ListItemDecorator>
-                    <ListItemContent>
-                      <Typography level="title-sm">Designer</Typography>
-                      <Typography level="body-xs">Pinterest</Typography>
-                    </ListItemContent>
-                    <Typography level="body-xs">2012-2015</Typography>
-                  </ListItem>
+                      >
+                        <Avatar
+                          src={company.logo}
+                          sx={{ '--Avatar-size': '24px' }}
+                        />
+                      </ListItemDecorator>
+                      <ListItemContent>
+                        <Typography level="title-sm">{company.role}</Typography>
+                        <Typography level="body-xs">{company.name}</Typography>
+                      </ListItemContent>
+                      <Typography level="body-xs">{company.years}</Typography>
+                    </ListItem>
+                  ))}
                 </List>
                 <Button
                   size="sm"
@@ -349,12 +461,16 @@ export default function TeamExample() {
                 <Divider component="div" sx={{ my: 2 }} />
                 <Typography level="title-sm">Skills tags:</Typography>
                 <Box sx={{ mt: 1.5, display: 'flex', gap: 1 }}>
-                  <Chip variant="outlined" color="neutral" size="sm">
-                    UI design
-                  </Chip>
-                  <Chip variant="outlined" color="neutral" size="sm">
-                    Illustration
-                  </Chip>
+                  {person.skills.map((skill, skillIndex) => (
+                    <Chip
+                      key={skillIndex}
+                      variant="outlined"
+                      color="neutral"
+                      size="sm"
+                    >
+                      {skill}
+                    </Chip>
+                  ))}
                 </Box>
               </Sheet>
             ))}
