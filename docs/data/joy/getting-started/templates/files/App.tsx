6e8085096937c8a86837a75f6935fc3f50ead7fa
zanivan <victorzanivan@gmail.com>
2023-11-02 18:43:22 -0300

    Design tweak to table mobile

--- docs/data/joy/getting-started/templates/files/App.tsx
+++ docs/data/joy/getting-started/templates/files/App.tsx
@@ -19,6 +19,7 @@ import TabList from '@mui/joy/TabList';
 import Tab from '@mui/joy/Tab';
 import List from '@mui/joy/List';
 import ListItem from '@mui/joy/ListItem';
+import ListDivider from '@mui/joy/ListDivider';
 import ListItemButton from '@mui/joy/ListItemButton';
 import ListItemContent from '@mui/joy/ListItemContent';
 import Stack from '@mui/joy/Stack';
@@ -144,6 +145,7 @@ export default function FilesExample() {
                 display: { xs: 'inherit', sm: 'none' },
                 borderRadius: 'sm',
                 overflow: 'auto',
+                backgroundColor: 'background.surface',
                 '& > *': {
                   '&:nth-child(n):not(:nth-last-child(-n+4))': {
                     borderBottom: '1px solid',
@@ -152,32 +154,47 @@ export default function FilesExample() {
                 },
               }}
             >
-              <List
-                aria-labelledby="table-in-list"
-                sx={{
-                  '& .JoyListItemButton-root': { p: '0px' },
-                }}
-              >
+              <List size="sm" aria-labelledby="table-in-list">
                 <ListItem>
                   <ListItemButton variant="soft" sx={{ bgcolor: 'transparent' }}>
-                    <ListItemContent sx={{ p: 2 }}>
+                    <ListItemContent sx={{ p: 1 }}>
                       <Box
                         sx={{
                           display: 'flex',
                           justifyContent: 'space-between',
-                          mb: 1,
                         }}
                       >
                         <Typography
-                          level="body-sm"
+                          level="title-sm"
                           startDecorator={<FolderRoundedIcon color="primary" />}
                           sx={{ alignItems: 'flex-start' }}
                         >
                           Travel pictures
                         </Typography>
-                        <Typography level="body-sm" sx={{ color: 'success.600' }}>
-                          987.5MB
-                        </Typography>
+                        <AvatarGroup
+                          size="sm"
+                          sx={{
+                            '--AvatarGroup-gap': '-8px',
+                            '--Avatar-size': '24px',
+                          }}
+                        >
+                          <Avatar
+                            src="https://i.pravatar.cc/24?img=6"
+                            srcSet="https://i.pravatar.cc/48?img=6 2x"
+                          />
+                          <Avatar
+                            src="https://i.pravatar.cc/24?img=7"
+                            srcSet="https://i.pravatar.cc/48?img=7 2x"
+                          />
+                          <Avatar
+                            src="https://i.pravatar.cc/24?img=8"
+                            srcSet="https://i.pravatar.cc/48?img=8 2x"
+                          />
+                          <Avatar
+                            src="https://i.pravatar.cc/24?img=9"
+                            srcSet="https://i.pravatar.cc/48?img=9 2x"
+                          />
+                        </AvatarGroup>
                       </Box>
                       <Box
                         sx={{
@@ -186,41 +203,126 @@ export default function FilesExample() {
                           mt: 2,
                         }}
                       >
-                        <div>
-                          <AvatarGroup
-                            size="sm"
-                            sx={{
-                              '--AvatarGroup-gap': '-8px',
-                              '--Avatar-size': '24px',
-                            }}
-                          >
-                            <Avatar
-                              src="https://i.pravatar.cc/24?img=6"
-                              srcSet="https://i.pravatar.cc/48?img=6 2x"
-                            />
-                            <Avatar
-                              src="https://i.pravatar.cc/24?img=7"
-                              srcSet="https://i.pravatar.cc/48?img=7 2x"
-                            />
-                            <Avatar
-                              src="https://i.pravatar.cc/24?img=8"
-                              srcSet="https://i.pravatar.cc/48?img=8 2x"
-                            />
-                            <Avatar
-                              src="https://i.pravatar.cc/24?img=9"
-                              srcSet="https://i.pravatar.cc/48?img=9 2x"
-                            />
-                          </AvatarGroup>
-                        </div>
-                        <Typography level="body-sm">21 October 2011, 3PM</Typography>
+                        <Typography level="body-sm">987.5MB</Typography>
+
+                        <Typography level="body-sm">21 Oct 2023, 3PM</Typography>
                       </Box>
                     </ListItemContent>
                   </ListItemButton>
                 </ListItem>
-                <Divider />
+                <ListDivider />
                 <ListItem>
                   <ListItemButton variant="soft" sx={{ bgcolor: 'transparent' }}>
-                    <ListItemContent sx={{ p: 2 }}>
+                    <ListItemContent sx={{ p: 1 }}>
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          justifyContent: 'space-between',
+                        }}
+                      >
+                        <Typography
+                          level="title-sm"
+                          startDecorator={<FolderRoundedIcon color="primary" />}
+                          sx={{ alignItems: 'flex-start' }}
+                        >
+                          Important documents
+                        </Typography>
+                        <AvatarGroup
+                          size="sm"
+                          sx={{
+                            '--AvatarGroup-gap': '-8px',
+                            '--Avatar-size': '24px',
+                          }}
+                        >
+                          <Avatar
+                            src="https://i.pravatar.cc/24?img=1"
+                            srcSet="https://i.pravatar.cc/48?img=1 2x"
+                          />
+                          <Avatar
+                            src="https://i.pravatar.cc/24?img=9"
+                            srcSet="https://i.pravatar.cc/48?img=9 2x"
+                          />
+                          <Avatar
+                            src="https://i.pravatar.cc/24?img=2"
+                            srcSet="https://i.pravatar.cc/48?img=2 2x"
+                          />
+                          <Avatar
+                            src="https://i.pravatar.cc/24?img=3"
+                            srcSet="https://i.pravatar.cc/48?img=3 2x"
+                          />
+                          <Avatar>+3</Avatar>
+                        </AvatarGroup>
+                      </Box>
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          justifyContent: 'space-between',
+                          mt: 2,
+                        }}
+                      >
+                        <Typography level="body-sm">232.3MB</Typography>
+
+                        <Typography level="body-sm">26 Sep 2023, 7PM</Typography>
+                      </Box>
+                    </ListItemContent>
+                  </ListItemButton>
+                </ListItem>
+                <ListDivider />
+                <ListItem>
+                  <ListItemButton variant="soft" sx={{ bgcolor: 'transparent' }}>
+                    <ListItemContent sx={{ p: 1 }}>
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          justifyContent: 'space-between',
+                        }}
+                      >
+                        <Typography
+                          level="title-sm"
+                          startDecorator={<FolderRoundedIcon color="primary" />}
+                          sx={{ alignItems: 'flex-start' }}
+                        >
+                          Projects
+                        </Typography>
+                        <AvatarGroup
+                          size="sm"
+                          sx={{
+                            '--AvatarGroup-gap': '-8px',
+                            '--Avatar-size': '24px',
+                          }}
+                        >
+                          <Avatar
+                            src="https://i.pravatar.cc/24?img=4"
+                            srcSet="https://i.pravatar.cc/48?img=4 2x"
+                          />
+                          <Avatar
+                            src="https://i.pravatar.cc/24?img=8"
+                            srcSet="https://i.pravatar.cc/48?img=8 2x"
+                          />
+                          <Avatar
+                            src="https://i.pravatar.cc/24?img=5"
+                            srcSet="https://i.pravatar.cc/48?img=5 2x"
+                          />
+                        </AvatarGroup>
+                      </Box>
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          justifyContent: 'space-between',
+                          mt: 2,
+                        }}
+                      >
+                        <Typography level="body-sm">1.6GB</Typography>
+
+                        <Typography level="body-sm">12 Aug 2021, 7PM</Typography>
+                      </Box>
+                    </ListItemContent>
+                  </ListItemButton>
+                </ListItem>
+                <ListDivider />
+                <ListItem>
+                  <ListItemButton variant="soft" sx={{ bgcolor: 'transparent' }}>
+                    <ListItemContent sx={{ p: 1 }}>
                       <Box
                         sx={{
                           display: 'flex',
@@ -229,15 +331,18 @@ export default function FilesExample() {
                         }}
                       >
                         <Typography
-                          level="body-sm"
+                          level="title-sm"
                           startDecorator={<FolderRoundedIcon color="primary" />}
                           sx={{ alignItems: 'flex-start' }}
                         >
-                          Important documents
+                          Invoices
                         </Typography>
-                        <Typography level="body-sm" sx={{ color: 'success.600' }}>
-                          123.3KB
-                        </Typography>
+                        <Avatar
+                          size="sm"
+                          src="https://i.pravatar.cc/24?img=2"
+                          srcSet="https://i.pravatar.cc/48?img=2 2x"
+                          sx={{ '--Avatar-size': '24px' }}
+                        />
                       </Box>
                       <Box
                         sx={{
@@ -246,33 +351,9 @@ export default function FilesExample() {
                           mt: 2,
                         }}
                       >
-                        <div>
-                          <AvatarGroup
-                            size="sm"
-                            sx={{
-                              '--AvatarGroup-gap': '-8px',
-                              '--Avatar-size': '24px',
-                            }}
-                          >
-                            <Avatar
-                              src="https://i.pravatar.cc/24?img=6"
-                              srcSet="https://i.pravatar.cc/48?img=6 2x"
-                            />
-                            <Avatar
-                              src="https://i.pravatar.cc/24?img=7"
-                              srcSet="https://i.pravatar.cc/48?img=7 2x"
-                            />
-                            <Avatar
-                              src="https://i.pravatar.cc/24?img=8"
-                              srcSet="https://i.pravatar.cc/48?img=8 2x"
-                            />
-                            <Avatar
-                              src="https://i.pravatar.cc/24?img=9"
-                              srcSet="https://i.pravatar.cc/48?img=9 2x"
-                            />
-                          </AvatarGroup>
-                        </div>
-                        <Typography level="body-sm">26 May 2010, 7PM</Typography>
+                        <Typography level="body-sm">123.3KB</Typography>
+
+                        <Typography level="body-sm">14 Mar 2021, 7PM</Typography>
                       </Box>
                     </ListItemContent>
                   </ListItemButton>
