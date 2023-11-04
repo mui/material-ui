42d3272cbae3f7a0a8e48c2f0213edcc491fb7cf
Lucas Hilgert <lhilgert@gmx.net>
2023-11-04 14:35:36 +0100

    fix imports in .test file

--- packages/mui-material-next/src/ButtonGroup/ButtonGroup.test.js
+++ packages/mui-material-next/src/ButtonGroup/ButtonGroup.test.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { expect } from 'chai';
 import { createRenderer, describeConformance, screen } from '@mui-internal/test-utils';
-import ButtonGroup, { buttonGroupClasses as classes } from '@mui/material/ButtonGroup';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Button, { buttonClasses } from '@mui/material/Button';
+import ButtonGroup, { buttonGroupClasses as classes } from '@mui/material-next/ButtonGroup';
+import { ThemeProvider, createTheme } from '@mui/material-next/styles';
+import Button, { buttonClasses } from '@mui/material-next/Button';
 import ButtonGroupContext from './ButtonGroupContext';
 
 describe('<ButtonGroup />', () => {
