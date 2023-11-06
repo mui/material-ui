6b9cee5d523304e0a8f3c8fd93a380d27be1df76
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2023-11-06 13:37:36 +0100

    [lab] Remove tree view import from `@mui/lab` (#39685)

--- packages/mui-lab/src/TreeItem/index.ts
+++ packages/mui-lab/src/TreeItem/index.ts
@@ -1,8 +1,2 @@
 export { default } from './TreeItem';
-export { getTreeItemUtilityClass, treeItemClasses, useTreeItem } from '@mui/x-tree-view/TreeItem';
-export type {
-  TreeItemProps,
-  TreeItemClasses,
-  TreeItemClassKey,
-  TreeItemContentProps,
-} from '@mui/x-tree-view/TreeItem';
+export * from './TreeItem';
