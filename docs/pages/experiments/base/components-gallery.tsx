74a3e03ca30c6b06407b75474b0618c2a396a79d
mnajdova <mnajdova@gmail.com>
2023-11-06 13:06:40 +0100

    popper, popup

--- docs/pages/experiments/base/components-gallery.tsx
+++ docs/pages/experiments/base/components-gallery.tsx
@@ -7,9 +7,31 @@ import { Menu } from '@mui/base/Menu';
 import { MenuItem } from '@mui/base/MenuItem';
 import { MenuButton } from '@mui/base/MenuButton';
 import { Dropdown } from '@mui/base/Dropdown';
+import { Popper } from '@mui/base/Popper';
+import { Unstable_Popup as Popup } from '@mui/base/Unstable_Popup';
 import { Unstable_NumberInput as NumberInput } from '@mui/base/Unstable_NumberInput';
 
 export default function ComponentsGallery() {
+  // Popper demo
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popper' : undefined;
+
+  // Popup demo
+  const [popupAnchor, setPopupAnchor] = React.useState<null | HTMLElement>(null);
+
+  const popupButtonHandleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setPopupAnchor(popupAnchor ? null : event.currentTarget);
+  };
+
+  const popupOpen = Boolean(popupAnchor);
+  const popupId = open ? 'simple-popup' : undefined;
+
   return (
     <Stack gap={2} style={{ padding: '8px' }}>
       <div>
@@ -58,6 +80,27 @@ export default function ComponentsGallery() {
           aria-label="Demo number input"
           placeholder="Type a number…"
         />
+      </div>
+      <div>
+        <button type="button" aria-describedby={id} className="GalleryButton" onClick={handleClick}>
+          Toggle Popper
+        </button>
+        <Popper id={id} open={open} anchorEl={anchorEl}>
+          <div className="GalleryPopper">The content of the Popper.</div>
+        </Popper>
+      </div>
+      <div>
+        <button
+          type="button"
+          aria-describedby={id}
+          className="GalleryButton"
+          onClick={popupButtonHandleClick}
+        >
+          Toggle Popup
+        </button>
+        <Popup id={popupId} open={popupOpen} anchor={popupAnchor}>
+          <div className="GalleryPopup">The content of the Popup.</div>
+        </Popup>
       </div>
     </Stack>
   );
