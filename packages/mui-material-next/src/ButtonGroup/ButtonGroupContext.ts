ce5b00f77bb5a9d5896b8b8dfde7949843e4fd41
Lucas Hilgert <77863078+lhilgert9@users.noreply.github.com>
2023-11-07 09:55:58 +0100

    fix ButtonGroupContext naming
    Co-authored-by: Albert Yu <albert@albertyu.co>
    Signed-off-by: Lucas Hilgert <77863078+lhilgert9@users.noreply.github.com>

--- packages/mui-material-next/src/ButtonGroup/ButtonGroupContext.ts
+++ packages/mui-material-next/src/ButtonGroup/ButtonGroupContext.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import type { ButtonGroupProps } from './ButtonGroup';
 
-interface IButtonGroupContext {
+interface ButtonGroupContext {
   className?: string;
   color?: ButtonGroupProps['color'];
   disabled?: boolean;
