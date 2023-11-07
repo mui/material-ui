06d0aadb6f89d866f3635838a810f8ab03f74116
Lucas Hilgert <lhilgert@gmx.net>
2023-11-07 14:43:36 +0100

    fix eslint

--- packages/mui-material-next/src/CircularProgress/index.ts
+++ packages/mui-material-next/src/CircularProgress/index.ts
@@ -1,5 +1,4 @@
 export { default } from './CircularProgress';
-export * from './CircularProgress';
 
 export { default as circularProgressClasses } from './circularProgressClasses';
 export * from './circularProgressClasses';
