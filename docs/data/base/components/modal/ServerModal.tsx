4f0dd0ede240e358c2b215b4f4dc83d0355c0bf1
zanivan <victorzanivan@gmail.com>
2023-11-09 17:31:36 -0300

    Fix server-side modal style

--- docs/data/base/components/modal/ServerModal.tsx
+++ docs/data/base/components/modal/ServerModal.tsx
@@ -64,39 +64,54 @@ const blue = {
 const Modal = styled(BaseModal)`
   position: fixed;
   z-index: 1300;
-  inset: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
   display: flex;
   align-items: center;
   justify-content: center;
-  padding: 8px;
-  background: inherit;
-  font-family: 'IBM Plex Sans', sans-serif;
+  &.MuiModal-hidden {
+    visibility: hidden;
+  }
 `;
 
 const style = {
   position: 'relative',
   width: 400,
-  height: 120,
 };
 
 const ModalContent = styled(Box)(
   ({ theme }) => `
-  font-family: IBM Plex Sans, sans-serif;
-  font-weight: 600;
-  font-size: 0.875rem;
-  line-height: 1.5;
-  padding: 8px 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  overflow: hidden;
+  background-color: ${theme.palette.mode === 'dark' ? grey[900] : '#FFF'};
   border-radius: 8px;
-  color: white;
-  transition: all 150ms ease;
-  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
   border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-  color: ${theme.palette.mode === 'dark' ? grey[200] : grey[900]};
-  box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
+  box-shadow: 0px 4px 12px ${
+    theme.palette.mode === 'dark' ? 'rgba(0,0,0, 0.50)' : 'rgba(0,0,0, 0.05)'
+  };
+  padding: 1rem;
+  color: ${theme.palette.mode === 'dark' ? grey[50] : grey[900]};
+  font-family: IBM Plex Sans, sans-serif;
+  font-weight: 500;
+  text-align: start;
+  position: relative;
 
-  &:focus-visible {
-    box-shadow: 0 0 0 4px ${theme.palette.mode === 'dark' ? blue[300] : blue[200]};
-    outline: none;
+
+  & .modal-title {
+    margin: 0;
+    line-height: 1.5rem;
+    margin-right: 0.5rem;
+  }
+
+  & .modal-description {
+    margin: 0;
+    line-height: 1.5rem;
+    font-weight: 400;
+    color: ${theme.palette.mode === 'dark' ? grey[400] : grey[800]};
   }
   `,
 );
