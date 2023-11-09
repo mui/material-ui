72a8a670bfc1d178efcc5df5f7d68c99e83e8665
zanivan <victorzanivan@gmail.com>
2023-11-09 17:26:53 -0300

    Fix button active state

--- docs/data/base/components/modal/NestedModal.tsx
+++ docs/data/base/components/modal/NestedModal.tsx
@@ -187,12 +187,11 @@ const TriggerButton = styled(Button)(
     border-color: ${theme.palette.mode === 'dark' ? grey[600] : grey[300]};
   }
   
-  &.active {
-    background-color: ${blue[700]};
-    box-shadow: none;
+  &:active {
+    background: ${theme.palette.mode === 'dark' ? grey[700] : grey[100]};
   }
   
-  &.focusVisible {
+  &:focus-visible {
     box-shadow: 0 0 0 4px ${theme.palette.mode === 'dark' ? blue[300] : blue[200]};
     outline: none;
   }
@@ -221,7 +220,8 @@ const ModalButton = styled(Button)(
   }
 
   &:active {
-    background: ${theme.palette.mode === 'dark' ? grey[700] : grey[100]};
+    background-color: ${blue[700]};
+    box-shadow: none;
   }
 
   &:focus-visible {
@@ -229,7 +229,7 @@ const ModalButton = styled(Button)(
     outline: none;
   }
 
-  &.disabled {
+  &:disabled {
     opacity: 0.4;
     cursor: not-allowed;
     box-shadow: none;
