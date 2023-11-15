44f20aaf6e022d938bb9b5d1f9f5a077ca98f29d
Lucas Hilgert <lhilgert@gmx.net>
2023-11-15 15:44:29 +0100

    copy files to material next

---
+++ packages/mui-material-next/src/Switch/switchClasses.ts
@@ -0,0 +1,55 @@
+import { unstable_generateUtilityClasses as generateUtilityClasses } from '@mui/utils';
+import generateUtilityClass from '../generateUtilityClass';
+
+export interface SwitchClasses {
+  /** Styles applied to the root element. */
+  root: string;
+  /** Styles applied to the root element if `edge="start"`. */
+  edgeStart: string;
+  /** Styles applied to the root element if `edge="end"`. */
+  edgeEnd: string;
+  /** Styles applied to the internal `SwitchBase` component's `root` class. */
+  switchBase: string;
+  /** Styles applied to the internal SwitchBase component's root element if `color="primary"`. */
+  colorPrimary: string;
+  /** Styles applied to the internal SwitchBase component's root element if `color="secondary"`. */
+  colorSecondary: string;
+  /** Styles applied to the root element if `size="small"`. */
+  sizeSmall: string;
+  /** Styles applied to the root element if `size="medium"`. */
+  sizeMedium: string;
+  /** State class applied to the internal `SwitchBase` component's `checked` class. */
+  checked: string;
+  /** State class applied to the internal SwitchBase component's disabled class. */
+  disabled: string;
+  /** Styles applied to the internal SwitchBase component's input element. */
+  input: string;
+  /** Styles used to create the thumb passed to the internal `SwitchBase` component `icon` prop. */
+  thumb: string;
+  /** Styles applied to the track element. */
+  track: string;
+}
+
+export type SwitchClassKey = keyof SwitchClasses;
+
+export function getSwitchUtilityClass(slot: string): string {
+  return generateUtilityClass('MuiSwitch', slot);
+}
+
+const switchClasses: SwitchClasses = generateUtilityClasses('MuiSwitch', [
+  'root',
+  'edgeStart',
+  'edgeEnd',
+  'switchBase',
+  'colorPrimary',
+  'colorSecondary',
+  'sizeSmall',
+  'sizeMedium',
+  'checked',
+  'disabled',
+  'input',
+  'thumb',
+  'track',
+]);
+
+export default switchClasses;
