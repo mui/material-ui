9e274436dc04e3a23785825f880164b449658765
Martin Trajanovski <martin.trajanovski@gmail.com>
2023-11-16 13:17:46 +0100

    remove autocomplete popup indicator negative right margin

--- packages/mui-material/src/Autocomplete/Autocomplete.js
+++ packages/mui-material/src/Autocomplete/Autocomplete.js
@@ -222,7 +222,6 @@ const AutocompleteClearIndicator = styled(IconButton, {
   slot: 'ClearIndicator',
   overridesResolver: (props, styles) => styles.clearIndicator,
 })({
-  marginRight: -2,
   padding: 4,
   visibility: 'hidden',
 });
@@ -236,7 +235,6 @@ const AutocompletePopupIndicator = styled(IconButton, {
   }),
 })(({ ownerState }) => ({
   padding: 2,
-  marginRight: -2,
   ...(ownerState.popupOpen && {
     transform: 'rotate(180deg)',
   }),
