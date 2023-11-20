7e4a6ccdc7d05e5759533ae4fcb7115c28034096
zanivan <victorzanivan@gmail.com>
2023-11-20 17:43:50 -0300

    Fix Stylelint

--- docs/data/base/components/modal/ServerModal.js
+++ docs/data/base/components/modal/ServerModal.js
@@ -58,10 +58,7 @@ const grey = {
 const Modal = styled(BaseModal)`
   position: fixed;
   z-index: 1300;
-  right: 0;
-  bottom: 0;
-  top: 0;
-  left: 0;
+  inset: 0;
   display: flex;
   align-items: center;
   justify-content: center;
