fad0d7f532ab9a8a9a0532cc4270d0c46f725a92
Lucas Hilgert <lhilgert@gmx.net>
2023-11-22 22:14:56 +0100

    fix fourColor colors

--- packages/mui-material-next/src/LinearProgress/LinearProgress.tsx
+++ packages/mui-material-next/src/LinearProgress/LinearProgress.tsx
@@ -149,11 +149,11 @@ const LinearProgressRoot = styled('span', {
   '--md-comp-linear-progress-indicator-four-color-active-indicator-one-color':
     tokens.sys.color.primary,
   '--md-comp-linear-progress-indicator-four-color-active-indicator-two-color':
-    tokens.sys.color.primaryContainer,
+    tokens.sys.color.onPrimaryContainer,
   '--md-comp-linear-progress-indicator-four-color-active-indicator-three-color':
     tokens.sys.color.tertiary,
   '--md-comp-linear-progress-indicator-four-color-active-indicator-four-color':
-    tokens.sys.color.tertiaryContainer,
+    tokens.sys.color.onTertiaryContainer,
   position: 'relative',
   overflow: 'hidden',
   display: 'block',
