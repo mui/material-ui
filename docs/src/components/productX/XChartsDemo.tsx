d4764983256484e1873314e14477ceeb0fa2a59c
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2023-11-27 03:35:15 -0300

    remove commented code

--- docs/src/components/productX/XChartsDemo.tsx
+++ docs/src/components/productX/XChartsDemo.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-// import { LineChart } from '@mui/x-charts/LineChart';
 import { BarChart } from '@mui/x-charts/BarChart';
 import { mangoFusionPalette } from '@mui/x-charts';
 import Frame from 'docs/src/components/action/Frame';
@@ -44,22 +43,6 @@ export default function XChartsDemo() {
           })}
         >
           <Box sx={{ p: 2 }}>
-            {/* <LineChart
-              sx={{
-                '& .MuiMarkElement-root': { display: 'none' },
-                '& .MuiMarkElement-root.MuiMarkElement-highlighted': { display: 'block' },
-              }}
-              margin={{ top: 10, bottom: 50, left: 40, right: 20 }}
-              series={[
-                { data: [35, 44, 24, 34], stack: 'total', area: true },
-                { data: [51, 32, 49, 30], stack: 'total', area: true },
-                { data: [5, 31, 30, 50], stack: 'total', area: true },
-                { data: [60, 50, 15, 10], stack: 'total', area: true },
-              ]}
-              xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'point' }]}
-              colors={mangoFusionPalette}
-              height={250}
-            /> */}
             <BarChart
               series={[
                 { data: [35, 44, 24, 34] },
