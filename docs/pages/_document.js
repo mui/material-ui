13db137d7f915089d5fef7fbfc0db6affc4b015d
siriwatknp <siriwatkunaporn@gmail.com>
2023-11-30 15:56:44 +0700

    fix as suggested

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -263,6 +263,8 @@ MyDocument.getInitialProps = async (ctx) => {
       canonicalAsServer: pathnameToLanguage(url).canonicalAsServer,
       userLanguage: ctx.query.userLanguage || 'en',
       styles: [
+        <style id="material-icon-font" key="material-icon-font" />,
+        <style id="font-awesome-css" key="font-awesome-css" />,
         ...finalProps.emotionStyleTags,
         <style id="app-search" key="app-search" />,
         <style id="prismjs" key="prismjs" />,
