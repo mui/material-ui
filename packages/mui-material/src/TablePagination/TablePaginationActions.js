b09516c767e736c15684cbc63d7cbea317d30450
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2023-12-07 19:48:43 +0530

    prettier

--- packages/mui-material/src/TablePagination/TablePaginationActions.js
+++ packages/mui-material/src/TablePagination/TablePaginationActions.js
@@ -160,7 +160,7 @@ TablePaginationActions.propTypes = {
     nextButton: PropTypes.object,
     previousButton: PropTypes.object,
   }),
-   /**
+  /**
    * The components used for each slot inside the TablePaginationActions.
    * Either a string to use a HTML element or a component.
    * @default {}
