f352b5cacd9df3db343ef454e7e07b8b2bd9029c
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2023-12-08 11:57:47 +0530

    add TS test

--- packages/mui-material/src/TablePagination/TablePagination.spec.tsx
+++ packages/mui-material/src/TablePagination/TablePagination.spec.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
 import TablePagination from '@mui/material/TablePagination';
+import SvgIcon from '@mui/material/SvgIcon';
+
+function SampleIcon() {
+  return (
+    <SvgIcon>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+  );
+}
 
 function classesTest() {
   const defaultProps = {
@@ -13,3 +22,23 @@ function classesTest() {
   // @ts-expect-error desired
   <TablePagination classes={{ alignCenter: 'center' }} {...defaultProps} />;
 }
+
+// slots type test
+<TablePagination
+  rowsPerPageOptions={[10, 25, 100]}
+  component="div"
+  count={1}
+  rowsPerPage={1}
+  page={1}
+  onPageChange={() => {}}
+  showFirstButton
+  showLastButton
+  slots={{
+    actions: {
+      firstPageIcon: SampleIcon,
+      lastPageIcon: SampleIcon,
+      nextPageIcon: SampleIcon,
+      previousPageIcon: SampleIcon,
+    },
+  }}
+/>;
