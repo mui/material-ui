a14be05ead50862271203dfc4a5ce39010ac4970
sai chand <60743144+sai6855@users.noreply.github.com>
2023-12-11 12:13:45 +0530

    Update PositionedPopper.tsx

--- docs/data/material/components/popper/PositionedPopper.tsx
+++ docs/data/material/components/popper/PositionedPopper.tsx
@@ -23,7 +23,7 @@ export default function PositionedPopper() {
   return (
     <Box sx={{ width: 500 }}>
       <Popper
-        // Note: The following zIndex style is specifically for documentation purposes and may not be necessary in your code.
+        // Note: The following zIndex style is specifically for documentation purposes and may not be necessary in your application.
         sx={{ zIndex: 1200 }}
         open={open}
         anchorEl={anchorEl}
