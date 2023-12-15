33f16fb66f22f99ea6cba9e4b6bc26fd85335390
Matt <github@nospam.33m.co>
2023-12-15 09:16:47 +0000

    [docs] Replace Twitter with X (#40193)

--- docs/pages/blog.tsx
+++ docs/pages/blog.tsx
@@ -13,7 +13,7 @@ import Pagination from '@mui/material/Pagination';
 import Button from '@mui/material/Button';
 import KeyboardArrowRightRoundedIcon from '@mui/icons-material/KeyboardArrowRightRounded';
 import Chip from '@mui/material/Chip';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import XIcon from '@mui/icons-material/X';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import YouTubeIcon from '@mui/icons-material/YouTube';
@@ -459,8 +459,7 @@ export default function Blog(props: InferGetStaticPropsType<typeof getStaticProp
                   GitHub
                 </Link>
                 <Link href="https://twitter.com/MUI_hq" target="_blank" fontSize={14}>
-                  <TwitterIcon fontSize="small" />
-                  Twitter
+                  <XIcon fontSize="small" />X
                 </Link>
                 <Link href="https://mui.com/r/discord/" target="_blank" fontSize={14}>
                   <DiscordIcon fontSize="small" />
