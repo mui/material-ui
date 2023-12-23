25b36c0866428c8f68c12ad7372686a5435d8908
anle9650 <andy.le@ucdenver.edu>
2023-12-22 23:47:29 -0700

    add expand icons

--- docs/data/material/components/accordion/AccordionUsage.js
+++ docs/data/material/components/accordion/AccordionUsage.js
@@ -3,27 +3,34 @@ import Accordion from '@mui/material/Accordion';
 import AccordionActions from '@mui/material/AccordionActions';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Button from '@mui/material/Button';
 
 export default function AccordionUsage() {
   return (
     <div>
       <Accordion>
-        <AccordionSummary>Accordion 1</AccordionSummary>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          Accordion 1
+        </AccordionSummary>
         <AccordionDetails>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
           malesuada lacus ex, sit amet blandit leo lobortis eget.
         </AccordionDetails>
       </Accordion>
       <Accordion>
-        <AccordionSummary>Accordion 2</AccordionSummary>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          Accordion 2
+        </AccordionSummary>
         <AccordionDetails>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
           malesuada lacus ex, sit amet blandit leo lobortis eget.
         </AccordionDetails>
       </Accordion>
       <Accordion expanded>
-        <AccordionSummary>Accordion Actions</AccordionSummary>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          Accordion Actions
+        </AccordionSummary>
         <AccordionDetails>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
           malesuada lacus ex, sit amet blandit leo lobortis eget.
