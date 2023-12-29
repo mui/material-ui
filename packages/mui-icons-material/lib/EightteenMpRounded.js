72bf678a097572368b49ed75b4c75bce18a6fcf4
Michał Dudak <michal@mui.com>
2023-12-29 21:31:42 +0100

    [icons] Update the icons

--- packages/mui-icons-material/lib/EightteenMpRounded.js
+++ packages/mui-icons-material/lib/EightteenMpRounded.js
@@ -7,11 +7,10 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = void 0;
 var _createSvgIcon = _interopRequireDefault(require("./utils/createSvgIcon"));
 var _jsxRuntime = require("react/jsx-runtime");
-var _default = (0, _createSvgIcon.default)([/*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
+var _default = exports.default = (0, _createSvgIcon.default)([/*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
   d: "M13.5 9H15v1.5h-1.5z"
 }, "0"), /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
   d: "M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM7.75 5.5H9c.55 0 1 .45 1 1v4.25c0 .41-.34.75-.75.75s-.75-.34-.75-.75V7h-.75C7.34 7 7 6.66 7 6.25s.34-.75.75-.75zm4.75 12.25c0 .41-.34.75-.75.75s-.75-.34-.75-.75V14h-1v2.25c0 .41-.34.75-.75.75s-.75-.34-.75-.75V14h-1v3.75c0 .41-.34.75-.75.75S6 18.16 6 17.75V13.5c0-.55.45-1 1-1h4.5c.55 0 1 .45 1 1v4.25zM12 10.5v-4c0-.55.45-1 1-1h2.5c.55 0 1 .45 1 1v4c0 .55-.45 1-1 1H13c-.55 0-1-.45-1-1zm6 5.5c0 .55-.45 1-1 1h-2v.75c0 .41-.34.75-.75.75s-.75-.34-.75-.75V13.5c0-.55.45-1 1-1H17c.55 0 1 .45 1 1V16z"
 }, "1"), /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
   d: "M13.5 6.5H15V8h-1.5zM15 14h1.5v1.5H15z"
 }, "2")], 'EightteenMpRounded');
-exports.default = _default;
--- packages/mui-icons-material/lib/esm/BookmarkSharp.js
+++ packages/mui-icons-material/lib/esm/BookmarkSharp.js
@@ -3,5 +3,5 @@
 import createSvgIcon from './utils/createSvgIcon';
 import { jsx as _jsx } from "react/jsx-runtime";
 export default createSvgIcon( /*#__PURE__*/_jsx("path", {
-  d: "M19 3H5v18l7-3 7 3V3z"
+  d: "M19 3H5v18l7-3 7 3z"
 }), 'BookmarkSharp');
