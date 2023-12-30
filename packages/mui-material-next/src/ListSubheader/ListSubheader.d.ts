cd99e0d94c03c9a9fc7b236d7f12b1efe82b3365
sai6855 <saichand685@gmail.com>
2023-12-30 12:18:44 +0530

    Update ListSubheader.d.ts

--- packages/mui-material-next/src/ListSubheader/ListSubheader.d.ts
+++ packages/mui-material-next/src/ListSubheader/ListSubheader.d.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { SxProps } from '@mui/system';
+import { OverridableComponent, OverrideProps } from '@mui/types';
 import { Theme } from '..';
-import { OverridableComponent, OverrideProps } from '../OverridableComponent';
 import { ListSubheaderClasses } from './listSubheaderClasses';
 
 export interface ListSubheaderOwnProps {
