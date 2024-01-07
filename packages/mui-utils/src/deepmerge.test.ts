756d68108d3695a898493ca7dffb9723ed064a50
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-01-07 19:36:15 +0100

    pnpm prettier

--- packages/mui-utils/src/deepmerge.test.ts
+++ packages/mui-utils/src/deepmerge.test.ts
@@ -5,13 +5,9 @@ import deepmerge from './deepmerge';
 describe('deepmerge', () => {
   // https://snyk.io/blog/after-three-years-of-silence-a-new-jquery-prototype-pollution-vulnerability-emerges-once-again/
   it('should not be subject to prototype pollution', () => {
-    deepmerge(
-      {},
-      JSON.parse('{ "myProperty": "a", "__proto__" : { "isAdmin" : true } }'),
-      {
-        clone: false,
-      },
-    );
+    deepmerge({}, JSON.parse('{ "myProperty": "a", "__proto__" : { "isAdmin" : true } }'), {
+      clone: false,
+    });
 
     expect({}).not.to.have.property('isAdmin');
   });
