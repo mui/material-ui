fa8986e0a5748ded8bea9e54e639a10ea32ee28a
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2024-01-08 14:38:15 -0600

    cleanup:

--- docs/data/material/components/alert/SimpleAlert.js
+++ docs/data/material/components/alert/SimpleAlert.js
@@ -5,7 +5,7 @@ import CheckIcon from '@mui/icons-material/Check';
 export default function SimpleAlert() {
   return (
     <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
-      Here's a gentle confirmation that your action was successful.
+      Here is a gentle confirmation that your action was successful.
     </Alert>
   );
 }
