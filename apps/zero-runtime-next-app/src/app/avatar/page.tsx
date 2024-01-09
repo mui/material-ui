fc09edee691a31f8fb90e3aba3363323fb07ec4c
mnajdova <mnajdova@gmail.com>
2024-01-09 12:38:21 +0100

    Add zero-runtime example

---
+++ apps/zero-runtime-next-app/src/app/avatar/page.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Avatar from '@/components/Avatar/Avatar';
+import Stack from '@mui/material/Stack';
+
+export default function Avatars() {
+  return (
+    <Stack direction="row" spacing={2}>
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+    </Stack>
+  );
+}
