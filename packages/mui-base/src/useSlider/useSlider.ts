f28366df33e875ff10bbcc166d580a89358df487
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2024-01-15 13:05:10 +0530

    remove checking typeof number changes

--- packages/mui-base/src/useSlider/useSlider.ts
+++ packages/mui-base/src/useSlider/useSlider.ts
@@ -253,7 +253,7 @@ export function useSlider(parameters: UseSliderParameters): UseSliderReturnValue
 
   const range = Array.isArray(valueDerived);
   let values = range ? valueDerived.slice().sort(asc) : [valueDerived];
-  values = values.map((value) => (typeof value === 'number' ? clamp(value, min, max) : min));
+  values = values.map((value) => clamp(value, min, max));
 
   const marks =
     marksProp === true && step !== null
@@ -335,15 +335,12 @@ export function useSlider(parameters: UseSliderParameters): UseSliderReturnValue
         }
       }
 
-      newValue = typeof newValue === 'number' ? clamp(newValue, min, max) : min;
+      newValue = clamp(newValue, min, max);
 
       if (range) {
         // Bound the new value to the thumb's neighbours.
         if (disableSwap) {
-          const prevValue = values[index - 1] || -Infinity;
-          const nextValue = values[index + 1] || Infinity;
-          newValue =
-            typeof newValue === 'number' ? clamp(newValue, prevValue, nextValue) : prevValue;
+          newValue = clamp(newValue, values[index - 1] || -Infinity, values[index + 1] || Infinity);
         }
 
         const previousValue = newValue;
@@ -411,7 +408,7 @@ export function useSlider(parameters: UseSliderParameters): UseSliderReturnValue
       newValue = marksValues[closestIndex!];
     }
 
-    newValue = typeof newValue === 'number' ? clamp(newValue, min, max) : min;
+    newValue = clamp(newValue, min, max);
     let activeIndex = 0;
 
     if (range) {
@@ -423,9 +420,11 @@ export function useSlider(parameters: UseSliderParameters): UseSliderReturnValue
 
       // Bound the new value to the thumb's neighbours.
       if (disableSwap) {
-        const prevValue = values[activeIndex - 1] || -Infinity;
-        const nextValue = values[activeIndex + 1] || Infinity;
-        newValue = typeof newValue === 'number' ? clamp(newValue, prevValue, nextValue) : prevValue;
+        newValue = clamp(
+          newValue,
+          values[activeIndex - 1] || -Infinity,
+          values[activeIndex + 1] || Infinity,
+        );
       }
 
       const previousValue = newValue;
