f2868376415ab85c350a80cebda6e5d2b4dee8eb
zanivan <victorzanivan@gmail.com>
2024-01-18 09:48:28 -0300

    remove custom divs

--- docs/data/material/components/slider/CustomMarks.js
+++ docs/data/material/components/slider/CustomMarks.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { styled } from '@mui/material/styles';
 import Slider from '@mui/material/Slider';
+import Typography from '@mui/material/Typography';
 
 const MAX = 100;
 const MIN = 0;
@@ -16,35 +16,39 @@ const marks = [
   },
 ];
 
-const Label = styled('div')(() => ({
+const labelStyle = {
   fontSize: '0.875rem',
-  lineHeight: '1.5',
-  fontWeight: 500,
+  lineHeight: '1.43',
+  fontWeight: 400,
   cursor: 'pointer',
-}));
+  color: 'rgba(0, 0, 0, 0.6)',
+};
 
 export default function CustomMarks() {
   const [val, setVal] = React.useState(MIN);
   const handleChange = (_, newValue) => {
     setVal(newValue);
   };
+
   return (
     <Box sx={{ width: 250 }}>
-      <div>
-        <Slider
-          marks={marks}
-          step={10}
-          value={val}
-          valueLabelDisplay="auto"
-          min={MIN}
-          max={MAX}
-          onChange={handleChange}
-        />
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Label onClick={() => setVal(MIN)}>{MIN} min</Label>
-        <Label onClick={() => setVal(MAX)}>{MAX} max</Label>
-      </div>
+      <Slider
+        marks={marks}
+        step={10}
+        value={val}
+        valueLabelDisplay="auto"
+        min={MIN}
+        max={MAX}
+        onChange={handleChange}
+      />
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography onClick={() => setVal(MIN)} sx={labelStyle}>
+          {MIN} min
+        </Typography>
+        <Typography onClick={() => setVal(MAX)} sx={labelStyle}>
+          {MAX} max
+        </Typography>
+      </Box>
     </Box>
   );
 }
