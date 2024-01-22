76eceb83fdd7f07c445ff87a78269a6acbe8ad2b
Michał Dudak <michal@mui.com>
2024-01-22 12:33:51 +0100

    Add transitions to plain CSS and Tailwind demos

--- docs/data/base/components/menu/MenuIntroduction/tailwind/index.js
+++ docs/data/base/components/menu/MenuIntroduction/tailwind/index.js
@@ -6,6 +6,8 @@ import { MenuButton as BaseMenuButton } from '@mui/base/MenuButton';
 import { MenuItem as BaseMenuItem } from '@mui/base/MenuItem';
 import { Dropdown } from '@mui/base/Dropdown';
 import { useTheme } from '@mui/system';
+import { PopupContext } from '@mui/base/Unstable_Popup';
+import { CssTransition } from '@mui/base/Transitions';
 
 function useIsDarkMode() {
   const theme = useTheme();
@@ -48,6 +50,9 @@ const Menu = React.forwardRef((props, ref) => {
     <BaseMenu
       ref={ref}
       {...props}
+      slots={{
+        listbox: AnimatedListbox,
+      }}
       slotProps={{
         ...props.slotProps,
         root: (ownerState) => {
@@ -71,7 +76,7 @@ const Menu = React.forwardRef((props, ref) => {
           return {
             ...resolvedSlotProps,
             className: clsx(
-              'text-sm box-border font-sans p-1.5 my-3 mx-0 rounded-xl overflow-auto outline-0 bg-white dark:bg-slate-900 border border-solid border-slate-200 dark:border-slate-700 text-slate-900 dark:text-slate-300 min-w-listbox shadow-md dark:shadow-slate-900',
+              'text-sm box-border font-sans p-1.5 my-3 mx-0 rounded-xl overflow-auto outline-0 bg-white dark:bg-slate-900 border border-solid border-slate-200 dark:border-slate-700 text-slate-900 dark:text-slate-300 min-w-listbox shadow-md dark:shadow-slate-900 [.open_&]:opacity-100 [.open_&]:scale-100 transition-[opacity,transform] [.closed_&]:opacity-0 [.closed_&]:scale-90 [.placement-top_&]:origin-bottom [.placement-bottom_&]:origin-top',
               resolvedSlotProps?.className,
             ),
           };
@@ -129,4 +134,31 @@ const MenuItem = React.forwardRef((props, ref) => {
 
 MenuItem.propTypes = {
   className: PropTypes.string,
+};
+
+const AnimatedListbox = React.forwardRef(function AnimatedListbox(props, ref) {
+  const { ownerState, ...other } = props;
+  const popupContext = React.useContext(PopupContext);
+
+  if (popupContext == null) {
+    throw new Error(
+      'The `AnimatedListbox` component cannot be rendered outside a `Popup` component',
+    );
+  }
+
+  const verticalPlacement = popupContext.placement.split('-')[0];
+
+  return (
+    <CssTransition
+      className={`placement-${verticalPlacement}`}
+      enterClassName="open"
+      exitClassName="closed"
+    >
+      <ul {...other} ref={ref} />
+    </CssTransition>
+  );
+});
+
+AnimatedListbox.propTypes = {
+  ownerState: PropTypes.object.isRequired,
 };
