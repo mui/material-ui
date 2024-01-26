d774936f282cd1960af58454f4f986322f537af2
zanivan <victorzanivan@gmail.com>
2024-01-26 16:51:39 -0300

    Alphabetize imports from FAQ

--- docs/data/material/getting-started/templates/landing-page/components/FAQ.tsx
+++ docs/data/material/getting-started/templates/landing-page/components/FAQ.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
