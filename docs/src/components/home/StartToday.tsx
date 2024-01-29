e8e7f07bed608fe3e414aa1d7803188cd09e53c2
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2024-01-29 18:15:12 -0300

    make the Start now section button full width on mobile

--- docs/src/components/home/StartToday.tsx
+++ docs/src/components/home/StartToday.tsx
@@ -11,8 +11,7 @@ export default function StartToday() {
       sx={{
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center',
-        position: 'relative',
+        alignItems: { xs: 'auto', sm: 'center' },
       }}
     >
       <SectionHeadline
