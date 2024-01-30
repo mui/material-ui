a02819c2d07dd958e200338b371c2c69e2116e41
sai6855 <saichand685@gmail.com>
2024-01-30 17:14:39 +0530

    fix

---
+++ packages/mui-codemod/src/deprecations/divider-props/divider-props.js
@@ -0,0 +1,25 @@
+import findComponentJSX from '../../util/findComponentJSX';
+
+/**
+ * @param {import('jscodeshift').FileInfo} file
+ * @param {import('jscodeshift').API} api
+ */
+export default function transformer(file, api, options) {
+  const j = api.jscodeshift;
+  const root = j(file.source);
+  const printOptions = options.printOptions;
+
+  findComponentJSX(j, { root, componentName: 'Divider' }, (elementPath) => {
+    console.log(elementPath.node.openingElement.name.name);
+    elementPath.node.openingElement.attributes = elementPath.node.openingElement.attributes.filter(
+      (attr) => {
+        if (attr.type === 'JSXAttribute' && attr.name.name === 'light') {
+          return false;
+        }
+        return true;
+      },
+    );
+  });
+
+  return root.toSource(printOptions);
+}
---
+++ packages/mui-codemod/src/deprecations/divider-props/test-cases/actual.js
@@ -0,0 +1,8 @@
+import Divider from '@mui/material/Divider';
+import { Card as MyDivider } from '@mui/material';
+
+import Button from '@mui/material/Button';
+
+<Divider a />;
+<MyDivider a />;
+<Button />
