9c991a997c1ff8fae5a83ec36d3f73592b6997cc
sai6855 <saichand685@gmail.com>
2024-01-30 17:15:38 +0530

    fix

--- packages/mui-codemod/src/util/findComponentJSX.js
+++ packages/mui-codemod/src/util/findComponentJSX.js
@@ -17,8 +17,10 @@ export default function findComponentJSX(j, options, callback) {
 
   root
     .find(j.ImportDeclaration)
-    .filter((path) =>
-      path.node.source.value.match(new RegExp(`^@mui/material/?(${componentName})?`)),
+    .filter(
+      (path) =>
+        path.node.source.value.match(new RegExp(`^@mui/material/(${componentName})`)) ||
+        path.node.source.value === '@mui/material',
     )
     .forEach((path) => {
       path.node.specifiers.forEach((specifier) => {
