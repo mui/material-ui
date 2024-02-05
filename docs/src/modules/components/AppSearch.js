a757b706f2faa77ad0e764585daadbb6a4f0c070
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-02-05 21:44:34 +0100

    Revert "fix search icon issue"
    This reverts commit c694740ab5c430af5f659763d0ace36b53ffb079.

--- docs/src/modules/components/AppSearch.js
+++ docs/src/modules/components/AppSearch.js
@@ -80,10 +80,12 @@ const SearchButton = styled('button')(({ theme }) => [
   }),
 ]);
 
-const SearchLabel = styled('span')(({ theme }) => ({
-  marginLeft: theme.spacing(1),
-  marginRight: 'auto',
-}));
+const SearchLabel = styled('span')(({ theme }) => {
+  return {
+    marginLeft: theme.spacing(1),
+    marginRight: 'auto',
+  };
+});
 
 const Shortcut = styled('div')(({ theme }) => {
   return {
@@ -106,7 +108,7 @@ function NewStartScreen() {
   const startScreenOptions = [
     {
       category: {
-        name: 'Material UI',
+        name: 'Material UI',
       },
       items: [
         {
@@ -133,7 +135,7 @@ function NewStartScreen() {
     },
     {
       category: {
-        name: 'Base UI',
+        name: 'Base UI',
       },
       items: [
         {
@@ -204,7 +206,7 @@ function NewStartScreen() {
     },
     {
       category: {
-        name: 'MUI Toolpad',
+        name: 'MUI Toolpad',
       },
       items: [
         {
@@ -226,7 +228,7 @@ function NewStartScreen() {
     },
     {
       category: {
-        name: 'MUI System',
+        name: 'MUI System',
       },
       items: [
         {
@@ -554,15 +556,6 @@ export default function AppSearch(props) {
               color: (theme.vars || theme).palette.primary[500],
               marginRight: theme.spacing(1.5),
               opacity: 0.6,
-              // Redefine SvgIcon-root style as ReactDOMServer.renderToStaticMarkup doesn't
-              // Generate the CSS.
-              // TODO v6: This hack should no longer be needed with static CSS rendering.
-              userSelect: 'none',
-              width: '1em',
-              height: '1em',
-              display: 'inline-block',
-              flexShrink: 0,
-              fill: 'currentColor',
             },
             '& .DocSearch-NewStartScreenItem': {
               display: 'flex',
