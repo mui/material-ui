1e209b5451e506c71d1874fcbd84a55ea3666585
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2024-02-05 13:07:01 +0530

    add callback types

--- packages/mui-material/src/styles/variants.d.ts
+++ packages/mui-material/src/styles/variants.d.ts
@@ -3,7 +3,9 @@ import { ComponentsPropsList } from './props';
 
 export type ComponentsVariants<Theme = unknown> = {
   [Name in keyof ComponentsPropsList]?: Array<{
-    props: Partial<ComponentsPropsList[Name]>;
+    props:
+      | Partial<ComponentsPropsList[Name]>
+      | ((props: Partial<ComponentsPropsList[Name]>) => boolean);
     style: Interpolation<{ theme: Theme }>;
   }>;
 };
