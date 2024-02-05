d65a2e67ed3bbab15f5830809ff27e258b6b6532
sai6855 <saichand685@gmail.com>
2024-02-05 22:10:57 +0530

    update listbox slot style

--- docs/data/base/guides/overriding-component-structure/OverridingInternalSlot.tsx
+++ docs/data/base/guides/overriding-component-structure/OverridingInternalSlot.tsx
@@ -4,7 +4,17 @@ import { Option } from '@mui/base/Option';
 
 export default function OverridingInternalSlot() {
   return (
-    <Select slots={{ listbox: 'ol' }} defaultValue="First option">
+    <Select
+      slots={{ listbox: 'ol' }}
+      slotProps={{
+        listbox: {
+          style: {
+            backgroundColor: 'white',
+          },
+        },
+      }}
+      defaultValue="First option"
+    >
       <Option value="First option">First option</Option>
       <Option value="Second option">Second option</Option>
     </Select>
