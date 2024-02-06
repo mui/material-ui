d26349f30b740b7a456f93156e0e27cbc37afa3d
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-02-06 01:15:14 +0100

    this feels easier to read and CI should catch them

--- docs/src/components/productTemplate/TemplateHero.tsx
+++ docs/src/components/productTemplate/TemplateHero.tsx
@@ -35,7 +35,7 @@ export default function TemplateHero() {
             <IconImage width={28} height={28} loading="eager" name="product-templates" /> Templates
           </Typography>
           <Typography variant="h1" sx={{ my: 2, maxWidth: 500 }}>
-            <GradientText>Fully built</GradientText> Material&nbsp;UI templates
+            <GradientText>Fully built</GradientText> Material UI templates
           </Typography>
           <Typography color="text.secondary" sx={{ mb: 3, maxWidth: 500 }}>
             A collection of 4.5 average rating templates, selected and curated by MUI&apos;s team of
