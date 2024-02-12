1f4baaa872db7c3dff89b6fbd67450d791a2fefd
siriwatknp <siriwatkunaporn@gmail.com>
2024-02-12 15:11:46 +0700

    remove codemod

--- packages/mui-codemod/src/deprecations/mui-utils-import/test-cases/base.actual.js
+++
@@ -1,5 +0,0 @@
-import {
-  unstable_composeClasses as composeClasses,
-  useSlotProps,
-  isHostComponent,
-} from '@mui/base';
--- packages/mui-codemod/src/deprecations/mui-utils-import/test-cases/base.expected.js
+++
@@ -1,2 +0,0 @@
-import { useSlotProps, isHostComponent } from "@mui/base/utils";
-import composeClasses from "@mui/utils/composeClasses";
