e451d6a798d5234e357bfe04d0b4bfbb1e3e3039
zanivan <victorzanivan@gmail.com>
2024-02-13 10:36:53 -0300

    Replace Material Design icon

--- docs/data/material/getting-started/templates/landing-page/LandingPage.tsx
+++ docs/data/material/getting-started/templates/landing-page/LandingPage.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
-import SvgMaterialDesign from 'docs/src/icons/SvgMaterialDesign';
+import AdbRoundedIcon from '@mui/icons-material/AdbRounded';
 import AppAppBar from './components/AppAppBar';
 import Hero from './components/Hero';
 import LogoCollection from './components/LogoCollection';
@@ -59,7 +59,7 @@ function ToggleCustomTheme({
           Custom theme
         </ToggleButton>
         <ToggleButton value={false}>
-          <SvgMaterialDesign sx={{ fontSize: '20px', mr: 1 }} />
+          <AdbRoundedIcon sx={{ fontSize: '20px', mr: 1 }} />
           Material Design
         </ToggleButton>
       </ToggleButtonGroup>
