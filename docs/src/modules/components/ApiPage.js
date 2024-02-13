b9215186262ae30514257dce70bbcad363c4ea4b
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2024-02-13 17:28:44 -0300

    bonus: use a p tag instead of span for proper styles in description texts

--- docs/src/modules/components/ApiPage.js
+++ docs/src/modules/components/ApiPage.js
@@ -240,7 +240,7 @@ export default function ApiPage(props) {
 `)}
           language="jsx"
         />
-        <span dangerouslySetInnerHTML={{ __html: t('api-docs.importDifference') }} />
+        <p dangerouslySetInnerHTML={{ __html: t('api-docs.importDifference') }} />
         {componentDescription ? (
           <React.Fragment>
             <br />
