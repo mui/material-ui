bba44423cb07ec6b78ebc939848ac12aaef49d2c
zanivan <victorzanivan@gmail.com>
2024-02-14 14:55:15 -0300

    Replace icon for 'MD2' label

--- docs/data/material/getting-started/templates/landing-page/LandingPage.js
+++ docs/data/material/getting-started/templates/landing-page/LandingPage.js
@@ -5,10 +5,10 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
-import AdbRoundedIcon from '@mui/icons-material/AdbRounded';
 import AppAppBar from './components/AppAppBar';
 import Hero from './components/Hero';
 import LogoCollection from './components/LogoCollection';
@@ -52,7 +52,9 @@ function ToggleCustomTheme({ showCustomTheme, toggleCustomTheme }) {
           Custom theme
         </ToggleButton>
         <ToggleButton value={false}>
-          <AdbRoundedIcon sx={{ fontSize: '20px', mr: 1 }} />
+          <Typography fontWeight={800} sx={{ mr: 1 }}>
+            MD2
+          </Typography>
           Material Design
         </ToggleButton>
       </ToggleButtonGroup>
--- docs/data/material/getting-started/templates/landing-page/LandingPage.tsx
+++ docs/data/material/getting-started/templates/landing-page/LandingPage.tsx
@@ -4,10 +4,10 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
-import AdbRoundedIcon from '@mui/icons-material/AdbRounded';
 import AppAppBar from './components/AppAppBar';
 import Hero from './components/Hero';
 import LogoCollection from './components/LogoCollection';
@@ -59,7 +59,9 @@ function ToggleCustomTheme({
           Custom theme
         </ToggleButton>
         <ToggleButton value={false}>
-          <AdbRoundedIcon sx={{ fontSize: '20px', mr: 1 }} />
+          <Typography fontWeight={800} sx={{ mr: 1 }}>
+            MD2
+          </Typography>
           Material Design
         </ToggleButton>
       </ToggleButtonGroup>
