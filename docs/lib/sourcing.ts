940452825b77b735eff8025f50b532d1050afc54
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-02-21 14:40:47 +0100

    [blog] Group product tags together

--- docs/lib/sourcing.ts
+++ docs/lib/sourcing.ts
@@ -33,14 +33,16 @@ export function getBlogPost(filePath: string): BlogPost {
 // Avoid typos in the blog markdown pages.
 // https://www.notion.so/mui-org/Blog-247ec2bff5fa46e799ef06a693c94917
 const ALLOWED_TAGS = [
-  'Base UI',
   'Company',
   'Developer Survey',
   'Guide',
+  'Product',
+  // Product tags
+  'Material UI',
+  'Base UI',
   'Joy UI',
   'MUI X',
-  'Material UI',
-  'Product',
+  'MUI System',
 ];
 
 export const getAllBlogPosts = () => {
