956bd6c951ec5e4b01f6344c6d579be332ae6a44
zanivan <victorzanivan@gmail.com>
2024-02-22 15:04:03 -0300

    docs:typescript:formatted

---
+++ docs/data/material/getting-started/templates/checkout/InfoMobile.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close'; // Import the Close icon
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
+
+import Info from './Info';
+
+export default function InfoMobile() {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 'auto', px: 2, pb: 2 }} role="presentation">
+      <IconButton
+        onClick={toggleDrawer(false)}
+        sx={{ position: 'absolute', right: 8, top: 8 }}
+      >
+        <CloseIcon />
+      </IconButton>
+      <Info />
+    </Box>
+  );
+
+  return (
+    <Box>
+      <Button
+        variant="text"
+        endIcon={<ExpandMoreRoundedIcon />}
+        onClick={toggleDrawer(true)}
+      >
+        View details
+      </Button>
+      <Drawer open={open} anchor="top" onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </Box>
+  );
+}
