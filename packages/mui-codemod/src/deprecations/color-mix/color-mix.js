d7e038e9deaa0e419dd903b04ddeb286ec42ad72
siriwatknp <siriwatkunaporn@gmail.com>
2024-02-22 18:20:21 +0700

    fix codemod

--- packages/mui-codemod/src/deprecations/color-mix/color-mix.js
+++ packages/mui-codemod/src/deprecations/color-mix/color-mix.js
@@ -3,17 +3,17 @@
  * @param {import('jscodeshift').API} api
  */
 export default function transformer(file, api, options) {
+  if (file.path.endsWith('.d.ts')) {
+    return file.source;
+  }
   const j = api.jscodeshift;
   const root = j(file.source);
   const printOptions = options.printOptions;
 
   root.find(j.TemplateLiteral).forEach((path) => {
     if (path.node.type === 'TemplateLiteral') {
-      if (path.node.quasis?.[0]?.value?.raw === 'rgba(') {
+      if (path.node.quasis?.[0]?.value?.raw.endsWith('rgba(')) {
         path.node.quasis[0] = j.templateElement({ raw: 'color-mix(', cooked: 'color-mix(' }, false);
-        path.node.expressions[0].property = j.identifier(
-          path.node.expressions[0].property.name.replace('Channel', ''),
-        );
 
         if (path.node.expressions.length === 2) {
           path.node.quasis[1] = j.templateElement(
@@ -29,5 +29,5 @@ export default function transformer(file, api, options) {
     }
   });
 
-  return root.toSource(printOptions);
+  return root.toSource(printOptions).replace(/([a-z][a-zA-Z]+)Channel/g, '$1');
 }
