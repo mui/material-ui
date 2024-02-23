dd9de0e665e89c8aa29e16ee2452aaf2c9bb1699
mnajdova <mnajdova@gmail.com>
2024-02-23 12:20:23 +0100

    add value in the props interface

--- packages/mui-system/src/RtlProvider/index.d.ts
+++ packages/mui-system/src/RtlProvider/index.d.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-declare const RtlProvider: React.FC<{ children: React.ReactNode }>;
+declare const RtlProvider: React.FC<{ children?: React.ReactNode; value?: boolean }>;
 export const useRtl: () => boolean;
 
 export default RtlProvider;
