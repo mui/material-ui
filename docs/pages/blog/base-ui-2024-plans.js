f4b0e52bfcd4f0130453081f96f1524c661b29a2
Michał Dudak <michal@mui.com>
2024-02-26 14:17:43 +0100

    Fix next build

--- docs/pages/blog/base-ui-2024-plans.js
+++ docs/pages/blog/base-ui-2024-plans.js
@@ -3,5 +3,5 @@ import TopLayoutBlog from 'docs/src/modules/components/TopLayoutBlog';
 import { docs } from './base-ui-2024-plans.md?muiMarkdown';
 
 export default function Page() {
-  return <TopLayoutBlog {...docs} />;
+  return <TopLayoutBlog docs={docs} />;
 }
