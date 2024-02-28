d6d407f401a766985d4ca696e7c21ef1443f618c
Steve Ernstberger <steve32285@gmail.com>
2024-02-28 10:04:26 -0500

    setup classes

--- docs/data/base/components/checkbox/UnstyledCheckboxIntroduction/tailwind/index.tsx
+++ docs/data/base/components/checkbox/UnstyledCheckboxIntroduction/tailwind/index.tsx
@@ -59,8 +59,48 @@ const Checkbox = React.forwardRef<HTMLSpanElement, CheckboxProps>((props, ref) =
           return {
             ...resolvedSlotProps,
             className: clsx(
-              // 'cursor-inherit absolute w-full h-full top-0 left-0 opacity-0 z-10 border-none',
-              'cursor-inherit z-10 ',
+              'w-4 h-4',
+
+              // from input styles
+              // text-sm
+              // font-sans
+              // font-normal
+              // leading-5
+              // px-3
+              // py-2
+              // rounded-lg
+              // shadow-md
+              // shadow-slate-100
+              // dark:shadow-slate-900
+              // focus:shadow-outline-purple
+              // dark:focus:shadow-outline-purple
+              // focus:shadow-lg
+              // border
+              // border-solid
+              // border-slate-300
+              // hover:border-purple-500
+              // dark:hover:border-purple-500
+              // focus:border-purple-500
+              // dark:focus:border-purple-500
+              // dark:border-slate-600
+              // bg-white
+              // dark:bg-slate-900
+              // text-slate-900
+              // dark:text-slate-300
+              // focus-visible:outline-0
+
+              // from checkbox example styles
+              // text-blue-600
+              // bg-gray-100
+              // border-gray-300
+              // rounded
+              // focus:ring-blue-500
+              // dark:focus:ring-blue-600
+              // dark:ring-offset-gray-800
+              // focus:ring-2
+              // dark:bg-gray-700
+              // dark:border-gray-600
+
               resolvedSlotProps?.className,
             ),
           };
