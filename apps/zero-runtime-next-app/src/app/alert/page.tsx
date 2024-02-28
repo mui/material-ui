4cc43248a2d98c884308b16a2a4d350f40105a09
siriwatknp <siriwatkunaporn@gmail.com>
2024-02-28 14:08:20 +0700

    Revert "add the rest of demos"
    This reverts commit b6f3ed314aef291574a7512e31b9dc5234efb03a.

--- apps/zero-runtime-next-app/src/app/alert/page.tsx
+++ apps/zero-runtime-next-app/src/app/alert/page.tsx
@@ -1,16 +1,10 @@
 import { styled } from '@mui/zero-runtime';
 import BasicAlerts from '../../../../../docs/data/material/components/alert/BasicAlerts';
-import ColorAlerts from '../../../../../docs/data/material/components/alert/ColorAlerts';
-import DescriptionAlerts from '../../../../../docs/data/material/components/alert/DescriptionAlerts';
-import FilledAlerts from '../../../../../docs/data/material/components/alert/FilledAlerts';
-import IconAlerts from '../../../../../docs/data/material/components/alert/IconAlerts';
-import OutlinedAlerts from '../../../../../docs/data/material/components/alert/OutlinedAlerts';
-import SimpleAlert from '../../../../../docs/data/material/components/alert/SimpleAlert';
 
 const Main = styled('div')(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
-  gap: '32px',
+  gap: '16px',
   marginInline: 'auto',
   maxWidth: '900px',
   paddingBlock: '16px',
@@ -25,21 +19,9 @@ const Main = styled('div')(({ theme }) => ({
 export default function MaterialUILayout() {
   return (
     <Main>
-      <h1>Alert</h1>
-      <h2>Basic</h2>
+      <h1>Material-UI</h1>
+      <p>This is an example app using Material-UI.</p>
       <BasicAlerts />
-      <h2>Color</h2>
-      <ColorAlerts />
-      <h2>Description</h2>
-      <DescriptionAlerts />
-      <h2>Filled</h2>
-      <FilledAlerts />
-      <h2>Icon</h2>
-      <IconAlerts />
-      <h2>Outlined</h2>
-      <OutlinedAlerts />
-      <h2>Simple</h2>
-      <SimpleAlert />
     </Main>
   );
 }
--- apps/zero-runtime-next-app/src/app/layout.tsx
+++ apps/zero-runtime-next-app/src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme';
 import '@mui/zero-runtime/styles.css';
 
 import './globals.css';
@@ -17,11 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
-        </AppRouterCacheProvider>
-      </body>
+      <body className={inter.className}>{props.children}</body>
     </html>
   );
 }
