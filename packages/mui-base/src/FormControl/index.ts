4a28f55efd39beb707117ae54ec8ae8ebf5b7f24
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2024-03-05 21:42:29 +0530

    export all types from FormControl.types.ts

--- packages/mui-base/src/FormControl/index.ts
+++ packages/mui-base/src/FormControl/index.ts
@@ -2,14 +2,7 @@ export { FormControl } from './FormControl';
 
 export { FormControlContext } from './FormControlContext';
 
-export type {
-  FormControlProps,
-  FormControlRootSlotPropsOverrides,
-  FormControlState,
-  UseFormControlContextReturnValue,
-  FormControlOwnProps,
-  FormControlOwnerState,
-} from './FormControl.types';
+export * from './FormControl.types';
 
 export * from './formControlClasses';
 
