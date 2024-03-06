196e0b0a92d9174b67b64ff60d332a7be9db2779
mnajdova <mnajdova@gmail.com>
2024-03-06 08:31:01 +0100

    prettier

--- docs/scripts/formattedTSDemos.js
+++ docs/scripts/formattedTSDemos.js
@@ -84,7 +84,8 @@ async function transpileFile(tsxPath, project) {
     const source = await fse.readFile(tsxPath, 'utf8');
 
     const transformOptions = { ...babelConfig, filename: tsxPath };
-    const enableJSXPreview = !tsxPath.includes(path.join('pages', 'premium-themes', 'templates'));
+    const enableJSXPreview =
+      !tsxPath.includes(path.join('pages', 'premium-themes')) && !tsxPath.includes('templates');
     if (enableJSXPreview) {
       transformOptions.plugins = transformOptions.plugins.concat([
         [
