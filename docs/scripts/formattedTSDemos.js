f1e6b31024568bec5c59ff9e0ce593501fe7d423
mnajdova <mnajdova@gmail.com>
2024-03-08 11:47:53 +0100

    more specific path

--- docs/scripts/formattedTSDemos.js
+++ docs/scripts/formattedTSDemos.js
@@ -85,7 +85,8 @@ async function transpileFile(tsxPath, project) {
 
     const transformOptions = { ...babelConfig, filename: tsxPath };
     const enableJSXPreview =
-      !tsxPath.includes(path.join('pages', 'premium-themes')) && !tsxPath.includes('templates');
+      !tsxPath.includes(path.join('pages', 'premium-themes')) &&
+      !tsxPath.includes(path.join('getting-started', 'templates'));
     if (enableJSXPreview) {
       transformOptions.plugins = transformOptions.plugins.concat([
         [
