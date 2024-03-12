42c2b43097ff4b823614a0f1326134927b9ba78b
siriwatknp <siriwatkunaporn@gmail.com>
2024-03-12 15:36:07 +0700

    fix createCssVarsTheme

--- packages/mui-system/src/cssVars/createCssVarsTheme.ts
+++ packages/mui-system/src/cssVars/createCssVarsTheme.ts
@@ -8,13 +8,22 @@ interface Theme extends DefaultCssVarsTheme {
 function createCssVarsTheme<T extends Theme, ThemeVars extends Record<string, any>>(theme: T) {
   const { cssVarPrefix, shouldSkipGeneratingVar, ...otherTheme } = theme;
 
-  return {
-    ...theme,
-    ...prepareCssVars<Omit<T, 'shouldSkipGeneratingVar' | 'cssVarPrefix'>, ThemeVars>(otherTheme, {
-      prefix: cssVarPrefix,
-      shouldSkipGeneratingVar,
-    }),
-  };
+  const output = otherTheme as typeof theme &
+    ReturnType<
+      typeof prepareCssVars<Omit<T, 'shouldSkipGeneratingVar' | 'cssVarPrefix'>, ThemeVars>
+    >;
+  const { vars, generateThemeVars, generateStyleSheets } = prepareCssVars<
+    Omit<T, 'shouldSkipGeneratingVar' | 'cssVarPrefix'>,
+    ThemeVars
+  >(output, {
+    prefix: cssVarPrefix,
+    shouldSkipGeneratingVar,
+  });
+  output.vars = vars;
+  output.generateThemeVars = generateThemeVars;
+  output.generateStyleSheets = generateStyleSheets;
+
+  return output;
 }
 
 export default createCssVarsTheme;
