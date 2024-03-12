5f36e63b4df84992f55e5d9e761e3edfc85d4535
siriwatknp <siriwatkunaporn@gmail.com>
2024-03-12 14:05:26 +0700

    update styled tests

--- packages/pigment-react/tests/styled/fixtures/styled.output.js
+++ packages/pigment-react/tests/styled/fixtures/styled.output.js
@@ -1,4 +1,3 @@
-import { styled as _styled4 } from '@pigment-css/react';
 import { styled as _styled3 } from '@pigment-css/react';
 import { styled as _styled2 } from '@pigment-css/react';
 import { styled as _styled } from '@pigment-css/react';
@@ -6,9 +5,12 @@ import _theme from '@pigment-css/react/theme';
 const Component = /*#__PURE__*/ _styled('div')({
   classes: ['c1vtarpi'],
 });
-const Component2 = /*#__PURE__*/ _styled2('div')({
-  classes: ['c1sjy0ja'],
+export const SliderRail = /*#__PURE__*/ _styled2('span', {
+  name: 'MuiSlider',
+  slot: 'Rail',
+})({
+  classes: ['s1sjy0ja'],
 });
-const SliderRail2 = /*#__PURE__*/ _styled4('span')({
-  classes: ['s1nn157y'],
+const SliderRail2 = /*#__PURE__*/ _styled3('span')({
+  classes: ['s6hrafw'],
 });
