a0aab75855f903ae905f899c539569e7e09dad71
Diego Andai <diego@mui.com>
2024-03-13 12:37:26 -0300

    Make gradient more noticeable

--- docs/data/material/components/progress/CustomizedProgressBars.tsx
+++ docs/data/material/components/progress/CustomizedProgressBars.tsx
@@ -62,8 +62,8 @@ function GradientCircularProgress() {
       <svg width={0} height={0}>
         <defs>
           <linearGradient id="my_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-            <stop offset="0%" stopColor="rgba(184, 74, 202, 1)" />
-            <stop offset="100%" stopColor="rgba(142, 66, 235, 1)" />
+            <stop offset="0%" stopColor="#e01cd5" />
+            <stop offset="100%" stopColor="#1CB5E0" />
           </linearGradient>
         </defs>
       </svg>
