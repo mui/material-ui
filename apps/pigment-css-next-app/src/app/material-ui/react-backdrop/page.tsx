6038ae988c7dbf33433191199481ff6ae25f580f
Andrej Acevski <andrejacevski@pantone.com>
2024-03-20 23:27:18 +0100

    Fixing lint errors

--- apps/pigment-css-next-app/src/app/material-ui/react-backdrop/page.tsx
+++ apps/pigment-css-next-app/src/app/material-ui/react-backdrop/page.tsx
@@ -1,16 +1,13 @@
 'use client';
-import * as React from 'react';
 import SimpleBackdrop from '../../../../../../docs/data/material/components/backdrop/SimpleBackdrop';
 
 export default function Backdrop() {
   return (
-    <React.Fragment>
-      <section>
-        <h2> Simple Backdrop</h2>
-        <div className="demo-container">
-          <SimpleBackdrop />
-        </div>
-      </section>
-    </React.Fragment>
+    <section>
+      <h2> Simple Backdrop</h2>
+      <div className="demo-container">
+        <SimpleBackdrop />
+      </div>
+    </section>
   );
 }
