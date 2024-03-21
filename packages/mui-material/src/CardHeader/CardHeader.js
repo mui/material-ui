68ec7d7a6c7176034c99bc3b83e3157b15702aac
Andrej <44305048+aacevski@users.noreply.github.com>
2024-03-21 04:01:48 +0100

    [material-ui][Card] Convert to support CSS extraction (#41580)

--- packages/mui-material/src/CardHeader/CardHeader.js
+++ packages/mui-material/src/CardHeader/CardHeader.js
@@ -4,9 +4,10 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import composeClasses from '@mui/utils/composeClasses';
 import Typography from '../Typography';
-import useThemeProps from '../styles/useThemeProps';
-import styled from '../styles/styled';
+import { styled, createUseThemeProps } from '../zero-styled';
 import cardHeaderClasses, { getCardHeaderUtilityClass } from './cardHeaderClasses';
+
+const useThemeProps = createUseThemeProps('MuiCardHeader');
 
 const useUtilityClasses = (ownerState) => {
   const { classes } = ownerState;
