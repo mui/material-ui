8fcc08e3ac67406531a16738a8b95653d409d4a1
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-03-26 21:57:55 +0100

    Fix typo
    Signed-off-by: Olivier Tassinari <olivier.tassinari@gmail.com>

--- netlify/edge-functions/og-image.tsx
+++ netlify/edge-functions/og-image.tsx
@@ -233,7 +233,7 @@ export default async function handler(req: Request) {
       ],
       // Manage the caching
       headers: {
-        // Cach control is already done by the package (https://github.com/ascorbic/og-edge/blob/d533ef878801d7f808eb004f254e782ec6ba1e3c/mod.ts#L233-L240)
+        // Cache control is already done by the package (https://github.com/ascorbic/og-edge/blob/d533ef878801d7f808eb004f254e782ec6ba1e3c/mod.ts#L233-L240)
         'Netlify-Vary': 'query',
       },
     },
