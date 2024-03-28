86b57bacf39498fecb3d04916726d738d9fb88f9
Brijesh Bittu <brijeshb42@gmail.com>
2024-03-28 16:39:24 +0530

    [pigment-css] Setup bundlesize check
    Also run the above check in CI.
    
    At the moment, it is only configured to run for @pigment-css/react
    but can be extended to be used in any of the other packages.
    
    This is meant to keep the sizes in check over time and fail CI
    everytime it increases beyond allowed threshold to allow introspection.

--- packages/pigment-css-react/tsup.config.ts
+++ packages/pigment-css-react/tsup.config.ts
@@ -1,22 +1,23 @@
 import { Options, defineConfig } from 'tsup';
+import pkg from './package.json';
 import config from '../../tsup.config';
 
 const processors = ['styled', 'sx', 'keyframes', 'generateAtomics', 'css', 'createUseThemeProps'];
 const external = ['react', 'react-is', 'prop-types'];
 
+const isBundleSizeCheck = process.env.BUNDLE_SIZE_CHECK === '1';
+
 const baseConfig: Options = {
   ...(config as Options),
   tsconfig: './tsconfig.build.json',
   external,
+  // output to a different dir for bundle size check
+  ...(isBundleSizeCheck ? { outDir: '.bundlesize' } : {}),
 };
 
 const BASE_FILES = ['index.ts', 'theme.ts', 'Box.jsx', 'RtlProvider.tsx'];
 
-export default defineConfig([
-  {
-    ...baseConfig,
-    entry: BASE_FILES.map((file) => `./src/${file}`),
-  },
+const submodules: Options[] = [
   {
     ...baseConfig,
     entry: processors.map((file) => `./src/processors/${file}.ts`),
@@ -31,4 +32,21 @@ export default defineConfig([
     ],
     outDir: 'utils',
   },
+];
+
+export default defineConfig([
+  {
+    ...baseConfig,
+    entry: BASE_FILES.map((file) => `./src/${file}`),
+    // embed dependencirs for bundle size check
+    ...(isBundleSizeCheck
+      ? {
+          noExternal: Object.keys(pkg.dependencies),
+          loader: {
+            '.js': 'jsx',
+          },
+        }
+      : {}),
+  },
+  ...(isBundleSizeCheck ? [] : submodules),
 ]);
