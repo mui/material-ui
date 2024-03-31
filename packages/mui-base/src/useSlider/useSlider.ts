482b812b2244c46adedd16950f5758e5e78e44a4
Jeff <jeff@consentist.com>
2024-03-31 08:04:17 -0400

    updates useSlider to create correct trackOffset and trackLeap values
    The trackOffset and trackLeap variables were resulting in the following style for the track when the value was 33: {
        width: 0%;
        left: 32.3232%;
    }.

--- packages/mui-base/src/useSlider/useSlider.ts
+++ packages/mui-base/src/useSlider/useSlider.ts
@@ -631,8 +631,8 @@ export function useSlider(parameters: UseSliderParameters): UseSliderReturnValue
       doc.addEventListener('mouseup', handleTouchEnd);
     };
 
-  const trackOffset = valueToPercent(range ? values[0] : min, min, max);
-  const trackLeap = valueToPercent(values[values.length - 1], min, max) - trackOffset;
+  const trackLeap = valueToPercent(range ? values[0] : min, min, max);
+  const trackOffset = valueToPercent(values[values.length - 1], min, max) - trackLeap;
 
   const getRootProps = <ExternalProps extends Record<string, unknown> = {}>(
     externalProps: ExternalProps = {} as ExternalProps,
