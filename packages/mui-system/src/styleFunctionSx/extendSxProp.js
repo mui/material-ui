b86442421e34cbc12ca177ca4355baf7959f1fc1
mnajdova <mnajdova@gmail.com>
2024-04-05 12:02:54 +0200

    fixes on some system components

--- packages/mui-system/src/styleFunctionSx/extendSxProp.js
+++ packages/mui-system/src/styleFunctionSx/extendSxProp.js
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { isPlainObject } from '@mui/utils/deepmerge';
 import defaultSxConfig from './defaultSxConfig';
 
@@ -22,7 +23,7 @@ const splitProps = (props) => {
 
 export default function extendSxProp(props) {
   const { sx: inSx, ...other } = props;
-  const { systemProps, otherProps } = splitProps(other);
+  let { systemProps, otherProps = {} } = splitProps(other);
 
   let finalSx;
   if (Array.isArray(inSx)) {
@@ -35,6 +36,27 @@ export default function extendSxProp(props) {
       }
       return { ...systemProps, ...result };
     };
+  } else if (typeof inSx === 'string') {
+    // Support for Pigment CSS string sx value
+    otherProps.className = clsx(otherProps.className, inSx);
+  } else if (typeof inSx === 'object' && (inSx.className || inSx.vars)) {
+    const sxClass = typeof inSx === 'string' ? inSx : inSx?.className;
+    const sxVars = inSx && typeof inSx !== 'string' ? inSx.vars : undefined;
+    const sxVarsStyles = {};
+
+    if (sxVars) {
+      Object.entries(sxVars).forEach(([cssVariable, [value, isUnitLess]]) => {
+        if (typeof value === 'string' || isUnitLess) {
+          sxVarsStyles[`--${cssVariable}`] = value;
+        } else {
+          sxVarsStyles[`--${cssVariable}`] = `${value}px`;
+        }
+      });
+    }
+
+    // Support for Pigment CSS string sx value
+    otherProps.style = { ...sxVarsStyles, ...otherProps.style };
+    otherProps.className = clsx(otherProps.className, sxClass);
   } else {
     finalSx = { ...systemProps, ...inSx };
   }
