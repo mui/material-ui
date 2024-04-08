76f7a5d9d9f63b9bef10de5bcbdaba6d6268e919
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2024-04-08 14:58:31 +0200

    [docs-infra] Fix drawer performances (#41807)

--- docs/src/modules/components/AppFrame.js
+++ docs/src/modules/components/AppFrame.js
@@ -166,6 +166,9 @@ export default function AppFrame(props) {
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [settingsOpen, setSettingsOpen] = React.useState(false);
 
+  const closeDrawer = React.useCallback(() => setMobileOpen(false), []);
+  const openDrawer = React.useCallback(() => setMobileOpen(true), []);
+
   const { activePage } = React.useContext(PageContext);
 
   const disablePermanent = activePage?.disableDrawer === true || disableDrawer === true;
@@ -230,8 +233,8 @@ export default function AppFrame(props) {
       </StyledAppBar>
       <StyledAppNavDrawer
         disablePermanent={disablePermanent}
-        onClose={() => setMobileOpen(false)}
-        onOpen={() => setMobileOpen(true)}
+        onClose={closeDrawer}
+        onOpen={openDrawer}
         mobileOpen={mobileOpen}
       />
       {children}
