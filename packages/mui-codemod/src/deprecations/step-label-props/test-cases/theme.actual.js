d5e817cf3070141bf2ede9d0aed21cf06b6e1beb
sai6855 <saichand685@gmail.com>
2024-04-10 18:53:54 +0530

    add codemods

--- packages/mui-codemod/src/deprecations/step-label-props/test-cases/theme.actual.js
+++ packages/mui-codemod/src/deprecations/step-label-props/test-cases/theme.actual.js
@@ -19,6 +19,18 @@ fn({
   MuiStepLabel: {
     defaultProps: {
       StepIconComponent: StepIconComponent,
+      StepIconProps: StepIconProps,
+    },
+  },
+});
+
+fn({
+  MuiStepLabel: {
+    defaultProps: {
+      componentsProps: { label: componentsLabelProps },
+      slotProps: { label: slotLabelProps },
+      StepIconComponent: StepIconComponent,
+      StepIconProps: StepIconProps,
     },
   },
 });
