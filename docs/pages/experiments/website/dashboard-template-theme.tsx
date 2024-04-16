ea545dd37d9bb75f0fcd3bbfc173007bc161f7a0
noraleonte <noraleonte00@gmail.com>
2024-04-16 16:27:36 +0300

    add highlighted card to experiments

--- docs/pages/experiments/website/dashboard-template-theme.tsx
+++ docs/pages/experiments/website/dashboard-template-theme.tsx
@@ -210,6 +210,27 @@ export default function DashboardTemplateTheme() {
               <Button size="small">Learn More</Button>
             </CardActions>
           </Card>
+          <Card variant="highlighted" sx={{ width: 250 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Word of the Day
+              </Typography>
+              <Typography variant="h5" component="div">
+                be•nev•o•lent
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                adjective
+              </Typography>
+              <Typography variant="body2">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
         </Stack>
         <LinearProgress variant="determinate" value={50} sx={{ width: 250 }} />
         <Link href="#" sx={{ maxWidth: 'fit-content' }}>
