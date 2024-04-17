c65966ea9da0188108948bd134243096ec602e93
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2024-04-17 09:22:11 -0300

    update the icons package

--- packages/mui-icons-material/lib/esm/ThreePSharp.js
+++ packages/mui-icons-material/lib/esm/ThreePSharp.js
@@ -3,5 +3,5 @@
 import createSvgIcon from './utils/createSvgIcon';
 import { jsx as _jsx } from "react/jsx-runtime";
 export default createSvgIcon( /*#__PURE__*/_jsx("path", {
-  d: "M2 2v20l4-4h16V2zm10 4c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2m4 8H8v-.57c0-.81.48-1.53 1.22-1.85.85-.37 1.79-.58 2.78-.58.99 0 1.93.21 2.78.58.74.32 1.22 1.04 1.22 1.85z"
+  d: "M2 2v20l4-4h16V2zm10 4c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2m4 8H8v-.57c0-.81.48-1.53 1.22-1.85.85-.37 1.79-.58 2.78-.58s1.93.21 2.78.58c.74.32 1.22 1.04 1.22 1.85z"
 }), 'ThreePSharp');
