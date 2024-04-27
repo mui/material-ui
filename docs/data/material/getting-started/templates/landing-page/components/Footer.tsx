e8e704b1cce03dcae7d2c46140594aed47e5d241
5-tom <132141431+5-tom@users.noreply.github.com>
2024-04-27 11:45:41 +0100

    [docs][material-ui][templates] Fix input props attributes in Landing Page template (#42013)
    Co-authored-by: ZeeshanTamboli <zeeshan.tamboli@gmail.com>

--- docs/data/material/getting-started/templates/landing-page/components/Footer.tsx
+++ docs/data/material/getting-started/templates/landing-page/components/Footer.tsx
@@ -81,8 +81,8 @@ export default function Footer() {
                 aria-label="Enter your email address"
                 placeholder="Your email address"
                 inputProps={{
-                  autocomplete: 'off',
-                  ariaLabel: 'Enter your email address',
+                  autoComplete: 'off',
+                  'aria-label': 'Enter your email address',
                 }}
               />
               <Button variant="contained" color="primary" sx={{ flexShrink: 0 }}>
