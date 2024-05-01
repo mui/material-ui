1e40285c7317d2863e957429157dc7cc5e2d8312
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2024-05-01 06:39:22 -0300

    run the icon scripts again

--- packages/mui-icons-material/lib/EmergencySharp.js
+++ packages/mui-icons-material/lib/EmergencySharp.js
@@ -9,5 +9,5 @@ exports.default = void 0;
 var _createSvgIcon = _interopRequireDefault(require("./utils/createSvgIcon"));
 var _jsxRuntime = require("react/jsx-runtime");
 var _default = exports.default = (0, _createSvgIcon.default)( /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
-  d: "M9 17H7v-7h2zm4 0h-2V7h2zm4 0h-2v-4h2zm2.5 2.1h-15V5h15zm0-16.1h-15c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2"
-}), 'InsertChartOutlined');
+  d: "m20.79 9.23-2-3.46L14 8.54V3h-4v5.54L5.21 5.77l-2 3.46L8 12l-4.79 2.77 2 3.46L10 15.46V21h4v-5.54l4.79 2.77 2-3.46L16 12z"
+}), 'EmergencySharp');
