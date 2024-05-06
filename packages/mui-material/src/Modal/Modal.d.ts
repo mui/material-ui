6a85d1d5529ad927871ed49bd5ea60d3cdc47cea
sai6855 <fayazceg@gmail.com>
2024-05-06 22:47:26 +0530

    add types

--- packages/mui-material/src/Modal/Modal.d.ts
+++ packages/mui-material/src/Modal/Modal.d.ts
@@ -7,6 +7,7 @@ import { Theme } from '../styles';
 import Backdrop, { BackdropProps } from '../Backdrop';
 import { OverridableComponent } from '../OverridableComponent';
 import { ModalClasses } from './modalClasses';
+import { CreateSlotsAndSlotProps, SlotProps } from '../utils/types';
 
 export interface ModalComponentsPropsOverrides {}
 
@@ -26,7 +27,15 @@ export interface ModalSlots {
   backdrop?: React.ElementType;
 }
 
-export interface ModalOwnProps {
+export type ModalSlotsAndSlotProps = CreateSlotsAndSlotProps<
+  ModalSlots,
+  {
+    root: SlotProps<'div', ModalComponentsPropsOverrides, ModalOwnerState>;
+    backdrop: SlotProps<typeof Backdrop, ModalComponentsPropsOverrides, ModalOwnerState>;
+  }
+>;
+
+export interface ModalOwnProps extends ModalSlotsAndSlotProps {
   /**
    * A backdrop component. This prop enables custom backdrop rendering.
    * @deprecated Use `slots.backdrop` instead. While this prop currently works, it will be removed in the next major version.
@@ -179,20 +188,6 @@ export interface ModalOwnProps {
    */
   open: boolean;
   /**
-   * The components used for each slot inside the Modal.
-   * Either a string to use a HTML element or a component.
-   * @default {}
-   */
-  slots?: ModalSlots;
-  /**
-   * The props used for each slot inside the Modal.
-   * @default {}
-   */
-  slotProps?: {
-    root?: SlotComponentProps<'div', ModalComponentsPropsOverrides, ModalOwnerState>;
-    backdrop?: SlotComponentProps<typeof Backdrop, ModalComponentsPropsOverrides, ModalOwnerState>;
-  };
-  /**
    * The system prop that allows defining system overrides as well as additional CSS styles.
    */
   sx?: SxProps<Theme>;
@@ -239,5 +234,7 @@ export type ModalProps<
 > = OverrideProps<ModalTypeMap<RootComponent, AdditionalProps>, RootComponent> & {
   component?: React.ElementType;
 };
+
+export interface ModalOwnerState extends ModalProps {}
 
 export default Modal;
