41a3f86ce8eaad02daf2f019b723c05e0e1436d8
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2024-05-07 18:03:59 -0300

    test a different approach not using tabs (but making them look similarly)

--- docs/src/modules/components/ComponentPageTabs.js
+++ docs/src/modules/components/ComponentPageTabs.js
@@ -6,17 +6,20 @@ import Box from '@mui/material/Box';
 import Tabs, { tabsClasses } from '@mui/material/Tabs';
 import Tab, { tabClasses } from '@mui/material/Tab';
 import { useTranslate } from 'docs/src/modules/utils/i18n';
-import NextLink from 'next/link';
+import { Link } from '@mui/docs/Link';
 
 export const HEIGHT = 50;
 
-const StyledTab = styled(Tab)(({ theme }) => ({
-  padding: theme.spacing(0.5),
-  border: '1px solid',
-  borderColor: 'transparent',
-  fontWeight: theme.typography.fontWeightSemiBold,
+const LinkTab = styled(Link)(({ theme }) => ({
   minHeight: 30,
   minWidth: 0,
+  padding: theme.spacing(0.5, 1),
+  border: '1px solid',
+  borderColor: 'transparent',
+  fontFamily: theme.typography.fontFamily,
+  fontWeight: theme.typography.fontWeightSemiBold,
+  fontSize: theme.typography.pxToRem(14),
+  color: (theme.vars || theme).palette.text.secondary,
   borderRadius: '8px',
   '&:hover': {
     background: (theme.vars || theme).palette.grey[50],
@@ -50,56 +53,86 @@ export default function ComponentPageTabs(props) {
   const componentsHref = `${apiPathname}components-api`;
   const hooksHref = `${apiPathname}hooks-api`;
 
+  const widthMap = {
+    '': '62px',
+    'components-api': '127px',
+    'hooks-api': '86px',
+  };
+
+  const width = widthMap[activeTab];
+
+  const positionMap = {
+    '': '1px', // Default width when no value is specified
+    'components-api': '67px',
+    'hooks-api': '198px',
+  };
+
+  const position = positionMap[activeTab];
+
+  const tabData = [
+    {
+      key: '',
+      label: t('api-docs.demos'),
+      href: demosHref,
+    },
+    ...(headers.components?.length > 0 && [
+      {
+        key: 'components-api',
+        label: t('api-docs.componentsApi'),
+        href: componentsHref,
+      },
+    ]),
+    ...(headers.hooks &&
+      headers.hooks.length > 0 && [
+        {
+          key: 'hooks-api',
+          label: t('api-docs.hooksApi'),
+          href: hooksHref,
+        },
+      ]),
+  ];
+
   return (
     <Box className="component-tabs" sx={{ display: 'inline' }}>
-      <Tabs
-        value={activeTab}
+      <Box
+        role="toolbar"
+        className="component-tabs"
         sx={{
           position: 'sticky',
           top: 65, // to be positioned below the app bar
+          width: '100%',
           mt: 2,
-          mx: -1,
           backgroundColor: 'background.default',
           borderBottom: 1,
           borderColor: 'divider',
           zIndex: 1000,
-          [`& .${tabsClasses.flexContainer}`]: {
-            p: 1,
-            gap: 1,
-          },
-          [`& .${tabsClasses.indicator}`]: {
-            transition: 'none',
-          },
-          // Make server side styles closer to hydrated
-          [`& .${tabClasses.root}`]: {
-            overflow: 'visible',
-            [`& .${tabsClasses.indicator}`]: {
-              top: '39px',
-              borderRadius: 0,
-            },
+          display: 'inline-flex',
+          py: 1,
+          gap: 0.5,
+          '&::before': {
+            content: "''",
+            position: 'absolute',
+            bottom: 0,
+            width: width,
+            left: position,
+            height: '2px',
+            backgroundColor: 'primary.light',
           },
         }}
       >
-        <StyledTab component={NextLink} href={demosHref} label={t('api-docs.demos')} value="" />
-        {headers.components?.length > 0 && (
-          <StyledTab
-            className="skip-algolia-crawler" // For more details, see https://github.com/mui/material-ui/pull/37539.
-            component={NextLink}
-            href={componentsHref}
-            label={t('api-docs.componentsApi')}
-            value="components-api"
-          />
-        )}
-        {headers.hooks && headers.hooks.length > 0 && (
-          <StyledTab
-            className="skip-algolia-crawler" // For more details, see https://github.com/mui/material-ui/pull/37539.
-            component={NextLink}
-            href={hooksHref}
-            label={t('api-docs.hooksApi')}
-            value="hooks-api"
-          />
-        )}
-      </Tabs>
+        {tabData.map((tab) => (
+          <LinkTab
+            key={tab.key}
+            href={tab.href}
+            aria-current={activeTab === tab.key ? 'page' : undefined}
+            sx={{
+              color: activeTab === tab.key ? 'primary.main' : 'inherit',
+            }}
+          >
+            {tab.label}
+          </LinkTab>
+        ))}
+      </Box>
       {children}
     </Box>
   );
