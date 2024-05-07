4ec5797a42bd7d537fd4d46e169973bafb5d33ba
noraleonte <noraleonte00@gmail.com>
2024-05-07 14:32:57 +0300

    fix linting

--- docs/src/components/showcase/FolderTreeView.tsx
+++ docs/src/components/showcase/FolderTreeView.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import * as React from 'react';
 import clsx from 'clsx';
 import { animated, useSpring } from '@react-spring/web';
