0fe989041b6674d45aa58a1b9a3ad63ac3d01a44
Tarun Chauhan <tarunrajput1337@gmail.com>
2024-05-15 20:50:53 +0530

    [material-ui][ToggleButtonGroup] Add missing `selected` class in ToggleButtonGroupClasses type (#42243)
    Co-authored-by: ZeeshanTamboli <zeeshan.tamboli@gmail.com>

--- packages/mui-material/src/ToggleButtonGroup/toggleButtonGroupClasses.ts
+++ packages/mui-material/src/ToggleButtonGroup/toggleButtonGroupClasses.ts
@@ -4,6 +4,10 @@ import generateUtilityClass from '@mui/utils/generateUtilityClass';
 export interface ToggleButtonGroupClasses {
   /** Styles applied to the root element. */
   root: string;
+  /** State class applied to the root element if `selected={true}`. */
+  selected: string;
+  /** Styles applied to the root element if `orientation="horizontal"`. */
+  horizontal: string;
   /** Styles applied to the root element if `orientation="vertical"`. */
   vertical: string;
   /** State class applied to the root element if `disabled={true}`. */
