a7d57bb09850a47e50c1bb5b62967f75490f47d6
alexandre <alex.fauquette@gmail.com>
2024-05-16 11:29:22 +0200

    prevent linking when range is selected

--- packages/mui-docs/src/MarkdownElement/MarkdownElement.tsx
+++ packages/mui-docs/src/MarkdownElement/MarkdownElement.tsx
@@ -136,6 +136,8 @@ const Root = styled('div')(
         position: 'relative',
         display: 'flex',
         alignItems: 'center',
+        userSelect: 'text',
+        WebkitUserDrag: 'none',
       },
       '& .anchor-icon': {
         // To prevent the link to get the focus.
@@ -802,6 +804,18 @@ const Root = styled('div')(
   }),
 );
 
+function handleClick(event: Event) {
+  const selection = document.getSelection();
+
+  if (selection === null) {
+    return;
+  }
+  const isRangeSelection = selection.type === 'Range';
+
+  if (isRangeSelection) {
+    event.preventDefault();
+  }
+}
 export interface MarkdownElementProps {
   className?: string;
   renderedMarkdown?: string;
@@ -810,6 +824,21 @@ export interface MarkdownElementProps {
 export const MarkdownElement = React.forwardRef<HTMLDivElement, MarkdownElementProps>(
   function MarkdownElement(props, ref) {
     const { className, renderedMarkdown, ...other } = props;
+
+    React.useEffect(() => {
+      const elements = document.getElementsByClassName('title-link-to-anchor');
+
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('click', handleClick, false);
+      }
+
+      return () => {
+        for (var i = 0; i < elements.length; i++) {
+          elements[i].removeEventListener('click', handleClick);
+        }
+      };
+    }, []);
+
     const more: React.ComponentProps<typeof Root> = {};
 
     if (typeof renderedMarkdown === 'string') {
