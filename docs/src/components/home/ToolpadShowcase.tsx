8215cf3985ae25d240f8f2218a9df53e0e82849a
Bharat Kashyap <bharatkashyap@outlook.com>
2024-05-16 18:04:42 +0900

    feat: GIF with motion blur and zoom

--- docs/src/components/home/ToolpadShowcase.tsx
+++ docs/src/components/home/ToolpadShowcase.tsx
@@ -2,30 +2,7 @@ import * as React from 'react';
 import { alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import ShowcaseContainer, { ShowcaseCodeWrapper } from 'docs/src/components/home/ShowcaseContainer';
-import { HighlightedCode } from '@mui/docs/HighlightedCode';
-import MoreInfoBox from 'docs/src/components/action/MoreInfoBox';
-import ROUTES from 'docs/src/route';
-
-const code = `import { PrismaClient, Prisma } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export async function getUsers() {
-  return prisma.user.findMany();
-}
-
-export async function addUser(user: Prisma.UserCreateInput) {
-  return prisma.user.create({ data: user });
-}
-
-export async function updateUser(id: number) {
-  return prisma.user.update({ where: { id } });
-}
-
-export async function deleteUser(id: number) {
-  return prisma.customer.user({ where: { id } });
-}`;
+import ShowcaseContainer from 'docs/src/components/home/ShowcaseContainer';
 
 export default function DataTable() {
   return (
@@ -47,30 +24,17 @@ export default function DataTable() {
             }),
           })}
         >
-          <Box sx={{ height: 300 }}>
+          <Box sx={{ height: 600 }}>
             <Box
               component="img"
-              src="/static/branding/toolpad/hero-1.png"
+              src="/static/branding/toolpad/landing-page.gif"
               alt="Toolpad user management app"
               loading="lazy"
-              height={411}
+              height={600}
               sx={{ width: { xs: 'auto', sm: '100%' } }}
             />
           </Box>
         </Paper>
-      }
-      code={
-        <React.Fragment>
-          <ShowcaseCodeWrapper maxHeight={260}>
-            <HighlightedCode copyButtonHidden code={code} language="jsx" plainStyle />
-          </ShowcaseCodeWrapper>
-          <MoreInfoBox
-            primaryBtnLabel="Start with Toolpad"
-            primaryBtnHref={ROUTES.toolpadStudioDocs}
-            secondaryBtnLabel="Learn more about why Toolpad exists"
-            secondaryBtnHref={ROUTES.toolpadWhy}
-          />
-        </React.Fragment>
       }
     />
   );
