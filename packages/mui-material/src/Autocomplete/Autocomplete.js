8046fdd75faef39118db49c96326904c07e9b4dc
Diego Andai <diego@mui.com>
2024-05-23 15:52:39 -0400

    [core] Fix React 18.3 warnings about spreading keys in the Material UI `Autocomplete` component (#42099) (#42241)

--- packages/mui-material/src/Autocomplete/Autocomplete.js
+++ packages/mui-material/src/Autocomplete/Autocomplete.js
@@ -552,14 +552,18 @@ const Autocomplete = React.forwardRef(function Autocomplete(inProps, ref) {
     if (renderTags) {
       startAdornment = renderTags(value, getCustomizedTagProps, ownerState);
     } else {
-      startAdornment = value.map((option, index) => (
-        <Chip
-          label={getOptionLabel(option)}
-          size={size}
-          {...getCustomizedTagProps({ index })}
-          {...ChipProps}
-        />
-      ));
+      startAdornment = value.map((option, index) => {
+        const { key, ...customTagProps } = getCustomizedTagProps({ index });
+        return (
+          <Chip
+            key={key}
+            label={getOptionLabel(option)}
+            size={size}
+            {...customTagProps}
+            {...ChipProps}
+          />
+        );
+      });
     }
   }
 
