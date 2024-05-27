dd6046b5a4845a75e94805cafaf8b02c816cf365
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2024-05-27 08:07:45 -0300

    [docs-infra] Standardize API pages Alert styles (#42386)

--- docs/src/modules/components/ApiPage.js
+++ docs/src/modules/components/ApiPage.js
@@ -3,17 +3,16 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import { exactProp } from '@mui/utils';
 import Typography from '@mui/material/Typography';
-import AdGuest from 'docs/src/modules/components/AdGuest';
 import Alert from '@mui/material/Alert';
+import AdGuest from 'docs/src/modules/components/AdGuest';
 import VerifiedRoundedIcon from '@mui/icons-material/VerifiedRounded';
-import { alpha } from '@mui/material/styles';
 import { useTranslate, useUserLanguage } from '@mui/docs/i18n';
 import { HighlightedCode } from '@mui/docs/HighlightedCode';
+import { BrandingProvider } from '@mui/docs/branding';
+import { SectionTitle } from '@mui/docs/SectionTitle';
 import MarkdownElement from 'docs/src/modules/components/MarkdownElement';
 import AppLayoutDocs from 'docs/src/modules/components/AppLayoutDocs';
 import Ad from 'docs/src/modules/components/Ad';
-import { BrandingProvider } from '@mui/docs/branding';
-import { SectionTitle } from '@mui/docs/SectionTitle';
 import PropertiesSection, {
   getPropsToC,
 } from 'docs/src/modules/components/ApiPage/sections/PropertiesSection';
@@ -191,46 +190,7 @@ export default function ApiPage(props) {
           )}
         </Typography>
         <Heading hash="demos" />
-        <Alert
-          severity="success"
-          variant="outlined"
-          icon={<VerifiedRoundedIcon sx={{ fontSize: 20 }} />}
-          sx={[
-            (theme) => ({
-              mt: 1.5,
-              pt: 1,
-              px: 2,
-              pb: 0,
-              fontSize: theme.typography.pxToRem(16),
-              backgroundColor: (theme.vars || theme).palette.success[50],
-              borderColor: (theme.vars || theme).palette.success[100],
-              '& .MuiAlert-message': {
-                '& * p': {
-                  color: (theme.vars || theme).palette.text.primary,
-                  mb: 1,
-                },
-                '& * a': {
-                  fontWeight: theme.typography.fontWeightMedium,
-                  color: (theme.vars || theme).palette.success[900],
-                  textDecorationColor: alpha(theme.palette.success[600], 0.3),
-                },
-              },
-              ...theme.applyDarkStyles({
-                backgroundColor: alpha(theme.palette.success[700], 0.12),
-                borderColor: alpha(theme.palette.success[400], 0.1),
-                '& .MuiAlert-message': {
-                  ul: {
-                    pl: 3,
-                  },
-                  '& * a': {
-                    color: (theme.vars || theme).palette.success[100],
-                    textDecorationColor: alpha(theme.palette.success[100], 0.3),
-                  },
-                },
-              }),
-            }),
-          ]}
-        >
+        <Alert severity="success" icon={<VerifiedRoundedIcon fontSize="small" />}>
           <span
             dangerouslySetInnerHTML={{
               __html: `<p>For examples and details on the usage of this React component, visit the component demo pages:</p>
