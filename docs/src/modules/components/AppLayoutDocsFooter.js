b41d190cbd9a8266d1e9f4524229e4500c837b64
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2024-05-30 17:37:18 -0300

    fix lint

--- docs/src/modules/components/AppLayoutDocsFooter.js
+++ docs/src/modules/components/AppLayoutDocsFooter.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-restricted-globals */
-/* eslint-disable material-ui/no-hardcoded-labels */
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import { useTheme } from '@mui/material/styles';
