3cd15329804e8df24a4d36b3f70f975964d96d28
Necati Özmen <necatiozmn@gmail.com>
2024-06-04 13:07:58 +0300

    Update docs/src/modules/components/MaterialShowcase.js
    docs: update wrong naming
    
    Co-authored-by: Aarón García Hervás <aaron.garcia.hervas@gmail.com>
    Signed-off-by: Necati Özmen <necatiozmn@gmail.com>

--- docs/src/modules/components/MaterialShowcase.js
+++ docs/src/modules/components/MaterialShowcase.js
@@ -410,7 +410,7 @@ const appList = [
   {
     title: 'Refine Foods demo',
     description: 'A full-featured Admin panel app',
-    image: 'refine-foods.jpg',
+    image: 'refine-foods.jpeg',
     link: 'https://example.mui.admin.refine.dev/',
     source: 'https://github.com/pankod/refine/tree/next/examples/fineFoods/admin/mui',
     stars: 2415,
