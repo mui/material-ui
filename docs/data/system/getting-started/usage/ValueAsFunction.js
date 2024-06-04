b07fdbbd53de4bd7a9fbc66bb4a2cf4b1ccceb37
alexandre <alex.fauquette@gmail.com>
2024-06-04 15:05:43 +0200

    docs:typescript:formatted

--- docs/data/system/getting-started/usage/ValueAsFunction.js
+++ docs/data/system/getting-started/usage/ValueAsFunction.js
@@ -5,7 +5,11 @@ export default function ValueAsFunction() {
   return (
     <div>
       <Box
-        sx={{ p: 1, border: 1, borderColor: (theme) => theme.palette.primary.main }}
+        sx={{
+          p: 1,
+          border: 1,
+          borderColor: (theme) => theme.palette.primary.main,
+        }}
       >
         Border color with theme value.
       </Box>
