24e66341cef9fc4452b1dc2675055bc09f0246ce
siriwatknp <siriwatkunaporn@gmail.com>
2024-06-05 19:03:41 -0400

    Fix GradientText

--- docs/src/components/typography/GradientText.tsx
+++ docs/src/components/typography/GradientText.tsx
@@ -3,17 +3,24 @@ import { styled } from '@mui/material/styles';
 const GradientText = styled('span')<{
   color?: 'primary' | 'error' | 'success' | 'warning';
 }>(({ theme }) => ({
-  WebkitBackgroundClip: 'text',
-  WebkitTextFillColor: 'transparent',
   variants: [
     ...Object.entries((theme.vars || theme).palette as Record<string, any>)
-      .filter(([, value]) => value && value[400])
+      .filter(([color, value]) => color !== 'primary' && value && value[400])
       .map(([color, value]) => ({
         props: { color },
         style: {
           background: `linear-gradient(90deg, ${value[400]} 5%, ${value.main} 90%)`,
         },
       })),
+    {
+      props: {},
+      style: {
+        background: `linear-gradient(90deg, ${(theme.vars || theme).palette.primary[400]} 5%, ${(theme.vars || theme).palette.primary.main} 90%)`,
+        // `Webkit` has to come later
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent',
+      },
+    },
   ],
 }));
 
