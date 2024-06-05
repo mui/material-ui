c725bef8fd24c9682a4ec2cca024022f6ae18a37
Alfin S <benjaminstwo@gmail.com>
2024-06-05 15:44:53 +0800

    [material-ui][useLocalStorageState] Rename callback function

--- packages/mui-utils/src/useSyncStorageState/useSyncStorageState.ts
+++ packages/mui-utils/src/useSyncStorageState/useSyncStorageState.ts
@@ -35,20 +35,20 @@ function emitCurrentTabStorageChange(key: string) {
   }
 }
 
-function subscribe(area: Storage, key: string | null, callbark: () => void): () => void {
+function subscribe(area: Storage, key: string | null, callback: () => void): () => void {
   if (!key) {
     return () => {};
   }
   const storageHandler = (event: StorageEvent) => {
     if (event.storageArea === area && event.key === key) {
-      callbark();
+      callback();
     }
   };
   window.addEventListener('storage', storageHandler);
-  onCurrentTabStorageChange(key, callbark);
+  onCurrentTabStorageChange(key, callback);
   return () => {
     window.removeEventListener('storage', storageHandler);
-    offCurrentTabStorageChange(key, callbark);
+    offCurrentTabStorageChange(key, callback);
   };
 }
 
@@ -122,7 +122,7 @@ function useSyncStorageStateBrowser(
   const { storage = window.localStorage } = options || {};
   const area = storage!;
   const subscribeKey = React.useCallback(
-    (callbark: () => void) => subscribe(area, key, callbark),
+    (callback: () => void) => subscribe(area, key, callback),
     [area, key],
   );
   const getKeySnapshot = React.useCallback(
