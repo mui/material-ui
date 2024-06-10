a2d86107ee03ec7fb3e665450391f7c2983d5c5d
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2024-06-10 11:13:00 -0300

    second iteration after sleeping on this

--- docs/src/modules/components/MuiProductSelector.tsx
+++ docs/src/modules/components/MuiProductSelector.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-// import { alpha } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import Box, { BoxProps } from '@mui/material/Box';
 // import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
@@ -22,12 +22,21 @@ const iconStyles = (theme) => ({
   color: (theme.vars || theme).palette.primary.main,
 });
 
+const NavLabel = styled(Typography)(({ theme }) => ({
+  padding: theme.spacing(0.5, 1, 1, 1),
+  fontSize: theme.typography.pxToRem(11),
+  fontWeight: theme.typography.fontWeightSemiBold,
+  textTransform: 'uppercase',
+  letterSpacing: '.1rem',
+  color: (theme.vars || theme).palette.text.tertiary,
+}));
+
 interface ProductItemProps {
   active?: boolean;
-  name: string;
+  description?: string;
   href?: string;
   icon?: React.ReactNode;
-  description?: string;
+  name: string;
 }
 
 function ProductItem({ active, href, description, name, icon }: ProductItemProps) {
@@ -36,11 +45,12 @@ function ProductItem({ active, href, description, name, icon }: ProductItemProps
       component={Link}
       href={href}
       sx={{
-        p: 1.5,
+        p: 1,
+        pl: '6px',
         display: 'flex',
-        flexDirection: 'column',
+        // flexDirection: 'column',
         alignItems: 'start',
-        gap: '2px',
+        gap: '8px',
         flexGrow: 1,
         backgroundColor: active ? 'rgba(15, 10, 222, 0.02)' : undefined,
         borderRadius: '8px',
@@ -53,15 +63,28 @@ function ProductItem({ active, href, description, name, icon }: ProductItemProps
         },
       }}
     >
-      <Box sx={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
+      <Box
+        sx={{
+          height: 21,
+          width: 21,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          '& svg': {
+            ml: '0 !important',
+          },
+        }}
+      >
         {icon}
+      </Box>
+      <div>
         <Typography color="text.primary" variant="body2" fontWeight="semiBold">
           {name}
         </Typography>
-      </Box>
-      <Typography color="text.secondary" fontSize=".813rem">
-        {description}
-      </Typography>
+        <Typography color="text.secondary" fontSize=".813rem">
+          {description}
+        </Typography>
+      </div>
     </Box>
   );
 }
@@ -70,14 +93,14 @@ const coreProducts = [
   {
     id: 'material-ui',
     name: 'Material UI',
-    description: 'Ready-to-use foundational React components.',
+    description: 'Ready-to-use foundational components.',
     icon: <SvgMuiLogomark width={14} height={14} />,
     href: ROUTES.materialDocs,
   },
   {
     id: 'joy-ui',
     name: 'Joy UI',
-    description: 'Beautiful foudational React components.',
+    description: 'Beautiful foudational components.',
     icon: <SvgMuiLogomark width={14} height={14} />,
     href: ROUTES.joyDocs,
   },
@@ -91,7 +114,7 @@ const coreProducts = [
   {
     id: 'system',
     name: 'MUI System',
-    description: 'Ready-to-use foundational React components.',
+    description: 'Ready-to-use foundational components.',
     icon: <SvgBaseUiLogo width={14} height={14} />,
     href: ROUTES.systemDocs,
   },
@@ -101,24 +124,28 @@ const advancedProducts = [
   {
     id: 'x-data-grid',
     name: 'Data Grid',
+    description: 'Bla bla bla bla description here.',
     icon: <BackupTableRoundedIcon sx={iconStyles} />,
     href: ROUTES.dataGridOverview,
   },
   {
     id: 'x-date-pickers',
     name: 'Date and Time Pickers',
+    description: 'Bla bla bla bla description here.',
     icon: <CalendarMonthRoundedIcon sx={iconStyles} />,
     href: ROUTES.datePickersOverview,
   },
   {
     id: 'x-charts',
     name: 'Charts',
+    description: 'Bla bla bla bla description here.',
     icon: <BarChartRoundedIcon sx={iconStyles} />,
     href: ROUTES.chartsOverview,
   },
   {
     id: 'x-tree-view',
     name: 'Tree View',
+    description: 'Bla bla bla bla description here.',
     icon: <AccountTreeRoundedIcon sx={iconStyles} />,
     href: ROUTES.treeViewOverview,
   },
@@ -134,7 +161,8 @@ export default function MuiProductSelector() {
         role="none"
         sx={{ p: 1, borderBottom: '1px solid', borderColor: 'divider' }}
       >
-        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, minmax(0, 1fr))', gap: '8px' }}>
+        {/* <NavLabel>Libraries</NavLabel> */}
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, minmax(0, 1fr))', gap: '4px' }}>
           {coreProducts.map((product) => (
             <ProductItem
               key={product.name}
@@ -152,16 +180,13 @@ export default function MuiProductSelector() {
         role="none"
         sx={{ p: 1, borderBottom: '1px solid', borderColor: 'divider' }}
       >
-        <Box
-          sx={{
-            display: 'flex',
-            gap: '8px',
-          }}
-        >
+        <NavLabel>MUI X Components</NavLabel>
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, minmax(0, 1fr))', gap: '4px' }}>
           {advancedProducts.map((product) => (
             <ProductItem
               key={product.name}
               name={product.name}
+              description={product.description}
               icon={product.icon}
               href={product.href}
               active={pageContext.productId === product.id}
@@ -174,11 +199,12 @@ export default function MuiProductSelector() {
         role="none"
         sx={{ p: 1, borderBottom: '1px solid', borderColor: 'divider' }}
       >
+        {/* <NavLabel>What do I put here?</NavLabel> */}
         <ProductItem
           name="Toolpad"
           href={ROUTES.toolpadStudioDocs}
-          // icon={product.icon}
-          description="A self-hosted, low-code internal tool builder"
+          icon={<SvgMuiLogomark width={14} height={14} />}
+          description="A self-hosted, low-code internal tool builder."
           // active={pageContext.productId === product.id}
         />
       </Box>
