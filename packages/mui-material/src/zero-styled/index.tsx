d569d3f39a580000ce6cc612d7422f0078f17c5b
Siriwat K <siriwatkunaporn@gmail.com>
2024-06-11 17:38:36 +0700

    [material-ui][InputBase] Use `globalCss` for Pigment integration (#42431)

--- packages/mui-material/src/zero-styled/index.tsx
+++ packages/mui-material/src/zero-styled/index.tsx
@@ -1,9 +1,17 @@
+import * as React from 'react';
 import { extendSxProp } from '@mui/system/styleFunctionSx';
 import useThemeProps from '../styles/useThemeProps';
+import GlobalStyles, { GlobalStylesProps } from '../GlobalStyles';
 
 export { css, keyframes } from '@mui/system';
 
 export { default as styled } from '../styles/styled';
+
+export function globalCss(styles: GlobalStylesProps['styles']) {
+  return function GlobalStylesWrapper() {
+    return <GlobalStyles styles={styles} />;
+  };
+}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function createUseThemeProps(name: string) {
