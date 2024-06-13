c7ac34aa90c09a53955f8fcb34549dfd3d4de9f3
sai6855 <fayazceg@gmail.com>
2024-06-13 13:38:50 +0530

    review: aaron

--- packages-internal/test-utils/src/mochaHooks.test.js
+++ packages-internal/test-utils/src/mochaHooks.test.js
@@ -69,7 +69,7 @@ describe('mochaHooks', () => {
         let unsafeSetState;
         function Parent() {
           const [state, setState] = React.useState(0);
-          // TODO: uncomment once we enable eslint-plugin-react-compiler eslint-disable-next-line react-compiler/react-compiler -- unsafeSetState is required outside of component
+          // TODO: uncomment once we enable eslint-plugin-react-compiler // eslint-disable-next-line react-compiler/react-compiler -- unsafeSetState is required outside the component
           unsafeSetState = setState;
 
           React.useEffect(() => {});
