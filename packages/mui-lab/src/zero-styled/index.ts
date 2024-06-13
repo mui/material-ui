4b89434812069c787c03a862c3132398978c6e34
siriwatknp <siriwatkunaporn@gmail.com>
2024-06-13 23:46:08 +0700

    remove createUseThemeProps

--- packages/mui-lab/src/zero-styled/index.ts
+++ packages/mui-lab/src/zero-styled/index.ts
@@ -1,8 +1,3 @@
 import { useThemeProps } from '@mui/material/styles';
 
 export { styled } from '@mui/material/styles';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function createUseThemeProps(name: string) {
-  return useThemeProps;
-}
