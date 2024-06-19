6baa01c5ff7bab7269d7a4d43cc38afae1aee015
Aarón García Hervás <aaron@mui.com>
2024-06-19 15:12:07 +0200

    Remove unused mount param from describeConformanceUnstyled calls

--- packages/mui-base/src/Modal/Modal.test.tsx
+++ packages/mui-base/src/Modal/Modal.test.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import { expect } from 'chai';
-import { createMount, createRenderer } from '@mui/internal-test-utils';
+import { createRenderer } from '@mui/internal-test-utils';
 import { Modal, modalClasses as classes, ModalRootSlotProps } from '@mui/base/Modal';
 import { describeConformanceUnstyled } from '../../test/describeConformanceUnstyled';
 
 describe('<Modal />', () => {
-  const mount = createMount();
   const { render } = createRenderer();
   let savedBodyStyle: CSSStyleDeclaration;
 
@@ -24,7 +23,6 @@ describe('<Modal />', () => {
     () => ({
       inheritComponent: 'div',
       render,
-      mount,
       refInstanceof: window.HTMLDivElement,
       slots: {
         root: {
