61ca07d4865219c4303f3c67233ff1c76032cea1
siriwatknp <siriwatkunaporn@gmail.com>
2024-06-19 11:24:35 +0700

    update types

--- packages/mui-material/src/zero-styled/index.tsx
+++ packages/mui-material/src/zero-styled/index.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
+import { Interpolation } from '@mui/system';
 import { extendSxProp } from '@mui/system/styleFunctionSx';
+import { Theme } from '../styles/createTheme';
 import useThemeProps from '../styles/useThemeProps';
 import useTheme from '../styles/useTheme';
-import GlobalStyles from '../GlobalStyles';
+import GlobalStyles, { GlobalStylesProps } from '../GlobalStyles';
 
 export { css, keyframes } from '@mui/system';
 
 export { default as styled } from '../styles/styled';
 
-export function globalCss(styles: any) {
+export function globalCss(styles: Interpolation<{ theme: Theme }>) {
   return function GlobalStylesWrapper() {
     return (
       // Pigment CSS `globalCss` support callback with theme inside an object but `GlobalStyles` support theme as a callback value.
-      <GlobalStyles styles={typeof styles === 'function' ? (theme) => styles({ theme }) : styles} />
+      <GlobalStyles
+        styles={
+          (typeof styles === 'function'
+            ? (theme) => styles({ theme })
+            : styles) as GlobalStylesProps['styles']
+        }
+      />
     );
   };
 }
