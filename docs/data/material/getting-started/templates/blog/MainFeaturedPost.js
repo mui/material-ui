90e65afd94ca3e8de10fb11e6f8adf29e6e3b863
Arron Hunt <arronjhunt@gmail.com>
2024-06-20 12:19:46 -0700

    change system props to sx

--- docs/data/material/getting-started/templates/blog/MainFeaturedPost.js
+++ docs/data/material/getting-started/templates/blog/MainFeaturedPost.js
@@ -49,7 +49,7 @@ function MainFeaturedPost(props) {
             <Typography component="h1" variant="h3" color="inherit" gutterBottom>
               {post.title}
             </Typography>
-            <Typography variant="h5" color="inherit" mb={2}>
+            <Typography variant="h5" color="inherit" sx={{ marginBottom: '16px' }}>
               {post.description}
             </Typography>
             <Link variant="subtitle1" href="#">
