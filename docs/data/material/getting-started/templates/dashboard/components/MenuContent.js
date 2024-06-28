522fdf18aa40cd595a7f3b6920d8ca9645862c66
zanivan <victorzanivan@gmail.com>
2024-06-28 14:04:02 -0300

    Fix tests

--- docs/data/material/getting-started/templates/dashboard/components/MenuContent.js
+++ docs/data/material/getting-started/templates/dashboard/components/MenuContent.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import List from '@mui/material/List';
-
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
