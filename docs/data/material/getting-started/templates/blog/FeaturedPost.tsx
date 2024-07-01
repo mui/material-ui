e0da5af275812f015ea610bf081bd780a01b8b78
Aarón García Hervás <aaron@mui.com>
2024-07-01 13:46:56 +0200

    Unitless margins

--- docs/data/material/getting-started/templates/blog/FeaturedPost.tsx
+++ docs/data/material/getting-started/templates/blog/FeaturedPost.tsx
@@ -35,7 +35,7 @@ export default function FeaturedPost(props: FeaturedPostProps) {
             >
               {post.date}
             </Typography>
-            <Typography variant="subtitle1" sx={{ marginBottom: '16px' }}>
+            <Typography variant="subtitle1" sx={{ marginBottom: 16 }}>
               {post.description}
             </Typography>
             <Typography variant="subtitle1" color="primary">
