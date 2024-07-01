4b200d178fca7e9af7e9b3b4f03f804a30577e78
Aarón García Hervás <aaron@mui.com>
2024-07-01 14:02:47 +0200

    Remove sx prop from integration test

--- test/integration/material-ui/components.spec.tsx
+++ test/integration/material-ui/components.spec.tsx
@@ -263,14 +263,12 @@ function CardMediaTest() {
       </CardActions>
       <Collapse in timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography sx={{ marginBottom: '16px' }} variant="body2">
-            Method:
-          </Typography>
-          <Typography sx={{ marginBottom: '16px' }}>
+          <Typography variant="body2">Method:</Typography>
+          <Typography>
             Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
             minutes.
           </Typography>
-          <Typography sx={{ marginBottom: '16px' }}>
+          <Typography>
             Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
             heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
             browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
@@ -278,7 +276,7 @@ function CardMediaTest() {
             pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
             saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
           </Typography>
-          <Typography sx={{ marginBottom: '16px' }}>
+          <Typography>
             Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
             without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
             medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
