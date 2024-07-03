1d5f95d9f53e5618cb434c54ee1cd2440f551630
sai6855 <fayazceg@gmail.com>
2024-07-03 17:15:08 +0530

    handle chidlren edge cases

--- packages/mui-material/src/utils/getChildRef.d.ts
+++ packages/mui-material/src/utils/getChildRef.d.ts
@@ -1,1 +1,1 @@
-export default function getChildRef(children: React.ReactNode): React.Ref<any>;
+export default function getChildRef(children: React.ReactNode): React.Ref<any> | null;
--- packages/mui-material/src/utils/getChildRef.js
+++ packages/mui-material/src/utils/getChildRef.js
@@ -1,4 +1,9 @@
+import * as React from 'react';
+
 export default function getChildRef(children) {
+  if (!children || !React.isValidElement(children) || React.Children.count(children) !== 1) {
+    return null;
+  }
   // 'ref' is passed as prop in React 19, whereas 'ref' is directly attached to children in React 18
   // below check is to ensure 'ref' is accessible in both cases
   return Object.keys(children.props).includes('ref') ? children.props.ref : children.ref;
