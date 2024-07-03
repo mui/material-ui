a8e91905465f521c6e5a759bb9aaddfc6445bbc9
sai6855 <fayazceg@gmail.com>
2024-07-03 17:03:31 +0530

    Revert "show diego react error"
    This reverts commit e7811580ba0f783be7164666243ec4f945d54505.

--- packages/mui-material/src/utils/getChildRef.js
+++ packages/mui-material/src/utils/getChildRef.js
@@ -1,6 +1,5 @@
 export default function getChildRef(children) {
   // 'ref' is passed as prop in React 19, whereas 'ref' is directly attached to children in React 18
   // below check is to ensure 'ref' is accessible in both cases
-  // return Object.keys(children.props).includes('ref') ? children.props.ref : children.ref;
-  return children.props.ref ?? children.ref;
+  return Object.keys(children.props).includes('ref') ? children.props.ref : children.ref;
 }
