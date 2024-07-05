28ce82da7a65d6161076653aaa0f2f089469f803
Matt <github@nospam.33m.co>
2024-07-05 19:25:10 +0100

    [docs][LinearBuffer] Fix demo

--- docs/data/material/components/progress/LinearBuffer.tsx
+++ docs/data/material/components/progress/LinearBuffer.tsx
@@ -6,25 +6,42 @@ export default function LinearBuffer() {
   const [progress, setProgress] = React.useState(0);
   const [buffer, setBuffer] = React.useState(10);
 
-  const progressRef = React.useRef(() => {});
+  const bufferRef = React.useRef(() => {});  
   React.useEffect(() => {
-    progressRef.current = () => {
-      if (progress > 100) {
-        setProgress(0);
-        setBuffer(10);
-      } else {
-        const diff = Math.random() * 10;
-        const diff2 = Math.random() * 10;
-        setProgress(progress + diff);
-        setBuffer(progress + diff + diff2);
+    bufferRef.current = () => {
+      if (buffer < 100) {
+          const newBuffer = buffer + 1 + Math.random() * 10;
+          setBuffer(newBuffer > 100 ? 100 : newBuffer);
       }
     };
   });
+  
+  const progressRef = React.useRef(() => {});
+  React.useEffect(() => {
+    progressRef.current = () => {
+      if (progress === 100) {
+        setProgress(0);
+        setBuffer(10);
+      } else {
+        setProgress(progress + 1);
+      }
+    };
+  });
+  
+    React.useEffect(() => {
+    const timer = setInterval(() => {
+      bufferRef.current();
+    }, 500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
   React.useEffect(() => {
     const timer = setInterval(() => {
       progressRef.current();
-    }, 500);
+    }, 100);
 
     return () => {
       clearInterval(timer);
