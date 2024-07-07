916818e4fd47e7fd19924395a3ee6053d3d64402
sai6855 <fayazceg@gmail.com>
2024-07-07 19:02:34 +0530

    remove checking child count

--- packages/mui-material/src/utils/getChildRef.js
+++ packages/mui-material/src/utils/getChildRef.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 export default function getChildRef(child) {
-  if (!child || !React.isValidElement(child) || React.Child.count(child) !== 1) {
+  if (!child || !React.isValidElement(child)) {
     return null;
   }
   // 'ref' is passed as prop in React 19, whereas 'ref' is directly attached to child in React 18
