95a319bf6721d652bf7b9e07bf3563e81eca2a8b
sai6855 <fayazceg@gmail.com>
2024-07-07 19:10:49 +0530

    fix comment

--- packages/mui-material/src/utils/getChildRef.js
+++ packages/mui-material/src/utils/getChildRef.js
@@ -4,7 +4,7 @@ export default function getChildRef(child) {
   if (!child || !React.isValidElement(child)) {
     return null;
   }
-  // 'ref' is passed as prop in React 19, whereas 'ref' is directly attached to child in React 18
+  // 'ref' is passed as prop in React 19, whereas 'ref' is directly attached to children in React 18
   // below check is to ensure 'ref' is accessible in both cases
   return child.props.propertyIsEnumerable('ref') ? child.props.ref : child.ref;
 }
