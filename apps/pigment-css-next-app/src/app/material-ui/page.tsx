6ac9e5edc9e77d2271b7e4ab9899ade16c21a155
Brijesh Bittu <brijeshb42@gmail.com>
2024-07-09 12:23:09 +0530

    Dedupe

--- apps/pigment-css-next-app/src/app/material-ui/page.tsx
+++ apps/pigment-css-next-app/src/app/material-ui/page.tsx
@@ -6,7 +6,9 @@ import path from 'path';
 import { css } from '@mui/material-pigment-css';
 
 export default async function MaterialUIPage() {
-  const rootPaths = await fs.readdir(path.join(process.cwd(), `src/app/material-ui`));
+  const rootPaths = (await fs.readdir(path.join(process.cwd(), `src/app/material-ui`))).filter(
+    (path) => !path.startsWith('.'),
+  );
   return (
     <div>
       <h1>Material UI Components</h1>
