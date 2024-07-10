0eb2ba11ab860f60c1444b26490f2520dd9c0e2c
Lucas Hilgert <77863078+lhilgert9@users.noreply.github.com>
2024-07-10 17:16:05 +0200

    [material-ui] Revert deprecation warnings for `*Components` and `*Props` for v6 (#42466)

--- packages/mui-codemod/src/deprecations/autocomplete-props/test-cases/expected.js
+++ packages/mui-codemod/src/deprecations/autocomplete-props/test-cases/expected.js
@@ -2,24 +2,16 @@ import Autocomplete from '@mui/material/Autocomplete';
 import {Autocomplete as MyAutocomplete} from '@mui/material';
 
 <Autocomplete
-  slots={{
-    paper: CustomPaper,
-    popper: CustomPopper,
-    listbox: CustomListbox
-  }}
   slotProps={{
-    listbox: { height: 12 },
-    chip: { height: 10 },
     clearIndicator: { width: 10 },
     paper: { width: 12 },
     popper: { width: 14 },
-    popupIndicator: { width: 16 }
-  }} />;
+    popupIndicator: { width: 16 },
+  }}
+/>;
 
 <Autocomplete
   slotProps={{
-    listbox: { height: 12 },
-    chip: { height: 10 },
     clearIndicator: { width: 10 },
     paper: { width: 12 },
     popper: { width: 14 },
@@ -28,32 +20,22 @@ import {Autocomplete as MyAutocomplete} from '@mui/material';
       ...{ width: 16 },
       ...{ width: 20 }
     }
-  }}
-  slots={{
-    paper: CustomPaper,
-    popper: CustomPopper,
-    listbox: CustomListbox
   }} />;
 
 <MyAutocomplete
-  slots={{
-    paper: CustomPaper,
-    popper: CustomPopper,
-    listbox: CustomListbox
-  }}
   slotProps={{
-    listbox: { height: 12 },
-    chip: { height: 10 },
     clearIndicator: { width: 10 },
     paper: { width: 12 },
     popper: { width: 14 },
-    popupIndicator: { width: 16 }
-  }} />;
+    popupIndicator: { width: 16 },
+  }}
+/>;
 
 <CustomAutocomplete
-  ChipProps={{ height: 10 }}
-  PaperComponent={CustomPaper}
-  PopperComponent={CustomPopper}
-  ListboxComponent={CustomListbox}
-  ListboxProps={{ height: 12 }}
+  componentsProps={{
+    clearIndicator: { width: 10 },
+    paper: { width: 12 },
+    popper: { width: 14 },
+    popupIndicator: { width: 16 },
+  }}
 />
