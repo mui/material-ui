09efcda8b758388ed1f67c0a5b247b45cfc162ef
mnajdova <mnajdova@gmail.com>
2024-07-11 11:20:43 +0200

    move Popper, FocusTrap

--- packages/mui-material/src/Unstable_TrapFocus/index.d.ts
+++ packages/mui-material/src/Unstable_TrapFocus/index.d.ts
@@ -1,2 +1,2 @@
-export { FocusTrap as default } from '@mui/base/FocusTrap';
-export { FocusTrapProps as TrapFocusProps } from '@mui/base/FocusTrap';
+export { FocusTrap as default } from './FocusTrap';
+export { FocusTrapProps as TrapFocusProps } from './FocusTrap.types';
