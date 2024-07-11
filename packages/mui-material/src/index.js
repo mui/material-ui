44b6dc77d62c000a81bf3f6f401497d2d1e66c6f
mnajdova <mnajdova@gmail.com>
2024-07-11 10:21:25 +0200

    move composeClasses

--- packages/mui-material/src/index.js
+++ packages/mui-material/src/index.js
@@ -409,7 +409,7 @@ export { default as useAutocomplete } from './useAutocomplete';
 export { default as GlobalStyles } from './GlobalStyles';
 export * from './GlobalStyles';
 
-export { unstable_composeClasses } from '@mui/base/composeClasses';
+export { default as unstable_composeClasses } from './utils/composeClasses';
 
 export { default as generateUtilityClass } from './generateUtilityClass';
 export * from './generateUtilityClass';
