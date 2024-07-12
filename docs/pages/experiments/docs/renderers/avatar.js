16c5d9111bcddc7d067a7299c681aabfcda29b5b
Armin Mehinovic <armin@mui.com>
2024-07-12 10:09:46 +0200

    Replace 2 tabs demo with multitabs demo for the grid

---
+++ docs/pages/experiments/docs/renderers/avatar.js
@@ -0,0 +1,13 @@
+import Avatar from '@mui/material/Avatar';
+
+export function renderAvatar(params) {
+  if (params.value == null) {
+    return '';
+  }
+
+  return (
+    <Avatar style={{ backgroundColor: params.value.color }}>
+      {params.value.name.toUpperCase().substring(0, 1)}
+    </Avatar>
+  );
+}
---
+++ docs/pages/experiments/docs/renderers/sparkline.js
@@ -0,0 +1,9 @@
+import { SparkLineChart } from '@mui/x-charts/SparkLineChart';
+
+export function renderSparkline(params) {
+  if (params.value == null) {
+    return '';
+  }
+
+  return <SparkLineChart data={params.value} width={params.colDef.computedWidth} plotType="bar" />;
+}
