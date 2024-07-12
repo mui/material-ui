b42fe78ed4b3eceb078acb634205f38d1dbd7375
Aarón García Hervás <aaron@mui.com>
2024-07-12 16:36:54 +0200

    Fix Divider props codemod

--- packages/mui-codemod/src/deprecations/divider-props/divider-props.js
+++ packages/mui-codemod/src/deprecations/divider-props/divider-props.js
@@ -12,43 +12,52 @@ export default function transformer(file, api, options) {
   const printOptions = options.printOptions;
 
   findComponentJSX(j, { root, componentName: 'Divider' }, (elementPath) => {
-    const hasLightProp =
-      elementPath.node.openingElement.attributes.findIndex(
-        (attr) => attr.type === 'JSXAttribute' && attr.name.name === 'light',
-      ) !== -1;
+    const lightProp = elementPath.node.openingElement.attributes.find(
+      (attr) => attr.type === 'JSXAttribute' && attr.name.name === 'light',
+    );
 
-    if (hasLightProp) {
-      elementPath.node.openingElement.attributes =
-        elementPath.node.openingElement.attributes.filter((attr) => {
-          if (attr.type === 'JSXAttribute' && attr.name.name === 'light') {
-            return false;
-          }
-          return true;
-        });
+    if (!lightProp) {
+      return;
+    }
 
-      const sxIndex = elementPath.node.openingElement.attributes.findIndex(
-        (attr) => attr.type === 'JSXAttribute' && attr.name.name === 'sx',
-      );
-      if (sxIndex === -1) {
-        appendAttribute(j, {
-          target: elementPath.node,
-          attributeName: 'sx',
-          expression: j.objectExpression([
-            j.objectProperty(j.identifier('opacity'), j.literal('0.6')),
-          ]),
-        });
-      } else {
-        const opacityIndex = elementPath.node.openingElement.attributes[
-          sxIndex
-        ].value.expression.properties.findIndex((key) => key.key.name === 'opacity');
-
-        if (opacityIndex === -1) {
-          assignObject(j, {
-            target: elementPath.node.openingElement.attributes[sxIndex],
-            key: 'opacity',
-            expression: j.literal('0.6'),
-          });
+    elementPath.node.openingElement.attributes = elementPath.node.openingElement.attributes.filter(
+      (attr) => {
+        if (attr.type === 'JSXAttribute' && attr.name.name === 'light') {
+          return false;
         }
+        return true;
+      },
+    );
+
+    const isLightPropTruthy =
+      !!lightProp.value?.expression.value || lightProp.value?.expression.value === undefined;
+
+    if (!isLightPropTruthy) {
+      return;
+    }
+
+    const sxIndex = elementPath.node.openingElement.attributes.findIndex(
+      (attr) => attr.type === 'JSXAttribute' && attr.name.name === 'sx',
+    );
+    if (sxIndex === -1) {
+      appendAttribute(j, {
+        target: elementPath.node,
+        attributeName: 'sx',
+        expression: j.objectExpression([
+          j.objectProperty(j.identifier('opacity'), j.literal('0.6')),
+        ]),
+      });
+    } else {
+      const opacityIndex = elementPath.node.openingElement.attributes[
+        sxIndex
+      ].value.expression.properties.findIndex((key) => key.key.name === 'opacity');
+
+      if (opacityIndex === -1) {
+        assignObject(j, {
+          target: elementPath.node.openingElement.attributes[sxIndex],
+          key: 'opacity',
+          expression: j.literal('0.6'),
+        });
       }
     }
   });
@@ -58,35 +67,40 @@ export default function transformer(file, api, options) {
       (key) => key.key.name === 'defaultProps',
     );
 
-    const hasLightProp =
-      defaultPropsObject.value.properties.findIndex((prop) => prop.key.name === 'light') !== -1;
+    const lightProp = defaultPropsObject.value.properties.find((prop) => prop.key.name === 'light');
 
-    if (hasLightProp) {
-      defaultPropsObject.value.properties = defaultPropsObject.value.properties.filter(
-        (prop) => !['light'].includes(prop?.key?.name),
+    if (!lightProp) {
+      return;
+    }
+
+    defaultPropsObject.value.properties = defaultPropsObject.value.properties.filter(
+      (prop) => !['light'].includes(prop?.key?.name),
+    );
+
+    const isLightPropTruthy = !!lightProp.value?.value || lightProp.value?.value === undefined;
+
+    if (!isLightPropTruthy) {
+      return;
+    }
+
+    const sxIndex = defaultPropsObject.value.properties.findIndex((prop) => prop.key.name === 'sx');
+
+    if (sxIndex === -1) {
+      defaultPropsObject.value.properties.push(
+        j.objectProperty(
+          j.identifier('sx'),
+          j.objectExpression([j.objectProperty(j.identifier('opacity'), j.literal('0.6'))]),
+        ),
+      );
+    } else {
+      const opacityIndex = defaultPropsObject.value.properties[sxIndex].value.properties.findIndex(
+        (key) => key.key.name === 'opacity',
       );
 
-      const sxIndex = defaultPropsObject.value.properties.findIndex(
-        (prop) => prop.key.name === 'sx',
-      );
-
-      if (sxIndex === -1) {
-        defaultPropsObject.value.properties.push(
-          j.objectProperty(
-            j.identifier('sx'),
-            j.objectExpression([j.objectProperty(j.identifier('opacity'), j.literal('0.6'))]),
-          ),
+      if (opacityIndex === -1) {
+        defaultPropsObject.value.properties[sxIndex].value.properties.push(
+          j.objectProperty(j.identifier('opacity'), j.literal('0.6')),
         );
-      } else {
-        const opacityIndex = defaultPropsObject.value.properties[
-          sxIndex
-        ].value.properties.findIndex((key) => key.key.name === 'opacity');
-
-        if (opacityIndex === -1) {
-          defaultPropsObject.value.properties[sxIndex].value.properties.push(
-            j.objectProperty(j.identifier('opacity'), j.literal('0.6')),
-          );
-        }
       }
     }
   });
