f40580d45c40d01140bfb5115fc056ab6452df75
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2024-07-16 16:17:47 -0500

    [material-ui][breadcrumbs] add support to override onClick handler for slot elements.

--- packages/mui-material/src/Breadcrumbs/Breadcrumbs.js
+++ packages/mui-material/src/Breadcrumbs/Breadcrumbs.js
@@ -152,7 +152,7 @@ const Breadcrumbs = React.forwardRef(function Breadcrumbs(inProps, ref) {
         key="ellipsis"
         slots={{ CollapsedIcon: slots.CollapsedIcon }}
         slotProps={{ collapsedIcon: collapsedIconSlotProps }}
-        onClick={handleClickExpand}
+        onClick={collapsedIconSlotProps.onClick || handleClickExpand}
       />,
       ...allItems.slice(allItems.length - itemsAfterCollapse, allItems.length),
     ];
