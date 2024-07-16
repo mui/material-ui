83ab52bf01eea1de52da2572b0309f1ef51be404
sai6855 <fayazceg@gmail.com>
2024-07-16 12:49:41 +0530

    fix

--- packages/mui-codemod/src/deprecations/select-classes/test-cases/actual.js
+++ packages/mui-codemod/src/deprecations/select-classes/test-cases/actual.js
@@ -1,8 +1,8 @@
 import { selectClasses } from '@mui/material/Select';
 
-("& .MuiSelect-filled ~ .MuiSelect-icon");
-("& .MuiSelect-outlined ~ .MuiSelect-icon");
-("& .MuiSelect-standard ~ .MuiSelect-icon");
-`& .${selectClasses.filled} ~ .${selectClasses.icon}`;
-`& .${selectClasses.outlined} ~ .${selectClasses.icon}`;
-`& .${selectClasses.standard} ~ .${selectClasses.icon}`;
+('& .MuiSelect-iconFilled');
+('& .MuiSelect-iconOutlined');
+('& .MuiSelect-iconStandard');
+`& .${selectClasses.iconFilled}`;
+`& .${selectClasses.iconOutlined}`;
+`& .${selectClasses.iconStandard}`;
