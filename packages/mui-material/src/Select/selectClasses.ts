cf7151b76f247bc363f3e6349e3ff42f60888e35
sai6855 <fayazceg@gmail.com>
2024-07-16 12:24:31 +0530

    set up

--- packages/mui-material/src/Select/selectClasses.ts
+++ packages/mui-material/src/Select/selectClasses.ts
@@ -20,11 +20,17 @@ export interface SelectClasses {
   icon: string;
   /** Styles applied to the icon component if the popup is open. */
   iconOpen: string;
-  /** Styles applied to the icon component if `variant="filled"`. */
+  /** Styles applied to the icon component if `variant="filled"`.
+   * @deprecated Combine the [.MuiSelect-icon](/material-ui/api/select/#select-classes-icon) and [.MuiSelect-filled](/material-ui/api/select/#select-classes-filled) classes instead. See [Migrating from deprecated APIs](/material-ui/migration/migrating-from-deprecated-apis/) for more details.
+   */
   iconFilled: string;
-  /** Styles applied to the icon component if `variant="outlined"`. */
+  /** Styles applied to the icon component if `variant="outlined"`.
+   * @deprecated Combine the [.MuiSelect-icon](/material-ui/api/select/#select-classes-icon) and [.MuiSelect-outlined](/material-ui/api/select/#select-classes-outlined) classes instead. See [Migrating from deprecated APIs](/material-ui/migration/migrating-from-deprecated-apis/) for more details.
+   */
   iconOutlined: string;
-  /** Styles applied to the icon component if `variant="standard"`. */
+  /** Styles applied to the icon component if `variant="standard"`.
+   * @deprecated Combine the [.MuiSelect-icon](/material-ui/api/select/#select-classes-icon) and [.MuiSelect-standard](/material-ui/api/select/#select-classes-standard) classes instead. See [Migrating from deprecated APIs](/material-ui/migration/migrating-from-deprecated-apis/) for more details.
+   */
   iconStandard: string;
   /** Styles applied to the underlying native input component. */
   nativeInput: string;
