2a3e5c0542fc1ffd85d63ec411a3f90d48241e6c
Aarón García Hervás <aaron@mui.com>
2024-07-22 13:29:27 +0200

    Stop using deprecated React.MutableRefObject

--- packages/mui-material/src/ButtonBase/ButtonBase.test.js
+++ packages/mui-material/src/ButtonBase/ButtonBase.test.js
@@ -475,7 +475,7 @@ describe('<ButtonBase />', () => {
 
       it('should not crash when changes enableRipple from false to true', async () => {
         function App() {
-          /** @type {React.MutableRefObject<import('./ButtonBase').ButtonBaseActions | null>} */
+          /** @type {React.RefObject<import('./ButtonBase').ButtonBaseActions | null>} */
           const buttonRef = React.useRef(null);
           const [enableRipple, setRipple] = React.useState(false);
 
