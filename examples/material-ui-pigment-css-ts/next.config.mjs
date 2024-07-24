86ae4671f78c1d5d43aced31497187e5908035c9
siriwatknp <siriwatkunaporn@gmail.com>
2024-07-24 17:49:31 +0700

    remove bundle analyzer

--- examples/material-ui-pigment-css-ts/next.config.mjs
+++ examples/material-ui-pigment-css-ts/next.config.mjs
@@ -1,15 +1,14 @@
-import withBundleAnalyzer from "@next/bundle-analyzer";
-import { withPigment } from "@pigment-css/nextjs-plugin";
-import { extendTheme } from "@mui/material";
+import { withPigment } from '@pigment-css/nextjs-plugin';
+import { extendTheme } from '@mui/material';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withPigment(withBundleAnalyzer({ enabled: false })(nextConfig), {
+export default withPigment(nextConfig, {
   theme: extendTheme({
     typography: {
-      fontFamily: "var(--font-family)",
+      fontFamily: 'var(--font-family)',
     },
   }),
-  transformLibraries: ["@mui/material"],
+  transformLibraries: ['@mui/material'],
 });
