86280e94ca90ded8339551224fe8238dd5887777
zanivan <victorzanivan@gmail.com>
2024-07-26 12:58:06 -0300

    Fix shadows

--- docs/data/material/getting-started/templates/blog/theme/themePrimitives.ts
+++ docs/data/material/getting-started/templates/blog/theme/themePrimitives.ts
@@ -1,5 +1,5 @@
 import { PaletteMode } from '@mui/material';
-import { createTheme, alpha } from '@mui/material/styles';
+import { createTheme, alpha, Shadows } from '@mui/material/styles';
 
 declare module '@mui/material/Paper' {
   interface PaperPropsVariantOverrides {
@@ -228,5 +228,5 @@ export const getDesignTokens = (mode: PaletteMode) => ({
     ...(mode === 'dark'
       ? 'hsla(220, 30%, 5%, 0.7) 0px 4px 16px 0px, hsla(220, 25%, 10%, 0.8) 0px 8px 16px -5px'
       : 'hsla(220, 30%, 5%, 0.07) 0px 4px 16px 0px, hsla(220, 25%, 10%, 0.07) 0px 8px 16px -5px'),
-  ],
+  ] as Shadows,
 });
