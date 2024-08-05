946cc5fc1727c05fdb8c381e35086aa6895f495a
zanivan <victorzanivan@gmail.com>
2024-08-05 16:36:31 -0300

    Add missing components to theme

--- docs/data/material/getting-started/templates/landing-page/theme/customizations/feedback.tsx
+++ docs/data/material/getting-started/templates/landing-page/theme/customizations/feedback.tsx
@@ -1,9 +1,37 @@
 import { Components } from '@mui/material';
-import { Theme } from '@mui/material/styles';
-import { gray } from '../themePrimitives';
+import { Theme, alpha } from '@mui/material/styles';
+import { gray, orange } from '../themePrimitives';
 
 /* eslint-disable import/prefer-default-export */
 export const feedbackCustomizations: Components<Theme> = {
+  MuiAlert: {
+    styleOverrides: {
+      root: ({ theme }) => ({
+        borderRadius: 10,
+        backgroundColor: orange[100],
+        color: theme.palette.text.primary,
+        border: `1px solid ${alpha(orange[300], 0.5)}`,
+        '& .MuiAlert-icon': {
+          color: orange[500],
+        },
+        ...theme.applyStyles('dark', {
+          backgroundColor: `${alpha(orange[900], 0.5)}`,
+          border: `1px solid ${alpha(orange[800], 0.5)}`,
+        }),
+      }),
+    },
+  },
+  MuiDialog: {
+    styleOverrides: {
+      root: ({ theme }) => ({
+        '& .MuiDialog-paper': {
+          borderRadius: '10px',
+          border: '1px solid',
+          borderColor: theme.palette.divider,
+        },
+      }),
+    },
+  },
   MuiLinearProgress: {
     styleOverrides: {
       root: ({ theme }) => ({
