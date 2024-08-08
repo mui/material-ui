8015bf5a1970bc0242507d5140a380ade083fb60
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-08 16:09:43 +0700

    move module augmentation to main

--- examples/material-ui-pigment-css-vite-ts/src/main.tsx
+++ examples/material-ui-pigment-css-vite-ts/src/main.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Theme, SxProps } from '@mui/material/styles';
+import {} from '@mui/material/themeCssVarsAugmentation';
 import '@mui/material-pigment-css/styles.css';
 import App from './App.tsx';
+
+declare module '@mui/material-pigment-css' {
+  interface ThemeArgs {
+    theme: Theme;
+  }
+}
+
+declare global {
+  namespace React {
+    interface HTMLAttributes<T> {
+      sx?: SxProps<Theme>;
+    }
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
