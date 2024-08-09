62e572633dd82fefc6319aec5ce58965ca3bb5b9
sai6855 <fayazceg@gmail.com>
2024-08-09 12:46:20 +0530

    move logic

--- packages/mui-utils/src/getReactNodeRef/getReactNodeRef.ts
+++ packages/mui-utils/src/getReactNodeRef/getReactNodeRef.ts
@@ -8,13 +8,9 @@ import * as React from 'react';
  * @returns React.Ref<any> | null
  */
 export default function getReactNodeRef(element: React.ReactNode): React.Ref<any> | null {
-  if (!element || !React.isValidElement(element)) {
-    return null;
-  }
-
   // 'ref' is passed as prop in React 19, whereas 'ref' is directly attached to children in older versions
-  return (element.props as any).propertyIsEnumerable('ref')
-    ? (element.props as any).ref
+  return (element as any)!.props.propertyIsEnumerable('ref')
+    ? (element as any)!.props.ref
     : // @ts-expect-error element.ref is not included in the ReactElement type
       // We cannot check for it, but isValidElement is true at this point
       // https://github.com/DefinitelyTyped/DefinitelyTyped/discussions/70189
