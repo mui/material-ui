ba82d4a010fd21e33fade498533f7bbbbfa120b3
binsmyth <binsmyth@gmail.com>
2024-08-11 13:31:14 +1000

    Update packages/mui-material/src/Tooltip/Tooltip.js
    Co-authored-by: Aarón García Hervás <aaron.garcia.hervas@gmail.com>
    Signed-off-by: binsmyth <binsmyth@gmail.com>

--- packages/mui-material/src/Tooltip/Tooltip.js
+++ packages/mui-material/src/Tooltip/Tooltip.js
@@ -376,8 +376,8 @@ const Tooltip = React.forwardRef(function Tooltip(inProps, ref) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const { current: isControlled } = React.useRef(openProp !== undefined);
 
-    // TODO: uncomment when compiler is enabled // eslint-disable-next-line react-compiler/react-compiler
-    // eslint-disable-next-line react-hooks/rules-of-hooks
+    // TODO: uncomment once we enable eslint-plugin-react-compiler // eslint-disable-next-line react-compiler/react-compiler
+    // eslint-disable-next-line react-hooks/rules-of-hooks -- process.env never changes
     React.useEffect(() => {
       if (
         childNode &&
