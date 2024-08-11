a78f175c8bf5c39e95e3e27de771f7075d69bdc5
binsmyth <binsmyth@gmail.com>
2024-08-11 13:29:17 +1000

    Update packages/mui-material/src/InputBase/InputBase.js
    Co-authored-by: Aarón García Hervás <aaron.garcia.hervas@gmail.com>
    Signed-off-by: binsmyth <binsmyth@gmail.com>

--- packages/mui-material/src/InputBase/InputBase.js
+++ packages/mui-material/src/InputBase/InputBase.js
@@ -339,7 +339,7 @@ const InputBase = React.forwardRef(function InputBase(inProps, ref) {
   const muiFormControl = useFormControl();
 
   if (process.env.NODE_ENV !== 'production') {
-    // TODO: uncomment when compiler enabled // eslint-disable-next-line react-compiler/react-compiler
+    // TODO: uncomment once we enable eslint-plugin-react-compiler // eslint-disable-next-line react-compiler/react-compiler
     // eslint-disable-next-line react-hooks/rules-of-hooks
     React.useEffect(() => {
       if (muiFormControl) {
