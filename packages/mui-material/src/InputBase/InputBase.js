4aa3c76f5bae76a78d637d428e183a3facdb9308
binsmyth <binsmyth@gmail.com>
2024-08-11 13:29:42 +1000

    Update packages/mui-material/src/InputBase/InputBase.js
    Co-authored-by: Aarón García Hervás <aaron.garcia.hervas@gmail.com>
    Signed-off-by: binsmyth <binsmyth@gmail.com>

--- packages/mui-material/src/InputBase/InputBase.js
+++ packages/mui-material/src/InputBase/InputBase.js
@@ -451,7 +451,7 @@ const InputBase = React.forwardRef(function InputBase(inProps, ref) {
   // or auto filled by the browser before the hydration (for SSR).
   React.useEffect(() => {
     checkDirty(inputRef.current);
-    // TODO: uncomment when compiler enabled // eslint-disable-next-line react-compiler/react-compiler
+    // TODO: uncomment once we enable eslint-plugin-react-compiler // eslint-disable-next-line react-compiler/react-compiler
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
