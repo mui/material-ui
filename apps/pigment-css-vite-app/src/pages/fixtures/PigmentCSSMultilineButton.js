ab0667487203853a863fe900b2ae29a8f93104ab
mnajdova <mnajdova@gmail.com>
2024-08-15 09:02:50 +0200

    improve index

--- apps/pigment-css-vite-app/src/pages/fixtures/PigmentCSSMultilineButton.js
+++ apps/pigment-css-vite-app/src/pages/fixtures/PigmentCSSMultilineButton.js
@@ -1,17 +1,14 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import Layout from './layout';
 
 export default function MultilineButton() {
   return (
-    <Layout>
-      <Button variant="contained" style={{ width: 400 }}>
-        {[
-          'Contained buttons are rectangular-shaped buttons.',
-          'They may be used inline.',
-          'They lift and display ink reactions on press.',
-        ].join(' ')}
-      </Button>
-    </Layout>
+    <Button variant="contained" style={{ width: 400 }}>
+      {[
+        'Contained buttons are rectangular-shaped buttons.',
+        'They may be used inline.',
+        'They lift and display ink reactions on press.',
+      ].join(' ')}
+    </Button>
   );
 }
--- apps/pigment-css-vite-app/src/pages/fixtures/index.tsx
+++ apps/pigment-css-vite-app/src/pages/fixtures/index.tsx
@@ -1,6 +1,53 @@
 import * as React from 'react';
-import Layout from './layout';
+import { useLocation, matchRoutes, Link } from 'react-router-dom';
+import routes from '~react-pages';
+import IndexLayout from '../../Layout';
 
-export default function Index() {
-  return <Layout />;
+export default function Layout(props: { children?: React.ReactNode }) {
+  const location = useLocation();
+  const matchedRoute = React.useMemo(
+    () => matchRoutes(routes, location.pathname)?.[0],
+    [location.pathname],
+  );
+
+  const demo = new URLSearchParams(location.search).get('demo');
+  const childRoutes = (matchedRoute?.route.children ?? []).filter(
+    (item) => !!item.path && item.path !== 'index.test' && item.path !== 'layout',
+  );
+
+  return (
+    <IndexLayout>
+      {demo && <div id="root-demo">{childRoutes.find((item) => item.path === demo)?.element}</div>}
+      <div>
+        <h1>Fixtures Material UI + PIgment CSS</h1>
+        <nav id="tests">
+          <ul
+            sx={{
+              margin: 0,
+              marginBlock: '1rem',
+              padding: 0,
+              paddingLeft: '1.5rem',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '0.5rem',
+            }}
+          >
+            {childRoutes.map((item) => (
+              <li key={item.path}>
+                <Link
+                  to={`/fixtures/?demo=${item.path}`}
+                  sx={{
+                    textDecoration: 'underline',
+                    fontSize: '17px',
+                  }}
+                >
+                  {item.path}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </IndexLayout>
+  );
 }
