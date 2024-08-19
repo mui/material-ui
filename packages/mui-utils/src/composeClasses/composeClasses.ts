c862b9241169644a104c2afcc8095cbbb224f9c7
Rom Grk <romgrk.cc@gmail.com>
2024-08-18 20:28:55 -0400

    lint

--- packages/mui-utils/src/composeClasses/composeClasses.ts
+++ packages/mui-utils/src/composeClasses/composeClasses.ts
@@ -1,4 +1,4 @@
-/* eslint no-restricted-syntax: 0, prefer-template: 0, no-unreachable-loop: 0
+/* eslint no-restricted-syntax: 0, prefer-template: 0
    ---
    These rules are preventing the performance optimizations below.
  */
@@ -8,62 +8,41 @@ export default function composeClasses<ClassKey extends string>(
   getUtilityClass: (slot: string) => string,
   classes: Record<string, string> | undefined = undefined,
 ): Record<ClassKey, string> {
-  const output = {} as Record<ClassKey, string>;
+  const output: Record<ClassKey, string> = {} as any;
 
-  /* This is split in 2 loops for performance reasons, as it allows to avoid the `if (classes)`
-   * check in both of those cases. If `classes` is either `{}` or `undefined`, we consider it empty. */
-  if (isEmpty(classes)) {
+  if (classes) {
     for (const slotName in slots) {
-      if (Object.prototype.hasOwnProperty.call(slots, slotName)) {
-        const slotNames = slots[slotName];
+      const slot = slots[slotName];
+      let buffer = '';
 
-        let result = '';
-        for (let i = 0; i < slotNames.length; i += 1) {
-          const slotPart = slotNames[i];
-          if (slotPart) {
-            const utilityClass = getUtilityClass(slotPart);
-            if (utilityClass) {
-              result += utilityClass + ' ';
-            }
+      for (let i = 0; i < slot.length; i++) {
+        const value = slot[i];
+        if (value) {
+          buffer += getUtilityClass(value) + ' ';
+
+          if (classes[value]) {
+            buffer += classes[value] + ' ';
           }
         }
-        output[slotName] = result;
       }
+
+      output[slotName] = buffer;
     }
   } else {
     for (const slotName in slots) {
-      if (Object.prototype.hasOwnProperty.call(slots, slotName)) {
-        const slotNames = slots[slotName];
+      const slot = slots[slotName];
+      let buffer = '';
 
-        let result = '';
-        for (let i = 0; i < slotNames.length; i += 1) {
-          const slotPart = slotNames[i];
-          if (slotPart) {
-            const utilityClass = getUtilityClass(slotPart);
-            if (utilityClass) {
-              result += utilityClass + ' ';
-            }
-            if (classes[slotPart]) {
-              result += classes[slotPart] + ' ';
-            }
-          }
+      for (let i = 0; i < slot.length; i++) {
+        const value = slot[i];
+        if (value) {
+          buffer += getUtilityClass(value) + ' ';
         }
-        output[slotName] = result;
       }
+
+      output[slotName] = buffer;
     }
   }
 
   return output;
-}
-
-function isEmpty(value: object | undefined): value is undefined {
-  if (!value) {
-    return true;
-  }
-  for (const key in value) {
-    if (Object.prototype.hasOwnProperty.call(value, key)) {
-      return false;
-    }
-  }
-  return true;
 }
