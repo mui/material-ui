mui/material-ui

commit c0b30d03df55f9bd38884e01353156ece72fcf0f

[material-ui] Remove remaining IE11 code (#42283)

packages/mui-material/src/InputBase/InputBase.js
@@ -184,12 +184,11 @@ export const InputBaseInput = styled('input', {
     display: 'block',
     // Make the flex item shrink with Firefox
     minWidth: 0,
-    width: '100%', // Fix IE11 width issue
+    flexGrow: 1,
     animationName: 'mui-auto-fill-cancel',
     animationDuration: '10ms',
     '&::-webkit-input-placeholder': placeholder,
     '&::-moz-placeholder': placeholder, // Firefox 19+
-    '&:-ms-input-placeholder': placeholder, // IE11
     '&::-ms-input-placeholder': placeholder, // Edge
     '&:focus': {
       outline: 0,
@@ -206,11 +205,9 @@ export const InputBaseInput = styled('input', {
     [`label[data-shrink=false] + .${inputBaseClasses.formControl} &`]: {
       '&::-webkit-input-placeholder': placeholderHidden,
       '&::-moz-placeholder': placeholderHidden, // Firefox 19+
-      '&:-ms-input-placeholder': placeholderHidden, // IE11
       '&::-ms-input-placeholder': placeholderHidden, // Edge
       '&:focus::-webkit-input-placeholder': placeholderVisible,
       '&:focus::-moz-placeholder': placeholderVisible, // Firefox 19+
-      '&:focus:-ms-input-placeholder': placeholderVisible, // IE11
       '&:focus::-ms-input-placeholder': placeholderVisible, // Edge
     },
     [`&.${inputBaseClasses.disabled}`]: {
@@ -390,13 +387,6 @@ const InputBase = React.forwardRef(function InputBase(inProps, ref) {
   }, [value, checkDirty, isControlled]);
 
   const handleFocus = (event) => {
-    // Fix a bug with IE11 where the focus/blur events are triggered
-    // while the component is disabled.
-    if (fcs.disabled) {
-      event.stopPropagation();
-      return;
-    }
-
     if (onFocus) {
       onFocus(event);
     }

packages/mui-material/src/StepLabel/StepLabel.js
@@ -104,7 +104,6 @@ const StepLabelIconContainer = styled('span', {
   slot: 'IconContainer',
   overridesResolver: (props, styles) => styles.iconContainer,
 })({
-  flexShrink: 0, // Fix IE11 issue
   display: 'flex',
   paddingRight: 8,
   [`&.${stepLabelClasses.alternativeLabel}`]: {

docs/data/material/components/modal/ServerModal.js
@@ -13,11 +13,6 @@ export default function ServerModal() {
         flexGrow: 1,
         minWidth: 300,
         transform: 'translateZ(0)',
-        // The position fixed scoping doesn't work in IE11.
-        // Disable this demo to preserve the others.
-        '@media all and (-ms-high-contrast: none)': {
-          display: 'none',
-        },
       }}
       ref={rootRef}
     >

docs/data/material/components/modal/ServerModal.tsx
@@ -13,11 +13,6 @@ export default function ServerModal() {
         flexGrow: 1,
         minWidth: 300,
         transform: 'translateZ(0)',
-        // The position fixed scoping doesn't work in IE11.
-        // Disable this demo to preserve the others.
-        '@media all and (-ms-high-contrast: none)': {
-          display: 'none',
-        },
       }}
       ref={rootRef}
     >

packages/mui-material/src/ButtonBase/ButtonBase.js
@@ -219,20 +219,9 @@ const ButtonBase = React.forwardRef(function ButtonBase(inProps, ref) {
     return component && component !== 'button' && !(button.tagName === 'A' && button.href);
   };
 
-  /**
-   * IE11 shim for https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/repeat
-   */
-  const keydownRef = React.useRef(false);
   const handleKeyDown = useEventCallback((event) => {
     // Check if key is already down to avoid repeats being counted as multiple activations
-    if (
-      focusRipple &&
-      !keydownRef.current &&
-      focusVisible &&
-      rippleRef.current &&
-      event.key === ' '
-    ) {
-      keydownRef.current = true;
+    if (focusRipple && !event.repeat && focusVisible && rippleRef.current && event.key === ' ') {
       rippleRef.current.stop(event, () => {
         rippleRef.current.start(event);
       });
@@ -270,7 +259,6 @@ const ButtonBase = React.forwardRef(function ButtonBase(inProps, ref) {
       focusVisible &&
       !event.defaultPrevented
     ) {
-      keydownRef.current = false;
       rippleRef.current.stop(event, () => {
         rippleRef.current.pulsate(event);
       });

packages/mui-material/src/ButtonBase/ButtonBase.test.js
@@ -901,9 +901,7 @@ describe('<ButtonBase />', () => {
 
       expect(container.querySelectorAll('.ripple-visible')).to.have.lengthOf(1);
 
-      // technically the second keydown should be fire with repeat: true
-      // but that isn't implemented in IE11 so we shouldn't mock it here either
-      fireEvent.keyDown(button, { key: 'Enter' });
+      fireEvent.keyDown(button, { key: 'Enter', repeat: true });
 
       expect(container.querySelectorAll('.ripple-visible')).to.have.lengthOf(1);
     });

packages/mui-material/src/Dialog/Dialog.js
@@ -117,7 +117,6 @@ const DialogPaper = styled(Paper, {
 })(({ theme }) => ({
   margin: 32,
   position: 'relative',
-  overflowY: 'auto', // Fix IE11 issue, to remove at some point.
   '@media print': {
     overflowY: 'visible',
     boxShadow: 'none',
@@ -140,7 +139,6 @@ const DialogPaper = styled(Paper, {
       style: {
         display: 'inline-block',
         verticalAlign: 'middle',
-        textAlign: 'left', // 'initial' doesn't work on IE11
       },
     },
     {

packages/mui-material/src/FilledInput/FilledInput.js
@@ -89,7 +89,6 @@ const FilledInputRoot = styled(InputBaseRoot, {
           '&::after': {
             left: 0,
             bottom: 0,
-            // Doing the other way around crash on IE11 "''" https://github.com/cssinjs/jss/issues/242
             content: '""',
             position: 'absolute',
             right: 0,
@@ -118,7 +117,6 @@ const FilledInputRoot = styled(InputBaseRoot, {
             }`,
             left: 0,
             bottom: 0,
-            // Doing the other way around crash on IE11 "''" https://github.com/cssinjs/jss/issues/242
             content: '"\\00a0"',
             position: 'absolute',
             right: 0,

commit 58d14a69e8c5d9c69ccd30aa60e18a098bcdb26e

[material-ui] Revert visual regressions from #42283 (#43364)

packages/mui-material/src/InputBase/InputBase.js
@@ -182,7 +182,7 @@ export const InputBaseInput = styled('input', {
     display: 'block',
     // Make the flex item shrink with Firefox
     minWidth: 0,
-    flexGrow: 1,
+    width: '100%',
     '&::-webkit-input-placeholder': placeholder,
     '&::-moz-placeholder': placeholder, // Firefox 19+
     '&::-ms-input-placeholder': placeholder, // Edge

packages/mui-material/src/StepLabel/StepLabel.js
@@ -103,6 +103,7 @@ const StepLabelIconContainer = styled('span', {
   slot: 'IconContainer',
   overridesResolver: (props, styles) => styles.iconContainer,
 })({
+  flexShrink: 0,
   display: 'flex',
   paddingRight: 8,
   [`&.${stepLabelClasses.alternativeLabel}`]: {

test/regressions/fixtures/StepLabel/NoShrinkIcon.js
@@ -1,0 +1,24 @@
+import * as React from 'react';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+
+function TestIcon() {
+  return <div style={{ border: '1px solid red' }}>should not shrink</div>;
+}
+
+const steps = ['Step 1', 'Step 2'];
+
+export default function CustomizedSteppers() {
+  return (
+    <Stepper sx={{ width: 200 }}>
+      {steps.map((label) => (
+        <Step key={label}>
+          <StepLabel StepIconComponent={TestIcon} sx={{ width: '1px' }}>
+            {label}
+          </StepLabel>
+        </Step>
+      ))}
+    </Stepper>
+  );
+}

test/regressions/fixtures/TextField/ConstrainedTextField.js
@@ -1,0 +1,16 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+
+// TextField shouldn't overflow the red bordered container
+export default function ConstrainedTextField() {
+  return (
+    <div
+      style={{
+        width: 100,
+        border: '1px solid red',
+      }}
+    >
+      <TextField label="Outlined" variant="outlined" />
+    </div>
+  );
+}