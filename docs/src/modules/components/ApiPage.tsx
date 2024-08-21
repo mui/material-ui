3e9de7bab79b7fcd262a3ddb6e8ffbc16dbc957f
alex <alex.fauquette@gmail.com>
2024-08-21 10:11:05 +0200

    fixes

--- docs/src/modules/components/ApiPage.tsx
+++ docs/src/modules/components/ApiPage.tsx
@@ -364,18 +364,21 @@ export default function ApiPage(props: ApiPageProps) {
         {pageContent.filename ? (
           <div
             className="MuiCallout-root MuiCallout-info"
-            dangerouslySetInnerHTML={{
-              __html: t('api-docs.seeSourceCode').replace(
-                '{{href}}',
-                `${process.env.SOURCE_CODE_REPO}/blob/v${process.env.LIB_VERSION}/${pageContent.filename}`,
-              ),
-            }}
             style={{
               alignItems: 'baseline',
               gap: '4px',
               marginTop: 0,
             }}
-          />
+          >
+            <p
+              dangerouslySetInnerHTML={{
+                __html: t('api-docs.seeSourceCode').replace(
+                  '{{href}}',
+                  `${process.env.SOURCE_CODE_REPO}/blob/v${process.env.LIB_VERSION}${pageContent.filename}`,
+                ),
+              }}
+            />
+          </div>
         ) : null}
       </MarkdownElement>
       <svg style={{ display: 'none' }} xmlns="http://www.w3.org/2000/svg">
