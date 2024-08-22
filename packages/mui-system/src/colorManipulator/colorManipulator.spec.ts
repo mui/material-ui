cf07f5d8da7863cef1361c018f005bafde93c107
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-22 09:51:16 +0700

    fix path

--- packages/mui-system/src/colorManipulator/colorManipulator.spec.ts
+++ packages/mui-system/src/colorManipulator/colorManipulator.spec.ts
@@ -14,7 +14,7 @@ import {
   alpha,
   lighten,
   darken,
-} from '@mui/system/color/manipulator';
+} from '@mui/system/colorManipulator';
 
 expectType<(color: string) => string, typeof hexToRgb>(hexToRgb);
 
