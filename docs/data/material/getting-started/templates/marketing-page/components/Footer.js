4e82a0b3cce044e63fd80d612fab3c10b0b38603
zanivan <victorzanivan@gmail.com>
2024-08-23 14:40:09 -0300

    Remove blank line

--- docs/data/material/getting-started/templates/marketing-page/components/Footer.js
+++ docs/data/material/getting-started/templates/marketing-page/components/Footer.js
@@ -8,7 +8,6 @@ import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-
 import FacebookIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/X';
