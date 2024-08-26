421aea9d49d6d9e03152f18b6bbae7bb93eaf06a
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-26 17:34:01 +0700

    enable_website_banner on marketing pages

--- docs/src/featureToggle.js
+++ docs/src/featureToggle.js
@@ -1,6 +1,6 @@
 // need to use commonjs export so that @mui/internal-markdown can use
 module.exports = {
-  enable_website_banner: false,
+  enable_website_banner: true,
   enable_toc_banner: true,
   enable_docsnav_banner: true,
   enable_job_banner: false,
