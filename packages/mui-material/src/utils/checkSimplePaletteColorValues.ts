a68c79b92d4e84c60c2c122e89f5dce8aab8571d
DiegoAndai <diego@mui.com>
2024-09-03 11:43:43 -0400

    Refactor checksimplePaletteColorValues util

--- packages/mui-material/src/utils/checkSimplePaletteColorValues.ts
+++ packages/mui-material/src/utils/checkSimplePaletteColorValues.ts
@@ -1,6 +1,6 @@
 import { PaletteColorOptions, SimplePaletteColorOptions } from '../styles/createPalette';
 
-function isSimplePaletteColor(obj: PaletteColorOptions): obj is SimplePaletteColorOptions {
+function hasCorrectMainProperty(obj: PaletteColorOptions): obj is SimplePaletteColorOptions {
   return typeof (obj as SimplePaletteColorOptions).main === 'string';
 }
 
@@ -10,18 +10,18 @@ function isSimplePaletteColor(obj: PaletteColorOptions): obj is SimplePaletteCol
  * Optionally, you can pass additional properties to check.
  *
  * @param obj - The object to check
- * @param optionalValuesToCheck - Array with "light", "dark", and/or "contrastText"
+ * @param additionalPropertiesToCheck - Array containing "light", "dark", and/or "contrastText"
  * @returns boolean
  */
 export default function checkSimplePaletteColorValues(
   obj: PaletteColorOptions,
-  optionalValuesToCheck: (keyof SimplePaletteColorOptions)[] = [],
+  additionalPropertiesToCheck: (keyof Omit<SimplePaletteColorOptions, 'main'>)[] = [],
 ): boolean {
-  if (!isSimplePaletteColor(obj)) {
+  if (!hasCorrectMainProperty(obj)) {
     return false;
   }
 
-  for (const value of optionalValuesToCheck) {
+  for (const value of additionalPropertiesToCheck) {
     if (!obj.hasOwnProperty(value) || typeof obj[value] !== 'string') {
       return false;
     }
