778a77479d038135c0c6a57d0de6e2c7ba0c350b
siriwatknp <siriwatkunaporn@gmail.com>
2024-09-06 16:05:02 +0700

    add a custom sheet

--- packages/mui-styled-engine/src/StyledEngineProvider/StyledEngineProvider.js
+++ packages/mui-styled-engine/src/StyledEngineProvider/StyledEngineProvider.js
@@ -3,12 +3,41 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
+import { StyleSheet } from '@emotion/sheet';
+
+const createEmotionCache = (options) => {
+  const cache = createCache(options);
+
+  /**
+   * This is for client-side apps only.
+   * A custom sheet is required to make the GlobalStyles API work with `prepend: true`.
+   * This is because the [sheet](https://github.com/emotion-js/emotion/blob/main/packages/react/src/global.js#L94-L99) does not consume the options.
+   */
+  class MyStyleSheet extends StyleSheet {
+    constructor(args) {
+      super(args);
+      this.prepend = cache.sheet.prepend;
+    }
+  }
+
+  // Do the same as https://github.com/emotion-js/emotion/blob/main/packages/cache/src/index.js#L238-L245
+  cache.sheet = new MyStyleSheet({
+    key: cache.key,
+    nonce: cache.sheet.nonce,
+    container: cache.sheet.container,
+    speedy: cache.sheet.isSpeedy,
+    prepend: cache.sheet.prepend,
+    insertionPoint: cache.sheet.insertionPoint,
+  });
+
+  return cache;
+};
 
 // prepend: true moves MUI styles to the top of the <head> so they're loaded first.
 // It allows developers to easily override MUI styles with other styling solutions, like CSS modules.
 let cache;
 if (typeof document === 'object') {
-  cache = createCache({ key: 'css', prepend: true });
+  cache = createEmotionCache({ key: 'css', prepend: true });
 }
 
 export default function StyledEngineProvider(props) {
