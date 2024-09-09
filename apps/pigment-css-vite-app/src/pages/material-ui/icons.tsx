36dde637147e0cf70df38b0d66d466b68c33ffce
DiegoAndai <diego@mui.com>
2024-09-09 12:49:37 -0300

    Remove font awesome visual tests

--- apps/pigment-css-vite-app/src/pages/material-ui/icons.tsx
+++ apps/pigment-css-vite-app/src/pages/material-ui/icons.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import MaterialUILayout from '../../Layout';
 import CreateSvgIcon from '../../../../../docs/data/material/components/icons/CreateSvgIcon.tsx';
-import FontAwesomeIcon from '../../../../../docs/data/material/components/icons/FontAwesomeIcon.tsx';
-import FontAwesomeIconSize from '../../../../../docs/data/material/components/icons/FontAwesomeIconSize.tsx';
-import FontAwesomeSvgIconDemo from '../../../../../docs/data/material/components/icons/FontAwesomeSvgIconDemo.tsx';
 import Icons from '../../../../../docs/data/material/components/icons/Icons.tsx';
 import SvgIconChildren from '../../../../../docs/data/material/components/icons/SvgIconChildren.tsx';
 import SvgIconsColor from '../../../../../docs/data/material/components/icons/SvgIconsColor.tsx';
@@ -18,24 +15,6 @@ export default function IconsPage() {
         <h2> Create Svg Icon</h2>
         <div className="demo-container">
           <CreateSvgIcon />
-        </div>
-      </section>
-      <section>
-        <h2> Font Awesome Icon</h2>
-        <div className="demo-container">
-          <FontAwesomeIcon />
-        </div>
-      </section>
-      <section>
-        <h2> Font Awesome Icon Size</h2>
-        <div className="demo-container">
-          <FontAwesomeIconSize />
-        </div>
-      </section>
-      <section>
-        <h2> Font Awesome Svg Icon Demo</h2>
-        <div className="demo-container">
-          <FontAwesomeSvgIconDemo />
         </div>
       </section>
       <section>
