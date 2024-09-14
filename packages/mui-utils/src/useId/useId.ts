3583ab2ab1a607fd42a98d51a7eae31f013c11f7
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-09-14 22:36:44 +0200

    cleanup

--- packages/mui-utils/src/useId/useId.ts
+++ packages/mui-utils/src/useId/useId.ts
@@ -21,6 +21,7 @@ function useGlobalId(idOverride?: string): string | undefined {
 // eslint-disable-next-line no-underscore-dangle, @typescript-eslint/naming-convention -- Workaround for https://github.com/webpack/webpack/issues/14814
 const _React = { ...React };
 const maybeReactUseId: undefined | (() => string) = _React.useId;
+
 /**
  *
  * @example <div id={useId()} />
@@ -28,10 +29,12 @@ const maybeReactUseId: undefined | (() => string) = _React.useId;
  * @returns {string}
  */
 export default function useId(idOverride?: string): string | undefined {
+  // React.useId() is only available from React 17.0.0.
   if (maybeReactUseId !== undefined) {
     const reactId = maybeReactUseId();
     return idOverride ?? reactId;
   }
+
   // TODO: uncomment once we enable eslint-plugin-react-compiler // eslint-disable-next-line react-compiler/react-compiler
   // eslint-disable-next-line react-hooks/rules-of-hooks -- `React.useId` is invariant at runtime.
   return useGlobalId(idOverride);
