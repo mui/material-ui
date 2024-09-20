bcfcb776e62dac26e9b64290b7b00ce9515dcc07
Brian Woolfolk <brian.woolfolk@encora.com>
2024-09-20 12:28:00 -0700

    refactor getScrollbarSize usages

--- packages/mui-material/src/Modal/ModalManager.test.ts
+++ packages/mui-material/src/Modal/ModalManager.test.ts
@@ -135,8 +135,12 @@ describe('ModalManager', () => {
       modalManager.add(modal, container1);
       modalManager.mount(modal, {});
       expect(container1.style.overflow).to.equal('hidden');
-      expect(container1.style.paddingRight).to.equal(`${20 + getScrollbarSize(document)}px`);
-      expect(fixedNode.style.paddingRight).to.equal(`${14 + getScrollbarSize(document)}px`);
+      expect(container1.style.paddingRight).to.equal(
+        `${20 + getScrollbarSize(document.defaultView)}px`,
+      );
+      expect(fixedNode.style.paddingRight).to.equal(
+        `${14 + getScrollbarSize(document.defaultView)}px`,
+      );
       modalManager.remove(modal);
       expect(container1.style.overflow).to.equal('');
       expect(container1.style.paddingRight).to.equal('20px');
@@ -171,8 +175,10 @@ describe('ModalManager', () => {
       modalManager.add(modal, container1);
       modalManager.mount(modal, {});
       expect(container1.style.overflow).to.equal('hidden');
-      expect(container1.style.paddingRight).to.equal(`${20 + getScrollbarSize(document)}px`);
-      expect(fixedNode.style.paddingRight).to.equal(`${getScrollbarSize(document)}px`);
+      expect(container1.style.paddingRight).to.equal(
+        `${20 + getScrollbarSize(document.defaultView)}px`,
+      );
+      expect(fixedNode.style.paddingRight).to.equal(`${getScrollbarSize(document.defaultView)}px`);
       modalManager.remove(modal);
       expect(container1.style.overflow).to.equal('');
       expect(container1.style.paddingRight).to.equal('20px');
