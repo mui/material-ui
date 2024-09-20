0b7fe3fb66c89b70f2bfd8293963c84845ae39a9
Bharat Kashyap <bharatkashyap@outlook.com>
2024-09-20 14:38:27 +0530

    fix: Add better notifs demo

--- docs/data/material/components/snackbars/ToolpadNotifications.js
+++ docs/data/material/components/snackbars/ToolpadNotifications.js
@@ -3,27 +3,51 @@ import {
   useNotifications,
   NotificationsProvider,
 } from '@toolpad/core/useNotifications';
-import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 
-function NotifyButton() {
+function NotifyRadioButton() {
   const notifications = useNotifications();
-  return (
-    <Button
-      onClick={() => {
-        notifications.show('Consider yourself notified!', {
+  const [online, setOnline] = React.useState(true);
+  const prevOnline = React.useRef(online);
+  React.useEffect(() => {
+    if (prevOnline.current === online) {
+      return () => {};
+    }
+    prevOnline.current = online;
+
+    // preview-start
+    const key = online
+      ? notifications.show('You are now online', {
+          severity: 'success',
           autoHideDuration: 3000,
+        })
+      : notifications.show('You are now offline', {
+          severity: 'error',
         });
-      }}
-    >
-      Notify me
-    </Button>
+
+    return () => {
+      notifications.close(key);
+    };
+    // preview-end
+  }, [notifications, online]);
+
+  return (
+    <div>
+      <FormControlLabel
+        control={
+          <Switch checked={online} onChange={() => setOnline((prev) => !prev)} />
+        }
+        label="Online"
+      />
+    </div>
   );
 }
 
 export default function ToolpadNotifications() {
   return (
     <NotificationsProvider>
-      <NotifyButton />
+      <NotifyRadioButton />
     </NotificationsProvider>
   );
 }
