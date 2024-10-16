709e5b5914828509a22f346faf66d802c674a5c8
sai6855 <fayazceg@gmail.com>
2024-10-16 18:11:43 +0530

    fix

--- packages/mui-system/src/Stack/Stack.test.js
+++ packages/mui-system/src/Stack/Stack.test.js
@@ -19,6 +19,19 @@ describe('<Stack />', () => {
 
   const theme = createTheme();
 
+  it.only('should handle 0 as a child', () => {
+    const { container } = render(
+      <Stack divider={<></>}>
+        <div>1</div>
+        {0}
+        <div>2</div>
+        <div>3</div>
+      </Stack>,
+    );
+
+    expect(container.firstChild.childNodes.length).to.equal(4);
+  });
+
   it('should handle breakpoints with a missing key', () => {
     expect(
       style({
