c70dce02c08d765bfba4744bd6acbdeff8e3eaba
mnajdova <mnajdova@gmail.com>
2024-10-25 11:18:35 +0200

    remove ListView demo

--- docs/data/material/components/autocomplete/components/ToolbarButton.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import { ButtonProps } from '@mui/material/Button';
-
-export function ToolbarButton(props: ButtonProps) {
-  const { className, children, ...other } = props;
-  return <IconButton {...other}>{children}</IconButton>;
-}
