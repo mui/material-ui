946eea3b1f29ed31ae674a10d004414cc6ac2089
zanivan <victorzanivan@gmail.com>
2024-10-29 10:00:17 -0300

    Remove wrong links

--- docs/src/modules/components/MaterialFreeTemplatesCollection.js
+++ docs/src/modules/components/MaterialFreeTemplatesCollection.js
@@ -164,14 +164,14 @@ export default function Templates() {
                 }}
               >
                 <Tooltip title="Edit in StackBlitz">
-                  <IconButton component="a" href={layout.source} color="primary" size="small">
+                  <IconButton color="primary" size="small">
                     <SvgIcon viewBox="0 0 19 28">
                       <path d="M8.13378 16.1087H0L14.8696 0L10.8662 11.1522L19 11.1522L4.13043 27.2609L8.13378 16.1087Z" />
                     </SvgIcon>
                   </IconButton>
                 </Tooltip>
                 <Tooltip title="Edit in CodeSandbox">
-                  <IconButton component="a" href={layout.source} color="primary" size="small">
+                  <IconButton color="primary" size="small">
                     <SvgIcon viewBox="0 0 1080 1080">
                       <path d="M755 140.3l0.5-0.3h0.3L512 0 268.3 140h-0.3l0.8 0.4L68.6 256v512L512 1024l443.4-256V256L755 140.3z m-30 506.4v171.2L548 920.1V534.7L883.4 341v215.7l-158.4 90z m-584.4-90.6V340.8L476 534.4v385.7L300 818.5V646.7l-159.4-90.6zM511.7 280l171.1-98.3 166.3 96-336.9 194.5-337-194.6 165.7-95.7L511.7 280z" />
                     </SvgIcon>
