7aec3b348a4afcae1bf04bb91b1aa42944257661
sai6855 <fayazceg@gmail.com>
2024-11-12 12:17:49 +0530

    undo

--- packages/mui-utils/src/createChainedFunction/createChainedFunction.ts
+++ packages/mui-utils/src/createChainedFunction/createChainedFunction.ts
@@ -5,7 +5,7 @@
  * otherwise will pass back existing functions or null.
  */
 export default function createChainedFunction<Args extends any[], This>(
-  ...funcs: Array<(this: This, ...args: Args) => any|null|undefined>
+  ...funcs: Array<(this: This, ...args: Args) => any>
 ): (this: This, ...args: Args) => void {
   return funcs.reduce(
     (acc, func) => {
