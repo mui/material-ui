7781a01b978267c3980ed6f5759c0071f7a6f14b
Siriwat K <siriwatkunaporn@gmail.com>
2024-11-18 16:51:39 +0700

    [Tooltip] Deprecate *Component and *Props for v6 (#44350)

--- packages/mui-codemod/src/deprecations/tooltip-props/test-cases/theme.expected.js
+++ packages/mui-codemod/src/deprecations/tooltip-props/test-cases/theme.expected.js
@@ -70,3 +70,36 @@ fn({
     },
   },
 });
+fn({
+  MuiTooltip: {
+    defaultProps: {
+      slots: {
+        popper: CustomPopper,
+        transition: CustomTransition
+      },
+
+      slotProps: {
+        popper: { disablePortal: true },
+        transition: { timeout: 200 }
+      }
+    },
+  },
+});
+
+fn({
+  MuiTooltip: {
+    defaultProps: {
+      slotProps: {
+        tooltip: { height: 20 },
+        popper: { disablePortal: true },
+        transition: { timeout: 200 }
+      },
+
+      slots: {
+        tooltip: "div",
+        popper: CustomPopper,
+        transition: CustomTransition
+      }
+    },
+  },
+});
