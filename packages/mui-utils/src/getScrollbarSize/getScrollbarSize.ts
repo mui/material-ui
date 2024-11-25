d8e6d3cee2cb3c0fcff42cb4daf29af0a0a2ccfd
Son1c-sound <makaradze98.s@gmail.com>
2024-11-25 15:26:26 -0500

    patched

--- packages/mui-utils/src/getScrollbarSize/getScrollbarSize.ts
+++ packages/mui-utils/src/getScrollbarSize/getScrollbarSize.ts
@@ -1,7 +1,8 @@
 // A change of the browser zoom change the scrollbar size.
 // Credit https://github.com/twbs/bootstrap/blob/488fd8afc535ca3a6ad4dc581f5e89217b6a36ac/js/src/util/scrollbar.js#L14-L18
-export default function getScrollbarSize(win: Window = window): number {
+export default function getScrollbarSize(win: Window | Document = window): number {
   // https://developer.mozilla.org/en-US/docs/Web/API/Window/innerWidth#usage_notes
-  const documentWidth = win.document.documentElement.clientWidth;
-  return win.innerWidth - documentWidth;
+  const doc = win instanceof Window ? win.document : win;
+  const documentWidth = doc.documentElement.clientWidth;
+  return win instanceof Window ? win.innerWidth : (doc.defaultView?.innerWidth ?? 0) - documentWidth;
 }
