46b70bd7229afaf2fb3cfca96766549550312a9a
sai6855 <fayazceg@gmail.com>
2024-11-27 13:13:10 +0530

    [material-ui][Popover] Improve Popover open on hover demo

--- docs/data/material/components/popover/MouseHoverPopover.js
+++ docs/data/material/components/popover/MouseHoverPopover.js
@@ -13,21 +13,58 @@ export default function MouseHoverPopover() {
     setAnchorEl(null);
   };
 
+  const handlePopoverMouseMove = (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    if (!typographyRef.current || !paperRef.current) {
+      return;
+    }
+
+    const isMouseInsideTypography =
+      x > typographyRef.current?.getBoundingClientRect().left &&
+      x < typographyRef.current?.getBoundingClientRect().right &&
+      y > typographyRef.current?.getBoundingClientRect().top &&
+      y < typographyRef.current?.getBoundingClientRect().bottom;
+
+    const isMouseInsidePaper =
+      x > paperRef.current?.getBoundingClientRect().left &&
+      x < paperRef.current?.getBoundingClientRect().right &&
+      y > paperRef.current?.getBoundingClientRect().top &&
+      y < paperRef.current?.getBoundingClientRect().bottom;
+
+    if (!isMouseInsideTypography && !isMouseInsidePaper) {
+      handlePopoverClose();
+    }
+  };
+
   const open = Boolean(anchorEl);
+
+  const paperRef = React.useRef(null);
+  const typographyRef = React.useRef(null);
 
   return (
     <div>
       <Typography
+        ref={typographyRef}
         aria-owns={open ? 'mouse-over-popover' : undefined}
         aria-haspopup="true"
         onMouseEnter={handlePopoverOpen}
-        onMouseLeave={handlePopoverClose}
+        sx={{
+          display: 'inline',
+        }}
       >
         Hover with a Popover.
       </Typography>
+
       <Popover
+        onMouseMove={handlePopoverMouseMove}
+        slotProps={{
+          paper: {
+            ref: paperRef,
+          },
+        }}
         id="mouse-over-popover"
-        sx={{ pointerEvents: 'none' }}
         open={open}
         anchorEl={anchorEl}
         anchorOrigin={{
--- docs/data/material/components/popover/MouseHoverPopover.tsx
+++ docs/data/material/components/popover/MouseHoverPopover.tsx
@@ -13,21 +13,56 @@ export default function MouseHoverPopover() {
     setAnchorEl(null);
   };
 
+  const handlePopoverMouseMove = (event: React.MouseEvent<HTMLElement>) => {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    if (!typographyRef.current || !paperRef.current) {
+      return;
+    }
+
+    const isMouseInsideTypography =
+      x > typographyRef.current?.getBoundingClientRect().left &&
+      x < typographyRef.current?.getBoundingClientRect().right &&
+      y > typographyRef.current?.getBoundingClientRect().top &&
+      y < typographyRef.current?.getBoundingClientRect().bottom;
+
+    const isMouseInsidePaper =
+      x > paperRef.current?.getBoundingClientRect().left &&
+      x < paperRef.current?.getBoundingClientRect().right &&
+      y > paperRef.current?.getBoundingClientRect().top &&
+      y < paperRef.current?.getBoundingClientRect().bottom;
+
+    if (!isMouseInsideTypography && !isMouseInsidePaper) {
+      handlePopoverClose();
+    }
+  };
+
   const open = Boolean(anchorEl);
+
+  const paperRef = React.useRef<HTMLDivElement>(null);
+  const typographyRef = React.useRef<HTMLSpanElement>(null);
 
   return (
     <div>
       <Typography
+        ref={typographyRef}
         aria-owns={open ? 'mouse-over-popover' : undefined}
         aria-haspopup="true"
         onMouseEnter={handlePopoverOpen}
-        onMouseLeave={handlePopoverClose}
+        onMouseLeave={handlePopoverMouseMove}
       >
         Hover with a Popover.
       </Typography>
+
       <Popover
+        onMouseMove={handlePopoverMouseMove}
+        slotProps={{
+          paper: {
+            ref: paperRef,
+          },
+        }}
         id="mouse-over-popover"
-        sx={{ pointerEvents: 'none' }}
         open={open}
         anchorEl={anchorEl}
         anchorOrigin={{
