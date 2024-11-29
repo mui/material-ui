5c75456e43cd7fbda678a5c1e5356c41a24b1ecb
sai6855 <fayazceg@gmail.com>
2024-11-29 12:53:56 +0530

    [Grid2] Add container styles from styleOverrides

--- packages/mui-material/src/Grid2/Grid2.test.js
+++ packages/mui-material/src/Grid2/Grid2.test.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { createRenderer } from '@mui/internal-test-utils';
 import Grid2, { grid2Classes as classes } from '@mui/material/Grid2';
+import { expect } from 'chai';
 import describeConformance from '../../test/describeConformance';
+import { createTheme, ThemeProvider } from '..';
 
 // The main tests are in mui-system Grid folder
 describe('<Grid2 />', () => {
@@ -20,4 +22,31 @@ describe('<Grid2 />', () => {
     testVariantProps: { container: true, spacing: 5 },
     skip: ['componentsProp', 'classesRoot'],
   }));
+
+  it('should render with the container class', () => {
+    const { getByTestId } = render(<Grid2 data-testid="grid" container />);
+    expect(getByTestId('grid')).to.have.class(classes.container);
+  });
+
+  it('should have container styles passed from theme', () => {
+    const theme = createTheme({
+      components: {
+        MuiGrid2: {
+          styleOverrides: {
+            container: {
+              padding: '11px',
+            },
+          },
+        },
+      },
+    });
+    const { getByTestId } = render(
+      <ThemeProvider theme={theme}>
+        <Grid2 data-testid="grid" container>
+          hello
+        </Grid2>
+      </ThemeProvider>,
+    );
+    expect(getByTestId('grid')).to.have.style('padding', '11px');
+  });
 });
