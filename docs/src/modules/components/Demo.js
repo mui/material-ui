b49c04de425f6dc7de1cb8ddca74b5dc3d06c081
SuperMaxine <32979328+SuperMaxine@users.noreply.github.com>
2024-12-02 08:43:02 +0000

    Fix ReDoS in /workspaces/material-ui/docs/src/modules/components/Demo.js

--- docs/src/modules/components/Demo.js
+++ docs/src/modules/components/Demo.js
@@ -473,7 +473,7 @@ export default function Demo(props) {
         `The following demos use TS directly: ${demoOptions.demo}.`,
         '',
         'Please run "pnpm docs:typescript:formatted" to generate a JS version and reference it:',
-        `{{"demo": "${demoOptions.demo.replace(/\.(.*)$/, '.js')}", …}}.`,
+        `{{"demo": "${demoOptions.demo.replace(/\.([^.]*)$/, '.js')}", …}}.`,
         '',
         "Otherwise, if it's not a code demo hide the toolbar:",
         `{{"demo": "${demoOptions.demo}", "hideToolbar": true, …}}.`,
---
+++ docs/src/modules/components/Demo.test.js
@@ -0,0 +1,38 @@
+const { expect } = require('chai');
+const { createRenderer,test } = require('@mui/internal-test-utils');
+import Demo from './Demo'
+
+describe('Function execution time test', function () {
+  this.timeout(15000); // Ensure that Mocha itself does not end tests due to the default timeout.
+  const { render } = createRenderer();
+  var str = "";
+  for (var i = 0; i < 1000000; i++) {
+  str += ".";
+  }
+  str += "\n@.ts";
+
+  it('should complete within 1 seconds', async function () {
+    const start = Date.now();
+      try {
+        expect(() => {
+          render(
+            <Demo
+            mode={""}
+            demo={""}
+            disableAd={""}
+            demoOptions={{
+              hideToolbar: "a",
+              demo: str,
+              defaultCodeOpen: false,
+              disableAd: false,
+            }}
+            githubLocation={""}
+          />
+          );
+        }).toErrorDev('Any errors do not affect a DoS attack; only time is crucial.');
+      } catch (error) { }
+      const end = Date.now();
+      const duration = (end - start) / 1000; // turn to seconds
+      expect(duration).to.be.lessThan(10);
+    });
+});
