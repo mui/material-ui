3467e669789d0dd0ce416572ca892c1234476237
alelthomas <alelthommas@gmail.com>
2024-12-10 19:59:14 -0300

    [blog] material-ui 2024 eoy updates

--- docs/pages/blog/material-ui-2024-updates.js
+++ docs/pages/blog/material-ui-2024-updates.js
@@ -1,5 +1,5 @@
-import TopLayoutBlog from 'docs/src/modules/components/TopLayoutBlog';
 import * as React from 'react';
+import TopLayoutBlog from 'docs/src/modules/components/TopLayoutBlog';
 import { docs } from './material-ui-2024-updates.md?muiMarkdown';
 
 export default function PAge() {
