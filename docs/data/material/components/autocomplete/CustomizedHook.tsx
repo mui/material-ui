a3adeeaebdb933729249a70b629d6bdc57fe9ab1
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2024-12-18 14:56:30 +0530

    fix import

--- docs/data/material/components/autocomplete/CustomizedHook.tsx
+++ docs/data/material/components/autocomplete/CustomizedHook.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import {
-  useAutocomplete,
+import useAutocomplete, {
   AutocompleteGetTagProps,
 } from '@mui/material/useAutocomplete';
 import CheckIcon from '@mui/icons-material/Check';
