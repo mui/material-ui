28c97e6537f44444430015939188fffa3817aea0
DiegoAndai <diego@mui.com>
2024-12-24 13:11:49 -0300

    Bump Next.js to 15 and React to 19

--- examples/material-ui-nextjs-pages-router/pages/_app.js
+++ examples/material-ui-nextjs-pages-router/pages/_app.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter';
 import { ThemeProvider } from '@mui/material/styles';
@@ -22,8 +21,3 @@ export default function MyApp(props) {
     </AppCacheProvider>
   );
 }
-
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
