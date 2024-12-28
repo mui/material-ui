adc326573551768c656e41d4dbcd42385834d464
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-12-28 23:31:08 +0100

    polish

--- docs/data/material/components/autocomplete/GoogleMaps.tsx
+++ docs/data/material/components/autocomplete/GoogleMaps.tsx
@@ -2,11 +2,16 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import Paper, { PaperProps } from '@mui/material/Paper';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Grid2 from '@mui/material/Grid2';
 import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
 import parse from 'autosuggest-highlight/parse';
-import throttle from 'lodash/throttle';
+// For the sake of this demo, we have to use debounce to reduce Google Maps Places API quote use
+// But prefer to use throttle in practice
+// import throttle from 'lodash/throttle';
+import { debounce } from '@mui/material/utils';
 
 // This key was created specifically for the demo in mui.com.
 // You need to create a new one for your application.
@@ -37,39 +42,80 @@ interface PlaceType {
   structured_formatting: StructuredFormatting;
 }
 
-const fetch = throttle(
+function CustomPaper(props: PaperProps) {
+  const theme = useTheme();
+
+  return (
+    <Paper {...props}>
+      {props.children}
+      {/* Legal requirment https://developers.google.com/maps/documentation/javascript/policies#logo */}
+      <Box
+        sx={(staticTheme) => ({
+          display: 'flex',
+          justifyContent: 'flex-end',
+          p: 1,
+          pt: '1px',
+          ...staticTheme.applyStyles('dark', {
+            opacity: 0.8,
+          }),
+        })}
+      >
+        <img
+          src={
+            theme.palette.mode === 'dark'
+              ? 'https://maps.gstatic.com/mapfiles/api-3/images/powered-by-google-on-non-white3_hdpi.png'
+              : 'https://maps.gstatic.com/mapfiles/api-3/images/powered-by-google-on-white3_hdpi.png'
+          }
+          alt=""
+          width="120"
+          height="14"
+        />
+      </Box>
+    </Paper>
+  );
+}
+
+const fetch = debounce(
   async (
     request: { input: string; sessionToken: any },
     callback: (results?: readonly PlaceType[]) => void,
   ) => {
-    const { suggestions } = await (
-      window as any
-    ).google.maps.places.AutocompleteSuggestion.fetchAutocompleteSuggestions(
-      request,
-    );
+    try {
+      const { suggestions } = await (
+        window as any
+      ).google.maps.places.AutocompleteSuggestion.fetchAutocompleteSuggestions(
+        request,
+      );
 
-    callback(
-      suggestions.map((suggestion: any) => {
-        const place = suggestion.placePrediction;
-        // Map to the old AutocompleteService.getPlacePredictions format
-        // https://developers.google.com/maps/documentation/javascript/places-migration-autocomplete
-        return {
-          description: place.text.text,
-          structured_formatting: {
-            main_text: place.mainText.text,
-            main_text_matched_substrings: place.mainText.matches.map(
-              (match: any) => ({
-                offset: match.startOffset,
-                length: match.endOffset - match.startOffset,
-              }),
-            ),
-            secondary_text: place.secondaryText?.text,
-          },
-        };
-      }),
-    );
+      callback(
+        suggestions.map((suggestion: any) => {
+          const place = suggestion.placePrediction;
+          // Map to the old AutocompleteService.getPlacePredictions format
+          // https://developers.google.com/maps/documentation/javascript/places-migration-autocomplete
+          return {
+            description: place.text.text,
+            structured_formatting: {
+              main_text: place.mainText.text,
+              main_text_matched_substrings: place.mainText.matches.map(
+                (match: any) => ({
+                  offset: match.startOffset,
+                  length: match.endOffset - match.startOffset,
+                }),
+              ),
+              secondary_text: place.secondaryText?.text,
+            },
+          };
+        }),
+      );
+    } catch (err: any) {
+      if (err.message === 'Quota exceeded for quota') {
+        callback(request.input.length === 1 ? fakeAnswer.p : fakeAnswer.paris);
+      }
+
+      throw err;
+    }
   },
-  300,
+  400,
 );
 
 const emptyOptions = [] as any;
@@ -155,6 +201,9 @@ export default function GoogleMaps() {
         typeof option === 'string' ? option : option.description
       }
       filterOptions={(x) => x}
+      slots={{
+        paper: CustomPaper,
+      }}
       options={options}
       autoComplete
       includeInputInList
@@ -212,3 +261,87 @@ export default function GoogleMaps() {
     />
   );
 }
+
+// Fake data in case Google Map Places API returns a rate limit.
+const fakeAnswer = {
+  p: [
+    {
+      description: 'Portugal',
+      structured_formatting: {
+        main_text: 'Portugal',
+        main_text_matched_substrings: [{ offset: 0, length: 1 }],
+      },
+    },
+    {
+      description: 'Puerto Rico',
+      structured_formatting: {
+        main_text: 'Puerto Rico',
+        main_text_matched_substrings: [{ offset: 0, length: 1 }],
+      },
+    },
+    {
+      description: 'Pakistan',
+      structured_formatting: {
+        main_text: 'Pakistan',
+        main_text_matched_substrings: [{ offset: 0, length: 1 }],
+      },
+    },
+    {
+      description: 'Philippines',
+      structured_formatting: {
+        main_text: 'Philippines',
+        main_text_matched_substrings: [{ offset: 0, length: 1 }],
+      },
+    },
+    {
+      description: 'Paris, France',
+      structured_formatting: {
+        main_text: 'Paris',
+        main_text_matched_substrings: [{ offset: 0, length: 1 }],
+        secondary_text: 'France',
+      },
+    },
+  ],
+  paris: [
+    {
+      description: 'Paris, France',
+      structured_formatting: {
+        main_text: 'Paris',
+        main_text_matched_substrings: [{ offset: 0, length: 5 }],
+        secondary_text: 'France',
+      },
+    },
+    {
+      description: 'Paris, TX, USA',
+      structured_formatting: {
+        main_text: 'Paris',
+        main_text_matched_substrings: [{ offset: 0, length: 5 }],
+        secondary_text: 'TX, USA',
+      },
+    },
+    {
+      description: "Paris Beauvais Airport, Route de l'Aéroport, Tillé, France",
+      structured_formatting: {
+        main_text: 'Paris Beauvais Airport',
+        main_text_matched_substrings: [{ offset: 0, length: 5 }],
+        secondary_text: "Route de l'Aéroport, Tillé, France",
+      },
+    },
+    {
+      description: 'Paris Las Vegas, South Las Vegas Boulevard, Las Vegas, NV, USA',
+      structured_formatting: {
+        main_text: 'Paris Las Vegas',
+        main_text_matched_substrings: [{ offset: 0, length: 5 }],
+        secondary_text: 'South Las Vegas Boulevard, Las Vegas, NV, USA',
+      },
+    },
+    {
+      description: "Paris La Défense Arena, Jardin de l'Arche, Nanterre, France",
+      structured_formatting: {
+        main_text: 'Paris La Défense Arena',
+        main_text_matched_substrings: [{ offset: 0, length: 5 }],
+        secondary_text: "Jardin de l'Arche, Nanterre, France",
+      },
+    },
+  ],
+};
