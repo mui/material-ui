8cf0d4125da991ee70473fd3bb99215179eefbe0
siriwatknp <siriwatkunaporn@gmail.com>
2025-01-13 13:41:36 +0700

    add tabs-props codemod

---
+++ packages/mui-codemod/src/deprecations/tabs-props/tabs-props.js
@@ -0,0 +1,37 @@
+import replaceComponentsWithSlots from '../utils/replaceComponentsWithSlots';
+import movePropIntoSlots from '../utils/movePropIntoSlots';
+import movePropIntoSlotProps from '../utils/movePropIntoSlotProps';
+
+/**
+ * @param {import('jscodeshift').FileInfo} file
+ * @param {import('jscodeshift').API} api
+ */
+export default function transformer(file, api, options) {
+  const j = api.jscodeshift;
+  const root = j(file.source);
+  const printOptions = options.printOptions;
+
+  replaceComponentsWithSlots(j, { root, componentName: 'Tabs' });
+
+  movePropIntoSlots(j, {
+    root,
+    componentName: 'Tabs',
+    propName: 'ScrollButtonComponent',
+    slotName: 'scrollButton',
+  });
+  movePropIntoSlotProps(j, {
+    root,
+    componentName: 'Tabs',
+    propName: 'TabScrollButtonProps',
+    slotName: 'scrollButton',
+  });
+
+  movePropIntoSlotProps(j, {
+    root,
+    componentName: 'Tabs',
+    propName: 'TabIndicatorProps',
+    slotName: 'indicator',
+  });
+
+  return root.toSource(printOptions);
+}
