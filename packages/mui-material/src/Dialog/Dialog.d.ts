4c61efb4aca2a2ead1e9b34574aa061fd1f3b1b0
sai6855 <fayazceg@gmail.com>
2025-01-15 13:16:25 +0530

    fix Backdrop import

--- packages/mui-material/src/Dialog/Dialog.d.ts
+++ packages/mui-material/src/Dialog/Dialog.d.ts
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { SxProps, Breakpoint } from '@mui/system';
-import { BackdropProps, InternalStandardProps as StandardProps, Theme } from '..';
+import { InternalStandardProps as StandardProps, Theme } from '..';
+import { BackdropProps } from '../Backdrop';
 import { PaperProps } from '../Paper';
 import { ModalProps } from '../Modal';
 import { TransitionProps } from '../transitions/transition';
