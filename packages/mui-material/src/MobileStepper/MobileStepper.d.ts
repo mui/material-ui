e29461666ddf142a16346db7430fd6331bd3e174
siriwatknp <siriwatkunaporn@gmail.com>
2025-01-16 14:18:13 +0700

    update MobileStepper and add tests

--- packages/mui-material/src/MobileStepper/MobileStepper.d.ts
+++ packages/mui-material/src/MobileStepper/MobileStepper.d.ts
@@ -4,8 +4,73 @@ import { InternalStandardProps as StandardProps, Theme } from '..';
 import { PaperProps } from '../Paper';
 import { LinearProgressProps } from '../LinearProgress';
 import { MobileStepperClasses } from './mobileStepperClasses';
+import { CreateSlotsAndSlotProps, SlotProps } from '../utils/types';
 
-export interface MobileStepperProps extends StandardProps<PaperProps, 'children' | 'variant'> {
+export interface MobileStepperSlots {
+  /**
+   * The component that renders the root slot.
+   * @default Paper
+   */
+  root: React.ElementType;
+  /**
+   * The component that renders the progress slot.
+   * @default LinearProgress
+   */
+  progress: React.ElementType;
+  /**
+   * The component that renders the dots slot.
+   * @default 'div'
+   */
+  dots: React.ElementType;
+  /**
+   * The component that renders the dot slot.
+   * @default 'div'
+   */
+  dot: React.ElementType;
+}
+
+export interface MobileStepperRootSlotPropsOverrides {}
+export interface MobileStepperProgressSlotPropsOverrides {}
+export interface MobileStepperDotsSlotPropsOverrides {}
+export interface MobileStepperDotSlotPropsOverrides {}
+
+export type MobileStepperSlotsAndSlotProps = CreateSlotsAndSlotProps<
+  MobileStepperSlots,
+  {
+    /**
+     * Props forwarded to the root slot.
+     * By default, the avaible props are based on the [Paper](https://mui.com/material-ui/api/paper/#props) component.
+     */
+    root: SlotProps<
+      React.ElementType<PaperProps>,
+      MobileStepperRootSlotPropsOverrides,
+      MobileStepperOwnerState
+    >;
+    /**
+     * Props forwarded to the progress slot.
+     * By default, the avaible props are based on the [LinearProgress](https://mui.com/material-ui/api/linear-progress/#props) component.
+     */
+    progress: SlotProps<
+      React.ElementType<LinearProgressProps>,
+      MobileStepperProgressSlotPropsOverrides,
+      MobileStepperOwnerState
+    >;
+    /**
+     * Props forwarded to the dots slot.
+     * By default, the avaible props are based on the div element.
+     */
+    dots: SlotProps<'div', MobileStepperDotsSlotPropsOverrides, MobileStepperOwnerState>;
+    /**
+     * Props forwarded to the dot slot.
+     * By default, the avaible props are based on the div element.
+     */
+    dot: SlotProps<'div', MobileStepperDotSlotPropsOverrides, MobileStepperOwnerState>;
+  }
+>;
+
+export interface MobileStepperProps
+  extends StandardProps<PaperProps, 'children' | 'variant'>,
+    MobileStepperSlotsAndSlotProps {
   /**
    * Set the active step (zero based index).
    * Defines which dot is highlighted when the variant is 'dots'.
@@ -22,6 +87,7 @@ export interface MobileStepperProps extends StandardProps<PaperProps, 'children'
   classes?: Partial<MobileStepperClasses>;
   /**
    * Props applied to the `LinearProgress` element.
+   * @deprecated Use `slotProps.progress` instead. This prop will be removed in v7. See [Migrating from deprecated APIs](/material-ui/migration/migrating-from-deprecated-apis/) for more details.
    */
   LinearProgressProps?: Partial<LinearProgressProps>;
   /**
@@ -47,6 +113,8 @@ export interface MobileStepperProps extends StandardProps<PaperProps, 'children'
    */
   variant?: 'text' | 'dots' | 'progress';
 }
+
+export interface MobileStepperOwnerState extends Omit<MobileStepperProps, 'slots' | 'slotProps'> {}
 
 /**
  *
