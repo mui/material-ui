48c4b1297d5c504ab93078c0305dc630e7e549a3
Josh Kelley <joshkel@gmail.com>
2025-01-17 14:15:33 -0500

    Fix arrow keys past the end for Slider with custom marks
    Fixes #45049

--- packages/mui-material/src/Slider/Slider.test.js
+++ packages/mui-material/src/Slider/Slider.test.js
@@ -1195,6 +1195,54 @@ describe('<Slider />', () => {
         });
       });
     });
+
+    it(`stops at the max value with custom marks`, () => {
+      const handleChange = stub().callsFake((_event, value) => value);
+      const { getByRole } = render(
+        <Slider
+          marks={[10, 20, 30].map((value) => ({ value }))}
+          step={null}
+          value={30}
+          onChange={handleChange}
+        />,
+      );
+
+      const slider = getByRole('slider');
+      expect(slider).to.have.attribute('aria-valuenow', '30');
+
+      act(() => {
+        slider.focus();
+      });
+
+      fireEvent.keyDown(slider, { key: 'ArrowRight' });
+
+      expect(handleChange.callCount).to.equal(0);
+      expect(slider).to.have.attribute('aria-valuenow', '30');
+    });
+
+    it(`stops at the min value with custom marks`, () => {
+      const handleChange = stub().callsFake((_event, value) => value);
+      const { getByRole } = render(
+        <Slider
+          marks={[10, 20, 30].map((value) => ({ value }))}
+          step={null}
+          value={10}
+          onChange={handleChange}
+        />,
+      );
+
+      const slider = getByRole('slider');
+      expect(slider).to.have.attribute('aria-valuenow', '10');
+
+      act(() => {
+        slider.focus();
+      });
+
+      fireEvent.keyDown(slider, { key: 'ArrowLeft' });
+
+      expect(handleChange.callCount).to.equal(0);
+      expect(slider).to.have.attribute('aria-valuenow', '10');
+    });
   });
 
   describe('warnings', () => {
