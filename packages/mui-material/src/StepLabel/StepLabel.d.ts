3596be66c467bd0c7aef52c27d95be8b5cd94f5f
DiegoAndai <diego@mui.com>
2025-01-27 15:25:25 -0300

    Update docs to point to `next.mui.com`

--- packages/mui-material/src/StepLabel/StepLabel.d.ts
+++ packages/mui-material/src/StepLabel/StepLabel.d.ts
@@ -86,11 +86,11 @@ export type StepLabelClasskey = keyof NonNullable<StepLabelProps['classes']>;
  *
  * Demos:
  *
- * - [Stepper](https://mui.com/material-ui/react-stepper/)
+ * - [Stepper](https://next.mui.com/material-ui/react-stepper/)
  *
  * API:
  *
- * - [StepLabel API](https://mui.com/material-ui/api/step-label/)
+ * - [StepLabel API](https://next.mui.com/material-ui/api/step-label/)
  */
 declare const StepLabel: ((props: StepLabelProps) => React.JSX.Element) & {
   muiName: string;
