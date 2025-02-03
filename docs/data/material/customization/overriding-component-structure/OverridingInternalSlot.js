2559c6f74dce4a61507acb30ffaca8a283823428
siriwatknp <siriwatkunaporn@gmail.com>
2025-02-03 15:28:02 +0700

    duplicate overriding-component-structure to material-ui

---
+++ docs/data/material/customization/overriding-component-structure/OverridingInternalSlot.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+
+function PopperComponent(props) {
+  const { disablePortal, anchorEl, open, ...other } = props;
+  return <div {...other} />;
+}
+
+PopperComponent.propTypes = {
+  anchorEl: PropTypes.any,
+  disablePortal: PropTypes.bool,
+  open: PropTypes.bool.isRequired,
+};
+
+export default function OverridingInternalSlot() {
+  return (
+    <Box
+      sx={{ display: 'flex', flexDirection: 'column', width: 320, minHeight: 220 }}
+    >
+      <Autocomplete
+        open
+        options={[
+          { label: '🆘 Need help' },
+          { label: '✨ Improvement' },
+          { label: '🚀 New feature' },
+          { label: '🐛 Bug fix' },
+        ]}
+        renderInput={(params) => <TextField {...params} />}
+        slots={{
+          popper: PopperComponent,
+        }}
+      />
+    </Box>
+  );
+}
