2ca7d37b4bc3d3071907d6081882efdfbaee931f
sai6855 <fayazceg@gmail.com>
2025-02-07 15:08:17 +0530

    fix actual.js

--- packages/mui-codemod/src/deprecations/input-base-classes/test-cases/actual.js
+++ packages/mui-codemod/src/deprecations/input-base-classes/test-cases/actual.js
@@ -1,14 +1,12 @@
 import { inputBaseClasses } from '@mui/material/InputBase';
 
 ('& .MuiInputBase-inputHiddenLabel');
-('& .MuiInputBase-inputTypeSearch');
 ('& .MuiInputBase-inputMultiline');
 ('& .MuiInputBase-inputAdornedStart');
 ('& .MuiInputBase-inputAdornedEnd');
 ('& .MuiInputBase-inputSizeSmall');
 
 `& .${inputBaseClasses.inputHiddenLabel}`;
-`& .${inputBaseClasses.inputTypeSearch}`;
 `& .${inputBaseClasses.inputMultiline}`;
 `& .${inputBaseClasses.inputAdornedStart}`;
 `& .${inputBaseClasses.inputAdornedEnd}`;
