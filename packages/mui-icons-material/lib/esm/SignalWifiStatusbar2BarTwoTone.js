d7eb57d81c673f499b8557539de8cffc1d4298f4
Siriwat K <siriwatkunaporn@gmail.com>
2025-02-07 23:32:54 +0700

    [icons] Change icon source URL and add overrides (#45020)

---
+++ packages/mui-icons-material/lib/esm/SignalWifiStatusbar2BarTwoTone.js
@@ -0,0 +1,10 @@
+"use client";
+
+import createSvgIcon from "./utils/createSvgIcon.js";
+import { jsx as _jsx } from "react/jsx-runtime";
+export default createSvgIcon([/*#__PURE__*/_jsx("path", {
+  fillOpacity: ".3",
+  d: "M19.77 13.22 24 8.98C20.93 5.9 16.69 4 12 4S3.07 5.9 0 8.98l4.23 4.24C6.22 11.23 8.97 10 12 10s5.78 1.23 7.77 3.22"
+}, "0"), /*#__PURE__*/_jsx("path", {
+  d: "M19.77 13.22C17.78 11.23 15.03 10 12 10s-5.78 1.23-7.77 3.22L12 21z"
+}, "1")], 'SignalWifiStatusbar2BarTwoTone');
---
+++ packages/mui-icons-material/lib/esm/SignalWifiStatusbarConnectedNoInternet2TwoTone.js
@@ -0,0 +1,10 @@
+"use client";
+
+import createSvgIcon from "./utils/createSvgIcon.js";
+import { jsx as _jsx } from "react/jsx-runtime";
+export default createSvgIcon([/*#__PURE__*/_jsx("path", {
+  fillOpacity: ".3",
+  d: "M17 11.21V8h5.92C19.97 5.51 16.16 4 12 4 7.31 4 3.07 5.9 0 8.98l4.23 4.24C6.22 11.23 8.97 10 12 10c1.8 0 3.5.44 5 1.21"
+}, "0"), /*#__PURE__*/_jsx("path", {
+  d: "M4.23 13.22 12 21l5-5.01v-4.78c-1.5-.77-3.2-1.21-5-1.21-3.03 0-5.78 1.23-7.77 3.22M19 18h2v2h-2zm0-8h2v6h-2z"
+}, "1")], 'SignalWifiStatusbarConnectedNoInternet2TwoTone');
