d26751fd141637da3970979a4078c3f1f417508a
Sahil Vaddoriya <112296220+sahilpatel444@users.noreply.github.com>
2025-02-07 16:20:59 +0530

    Delete apps directory
    Signed-off-by: Sahil Vaddoriya <112296220+sahilpatel444@users.noreply.github.com>

--- apps/pigment-css-vite-app/.mocharc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  recursive: true,
-  slow: 500,
-  timeout: (process.env.CIRCLECI === 'true' ? 4 : 2) * 1000, // Circle CI has low-performance CPUs.
-  reporter: 'dot',
-  require: ['@mui/internal-test-utils/setupBabelPlaywright'],
-};
