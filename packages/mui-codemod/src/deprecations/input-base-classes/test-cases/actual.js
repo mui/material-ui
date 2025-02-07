32877990c95a85b5871524bd83062826c2d6aa49
sai6855 <fayazceg@gmail.com>
2025-02-07 15:07:52 +0530

    fix actual.js

--- packages/mui-codemod/src/deprecations/input-base-classes/test-cases/actual.js
+++ packages/mui-codemod/src/deprecations/input-base-classes/test-cases/actual.js
@@ -1,13 +1,15 @@
 import { inputBaseClasses } from '@mui/material/InputBase';
 
-("&.MuiInputBase-hiddenLabel > .MuiInputBase-input");
-("&.MuiInputBase-multiline > .MuiInputBase-input");
-("&.MuiInputBase-adornedStart > .MuiInputBase-input");
-("&.MuiInputBase-adornedEnd > .MuiInputBase-input");
-("&.MuiInputBase-sizeSmall > .MuiInputBase-input");
+('& .MuiInputBase-inputHiddenLabel');
+('& .MuiInputBase-inputTypeSearch');
+('& .MuiInputBase-inputMultiline');
+('& .MuiInputBase-inputAdornedStart');
+('& .MuiInputBase-inputAdornedEnd');
+('& .MuiInputBase-inputSizeSmall');
 
-`&.${inputBaseClasses.hiddenLabel} > .${inputBaseClasses.input}`;
-`&.${inputBaseClasses.multiline} > .${inputBaseClasses.input}`;
-`&.${inputBaseClasses.adornedStart} > .${inputBaseClasses.input}`;
-`&.${inputBaseClasses.adornedEnd} > .${inputBaseClasses.input}`;
-`&.${inputBaseClasses.sizeSmall} > .${inputBaseClasses.input}`;
+`& .${inputBaseClasses.inputHiddenLabel}`;
+`& .${inputBaseClasses.inputTypeSearch}`;
+`& .${inputBaseClasses.inputMultiline}`;
+`& .${inputBaseClasses.inputAdornedStart}`;
+`& .${inputBaseClasses.inputAdornedEnd}`;
+`& .${inputBaseClasses.inputSizeSmall}`;
