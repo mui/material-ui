d9ec3c7e683708ce2dcd774b2ea327c93f4cee6d
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2025-02-11 18:23:56 +0530

    add codemod to replace size normal with medium for InputLabel component

---
+++ packages/mui-codemod/src/v7.0.0/input-label-size-normal-medium/test-cases/actual.js
@@ -0,0 +1,12 @@
+<div>
+  <InputLabel size="normal">Label</InputLabel>
+  <InputLabel
+    sx={{
+      '& .MuiInputLabel-normal': {
+        color: 'red',
+      },
+    }}
+  >
+    Label
+  </InputLabel>
+</div>;
---
+++ packages/mui-codemod/src/v7.0.0/input-label-size-normal-medium/test-cases/expected.js
@@ -0,0 +1,12 @@
+<div>
+  <InputLabel size="medium">Label</InputLabel>
+  <InputLabel
+    sx={{
+      '& .MuiInputLabel-medium': {
+        color: 'red',
+      },
+    }}
+  >
+    Label
+  </InputLabel>
+</div>;
