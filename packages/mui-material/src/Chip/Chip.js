182994891f4e07135a12c1f4d8f1edf1625b353d
smeresa <141207612+smeresa@users.noreply.github.com>
2025-02-16 23:45:12 -0800

    Fix misaligned adornments in partial rtl mode

--- packages/mui-material/src/Chip/Chip.js
+++ packages/mui-material/src/Chip/Chip.js
@@ -79,6 +79,7 @@ const ChipRoot = styled('div', {
   },
 })(
   memoTheme(({ theme }) => {
+    const isRTL = theme.direction === 'rtl';
     const textColor =
       theme.palette.mode === 'light' ? theme.palette.grey[700] : theme.palette.grey[300];
     return {
@@ -108,8 +109,8 @@ const ChipRoot = styled('div', {
         pointerEvents: 'none',
       },
       [`& .${chipClasses.avatar}`]: {
-        marginLeft: 5,
-        marginRight: -6,
+        [isRTL ? 'marginRight' : 'marginLeft']: 5,
+        [isRTL ? 'marginLeft' : 'marginRight']: -6,
         width: 24,
         height: 24,
         color: theme.vars ? theme.vars.palette.Chip.defaultAvatarColor : textColor,
@@ -124,15 +125,15 @@ const ChipRoot = styled('div', {
         backgroundColor: (theme.vars || theme).palette.secondary.dark,
       },
       [`& .${chipClasses.avatarSmall}`]: {
-        marginLeft: 4,
-        marginRight: -4,
+        [isRTL ? 'marginRight' : 'marginLeft']: 4,
+        [isRTL ? 'marginLeft' : 'marginRight']: -4,
         width: 18,
         height: 18,
         fontSize: theme.typography.pxToRem(10),
       },
       [`& .${chipClasses.icon}`]: {
-        marginLeft: 5,
-        marginRight: -6,
+        [isRTL ? 'marginRight' : 'marginLeft']: 5,
+        [isRTL ? 'marginLeft' : 'marginRight']: -6,
       },
       [`& .${chipClasses.deleteIcon}`]: {
         WebkitTapHighlightColor: 'transparent',
@@ -141,7 +142,8 @@ const ChipRoot = styled('div', {
           : alpha(theme.palette.text.primary, 0.26),
         fontSize: 22,
         cursor: 'pointer',
-        margin: '0 5px 0 -6px',
+        [isRTL ? 'marginLeft' : 'marginRight']: 5,
+        [isRTL ? 'marginRight' : 'marginLeft']: -6,
         '&:hover': {
           color: theme.vars
             ? `rgba(${theme.vars.palette.text.primaryChannel} / 0.4)`
@@ -155,13 +157,13 @@ const ChipRoot = styled('div', {
             height: 24,
             [`& .${chipClasses.icon}`]: {
               fontSize: 18,
-              marginLeft: 4,
-              marginRight: -4,
+              [isRTL ? 'marginRight' : 'marginLeft']: 4,
+              [isRTL ? 'marginLeft' : 'marginRight']: -4,
             },
             [`& .${chipClasses.deleteIcon}`]: {
               fontSize: 16,
-              marginRight: 4,
-              marginLeft: -4,
+              [isRTL ? 'marginLeft' : 'marginRight']: 4,
+              [isRTL ? 'marginRight' : 'marginLeft']: -4,
             },
           },
         },
@@ -278,22 +280,22 @@ const ChipRoot = styled('div', {
               backgroundColor: (theme.vars || theme).palette.action.focus,
             },
             [`& .${chipClasses.avatar}`]: {
-              marginLeft: 4,
+              [isRTL ? 'marginRight' : 'marginLeft']: 4,
             },
             [`& .${chipClasses.avatarSmall}`]: {
-              marginLeft: 2,
+              [isRTL ? 'marginRight' : 'marginLeft']: 2,
             },
             [`& .${chipClasses.icon}`]: {
-              marginLeft: 4,
+              [isRTL ? 'marginRight' : 'marginLeft']: 4,
             },
             [`& .${chipClasses.iconSmall}`]: {
-              marginLeft: 2,
+              [isRTL ? 'marginRight' : 'marginLeft']: 2,
             },
             [`& .${chipClasses.deleteIcon}`]: {
-              marginRight: 5,
+              [isRTL ? 'marginLeft' : 'marginRight']: 5,
             },
             [`& .${chipClasses.deleteIconSmall}`]: {
-              marginRight: 3,
+              [isRTL ? 'marginLeft' : 'marginRight']: 3,
             },
           },
         },
