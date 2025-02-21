698dd35632bea0b4a57c653bfc4521c93a70f122
Siriwat K <siriwatkunaporn@gmail.com>
2025-02-22 06:50:00 +0700

    [Menu] Deprecate *Props and complete `slots`, `slotProps`  (#45369)

--- packages/mui-codemod/src/deprecations/all/deprecations-all.js
+++ packages/mui-codemod/src/deprecations/all/deprecations-all.js
@@ -40,6 +40,7 @@ import transformSnackbarProps from '../snackbar-props';
 import transformerTabsProps from '../tabs-props';
 import transformerTabsClasses from '../tabs-classes';
 import transformDrawerProps from '../drawer-props';
+import transformMenuProps from '../menu-props';
 
 /**
  * @param {import('jscodeshift').FileInfo} file
@@ -88,6 +89,7 @@ export default function deprecationsAll(file, api, options) {
   file.source = transformerTabsProps(file, api, options);
   file.source = transformerTabsClasses(file, api, options);
   file.source = transformDrawerProps(file, api, options);
+  file.source = transformMenuProps(file, api, options);
 
   return file.source;
 }
