3409cb9f474e5d89dd1fb85b07bd3b4133610c4e
siriwatknp <siriwatkunaporn@gmail.com>
2025-02-27 13:31:58 +0700

    add `enableCssLayer` option

--- packages/mui-styled-engine/src/StyledEngineProvider/StyledEngineProvider.js
+++ packages/mui-styled-engine/src/StyledEngineProvider/StyledEngineProvider.js
@@ -23,11 +23,11 @@ const createEmotionCache = (options, CustomSheet) => {
   return cache;
 };
 
-let cache;
+let insertionPoint;
 if (typeof document === 'object') {
   // Use `insertionPoint` over `prepend`(deprecated) because it can be controlled for GlobalStyles injection order
   // For more information, see https://github.com/mui/material-ui/issues/44597
-  let insertionPoint = document.querySelector('[name="emotion-insertion-point"]');
+  insertionPoint = document.querySelector('[name="emotion-insertion-point"]');
   if (!insertionPoint) {
     insertionPoint = document.createElement('meta');
     insertionPoint.setAttribute('name', 'emotion-insertion-point');
@@ -37,25 +37,52 @@ if (typeof document === 'object') {
       head.prepend(insertionPoint);
     }
   }
-  /**
-   * This is for client-side apps only.
-   * A custom sheet is required to make the GlobalStyles API injected above the insertion point.
-   * This is because the [sheet](https://github.com/emotion-js/emotion/blob/main/packages/react/src/global.js#L94-L99) does not consume the options.
-   */
-  class MyStyleSheet extends StyleSheet {
-    insert(rule, options) {
-      if (this.key && this.key.endsWith('global')) {
-        this.before = insertionPoint;
+}
+
+function getCache(injectFirst, enableCssLayer) {
+  if (injectFirst || enableCssLayer) {
+    /**
+     * This is for client-side apps only.
+     * A custom sheet is required to make the GlobalStyles API injected above the insertion point.
+     * This is because the [sheet](https://github.com/emotion-js/emotion/blob/main/packages/react/src/global.js#L94-L99) does not consume the options.
+     */
+    class MyStyleSheet extends StyleSheet {
+      insert(rule, options) {
+        if (this.key && this.key.endsWith('global')) {
+          this.before = insertionPoint;
+        }
+        return super.insert(rule, options);
       }
-      return super.insert(rule, options);
     }
+    const emotionCache = createEmotionCache(
+      {
+        key: 'css',
+        insertionPoint: injectFirst ? insertionPoint : undefined,
+      },
+      MyStyleSheet,
+    );
+    if (enableCssLayer) {
+      const prevInsert = emotionCache.insert;
+      emotionCache.insert = (...args) => {
+        if (!args[1].styles.startsWith('@layer')) {
+          // avoid nested @layer
+          args[1].styles = `@layer mui {${args[1].styles}}`;
+        }
+        return prevInsert(...args);
+      };
+    }
+    return emotionCache;
   }
-  cache = createEmotionCache({ key: 'css', insertionPoint }, MyStyleSheet);
+  return undefined;
 }
 
 export default function StyledEngineProvider(props) {
-  const { injectFirst, children } = props;
-  return injectFirst && cache ? <CacheProvider value={cache}>{children}</CacheProvider> : children;
+  const { injectFirst, enableCssLayer, children } = props;
+  const cache = React.useMemo(
+    () => getCache(injectFirst, enableCssLayer),
+    [injectFirst, enableCssLayer],
+  );
+  return cache ? <CacheProvider value={cache}>{children}</CacheProvider> : children;
 }
 
 StyledEngineProvider.propTypes = {
@@ -63,6 +90,11 @@ StyledEngineProvider.propTypes = {
    * Your component tree.
    */
   children: PropTypes.node,
+  /**
+   * If `true`, the styles are wrapped in `@layer mui`.
+   * Learn more about [Cascade layers](https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Styling_basics/Cascade_layers).
+   */
+  enableCssLayer: PropTypes.bool,
   /**
    * By default, the styles are injected last in the <head> element of the page.
    * As a result, they gain more specificity than any other style sheet.
