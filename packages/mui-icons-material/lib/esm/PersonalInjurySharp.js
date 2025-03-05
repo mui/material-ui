5eaeac96a6956a2f0352396d8054392cdbb6dd37
yermartee <yermartee@proton.me>
2025-03-05 22:21:53 +0400

    Add new SVG icons: NewLabel, DriveFileMoveRtl, IncompleteCircle, and their variants

---
+++ packages/mui-icons-material/lib/esm/PersonalInjurySharp.js
@@ -0,0 +1,7 @@
+"use client";
+
+import createSvgIcon from "./utils/createSvgIcon.js";
+import { jsx as _jsx } from "react/jsx-runtime";
+export default createSvgIcon(/*#__PURE__*/_jsx("path", {
+  d: "M8 6c0-2.21 1.79-4 4-4s4 1.79 4 4-1.79 4-4 4-4-1.79-4-4m9 16h1c1.1 0 2-.9 2-2v-4.78c0-1.12-.61-2.15-1.61-2.66-.43-.22-.9-.43-1.39-.62zm-4.66-5L15 11.33c-.93-.21-1.93-.33-3-.33-2.53 0-4.71.7-6.39 1.56-1 .51-1.61 1.54-1.61 2.66V22h2.34c-.22-.45-.34-.96-.34-1.5C6 18.57 7.57 17 9.5 17zM10 22l1.41-3H9.5c-.83 0-1.5.67-1.5 1.5S8.67 22 9.5 22z"
+}), 'PersonalInjurySharp');
