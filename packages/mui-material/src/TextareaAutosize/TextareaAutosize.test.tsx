dd4b7aa1ef442606005d6ca7356ed0c42e70ca70
DiegoAndai <diego@mui.com>
2025-03-07 16:04:29 -0300

    Bump test from 3 to 4 times to include FF 123

--- packages/mui-material/src/TextareaAutosize/TextareaAutosize.test.tsx
+++ packages/mui-material/src/TextareaAutosize/TextareaAutosize.test.tsx
@@ -470,8 +470,8 @@ describe('<TextareaAutosize />', () => {
 
     await render(<App />);
     await sleep(100);
-    // when the component mounts and idles this fires 3 times in browser tests
+    // when the component mounts and idles this fires 4 times in browser tests
     // and 2 times in a real browser
-    expect(handleSelectionChange.callCount).to.lessThanOrEqual(3);
+    expect(handleSelectionChange.callCount).to.lessThanOrEqual(4);
   });
 });
