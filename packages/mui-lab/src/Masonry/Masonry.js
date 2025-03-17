e5f8eee6f4b561374e2c2d146ab0bc6ce164feca
mnajdova <mnajdova@gmail.com>
2025-03-17 16:55:14 +0100

    composeClasses imports

--- packages/mui-lab/src/Masonry/Masonry.js
+++ packages/mui-lab/src/Masonry/Masonry.js
@@ -1,5 +1,5 @@
 'use client';
-import { unstable_composeClasses as composeClasses } from '@mui/base';
+import { unstable_composeClasses as composeClasses } from '@mui/material';
 import * as ReactDOM from 'react-dom';
 import { styled, useThemeProps } from '@mui/material/styles';
 import {
