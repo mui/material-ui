6373bab7711619acf03c11a9ade648f742e59497
siriwatknp <siriwatkunaporn@gmail.com>
2025-03-18 17:13:22 +0700

    use mjs

--- docs/tailwind.config.mjs
+++ docs/tailwind.config.mjs
@@ -2,10 +2,10 @@
 // eslint-disable-next-line import/no-import-module-exports
 import plugin from 'tailwindcss/plugin';
 
-const defaultTheme = require('tailwindcss/defaultTheme');
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ['class', '[data-mui-color-scheme="dark"]'],
   content: [
     './data/**/*.{js,ts,jsx,tsx,mdx}',
