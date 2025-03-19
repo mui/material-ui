bff365c8c12e42bc22c3588dc340eda686a61fdb
Prakhar Gupta <92228082+prakhargupta1@users.noreply.github.com>
2025-03-19 11:36:14 +0300

    ...

--- docs/src/components/pricing/PricingCards.tsx
+++ docs/src/components/pricing/PricingCards.tsx
@@ -71,7 +71,7 @@ export const planInfo: Record<
     description:
       'The most advanced features for data-rich applications along with standard support.',
     features: [
-      { text: 'All Pro', text2: 'features and...', icon: 'check' },
+      { text: 'All Pro', text2: 'features and…', icon: 'check' },
       { text: 'MUI X', highlight: 'Premium', text2: 'access', icon: 'check' },
       { text: '5+', highlight: 'Premium', text2: 'features', icon: 'check' },
       {
