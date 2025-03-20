3e0b86073cd4818e40489b4d627348026e9314cc
mnajdova <mnajdova@gmail.com>
2025-03-20 09:35:22 +0100

    remove unnecessary changes

--- packages/mui-material/src/Autocomplete/index.d.ts
+++ packages/mui-material/src/Autocomplete/index.d.ts
@@ -3,8 +3,3 @@ export * from './Autocomplete';
 
 export { default as autocompleteClasses } from './autocompleteClasses';
 export * from './autocompleteClasses';
-
-export {
-  default as unstable_useAutocomplete,
-  createFilterOptions as unstable_createFilterOptions,
-} from '../useAutocomplete';
--- packages/mui-material/src/Autocomplete/index.js
+++ packages/mui-material/src/Autocomplete/index.js
@@ -2,8 +2,3 @@ export { default, createFilterOptions } from './Autocomplete';
 
 export { default as autocompleteClasses } from './autocompleteClasses';
 export * from './autocompleteClasses';
-
-export {
-  default as unstable_useAutocomplete,
-  createFilterOptions as unstable_createFilterOptions,
-} from '../useAutocomplete';
