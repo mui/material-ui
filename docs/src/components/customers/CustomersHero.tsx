f4ebea1d7fe663c1f562a0ee42b5a0e739ebee68
alelthomas <alelthommas@gmail.com>
2025-03-25 14:02:23 +0000

    hero and empty showcase

---
+++ docs/src/components/customers/CustomersHero.tsx
@@ -0,0 +1,24 @@
+import Section from 'docs/src/layouts/Section';
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import SectionHeadline from '../typography/SectionHeadline';
+import GradientText from '../typography/GradientText';
+
+export default function CustomersHero() {
+  return (
+    <Section cozy noPaddingBottom>
+      <SectionHeadline
+        alwaysCenter
+        overline="Customers"
+        title={
+          <Typography variant="h1" component="h1" sx={{ mb: 2 }}>
+            Meet the <GradientText color="primary">teams</GradientText>
+            <br />
+            powered by <GradientText color="primary">MUI</GradientText>
+          </Typography>
+        }
+        description="See how MUI's comprehensive suite of UI tools helps them ship better and faster"
+      />
+    </Section>
+  );
+}
