726b10d9ee61e0d69e47777b87fa71fc6a4b9736
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2025-04-15 11:20:42 +0530

    fix lint

--- examples/material-ui-vite-styled-components-ts/src/main.tsx
+++ examples/material-ui-vite-styled-components-ts/src/main.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { createRoot } from 'react-dom/client';
+import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
