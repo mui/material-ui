6b702d060d7d3df2149e3383927522be2c3b249a
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2025-04-23 11:54:12 +0100

    feat: add avatar upload demo

---
+++ docs/data/material/components/avatars/UploadAvatars.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import ButtonBase from '@mui/material/ButtonBase';
+
+export default function UploadAvatars() {
+  const [avatarSrc, setAvatarSrc] = React.useState<string | undefined>(undefined);
+  const handleAvatarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      // Read the file as a data URL
+      const reader = new FileReader();
+      reader.onload = () => {
+        setAvatarSrc(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  return (
+    <ButtonBase
+      component="label"
+      tabIndex="-1" // prevent label from tab focus
+      aria-label="Avatar image"
+      sx={{
+        borderRadius: '40px',
+        '&:has(:focus-visible)': {
+          outline: '2px solid',
+          outlineOffset: '2px',
+        },
+      }}
+    >
+      <Avatar alt="Upload new avatar" src={avatarSrc} />
+      <input
+        type="file"
+        style={{
+          border: 0,
+          clip: 'rect(0 0 0 0)',
+          height: '1px',
+          margin: '-1px',
+          overflow: 'hidden',
+          padding: 0,
+          position: 'absolute',
+          whiteSpace: 'nowrap',
+          width: '1px',
+        }}
+        onChange={handleAvatarChange}
+      />
+    </ButtonBase>
+  );
+}
