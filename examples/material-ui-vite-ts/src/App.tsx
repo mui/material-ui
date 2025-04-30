78e50b76c1e1aaa27a446526074d6cd4b6c90941
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2025-04-30 12:34:52 +0530

    fix lint and docs link

--- examples/material-ui-vite-ts/src/App.tsx
+++ examples/material-ui-vite-ts/src/App.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
--- examples/material-ui-vite-ts/src/main.tsx
+++ examples/material-ui-vite-ts/src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 );
