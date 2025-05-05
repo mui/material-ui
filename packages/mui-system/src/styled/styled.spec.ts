5f6282bda086f3b99edcc0871f6eb355668e3dd2
siriwatknp <siriwatkunaporn@gmail.com>
2025-05-05 11:01:41 +0700

    fix props record types

---
+++ packages/mui-system/src/styled/styled.spec.ts
@@ -0,0 +1,52 @@
+import { styled } from '@mui/system';
+
+type OwnerState = {
+  variant?: 'success' | 'error' | 'processing';
+  showChanges: boolean;
+};
+
+const VariantObjects = styled('div')<{ ownerState: OwnerState } & OwnerState>({
+  font: 'inherit',
+  color: 'inherit',
+  variants: [
+    {
+      props: { variant: 'success' },
+      style: {
+        color: 'red',
+      },
+    },
+    {
+      props: { showChanges: false },
+      style: {
+        background: 'red',
+      },
+    },
+  ],
+});
+
+const VariantCallbacks = styled('div')<{ ownerState: OwnerState } & OwnerState>({
+  font: 'inherit',
+  color: 'inherit',
+  variants: [
+    {
+      props: ({ ownerState }) => ownerState.showChanges,
+      style: {
+        color: 'red',
+      },
+    },
+  ],
+});
+
+// @ts-expect-error the props callback must return a boolean
+const VariantCallbacksError = styled('div')<{ ownerState: OwnerState } & OwnerState>({
+  font: 'inherit',
+  color: 'inherit',
+  variants: [
+    {
+      props: ({ ownerState }) => ownerState.variant,
+      style: {
+        color: 'red',
+      },
+    },
+  ],
+});
