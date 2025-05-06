afb33d87e8edc9105d1734f192391d0af0b80c63
siriwatknp <siriwatkunaporn@gmail.com>
2025-05-06 14:21:34 +0700

    update experiment

--- docs/pages/experiments/material-ui/color-mix.tsx
+++ docs/pages/experiments/material-ui/color-mix.tsx
@@ -1,105 +1,104 @@
 import * as React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import Alert from '@mui/material/Alert';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
-import Fab from '@mui/material/Fab';
-import MenuIcon from '@mui/icons-material/Menu';
-import Snackbar from '@mui/material/Snackbar';
+import { BrandingProvider } from '@mui/docs/branding';
+import { HighlightedCode } from '@mui/docs/HighlightedCode';
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 
-const theme = createTheme({
-  colorSpace: 'oklch',
-  cssVariables: true,
-  colorSchemes: {
-    light: {
-      palette: {
-        primary: {
-          main: 'oklch(81% 0.50 78)',
-          // light: 'oklch(70% 0.50 78)',
-          // dark: 'oklch(49% 0.50 78)',
-          // contrastText: '#fff',
-        },
-        info: {
-          main: 'oklch(81% 0.50 78)',
-          // light: 'oklch(70% 0.50 78)',
-          // dark: 'oklch(49% 0.50 78)',
-          // contrastText: '#fff',
-        },
-        Alert: {
-          infoFilledBg: 'oklch(21% 0.50 78)',
-        },
-      },
-    },
-    dark: true,
-  },
-});
-
-export default function ColorMix() {
+export default function ColorMixPage() {
+  const [value, setValue] = React.useState('oklch(80% 0.30 50)');
+  const [primary, setPrimary] = React.useState('oklch(80% 0.30 50)');
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <ThemeProvider
+      disableNestedContext
+      theme={createTheme({
+        cssVariables: {
+          experimental_relativeColor: true,
+        },
+        colorSchemes: {
+          light: {
+            palette: { primary: { main: primary } },
+          },
+        },
+      })}
+    >
       <Box
         sx={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
-          gap: 2,
-          p: 2,
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 4,
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100lvh',
         }}
       >
-        <AppBar position="static" color="default">
-          <Toolbar>
-            <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-
-        <Fab>
-          <MenuIcon />
-        </Fab>
-
-        <Snackbar open autoHideDuration={6000} message="Note archived" />
-
-        <Stack spacing={2}>
-          <Button variant="outlined">Text</Button>
-          <Button variant="contained">Text</Button>
-          <Button variant="text">Text</Button>
-        </Stack>
-
-        <Stack spacing={2}>
-          <Alert severity="info" variant="filled">
-            This is an info alert — check it out!
-          </Alert>
-          <Alert severity="success">This is a success alert — check it out!</Alert>
-          <Alert severity="warning">This is a warning alert — check it out!</Alert>
-          <Alert severity="error">This is an error alert — check it out!</Alert>
-        </Stack>
-
-        <Stack spacing={2}>
-          <Alert severity="info" variant="filled">
-            This is an info alert — check it out!
-          </Alert>
-          <Alert severity="success" variant="filled">
-            {' '}
-            This is a success alert — check it out!
-          </Alert>
-          <Alert severity="warning" variant="filled">
-            This is a warning alert — check it out!
-          </Alert>
-          <Alert severity="error" variant="filled">
-            This is an error alert — check it out!
-          </Alert>
-        </Stack>
+        <TextField
+          label="Primary color"
+          value={value}
+          onChange={(event) => setValue(event.target.value as string)}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton edge="end" onClick={() => setPrimary(value)}>
+                  <KeyboardReturnIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              setPrimary(value);
+            }
+          }}
+          helperText="Try one of: #f00, oklch(80% 0.30 50), hsl(255 50% 50%)"
+          sx={{ width: 360 }}
+        />
+        <Box sx={{ display: 'flex', gap: 1 }}>
+          <Button variant="text" color="primary">
+            Text
+          </Button>
+          <Button variant="contained" color="primary">
+            Contained
+          </Button>
+          <Button variant="outlined" color="primary">
+            Outlined
+          </Button>
+        </Box>
+        <div>
+          <BrandingProvider mode="light">
+            <HighlightedCode
+              language="javascript"
+              code={`extendTheme({
+  cssVariables: {
+    experimental_relativeColor: true,
+  },
+  colorSchemes: {
+    light: {
+      palette: { primary: { main: '${primary}' } },
+    },
+  },
+})`}
+            />
+          </BrandingProvider>
+          <BrandingProvider mode="light">
+            <HighlightedCode
+              language="css"
+              code={`
+--mui-palette-primary-main: ${primary};
+--mui-palette-primary-light:
+  color-mix(in oklch, ${primary}, #fff 20%);
+--mui-palette-primary-dark:
+  color-mix(in oklch, ${primary}, #000 30%);
+--mui-palette-primary-contrastText:
+  oklch(from var(--mui-palette-primary-main) var(--__l) 0 h / var(--__a));
+`}
+            />
+          </BrandingProvider>
+        </div>
       </Box>
     </ThemeProvider>
   );
