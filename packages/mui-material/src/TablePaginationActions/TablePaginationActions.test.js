645fa18059bcbcf04181db7b6aa084428347c998
sai6855 <fayazceg@gmail.com>
2025-05-15 15:32:14 +0530

    fix tests

--- packages/mui-material/src/TablePaginationActions/TablePaginationActions.test.js
+++ packages/mui-material/src/TablePaginationActions/TablePaginationActions.test.js
@@ -32,6 +32,7 @@ describe('<TablePaginationActions />', () => {
         'componentProp',
         'themeDefaultProps',
         'themeStyleOverrides',
+        'themeVariants',
       ],
     }),
   );
