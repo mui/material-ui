bb4df1946cb0267c0eed506eb953f47c74e341b6
siriwatknp <siriwatkunaporn@gmail.com>
2025-05-20 15:16:57 +0700

    update demo

--- docs/data/material/customization/css-layers/CssLayersInput.js
+++ docs/data/material/customization/css-layers/CssLayersInput.js
@@ -1,43 +1,50 @@
 import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import FormHelperText from '@mui/material/FormHelperText';
 
+const theme = createTheme({
+  experimental_modularCssLayers: true,
+});
+
 export default function CssLayersInput() {
   return (
-    <FormControl variant="outlined">
-      <InputLabel
-        shrink
-        htmlFor="css-layers-input"
-        sx={{
-          width: 'fit-content',
-          transform: 'none',
-          position: 'relative',
-          mb: 0.25,
-          fontWeight: 'medium',
-          color: 'text.primary',
-          pointerEvents: 'auto',
-        }}
-      >
-        Label
-      </InputLabel>
-      <OutlinedInput
-        id="css-layers-input"
-        placeholder="Type something"
-        slotProps={{
-          input: {
-            sx: { py: 1.5 },
-          },
-        }}
-      />
-      <FormHelperText
-        sx={{
-          marginLeft: 0,
-        }}
-      >
-        Helper text goes here
-      </FormHelperText>
-    </FormControl>
+    <ThemeProvider theme={theme}>
+      <FormControl variant="outlined">
+        <InputLabel
+          shrink
+          htmlFor="css-layers-input"
+          sx={{
+            width: 'fit-content',
+            transform: 'none',
+            position: 'relative',
+            mb: 0.25,
+            fontWeight: 'medium',
+            color: 'text.primary',
+            pointerEvents: 'auto',
+          }}
+        >
+          Label
+        </InputLabel>
+        <OutlinedInput
+          id="css-layers-input"
+          placeholder="Type something"
+          slotProps={{
+            input: {
+              sx: { py: 1.5 },
+            },
+          }}
+        />
+        <FormHelperText
+          sx={{
+            marginLeft: 0,
+          }}
+        >
+          Helper text goes here
+        </FormHelperText>
+      </FormControl>
+    </ThemeProvider>
   );
 }
