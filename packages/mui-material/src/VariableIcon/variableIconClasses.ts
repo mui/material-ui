d368f55523dff3017f45cbc82222c0288fa4272b
dav-is <mail@connordav.is>
2025-05-20 19:40:19 -0400

    Add VariableIconClasses

---
+++ packages/mui-material/src/VariableIcon/variableIconClasses.ts
@@ -0,0 +1,49 @@
+import generateUtilityClasses from '@mui/utils/generateUtilityClasses';
+import generateUtilityClass from '@mui/utils/generateUtilityClass';
+
+export interface VariableIconClasses {
+  /** Styles applied to the root element. */
+  root: string;
+  /** Styles applied to the root element if `color="primary"`. */
+  colorPrimary: string;
+  /** Styles applied to the root element if `color="secondary"`. */
+  colorSecondary: string;
+  /** Styles applied to the root element if `color="action"`. */
+  colorAction: string;
+  /** Styles applied to the root element if `color="error"`. */
+  colorError: string;
+  /** Styles applied to the root element if `color="disabled"`. */
+  colorDisabled: string;
+  /** Styles applied to the root element if `fontSize="inherit"`. */
+  fontSizeInherit: string;
+  /** Styles applied to the root element if `fontSize="small"`. */
+  fontSizeSmall: string;
+  /** Styles applied to the root element if `fontSize="medium"`. */
+  fontSizeMedium: string;
+  /** Styles applied to the root element if `fontSize="large"`. */
+  fontSizeLarge: string;
+  /** Styles applied to the root element if `fontSize="larger"`. */
+  fontSizeLarger: string;
+}
+
+export type VariableIconClassKey = keyof VariableIconClasses;
+
+export function getVariableIconUtilityClass(slot: string): string {
+  return generateUtilityClass('MuiVariableIcon', slot);
+}
+
+const variableIconClasses: VariableIconClasses = generateUtilityClasses('MuiVariableIcon', [
+  'root',
+  'colorPrimary',
+  'colorSecondary',
+  'colorAction',
+  'colorError',
+  'colorDisabled',
+  'fontSizeInherit',
+  'fontSizeSmall',
+  'fontSizeMedium',
+  'fontSizeLarge',
+  'fontSizeLarger',
+]);
+
+export default variableIconClasses;
