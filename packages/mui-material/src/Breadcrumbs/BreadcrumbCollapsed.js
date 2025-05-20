7cb2bd493c4cb8755bc5b823aafed2643db83a80
siriwatknp <siriwatkunaporn@gmail.com>
2025-05-20 13:57:53 +0700

    add name to all components

--- packages/mui-material/src/Breadcrumbs/BreadcrumbCollapsed.js
+++ packages/mui-material/src/Breadcrumbs/BreadcrumbCollapsed.js
@@ -7,7 +7,10 @@ import memoTheme from '../utils/memoTheme';
 import MoreHorizIcon from '../internal/svg-icons/MoreHoriz';
 import ButtonBase from '../ButtonBase';
 
-const BreadcrumbCollapsedButton = styled(ButtonBase)(
+const BreadcrumbCollapsedButton = styled(ButtonBase, {
+  name: 'MuiBreadcrumbCollapsed',
+  skipVariantsResolver: true,
+})(
   memoTheme(({ theme }) => ({
     display: 'flex',
     marginLeft: `calc(${theme.spacing(1)} * 0.5)`,
--- packages/mui-styled-engine/src/StyledEngineProvider/StyledEngineProvider.js
+++ packages/mui-styled-engine/src/StyledEngineProvider/StyledEngineProvider.js
@@ -87,7 +87,7 @@ function getCache(injectFirst, enableCssLayer) {
     if (enableCssLayer) {
       const prevInsert = emotionCache.insert;
       emotionCache.insert = (...args) => {
-        if (!args[1].styles.startsWith('@layer') && args[1].styles.includes('{')) {
+        if (!args[1].styles.match(/^@layer\s+[^{]*$/)) {
           // avoid nested @layer
           args[1].styles = `@layer mui {${args[1].styles}}`;
         }
