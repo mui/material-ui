4f334c4a08d81ce76e33747946b3b74c4f06c193
sai6855 <fayazceg@gmail.com>
2025-05-21 18:07:10 +0530

    add div wrapper

--- docs/data/material/components/toggle-button/HorizontalSpacingToggleButton.js
+++ docs/data/material/components/toggle-button/HorizontalSpacingToggleButton.js
@@ -36,24 +36,26 @@ export default function HorizontalSpacingToggleButton() {
   };
 
   return (
-    <StyledToggleButtonGroup
-      value={alignment}
-      exclusive
-      onChange={handleAlignment}
-      aria-label="text alignment"
-    >
-      <ToggleButton value="left" aria-label="left aligned">
-        <FormatAlignLeftIcon />
-      </ToggleButton>
-      <ToggleButton value="center" aria-label="centered">
-        <FormatAlignCenterIcon />
-      </ToggleButton>
-      <ToggleButton value="right" aria-label="right aligned">
-        <FormatAlignRightIcon />
-      </ToggleButton>
-      <ToggleButton value="justify" aria-label="justified" disabled>
-        <FormatAlignJustifyIcon />
-      </ToggleButton>
-    </StyledToggleButtonGroup>
+    <div>
+      <StyledToggleButtonGroup
+        value={alignment}
+        exclusive
+        onChange={handleAlignment}
+        aria-label="text alignment"
+      >
+        <ToggleButton value="left" aria-label="left aligned">
+          <FormatAlignLeftIcon />
+        </ToggleButton>
+        <ToggleButton value="center" aria-label="centered">
+          <FormatAlignCenterIcon />
+        </ToggleButton>
+        <ToggleButton value="right" aria-label="right aligned">
+          <FormatAlignRightIcon />
+        </ToggleButton>
+        <ToggleButton value="justify" aria-label="justified" disabled>
+          <FormatAlignJustifyIcon />
+        </ToggleButton>
+      </StyledToggleButtonGroup>
+    </div>
   );
 }
