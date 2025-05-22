2606b425ee087afad8a84a25fc231e7164df3f51
dav-is <mail@connordav.is>
2025-05-22 09:08:08 -0400

    Fix bug in font loading template

--- packages/mui-symbols-material/templateFontGoogleLoadingComponent.js
+++ packages/mui-symbols-material/templateFontGoogleLoadingComponent.js
@@ -1,6 +1,6 @@
 'use client';
 
 export default function MUISymbols{{{variation}}}GoogleFont({ icons }) {
-  const iconNames = icons.join(',')
+  const iconNames = icons && icons.join(',')
   return <link rel="stylesheet" href={`https://fonts.googleapis.com/css2?family=Material+Symbols+{{theme}}:opsz,wght,FILL,GRAD@20..48,{{weight}},0..1,-25..200${iconNames ? `&icon_names=${iconNames}` : ''}`} />
 }
