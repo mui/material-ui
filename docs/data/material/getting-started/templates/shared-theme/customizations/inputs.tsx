9a635eff9173f10351aaad1077eeb52ea215e151
siriwatknp <siriwatkunaporn@gmail.com>
2025-05-22 12:28:36 +0700

    fix template theme

--- docs/data/material/getting-started/templates/shared-theme/customizations/inputs.tsx
+++ docs/data/material/getting-started/templates/shared-theme/customizations/inputs.tsx
@@ -379,7 +379,8 @@ export const inputsCustomizations: Components<Theme> = {
   MuiOutlinedInput: {
     styleOverrides: {
       input: {
-        padding: 0,
+        paddingBlock: 0,
+        paddingLeft: 0,
       },
       root: ({ theme }) => ({
         padding: '8px 12px',
