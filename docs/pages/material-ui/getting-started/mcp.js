d80cd3dcd510eef6a43a343105a2bc43bb26d8d0
Bharat Kashyap <bharatkashyap@outlook.com>
2025-05-29 17:24:58 +0530

    fix: review, CI

--- docs/pages/material-ui/getting-started/mcp.js
+++ docs/pages/material-ui/getting-started/mcp.js
@@ -3,5 +3,5 @@ import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
 import * as pageProps from 'docs/data/material/getting-started/mcp/mcp.md?muiMarkdown';
 
 export default function Page() {
-  return <MarkdownDocs {...pageProps} disableToc />;
+  return <MarkdownDocs {...pageProps} />;
 }
