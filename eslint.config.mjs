5caf7b4f54a35700e619cc7f96921197f493d1cf
Brijesh Bittu <brijesh42@gmail.com>
2025-06-19 10:09:04 +0530

    [code-infra] Setup flat eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import { includeIgnoreFile } from '@eslint/compat';
+import { createBaseConfig } from '@mui/internal-code-infra/eslint';
+import { defineConfig } from 'eslint/config';
+import * as path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const ENABLE_REACT_COMPILER_PLUGIN = false;
+
+export default defineConfig(
+  includeIgnoreFile(path.join(dirname, '.gitignore')),
+  includeIgnoreFile(path.join(dirname, '.eslintignore')),
+  {
+    name: 'Base ESLint Configuration',
+    extends: createBaseConfig({
+      enableReactCompiler: ENABLE_REACT_COMPILER_PLUGIN,
+    }),
+    settings: {
+      'import/resolver': {
+        webpack: {
+          config: path.join(dirname, './webpackBaseConfig.js'),
+        },
+      },
+    },
+  },
+);
