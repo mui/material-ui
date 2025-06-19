6bf39dc08a1b2d325e324006ffdb425f49664956
Brijesh Bittu <brijesh42@gmail.com>
2025-06-19 10:45:51 +0530

    Linting of test files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import { includeIgnoreFile } from '@eslint/compat';
-import { createBaseConfig } from '@mui/internal-code-infra/eslint';
+import { createBaseConfig, createTestConfig, baseSpecRules } from '@mui/internal-code-infra/eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import * as path from 'node:path';
 import { fileURLToPath } from 'url';
@@ -24,6 +24,46 @@ export default defineConfig(
           config: path.join(dirname, './webpackBaseConfig.js'),
         },
       },
+    },
+  },
+  ...['mui-material', 'mui-system', 'mui-utils', 'mui-lab', 'mui-utils', 'mui-styled-engine'].map(
+    (packageName) => ({
+      files: [`packages/${packageName}/src/**/*.?(c|m)[jt]s?(x)`],
+      ignores: ['*.test.*', '*.spec.*'],
+      rules: {
+        'material-ui/no-restricted-resolved-imports': [
+          'error',
+          [
+            {
+              pattern: `**/packages/${packageName}/src/index.*`,
+              message:
+                "Don't import from the package index. Import the specific module directly instead.",
+            },
+          ],
+        ],
+      },
+    }),
+  ),
+  {
+    files: [
+      // matching the pattern of the test runner
+      '**/*.test.?(c|m)[jt]s?(x)',
+    ],
+    extends: createTestConfig(),
+    rules: {
+      'testing-library/prefer-screen-queries': 'off',
+      'testing-library/no-container': 'off',
+      'testing-library/no-dom-import': 'off',
+      'testing-library/no-node-access': 'off',
+      'testing-library/render-result-naming-convention': 'off',
+      'testing-library/no-await-sync-queries': 'off',
+    },
+  },
+  baseSpecRules,
+  {
+    files: ['**/*.mjs'],
+    rules: {
+      'import/extensions': ['error', 'ignorePackages'],
     },
   },
 );
