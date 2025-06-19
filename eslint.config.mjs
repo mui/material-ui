4151cf6f57922ddb5b5dfee28fadc6bef30bf35e
Brijesh Bittu <brijesh42@gmail.com>
2025-06-19 11:00:33 +0530

    Docs setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { includeIgnoreFile } from '@eslint/compat';
 import { createBaseConfig, createTestConfig, baseSpecRules } from '@mui/internal-code-infra/eslint';
+import { createDocsConfig } from '@mui/internal-code-infra/eslint-docs';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import * as path from 'node:path';
 import { fileURLToPath } from 'url';
@@ -7,7 +8,40 @@ import { fileURLToPath } from 'url';
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
+const OneLevelImportMessage = [
+  'Prefer one level nested imports to avoid bundling everything in dev mode or breaking CJS/ESM split.',
+  'See https://github.com/mui/material-ui/pull/24147 for the kind of win it can unlock.',
+].join('\n');
+
 const ENABLE_REACT_COMPILER_PLUGIN = false;
+
+const NO_RESTRICTED_IMPORTS_PATHS_TOP_LEVEL_PACKAGES = [
+  {
+    name: '@mui/material',
+    message: OneLevelImportMessage,
+  },
+  {
+    name: '@mui/lab',
+    message: OneLevelImportMessage,
+  },
+];
+
+const NO_RESTRICTED_IMPORTS_PATTERNS_DEEPLY_NESTED = [
+  {
+    group: [
+      '@mui/*/*/*',
+      '@pigment-css/*/*/*',
+      '@base-ui/*/*/*',
+      // Allow any import depth with any internal packages
+      '!@mui/internal-*/**',
+      // TODO delete
+      '@base-ui-components/*/*/*', // Wait for migration to @base-ui/
+      '@base_ui/*/*/*', // Legacy, moved to @base-ui-components/
+      '!@mui/docs/**', // @mui/docs should be @mui/internal-docs
+    ],
+    message: OneLevelImportMessage,
+  },
+];
 
 export default defineConfig(
   includeIgnoreFile(path.join(dirname, '.gitignore')),
@@ -44,6 +78,32 @@ export default defineConfig(
       },
     }),
   ),
+  // Docs start
+  {
+    files: ['docs/**/*'],
+    extends: createDocsConfig(),
+    rules: {
+      '@next/next/no-img-element': 'error',
+      'no-restricted-imports': [
+        'error',
+        {
+          paths: NO_RESTRICTED_IMPORTS_PATHS_TOP_LEVEL_PACKAGES,
+          patterns: NO_RESTRICTED_IMPORTS_PATTERNS_DEEPLY_NESTED,
+        },
+      ],
+    },
+  },
+  {
+    files: ['docs/src/modules/components/**/*'],
+    rules: {
+      'material-ui/no-hardcoded-labels': [
+        'error',
+        { allow: ['MUI', 'X', 'GitHub', 'Stack Overflow'] },
+      ],
+    },
+  },
+  // Docs end
+  // Test start
   {
     files: [
       // matching the pattern of the test runner
