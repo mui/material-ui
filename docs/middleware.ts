6837cdfc76f135ff45912ecaab63f7c0377573ba
howlettt <1786243+howlettt@users.noreply.github.com>
2025-06-19 21:23:27 -0600

    Fix middleware

--- docs/middleware.ts
+++ docs/middleware.ts
@@ -4,10 +4,13 @@ import { NextResponse, NextRequest } from 'next/server';
  * Hide experiments in production
  */
 export function middleware(request: NextRequest) {
-  if (
-    request.nextUrl.pathname.startsWith('/experiments') &&
-    process.env.NODE_ENV === 'production'
-  ) {
+  if (process.env.NODE_ENV === 'production') {
     return NextResponse.rewrite(new URL('/404', request.url));
   }
+
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: '/experiments/:path*',
+};
