4251393bcb4b305a564a7c07df5bdb75c86ddeb9
howlettt <1786243+howlettt@users.noreply.github.com>
2025-06-19 18:35:16 -0600

    Fix file case

--- docs/app/not-found.tsx
+++ docs/app/not-found.tsx
@@ -1,6 +1,6 @@
 import createMetadata, { GenerateMetadataProps } from 'docs/src/modules/utils/createMetadata';
 import { Metadata } from 'next';
-import Custom404 from './Custom404';
+import Custom404 from './custom404';
 
 export function generateMetadata(props: GenerateMetadataProps): Promise<Metadata> {
   return createMetadata(props, {
