16b1638f9f9b0dd43b5d9379fe4bfc3f9446aa92
siriwatknp <siriwatkunaporn@gmail.com>
2025-06-23 15:10:59 +0700

    remove unnecessary scripts

--- examples/material-ui-react-router-ts/vite.config.ts
+++ examples/material-ui-react-router-ts/vite.config.ts
@@ -6,9 +6,12 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   ssr: {
+    optimizeDeps: {
+      include: ['@emotion/react', '@emotion/styled', '@emotion/cache'],
+    },
     // Workaround for resolving dependencies in the server bundle
     // Without this, the React context will be different between direct import and transitive imports in development environment
     // For more information, see https://github.com/mui/material-ui/issues/45878#issuecomment-2987441663
-    noExternal: ['@emotion/*', '@mui/*'],
+    noExternal: ['@emotion/*'],
   },
 });
