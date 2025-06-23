0efd2528406ab10450cfa73fd440ccc098750f1c
siriwatknp <siriwatkunaporn@gmail.com>
2025-06-23 16:51:50 +0700

    remove outdated comment

--- examples/material-ui-react-router-ts/app/createCache.ts
+++ examples/material-ui-react-router-ts/app/createCache.ts
@@ -1,8 +1,5 @@
 import createCache from '@emotion/cache';
 
-// On the client side, Create a meta tag at the top of the <head> and set it as insertionPoint.
-// This assures that MUI styles are loaded first.
-// It allows developers to easily override MUI styles with other styling solutions, like CSS modules.
 export default function createEmotionCache(options?: Parameters<typeof createCache>[0]) {
   const emotionCache = createCache({ key: 'mui', ...options });
   const prevInsert = emotionCache.insert;
