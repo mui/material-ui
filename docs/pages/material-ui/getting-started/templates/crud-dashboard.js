869598068967dc6038a85b7ba64a5a68ac58e780
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2025-06-25 02:08:26 +0100

    Adjust imports

--- docs/pages/material-ui/getting-started/templates/crud-dashboard.js
+++ docs/pages/material-ui/getting-started/templates/crud-dashboard.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import dynamic from 'next/dynamic';
+import { NoSsr } from '@mui/material/NoSsr';
 import AppTheme from 'docs/src/modules/components/AppTheme';
 import TemplateFrame from 'docs/src/modules/components/TemplateFrame';
-import { NoSsr } from '@mui/material/NoSsr';
 
 const CrudDashboard = dynamic(
   () => import('docs/data/material/getting-started/templates/crud-dashboard/CrudDashboard'),
