fb0feea32a365f63a4e2da345069dbcb04a709dc
Pedro Ferreira <10789765+apedroferreira@users.noreply.github.com>
2025-06-25 02:49:04 +0100

    unused import

--- docs/pages/material-ui/getting-started/templates/crud-dashboard.js
+++ docs/pages/material-ui/getting-started/templates/crud-dashboard.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import dynamic from 'next/dynamic';
 import AppTheme from 'docs/src/modules/components/AppTheme';
 import TemplateFrame from 'docs/src/modules/components/TemplateFrame';
-import { NoSsr } from '@mui/material/NoSsr';
 
 const CrudDashboard = dynamic(
   () => import('../../../../data/material/getting-started/templates/crud-dashboard/CrudDashboard'),
