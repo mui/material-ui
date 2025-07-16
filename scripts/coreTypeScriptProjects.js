0a1306a87a0e73f3ca58a40c982450855e172199
Janpot <2109932+Janpot@users.noreply.github.com>
2025-07-16 15:41:33 +0200

    Update coreTypeScriptProjects.js

--- scripts/coreTypeScriptProjects.js
+++ scripts/coreTypeScriptProjects.js
@@ -3,18 +3,22 @@ import path from 'path';
 export default {
   material: {
     rootPath: path.join(process.cwd(), 'packages/mui-material'),
+    tsConfigPath: 'tsconfig.json',
     entryPointPath: 'src/index.d.ts',
   },
   lab: {
     rootPath: path.join(process.cwd(), 'packages/mui-lab'),
+    tsConfigPath: 'tsconfig.json',
     entryPointPath: 'src/index.d.ts',
   },
   joy: {
     rootPath: path.join(process.cwd(), 'packages/mui-joy'),
+    tsConfigPath: 'tsconfig.json',
     entryPointPath: 'src/index.ts',
   },
   system: {
     rootPath: path.join(process.cwd(), 'packages/mui-system'),
+    tsConfigPath: 'tsconfig.json',
     entryPointPath: 'src/index.d.ts',
   },
   docs: {
