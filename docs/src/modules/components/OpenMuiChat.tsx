91f2f37a1f489a0e264b50a63039997e7b6a57bd
siriwatknp <siriwatkunaporn@gmail.com>
2025-07-21 15:27:32 +0700

    update styles

--- docs/src/modules/components/OpenMuiChat.tsx
+++ docs/src/modules/components/OpenMuiChat.tsx
@@ -5,7 +5,6 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
 import PageContext from 'docs/src/modules/components/PageContext';
-import SvgMuiLogomark from 'docs/src/icons/SvgMuiLogomark';
 import { createMuiChat } from '../sandbox/MuiChat';
 import { DemoData } from '../sandbox/types';
 
@@ -23,45 +22,48 @@ const rainbow = keyframes`
 `;
 
 const RainbowButton = styled(Button)(({ theme }) => ({
-  '--color-1': '0 100% 63%',
-  '--color-2': '270 100% 63%',
-  '--color-3': '210 100% 63%',
-  '--color-4': '195 100% 63%',
-  '--color-5': '90 100% 63%',
+  '--color-1': '0 100% 50%',
+  '--color-2': '270 100% 50%',
+  '--color-3': '210 100% 50%',
+  '--color-4': '195 100% 50%',
+  '--color-5': '90 100% 50%',
   position: 'relative',
   display: 'inline-flex',
   height: 26,
   padding: '7px 8px 8px 8px', // 7px for optical alignment
   flexShrink: 0,
-  borderRadius: '6px',
+  borderRadius: 999,
   border: '1px solid transparent',
-  borderBottomWidth: '3px',
-  borderBottomColor: 'transparent',
-  boxShadow: '0 -1px 4px 0px rgba(255, 255, 255, 0.32)',
+  borderBottomWidth: '2.5px',
+  borderColor: alpha(theme.palette.grey[200], 0.8),
   '&.MuiButton-loading': {
-    boxShadow: '0 -1px 4px 0px rgba(255, 255, 255, 0.32)',
+    '& > svg': {
+      transform: 'translateX(36px)',
+    },
   },
-  color: '#fff',
   fontSize: theme.typography.pxToRem(13),
   fontWeight: theme.typography.fontWeightMedium,
   backgroundSize: '200%',
   backgroundClip: 'padding-box, border-box, border-box',
   backgroundOrigin: 'border-box',
   animation: `${rainbow} 2s linear infinite`,
-  '--bg-color-raw': '16, 18, 20',
+  '--bg-color-raw': '255,255,255',
   '--bg-color': 'rgb(var(--bg-color-raw))',
-  backgroundImage: `linear-gradient(var(--bg-color), var(--bg-color)), linear-gradient(var(--bg-color) 50%, rgba(var(--bg-color-raw), 0.6) 80%, rgba(var(--bg-color-raw), 0)), linear-gradient(90deg, hsl(var(--color-1)), hsl(var(--color-5)), hsl(var(--color-3)), hsl(var(--color-4)), hsl(var(--color-2)))`,
-  ...theme.applyDarkStyles({
-    borderColor: alpha(theme.palette.primary[300], 0.2),
-  }),
+  backgroundImage: `linear-gradient(var(--bg-color), var(--bg-color)), linear-gradient(var(--bg-color) 50%, rgba(var(--bg-color-raw)) 80%, rgba(var(--bg-color-raw), 0)), linear-gradient(90deg, hsl(var(--color-1)), hsl(var(--color-5)), hsl(var(--color-3)), hsl(var(--color-4)), hsl(var(--color-2)))`,
   '&:hover': {
-    boxShadow: '0 -1px 4px 0px rgba(255, 255, 255, 0.56)',
-    animationPlayState: 'paused',
+    '--bg-color-raw': '235,245,255',
   },
+  ...theme.applyDarkStyles({
+    '--bg-color-raw': '16, 18, 20',
+    borderColor: alpha(theme.palette.primary[300], 0.2),
+    '&:hover': {
+      '--bg-color-raw': '13, 38, 63',
+    },
+  }),
   '&::before': {
     content: '""',
     position: 'absolute',
-    bottom: '-20%',
+    bottom: '-50%',
     left: '50%',
     zIndex: 0,
     height: '20%',
@@ -73,12 +75,12 @@ const RainbowButton = styled(Button)(({ theme }) => ({
     filter: 'blur(0.8rem)',
   },
   '& > svg': {
-    height: 12,
-    width: 12,
-    margin: '1px 4px 0 4px',
-  },
-  '& > svg > path': {
+    height: 18,
+    width: 18,
+    margin: '0 4px 0 0',
+    stroke: (theme.vars || theme).palette.primary.main,
     fill: (theme.vars || theme).palette.primary.main,
+    transition: 'transform 0.4s',
   },
 }));
 
@@ -119,7 +121,20 @@ const OpenInMUIChatButton = React.forwardRef<HTMLButtonElement, OpenInMUIChatBut
           onClick={handleClick}
           {...props}
         >
-          Edit in <SvgMuiLogomark /> Chat
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+            />
+          </svg>{' '}
+          Edit in Chat
         </RainbowButton>
         <Snackbar
           open={!!error}
