79a55fa4617e700e126ed8fdbe3545b3b93ab5c7
Janpot <2109932+Janpot@users.noreply.github.com>
2025-08-18 16:55:00 +0200

    Update dangerFileContent.ts

--- scripts/dangerFileContent.ts
+++ scripts/dangerFileContent.ts
@@ -1,6 +1,5 @@
 import type * as dangerModule from 'danger';
 import replaceUrl from '@mui-internal/api-docs-builder/utils/replaceUrl';
-import { renderMarkdownReport } from '@mui/internal-bundle-size-checker';
 
 declare const danger: (typeof dangerModule)['danger'];
 declare const markdown: (typeof dangerModule)['markdown'];
@@ -28,6 +27,7 @@ async function reportBundleSize() {
 
   const circleciBuildNumber = process.env.CIRCLE_BUILD_NUM;
 
+  const { renderMarkdownReport } = await import('@mui/internal-bundle-size-checker');
   markdownContent += await renderMarkdownReport(danger.github.pr, circleciBuildNumber, {
     track: ['@mui/material', '@mui/lab', '@mui/system', '@mui/utils'],
   });
