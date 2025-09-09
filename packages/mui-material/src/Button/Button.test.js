ed197a4932eb2d0439f2b64ef750a302a1fe2566
Janpot <2109932+Janpot@users.noreply.github.com>
2025-09-09 15:34:48 +0200

    Update Button.test.js

--- packages/mui-material/src/Button/Button.test.js
+++ packages/mui-material/src/Button/Button.test.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { expect } from 'chai';
 import { createRenderer, screen, simulateKeyboardDevice, within } from '@mui/internal-test-utils';
+import describeSkipIf from '@mui/internal-test-utils/describeSkipIf';
 import { ClassNames } from '@emotion/react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Button, { buttonClasses as classes } from '@mui/material/Button';
