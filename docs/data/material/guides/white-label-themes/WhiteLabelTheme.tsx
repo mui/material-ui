8a07b76e54a488f72f0788c11b92a18fdf607300
siriwatknp <siriwatkunaporn@gmail.com>
2025-09-09 16:52:15 +0700

    add white label themes page

---
+++ docs/data/material/guides/white-label-themes/WhiteLabelTheme.tsx
@@ -0,0 +1,161 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { ThemeProvider, ThemeOptions, createTheme } from '@mui/material/styles';
+
+// TODO: move this to a separate file
+const baseTokens: ThemeOptions = {
+  palette: {
+    primary: {
+      main: '#000000',
+    },
+    secondary: {
+      main: 'rgb(229, 229, 234)',
+    },
+  },
+  shape: {
+    borderRadius: 4,
+  },
+  typography: {
+    fontFamily:
+      'var(--font-primary, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif)',
+  },
+  shadows: [
+    'none',
+    '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+    '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+    '0 2px 4px 0 rgb(0 0 0 / 0.06)',
+    '0 2px 4px -1px rgb(0 0 0 / 0.06), 0 1px 2px -1px rgb(0 0 0 / 0.04)',
+    '0 3px 5px -1px rgb(0 0 0 / 0.07), 0 1px 3px -1px rgb(0 0 0 / 0.05)',
+    '0 4px 6px -1px rgb(0 0 0 / 0.07), 0 2px 4px -1px rgb(0 0 0 / 0.05)',
+    '0 5px 8px -2px rgb(0 0 0 / 0.08), 0 2px 4px -1px rgb(0 0 0 / 0.05)',
+    '0 6px 10px -2px rgb(0 0 0 / 0.08), 0 3px 5px -2px rgb(0 0 0 / 0.06)',
+    '0 8px 12px -3px rgb(0 0 0 / 0.09), 0 3px 6px -2px rgb(0 0 0 / 0.06)',
+    '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 7px -3px rgb(0 0 0 / 0.07)',
+    '0 12px 18px -4px rgb(0 0 0 / 0.11), 0 5px 9px -3px rgb(0 0 0 / 0.08)',
+    '0 15px 22px -4px rgb(0 0 0 / 0.12), 0 6px 11px -4px rgb(0 0 0 / 0.09)',
+    '0 18px 28px -5px rgb(0 0 0 / 0.13), 0 7px 13px -4px rgb(0 0 0 / 0.1)',
+    '0 22px 34px -6px rgb(0 0 0 / 0.14), 0 8px 16px -5px rgb(0 0 0 / 0.11)',
+    '0 26px 40px -7px rgb(0 0 0 / 0.15), 0 10px 19px -5px rgb(0 0 0 / 0.12)',
+    '0 31px 47px -8px rgb(0 0 0 / 0.16), 0 12px 23px -6px rgb(0 0 0 / 0.13)',
+    '0 36px 54px -9px rgb(0 0 0 / 0.17), 0 14px 27px -7px rgb(0 0 0 / 0.14)',
+    '0 42px 62px -10px rgb(0 0 0 / 0.18), 0 16px 31px -8px rgb(0 0 0 / 0.15)',
+    '0 48px 70px -11px rgb(0 0 0 / 0.2), 0 18px 36px -9px rgb(0 0 0 / 0.16)',
+    '0 54px 78px -12px rgb(0 0 0 / 0.21), 0 20px 41px -10px rgb(0 0 0 / 0.17)',
+    '0 60px 86px -13px rgb(0 0 0 / 0.22), 0 23px 46px -11px rgb(0 0 0 / 0.18)',
+    '0 66px 94px -14px rgb(0 0 0 / 0.23), 0 26px 52px -12px rgb(0 0 0 / 0.19)',
+    '0 72px 102px -15px rgb(0 0 0 / 0.24), 0 29px 58px -13px rgb(0 0 0 / 0.2)',
+    '0 58px 82px -11px rgb(0 0 0 / 0.26), 0 21px 40px -11px rgb(0 0 0 / 0.22)',
+  ],
+};
+
+const baseComponents: ThemeOptions['components'] = {
+  MuiButton: {
+    defaultProps: {
+      disableElevation: true,
+    },
+    styleOverrides: {
+      root: ({ theme }) => ({
+        minWidth: 'unset',
+        textTransform: 'capitalize',
+        fontSize: '1rem',
+        '&:hover': {
+          textDecoration: 'underline',
+        },
+        [theme.breakpoints.up('md')]: {
+          fontSize: '0.875rem',
+        },
+      }),
+    },
+  },
+};
+
+const baseTheme = createTheme({
+  ...baseTokens,
+  components: baseComponents,
+});
+
+/**
+ * Application theme
+ */
+
+const appTheme = createTheme({
+  ...baseTokens,
+  palette: {
+    ...baseTokens.palette,
+    primary: {
+      main: '#1976d2',
+    },
+  },
+  components: {
+    ...baseComponents,
+    MuiButton: {
+      styleOverrides: {
+        root: [
+          baseComponents?.MuiButton?.styleOverrides?.root,
+          {
+            transition: 'transform 0.2s ease-in-out',
+            '&:hover': {
+              transform: 'translateY(-2px)',
+            },
+          },
+        ],
+      },
+    },
+  },
+});
+
+function App1() {
+  return (
+    <ThemeProvider theme={appTheme}>
+      <Button>App Button</Button>
+    </ThemeProvider>
+  );
+}
+
+const appTheme2 = createTheme({
+  ...baseTokens,
+  palette: {
+    ...baseTokens.palette,
+    primary: {
+      main: '#ffa726',
+    },
+  },
+  components: {
+    ...baseComponents,
+    MuiButton: {
+      defaultProps: {
+        ...baseComponents?.MuiButton?.defaultProps,
+        variant: 'outlined',
+      },
+      styleOverrides: {
+        root: [
+          baseComponents?.MuiButton?.styleOverrides?.root,
+          ({ theme }) => ({
+            color: theme.palette.primary.dark,
+          }),
+        ],
+      },
+    },
+  },
+});
+
+function App2() {
+  return (
+    <ThemeProvider theme={appTheme2}>
+      <Button>App 2 Button</Button>
+    </ThemeProvider>
+  );
+}
+
+export default function WhiteLabelTheme() {
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+      <ThemeProvider theme={baseTheme}>
+        <Button>Base Button</Button>
+      </ThemeProvider>
+      <App1 />
+      <App2 />
+    </Box>
+  );
+}
---
+++ docs/data/material/guides/white-label-themes/white-label-themes.md
@@ -0,0 +1,140 @@
+# White-label themes
+
+<p class="description">Learn how to build white-label themes with Material UI.</p>
+
+## Introduction
+
+The white-label theme or in short, base theme, serve as the foundation for a brand-specific theme. The base theme is then used to build mutiple applications with or without minor tweaks.
+
+This guide focus on the theming recommendations for building a white-label theme with Material UI.
+
+## Base theme
+
+This is the source of truth for the brand-specific theme. It represents the brand's visual identity like colors, typography, spacing, shape, etc.
+
+In general, it's recomended to export tokens, components, and the the base theme from a file.
+
+```js title="baseTheme.ts"
+import type { ThemeOptions } from '@mui/material/styles';
+
+export const baseTokens: ThemeOptions = {
+  palette: {
+    primary: {
+      main: '#000000',
+    },
+    secondary: {
+      main: 'rgb(229, 229, 234)',
+    },
+  },
+  shape: {
+    borderRadius: 4,
+  },
+  typography: {
+    fontFamily:
+      'var(--font-primary, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif)',
+  },
+};
+
+export const baseComponents: ThemeOptions['components'] = {
+  MuiButton: {
+    defaultProps: {
+      disableElevation: true,
+    },
+    styleOverrides: {
+      root: {
+        minWidth: 'unset',
+        textTransform: 'capitalize',
+        '&:hover': {
+          textDecoration: 'underline',
+        },
+      },
+    },
+  },
+};
+
+export default createTheme({
+  ...baseTokens,
+  components: baseComponents,
+});
+```
+
+For a more optimized approach, the base components can be split into multiple files and let the application level import only the components that are needed.
+
+```js title="baseButtons.ts"
+import type { ThemeOptions } from "@mui/material/styles";
+
+export const buttonTheme: ThemeOptions["components"] = {
+  MuiButtonBase: {},
+  MuiButton: {},
+  MuiIconButton: {},
+};
+```
+
+```js title="baseTheme.ts"
+import { buttonTheme } from './baseButtons';
+// import other base components as needed
+
+export const baseTokens: ThemeOptions = {}
+
+export default createTheme({
+  ...baseTokens,
+  components: {
+    ...buttonTheme,
+    // other base components
+  },
+});
+```
+
+## Application theme
+
+The application may use the base theme directly or extend it with some tweaks.
+
+For example, to customize the hover styles of the button, do the following:
+
+```js title="appTheme.ts"
+import { createTheme } from '@mui/material/styles';
+import { baseTokens, baseComponents } from './baseTheme'; // or from an npm package.
+
+const appTheme = createTheme({
+  ...baseTokens,
+  palette: {
+    ...baseTokens.palette,
+    primary: {
+      main: '#1976d2',
+    },
+  },
+  components: {
+    ...baseComponents,
+    MuiButton: {
+      styleOverrides: {
+        root: [
+          baseComponents?.MuiButton?.styleOverrides?.root,
+          {
+            '&:hover': {
+              transform: 'translateY(-2px)',
+            },
+          },
+        ],
+      },
+    },
+  },
+});
+```
+
+:::success
+It's recommended to use array syntax to merge the base styles with the application styles.
+
+The array syntax ensures that the states and pseudo-classes styles from the base theme are preserved.
+:::
+
+:::warning
+We don't recommend JavaScript functions or any utilities to do deep merge between the base and the application theme.
+
+Doing so will introduce performance overhead on the first render of the application. The impact depends on the size of the themes.
+
+Instead, use the object spread and array syntax to merge the themes as shown above.
+:::
+
+## Full example
+
+{{"demo": "WhiteLabelTheme.js", "defaultCodeOpen": true}}
