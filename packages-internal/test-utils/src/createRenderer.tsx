290e2a39de716f388ea3dcc344c9a56b00b858c4
Janpot <2109932+Janpot@users.noreply.github.com>
2025-09-10 14:46:46 +0200

    Update createRenderer.tsx

--- packages-internal/test-utils/src/createRenderer.tsx
+++ packages-internal/test-utils/src/createRenderer.tsx
@@ -842,6 +842,6 @@ function act<T>(callback: () => void | T | Promise<T>) {
 
 const bodyBoundQueries = within(document.body, { ...queries, ...customQueries });
 
-export * from '@testing-library/react/pure';
+export { renderHook, waitFor, within } from '@testing-library/react/pure';
 export { act, fireEvent };
 export const screen: Screen & typeof bodyBoundQueries = { ...rtlScreen, ...bodyBoundQueries };
