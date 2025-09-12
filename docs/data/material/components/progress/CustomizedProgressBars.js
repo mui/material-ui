c0fe6486ddcff5307dbcac32d444cce6b5e905f7
siriwatknp <siriwatkunaporn@gmail.com>
2025-09-12 12:41:06 +0700

    fix lint

--- docs/data/material/components/progress/CustomizedProgressBars.js
+++ docs/data/material/components/progress/CustomizedProgressBars.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-
 import Stack from '@mui/material/Stack';
 import CircularProgress, {
   circularProgressClasses,
--- docs/data/material/components/progress/CustomizedProgressBars.tsx
+++ docs/data/material/components/progress/CustomizedProgressBars.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import CircularProgress, {
   circularProgressClasses,
