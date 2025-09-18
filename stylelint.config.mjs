9b6ae60a7f6711379e0ed36a32d0f4e83bedf401
Janpot <2109932+Janpot@users.noreply.github.com>
2025-09-18 17:10:04 +0200

    [code-infra] Centralize stylelint

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,19 +1,6 @@
 export default {
-  extends: 'stylelint-config-standard',
-  rules: {
-    'alpha-value-notation': null,
-    'custom-property-pattern': null,
-    'media-feature-range-notation': null,
-    'no-empty-source': null,
-    'selector-class-pattern': null,
-    'string-no-newline': null, // not compatible with prettier
-    'value-keyword-case': null,
-  },
+  extends: '@mui/internal-code-infra/stylelint',
   overrides: [
-    {
-      files: ['**/*.js', '**/*.cjs', '**/*.mjs', '**/*.mts', '**/*.ts', '**/*.tsx'],
-      customSyntax: 'postcss-styled-syntax',
-    },
     {
       files: [
         'docs/data/material/integrations/interoperability/**/*.js',
