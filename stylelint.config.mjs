0cd4a463a9290413538d7686daac72051eac889e
Janpot <2109932+Janpot@users.noreply.github.com>
2025-09-22 14:48:21 +0200

    update

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,5 +1,11 @@
+import base from '@mui/internal-code-infra/stylelint';
+
+// Note: To debug stylelint config resolution for a specific file, use
+//         pnpm exec stylelint --print-config <path-to-file>
+
+/** @type {import('stylelint').Config} */
 export default {
-  extends: '@mui/internal-code-infra/stylelint',
+  extends: base,
   overrides: [
     {
       files: [
