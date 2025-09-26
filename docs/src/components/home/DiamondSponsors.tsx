7263e57d2a3fe3ee36240133392e21951862e7b0
Andrew <andrew@mui.com>
2025-09-26 12:41:41 +0200

    add srcDark support for SponsorCard

--- docs/src/components/home/DiamondSponsors.tsx
+++ docs/src/components/home/DiamondSponsors.tsx
@@ -17,6 +17,7 @@ const DIAMONDs = [
   },
   {
     src: '/static/sponsors/kombai-square-light.svg',
+    srcDark: '/static/sponsors/kombai-square-dark.svg',
     name: 'Kombai',
     description: 'AI agent for Frontend - turn Figma, text, or images into clean MUI code.',
     href: 'https://kombai.com/?utm_source=MUI&utm_medium=website&utm_campaign=sponsor&utm_content=homepage',
