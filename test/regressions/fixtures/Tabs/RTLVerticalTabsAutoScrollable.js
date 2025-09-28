feafd74a149783ba3e436c6003331b0e146ac1ae
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2025-09-28 14:21:13 +0530

    prettier

--- test/regressions/fixtures/Tabs/RTLVerticalTabsAutoScrollable.js
+++ test/regressions/fixtures/Tabs/RTLVerticalTabsAutoScrollable.js
@@ -22,13 +22,13 @@ export default function RTLVerticalTabs() {
         <ThemeProvider theme={createTheme({ direction: 'rtl' })}>
           <Box dir="rtl" sx={{ height: 200, display: 'flex' }}>
             <Tabs value={2} variant="scrollable" scrollButtons="auto" orientation="vertical">
-            <Tab label="Tab A" />
-            <Tab label="Tab B" />
-            <Tab label="Tab C" />
-            <Tab label="Tab D" />
-            <Tab label="Tab E" />
-            <Tab label="Tab F" />
-            <Tab label="Tab G" />
+              <Tab label="Tab A" />
+              <Tab label="Tab B" />
+              <Tab label="Tab C" />
+              <Tab label="Tab D" />
+              <Tab label="Tab E" />
+              <Tab label="Tab F" />
+              <Tab label="Tab G" />
             </Tabs>
           </Box>
         </ThemeProvider>
