dd57ab76a75f10bfe1d228b80119c03e6643a52a
Awais Shahzad <142084070+CODEKRUNCH@users.noreply.github.com>
2025-10-18 06:21:10 -0700

    [docs] Replace deprecated `inputProps` with `slotProps.input` in checkbox demos (#47083)
    Co-authored-by: ZeeshanTamboli <zeeshan.tamboli@gmail.com>

--- docs/data/material/components/checkboxes/ColorCheckboxes.tsx
+++ docs/data/material/components/checkboxes/ColorCheckboxes.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { pink } from '@mui/material/colors';
 import Checkbox from '@mui/material/Checkbox';
 
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+const label = { slotProps: { input: { 'aria-label': 'Checkbox demo' } } };
 
 export default function ColorCheckboxes() {
   return (
