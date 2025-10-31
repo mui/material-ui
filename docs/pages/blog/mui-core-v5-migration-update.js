8367f92f8952de9ce6d9105fb4212238dce584b0
Janpot <2109932+Janpot@users.noreply.github.com>
2025-10-31 14:22:28 +0100

    add blog pages

--- docs/pages/blog/mui-core-v5-migration-update.js
+++ docs/pages/blog/mui-core-v5-migration-update.js
@@ -1,6 +1,11 @@
 import TopLayoutBlog from 'docs/src/modules/components/TopLayoutBlog';
+import { withTranslations } from 'docs/src/modules/utils/withTranslations';
 import { docs } from './mui-core-v5-migration-update.md?muiMarkdown';
 
 export default function Page() {
   return <TopLayoutBlog docs={docs} />;
+}
+
+export async function getStaticProps() {
+  return { props: withTranslations({}) };
 }
--- docs/pages/blog/mui-x-v8-alpha-zero.js
+++ docs/pages/blog/mui-x-v8-alpha-zero.js
@@ -1,6 +1,11 @@
 import TopLayoutBlog from 'docs/src/modules/components/TopLayoutBlog';
+import { withTranslations } from 'docs/src/modules/utils/withTranslations';
 import { docs } from './mui-x-v8-alpha-zero.md?muiMarkdown';
 
 export default function Page() {
   return <TopLayoutBlog docs={docs} />;
+}
+
+export async function getStaticProps() {
+  return { props: withTranslations({}) };
 }
