a03be91e0b75c1057bb4bb7fbe5a48b59a73bb1c
Brice Frisco <bricemfrisco@gmail.com>
2025-12-10 10:03:43 -0600

    set AutocompletePaper overflow to hidden

--- packages/mui-material/src/Autocomplete/Autocomplete.js
+++ packages/mui-material/src/Autocomplete/Autocomplete.js
@@ -294,7 +294,12 @@ const AutocompletePopper = styled(Popper, {
 const AutocompletePaper = styled(Paper, {
   name: 'MuiAutocomplete',
   slot: 'Paper',
-})(memoTheme(({ theme }) => theme.typography.body1));
+})(
+  memoTheme(({ theme }) => ({
+    ...theme.typography.body1,
+    overflow: 'hidden',
+  })),
+);
 
 const AutocompleteLoading = styled('div', {
   name: 'MuiAutocomplete',
