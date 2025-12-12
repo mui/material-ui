f874428a3212d40464c220c31ea05a4bc2a64e40
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2025-12-12 11:15:48 +0530

    update material-ui-nextjs-ts example

--- examples/material-ui-nextjs-ts/src/app/page.tsx
+++ examples/material-ui-nextjs-ts/src/app/page.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Link from '@/components/Link';
+import MaterialUILink from '@mui/material/Link';
+import NextLink from '@/components/Link';
 import ProTip from '@/components/ProTip';
 import Copyright from '@/components/Copyright';
 
@@ -22,9 +22,9 @@ export default function Home() {
         <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
           Material UI - Next.js example in TypeScript
         </Typography>
-        <Button component={Link} href="/about" color="secondary">
+        <MaterialUILink component={NextLink} href="/about" color="secondary">
           Go to the about page
-        </Button>
+        </MaterialUILink>
         <ProTip />
         <Copyright />
       </Box>
