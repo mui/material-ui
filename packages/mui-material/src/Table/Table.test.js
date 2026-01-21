712cb9610d6f012f6042073b75acd20e785c69e5
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2026-01-21 16:02:05 +0530

    Move to integration file

--- packages/mui-material/src/Table/Table.test.js
+++ packages/mui-material/src/Table/Table.test.js
@@ -1,11 +1,6 @@
 import { expect } from 'chai';
 import { createRenderer, screen } from '@mui/internal-test-utils';
 import Table, { tableClasses as classes } from '@mui/material/Table';
-import TableHead from '@mui/material/TableHead';
-import TableBody from '@mui/material/TableBody';
-import TableRow from '@mui/material/TableRow';
-import TableFooter from '@mui/material/TableFooter';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import describeConformance from '../../test/describeConformance';
 
 describe('<Table />', () => {
@@ -48,39 +43,5 @@ describe('<Table />', () => {
     );
 
     expect(screen.getByTestId('children')).not.to.equal(null);
-  });
-
-  it('should apply Table section and layout context to TableCell', () => {
-    render(
-      <Table padding="none" size="small" stickyHeader>
-        <TableHead>
-          <TableRow>
-            <TableCell data-testid="head">Head</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell data-testid="cell">Cell</TableCell>
-          </TableRow>
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell data-testid="footer">Footer</TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>,
-    );
-
-    expect(screen.getByTestId('head')).to.have.class(tableCellClasses.stickyHeader);
-    expect(screen.getByTestId('head')).to.have.class(tableCellClasses.paddingNone);
-    expect(screen.getByTestId('head')).to.have.class(tableCellClasses.sizeSmall);
-
-    expect(screen.getByTestId('cell')).to.have.class(tableCellClasses.paddingNone);
-    expect(screen.getByTestId('cell')).to.have.class(tableCellClasses.sizeSmall);
-    expect(screen.getByTestId('cell')).not.to.have.class(tableCellClasses.stickyHeader);
-
-    expect(screen.getByTestId('footer')).to.have.class(tableCellClasses.footer);
-    expect(screen.getByTestId('footer')).not.to.have.class(tableCellClasses.head);
-    expect(screen.getByTestId('footer')).not.to.have.class(tableCellClasses.body);
   });
 });
