a6cc68a1a4ee83829f35ad4f9f755359af19313c
Janpot <2109932+Janpot@users.noreply.github.com>
2026-01-26 14:00:30 +0100

    fix lint

--- packages-internal/docs-utils/src/index.ts
+++ packages-internal/docs-utils/src/index.ts
@@ -9,6 +9,7 @@ export function getLineFeed(source: string): string {
   return match === null ? EOL : match[0];
 }
 
+// eslint-disable-next-line compat/compat
 const fixBabelIssuesRegExp = /(?<=(\/>)|,)(\r?\n){2}/g;
 
 export function fixBabelGeneratorIssues(source: string): string {
