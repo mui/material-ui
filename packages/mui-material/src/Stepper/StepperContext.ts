c22b2c2aa365269a2f159495528fac3437ec31a6
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2026-01-27 09:36:28 +0200

    implement keyboard navigation

--- packages/mui-material/src/Stepper/StepperContext.ts
+++ packages/mui-material/src/Stepper/StepperContext.ts
@@ -8,13 +8,21 @@ export interface StepperContextType {
   nonLinear: boolean;
   orientation: 'horizontal' | 'vertical';
   totalSteps: number;
+  focusableStep: number;
+  registerStep: (
+    stepIndex: number,
+    stepRef: React.RefObject<HTMLElement>,
+    disabled: boolean,
+  ) => void;
+  onStepClick: (stepIndex: number) => void;
+  onStepKeyDown: (event: React.KeyboardEvent<HTMLElement>) => void;
 }
 
 /**
  * Provides information about the current step in Stepper.
  * @internal
  */
-const StepperContext = React.createContext<StepperContextType | null>(null);
+const StepperContext = React.createContext<StepperContextType | {}>({});
 
 if (process.env.NODE_ENV !== 'production') {
   StepperContext.displayName = 'StepperContext';
@@ -24,14 +32,8 @@ if (process.env.NODE_ENV !== 'production') {
  * Returns the current StepperContext or an empty object if no StepperContext
  * has been defined in the component tree.
  */
-export function useStepperContext(): StepperContextType | null {
-  const stepperContext = React.useContext(StepperContext);
-
-  if (stepperContext === null) {
-    throw new Error('useStepperContext() called outside of a StepperContext provider.');
-  }
-
-  return stepperContext;
+export function useStepperContext(): StepperContextType | {} {
+  return React.useContext(StepperContext);
 }
 
 export const StepperContextProvider = StepperContext.Provider;
---
+++ packages/mui-material/src/Stepper/utils/useRovingTabIndexFocus.test.tsx
@@ -0,0 +1,161 @@
+import * as React from 'react';
+import { expect } from 'chai';
+import { createRenderer, screen } from '@mui/internal-test-utils';
+import useRovingTabIndexFocus from './useRovingTabIndexFocus';
+
+function TestComponent({ initialIndex }: { initialIndex: number }) {
+  const { handleElementKeyDown, focusableIndex, registerElementRef, setFocusableIndex } =
+    useRovingTabIndexFocus({ initialFocusableIndex: initialIndex });
+
+  const button1Ref = React.useRef<HTMLButtonElement>(null);
+  const button2Ref = React.useRef<HTMLButtonElement>(null);
+  const button3Ref = React.useRef<HTMLButtonElement>(null);
+  const button4Ref = React.useRef<HTMLButtonElement>(null);
+
+  React.useLayoutEffect(() => {
+    registerElementRef(0, button1Ref, false);
+    registerElementRef(1, button2Ref, false);
+    registerElementRef(2, button3Ref, true);
+    registerElementRef(3, button4Ref, false);
+  }, [registerElementRef]);
+
+  return (
+    <div data-testid="container">
+      <button
+        ref={button1Ref}
+        tabIndex={focusableIndex === 0 ? 0 : -1}
+        onKeyDown={handleElementKeyDown}
+        onClick={() => setFocusableIndex(0)}
+        data-testid="button-1"
+      >
+        Button 1
+      </button>
+      <button
+        ref={button2Ref}
+        tabIndex={focusableIndex === 1 ? 0 : -1}
+        onKeyDown={handleElementKeyDown}
+        onClick={() => setFocusableIndex(1)}
+        data-testid="button-2"
+      >
+        Button 2
+      </button>
+      <button
+        ref={button3Ref}
+        tabIndex={focusableIndex === 2 ? 0 : -1}
+        onKeyDown={handleElementKeyDown}
+        onClick={() => setFocusableIndex(2)}
+        data-testid="button-3"
+        disabled
+      >
+        Button 3
+      </button>
+      <button
+        ref={button4Ref}
+        tabIndex={focusableIndex === 3 ? 0 : -1}
+        onKeyDown={handleElementKeyDown}
+        onClick={() => setFocusableIndex(3)}
+        data-testid="button-4"
+      >
+        Button 4
+      </button>
+    </div>
+  );
+}
+
+describe('useRovingTabIndexFocus', () => {
+  const { render } = createRenderer();
+
+  it('inital focusable index is set correctly', () => {
+    const initialIndex = 1;
+
+    render(<TestComponent initialIndex={initialIndex} />);
+
+    expect(screen.getByTestId('button-1').getAttribute('tabindex')).to.equal('-1');
+    expect(screen.getByTestId('button-2').getAttribute('tabindex')).to.equal('0');
+    expect(screen.getByTestId('button-3').getAttribute('tabindex')).to.equal('-1');
+    expect(screen.getByTestId('button-4').getAttribute('tabindex')).to.equal('-1');
+  });
+
+  it('setter function updates focusable index correctly', async () => {
+    const initialIndex = 0;
+
+    const { user } = render(<TestComponent initialIndex={initialIndex} />);
+
+    const button1 = screen.getByTestId('button-1');
+    const button2 = screen.getByTestId('button-2');
+    const button3 = screen.getByTestId('button-3');
+    const button4 = screen.getByTestId('button-4');
+
+    expect(button1.getAttribute('tabindex')).to.equal('0');
+    expect(button2.getAttribute('tabindex')).to.equal('-1');
+    expect(button3.getAttribute('tabindex')).to.equal('-1');
+    expect(button4.getAttribute('tabindex')).to.equal('-1');
+
+    await user.click(button2);
+
+    expect(button1.getAttribute('tabindex')).to.equal('-1');
+    expect(button2.getAttribute('tabindex')).to.equal('0');
+    expect(button3.getAttribute('tabindex')).to.equal('-1');
+    expect(button4.getAttribute('tabindex')).to.equal('-1');
+  });
+
+  it('keyboard navigation updates focusable index correctly', async () => {
+    const initialIndex = 0;
+
+    const { user } = render(<TestComponent initialIndex={initialIndex} />);
+
+    const button1 = screen.getByTestId('button-1');
+    const button2 = screen.getByTestId('button-2');
+    const button3 = screen.getByTestId('button-3');
+    const button4 = screen.getByTestId('button-4');
+
+    await user.click(button1);
+    await user.keyboard('{ArrowRight}');
+
+    expect(button1.getAttribute('tabindex')).to.equal('-1');
+    expect(button2.getAttribute('tabindex')).to.equal('0');
+    expect(button3.getAttribute('tabindex')).to.equal('-1');
+    expect(button4.getAttribute('tabindex')).to.equal('-1');
+    expect(button2).toHaveFocus();
+
+    await user.keyboard('{ArrowRight}');
+
+    expect(button1.getAttribute('tabindex')).to.equal('-1');
+    expect(button2.getAttribute('tabindex')).to.equal('-1');
+    expect(button3.getAttribute('tabindex')).to.equal('-1');
+    expect(button4.getAttribute('tabindex')).to.equal('0');
+    expect(button4).toHaveFocus();
+
+    await user.keyboard('{ArrowRight}');
+
+    expect(button1.getAttribute('tabindex')).to.equal('0');
+    expect(button2.getAttribute('tabindex')).to.equal('-1');
+    expect(button3.getAttribute('tabindex')).to.equal('-1');
+    expect(button4.getAttribute('tabindex')).to.equal('-1');
+    expect(button1).toHaveFocus();
+
+    await user.keyboard('{ArrowLeft}');
+
+    expect(button1.getAttribute('tabindex')).to.equal('-1');
+    expect(button2.getAttribute('tabindex')).to.equal('-1');
+    expect(button3.getAttribute('tabindex')).to.equal('-1');
+    expect(button4.getAttribute('tabindex')).to.equal('0');
+    expect(button4).toHaveFocus();
+
+    await user.keyboard('{ArrowLeft}');
+
+    expect(button1.getAttribute('tabindex')).to.equal('-1');
+    expect(button2.getAttribute('tabindex')).to.equal('0');
+    expect(button3.getAttribute('tabindex')).to.equal('-1');
+    expect(button4.getAttribute('tabindex')).to.equal('-1');
+    expect(button2).toHaveFocus();
+
+    await user.keyboard('{ArrowLeft}');
+
+    expect(button1.getAttribute('tabindex')).to.equal('0');
+    expect(button2.getAttribute('tabindex')).to.equal('-1');
+    expect(button3.getAttribute('tabindex')).to.equal('-1');
+    expect(button4.getAttribute('tabindex')).to.equal('-1');
+    expect(button1).toHaveFocus();
+  });
+});
