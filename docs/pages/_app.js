cf638121431a51d9dfc35751dae918190c343142
Connor Davis <mail@connordav.is>
2026-02-05 14:32:38 +0100

    [docs-infra] Add Cookie Banner and Analytics Provider (#47445)
    Signed-off-by: Connor Davis <mail@connordav.is>
    Co-authored-by: Nora <72460825+noraleonte@users.noreply.github.com>

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -19,6 +19,7 @@ import GoogleAnalytics from 'docs/src/modules/components/GoogleAnalytics';
 import { CodeCopyProvider } from '@mui/docs/CodeCopy';
 import { ThemeProvider } from 'docs/src/modules/components/ThemeContext';
 import { CodeVariantProvider } from 'docs/src/modules/utils/codeVariant';
+import { AnalyticsProvider } from 'docs/src/modules/components/AnalyticsProvider';
 import DocsStyledEngineProvider from 'docs/src/modules/utils/StyledEngineProvider';
 import createEmotionCache from 'docs/src/createEmotionCache';
 import findActivePage from 'docs/src/modules/utils/findActivePage';
@@ -316,10 +317,12 @@ function AppWrapper(props) {
           <CodeVariantProvider>
             <PageContext.Provider value={pageContextValue}>
               <ThemeProvider>
-                <DocsStyledEngineProvider cacheLtr={emotionCache}>
-                  {children}
-                  <GoogleAnalytics />
-                </DocsStyledEngineProvider>
+                <AnalyticsProvider>
+                  <DocsStyledEngineProvider cacheLtr={emotionCache}>
+                    {children}
+                    <GoogleAnalytics />
+                  </DocsStyledEngineProvider>
+                </AnalyticsProvider>
               </ThemeProvider>
             </PageContext.Provider>
           </CodeVariantProvider>
