43c9fc8ff0da3af18d3929a996a0f9681c3e17e1
Jose Quintas <juniorquintas@gmail.com>
2026-02-18 07:19:19 -0300

    revert irrelevant option.title change

--- packages/mui-joy/src/Autocomplete/Autocomplete.test.tsx
+++ packages/mui-joy/src/Autocomplete/Autocomplete.test.tsx
@@ -487,7 +487,7 @@ describe.skipIf(isJsdom())('Joy <Autocomplete />', () => {
                 const { key, ...tagProps } = getTagProps({ index });
                 return (
                   <Chip key={index} endDecorator={<ChipDelete key={key} {...tagProps} />}>
-                    {option}
+                    {option.title}
                   </Chip>
                 );
               })
@@ -1159,9 +1159,9 @@ describe.skipIf(isJsdom())('Joy <Autocomplete />', () => {
       }).toErrorDev([
         'MUI: The `getOptionLabel` method of Autocomplete returned undefined instead of a string',
         !strictModeDoubleLoggingSuppressed &&
-          'MUI: The `getOptionLabel` method of Autocomplete returned undefined instead of a string',
+        'MUI: The `getOptionLabel` method of Autocomplete returned undefined instead of a string',
         !strictModeDoubleLoggingSuppressed &&
-          'MUI: The `getOptionLabel` method of Autocomplete returned undefined instead of a string',
+        'MUI: The `getOptionLabel` method of Autocomplete returned undefined instead of a string',
         'MUI: The `getOptionLabel` method of Autocomplete returned undefined instead of a string',
         'MUI: The `getOptionLabel` method of Autocomplete returned undefined instead of a string',
         'MUI: The `getOptionLabel` method of Autocomplete returned undefined instead of a string',
