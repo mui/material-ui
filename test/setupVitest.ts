a4e67050847111a1f49ad825e71b1d5bcc8671a8
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2026-02-19 14:58:50 +0100

    [code-infra] Exclusively enable test mode in jsdom (#47812)

--- test/setupVitest.ts
+++ test/setupVitest.ts
@@ -1,5 +1,7 @@
 import setupVitest from '@mui/internal-test-utils/setupVitest';
 
-(globalThis as any).MUI_TEST_ENV = true;
+if (navigator.userAgent.includes('jsdom')) {
+  (globalThis as any).MUI_TEST_ENV = true;
+}
 
 setupVitest({ emotion: true });
