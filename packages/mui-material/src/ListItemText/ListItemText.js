6c6bc512e5ece5f13382857a00e031ea82f0f79c
Josh Kelley <joshkel@gmail.com>
2026-03-09 10:28:04 -0400

    Code review feedback
    From @ZeeshanTamboli, https://github.com/mui/material-ui/pull/47927/changes#r2904864949

--- packages/mui-material/src/ListItemText/ListItemText.js
+++ packages/mui-material/src/ListItemText/ListItemText.js
@@ -42,6 +42,7 @@ const ListItemTextRoot = styled('div', {
   minWidth: 0,
   marginTop: 4,
   marginBottom: 4,
+  // Combine this and the below selector once https://github.com/emotion-js/emotion/issues/3366 is solved
   [`.${typographyClasses.root}:where(& .${listItemTextClasses.primary})`]: {
     display: 'block',
   },
