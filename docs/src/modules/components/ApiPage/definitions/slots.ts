b76b61785a105ba5ba7da1d48b4e88d7ed097d4c
Brijesh Bittu <717550+brijeshb42@users.noreply.github.com>
2026-03-10 14:28:46 +0530

    [docs-infra] Migrate slots definitions to @mui/docs (2.3)

--- docs/src/modules/components/ApiPage/definitions/slots.ts
+++ docs/src/modules/components/ApiPage/definitions/slots.ts
@@ -1,57 +1,6 @@
-import { PropsTranslations, ComponentApiContent } from '@mui-internal/api-docs-builder';
-import { Translate } from '@mui/docs/i18n';
-import type { TableOfContentsParams } from '@mui/docs/ApiPage';
-
-export type SlotDefinition = {
-  className: string | null;
-  hash: string;
-  description?: string;
-  name: string;
-  defaultValue?: string;
-};
-
-export interface GetSlotsApiDefinitionsParams {
-  componentSlots: ComponentApiContent['slots'];
-  slotDescriptions: PropsTranslations['slotDescriptions'];
-  componentName: string;
-}
-
-export function getSlotsApiDefinitions(params: GetSlotsApiDefinitionsParams): SlotDefinition[] {
-  const { componentSlots, slotDescriptions, componentName } = params;
-
-  if (!componentSlots) {
-    return [];
-  }
-  return componentSlots.map(({ class: className, name, default: defaultValue }) => {
-    return {
-      description: slotDescriptions?.[name],
-      className,
-      name,
-      defaultValue,
-      hash: `${componentName}-css-${className ?? name}`,
-    };
-  });
-}
-
-export type GetCssToCParams = {
-  slots: SlotDefinition[];
-  t: Translate;
-  hash?: string;
-};
-
-export const getSlotsToc = ({ slots, t, hash }: GetCssToCParams): TableOfContentsParams[] =>
-  !slots || slots.length === 0
-    ? []
-    : [
-        {
-          text: t('api-docs.slots'),
-          hash: hash ?? 'slots',
-          children: [
-            ...slots.map(({ name, hash: slotHash }) => ({
-              text: name,
-              hash: slotHash,
-              children: [],
-            })),
-          ],
-        },
-      ];
+export { getSlotsApiDefinitions, getSlotsToc } from '@mui/docs/ApiPage/definitions/slots';
+export type {
+  SlotDefinition,
+  GetSlotsApiDefinitionsParams,
+  GetCssToCParams,
+} from '@mui/docs/ApiPage/definitions/slots';
