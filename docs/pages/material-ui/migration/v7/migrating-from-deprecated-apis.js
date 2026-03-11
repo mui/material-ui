33608bdacf44d694df6a795eacd0f165b9b37e9d
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2026-03-11 16:04:55 +0530

    fix docs

--- docs/pages/material-ui/migration/v7/migrating-from-deprecated-apis.js
+++ docs/pages/material-ui/migration/v7/migrating-from-deprecated-apis.js
@@ -1,5 +1,5 @@
 import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
-import * as pageProps from 'docs/data/material/migration/upgrade-to-v7/migrating-from-deprecated-apis-v7.md?muiMarkdown';
+import * as pageProps from 'docs/data/material/migration/upgrade-to-v7/migrating-from-deprecated-apis.md?muiMarkdown';
 
 export default function Page() {
   return <MarkdownDocs {...pageProps} />;
