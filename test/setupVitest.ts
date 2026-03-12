ffaecf7f6a5398b31c4416984b5c652fbab2bbf7
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2026-03-12 15:15:51 +0200

    bring back the mock

--- test/setupVitest.ts
+++ test/setupVitest.ts
@@ -1,4 +1,4 @@
-// import { beforeAll, afterAll } from 'vitest';
+import { beforeAll, afterAll } from 'vitest';
 import setupVitest from '@mui/internal-test-utils/setupVitest';
 
 setupVitest({ emotion: true });
@@ -6,25 +6,25 @@ setupVitest({ emotion: true });
 // In Firefox, calling focus() with arguments (e.g. focusOptions) fails silently,
 // which causes focus-visible related tests to fail as a consequence.
 // This override is only applied in a browser environment running Firefox.
-// if (typeof globalThis.navigator !== 'undefined' && !navigator.userAgent.includes('jsdom')) {
-//   const isFirefox = navigator.userAgent.toLowerCase().includes('firefox');
+if (typeof globalThis.navigator !== 'undefined' && !navigator.userAgent.includes('jsdom')) {
+  const isFirefox = navigator.userAgent.toLowerCase().includes('firefox');
 
-//   if (isFirefox) {
-//     const originalFocus = HTMLElement.prototype.focus;
+  if (isFirefox) {
+    const originalFocus = HTMLElement.prototype.focus;
 
-//     beforeAll(() => {
-//       Object.defineProperty(HTMLElement.prototype, 'focus', {
-//         configurable: true,
-//         value: function focusWithoutArguments() {
-//           originalFocus.call(this); // always call without arguments
-//         },
-//       });
-//     });
+    beforeAll(() => {
+      Object.defineProperty(HTMLElement.prototype, 'focus', {
+        configurable: true,
+        value: function focusWithoutArguments() {
+          originalFocus.call(this); // always call without arguments
+        },
+      });
+    });
 
-//     afterAll(() => {
-//       Object.defineProperty(HTMLElement.prototype, 'focus', {
-//         value: originalFocus,
-//       });
-//     });
-//   }
-// }
+    afterAll(() => {
+      Object.defineProperty(HTMLElement.prototype, 'focus', {
+        value: originalFocus,
+      });
+    });
+  }
+}
