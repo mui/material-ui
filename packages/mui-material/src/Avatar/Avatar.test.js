0334cc154288237a8557ecc3207491d74538ea68
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2026-03-13 16:26:02 +0200

    remove another test

--- packages/mui-material/src/Avatar/Avatar.test.js
+++ packages/mui-material/src/Avatar/Avatar.test.js
@@ -53,15 +53,6 @@ describe('<Avatar />', () => {
       expect(img).to.have.attribute('src', '/fake.png');
     });
 
-    it('should be able to add more props to the image', () => {
-      // TODO: remove this test in v7
-      const onError = spy();
-      const { container } = render(<Avatar src="/fake.png" imgProps={{ onError }} />);
-      const img = container.querySelector('img');
-      fireEvent.error(img);
-      expect(onError.callCount).to.equal(1);
-    });
-
     it('should be able to add more props to the img slot', () => {
       const onError = spy();
       const { container } = render(<Avatar src="/fake.png" slotProps={{ img: { onError } }} />);
