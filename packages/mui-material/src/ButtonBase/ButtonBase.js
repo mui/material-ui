1ddea1388873b8169e903f2ce034137325b79e3b
Albert Yu <albert@albertyu.co>
2026-03-15 16:02:40 +0800

    Fix native button check

--- packages/mui-material/src/ButtonBase/ButtonBase.js
+++ packages/mui-material/src/ButtonBase/ButtonBase.js
@@ -192,7 +192,16 @@ const ButtonBase = React.forwardRef(function ButtonBase(inProps, ref) {
 
   const isNonNativeButton = () => {
     const button = buttonRef.current;
-    return component && component !== 'button' && !(button.tagName === 'A' && button.href);
+
+    if (!button) {
+      return component && component !== 'button';
+    }
+
+    if (button.tagName === 'BUTTON') {
+      return false;
+    }
+
+    return !(button.tagName === 'A' && button.href);
   };
 
   const handleKeyDown = useEventCallback((event) => {
