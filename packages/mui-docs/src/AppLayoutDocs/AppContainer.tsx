78fa41c45824d591f2ae5c01d5c06e14a4cc89af
Brijesh Bittu <717550+brijeshb42@users.noreply.github.com>
2026-03-17 18:12:27 +0530

    [docs-infra] Migrate AppContainer to @mui/docs (6.2)

---
+++ packages/mui-docs/src/AppLayoutDocs/AppContainer.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Container, { ContainerProps } from '@mui/material/Container';
+
+const StyledAppContainer = styled(Container)(({ theme }) => {
+  return {
+    paddingTop: `calc(var(--MuiDocs-header-height) + ${theme.spacing(4)})`,
+    // We're mostly hosting text content so max-width by px does not make sense considering font-size is system-adjustable.
+    // 105ch ≈ 930px
+    fontFamily: 'Arial',
+    maxWidth: '105ch',
+    [theme.breakpoints.up('lg')]: {
+      paddingLeft: theme.spacing(8),
+      paddingRight: theme.spacing(8),
+    },
+  };
+});
+
+export default function AppContainer(props: ContainerProps) {
+  return <StyledAppContainer id="main-content" maxWidth={false} {...props} />;
+}
