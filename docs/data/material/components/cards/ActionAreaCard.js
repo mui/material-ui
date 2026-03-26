0f2797b7eb3d2eb38a2bb37a9699522b88d8d6e6
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2026-03-26 12:52:05 +0200

    revert actionareacard

--- docs/data/material/components/cards/ActionAreaCard.js
+++ docs/data/material/components/cards/ActionAreaCard.js
@@ -1,69 +1,29 @@
-import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import CardHeader from '@mui/material/CardHeader';
-import CardActions from '@mui/material/CardActions';
-import IconButton from '@mui/material/IconButton';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
+import CardActionArea from '@mui/material/CardActionArea';
 
 export default function ActionAreaCard() {
   return (
-    <React.Fragment>
-      <Card href="#primary-action" sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
         <CardMedia
           component="img"
           height="140"
           image="/static/images/cards/contemplative-reptile.jpg"
           alt="green iguana"
         />
-        <CardHeader title="Lizard" />
         <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
           <Typography variant="body2" sx={{ color: 'text.secondary' }}>
             Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica
           </Typography>
         </CardContent>
-        <CardActions>
-          <IconButton
-            aria-label="add to favorites"
-            onClick={() => alert('Favorite clicked')}
-          >
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="share" onClick={() => alert('Share clicked')}>
-            <ShareIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
-      <Card onClick={() => alert('Card clicked')} sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardHeader title="Lizard" />
-        <CardContent>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <IconButton
-            aria-label="add to favorites"
-            onClick={() => alert('Favorite clicked')}
-          >
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="share" onClick={() => alert('Share clicked')}>
-            <ShareIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
-    </React.Fragment>
+      </CardActionArea>
+    </Card>
   );
 }
