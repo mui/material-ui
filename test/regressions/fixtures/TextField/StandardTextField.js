98c39fc083dbb0d33280c343f4b799ef10a8c2ec
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2026-03-31 11:32:28 +0300

    more tests

--- test/regressions/fixtures/TextField/StandardTextField.js
+++ test/regressions/fixtures/TextField/StandardTextField.js
@@ -4,10 +4,18 @@ import TextField from '@mui/material/TextField';
 export default function StandardTextField() {
   return (
     <div>
-      <TextField label="Standard" variant="standard" />
-      <TextField label="Standard disabled" variant="standard" disabled />
-      <TextField label="Standard error" variant="standard" error />
-      <TextField label="Standard required" variant="standard" required />
+      <div>
+        <TextField label="Standard" variant="standard" />
+        <TextField label="Standard disabled" variant="standard" disabled />
+        <TextField label="Standard error" variant="standard" error />
+        <TextField label="Standard required" variant="standard" required />
+      </div>
+      <div>
+        <TextField value="Material" label="Standard" variant="standard" />
+        <TextField value="Material" label="Standard disabled" variant="standard" disabled />
+        <TextField value="Material" label="Standard error" variant="standard" error />
+        <TextField value="Material" label="Standard required" variant="standard" required />
+      </div>
     </div>
   );
 }
