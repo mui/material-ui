4f41110a6430a40d5d5aece0b239b45f6a187e11
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2026-04-01 11:38:42 +0300

    more changes after removing prop support

--- docs/src/pages/premium-themes/onepirate/modules/views/AppFooter.js
+++ docs/src/pages/premium-themes/onepirate/modules/views/AppFooter.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
 import Typography from '../components/Typography';
 import TextField from '../components/TextField';
@@ -51,8 +52,7 @@ export default function AppFooter() {
       <Container sx={{ my: 8, display: 'flex' }}>
         <Grid container spacing={5}>
           <Grid size={{ xs: 6, sm: 4, md: 3 }}>
-            <Grid
-              container
+            <Stack
               direction="column"
               spacing={2}
               sx={{ justifyContent: 'flex-end', height: 120 }}
@@ -71,7 +71,7 @@ export default function AppFooter() {
               <Grid>
                 <Copyright />
               </Grid>
-            </Grid>
+            </Stack>
           </Grid>
           <Grid size={{ xs: 6, sm: 4, md: 2 }}>
             <Typography variant="h6" marked="left" gutterBottom>
