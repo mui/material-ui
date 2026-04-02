dfa71f5b2aaba8a67978ecbc385f4f821fca69d4
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2026-04-02 10:59:00 +0300

    pnpm docs:typescript:formatted

--- docs/data/material/components/progress/CircularWithValueLabel.js
+++ docs/data/material/components/progress/CircularWithValueLabel.js
@@ -39,7 +39,7 @@ function CircularProgressWithLabel(props) {
 CircularProgressWithLabel.propTypes = {
   /**
    * The value of the progress indicator for the determinate variant.
-   * Value between 0 and 100.
+   * Value between `minValue` and `maxValue`.
    * @default 0
    */
   value: PropTypes.number.isRequired,
