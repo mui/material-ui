0274aad06ab7c8d25928133ac40f51e3b567a422
mnajdova <mnajdova@gmail.com>
2026-04-13 11:50:09 +0300

    CI fixes

--- docs/data/docs/pages.ts
+++ docs/data/docs/pages.ts
@@ -2,7 +2,6 @@ import type { MuiPage } from '@mui/internal-core-docs/MuiPage';
 import standardNavIcons from 'docs/src/modules/components/AppNavIcons';
 
 const pages: readonly MuiPage[] = [
-  { pathname: 'https://mui.com/versions/' },
   {
     pathname: 'https://mui.com/store/',
     title: 'Templates',
