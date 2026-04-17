2dce767cdf56c788a3bd847026af4f91fa192408
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2026-04-17 10:13:11 +0300

    changed the error and disabled order

--- packages/mui-material/src/FormLabel/FormLabel.js
+++ packages/mui-material/src/FormLabel/FormLabel.js
@@ -62,16 +62,16 @@ export const FormLabelRoot = styled('label', {
       {
         props: {},
         style: {
-          [`&.${formLabelClasses.disabled}`]: {
-            color: (theme.vars || theme).palette.text.disabled,
-            '@media (forced-colors: active)': {
-              color: 'GrayText',
-            },
-          },
           [`&.${formLabelClasses.error}`]: {
             color: (theme.vars || theme).palette.error.main,
             '@media (forced-colors: active)': {
               color: 'mark',
+            },
+          },
+          [`&.${formLabelClasses.disabled}`]: {
+            color: (theme.vars || theme).palette.text.disabled,
+            '@media (forced-colors: active)': {
+              color: 'GrayText',
             },
           },
         },
