25ad1774c280827cf7f1bd59b9951e4e8ac71762
Janpot <2109932+Janpot@users.noreply.github.com>
2026-04-17 11:36:53 +0200

    [docs] Apply prettier to RowRadioButtonsGroup

--- docs/data/material/components/radio-buttons/RowRadioButtonsGroup.tsx
+++ docs/data/material/components/radio-buttons/RowRadioButtonsGroup.tsx
@@ -10,11 +10,7 @@ export default function RowRadioButtonsGroup() {
   return (
     <FormControl>
       <FormLabel id={`${id}-label`}>Gender</FormLabel>
-      <RadioGroup
-        row
-        aria-labelledby={`${id}-label`}
-        name="row-radio-buttons-group"
-      >
+      <RadioGroup row aria-labelledby={`${id}-label`} name="row-radio-buttons-group">
         <FormControlLabel value="female" control={<Radio />} label="Female" />
         <FormControlLabel value="male" control={<Radio />} label="Male" />
         <FormControlLabel value="other" control={<Radio />} label="Other" />
