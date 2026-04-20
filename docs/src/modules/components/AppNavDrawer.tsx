4b1ef3e086c3f4c7c2d607c419cf26be2e2a9721
Marija Najdova <mnajdova@gmail.com>
2026-04-20 09:51:59 +0200

    Apply suggestion from @mnajdova
    Signed-off-by: Marija Najdova <mnajdova@gmail.com>

--- docs/src/modules/components/AppNavDrawer.tsx
+++ docs/src/modules/components/AppNavDrawer.tsx
@@ -393,7 +393,6 @@ export default function AppNavDrawer(props: AppNavDrawerProps) {
       if (!versions?.length) {
         return null;
       }
-      console.log(versions);
       const currentVersion = versions.find((version) => version.current) || versions[0];
       return (
         <React.Fragment>
