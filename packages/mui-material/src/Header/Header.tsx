8db96929c7a80e294d8f83de6ca9e4cba9100e7b
mnajdova <mnajdova@gmail.com>
2026-04-24 23:07:06 +0200

    Refactor Header to use global CSS

--- packages/mui-material/src/Header/Header.tsx
+++ packages/mui-material/src/Header/Header.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import clsx from 'clsx';
 import { styled } from '@mui/system';
-import classes from './Header.module.css';
+import './Header.css';
 
 const HeaderRoot = styled('h1')({});
 
 function Header(props: React.ComponentPropsWithRef<'h1'>) {
-  return (
-    <HeaderRoot {...props} className={clsx('MuiHeader-root', classes.root, props.className)} />
-  );
+  return <HeaderRoot {...props} className={clsx('MuiHeader-root', props.className)} />;
 }
 
 export default Header;
