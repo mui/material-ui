d043c3fcd6201efcd74d438fa1e8705e67712f58
dav-is <mail@connordav.is>
2026-05-07 12:39:54 -0400

    Remove js

--- docs/data/material/components/buttons/demos/basic/index.js
+++
@@ -1,8 +0,0 @@
-import { createDemo } from '@mui/internal-core-docs/utils';
-import ClientProvider from './client';
-
-import BasicButtons from './BasicButtons';
-
-export default createDemo(import.meta.url, BasicButtons, {
-  ClientProvider,
-});
