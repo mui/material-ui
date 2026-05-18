2ad4dc15f13888ba24573e5d67fcce312d87df60
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2026-05-18 11:08:03 +0300

    add CssVarsInjector to the skipComponent regex

--- packages-internal/api-docs-builder-core/materialUi/projectSettings.ts
+++ packages-internal/api-docs-builder-core/materialUi/projectSettings.ts
@@ -39,7 +39,9 @@ export const projectSettings: ProjectSettings = {
   translationLanguages: LANGUAGES,
   skipComponent(filename: string) {
     return (
-      filename.match(/(ThemeProvider|CssVarsProvider|DefaultPropsProvider|SliderEmotion)/) !== null
+      filename.match(
+        /(ThemeProvider|CssVarsProvider|DefaultPropsProvider|SliderEmotion|CssVarsInjector)/,
+      ) !== null
     );
   },
   translationPagesDirectory: 'docs/translations/api-docs',
