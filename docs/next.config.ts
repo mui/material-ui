7ab292853e94f46e2cae57c084d045dfb57a738d
dav-is <mail@connordav.is>
2026-05-18 18:59:44 -0400

    add transforms

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -229,6 +229,7 @@ export default withDocsInfra({
                   // shift highlighted/focus frames left when collapsed.
                   emphasisOptions: { emitFrameIndent: true },
                   requireClient: '@mui/internal-core-docs/utils/createDemoClient',
+                  transformTypescriptToJavascript: true,
                 },
               },
             ],
