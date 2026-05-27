74a617750edcc6e8e75bff01a386aae7eafb4e97
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2026-05-27 10:32:29 +0200

    [code-infra] Convert @mui/private-theming to TypeScript (#48565)

--- packages/mui-private-theming/src/ThemeProvider/ThemeProvider.d.ts
+++
@@ -1,9 +0,0 @@
-import { DefaultTheme } from '../defaultTheme';
-
-export interface ThemeProviderProps<Theme = DefaultTheme> {
-  children?: React.ReactNode;
-  theme: Partial<Theme> | ((outerTheme: Theme) => Theme);
-}
-export default function ThemeProvider<T = DefaultTheme>(
-  props: ThemeProviderProps<T>,
-): React.ReactElement<ThemeProviderProps<T>>;
--- packages/mui-private-theming/src/useTheme/ThemeContext.ts
+++ packages/mui-private-theming/src/useTheme/ThemeContext.ts
@@ -1,7 +1,7 @@
 'use client';
 import * as React from 'react';
 
-const ThemeContext = React.createContext(null);
+const ThemeContext = React.createContext<any>(null);
 
 if (process.env.NODE_ENV !== 'production') {
   ThemeContext.displayName = 'ThemeContext';
