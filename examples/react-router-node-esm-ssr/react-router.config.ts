c5ca3e915f2d227ccab7695d8adaf26d7eef72fa
Albert Yu <albert@albertyu.co>
2026-06-10 00:05:42 +0800

    fix RTG import in ESM

---
+++ examples/react-router-node-esm-ssr/react-router.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from '@react-router/dev/config';
+
+export default {
+  ssr: true,
+  future: {
+    v8_middleware: true,
+    v8_passThroughRequests: true,
+    v8_splitRouteModules: true,
+    v8_trailingSlashAwareDataRequests: true,
+    v8_viteEnvironmentApi: true,
+  },
+} satisfies Config;
