90aef5f59695faaed739e8a394281a03c5741aed
Albert Yu <albert@albertyu.co>
2026-06-18 02:38:25 +0800

    remove temp examples

--- examples/react-router-node-esm-ssr/app/routes.ts
+++
@@ -1,3 +0,0 @@
-import { type RouteConfig, index } from '@react-router/dev/routes';
-
-export default [index('routes/home.tsx')] satisfies RouteConfig;
--- examples/react-router-node-esm-ssr/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { reactRouter } from '@react-router/dev/vite';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  plugins: [reactRouter()],
-  ssr: {
-    external: ['@mui/material'],
-  },
-});
