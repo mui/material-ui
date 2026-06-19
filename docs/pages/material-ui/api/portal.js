cd1d0fecc0f3ca1c758d264f86a648d68dc1d629
Brijesh Bittu <717550+brijeshb42@users.noreply.github.com>
2026-06-19 16:43:48 +0530

    Regenerate files

--- docs/pages/material-ui/api/portal.js
+++ docs/pages/material-ui/api/portal.js
@@ -1,16 +1,8 @@
 import * as React from 'react';
 import { ApiPage } from '@mui/internal-core-docs/ApiPage';
-import { mapApiPageTranslation } from '@mui/internal-core-docs/mapApiPageTranslations';
-import translation from 'docs/translations/api-docs/portal/portal.json';
+import descriptions from 'docs/translations/api-docs/portal/portal.json';
 import jsonPageContent from './portal.json';
 
-export default function Page(props) {
-  const { descriptions } = props;
+export default function Page() {
   return <ApiPage descriptions={descriptions} pageContent={jsonPageContent} />;
-}
-
-export async function getStaticProps() {
-  const descriptions = mapApiPageTranslation(translation);
-
-  return { props: { descriptions } };
 }
