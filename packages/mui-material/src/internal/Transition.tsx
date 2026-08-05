eeaa208336ea1976f51cf9113d4d9cb00316f7de
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2026-08-05 18:07:45 +0530

    Add fix

--- packages/mui-material/src/internal/Transition.tsx
+++ packages/mui-material/src/internal/Transition.tsx
@@ -410,14 +410,14 @@ function Transition(props: InternalTransitionProps): React.ReactNode {
       if (statusChanged) {
         current.onEntering?.(isAppearingRef.current);
       }
-      if (nextCallbackRef.current === null) {
+      if (nextCallbackRef.current === null && statusRef.current === status) {
         scheduleTransitionEnd('entered', 'entering');
       }
     } else if (status === 'exiting') {
       if (statusChanged) {
         current.onExiting?.();
       }
-      if (nextCallbackRef.current === null) {
+      if (nextCallbackRef.current === null && statusRef.current === status) {
         scheduleTransitionEnd('exited', 'exiting');
       }
     } else if (status === 'entered' && statusChanged) {
