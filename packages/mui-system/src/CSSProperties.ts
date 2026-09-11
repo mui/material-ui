0431e56c7a0eafe83d187f56a01cc07013abb69c
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2026-09-11 12:32:37 +0530

    Remove CSSProperties.ts file which is dead code

--- packages/mui-system/src/CSSProperties.ts
+++
@@ -1,8 +0,0 @@
-import type * as CSS from 'csstype';
-
-/**
- * All non-vendor-prefixed CSS properties. (Also allows `number` in order to support CSS-in-JS libs,
- * since they are converted to `px`.)
- */
-export interface CSSProperties
-  extends CSS.StandardProperties<number | string>, CSS.SvgProperties<number | string> {}
