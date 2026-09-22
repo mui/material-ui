a74afc37c0977a1ab45c4c117529c037bf084d72
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2026-09-22 14:04:48 +0200

    [material] Convert colors to TypeScript (#49180)

--- packages/mui-material/src/colors/common.ts
+++ packages/mui-material/src/colors/common.ts
@@ -1,15 +1,15 @@
 /**
  * ![common black](https://mui.com/static/colors-preview/common-black-24x24.png) ![common white](https://mui.com/static/colors-preview/common-white-24x24.png)
  */
-declare const common: {
+const common = {
   /**
    * Preview: ![common black](https://mui.com/static/colors-preview/common-black-24x24.png)
    */
-  black: '#000';
+  black: '#000',
   /**
    * Preview: ![common white](https://mui.com/static/colors-preview/common-white-24x24.png)
    */
-  white: '#fff';
-};
+  white: '#fff',
+} as const;
 
 export default common;
