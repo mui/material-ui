cc3fb10ff929b33c41b7f175b968c47c83db3ecf
Maksym <leus.maximus@gmail.com>
2026-09-22 16:06:45 +0300

    [docs][system] Avoid `any` when spreading the `sx` prop (#49083)
    Signed-off-by: Maksym <leus.maximus@gmail.com>
    Co-authored-by: sai chand <60743144+sai6855@users.noreply.github.com>
    Co-authored-by: ZeeshanTamboli <zeeshan.tamboli@gmail.com>

--- docs/data/system/flexbox/JustifyContent.tsx
+++ docs/data/system/flexbox/JustifyContent.tsx
@@ -1,7 +1,7 @@
 import Box, { BoxProps } from '@mui/material/Box';
 
 function Item(props: BoxProps) {
-  const { sx, ...other } = props;
+  const { sx = [], ...other } = props;
   return (
     <Box
       sx={[
@@ -21,7 +21,7 @@ function Item(props: BoxProps) {
             borderColor: 'grey.800',
           }),
         }),
-        ...(Array.isArray(sx) ? sx : [sx]),
+        ...[sx].flat(),
       ]}
       {...other}
     />
