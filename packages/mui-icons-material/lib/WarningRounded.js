f7fbcb09279713be73a7d1f0700c4c9417e95b23
Marcel Bechtold <57720296+itd-mb@users.noreply.github.com>
2026-09-24 12:19:01 +0200

    [icons] Make sure WarningRounded icon is vertically centered within its viewBox (#49043)
    Co-authored-by: Marcel Bechtold <itd-mb@users.noreply.github.com>
    Co-authored-by: Zeeshan Tamboli <zeeshan.tamboli@gmail.com>

--- packages/mui-icons-material/lib/WarningRounded.js
+++ packages/mui-icons-material/lib/WarningRounded.js
@@ -9,5 +9,5 @@ exports.default = void 0;
 var _createSvgIcon = _interopRequireDefault(require("./utils/createSvgIcon"));
 var _jsxRuntime = require("react/jsx-runtime");
 var _default = exports.default = (0, _createSvgIcon.default)(/*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
-  d: "M4.47 21h15.06c1.54 0 2.5-1.67 1.73-3L13.73 4.99c-.77-1.33-2.69-1.33-3.46 0L2.74 18c-.77 1.33.19 3 1.73 3M12 14c-.55 0-1-.45-1-1v-2c0-.55.45-1 1-1s1 .45 1 1v2c0 .55-.45 1-1 1m1 4h-2v-2h2z"
+  d: "M4.47 20.5h15.06c1.54 0 2.5-1.67 1.73-3L13.73 4.49c-.77-1.33-2.69-1.33-3.46 0L2.74 17.5c-.77 1.33.19 3 1.73 3M12 13.5c-.55 0-1-.45-1-1v-2c0-.55.45-1 1-1s1 .45 1 1v2c0 .55-.45 1-1 1m1 4h-2v-2h2z"
 }), 'WarningRounded');
